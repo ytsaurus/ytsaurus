44642fbb1db185e545d8fd2ea3f8003703bd3fc5
sandello <sandello@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2011-07-12 22:40:08 +0000

    Added Doxygen for debugging facilities.
    git-svn-id: svn+ssh://svn.yandex.ru/yt/trunk@930 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- yt/ytlib/misc/backtrace.h
+++ yt/ytlib/misc/backtrace.h
@@ -6,9 +6,25 @@ namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//! Prints current call stack to the specified output.
+/*!
+ * Currently, this method works only with GCC.
+ * For your convenience, use macro PrintCallStack() instead of this one.
+ *
+ * \param output Output stream.
+ * \param file Auxiliary information about caller file.
+ * \param line Auxiliary information about caller line.
+ */
 void PrintCallStackAux(TOutputStream& output, const char* file, int line);
+
+//! Prints current call stack to the standard error.
+//! \see PrintCallStackAux
 #define PrintCallStack() PrintCallStackAux(Cerr, __FILE__, __LINE__)
 
+//! Breaks program execution and triggers debugger.
+/*!
+ * Currently, this method works only with GCC.
+ */
 void Break();
 
 ////////////////////////////////////////////////////////////////////////////////
