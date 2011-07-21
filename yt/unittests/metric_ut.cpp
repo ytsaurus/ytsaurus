4ded9bc10b3d81d574611dfa9ebf81621be103ec
panin <panin@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2011-07-21 08:56:52 +0000

    unittest for metric
    git-svn-id: svn+ssh://svn.yandex.ru/yt/trunk@1016 41d65440-b5be-11dd-afe3-b2e846d9b4f8

---
+++ yt/unittests/metric_ut.cpp
@@ -0,0 +1,80 @@
+#include "../ytlib/misc/metric.h"
+
+#include "framework/framework.h"
+
+namespace NYT {
+namespace NUnitTest {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TMetricTest : public ::testing::Test {
+};
+
+TEST_F(TMetricTest, ZeroValues)
+{
+    TMetric metric(50, 100, 10);
+
+    EXPECT_EQ(metric.GetMean(), 0);
+    EXPECT_EQ(metric.GetStd(), 0);
+}
+
+
+TEST_F(TMetricTest, OneValue)
+{
+    TMetric metric(50, 100, 10);
+    metric.AddValue(75.);
+
+    EXPECT_DOUBLE_EQ(metric.GetMean(), 75);
+    EXPECT_DOUBLE_EQ(metric.GetStd(), 0);
+}
+
+TEST_F(TMetricTest, ManyValues)
+{
+    TMetric metric(1, 2, 10);
+
+    // from random import randint
+    // for s in ["x.push_back(%.3lf);" % uniform(0, 4) for i in xrange(10)]:print s
+
+    yvector<double> x;
+    x.push_back(1.907);
+    x.push_back(2.259);
+    x.push_back(3.374);
+    x.push_back(0.313);
+    x.push_back(1.125);
+    x.push_back(2.751);
+    x.push_back(0.715);
+    x.push_back(1.467);
+    x.push_back(3.252);
+    x.push_back(1.986);
+
+
+    for (int i = 0; i < x.ysize(); ++i) {
+        metric.AddValue(x[i]);
+    }
+
+    double sum = 0;
+    for(int i = 0; i < x.ysize(); ++i) {
+        sum += x[i];
+    }
+    double mean = sum / x.size();
+
+    double sumDeltaSq = 0;
+    for (int i = 0; i < x.ysize(); ++i) {
+        double delta = (x[i] - mean);
+        sumDeltaSq += delta * delta;
+    }
+    double std = sqrt(sumDeltaSq / x.size());
+
+    double eps = 1e-10;
+    EXPECT_TRUE( fabs(metric.GetMean()- mean) < eps);
+    EXPECT_TRUE( fabs(metric.GetStd() - std) < eps);
+}
+
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NUnitTest
+} // namespace NYT
+
