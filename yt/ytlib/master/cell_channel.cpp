2f80ea929a3adc94583e93479c89243073c9e5fb
babenko <babenko@ytsaurus.tech>
2011-07-22 19:54:17 +0000

    Missing files added
    git-svn-id: svn+ssh://svn.yandex.ru/yt/trunk@1040 41d65440-b5be-11dd-afe3-b2e846d9b4f8

---
+++ yt/ytlib/master/cell_channel.cpp
@@ -0,0 +1,121 @@
+#include "cell_channel.h"
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TCellChannel::TCellChannel(const TLeaderLookup::TConfig& config)
+    : LeaderLookup(new TLeaderLookup(config))
+    , State(EState::NotConnected)
+{ }
+
+TAsyncResult<TVoid>::TPtr TCellChannel::Send(
+    TIntrusivePtr<NRpc::TClientRequest> request,
+    TIntrusivePtr<NRpc::TClientResponse> response,
+    TDuration timeout)
+{
+    return GetChannel()->Apply(FromMethod(
+        &TCellChannel::OnGotChannel,
+        TPtr(this),
+        request,
+        response,
+        timeout));
+}
+
+TAsyncResult<TVoid>::TPtr TCellChannel::OnGotChannel(
+    NRpc::IChannel::TPtr channel,
+    NRpc::TClientRequest::TPtr request,
+    NRpc::TClientResponse::TPtr response,
+    TDuration timeout)
+{
+    if (~channel == NULL) {
+        NRpc::TRequestId requestId = NRpc::TRequestId::Create();
+        response->SetRequestId(requestId);
+        response->OnAcknowledgement(NBus::IBus::ESendResult::Failed);
+        return new TAsyncResult<TVoid>(TVoid());
+    }
+
+    return
+        channel
+        ->Send(request, response, timeout)
+        ->Apply(FromMethod(
+            &TCellChannel::OnResponseReady,
+            TPtr(this),
+            response));
+}
+
+NYT::TVoid TCellChannel::OnResponseReady(
+    TVoid,
+    NRpc::TClientResponse::TPtr response)
+{
+    NRpc::EErrorCode errorCode = response->GetErrorCode();
+    if (errorCode == NRpc::EErrorCode::TransportError ||
+        errorCode == NRpc::EErrorCode::Unavailable)
+    {
+        TGuard<TSpinLock> guard(SpinLock);
+        State = EState::Failed;
+        LookupResult = NULL;
+        Channel = NULL;
+    }
+    return TVoid();
+}
+
+TAsyncResult<NRpc::IChannel::TPtr>::TPtr TCellChannel::GetChannel()
+{
+    TGuard<TSpinLock> guard(SpinLock);
+    switch (State) {
+        case EState::NotConnected:
+        case EState::Failed:
+            YASSERT(~LookupResult == NULL);
+            YASSERT(~Channel == NULL);
+            State = EState::Connecting;
+            LookupResult = LeaderLookup->GetLeader();
+            return LookupResult->Apply(FromMethod(
+                &TCellChannel::OnFirstLookupResult,
+                TPtr(this)));
+
+        case EState::Connected:
+            YASSERT(~LookupResult == NULL);
+            YASSERT(~Channel != NULL);
+            return new TAsyncResult<NRpc::IChannel::TPtr>(~Channel);
+
+        case EState::Connecting:
+            YASSERT(~LookupResult != NULL);
+            YASSERT(~Channel == NULL);
+            return LookupResult->Apply(FromMethod(
+                &TCellChannel::OnSecondLookupResult,
+                TPtr(this)));
+
+        default:
+            YASSERT(false);
+            return NULL;
+    }
+}
+
+TAsyncResult<NRpc::IChannel::TPtr>::TPtr TCellChannel::OnFirstLookupResult(
+    TLeaderLookup::TResult result)
+{
+    TGuard<TSpinLock> guard(SpinLock);
+
+    YASSERT(State == EState::Connecting);
+
+    if (result.Id == InvalidMasterId) {
+        State = EState::Failed;
+        return new TAsyncResult<NRpc::IChannel::TPtr>(NULL);
+    }
+
+    State = EState::Connected;
+    Channel = new NRpc::TChannel(result.Address);
+    LookupResult = NULL;
+    return new TAsyncResult<NRpc::IChannel::TPtr>(~Channel);
+}
+
+TAsyncResult<NRpc::IChannel::TPtr>::TPtr TCellChannel::OnSecondLookupResult(
+    TLeaderLookup::TResult)
+{
+    return GetChannel();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
---
+++ yt/ytlib/master/cell_channel.h
@@ -0,0 +1,58 @@
+#pragma once
+
+#include "../election/leader_lookup.h"
+
+#include "../rpc/client.h"
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TCellChannel
+    : public NRpc::IChannel
+{
+public:
+    typedef TIntrusivePtr<TCellChannel> TPtr;
+
+    TCellChannel(const TLeaderLookup::TConfig& config);
+    
+    virtual TAsyncResult<TVoid>::TPtr Send(
+        TIntrusivePtr<NRpc::TClientRequest> request,
+        TIntrusivePtr<NRpc::TClientResponse> response,
+        TDuration timeout);
+
+private:
+    DECLARE_ENUM(EState,
+        (NotConnected)
+        (Connecting)
+        (Connected)
+        (Failed)
+    );
+
+    TAsyncResult<TVoid>::TPtr OnGotChannel(
+        NRpc::IChannel::TPtr channel,
+        NRpc::TClientRequest::TPtr request,
+        NRpc::TClientResponse::TPtr response,
+        TDuration timeout);
+
+    TVoid OnResponseReady(
+        TVoid,
+        NRpc::TClientResponse::TPtr response);
+  
+    TAsyncResult<NRpc::IChannel::TPtr>::TPtr GetChannel();
+
+    TAsyncResult<NRpc::IChannel::TPtr>::TPtr OnFirstLookupResult(TLeaderLookup::TResult result);
+    TAsyncResult<NRpc::IChannel::TPtr>::TPtr OnSecondLookupResult(TLeaderLookup::TResult);
+
+
+    TSpinLock SpinLock;
+    TLeaderLookup::TPtr LeaderLookup;
+    EState State;
+    TLeaderLookup::TLookupResult::TPtr LookupResult;
+    NRpc::TChannel::TPtr Channel;
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
