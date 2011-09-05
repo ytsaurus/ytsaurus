a6b909d258ba088d777bd177626072039c234864
Pavel Sushin <psushin@ytsaurus.tech>
2011-09-05 18:56:35 +0400

    Implemented remote chunk reader

---
+++ yt/ytlib/chunk_client/remote_chunk_reader.cpp
@@ -0,0 +1,46 @@
+﻿#include "remote_chunk_reader.h"
+#include "../actions/action_util.h"
+
+namespace NYT
+{
+
+TRemoteChunkReader::TRemoteChunkReader(const TChunkId& chunkId, Stroka holderAddress)
+    : ChunkId(chunkId)
+    , Timeout(TDuration::Seconds(15)) // ToDo: make configurable
+    , Proxy(~TRemoteChunkReader::ChannelCache.GetChannel(holderAddress))
+{ }
+
+TAsyncResult<IChunkReader::TReadResult>::TPtr
+TRemoteChunkReader::AsyncReadBlocks(const yvector<int>& blockIndexes)
+{
+    auto result = New< TAsyncResult<TReadResult> >();
+    auto req = Proxy.GetBlocks();
+    req->SetChunkId(ChunkId.ToProto());
+
+    for (int i = 0; i < blockIndexes.ysize(); ++i) {
+        req->AddBlockIndexes(blockIndexes[i]);
+    }
+
+    req->Invoke(Timeout)->Subscribe(FromMethod(
+        &TRemoteChunkReader::OnBlocksRead, 
+        TPtr(this), 
+        result));
+
+    return result;
+}
+
+void TRemoteChunkReader::OnBlocksRead(TRspGetBlocks::TPtr rsp, TAsyncResult<TReadResult>::TPtr result)
+{
+    TReadResult readResult;
+    for (int i = 0; i < rsp->Attachments().ysize(); i++) {
+        // Since all attachments reference the same rpc response
+        // memory will be freed only when all the blocks will die
+        readResult.Blocks.push_back(rsp->Attachments()[i]);
+    }
+
+    result->Set(readResult);
+}
+
+NRpc::TChannelCache TRemoteChunkReader::ChannelCache;
+
+} // namespace NYT
---
+++ yt/ytlib/chunk_client/remote_chunk_reader.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "chunk_reader.h"
+#include "common.h"
+#include "../chunk_holder/chunk_holder_rpc.h"
+
+namespace NYT
+{
+
+///////////////////////////////////////////////////////////////////////////////
+
+class TRemoteChunkReader
+    : public IChunkReader
+{
+public:
+    TRemoteChunkReader(const TChunkId& chunkId, Stroka holderAddress);
+    TAsyncResult<TReadResult>::TPtr AsyncReadBlocks(const yvector<int>& blockIndexes);
+
+private:
+    typedef TIntrusivePtr<TRemoteChunkReader> TPtr;
+    typedef NChunkHolder::TChunkHolderProxy TProxy;
+    USE_RPC_PROXY_METHOD(TProxy, GetBlocks);
+
+    void OnBlocksRead(TRspGetBlocks::TPtr rsp, TAsyncResult<TReadResult>::TPtr result);
+
+    TChunkId ChunkId;
+    TDuration Timeout;
+    TProxy Proxy;
+
+    static NRpc::TChannelCache ChannelCache;
+};
+///////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
