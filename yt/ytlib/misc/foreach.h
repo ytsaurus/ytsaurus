7ddd7d956983560cb31f18f45b3932c3d22331a6
Dmitry Panin <panin@yandex-team.ru>
2011-09-13 19:28:33 +0400

    changed comment

--- yt/ytlib/misc/foreach.h
+++ yt/ytlib/misc/foreach.h
@@ -27,19 +27,6 @@ inline auto End(T& collection) -> decltype(collection.end())
     return collection.end();
 }
 
-// specialisation for List (has only .Begin() and .End(), not .begin(), .end() )
-template<class T>
-inline auto Begin(TIntrusiveList<T>& collection) -> decltype(collection.Begin())
-{
-    return collection.Begin();
-}
-
-template<class T>
-inline auto End(TIntrusiveList<T>& collection) -> decltype(collection.End())
-{
-    return collection.End();
-}
-
 
 template<class T>
 inline auto Deref(T& it) -> decltype(*it)
@@ -51,6 +38,19 @@ template<class T>
 inline void MoveNext(T& it)
 {
     ++it;
+}
+
+// Provide a speciailization for TIntrusiveList, which has Begin/End instead of begin/end.
+template<class T>
+inline auto Begin(TIntrusiveList<T>& collection) -> decltype(collection.Begin())
+{
+    return collection.Begin();
+}
+
+template<class T>
+inline auto End(TIntrusiveList<T>& collection) -> decltype(collection.End())
+{
+    return collection.End();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
