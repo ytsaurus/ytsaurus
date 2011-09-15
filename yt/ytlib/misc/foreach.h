e31a8afd6153f4b612da1600b9daf85e8ac4d643
Maxim Babenko <babenko@ytsaurus.tech>
2011-09-15 11:23:21 +0400

    Fix indent

--- yt/ytlib/misc/foreach.h
+++ yt/ytlib/misc/foreach.h
@@ -87,18 +87,18 @@ inline void MoveNext(T& it)
  *    hence one should avoid using at hotspots
  */
 #define FOREACH(var, collection) \
-auto&& PP_CONCAT(foreach_collection_, __LINE__) = collection; \
-auto PP_CONCAT(foreach_current_, __LINE__) = ::NYT::NForeach::Begin(PP_CONCAT(foreach_collection_, __LINE__)); \
-auto PP_CONCAT(foreach_end_, __LINE__) = ::NYT::NForeach::End(PP_CONCAT(foreach_collection_, __LINE__)); \
-for (bool PP_CONCAT(foreach_continue_, __LINE__) = true; \
-     PP_CONCAT(foreach_continue_, __LINE__) && \
-     PP_CONCAT(foreach_current_, __LINE__) != PP_CONCAT(foreach_end_, __LINE__); \
-     PP_CONCAT(foreach_continue_, __LINE__) \
-     ? ::NYT::NForeach::MoveNext(PP_CONCAT(foreach_current_, __LINE__)) \
-     : (void) 0) \
-if (::NYT::NForeach::SetFalse(PP_CONCAT(foreach_continue_, __LINE__))) { } else \
-for (var = ::NYT::NForeach::Deref(PP_CONCAT(foreach_current_, __LINE__)); \
-     !PP_CONCAT(foreach_continue_, __LINE__); \
-     PP_CONCAT(foreach_continue_, __LINE__) = true)
+    auto&& PP_CONCAT(foreach_collection_, __LINE__) = collection; \
+    auto PP_CONCAT(foreach_current_, __LINE__) = ::NYT::NForeach::Begin(PP_CONCAT(foreach_collection_, __LINE__)); \
+    auto PP_CONCAT(foreach_end_, __LINE__) = ::NYT::NForeach::End(PP_CONCAT(foreach_collection_, __LINE__)); \
+    for (bool PP_CONCAT(foreach_continue_, __LINE__) = true; \
+         PP_CONCAT(foreach_continue_, __LINE__) && \
+         PP_CONCAT(foreach_current_, __LINE__) != PP_CONCAT(foreach_end_, __LINE__); \
+         PP_CONCAT(foreach_continue_, __LINE__) \
+         ? ::NYT::NForeach::MoveNext(PP_CONCAT(foreach_current_, __LINE__)) \
+         : (void) 0) \
+    if (::NYT::NForeach::SetFalse(PP_CONCAT(foreach_continue_, __LINE__))) { } else \
+    for (var = ::NYT::NForeach::Deref(PP_CONCAT(foreach_current_, __LINE__)); \
+         !PP_CONCAT(foreach_continue_, __LINE__); \
+         PP_CONCAT(foreach_continue_, __LINE__) = true)
 
 ////////////////////////////////////////////////////////////////////////////////
