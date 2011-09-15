30d4c0c402831b527606e4701aea407720486dcf
Dmitry Panin <panin@yandex-team.ru>
2011-09-15 16:05:45 +0400

    cosmetics

--- yt/ytlib/misc/fs.cpp
+++ yt/ytlib/misc/fs.cpp
@@ -25,7 +25,7 @@ static NLog::TLogger Logger("FS");
 
 //////////////////////////////////////////////////////////////////////////////
 
-bool Remove(Stroka name)
+bool Remove(const Stroka& name)
 {
 #if defined(_win_)
     return DeleteFile(~name);
@@ -42,7 +42,7 @@ bool Remove(Stroka name)
 #endif
 }
 
-bool Rename(Stroka oldName, Stroka newName)
+bool Rename(const Stroka& oldName, const Stroka& newName)
 {
 #if defined(_win_)
     return MoveFileEx(~oldName, ~newName, MOVEFILE_REPLACE_EXISTING) != 0;
@@ -51,7 +51,7 @@ bool Rename(Stroka oldName, Stroka newName)
 #endif
 }
 
-Stroka GetFileName(Stroka filePath)
+Stroka GetFileName(const Stroka& filePath)
 {
     size_t delimPos = filePath.rfind('/');
 #ifdef _win32_
@@ -63,7 +63,7 @@ Stroka GetFileName(Stroka filePath)
     return (delimPos == Stroka::npos) ? filePath : filePath.substr(delimPos+1);
 }
 
-Stroka GetFileExtension(Stroka filePath)
+Stroka GetFileExtension(const Stroka& filePath)
 {
     i32 dotPosition = filePath.find_last_of('.');
     if (dotPosition < 0) {
@@ -72,17 +72,17 @@ Stroka GetFileExtension(Stroka filePath)
     return filePath.substr(dotPosition + 1, filePath.size() - dotPosition - 1);
 }
 
-Stroka GetFileNameWithoutExtension(Stroka filePath)
+Stroka GetFileNameWithoutExtension(const Stroka& filePath)
 {
-    filePath = GetFileName(filePath);
-    i32 dotPosition = filePath.find_last_of('.');
+    Stroka fileName = GetFileName(filePath);
+    i32 dotPosition = fileName.find_last_of('.');
     if (dotPosition < 0) {
-        return filePath;
+        return fileName;
     }
-    return filePath.substr(0, dotPosition);
+    return fileName.substr(0, dotPosition);
 }
 
-void CleanTempFiles(Stroka location)
+void CleanTempFiles(const Stroka& location)
 {
     LOG_INFO("Cleaning temp files in %s", ~location);
     TFileList fileList;
@@ -126,7 +126,7 @@ i64 GetAvailableSpace(const Stroka& path)
     return availableSpace;
 }
 
-void ForcePath(Stroka path, int mode)
+void ForcePath(const Stroka& path, int mode)
 {
     MakePathIfNotExist(~path, mode);
 }
