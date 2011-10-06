42df79d869716ff96df93bda4bc566c68504e04c
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2011-10-06 13:32:18 +0400

    Remove irrelevant TODO

--- yt/unittests/ytree_fluent_ut.cpp
+++ yt/unittests/ytree_fluent_ut.cpp
@@ -98,8 +98,6 @@ TYPED_TEST_P(TYTreeFluentIntegerScalarTest, Ok)
         .EndTree();
 }
 
-// TODO: Add <ui8, ui16, ui32, ui64> to the list of used types.
-// Currently does not compile.
 typedef Types<i8, ui8, i16, ui16, i32, ui32, i64>
     TYTreeFluentIntegerScalarTestTypes;
 REGISTER_TYPED_TEST_CASE_P(TYTreeFluentIntegerScalarTest, Ok);
