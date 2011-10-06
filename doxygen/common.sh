073230d43eff6f4faf842cff216b40dbf652aa67
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2011-10-06 12:47:25 +0400

    YT-157 - Redeploy Doxygen site

--- doxygen/common.sh
+++ doxygen/common.sh
@@ -4,13 +4,15 @@ set -e
 machine=$(hostname -f)
 
 case ${machine} in
-    "yt-dev01d.yandex-net")
+    "build01-01g.yt.yandex.net")
         CELLAR=/home/sandello/Cellar/stage
         SOURCE=/home/sandello/source
+        TARGET=/home/sandello/doxygen
         ;;
     "CoreMoonlight.local")
         CELLAR=/usr/local/Cellar/doxygen/1.7.4/
         SOURCE=/Users/sandello/YT/source
+        TARGET=/Users/sandello/YT/doxygen
         ;;
     *)
         echo "Unknown machine; please, include your configuration into common.sh."
--- doxygen/make_doxygen.sh
+++ doxygen/make_doxygen.sh
@@ -2,20 +2,20 @@
 source $(dirname ${BASH_SOURCE[0]})/common.sh
 set -x
 
-LAST_CHANGED_REV=$(svn info ${SOURCE} | grep 'Last Changed Rev' | cut -d : -f 2- | cut -c 2-)
-LAST_CHANGED_DATE=$(svn info ${SOURCE} | grep 'Last Changed Date' | cut -d : -f 2- | cut -c 2-)
-LAST_GENERATED_DATE=$(date +"%F %T %z (%a, %d %b %Y)")
+CURRENT_BRANCH=$( ( cd ${SOURCE} && git branch --no-color --no-abbrev -v ) | awk '{print $2}' )
+CURRENT_COMMIT=$( ( cd ${SOURCE} && git branch --no-color --no-abbrev -v ) | awk '{print $3}' )
+GENERATED_AT=$(date +"%F %T %z (%a, %d %b %Y)")
 
 svn update $SOURCE
 
 cat ${SOURCE}/doxygen/yt.cfg.template \
-    | sed "s!%%LAST_CHANGED_REV%%!${LAST_CHANGED_REV}!" \
-    | sed "s!%%LAST_CHANGED_DATE%%!${LAST_CHANGED_DATE}!" \
-    | sed "s!%%LAST_GENERATED_DATE%%!${LAST_GENERATED_DATE}!" \
+    | sed "s!%%CURRENT_BRANCH%%!${CURRENT_BRANCH}!" \
+    | sed "s!%%CURRENT_COMMIT%%!${CURRENT_COMMIT}!" \
+    | sed "s!%%GENERATED_AT%%!${GENERATED_AT}!" \
     | sed "s!%%STRIP_FROM_PATH%%!${SOURCE}!" \
-    | sed "s!%%OUTPUT_DIRECTORY%%!${SOURCE}/doxygen/output/!" \
+    | sed "s!%%OUTPUT_DIRECTORY%%!${TARGET}/pending/!" \
     | sed "s!%%INPUT%%!${SOURCE}/yt/ytlib/!" \
-    > ${SOURCE}/doxygen/output/yt.cfg
+    > ${TARGET}/pending/yt.cfg
 
-${CELLAR}/bin/doxygen ${SOURCE}/doxygen/output/yt.cfg
+${CELLAR}/bin/doxygen ${TARGET}/pending/yt.cfg
 
