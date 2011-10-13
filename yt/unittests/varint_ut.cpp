4108cb1fca9fccc467fb7bcc028c4f0dcee27b7e
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2011-10-13 23:25:45 +0400

    Refactored varint_ut.cpp for a little bit

--- yt/unittests/varint_ut.cpp
+++ yt/unittests/varint_ut.cpp
@@ -1,5 +1,6 @@
 #include "../ytlib/misc/serialize.h"
 
+#include <util/string/escape.h>
 #include <util/random/random.h>
 
 #include <contrib/testing/framework.h>
@@ -27,21 +28,6 @@ TEST_P(TWriteVarIntTest, Serialization)
     EXPECT_EQ(rightAnswer, outputStream.Str());
 }
 
-INSTANTIATE_TEST_CASE_P(ValueParametrized, TWriteVarIntTest, Values(
-    make_tuple(0x0ull,                  Stroka("\x00", 1)),
-    make_tuple(0x1ull,                  Stroka("\x01", 1)),
-    make_tuple(0x2ull,                  Stroka("\x02", 1)),
-    make_tuple(0x3ull,                  Stroka("\x03", 1)),
-    make_tuple(0x10ull,                 Stroka("\x10", 1)),
-    make_tuple(0x1000ull,               Stroka("\x80\x20", 2)),
-    make_tuple(0x100000ull,             Stroka("\x80\x80\x40", 3)),
-    make_tuple(0x10000000ull,           Stroka("\x80\x80\x80\x80\x01", 5)),
-    make_tuple(0x1000000000ull,         Stroka("\x80\x80\x80\x80\x80\x02", 6)),
-    make_tuple(0x100000000000ull,       Stroka("\x80\x80\x80\x80\x80\x80\x04", 7)),
-    make_tuple(0x10000000000000ull,     Stroka("\x80\x80\x80\x80\x80\x80\x80\x08", 8)),
-    make_tuple(0x1000000000000000ull,   Stroka("\x80\x80\x80\x80\x80\x80\x80\x80\x10", 9))
-));
-
 ////////////////////////////////////////////////////////////////////////////////
 
 class TReadVarIntTest: public ::testing::TestWithParam< tuple<ui64, Stroka> >
@@ -52,55 +38,82 @@ TEST_P(TReadVarIntTest, Serialization)
     ui64 rightAnswer = get<0>(GetParam());
     Stroka input = get<1>(GetParam());
 
-    TMemoryInput inputStream(input.c_str(), input.length());
+    TStringInput inputStream(input);
     ui64 value = ReadVarInt(&inputStream);
     EXPECT_EQ(rightAnswer, value);
 }
 
-INSTANTIATE_TEST_CASE_P(ValueParametrized, TReadVarIntTest, Values(
-    make_tuple(0x0ull,                  Stroka("\x00", 1)),
-    make_tuple(0x1ull,                  Stroka("\x01", 1)),
-    make_tuple(0x2ull,                  Stroka("\x02", 1)),
-    make_tuple(0x3ull,                  Stroka("\x03", 1)),
-    make_tuple(0x10ull,                 Stroka("\x10", 1)),
-    make_tuple(0x1000ull,               Stroka("\x80\x20", 2)),
-    make_tuple(0x100000ull,             Stroka("\x80\x80\x40", 3)),
-    make_tuple(0x10000000ull,           Stroka("\x80\x80\x80\x80\x01", 5)),
-    make_tuple(0x1000000000ull,         Stroka("\x80\x80\x80\x80\x80\x02", 6)),
-    make_tuple(0x100000000000ull,       Stroka("\x80\x80\x80\x80\x80\x80\x04", 7)),
-    make_tuple(0x10000000000000ull,     Stroka("\x80\x80\x80\x80\x80\x80\x80\x08", 8)),
-    make_tuple(0x1000000000000000ull,   Stroka("\x80\x80\x80\x80\x80\x80\x80\x80\x10", 9))
-));
+////////////////////////////////////////////////////////////////////////////////
+
+auto ValuesForVarIntTests = Values(
+    // Simple cases
+    make_tuple(0x0ull,                Stroka("\x00", 1)),
+    make_tuple(0x1ull,                Stroka("\x01", 1)),
+    make_tuple(0x2ull,                Stroka("\x02", 1)),
+    make_tuple(0x3ull,                Stroka("\x03", 1)),
+    make_tuple(0x4ull,                Stroka("\x04", 1)),
+
+    // Following "magic numbers" are critical points for varint encoding
+    make_tuple((1ull << 7) - 1,       Stroka("\x7f", 1)),
+    make_tuple((1ull << 7),           Stroka("\x80\x01", 2)),
+    make_tuple((1ull << 14) - 1,      Stroka("\xff\x7f", 2)),
+    make_tuple((1ull << 14),          Stroka("\x80\x80\x01", 3)),
+    make_tuple((1ull << 21) - 1,      Stroka("\xff\xff\x7f", 3)),
+    make_tuple((1ull << 21),          Stroka("\x80\x80\x80\x01", 4)),
+    make_tuple((1ull << 28) - 1,      Stroka("\xff\xff\xff\x7f", 4)),
+    make_tuple((1ull << 28),          Stroka("\x80\x80\x80\x80\x01", 5)),
+    make_tuple((1ull << 35) - 1,      Stroka("\xff\xff\xff\xff\x7f", 5)),
+    make_tuple((1ull << 35),          Stroka("\x80\x80\x80\x80\x80\x01", 6)),
+    make_tuple((1ull << 42) - 1,      Stroka("\xff\xff\xff\xff\xff\x7f", 6)),
+    make_tuple((1ull << 42),          Stroka("\x80\x80\x80\x80\x80\x80\x01", 7)),
+    make_tuple((1ull << 49) - 1,      Stroka("\xff\xff\xff\xff\xff\xff\x7f", 7)),
+    make_tuple((1ull << 49),          Stroka("\x80\x80\x80\x80\x80\x80\x80\x01", 8)),
+    make_tuple((1ull << 56) - 1,      Stroka("\xff\xff\xff\xff\xff\xff\xff\x7f", 8)),
+    make_tuple((1ull << 56),          Stroka("\x80\x80\x80\x80\x80\x80\x80\x80\x01", 9)),
+    make_tuple((1ull << 63) - 1,      Stroka("\xff\xff\xff\xff\xff\xff\xff\xff\x7f", 9)),
+    make_tuple((1ull << 63),          Stroka("\x80\x80\x80\x80\x80\x80\x80\x80\x80\x01", 10)),
+
+    // Boundary case
+    make_tuple(static_cast<ui64>(-1), Stroka("\xff\xff\xff\xff\xff\xff\xff\xff\xff\x01", 10))
+);
+
+INSTANTIATE_TEST_CASE_P(ValueParametrized, TWriteVarIntTest,
+    ValuesForVarIntTests);
+
+INSTANTIATE_TEST_CASE_P(ValueParametrized, TReadVarIntTest,
+    ValuesForVarIntTests);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TEST(TVarInt32Test, SomeValues)
+TEST(TVarInt32Test, RandomValues)
 {
-    srand(100500); // set seed
-    int numValues = 10000;
+    srand(100500); // Set seed
+    const int numberOfValues = 10000;
 
     TStringStream stream;
-    for (int i = 0; i < numValues; ++i) {
-        i32 value = static_cast<i32>(RandomNumber<ui32>());
-        WriteVarInt32(value, &stream);
-        i32 readValue = ReadVarInt32(&stream);
-        EXPECT_EQ(value, readValue);
+    for (int i = 0; i < numberOfValues; ++i) {
+        i32 expected = static_cast<i32>(RandomNumber<ui32>());
+        WriteVarInt32(expected, &stream);
+        i32 actual = ReadVarInt32(&stream);
+        EXPECT_EQ(expected, actual)
+            << "Encoded Variant: " << EscapeC(stream.Str());
     }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TEST(TVarInt64Test, SomeValues)
+TEST(TVarInt64Test, RandomValues)
 {
-    srand(100500); // set seed
-    int numValues = 10000;
+    srand(100500); // Set seed
+    const int numberOfValues = 10000;
 
     TStringStream stream;
-    for (int i = 0; i < numValues; ++i) {
-        i64 value = static_cast<i64>(RandomNumber<ui64>());
-        WriteVarInt64(value, &stream);
-        i64 readValue = ReadVarInt64(&stream);
-        EXPECT_EQ(value, readValue);
+    for (int i = 0; i < numberOfValues; ++i) {
+        i64 expected = static_cast<i64>(RandomNumber<ui64>());
+        WriteVarInt64(expected, &stream);
+        i64 actual = ReadVarInt64(&stream);
+        EXPECT_EQ(expected, actual)
+            << "Encoded Variant: " << EscapeC(stream.Str());
     }
 }
 
