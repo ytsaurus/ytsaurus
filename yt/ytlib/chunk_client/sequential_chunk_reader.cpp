6a73202cb6f5323791a83bcf6e58839e1054531f
Pavel Sushin <psushin@ytsaurus.tech>
2011-10-21 18:09:51 +0400

    Fixed stupid bug

--- yt/ytlib/chunk_client/sequential_chunk_reader.cpp
+++ yt/ytlib/chunk_client/sequential_chunk_reader.cpp
@@ -117,7 +117,7 @@ void TSequentialChunkReader::DoShiftWindow()
 void TSequentialChunkReader::FetchNextGroup()
 {
     VERIFY_THREAD_AFFINITY(ReaderThread);
-    YASSERT(FreeSlots > Config.GroupSize);
+    YASSERT(FreeSlots >= Config.GroupSize);
 
     auto groupBegin = BlockIndexSequence.begin() + FirstUnfetchedIndex;
     auto groupEnd = BlockIndexSequence.end();
