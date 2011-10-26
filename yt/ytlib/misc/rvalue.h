0c0fe87500934875429a22c5349c9db2c73a0be3
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2011-10-26 18:29:51 +0400

    Added throw() specifications for MoveRV and ForwardRV

--- yt/ytlib/misc/rvalue.h
+++ yt/ytlib/misc/rvalue.h
@@ -13,28 +13,28 @@ namespace NYT {
 
 //! Implements #std::move<T> behaviour from C++11.
 template<typename T>
-FORCED_INLINE typename TTypeTraits<T>::TReferenceTo&& MoveRV(T&& x)
+FORCED_INLINE typename TTypeTraits<T>::TReferenceTo&& MoveRV(T&& x) throw()
 {
-    return static_cast<typename TTypeTraits<T>::TReferenceTo<T>::TType&&>(x);
+    return static_cast<typename TTypeTraits<T>::TReferenceTo&&>(x);
 }
 
 //! Implements #std::forward<T> behaviour from C++11.
 template<typename T>
-FORCED_INLINE T&& ForwardRV(typename TTypeTraits<T>::TReferenceTo<T>::TType& x)
+FORCED_INLINE T&& ForwardRV(typename TTypeTraits<T>::TReferenceTo& x) throw()
 {
-    return static_cast<T&&>(x);
+    return x;
 }
 
 //! Fix instantination errors (mainly due to reference collapsing).
 //! \{
 template<typename T>
-FORCED_INLINE T& ForwardRV(T& x)
+FORCED_INLINE T& ForwardRV(T& x) throw()
 {
     return x;
 }
 
 template<typename T>
-FORCED_INLINE const T*&& ForwardRV(const T*& x)
+FORCED_INLINE const T*&& ForwardRV(const T*& x) throw()
 {
     return x;
 }
