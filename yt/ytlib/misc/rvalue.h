a61be1b1286cb1338f8676f148bff9974bf28f6f
Maxim Babenko <babenko@ytsaurus.tech>
2011-10-27 10:59:13 +0400

    VS build fixed

--- yt/ytlib/misc/rvalue.h
+++ yt/ytlib/misc/rvalue.h
@@ -18,6 +18,8 @@ FORCED_INLINE typename TTypeTraits<T>::TReferenceTo&& MoveRV(T&& x) throw()
     return static_cast<typename TTypeTraits<T>::TReferenceTo&&>(x);
 }
 
+#ifdef __GNUC__
+
 //! Implements #std::forward<T> behaviour from C++11.
 template<typename T>
 FORCED_INLINE T&& ForwardRV(typename TTypeTraits<T>::TReferenceTo& x) throw()
@@ -25,7 +27,7 @@ FORCED_INLINE T&& ForwardRV(typename TTypeTraits<T>::TReferenceTo& x) throw()
     return x;
 }
 
-//! Fix instantination errors (mainly due to reference collapsing).
+//! Fix instantiation errors (mainly due to reference collapsing).
 //! \{
 template<typename T>
 FORCED_INLINE T& ForwardRV(T& x) throw()
@@ -39,6 +41,26 @@ FORCED_INLINE const T*&& ForwardRV(const T*& x) throw()
     return x;
 }
 //! \}
+
+#else
+
+// TODO: This weird implementation of ForwardRV is intentional;
+// the main rationale is to provide a correct build under MSVS ignoring
+// any performance considerations.
+
+template<typename T>
+FORCED_INLINE T& ForwardRV(T& x) throw()
+{
+    return x;
+}
+
+template<typename T>
+FORCED_INLINE const T& ForwardRV(const T& x) throw()
+{
+    return x;
+}
+
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 
