7011085e5fcc88217e62f4c554d7d1be22b230b9
Maxim Babenko <babenko@ytsaurus.tech>
2011-10-31 13:13:45 +0300

    Added async stubs and more comments

--- yt/ytlib/table_client/table_chunk_reader.h
+++ yt/ytlib/table_client/table_chunk_reader.h
@@ -13,6 +13,8 @@ namespace NTableClient {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+// TODO: extract interface ITableReader
+// TODO: write something smart and funny here
 class TTableChunkReader
     : public TRefCountedBase
 {
@@ -24,10 +26,69 @@ public:
         const TChannel& channel,
         IChunkReader::TPtr chunkReader);
 
+    // TODO: refactor using the following declarations
+    DECLARE_ENUM(ECode,
+        (OK)
+        (Finished)
+        (TryLater)
+        (Failed)
+    );
+
+    struct TResult
+    {
+        ECode Code;
+        // TODO: use an appropriate type for errors
+        Stroka Error;
+    };
+
+    //! Asynchronously switches the reader to the next row.
+    /*!
+     *  This call cannot block.
+     *  
+     *  \param ready A future that gets filled when no more
+     *  data is available at hand and then gets set when more data arrives.
+     *  
+     *  \return Possible outcomes are:
+     *  - ECode::OK: the next row is fetched, the client must invoke
+     *  #NextColumn to fetch its first column.
+     *  - ECode::Finished: the table has no more rows.
+     *  - ECode::TryLater: no data is currently available, the client
+     *  must wait for #event.
+     *  - ECode::Failed: something went wrong.
+     */
+    TResult AsyncNextRow(TFuture<TVoid>::TPtr* ready);
+
+
+    // TODO: refactor using AsyncNextRow
+
+    //! Switches the reader to the next row.
+    /*!
+     *  This call may block.
+     *  Internally it invokes #AsyncNextRow.
+     *  This call throws an exception on failure.
+     *  
+     *  \return True iff a new row is fetched, False if the table chunk is finished.
+     */
     bool NextRow();
+
+    //! Switches the reader to the next column in the current row.
+    /*!
+     *  This call cannot block.
+     *  This call does not throw.
+     *  
+     *  \return True iff a new column is fetched, False is the row has finished.
+     */
     bool NextColumn();
 
+    //! Returns the name of the current column.
     const TColumn& GetColumn() const;
+
+    //! Returns the value of the current column.
+    /*!
+     *  This call returns a pointer to the actual data, which is held
+     *  by the reader as long as the current row does not change. The client
+     *  must make an explicit copy of it if needed.
+     */
     TValue GetValue() const;
 
 private:
