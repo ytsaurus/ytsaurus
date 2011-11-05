dac2914120660e6112271e6787d5e4885ef478ec
Maxim Babenko <babenko@ytsaurus.tech>
2011-11-05 21:04:05 +0400

    Refactoring and cleanup

--- yt/ytlib/rpc/retry.cpp
+++ yt/ytlib/rpc/retry.cpp
@@ -14,11 +14,12 @@ using namespace NBus;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TRetryRequestWrapper
+//! Wraps an original request and provides a fresh request id.
+class TRequestWrapper
     : public IClientRequest
 {
 public:
-    TRetryRequestWrapper(IClientRequest::TPtr originalRequest)
+    TRequestWrapper(IClientRequest* originalRequest)
         : RequestId(TRequestId::Create())
         , OriginalRequest(originalRequest)
     { }
@@ -28,7 +29,7 @@ public:
         return RequestId;
     }
 
-    NBus::IMessage::TPtr Serialize() const
+    IMessage::TPtr Serialize() const
     {
         return OriginalRequest->Serialize();
     }
@@ -46,185 +47,148 @@ public:
 private:
     const TRequestId RequestId;
     IClientRequest::TPtr OriginalRequest;
+
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// ToDo: consider a better name
-class TRetriableRpcCall;
-
-//! Although several handler routines could be called for a #TRetryResponseHandler
-//! instance, error or success will be reported to TRetriableRpcCall only once.
-class TRetryResponseHandler
+class TRetriableRequest
     : public IClientResponseHandler
 {
 public:
-    typedef TIntrusivePtr<TRetryResponseHandler> TPtr;
+    typedef TIntrusivePtr<TRetriableRequest> TPtr;
 
-    TRetryResponseHandler(TIntrusivePtr<TRetriableRpcCall> retriableCall);
-
-    void OnAcknowledgement(IBus::ESendResult sendResult);
-    void OnResponse(EErrorCode errorCode, NBus::IMessage::TPtr message);
-    void OnTimeout();
-
-private:
-    DECLARE_ENUM(EState, 
-        (Sent)
-        (Done)
-    );
-
-    EState State;
-
-    TIntrusivePtr<TRetriableRpcCall> RetriableCall;
-    TSpinLock SpinLock;
-};
-
-////////////////////////////////////////////////////////////////////////////////
-
-//! As far as no more than one handler is active at any moment,
-//! no locking is required.
-class TRetriableRpcCall
-    : public virtual TRefCountedBase
-{
-public:
-    typedef TIntrusivePtr<TRetriableRpcCall> TPtr;
-
-    TRetriableRpcCall(
-        TRetriableChannel::TPtr channel,
-        IClientRequest::TPtr originalRequest,
-        IClientResponseHandler::TPtr originalResponseHandler,
-        TDuration timeout,
-        TFuture<EErrorCode>::TPtr sendResult)
-        : RetryCount(0)
+    TRetriableRequest(
+        TRetriableChannel* channel,
+        IClientRequest* originalRequest,
+        IClientResponseHandler* originalResponseHandler,
+        TDuration timeout)
+        : CurrentAttempt(0)
         , Channel(channel)
         , OriginalRequest(originalRequest)
         , OriginalResponseHandler(originalResponseHandler)
         , Timeout(timeout)
-        , SendResult(sendResult)
+        , SendResult(New< TFuture<EErrorCode> >())
     {
-        YASSERT(~channel != NULL);
-        YASSERT(~originalRequest != NULL);
-        YASSERT(~originalResponseHandler != NULL);
-        YASSERT(~sendResult != NULL);
-
-        Send();
+        YASSERT(channel != NULL);
+        YASSERT(originalRequest != NULL);
+        YASSERT(originalResponseHandler != NULL);
     }
 
-    void OnFail()
+    TFuture<EErrorCode>::TPtr Send() 
     {
-        ++RetryCount;
-        if (RetryCount < Channel->GetRetryCount()) {
+        DoSend();
+        return SendResult;
+    }
+
+private:
+    //! The current attempt number (starting from 0).
+    TAtomic CurrentAttempt;
+    TRetriableChannel::TPtr Channel;
+    IClientRequest::TPtr OriginalRequest;
+    IClientResponseHandler::TPtr OriginalResponseHandler;
+    TDuration Timeout;
+    //! Result returned by #IChannel::Send
+    TFuture<EErrorCode>::TPtr SendResult;
+
+    DECLARE_ENUM(EState, 
+        (Sent)
+        (Acked)
+        (Done)
+    );
+
+    //! Protects state transitions.
+    TSpinLock SpinLock;
+    EState State;
+
+    void DoSend()
+    {
+        auto request = New<TRequestWrapper>(~OriginalRequest);
+        Channel->GetUnderlyingChannel()->Send(
+            request,
+            this,
+            Timeout);
+    }
+
+    virtual void OnAcknowledgement(IBus::ESendResult sendResult)
+    {
+        TGuard<TSpinLock> guard(SpinLock);
+        if (State == EState::Sent) {
+            switch (sendResult) {
+                case IBus::ESendResult::OK:
+                    State = EState::Acked;
+                    guard.Release();
+
+                    OriginalResponseHandler->OnAcknowledgement(sendResult);
+                    break;
+
+                case IBus::ESendResult::Failed:
+                    State = EState::Done;
+                    guard.Release();
+
+                    OnAttemptFailed();
+                    break;
+
+                default:
+                    YUNREACHABLE();
+            }
+        }
+    }
+
+    virtual void OnTimeout() 
+    {
+        TGuard<TSpinLock> guard(SpinLock);
+        if (State == EState::Sent) {
+            State = EState::Done;
+            guard.Release();
+
+            OnAttemptFailed();
+        }
+    }
+
+    virtual void OnResponse(EErrorCode errorCode, IMessage::TPtr message)
+    {
+        TGuard<TSpinLock> guard(SpinLock);
+        if (State == EState::Sent) {
+            State = EState::Done;
+            guard.Release();
+
+            if (!errorCode.IsRpcError()) {
+                OriginalResponseHandler->OnResponse(errorCode, message);
+                SendResult->Set(errorCode);
+            } else {
+                OnAttemptFailed();
+            }
+        }
+    }
+
+
+    void OnAttemptFailed()
+    {
+        int count = AtomicIncrement(CurrentAttempt);
+        if (count < Channel->GetRetryCount()) {
             TDelayedInvoker::Get()->Submit(
-                FromMethod(&TRetriableRpcCall::Send, TPtr(this)),
+                FromMethod(&TRetriableRequest::DoSend, TPtr(this)),
                 TInstant::Now() + Channel->GetBackoffTime());
         } else {
             OriginalResponseHandler->OnResponse(EErrorCode::Unavailable, NULL);
             SendResult->Set(EErrorCode::Unavailable);
         }
     }
-
-    void OnResponse(EErrorCode errorCode, NBus::IMessage::TPtr message) 
-    {
-        YASSERT(~message != NULL);
-
-        if (!errorCode.IsRpcError()) {
-            OriginalResponseHandler->OnResponse(errorCode, message);
-            SendResult->Set(errorCode);
-        } else {
-            OnFail();
-        }
-    }
-
-private:
-    void Send() 
-    {
-        auto request = New<TRetryRequestWrapper>(OriginalRequest);
-        auto responseHandlers = New<TRetryResponseHandler>(this);
-
-        Channel->GetChannel()->Send(
-            request,
-            responseHandlers,
-            Timeout
-        );
-    }
-
-    int RetryCount;
-    TRetriableChannel::TPtr Channel;
-    IClientRequest::TPtr OriginalRequest;
-    IClientResponseHandler::TPtr OriginalResponseHandler;
-    TDuration Timeout;
-
-    //! Result, returned by #IChannel::Send
-    TFuture<EErrorCode>::TPtr SendResult;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TRetryResponseHandler::TRetryResponseHandler(
-    TRetriableRpcCall::TPtr retriableCall)
-    : State(EState::Sent)
-    , RetriableCall(retriableCall)
-{
-    YASSERT(~retriableCall != NULL);
-}
-
-void TRetryResponseHandler::OnAcknowledgement(
-    IBus::ESendResult sendResult)
-{
-    TGuard<TSpinLock> guard(SpinLock);
-    if (State == EState::Sent) {
-        switch (sendResult) {
-        case IBus::ESendResult::OK:
-            break;
-
-        case IBus::ESendResult::Failed:
-            State = EState::Done;
-            guard.Release();
-
-            RetriableCall->OnFail();
-            break;
-
-        default:
-            YUNREACHABLE();
-        }
-    }
-}
-
-void TRetryResponseHandler::OnResponse(
-    EErrorCode errorCode, 
-    NBus::IMessage::TPtr message)
-{
-    TGuard<TSpinLock> guard(SpinLock);
-    if (State == EState::Sent) {
-        State = EState::Done;
-        guard.Release();
-
-        RetriableCall->OnResponse(errorCode, message);
-    }
-}
-
-void TRetryResponseHandler::OnTimeout() 
-{
-    TGuard<TSpinLock> guard(SpinLock);
-    if (State == EState::Sent) {
-        State = EState::Done;
-        guard.Release();
-
-        RetriableCall->OnFail();
-    }
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 TRetriableChannel::TRetriableChannel(
-    IChannel::TPtr channel, 
+    IChannel* underlyingChannel, 
     TDuration backoffTime, 
     int retryCount)
-    : Channel(channel)
-    , RetryCount(retryCount)
-    , BackoffTime(backoffTime)
+    : UnderlyingChannel_(underlyingChannel)
+    , RetryCount_(retryCount)
+    , BackoffTime_(backoffTime)
 {
-    YASSERT(~channel != NULL);
+    YASSERT(underlyingChannel != NULL);
+    YASSERT(retryCount >= 1);
 }
 
 TFuture<EErrorCode>::TPtr TRetriableChannel::Send(
@@ -232,30 +196,21 @@ TFuture<EErrorCode>::TPtr TRetriableChannel::Send(
     IClientResponseHandler::TPtr responseHandler, 
     TDuration timeout)
 {
-    // TODO: encapsulate this result in TRetriableRpcCall
-    // provide GetResult method and return it here
     YASSERT(~request != NULL);
     YASSERT(~responseHandler != NULL);
 
-    auto result = New< TFuture<EErrorCode> > ();
-    auto retriableCall = New<TRetriableRpcCall>(
+    auto retriableRequest = New<TRetriableRequest>(
         this,
-        request,
-        responseHandler,
-        timeout,
-        result);
+        ~request,
+        ~responseHandler,
+        timeout);
 
-    return result;
-}
-
-IChannel::TPtr TRetriableChannel::GetChannel() const
-{
-    return Channel;
+    return retriableRequest->Send();
 }
 
 void TRetriableChannel::Terminate()
 {
-    Channel->Terminate();
+    UnderlyingChannel_->Terminate();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
