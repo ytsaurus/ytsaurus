461581f7fc52526162866285d0cdc1178350ac19
Dmitry Panin <panin@yandex-team.ru>
2011-11-15 18:02:10 +0400

    Unittests for Rpc

--- yt/unittests/bus_ut.cpp
+++ yt/unittests/bus_ut.cpp
@@ -77,10 +77,10 @@ public:
 
 TEST(TBusTest, OK)
 {
-    TBusServer::TPtr listener = New<TBusServer>(2000, New<TEmptyBusHandler>());
-    TBusClient::TPtr client = New<TBusClient>("localhost:2000");
-    IBus::TPtr bus = client->CreateBus(~New<TEmptyBusHandler>());
-    IMessage::TPtr message = CreateMessage(1);
+    auto listener = New<TBusServer>(2000, New<TEmptyBusHandler>());
+    auto client = New<TBusClient>("localhost:2000");
+    auto bus = client->CreateBus(~New<TEmptyBusHandler>());
+    auto message = CreateMessage(1);
     auto result = bus->Send(message)->Get();
     EXPECT_EQ(IBus::ESendResult::OK, result);
     listener->Terminate();
@@ -88,24 +88,24 @@ TEST(TBusTest, OK)
 
 TEST(TBusTest, Failed)
 {
-    TBusClient::TPtr client = New<TBusClient>("localhost:2000");
-    IBus::TPtr bus = client->CreateBus(~New<TEmptyBusHandler>());
-    IMessage::TPtr message = CreateMessage(1);
+    auto client = New<TBusClient>("localhost:2000");
+    auto bus = client->CreateBus(~New<TEmptyBusHandler>());
+    auto message = CreateMessage(1);
     auto result = bus->Send(message)->Get();
     EXPECT_EQ(IBus::ESendResult::Failed, result);
 }
 
 TEST(TBusTest, Reply)
 {
-    TBusServer::TPtr listener = New<TBusServer>(2000, ~New<TReplying42BusHandler>());
-    TBusClient::TPtr client = New<TBusClient>("localhost:2000");
-    TChecking42BusHandler::TPtr handler = New<TChecking42BusHandler>();
-    IBus::TPtr bus = client->CreateBus(~handler);
-    IMessage::TPtr message = CreateMessage(1);
-    bus->Send(message);
+    auto listener = New<TBusServer>(2000, ~New<TReplying42BusHandler>());
+    auto client = New<TBusClient>("localhost:2000");
+    auto handler = New<TChecking42BusHandler>();
+    auto bus = client->CreateBus(~handler);
+    auto message = CreateMessage(1);
+    bus->Send(message)->Get();
 
     if(!handler->Event_.Wait(2000)) {
-        EXPECT_TRUE(false);
+        EXPECT_IS_TRUE(false);
     }
 
     listener->Terminate();
---
+++ yt/unittests/rpc_ut.cpp
@@ -0,0 +1,191 @@
+#include "rpc_ut.pb.h"
+
+#include <yt/ytlib/bus/bus.h>
+#include <yt/ytlib/rpc/client.h>
+#include <yt/ytlib/rpc/server.h>
+#include <yt/ytlib/rpc/service.h>
+
+#include <contrib/testing/framework.h>
+
+namespace NYT {
+namespace NRpc {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TMyProxy
+    : public TProxyBase
+{
+public:
+    typedef TIntrusivePtr<TMyProxy> TPtr;
+
+    static const Stroka ServiceName;
+
+    TMyProxy(IChannel::TPtr channel)
+        : TProxyBase(channel, ServiceName)
+    { }
+
+    RPC_PROXY_METHOD(NMyRpc, SomeCall);
+    RPC_PROXY_METHOD(NMyRpc, EmptyCall);
+    RPC_PROXY_METHOD(NMyRpc, CustomMessageError);
+    RPC_PROXY_METHOD(NMyRpc, NotRegistredCall);
+};
+
+const Stroka TMyProxy::ServiceName = "RpcUT";
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TNonExistingServiceProxy
+    : public TProxyBase
+{
+public:
+    typedef TIntrusivePtr<TNonExistingServiceProxy> TPtr;
+
+    static const Stroka ServiceName;
+
+    TNonExistingServiceProxy(IChannel::TPtr channel)
+        : TProxyBase(channel, ServiceName)
+    { }
+
+    RPC_PROXY_METHOD(NMyRpc, EmptyCall);
+};
+
+const Stroka TNonExistingServiceProxy::ServiceName = "NonExistingService";
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TMyService
+    : public TServiceBase
+{
+public:
+    typedef TIntrusivePtr<TMyService> TPtr;
+    typedef TMyService TThis;
+    TMyService(IInvoker* invoker)
+        : TServiceBase(
+            invoker,
+            TMyProxy::ServiceName,
+            "Main")
+    {
+        RegisterMethod(RPC_SERVICE_METHOD_DESC(SomeCall));
+        RegisterMethod(RPC_SERVICE_METHOD_DESC(EmptyCall));
+        RegisterMethod(RPC_SERVICE_METHOD_DESC(CustomMessageError));
+    }
+
+    RPC_SERVICE_METHOD_DECL(NMyRpc, SomeCall);
+    RPC_SERVICE_METHOD_DECL(NMyRpc, EmptyCall);
+    RPC_SERVICE_METHOD_DECL(NMyRpc, CustomMessageError);
+    RPC_SERVICE_METHOD_DECL(NMyRpc, NotRegistredCall);
+
+};
+
+RPC_SERVICE_METHOD_IMPL(TMyService, SomeCall)
+{
+    int a = request->GetA();
+    response->SetB(a + 100);
+    context->Reply();
+}
+
+RPC_SERVICE_METHOD_IMPL(TMyService, EmptyCall)
+{ }
+
+RPC_SERVICE_METHOD_IMPL(TMyService, NotRegistredCall)
+{
+    context->Reply();
+}
+
+RPC_SERVICE_METHOD_IMPL(TMyService, CustomMessageError)
+{
+    context->Reply(TError(42, "Some Error"));
+}
+
+class TRpcTest
+    : public ::testing::Test
+{
+    TServer::TPtr Server;
+
+public:
+    virtual void SetUp()
+    {
+        Server = New<TServer>(2000);
+        auto queue = New<TActionQueue>();
+        Server->RegisterService(New<TMyService>(~queue->GetInvoker()));
+        Server->Start();
+    }
+
+    virtual void TearDown()
+    {
+        Server->Stop();
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST_F(TRpcTest, Send)
+{
+    auto proxy = new TMyProxy(New<TChannel>("localhost:2000"));
+    auto request = proxy->SomeCall();
+    request->SetA(42);
+    auto result = request->Invoke();
+    auto response = result->Get();
+
+    EXPECT_IS_TRUE(response->IsOK());
+    EXPECT_EQ(142, response->GetB());
+}
+
+// Now test different types of errors
+TEST_F(TRpcTest, TransportError)
+{
+    auto proxy = new TMyProxy(New<TChannel>("localhost:2001"));
+    auto request = proxy->EmptyCall();
+    auto result = request->Invoke();
+    auto response = result->Get();
+
+    EXPECT_EQ(EErrorCode::TransportError, response->GetErrorCode());
+}
+
+// TODO: uncomment this when YT-276 is fixed
+//TEST_F(TRpcTest, NoService)
+//{
+//    auto proxy = new TNonExistingServiceProxy(New<TChannel>("localhost:2000"));
+//    auto request = proxy->EmptyCall();
+//    auto result = request->Invoke();
+//    auto response = result->Get();
+
+//    EXPECT_EQ(EErrorCode::NoService, response->GetErrorCode());
+//}
+
+//TEST_F(TRpcTest, NoMethod)
+//{
+//    auto proxy = new TMyProxy(New<TChannel>("localhost:2000"));
+//    auto request = proxy->NotRegistredCall();
+//    auto result = request->Invoke();
+//    auto response = result->Get();
+
+//    EXPECT_EQ(EErrorCode::NoMethod, response->GetErrorCode());
+//}
+
+TEST_F(TRpcTest, Timeout)
+{
+    auto proxy = new TMyProxy(New<TChannel>("localhost:2000"));
+    auto request = proxy->EmptyCall();
+    auto result = request->Invoke(TDuration::Seconds(1));
+    auto response = result->Get();
+
+    EXPECT_EQ(EErrorCode::Timeout, response->GetErrorCode());
+}
+
+TEST_F(TRpcTest, CustomMessage)
+{
+    auto proxy = new TMyProxy(New<TChannel>("localhost:2000"));
+    auto request = proxy->CustomMessageError();
+    auto result = request->Invoke();
+    auto response = result->Get();
+
+    EXPECT_EQ(42, response->GetErrorCode());
+    EXPECT_EQ("Some Error", response->GetError().GetMessage());
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NRpc
+} // namespace NYT
