71ebdd01db724b3e375b308b9d14787ed2ba4231
Michael Roizner <roizner@yandex-team.ru>
2011-11-16 13:35:22 +0400

    SetDefaults in configs

--- yt/ytlib/misc/new_config.cpp
+++ yt/ytlib/misc/new_config.cpp
@@ -8,20 +8,19 @@ TConfigBase::~TConfigBase()
 
 void TConfigBase::Load(NYTree::INode* node, const Stroka& path)
 {
+    YASSERT(node != NULL);
     NYTree::IMapNode::TPtr mapNode;
-    if (node != NULL) {
-        try {
-            mapNode = node->AsMap();
-        } catch(...) {
-            ythrow yexception()
-                << Sprintf("Config can be loaded only from map node (Path: %s, InnerException: %s)",
-                    ~path, ~CurrentExceptionMessage());
-        }
+    try {
+        mapNode = node->AsMap();
+    } catch(...) {
+        ythrow yexception()
+            << Sprintf("Config can be loaded only from map node (Path: %s, InnerException: %s)",
+                ~path, ~CurrentExceptionMessage());
     }
     FOREACH (auto pair, Parameters) {
         auto name = pair.First();
         Stroka childPath = path + "/" + name;
-        auto child = ~mapNode != NULL ? mapNode->FindChild(name) : NULL;
+        auto child = mapNode->FindChild(name); // can be NULL
         pair.Second()->Load(~child, childPath);
     }
 }
@@ -30,6 +29,15 @@ void TConfigBase::Validate(const Stroka& path) const
 {
     FOREACH (auto pair, Parameters) {
         pair.Second()->Validate(path + "/" + pair.First());
+    }
+}
+
+void TConfigBase::SetDefaults(bool skipRequiredParameters, const Stroka& path)
+{
+    FOREACH (auto pair, Parameters) {
+        auto name = pair.First();
+        Stroka childPath = path + "/" + name;
+        pair.Second()->SetDefaults(skipRequiredParameters, childPath);
     }
 }
 
