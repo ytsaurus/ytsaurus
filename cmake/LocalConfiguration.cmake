b103edb28f8722120ddcd1c106bc152db8ed525d
Petr Ovtchenkov <yeti@yandex-team.ru>
2011-11-25 23:07:09 +0400

    CMake rules evolution.
    Build on Linux/gcc (all, except yt/experiments).
    
    In many cases used shared libraries.
    
    Shared libraries situated within /lib,
    executables in /bin.

---
+++ cmake/LocalConfiguration.cmake
@@ -0,0 +1,99 @@
+
+################################################################################
+# Enforce developer to specify build type.
+
+if (CMAKE_BUILD_TYPE STREQUAL "")
+    set(CMAKE_BUILD_TYPE "RelWithDebInfo" CACHE STRING "Choose the type of build, options are: None (CMAKE_CXX_FLAGS or CMAKE_C_FLAGS used) Debug Release RelWithDebInfo MinSizeRel." FORCE)
+endif()
+
+# Configure compiler options for msvc.
+if (MSVC)
+    message(STATUS "Looks like we're using msvc...")
+
+    if (MSVC_VERSION LESS 1600)
+        message(FATAL_ERROR "msvc >= 10.0 is mandatory due to C++11 usage")
+    endif()
+endif()
+
+# Configure compiler options for g++.
+if (CMAKE_COMPILER_IS_GNUCXX)
+    message(STATUS "Looks like we're using gcc...")
+
+    execute_process(
+        COMMAND ${CMAKE_CXX_COMPILER} -dumpversion
+        OUTPUT_VARIABLE GCC_VERSION
+    )
+
+    if (GCC_VERSION VERSION_LESS 4.5)
+        message(FATAL_ERROR "g++ >= 4.5.0 is mandatory due to C++11 usage")
+    endif()
+
+    #check_cxx_compiler_flag(-std=c++0x GCC_CXX0X)
+    #if (NOT GCC_CXX0X)
+    #    message(FATAL_ERROR "g++ lacks support of -std=c++0x")
+    #endif()
+
+    #check_cxx_compiler_flag(-fvisibility=hidden GCC_VISIBILITY)
+    #if (NOT GCC_VISIBILITY)
+    #    message(FATAL_ERROR "g++ lacks support of -fvisibility=hidden")
+    #endif()
+
+    # add_definitions(-std=c++0x)
+    #SET( CMAKE_CXX_FLAGS  "-std=c++0x"
+    #     CACHE STRING "C++ compiler flags" FORCE )
+    #add_definitions(-fvisibility=hidden)
+    #add_definitions(-fvisibility-inlines-hidden)
+
+    #set(XCODE_ATTRIBUTE_GCC_SYMBOLS_PRIVATE_EXTERN "YES")
+    #set(XCODE_ATTRIBUTE_GCC_INLINES_ARE_PRIVATE_EXTERN "YES")
+endif()
+
+################################################################################
+# Configure build.
+
+option(YT_BUILD_EXPERIMENTS "Build experiments" TRUE)
+option(YT_BUILD_TESTS "Build tests" TRUE)
+option(YT_BUILD_WITH_STLPORT "Build with STLport" TRUE)
+
+if (CMAKE_COMPILER_IS_GNUCXX)
+  if ( CMAKE_BUILD_TYPE STREQUAL "" )
+    if (YT_BUILD_WITH_STLPORT)
+      SET( CMAKE_CXX_FLAGS "-std=gnu++0x -pthread -I${CMAKE_SOURCE_DIR}/extern/STLport/stlport" CACHE STRING "C++ compiler flags" FORCE )
+    else ()
+      SET( CMAKE_CXX_FLAGS "-std=gnu++0x -pthread" CACHE STRING "C++ compiler flags" FORCE )
+    endif()
+    SET( CMAKE_C_FLAGS "-pthread" CACHE STRING "C compiler flags" FORCE )
+  endif()
+
+  if ( CMAKE_BUILD_TYPE STREQUAL "Release" )
+    if (YT_BUILD_WITH_STLPORT)
+      SET( CMAKE_CXX_FLAGS_RELEASE  "-std=gnu++0x -pthread -I${CMAKE_SOURCE_DIR}/extern/STLport/stlport" CACHE STRING "C++ compiler flags" FORCE )
+    else ()
+      SET( CMAKE_CXX_FLAGS_RELEASE  "-std=gnu++0x -pthread" CACHE STRING "C++ compiler flags" FORCE )
+    endif()
+    SET( CMAKE_C_FLAGS_RELEASE  "-pthread" CACHE STRING "C compiler flags" FORCE )
+  endif()
+
+  if ( CMAKE_BUILD_TYPE STREQUAL "Debug" )
+    if (YT_BUILD_WITH_STLPORT)
+      SET( CMAKE_CXX_FLAGS_DEBUG "-std=gnu++0x -pthread -I${CMAKE_SOURCE_DIR}/extern/STLport/stlport" CACHE STRING "C++ compiler flags" FORCE )
+    else()
+      SET( CMAKE_CXX_FLAGS_DEBUG "-std=gnu++0x -pthread" CACHE STRING "C++ compiler flags" FORCE )
+    endif()
+    SET( CMAKE_C_FLAGS_DEBUG "-pthread" CACHE STRING "C compiler flags" FORCE )
+  endif()
+
+  if ( CMAKE_BUILD_TYPE STREQUAL "RelWithDebInfo" )
+    if (YT_BUILD_WITH_STLPORT)
+      SET( CMAKE_CXX_FLAGS_RELWITHDEBINFO "-std=gnu++0x -pthread -I${CMAKE_SOURCE_DIR}/extern/STLport/stlport" CACHE STRING "C++ compiler flags" FORCE )
+    else()
+      SET( CMAKE_CXX_FLAGS_RELWITHDEBINFO "-std=gnu++0x -pthread" CACHE STRING "C++ compiler flags" FORCE )
+    endif()
+    SET( CMAKE_C_FLAGS_RELWITHDEBINFO  "-pthread" CACHE STRING "C compiler flags" FORCE )
+  endif()
+endif()
+
+if (UNIX)
+  SET( LIBRARY_OUTPUT_PATH ${CMAKE_BINARY_DIR}/lib )
+  SET( EXECUTABLE_OUTPUT_PATH ${CMAKE_BINARY_DIR}/bin )
+endif()
---
+++ cmake/LocalFunctions.cmake
@@ -0,0 +1,32 @@
+
+# Generate C++ .h, .cc from ptotobuf description
+#   proto  - .proto file
+#   srcgen - name of CMake variable (possibly with list of sources)
+#            where names of .pb.h and .pb.cc will be added
+
+function( PROTOC proto srcgen )
+  get_filename_component( rp ${proto} PATH )
+  if ( "${rp}" STREQUAL "" )
+    SET( rp ${CMAKE_CURRENT_SOURCE_DIR} )
+    SET( ap_proto ${rp}/${proto} )
+  else()
+    SET( ap_proto ${proto} )
+  endif()
+  get_filename_component( basename ${proto} NAME_WE )
+  string( REPLACE "${CMAKE_SOURCE_DIR}" "" notop "${rp}" )
+
+  # command "protoc blah-blah-blah"
+  SET( _protoc_ ${CMAKE_BINARY_DIR}/bin/protoc -I${CMAKE_BINARY_DIR}${notop} --cpp_out=${CMAKE_BINARY_DIR}${notop} --cpp_styleguide_out=${CMAKE_BINARY_DIR}${notop} --plugin=protoc-gen-cpp_styleguide=${CMAKE_BINARY_DIR}/bin/cpp_styleguide ${ap_proto} )
+
+  # add generated .pb.h and .pb.cc into source list ${${srcgen}}
+  SET( ${srcgen} ${${srcgen}} ${CMAKE_BINARY_DIR}${notop}/${basename}.pb.h ${CMAKE_BINARY_DIR}${notop}/${basename}.pb.cc PARENT_SCOPE )
+
+  # custom command, how to generate .pb.h and .pb.cc
+  add_custom_command(
+    OUTPUT ${CMAKE_BINARY_DIR}${notop}/${basename}.pb.h ${CMAKE_BINARY_DIR}${notop}/${basename}.pb.cc
+    COMMAND ${_protoc_}
+    MAIN_DEPENDENCY ${proto}
+    WORKING_DIRECTORY ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT "Generating protobuf"
+  )
+endfunction( PROTOC )
