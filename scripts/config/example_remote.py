aa65d76221edf39fff21e1af0aa529792f49d743
Maxim Babenko <babenko@ytsaurus.tech>
2011-12-12 10:43:15 +0100

    Updated remote scripts

--- scripts/config/example_remote.py
+++ scripts/config/example_remote.py
@@ -32,7 +32,18 @@ MasterAddresses = opts.limit_iter('--masters', ['meta01-00%dg:%d' % (i, Port) fo
 class Base(AggrBase):
     path = opts.get_string('--name', 'control')
     base_dir = '/yt/disk1/'
-    libs = ['/home/yt/build/debug/extern/STLport/build/cmake/libstlport.so.5.2']
+    libs = [
+        '/home/yt/build/lib/libstlport.so.5.2',
+        '/home/yt/build/lib/libsnappy.so.1.0',
+        '/home/yt/build/lib/libyajl.so.2',
+        '/home/yt/build/lib/libminilzo.so.2.0',
+        '/home/yt/build/lib/liblz4.so.0.1',
+        '/home/yt/build/lib/libfastlz.so.0.1',
+        '/home/yt/build/lib/libjson.so',
+        '/home/yt/build/lib/libarczlib.so.1.2.3',
+        '/home/yt/build/lib/libqmisc.so',
+        '/home/yt/build/lib/libNetLiba.so'
+    ]
     
     def get_log(cls, fd):
         print >>fd, shebang
@@ -44,24 +55,22 @@ class Server(Base, RemoteServer):
 class Master(Server):
     base_dir = './'
     address = Subclass(MasterAddresses)
-    params = Template('--cell-master --config %(config_path)s --port %(port)d --id %(__name__)s --new_config %(yson_config_path)s')
+    params = Template('--cell-master --config %(config_path)s --old_config %(old_config_path)s --port %(port)d --id %(__name__)s')
 
     config = Template({
-        'MetaState' : {
-	        'Cell' : {
-    	        'Addresses' : MasterAddresses
-        	},
-            'SnapshotLocation' : '%(work_dir)s/snapshots',
-            'LogLocation' : '%(work_dir)s/logs',
-            'MaxChangesBetweenSnapshots' : 1000
+        'meta_state' : {
+            'cell' : {
+                'addresses' : MasterAddresses
+            },
+            'snapshot_path' : '%(work_dir)s/snapshots',
+            'log_path' : '%(work_dir)s/logs',
+            'max_changes_between_snapshots' : 1000
         },            
         'Logging' : Logging
     })
     
     def do_run(cls, fd):
         print >>fd, shebang
-        print >>fd, wrap_cmd('mkdir -p %s' % cls.config['MetaState']['SnapshotLocation'])
-        print >>fd, wrap_cmd('mkdir -p %s' % cls.config['MetaState']['LogLocation'])
         print >>fd, ulimit
         print >>fd, cls.export_ld_path
         print >>fd, wrap_cmd(cls.run_tmpl)
@@ -69,8 +78,8 @@ class Master(Server):
     def do_clean(cls, fd):
         print >>fd, shebang
         print >>fd, 'rm -f %s' % cls.log_path
-        print >>fd, 'rm %s/*' % cls.config['MetaState']['SnapshotLocation']
-        print >>fd, 'rm %s/*' % cls.config['MetaState']['LogLocation']
+        print >>fd, 'rm %s/*' % cls.config['meta_state']['snapshot_path']
+        print >>fd, 'rm %s/*' % cls.config['meta_state']['log_location']
     
     
 class Holder(Server):
@@ -79,19 +88,30 @@ class Holder(Server):
     host = Template('n01-04%(groupid)d%(nodeid)dg')
     port = Port
     
-    params = Template('--chunk-holder --config %(config_path)s --port %(port)d')
+    params = Template('--chunk-holder --config %(config_path)s --old_config %(old_config_path)s --port %(port)d')
     
     config = Template({ 
-        'Masters' : { 'Addresses' : MasterAddresses },
-        'Locations' : ['/yt/disk2/node', '/yt/disk3/node', '/yt/disk4/node'],
+        'masters' : { 'addresses' : MasterAddresses },
+        'storage_locations' : [
+            { 'path' : '/yt/disk1/chunk_storage' },
+            { 'path' : '/yt/disk2/chunk_storage' },
+            { 'path' : '/yt/disk3/chunk_storage' },
+            { 'path' : '/yt/disk4/chunk_storage' }
+        ],
+        'cache_location' : {
+            'path' : '/yt/disk1/chunk_cache'
+        },
+        'cache_remote_reader' : { },
+        'cache_sequential_reader' : { },
         'Logging' : Logging
     })
     
     def do_clean(cls, fd):
         print >>fd, shebang
         print >>fd, 'rm -f %s' % cls.log_path
-        for location in cls.config['Locations']:
+        for location in cls.config['storage_locations']['path']:
             print >>fd, 'rm -rf %s' % location
+        print >>fd, 'rm -rf %s' % cls.config['cache_location']['path']
 
 class Client(Base, RemoteNode):
     bin_path = '/home/yt/src/yt/experiments/send_chunk/send_chunk'
