fb132d911ffa6954ed0bcdeef473287dc8660b9b
Dmitry Panin <panin@yandex-team.ru>
2011-12-13 19:20:08 +0400

    Moved election to new config

--- yt/ytlib/logging/log_manager.cpp
+++ yt/ytlib/logging/log_manager.cpp
@@ -178,92 +178,92 @@ void TLogManager::TConfig::ConfigureRules()
     }
 }
 
-void TLogManager::TConfig::ConfigureWriters(const TJsonObject* root)
-{
-    if (root == NULL) {
-        ythrow yexception() << "TJsonObject of Writers is NULL";
-    }
+//void TLogManager::TConfig::ConfigureWriters(const TJsonObject* root)
+//{
+//    if (root == NULL) {
+//        ythrow yexception() << "TJsonObject of Writers is NULL";
+//    }
 
-    const TJsonArray* writers = static_cast<const TJsonArray*>(root);
-    for (int i = 0; i < writers->Length(); ++i) {
-        Stroka name, type, pattern;
-        const TJsonObject* item = writers->Item(i);
-        if(!TryRead(item, L"Name", &name)) {
-            ythrow yexception() <<
-                Sprintf("Couldn't read property Name at writer #%d", i);
-        }
+//    const TJsonArray* writers = static_cast<const TJsonArray*>(root);
+//    for (int i = 0; i < writers->Length(); ++i) {
+//        Stroka name, type, pattern;
+//        const TJsonObject* item = writers->Item(i);
+//        if(!TryRead(item, L"Name", &name)) {
+//            ythrow yexception() <<
+//                Sprintf("Couldn't read property Name at writer #%d", i);
+//        }
 
-        if (Writers.find(name) != Writers.end()) {
-            ythrow yexception() <<
-                Sprintf("Writer %s is already defined", ~name);
-        }
+//        if (Writers.find(name) != Writers.end()) {
+//            ythrow yexception() <<
+//                Sprintf("Writer %s is already defined", ~name);
+//        }
 
-        if(!TryRead(item, L"Pattern", &pattern)) {
-            ythrow yexception() <<
-                Sprintf("Couldn't read property Pattern at writer %s", ~name);
-        }
+//        if(!TryRead(item, L"Pattern", &pattern)) {
+//            ythrow yexception() <<
+//                Sprintf("Couldn't read property Pattern at writer %s", ~name);
+//        }
 
-        Stroka errorMessage;
-        if (!ValidatePattern(pattern, & errorMessage)) {
-            ythrow yexception() <<
-                Sprintf("Invalid pattern at writer %s\n%s", ~name, ~errorMessage);
-        }
+//        Stroka errorMessage;
+//        if (!ValidatePattern(pattern, & errorMessage)) {
+//            ythrow yexception() <<
+//                Sprintf("Invalid pattern at writer %s\n%s", ~name, ~errorMessage);
+//        }
 
-        if (!TryRead(item, L"Type", &type)) {
-            ythrow yexception() <<
-                Sprintf("Couldn't read property Type at writer %s", ~name);
-        }
+//        if (!TryRead(item, L"Type", &type)) {
+//            ythrow yexception() <<
+//                Sprintf("Couldn't read property Type at writer %s", ~name);
+//        }
 
-        if (type == "File") {
-            Stroka fileName;
-            if(!TryRead(item, L"FileName", &fileName)) {
-                ythrow yexception() <<
-                    Sprintf("Couldn't read property FileName at writer %s", ~name);
-            }
-            Writers[name] = New<TFileLogWriter>(fileName, pattern);
-        } else if (type == "StdErr") {
-            Writers[name] = New<TStdErrLogWriter>(pattern);
-        } else if (type == "StdOut") {
-            Writers[name] = New<TStdOutLogWriter>(pattern);
-        } else {
-            ythrow yexception() <<
-                Sprintf("%s is unknown type of writer", ~type);
-        }
-    }
-}
+//        if (type == "File") {
+//            Stroka fileName;
+//            if(!TryRead(item, L"FileName", &fileName)) {
+//                ythrow yexception() <<
+//                    Sprintf("Couldn't read property FileName at writer %s", ~name);
+//            }
+//            Writers[name] = New<TFileLogWriter>(fileName, pattern);
+//        } else if (type == "StdErr") {
+//            Writers[name] = New<TStdErrLogWriter>(pattern);
+//        } else if (type == "StdOut") {
+//            Writers[name] = New<TStdOutLogWriter>(pattern);
+//        } else {
+//            ythrow yexception() <<
+//                Sprintf("%s is unknown type of writer", ~type);
+//        }
+//    }
+//}
 
-void TLogManager::TConfig::ConfigureRules(const TJsonObject* root)
-{
-    if (root == NULL) {
-        ythrow yexception() << "TJsonObject of Rules is NULL";
-    }
+//void TLogManager::TConfig::ConfigureRules(const TJsonObject* root)
+//{
+//    if (root == NULL) {
+//        ythrow yexception() << "TJsonObject of Rules is NULL";
+//    }
 
-    const TJsonArray* rules = static_cast<const TJsonArray*>(root);
-    for (int i = 0; i < rules->Length(); ++i) {
-        TRule rule;
-        const TJsonObject* item = rules->Item(i);
-        yvector<Stroka> categories;
-        if(!TryRead(item, L"Categories", &categories)) {
-            ythrow yexception() <<
-                Sprintf("Couldn't read property Categories at Rule #%d", i);
-        }
-        if (categories.size() == 1 && categories[0] == AllCategoriesName) {
-            rule.AllCategories = true;
-        } else {
-            rule.AllCategories = false;
-            rule.Categories.insert(categories.begin(), categories.end());
-        }
+//    const TJsonArray* rules = static_cast<const TJsonArray*>(root);
+//    for (int i = 0; i < rules->Length(); ++i) {
+//        TRule rule;
+//        const TJsonObject* item = rules->Item(i);
+//        yvector<Stroka> categories;
+//        if(!TryRead(item, L"Categories", &categories)) {
+//            ythrow yexception() <<
+//                Sprintf("Couldn't read property Categories at Rule #%d", i);
+//        }
+//        if (categories.size() == 1 && categories[0] == AllCategoriesName) {
+//            rule.AllCategories = true;
+//        } else {
+//            rule.AllCategories = false;
+//            rule.Categories.insert(categories.begin(), categories.end());
+//        }
 
-        ReadEnum<ELogLevel>(item, L"MinLevel", &rule.MinLevel, ELogLevel::Minimum);
-        ReadEnum<ELogLevel>(item, L"MaxLevel", &rule.MaxLevel, ELogLevel::Maximum);
-        if (!TryRead(item, L"Writers", &rule.Writers)) {
-            ythrow yexception() <<
-                Sprintf("Couldn't read property Writers at Rule #%d", i);
-        }
-        ValidateRule(rule);
-        Rules.push_back(rule);
-    }
-}
+//        ReadEnum<ELogLevel>(item, L"MinLevel", &rule.MinLevel, ELogLevel::Minimum);
+//        ReadEnum<ELogLevel>(item, L"MaxLevel", &rule.MaxLevel, ELogLevel::Maximum);
+//        if (!TryRead(item, L"Writers", &rule.Writers)) {
+//            ythrow yexception() <<
+//                Sprintf("Couldn't read property Writers at Rule #%d", i);
+//        }
+//        ValidateRule(rule);
+//        Rules.push_back(rule);
+//    }
+//}
 
 void TLogManager::TConfig::ValidateRule(const TRule& rule)
 {
@@ -301,14 +301,14 @@ TLogManager::TConfig::TConfig()
     Rules.push_back(fileRule);
 }
 
-TLogManager::TConfig::TConfig(const TJsonObject* root)
-{
-    if (root == NULL) {
-        ythrow yexception() << "TJsonObject of Config is NULL";
-    }
-    ConfigureWriters(root->Value(L"Writers"));
-    ConfigureRules(root->Value(L"Rules"));
-}
+//TLogManager::TConfig::TConfig(const TJsonObject* root)
+//{
+//    if (root == NULL) {
+//        ythrow yexception() << "TJsonObject of Config is NULL";
+//    }
+//    ConfigureWriters(root->Value(L"Writers"));
+//    ConfigureRules(root->Value(L"Rules"));
+//}
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -445,15 +445,15 @@ NYT::NLog::ELogLevel TLogManager::GetMinLevel(Stroka category)
     return level;
 }
 
-void TLogManager::Configure(TJsonObject* root)
-{
-    auto configuration = New<TConfig>(root);
-    {
-        TGuard<TSpinLock> guard(&SpinLock);
-        Configuration = configuration;
-        ConfigVersion += 1;
-    }
-}
+//void TLogManager::Configure(TJsonObject* root)
+//{
+//    auto configuration = New<TConfig>(root);
+//    {
+//        TGuard<TSpinLock> guard(&SpinLock);
+//        Configuration = configuration;
+//        ConfigVersion += 1;
+//    }
+//}
 
 void TLogManager::Configure(const Stroka& fileName, const Stroka& rootPath)
 {
--- yt/ytlib/misc/config.h
+++ yt/ytlib/misc/config.h
@@ -13,147 +13,147 @@
 namespace NYT {
 
 
-////////////////////////////////////////////////////////////////////////////////
-// JSON configs helper
+//////////////////////////////////////////////////////////////////////////////////
+//// JSON configs helper
 
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 
-inline bool TryRead(
-    const TJsonObject* json,
-    const wchar_t* name,
-    Stroka* result)
-{
-    const TJsonObject* value = json->Value(name);
-    if (value != NULL) {
-        *result = WideToChar(value->ToString());
-        return true;
-    }
-    return false;
-}
+//inline bool TryRead(
+//    const TJsonObject* json,
+//    const wchar_t* name,
+//    Stroka* result)
+//{
+//    const TJsonObject* value = json->Value(name);
+//    if (value != NULL) {
+//        *result = WideToChar(value->ToString());
+//        return true;
+//    }
+//    return false;
+//}
 
-inline Stroka Read(
-    TJsonObject* json,
-    const wchar_t* name)
-{
-    Stroka result;
-    if (!TryRead(json, name, &result))
-        ythrow yexception() << Sprintf("Error reading string %S", name);
-    return result;
-}
+//inline Stroka Read(
+//    TJsonObject* json,
+//    const wchar_t* name)
+//{
+//    Stroka result;
+//    if (!TryRead(json, name, &result))
+//        ythrow yexception() << Sprintf("Error reading string %S", name);
+//    return result;
+//}
 
-inline Stroka Read(
-    TJsonObject* json,
-    const wchar_t* name,
-    Stroka defaultValue)
-{
-    Stroka result = defaultValue;
-    TryRead(json, name, &result);
-    return result;
-}
+//inline Stroka Read(
+//    TJsonObject* json,
+//    const wchar_t* name,
+//    Stroka defaultValue)
+//{
+//    Stroka result = defaultValue;
+//    TryRead(json, name, &result);
+//    return result;
+//}
 
-////////////////////////////////////////////////////////////////////////////////
+//////////////////////////////////////////////////////////////////////////////////
 
-//TODO: implement other read functions with defaultValue
-inline bool TryRead(
-    const TJsonObject* jsonConfig,
-    const wchar_t* paramName,
-    int* param)
-{
-    const TJsonObject* value = jsonConfig->Value(paramName);
-    if (value != NULL) {
-        *param = value->ToInt();
-        return true;
-    }
-    return false;
-}
+////TODO: implement other read functions with defaultValue
+//inline bool TryRead(
+//    const TJsonObject* jsonConfig,
+//    const wchar_t* paramName,
+//    int* param)
+//{
+//    const TJsonObject* value = jsonConfig->Value(paramName);
+//    if (value != NULL) {
+//        *param = value->ToInt();
+//        return true;
+//    }
+//    return false;
+//}
 
-inline bool TryRead(
-    const TJsonObject* jsonConfig,
-    const wchar_t* paramName,
-    double* param)
-{
-    const TJsonObject* value = jsonConfig->Value(paramName);
-    if (value != NULL) {
-        *param = value->ToDouble();
-        return true;
-    }
-    return false;
-}
+//inline bool TryRead(
+//    const TJsonObject* jsonConfig,
+//    const wchar_t* paramName,
+//    double* param)
+//{
+//    const TJsonObject* value = jsonConfig->Value(paramName);
+//    if (value != NULL) {
+//        *param = value->ToDouble();
+//        return true;
+//    }
+//    return false;
+//}
 
-inline bool TryRead(
-    const TJsonObject* jsonConfig,
-    const wchar_t* paramName,
-    bool* param)
-{
-    const TJsonObject* value = jsonConfig->Value(paramName);
-    if (value != NULL) {
-        *param = value->ToBoolean();
-        return true;
-    }
-    return false;
-}
+//inline bool TryRead(
+//    const TJsonObject* jsonConfig,
+//    const wchar_t* paramName,
+//    bool* param)
+//{
+//    const TJsonObject* value = jsonConfig->Value(paramName);
+//    if (value != NULL) {
+//        *param = value->ToBoolean();
+//        return true;
+//    }
+//    return false;
+//}
 
-inline bool TryRead(
-    const TJsonObject* jsonConfig,
-    const wchar_t* paramName,
-    yvector<Stroka>* param)
-{
-    const TJsonObject* value = jsonConfig->Value(paramName);
-    if (value != NULL) {
-        const TJsonArray* array = static_cast<const TJsonArray*>(value);
-        param->resize(array->Length());
-        for (i32 i = 0; i < param->ysize(); ++i) {
-            (*param)[i] = WideToChar(array->Item(i)->ToString());
-        }
-        return true;
-    }
-    return false;
-}
+//inline bool TryRead(
+//    const TJsonObject* jsonConfig,
+//    const wchar_t* paramName,
+//    yvector<Stroka>* param)
+//{
+//    const TJsonObject* value = jsonConfig->Value(paramName);
+//    if (value != NULL) {
+//        const TJsonArray* array = static_cast<const TJsonArray*>(value);
+//        param->resize(array->Length());
+//        for (i32 i = 0; i < param->ysize(); ++i) {
+//            (*param)[i] = WideToChar(array->Item(i)->ToString());
+//        }
+//        return true;
+//    }
+//    return false;
+//}
 
-template <class T>
-inline void ReadEnum(
-    const TJsonObject* jsonConfig,
-    const wchar_t* paramName,
-    T* param)
-{
-    Stroka value;
-    if (TryRead(jsonConfig, paramName, &value)) {
-        *param = T::FromString(value);
-    } else {
-        ythrow yexception() << Sprintf("Error reading enumeration %s", paramName);
-    }
-}
+//template <class T>
+//inline void ReadEnum(
+//    const TJsonObject* jsonConfig,
+//    const wchar_t* paramName,
+//    T* param)
+//{
+//    Stroka value;
+//    if (TryRead(jsonConfig, paramName, &value)) {
+//        *param = T::FromString(value);
+//    } else {
+//        ythrow yexception() << Sprintf("Error reading enumeration %s", paramName);
+//    }
+//}
 
-template <class T>
-inline bool ReadEnum(
-    const TJsonObject* jsonConfig,
-    const wchar_t* paramName,
-    T* param,
-    T defaultValue)
-{
-    Stroka value;
-    if (TryRead(jsonConfig, paramName, &value)) {
-        *param = T::FromString(value);
-        return true;
-    } else {
-        *param = defaultValue;
-        return false;
-    }
-}
+//template <class T>
+//inline bool ReadEnum(
+//    const TJsonObject* jsonConfig,
+//    const wchar_t* paramName,
+//    T* param,
+//    T defaultValue)
+//{
+//    Stroka value;
+//    if (TryRead(jsonConfig, paramName, &value)) {
+//        *param = T::FromString(value);
+//        return true;
+//    } else {
+//        *param = defaultValue;
+//        return false;
+//    }
+//}
 
-inline TJsonObject* GetSubTree(TJsonObject* object, Stroka rootPath) {
-    VectorWtrok path;
-    TJsonObject* root = object;
-    SplitStroku(&path, CharToWide(~rootPath), (wchar16*) ".");
-    for (int i = 0; i < path.ysize(); ++i) {
-        if (root == NULL) {
-            ythrow yexception() <<
-                Sprintf("Couldn't procede path %s", ~WideToUTF8(path[i]));
-        }
-        root = root->Value(path[i]);
-    }
-    return root;
-}
+//inline TJsonObject* GetSubTree(TJsonObject* object, Stroka rootPath) {
+//    VectorWtrok path;
+//    TJsonObject* root = object;
+//    SplitStroku(&path, CharToWide(~rootPath), (wchar16*) ".");
+//    for (int i = 0; i < path.ysize(); ++i) {
+//        if (root == NULL) {
+//            ythrow yexception() <<
+//                Sprintf("Couldn't procede path %s", ~WideToUTF8(path[i]));
+//        }
+//        root = root->Value(path[i]);
+//    }
+//    return root;
+//}
 
 ////////////////////////////////////////////////////////////////////////////////
 
