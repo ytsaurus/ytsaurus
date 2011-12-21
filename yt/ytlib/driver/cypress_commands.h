ce56b15c1d5bbd4b6c40d7e613f569f261bcb91f
Dmitry Panin <panin@yandex-team.ru>
2011-12-21 22:19:23 +0400

    Remove, List, Create commands for driver

--- yt/ytlib/driver/cypress_commands.h
+++ yt/ytlib/driver/cypress_commands.h
@@ -73,6 +73,88 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 
+struct TRemoveRequest
+    : TRequestBase
+{
+    NYTree::TYPath Path;
+
+    TRemoveRequest()
+    {
+        Register("path", Path);
+    }
+};
+
+class TRemoveCommand
+    : public TCommandBase<TRemoveRequest>
+{
+public:
+    TRemoveCommand(IDriverImpl* driverImpl)
+        : TCommandBase(driverImpl)
+    { }
+
+private:
+    virtual void DoExecute(TRemoveRequest* request);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TListRequest
+    : TRequestBase
+{
+    NYTree::TYPath Path;
+    NYTree::INode::TPtr Stream;
+
+    TListRequest()
+    {
+        Register("path", Path);
+        Register("stream", Stream).Default(NULL).CheckThat(~StreamSpecIsValid);
+    }
+};
+
+class TListCommand
+    : public TCommandBase<TListRequest>
+{
+public:
+    TListCommand(IDriverImpl* driverImpl)
+        : TCommandBase(driverImpl)
+    { }
+
+private:
+    virtual void DoExecute(TListRequest* request);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TCreateRequest
+    : TRequestBase
+{
+    NYTree::TYPath Path;
+    NYTree::INode::TPtr Stream;
+    Stroka Type;
+    NYTree::TYson Manifest;
+
+    TCreateRequest()
+    {
+        Register("path", Path);
+        Register("stream", Stream).Default(NULL).CheckThat(~StreamSpecIsValid);
+        Register("type", Type);
+    }
+};
+
+class TCreateCommand
+    : public TCommandBase<TCreateRequest>
+{
+public:
+    TCreateCommand(IDriverImpl* driverImpl)
+        : TCommandBase(driverImpl)
+    { }
+
+private:
+    virtual void DoExecute(TCreateRequest* request);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 } // namespace NDriver
 } // namespace NYT
 
