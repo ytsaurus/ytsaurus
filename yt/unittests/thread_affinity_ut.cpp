97cbc8dc2a4b9baa86bdb5f114844fa7cb7a5512
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2011-12-22 17:18:17 +0400

    Refactored thread_affinity_ut.cpp

--- yt/unittests/thread_affinity_ut.cpp
+++ yt/unittests/thread_affinity_ut.cpp
@@ -14,65 +14,128 @@ namespace NYT {
 namespace {
     class TMyObject
     {
-        DECLARE_THREAD_AFFINITY_SLOT(ServiceThread);
+        DECLARE_THREAD_AFFINITY_SLOT(FirstThread);
+        DECLARE_THREAD_AFFINITY_SLOT(SecondThread);
 
     public:
-        TVoid F() {
-            VERIFY_THREAD_AFFINITY(ServiceThread);
+        TVoid A() {
+            VERIFY_THREAD_AFFINITY(FirstThread);
             return TVoid();
         }
 
-        TVoid G() {
-            VERIFY_THREAD_AFFINITY(ServiceThread);
+        TVoid B() {
+            VERIFY_THREAD_AFFINITY(SecondThread);
+            return TVoid();
+        }
+
+        TVoid C() {
+            VERIFY_THREAD_AFFINITY(FirstThread);
             return TVoid();
         }
     };
+
+#define PROLOGUE() \
+    auto queue1 = New<TActionQueue>(); \
+    auto queue2 = New<TActionQueue>(); \
+    auto invoker1 = queue1->GetInvoker(); \
+    auto invoker2 = queue2->GetInvoker(); \
+
+    void SingleThreadedAccess(TMyObject* object)
+    {
+        PROLOGUE();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker1)->Do()->Get();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker1)->Do()->Get();
+    }
+
+    void UntangledThreadAccess(TMyObject* object)
+    {
+        PROLOGUE();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+    }
+
+    void UntangledThreadAccessToSharedSlot(TMyObject* object)
+    {
+        PROLOGUE();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+        FromMethod(&TMyObject::C, object)->AsyncVia(invoker1)->Do()->Get();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+        FromMethod(&TMyObject::C, object)->AsyncVia(invoker1)->Do()->Get();
+    }
+
+    void TangledThreadAccess1(TMyObject* object)
+    {
+        PROLOGUE();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker1)->Do()->Get();
+    }
+
+    void TangledThreadAccess2(TMyObject* object)
+    {
+        PROLOGUE();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker1)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+
+        FromMethod(&TMyObject::A, object)->AsyncVia(invoker2)->Do()->Get();
+        FromMethod(&TMyObject::B, object)->AsyncVia(invoker2)->Do()->Get();
+    }
+#undef PROLOGUE
 } // namespace <anonymous>
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TThreadAffinityTest : public ::testing::Test
-{ };
-
-TEST_F(TThreadAffinityTest, OneThread)
+TEST(TThreadAffinityTest, SingleThreadedAccess)
 {
-    TMyObject myObject;
-    TActionQueue::TPtr actionQueue = New<TActionQueue>();
-    auto invoker = actionQueue->GetInvoker();
-
-    FromMethod(&TMyObject::F, &myObject)->AsyncVia(invoker)->Do()->Get();
-    FromMethod(&TMyObject::F, &myObject)->AsyncVia(invoker)->Do()->Get();
-    FromMethod(&TMyObject::G, &myObject)->AsyncVia(invoker)->Do()->Get();
-    FromMethod(&TMyObject::F, &myObject)->AsyncVia(invoker)->Do()->Get();
-    FromMethod(&TMyObject::F, &myObject)->AsyncVia(invoker)->Do()->Get();
-    FromMethod(&TMyObject::G, &myObject)->AsyncVia(invoker)->Do()->Get();
+    TMyObject object;
+    SingleThreadedAccess(&object);
 
     SUCCEED();
 }
 
-TEST_F(TThreadAffinityTest, OneFunctionDifferentThreads)
+TEST(TThreadAffinityTest, UntangledThreadAccess)
 {
-    TMyObject myObject;
-    ASSERT_DEATH({
-        TActionQueue::TPtr actionQueue1 = New<TActionQueue>();
-        TActionQueue::TPtr actionQueue2 = New<TActionQueue>();
-        FromMethod(&TMyObject::F, &myObject)->AsyncVia(actionQueue1->GetInvoker())->Do()->Get();
-        FromMethod(&TMyObject::F, &myObject)->AsyncVia(actionQueue2->GetInvoker())->Do()->Get();
-    }, ".*");
+    TMyObject object;
+    UntangledThreadAccess(&object);
 
+    SUCCEED();
 }
 
-TEST_F(TThreadAffinityTest, DifferentFunctionsDifferentThreads)
+TEST(TThreadAffinityTest, UntangledThreadAccessToSharedSlot)
 {
-    TMyObject myObject;
-    ASSERT_DEATH({
-        TActionQueue::TPtr actionQueue1 = New<TActionQueue>();
-        TActionQueue::TPtr actionQueue2 = New<TActionQueue>();
-        FromMethod(&TMyObject::F, &myObject)->AsyncVia(actionQueue1->GetInvoker())->Do()->Get();
-        FromMethod(&TMyObject::G, &myObject)->AsyncVia(actionQueue2->GetInvoker())->Do()->Get();
-    }, ".*");
+    TMyObject object;
+    UntangledThreadAccessToSharedSlot(&object);
+
+    SUCCEED();
 }
 
+TEST(TThreadAffinityDeathTest, TangledThreadAccess1)
+{
+    TMyObject object;
+    ASSERT_DEATH({ TangledThreadAccess1(&object); }, ".*");
+}
+
+TEST(TThreadAffinityDeathTest, TangledThreadAccess2)
+{
+    TMyObject object;
+    ASSERT_DEATH({ TangledThreadAccess2(&object); }, ".*");
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
