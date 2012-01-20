43e7532c11ca936617b3d1f2c2aa02e2e7fbcea4
Maxim Babenko <babenko@ytsaurus.tech>
2012-01-20 13:57:21 +0100

    Write back implemented

--- yt/ytlib/ytree/yson_file_service.cpp
+++ yt/ytlib/ytree/yson_file_service.cpp
@@ -7,9 +7,175 @@
 namespace NYT {
 namespace NYTree {
 
+using namespace NRpc;
+
 ////////////////////////////////////////////////////////////////////////////////
 
-// TOOD: read-only?
+namespace {
+
+// TODO(babenko): consider moving to some general place
+class TReplyInterceptorContext
+    : public IServiceContext
+{
+public:
+    TReplyInterceptorContext(
+        IServiceContext* underlyingContext,
+        IAction* onReply)
+        : UnderlyingContext(underlyingContext)
+        , OnReply(onReply)
+    {
+        YASSERT(underlyingContext);
+        YASSERT(onReply);
+    }
+
+    virtual NBus::IMessage::TPtr GetRequestMessage() const
+    {
+        return UnderlyingContext->GetRequestMessage();
+    }
+
+    virtual const Stroka& GetPath() const
+    {
+        return UnderlyingContext->GetPath();
+    }
+
+    virtual const Stroka& GetVerb() const
+    {
+        return UnderlyingContext->GetVerb();
+    }
+
+    virtual bool IsOneWay() const
+    {
+        return UnderlyingContext->IsOneWay();
+    }
+
+    virtual bool IsReplied() const
+    {
+        return UnderlyingContext->IsReplied();
+    }
+
+    virtual void Reply(const TError& error)
+    {
+        UnderlyingContext->Reply(error);
+        OnReply->Do();
+    }
+
+    virtual TError GetError() const
+    {
+        return UnderlyingContext->GetError();
+    }
+
+    virtual TSharedRef GetRequestBody() const
+    {
+        return UnderlyingContext->GetRequestBody();
+    }
+
+    virtual void SetResponseBody(const TSharedRef& responseBody)
+    {
+        UnderlyingContext->SetResponseBody(responseBody);
+    }
+
+    virtual const yvector<TSharedRef>& RequestAttachments() const
+    {
+        return UnderlyingContext->RequestAttachments();
+    }
+
+    virtual yvector<TSharedRef>& ResponseAttachments()
+    {
+        return UnderlyingContext->ResponseAttachments();
+    }
+
+    virtual void SetRequestInfo(const Stroka& info)
+    {
+       UnderlyingContext->SetRequestInfo(info);
+    }
+
+    virtual Stroka GetRequestInfo() const
+    {
+        return UnderlyingContext->GetRequestInfo();
+    }
+
+    virtual void SetResponseInfo(const Stroka& info)
+    {
+        UnderlyingContext->SetRequestInfo(info);
+    }
+
+    virtual Stroka GetResponseInfo()
+    {
+        return UnderlyingContext->GetRequestInfo();
+    }
+
+    virtual IAction::TPtr Wrap(IAction* action) 
+    {
+        return UnderlyingContext->Wrap(action);
+    }
+
+private:
+    IServiceContext::TPtr UnderlyingContext;
+    IAction::TPtr OnReply;
+
+};
+
+class TWriteBackService
+    : public IYPathService
+{
+public:
+    typedef TIntrusivePtr<TWriteBackService> TPtr;
+
+    TWriteBackService(
+        const Stroka& fileName,
+        INode* root,
+        IYPathService* underlyingService)
+        : FileName(fileName)
+        , Root(root)
+        , UnderlyingService(underlyingService)
+    { }
+
+    virtual TResolveResult Resolve(const TYPath& path, const Stroka& verb)
+    {
+        auto result = UnderlyingService->Resolve(path, verb);
+        if (result.IsHere()) {
+            return TResolveResult::Here(result.GetPath());
+        } else {
+            return TResolveResult::There(
+                ~New<TWriteBackService>(FileName, ~Root, ~UnderlyingService),
+                result.GetPath());
+        }
+    }
+
+    virtual void Invoke(IServiceContext* context)
+    {
+        auto wrappedContext = New<TReplyInterceptorContext>(
+            context,
+            ~FromMethod(&TWriteBackService::SaveFile, TPtr(this)));
+        UnderlyingService->Invoke(~wrappedContext);
+    }
+
+    virtual Stroka GetLoggingCategory() const
+    {
+        return UnderlyingService->GetLoggingCategory();
+    }
+
+private:
+    Stroka FileName;
+    INode::TPtr Root;
+    IYPathService::TPtr UnderlyingService;
+
+    void SaveFile()
+    {
+        try {
+            TOFStream stream(FileName);
+            // TODO(babenko): make format configurable
+            SerializeToYson(~Root, stream, TYsonWriter::EFormat::Pretty);
+        } catch (...) {
+            throw yexception() << Sprintf("Error saving YSON file %s\n%s",
+                ~FileName.Quote(),
+                ~CurrentExceptionMessage());
+        }
+    }
+};
+
+} // namespace <anonymous>
+
 class TYsonFileService
     : public IYPathService
 {
@@ -22,8 +188,15 @@ public:
     {
         UNUSED(verb);
 
-        auto node = LoadFile();
-        return TResolveResult::There(~node, path);
+        auto root = LoadFile();
+        
+        // TODO(babenko): refactor using IYPathService::IsReadOnly
+        auto service =
+            IsReadOnly(verb)
+            ? IYPathService::TPtr(root)
+            : IYPathService::TPtr(New<TWriteBackService>(FileName, ~root, ~root));
+
+        return TResolveResult::There(~service, path);
     }
 
     virtual void Invoke(NRpc::IServiceContext* context)
@@ -47,9 +220,16 @@ private:
            return DeserializeFromYson(&stream);
         } catch (...) {
             throw yexception() << Sprintf("Error loading YSON file %s\n%s",
-                ~FileName,
+                ~FileName.Quote(),
                 ~CurrentExceptionMessage());
         }
+    }
+
+    static bool IsReadOnly(const Stroka& verb)
+    {
+        return
+            verb != "Set" &&
+            verb != "Remove";
     }
 };
 
