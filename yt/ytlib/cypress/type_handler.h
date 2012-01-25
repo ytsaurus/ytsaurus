262024d4d0ab83e2e2423ce771a25c893fa2fc95
Maxim Babenko <babenko@ytsaurus.tech>
2012-01-25 21:02:31 +0100

    Changes refcounted base (hack)

--- yt/ytlib/cypress/type_handler.h
+++ yt/ytlib/cypress/type_handler.h
@@ -21,7 +21,7 @@ struct ICypressNodeProxy;
  *  Behaviors are only created for non-branched nodes.
  */
 struct INodeBehavior
-    : virtual TRefCounted
+    : virtual TExtrinsicRefCounted
 {
     typedef TIntrusivePtr<INodeBehavior> TPtr;
 
