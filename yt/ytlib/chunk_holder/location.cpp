324689a87a0fe64886b48fa8248625162ec0a889
Dmitry Panin <panin@yandex-team.ru>
2012-02-02 15:08:25 +0400

    Changed the byte that is used for defining directory for chunks

--- yt/ytlib/chunk_holder/location.cpp
+++ yt/ytlib/chunk_holder/location.cpp
@@ -113,7 +113,7 @@ int TLocation::GetSessionCount() const
 
 Stroka TLocation::GetChunkFileName(const TChunkId& chunkId) const
 {
-    ui8 firstHashByte = static_cast<ui8>(chunkId.Parts[3] & 0xff);
+    ui8 firstHashByte = static_cast<ui8>(chunkId.Parts[0] & 0xff);
     return NFS::CombinePaths(
         GetPath(),
         Sprintf("%x/%s", firstHashByte, ~chunkId.ToString()));
