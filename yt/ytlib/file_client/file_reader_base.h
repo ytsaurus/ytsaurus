d8f25b41f100119922a433713dcd43f8a980dd03
Petr Ovtchenkov <yeti@yandex-team.ru>
2012-02-03 15:14:54 +0400

    TFileReader[Base]: transactions
    TFileReaderBase is basic chunk reader (without cypress's
    path). It don't do with transaction (expected, that
    transaction is outside it).
    
    TFileReader is based on TFileReaderBase. It introduce
    interaction with cypress and transaction.
    
    Functionality of TFileReader should remains unmodified.

---
+++ yt/ytlib/file_client/file_reader_base.h
@@ -0,0 +1,94 @@
+#pragma once
+
+#include "common.h"
+
+#include <ytlib/misc/thread_affinity.h>
+#include <ytlib/misc/codec.h>
+#include <ytlib/misc/configurable.h>
+#include <ytlib/rpc/channel.h>
+#include <ytlib/transaction_client/transaction.h>
+#include <ytlib/transaction_client/transaction_listener.h>
+#include <ytlib/cypress/cypress_service_proxy.h>
+#include <ytlib/chunk_client/sequential_reader.h>
+#include <ytlib/chunk_client/block_cache.h>
+#include <ytlib/chunk_client/remote_reader.h>
+#include <ytlib/logging/tagged_logger.h>
+
+namespace NYT {
+namespace NFileClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! A client-side facade for reading files.
+/*!
+ *  The client must call #Open and then read the file block-by-block
+ *  calling #Read.
+ */
+class TFileReaderBase
+    : public NTransactionClient::TTransactionListener
+{
+public:
+    typedef TIntrusivePtr<TFileReaderBase> TPtr;
+
+    struct TConfig
+        : public TConfigurable
+    {
+        typedef TIntrusivePtr<TConfig> TPtr;
+    
+        TDuration MasterRpcTimeout;
+        NChunkClient::TSequentialReader::TConfig::TPtr SequentialReader;
+        NChunkClient::TRemoteReaderConfig::TPtr RemoteReader;
+
+        TConfig()
+        {
+            Register("master_rpc_timeout", MasterRpcTimeout).Default(TDuration::MilliSeconds(5000));
+            Register("sequential_reader", SequentialReader).DefaultNew();
+            Register("remote_reader", RemoteReader).DefaultNew();
+        }
+    };
+
+    //! Initializes an instance.
+    TFileReaderBase(
+        TConfig* config,
+        NRpc::IChannel* masterChannel,
+        NChunkClient::IBlockCache* blockCache);
+
+    //! Opens the reader.
+    void Open(const NChunkServer::TChunkId&, const yvector<Stroka>&);
+
+    //! Returns the size of the file.
+    i64 GetSize() const;
+
+    //! Reads the next block.
+    /*!
+     *  \returns The next block or NULL reference is the end is reached.
+     */
+    TSharedRef Read();
+
+private:
+    TConfig::TPtr Config;
+    NRpc::IChannel::TPtr MasterChannel;
+    NChunkClient::IBlockCache::TPtr BlockCache;
+    NYTree::TYPath Path;
+    bool IsOpen;
+    i32 BlockCount;
+    i32 BlockIndex;
+protected:
+    NCypress::TCypressServiceProxy Proxy;
+    NLog::TTaggedLogger Logger;
+
+private:
+    NChunkClient::TSequentialReader::TPtr SequentialReader;
+    i64 Size;
+    ICodec* Codec;
+    Stroka FileName;
+    bool Executable;
+
+    DECLARE_THREAD_AFFINITY_SLOT(Client);
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NFileClient
+} // namespace NYT
