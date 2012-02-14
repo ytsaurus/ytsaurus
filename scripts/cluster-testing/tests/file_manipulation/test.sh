a8da473c56c620f1eae59e3a9d5ccf9225f7c73c
Dmitry Panin <panin@yandex-team.ru>
2012-02-14 18:41:35 +0400

    Test for uploading/downloading file

---
+++ scripts/cluster-testing/tests/file_manipulation/test.sh
@@ -0,0 +1,18 @@
+#!/bin/bash
+
+rm -f downloaded_file.txt
+
+dd if=/dev/urandom of=some_file.txt bs=1000 count=1000 2>/dev/null
+
+echo '{do=upload; path= "/file"; stream = "<some_file.txt" }' |ytdriver
+
+echo '{do=get; path = "/file@size"}' |ytdriver
+echo '{do=get; path = "/sys/chunks@size"}' |ytdriver
+
+echo '{do=download; path= "/file"; stream = ">downloaded_file.txt" }' |ytdriver
+
+diff= `diff some_file.txt downloaded_file.txt`
+echo "Diff: $diff"
+
+echo '{do=remove; path = "/file"}' |ytdriver
+echo '{do=get; path = "/sys/chunks@size"}' |ytdriver
