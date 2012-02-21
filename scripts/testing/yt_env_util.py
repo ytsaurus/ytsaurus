005871b75fdeabaadd39dac267170717d199aeaf
Dmitry Panin <panin@yandex-team.ru>
2012-02-21 21:16:45 +0400

    Some useful functions for testing framework

---
+++ scripts/testing/yt_env_util.py
@@ -0,0 +1,23 @@
+import subprocess
+
+def do_get(path, yt = None):
+    return execute_cmd('{{do = get; path="{path}"}}'.format(**vars()), yt)
+    
+def execute_cmd(cmd, yt = None):
+    if yt:
+        yt.stdin.write(cmd + '\n')
+        return yt.stdout.readline().strip('\n')
+    else:
+        yt = launch_yt()
+        return yt.communicate(cmd + '\n')[0]
+
+def execute_with_error(cmd, yt = None):
+    if yt:
+        yt.stdin.write(cmd + '\n')
+        return yt.stderr.readline().strip('\n')
+    else:
+        yt = launch_yt()
+        return yt.communicate(cmd + '\n')[1]
+
+def launch_yt():
+    return subprocess.Popen(['ytdriver'], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
