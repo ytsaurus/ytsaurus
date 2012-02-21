16973904ba9dd09dc63d6a2aead49ee12b7abf78
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2012-02-21 19:37:06 +0400

    Remove redundant type traits in favor of MPL

--- yt/ytlib/misc/rvalue.h
+++ yt/ytlib/misc/rvalue.h
@@ -18,42 +18,19 @@ struct TIdentity {
     typedef T TType;
 };
 
-// N3242, 20.9.7.2
-template<typename T>
-struct TRemoveReference {
-    typedef T TType;
-};
-
-template<typename T>
-struct TRemoveReference<T&> {
-    typedef T TType;
-};
-
-template<typename T>
-struct TRemoveReference<const T&> {
-    typedef const T TType;
-};
-
-template<typename T>
-struct TRemoveReference<T&&> {
-    typedef T TType;
-};
-
 } // namespace NDetail
 
 ////////////////////////////////////////////////////////////////////////////////
 
 //! Implements #std::move<T>-like behaviour from C++11.
 template<typename T>
-FORCED_INLINE typename NYT::NDetail::TRemoveReference<T>::TType&&
+FORCED_INLINE typename NMpl::TRemoveReference<T>::TType&&
 MoveRV(T&& x) // noexcept
 {
-    return static_cast<typename NYT::NDetail::TRemoveReference<T>::TType&&>(x);
+    return static_cast<typename NMpl::TRemoveReference<T>::TType&&>(x);
 }
 
 // Implements #std::forward<T>-like behaviour from C++11.
-#ifdef __GNUC__
-// GCC
 // N2951, http://www.open-std.org/jtc1/sc22/wg21/docs/papers/2009/n2951.html
 // N3242, 20.2.3, for standard definition of C++11.
 template<class T, class U,
@@ -63,15 +40,15 @@ template<class T, class U,
             NMpl::TIsLvalueReference<U>::Value :
             true)
         && NMpl::TIsConvertible<
-            typename NDetail::TRemoveReference<U>::TType*,
-            typename NDetail::TRemoveReference<T>::TType*
+            typename NMpl::TRemoveReference<U>::TType*,
+            typename NMpl::TRemoveReference<T>::TType*
         >::Value
     >::TType>
 FORCED_INLINE T&& ForwardRV(U&& arg) // noexcept
 {
     return static_cast<T&&>(arg);
 }
-#else
+#if 0
 // MSVC
 // http://msdn.microsoft.com/en-us/library/ee390914.aspx
 // http://blogs.msdn.com/b/vcblog/archive/2009/02/03/rvalue-references-c-0x-features-in-vc10-part-2.aspx
