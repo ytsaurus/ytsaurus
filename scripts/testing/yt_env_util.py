d463b25b7b9dccb041f062ce3e6045a055b0acc9
Dmitry Panin <panin@yandex-team.ru>
2012-02-22 16:54:52 +0400

    Added generic function to execute command via driver

--- scripts/testing/yt_env_util.py
+++ scripts/testing/yt_env_util.py
@@ -1,11 +1,37 @@
 import subprocess
 
+
+def launch_yt(**kw):
+    args = dict(
+        bufsize=1,
+        stdin=subprocess.PIPE, 
+        stdout=subprocess.PIPE, 
+        stderr=subprocess.PIPE,
+        )
+    args.update(kw)
+    return subprocess.Popen(['ytdriver'], **args)
+        bufsize=1,
+        stdin=subprocess.PIPE,
+        stdout=subprocess.PIPE,
+        stderr=subprocess.PIPE)
+
+def exec_cmd(cmd):
+    yt = launch_yt()
+    output = yt.communicate(cmd + '\n')
+    return (*output, yt.returncode)
+
+
+###########################################################################
+
+#TODO: think about necessity of this functions
 def do_get(path, yt = None):
     return execute_cmd('{{do = get; path="{path}"}}'.format(**vars()), yt)
     
 def execute_cmd(cmd, yt = None):
     if yt:
         yt.stdin.write(cmd + '\n')
+        yt.stdin.flush()
+        print 'executed', cmd
         return yt.stdout.readline().strip('\n')
     else:
         yt = launch_yt()
@@ -14,10 +40,8 @@ def execute_cmd(cmd, yt = None):
 def execute_error_cmd(cmd, yt = None):
     if yt:
         yt.stdin.write(cmd + '\n')
+        yt.stdin.flush()
         return yt.stderr.readline().strip('\n')
     else:
         yt = launch_yt()
         return yt.communicate(cmd + '\n')[1]
-
-def launch_yt():
-    return subprocess.Popen(['ytdriver'], stdin = subprocess.PIPE, stdout = subprocess.PIPE, stderr = subprocess.PIPE)
