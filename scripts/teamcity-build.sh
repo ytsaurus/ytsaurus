d47c88448b47192272f3b422fddebffbe5a1c7c9
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2012-02-22 19:32:03 +0400

    Fixed TC build script (correct handling of WDs)

--- scripts/teamcity-build.sh
+++ scripts/teamcity-build.sh
@@ -61,13 +61,15 @@ make -j 8 >/dev/null 2>/dev/null || true
 echo "* Running make (2/2; slow)..." >&2
 make -j 1
 
+cd $WORKING_DIRECTORY
 gdb --batch --command=$CHECKOUT_DIRECTORY/scripts/teamcity-gdb-script --args \
     ./bin/unittester \
         --gtest_color=no \
         --gtest_output=xml:$WORKING_DIRECTORY/test_unit.xml
 
+cd $CHECKOUT_DIRECTORY/scripts/testing
 PATH=$WORKING_DIRECTORY/bin:$PATH \
-    (cd $CHECKOUT_DIRECTORY/scripts/testing && py.test \
+    py.test \
         -rxs -v \
         --assert=plain \
-        --junitxml=$WORKING_DIRECTORY/test_integration.xml)
+        --junitxml=$WORKING_DIRECTORY/test_integration.xml
