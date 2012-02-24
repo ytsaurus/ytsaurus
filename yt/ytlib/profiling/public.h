c52525374db6f9da2566e161667080791e46595f
Maxim Babenko <babenko@ytsaurus.tech>
2012-02-25 01:42:17 +0400

    Cosmetics

--- yt/ytlib/profiling/public.h
+++ yt/ytlib/profiling/public.h
@@ -10,12 +10,11 @@ namespace NProfiling {
 class TProfiler;
 class TProfilingManager;
 
+struct TTimer;
 struct TQueuedSample;
 
 //! Generic value for samples. 
 typedef i64 TValue;
-
-struct TTimer;
 
 ////////////////////////////////////////////////////////////////////////////////
 
