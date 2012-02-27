5f68a4ee072900bbe159141667e54b3939c29bc1
Dmitry Panin <panin@yandex-team.ru>
2012-02-27 12:13:39 +0400

    Fixed test_attributes

--- scripts/testing/tests/test_cypress/test_attributes.sh
+++ scripts/testing/tests/test_cypress/test_attributes.sh
@@ -13,12 +13,16 @@ echo '{do = get; path = "/root@"}' | ytdriver
 echo '{do = remove; path = "/root/nodes"}' | ytdriver
 echo '{do = get; path = "/root"}' | ytdriver
 
+echo 'changing attributes'
 echo '{do = set; path = "/root/2"; value = [] < author=ignat >}' | ytdriver
 echo '{do = get; path = "/root/2"}' | ytdriver
 echo '{do = get; path = "/root/2@"}' | ytdriver
 echo '{do = get; path = "/root/2@author"}' | ytdriver
 
-#nested attributes
+echo '{do = set; path = "/root/2@author"; value = "not_ignat" }' | ytdriver
+echo '{do = get; path = "/root/2@author"}' | ytdriver
+
+echo 'nested attributes'
 echo '{do = set; path = "/root/3"; value = [] <dir=<file=-100<>>>}' | ytdriver
 echo '{do = get; path = "/root/3@"}' | ytdriver
 echo '{do = get; path = "/root/3@dir@"}' | ytdriver
