787ce79bbc3863f10f8fda20fba6b70665a32e4b
Dmitry Panin <panin@yandex-team.ru>
2012-02-27 22:19:07 +0400

    Fixed test_table test

--- scripts/testing/tests/test_table.sh
+++ scripts/testing/tests/test_table.sh
@@ -1,21 +1,28 @@
 #!/bin/bash
-#% NUM_MASTERS = 1
+#% NUM_MASTERS = 3
 #% NUM_HOLDERS = 5
 #% SETUP_TIMEOUT = 10
 
 echo '{do=create; path="/table"; type=table}' | ytdriver
 
+echo '{do=get; path = "/table"}' | ytdriver
 echo '{do=get; path = "/table@row_count"}' | ytdriver
 
-# write one value
+echo 'write one value'
 echo '{do=write; path = "/table"; value=[{b="hello"}]}' | ytdriver
+echo '{do=get; path = "/table@row_count"}' | ytdriver
+echo '{do=read; path = "/table"}' | ytdriver
 
-# write many values
+echo 'write many values'
 echo '{do=write; path = "/table"; value=[{b=2;a=1};{x=10;y=20;a=30}]}' | ytdriver
+echo '{do=get; path = "/table@row_count"}' | ytdriver
+echo '{do=read; path = "/table"}' | ytdriver
 
-#write from stream
+echo 'write from stream'
 echo '{row = some}' > table.txt
 echo '{do=write; path = "/table"; stream="<table.txt"}' | ytdriver
-
 echo '{do=get; path = "/table@row_count"}' | ytdriver
+echo '{do=read; path = "/table"}' | ytdriver
+
+#TODO(panin): add more checks of read methods
 
