3dd868594c5a4f2c37e62aff609c411cffdd84b4
Michael Roizner <roizner@yandex-team.ru>
2012-02-27 18:59:34 +0400

    Fix in log manager (Impl became ref counted)

--- yt/ytlib/logging/log_manager.h
+++ yt/ytlib/logging/log_manager.h
@@ -32,7 +32,7 @@ public:
 
 private:
     class TImpl;
-    THolder<TImpl> Impl;
+    TIntrusivePtr<TImpl> Impl;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
