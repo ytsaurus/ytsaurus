31bb7aa927a04d6fc9265390abc7416ee80a40ae
Michael Roizner <roizner@yandex-team.ru>
2012-02-27 13:13:36 +0400

    Incorrect symbols erased.

--- yt/ytlib/meta_state/snapshot.cpp
+++ yt/ytlib/meta_state/snapshot.cpp
@@ -149,7 +149,7 @@ void TSnapshotWriter::Open(i32 prevRecordCount)
 
 TOutputStream& TSnapshotWriter::GetStream() const
 {
-    YASSERT(~ChecksummableOutput);©≠ош
+    YASSERT(~ChecksummableOutput);
     return *ChecksummableOutput;
 }
 
