044e1a38c2c5408f75aa5ea3e5a6c2333c65dbe5
Michael Roizner <roizner@yandex-team.ru>
2012-03-06 17:11:15 +0400

    New YSON parser added

---
+++ yt/ytlib/ytree/yson_parser.h
@@ -0,0 +1,48 @@
+#pragma once
+
+#include "public.h"
+
+#include <ytlib/misc/property.h>
+
+#include <stack>
+
+namespace NYT {
+namespace NYTree {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TYsonParser
+{
+public:
+    TYsonParser(IYsonConsumer* consumer);
+
+    void Consume(char ch);
+    void Finish();
+
+    //! Returns true iff the parser is awaiting for more chars
+    bool IsAwaiting(bool ignoreAttributes = true) const;
+
+private:
+    class TImpl;
+    THolder<TImpl> Impl;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+            
+void ParseYson(TInputStream* input, IYsonConsumer* consumer);
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TYsonFragmentParser
+{
+public:
+    TYsonFragmentParser(IYsonConsumer* consumer, TInputStream* input);
+
+    bool HasNext() const;
+    void ParseNext();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYtree
+} // namespace NYT
