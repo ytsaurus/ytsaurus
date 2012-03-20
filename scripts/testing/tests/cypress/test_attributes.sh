1c6ef6552b30d7b05f146ebdc44f0dcee837cf26
Dmitry Panin <panin@yandex-team.ru>
2012-03-20 18:04:53 +0400

    Fixing tests

--- scripts/testing/tests/cypress/test_attributes.sh
+++ scripts/testing/tests/cypress/test_attributes.sh
@@ -2,7 +2,7 @@
 #% NUM_MASTERS = 3
 #% NUM_HOLDERS = 0
 
-yt set /root '{nodes=[1; 2]} <attr=100;mode=rw>}'
+yt set /root '{nodes=[1; 2]} <attr=100;mode=rw>'
 yt get /root@
 yt get /root@attr
 
@@ -12,7 +12,7 @@ yt get /root@
 yt remove /root/nodes
 yt get /root
 
-yt 'changing attributes'
+echo 'changing attributes'
 yt set /root/2 '[] < author=ignat >'
 yt get /root/2
 yt get /root/2@
@@ -21,7 +21,7 @@ yt get /root/2@author
 yt set /root/2@author not_ignat
 yt get /root/2@author 
 
-yt 'nested attributes'
+echo 'nested attributes'
 yt set /root/3 '[] <dir=<file=-100<>>>' 
 yt get /root/3@
 yt get /root/3@dir@
