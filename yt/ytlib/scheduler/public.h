231f6ae5140e3ed04453dd3c7ff1b5e922967eed
Maxim Babenko <babenko@ytsaurus.tech>
2012-03-20 11:07:58 +0400

    Just a couple of protos

---
+++ yt/ytlib/scheduler/public.h
@@ -0,0 +1,37 @@
+#pragma once
+
+#include <ytlib/misc/common.h>
+#include <ytlib/misc/guid.h>
+#include <ytlib/misc/enum.h>
+
+namespace NYT {
+namespace NScheduler {
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef TGuid TJobId;
+
+typedef TGuid TTaskId;
+
+DECLARE_ENUM(ETaskType,
+	((Map)(0))
+);
+
+DECLARE_ENUM(EJobType,
+	((Map)(0))
+);
+
+DECLARE_ENUM(ETaskState,
+	((Prepaing)(0))
+);
+
+DECLARE_ENUM(EJobState,
+	((Running)(0))
+	((Completed)(1))
+	((Failed)(2))
+);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NScheduler
+} // namespace NYT
