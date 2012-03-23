4e1e84ac39739b9d4dbd54a68e174a0568fdf346
Maxim Babenko <babenko@ytsaurus.tech>
2012-03-23 17:06:45 +0100

    Cosmetics

--- yt/ytlib/orchid/cypress_integration.cpp
+++ yt/ytlib/orchid/cypress_integration.cpp
@@ -69,7 +69,7 @@ public:
         requestHeader.set_path(path);
         auto innerRequestMessage = SetRequestHeader(~requestMessage, requestHeader);
 
-        auto outerRequest =proxy.Execute();
+        auto outerRequest = proxy.Execute();
         outerRequest->Attachments() = innerRequestMessage->GetParts();
 
         LOG_INFO("Sending request to a remote Orchid (RemoteAddress: %s, Path: %s, Verb: %s, RequestId: %s)",
