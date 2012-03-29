5fd7477efe91cc3cdec90fb2db4d2cabe63bfdb1
Maxim Babenko <babenko@ytsaurus.tech>
2012-03-29 13:11:35 +0200

    Extracted detail

---
+++ yt/ytlib/scheduler/operation_controller_detail.cpp
@@ -0,0 +1,354 @@
+#include "stdafx.h"
+#include "operation_controller_detail.h"
+#include "operation.h"
+#include "job.h"
+#include "exec_node.h"
+
+namespace NYT {
+namespace NScheduler {
+
+    using namespace NCypress;
+using namespace NTransactionServer;
+using namespace NTableClient::NProto;
+using namespace NChunkServer;
+
+////////////////////////////////////////////////////////////////////
+
+TOperationControllerBase::TOperationControllerBase(IOperationHost* host, TOperation* operation)
+    : Host(host)
+    , Operation(operation)
+    , CypressProxy(host->GetMasterChannel())
+    , Logger(OperationsLogger)
+{ }
+
+void TOperationControllerBase::Initialize()
+{ }
+
+TFuture<TError>::TPtr TOperationControllerBase::Prepare()
+{
+    return MakeFuture(TError());
+}
+
+void TOperationControllerBase::OnJobRunning(TJobPtr job)
+{
+    UNUSED(job);
+}
+
+void TOperationControllerBase::OnJobCompleted(TJobPtr job)
+{
+    UNUSED(job);
+}
+
+void TOperationControllerBase::OnJobFailed(TJobPtr job)
+{
+    UNUSED(job);
+}
+
+void TOperationControllerBase::OnOperationAborted()
+{ }
+
+void TOperationControllerBase::ScheduleJobs(
+    TExecNodePtr node,
+    std::vector<TJobPtr>* jobsToStart,
+    std::vector<TJobPtr>* jobsToAbort)
+{
+    UNUSED(node);
+    UNUSED(jobsToStart);
+    UNUSED(jobsToAbort);
+}
+
+void TOperationControllerBase::OnOperationFailed(const TError& error)
+{
+    VERIFY_THREAD_AFFINITY_ANY();
+
+    LOG_INFO("Operation failed\n%s", ~error.ToString());
+
+    Host->OnOperationFailed(Operation, error);
+
+    Host->GetControlInvoker()->Invoke(FromMethod(
+        &TOperationControllerBase::AbortOperation,
+        MakeWeak(this)));
+}
+
+void TOperationControllerBase::OnOperationCompleted()
+{
+    VERIFY_THREAD_AFFINITY_ANY();
+
+    LOG_INFO("Operation completed");
+
+    Host->OnOperationCompleted(Operation);
+}
+
+void TOperationControllerBase::AbortOperation()
+{
+    VERIFY_THREAD_AFFINITY(ControlThread);
+}
+
+////////////////////////////////////////////////////////////////////
+
+TChunkPool::TChunkPool(TOperationPtr operation)
+    : Logger(OperationsLogger)
+{
+    Logger.AddTag(Sprintf("OperationId: %s", ~operation->GetOperationId().ToString()));
+}
+
+int TChunkPool::PutChunk(const TInputChunk& chunk, i64 weight)
+{
+    YASSERT(weight > 0);
+
+    TChunkInfo info;
+    info.Chunk = chunk;
+    info.Weight = weight;
+    int index = static_cast<int>(ChunkInfos.size());
+    ChunkInfos.push_back(info);
+    RegisterChunk(index);
+    return index;
+}
+
+const TInputChunk& TChunkPool::GetChunk(int index)
+{
+    return ChunkInfos[index].Chunk;
+}
+
+void TChunkPool::AllocateChunks(const Stroka& address, i64 maxWeight, std::vector<int>* indexes, i64* allocatedWeight, int* localCount, int* remoteCount)
+{
+    *allocatedWeight = 0;
+
+    // Take local chunks first.
+    *localCount = 0;
+    auto addressIt = AddressToIndexSet.find(address);
+    if (addressIt != AddressToIndexSet.end()) {
+        const auto& localIndexes = addressIt->second;
+        FOREACH (int chunkIndex, localIndexes) {
+            if (*allocatedWeight >= maxWeight) {
+                break;
+            }
+            indexes->push_back(chunkIndex);
+            ++*localCount;
+            *allocatedWeight += ChunkInfos[chunkIndex].Weight;
+        }
+    }
+
+    // Unregister taken local chunks.
+    // We have to do this right away, otherwise we risk getting same chunks
+    // in the next phase.
+    for (int i = 0; i < *localCount; ++i) {
+        UnregisterChunk((*indexes)[i]);
+    }
+
+    // Take remote chunks.
+    *remoteCount = 0;
+    FOREACH (int chunkIndex, UnallocatedIndexes) {
+        if (*allocatedWeight >= maxWeight) {
+            break;
+        }
+        indexes->push_back(chunkIndex);
+        ++*remoteCount;
+        *allocatedWeight += ChunkInfos[chunkIndex].Weight;
+    }
+
+    // Unregister taken remote chunks.
+    for (int i = *localCount; i < *localCount + *remoteCount; ++i) {
+        UnregisterChunk((*indexes)[i]);
+    }
+
+    LOG_DEBUG("Extracted chunks [%s] from the pool", ~JoinToString(*indexes));
+}
+
+void TChunkPool::DeallocateChunks(const std::vector<int>& indexes)
+{
+    FOREACH (auto index, indexes) {
+        RegisterChunk(index);
+    }
+
+    LOG_DEBUG("Chunks [%s] are back in the pool", ~JoinToString(indexes));
+}
+
+void TChunkPool::RegisterChunk(int index)
+{
+    const auto& info = ChunkInfos[index];
+    FOREACH (const auto& address, info.Chunk.holder_addresses()) {
+        YVERIFY(AddressToIndexSet[address].insert(index).second);
+    }
+    YVERIFY(UnallocatedIndexes.insert(index).second);
+}
+
+void TChunkPool::UnregisterChunk(int index)
+{
+    const auto& info = ChunkInfos[index];
+    FOREACH (const auto& address, info.Chunk.holder_addresses()) {
+        YVERIFY(AddressToIndexSet[address].erase(index) == 1);
+    }
+    YVERIFY(UnallocatedIndexes.erase(index) == 1);
+}
+
+////////////////////////////////////////////////////////////////////
+
+TChunkListPool::TChunkListPool(
+    NRpc::IChannel::TPtr masterChannel,
+    IInvoker::TPtr controlInvoker,
+    TOperationPtr operation,
+    const TTransactionId& transactionId)
+    : MasterChannel(masterChannel)
+    , ControlInvoker(controlInvoker)
+    , Operation(operation)
+    , TransactionId(transactionId)
+    , Logger(OperationsLogger)
+    , RequestInProgress(false)
+{
+    Logger.AddTag(Sprintf("OperationId: %s", ~operation->GetOperationId().ToString()));
+}
+
+int TChunkListPool::GetSize() const
+{
+    return static_cast<int>(Ids.size());
+}
+
+NYT::NChunkServer::TChunkListId TChunkListPool::Extract()
+{
+    YASSERT(!Ids.empty());
+    auto id = Ids.back();
+    Ids.pop_back();
+
+    LOG_DEBUG("Extracted chunk list %s from the pool, %d remaining",
+        ~id.ToString(),
+        static_cast<int>(Ids.size()));
+
+    return id;
+}
+
+void TChunkListPool::Allocate(int count)
+{
+    if (RequestInProgress) {
+        LOG_DEBUG("Cannot allocate more chunk lists, another request is in progress");
+        return;
+    }
+
+    LOG_INFO("Allocating %d chunk lists for pool", count);
+
+    TCypressServiceProxy cypressProxy(MasterChannel);
+    auto batchReq = cypressProxy.ExecuteBatch();
+
+    for (int index = 0; index < count; ++index) {
+        auto req = TTransactionYPathProxy::CreateObject(FromObjectId(TransactionId));
+        req->set_type(EObjectType::ChunkList);
+        batchReq->AddRequest(req);
+    }
+
+    batchReq->Invoke()->Subscribe(
+        FromMethod(&TChunkListPool::OnChunkListsCreated, MakeWeak(this))
+        ->Via(ControlInvoker));
+
+    RequestInProgress = true;
+}
+
+void TChunkListPool::OnChunkListsCreated(TCypressServiceProxy::TRspExecuteBatch::TPtr batchRsp)
+{
+    YASSERT(RequestInProgress);
+    RequestInProgress = false;
+
+    if (!batchRsp->IsOK()) {
+        LOG_ERROR("Error allocating chunk lists\n%s", ~batchRsp->GetError().ToString());
+        // TODO(babenko): backoff time?
+        return;
+    }
+
+    LOG_INFO("Chunk lists allocated");
+
+    YASSERT(Ids.empty());
+
+    FOREACH (auto rsp, batchRsp->GetResponses<TTransactionYPathProxy::TRspCreateObject>()) {
+        Ids.push_back(TChunkListId::FromProto(rsp->object_id()));
+    }
+}
+
+TIntrusivePtr< TAsyncPipeline<TVoid> > StartAsyncPipeline(IInvoker::TPtr invoker)
+{
+    return New< TAsyncPipeline<TVoid> >(
+        invoker,
+        FromFunctor([=] () -> TIntrusivePtr< TFuture< TValueOrError<TVoid> > > {
+            return MakeFuture(TValueOrError<TVoid>(TVoid()));
+    }));
+}
+
+////////////////////////////////////////////////////////////////////
+
+TRunningCounter::TRunningCounter()
+    : Total_(-1)
+    , Running_(-1)
+    , Done_(-1)
+    , Pending_(-1)
+    , Failed_(-1)
+{ }
+
+void TRunningCounter::Init(i64 total)
+{
+    Total_ = total;
+    Running_ = 0;
+    Done_ = 0;
+    Pending_ = total;
+    Failed_ = 0;
+}
+
+i64 TRunningCounter::GetTotal() const
+{
+    return Total_;
+}
+
+i64 TRunningCounter::GetRunning() const
+{
+    return Running_;
+}
+
+i64 TRunningCounter::GetDone() const
+{
+    return Done_;
+}
+
+
+i64 TRunningCounter::GetPending() const
+{
+    return Pending_;
+}
+i64 TRunningCounter::GetFailed() const
+{
+    return Failed_;
+}
+
+void TRunningCounter::Start(i64 count)
+{
+    YASSERT(Pending_ >= count);
+    Running_ += count;
+    Pending_ -= count;
+}
+
+void TRunningCounter::Completed(i64 count)
+{
+    YASSERT(Running_ >= count);
+    Running_ -= count;
+    Done_ += count;
+}
+
+void TRunningCounter::Failed(i64 count)
+{
+    YASSERT(Running_ >= count);
+    Running_ -= count;
+    Pending_ += count;
+    Failed_ += count;
+}
+
+Stroka ToString(const TRunningCounter& counter)
+{
+    return Sprintf("T: %" PRId64 ", R: %" PRId64 ", D: %" PRId64 ", P: %" PRId64 ", F: %" PRId64,
+        counter.GetTotal(),
+        counter.GetRunning(),
+        counter.GetDone(),
+        counter.GetPending(),
+        counter.GetFailed());
+}
+
+////////////////////////////////////////////////////////////////////
+
+} // namespace NScheduler
+} // namespace NYT
+
---
+++ yt/ytlib/scheduler/operation_controller_detail.h
@@ -0,0 +1,253 @@
+#pragma once
+
+#include "private.h"
+#include "operation_controller.h"
+
+#include <ytlib/logging/tagged_logger.h>
+#include <ytlib/misc/thread_affinity.h>
+#include <ytlib/chunk_server/public.h>
+
+namespace NYT {
+namespace NScheduler {
+
+////////////////////////////////////////////////////////////////////////////////
+
+// TODO(babenko): extract to a proper place
+class TOperationControllerBase
+    : public IOperationController
+{
+public:
+    TOperationControllerBase(IOperationHost* host, TOperation* operation);
+
+    virtual void Initialize();
+    virtual TFuture<TError>::TPtr Prepare();
+
+    virtual void OnJobRunning(TJobPtr job);
+    virtual void OnJobCompleted(TJobPtr job);
+    virtual void OnJobFailed(TJobPtr job);
+
+
+    virtual void OnOperationAborted();
+
+    virtual void ScheduleJobs(
+        TExecNodePtr node,
+        std::vector<TJobPtr>* jobsToStart,
+        std::vector<TJobPtr>* jobsToAbort);
+
+protected:
+    IOperationHost* Host;
+    TOperation* Operation;
+
+    NCypress::TCypressServiceProxy CypressProxy;
+    NLog::TTaggedLogger Logger;
+
+    DECLARE_THREAD_AFFINITY_SLOT(ControlThread);
+    DECLARE_THREAD_AFFINITY_SLOT(BackgroundThread);
+
+
+    void OnOperationFailed(const TError& error);
+
+    void OnOperationCompleted();
+
+    virtual void AbortOperation();
+};
+
+////////////////////////////////////////////////////////////////////
+
+// TODO(babenko): extract to a proper place
+class TChunkPool
+{
+public:
+    TChunkPool(TOperationPtr operation);
+
+    int PutChunk(const NTableClient::NProto::TInputChunk& chunk, i64 weight);
+
+    const NTableClient::NProto::TInputChunk& GetChunk(int index);
+
+    void AllocateChunks(
+        const Stroka& address,
+        i64 maxWeight,
+        std::vector<int>* indexes,
+        i64* allocatedWeight,
+        int* localCount,
+        int* remoteCount);
+
+    void DeallocateChunks(const std::vector<int>& indexes);
+
+private:
+    NLog::TTaggedLogger Logger;
+
+    struct TChunkInfo
+    {
+        NTableClient::NProto::TInputChunk Chunk;
+        i64 Weight;
+    };
+
+    std::vector<TChunkInfo> ChunkInfos;
+    yhash_map<Stroka, yhash_set<int> > AddressToIndexSet;
+    yhash_set<int> UnallocatedIndexes;
+
+    void RegisterChunk(int index);
+
+    void UnregisterChunk(int index);
+};
+
+////////////////////////////////////////////////////////////////////
+
+// TODO(babenko): extract to a proper place
+class TChunkListPool
+    : public TRefCounted
+{
+public:
+    TChunkListPool(
+        NRpc::IChannel::TPtr masterChannel,
+        IInvoker::TPtr controlInvoker,
+        TOperationPtr operation,
+        const TTransactionId& transactionId);
+
+    int GetSize() const;
+
+    NChunkServer::TChunkListId Extract();
+
+    void Allocate(int count);
+
+private:
+    NRpc::IChannel::TPtr MasterChannel;
+    IInvoker::TPtr ControlInvoker;
+    TOperationPtr Operation;
+    TTransactionId TransactionId;
+
+    NLog::TTaggedLogger Logger;
+    bool RequestInProgress;
+    std::vector<NChunkServer::TChunkListId> Ids;
+
+    void OnChunkListsCreated(NCypress::TCypressServiceProxy::TRspExecuteBatch::TPtr batchRsp);
+};
+
+typedef TIntrusivePtr<TChunkListPool> TChunkListPoolPtr;
+
+////////////////////////////////////////////////////////////////////
+
+// TODO(babenko): extract to a proper place
+class TRunningCounter
+{
+public:
+    TRunningCounter();
+
+    void Init(i64 total);
+
+    i64 GetTotal() const;
+    i64 GetRunning() const;
+    i64 GetDone() const;
+    i64 GetPending() const;
+    i64 GetFailed() const;
+
+
+    void Start(i64 count);
+    void Completed(i64 count);
+    void Failed(i64 count);
+
+private:
+    i64 Total_;
+    i64 Running_;
+    i64 Done_;
+    i64 Pending_;
+    i64 Failed_;
+};
+
+Stroka ToString(const TRunningCounter& counter);
+
+////////////////////////////////////////////////////////////////////
+
+// TODO(babenko): move to a proper place
+
+template <class T>
+class TAsyncPipeline
+    : public TRefCounted
+{
+public:
+    TAsyncPipeline(
+        IInvoker::TPtr invoker,
+        TIntrusivePtr< IFunc< TIntrusivePtr < TFuture< TValueOrError<T> > > > > head)
+        : Invoker(invoker)
+        , Lazy(head)
+    { }
+
+    TIntrusivePtr< TFuture< TValueOrError<T> > > Run()
+    {
+        return Lazy->Do();
+    }
+
+    typedef T T1;
+
+    template <class T2>
+    TIntrusivePtr< TAsyncPipeline<T2> > Add(TIntrusivePtr< IParamFunc<T1, T2> > func)
+    {
+        auto wrappedFunc = FromFunctor([=] (TValueOrError<T1> x) -> TIntrusivePtr< TFuture< TValueOrError<T2> > > {
+            if (!x.IsOK()) {
+                return MakeFuture(TValueOrError<T2>(TError(x)));
+            }
+            try {
+                auto y = func->Do(x.Value());
+                return MakeFuture(TValueOrError<T2>(y));
+            } catch (const std::exception& ex) {
+                return MakeFuture(TValueOrError<T2>(TError(ex.what())));
+            }
+        });
+
+        if (Invoker) {
+            wrappedFunc = wrappedFunc->AsyncVia(Invoker);
+        }
+
+        auto lazy = Lazy;
+        auto newLazy = FromFunctor([=] () {
+            return lazy->Do()->Apply(wrappedFunc);
+        });
+
+        return New< TAsyncPipeline<T2> >(Invoker, newLazy);
+    }
+
+    template <class T2>
+    TIntrusivePtr< TAsyncPipeline<T2> > Add(TIntrusivePtr< IParamFunc<T1, TIntrusivePtr< TFuture<T2> > > > func)
+    {
+        auto toValueOrError = FromFunctor([] (T2 x) {
+            return TValueOrError<T2>(x);
+        });
+
+        auto wrappedFunc = FromFunctor([=] (TValueOrError<T1> x) -> TIntrusivePtr< TFuture< TValueOrError<T2> > > {
+            if (!x.IsOK()) {
+                return MakeFuture(TValueOrError<T2>(TError(x)));
+            }
+            try {
+                auto y = func->Do(x.Value());
+                return y->Apply(toValueOrError);
+            } catch (const std::exception& ex) {
+                return MakeFuture(TValueOrError<T2>(TError(ex.what())));
+            }
+        });
+
+        if (Invoker) {
+            wrappedFunc = wrappedFunc->AsyncVia(Invoker);
+        }
+
+        auto lazy = Lazy;
+        auto newLazy = FromFunctor([=] () {
+            return lazy->Do()->Apply(wrappedFunc);
+        });
+
+        return New< TAsyncPipeline<T2> >(Invoker, newLazy);
+    }
+
+
+private:
+    IInvoker::TPtr Invoker;
+    TIntrusivePtr< IFunc< TIntrusivePtr < TFuture< TValueOrError<T> > > > > Lazy;
+
+};
+
+TIntrusivePtr< TAsyncPipeline<TVoid> > StartAsyncPipeline(IInvoker::TPtr invoker = NULL);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NScheduler
+} // namespace NYT
