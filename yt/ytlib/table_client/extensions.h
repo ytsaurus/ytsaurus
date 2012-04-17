1bea08ac93dfe34082f4f55c26305c98b1018901
Pavel Sushin <psushin@ytsaurus.tech>
2012-04-17 16:06:59 +0400

    Defined extensions.

---
+++ yt/ytlib/table_client/extensions.h
@@ -0,0 +1,17 @@
+﻿#pragma once
+
+#include <ytlib/table_client/table_chunk_meta.pb.h>
+#include <ytlib/misc/protobuf_helpers.h>
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+DECLARE_PROTO_EXTENSION(NTableClient::NProto::TChannels, 10)
+DECLARE_PROTO_EXTENSION(NTableClient::NProto::TSamples, 11)
+DECLARE_PROTO_EXTENSION(NTableClient::NProto::TIndex, 12)
+DECLARE_PROTO_EXTENSION(NTableClient::NProto::TBoundaryKeys, 13)
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
