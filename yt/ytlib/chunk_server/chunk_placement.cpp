097eee3276341cda4ecb58c34b8185a7e0ec178d
Maxim Babenko <babenko@ytsaurus.tech>
2012-04-20 14:09:22 +0400

    TODOs

--- yt/ytlib/chunk_server/chunk_placement.cpp
+++ yt/ytlib/chunk_server/chunk_placement.cpp
@@ -90,7 +90,6 @@ yvector<THolderId> TChunkPlacement::GetUploadTargets(int count)
 
 yvector<THolderId> TChunkPlacement::GetUploadTargets(int count, const yhash_set<Stroka>& forbiddenAddresses)
 {
-    // TODO(babenko): speed up
     // TODO: check replication fan-in in case this is a replication job
     yvector<const THolder*> holders;
     holders.reserve(LoadFactorMap.size());
@@ -186,10 +185,9 @@ yvector<THolderId> TChunkPlacement::GetRemovalTargets(const TChunk& chunk, int c
     }
 
     // Sort by loadFactor in descending order.
-    std::sort(
-        candidates.begin(),
-        candidates.end(),
-        [] (const TCandidatePair& lhs, const TCandidatePair& rhs) {
+    std::sort(candidates.begin(), candidates.end(),
+        [] (const TCandidatePair& lhs, const TCandidatePair& rhs)
+        {
             return lhs.second > rhs.second;
         });
 
