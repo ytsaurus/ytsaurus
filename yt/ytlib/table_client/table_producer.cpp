3ce5e6f9a61722c570915cceb5a8adddee502d09
Pavel Sushin <psushin@ytsaurus.tech>
2012-04-22 10:42:11 +0400

    Add forgotten files.

---
+++ yt/ytlib/table_client/table_producer.cpp
@@ -0,0 +1,52 @@
+﻿#include "stdafx.h"
+#include "table_producer.h"
+#include "sync_reader.h"
+
+#include <ytlib/ytree/yson_consumer.h>
+#include <ytlib/misc/foreach.h>
+
+namespace NYT {
+namespace NTableClient {
+
+using namespace NYTree;
+
+////////////////////////////////////////////////////////////////////////////////
+
+TTableProducer::TTableProducer(
+    ISyncReaderPtr reader, 
+    IYsonConsumer* consumer)
+    : Reader(reader)
+    , Consumer(consumer)
+{ }
+
+bool TTableProducer::ProduceRow()
+{
+    if (!Reader->IsValid())
+        return false;
+
+    Consumer->OnRaw(Reader->GetRowAttributes(), EYsonType::Node);
+
+    Consumer->OnListItem();
+    Consumer->OnBeginMap();
+    FOREACH(auto& pair, Reader->GetRow()) {
+        Consumer->OnKeyedItem(pair.first);
+        Consumer->OnRaw(pair.second, EYsonType::Node);
+    }
+    Consumer->OnEndMap();
+
+    Reader->NextRow();
+    return true;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+void ProduceYson(ISyncReaderPtr reader, NYTree::IYsonConsumer* consumer)
+{
+    TTableProducer producer(reader, consumer);
+    while (producer.ProduceRow());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NTableClient
+} // namespace NYT
---
+++ yt/ytlib/table_client/table_producer.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+#include "public.h"
+#include <ytlib/ytree/public.h>
+
+namespace NYT {
+namespace NTableClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TTableProducer
+{
+public:
+    TTableProducer(ISyncReaderPtr reader, NYTree::IYsonConsumer* consumer);
+    bool ProduceRow();
+
+private:
+    ISyncReaderPtr Reader;
+    NYTree::IYsonConsumer* Consumer;
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+void ProduceYson(ISyncReaderPtr Reader, NYTree::IYsonConsumer* consumer);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NTableClient
+} // namespace NYT
