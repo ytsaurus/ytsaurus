6e1afe0baf638f494d356636525712147bc049de
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2012-05-06 12:12:06 +0400

    Cosmetics

--- yt/nodejs/yt_test.js
+++ yt/nodejs/yt_test.js
@@ -3,7 +3,7 @@ var yt = require('./build/Release/yt_test');
 var expect = require('chai').expect;
 var assert = require('chai').assert;
 
-describe("interface to streams", function() {
+describe("input stream interface", function() {
     before(function() {
         this.stream = new yt.TNodeJSInputStream();
         this.reader = new yt.TTestInputStream(this.stream);
@@ -100,15 +100,4 @@ describe("interface to streams", function() {
             done();
         }).bind(this));
     });
-
-    /*
-    it("should work in case 3", function() {
-        this.topic.write_to_input(new Buffer("12345"), 0, 5);
-        this.topic.close_input();
-
-        this.topic.test(3);
-        this.topic.test(3);
-        this.topic.test(3);
-    });
-    */
 });
