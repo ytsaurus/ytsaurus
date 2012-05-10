582c14a494d3c3d47fa71ab919ca8199e2b5ecac
Maxim Babenko <babenko@ytsaurus.tech>
2012-05-10 19:28:20 +0400

    result -> errno

--- yt/ytlib/exec_agent/unsafe_environment.cpp
+++ yt/ytlib/exec_agent/unsafe_environment.cpp
@@ -134,7 +134,7 @@ public:
 
         auto result = killpg(ProcessId, 9);
         if (result != 0) {
-            switch (result) {
+            switch (errno) {
                 case ESRCH:
                     // Process group doesn't exist already.
                     return;
