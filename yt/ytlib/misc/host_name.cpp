9d30e57ae3e140f5760a6e00610a5b7719220f57
Maxim Babenko <babenko@ytsaurus.tech>
2012-05-19 20:46:09 +0400

    Host name getter, service address constructors and crackers

---
+++ yt/ytlib/misc/host_name.cpp
@@ -0,0 +1,54 @@
+﻿#include "stdafx.h"
+#include "host_name.h"
+
+#include <util/system/hostname.h>
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+Stroka GetHostName()
+{
+    return ::GetHostName();
+}
+
+Stroka BuildServiceAddress(const Stroka& hostName, int port)
+{
+    return hostName + ":" + ToString(port);
+}
+
+void ParseServiceAddress(const Stroka& address, Stroka* hostName, int* port)
+{
+    int colonIndex = address.find_first_of(':');
+    if (colonIndex == Stroka::npos) {
+        ythrow yexception() << Sprintf("Service address %s is malformed",
+            ~address.Quote());
+    }
+
+    if (hostName) {
+        *hostName =  address.substr(0, colonIndex);
+    }
+
+    if (port) {
+        *port = FromString<int>(address.substr(colonIndex + 1));
+    }
+}
+
+int GetServicePort(const Stroka& address)
+{
+    int result;
+    ParseServiceAddress(address, NULL, &result);
+    return result;
+}
+
+Stroka GetServiceHostName(const Stroka& address)
+{
+    Stroka result;
+    ParseServiceAddress(address, &result, NULL);
+    return result;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
+
---
+++ yt/ytlib/misc/host_name.h
@@ -0,0 +1,30 @@
+﻿#pragma once
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! Returns the FQDN of the local host.
+Stroka GetHostName();
+
+//! Constructs an address of the form |hostName:port|.
+Stroka BuildServiceAddress(const Stroka& hostName, int port);
+
+//! Parses service address into host name and port number.
+//! Both #hostName and #port can be |NULL|.
+//! Throws if the address is malformed.
+void ParseServiceAddress(
+    const Stroka& address,
+    Stroka* hostName,
+    int* port);
+
+//! Extracts port number from a service address.
+//! Throws if the address is malformed.
+int GetServicePort(const Stroka& address);
+
+//! Extracts host name from a service address.
+Stroka GetServiceHostName(const Stroka& address);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
