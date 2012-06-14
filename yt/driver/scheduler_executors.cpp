b79c7fbd8299e5d20c2d8a374ac49690655b84d5
Maxim Babenko <babenko@ytsaurus.tech>
2012-06-14 14:29:48 +0400

    Build fixed

--- yt/driver/scheduler_executors.cpp
+++ yt/driver/scheduler_executors.cpp
@@ -263,7 +263,7 @@ private:
 
             if (!failedJobIds.empty()) {
                 printf("\n");
-                printf("%   " PRISZT " job(s) have failed:", failedJobIds.size());
+                printf("%" PRISZT " job(s) have failed:", failedJobIds.size());
                 printf("%35s %15s\n", "Id", "Address");
                 FOREACH (const auto& jobId, failedJobIds) {
                     auto job = jobs->GetChild(jobId.ToString());
