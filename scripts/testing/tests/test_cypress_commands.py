7720c5e7198aa42d3b7e1315c1ad5fe4ef40a294
Maxim Babenko <babenko@ytsaurus.tech>
2012-06-22 16:03:44 +0400

    Trying to fix the tests

--- scripts/testing/tests/test_cypress_commands.py
+++ scripts/testing/tests/test_cypress_commands.py
@@ -5,7 +5,7 @@ from yt_commands import *
 
 ##################################################################
 
-class TestCypressCommands(unittest.TestCase, YTEnvSetup):
+class TestCypressCommands(YTEnvSetup, unittest.TestCase):
     NUM_MASTERS = 3
     NUM_HOLDERS = 0
 
