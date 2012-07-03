b6523482d6d0177d89fb6c6040f6b4e977ea3778
Dmitry Panin <panin@yandex-team.ru>
2012-07-03 19:10:27 +0400

    Tests: added sort by missing column

--- scripts/testing/tests/test_scheduler_sort.py
+++ scripts/testing/tests/test_scheduler_sort.py
@@ -63,3 +63,20 @@ class TestSchedulerSortCommands(YTEnvSetup):
              out='//tmp/t_out',
              key_columns='foo')
 
+    def test_missing_column(self):
+        v1 = {'key' : 'aaa'}
+        v2 = {'key' : 'bb'}
+        v3 = {'key' : 'bbxx'}
+        v4 = {'key' : 'zfoo'}
+        v5 = {'key' : 'zzz'}
+
+        create('table', '//tmp/t_in')
+        write('//tmp/t_in', [v3, v5, v1, v2, v4]) # some random order
+
+        create('table', '//tmp/t_out')
+
+        sort(in_='//tmp/t_in',
+             out='//tmp/t_out',
+             key_columns='missing_key')
+
+        assert len(read('//tmp/t_out')) == 5 # check only the number of raws
