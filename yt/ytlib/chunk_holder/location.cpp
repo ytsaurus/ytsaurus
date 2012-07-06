d6b27bc46d3f21478cd6f35fcb8f01313b6ad010
Maxim Babenko <babenko@ytsaurus.tech>
2012-07-06 10:26:18 +0400

    Logging cosmetics

--- yt/ytlib/chunk_holder/location.cpp
+++ yt/ytlib/chunk_holder/location.cpp
@@ -189,7 +189,7 @@ std::vector<TChunkDescriptor> TLocation::Scan()
             i64 chunkDataSize = NFS::GetFileSize(chunkDataFileName);
             i64 chunkMetaSize = NFS::GetFileSize(chunkMetaFileName);
             if (chunkMetaSize == 0) {
-                LOG_FATAL("Chunk %s has empty meta file", ~chunkMetaFileName);
+                LOG_FATAL("Chunk meta file %s is empty", ~chunkMetaFileName);
             }
             TChunkDescriptor descriptor;
             descriptor.Id = chunkId;
