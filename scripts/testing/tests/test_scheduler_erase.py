6c70030d839532272892220660c03a60233ff625
Dmitry Panin <panin@yandex-team.ru>
2012-07-09 18:08:06 +0400

    Turned on failing test (Issue #372) -- hope it will be fixed soon

--- scripts/testing/tests/test_scheduler_erase.py
+++ scripts/testing/tests/test_scheduler_erase.py
@@ -73,7 +73,7 @@ class TestSchedulerEraseCommands(YTEnvSetup):
 
 ###############################################################
 
-    @pytest.mark.xfail(run = False, reason = 'Issue #372')
+    #@pytest.mark.xfail(run = False, reason = 'Issue #372')
     def test_by_key(self):
         v = \
         [
