f39549ff89fc4e421bdb75a454c4a803c4ec10e2
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-07-12 18:20:14 +0400

    Fix copy_from_yt. It is working correctly now.

--- scripts/mapreduce_test/copy_from_yt/check.py
+++ scripts/mapreduce_test/copy_from_yt/check.py
@@ -9,4 +9,5 @@ operation = '"%s"' % sys.argv[1]
 for job in yt.list("//sys/operations/%s/jobs" % operation):
     job_path = "//sys/operations/%s/jobs/%s" % (operation, job)
     if yt.get(job_path + "/@state") == "failed":
+        print yt.get(job_path + "/@error")
         print yt.get(job_path + "/@address")
