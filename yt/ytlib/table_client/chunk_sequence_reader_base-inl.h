737a00295b55428ee1c179f850feb19006948953
Pavel Sushin <psushin@ytsaurus.tech>
2012-07-18 21:54:45 +0400

    Working on templated chunk_seq_reader.

--- yt/ytlib/table_client/chunk_sequence_reader_base-inl.h
+++ yt/ytlib/table_client/chunk_sequence_reader_base-inl.h
@@ -1,6 +1,46 @@
-﻿
+﻿#ifndef CHUNK_SEQUENCE_READER_BASE_INL_H_
+#error "Direct inclusion of this file is not allowed, include chunk_sequence_reader_base.h"
+#endif
+#undef CHUNK_SEQUENCE_READER_BASE_INL_H_
 
 namespace NYT {
+namespace NTableClient {
 
+////////////////////////////////////////////////////////////////////////////////
 
+template <class TReader>
+TChunkSequenceReaderBase<TReader>::TChunkSequenceReaderBase(
+    TChunkSequenceReaderConfigPtr config,
+    NRpc::IChannelPtr masterChannel,
+    NChunkClient::IBlockCachePtr blockCache,
+    std::vector<NProto::TInputChunk>&& inputChunks)
+    : Config(config)
+    , BlockCache(blockCache)
+    , InputChunks(inputChunks)
+    , MasterChannel(masterChannel)
+    , CurrentReaderIndex(-1)
+    , LastInitializedReader(-1)
+    , LastPreparedReader(-1)
+{
+    LOG_DEBUG("Chunk sequence reader created (ChunkCount: %d)", 
+        static_cast<int>(InputChunks.size()));
+
+    for (int i = 0; i < static_cast<int>(InputChunks.size()); ++i) {
+        Readers.push_back(NewPromise<TReaderPtr>());
+    }
+
+    for (int i = 0; i < Config->PrefetchWindow; ++i) {
+        PrepareNextChunk();
+    }
+}
+
+template <class TReader>
+void TChunkSequenceReaderBase<TReader>::PrepareNextChunk()
+{
+
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NTableClient
 } // namespace NYT
--- yt/ytlib/table_client/chunk_sequence_reader_base.h
+++ yt/ytlib/table_client/chunk_sequence_reader_base.h
@@ -1,4 +1,74 @@
 ﻿#pragma once
 
+#include "public.h"
+#include "private.h"
+#include "async_reader.h"
+
+#include <ytlib/table_client/table_reader.pb.h>
+#include <ytlib/chunk_client/public.h>
+#include <ytlib/transaction_client/transaction.h>
+#include <ytlib/rpc/channel.h>
+#include <ytlib/misc/async_stream_state.h>
+
+
 namespace NYT {
+namespace NTableClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TReader>
+class TChunkSequenceReaderBase
+    : public virtual TRefCounted
+{
+public:
+    TChunkSequenceReaderBase(
+        TChunkSequenceReaderConfigPtr config,
+        NRpc::IChannelPtr masterChannel,
+        NChunkClient::IBlockCachePtr blockCache,
+        std::vector<NProto::TInputChunk>&& inputChunks);
+
+    TAsyncError AsyncOpen();
+
+protected:
+    typedef TIntrusivePtr<TReader> TReaderPtr;
+
+    TChunkSequenceReaderConfigPtr Config;
+    NChunkClient::IBlockCachePtr BlockCache;
+    std::vector<NProto::TInputChunk> InputChunks;
+
+    NRpc::IChannelPtr MasterChannel;
+    TAsyncStreamState State;
+
+    int CurrentReaderIndex;
+    TReaderPtr CurrentReader;
+
+    /*!
+     *  If #TReaderOptions::KeepBlocks option is set then the reader keeps references
+     *  to all chunk readers it has opened during its lifetime.
+     */
+    std::vector< TPromise<TReaderPtr> > Readers;
+    int LastInitializedReader;
+    int LastPreparedReader;
+
+    virtual TReaderPtr CreateNewReader() = 0;
+
+    void PrepareNextChunk();
+
+    void OnReaderOpened(
+        TReaderPtr reader, 
+        int chunkIndex,
+        TError error);
+
+    void SwitchCurrentChunk(bool dropCurrentReader, TReaderPtr nextReader);
+    void OnItemFetched(TError error);
+
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NTableClient
 } // namespace NYT
+
+#define CHUNK_SEQUENCE_READER_BASE_INL_H_
+#include "chunk_sequence_reader_base-inl.h"
+#undef CHUNK_SEQUENCE_READER_BASE_INL_H_
