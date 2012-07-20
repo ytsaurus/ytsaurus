ef7dd3f8bedcbea169dbc6cce0abee9ab7d10b58
Dmitry Panin <panin@yandex-team.ru>
2012-07-20 15:30:10 +0400

    Removed unused files

--- yt/ytlib/cypress_client/public.h
+++ yt/ytlib/cypress_client/public.h
@@ -7,20 +7,6 @@ namespace NCypressClient {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TCypressManager;
-typedef TIntrusivePtr<TCypressManager> TCypressManagerPtr;
-
-struct INodeBehavior;
-typedef TIntrusivePtr<INodeBehavior> INodeBehaviorPtr;
-
-struct INodeTypeHandler;
-typedef TIntrusivePtr<INodeTypeHandler> INodeTypeHandlerPtr;
-
-struct ICypressNode;
-
-struct ICypressNodeProxy;
-typedef TIntrusivePtr<ICypressNodeProxy> ICypressNodeProxyPtr;
-
 ////////////////////////////////////////////////////////////////////////////////
             
 } // namespace NCypressClient
