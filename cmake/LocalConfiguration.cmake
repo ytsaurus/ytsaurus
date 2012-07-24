19f5c21347b7d4733e89e5fc28285af1144af70a
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2012-07-24 22:55:48 +0400

    By default build with bundled STL
    Closes #159

--- cmake/LocalConfiguration.cmake
+++ cmake/LocalConfiguration.cmake
@@ -13,9 +13,9 @@
 # Specify available build options.
 
 option(YT_BUILD_ENABLE_EXPERIMENTS "Build experiments" TRUE)
-option(YT_BUILD_ENABLE_TESTS "Build tests" TRUE)
-option(YT_BUILD_ENABLE_NODEJS "Build NodeJS extensions" FALSE)
-option(YT_BUILD_WITH_STLPORT "Build with STLport" TRUE)
+option(YT_BUILD_ENABLE_TESTS       "Build tests" TRUE)
+option(YT_BUILD_ENABLE_NODEJS      "Build NodeJS extensions" FALSE)
+option(YT_BUILD_WITH_STLPORT       "Build with STLport" FALSE)
 
 ################################################################################
 # Enforce developer to specify build type.
