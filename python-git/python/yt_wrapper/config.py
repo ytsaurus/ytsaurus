0c828b63440b4a215401b1b62c77794dc80606ee
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-07-29 03:07:39 +0400

    Fix paths in wrapper config

--- python-git/python/yt_wrapper/config.py
+++ python-git/python/yt_wrapper/config.py
@@ -5,8 +5,8 @@ DEFAULT_FORMAT = YamrFormat(has_subkey=True, lenval=False)
 WAIT_TIMEOUT = 5.0
 WRITE_BUFFER_SIZE = 10 ** 8
 READ_BUFFER_SIZE = 10 ** 8
-FILE_STORAGE = "//home/files"
-TEMP_TABLES_STORAGE = "//tmp"
+FILE_STORAGE = "//tmp/yt_wrapper/file_storage"
+TEMP_TABLES_STORAGE = "//tmp/yt_wrapper/table_storage"
 
 REPLACE_FILES_IN_OPERATION = True
 MERGE_SRC_TABLES_BEFORE_OPERATION = False
