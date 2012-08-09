f36342153a2c1ad83a5f019f41ee5b44daf717ab
Maxim Babenko <babenko@ytsaurus.tech>
2012-08-10 00:31:27 +0400

    Reverted [Speeding up protobuf serialization] since SerializedToCodedStream calls ByteSize anyway

--- yt/ytlib/misc/protobuf_helpers.cpp
+++ yt/ytlib/misc/protobuf_helpers.cpp
@@ -7,53 +7,11 @@ namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static const size_t MinOutputBlobSize = 256;
-
-class TBlobOutputStream
-    : public google::protobuf::io::ZeroCopyOutputStream
-{
-public:
-    explicit TBlobOutputStream(TBlob* blob)
-        : Blob(blob)
-    {
-        Blob->clear();
-    }
-
-    virtual bool Next(void** data, int* size) override
-    {
-        size_t oldSize = Blob->size();
-
-        if (oldSize < Blob->capacity()) {
-            Blob->resize(Blob->capacity());
-        } else {
-            Blob->resize(std::max(oldSize * 2, MinOutputBlobSize));
-        }
-
-        *data = Blob->data() + oldSize;
-        *size = Blob->size() - oldSize;
-        return true;
-    }
-
-    void BackUp(int count) override
-    {
-        Blob->resize(Blob->size() - count);
-    }
-
-    i64 ByteCount() const override
-    {
-        return Blob->size();
-    }
-
-private:
-    TBlob* Blob;
-
-};
-
 bool SerializeToProto(const google::protobuf::Message* message, TBlob* data)
 {
-    TBlobOutputStream zeroCopyStream(data);
-    google::protobuf::io::CodedOutputStream codedOutputStream(&zeroCopyStream);
-    return message->SerializePartialToCodedStream(&codedOutputStream);
+    int size = message->ByteSize();
+    data->resize(size);
+    return message->SerializeToArray(&*data->begin(), size);
 }
 
 bool DeserializeFromProto(google::protobuf::Message* message, TRef data)
