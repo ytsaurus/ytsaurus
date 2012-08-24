37da935fe262f3594307e8e73c8e676ddd25ed70
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2012-08-24 14:18:47 +0400

    Update run_perf.sh

--- scripts/run_perf.sh
+++ scripts/run_perf.sh
@@ -1,10 +1,19 @@
 #!/bin/bash
 
+if [[ "$1" == "fork" ]]; then
+    $0 > /dev/null 2> /dev/null &
+    disown
+    exit
+fi
+
 PERF=/usr/bin/perf_2.6.38-15
 DATA=/yt/disk1/profile
 
 mkdir -p $DATA
 
 while [[ -f $DATA/_COLLECT_PROFILE ]]; do
-    $PERF -g -T -a -o $DATA/$(date +%Y%m%d_%H%M%S) sleep 5
-end
+    _PATH=$DATA/$(date +%Y%m%d)/$(date +%H00)
+    _FILE=$(date +%H%M%S)
+    mkdir -p $_PATH
+    $PERF record -g -T -a -o $_PATH/$_FILE sleep 5
+done
