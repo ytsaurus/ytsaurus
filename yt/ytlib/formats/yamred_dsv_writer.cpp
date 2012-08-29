908e15df7dfd9f70645f3c95b31e88cceb419dbf
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-08-29 19:21:17 +0400

    Add forgotten files

---
+++ yt/ytlib/formats/yamred_dsv_writer.cpp
@@ -0,0 +1,152 @@
+#include "yamred_dsv_writer.h"
+
+#include <ytree/yson_format.h>
+
+namespace NYT {
+namespace NFormats {
+
+using namespace NYTree;
+
+////////////////////////////////////////////////////////////////////////////////
+
+TYamredDsvWriter::TYamredDsvWriter(TOutputStream* stream, TYamredDsvFormatConfigPtr config)
+    : Stream(stream)
+    , Config(config ? config : New<TYamredDsvFormatConfig>())
+    , State(EState::None)
+    , KeyColumnNames(Config->KeyColumnNames.begin(), Config->KeyColumnNames.end())
+    , SubkeyColumnNames(Config->SubkeyColumnNames.begin(), Config->SubkeyColumnNames.end())
+    , AllowBeginMap(true)
+    , IsValueEmpty(true)
+{ }
+
+TYamredDsvWriter::~TYamredDsvWriter()
+{ }
+
+void TYamredDsvWriter::OnIntegerScalar(i64 value)
+{
+    RememberValue(ToString(value));
+}
+
+void TYamredDsvWriter::OnDoubleScalar(double value)
+{
+    RememberValue(ToString(value));
+}
+
+void TYamredDsvWriter::OnStringScalar(const TStringBuf& value)
+{
+    RememberValue(value);
+}
+
+void TYamredDsvWriter::OnEntity()
+{
+    ythrow yexception() << "Entities are not supported by Yamred Dsv";
+}
+
+void TYamredDsvWriter::OnBeginList()
+{
+    ythrow yexception() << "Lists are not supported by Yamred Dsv";
+}
+
+void TYamredDsvWriter::OnListItem()
+{ }
+
+void TYamredDsvWriter::OnEndList()
+{
+    YUNREACHABLE();
+}
+
+void TYamredDsvWriter::OnBeginMap()
+{
+    if (!AllowBeginMap) {
+        ythrow yexception() << "Embedded maps are not supported by Yamred Dsv";
+    }
+    AllowBeginMap = false;
+
+    KeyFields.clear();
+    SubkeyFields.clear();
+}
+
+void TYamredDsvWriter::OnKeyedItem(const TStringBuf& key)
+{
+    if (State != EState::None) {
+        ythrow yexception() << "Pass key without value is forbidden";
+    }
+    Key = key;
+    State = EState::ExpectingValue;
+}
+
+void TYamredDsvWriter::OnEndMap()
+{
+    AllowBeginMap = true;
+    WriteRow();
+}
+
+void TYamredDsvWriter::OnBeginAttributes()
+{
+    ythrow yexception() << "Attributes are not supported by Yamred Dsv";
+}
+
+void TYamredDsvWriter::OnEndAttributes()
+{
+    YUNREACHABLE();
+}
+
+void TYamredDsvWriter::RememberValue(const TStringBuf& value)
+{
+    if (State != EState::ExpectingValue) {
+        ythrow yexception() << "Pass value without key is forbidden";
+    }
+    if (KeyColumnNames.count(Key) > 0) {
+        YASSERT(KeyFields.count(Key) == 0);
+        KeyFields[Key] = value;
+    }
+    else if (SubkeyColumnNames.count(Key) > 0) {
+        YASSERT(SubkeyFields.count(Key) == 0);
+        SubkeyFields[Key] = value;
+    }
+    else {
+        if (IsValueEmpty) {
+            IsValueEmpty = false;
+        }
+        else {
+            ValueBuffer.Write(Config->FieldSeparator);
+        }
+        ValueBuffer.Write(Key);
+        ValueBuffer.Write(Config->KeyValueSeparator);
+        ValueBuffer.Write(value);
+    }
+    State = EState::None;
+}
+
+void TYamredDsvWriter::WriteRow()
+{
+    WriteYamrField(Config->KeyColumnNames, KeyFields);
+    if (Config->HasSubkey) {
+        WriteYamrField(Config->SubkeyColumnNames, SubkeyFields);
+    }
+    Stream->Write(ValueBuffer.Begin(), ValueBuffer.GetSize());
+    Stream->Write(Config->RecordSeparator);
+}
+
+void TYamredDsvWriter::WriteYamrField(
+    const std::vector<Stroka>& columnNames,
+    const yhash_map<Stroka, Stroka>& fieldValues)
+{
+    for (int i = 0; i < columnNames.size(); ++i) {
+        if (fieldValues.count(columnNames[i]) == 0) {
+            ythrow yexception() << "There is no required column with name " << columnNames[i];
+        }
+        Stream->Write(fieldValues.find(columnNames[i])->second);
+        if (i + 1 != columnNames.size()) {
+            Stream->Write(Config->YamrKeysSeparator);
+        }
+    }
+    Stream->Write(Config->FieldSeparator);
+}
+
+
+////////////////////////////////////////////////////////////////////////////////
+            
+} // namespace NFormats
+} // namespace NYT
+
---
+++ yt/ytlib/formats/yamred_dsv_writer.h
@@ -0,0 +1,73 @@
+#pragma once
+
+#include "public.h"
+#include "config.h"
+#include "helpers.h"
+
+#include <ytlib/misc/blob_output.h>
+
+namespace NYT {
+namespace NFormats {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! Note: #TYamrWriter supports only tabular data
+class TYamredDsvWriter
+    : public TFormatsConsumerBase
+{
+public:
+    explicit TYamredDsvWriter(
+        TOutputStream* stream,
+        TYamredDsvFormatConfigPtr config = NULL);
+
+    ~TYamredDsvWriter();
+
+    // IYsonConsumer overrides.
+    virtual void OnStringScalar(const TStringBuf& value) override;
+    virtual void OnIntegerScalar(i64 value) override;
+    virtual void OnDoubleScalar(double value) override;
+    virtual void OnEntity() override;
+    virtual void OnBeginList() override;
+    virtual void OnListItem() override;
+    virtual void OnEndList() override;
+    virtual void OnBeginMap() override;
+    virtual void OnKeyedItem(const TStringBuf& key) override;
+    virtual void OnEndMap() override;
+    virtual void OnBeginAttributes() override;
+    virtual void OnEndAttributes() override;
+
+private:
+    TOutputStream* Stream;
+    TYamredDsvFormatConfigPtr Config;
+
+    Stroka Key;
+    DECLARE_ENUM(EState,
+        (None)
+        (ExpectingValue)
+    );
+    EState State;
+    
+    yhash_set<Stroka> KeyColumnNames;
+    yhash_set<Stroka> SubkeyColumnNames;
+
+    yhash_map<Stroka, Stroka> KeyFields;
+    yhash_map<Stroka, Stroka> SubkeyFields;
+
+    TBlobOutput ValueBuffer;
+
+    bool IsValueEmpty;
+    bool AllowBeginMap;
+
+    void RememberValue(const TStringBuf& value);
+
+    void WriteRow();
+    void WriteYamrField(
+        const std::vector<Stroka>& columnNames,
+        const yhash_map<Stroka, Stroka>& fieldValues);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+            
+} // namespace NFormats
+} // namespace NYT
+
