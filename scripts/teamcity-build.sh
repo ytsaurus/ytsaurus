ddf2a8fd323c593676c800b87a855437f3443533
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2012-09-21 18:03:07 +0400

    Temporary disable gcc-4.7

--- scripts/teamcity-build.sh
+++ scripts/teamcity-build.sh
@@ -78,7 +78,8 @@ try_to_find_compiler() {
     fi
 }
 
-try_to_find_compiler "4.7"
+# TODO(sandello): Fix build under gcc-4.7.
+# try_to_find_compiler "4.7"
 try_to_find_compiler "4.6"
 try_to_find_compiler "4.5"
 
