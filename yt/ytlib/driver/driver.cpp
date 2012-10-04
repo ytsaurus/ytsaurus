19c1611bc9bfd0feac8150e7c212356bcf380f04
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-10-04 13:07:34 +0400

    Fix exists command

--- yt/ytlib/driver/driver.cpp
+++ yt/ytlib/driver/driver.cpp
@@ -77,7 +77,7 @@ public:
         REGISTER(TLockCommand,              "lock",        Null,       Null,       true,  false);
         REGISTER(TCopyCommand,              "copy",        Null,       Structured, true,  false);
         REGISTER(TMoveCommand,              "move",        Null,       Null,       true,  false);
-        REGISTER(TExistsCommand,            "exists",      Null,       Structured, true,  false);
+        REGISTER(TExistsCommand,            "exists",      Null,       Structured, false,  false);
 
         REGISTER(TUploadCommand,            "upload",      Binary,     Structured, true,  true );
         REGISTER(TDownloadCommand,          "download",    Null,       Binary,     false, true );
