30033548021da781a462f169ec25cbae75ac85b7
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-10-24 00:44:06 +0400

    Fix hack changelog test

--- tests/integration/tests/test_hack_changelog.py
+++ tests/integration/tests/test_hack_changelog.py
@@ -4,52 +4,56 @@ from yt_env_setup import YTEnvSetup
 from yt_commands import *
 
 import os
-import time
 import subprocess
 
 class TestHackChangelog(YTEnvSetup):
     NUM_MASTERS = 1
     NUM_NODES = 0
 
-    def test_all(self):
-        set_str("//home/@test_attribute", "123")
+    def prepare_changelog_path(self):
+        # Search changelog
+        changelogs_path = self.Env._master_configs[0]["meta_state"]["changelogs"]["path"]
+        self.changelog = os.path.join(
+            changelogs_path,
+            filter(lambda f: not f.endswith("index"), os.listdir(changelogs_path))[0])
 
-        self.assertTrue(get_str("//home/@").find("test_attribute") != -1)
-
+    def stop_master(self):
         # TODO(panin): make convenient way for this
         # Stop master
         self.kill_process(*self.Env.process_to_kill[0])
         self.Env.process_to_kill.pop()
-        
-        changelogs_path = self.Env._master_configs[0]["meta_state"]["changelogs"]["path"]
-        changelog = os.path.join(
-            changelogs_path,
-            filter(lambda f: not f.endswith("index"), os.listdir(changelogs_path))[0])
 
-        # Fetch number of records in changelog
+    def get_record_count(self):
         output = subprocess.check_output(
             ["process_changelog",
              "--action", "read",
-             "--filename", changelog])
+             "--filename", self.changelog])
 
-        record_count = None
         for line in output.split("\n"):
             if "Record count" in line:
-                record_count = int(line[line.find("=") + 1:])
+                return int(line[line.find("=") + 1:])
 
-        # Replace last record with no operation
+    def truncate_changelog(self, truncate_record_number):
+        record_count = self.get_record_count()
         subprocess.check_call(
             ["process_changelog",
              "--action", "remove",
-             "--filename", changelog,
-             "-f", str(record_count - 1),
+             "--filename", self.changelog,
+             "-f", str(truncate_record_number),
              "-t", str(record_count - 1)],
             stdin=subprocess.PIPE)
 
+    def test_all(self):
+        self.prepare_changelog_path()
+        record_count = self.get_record_count()
 
-        # Restore master
+        set_str("//home/@test_attribute", "123")
+        assert get_str("//home/@").find("test_attribute") != -1
+
+        self.stop_master()
+        self.truncate_changelog(record_count)
+
         self.Env._run_masters(prepare_files=False)
-        
         assert get_str("//home/@").find("test_attribute") == -1
 
 
