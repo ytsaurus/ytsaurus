e29e418f3da2676c97a78ae8d952fdf468b67b2b
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-10-30 14:37:06 +0400

    Fix setting of option 'ping_ancestor_transactions'

--- python-git/python/yt/wrapper/transaction_commands.py
+++ python-git/python/yt/wrapper/transaction_commands.py
@@ -1,12 +1,12 @@
 import config
 from http import make_request
-from common import update
+from common import update, bool_to_string
 
 from copy import deepcopy
 
 def add_transaction_params(params):
     return update(deepcopy(params), {
-        "ping_ancestor_transactions": config.PING_ANSECTOR_TRANSACTIONS,
+        "ping_ancestor_transactions": bool_to_string(config.PING_ANSECTOR_TRANSACTIONS),
         "transaction_id": config.TRANSACTION})
 
 
