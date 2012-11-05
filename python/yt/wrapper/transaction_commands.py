5a388dc617a656e93afa34d02f3e470c54628f3c
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-11-06 02:00:21 +0400

    Fix transactions

--- python/yt/wrapper/transaction_commands.py
+++ python/yt/wrapper/transaction_commands.py
@@ -4,29 +4,23 @@ from common import update, bool_to_string
 
 from copy import deepcopy
 
+def transaction_params(transaction=None, ping_ancestor_transactions=None):
+    if transaction is None: transaction = config.TRANSACTION
+    if ping_ancestor_transactions is None: ping_ancestor_transactions = config.PING_ANSECTOR_TRANSACTIONS
+    return {"ping_ancestor_transactions": bool_to_string(ping_ancestor_transactions),
+            "transaction_id": transaction}
+
 def add_transaction_params(params):
-    return update(deepcopy(params), {
-        "ping_ancestor_transactions": bool_to_string(config.PING_ANSECTOR_TRANSACTIONS),
-        "transaction_id": config.TRANSACTION})
+    return update(deepcopy(params), transaction_params())
 
+def start_transaction(parent_transaction=None, ping_ansector_transactions=None):
+    return make_request("start_tx", transaction_params(parent_transaction, ping_ansector_transactions))
 
-def start_transaction(parent_transaction=None, ping_ansector_transactions=False):
-    if parent_transaction is None: parent_transaction = config.TRANSACTION
-    params = {"ping_ansector_transactions": config.PING_ANSECTOR_TRANSACTIONS,
-              "transaction_id": parent_transaction}
-    return make_request("start_tx", params)
+def abort_transaction(transaction, ping_ansector_transactions=None):
+    return make_request("abort_tx", transaction_params(transaction, ping_ansector_transactions))
 
-def abort_transaction(transaction, ping_ansector_transactions=False):
-    params = {"ping_ansector_transactions": config.PING_ANSECTOR_TRANSACTIONS,
-              "transaction_id": transaction}
-    return make_request("abort_tx", params)
+def commit_transaction(transaction, ping_ansector_transactions=None):
+    return make_request("commit_tx", transaction_params(transaction, ping_ansector_transactions))
 
-def commit_transaction(transaction, ping_ansector_transactions=False):
-    params = {"ping_ansector_transactions": config.PING_ANSECTOR_TRANSACTIONS,
-              "transaction_id": transaction}
-    return make_request("commit_tx", params)
-
-def renew_transaction(transaction, ping_ansector_transactions=False):
-    params = {"ping_ansector_transactions": config.PING_ANSECTOR_TRANSACTIONS,
-              "transaction_id": transaction}
-    return make_request("renew_tx", params)
+def renew_transaction(transaction, ping_ansector_transactions=None):
+    return make_request("renew_tx", transaction_params(transaction, ping_ansector_transactions))
