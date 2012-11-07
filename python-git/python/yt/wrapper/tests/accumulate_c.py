e1430d88e2b32f8e6b725dccc958d88449697822
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-11-07 21:29:06 +0400

    Add forgotten file

---
+++ python-git/python/yt/wrapper/tests/accumulate_c.py
@@ -0,0 +1,44 @@
+#!/usr/bin/env python
+
+import sys
+from itertools import imap
+from functools import partial
+
+def capitilizeB(rec):
+    if "b" in rec: rec["b"] = rec["b"].upper()
+    return rec
+
+""" Methods for records convertion """
+def record_to_line(rec, eoln=True):
+    body = "\t".join("=".join(map(str, item)) for item in rec.iteritems())
+    return "%s%s" % (body, "\n" if eoln else "")
+
+def line_to_record(line):
+    return dict(field.split("=", 1) for field in line.strip("\n").split("\t"))
+
+if __name__ == "__main__":
+    lines = sys.stdin.readlines()
+    print >>sys.stderr, lines
+    recs = map(partial(line_to_record), lines)
+    print >>sys.stderr, recs
+
+    res = []
+
+    curA = None
+    sum = 0
+    for rec in recs:
+        if "a" not in rec: continue
+        a = rec["a"]
+        if a != curA:
+            if curA is not None:
+                res.append({"a": curA, "c": sum})
+            curA = a
+            sum = 0.0
+        sum += float(rec.get("c", 0.0))
+
+    if curA is not None:
+        res.append({"a": curA, "c": sum})
+
+    sys.stdout.writelines(imap(partial(record_to_line), res))
+    
+
