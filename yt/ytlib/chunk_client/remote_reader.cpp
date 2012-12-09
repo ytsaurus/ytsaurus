123f53cb2c0591f72f20d531657fb51e38ee160f
Maxim Babenko <babenko@ytsaurus.tech>
2012-12-09 16:30:23 +0400

    Cosmetics

--- yt/ytlib/chunk_client/remote_reader.cpp
+++ yt/ytlib/chunk_client/remote_reader.cpp
@@ -775,8 +775,9 @@ private:
         *request->mutable_chunk_id() = reader->ChunkId.ToProto();
         request->set_all_extension_tags(AllExtensionTags);
 
-        if (PartitionTag)
+        if (PartitionTag) {
             request->set_partition_tag(PartitionTag.Get());
+        }
 
         ToProto(request->mutable_extension_tags(), ExtensionTags);
         request->Invoke().Subscribe(
