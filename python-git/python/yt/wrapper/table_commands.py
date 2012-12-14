13084adde10fd66c957a03c955cfbff40667a0f4
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-12-15 01:26:21 +0400

    Fix finalize. Fix smart job count.

--- python-git/python/yt/wrapper/table_commands.py
+++ python-git/python/yt/wrapper/table_commands.py
@@ -453,7 +453,8 @@ def run_map_reduce(mapper, reducer, source_table, destination_table,
             spec = update(
                 {"map_job_count": map_job_count,
                  "partition_count": max(1, map_job_count / 2),
-                 "min_data_size_per_map_job": config.MIN_SIZE_PER_JOB},
+                 "min_data_size_per_map_job": config.MIN_SIZE_PER_JOB,
+                 "min_data_size_per_partition": 2 * config.MIN_SIZE_PER_JOB},
                 spec)
         return spec
 
