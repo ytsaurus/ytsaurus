3607c92924ec65e5f7a58379eafc2088aac72f1c
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-12-16 02:00:15 +0400

    Fix smart job count in wrapper

--- python-git/python/yt/wrapper/table_commands.py
+++ python-git/python/yt/wrapper/table_commands.py
@@ -394,7 +394,8 @@ def sort_table(source_table, destination_table=None, sort_by=None,
         if "partition_count" in spec:
             return spec
         return update(
-            {"partition_count": _calc_job_count(source_table, max_data=config.MAX_SIZE_PER_JOB/2),
+            {"partition_job_count": _calc_job_count(source_table, max_data=config.MAX_SIZE_PER_JOB),
+             "partition_count": _calc_job_count(source_table, max_data=config.MAX_SIZE_PER_JOB/2),
              "min_data_size_per_sort_job": config.MIN_SIZE_PER_JOB},
             spec)
 
