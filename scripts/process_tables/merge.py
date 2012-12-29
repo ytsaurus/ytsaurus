c90e10ccfc8d6b9a590ab832bda3fa55a86f1b82
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2012-12-29 14:31:19 +0400

    Add processing errors in merge script

--- scripts/process_tables/merge.py
+++ scripts/process_tables/merge.py
@@ -4,12 +4,14 @@ from atomic import process_tasks_from_list
 
 import yt.wrapper as yt
 
+def merge(table):
+    try:
+        yt.merge_tables(table, table, "unordered",
+                        table_writer={"codec":"gzip_best_compression"},
+                        spec={"combine_chunks":"true"})
+    except yt.YtError as e:
+        print "Failed to merge table %s with error %s" % (table, repr(e))
+
 if __name__ == "__main__":
-    process_tasks_from_list(
-        "//home/ignat/tables_to_merge", 
-        lambda table: \
-            yt.merge_tables(table, table, "unordered",
-                            table_writer={"codec":"gzip_best_compression"},
-                            spec={"combine_chunks":"true"})
-    )
+    process_tasks_from_list("//home/ignat/tables_to_merge", merge)
 
