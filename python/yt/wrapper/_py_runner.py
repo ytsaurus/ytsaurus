4eba8e3d74bc2db3599434a7bf885d548d60e572
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-01-08 09:26:17 +0400

    Fix running python operations

--- python/yt/wrapper/_py_runner.py
+++ python/yt/wrapper/_py_runner.py
@@ -1,26 +1,44 @@
-import imp
-import sys
-import itertools
-import zipfile
+def main():
+    # We should use local imports because of replacing __main__ module cause cleaning globals
+    import sys
+    import itertools
+    #import imp
+    import shelve
+    import zipfile
 
-if __name__ == "__main__":
     # Variable names start with "__" to avoid accidental intersection with scope of user function
     __operation_dump = sys.argv[1]
     __modules_archive = sys.argv[2]
-    __main_filename = sys.argv[3]
-    __main_module_name = sys.argv[4]
-    
-    with zipfile.ZipFile(__modules_archive) as __zip:
-        __zip.extractall("modules")
-    sys.path = ["./modules"] + sys.path
-    
-    from yt.wrapper.pickling import load
+    #__main_filename = sys.argv[3]
+    #__main_module_name = sys.argv[4]
+    __config_dump_filename = sys.argv[5]
 
-    # Magically replace out main module  by client side main
-    sys.modules['__main__'] = imp.load_module(__main_module_name, open(__main_filename, 'U'), __main_filename, ('.py', 'U', 1))
-    imp.reload(sys)
-    
+    # Unfortunately we cannot use fixes version of ZipFile
+    __zip = zipfile.ZipFile(__modules_archive)
+    __zip.extractall("modules")
+    __zip.close()
+
+    sys.path = ["./modules"] + sys.path
+
+    # TODO(ignat): remove if it is unnesessary.
+    # I don't understand now what the reason of this action.
+    # ...
+    # Magically replace out main module by client side main. It is get from mapreducelib.py
+    #sys.modules['__main__'] = imp.load_module(__main_module_name, open(__main_filename, 'rU'), __main_filename, ('', 'rU', imp.PY_SOURCE))
+
+    from yt.wrapper.pickling import load
     operation = load(open(__operation_dump))
+
+    import yt.wrapper.config as config
+    config_shelve = shelve.open(__config_dump_filename)
+    try:
+        for key, value in config_shelve.iteritems():
+            config.__dict__[key] = value
+    finally:
+        config_shelve.close()
+
     sys.stdout.writelines(itertools.chain(*itertools.imap(operation, sys.stdin.xreadlines())))
 
+if __name__ == "__main__":
+    main()
 
