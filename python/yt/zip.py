934624ac9a371204553837bd16f2fca677c9e921
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-01-15 21:52:34 +0400

    Fix using ZipFile for python2.6. Fix recursive mkdir with set prefix.

--- python/yt/zip.py
+++ python/yt/zip.py
@@ -3,7 +3,7 @@ import sys
 if sys.version_info[:2] == (2, 6):
     import zipfile
     class ZipFile(zipfile.ZipFile):
-        def __init__(self, file, mode="r", compression=zipfile.ZipFile.ZIP_STORED, allowZip64=False):
+        def __init__(self, file, mode="r", compression=zipfile.ZIP_STORED, allowZip64=False):
             super(self, ZipFile).__init__(file, mode, compression, allowZip64)
         def __enter__(self):
             return self
