a505208fca1e4f9e0e824bb99fecc7ac5e5aa90f
Pavel Sushin <psushin@ytsaurus.tech>
2013-01-25 03:16:51 +0400

    Disable memory limits for job proxy. Return footprint to 256Mb.

--- yt/server/scheduler/job_resources.cpp
+++ yt/server/scheduler/job_resources.cpp
@@ -18,7 +18,7 @@ using namespace NJobProxy;
 
 //! Additive term for each job memory usage.
 //! Accounts for job proxy process and other lightweight stuff.
-static const i64 FootprintMemorySize = (i64) 512 * 1024 * 1024;
+static const i64 FootprintMemorySize = (i64) 256 * 1024 * 1024;
 
 //! Nodes having less free memory are considered fully occupied.
 static const i64 LowWatermarkMemorySize = (i64) 512 * 1024 * 1024;
