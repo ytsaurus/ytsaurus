031de829c7612ac38277c4f7b8b033d08d0cd688
Maxim Babenko <babenko@ytsaurus.tech>
2013-03-11 13:02:58 +0100

    Don't enable totals for UnorderedMergeJobCounter

--- yt/server/scheduler/sort_controller.cpp
+++ yt/server/scheduler/sort_controller.cpp
@@ -67,7 +67,8 @@ public:
         , SortStartThresholdReached(false)
         , MergeStartThresholdReached(false)
         , SortedMergeJobCounter(0)
-        , UnorderedMergeJobCounter(0)
+        // Cannot do similar for UnorderedMergeJobCounter since the number of unsorted merge jobs
+        // is hard to predict.
         , SimpleSort(false)
     { }
 
@@ -1527,7 +1528,6 @@ private:
                     skippedCount);
 
                 lastPartition->Maniac = true;
-                UnorderedMergeJobCounter.Increment(1);
                 YCHECK(skippedCount >= 1);
 
                 auto successorKey = GetSuccessorKey(*sampleKey);
