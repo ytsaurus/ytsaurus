5a3909f7856298035e81d94a6681ef12d36b291b
Maxim Babenko <babenko@ytsaurus.tech>
2013-03-16 22:02:25 +0400

    Fix warning

--- yt/ytlib/meta_state/composite_meta_state.h
+++ yt/ytlib/meta_state/composite_meta_state.h
@@ -35,8 +35,6 @@ public:
         TCompositeMetaStatePtr metaState);
 
 protected:
-    friend class TCompositeMetaState;
-
     IMetaStateManagerPtr MetaStateManager;
     TCompositeMetaStatePtr MetaState;
 
