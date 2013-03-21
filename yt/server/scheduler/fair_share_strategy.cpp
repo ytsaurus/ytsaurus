114d1c21227fe9cc8770861a2f280fdc06db4308
Maxim Babenko <babenko@ytsaurus.tech>
2013-03-21 21:51:43 +0400

    Towards real-time operation settings. Episode 2: Watching the spec

--- yt/server/scheduler/fair_share_strategy.cpp
+++ yt/server/scheduler/fair_share_strategy.cpp
@@ -8,6 +8,8 @@
 #include <ytlib/ytree/ypath_proxy.h>
 #include <ytlib/ytree/fluent.h>
 
+#include <ytlib/scheduler/helpers.h>
+
 #include <ytlib/object_client/object_service_proxy.h>
 
 #include <ytlib/logging/log.h>
@@ -877,19 +879,24 @@ private:
     }
 
 
+    TPooledOperationSpecPtr ParseSpec(TOperationPtr operation, INodePtr specNode)
+    {
+        try {
+            return ConvertTo<TPooledOperationSpecPtr>(specNode);
+        } catch (const std::exception& ex) {
+            LOG_ERROR(ex, "Error parsing spec of pooled operation %s, defaults will be used",
+                ~ToString(operation->GetOperationId()));
+            return New<TPooledOperationSpec>();
+        }
+    }
+
+
     void OnOperationRegistered(TOperationPtr operation)
     {
         auto operationElement = New<TOperationElement>(Config, Host, operation);
         YCHECK(OperationToElement.insert(std::make_pair(operation, operationElement)).second);
 
-        TPooledOperationSpecPtr spec;
-        try {
-            spec = ConvertTo<TPooledOperationSpecPtr>(operation->GetSpec());
-        } catch (const std::exception& ex) {
-            LOG_ERROR(ex, "Error parsing spec of pooled operation %s, defaults will be used",
-                ~ToString(operation->GetOperationId()));
-            spec = New<TPooledOperationSpec>();
-        }
+        auto spec = ParseSpec(operation, operation->GetSpec());
 
         auto poolId = spec->Pool ? spec->Pool.Get() : DefaultPoolId;
         auto pool = FindPool(poolId);
@@ -903,6 +910,13 @@ private:
         operationElement->SetPool(~pool);
         pool->AddChild(operationElement);
         pool->ResourceUsage() += operationElement->ResourceUsage();
+
+        Host->GetMasterConnector()->AddOperationWatcherRequester(
+            operation,
+            BIND(&TFairShareStrategy::RequestOperationSpec, this, operation));
+        Host->GetMasterConnector()->AddOperationWatcherHandler(
+            operation,
+            BIND(&TFairShareStrategy::HandleOperationSpec, this, operation));
 
         LOG_INFO("Operation added to pool (OperationId: %s, Pool: %s)",
             ~ToString(operation->GetOperationId()),
@@ -925,6 +939,30 @@ private:
         if (pool->IsEmpty() && pool->IsDefaultConfigured()) {
             UnregisterPool(pool);
         }
+    }
+
+
+    void RequestOperationSpec(
+        TOperationPtr operation,
+        TObjectServiceProxy::TReqExecuteBatchPtr batchReq)
+    {
+        auto operationPath = GetOperationPath(operation->GetOperationId());
+        auto req = TYPathProxy::Get(operationPath + "/@spec");
+        batchReq->AddRequest(req, "get_spec");
+    }
+
+    void HandleOperationSpec(
+        TOperationPtr operation,
+        TObjectServiceProxy::TRspExecuteBatchPtr batchRsp)
+    {
+        auto element = FindOperationElement(operation);
+        if (!element)
+            return;
+
+        auto rsp = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_spec");
+        auto specNode = ConvertToNode(TYsonString(rsp->value()));
+        auto spec = ParseSpec(operation, specNode);
+        element->SetSpec(spec);
     }
 
 
