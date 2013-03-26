cc923eb106830620c4e8a773e2059997ccbd2f68
Pavel Sushin <psushin@ytsaurus.tech>
2013-03-26 20:41:54 +0400

    Hopefully fixed test.

--- python/yt/wrapper/tests/files/many_output.py
+++ python/yt/wrapper/tests/files/many_output.py
@@ -7,7 +7,7 @@ if __name__ == "__main__":
     for line in sys.stdin:
         pass
 
-    for i in xrange(15):
+    for i in xrange(15 + 1):
         value = str(i)
         rec = '\t'.join([value] * 3) + '\n'
         os.write(i * 3 + 1, rec)
