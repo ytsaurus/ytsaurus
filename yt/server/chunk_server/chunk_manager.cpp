0e26bb6aa9fd06051a6003c56e2474596885e1a1
Maxim Babenko <babenko@ytsaurus.tech>
2013-03-26 13:12:22 +0400

    Don't call ScheduleNext in auto mode

--- yt/server/chunk_server/chunk_manager.cpp
+++ yt/server/chunk_server/chunk_manager.cpp
@@ -1808,7 +1808,6 @@ private:
             Profiler.Enqueue("/refresh_list_size", ChunkReplicator->GetRefreshListSize());
             Profiler.Enqueue("/rf_update_list_size", ChunkReplicator->GetRFUpdateListSize());
         }
-        ProfilingInvoker->ScheduleNext();
     }
 
 
