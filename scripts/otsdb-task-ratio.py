9e6f222148f71eaae29904ace2a897a92c8517ab
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2013-04-12 19:16:24 +0400

    Refactoring

--- scripts/otsdb-task-ratio.py
+++ scripts/otsdb-task-ratio.py
@@ -6,39 +6,55 @@ import time
 import urllib2
 import os
 import urllib
+import traceback
 
-def get_timestampstr(timestamp):
-    return time.strftime("%Y/%m/%d-%H:%M:%S", time.localtime(timestamp))
+CONFIG = {'window' : 5 * 60,
+          'time_format' : '%Y/%m/%d-%H:%M:%S',
+          'hosts' : ['w394.hdp.yandex.net:8444', 'w395.hdp.yandex.net:8444', 'w396.hdp.yandex.net:8444'],
+          'tag' : '{host=scheduler01-001g.yt.yandex.net}',
+          'delta' : 15 * 60,
+          'offset' : 5 * 60}
 
-def load_data(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max', timewindow = 5 * 60):
-    params = {'start' : get_timestampstr(timestamp - timewindow), 'end' : get_timestampstr(timestamp), 'm' : 'sum:' + metric + '{host=scheduler01-001g.yt.yandex.net}'}
-    otsdburl = "http://otsdb/q?" +  urllib.urlencode(params).replace("%3A", ":").replace("%2F", "/") + "&ascii"
-    return urllib2.urlopen(otsdburl, timeout=5).read()
+def get_timestamp_str(timestamp):
+    return time.strftime(CONFIG['time_format'], time.localtime(timestamp))
 
-def get_metric_value(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max', timewindow = 7200):
-    data = load_data(timestamp, metric, timewindow).split('\n')
-    (prev_time, prev_value) = (0, 0);
+def load_data(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max'):
+    params = {'start' : get_timestamp_str(timestamp - CONFIG['window']),
+              'end' : get_timestamp_str(timestamp),
+              'm' : 'sum:' + metric + CONFIG['tag']}
+    for host in CONFIG['hosts']:
+        try:
+            otsdburl = "http://" + host + "/q?" +  urllib.urlencode(params).replace("%3A", ":").replace("%2F", "/") + "&ascii"
+            return urllib2.urlopen(otsdburl, timeout = 30).read()
+        except Exception, e:
+            continue
+            #print >>sys.stderr, 'Failed to collect data from ' + host + '\n' + traceback.format_exc()
+
+def get_metric_value(timestamp, metric):
+    data = load_data(timestamp, metric).split('\n')
+    prev_time, prev_value = 0, 0;
     for line in data[0:-1]:
-        (currtime, value) = line.split(' ')[1:3]
-        (currtime, value) = (float(currtime), float(value))
+        currtime, value = line.split(' ')[1:3]
+        currtime, value = float(currtime), float(value)
         if currtime > timestamp:
-            return prev_value #(timestamp - prev_time) / (currtime - prev_value) * (value - prev_value) + prev_value
+            return prev_value
         else:
-            (prev_time, prev_value) = (currtime, value)
+            prev_time, prev_value = currtime, value
     return prev_value
 
-def get_metric_diff(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max', delta = 60 * 15, timewindow = 7200):
-    return get_metric_value(timestamp, metric, timewindow) - get_metric_value(timestamp - delta, metric, timewindow)
+def get_metric_diff(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max'):
+    prev_value = get_metric_value(timestamp - CONFIG['delta'], metric)
+    curr_value = get_metric_value(timestamp, metric)
+    return curr_value - prev_value
 
 
-def print_ratio(timewindow = 7200):
+def print_ratio(curr_time):
     aborted_metric = 'yt.scheduler.total_aborted_job_time.max'
     failed_metric = 'yt.scheduler.total_failed_job_time.max'
     completed_metric = 'yt.scheduler.total_completed_job_time.max'
-    curr_time = time.time() - 5 * 60
-    completed_dtime = get_metric_diff(curr_time, completed_metric, timewindow)
-    aborted_dtime = get_metric_diff(curr_time, aborted_metric, timewindow)
-    failed_dtime = get_metric_diff(curr_time, failed_metric, timewindow)
+    completed_dtime = get_metric_diff(curr_time, completed_metric)
+    aborted_dtime = get_metric_diff(curr_time, aborted_metric)
+    failed_dtime = get_metric_diff(curr_time, failed_metric)
     full_dtime = completed_dtime + aborted_dtime + failed_dtime
     completed_ratio = completed_dtime / full_dtime
     aborted_ratio = aborted_dtime / full_dtime
@@ -47,12 +63,12 @@ def print_ratio(timewindow = 7200):
     print ("yt.scheduler.aborted_ratio %d %f" % ( int(curr_time), aborted_ratio))
     print ("yt.scheduler.failed_ratio %d %f" % ( int(curr_time), failed_ratio))
 
-
 def main():
     """ifstat main loop"""
-    interval = 15
+    interval = CONFIG['delta']
     while True:
-        print_ratio()
+        curr_time = time.time() - CONFIG['offset']
+        print_ratio(curr_time)
         sys.stdout.flush()
         time.sleep(interval)
 
