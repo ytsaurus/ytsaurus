9aae1765c5ea47f05f80bcdab89db787ddd6eae7
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2013-04-14 20:10:05 +0400

    Cosmetics

--- scripts/otsdb-task-ratio.py
+++ scripts/otsdb-task-ratio.py
@@ -9,68 +9,85 @@ import urllib
 import traceback
 
 CONFIG = {'window' : 5 * 60,
+          'interval' : 15 * 60,
           'time_format' : '%Y/%m/%d-%H:%M:%S',
-          'hosts' : ['w394.hdp.yandex.net:8444', 'w395.hdp.yandex.net:8444', 'w396.hdp.yandex.net:8444'],
-          'tag' : '{host=scheduler01-001g.yt.yandex.net}',
-          'delta' : 15 * 60,
-          'offset' : 5 * 60}
+          'hosts' : [
+              'w394.hdp.yandex.net:8444',
+              'w395.hdp.yandex.net:8444',
+              'w396.hdp.yandex.net:8444'
+          ],
+          'tags' : '{host=scheduler01-001g.yt.yandex.net}'}
+
 
 def get_timestamp_str(timestamp):
     return time.strftime(CONFIG['time_format'], time.localtime(timestamp))
 
-def load_data(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max'):
+
+def load_data(timestamp, metric):
     params = {'start' : get_timestamp_str(timestamp - CONFIG['window']),
               'end' : get_timestamp_str(timestamp),
-              'm' : 'sum:' + metric + CONFIG['tag']}
+              'm' : 'sum:' + metric + CONFIG['tags']}
     for host in CONFIG['hosts']:
         try:
-            otsdburl = "http://" + host + "/q?" +  urllib.urlencode(params).replace("%3A", ":").replace("%2F", "/") + "&ascii"
-            return urllib2.urlopen(otsdburl, timeout = 30).read()
-        except Exception, e:
-            continue
-            #print >>sys.stderr, 'Failed to collect data from ' + host + '\n' + traceback.format_exc()
+            otsdb_url = 'http://' + host + '/q?'
+            otsdb_url += urllib.urlencode(params).replace('%3A', ':').replace('%2F', '/')
+            otsdb_url += '&ascii'
+            return urllib2.urlopen(otsdb_url, timeout = 30).read()
+        except:
+            print >>sys.stderr, 'Failed to collect data from ' + host + '\n' + \
+                                traceback.format_exc()
+    raise RuntimeError('Failed to collect data from all OTSDB hosts')
+
 
 def get_metric_value(timestamp, metric):
     data = load_data(timestamp, metric).split('\n')
-    prev_time, prev_value = 0, 0;
+    prev_time, prev_value = 0.0, 0.0
+    curr_time, curr_value = 0.0, 0.0
     for line in data[0:-1]:
-        currtime, value = line.split(' ')[1:3]
-        currtime, value = float(currtime), float(value)
-        if currtime > timestamp:
+        curr_time, curr_value = line.split(' ')[1:3]
+        curr_time, curr_value = float(curr_time), float(curr_value)
+        if curr_time > timestamp:
             return prev_value
         else:
             prev_time, prev_value = currtime, value
     return prev_value
 
-def get_metric_diff(timestamp, metric = 'yt.scheduler.total_aborted_job_time.max'):
-    prev_value = get_metric_value(timestamp - CONFIG['delta'], metric)
+
+def get_metric_diff(timestamp, metric):
+    prev_value = get_metric_value(timestamp - CONFIG['interval'], metric)
     curr_value = get_metric_value(timestamp, metric)
     return curr_value - prev_value
 
 
 def print_ratio(curr_time):
+    completed_metric = 'yt.scheduler.total_completed_job_time.max'
     aborted_metric = 'yt.scheduler.total_aborted_job_time.max'
     failed_metric = 'yt.scheduler.total_failed_job_time.max'
-    completed_metric = 'yt.scheduler.total_completed_job_time.max'
+
     completed_dtime = get_metric_diff(curr_time, completed_metric)
     aborted_dtime = get_metric_diff(curr_time, aborted_metric)
     failed_dtime = get_metric_diff(curr_time, failed_metric)
     full_dtime = completed_dtime + aborted_dtime + failed_dtime
+
     completed_ratio = completed_dtime / full_dtime
     aborted_ratio = aborted_dtime / full_dtime
     failed_ratio = failed_dtime / full_dtime
-    print ("yt.scheduler.completed_ratio %d %f" % ( int(curr_time), completed_ratio))
-    print ("yt.scheduler.aborted_ratio %d %f" % ( int(curr_time), aborted_ratio))
-    print ("yt.scheduler.failed_ratio %d %f" % ( int(curr_time), failed_ratio))
+
+    print "yt.scheduler.completed_ratio %d %f" % (int(curr_time), completed_ratio)
+    print "yt.scheduler.aborted_ratio %d %f" % (int(curr_time), aborted_ratio)
+    print "yt.scheduler.failed_ratio %d %f" % (int(curr_time), failed_ratio)
+
 
 def main():
-    """ifstat main loop"""
-    interval = CONFIG['delta']
     while True:
-        curr_time = time.time() - CONFIG['offset']
-        print_ratio(curr_time)
-        sys.stdout.flush()
-        time.sleep(interval)
+        try:
+            print_ratio(time.time())
+        except:
+            print >>sys.stderr, 'Failed to compute time ratios\n' + \
+                                 traceback.format_exc()
+        finally:
+            sys.stdout.flush()
+            time.sleep(CONFIG['interval'])
 
 if __name__ == "__main__":
     main()
