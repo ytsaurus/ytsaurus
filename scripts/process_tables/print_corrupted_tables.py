c34996b5b223e9295385e8c0ee43d0dec8d800f1
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-04-23 02:39:32 +0400

    Fake changes to apply previous commit in bare repository

--- scripts/process_tables/print_corrupted_tables.py
+++ scripts/process_tables/print_corrupted_tables.py
@@ -2,16 +2,17 @@
 
 import yt.wrapper as yt
 
-a = yt.list('//sys/lost_vital_chunks', max_size=10000)
+if __name__ == "__main__":
+    a = yt.list('//sys/lost_vital_chunks', max_size=10000)
 
-owning = set()
-for ch in a:
-    for table in yt.get('//sys/chunks/%s/@owning_nodes' % ch):
-        owning.add(table)
+    owning = set()
+    for ch in a:
+        for table in yt.get('//sys/chunks/%s/@owning_nodes' % ch):
+            owning.add(table)
 
-owning = list(owning)
-owning.sort()
+    owning = list(owning)
+    owning.sort()
 
-for table in owning:
-    print table
+    for table in owning:
+        print table
 
