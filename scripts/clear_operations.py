a449e2d81979fec2998a35a5baa9c2664a26adb0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-04-25 15:50:12 +0400

    Move clear operations script

--- scripts/clear_operations.py
+++ scripts/clear_operations.py
@@ -7,34 +7,46 @@ from dateutil.parser import parse
 from collections import namedtuple
 from datetime import datetime, timedelta
 
-Oper = namedtuple("Oper", ["time", "id", "state"]);
+Oper = namedtuple("Oper", ["time", "id", "state", "spec"]);
 
 def clean_operations(count, failed_timeout):
-    '''Clean all operations started no more than #days days ago,
-leaving no more than #count most recent operations.'''
+    """Clean all operations started no more than #days days ago,
+       leaving no more than #count most recent operations."""
 
-    operations = yt.get("//sys/operations", attributes=['state', 'start_time'])
-    operations = [Oper(parse(v[u"$attributes"]["start_time"]).replace(tzinfo=None), k, \
-        v[u"$attributes"]["state"]) for k, v in operations.iteritems()];
+    operations = yt.get("//sys/operations", attributes=['state', 'start_time', 'spec'])
+    operations = [Oper(parse(v.attributes["start_time"]).replace(tzinfo=None), k, \
+        v.attributes["state"], v.attributes['spec']) for k, v in operations.iteritems()];
     operations.sort(reverse=True)
     
     running = 0
     saved = 0
     to_remove = []
 
-    for x in operations:
-        time_since = datetime.utcnow() - x.time 
-        if x.state == 'failed' and time_since > failed_timeout:
-            to_remove.append(x.id)
-        elif x.state not in ['completed', 'aborted']:
-            running += 1
-        elif (saved + running >= count):
-            to_remove.append(x.id)
-        else:
-            saved += 1
+    def is_final(state):
+        return state in ["completed", "aborted", "failed"]
 
+    for op in operations:
+        is_casual = (op.state in ["completed", "aborted"]) and (len(yt.get("//sys/operations/%s/jobs" % op.id)) == 0)
+
+        time_since = datetime.utcnow() - op.time 
+        is_old = (time_since > failed_timeout)
+
+        is_regular = (op.spec.get("system_user", "unknown") in ["crawler", "cron", "odin"])
+
+        if is_casual:
+            if is_regular or saved >= count:
+		to_remove.append(op.id)
+	    else:
+		saved += 1
+        
+	if is_final(op.state) and is_old:
+	    to_remove.append(op.id)
+   
     for x in to_remove:
-        os.system('''yt remove '//sys/operations/%s' ''' % x)
+        if not is_final(yt.get("//sys/operations/%s/@state" % x)):
+            print >>sys.stderr, "YOU TRYING TO REMOVE OPERATION THAT NOT IN FINAL STATE"
+            sys.exit(1)
+        yt.remove("//sys/operations/%s" % x, recursive=True)
     
 
 def main():
@@ -43,7 +55,7 @@ def main():
     parser = argparse.ArgumentParser(description='Clean operations from cypress.')
     parser.add_argument('--count', metavar='N', type=int, default=50,
                        help='leave history no more than N operations')
-    parser.add_argument('--failed_timeout', metavar='N', type=int, default=3,
+    parser.add_argument('--failed_timeout', metavar='N', type=int, default=2,
                        help='remove all failed operation older than N days')
 
     args = parser.parse_args()
