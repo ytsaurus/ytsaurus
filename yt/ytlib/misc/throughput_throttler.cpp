00f492fc87ccff89f4700dbf6b09b7b193ce013f
Maxim Babenko <babenko@ytsaurus.tech>
2013-05-19 18:37:32 +0400

    Throttler interface and implementations

---
+++ yt/ytlib/misc/throughput_throttler.cpp
@@ -0,0 +1,120 @@
+#include "stdafx.h"
+#include "throughput_throttler.h"
+#include "periodic_invoker.h"
+#include "singleton.h"
+#include "thread_affinity.h"
+
+#include <ytlib/actions/invoker_util.h>
+
+#include <queue>
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+static TFuture<void> PresetResult = MakePromise();
+
+class TThroughputThrottler
+    : public IThroughputThrottler
+{
+public:
+    explicit TThroughputThrottler(TThroughputThrottlerConfigPtr config)
+        : ThroughputPerPeriod(static_cast<i64>(config->Period.SecondsFloat() * (*config->Limit)))
+        , Available(ThroughputPerPeriod)
+    {
+        Invoker = New<TPeriodicInvoker>(
+            GetSyncInvoker(),
+            BIND(&TThroughputThrottler::OnTick, MakeWeak(this)),
+            config->Period);
+        Invoker->Start();
+    }
+
+    virtual TFuture<void> Throttle(i64 count) override
+    {
+        VERIFY_THREAD_AFFINITY_ANY();
+        YCHECK(count >= 0);
+
+        if (count == 0) {
+            return PresetResult;
+        }
+
+        TGuard<TSpinLock> guard(SpinLock);
+
+        if (Available > 0) {
+            // Execute immediately.
+            Available -= count;
+            return PresetResult;
+        }
+
+        // Enqueue request to be executed later.
+        TRequest request;
+        request.Count = count;
+        request.Promise = MakePromise();
+        Requests.push(request);
+        return request.Promise;
+    }
+
+private:
+    struct TRequest
+    {
+        i64 Count;
+        TPromise<void> Promise;
+    };
+
+    i64 ThroughputPerPeriod;
+    TPeriodicInvokerPtr Invoker;
+
+    //! Protects the section immediately following it.
+    TSpinLock SpinLock;
+    i64 Available;
+    std::queue<TRequest> Requests;
+
+    void OnTick()
+    {
+        std::vector<TPromise<void>> releaseList;
+
+        {
+            TGuard<TSpinLock> guard(SpinLock);
+            Available += ThroughputPerPeriod;
+            while (!Requests.empty() && Available > 0) {
+                auto& request = Requests.front();
+                Available -= request.Count;
+                releaseList.push_back(std::move(request.Promise));
+                Requests.pop();
+            }
+        }
+
+        FOREACH (auto promise, releaseList) {
+            promise.Set();
+        }
+    }
+};
+
+IThroughputThrottlerPtr CreateThrottler(TThroughputThrottlerConfigPtr config)
+{
+    return config->Limit ? New<TThroughputThrottler>(config) : GetUnlimitedThrottler();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TUnlimitedThroughtputThrottler
+    : public IThroughputThrottler
+{
+public:
+    virtual TFuture<void> Throttle(i64 count) override
+    {
+        VERIFY_THREAD_AFFINITY_ANY();
+        YCHECK(count >= 0);
+
+        return PresetResult;
+    }
+};
+
+IThroughputThrottlerPtr GetUnlimitedThrottler()
+{
+    return RefCountedSingleton<TUnlimitedThroughtputThrottler>();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
---
+++ yt/ytlib/misc/throughput_throttler.h
@@ -0,0 +1,63 @@
+#pragma once
+
+#include "common.h"
+
+#include <ytlib/actions/future.h>
+
+#include <ytlib/ytree/yson_serializable.h>
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+   
+// TODO(babenko): move to public.h
+class TThroughputThrottlerConfig;
+typedef TIntrusivePtr<TThroughputThrottlerConfig> TThroughputThrottlerConfigPtr;
+
+class IThroughputThrottler;
+typedef TIntrusivePtr<IThroughputThrottler> IThroughputThrottlerPtr;
+
+class TThroughputThrottlerConfig
+    : public TYsonSerializable
+{
+public:
+    TThroughputThrottlerConfig()
+    {
+        RegisterParameter("period", Period)
+            .Default(TDuration::Seconds(5));
+        RegisterParameter("limit", Limit)
+            .Default(Null)
+            .GreaterThanOrEqual(0);
+    }
+
+    //! Gives a period for which the bandwidth limit applies.
+    TDuration Period;
+
+    //! Gives a limit on average throughput (per sec). Null means unlimited.
+    TNullable<i64> Limit;
+};
+
+//! Enables async operation to throttle based on throughput limit.
+/*!
+ *  This interface and its implementations are vastly inspired by |DataTransferThrottler| class from Hadoop
+ *  but return promise instead of using direct sleep calls.
+ */
+struct IThroughputThrottler
+    : public virtual TRefCounted
+{
+    //! Assuming that we are about to transfer #count bytes,
+    //! returns a future that is set when enough time has passed
+    //! to ensure proper bandwidth utilization.
+    /*!
+     *  \note Thread affinity: any
+     */
+    virtual TFuture<void> Throttle(i64 count) = 0;
+};
+
+IThroughputThrottlerPtr CreateThrottler(TThroughputThrottlerConfigPtr config);
+IThroughputThrottlerPtr GetUnlimitedThrottler();
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
+
