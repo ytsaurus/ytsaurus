f49dad485105f53bfc7f34ff9798fba45a145987
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-05-30 00:44:56 +0400

    Extract http config

--- python-git/python/yt/wrapper/config.py
+++ python-git/python/yt/wrapper/config.py
@@ -1,21 +1,5 @@
 from common import update_from_env
 
-import os
-
-PROXY = ""
-USE_TOKEN = True
-TOKEN = None
-
-ACCEPT_ENCODING = os.environ.get("ACCEPT_ENCODING", "identity, gzip")
-
-CONNECTION_TIMEOUT = 120.0
-
-HTTP_RETRIES_COUNT = 5
-HTTP_RETRY_TIMEOUT = 10
-
-# COMPAT(ignat): remove option when version 14 become stable
-RETRY_VOLATILE_COMMANDS = False
-
 USE_HOSTS = True
 
 # Turn off gzip encoding if you want to speed up reading and writing tables
@@ -102,3 +86,6 @@ def set_mapreduce_mode():
     DEFAULT_FORMAT = YamrFormat(has_subkey=True, lenval=False)
 
 update_from_env(globals())
+
+from http_config import *
+
