9a4ceb3c5787cdd847bb6eb5b2cc28c86797e46b
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-05-30 00:44:56 +0400

    Extract http config

---
+++ python/yt/wrapper/http_config.py
@@ -0,0 +1,19 @@
+from common import update_from_env
+
+PROXY = ""
+
+TOKEN = None
+USE_TOKEN = True
+
+ACCEPT_ENCODING = "identity, gzip"
+
+CONNECTION_TIMEOUT = 120.0
+
+HTTP_RETRIES_COUNT = 5
+HTTP_RETRY_TIMEOUT = 10
+
+# COMPAT(ignat): remove option when version 14 become stable
+RETRY_VOLATILE_COMMANDS = False
+
+update_from_env(globals())
+
