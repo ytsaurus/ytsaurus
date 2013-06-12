2d443b4018948e5df934072b628018e3e3700086
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2013-06-12 12:55:37 +0400

    Add forgotten file

---
+++ yt/ytlib/ytree/convert.cpp
@@ -0,0 +1,15 @@
+#include "convert.h"
+
+namespace NYT {
+namespace NYTree {
+
+////////////////////////////////////////////////////////////////////////////////
+
+template TYsonString ConvertToYsonString<int>(const int&);
+template TYsonString ConvertToYsonString<TRawString>(const TRawString&);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYTree
+} // namespace NYT
+
