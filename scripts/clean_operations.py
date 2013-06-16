899c6b505947cec6665f45b53e679b69bcf9d1a3
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-06-17 00:51:35 +0400

    Logging and fixes in clear_operations.py

--- scripts/clean_operations.py
+++ scripts/clean_operations.py
@@ -1,13 +1,22 @@
 #!/usr/bin/python
-#coding: utf8
 
-import os
+import sys
 import yt.wrapper as yt
 from dateutil.parser import parse
 from collections import namedtuple
 from datetime import datetime, timedelta
 
+import argparse
+import logging
+
 Oper = namedtuple("Oper", ["time", "id", "state", "spec"]);
+
+logger = logging.getLogger("Cron")
+logger.setLevel(level="INFO")
+
+formatter = logging.Formatter('%(asctime)-15s: %(message)s')
+logger.addHandler(logging.StreamHandler())
+logger.handlers[0].setFormatter(formatter)
 
 def clean_operations(count, failed_timeout):
     """Clean all operations started no more than #days days ago,
@@ -18,7 +27,6 @@ def clean_operations(count, failed_timeout):
         v.attributes["state"], v.attributes['spec']) for k, v in operations.iteritems()];
     operations.sort(reverse=True)
     
-    running = 0
     saved = 0
     to_remove = []
 
@@ -35,22 +43,24 @@ def clean_operations(count, failed_timeout):
 
         if is_casual:
             if is_regular or saved >= count:
-		to_remove.append(op.id)
-	    else:
-		saved += 1
+                to_remove.append(op.id)
+            else:
+                saved += 1
         
-	if is_final(op.state) and is_old:
-	    to_remove.append(op.id)
+        if is_final(op.state) and is_old:
+            to_remove.append(op.id)
    
-    for x in to_remove:
-        if not is_final(yt.get("//sys/operations/%s/@state" % x)):
-            print >>sys.stderr, "YOU TRYING TO REMOVE OPERATION THAT NOT IN FINAL STATE"
+    for op in to_remove:
+        if not yt.exists("//sys/operations/%s" % op):
+            continue
+        if not is_final(yt.get("//sys/operations/%s/@state" % op)):
+            logger.error("Trying to remove operation (%s) that is not in final state", op)
             sys.exit(1)
-        yt.remove("//sys/operations/%s" % x, recursive=True)
+        logger.info("Removing operation %s" % op)
+        yt.remove("//sys/operations/%s" % op, recursive=True)
     
 
 def main():
-    import argparse
 
     parser = argparse.ArgumentParser(description='Clean operations from cypress.')
     parser.add_argument('--count', metavar='N', type=int, default=50,
@@ -62,4 +72,4 @@ def main():
     clean_operations(args.count, timedelta(days=args.failed_timeout))
 
 if __name__ == "__main__":
-	main()
+    main()
