7774beb145700a19e337d170af547d93a30fe62c
Maxim Babenko <babenko@ytsaurus.tech>
2013-06-26 21:27:25 +0400

    Cosmetics

--- yt/server/cypress_server/node_proxy_detail.cpp
+++ yt/server/cypress_server/node_proxy_detail.cpp
@@ -1313,7 +1313,7 @@ IYPathServicePtr TLinkNodeProxy::GetTargetService() const
     const auto& targetId = impl->GetTargetId();
     auto* target = objectManager->FindObject(targetId);
     if (!IsObjectAlive(target)) {
-        THROW_ERROR_EXCEPTION("Link target does not exist: %s", ~ToString(targetId));
+        THROW_ERROR_EXCEPTION("Link target %s does not exist", ~ToString(targetId));
     }
     return objectManager->GetProxy(target, Transaction);
 }
