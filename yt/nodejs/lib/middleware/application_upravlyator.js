313e4b85e180af4084e410b10d14eb9366b8100a
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2013-07-23 21:31:02 +0400

    Create users lazily and fetch tokens from Cypress

--- yt/nodejs/lib/middleware/application_upravlyator.js
+++ yt/nodejs/lib/middleware/application_upravlyator.js
@@ -9,14 +9,10 @@ exports.that = function Middleware__YtApplicationUpravlyator()
 {
     "use strict";
 
-    var config = YtRegistry.get("config", "upravlyator");
     var logger = YtRegistry.get("logger");
     var driver = YtRegistry.get("driver");
 
-    var app = new YtApplicationUpravlyator(
-        config,
-        logger,
-        driver);
+    var app = new YtApplicationUpravlyator(logger, driver);
 
     return function(req, rsp, next) {
         return Q(app.dispatch(req, rsp, next)).done();
