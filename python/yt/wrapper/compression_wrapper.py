65535c8dd014080d1407d4c2e0c52c74c306a4fa
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-08-09 20:27:13 +0400

    Fix applying zlib to string

--- python/yt/wrapper/compression_wrapper.py
+++ python/yt/wrapper/compression_wrapper.py
@@ -1,13 +1,14 @@
 import zlib
 
-import sys
-
-def create_zlib_generator(stream):
+def create_zlib_generator(obj):
     zlib_obj = zlib.compressobj()
-    for chunk in stream:
-        c = zlib_obj.compress(chunk)
+    if isinstance(obj, str):
+        yield zlib_obj.compress(obj)
+    else:
+        for chunk in obj:
+            c = zlib_obj.compress(chunk)
+            if c:
+                yield c
+        c = zlib_obj.flush()
         if c:
             yield c
-    c = zlib_obj.flush()
-    if c:
-        yield c
