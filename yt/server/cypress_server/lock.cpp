6bddb232c8bcf1c5c042671952d1de40ccdc9176
Maxim Babenko <babenko@ytsaurus.tech>
2013-08-27 13:47:13 +0400

    Allow null trunk node in locks (cont)

--- yt/server/cypress_server/lock.cpp
+++ yt/server/cypress_server/lock.cpp
@@ -86,7 +86,7 @@ void TLock::Save(NCellMaster::TSaveContext& context) const
     Save(context, State_);
     Save(context, Request_);
     // TODO(babenko): refactor when new serialization API is ready
-    Save(context, GetObjectId(TrunkNode_));
+    Save(context, TrunkNode_ ? TrunkNode_->GetId() : NObjectServer::NullObjectId);
     SaveObjectRef(context, Transaction_);
 }
 
