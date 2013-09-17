3b5a5e9603e420e2cd8ddff7803752a1e7130a3a
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-09-18 02:11:22 +0400

    Save last user operation

--- scripts/cron/clear_operations.py
+++ scripts/cron/clear_operations.py
@@ -21,12 +21,14 @@ logger.handlers[0].setFormatter(formatter)
 def clean_operations(count, total_count, failed_timeout, robots, log):
     """Clean all operations started no more than #days days ago,
        leaving no more than #count most recent operations."""
+    if robots is None:
+        robots = []
 
     operations = yt.get("//sys/operations", attributes=['state', 'start_time', 'spec'])
     operations = [Oper(parse(v.attributes["start_time"]).replace(tzinfo=None), k, \
         v.attributes["state"], v.attributes['spec']) for k, v in operations.iteritems()];
     operations.sort(reverse=True)
-    
+
     saved = 0
     to_remove = []
 
@@ -35,25 +37,30 @@ def clean_operations(count, total_count, failed_timeout, robots, log):
 
     operations.sort(key=lambda op: op.time, reverse=True)
 
+    users = set()
     for op in operations:
         if not is_final(op.state):
             continue
-        
-        is_casual = (op.state in ["completed", "aborted"]) and (len(yt.get("//sys/operations/%s/jobs" % op.id)) == 0)
 
-        time_since = datetime.utcnow() - op.time 
+        user = op.spec.get("authenticated_user", "unknown")
+
+        is_casual = (op.state in ["completed", "aborted"]) and (len(yt.get("//sys/operations/%s/jobs" % op.id)) == 0) and (user in users)
+
+        time_since = datetime.utcnow() - op.time
         is_old = (time_since > failed_timeout)
 
-        is_regular = (op.spec.get("authenticated_user", "unknown") in robots)
+        is_regular = (user in robots)
 
         if is_regular or (is_casual and saved >= count) or is_old or (saved >= total_count):
             to_remove.append(op.id)
         else:
             saved += 1
 
+        users.add(user)
+
     if log is not None:
         log_output = open(log, "a")
-   
+
     for op in to_remove:
         if not yt.exists("//sys/operations/%s" % op):
             continue
@@ -68,11 +75,11 @@ def clean_operations(count, total_count, failed_timeout, robots, log):
 
     if log is not None:
         log_output.close()
-    
+
 
 def main():
     parser = argparse.ArgumentParser(description='Clean operations from cypress.')
-    parser.add_argument('--count', metavar='N', type=int, default=50,
+    parser.add_argument('--count', metavar='N', type=int, default=100,
                        help='leave no more than N completed (without stderr) or aborted operations')
     parser.add_argument('--total-count', metavar='N', type=int, default=2000,
                        help='leave no more that N operations totally')
