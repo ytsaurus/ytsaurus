5013629b0ef2d87ee17d147519e05d88982fb71b
Maxim Babenko <babenko@ytsaurus.tech>
2013-09-24 20:53:25 +0400

    Fix test

--- tests/integration/tests/test_cypress_commands.py
+++ tests/integration/tests/test_cypress_commands.py
@@ -497,7 +497,6 @@ class TestCypressCommands(YTEnvSetup):
         set("//tmp/t1", 1, tx=tx)
         link("//tmp/t1", "//tmp/l1", tx=tx)
         assert get("//tmp/l1", tx=tx) == 1
-        abort_tx(tx)
 
     def test_access_stat1(self):
         c1 = get('//tmp/@access_counter')
