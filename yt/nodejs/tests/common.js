afe39182c2192d7244e078b75815ff64d9105277
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2013-09-26 20:25:55 +0400

    Cosmetics in debug output

--- yt/nodejs/tests/common.js
+++ yt/nodejs/tests/common.js
@@ -7,11 +7,12 @@ global.expect = chai.expect;
 if (process.env.NODE_DEBUG && /YTTEST/.test(process.env.NODE_DEBUG)) {
     global.__DBG = function(x) {
         "use strict";
-        console.error("\nYT Tests:", x);
+        process.stderr.write("__DBG : ");
+        console.error(x);
     };
     global.__LOG = function() {
         "use strict";
-        console.error("\nYT Logger:");
+        process.stderr.write("__LOG : ");
         console.error.apply(null, arguments);
     };
 } else {
