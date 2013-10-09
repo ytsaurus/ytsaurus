a3f8227dadc708cf3ba3d4ab247e31eca5511c89
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-10-09 18:30:03 +0400

    Kill process if pinging thread failed

--- python/yt/tools/binaries/lock.py
+++ python/yt/tools/binaries/lock.py
@@ -3,7 +3,9 @@
 import yt.logger as logger
 import yt.wrapper as yt
 
+import os
 import sys
+import time
 import argparse
 import subprocess
 
@@ -11,9 +13,10 @@ def main():
     parser = argparse.ArgumentParser(description='Run command under lock')
     parser.add_argument('path')
     parser.add_argument('command')
+    parser.add_argument('--step', type=float, default=1.0)
     args = parser.parse_args()
 
-    with yt.Transaction():
+    with yt.PingableTransaction() as tx:
         try:
             yt.lock(args.path)
         except yt.YtResponseError as error:
@@ -22,7 +25,21 @@ def main():
                 return
             raise
 
-        subprocess.call(args.command, stdout=sys.stdout, stderr=sys.stderr, shell=True)
+        logger.info("Running command")
+        proc = subprocess.Popen(args.command, stdout=sys.stdout, stderr=sys.stderr, shell=True, env=os.environ.copy())
+
+        while True:
+            if not tx.ping.is_alive():
+                logger.error("Pinging thread failed. Terminating command.")
+                proc.signal(2)
+                time.sleep(args.step)
+                proc.terminate()
+                break
+
+            if proc.poll() is not None:
+                break
+
+            time.sleep(args.step)
 
 if __name__ == "__main__":
     main()
