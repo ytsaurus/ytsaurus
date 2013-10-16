6d9f4de655a2919e3016ebc229bdd4de08c900e4
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-10-16 21:06:48 +0400

    Cosmetics

--- scripts/cron/mail_if_failed.sh
+++ scripts/cron/mail_if_failed.sh
@@ -7,5 +7,5 @@ sh -c "$COMMAND"
 EXIT_CODE=$?
 
 if [ "$EXIT_CODE" != "0" ]; then
-    echo -e "'$@' failed with code ${EXIT_CODE}.\n\nignat@yandex-team.ru" | mail -s "Regular process failed" $EMAIL
+    echo -e "'$COMMAND' failed with code ${EXIT_CODE}.\n\nignat@yandex-team.ru" | mail -s "Regular process failed" $EMAIL
 fi
