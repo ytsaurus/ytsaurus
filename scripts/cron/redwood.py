4aaad2a7181ca228f49d436ec9ca66e70d3efe18
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-10-18 18:58:06 +0400

    Create symlink to YYYY-MM-DD for user_sessions

--- scripts/cron/redwood.py
+++ scripts/cron/redwood.py
@@ -6,7 +6,7 @@ import os
 import argparse
 from datetime import date, timedelta
 
-def process_logs(import_set, remove_set, log_path, log_name, subnames, days):
+def process_logs(import_list, remove_list, link_queue, log_path, log_name, subnames, days, make_link):
     if subnames is None:
         subnames = [None]
 
@@ -19,32 +19,39 @@ def process_logs(import_set, remove_set, log_path, log_name, subnames, days):
         for subname in subnames:
             table_date = date.today() - timedelta(days=i)
             table_name = os.path.join(log_name, table_date.strftime("%Y%m%d"))
+            link_table_name = os.path.join(log_name, table_date.strftime("%Y-%m-%d"))
             if subname is not None:
                 table_name = os.path.join(table_name, subname)
             table_path = os.path.join(log_path, table_name)
             if days is not None and i >= days: # Remove case
-                if table_name in remove_set:
+                if table_name in remove_list:
                     continue
                 elif yt.exists(table_path):
-                    remove_set.add(table_path)
+                    if make_link:
+                        remove_list.append(link_table_name)
+                    remove_list.append(table_path)
             else: # Import case
-                if table_name in import_set:
+                if table_name in import_list:
                     continue
                 elif not yt.exists(table_path):
-                    import_set.add(table_name)
+                    import_list.append(table_name)
+                    if make_link:
+                        link_queue.append({"src": table_name, "dst": link_table_name})
 
 def main():
     parser = argparse.ArgumentParser(description='Prepare tables to merge')
     parser.add_argument('--path', required=True)
     parser.add_argument('--import-queue', required=True)
     parser.add_argument('--remove-queue', required=True)
+    parser.add_argument('--link-queue', required=True)
     args = parser.parse_args()
     
-    tables_to_import = set(yt.get(args.import_queue))
-    tables_to_remove = set(yt.get(args.remove_queue))
-    process_logs(tables_to_import, tables_to_remove, args.path, "user_sessions",      None,           60)
-    process_logs(tables_to_import, tables_to_remove, args.path, "user_intents",       None,           None)
-    process_logs(tables_to_import, tables_to_remove, args.path, "reqregscdata",       ["www", "xml"], None)
+    tables_to_import = yt.get(args.import_queue)
+    tables_to_remove = yt.get(args.remove_queue)
+    link_queue = yt.get(args.link_queue)
+    process_logs(tables_to_import, tables_to_remove, link_queue, args.path, "user_sessions",      None,           60,   True)
+    process_logs(tables_to_import, tables_to_remove, link_queue, args.path, "user_intents",       None,           None, False)
+    process_logs(tables_to_import, tables_to_remove, link_queue, args.path, "reqregscdata",       ["www", "xml"], None, False)
 
     for i in xrange(9, 16):
         date_str = date(2013, 9, i).strftime("%Y%m%d")
@@ -54,6 +61,7 @@ def main():
 
     yt.set(args.import_queue, list(tables_to_import))
     yt.set(args.remove_queue, list(tables_to_remove))
+    yt.set(args.link_queue, link_queue)
 
 if __name__ == "__main__":
     main()
--- scripts/cron/redwood.sh
+++ scripts/cron/redwood.sh
@@ -5,9 +5,10 @@ export YT_PROXY="kant.yt.yandex.net"
 IMPORT_PATH="//userdata"
 IMPORT_QUEUE="//sys/cron/tables_to_import_from_redwood"
 REMOVE_QUEUE="//sys/cron/tables_to_remove"
+LINK_QUEUE="//sys/cron/link_tasks"
 LOCK_PATH="//sys/cron/redwood_lock"
 
-/opt/cron/redwood.py --path $IMPORT_PATH --import-queue $IMPORT_QUEUE --remove-queue $REMOVE_QUEUE
+/opt/cron/redwood.py --path $IMPORT_PATH --import-queue $IMPORT_QUEUE --remove-queue $REMOVE_QUEUE --link-queue $LINK_QUEUE
 
 /opt/cron/tools/remove.py $REMOVE_QUEUE
 
@@ -19,3 +20,5 @@ import_from_mr.py \
     --compression-codec "gzip_best_compression" --erasure-codec "lrc_12_2_2" \
     --yt-pool "redwood_restricted" \
     --fastbone
+
+/opt/cron/tools/link.py $LINK_QUEUE
