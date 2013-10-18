195976b0a9eb7e4cbbe44da06fde1557dfa1c36e
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-10-18 18:56:54 +0400

    Add script to hide token in mail

--- scripts/cron/mail_if_failed.sh
+++ scripts/cron/mail_if_failed.sh
@@ -7,5 +7,5 @@ sh -c "$COMMAND"
 EXIT_CODE=$?
 
 if [ "$EXIT_CODE" != "0" ]; then
-    echo -e "'$COMMAND' failed with code ${EXIT_CODE}.\n\nignat@yandex-team.ru" | mail -s "Regular process failed" $EMAIL
+    echo -e "'$COMMAND' failed with code ${EXIT_CODE}.\n\nignat@yandex-team.ru" | /opt/cron/tools/hide_tokens.py | mail -s "Regular process failed" $EMAIL
 fi
