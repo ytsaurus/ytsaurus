cbf20df90a38031067ccd9e49405cf53d5c565b7
Maxim Babenko <babenko@ytsaurus.tech>
2013-11-03 14:29:26 +0400

    Still looking for good ports

--- tests/integration/yt_env_setup.py
+++ tests/integration/yt_env_setup.py
@@ -43,10 +43,10 @@ class YTEnvSetup(YTEnv):
 
         cls.path_to_test = path_to_test
         ports = {
-            "master": 29501,
-            "node": 28501,
-            "scheduler": 29501,
-            "proxy": 29580}
+            "master": 29000,
+            "node": 29100,
+            "scheduler": 29200,
+            "proxy": 29300}
         cls.Env = cls()
         cls.Env.set_environment(path_to_run, pids_filename, ports)
 
