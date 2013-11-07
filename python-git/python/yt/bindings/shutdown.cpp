8dc78b8386f491e4109df235742648aea66ca420
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2013-11-07 16:09:56 +0400

    The Shutdown Club

--- python-git/python/yt/bindings/shutdown.cpp
+++ python-git/python/yt/bindings/shutdown.cpp
@@ -1,5 +1,7 @@
 #include "shutdown.h"
 
+#include <core/misc/address.h>
+
 #include <core/profiling/profiling_manager.h>
 
 #include <core/rpc/dispatcher.h>
@@ -28,13 +30,21 @@ void RegisterShutdown()
 
 void Shutdown()
 {
-    // TODO: refactor system shutdown
-    // XXX(sandello): Keep in sync with server/main.cpp, driver/main.cpp and utmain.cpp, python_bindings/driver.cpp
+    // TODO(sandello): Refactor this.
+    // XXX(sandello): Keep in sync with...
+    //   server/main.cpp
+    //   driver/main.cpp
+    //   unittests/utmain.cpp
+    //   nodejs/src/common.cpp
+    //   ../python/yt/bindings/shutdown.cpp
+    // Feel free to add your cpp here. Welcome to the Shutdown Club!
+
     NBus::TTcpDispatcher::Get()->Shutdown();
     NRpc::TDispatcher::Get()->Shutdown();
     NChunkClient::TDispatcher::Get()->Shutdown();
     NProfiling::TProfilingManager::Get()->Shutdown();
     NConcurrency::TDelayedExecutor::Shutdown();
+    TAddressResolver::Get()->Shutdown();
     NLog::TLogManager::Get()->Shutdown();
 }
 
