3af519b79d90ced853782b8d46e0805690168771
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-11-08 14:37:28 +0400

    Revert "Delete changelog hacking test because it is too hacky"
    This reverts commit c77389161402c2d92450ca2024c79ab387f76e5f.

---
+++ tests/integration/tests/test_hack_changelog.py
@@ -0,0 +1,76 @@
+import pytest
+
+from yt_env_setup import YTEnvSetup
+from yt_commands import *
+
+import os
+import subprocess
+
+
+if not hasattr(subprocess, "check_output"):
+    def check_output(*args, **kwargs):
+        child = subprocess.Popen(stdout=subprocess.PIPE, *args, **kwargs)
+        out, _ = child.communicate()
+        result = child.poll()
+        if result:
+            cmd = kwargs.get("args")
+            if cmd is None:
+                cmd = args[0]
+            error = subprocess.CalledProcessError(result, cmd)
+            error.output = output
+            raise error
+        return out
+    subprocess.check_output = check_output
+
+
+class TestHackChangelog(YTEnvSetup):
+    NUM_MASTERS = 1
+    NUM_NODES = 0
+
+    def prepare_changelog_path(self):
+        # Search changelog
+        changelogs_path = self.Env._master_configs[0]["meta_state"]["changelogs"]["path"]
+        self.changelog = os.path.join(
+            changelogs_path,
+            filter(lambda f: not f.endswith("index"), os.listdir(changelogs_path))[0])
+
+    def stop_master(self):
+        # TODO(panin): make convenient way for this
+        # Stop master
+        self.kill_process(*self.Env.process_to_kill[0])
+        self.Env.process_to_kill.pop()
+
+    def get_record_count(self):
+        output = subprocess.check_output(
+            ["process_changelog",
+             "--action", "read",
+             "--filename", self.changelog])
+
+        for line in output.split("\n"):
+            if "Record count" in line:
+                return int(line[line.find("=") + 1:])
+
+    def truncate_changelog(self, truncate_record_number):
+        record_count = self.get_record_count()
+        subprocess.check_call(
+            ["process_changelog",
+             "--action", "remove",
+             "--filename", self.changelog,
+             "-f", str(truncate_record_number),
+             "-t", str(record_count - 1)],
+            stdin=subprocess.PIPE)
+
+    def test_all(self):
+        self.prepare_changelog_path()
+        record_count = self.get_record_count()
+
+        set("//home/@test_attribute", 123)
+        assert "test_attribute" in get("//home/@")
+
+        self.stop_master()
+        self.truncate_changelog(record_count)
+
+        self.Env._run_masters(prepare_files=False)
+        assert "test_attribute" not in get("//home/@")
+
+
