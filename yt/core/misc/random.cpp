c697f4d290c289385ca70077dba98b8699bb83c7
Maxim Babenko <babenko@ytsaurus.tech>
2013-11-12 23:25:03 +0400

    Cosmetics

--- yt/core/misc/random.cpp
+++ yt/core/misc/random.cpp
@@ -7,7 +7,7 @@ namespace NYT {
 
 ui64 TRandomGenerator::GenerateInteger()
 {
-    // Parametres are taken from http://en.wikipedia.org/wiki/Linear_congruential_generator
+    // Parameters are taken from http://en.wikipedia.org/wiki/Linear_congruential_generator
     // Taking modulo 2^64 is implemented via overflow in ui64
     X = 6364136223846793005ll * X + 1442695040888963407ll;
     return X;
