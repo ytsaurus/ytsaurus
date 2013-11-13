6a85cdb85009ec9f591c6d94a996bc26cc061dfd
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2013-11-13 17:25:50 +0400

    Use v120_CTP_Nov2012 in Visual Studio 2012

--- cmake/LocalConfiguration.cmake
+++ cmake/LocalConfiguration.cmake
@@ -19,11 +19,16 @@ endif()
 ################################################################################
 # Check available compiler version.
 
-if (MSVC)
+if (MSVC OR MSVC_IDE)
   message(STATUS "Looks like we are using msvc..." )
 
-  if (MSVC_VERSION LESS 1600)
-    message(FATAL_ERROR "msvc >= 10.0 is mandatory due to C++11 usage")
+  if( MSVC_VERSION LESS 1700 )       # VC10-/VS2010- 
+    message(FATAL_ERROR "The project requires C++11 features. You need at least Visual Studio 11 (Microsoft Visual Studio 2012), " 
+      "with Microsoft Visual C++ Compiler Nov 2012 CTP (v120_CTP_Nov2012).") 
+  elseif( MSVC_VERSION EQUAL 1700 )  # VC11/VS2012 
+    message( "Using Microsoft Visual Studio 2012 with Microsoft Visual C++ Compiler Nov 2012 CTP (v120_CTP_Nov2012)" ) 
+    set(CMAKE_GENERATOR_TOOLSET "v120_CTP_Nov2012" CACHE STRING "Platform Toolset" FORCE) 
+  else() # VC12+, assuming C++11 supported. 
   endif()
 endif()
 
