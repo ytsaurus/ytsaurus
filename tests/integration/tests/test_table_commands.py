a23acc408c198a47aa3c726c1eda0444f5bc29ca
Maxim Babenko <babenko@ytsaurus.tech>
2013-11-16 21:50:58 +0400

    Enable scheduler for table tests

--- tests/integration/tests/test_table_commands.py
+++ tests/integration/tests/test_table_commands.py
@@ -10,6 +10,7 @@ from time import sleep
 class TestTableCommands(YTEnvSetup):
     NUM_MASTERS = 3
     NUM_NODES = 5
+    START_SCHEDULER = True
 
     def test_invalid_type(self):
         with pytest.raises(YtError): read('//tmp')
