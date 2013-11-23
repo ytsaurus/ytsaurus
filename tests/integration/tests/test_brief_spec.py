b13fa2b99c84c8656c6c172ea459c2273007c8e9
Maxim Babenko <babenko@ytsaurus.tech>
2013-11-23 15:21:36 +0400

    Fix and refactor erase test
    assert_attributes -> check_attributes
    YPaths must start with /
    Don't use YPath attributes

--- tests/integration/tests/test_brief_spec.py
+++ tests/integration/tests/test_brief_spec.py
@@ -9,31 +9,30 @@ from yt.yson import to_yson_type
 
 ##################################################################
 
-def assert_attributes(op_id, options):
-
-    spec_path = '//sys/operations/' + op_id + '/@spec/'
-    brief_spec_path = '//sys/operations/' + op_id + '/@brief_spec/'
+def check_attributes(op_id, options):
+    spec_path = '//sys/operations/' + op_id + '/@spec'
+    brief_spec_path = '//sys/operations/' + op_id + '/@brief_spec'
 
     if 'pool' in options:
-        assert get(spec_path + 'pool') == get(brief_spec_path + 'pool')
+        assert get(spec_path + '/pool') == get(brief_spec_path + '/pool')
     if 'reducer' in options:
-        assert get(spec_path + 'reducer/command') == get(brief_spec_path + 'reducer/command')
+        assert get(spec_path + '/reducer/command') == get(brief_spec_path + '/reducer/command')
     if 'mapper' in options:
-        assert get(spec_path + 'mapper/command') == get(brief_spec_path + 'mapper/command')
+        assert get(spec_path + '/mapper/command') == get(brief_spec_path + '/mapper/command')
     if 'table_path' in options:
-        assert get(spec_path + 'table_path') == get(brief_spec_path + 'table_path')
+        assert get(spec_path + '/table_path') == get(brief_spec_path + '/table_path')
 
     if 'input_table_path' in options:
-        assert get(brief_spec_path + 'input_table_paths/@count') == len(list(get(spec_path + 'input_table_paths')))
-        assert get(spec_path + 'input_table_paths/0') == get(brief_spec_path + 'input_table_paths/0')
+        assert get(brief_spec_path + '/input_table_paths/@count') == len(list(get(spec_path + '/input_table_paths')))
+        assert get(spec_path + '/input_table_paths/0') == get(brief_spec_path + '/input_table_paths/0')
 
     if 'output_table_path' in options:
-        assert get(brief_spec_path + 'output_table_paths/@count') == len(list(get(spec_path + 'output_table_paths')))
-        assert get(spec_path + 'output_table_paths/0') == get(brief_spec_path + 'output_table_paths/0')
+        assert get(brief_spec_path + '/output_table_paths/@count') == len(list(get(spec_path + '/output_table_paths')))
+        assert get(spec_path + '/output_table_paths/0') == get(brief_spec_path + '/output_table_paths/0')
     
     if 'output_table_path_1' in options:
-        assert get(brief_spec_path + 'output_table_paths/@count') == 1
-        assert get(spec_path + 'output_table_path') == get(brief_spec_path + 'output_table_paths/0')
+        assert get(brief_spec_path + '/output_table_paths/@count') == 1
+        assert get(spec_path + '/output_table_path') == get(brief_spec_path + '/output_table_paths/0')
 
 
 class TestBriefSpec(YTEnvSetup):
@@ -46,7 +45,7 @@ class TestBriefSpec(YTEnvSetup):
         create('table', '//tmp/t2')
         op_id = map(in_='//tmp/t1', out='//tmp/t2', command='cat')
         
-        assert_attributes(op_id, ['mapper', 'input_table_path', 'output_table_path'])
+        check_attributes(op_id, ['mapper', 'input_table_path', 'output_table_path'])
 
     def test_sort(self):
         create('table', '//tmp/t1')
@@ -55,7 +54,7 @@ class TestBriefSpec(YTEnvSetup):
              out='//tmp/t1',
              sort_by='key')
 
-        assert_attributes(op_id, ['input_table_path', 'output_table_path_1'])
+        check_attributes(op_id, ['input_table_path', 'output_table_path_1'])
 
     def test_reduce(self):
         create('table', '//tmp/t1')
@@ -67,7 +66,7 @@ class TestBriefSpec(YTEnvSetup):
         create('table', '//tmp/t2')
         op_id = reduce(in_='//tmp/t1', out='//tmp/t2', command='cat', reduce_by = 'key')
         
-        assert_attributes(op_id, ['reducer', 'input_table_path', 'output_table_path'])        
+        check_attributes(op_id, ['reducer', 'input_table_path', 'output_table_path'])        
     
     def test_map_reduce(self):
         create('table', '//tmp/t1')
@@ -79,7 +78,7 @@ class TestBriefSpec(YTEnvSetup):
                reduce_combiner_command='cat',
                reducer_command='cat')
         
-        assert_attributes(op_id, ['mapper', 'reducer', 'input_table_path', 'output_table_path'])   
+        check_attributes(op_id, ['mapper', 'reducer', 'input_table_path', 'output_table_path'])   
 
     def test_merge(self):
         create('table', '//tmp/t1')
@@ -90,13 +89,13 @@ class TestBriefSpec(YTEnvSetup):
               in_=['//tmp/t1', '//tmp/t2'],
               out='//tmp/t3')
         
-        assert_attributes(op_id, ['input_table_path', 'output_table_path_1'])
+        check_attributes(op_id, ['input_table_path', 'output_table_path_1'])
     
     def test_erase(self):
         create('table', '//tmp/t1')
 
-        op_id = erase('//tmp/t1[#0:#0]')
+        op_id = erase('//tmp/t1')
 
-        assert_attributes(op_id, ['table_path'])
+        check_attributes(op_id, ['table_path'])
 
 
