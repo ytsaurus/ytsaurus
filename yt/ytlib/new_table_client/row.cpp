241ad0ff4d988d1fd91edbbfbf8cdc9ea36d1cf9
Maxim Babenko <babenko@ytsaurus.tech>
2013-12-17 20:50:13 +0400

    Fix key succeessor generation

--- yt/ytlib/new_table_client/row.cpp
+++ yt/ytlib/new_table_client/row.cpp
@@ -302,8 +302,10 @@ size_t GetHash(const TUnversionedValue& value)
 TOwningKey GetKeySuccessorImpl(const TOwningKey& key, int prefixLength, EValueType sentinelType)
 {
     auto rowData = TSharedRef::Allocate<TOwningRowTag>(GetRowDataSize<TUnversionedValue>(prefixLength + 1), false);
+    auto* header = reinterpret_cast<TRowHeader*>(rowData.Begin());
+    header->ValueCount = prefixLength + 1;
     ::memcpy(rowData.Begin(), key.RowData.Begin(), GetRowDataSize<TUnversionedValue>(prefixLength));
-    TKey result(reinterpret_cast<TRowHeader*>(rowData.Begin()));
+    TKey result(header);
     result[prefixLength] = MakeSentinelValue<TUnversionedValue>(sentinelType);
     return TOwningKey(std::move(rowData), key.StringData);
 }
