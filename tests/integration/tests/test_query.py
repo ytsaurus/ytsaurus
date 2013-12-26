1406a4765ee2fb6b89326298a259a75b209c3d7d
Ivan Pouzyrevsky <sandello@yandex-team.ru>
2013-12-26 17:57:47 +0400

    Say "Hi!" to query tests

--- tests/integration/tests/test_query.py
+++ tests/integration/tests/test_query.py
@@ -7,25 +7,50 @@ import sys
 
 ##################################################################
 
-@pytest.mark.skipif("True")
 class TestQuery(YTEnvSetup):
     NUM_MASTERS = 3
     NUM_NODES = 3
     NUM_SCHEDULERS = 0
 
+    def _sample_data(self, path="//tmp/t", chunks=3, stripe=3):
+        create("table", path)
+
+        for i in xrange(chunks):
+            data = [
+                {"a": (i * stripe + j), "b": (i * stripe + j) * 10}
+                for j in xrange(1, 1 + stripe)]
+            write("<append=true>" + path, data)
+
+        set(path + "/@schema", [
+            {"name": "a", "type": "integer"},
+            {"name": "b", "type": "integer"}
+        ])
+
+    # TODO(sandello): TableMountCache is not invalidated at the moment,
+    # so table names must be unique.
     def test_simple(self):
-        for i in xrange (1, 100):
-            print "== {0} records per write ==".format(i)
-            tbl = '//tmp/t{0}'.format(i)
-            create('table', tbl)
-            data = [ {'a' : j, 'b' : j * 10} for j in xrange(i) ]
+        for i in xrange(0, 50, 10):
+            path = "//tmp/t{0}".format(i)
 
-            write(tbl, data)
-            write('<append=true>{0}'.format(tbl), data)
-            write('<append=true>{0}'.format(tbl), data)
-            set('{0}/@schema'.format(tbl), 
-                [{'name' : 'a', 'type' : 'integer'}, {'name' : 'b', 'type' : 'integer'}])
+            self._sample_data(path=path, chunks=i, stripe=10)
+            result = select("a, b from [{0}]".format(path), verbose=False)
 
-            res = select('a, b from [{0}]'.format(tbl))
-            assert len(res) == 3 * i
+            assert len(result) == 10 * i
 
+    def test_project1(self):
+        self._sample_data(path="//tmp/p1")
+        expected = [{"s": 2 * i + 10 * i - 1} for i in xrange(1, 10)]
+        actual = select("2 * a + b - 1 as s from [//tmp/p1]")
+        assert expected == actual
+
+    def test_group_by1(self):
+        self._sample_data(path="//tmp/g1")
+        expected = [{"s": 450}]
+        actual = select("sum(b) as s from [//tmp/g1] group by 1 as k")
+        self.assertItemsEqual(expected, actual)
+
+    def test_group_by2(self):
+        self._sample_data(path="//tmp/g2")
+        expected = [{"k": 0, "s": 200}, {"k": 1, "s": 250}]
+        actual = select("k, sum(b) as s from [//tmp/g2] group by a % 2 as k")
+        self.assertItemsEqual(expected, actual)
