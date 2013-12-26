297cb08453a27583b6bf11d6fba8257c6b67d3f0
Maxim Babenko <babenko@ytsaurus.tech>
2013-12-26 13:53:30 +0400

    Make reader-side cheaper. Writers will take precedence over readers.

--- yt/core/concurrency/rw_spinlock.h
+++ yt/core/concurrency/rw_spinlock.h
@@ -4,12 +4,19 @@
 
 #include <util/system/rwlock.h>
 
+#include <atomic>
+
 namespace NYT {
 namespace NConcurrency {
 
 ////////////////////////////////////////////////////////////////////////////////
 
 //! Single-writer multiple-readers spin lock.
+/*!
+ *  Reader-side calls are pretty cheap.
+ *  If no writers are present then readers never spin.
+ *  Writers take precedence over readers and can make the latter starve.
+ */
 class TReaderWriterSpinLock
 {
 public:
@@ -20,46 +27,72 @@ public:
     void AcquireReader()
     {
         while (true) {
-            auto value = AtomicGet(Value_);
-            if (value != 1) {
-                if (AtomicCas(&Value_, value + 2, value))
-                    return;
-            }
+            if (TryAcquireReader())
+                break;
             SpinLockPause();
         }
     }
 
     void ReleaseReader()
     {
-        AtomicAdd(Value_, -2);
+        Value_.fetch_sub(ReaderDelta, std::memory_order_release);
     }
 
     void AcquireWriter()
     {
-        if (AtomicCas(&Value_, 1, 0))
-            return;
-
         while (true) {
-            auto value = AtomicGet(Value_);
-            if (value == 0) {
-                if (AtomicCas(&Value_, 1, 0))
-                    return;
-            }
+            if (TryAcquireWriter())
+                break;
             SpinLockPause();
         }
     }
 
     void ReleaseWriter()
     {
-        ATOMIC_COMPILER_BARRIER();
-        AtomicSet(Value_, 0);
+        Value_.store(0, std::memory_order_release);
     }
 
 private:
-    //! 0 if not taken.
-    //! 1 if taken by a writer.
-    //! Number of readers * 2 if taken by readers.
-    TAtomic Value_;
+    std::atomic<intptr_t> Value_;
+
+    static const intptr_t WriterMask = 1;
+    static const intptr_t PendingWriterMask = 2;
+    static const intptr_t ReaderDelta = 4;
+
+    bool TryAcquireReader()
+    {
+        // Fail to take the lock in case of pending or active writers.
+        auto value = Value_.load(std::memory_order_relaxed);
+        if (value & (WriterMask | PendingWriterMask)) {
+            return false;
+        }
+
+        auto oldValue = Value_.fetch_add(ReaderDelta, std::memory_order_acquire);
+        if (oldValue & (WriterMask | PendingWriterMask)) {
+            // Backoff.
+            Value_.fetch_sub(ReaderDelta, std::memory_order_relaxed);
+            return false;
+        }
+
+        return true;
+    }
+
+    bool TryAcquireWriter()
+    {
+        // Prevent more readers from acquiring the lock.
+        auto value = Value_.fetch_or(PendingWriterMask, std::memory_order_acquire);
+        if (value & ~PendingWriterMask) {
+            // Fail if there are active readers or writers.
+            return false;
+        }
+
+        auto expected = PendingWriterMask;
+        if (!Value_.compare_exchange_weak(expected, WriterMask, std::memory_order_acquire)) {
+            return false;
+        }
+
+        return true;
+    }
 
 };
 
