386816ed42a1bdac2f3e9d75c8f5bb3160e3f577
Maxim Babenko <babenko@ytsaurus.tech>
2013-12-27 15:52:53 +0400

    Add versioned_{reader,writer} into cmakelists. Expand doc for versioned reader.

--- yt/ytlib/new_table_client/versioned_reader.h
+++ yt/ytlib/new_table_client/versioned_reader.h
@@ -10,8 +10,8 @@ namespace NVersionedTableClient {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//! Reads a schemed versioned rowset.
 /*!
- *  Reads versioned rowset with given schema.
  *  Versioned rowset implies that it is:
  *  1. Schemed.
  *  2. Sorted.
@@ -22,22 +22,29 @@ namespace NVersionedTableClient {
 struct IVersionedReader
     : public virtual TRefCounted
 {
+    //! Initializes the reader. Must be called (and its result must be waited for)
+    //! before making any other calls.
     virtual TAsyncError Open() = 0;
 
+    //! Tries to read more rows from the reader.
     /*!
      *  Depending on implementation, rows may come in two different flavours.
      *  1. Rows containing no more than one versioned value for each cell, 
      *     and exactly one timestamp, either tombstone or last committed (for merging).
-     *  2. Rows containing all available versions for  and a list of timestamps (for compactions).
+     *  2. Rows containing all available versions and a list of timestamps (for compactions).
      *
      *  Value ids correspond to column indexes in schema.
      *  Values are sorted in ascending order by ids, and then in descending order by timestamps.
      *
-     *  Returns false if reading is complete.
-     *  If returns true and empty rowset, caller should wait for ready event, until new data is available.
+     *  If |false| is returned then the end of the rowset is reached.
+     *  If |true| is returned but |rows| is empty then no more data is available at the moment.
+     *  The caller must wait for asynchronous flag provided by #GetReadyEvent to become set.
+     *  The latter may indicate an error occured while fetching more data.
      */
     virtual bool Read(std::vector<TVersionedRow>* rows) = 0;
 
+    //! Returns an asynchrounous flag enabling to wait for more data to come.
+    //! \see #Read.
     virtual TAsyncError GetReadyEvent() = 0;
 
 };
