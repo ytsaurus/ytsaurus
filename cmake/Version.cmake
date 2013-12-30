2158e6a40bfa8eb7d388a5f3d7f177461d84f6ae
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-12-30 22:17:11 +0400

    Propogate version in cmake

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -1,7 +1,7 @@
 # Set the build version
 set(YT_VERSION_MAJOR 0)
-set(YT_VERSION_MINOR 15)
-set(YT_VERSION_PATCH 12)
+set(YT_VERSION_MINOR 16)
+set(YT_VERSION_PATCH 1)
 
 if (NOT YT_BUILD_BRANCH)
   set(YT_BUILD_BRANCH "unknown")
