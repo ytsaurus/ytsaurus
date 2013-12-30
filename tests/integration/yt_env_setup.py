8c101367792e1edb85c2164a925690316433f0bc
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-12-30 17:23:26 +0400

    Do not create empty dirs per each test

--- tests/integration/yt_env_setup.py
+++ tests/integration/yt_env_setup.py
@@ -25,19 +25,20 @@ class YTEnvSetup(YTEnv):
         path_to_test = os.path.join(SANDBOX_ROOTDIR, test_name)
 
         # For running parallel
-        counter = 0
-        while True:
-            counter += 1
-            path_to_run = os.path.join(path_to_test, "run" + str(counter))
-            try:
-                if os.path.exists(path_to_run):
-                    age = datetime.now() - datetime.fromtimestamp(os.path.getmtime(path_to_run)) 
-                    if age.days > 0 or age.seconds > 60:
-                        shutil.rmtree(path_to_run, ignore_errors=True)
-                os.makedirs(path_to_run)
-                break
-            except OSError:
-                pass
+        # counter = 0
+        # while True:
+        #     counter += 1
+        #     path_to_run = os.path.join(path_to_test, "run" + str(counter))
+        #     try:
+        #         if os.path.exists(path_to_run):
+        #             age = datetime.now() - datetime.fromtimestamp(os.path.getmtime(path_to_run)) 
+        #             if age.days > 0 or age.seconds > 60:
+        #                 shutil.rmtree(path_to_run, ignore_errors=True)
+        #         os.makedirs(path_to_run)
+        #         break
+        #     except OSError:
+        #         pass
+        path_to_run = os.path.join(path_to_test, "run")
 
         pids_filename = os.path.join(path_to_run, 'pids.txt')
 
@@ -60,12 +61,6 @@ class YTEnvSetup(YTEnv):
         cls.Env.clear_environment()
 
     def setup_method(self, method):
-        path_to_test_case = os.path.join(self.path_to_test, method.__name__)
-
-        if os.path.exists(path_to_test_case):
-            shutil.rmtree(path_to_test_case)
-        os.makedirs(path_to_test_case)
-        os.chdir(path_to_test_case)
         if self.Env.NUM_MASTERS > 0:
             self.transactions_at_start = set(yt_commands.get_transactions())
 
