238d223a6417aaa1098c04bd23c1178ab57bc84d
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2013-12-30 17:00:34 +0400

    Add inner errors to 'error getting object type'

--- yt/ytlib/file_client/file_reader.cpp
+++ yt/ytlib/file_client/file_reader.cpp
@@ -109,14 +109,13 @@ TAsyncError TAsyncReader::AsyncOpen()
 TAsyncError TAsyncReader::OnInfoFetched(TObjectServiceProxy::TRspExecuteBatchPtr batchRsp)
 {
     if (!batchRsp->IsOK()) {
-        return MakeFuture(TError("Error fetching file info")
-            << *batchRsp);
+        return MakeFuture(TError("Error fetching file info") << *batchRsp);
     }
 
     {
         auto rsp = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_type");
         if (!rsp->IsOK()) {
-            return MakeFuture(TError("Error getting object type"));
+            return MakeFuture(TError("Error getting object type") << *rsp);
         }
 
         auto type = ConvertTo<EObjectType>(TYsonString(rsp->value()));
