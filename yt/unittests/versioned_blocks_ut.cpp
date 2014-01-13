c070d5991eddf6b4ef29200dd3fa84ace1eeb66b
Pavel Sushin <psushin@ytsaurus.tech>
2014-01-13 20:05:40 +0400

    Init unittest for versioned blocks.

---
+++ yt/unittests/versioned_blocks_ut.cpp
@@ -0,0 +1,28 @@
+#include "stdafx.h"
+#include "framework.h"
+
+#include <ytlib/new_table_client/chunk_reader.h>
+#include <ytlib/new_table_client/chunk_writer.h>
+#include <ytlib/new_table_client/name_table.h>
+#include <ytlib/new_table_client/reader.h>
+#include <ytlib/new_table_client/unversioned_row.h>
+
+#include <ytlib/chunk_client/memory_reader.h>
+#include <ytlib/chunk_client/memory_writer.h>
+
+#include <core/compression/public.h>
+
+namespace NYT {
+namespace NVersionedTableClient {
+namespace {
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NVersionedTableClient
+} // namespace NYT
