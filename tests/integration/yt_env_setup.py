75cdbe0e8b7ffeadbf7168674a780938d46060e5
Maxim Babenko <babenko@ytsaurus.tech>
2014-01-13 18:27:16 +0400

    Choose random ports

--- tests/integration/yt_env_setup.py
+++ tests/integration/yt_env_setup.py
@@ -43,18 +43,8 @@ class YTEnvSetup(YTEnv):
         pids_filename = os.path.join(path_to_run, 'pids.txt')
 
         cls.path_to_test = path_to_test
-        try:
-            import settings_local
-            ports = settings_local.ports
-        except Exception, e:
-            ports = {
-                "master": 29000,
-                "node": 29100,
-                "scheduler": 29200,
-                "proxy": 29300}
-
         cls.Env = cls()
-        cls.Env.set_environment(path_to_run, pids_filename, ports)
+        cls.Env.set_environment(path_to_run, pids_filename)
 
     @classmethod
     def teardown_class(cls):
