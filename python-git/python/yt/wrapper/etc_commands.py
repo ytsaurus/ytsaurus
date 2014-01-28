df13989b24b128ea1e3066e3e35af356109d651c
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-01-28 19:34:35 +0400

    parse_ypath works with YsonString

--- python-git/python/yt/wrapper/etc_commands.py
+++ python-git/python/yt/wrapper/etc_commands.py
@@ -1,8 +1,16 @@
 from driver import make_request
 from format import YsonFormat
 
-from yt.yson import loads
+from yt.common import update
+from yt.yson import loads, YsonString
 
 def parse_ypath(path):
-    return loads(make_request("parse_ypath", {"path": path, "output_format": YsonFormat().json()}))
-    
+    attributes = {}
+    if isinstance(path, YsonString):
+        attributes = path.attributes
+
+    result = loads(make_request("parse_ypath", {"path": path, "output_format": YsonFormat().json()}))
+    result.attributes = update(attributes, result.attributes)
+
+    return result
+
