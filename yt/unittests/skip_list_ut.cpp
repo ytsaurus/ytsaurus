1a3cf5554517c200905f997e87f13b075a4b1192
Maxim Babenko <babenko@ytsaurus.tech>
2014-02-11 23:35:33 +0400

    ASSERT -> EXPECT

--- yt/unittests/skip_list_ut.cpp
+++ yt/unittests/skip_list_ut.cpp
@@ -38,62 +38,62 @@ public:
 
 TEST_F(TSkipListTest, Empty)
 {
-    ASSERT_EQ(List.Size(), 0);
+    EXPECT_EQ(List.Size(), 0);
 
-    ASSERT_FALSE(List.FindEqualTo(1).IsValid());
+    EXPECT_FALSE(List.FindEqualTo(1).IsValid());
 
-    ASSERT_FALSE(List.FindGreaterThanOrEqualTo(1).IsValid());
+    EXPECT_FALSE(List.FindGreaterThanOrEqualTo(1).IsValid());
 }
 
 TEST_F(TSkipListTest, Singleton)
 {
-    ASSERT_TRUE(List.Insert(0));
-    ASSERT_EQ(List.Size(), 1);
+    EXPECT_TRUE(List.Insert(0));
+    EXPECT_EQ(List.Size(), 1);
 
-    ASSERT_FALSE(List.FindEqualTo(1).IsValid());
+    EXPECT_FALSE(List.FindEqualTo(1).IsValid());
 
-    ASSERT_FALSE(List.FindGreaterThanOrEqualTo(1).IsValid());
+    EXPECT_FALSE(List.FindGreaterThanOrEqualTo(1).IsValid());
 
     {
         auto it = List.FindGreaterThanOrEqualTo(-1);
-        ASSERT_TRUE(it.IsValid());
-        ASSERT_EQ(it.GetCurrent(), 0);
+        EXPECT_TRUE(it.IsValid());
+        EXPECT_EQ(it.GetCurrent(), 0);
         it.MoveNext();
-        ASSERT_FALSE(it.IsValid());
+        EXPECT_FALSE(it.IsValid());
     }
 
     {
         auto it = List.FindGreaterThanOrEqualTo(0);
-        ASSERT_TRUE(it.IsValid());
-        ASSERT_EQ(it.GetCurrent(), 0);
+        EXPECT_TRUE(it.IsValid());
+        EXPECT_EQ(it.GetCurrent(), 0);
         it.MoveNext();
-        ASSERT_FALSE(it.IsValid());
+        EXPECT_FALSE(it.IsValid());
     }
 }
 
 TEST_F(TSkipListTest, 1to10)
 {
     for (int i = 0; i < 10; ++i) {
-        ASSERT_TRUE(List.Insert(i));
+        EXPECT_TRUE(List.Insert(i));
     }
-    ASSERT_EQ(List.Size(), 10);
+    EXPECT_EQ(List.Size(), 10);
 
     for (int i = 0; i < 10; ++i) {
         auto it = List.FindGreaterThanOrEqualTo(i);
         for (int j = i; j < 10; ++j) {
-            ASSERT_TRUE(it.IsValid());
-            ASSERT_EQ(it.GetCurrent(), j);
+            EXPECT_TRUE(it.IsValid());
+            EXPECT_EQ(it.GetCurrent(), j);
             it.MoveNext();
         }
-        ASSERT_FALSE(it.IsValid());
+        EXPECT_FALSE(it.IsValid());
     }
 
     for (int i = 0; i < 10; ++i) {
-        ASSERT_TRUE(List.FindEqualTo(i).IsValid());
+        EXPECT_TRUE(List.FindEqualTo(i).IsValid());
     }
 
-    ASSERT_FALSE(List.FindEqualTo(-1).IsValid());
-    ASSERT_FALSE(List.FindEqualTo(11).IsValid());
+    EXPECT_FALSE(List.FindEqualTo(-1).IsValid());
+    EXPECT_FALSE(List.FindEqualTo(11).IsValid());
 }
 
 TEST_F(TSkipListTest, Random1000000)
@@ -102,18 +102,18 @@ TEST_F(TSkipListTest, Random1000000)
     std::set<int> set;
     for (int i = 0; i < 1000000; ++i) {
         int value = rand();
-        ASSERT_EQ(List.Insert(value), set.insert(value).second);
+        EXPECT_EQ(List.Insert(value), set.insert(value).second);
     }
-    ASSERT_EQ(List.Size(), set.size());
+    EXPECT_EQ(List.Size(), set.size());
 
     for (int value : set) {
-        ASSERT_TRUE(List.FindEqualTo(value).IsValid());
+        EXPECT_TRUE(List.FindEqualTo(value).IsValid());
     }
 
     auto it = List.FindGreaterThanOrEqualTo(*set.begin());
     for (int value : set) {
-        ASSERT_TRUE(it.IsValid());
-        ASSERT_EQ(it.GetCurrent(), value);
+        EXPECT_TRUE(it.IsValid());
+        EXPECT_EQ(it.GetCurrent(), value);
         it.MoveNext();
     }
 }
