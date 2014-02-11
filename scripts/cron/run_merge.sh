2aca118a5ad96aa9f75311f2bc34681501269ae0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-02-11 20:21:14 +0400

    mail_if_failed print tail of output. Fix return code of yt_lock.py. Add run_merge for kant and smith.

--- scripts/cron/run_merge.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash -eux
-
-QUEUE="//sys/cron/tables_to_merge"
-
-/opt/cron/tools/chunk_size_distribution.py --create-merge-queue --queue-path $QUEUE --filter-out "//tmp" --filter-out "//sys"
-
-/opt/cron/tools/run_parallel.sh "/opt/cron/tools/merge.py $QUEUE" 20 /opt/cron/merging_log
-
