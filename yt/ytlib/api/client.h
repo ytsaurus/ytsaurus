4b0a88622b5e675ac9c09bd15ada4a2c269a4c59
Maxim Babenko <babenko@ytsaurus.tech>
2014-02-23 15:16:27 +0400

    squashme

--- yt/ytlib/api/client.h
+++ yt/ytlib/api/client.h
@@ -437,7 +437,7 @@ DEFINE_REFCOUNTED_TYPE(IClient)
 
 IClientPtr CreateClient(
     IConnectionPtr connection,
-    const TClientOptions& options);
+    const TClientOptions& options = TClientOptions());
 
 ///////////////////////////////////////////////////////////////////////////////
 
