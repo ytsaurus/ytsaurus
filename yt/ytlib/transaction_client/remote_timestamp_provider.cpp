65e1832293bf48cb2f629d0960ae7f1306eeaeb2
Maxim Babenko <babenko@ytsaurus.tech>
2014-03-12 19:22:36 +0400

    Batch timestamp requests

--- yt/ytlib/transaction_client/remote_timestamp_provider.cpp
+++ yt/ytlib/transaction_client/remote_timestamp_provider.cpp
@@ -16,7 +16,6 @@ using namespace NHydra;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-// TODO(babenko): this needs much improvement
 class TRemoteTimestampProvider
     : public ITimestampProvider
 {
@@ -27,19 +26,29 @@ public:
         : Config_(config)
         , Channel_(CreatePeerChannel(config, channelFactory, EPeerRole::Leader))
         , Proxy(Channel_)
+        , RequestInProgress_(false)
         , LatestTimestamp_(NullTimestamp)
     { }
 
     virtual TFuture<TErrorOr<TTimestamp>> GenerateTimestamps(int count) override
     {
         VERIFY_THREAD_AFFINITY_ANY();
+        YCHECK(count > 0);
 
-        auto req = Proxy.GenerateTimestamps();
-        req->set_count(count);
-        return req->Invoke().Apply(BIND(
-            &TRemoteTimestampProvider::OnGenerateTimestampsResponse,
-            MakeStrong(this),
-            count));
+        TRequest request;
+        request.Count = count;
+        request.Promise = NewPromise<TErrorOr<TTimestamp>>();
+
+        {
+            TGuard<TSpinLock> guard(SpinLock_);
+            PendingRequests_.push_back(request);
+            if (!RequestInProgress_) {
+                YCHECK(PendingRequests_.size() == 1);
+                SendRequest();
+            }
+        }
+
+        return request.Promise;
     }
 
     virtual TTimestamp GetLatestTimestamp() override
@@ -56,21 +65,64 @@ private:
     IChannelPtr Channel_;
     TTimestampServiceProxy Proxy;
 
+    struct TRequest
+    {
+        int Count;
+        TPromise<TErrorOr<TTimestamp>> Promise;
+    };
+
     TSpinLock SpinLock_;
-    TTimestamp LatestTimestamp_;
+    bool RequestInProgress_;
+    volatile TTimestamp LatestTimestamp_;
+    std::vector<TRequest> PendingRequests_;
 
 
-    TErrorOr<TTimestamp> OnGenerateTimestampsResponse(
-        int count,
+    void SendRequest()
+    {
+        VERIFY_SPINLOCK_AFFINITY(SpinLock_);
+        YCHECK(!RequestInProgress_);
+
+        std::vector<TRequest> requests;
+        requests.swap(PendingRequests_);
+
+        int count = 0;
+        for (const auto& request : requests) {
+            count += request.Count;
+        }
+
+        auto req = Proxy.GenerateTimestamps();
+        req->set_count(count);
+
+        RequestInProgress_ = true;
+
+        req->Invoke().Subscribe(BIND(
+            &TRemoteTimestampProvider::OnResponse,
+            MakeStrong(this),
+            Passed(std::move(requests))));
+    }
+
+    void OnResponse(
+        std::vector<TRequest> requests,
         TTimestampServiceProxy::TRspGenerateTimestampsPtr rsp)
     {
+        TGuard<TSpinLock> guard(SpinLock_);
+
+        RequestInProgress_ = false;
+
         if (rsp->IsOK()) {
-            TGuard<TSpinLock> guard(SpinLock_);
             auto timestamp = TTimestamp(rsp->timestamp());
-            LatestTimestamp_ = std::max(LatestTimestamp_, timestamp + count - 1);
-            return TErrorOr<TTimestamp>(timestamp);
+            for (auto& request : requests) {
+                request.Promise.Set(TErrorOr<TTimestamp>(timestamp));
+                timestamp += request.Count;
+            }
         } else {
-            return TErrorOr<TTimestamp>(rsp->GetError());
+            for (auto& request : requests) {
+                request.Promise.Set(rsp->GetError());
+            }
+        }
+
+        if (!PendingRequests_.empty()) {
+            SendRequest();
         }
     }
 
