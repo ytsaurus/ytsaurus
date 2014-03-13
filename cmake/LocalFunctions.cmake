68f361a226e56bf982d7ede4e8c21b7d281ef14d
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2014-03-13 21:17:05 +0400

    Refactor CMakeLists.txt in favor of upcoming Perl
    Mainly, it is a cleanup + introduction of PerlXSCpp() method.

--- cmake/LocalFunctions.cmake
+++ cmake/LocalFunctions.cmake
@@ -62,102 +62,111 @@ function(PUMP pump output)
 )
 endfunction()
 
-find_program(RAGEL_EXECUTABLE ragel)
-mark_as_advanced(RAGEL_EXECUTABLE)
-
-if(YT_BUILD_HAVE_RAGEL)
-  if(NOT RAGEL_EXECUTABLE)
-    message(FATAL_ERROR "Could not find Ragel.")
-  endif()
-
-  execute_process(
-    COMMAND ${RAGEL_EXECUTABLE} --version
-    OUTPUT_VARIABLE RAGEL_VERSION
-  )
-
-  if(NOT RAGEL_VERSION MATCHES "6.8")
-    message(FATAL_ERROR "Ragel 6.8 is required.")
-  endif()
-
-  message(STATUS "Found Ragel: ${RAGEL_EXECUTABLE}")
-endif()
-
-function(RAGEL input output)
-  get_filename_component(_real_path ${input} REALPATH)
-  get_filename_component(_dirname ${_real_path} PATH)
-  get_filename_component(_basename ${_real_path} NAME_WE)
-  set(_result ${_dirname}/${_basename}.cpp)
+function(RAGEL source result_variable)
+  get_filename_component(_realpath ${source} REALPATH)
+  get_filename_component(_dirname ${_realpath} PATH)
+  get_filename_component(_basename ${_realpath} NAME_WE)
+  set(_input ${_realpath})
+  set(_output ${_dirname}/${_basename}.cpp)
 
   if(YT_BUILD_HAVE_RAGEL)
     # Specify custom command how to generate .cpp.
     add_custom_command(
       OUTPUT
-        ${_result}
+        ${_output}
       COMMAND
-        ${RAGEL_EXECUTABLE} -C -G2 ${_real_path} -o ${_result}
+        ${RAGEL_EXECUTABLE} -C -G2 ${_input} -o ${_output}
       COMMAND
-        perl -ni -e 'print unless /^\#line/' ${_result}
+        ${PERL_EXECUTABLE} -ni -e 'print unless /^\#line/' ${_output}
       MAIN_DEPENDENCY
-        ${_real_path}
+        ${_input}
       WORKING_DIRECTORY
-        ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Generating Ragel automata from ${input}..."
+        ${CMAKE_CURRENT_SOURCE_DIR}
+      COMMENT
+        "Generating Ragel automata from ${input}..."
     )
   endif()
 
-  # Append generated .cpp to the output variable.
-  set(${output} ${${output}} ${_result} PARENT_SCOPE)
+  set(${result_variable} ${${result_variable}} ${_output} PARENT_SCOPE)
 endfunction()
 
-find_program(BISON_EXECUTABLE bison)
-mark_as_advanced(BISON_EXECUTABLE)
-
-if(YT_BUILD_HAVE_BISON)
-  if(NOT BISON_EXECUTABLE)
-    message(FATAL_ERROR "Could not find Bison.")
-  endif()
-
-  execute_process(
-    COMMAND ${BISON_EXECUTABLE} --version
-    OUTPUT_VARIABLE BISON_VERSION
-  )
-
-  if(NOT BISON_VERSION MATCHES "3.0")
-    message(FATAL_ERROR "Bison 3.0 is required.")
-  endif()
-
-  message(STATUS "Found Bison: ${BISON_EXECUTABLE}")
-endif()
-
-function(BISON input output)
-  get_filename_component(_real_path ${input} REALPATH)
-  get_filename_component(_dirname ${_real_path} PATH)
-  get_filename_component(_basename ${_real_path} NAME_WE)
-  set(_result ${_dirname}/${_basename}.cpp)
-  set(_result_aux ${_dirname}/${_basename}.hpp ${_dirname}/stack.hh)
+function(BISON source result_variable)
+  get_filename_component(_realpath ${source} REALPATH)
+  get_filename_component(_dirname ${_realpath} PATH)
+  get_filename_component(_basename ${_realpath} NAME_WE)
 
   if (YT_BUILD_HAVE_BISON)
     # Specify custom command how to generate .cpp.
     add_custom_command(
       OUTPUT
-        ${_result} ${_result_aux}
+        ${_dirname}/${_basename}.cpp
+        ${_dirname}/${_basename}.hpp
+        ${_dirname}/stack.hh
       COMMAND
-        ${BISON_EXECUTABLE} --locations -fcaret ${_real_path} -o ${_result}
+        ${BISON_EXECUTABLE} --locations -fcaret ${_realpath} -o ${_dirname}/${_basename}.cpp
       COMMAND
-        perl -ni -e 'print unless /^\#line/' ${_dirname}/${_basename}.cpp
+        ${PERL_EXECUTABLE} -ni -e 'print unless /^\#line/' ${_dirname}/${_basename}.cpp
       COMMAND
-        perl -ni -e 'print unless /^\#line/' ${_dirname}/${_basename}.hpp
+        ${PERL_EXECUTABLE} -ni -e 'print unless /^\#line/' ${_dirname}/${_basename}.hpp
       COMMAND
-        perl -ni -e 'print unless /^\#line/' ${_dirname}/stack.hh
+        ${PERL_EXECUTABLE} -ni -e 'print unless /^\#line/' ${_dirname}/stack.hh
       MAIN_DEPENDENCY
-        ${_real_path}
+        ${_realpath}
       WORKING_DIRECTORY
-        ${CMAKE_CURRENT_BINARY_DIR}
-      COMMENT "Generating Bison parser from ${input}..."
+        ${CMAKE_CURRENT_SOURCE_DIR}
+      COMMENT
+        "Generating Bison parser from ${source}..."
     )
   endif()
 
-  # Append generated .cpp to the output variable.
-  set(${output} ${${output}} ${_result} ${_result_aux} PARENT_SCOPE)
+  set(
+    ${result_variable}
+    ${${result_variable}}
+    ${_dirname}/${_basename}.cpp
+    ${_dirname}/${_basename}.hpp
+    ${_dirname}/stack.hh
+    PARENT_SCOPE
+  )
+endfunction()
+
+function(PERLXSCPP source result_variable)
+  get_filename_component(_realpath ${source} REALPATH)
+  get_filename_component(_dirname ${_realpath} PATH)
+  get_filename_component(_basename ${_realpath} NAME_WE)
+  set(_input ${_realpath})
+  string(REPLACE ${CMAKE_SOURCE_DIR} ${CMAKE_BINARY_DIR} _output ${_dirname}/${_basename}.xs.cpp)
+
+  add_custom_command(
+    OUTPUT
+      ${_output}
+      ${CMAKE_CURRENT_BINARY_DIR}/ppport.h
+    COMMAND
+      ${PERL_EXECUTABLE}
+        -mDevel::PPPort
+        -eDevel::PPPort::WriteFile
+    COMMAND
+      ${PERL_EXECUTABLE}
+        ${PERL_PRIVLIB}/ExtUtils/xsubpp
+        -typemap ${PERL_PRIVLIB}/ExtUtils/typemap
+        -csuffix .cpp
+        -prototypes
+        -output ${_output}
+        ${_input}
+    MAIN_DEPENDENCY
+      ${_input}
+    WORKING_DIRECTORY
+      ${CMAKE_CURRENT_BINARY_DIR}
+    COMMENT
+      "Building ${source} with perl (.xs.cpp)..."
+    )
+
+  set_source_files_properties(
+    ${_output}
+    PROPERTIES
+    COMPILE_FLAGS ${PERL_EXTRA_C_FLAGS} -Wno-unused-variable -Wno-literal-suffix
+    GENERATED TRUE
+  )
+
+  set(${result_variable} ${${result_variable}} ${_output} PARENT_SCOPE)
 endfunction()
 
