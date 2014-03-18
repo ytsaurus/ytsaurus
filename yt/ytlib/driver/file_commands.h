4b8ab966e274b6d5a502de23264f5af82806694c
Maxim Babenko <babenko@ytsaurus.tech>
2014-03-18 11:27:28 +0400

    Use new API in upload and download commands

--- yt/ytlib/driver/file_commands.h
+++ yt/ytlib/driver/file_commands.h
@@ -11,6 +11,7 @@ namespace NDriver {
 
 struct TDownloadRequest
     : public TTransactionalRequest
+    , public TSuppressableAccessTrackingRequest
 {
     NYPath::TRichYPath Path;
     TNullable<i64> Offset;
