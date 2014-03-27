b95a358c5abf56d0d9bfd2e7fcd08f7f3c3e43e4
Pavel Sushin <psushin@ytsaurus.tech>
2014-03-27 16:23:47 +0400

    Fix build.

--- yt/core/misc/chunked_memory_pool.h
+++ yt/core/misc/chunked_memory_pool.h
@@ -35,7 +35,7 @@ public:
         Initialize(
             GetRefCountedTrackerCookie<TTag>(),
             chunkSize,
-            maxSmallBlockSizeRatio)
+            maxSmallBlockSizeRatio);
     }
 
     //! Allocates #sizes bytes without any alignment.
