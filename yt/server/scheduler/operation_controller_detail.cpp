c06eb2dfc4f591ac6ddb95c9957f69f5332d163d
Pavel Sushin <psushin@ytsaurus.tech>
2014-04-10 01:30:34 +0400

    Register all input chunk specs in the stripe.

--- yt/server/scheduler/operation_controller_detail.cpp
+++ yt/server/scheduler/operation_controller_detail.cpp
@@ -3239,14 +3239,8 @@ void TOperationControllerBase::RegisterInputStripe(TChunkStripePtr stripe, TTask
         auto chunkSpec = slice->GetChunkSpec();
         auto chunkId = FromProto<TChunkId>(chunkSpec->chunk_id());
 
-        if (!visitedChunks.insert(chunkId).second) {
-            // We have already seen this chunk in this stripe.
-            continue;
-        }
-
         auto pair = InputChunkMap.insert(std::make_pair(chunkId, TInputChunkDescriptor()));
         auto& chunkDescriptor = pair.first->second;
-        chunkDescriptor.InputStripes.push_back(stripeDescriptor);
 
         if (InputChunkSpecs.insert(chunkSpec).second) {
             chunkDescriptor.ChunkSpecs.push_back(chunkSpec);
@@ -3254,6 +3248,10 @@ void TOperationControllerBase::RegisterInputStripe(TChunkStripePtr stripe, TTask
 
         if (IsUnavailable(*chunkSpec, NeedsAllChunkParts())) {
             chunkDescriptor.State = EInputChunkState::Waiting;
+        }
+
+        if (visitedChunks.insert(chunkId).second) {
+            chunkDescriptor.InputStripes.push_back(stripeDescriptor);
         }
     }
 }
