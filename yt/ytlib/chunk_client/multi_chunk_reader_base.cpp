a8fdaffdc99019284fbf2f5e98c9f661a11ea08c
Pavel Sushin <psushin@ytsaurus.tech>
2014-04-11 12:41:51 +0400

    Drafts of multi chunk reader.

--- yt/ytlib/chunk_client/multi_chunk_reader_base.cpp
+++ yt/ytlib/chunk_client/multi_chunk_reader_base.cpp
@@ -1,7 +1,33 @@
 #include "stdafx.h"
 
+#include "multi_chunk_reader_base.h"
+
+#include "block_cache.h"
+#include "chunk_meta_extensions.h"
+#include "chunk_reader_base.h"
+#include "chunk_spec.h"
+#include "config.h"
+#include "dispatcher.h"
+#include "erasure_reader.h"
+#include "private.h"
+#include "replication_reader.h"
+
+#include <ytlib/node_tracker_client/node_directory.h>
+
+#include <core/concurrency/fiber.h>
+#include <core/concurrency/parallel_awaiter.h>
+
+#include <core/erasure/codec.h>
+
+#include <core/misc/protobuf_helpers.h>
+
 namespace NYT {
 namespace NChunkClient {
+
+using namespace NConcurrency;
+using namespace NErasure;
+using namespace NProto;
+using namespace NNodeTrackerClient;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -41,22 +67,30 @@ TNontemplateMultiChunkReaderBase::TNontemplateMultiChunkReaderBase(
     TMultiChunkReaderConfigPtr config,
     TMultiChunkReaderOptionsPtr options,
     NRpc::IChannelPtr masterChannel,
-    NChunkClient::IBlockCachePtr blockCache,
+    IBlockCachePtr blockCache,
     NNodeTrackerClient::TNodeDirectoryPtr nodeDirectory,
     const std::vector<NProto::TChunkSpec>& chunkSpecs)
     : Logger(ChunkReaderLogger)
-    , ChunkSpecs_(chunkSpecs)
     , Options_(options)
-    , MasterChannel_(masterChannel)
+    , ChunkSpecs_(chunkSpecs)
+    , CompletionError_(NewPromise<TError>())
     , BlockCache_(blockCache)
+    , MasterChannel_(masterChannel)
     , NodeDirectory_(nodeDirectory)
+    , PrefetchReaderIndex_(0)
+    , FetchingCompletedAwaiter_(New<TParallelAwaiter>(GetSyncInvoker()))
     , IsOpen_(false)
-    , LastOpenedReaderIndex_(-1)
+    , OpenedReaderCount_(0)
 {
+    Logger.AddTag(Sprintf("Reader: %p", this));
+
     Config_ = CloneYsonSerializable(config);
 
+    CurrentSession_.Reset();
+
+    LOG_DEBUG("Creating multi chunk reader for %d chunks", static_cast<int>(ChunkSpecs_.size()));
+
     if (ChunkSpecs_.empty()) {
-        LOG_DEBUG("Preparing reader - no chunk specs");
         CompletionError_.Set(TError());
         return;
     }
@@ -89,28 +123,318 @@ TNontemplateMultiChunkReaderBase::TNontemplateMultiChunkReaderBase(
         PrefetchWindow_ = CalculatePrefetchWindow(sortedChunkSpecs, Config_);
     }
 
-    LOG_DEBUG("Preparing reader (PrefetchWindow: %d)", PrefetchWindow);
+    LOG_DEBUG("Created multi chunk reader (PrefetchWindow: %d)", PrefetchWindow_);
 }
 
 TAsyncError TNontemplateMultiChunkReaderBase::Open()
 {
     YCHECK(!IsOpen_);
+    IsOpen_ = true;
     if (CompletionError_.IsSet())
         return CompletionError_.ToFuture();
 
-    return BIND(&TNontemplateMultiChunkReaderBase::DoOpen, MakeWeak(this))
+    return BIND(&TNontemplateMultiChunkReaderBase::DoOpen, MakeStrong(this))
         .AsyncVia(TDispatcher::Get()->GetReaderInvoker())
         .Run();
 }
 
+TAsyncError TNontemplateMultiChunkReaderBase::GetReadyEvent()
+{
+    return ReadyEvent_;
+}
+
+TDataStatistics TNontemplateMultiChunkReaderBase::GetDataStatistics() const
+{
+    TGuard<TSpinLock> guard(DataStatisticsLock_);
+    auto dataStatistics = DataStatistics_;
+    for (auto reader : ActiveReaders_) {
+        dataStatistics += reader->GetDataStatistics();
+    }
+    return dataStatistics;
+}
+
+bool TNontemplateMultiChunkReaderBase::IsFetchingCompleted() const
+{
+    return FetchingCompletedAwaiter_->IsCompleted() &&
+        (FetchingCompletedAwaiter_->GetRequestCount() == FetchingCompletedAwaiter_->GetResponseCount());
+}
+
+void TNontemplateMultiChunkReaderBase::OpenPrefetchChunks()
+{
+    for (int i = 0; i < PrefetchWindow_; ++i) {
+        OpenNextChunk();
+    }
+}
+
+void TNontemplateMultiChunkReaderBase::OpenNextChunk()
+{
+    BIND(
+        &TNontemplateMultiChunkReaderBase::DoOpenNextChunk, 
+        MakeWeak(this))
+    .Via(TDispatcher::Get()->GetReaderInvoker())
+    .Run();
+}
+
+void TNontemplateMultiChunkReaderBase::DoOpenNextChunk()
+{
+    if (CompletionError_.IsSet())
+        return;
+
+    if (PrefetchReaderIndex_ >= ChunkSpecs_.size())
+        return;
+
+    int chunkIndex = PrefetchReaderIndex_++;
+    auto& chunkSpec = ChunkSpecs_[chunkIndex];
+
+    LOG_DEBUG("Opening chunk (ChunkIndex: %d)", chunkIndex);
+    auto remoteReader = CreateRemoteReader(chunkSpec);
+
+    auto reader = CreateTemplateReader(chunkSpec, remoteReader);
+    auto error = WaitFor(reader->Open());
+
+    if (!error.IsOK()) {
+        CompletionError_.TrySet(error);
+        RegisterFailedChunk(chunkIndex);
+        return;
+    }
+
+    OnReaderOpened(reader, chunkIndex);
+
+    FetchingCompletedAwaiter_->Await(reader->GetFetchingCompletedEvent());
+    if (++OpenedReaderCount_ == ChunkSpecs_.size()) {
+        FetchingCompletedAwaiter_->Complete();
+    }
+
+    TGuard<TSpinLock> guard(DataStatisticsLock_);
+    YCHECK(ActiveReaders_.insert(reader).second);
+}
+
+IAsyncReaderPtr TNontemplateMultiChunkReaderBase::CreateRemoteReader(const TChunkSpec& chunkSpec)
+{
+    auto chunkId = NYT::FromProto<TChunkId>(chunkSpec.chunk_id());
+    auto replicas = NYT::FromProto<TChunkReplica, TChunkReplicaList>(chunkSpec.replicas());
+
+    LOG_DEBUG("Creating remote reader (ChunkId: %s)", ~ToString(chunkId));
+
+    if (!IsErasureChunkId(chunkId)) {
+        return CreateReplicationReader(
+            Config_,
+            BlockCache_,
+            MasterChannel_,
+            NodeDirectory_,
+            Null,
+            chunkId,
+            replicas);
+    }
+     
+    std::sort(
+        replicas.begin(),
+        replicas.end(),
+        [] (TChunkReplica lhs, TChunkReplica rhs) {
+            return lhs.GetIndex() < rhs.GetIndex();
+        });
+
+    auto erasureCodecId = ECodec(chunkSpec.erasure_codec());
+    auto* erasureCodec = GetCodec(erasureCodecId);
+    auto dataPartCount = erasureCodec->GetDataPartCount();
+
+    std::vector<IAsyncReaderPtr> readers;
+    readers.reserve(dataPartCount);
+
+    auto it = replicas.begin();
+    while (it != replicas.end() && it->GetIndex() < dataPartCount) {
+        auto jt = it;
+        while (jt != replicas.end() && it->GetIndex() == jt->GetIndex()) {
+            ++jt;
+        }
+
+        TChunkReplicaList partReplicas(it, jt);
+        auto partId = ErasurePartIdFromChunkId(chunkId, it->GetIndex());
+        auto reader = CreateReplicationReader(
+            Config_,
+            BlockCache_,
+            MasterChannel_,
+            NodeDirectory_,
+            Null,
+            partId,
+            partReplicas);
+        readers.push_back(reader);
+
+        it = jt;
+    }
+    
+    YCHECK(readers.size() == dataPartCount);
+    return CreateNonReparingErasureReader(readers);
+}
+
+void TNontemplateMultiChunkReaderBase::OnReaderFinished()
+{
+    if (Options_->KeepInMemory) {
+        FinishedReaders_.push_back(CurrentSession_.ChunkReader);
+    }
+
+    TGuard<TSpinLock> guard(DataStatisticsLock_);
+    DataStatistics_ += CurrentSession_.ChunkReader->GetDataStatistics();
+    YCHECK(ActiveReaders_.erase(CurrentSession_.ChunkReader));
+
+    CurrentSession_.Reset();
+
+    OpenNextChunk();
+}
+
+bool TNontemplateMultiChunkReaderBase::OnEmptyRead(bool readerFinished)
+{
+    if (readerFinished) {
+        OnReaderFinished();
+        return !CompletionError_.IsSet();
+    } else {
+        OnReaderBlocked();
+        return true;
+    }
+}
+
+void TNontemplateMultiChunkReaderBase::OnError()
+{ }
+
+void TNontemplateMultiChunkReaderBase::RegisterFailedChunk(int chunkIndex)
+{   
+    auto chunkId = NYT::FromProto<TChunkId>(ChunkSpecs_[chunkIndex].chunk_id());
+    LOG_WARNING("Chunk reader failed (ChunkId: %s)", ~ToString(chunkId));
+
+    OnError();
+
+    TGuard<TSpinLock> guard(FailedChunksLock_);
+    FailedChunks_.push_back(chunkId);
+}
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TAsyncError TNontemplateSequentialMultiChunkReaderBase::DoOpen()
+TNontemplateSequentialMultiChunkReaderBase::TNontemplateSequentialMultiChunkReaderBase(
+    TMultiChunkReaderConfigPtr config,
+    TMultiChunkReaderOptionsPtr options,
+    NRpc::IChannelPtr masterChannel,
+    NChunkClient::IBlockCachePtr blockCache,
+    NNodeTrackerClient::TNodeDirectoryPtr nodeDirectory,
+    const std::vector<NProto::TChunkSpec>& chunkSpecs)
+    : TNontemplateMultiChunkReaderBase(
+        config, 
+        options, 
+        masterChannel, 
+        blockCache, 
+        nodeDirectory, 
+        chunkSpecs)
+    , NextReaderIndex_(0)
 {
-
-    
+    NextReaders_.reserve(ChunkSpecs_.size());
+    for (int i = 0; i < ChunkSpecs_.size(); ++i) {
+        NextReaders_.push_back(NewPromise<IChunkReaderBasePtr>());
+    }
 }
+
+TError TNontemplateSequentialMultiChunkReaderBase::DoOpen()
+{
+    OpenPrefetchChunks();
+    return WaitForNextReader();
+}
+
+void TNontemplateSequentialMultiChunkReaderBase::OnReaderOpened(IChunkReaderBasePtr chunkReader, int chunkIndex)
+{
+    // May have already been set in case of error.
+    NextReaders_[chunkIndex].TrySet(chunkReader);
+}
+
+void TNontemplateSequentialMultiChunkReaderBase::OnReaderBlocked()
+{
+    ReadyEvent_ = BIND(
+        &TNontemplateSequentialMultiChunkReaderBase::WaitForCurrentReader, 
+        MakeStrong(this))
+    .AsyncVia(TDispatcher::Get()->GetReaderInvoker())
+    .Run();
+}
+
+void TNontemplateSequentialMultiChunkReaderBase::OnReaderFinished()
+{
+    TNontemplateMultiChunkReaderBase::OnReaderFinished();
+ 
+    if (NextReaderIndex_ < ChunkSpecs_.size()) {
+        ReadyEvent_ = BIND(
+            &TNontemplateSequentialMultiChunkReaderBase::WaitForNextReader, 
+            MakeStrong(this))
+        .AsyncVia(TDispatcher::Get()->GetReaderInvoker())
+        .Run();
+    } else {
+        CompletionError_.TrySet(TError());
+        ReadyEvent_ = CompletionError_.ToFuture();
+    }
+}
+
+TError TNontemplateSequentialMultiChunkReaderBase::WaitForNextReader()
+{
+    CurrentSession_.ChunkSpecIndex = NextReaderIndex_;
+    CurrentSession_.ChunkReader = WaitFor(NextReaders_[NextReaderIndex_].ToFuture());
+    
+    OnReaderSwitched();
+
+    ++NextReaderIndex_;
+
+    return CompletionError_.IsSet() ? CompletionError_.Get() : TError();
+}
+
+TError TNontemplateSequentialMultiChunkReaderBase::WaitForCurrentReader()
+{
+    auto error = WaitFor(CurrentSession_.ChunkReader->GetReadyEvent());
+    if (!error.IsOK()) {
+        CompletionError_.TrySet(error);
+        RegisterFailedChunk(CurrentSession_.ChunkSpecIndex);
+    }
+
+    return error;
+}
+
+void TNontemplateSequentialMultiChunkReaderBase::OnError()
+{
+    // This is to avoid infinite waiting and memory leaks.
+    for (auto& nextReader : NextReaders_) {
+        nextReader.TrySet(nullptr);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+/*
+void TNontemplateParallelMultiChunkReaderBase::OnReaderBlocked()
+{
+    BIND(
+        &TNontemplateSequentialMultiChunkReaderBase::WaitForCurrentReader, 
+        MakeStrong(this))
+    .Via(TDispatcher::Get()->GetReaderInvoker())
+    .Run();
+
+    ReadyEvent_ = BIND(
+        &TNontemplateSequentialMultiChunkReaderBase::WaitForReadyReader, 
+        MakeStrong(this))
+    .AsyncVia(TDispatcher::Get()->GetReaderInvoker())
+    .Run();
+}
+
+void TNontemplateSequentialMultiChunkReaderBase::OnReaderFinished()
+{
+    OpenNextChunk();
+ 
+    ++FinishedReaderCount_;
+
+    if (FinishedReaderCount_ < ChunkSpecs_.size()) {
+        ReadyEvent_ = BIND(
+            &TNontemplateSequentialMultiChunkReaderBase::WaitForReadyReader, 
+            MakeStrong(this))
+        .AsyncVia(TDispatcher::Get()->GetReaderInvoker())
+        .Run();
+    } else {
+        CompletionError_.TrySet(TError());
+        ReadyEvent_ = CompletionError_.ToFuture();
+    }
+}
+*/
 
 ////////////////////////////////////////////////////////////////////////////////
 
