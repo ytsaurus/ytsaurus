ca717834fd97f3b628293aa04d7b5a86776a626f
Maxim Babenko <babenko@ytsaurus.tech>
2014-05-10 15:07:06 +0400

    Implement schemaful merging reader

---
+++ yt/ytlib/new_table_client/schemaful_merging_reader.cpp
@@ -0,0 +1,113 @@
+#include "stdafx.h"
+#include "schemaful_merging_reader.h"
+#include "schemaful_reader.h"
+
+namespace NYT {
+namespace NVersionedTableClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TSchemafulMergingReader
+    : public ISchemafulReader
+{
+public:
+    explicit TSchemafulMergingReader(const std::vector<ISchemafulReaderPtr>& readers)
+    {
+        for (const auto& reader : readers) {
+            TSession session;
+            session.Reader = reader;
+            Sessions_.push_back(session);
+        }
+    }
+
+    virtual TAsyncError Open(const TTableSchema& schema) override
+    {
+        for (auto& session : Sessions_) {
+            session.ReadyEvent = session.Reader->Open(schema);
+        }
+        static auto result = MakeFuture(TError());
+        return result;
+    }
+
+    virtual bool Read(std::vector<TUnversionedRow>* rows) override
+    {
+        bool pending = false;
+
+        for (auto& session : Sessions_) {
+            if (session.Exhausted) {
+                continue;
+            }
+
+            if (session.ReadyEvent) {
+                if (!session.ReadyEvent.IsSet()) {
+                    pending = true;
+                    continue;
+                }
+
+                const auto& error = session.ReadyEvent.Get();
+                if (!error.IsOK()) {
+                    ReadyEvent_ = session.ReadyEvent;
+                    return true;
+                }
+
+                session.ReadyEvent.Reset();
+            }
+
+            if (!session.Reader->Read(rows)) {
+                session.Exhausted = true;
+                continue;
+            }
+
+            if (!rows->empty()) {
+                return true;
+            }
+
+            YASSERT(!session.ReadyEvent);
+            session.ReadyEvent = session.Reader->GetReadyEvent();
+            pending = true;
+        }
+
+        if (!pending) {
+            return false;
+        }
+
+        auto readyEvent = NewPromise<TError>();
+        for (auto& session : Sessions_) {
+            if (session.ReadyEvent) {
+                session.ReadyEvent.Subscribe(BIND([readyEvent] (TError error) mutable {
+                    readyEvent.TrySet(error);
+                }));
+            }
+        }
+        ReadyEvent_ = readyEvent;
+
+        return true;
+    }
+
+    virtual TAsyncError GetReadyEvent() override
+    {
+        return ReadyEvent_;
+    }
+
+private:
+    struct TSession
+    {
+        ISchemafulReaderPtr Reader;
+        TAsyncError ReadyEvent;
+        bool Exhausted = false;
+    };
+
+    std::vector<TSession> Sessions_;
+    TAsyncError ReadyEvent_;
+
+};
+
+ISchemafulReaderPtr CreateSchemafulMergingReader(const std::vector<ISchemafulReaderPtr>& readers)
+{
+    return New<TSchemafulMergingReader>(readers);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NVersionedTableClient
+} // namespace NYT
