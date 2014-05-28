76c34228a926c31662916f2ff36953224308108d
Pavel Sushin <psushin@ytsaurus.tech>
2014-05-28 19:42:33 +0400

    Sorted merge job, refactoring

--- yt/server/job_proxy/merge_job.cpp
+++ yt/server/job_proxy/merge_job.cpp
@@ -14,17 +14,13 @@
 
 #include <ytlib/transaction_client/public.h>
 
-#include <core/concurrency/scheduler.h>
-
 #include <core/ytree/yson_string.h>
 
 namespace NYT {
 namespace NJobProxy {
 
-using namespace NConcurrency;
 using namespace NChunkClient;
 using namespace NChunkClient::NProto;
-using namespace NJobTrackerClient::NProto;
 using namespace NScheduler::NProto;
 using namespace NTransactionClient;
 using namespace NVersionedTableClient;
@@ -32,28 +28,26 @@ using namespace NYTree;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static const auto& Logger = JobProxyLogger;
 static auto& Profiler = JobProxyProfiler;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 class TMergeJob
-    : public TJob
+    : public TSimpleJobBase
 {
 public:
     explicit TMergeJob(IJobHost* host, bool parallelReader)
-        : TJob(host)
-        , JobSpec_(host->GetJobSpec())
-        , SchedulerJobSpecExt_(JobSpec_.GetExtension(TSchedulerJobSpecExt::scheduler_job_spec_ext))
-        , TotalRowCount_(0)
+        : TSimpleJobBase(host)
     {
         auto config = host->GetConfig();
 
         YCHECK(SchedulerJobSpecExt_.output_specs_size() == 1);
 
+        TKeyColumns keyColumns;
+
         if (JobSpec_.HasExtension(TMergeJobSpecExt::merge_job_spec_ext)) {
             const auto& mergeJobSpec = JobSpec_.GetExtension(TMergeJobSpecExt::merge_job_spec_ext);
-            KeyColumns_ = FromProto<Stroka>(mergeJobSpec.key_columns());
+            keyColumns = FromProto<Stroka>(mergeJobSpec.key_columns());
             LOG_INFO("Ordered merge produces sorted output");
         }
 
@@ -82,7 +76,6 @@ public:
             nameTable,
             TKeyColumns());
 
-        // ToDo(psushin): estimate row count for writer.
         auto transactionId = FromProto<TTransactionId>(SchedulerJobSpecExt_.output_transaction_id());
         const auto& outputSpec = SchedulerJobSpecExt_.output_specs(0);
         auto chunkListId = FromProto<TChunkListId>(outputSpec.chunk_list_id());
@@ -92,100 +85,12 @@ public:
             config->JobIO->NewTableWriter,
             options,
             nameTable,
-            KeyColumns_,
+            keyColumns,
             host->GetMasterChannel(),
             transactionId,
             chunkListId,
             true); // Allow value reordering if key columns are present.
     }
-
-    virtual NJobTrackerClient::NProto::TJobResult Run() override
-    {
-        PROFILE_TIMING ("/merge_time") {
-            LOG_INFO("Initializing");
-
-            {
-                auto error = WaitFor(Reader_->Open());
-                THROW_ERROR_EXCEPTION_IF_FAILED(error);
-            } {
-                auto error = WaitFor(Writer_->Open());
-                THROW_ERROR_EXCEPTION_IF_FAILED(error);
-            }
-
-            PROFILE_TIMING_CHECKPOINT("init");
-
-            LOG_INFO("Merging");
-            {
-                std::vector<TUnversionedRow> rows;
-                rows.reserve(10000);
-
-                while (Reader_->Read(&rows)) {
-                    if (rows.empty()) {
-                        auto error = WaitFor(Reader_->GetReadyEvent());
-                        THROW_ERROR_EXCEPTION_IF_FAILED(error);
-                        continue;
-                    }
-
-                    if (!Writer_->Write(rows)) {
-                        auto error = WaitFor(Writer_->GetReadyEvent());
-                        THROW_ERROR_EXCEPTION_IF_FAILED(error);
-                    }
-                }
-
-                YCHECK(rows.empty());
-            }
-
-            PROFILE_TIMING_CHECKPOINT("merge");
-
-            LOG_INFO("Finalizing");
-            {
-                auto error = WaitFor(Writer_->Close());
-                THROW_ERROR_EXCEPTION_IF_FAILED(error);
-
-                TJobResult result;
-                ToProto(result.mutable_error(), TError());
-                return result;
-            }
-        }
-    }
-
-    virtual double GetProgress() const override
-    {
-        if (TotalRowCount_ == 0) {
-            LOG_WARNING("Job progress: empty total");
-            return 0;
-        } else {
-            i64 rowCount = Reader_->GetDataStatistics().row_count();
-            double progress = (double) rowCount / TotalRowCount_;
-            LOG_DEBUG("Job progress: %lf, read row count: %" PRId64, progress, rowCount);
-            return progress;
-        }
-    }
-
-    virtual std::vector<NChunkClient::TChunkId> GetFailedChunkIds() const override
-    {
-        return Reader_->GetFailedChunkIds();
-    }
-
-    virtual TJobStatistics GetStatistics() const override
-    {
-        TJobStatistics result;
-        result.set_time(GetElapsedTime().MilliSeconds());
-        ToProto(result.mutable_input(), Reader_->GetDataStatistics());
-        ToProto(result.add_output(), Writer_->GetDataStatistics());
-        return result;
-    }
-
-private:
-    const TJobSpec& JobSpec_;
-    const TSchedulerJobSpecExt& SchedulerJobSpecExt_;
-
-    ISchemalessMultiChunkReaderPtr Reader_;
-    ISchemalessMultiChunkWriterPtr Writer_;
-
-    TKeyColumns KeyColumns_;
-
-    i64 TotalRowCount_;
 
 };
 
