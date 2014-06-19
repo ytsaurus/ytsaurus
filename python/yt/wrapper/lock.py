7827a5c518ff702a4017505e4a0da9dd54c623bf
Veronika Ivannikova <veronikaiv@yandex-team.ru>
2014-06-19 17:14:28 +0400

    YT-275: cosmetics

--- python/yt/wrapper/lock.py
+++ python/yt/wrapper/lock.py
@@ -15,7 +15,7 @@ def lock(path, mode=None, waitable=False, wait_for=None, client=None):
 
     :param mode: (optional) blocking type ["snapshot", "shared" or "exclusive" (default)]
     :param waitable: (bool) wait for lock if node under blocking
-    :param wait_for: (int) wait interval in milliseconds. If timeout occurred, ``YtError`` raised
+    :param wait_for: (int) wait interval in milliseconds. If timeout occurred, `YtError` raised
     :return: taken lock id (YSON string) or ``None`` if lock was not taken.
 
     .. seealso:: `lock on wiki <https://wiki.yandex-team.ru/yt/userdoc/transactions#versionirovanieiloki>`_
