70c1f52308ad75c20eaea31ca79e4ad6396041ce
Pavel Sushin <psushin@ytsaurus.tech>
2014-06-25 18:14:25 +0400

    WIP

--- yt/ytlib/new_table_client/helpers.h
+++ yt/ytlib/new_table_client/helpers.h
@@ -11,11 +11,30 @@ namespace NVersionedTableClient {
 
 //////////////////////////////////////////////////////////////////////////////////
 
+class TTableOutput
+    : public TOutputStream
+{
+public:
+    TTableOutput(const NFormats::TFormat& format, NTableClient::TWritingTableConsumer* consumer);
+    ~TTableOutput() throw();
+
+private:
+    void DoWrite(const void* buf, size_t len);
+    void DoFinish();
+
+
+    NTableClient::TWritingTableConsumer* Consumer_;
+    std::unique_ptr<NFormats::IParser> Parser_;
+    bool IsParserValid_;
+
+};
+
+//////////////////////////////////////////////////////////////////////////////////
+
 void ReadToWriter(ISchemalessReaderPtr reader, ISchemalessWriterPtr writer, int rowBufferSize);
 
-void ReadToConsumer(
-    const NFormats::TFormat& format,
-    NTableClient::TWritingTableConsumer* consumer,
+void ReadToOutputStream(
+    TOutputStream* output,
     TInputStream* input,
     int readBufferSize);
 
