ac4cb349ac6bbd4e711f235fac658a118531bd83
Maxim Babenko <babenko@ytsaurus.tech>
2014-07-07 12:52:15 +0400

    New Format utility with unittests

---
+++ yt/core/misc/format-inl.h
@@ -0,0 +1,370 @@
+#ifndef FORMAT_INL_H_
+#error "Direct inclusion of this file is not allowed, include format.h"
+#endif
+#undef FORMAT_INL_H_
+
+#include "mpl.h"
+#include "string.h"
+#include "nullable.h"
+#include "enum.h"
+#include "guid.h"
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+const char GenericSpecSymbol = 'v';
+
+template <class TValue>
+void FormatValue(TStringBuilder* builder, const TValue& value, const TStringBuf& format);
+
+#ifdef __GNUC__
+// Catch attempts to format 128-bit numbers early.
+void FormatValue(TStringBuilder* builder, __int128 value, const TStringBuf& format) YDEPRECATED;
+//void FormatValue(TStringBuilder* builder, __uint128 value, const TStringBuf& format) YDEPRECATED;
+#endif
+
+// TStringBuf
+inline void FormatValue(TStringBuilder* builder, const TStringBuf& value, const TStringBuf& format)
+{
+    builder->AppendString(value);
+}
+
+// Stroka
+inline void FormatValue(TStringBuilder* builder, const Stroka& value, const TStringBuf& format)
+{
+    FormatValue(builder, TStringBuf(value), format);
+}
+
+// const char*
+inline void FormatValue(TStringBuilder* builder, const char* value, const TStringBuf& format)
+{
+    FormatValue(builder, TStringBuf(value), format);
+}
+
+// bool
+inline void FormatValue(TStringBuilder* builder, bool value, const TStringBuf& format)
+{
+    // Parse custom flags.
+    bool lowercase = false;
+    const char* current = format.begin();
+    while (current != format.end()) {
+        if (*current == 'l') {
+            ++current;
+            lowercase = true;
+        } else
+            break;
+    }
+
+    auto str = lowercase
+        ? (value ? STRINGBUF("true") : STRINGBUF("false"))
+        : (value ? STRINGBUF("True") : STRINGBUF("False"));
+
+    builder->AppendString(str);
+}
+
+// Default (via ToString).
+template <class TValue, class = void>
+struct TValueFormatter
+{
+    static void Do(TStringBuilder* builder, const TValue& value, const TStringBuf& format)
+    {
+        using ::ToString;
+        FormatValue(builder, ToString(value), format);
+    }
+};
+
+// Enums
+template <class TEnum>
+struct TValueFormatter<
+    TEnum,
+    typename NMpl::TEnableIf<
+        NMpl::TIsConvertible<TEnum&, TEnumBase<TEnum>&>
+    >::TType>
+{
+    static void Do(TStringBuilder* builder, TEnum value, const TStringBuf& format)
+    {
+        // Obtain literal.
+        int rawValue = static_cast<int>(value);
+        auto* literal = TEnum::GetLiteralByValue(rawValue);
+        if (!literal) {
+            Format(builder, "%v(%v)", TEnum::GetTypeName(), rawValue);
+            return;
+        }
+
+        // Parse custom flags.
+        bool lowercase = false;
+        const char* current = format.begin();
+        while (current != format.end()) {
+            if (*current == 'l') {
+                ++current;
+                lowercase = true;
+            } else
+                break;
+        }
+
+        if (lowercase) {
+            CamelCaseToUnderscoreCase(builder, *literal);
+        } else {
+            builder->AppendString(*literal);
+        }
+    }
+};
+
+// Pointers
+template <class T>
+void FormatValue(TStringBuilder* builder, T* value, const TStringBuf& format)
+{
+    FormatValueStd(builder, value, format, STRINGBUF("p"));
+}
+
+// TGuid (specialize for performance reasons)
+inline void FormatValue(TStringBuilder* builder, const TGuid& value, const TStringBuf& /*format*/)
+{
+    char buf[4 + 4 * 8];
+    sprintf(buf, "%x-%x-%x-%x", value.Parts[3], value.Parts[2], value.Parts[1], value.Parts[0]);
+    builder->AppendString(buf);
+}
+
+// TNullable
+inline void FormatValue(TStringBuilder* builder, TNull, const TStringBuf& /*format*/)
+{
+    builder->AppendString("<null>");
+}
+
+template <class T>
+struct TValueFormatter<TNullable<T>>
+{
+    static void Do(TStringBuilder* builder, const TNullable<T>& value, const TStringBuf& format)
+    {
+        if (value) {
+            FormatValue(builder, *value, format);
+        } else {
+            FormatValue(builder, Null, format);
+        }
+    }
+};
+
+template <class TValue>
+void FormatValue(TStringBuilder* builder, const TValue& value, const TStringBuf& format)
+{
+    TValueFormatter<TValue>::Do(builder, value, format);
+}
+
+template <class TValue>
+void FormatValueStd(TStringBuilder* builder, TValue value, const TStringBuf& format, const TStringBuf& genericSpec)
+{
+    const int MaxFormatSize = 64;
+    const int MaxResultSize = 64;
+
+    char formatBuf[MaxFormatSize];
+    YCHECK(format.length() >= 1 && format.length() <= MaxFormatSize - 2); // one for %, one for \0
+    formatBuf[0] = '%';
+    if (format[format.length() - 1] == GenericSpecSymbol) {
+        memcpy(formatBuf + 1, format.begin(), format.length() - 1);
+        memcpy(formatBuf + format.length(), genericSpec.begin(), genericSpec.length());
+        formatBuf[format.length() + genericSpec.length()] = '\0';
+    } else {
+        memcpy(formatBuf + 1, format.begin(), format.length());
+        formatBuf[format.length() + 1] = '\0';
+    }
+
+    char* result = builder->Preallocate(MaxResultSize);
+    size_t resultSize = snprintf(result, MaxResultSize, formatBuf, value);
+    builder->Advance(resultSize);
+}
+
+#define IMPLEMENT_FORMAT_VALUE_STD(valueType, castType, genericSpec) \
+    inline void FormatValue(TStringBuilder* builder, valueType value, const TStringBuf& format) \
+    { \
+        FormatValueStd(builder, static_cast<castType>(value), format, genericSpec); \
+    }
+
+IMPLEMENT_FORMAT_VALUE_STD(char,            char,               STRINGBUF("c"))
+IMPLEMENT_FORMAT_VALUE_STD(i8,              int,                STRINGBUF("d"))
+IMPLEMENT_FORMAT_VALUE_STD(ui8,             unsigned int,       STRINGBUF("u"))
+IMPLEMENT_FORMAT_VALUE_STD(i16,             int,                STRINGBUF("d"))
+IMPLEMENT_FORMAT_VALUE_STD(ui16,            unsigned int,       STRINGBUF("u"))
+IMPLEMENT_FORMAT_VALUE_STD(i32,             int,                STRINGBUF("d"))
+IMPLEMENT_FORMAT_VALUE_STD(ui32,            unsigned int,       STRINGBUF("u"))
+IMPLEMENT_FORMAT_VALUE_STD(long,            long,               STRINGBUF("ld"))
+IMPLEMENT_FORMAT_VALUE_STD(unsigned long,   unsigned long,      STRINGBUF("lu"))
+#ifdef _win_
+IMPLEMENT_FORMAT_VALUE_STD(i64,             i64,                STRINGBUF(PRId64))
+IMPLEMENT_FORMAT_VALUE_STD(ui64,            ui64,               STRINGBUF(PRIu64))
+#endif
+IMPLEMENT_FORMAT_VALUE_STD(double,          double,             STRINGBUF("lf"))
+IMPLEMENT_FORMAT_VALUE_STD(float,           float,              STRINGBUF("f"))
+
+#undef IMPLEMENT_STD_FORMAT_VALUE
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TArgFormatter>
+void FormatImpl(
+    TStringBuilder* builder,
+    const char* format,
+    const TArgFormatter& argFormatter)
+{
+    size_t argIndex = 0;
+    const char* current = format;
+    while (true) {
+        // Scan verbatim part until stop symbol.
+        const char* verbatimBegin = current;
+        const char* verbatimEnd = verbatimBegin;
+        while (*verbatimEnd != '\0' && *verbatimEnd != '%') {
+            ++verbatimEnd;
+        }
+
+        // Copy verbatim part, if any.
+        size_t verbatimSize = verbatimEnd - verbatimBegin;
+        if (verbatimSize > 0) {
+            builder->AppendString(TStringBuf(verbatimBegin, verbatimSize));
+        }
+
+        // Handle stop symbol.
+        current = verbatimEnd;
+        if (*current == '\0')
+            break;
+
+        YASSERT(*current == '%');
+        ++current;
+
+        if (*current == '%') {
+            // Verbatim %.
+            builder->AppendChar('%');
+            ++current;
+        } else {
+            // Scan format part until stop symbol.
+            const char* argFormatBegin = current;
+            const char* argFormatEnd = argFormatBegin;
+            while (
+                *argFormatEnd != '\0' &&                  // end of format string
+                *argFormatEnd != GenericSpecSymbol &&     // value in generic format
+                *argFormatEnd != 'd' &&                   // others are standard specifiers supported by printf
+                *argFormatEnd != 'i' &&
+                *argFormatEnd != 'u' &&
+                *argFormatEnd != 'o' &&
+                *argFormatEnd != 'x' &&
+                *argFormatEnd != 'X' &&
+                *argFormatEnd != 'f' &&
+                *argFormatEnd != 'F' &&
+                *argFormatEnd != 'e' &&
+                *argFormatEnd != 'E' &&
+                *argFormatEnd != 'g' &&
+                *argFormatEnd != 'G' &&
+                *argFormatEnd != 'a' &&
+                *argFormatEnd != 'A' &&
+                *argFormatEnd != 'c' &&
+                *argFormatEnd != 's' &&
+                *argFormatEnd != 'p' &&
+                *argFormatEnd != 'n')
+            {
+                ++argFormatEnd;
+            }
+
+            // Handle end of format string.
+            if (*argFormatEnd != '\0') {
+                ++argFormatEnd;
+            }
+
+            // 'n' means 'nothing'; skip the argument.
+            if (*argFormatBegin != 'n') {
+                // Parse custom flags.
+                bool singleQuotes = false;
+                bool doubleQuotes = false;
+                while (argFormatBegin != argFormatEnd) {
+                    if (*argFormatBegin == 'q') {
+                        ++argFormatBegin;
+                        singleQuotes = true;
+                    } else if (*argFormatBegin == 'Q') {
+                        ++argFormatBegin;
+                        doubleQuotes = true;
+                    } else
+                        break;
+                }
+
+                // Format argument.
+                TStringBuf argFormat(argFormatBegin, argFormatEnd);
+                if (singleQuotes) {
+                    builder->AppendChar('\'');
+                }
+                if (doubleQuotes) {
+                    builder->AppendChar('"');
+                }
+                argFormatter(argIndex++, builder, argFormat);
+                if (singleQuotes) {
+                    builder->AppendChar('\'');
+                }
+                if (doubleQuotes) {
+                    builder->AppendChar('"');
+                }
+            }
+
+
+            current = argFormatEnd;
+        }
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <size_t IndexBase, class... TArgs>
+struct TArgFormatterImpl;
+
+template <size_t IndexBase>
+struct TArgFormatterImpl<IndexBase>
+{
+    void operator() (size_t index, TStringBuilder* builder, const TStringBuf& /*format*/) const
+    {
+        builder->AppendString(STRINGBUF("<missing argument>"));
+    }
+};
+
+template <size_t IndexBase, class THeadArg, class... TTailArgs>
+struct TArgFormatterImpl<IndexBase, THeadArg, TTailArgs...>
+{
+    explicit TArgFormatterImpl(const THeadArg& headArg, const TTailArgs&... tailArgs)
+        : HeadArg(headArg)
+        , TailFormatter(tailArgs...)
+    { }
+
+    const THeadArg& HeadArg;
+    TArgFormatterImpl<IndexBase + 1, TTailArgs...> TailFormatter;
+
+    void operator() (size_t index, TStringBuilder* builder, const TStringBuf& format) const
+    {
+        YASSERT(index >= IndexBase);
+        if (index == IndexBase) {
+            FormatValue(builder, HeadArg, format);
+        } else {
+            TailFormatter(index, builder, format);
+        }
+    }
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class... TArgs>
+void Format(
+    TStringBuilder* builder,
+    const char* format,
+    const TArgs&... args)
+{
+    TArgFormatterImpl<0, TArgs...> argFormatter(args...);
+    FormatImpl(builder, format, argFormatter);
+}
+
+template <class... TArgs>
+Stroka Format(
+    const char* format,
+    const TArgs&... args)
+{
+    TStringBuilder builder;
+    Format(&builder, format, args...);
+    return builder.Flush();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
---
+++ yt/unittests/format_ut.cpp
@@ -0,0 +1,117 @@
+#include "stdafx.h"
+#include "framework.h"
+
+#include <core/misc/format.h>
+
+namespace NYT {
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TFormatTest, Nothing)
+{
+    EXPECT_EQ("abc", Format("a%nb%nc", 1, 2));
+}
+
+TEST(TFormatTest, Verbatim)
+{
+    EXPECT_EQ("", Format(""));
+    EXPECT_EQ("test", Format("test"));
+    EXPECT_EQ("%", Format("%%"));
+    EXPECT_EQ("%hello%world%", Format("%%hello%%world%%"));
+}
+
+TEST(TFormatTest, MultipleArgs)
+{
+    EXPECT_EQ("2+2=4", Format("%v+%v=%v", 2, 2, 4));
+}
+
+TEST(TFormatTest, Strings)
+{
+    EXPECT_EQ("test", Format("%s", "test"));
+    EXPECT_EQ("test", Format("%s", STRINGBUF("test")));
+    EXPECT_EQ("test", Format("%s", Stroka("test")));
+}
+
+TEST(TFormatTest, Integers)
+{
+    EXPECT_EQ("123", Format("%d", 123));
+    EXPECT_EQ("123", Format("%v", 123));
+
+    EXPECT_EQ("2147483647", Format("%d", std::numeric_limits<i32>::max()));
+    EXPECT_EQ("-2147483648", Format("%d", std::numeric_limits<i32>::min()));
+
+    EXPECT_EQ("0", Format("%u", 0U));
+    EXPECT_EQ("0", Format("%v", 0U));
+    EXPECT_EQ("4294967295", Format("%u", std::numeric_limits<ui32>::max()));
+    EXPECT_EQ("4294967295", Format("%v", std::numeric_limits<ui32>::max()));
+
+    EXPECT_EQ("9223372036854775807", Format("%" PRId64, std::numeric_limits<i64>::max()));
+    EXPECT_EQ("9223372036854775807", Format("%v", std::numeric_limits<i64>::max()));
+    EXPECT_EQ("-9223372036854775808", Format("%" PRId64, std::numeric_limits<i64>::min()));
+    EXPECT_EQ("-9223372036854775808", Format("%v", std::numeric_limits<i64>::min()));
+
+    EXPECT_EQ("0", Format("%" PRIu64, 0ULL));
+    EXPECT_EQ("0", Format("%v", 0ULL));
+    EXPECT_EQ("18446744073709551615", Format("%" PRIu64, std::numeric_limits<ui64>::max()));
+    EXPECT_EQ("18446744073709551615", Format("%v", std::numeric_limits<ui64>::max()));
+}
+
+TEST(TFormatTest, Floats)
+{
+    EXPECT_EQ("3.14", Format("%.2f", 3.1415F));
+    EXPECT_EQ("3.14", Format("%.2v", 3.1415F));
+    EXPECT_EQ("3.14", Format("%.2lf", 3.1415));
+    EXPECT_EQ("3.14", Format("%.2v", 3.1415));
+}
+
+DECLARE_ENUM(EColor,
+    (Red)
+    (BlackAndWhite)
+);
+
+TEST(TFormatTest, Enum)
+{
+    EXPECT_EQ("Red", Format("%v", EColor(EColor::Red)));
+    EXPECT_EQ("red", Format("%lv", EColor(EColor::Red)));
+
+    EXPECT_EQ("BlackAndWhite", Format("%v", EColor(EColor::BlackAndWhite)));
+    EXPECT_EQ("black_and_white", Format("%lv", EColor(EColor::BlackAndWhite)));
+
+    EXPECT_EQ("EColor(100)", Format("%v", EColor(100)));
+}
+
+TEST(TFormatTest, Bool)
+{
+    EXPECT_EQ("True", Format("%v", true));
+    EXPECT_EQ("False", Format("%v", false));
+    EXPECT_EQ("true", Format("%lv", true));
+    EXPECT_EQ("false", Format("%lv", false));
+}
+
+TEST(TFormatTest, Quotes)
+{
+    EXPECT_EQ("\"True\"", Format("%Qv", true));
+    EXPECT_EQ("'False'", Format("%qv", false));
+}
+
+TEST(TFormatTest, Nullable)
+{
+    EXPECT_EQ("1", Format("%v", MakeNullable<int>(1)));
+    EXPECT_EQ("<null>", Format("%v", Null));
+    EXPECT_EQ("<null>", Format("%v", TNullable<int>()));
+    EXPECT_EQ("3.14", Format("%.2f", TNullable<double>(3.1415)));
+}
+
+TEST(TFormatTest, Pointers)
+{
+    // No idea if pointer format is standardized, check against Sprintf.
+    auto p = reinterpret_cast<void*>(123);
+    EXPECT_EQ(Sprintf("%p", reinterpret_cast<void*>(123)), Format("%p", p));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT
+
