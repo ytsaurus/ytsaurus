36d8325cf60f688349f205b65f44d4dd9672aef9
Maxim Babenko <babenko@ytsaurus.tech>
2014-07-09 23:40:31 +0400

    Use RemoveChangelogFiles instead of adhoc calls

--- yt/server/hydra/file_changelog_dispatcher.cpp
+++ yt/server/hydra/file_changelog_dispatcher.cpp
@@ -478,8 +478,7 @@ public:
 
         changelog->Close();
 
-        NFS::Remove(path);
-        NFS::Remove(path + "." + ChangelogIndexExtension);
+        RemoveChangelogFiles(path);
     }
 
 private:
