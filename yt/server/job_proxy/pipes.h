b944cd020b919f9168a586f6ee64a89eb724a434
Aleksandr Primak <tramsmm@yandex-team.ru>
2014-07-11 21:24:44 +0400

    Remove dead code

--- yt/server/job_proxy/pipes.h
+++ yt/server/job_proxy/pipes.h
@@ -26,24 +26,6 @@ void CheckJobDescriptor(int fd);
 
 ////////////////////////////////////////////////////////////////////
 
-struct TPipe
-{
-    int ReadFd;
-    int WriteFd;
-
-    TPipe(int fd[2])
-        : ReadFd(fd[0])
-        , WriteFd(fd[1])
-    { }
-
-    TPipe()
-        : ReadFd(-1)
-        , WriteFd(-1)
-    { }
-};
-
-////////////////////////////////////////////////////////////////////
-
 struct IDataPipe
     : public virtual TRefCounted
 {
