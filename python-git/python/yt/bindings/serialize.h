4b8cc345d73002dbb4ad9fa319fcc2344701a632
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-07-16 14:11:23 +0400

    Add boolean type to yson. Cosmetics.

--- python-git/python/yt/bindings/serialize.h
+++ python-git/python/yt/bindings/serialize.h
@@ -30,6 +30,7 @@ public:
     virtual void OnStringScalar(const TStringBuf& value) override;
     virtual void OnInt64Scalar(i64 value) override;
     virtual void OnDoubleScalar(double value) override;
+    virtual void OnBooleanScalar(bool value) override;
     virtual void OnEntity() override;
     virtual void OnBeginList() override;
     virtual void OnListItem() override;
@@ -55,6 +56,7 @@ private:
     Py::Callable YsonString;
     Py::Callable YsonInt64;
     Py::Callable YsonDouble;
+    Py::Callable YsonBoolean;
     Py::Callable YsonEntity;
 
     std::queue<Py::Object> Objects_;
