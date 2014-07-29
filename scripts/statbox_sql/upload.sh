eef82bf2f37986c2ec793f2105292f57cbf5f4f0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-07-29 14:20:45 +0400

    [statface sql] Fix upload.sh script

--- scripts/statbox_sql/upload.sh
+++ scripts/statbox_sql/upload.sh
@@ -1,11 +1,27 @@
 #!/bin/sh -eu
 
+#export YT_CONFIG="./ytdriver.conf"
+
+output_table="$1"
+
+portion_number=0
 index=0
+
+insert() {
+    portion_number=$(($portion_number + 1))
+    echo "Portion number $portion_number" >&2
+    cat file | yt insert "$output_table" --format yson
+    rm -rf file
+    index=0
+}
+
 for line in $(cat | grep -v "#;$"); do
     index=$((index + 1))
     echo "$line" >>file
-    if [ "$(($index % 1000))" = "0" ]; then
-        cat file | yt insert "$1" --format yson &
+    if [ "$index" -gt "1000" ] && [[ "$line" == *";" ]]; then
+        insert
     fi
 done
+
+insert
 
