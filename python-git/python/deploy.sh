890d4997758603f83671fa3c61cf517a543e77d3
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-09-12 15:28:21 +0400

    Move pyinstaller to submodules. Simplify deploy.sh

--- python-git/python/deploy.sh
+++ python-git/python/deploy.sh
@@ -2,67 +2,48 @@
 
 PACKAGE=$1
 
-rm -rf debian setup.py docs
-
+# Copy package files to the python root
 # NB: Symbolic links doesn't work correctly with `sdist upload`
 cp -r $PACKAGE/debian $PACKAGE/setup.py .
 if [ -f "$PACKAGE/MANIFEST.in" ]; then
     cp $PACKAGE/MANIFEST.in .
 fi
 
+
+# Initial cleanup
+rm -rf docs/
 python setup.py clean
 sudo make -f debian/rules clean
 
-# Build and upload package
-make deb_without_test
 
+# Build debian package
+DEB=1 python setup.py sdist --dist-dir=../
+dpkg-buildpackage -i -I -rfakeroot
+
+# Upload debian package
 if [ "$PACKAGE" = "yandex-yt-python-yson" ]; then
     REPO="precise"
+elif [ "$PACKAGE" = "yandex-yt-python-fennel" ] || [ "$PACKAGE" = "yandex-yt-transfer-manager" ]; then
+    REPO="yt-common"
 else
-    if [ "$PACKAGE" = "yandex-yt-python-fennel" ] || [ "$PACKAGE" = "yandex-yt-transfer-manager" ]; then
-        REPO="yt-common"
-    else
-        REPO="common"
-    fi
+    REPO="common"
 fi
 
 VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
 dupload "../${PACKAGE}_${VERSION}_amd64.changes" --to $REPO
 
-if [ "$PACKAGE" = "yandex-yt-python-yson" ]; then
-    python setup.py bdist_wheel upload -r yandex
+
+# Upload python wheel
+python setup.py bdist_wheel upload -r yandex
+
+
+# Some postprocess steps
+if [ -f "$PACKAGE/postprocess.sh" ]; then
+    $PACKAGE/postprocess.sh
 fi
 
-if [ "$PACKAGE" = "yandex-yt-python" ]; then
-    # Upload egg
-    export YT_PROXY=kant.yt.yandex.net
-    DEST="//home/files"
 
-    python setup.py bdist_wheel upload -r yandex
-
-    YT="yt/wrapper/yt"
-
-    make egg
-    EGG_VERSION=$(echo $VERSION | tr '-' '_')
-    eggname="yandex_yt-${EGG_VERSION}-py2.7.egg"
-    cat dist/$eggname | $YT upload "$DEST/yandex_yt-${VERSION}-py2.7.egg"
-    cat dist/$eggname | $YT upload "$DEST/yandex_yt-py2.7.egg"
-    
-    # Upload self-contained binaries
-    mv yt/wrapper/pickling.py pickling.py
-    cp standard_pickling.py yt/wrapper/pickling.py
-    
-    for name in yt mapreduce-yt; do
-        rm -rf build dist
-        pyinstaller/pyinstaller.py --noconfirm --onefile yt/wrapper/$name
-        pyinstaller/pyinstaller.py --noconfirm "${name}.spec"
-        cat dist/$name | $YT upload "$DEST/${name}_${VERSION}"
-        cat dist/$name | $YT upload "$DEST/$name"
-    done
-    
-    mv pickling.py yt/wrapper/pickling.py
-fi
-
+# Final cleanup
 python setup.py clean
 sudo make -f debian/rules clean
-rm -rf debian setup.py VERSION MANIFEST.in
+rm -rf debian setup.py MANIFEST.in
---
+++ python-git/python/yandex-yt-python/postprocess.sh
@@ -0,0 +1,21 @@
+#!/bin/bash -eux
+# Build and upload self-contained binaries
+
+export YT_PROXY=kant.yt.yandex.net
+DEST="//home/files"
+YT="yt/wrapper/yt"
+
+# Upload self-contained binaries
+mv yt/wrapper/pickling.py pickling.py
+cp standard_pickling.py yt/wrapper/pickling.py
+
+for name in yt mapreduce-yt; do
+    rm -rf build dist
+    pyinstaller/pyinstaller.py --noconfirm --onefile yt/wrapper/$name
+    pyinstaller/pyinstaller.py --noconfirm "${name}.spec"
+    VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
+    cat dist/$name | $YT upload "$DEST/${name}_${VERSION}"
+    cat dist/$name | $YT upload "$DEST/$name"
+done
+
+mv pickling.py yt/wrapper/pickling.py
