ab495108065843d9c5a4cebc98fe42da9b0bdd45
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-09-20 21:59:43 +0400

    Add minimal-age to merge

--- scripts/cron/run_merge.sh
+++ scripts/cron/run_merge.sh
@@ -2,7 +2,7 @@
 
 QUEUE="//sys/cron/tables_to_merge"
 
-/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE
+/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimal-age 36
 
 /opt/cron/tools/run_parallel.sh "/opt/cron/tools/merge.py $QUEUE" 40 "/opt/cron/merging_log_$YT_PROXY"
 
