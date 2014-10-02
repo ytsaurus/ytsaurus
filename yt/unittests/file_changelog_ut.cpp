a34b91eba21f393f467ebb186945b4814123e4f8
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-10-02 13:39:21 +0400

    Return file changelog tests

--- yt/unittests/file_changelog_ut.cpp
+++ yt/unittests/file_changelog_ut.cpp
@@ -1,42 +1,49 @@
 #include "stdafx.h"
 #include "framework.h"
 
-#include <ytlib/meta_state/async_change_log.h>
-#include <ytlib/meta_state/change_log.h>
+#include <server/hydra/config.h>
+#include <server/hydra/changelog.h>
+#include <server/hydra/local_changelog_store.h>
+
+#include <core/concurrency/action_queue.h>
+
+#include <core/misc/fs.h>
 
 #include <util/random/random.h>
 #include <util/system/tempfile.h>
 
 namespace NYT {
-namespace NMetaState {
+namespace NHydra {
 namespace {
 
 using namespace NConcurrency;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TAsyncChangeLogTest
+class TFileChangelogTest
     : public ::testing::Test
 {
 protected:
-    std::unique_ptr<TTempFile> TemporaryFile;
-    std::unique_ptr<TTempFile> TemporaryIndexFile;
-
-    TChangeLogPtr ChangeLog;
-    std::unique_ptr<TAsyncChangeLog> AsyncChangeLog;
+    TFileChangelogStoreConfigPtr ChangelogStoreConfig;
+    IChangelogStorePtr ChangelogStore;
+    IChangelogPtr Changelog;
 
     TActionQueuePtr ActionQueue;
     IInvokerPtr Invoker;
 
     virtual void SetUp()
     {
-        TemporaryFile.reset(new TTempFile(GenerateRandomFileName("AsyncChangeLog")));
-        TemporaryIndexFile.reset(new TTempFile(TemporaryFile->Name() + ".index"));
+        ChangelogStoreConfig = New<TFileChangelogStoreConfig>();
+        ChangelogStoreConfig->Path = "FileChangelog";
 
-        ChangeLog = New<TChangeLog>(TemporaryFile->Name(), 0, /*index block size*/ 64);
-        ChangeLog->Create(0, TEpochId());
+        ChangelogStore = CreateLocalChangelogStore("changelog_thread", ChangelogStoreConfig);
 
-        AsyncChangeLog.reset(new TAsyncChangeLog(ChangeLog));
+        TSharedRef ref;
+        auto changelogOrError = ChangelogStore->CreateChangelog(0, ref).Get();
+
+        ASSERT_TRUE(changelogOrError.IsOK());
+
+        Changelog = changelogOrError.Value();
 
         ActionQueue = New<TActionQueue>();
         Invoker = ActionQueue->GetInvoker();
@@ -44,13 +51,9 @@ protected:
 
     virtual void TearDown()
     {
-        NFS::Remove(TemporaryFile->Name());
-        NFS::Remove(TemporaryIndexFile->Name());
+        NFS::RemoveRecursive(ChangelogStoreConfig->Path);
     }
-
 };
-
-namespace {
 
 static void CheckRecord(i32 data, const TSharedRef& record)
 {
@@ -58,11 +61,9 @@ static void CheckRecord(i32 data, const TSharedRef& record)
     EXPECT_EQ(       data , *(reinterpret_cast<const i32*>(record.Begin())));
 }
 
-void ReadRecord(TAsyncChangeLog* asyncChangeLog, i32 recordIndex)
+void ReadRecord(IChangelog* asyncChangeLog, i32 recordIndex)
 {
-    std::vector<TSharedRef> result;
-    result.clear();
-    asyncChangeLog->Read(recordIndex, 1, std::numeric_limits<i64>::max(), &result);
+    std::vector<TSharedRef> result = asyncChangeLog->Read(recordIndex, 1, std::numeric_limits<i64>::max());
     EXPECT_EQ(1, result.size());
     CheckRecord(recordIndex, result[0]);
 }
@@ -74,34 +75,34 @@ TSharedRef MakeData(i32 data)
     return result;
 }
 
-} // namespace
+TEST_F(TFileChangelogTest, Empty)
+{ }
 
-TEST_F(TAsyncChangeLogTest, ReadTrailingRecords)
+TEST_F(TFileChangelogTest, ReadTrailingRecords)
 {
     const int recordCount = 10000;
     TFuture<void> readResult;
     for (int recordIndex = 0; recordIndex < recordCount; ++recordIndex) {
-        auto flushResult = AsyncChangeLog->Append(recordIndex, MakeData(recordIndex));
+        auto flushResult = Changelog->Append(MakeData(recordIndex));
         if (recordIndex % 1000 == 0) {
             flushResult.Get();
         }
         if (recordIndex % 10 == 0) {
-            readResult = BIND(&ReadRecord, ~AsyncChangeLog, recordIndex).AsyncVia(Invoker).Run();
+            readResult = BIND(&ReadRecord, Changelog.Get(), recordIndex).AsyncVia(Invoker).Run();
         }
     }
     readResult.Get();
 }
 
-TEST_F(TAsyncChangeLogTest, ReadWithSizeLimit)
+TEST_F(TFileChangelogTest, ReadWithSizeLimit)
 {
     for (int recordIndex = 0; recordIndex < 40; ++recordIndex) {
-        AsyncChangeLog->Append(recordIndex, MakeData(recordIndex));
+        Changelog->Append(MakeData(recordIndex));
     }
 
     auto check = [&] (int maxSize) {
-        std::vector<TSharedRef> records;
-        AsyncChangeLog->Read(0, 1000, maxSize, &records);
-        EXPECT_EQ(records.size(), maxSize / sizeof(i32) + 1);
+        std::vector<TSharedRef> records = Changelog->Read(0, 1000, maxSize);
+        EXPECT_EQ(records.size(), (maxSize - 1) / sizeof(i32) + 1);
         for (int recordIndex = 0; recordIndex < static_cast<int>(records.size()); ++recordIndex) {
             CheckRecord(recordIndex, records[recordIndex]);
         }
