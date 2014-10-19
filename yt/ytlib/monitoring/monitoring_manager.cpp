ac61a486cfc8b69dfe5588337fe566a616e71ff3
Maxim Babenko <babenko@ytsaurus.tech>
2014-10-19 17:58:37 +0400

    Tidy up TMonitoringManager and also fix a race

--- yt/ytlib/monitoring/monitoring_manager.cpp
+++ yt/ytlib/monitoring/monitoring_manager.cpp
@@ -1,117 +1,169 @@
 #include "stdafx.h"
 #include "monitoring_manager.h"
+#include "private.h"
 
-#include <core/actions/bind.h>
-
-#include <core/yson/writer.h>
+#include <core/concurrency/action_queue.h>
+#include <core/concurrency/periodic_executor.h>
 
 #include <core/ytree/ephemeral_node_factory.h>
 #include <core/ytree/ypath_detail.h>
 #include <core/ytree/tree_visitor.h>
-#include <core/ytree/ypath_proxy.h>
 #include <core/ytree/node.h>
 #include <core/ytree/convert.h>
-
-#include <core/profiling/profiler.h>
 
 namespace NYT {
 namespace NMonitoring {
 
 using namespace NYTree;
+using namespace NYPath;
+using namespace NYson;
 using namespace NRpc;
 using namespace NConcurrency;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-static NLog::TLogger Logger("Monitoring");
-static NProfiling::TProfiler Profiler("/monitoring");
+static auto& Logger = MonitoringLogger;
+static auto& Profiler = MonitoringProfiler;
 
-static const TDuration UpdatePeriod = TDuration::Seconds(3);
+static const auto UpdatePeriod = TDuration::Seconds(3);
+static const auto EmptyRoot = GetEphemeralNodeFactory()->CreateMap();
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TMonitoringManager::TYPathService
-    : public TYPathServiceBase
+class TMonitoringManager::TImpl
+    : public TRefCounted
 {
 public:
-    explicit TYPathService(TMonitoringManagerPtr owner)
-        : Owner(owner)
-    { }
-
-    virtual TResolveResult Resolve(const TYPath& path, IServiceContextPtr context) override
+    void Register(const TYPath& path, TYsonProducer producer)
     {
-        return TResolveResult::There(Owner->Root, path);
+        TGuard<TSpinLock> guard(SpinLock_);
+        YCHECK(PathToProducer_.insert(std::make_pair(path, producer)).second);
+    }
+
+    void Unregister(const TYPath& path)
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+        YCHECK(PathToProducer_.erase(path) == 1);
+    }
+
+    IYPathServicePtr GetService()
+    {
+        return New<TYPathService>(this);
+    }
+
+    void Start()
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+
+        YCHECK(!Started_);
+
+        PeriodicExecutor_ = New<TPeriodicExecutor>(
+            ActionQueue_->GetInvoker(),
+            BIND(&TImpl::Update, MakeWeak(this)),
+            UpdatePeriod);
+        PeriodicExecutor_->Start();
+
+        Started_ = true;
+    }
+
+    void Stop()
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+
+        if (!Started_)
+            return;
+
+        Started_ = false;
+        PeriodicExecutor_->Stop();
+        Root_.Reset();
     }
 
 private:
-    TMonitoringManagerPtr Owner;
+    class TYPathService
+        : public TYPathServiceBase
+    {
+    public:
+        explicit TYPathService(TIntrusivePtr<TImpl> owner)
+            : Owner_(owner)
+        { }
+
+        virtual TResolveResult Resolve(const TYPath& path, IServiceContextPtr context) override
+        {
+            return TResolveResult::There(Owner_->GetRoot(), path);
+        }
+
+    private:
+        TIntrusivePtr<TImpl> Owner_;
+
+    };
+
+    bool Started_ = false;
+    TActionQueuePtr ActionQueue_ = New<TActionQueue>("Monitoring");
+    TPeriodicExecutorPtr PeriodicExecutor_;
+
+    TSpinLock SpinLock_;
+    yhash<Stroka, NYTree::TYsonProducer> PathToProducer_;
+    INodePtr Root_;
+
+    void Update()
+    {
+        LOG_DEBUG("Started updating monitoring state");
+        PROFILE_TIMING ("/update_time") {
+            auto newRoot = GetEphemeralNodeFactory()->CreateMap();
+
+            for (const auto& pair : PathToProducer_) {
+                auto value = ConvertToYsonString(pair.second);
+                SyncYPathSet(newRoot, pair.first, value);
+            }
+
+            if (Started_) {
+                TGuard<TSpinLock> guard(SpinLock_);
+                Root_ = newRoot;
+            }
+        }
+        LOG_DEBUG("Finished updating monitoring state");
+    }
+
+    INodePtr GetRoot()
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+        return Root_ ? Root_ : EmptyRoot;
+    }
 
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
 TMonitoringManager::TMonitoringManager()
-    : IsStarted(false)
-    , ActionQueue(New<TActionQueue>("Monitoring"))
+    : Impl_(New<TImpl>())
+{ }
+
+TMonitoringManager::~TMonitoringManager()
 { }
 
 void TMonitoringManager::Register(const TYPath& path, TYsonProducer producer)
 {
-    TGuard<TSpinLock> guard(SpinLock);
-    YCHECK(ProducerMap.insert(std::make_pair(path, producer)).second);
+    Impl_->Register(path, producer);
 }
 
 void TMonitoringManager::Unregister(const TYPath& path)
 {
-    TGuard<TSpinLock> guard(SpinLock);
-    YCHECK(ProducerMap.erase(path) == 1);
+    Impl_->Unregister(path);
 }
 
 IYPathServicePtr TMonitoringManager::GetService()
 {
-    return New<TYPathService>(this);
+    return Impl_->GetService();
 }
 
 void TMonitoringManager::Start()
 {
-    YASSERT(!IsStarted);
-
-    // Create an empty root immediately to prevent GetRoot from returning NULL.
-    Root = GetEphemeralNodeFactory()->CreateMap();
-
-    PeriodicExecutor = New<TPeriodicExecutor>(
-        ActionQueue->GetInvoker(),
-        BIND(&TMonitoringManager::Update, MakeStrong(this)),
-        UpdatePeriod);
-    PeriodicExecutor->Start();
-
-    IsStarted = true;
+    Impl_->Start();
 }
 
 void TMonitoringManager::Stop()
 {
-    if (!IsStarted)
-        return;
-
-    IsStarted = false;
-    PeriodicExecutor->Stop();
-    Root.Reset();
-}
-
-void TMonitoringManager::Update()
-{
-    PROFILE_TIMING ("/update_time") {
-        INodePtr newRoot = GetEphemeralNodeFactory()->CreateMap();
-
-        for (const auto& pair : ProducerMap) {
-            TYsonString value = ConvertToYsonString(pair.second);
-            SyncYPathSet(newRoot, pair.first, value);
-        }
-
-        if (IsStarted) {
-            Root = newRoot;
-        }
-    }
+    Impl_->Stop();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
