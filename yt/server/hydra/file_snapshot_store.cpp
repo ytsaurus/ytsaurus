22055c7a42609a322500c25e62e31a7b3ed52b23
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-10-31 15:37:38 +0300

    Hack to fix reading 0.16 snapshot

--- yt/server/hydra/file_snapshot_store.cpp
+++ yt/server/hydra/file_snapshot_store.cpp
@@ -208,7 +208,7 @@ public:
                     FacadeInput_ = FileInput_.get();
                 } else {
                     CodecInput_.reset(new TSnappyDecompress(FileInput_.get()));
-                    FakeCheckpointableInput_ = CreateFakeCheckpointableInputStream(CodecInput_.get(), legacyHeader.DataLength);
+                    FakeCheckpointableInput_ = CreateFakeCheckpointableInputStream(CodecInput_.get(), 128ll * (1ll << 30));
                     FacadeInput_ = FakeCheckpointableInput_.get();
                 }
             } else {
