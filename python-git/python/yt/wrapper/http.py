023a26db34585347b42b516a063f988d65b1e4f0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2014-11-03 01:42:07 +0300

    Fix hiding token

--- python-git/python/yt/wrapper/http.py
+++ python-git/python/yt/wrapper/http.py
@@ -114,7 +114,11 @@ def make_request_with_retries(method, url, make_retries=True, retry_unavailable_
             if response.raw_response.status_code == 503:
                 raise YtProxyUnavailable("Retrying response with code 503 and body %s" % response.content())
             if not response.is_ok():
-                raise YtResponseError(url, kwargs.get("headers", {}), response.error())
+                headers = kwargs.get("headers", {})
+                # Hide token for security reasons
+                if "Authorization" in headers:
+                    headers["Authorization"] = "x" * 32
+                raise YtResponseError(url, headers, response.error())
 
             return response
 
