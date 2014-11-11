76ed493ea4842611f80886c54cbea43b96457752
Maxim Babenko <babenko@ytsaurus.tech>
2014-11-11 22:26:01 +0300

    Better diagnostics when retries fail

--- yt/server/query_agent/helpers.cpp
+++ yt/server/query_agent/helpers.cpp
@@ -4,6 +4,7 @@
 #include <core/logging/log.h>
 
 #include <server/data_node/public.h>
+#include <Python/Python.h>
 
 namespace NYT {
 namespace NQueryAgent {
@@ -24,20 +25,24 @@ void ExecuteRequestWithRetries(
     const std::function<void()>& callback)
 {
     const auto& Logger = logger;
+    std::vector<TError> errors;
     for (int retryIndex = 0; retryIndex < maxRetries; ++retryIndex) {
         try {
             callback();
-            break;
+            return;
         } catch (const std::exception& ex) {
             auto error = TError(ex);
             if (IsRetriableError(error)) {
                 LOG_INFO(error, "Request failed, retrying");
+                errors.push_back(error);
                 continue;
             } else{
                 throw;
             }
         }
     }
+    THROW_ERROR_EXCEPTION("Request failed after %v retries", maxRetries)
+        << errors;
 }
 
 ////////////////////////////////////////////////////////////////////
