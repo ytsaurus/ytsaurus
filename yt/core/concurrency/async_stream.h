d2c4dd9cfc8fcba437058d187acf5c20dfcbc84f
Pavel Sushin <psushin@ytsaurus.tech>
2014-12-01 21:40:04 +0300

    Fix comments

--- yt/core/concurrency/async_stream.h
+++ yt/core/concurrency/async_stream.h
@@ -17,14 +17,8 @@ namespace NConcurrency {
 //! Provides an asynchronous interface for reading from a stream.
 /*!
  *  Call #Read and provide a buffer to start reading.
- *  If |true| is returned then some data was read synchronously.
- *  Call #GetReadLength to obtain its length.
- *  
- *  Otherwise no data could be read synchronously and an asynchronous request
- *  has been issued. Call #GetReadyEvent and subscribe to its return value to figure
- *  out when this request completes. Buffer passed to #Read must remain valid
- *  for the duration of the request. Upon request completion, call #GetReadLength
- *  (as above) to obtain the actual read length.
+ *  Buffer passed to #Read must remain valid until the returned future is set. 
+ *  Returns number of bytes read or error.
  *
  */
 struct IAsyncInputStream
@@ -43,12 +37,7 @@ IAsyncInputStreamPtr CreateAsyncInputStream(TInputStream* syncStream);
 //! Provides an asynchronous interface for writing to a stream.
 /*!
  *  Call #Write to issue a write request.
- *  If |true| is returned then the request is completed synchronously.
- *  
- *  Otherwise the data passed to #Write has been accepted but the request
- *  involves some asynchronous activities. Call #GetReadyEvent and subscribe
- *  to its return value to figure out when this request completes.
- *  Buffer passed to #Write must remain valid for the duration of the request.
+ *  Buffer passed to #Write must remain valid until the retured future is set.
  *
  */
 struct IAsyncOutputStream
