f5c56c595865502af9de7f478aad72d21376eb3e
Maxim Babenko <babenko@ytsaurus.tech>
2015-01-08 21:12:04 +0300

    Cosmetics

--- yt/ytlib/chunk_client/file_writer.h
+++ yt/ytlib/chunk_client/file_writer.h
@@ -3,10 +3,10 @@
 #include "public.h"
 #include "chunk_writer.h"
 
+#include <core/misc/checksum.h>
+
 #include <ytlib/chunk_client/chunk_meta.pb.h>
 #include <ytlib/chunk_client/chunk_info.pb.h>
-
-#include <core/misc/checksum.h>
 
 #include <util/system/file.h>
 
@@ -24,6 +24,7 @@ public:
         const Stroka& fileName,
         bool syncOnClose = true);
 
+    // IChunkWriter implementation.
     virtual TFuture<void> Open() override;
 
     virtual bool WriteBlock(const TSharedRef& block) override;
@@ -33,40 +34,43 @@ public:
 
     virtual TFuture<void> Close(const NChunkClient::NProto::TChunkMeta& chunkMeta) override;
 
-    void Abort();
-
-    //! Returns chunk info. The writer must be already closed.
     virtual const NChunkClient::NProto::TChunkInfo& GetChunkInfo() const override;
-
     virtual TChunkReplicaList GetWrittenChunkReplicas() const override;
 
-    //! The writer must be already closed.
+    //! Returns the chunk meta.
+    /*!
+     *  The writer must be already closed.
+     */
     const NChunkClient::NProto::TChunkMeta& GetChunkMeta() const;
 
-    //! Can be called at any time.
+    //! Aborts the writer, removing the temporary files.
+    void Abort();
+
+    //! Returns the total data size accumulated so far.
+    /*!
+     *  Can be called at any time.
+     */
     i64 GetDataSize() const;
 
 private:
     Stroka FileName_;
     bool SyncOnClose_;
 
-    bool IsOpen_;
-    bool IsClosed_;
-    i64 DataSize_;
+    bool IsOpen_ = false;
+    bool IsClosed_ = false;
+    i64 DataSize_ = 0;
+
     std::unique_ptr<TFile> DataFile_;
+
     NChunkClient::NProto::TChunkInfo ChunkInfo_;
     NChunkClient::NProto::TBlocksExt BlocksExt_;
     NChunkClient::NProto::TChunkMeta ChunkMeta_;
 
-    TFuture<void> Result_;
+    TError Error_;
 
 };
 
 DEFINE_REFCOUNTED_TYPE(TFileWriter)
-
-///////////////////////////////////////////////////////////////////////////////
-
-void RemoveChunkFiles(const Stroka& dataFileName);
 
 ///////////////////////////////////////////////////////////////////////////////
 
