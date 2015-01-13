166439aef1671574ca5d1b3c00b85f56f8f29a81
Maxim Babenko <babenko@ytsaurus.tech>
2015-01-13 21:03:58 +0300

    Fix passing transaction id to scheduler

--- yt/server/scheduler/scheduler_service.cpp
+++ yt/server/scheduler/scheduler_service.cpp
@@ -10,6 +10,8 @@
 
 #include <ytlib/security_client/public.h>
 
+#include <ytlib/cypress_client/rpc_helpers.h>
+
 #include <server/cell_scheduler/bootstrap.h>
 
 namespace NYT {
@@ -20,6 +22,7 @@ using namespace NCellScheduler;
 using namespace NTransactionClient;
 using namespace NYTree;
 using namespace NSecurityClient;
+using namespace NCypressClient;
 
 ////////////////////////////////////////////////////////////////////
 
@@ -49,8 +52,8 @@ private:
     DECLARE_RPC_SERVICE_METHOD(NProto, StartOperation)
     {
         auto type = EOperationType(request->type());
-        auto transactionId = FromProto<TTransactionId>(request->transaction_id());
-        auto mutationId = GetMutationId(context->RequestHeader());
+        auto transactionId = GetTransactionId(context);
+        auto mutationId = GetMutationId(context);
 
         auto maybeUser = FindAuthenticatedUser(context);
         auto user = maybeUser ? *maybeUser : RootUserName;
