b1d287b71a9e10a1b96040498ec873e4441bb5d1
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2015-01-20 20:13:59 +0300

    YT-1508: Fix output limit when using group by

--- yt/server/query_agent/query_executor.cpp
+++ yt/server/query_agent/query_executor.cpp
@@ -216,7 +216,7 @@ private:
 
                 return WaitFor(asyncQueryStatisticsOrError)
                     .ValueOrThrow();
-            }, false);
+            });
     }
 
     TDataSplits Split(
