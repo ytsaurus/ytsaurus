bfc41cec74a19840f9c9260053847609d55f36b6
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2015-01-26 16:21:18 +0300

    Do not regroup splits

--- yt/server/query_agent/query_executor.cpp
+++ yt/server/query_agent/query_executor.cpp
@@ -168,14 +168,8 @@ private:
             [&] (const TDataSplits& prunedSplits) {
                 auto splits = Split(prunedSplits, nodeDirectory, Logger);
 
-                LOG_DEBUG("Regrouping %v splits", splits.size());
-                size_t splitsPerQuery = std::sqrt(splits.size());
-
-                for (auto offset = splits.begin(); offset < splits.end();) {
-                    size_t rest = std::distance(offset, splits.end());
-                    auto offsetEnd = offset + std::min(rest, splitsPerQuery);
-                    groupedSplits.emplace_back(offset, offsetEnd);
-                    offset = offsetEnd;
+                for (const auto& split : splits) {
+                    groupedSplits.emplace_back(1, split);
                 }
 
                 return GetRanges(groupedSplits);
