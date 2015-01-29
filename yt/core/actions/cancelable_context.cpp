779251d0e68c0cddca53ac0b8642d1d95e2653a8
Maxim Babenko <babenko@ytsaurus.tech>
2015-01-29 22:06:35 +0300

    Cancelation propagation

--- yt/core/actions/cancelable_context.cpp
+++ yt/core/actions/cancelable_context.cpp
@@ -51,12 +51,86 @@ bool TCancelableContext::IsCanceled() const
 
 void TCancelableContext::Cancel()
 {
-    Canceled_ = true;
+    yhash_set<TWeakPtr<TCancelableContext>> propagateToContexts;
+    yhash_set<TFuture<void>> propagateToFutures;
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+        if (Canceled_)
+            return;
+        Canceled_ = true;
+        PropagateToContexts_.swap(propagateToContexts);
+        PropagateToFutures_.swap(propagateToFutures);
+    }
+
+    Handlers_.FireAndClear();
+
+    for (auto weakContext : propagateToContexts) {
+        auto context = weakContext.Lock();
+        if (context) {
+            context->Cancel();
+        }
+    }
+
+    for (auto future : propagateToFutures) {
+        future.Cancel();
+    }
 }
 
 IInvokerPtr TCancelableContext::CreateInvoker(IInvokerPtr underlyingInvoker)
 {
     return New<TCancelableInvoker>(this, std::move(underlyingInvoker));
+}
+
+void TCancelableContext::SubscribeCanceled(const TClosure& callback)
+{
+    TGuard<TSpinLock> guard(SpinLock_);
+    if (Canceled_) {
+        guard.Release();
+        callback.Run();
+        return;
+    }
+    Handlers_.Subscribe(callback);
+}
+
+void TCancelableContext::UnsubscribeCanceled(const TClosure& /*callback*/)
+{
+    YUNREACHABLE();
+}
+
+void TCancelableContext::PropagateTo(TCancelableContextPtr context)
+{
+    auto weakContext = MakeWeak(context);
+
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+        PropagateToContexts_.insert(context);
+    }
+
+    auto weakThis = MakeWeak(this);
+    context->SubscribeCanceled(BIND([=] () {
+        auto this_ = weakThis.Lock();
+        if (this_) {
+            TGuard<TSpinLock> guard(this_->SpinLock_);
+            this_->PropagateToContexts_.erase(context);
+        }
+    }));
+}
+
+void TCancelableContext::PropagateTo(TFuture<void> future)
+{
+    {
+        TGuard<TSpinLock> guard(SpinLock_);
+        PropagateToFutures_.insert(future);
+    }
+
+    auto weakThis = MakeWeak(this);
+    future.Subscribe(BIND([=] (const TError&) {
+        auto this_ = weakThis.Lock();
+        if (this_) {
+            TGuard<TSpinLock> guard(this_->SpinLock_);
+            this_->PropagateToFutures_.erase(future);
+        }
+    }));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
