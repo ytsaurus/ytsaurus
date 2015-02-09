99b73ad7cac419a9b747d227423a07780c7e88ca
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-02-09 12:12:05 +0300

    disable computed column test if built without LLVM

--- tests/integration/tests/test_tablets.py
+++ tests/integration/tests/test_tablets.py
@@ -160,6 +160,7 @@ class TestTablets(YTEnvSetup):
         with pytest.raises(YtError): read("//tmp/t")
         with pytest.raises(YtError): write("//tmp/t", [{"key": 1, "value": 2}])
 
+    @pytest.mark.skipif('os.environ.get("BUILD_ENABLE_LLVM", None) == "NO"')
     def test_computed_column(self):
         self._sync_create_cells(1, 1)
         self._create_table_with_computed_column("//tmp/t")
