6480e485d2e62c718021bb2a849b55856a25e38f
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-02-13 18:34:57 +0300

    Simplify export_to_yt.py script

--- python-git/python/yt/tools/binaries/export_to_yt.py
+++ python-git/python/yt/tools/binaries/export_to_yt.py
@@ -1,10 +1,11 @@
 #!/usr/bin/env python
 
-from yt.tools.atomic import process_tasks_from_list, CANCEL, REPEAT
+from yt.tools.atomic import process_tasks_from_list, CANCEL
 from yt.tools.common import update_args
+from yt.tools.remote_copy_tools import copy_yt_to_yt_through_proxy
 from yt.wrapper.cli_helpers import die
-
-from yt.wrapper.table_commands import get_sorted_by
+from yt.wrapper.client import Yt
+from yt.wrapper.http import get_token
 
 import yt.logger as logger
 import yt.wrapper as yt
@@ -44,83 +45,20 @@ def export_table(object, args):
         logger.error("Source and destination proxies should be different")
         return CANCEL
 
-    old_proxy = yt.config.http.PROXY
-    try:
-        yt.config.set_proxy(params.yt_proxy)
-        if yt.exists(dst) and yt.records_count(dst) != 0:
-            if params.force:
-                yt.remove(dst)
-            else:
-                logger.error("Destination table '%s' is not empty" % dst)
-                return CANCEL
-        yt.create_table(dst, recursive=True, ignore_existing=True)
-    finally:
-        yt.config.set_proxy(old_proxy)
+    source_client = Yt(yt.config.http.PROXY, get_token())
+    destination_client = Yt(params.yt_proxy, params.yt_token)
 
-    with yt.Transaction():
-        yt.lock(src, mode="snapshot")
+    yt.config.http.PROXY = None
 
-
-        record_count = yt.records_count(src)
-
-        is_sorted = yt.is_sorted(src)
-        if is_sorted:
-            sorted_by = get_sorted_by(src)
-
-        hosts = "hosts/fb" if params.fastbone else "hosts"
-
-        command = "YT_TOKEN={0} YT_HOSTS={1} yt2 write --proxy {2} --format '<format=binary>yson' '<append=true>{3}'"\
-                .format(params.yt_token, hosts, params.yt_proxy, dst)
-
-        logger.info("Running map '%s'", command)
-        try:
-            yt.run_map(command, src, yt.create_temp_table(),
-                       format=yt.YsonFormat(format="binary"),
-                       memory_limit=3500 * yt.common.MB,
-                       spec={"pool": params.yt_pool,
-                             "data_size_per_job": 2 * 1024 * yt.common.MB})
-        except yt.YtOperationFailedError as error:
-            logger.exception("Export operation failed")
-            # "YtNetworkError" in error message usually means that some jobs failed
-            # to write data because of network problems.
-            if "YtNetworkError" in error.message:
-                return REPEAT
+    if destination_client.exists(dst) and destination_client.records_count(dst) != 0:
+        if params.force:
+            destination_client.remove(dst)
+        else:
+            logger.error("Destination table '%s' is not empty" % dst)
             return CANCEL
+    destination_client.create_table(dst, recursive=True, ignore_existing=True)
 
-    try:
-        yt.config.set_proxy(params.yt_proxy)
-        result_record_count = yt.records_count(dst)
-        if record_count != result_record_count:
-            logger.error("Incorrect record count (expected: %d, actual: %d)", record_count, result_record_count)
-            yt.remove(dst)
-            return REPEAT
-
-        if is_sorted:
-            logger.info("Running sort")
-            yt.run_sort(dst, sort_by=sorted_by)
-
-        # TODO(ignat): remove this copypaste (code is borrowed from import_from_mr.py)
-        if params.erasure_codec is not None and params.erasure_codec == "none":
-            params.erasure_codec = None
-
-        if params.compression_codec is not None or params.erasure_codec is not None:
-            mode = "sorted" if is_sorted else "unordered"
-            spec = {"combine_chunks": "true",
-                    "force_transform": "true"}
-
-            if params.compression_codec is not None:
-                yt.set_attribute(dst, "compression_codec", params.compression_codec)
-            if params.erasure_codec is not None:
-                yt.set_attribute(dst, "erasure_codec", params.erasure_codec)
-                spec["job_io"] = {"table_writer": {"desired_chunk_size": 2 * 1024 ** 3}}
-                spec["data_size_per_job"] = max(1, int(4 * (1024 ** 3) / yt.get(dst + "/@compression_ratio")))
-
-            logger.info("Merging '%s' with spec '%s'", dst, repr(spec))
-            yt.run_merge(dst, dst, mode=mode, spec=spec)
-
-    finally:
-        yt.config.set_proxy(old_proxy)
-
+    copy_yt_to_yt_through_proxy(source_client, destination_client, src, dst, params.fastbone)
 
 
 def main():
