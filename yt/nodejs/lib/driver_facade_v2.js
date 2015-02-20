bc3979da0f28109a49606453393c40ad11c209f8
Maxim Babenko <babenko@ytsaurus.tech>
2015-02-20 15:40:22 +0300

    Remove redundant verbs

--- yt/nodejs/lib/driver_facade_v2.js
+++ yt/nodejs/lib/driver_facade_v2.js
@@ -42,8 +42,7 @@ function YtDriverFacadeV2(driver)
         "read": "read_table",
         "write": "write_table",
         "download": "read_file",
-        "upload": "write_file",
-        "select": "select_rows"
+        "upload": "write_file"
     };
 
     var descriptors = {};
@@ -85,7 +84,6 @@ function YtDriverFacadeV2(driver)
     delete descriptors.delete_rows;
     delete descriptors.insert_rows;
     delete descriptors.lookup_rows;
-    delete descriptors.select;
     delete descriptors.select_rows;
 
     delete descriptors.dump_input_context
