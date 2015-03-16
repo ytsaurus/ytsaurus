8ba70612cb394b25afe0d54e98bb1c91e22a5d7a
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2015-03-16 20:05:57 +0300

    Fix Blackbox /sessionid authentication

--- yt/nodejs/lib/middleware/acao.js
+++ yt/nodejs/lib/middleware/acao.js
@@ -7,13 +7,16 @@ exports.that = function Middleware__YtAcao()
     "use strict";
 
     return function(req, rsp, next) {
+        var origin = req.headers["origin"] || "*";
+
         if (req.method === "GET" || req.method === "POST" || req.method === "PUT") {
             rsp.setHeader("Access-Control-Allow-Origin", "*");
         }
 
         if (req.method === "OPTIONS") {
-            rsp.setHeader("Access-Control-Allow-Origin", "*");
-            rsp.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
+            rsp.setHeader("Access-Control-Allow-Credentials", "true");
+            rsp.setHeader("Access-Control-Allow-Origin", origin);
+            rsp.setHeader("Access-Control-Allow-Methods", "post, put, get, options");
             rsp.setHeader("Access-Control-Allow-Headers", "authorization, origin, content-type, accept, x-yt-parameters, x-yt-input-format, x-yt-output-format");
             rsp.setHeader("Access-Control-Max-Age", "3600");
             return void utils.dispatchAs(rsp);
