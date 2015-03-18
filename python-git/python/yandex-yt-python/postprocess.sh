e8c55fdcddfbeb4ad042ee45f3afae87b77ee9cb
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-03-18 16:54:57 +0300

    Fix uploading yandex-yt-python package binaries

--- python-git/python/yandex-yt-python/postprocess.sh
+++ python-git/python/yandex-yt-python/postprocess.sh
@@ -1,7 +1,7 @@
 #!/bin/bash -eux
 # Build and upload self-contained binaries
 
-export YT_PROXY=kant.yt.yandex.net
+export YT_PROXY=locke.yt.yandex.net
 export YT_TOKEN=1da6afc98d189e8ba59d2ea39f29d0f1 #teamcity user
 DEST="//home/files"
 YT="yt/wrapper/yt"
@@ -15,8 +15,7 @@ for name in yt mapreduce-yt; do
     pyinstaller/pyinstaller.py --noconfirm --onefile yt/wrapper/$name
     pyinstaller/pyinstaller.py --noconfirm "${name}.spec"
     VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
-    cat dist/$name | $YT upload "$DEST/${name}_${VERSION}"
-    cat dist/$name | $YT upload "$DEST/$name"
+    cat dist/$name | $YT upload "$DEST/${name}_${VERSION}_$(lsb_release --short --codename)"
 done
 
 mv pickling.py yt/wrapper/pickling.py
