fb376a76750882a976f2f3efccaad5349b5befcc
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-03-20 16:00:35 +0300

    Reshard script for tables with farm_hash

---
+++ scripts/reshard.py
@@ -0,0 +1,31 @@
+#!/usr/bin/python
+
+import yt.wrapper as yt
+
+import argparse
+from time import sleep
+
+yt.config.VERSION="v3"
+
+def reshard(path, shard_count, is_unsigned=False):
+    delta = 0 if is_unsigned else 2**63
+    pivot_keys = [[]] + [[int((i * 2**64) / shard_count - delta)] for i in xrange(1, shard_count)]
+    print "Unmounting..."
+    yt.unmount_table(path)
+    while not all(x["state"] == "unmounted" for x in yt.get(path + "/@tablets")):
+        sleep(1)
+    print "Resharding..."
+    yt.reshard_table(path, pivot_keys)
+    print "Mounting..."
+    yt.mount_table(path)
+    while not all(x["state"] == "mounted" for x in yt.get(path + "/@tablets")):
+        sleep(1)
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser(description="Copy table with tablets.")
+    parser.add_argument("--proxy", type=yt.config.set_proxy, help="YT proxy")    
+    parser.add_argument("--table", type=str, required=True, help="Table to reshard")
+    parser.add_argument("--shard_count", type=int, required=True, help="Number of shards")
+    parser.add_argument("--unsigned", type=bool, help="Make unsigned pivots")
+    args = parser.parse_args()
+    reshard(args.table, args.shard_count, args.unsigned)
