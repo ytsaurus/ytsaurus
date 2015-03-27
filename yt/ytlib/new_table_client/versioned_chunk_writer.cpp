a1772d8efce114ac29c666ca5c2823fe72c950e9
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-03-27 14:47:39 +0300

    Disable bloom filter in chunk writer

--- yt/ytlib/new_table_client/versioned_chunk_writer.cpp
+++ yt/ytlib/new_table_client/versioned_chunk_writer.cpp
@@ -114,7 +114,9 @@ TVersionedChunkWriter::TVersionedChunkWriter(
     , BlockWriter_(new TSimpleVersionedBlockWriter(Schema_, KeyColumns_))
     , MinTimestamp_(MaxTimestamp)
     , MaxTimestamp_(MinTimestamp)
+#ifdef 0
     , KeyFilter_(Config_->MaxKeyFilterSize, Config_->KeyFilterFalsePositiveRate)
+#endif
 { }
 
 TFuture<void> TVersionedChunkWriter::Open()
