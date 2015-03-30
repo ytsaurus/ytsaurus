a73036f8d7e95920b6564bc7b23eeccdd9d168c7
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2015-03-30 16:02:45 +0300

    Fix CORS?

--- yt/nodejs/lib/middleware/acao.js
+++ yt/nodejs/lib/middleware/acao.js
@@ -16,7 +16,7 @@ exports.that = function Middleware__YtAcao()
         if (req.method === "OPTIONS") {
             rsp.setHeader("Access-Control-Allow-Credentials", "true");
             rsp.setHeader("Access-Control-Allow-Origin", origin);
-            rsp.setHeader("Access-Control-Allow-Methods", "post, put, get, options");
+            rsp.setHeader("Access-Control-Allow-Methods", "POST, PUT, GET, OPTIONS");
             rsp.setHeader("Access-Control-Allow-Headers", "authorization, origin, content-type, accept, x-yt-parameters, x-yt-input-format, x-yt-output-format");
             rsp.setHeader("Access-Control-Max-Age", "3600");
             return void utils.dispatchAs(rsp);
