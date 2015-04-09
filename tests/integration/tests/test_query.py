cb990b25d03ad5bfc4d6d4e2f91d36e98a1c6fe2
Lewis Brown <lbrown@yandex-team.ru>
2015-04-09 11:36:14 +0300

    Fix filename in udf integration test

--- tests/integration/tests/test_query.py
+++ tests/integration/tests/test_query.py
@@ -265,7 +265,7 @@ class TestQuery(YTEnvSetup):
         }}
         set(registry_path, data)
 
-        local_implementation_path = os.path.join(os.path.dirname(__file__), "../../../yt/unittests/udf/abs_udf.bc")
+        local_implementation_path = os.path.join(os.path.dirname(__file__), "../../../yt/unittests/udf/test_udfs.bc")
         create("file", implementation_path)
         upload_file(implementation_path, local_implementation_path)
 
