29bb61d55808c4af68d1ebd580ddb1bd66b49e10
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-04-09 19:47:52 +0300

    Support unsigned pivot keys in reshard script

--- scripts/reshard.py
+++ scripts/reshard.py
@@ -1,15 +1,20 @@
 #!/usr/bin/python
 
 import yt.wrapper as yt
+import yt.yson as yson
 
 import argparse
 from time import sleep
 
 yt.config.VERSION="v3"
+yt.config.http.HEADER_FORMAT="yson"
+
+def make_yson(value, is_unsigned):
+    return yson.YsonUint64(value) if is_unsigned else yson.YsonInt64(value)
 
 def reshard(path, shard_count, is_unsigned=False):
     delta = 0 if is_unsigned else 2**63
-    pivot_keys = [[]] + [[int((i * 2**64) / shard_count - delta)] for i in xrange(1, shard_count)]
+    pivot_keys = [[]] + [[make_yson((i * 2**64) / shard_count - delta, is_unsigned)] for i in xrange(1, shard_count)]
     print "Unmounting..."
     yt.unmount_table(path)
     while not all(x["state"] == "unmounted" for x in yt.get(path + "/@tablets")):
@@ -26,6 +31,6 @@ if __name__ == "__main__":
     parser.add_argument("--proxy", type=yt.config.set_proxy, help="YT proxy")    
     parser.add_argument("--table", type=str, required=True, help="Table to reshard")
     parser.add_argument("--shard_count", type=int, required=True, help="Number of shards")
-    parser.add_argument("--unsigned", type=bool, help="Make unsigned pivots")
+    parser.add_argument("--unsigned", action="store_true", help="Make unsigned pivots")
     args = parser.parse_args()
     reshard(args.table, args.shard_count, args.unsigned)
