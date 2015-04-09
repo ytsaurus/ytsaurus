3d8d51d7de665da4d4b6f1bfe852b6397187b83b
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2015-04-10 01:30:02 +0300

    Fix windows build

--- yt/ytlib/query_client/cg_fragment_compiler.cpp
+++ yt/ytlib/query_client/cg_fragment_compiler.cpp
@@ -26,13 +26,6 @@
 //  - TBAA is a king
 //  - Capture pointers by value in ViaClosure
 
-// FIXME: try to fix in new visual studio
-#ifdef _win_
-#define MOVE(name) name
-#else
-#define MOVE(name) name = std::move(name)
-#endif
-
 namespace NYT {
 namespace NQueryClient {
 
