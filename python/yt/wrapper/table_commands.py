a085a9c72f8640e23f63394882459cc7f27ffe38
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-04-24 21:55:42 +0300

    Rename attribute channel to columns

--- python/yt/wrapper/table_commands.py
+++ python/yt/wrapper/table_commands.py
@@ -361,8 +361,6 @@ def _get_format_from_tables(tables, ignore_unexisting_tables):
         if has_attribute(table_name, "_format"):
             format_name = get(table_name + "/@_format", format=YsonFormat())
             return create_format(format_name)
-        if "channel" in table_name.attributes:
-            return create_format("<line_prefix=tskv>dsv")
         return None
     formats = map(extract_format, tables_to_extract)
 
