f4f1945918c1aced6e565168c62575fe5cb8447e
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-05-01 00:48:31 +0300

    Automatically create conductor ticket for yandex-yt-python

--- python/yandex-yt-python/postprocess.sh
+++ python/yandex-yt-python/postprocess.sh
@@ -6,6 +6,7 @@ export YT_TOKEN=1da6afc98d189e8ba59d2ea39f29d0f1 #teamcity user
 DEST="//home/files"
 YT="yt/wrapper/yt"
 UBUNTU_VERSION="$(lsb_release --short --codename )"
+VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
 
 make_link()
 {
@@ -31,9 +32,11 @@ for name in yt mapreduce-yt; do
     rm -rf build dist
     pyinstaller/pyinstaller.py --noconfirm --onefile yt/wrapper/$name
     pyinstaller/pyinstaller.py --noconfirm "${name}.spec"
-    VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
     cat dist/$name | $YT upload "$DEST/${name}_${VERSION}_${UBUNTU_VERSION}"
     make_link "$DEST/${name}_${VERSION}_${UBUNTU_VERSION}" "$DEST/${name}_${UBUNTU_VERSION}"
 done
+
+# Create ticket in coductor
+curl -H "Cookie: conductor_auth=419fb75155c27d44f1d110ec833400fa" "http://c.yandex-team.ru/auth_update/ticket_add?package\[0\]=yandex-yt-python&version\[0\]=$VERSION&ticket\[branch\]=testing"
 
 mv pickling.py yt/wrapper/pickling.py
