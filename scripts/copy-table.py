b9908d28ae931054448d5644b8ea23e0488dc4f4
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-05-07 20:25:37 +0300

    Support multiple clusters in copy-table

--- scripts/copy-table.py
+++ scripts/copy-table.py
@@ -3,7 +3,10 @@
 import yt.wrapper as yt
 import yt.yson as yson
 from yt.common import YtError
+from yt.wrapper.client import Yt
+from yt.wrapper.http import get_token
 
+import os
 import sys
 import argparse
 import subprocess as sp
@@ -31,6 +34,26 @@ MAX_FAILDED_JOB_COUNT = 10
 MAX_RETRY_COUNT = 10
 # Sleep interval between retries
 SLEEP_INTERVAL = 120
+
+# Parse human-writable size.
+def parse_size(size):
+    scale = {"kb": 2**10, "mb": 2**20, "gb": 2**30}
+    try:
+        return int(float(size[:-2]) * scale[size[-2:].lower()]) if size[-2:].lower() in scale.keys() else int(size)
+    except:
+        raise ValueError("Invalid size: '%s'. Valid suffixes are: %s." %
+            (size, ", ".join(["'%s'" % key for key in scale.keys()])))
+
+# Prepare rows data.
+def prepare(value):
+    if not isinstance(value, list):
+        value = [value]
+    # remove surrounding [ ]
+    return yson.dumps(value)[1:-1]
+
+# Check that table is mounted
+def mounted(yt_client, path):
+    return all(x["state"] == "mounted" for x in yt_client.get(src + "/@tablets"))
 
 # Simple transformation class - just copy everything.
 class Copy:
@@ -83,46 +106,35 @@ class Rehash:
     def row(row):
         return row
 
+# Rehash transformation class: make 'hash' column to be a modulo computed column.
+class ModuloRehash:
+    def __init__(self):
+        self._mod = 100
+    def schema(self, schema):
+        assert schema[0]["name"] == "YTHash"
+        assert schema[1]["name"] == "UpdateTime"
+        schema[0]["type"] = "int64"
+        schema[0]["expression"] = "int64(farm_hash(UpdateTime, PageID)) %% %s" % self._mod
+        return schema
+    def key_columns(self, key_columns):
+        return key_columns
+    def select_columns(self, schema):
+        return ",".join([x["name"] for x in schema if "expression" not in x.keys()])
+    def pivot_keys(self, pivot_keys):
+        shard_count = 100
+        pivot_keys = [[]] + [[int(i * (self._mod * 2 + 1) / shard_count - self._mod)] for i in xrange(1, shard_count)]
+        return pivot_keys
+    def row(self, row):
+        return row
+
 # Transformation object.
 Transform = Copy()
 
-# Parse human-writable size.
-def parse_size(size):
-    scale = {"kb": 2**10, "mb": 2**20, "gb": 2**30}
-    try:
-        return int(float(size[:-2]) * scale[size[-2:].lower()]) if size[-2:].lower() in scale.keys() else int(size)
-    except:
-        raise ValueError("Invalid size: '%s'. Valid suffixes are: %s." %
-            (size, ", ".join(["'%s'" % key for key in scale.keys()])))
-
-# Prepare rows data.
-def prepare(value):
-    if not isinstance(value, list):
-        value = [value]
-    # remove surrounding [ ]
-    return yson.dumps(value)[1:-1]
-
-# Check that table is mounted
-def mounted(path):
-    return all(x["state"] == "mounted" for x in yt.get(src + "/@tablets"))
-
-# Mapper - get tablet partition pivot keys.
-def tablets_mapper(tablet):
-    yield {"pivot_key":tablet["pivot_key"]}
-    tablet_id = tablet["tablet_id"]
-    cell_id = tablet["cell_id"]
-    node = yson.loads(sp.check_output(["yt", "get", "--format", "<format=text>yson", "#" + cell_id + "/@peers/0/address"]))
-    partitions_path = "//sys/nodes/%s/orchid/tablet_cells/%s/tablets/%s/partitions" % (node, cell_id, tablet_id)
-    partitions = sp.check_output(["yt", "get", "--format", "<format=text>yson", partitions_path])
-    if partitions != None:
-        for partition in yson.loads(partitions):
-            yield {"pivot_key": partition["pivot_key"]}
-
 # Call subprocess with retries
-def call_subprocess(command, stdin, max_retry_count, sleep_interval):
+def call_subprocess(command, stdin, max_retry_count, sleep_interval, env):
     errors = []
     def single_call():
-        p = sp.Popen(command, stdin=sp.PIPE if stdin is not None else None, stdout=sp.PIPE, stderr=sp.PIPE)
+        p = sp.Popen(command, stdin=sp.PIPE if stdin is not None else None, stdout=sp.PIPE, stderr=sp.PIPE, env=env)
         out, err = p.communicate(stdin)
         return p.returncode, out, err
     attempt = 0
@@ -137,15 +149,81 @@ def call_subprocess(command, stdin, max_retry_count, sleep_interval):
         return out
     else:
         errors = ["try: %s\nreturn code: %s\nstdout:\n%s\n\nstderr:\n%s\n" % (attempt, ret, out, err) for attempt, ret, out, err in errors]
-        stderr = "\n\n===================================================================\n\n".join(errors)
+        errors = [e +  "\n\n===================================================================\n\n" for e in errors]
+        stderr = "".join(errors)
+        print >> sys.stderr, "stdin:\n%s\n" % stdin
         print >> sys.stderr, stderr
         raise sp.CalledProcessError(ret, " ".join(command), errors)
+
+class Driver:
+    def __init__(self, data):
+        self._data = data
+    def execute(self, command, stdin=None):
+        env = deepcopy(os.environ)
+        if not self._data["token"] == None:
+            env["YT_TOKEN"] = str(self._data["token"])
+        env["YT_VERSION"] = "v3"
+        if self._data.get("disabled"):
+            print >> sys.stderr, self._data["command"] + command
+            print >> sys.stderr, env
+            print >> sys.stderr, " ".join(self._data["command"] + command)
+        else:
+            return call_subprocess(
+                self._data["command"] + command,
+                stdin,
+                self._data["max_retry_count"],
+                self._data["sleep_interval"],
+                env)
+    def execute_select(self, query):
+        return self.execute(self._data["select"] + [query])
+    def execute_command(self, command, destination, rows):
+        return self.execute(self._data[command] + [destination], rows)
+    def disable(self):
+        self._data["disabled"] = True
+    def serialize(self):
+        return self._data
+    @staticmethod
+    def deserialize(data):
+        return Driver(data)
+    @staticmethod
+    def create(max_retry_count, sleep_interval, input_row_limit, output_row_limit, proxy=None, token=None):
+        select_cmd = "select_rows" if not proxy else "select-rows"
+        input_opt = "--input_row_limit" if not proxy else "--input-row-limit"
+        output_opt = "--output_row_limit" if not proxy else "--output-row-limit"
+        data = {
+            "max_retry_count" : max_retry_count,
+            "sleep_interval" : sleep_interval,
+            "select": [select_cmd, input_opt, str(input_row_limit), output_opt, str(output_row_limit), "--format", "<format=text>yson"],
+            "insert": ["insert_rows" if not proxy else "insert-rows", "--format", "<format=text>yson"],
+            "delete": ["delete_rows" if not proxy else "delete-rows", "--format", "<format=text>yson"],
+            "command": ["yt"] if not proxy else ["yt2", "--proxy", proxy],
+            "token": token}
+        return Driver(data)
+
+# Mapper - get tablet partition pivot keys.
+def tablets_mapper(tablet):
+    # Read config.
+    f = open(CONFIG_FILE_NAME, 'r')
+    config = yson.load(f)
+    driver = Driver.deserialize(config["driver_src"])
+
+    yield {"pivot_key":tablet["pivot_key"]}
+    tablet_id = tablet["tablet_id"]
+    cell_id = tablet["cell_id"]
+    node = yson.loads(driver.execute(["get", "--format", "<format=text>yson", "#" + cell_id + "/@peers/0/address"]))
+    partitions_path = "//sys/nodes/%s/orchid/tablet_cells/%s/tablets/%s/partitions" % (node, cell_id, tablet_id)
+    partitions = driver.execute(["get", "--format", "<format=text>yson", partitions_path])
+    if partitions != None:
+        for partition in yson.loads(partitions):
+            yield {"pivot_key": partition["pivot_key"]}
 
 # Mapper - copy content with keys between r["left"] and r["right"].
 def regions_mapper(r):
     # Read config.
     f = open(CONFIG_FILE_NAME, 'r')
     config = yson.load(f)
+    driver_src = Driver.deserialize(config["driver_src"])
+    driver_dst = Driver.deserialize(config["driver_dst"])
 
     # Get something like ((key1, key2, key3), (bound1, bound2, bound3)) from a bound.
     get_bound_value = lambda bound : ",".join([yson.dumps(x, yson_format="text") for x in bound])
@@ -159,15 +237,13 @@ def regions_mapper(r):
         bounds = [x for x in [left, right, config.get("select_where")] if x is not None]
         where = (" where " + " and ".join(bounds)) if len(bounds) > 0 else ""
         query = "%s from [%s]%s" % (config["select_columns"], config["source"], where)
-        select_cmd = ["yt", "select_rows", "--output_row_limit", str(config["output_row_limit"]),"--format", "<format=text>yson",  query]
-        return call_subprocess(select_cmd, None, config["max_retry_count"], config["sleep_interval"]) 
+        return driver_src.execute_select(query)
     raw_data = StringIO(query(r["left"], r["right"]))
 
     # Insert data into destination table.
-    insert_cmd = ["yt", config["command"], "--format", "<format=text>yson", config["destination"]]
     new_data = []
     def dump_data():
-        call_subprocess(insert_cmd, prepare(new_data), config["max_retry_count"], config["sleep_interval"])
+        driver_dst.execute_command(config["mode"], config["destination"], prepare(new_data))
         del new_data[:]
 
     # Process data.
@@ -185,7 +261,11 @@ if __name__ == "__main__":
     mode.add_argument("--copy", nargs=2, metavar=('SOURCE', 'DESTINATION'), help="Copy table")
     mode.add_argument("--delete", metavar=('SOURCE'), help="Delete rows from table")
     parser.add_argument("--force", action="store_true", help="Overwrite destination table if it exists")
-    parser.add_argument("--proxy", type=yt.config.set_proxy, help="YT proxy")
+    parser.add_argument("--proxy", type=str, help="YT proxy")
+    parser.add_argument("--proxy_src", type=str, help="source YT proxy")
+    parser.add_argument("--proxy_dst", type=str, help="destination YT proxy")
+    parser.add_argument("--token_src", type=str, help="source YT token")
+    parser.add_argument("--token_dst", type=str, help="destination YT token")
     parser.add_argument("--job_count", type=int, default=JOB_COUNT, help="Numbser of jobs in copy task")
     parser.add_argument("--user_slots", type=int, help="Maximum number of simultaneous jobs running")
     parser.add_argument("--max_failed_job_count", type=int, default=MAX_FAILDED_JOB_COUNT, help="Maximum number of failed jobs")
@@ -198,57 +278,85 @@ if __name__ == "__main__":
     parser.add_argument("--where", type=str, help="Additional predicate for 'yt select'")
     args = parser.parse_args()
 
+    # Source and destination table paths.
     src = args.copy[0] if args.copy else args.delete
     dst = args.copy[1] if args.copy else None
-    
-    if dst and yt.exists(dst):
+
+    # YT proxies connections.
+    existent = lambda x: None if len(x) == 0 else x[0] if x[0] is not None else existent(x[1:])
+    yt.config.set_proxy(args.proxy)
+    yt_src = Yt(existent([args.proxy_src, args.proxy]), existent([args.token_src, get_token()]))
+    yt_dst = Yt(existent([args.proxy_dst, args.proxy]), existent([args.token_dst, get_token()]))
+    yt_dst.VERSION="v3"
+
+    # YT drivers to use inside operations.
+    driver_src = Driver.create(
+        args.max_retry_count,
+        args.sleep_interval,
+        args.input_row_limit,
+        args.output_row_limit,
+        args.proxy_src,
+        args.token_src)
+    driver_dst = Driver.create(
+        args.max_retry_count,
+        args.sleep_interval,
+        args.input_row_limit,
+        args.output_row_limit,
+        args.proxy_dst,
+        args.token_dst)
+
+    # Source table schema.
+    schema = yt_src.get(src + "/@schema")
+    key_columns = yt_src.get(src + "/@key_columns")
+
+    # Config passed to mapper.
+    config = {
+        "key_columns": deepcopy(key_columns),
+        "source": src,
+        "rows_per_insert": args.insert_size,
+        "driver_src": driver_src.serialize(),
+        "driver_dst": driver_dst.serialize()}
+    if (args.where): config["select_where"] = args.where
+    def write_config(config):
+        with open(CONFIG_FILE_NAME, "w") as config_file: config_file.write(yson.dumps(config))
+    write_config(config)
+
+    # Prepare tables.
+    if dst and yt_dst.exists(dst):
         if args.force:
-            yt.remove(dst)
+            yt_dst.remove(dst)
         else:
             raise Exception("Destination table exists. Use --force")
-
-    if not mounted(src):
-        yt.mount_table(src)
-        while not mounted(src):
+    if not mounted(yt_src, src):
+        yt_src.mount_table(src)
+        while not mounted(yt_src, src):
             sleep(1)
 
-    tablets = yt.get(src + "/@tablets")
+    tablets = yt_src.get(src + "/@tablets")
 
     # Get pivot keys. For a large number of tablets use map-reduce version.
     # Tablet pivots are merged with partition pivots
     print "Prepare partition keys:"
     partition_keys = []
-    if len(tablets) < 10:
-        tablet_idx = 0
-        for tablet in tablets:
-            partition_keys.append(tablet["pivot_key"])
-            tablet_idx += 1
-            sys.stdout.write("\rTablet %s of %s" % (tablet_idx, len(tablets)))
-            sys.stdout.flush()
-            tablet_id = tablet["tablet_id"]
-            cell_id = tablet["cell_id"]
-            node = yt.get("#" + cell_id + "/@peers/0/address")
-            for partition in yt.get("//sys/nodes/%s/orchid/tablet_cells/%s/tablets/%s/partitions" % (node, cell_id, tablet_id)):
-                partition_keys.append(partition["pivot_key"])
-        print ""
-    else:
-        tablets_table = yt.create_temp_table()
-        partitions_table = yt.create_temp_table()
-        yt.write_table(tablets_table, tablets, format=yt.YsonFormat(format="text"), raw=False)
-        try:
-            yt.run_map(
-                tablets_mapper,
-                tablets_table,
-                partitions_table,
-                spec={"job_count": 100, "max_failed_job_count":10},
-                format=yt.YsonFormat(format="text"))
-        except YtError as e:
-            print str(e)
-            raise e
-        partition_keys = yt.read_table(partitions_table, format=yt.YsonFormat(format="text"), raw=False)
-        partition_keys = [p["pivot_key"] for p in partition_keys]
-        yt.remove(tablets_table)   
-        yt.remove(partitions_table)
+
+    tablets_table = yt.create_temp_table()
+    partitions_table = yt.create_temp_table()
+    yt.write_table(tablets_table, tablets, format=yt.YsonFormat(format="text"), raw=False)
+    try:
+        yt.run_map(
+            tablets_mapper,
+            tablets_table,
+            partitions_table,
+            spec={"job_count": 100, "max_failed_job_count":10, "resource_limits": {"user_slots": 50}},
+            format=yt.YsonFormat(format="text"),
+            local_files=CONFIG_FILE_NAME)
+    except YtError as e:
+        print str(e)
+        raise e
+    partition_keys = yt.read_table(partitions_table, format=yt.YsonFormat(format="text"), raw=False)
+    partition_keys = [p["pivot_key"] for p in partition_keys]
+    yt.remove(tablets_table)
+    yt.remove(partitions_table)
 
     partition_keys = [list(it.takewhile(lambda x : x is not None, key)) for key in partition_keys]
     partition_keys = [key for key in partition_keys if len(key) > 0]
@@ -267,22 +375,13 @@ if __name__ == "__main__":
         regions,
         format=yt.YsonFormat(format="text"), raw=False)
 
-    schema = yt.get(src + "/@schema")
-    key_columns = yt.get(src + "/@key_columns")
+    # Only print commands to stderr, without execution.
+    #driver_src.disable()
+    #driver_dst.disable()
+    #config["driver_src"] = driver_src.serialize()
+    #config["driver_dst"] = driver_dst.serialize()
 
-    # Config passed to mapper.
-    config = {
-        "key_columns": deepcopy(key_columns),
-        "source": src,
-        "rows_per_insert": args.insert_size,
-        "input_row_limit": args.input_row_limit,
-        "output_row_limit": args.output_row_limit,
-        "max_retry_count": args.max_retry_count,
-        "sleep_interval": args.sleep_interval}
-    if (args.where): config["select_where"] = args.where
-    def write_config(config):
-        with open(CONFIG_FILE_NAME, "w") as config_file: config_file.write(yson.dumps(config))
-
+    # Map operation spec.
     spec={
         "enable_job_proxy_memory_control": False,
         "job_count": args.job_count,
@@ -290,23 +389,23 @@ if __name__ == "__main__":
         "job_proxy_memory_control": False,
         "mapper": {"memory_limit": args.memory_limit}}
     if args.user_slots: spec["resource_limits"] = {"user_slots": args.user_slots}
-    
+
     # Copy tablet pivot keys from source table.
     pivot_keys = sorted([tablet["pivot_key"] for tablet in tablets])
     #if len(pivot_keys) > 1 and pivot_keys[1][0] == -2**63: pivot_keys = pivot_keys[0] + pivot_keys[2:]
 
     if args.copy:
         # Create destination table.
-        yt.create_table(dst)
-        yt.set_attribute(dst, "schema", Transform.schema(schema))
-        yt.set_attribute(dst, "key_columns", Transform.key_columns(key_columns))
-        yt.reshard_table(dst, Transform.pivot_keys(pivot_keys))
-        yt.mount_table(dst)
-        while not all(x["state"] == "mounted" for x in yt.get(dst + "/@tablets")):
+        yt_dst.create_table(dst)
+        yt_dst.set_attribute(dst, "schema", Transform.schema(schema))
+        yt_dst.set_attribute(dst, "key_columns", Transform.key_columns(key_columns))
+        yt_dst.reshard_table(dst, Transform.pivot_keys(pivot_keys))
+        yt_dst.mount_table(dst)
+        while not mounted(yt_dst, dst):
             sleep(1)
 
         config["destination"] = dst
-        config["command"] = "insert_rows"
+        config["mode"] = "insert"
         config["select_columns"] = Transform.select_columns(schema)
         write_config(config)
 
@@ -324,7 +423,7 @@ if __name__ == "__main__":
             raise e
     else:
         config["destination"] = src
-        config["command"] = "delete_rows"
+        config["mode"] = "delete"
         config["select_columns"] = ",".join([x["name"] for x in schema if "expression" not in x.keys() and x["name"] in key_columns])
         write_config(config)
 
@@ -341,6 +440,6 @@ if __name__ == "__main__":
             print str(e)
             raise e
 
-    yt.remove(regions_table)   
+    yt.remove(regions_table)
     yt.remove(out_regions_table)
 
