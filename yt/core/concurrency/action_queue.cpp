fa2c65a556422c1515361eae7ba5c2524dc3329e
Maxim Babenko <babenko@ytsaurus.tech>
2015-05-13 00:47:34 +0300

    Another fix in TSerializedInvoker

--- yt/core/concurrency/action_queue.cpp
+++ yt/core/concurrency/action_queue.cpp
@@ -348,7 +348,6 @@ class TSerializedInvoker
 public:
     explicit TSerializedInvoker(IInvokerPtr underlyingInvoker)
         : TInvokerWrapper(std::move(underlyingInvoker))
-        , FinishedCallback_(BIND(&TSerializedInvoker::OnFinished, MakeWeak(this)))
     {
         Lock_.clear();
     }
@@ -362,10 +361,6 @@ public:
 private:
     TLockFreeQueue<TClosure> Queue_;
     std::atomic_flag Lock_;
-    bool LockReleased_;
-    TClosure FinishedCallback_;
-
-    static PER_THREAD TSerializedInvoker* CurrentRunningInvoker_;
 
 
     class TInvocationGuard
@@ -376,6 +371,12 @@ private:
         { }
 
         TInvocationGuard(TInvocationGuard&& other) = default;
+        TInvocationGuard(const TInvocationGuard& other) = delete;
+
+        void Reset()
+        {
+            Owner_.Reset();
+        }
 
         ~TInvocationGuard()
         {
@@ -397,18 +398,19 @@ private:
 
         if (!Lock_.test_and_set(std::memory_order_acquire)) {
             UnderlyingInvoker_->Invoke(BIND(
-                &TSerializedInvoker::RunCallbacks,
+                &TSerializedInvoker::RunCallback,
                 MakeStrong(this),
                 Passed(TInvocationGuard(this))));
         }
     }
 
-    void RunCallbacks(TInvocationGuard /*invocationGuard*/)
+    void RunCallback(TInvocationGuard invocationGuard)
     {
         TCurrentInvokerGuard currentInvokerGuard(this);
-        TContextSwitchedGuard contextSwitchGuard(FinishedCallback_);
-
-        LockReleased_ = false;
+        TContextSwitchedGuard contextSwitchGuard(BIND(
+            &TSerializedInvoker::OnContextSwitched,
+            MakeStrong(this),
+            &invocationGuard));
 
         TClosure callback;
         if (Queue_.Dequeue(&callback)) {
@@ -416,13 +418,16 @@ private:
         }
     }
 
+    void OnContextSwitched(TInvocationGuard* invocationGuard)
+    {
+        invocationGuard->Reset();
+        OnFinished();
+    }
+
     void OnFinished()
     {
-        if (!LockReleased_) {
-            LockReleased_ = true;
-            Lock_.clear(std::memory_order_release);
-            TrySchedule();
-        }
+        Lock_.clear(std::memory_order_release);
+        TrySchedule();
     }
 
 };
@@ -556,6 +561,7 @@ private:
         { }
 
         TInvocationGuard(TInvocationGuard&& other) = default;
+        TInvocationGuard(const TInvocationGuard& other) = delete;
 
         ~TInvocationGuard()
         {
