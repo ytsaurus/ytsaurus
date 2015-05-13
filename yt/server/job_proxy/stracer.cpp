40e7f7c2ab459259a3ef475866eb3c75bc7ecbce
Alexandr Priymak <tramsmm@yandex-team.ru>
2015-05-13 19:33:29 +0300

    Fix stracer deadlock
    * Do not use serialize invoker with subprocess. It does not quite work.
    * Use TThreadPool to increase user waiting time

--- yt/server/job_proxy/stracer.cpp
+++ yt/server/job_proxy/stracer.cpp
@@ -1,6 +1,8 @@
 #include "stracer.h"
 #include "private.h"
 
+#include <core/concurrency/action_queue.h>
+
 #include <core/misc/subprocess.h>
 #include <core/misc/proc.h>
 
@@ -20,13 +22,14 @@ using namespace NConcurrency;
 static const auto& Logger = JobProxyLogger;
 static const TDuration TraceInterval = TDuration::Seconds(5);
 static const TDuration TraceTimeout = TDuration::Seconds(10);
+static const int StraceConcurrencyFactor = 4;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 TStracerResult TStraceTool::operator()(const std::vector<int>& pids) const
 {
     SafeSetUid(0);
-    return NJobProxy::Strace(pids);;
+    return Strace(pids);;
 }
 
 REGISTER_TOOL(TStraceTool);
@@ -84,65 +87,83 @@ void Deserialize(TStracerResult& value, INodePtr node)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+TStrace DoStrace(int pid)
+{
+    TStrace trace;
+
+    try {
+        trace.ProcessName = GetProcessName(pid);
+        trace.ProcessCommandLine = GetProcessCommandLine(pid);
+    } catch (const std::exception& ex) {
+        LOG_ERROR(ex, "Failed to get process information for %v", pid);
+    }
+
+    TSubprocess tracer("/usr/bin/strace");
+    tracer.AddArguments({
+        "-tt",
+        "-p",
+        ToString(pid)
+    });
+
+    auto cookie = TDelayedExecutor::Submit(BIND([&] () {
+            try {
+                tracer.Kill(2);
+            } catch (const std::exception& ex) {
+                LOG_ERROR(ex, "Failed to interrupt stracer %v", pid);
+            }
+        }),
+        TraceInterval);
+
+    auto killCookie = TDelayedExecutor::Submit(BIND([&] () {
+            try {
+                tracer.Kill(9);
+            } catch (const std::exception& ex) {
+                LOG_ERROR(ex, "Failed to kill stracer %v", pid);
+            }
+        }),
+        TraceTimeout);
+
+    TSubprocessResult tracerResult;
+    try {
+        tracerResult = tracer.Execute();
+
+        TDelayedExecutor::Cancel(cookie);
+        TDelayedExecutor::Cancel(killCookie);
+    } catch (const std::exception&) {
+        TDelayedExecutor::Cancel(cookie);
+        TDelayedExecutor::Cancel(killCookie);
+        throw;
+    }
+
+    if (!tracerResult.Status.IsOK()) {
+        THROW_ERROR_EXCEPTION("Failed to strace %v", pid)
+            << TErrorAttribute("stderr", ToString(tracerResult.Error))
+            << tracerResult.Status;
+    }
+
+    trace.Trace = Stroka(tracerResult.Error.Begin(), tracerResult.Error.End());
+    return trace;
+}
+
 TStracerResult Strace(const std::vector<int>& pids)
 {
     TStracerResult result;
 
+    auto pool = New<TThreadPool>(StraceConcurrencyFactor, "StracePool");
+
+    std::map<int, TFuture<TStrace>> traceFutures;
+
     for (const auto& pid : pids) {
-        TStrace trace;
+        traceFutures[pid] =
+            BIND([=] () {
+                return DoStrace(pid);
+            })
+            .AsyncVia(pool->GetInvoker())
+            .Run();
+    }
 
-        try {
-            trace.ProcessName = GetProcessName(pid);
-            trace.ProcessCommandLine = GetProcessCommandLine(pid);
-        } catch (const std::exception& ex) {
-            LOG_ERROR(ex, "Failed to get process information for %v", pid);
-        }
-
-        TSubprocess tracer("/usr/bin/strace");
-        tracer.AddArguments({
-            "-tt",
-            "-p",
-            ToString(pid)
-        });
-
-        auto cookie = TDelayedExecutor::Submit(BIND([&] () {
-                try {
-                    tracer.Kill(2);
-                } catch (const std::exception& ex) {
-                    LOG_ERROR(ex, "Failed to interrupt stracer %v", pid);
-                }
-            }),
-            TraceInterval);
-
-        auto killCookie = TDelayedExecutor::Submit(BIND([&] () {
-                try {
-                    tracer.Kill(9);
-                } catch (const std::exception& ex) {
-                    LOG_ERROR(ex, "Failed to kill stracer %v", pid);
-                }
-            }),
-            TraceTimeout);
-
-        TSubprocessResult tracerResult;
-        try {
-            tracerResult = tracer.Execute();
-
-            TDelayedExecutor::Cancel(cookie);
-            TDelayedExecutor::Cancel(killCookie);
-        } catch (const std::exception&) {
-            TDelayedExecutor::Cancel(cookie);
-            TDelayedExecutor::Cancel(killCookie);
-            throw;
-        }
-
-        if (!tracerResult.Status.IsOK()) {
-            THROW_ERROR_EXCEPTION("Failed to strace %v", pid)
-                << TErrorAttribute("stderr", ToString(tracerResult.Error))
-                << tracerResult.Status;
-        }
-
-        trace.Trace = Stroka(tracerResult.Error.Begin(), tracerResult.Error.End());
-        result.Traces[pid] = trace;
+    for (const auto& pid : pids) {
+        result.Traces[pid] = traceFutures[pid].Get().ValueOrThrow();;
     }
     return result;
 }
