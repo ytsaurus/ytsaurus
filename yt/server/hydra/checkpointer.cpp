d8856adfa28bd68a3833c2186fdcc31c395003ad
Maxim Babenko <babenko@ytsaurus.tech>
2015-05-15 12:02:39 +0300

    Tidy up logging

--- yt/server/hydra/checkpointer.cpp
+++ yt/server/hydra/checkpointer.cpp
@@ -180,13 +180,8 @@ private:
             for (TPeerId id2 = id1 + 1; id2 < SnapshotChecksums_.size(); ++id2) {
                 const auto& checksum2 = SnapshotChecksums_[id2];
                 if (checksum1 && checksum2 && checksum1 != checksum2) {
-                    LOG_ERROR(
-                        "Snapshot %v checksum mismatch: "
-                        "peer %v reported %v, "
-                        "peer %v reported %v",
-                        Version_.SegmentId + 1,
-                        id1, *checksum1,
-                        id2, *checksum2);
+                    LOG_ERROR("Snapshot checksum mismatch (SnapshotId: %v)"
+                        Version_.SegmentId + 1);
                 }
             }
         }
