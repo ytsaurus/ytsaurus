9c5e87a84e328eb2c0e87fc1830b6d0c3563bc4c
Lewis Brown <lbrown@yandex-team.ru>
2015-05-18 19:01:44 +0300

    Implement sum and min as builtin UDAFs

--- yt/ytlib/query_client/udf/builtin_aggregates.c
+++ yt/ytlib/query_client/udf/builtin_aggregates.c
@@ -65,13 +65,6 @@ void avg_finalize(
     }
 }
 
-void max_init(
-    TExecutionContext* context,
-    TUnversionedValue* result)
-{
-    result->Type = Null;
-}
-
 static int string_less_than(
     TUnversionedValue* string1,
     TUnversionedValue* string2)
@@ -87,6 +80,13 @@ static int string_less_than(
     return (cmp_result < 0) || (cmp_result == 0 && length1_is_less);
 }
 
+void max_init(
+    TExecutionContext* context,
+    TUnversionedValue* result)
+{
+    result->Type = Null;
+}
+
 static void max_iteration(
     TExecutionContext* context,
     TUnversionedValue* result,
@@ -97,7 +97,7 @@ static void max_iteration(
         result->Type = state->Type;
         result->Length = state->Length;
         result->Data = state->Data;
-    } else if (state->Type == Null 
+    } else if (state->Type == Null
         || (newValue->Type == Int64 && state->Data.Int64 < newValue->Data.Int64)
         || (newValue->Type == Uint64 && state->Data.Uint64 < newValue->Data.Uint64)
         || (newValue->Type == Double && state->Data.Double < newValue->Data.Double)
@@ -138,5 +138,124 @@ void max_finalize(
 {
     result->Type = state->Type;
     result->Length = state->Length;
+    result->Data = state->Data;
+}
+
+void min_init(
+    TExecutionContext* context,
+    TUnversionedValue* result)
+{
+    result->Type = Null;
+}
+
+static void min_iteration(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* state,
+    TUnversionedValue* newValue)
+{
+    if (newValue->Type == Null) {
+        result->Type = state->Type;
+        result->Length = state->Length;
+        result->Data = state->Data;
+    } else if (state->Type == Null
+        || (newValue->Type == Int64 && state->Data.Int64 >= newValue->Data.Int64)
+        || (newValue->Type == Uint64 && state->Data.Uint64 >= newValue->Data.Uint64)
+        || (newValue->Type == Double && state->Data.Double >= newValue->Data.Double)
+        || (newValue->Type == String && !string_less_than(state, newValue)))
+    {
+        result->Type = newValue->Type;
+        result->Length = newValue->Length;
+        result->Data = newValue->Data;
+    } else {
+        result->Type = state->Type;
+        result->Length = state->Length;
+        result->Data = state->Data;
+    }
+}
+
+void min_update(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* state,
+    TUnversionedValue* newValue)
+{
+    min_iteration(context, result, state, newValue);
+}
+
+void min_merge(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* dstState,
+    TUnversionedValue* state)
+{
+    min_iteration(context, result, dstState, state);
+}
+
+void min_finalize(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* state)
+{
+    result->Type = state->Type;
+    result->Length = state->Length;
+    result->Data = state->Data;
+}
+
+void sum_init(
+    TExecutionContext* context,
+    TUnversionedValue* result)
+{
+    result->Type = Null;
+}
+
+static void sum_iteration(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* state,
+    TUnversionedValue* newValue)
+{
+    if (newValue->Type == Null) {
+        result->Type = state->Type;
+        result->Data = state->Data;
+    } else if (state->Type == Null) {
+        result->Type = newValue->Type;
+        result->Data = newValue->Data;
+    } else if (newValue->Type == Int64) {
+        result->Type = state->Type;
+        result->Data.Int64 = state->Data.Int64 + newValue->Data.Int64;
+    } else if (newValue->Type == Uint64) {
+        result->Type = state->Type;
+        result->Data.Uint64 = state->Data.Uint64 + newValue->Data.Uint64;
+    } else if (newValue->Type == Double) {
+        result->Type = state->Type;
+        result->Data.Double = state->Data.Double + newValue->Data.Double;
+    }
+}
+
+void sum_update(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* state,
+    TUnversionedValue* newValue)
+{
+    sum_iteration(context, result, state, newValue);
+}
+
+void sum_merge(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* dstState,
+    TUnversionedValue* state)
+{
+    sum_iteration(context, result, dstState, state);
+}
+
+void sum_finalize(
+    TExecutionContext* context,
+    TUnversionedValue* result,
+    TUnversionedValue* state)
+{
+    result->Type = state->Type;
     result->Data = state->Data;
 }
