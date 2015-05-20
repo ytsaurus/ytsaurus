e1b4a2945e6cac08e4ee453b6b54d1203890f8bd
Maxim Babenko <babenko@ytsaurus.tech>
2015-05-20 09:48:42 +0300

    Fix backing store locking

--- yt/server/tablet_node/chunk_store.cpp
+++ yt/server/tablet_node/chunk_store.cpp
@@ -219,8 +219,18 @@ const TChunkMeta& TChunkStore::GetChunkMeta() const
     return ChunkMeta_;
 }
 
+IStorePtr TChunkStore::GetBackingStore()
+{
+    VERIFY_THREAD_AFFINITY_ANY();
+
+    TReaderGuard guard(BackingStoreLock_);
+    return BackingStore_;
+}
+
 void TChunkStore::SetBackingStore(IStorePtr store)
 {
+    VERIFY_THREAD_AFFINITY_ANY();
+
     TWriterGuard guard(BackingStoreLock_);
     BackingStore_ = store;
 }
@@ -580,14 +590,6 @@ TCachedVersionedChunkMetaPtr TChunkStore::PrepareCachedVersionedChunkMeta(IChunk
     }
 
     return cachedMeta;
-}
-
-IStorePtr TChunkStore::GetBackingStore()
-{
-    VERIFY_THREAD_AFFINITY_ANY();
-
-    TReaderGuard guard(PreloadedBlockCacheLock_);
-    return BackingStore_;
 }
 
 IBlockCachePtr TChunkStore::GetBlockCache()
