27b2f7d6e0abdd65163e1528ceff98d7b1e80ff0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-05-27 13:59:12 +0300

    Fix yandex-yt-python binary upload trouble

--- python-git/python/yandex-yt-python/postprocess.sh
+++ python-git/python/yandex-yt-python/postprocess.sh
@@ -3,6 +3,7 @@
 
 export YT_PROXY=locke.yt.yandex.net
 export YT_TOKEN=1da6afc98d189e8ba59d2ea39f29d0f1 #teamcity user
+export PYTHONPATH="."
 DEST="//home/files"
 YT="yt/wrapper/yt"
 UBUNTU_VERSION="$(lsb_release --short --codename )"
