c7ea656711e0121697e388eb23f6bc83362a17ec
Lewis Brown <lbrown@yandex-team.ru>
2015-06-02 17:58:42 +0300

    Support HyperLogLog on byte arrays

--- yt/core/misc/hyperloglog.h
+++ yt/core/misc/hyperloglog.h
@@ -11,6 +11,8 @@ public:
 
     void Add(ui64 value);
 
+    void Add(char* data, size_t length);
+
     void Merge(HyperLogLog that);
 
     ui64 EstimateCardinality();
@@ -20,6 +22,8 @@ private:
     ui64 RegisterCount_;
     ui64 PrecisionMask_;
     std::vector<long> ZeroCounts_;
+
+    void AddHash(ui64 hash);
 };
 
 } // namespace NYT
