0bb0ead68b07cbaa4fbca4c0ac0ca20b4e6647ff
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2015-06-11 15:16:21 +0300

    Made direct content iterator for ResponseStream Issue: YT-2110

--- python-git/python/yt/wrapper/response_stream.py
+++ python-git/python/yt/wrapper/response_stream.py
@@ -1,3 +1,5 @@
+import common
+
 class ResponseStream(object):
     """Iterator over response"""
     def __init__(self, get_response, iter_content, close, process_error, get_response_parameters):
@@ -11,6 +13,13 @@ class ResponseStream(object):
 
         self._fetch()
         self.response_parameters = get_response_parameters()
+
+    def chunk_iter(self):
+        while True:
+            result = self._read_chunk()
+            if not result:
+                break
+            yield result
 
     def read(self, length=None):
         if length is None:
@@ -47,6 +56,22 @@ class ResponseStream(object):
                 break
         return "".join(result)
 
+    def _read_chunk(self):
+        if not self._buffer and not self._fetch():
+            return ""
+
+        if self._pos == 0:
+            remaining_buffer = self._buffer
+        elif self._pos == len(self._buffer):
+            self._fetch()
+            remaining_buffer = self._buffer
+        else:
+            remaining_buffer = self._buffer[self._pos:]
+
+        self._buffer = ""
+        self._pos = 0
+        return remaining_buffer
+
     def _fetch(self):
         try:
             self._buffer = self._iter_content.next()
@@ -74,6 +99,9 @@ class ResponseStream(object):
 class EmptyResponseStream(object):
     def read(self, length=None):
         return ""
+
+    def chunk_iter(self):
+        return common.EMPTY_GENERATOR
 
     def readline(self):
         return ""
