1b6f599f90b84640a73c599b39cedbd3ea0dd7e0
Maxim Babenko <babenko@ytsaurus.tech>
2015-06-13 10:26:57 +0300

    Remove invalid tests

--- tests/integration/tests/test_racks.py
+++ tests/integration/tests/test_racks.py
@@ -129,38 +129,20 @@ class TestRacks(YTEnvSetup):
         n = get_nodes()[0]
         with pytest.raises(YtError): self._set_rack(n, "r")
 
-    def test_regular_not_enough_racks(self):
-        self._init_n_racks(1)
-        create("file", "//tmp/file")
-        with pytest.raises(YtError): write_file("//tmp/file", self.FILE_DATA, file_writer={"upload_replication_factor": 3})
-        
-    def test_regular_enough_racks(self):
+    def test_write_regular(self):
         self._init_n_racks(2)
         create("file", "//tmp/file")
         write_file("//tmp/file", self.FILE_DATA, file_writer={"upload_replication_factor": 3})
 
-        
-    def test_erasure_not_enough_racks(self):
-        self._init_n_racks(5)
-        create("file", "//tmp/file", attributes={"erasure_codec": "lrc_12_2_2"})
-        with pytest.raises(YtError): write_file("//tmp/file", self.FILE_DATA)
-        
-    def test_erasure_enough_racks(self):
+    def test_write_erasure(self):
         self._init_n_racks(6)
         create("file", "//tmp/file", attributes={"erasure_codec": "lrc_12_2_2"})
         write_file("//tmp/file", self.FILE_DATA)
 
-
-    def test_journal_not_enough_racks(self):
-        self._init_n_racks(2)
-        create("journal", "//tmp/j")
-        with pytest.raises(YtError): write_journal("//tmp/j", self.JOURNAL_DATA)
-
-    def test_journal_enough_racks(self):
+    def test_write_journal(self):
         self._init_n_racks(3)
         create("journal", "//tmp/j")
         write_journal("//tmp/j", self.JOURNAL_DATA)
-
         
     def test_unsafely_placed(self):
         create("file", "//tmp/file", file_writer={"upload_replication_factor": 3})
