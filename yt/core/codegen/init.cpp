78731c4c4d86e73ee95b4ce241d6f339932e281e
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2015-06-16 14:29:10 +0300

    YT-1900: Remove workaround for gcc < 4.9

--- yt/core/codegen/init.cpp
+++ yt/core/codegen/init.cpp
@@ -7,17 +7,6 @@
 
 #include <mutex>
 
-// XXX(lukyan): remove after migrating to gcc 4.9
-#if defined(__GNUC__) && !defined(__clang__) && __GNUC__ == 4 && __GNUC_MINOR__ < 9
-namespace std {
-
-void __throw_out_of_range_fmt(char const*, ...)
-{ }
-
-} // namespace std
-
-#endif
-
 namespace NYT {
 namespace NCodegen {
 
