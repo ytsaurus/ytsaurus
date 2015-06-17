75b7b0e64f7ffbbe682f60ba1c2bef2ee42ef9eb
Lewis Brown <lbrown@yandex-team.ru>
2015-06-17 13:17:36 +0300

    Test simple calling convention shared object udaf

--- yt/unittests/udf/test_udfs_so.c
+++ yt/unittests/udf/test_udfs_so.c
@@ -14,3 +14,43 @@ int64_t exp_udf_so(TExecutionContext* context, int64_t n, int64_t m)
     return result;
 }
 
+uint64_t max_udaf_init(
+    TExecutionContext* context)
+{
+    return 0;
+}
+
+static uint64_t max_udaf_iteration(
+    TExecutionContext* context,
+    uint64_t state,
+    uint64_t newValue)
+{
+    if (state < newValue) {
+        return newValue;
+    } else {
+        return state;
+    }
+}
+
+uint64_t max_udaf_update(
+    TExecutionContext* context,
+    uint64_t state,
+    uint64_t newValue)
+{
+    return max_udaf_iteration(context, state, newValue);
+}
+
+uint64_t max_udaf_merge(
+    TExecutionContext* context,
+    uint64_t dstState,
+    uint64_t state)
+{
+    return max_udaf_iteration(context, dstState, state);
+}
+
+uint64_t max_udaf_finalize(
+    TExecutionContext* context,
+    uint64_t state)
+{
+    return state;
+}
