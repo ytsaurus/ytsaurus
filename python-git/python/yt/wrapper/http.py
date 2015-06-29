42b86535c0697c01a259bea54f662bd1864668f0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-06-30 01:26:15 +0300

    I hope that I finally fix setting retry flag in python wrapper

--- python-git/python/yt/wrapper/http.py
+++ python-git/python/yt/wrapper/http.py
@@ -140,7 +140,7 @@ def make_request_with_retries(method, url, make_retries=True, retry_unavailable_
             message =  "HTTP %s request %s has failed with error %s, message: '%s', headers: %s" % (method, url, type(error), str(error), headers)
             if make_retries and attempt + 1 < get_config(client)["proxy"]["request_retry_count"]:
                 if retry_action is not None:
-                    retry_action()
+                    retry_action(kwargs)
                 backoff = get_backoff(get_config(client)["proxy"]["request_retry_timeout"], current_time)
                 if backoff:
                     logger.warning("%s. Sleep for %.2lf seconds...", message, backoff)
