52576f8c8749b8a4d4dc2c6a79ecfe7ed761835d
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2015-06-29 23:05:40 +0300

    Wrap queue flush in 100 iterations, for cascaded dtors

--- yt/core/actions/invoker_util.cpp
+++ yt/core/actions/invoker_util.cpp
@@ -86,8 +86,10 @@ void ShutdownFinalizerThread()
 {
     if (FinalizerThread.HasValue()) {
         // Await completion of every action enqueued before this shutdown call.
-        auto sentinel = BIND([] () {}).AsyncVia(FinalizerThread->GetInvoker()).Run();
-        sentinel.Get().ThrowOnError();
+        for (int i = 0; i < 100; ++i) {
+            auto sentinel = BIND([] () {}).AsyncVia(FinalizerThread->GetInvoker()).Run();
+            sentinel.Get().ThrowOnError();
+        }
         // Now kill the thread.
         FinalizerThread->Shutdown();
     }
