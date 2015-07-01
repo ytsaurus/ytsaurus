a07c0250ec1ab9e63bd67fe93127a2681a5f667f
Alexander Dmitriev <monster@yandex-team.ru>
2015-07-01 22:20:35 +0300

    Multiple paths for each job slot

--- yt/server/exec_agent/slot_manager.cpp
+++ yt/server/exec_agent/slot_manager.cpp
@@ -29,10 +29,10 @@ static const auto& Logger = ExecAgentLogger;
 TSlotManager::TSlotManager(
     TSlotManagerConfigPtr config,
     TBootstrap* bootstrap)
-    : Config(config)
-    , Bootstrap(bootstrap)
-    , ActionQueue(New<TActionQueue>("ExecSlots"))
-    , IsEnabled(true)
+    : Config_(config)
+    , Bootstrap_(bootstrap)
+    , ActionQueue_(New<TActionQueue>("ExecSlots"))
+    , IsEnabled_(true)
 {
     YCHECK(config);
     YCHECK(bootstrap);
@@ -46,7 +46,7 @@ void TSlotManager::Initialize(int slotCount)
     bool jobControlEnabled = false;
 
 #if defined(_unix_) && !defined(_darwin_)
-    if (Config->EnforceJobControl) {
+    if (Config_->EnforceJobControl) {
         uid_t ruid, euid, suid;
         YCHECK(getresuid(&ruid, &euid, &suid) == 0);
         if (suid != 0) {
@@ -57,55 +57,73 @@ void TSlotManager::Initialize(int slotCount)
     }
 #endif
 
-    try {
-        auto nodeRpcPort = Bootstrap->GetConfig()->RpcPort;
+    SlotPathCounters_.resize(Config_->Paths.size());
 
-        const auto& execAgentConfig = Bootstrap->GetConfig()->ExecAgent;
-        Config->EnableCGroups = execAgentConfig->EnableCGroups;
-        Config->SupportedCGroups = execAgentConfig->SupportedCGroups;
+    try {
+        auto nodeRpcPort = Bootstrap_->GetConfig()->RpcPort;
+
+        const auto& execAgentConfig = Bootstrap_->GetConfig()->ExecAgent;
+        Config_->EnableCGroups = execAgentConfig->EnableCGroups;
+        Config_->SupportedCGroups = execAgentConfig->SupportedCGroups;
 
         for (int slotId = 0; slotId < slotCount; ++slotId) {
             auto slotName = ToString(slotId);
-            auto slotPath = NFS::CombinePaths(Config->Path, slotName);
+            std::vector<Stroka> slotPaths;
+            for (const auto& path : Config_->Paths) {
+                slotPaths.push_back(NFS::CombinePaths(path, slotName));
+            }
             TNullable<int> userId(Null);
             if (jobControlEnabled) {
-                userId = Config->StartUid + slotId;
+                userId = Config_->StartUid + slotId;
             }
             auto slot = New<TSlot>(
-                Config,
-                slotPath,
+                Config_,
+                slotPaths,
                 Format("yt-node-%v", nodeRpcPort),
-                ActionQueue->GetInvoker(),
+                ActionQueue_->GetInvoker(),
                 slotId,
                 userId);
             slot->Initialize();
-            Slots.push_back(slot);
+            Slots_.push_back(slot);
         }
     } catch (const std::exception& ex) {
         auto error = TError("Failed to initialize slots") << ex;
         LOG_WARNING(error);
-        Bootstrap->GetMasterConnector()->RegisterAlert(error);
-        IsEnabled = false;
+        Bootstrap_->GetMasterConnector()->RegisterAlert(error);
+        IsEnabled_ = false;
     }
 
-    auto chunkCache = Bootstrap->GetChunkCache();
-    IsEnabled &= chunkCache->IsEnabled();
+    auto chunkCache = Bootstrap_->GetChunkCache();
+    IsEnabled_ &= chunkCache->IsEnabled();
 }
 
 TSlotPtr TSlotManager::AcquireSlot()
 {
-    for (auto slot : Slots) {
+    auto pathIndexIt = std::min_element(
+        SlotPathCounters_.begin(),
+        SlotPathCounters_.end());
+    int pathIndex = std::distance(SlotPathCounters_.begin(), pathIndexIt);
+
+    for (auto slot : Slots_) {
         if (slot->IsFree()) {
-            slot->Acquire();
+            ++SlotPathCounters_[pathIndex];
+            slot->Acquire(pathIndex);
             return slot;
         }
     }
     YUNREACHABLE();
 }
 
+void TSlotManager::ReleaseSlot(TSlotPtr slot)
+{
+    auto pathIndex = slot->GetPathIndex();
+    --SlotPathCounters_[pathIndex];
+    slot->Release();
+}
+
 int TSlotManager::GetSlotCount() const
 {
-    return IsEnabled ? static_cast<int>(Slots.size()) : 0;
+    return IsEnabled_ ? static_cast<int>(Slots_.size()) : 0;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
