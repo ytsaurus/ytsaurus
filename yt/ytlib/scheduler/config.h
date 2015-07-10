8e32641fe72ac6f8a529f37b5682e3e8b374f64b
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-07-10 18:34:50 +0300

    Check multichunk files in operation

--- yt/ytlib/scheduler/config.h
+++ yt/ytlib/scheduler/config.h
@@ -104,6 +104,8 @@ public:
     //! Limit on operation execution time.
     TNullable<TDuration> TimeLimit;
 
+    bool CheckMultichunkFiles;
+
     TOperationSpecBase()
     {
         RegisterParameter("intermediate_data_account", IntermediateDataAccount)
@@ -145,6 +147,9 @@ public:
 
         RegisterParameter("scheduling_tag", SchedulingTag)
             .Default();
+
+        RegisterParameter("check_multichunk_files", CheckMultichunkFiles)
+            .Default(true);
 
         SetKeepOptions(true);
 
