234162b7ef8a35969f4572c221b1b80a6fa387ab
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-07-13 16:11:09 +0300

    Fix setting columns attributes

--- python-git/python/yt/wrapper/table.py
+++ python-git/python/yt/wrapper/table.py
@@ -94,6 +94,11 @@ class TablePath(object):
         if end_index is not None and upper_key is not None:
             raise YtError("You could not specify upper key bound and end index simultaneously")
 
+        # NB(ignat): for bakcward compatibility with 0.16
+        if "columns" in attributes:
+            attributes["channel"] = attributes["columns"]
+            del attributes["columns"]
+
         if lower_key is not None:
             attributes["lower_limit"] = {"key": flatten(lower_key)}
         if upper_key is not None:
