dd181c7b5a22dfa9e8e06bc051c211498fe577ce
Maxim Babenko <babenko@ytsaurus.tech>
2015-07-17 18:59:23 +0300

    Refactor block read pipeline. Fix memory leak caused by not copying blocks before putting them into cache.

--- yt/server/data_node/journal_chunk.h
+++ yt/server/data_node/journal_chunk.h
@@ -30,10 +30,18 @@ public:
         i64 priority,
         const TNullable<std::vector<int>>& extensionTags) override;
 
-    virtual TFuture<std::vector<TSharedRef>> ReadBlocks(
+    virtual TFuture<std::vector<TSharedRef>> ReadBlockSet(
+        const std::vector<int>& blockIndexes,
+        i64 priority,
+        bool populateCache,
+        NChunkClient::IBlockCachePtr blockCache) override;
+
+    virtual TFuture<std::vector<TSharedRef>> ReadBlockRange(
         int firstBlockIndex,
         int blockCount,
-        i64 priority) override;
+        i64 priority,
+        bool populateCache,
+        NChunkClient::IBlockCachePtr blockCache) override;
 
     void AttachChangelog(NHydra::IChangelogPtr changelog);
     void DetachChangelog();
@@ -64,7 +72,7 @@ private:
 
     virtual TFuture<void> AsyncRemove() override;
 
-    void DoReadBlocks(
+    void DoReadBlockRange(
         int firstBlockIndex,
         int blockCount,
         TPromise<std::vector<TSharedRef>> promise);
