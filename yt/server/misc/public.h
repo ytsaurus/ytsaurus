80fbb93659f44de3f61e1dfab316c53ed9ec7556
Vladislav Klyachin <klyachin@yandex-team.ru>
2015-07-28 13:39:06 +0300

    Track memory for unflushed blocks in blob writer. YT-2406

--- yt/server/misc/public.h
+++ yt/server/misc/public.h
@@ -9,6 +9,9 @@ namespace NYT {
 template <class ECategory>
 class TMemoryUsageTracker;
 
+template <class ECategory>
+class TMemoryUsageTrackerGuard;
+
 class TServerConfig;
 typedef TIntrusivePtr<TServerConfig> TServerConfigPtr;
 
