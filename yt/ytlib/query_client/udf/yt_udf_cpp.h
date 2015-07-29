9d01b3532fc6093ebfba81dea2658c1cbb0c2630
Lewis Brown <lbrown@yandex-team.ru>
2015-07-29 20:06:32 +0300

    Allow UDFs to throw exceptions

--- yt/ytlib/query_client/udf/yt_udf_cpp.h
+++ yt/ytlib/query_client/udf/yt_udf_cpp.h
@@ -5,3 +5,5 @@
 extern "C" char* AllocatePermanentBytes(TExecutionContext* context, size_t size);
 
 extern "C" char* AllocateBytes(TExecutionContext* context, size_t size);
+
+extern "C" void ThrowException(const char* error);
