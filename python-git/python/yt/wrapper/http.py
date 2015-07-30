3cd27c3d0c6b36de0be0b3ad01ca5b77b6f0b997
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-07-31 01:24:45 +0300

    Fix None token processing in get_user_name. YT-2493

--- python-git/python/yt/wrapper/http.py
+++ python-git/python/yt/wrapper/http.py
@@ -250,3 +250,32 @@ def get_token(client=None):
         token = None
     return token
 
+def get_user_name(token=None, headers=None, client=None):
+    if token is None:
+        token = get_token(client)
+
+    version = get_api_version(client=client)
+    proxy = get_proxy_url(None, client=client)
+
+    if version == "v3":
+        if headers is None:
+            headers = {}
+        if token is not None:
+            headers["Authorization"] = "OAuth " + token.strip()
+        data = None
+        verb = "whoami"
+    else:
+        if not token:
+            return None
+        data = "token=" + token.strip()
+        verb = "login"
+
+    response = make_request_with_retries(
+        "post",
+        "http://{0}/auth/{1}".format(proxy, verb),
+        headers=headers,
+        data=data)
+    login = response.json()["login"]
+    if not login:
+        return None
+    return login
