3eef1d16fa966c0e6d01efc70bf04332200f3027
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2015-08-04 16:51:29 +0300

    Removed proxy url from client test

--- python/yt/wrapper/tests/test_native_mode.py
+++ python/yt/wrapper/tests/test_native_mode.py
@@ -1476,125 +1476,129 @@ class NativeModeTester(YtTestBase, YTEnv):
         other_client.config["proxy"]["force_ipv4"] = True
         other_client.config["tabular_data_format"] = yt.JsonFormat()
 
-        if yt.config["api_version"] == "v2":
-            assert client.get_user_name("") == None
-        else:
-            assert client.get_user_name("") == "root"
+        old_proxy_url = yt.config["proxy"]["url"]
+        try:
+            yt.config["proxy"]["url"] = None
+            if yt.config["api_version"] == "v2":
+                assert client.get_user_name("") == None
+            else:
+                assert client.get_user_name("") == "root"
 
-        client.set(TEST_DIR + "/node", "abc")
-        assert client.get(TEST_DIR + "/node") == "abc"
+            client.set(TEST_DIR + "/node", "abc")
+            assert client.get(TEST_DIR + "/node") == "abc"
 
-        assert client.list(TEST_DIR) == ["node"]
+            assert client.list(TEST_DIR) == ["node"]
 
-        client.remove(TEST_DIR + "/node")
-        assert not client.exists(TEST_DIR + "/node")
+            client.remove(TEST_DIR + "/node")
+            assert not client.exists(TEST_DIR + "/node")
 
-        client.mkdir(TEST_DIR + "/folder")
-        assert client.get_type(TEST_DIR + "/folder") == "map_node"
+            client.mkdir(TEST_DIR + "/folder")
+            assert client.get_type(TEST_DIR + "/folder") == "map_node"
 
-        table = TEST_DIR + "/table"
-        client.create("table", table)
-        client.write_table(table, ["a=b\n"])
-        assert "a=b\n" == client.read_table(table, raw=True).read()
+            table = TEST_DIR + "/table"
+            client.create("table", table)
+            client.write_table(table, ["a=b\n"])
+            assert "a=b\n" == client.read_table(table, raw=True).read()
 
-        assert set(client.search(TEST_DIR)) == set([TEST_DIR, TEST_DIR + "/folder", table])
+            assert set(client.search(TEST_DIR)) == set([TEST_DIR, TEST_DIR + "/folder", table])
 
-        other_table = TEST_DIR + "/other_table"
-        client.copy(table, other_table)
-        assert "a=b\n" == client.read_table(other_table, raw=True).read()
-        client.move(table, TEST_DIR + "/moved_table")
-        assert "a=b\n" == client.read_table(TEST_DIR + "/moved_table", raw=True).read()
-        assert not client.exists(table)
+            other_table = TEST_DIR + "/other_table"
+            client.copy(table, other_table)
+            assert "a=b\n" == client.read_table(other_table, raw=True).read()
+            client.move(table, TEST_DIR + "/moved_table")
+            assert "a=b\n" == client.read_table(TEST_DIR + "/moved_table", raw=True).read()
+            assert not client.exists(table)
 
-        client.link(other_table, TEST_DIR + "/table_link")
-        assert client.get_attribute(TEST_DIR + "/table_link&", "target_id") == \
-               client.get_attribute(other_table, "id")
-        assert client.has_attribute(TEST_DIR + "/table_link&", "broken")
+            client.link(other_table, TEST_DIR + "/table_link")
+            assert client.get_attribute(TEST_DIR + "/table_link&", "target_id") == \
+                   client.get_attribute(other_table, "id")
+            assert client.has_attribute(TEST_DIR + "/table_link&", "broken")
 
-        client.set_attribute(other_table, "test_attr", "value")
-        for attribute in ["id", "test_attr"]:
-            assert attribute in client.list_attributes(other_table)
+            client.set_attribute(other_table, "test_attr", "value")
+            for attribute in ["id", "test_attr"]:
+                assert attribute in client.list_attributes(other_table)
 
-        assert not client.exists(client.find_free_subpath(TEST_DIR))
+            assert not client.exists(client.find_free_subpath(TEST_DIR))
 
-        assert client.check_permission("tester", "write", "//sys")["action"] == "deny"
+            assert client.check_permission("tester", "write", "//sys")["action"] == "deny"
 
-        client.add_member("tester", "testers")
-        assert client.get_attribute("//sys/groups/testers", "members") == ["tester"]
-        client.remove_member("tester", "testers")
-        assert client.get_attribute("//sys/groups/testers", "members") == []
+            client.add_member("tester", "testers")
+            assert client.get_attribute("//sys/groups/testers", "members") == ["tester"]
+            client.remove_member("tester", "testers")
+            assert client.get_attribute("//sys/groups/testers", "members") == []
 
-        client.create_table(TEST_DIR + "/table")
-        assert client.exists(TEST_DIR + "/table")
+            client.create_table(TEST_DIR + "/table")
+            assert client.exists(TEST_DIR + "/table")
 
-        temp_table = client.create_temp_table(TEST_DIR)
-        assert client.get_type(temp_table) == "table"
-        assert client.is_empty(temp_table)
+            temp_table = client.create_temp_table(TEST_DIR)
+            assert client.get_type(temp_table) == "table"
+            assert client.is_empty(temp_table)
 
-        client.write_table(temp_table, self.get_temp_dsv_records())
-        client.run_sort(temp_table, sort_by=["x"])
-        assert client.is_sorted(temp_table)
+            client.write_table(temp_table, self.get_temp_dsv_records())
+            client.run_sort(temp_table, sort_by=["x"])
+            assert client.is_sorted(temp_table)
+            client.run_erase(TablePath(temp_table, start_index=0, end_index=5, client=client))
+            assert client.records_count(temp_table) == 5
 
-        client.run_erase(TablePath(temp_table, start_index=0, end_index=5))
-        assert client.records_count(temp_table) == 5
+            client.run_map("cat", other_table, TEST_DIR + "/map_output")
+            assert "a=b\n" == client.read_table(other_table, raw=True).read()
 
-        client.run_map("cat", other_table, TEST_DIR + "/map_output")
-        assert "a=b\n" == client.read_table(other_table, raw=True).read()
+            client.write_table(TEST_DIR + "/first", ["x=1\n"])
+            client.write_table(TEST_DIR + "/second", ["x=2\n"])
+            client.run_merge([TEST_DIR + "/first", TEST_DIR + "/second"], TEST_DIR + "/merged_table")
+            assert client.read_table(TEST_DIR + "/merged_table").read() == "x=1\nx=2\n"
 
-        client.write_table(TEST_DIR + "/first", ["x=1\n"])
-        client.write_table(TEST_DIR + "/second", ["x=2\n"])
-        client.run_merge([TEST_DIR + "/first", TEST_DIR + "/second"], TEST_DIR + "/merged_table")
-        assert client.read_table(TEST_DIR + "/merged_table").read() == "x=1\nx=2\n"
+            client.run_reduce("head -n 3", temp_table, TEST_DIR + "/reduce_output", reduce_by=["x"])
+            assert client.records_count(TEST_DIR + "/reduce_output") == 3
 
-        client.run_reduce("head -n 3", temp_table, TEST_DIR + "/reduce_output", reduce_by=["x"])
-        assert client.records_count(TEST_DIR + "/reduce_output") == 3
+            mr_operation = client.run_map_reduce("cat", "head -n 3", temp_table, TEST_DIR + "/mapreduce_output",
+                                                 reduce_by=["x"])
+            assert client.get_operation_state(mr_operation.id) == "completed"
+            assert client.records_count(TEST_DIR + "/mapreduce_output") == 3
 
-        mr_operation = client.run_map_reduce("cat", "head -n 3", temp_table, TEST_DIR + "/mapreduce_output",
-                                             reduce_by=["x"])
-        assert client.get_operation_state(mr_operation.id) == "completed"
-        assert client.records_count(TEST_DIR + "/mapreduce_output") == 3
+            with client.Transaction():
+                client.set("//@attr", 10)
+                assert client.exists("//@attr")
 
-        with client.Transaction():
-            yt.set("//@attr", 10)
-            assert yt.exists("//@attr")
+            with client.PingableTransaction():
+                client.set("//@other_attr", 10)
+                assert client.exists("//@other_attr")
 
-        with client.PingableTransaction():
-            yt.set("//@other_attr", 10)
-            assert yt.exists("//@other_attr")
+            tx = client.start_transaction(timeout=5000)
+            with client.PingTransaction(tx, delay=1):
+                assert client.exists("//sys/transactions/{0}".format(tx))
+                client.TRANSACTION = tx
+                assert client.lock(table) != "0-0-0-0"
+                client.TRANSACTION = "0-0-0-0"
 
-        tx = client.start_transaction(timeout=5000)
-        with client.PingTransaction(tx, delay=1):
-            assert client.exists("//sys/transactions/{0}".format(tx))
-            client.TRANSACTION = tx
-            assert client.lock(table) != "0-0-0-0"
-            client.TRANSACTION = "0-0-0-0"
+            client.ping_transaction(tx)
+            client.abort_transaction(tx)
+            with pytest.raises(yt.YtError):
+                client.commit_transaction(tx)
 
-        client.ping_transaction(tx)
-        client.abort_transaction(tx)
-        with pytest.raises(yt.YtError):
-            client.commit_transaction(tx)
+            op = client.run_map("sleep 10; cat", temp_table, table, sync=False)
+            assert not client.get_operation_state(op.id).is_unsuccessfully_finished()
+            assert op.get_attributes()["state"] != "failed"
+            time.sleep(0.5)
+            client.suspend_operation(op.id)
+            time.sleep(2.5)
+            client.resume_operation(op.id)
+            time.sleep(2.5)
+            client.abort_operation(op.id)
+            # Second abort on aborted operation should be silent
+            client.abort_operation(op.id)
+            assert op.get_progress()["total"] != 0
+            assert op.get_stderrs() == []
 
-        op = client.run_map("sleep 10; cat", temp_table, table, sync=False)
-        assert not client.get_operation_state(op.id).is_unsuccessfully_finished()
-        assert op.get_attributes()["state"] != "failed"
-        time.sleep(0.5)
-        client.suspend_operation(op.id)
-        time.sleep(2.5)
-        client.resume_operation(op.id)
-        time.sleep(2.5)
-        client.abort_operation(op.id)
-        # Second abort on aborted operation should be silent
-        client.abort_operation(op.id)
-        assert op.get_progress()["total"] != 0
-        assert op.get_stderrs() == []
+            client.upload_file("0" * 1000, TEST_DIR + "/file")
+            assert client.download_file(TEST_DIR + "/file").read() == "0" * 1000
+            with pytest.raises(yt.YtError):
+                client.smart_upload_file("/unexisting")
 
-        client.upload_file("0" * 1000, TEST_DIR + "/file")
-        assert client.download_file(TEST_DIR + "/file").read() == "0" * 1000
-        with pytest.raises(yt.YtError):
-            client.smart_upload_file("/unexisting")
-
-        assert other_client.get("/")
-        assert '{"a":"b"}\n' == other_client.read_table(other_table, raw=True).read()
+            assert other_client.get("/")
+            assert '{"a":"b"}\n' == other_client.read_table(other_table, raw=True).read()
+        finally:
+            yt.config["proxy"]["url"] = old_proxy_url
 
     def test_client_with_unknown_api_version(self):
         client = Yt(config=self.config)
