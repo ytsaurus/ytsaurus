cd0a9ce7dcc3e8c44e5af8e5ee7ca3fdc9ee450b
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-08-20 12:16:57 +0300

    Fix upload_file in client for backward compatibility

--- python/yt/wrapper/client.py
+++ python/yt/wrapper/client.py
@@ -9,7 +9,7 @@ from table_commands import create_table, create_temp_table, write_table, read_ta
                            run_map, run_reduce, run_map_reduce, run_remote_copy, \
                            mount_table, unmount_table, remount_table, reshard_table, select_rows
 from operation_commands import get_operation_state, abort_operation, suspend_operation, resume_operation
-from file_commands import read_file, write_file, smart_upload_file
+from file_commands import read_file, write_file, upload_file, smart_upload_file
 from transaction_commands import start_transaction, abort_transaction, commit_transaction, ping_transaction
 from http import get_user_name
 from transaction import Transaction, PingableTransaction, PingTransaction
@@ -191,7 +191,7 @@ class Yt(object):
 
     def upload_file(self, *args, **kwargs):
         """Deprecated. For backward compatibility only"""
-        return write_file(*args, client=self, **kwargs)
+        return upload_file(*args, client=self, **kwargs)
 
     def smart_upload_file(self, *args, **kwargs):
         return smart_upload_file(*args, client=self, **kwargs)
