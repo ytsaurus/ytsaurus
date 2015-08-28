a72c7b73454136c3bd28bca7b002bbe5f23989a0
Maxim Babenko <babenko@ytsaurus.tech>
2015-08-28 14:10:29 +0300

    Take dynamic snapshot store snapshot during rotation

--- yt/server/tablet_node/private.h
+++ yt/server/tablet_node/private.h
@@ -11,6 +11,12 @@ namespace NTabletNode {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+static const ui32 UncommittedRevision = 0;
+static const ui32 InvalidRevision = 0;
+static const ui32 MaxRevision = std::numeric_limits<ui32>::max();
+
+////////////////////////////////////////////////////////////////////////////////
+
 extern const NLogging::TLogger TabletNodeLogger;
 extern const NProfiling::TProfiler TabletNodeProfiler;
 
