3ac3d9c2cb54a33193cef55abe798e82d0c5808c
Maxim Babenko <babenko@ytsaurus.tech>
2015-08-28 14:14:02 +0300

    Cluster connection is mandatory

--- yt/server/job_proxy/config.h
+++ yt/server/job_proxy/config.h
@@ -58,8 +58,7 @@ public:
     {
         RegisterParameter("rpc_server", RpcServer)
             .DefaultNew();
-        RegisterParameter("cluster_connection", ClusterConnection)
-            .DefaultNew();
+        RegisterParameter("cluster_connection", ClusterConnection);
         RegisterParameter("supervisor_connection", SupervisorConnection);
         RegisterParameter("supervisor_rpc_timeout", SupervisorRpcTimeout)
             .Default(TDuration::Seconds(30));
