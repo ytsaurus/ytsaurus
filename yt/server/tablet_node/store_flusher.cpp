1c395f466e4fa5c79db012f3de35e371f2d02ce3
Maxim Babenko <babenko@ytsaurus.tech>
2015-09-02 18:56:49 +0300

    No more double flush for rows crossing rotation boundary

--- yt/server/tablet_node/store_flusher.cpp
+++ yt/server/tablet_node/store_flusher.cpp
@@ -286,12 +286,8 @@ private:
         try {
             LOG_INFO("Store flush started");
 
-            auto reader = store->CreateReader(
-                MinKey(),
-                MaxKey(),
-                AllCommittedTimestamp,
-                TColumnFilter());
-        
+            auto reader = store->CreateFlushReader();
+
             // NB: Memory store reader is always synchronous.
             YCHECK(reader->Open().Get().IsOK());
 
