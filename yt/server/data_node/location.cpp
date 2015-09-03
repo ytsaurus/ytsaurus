b709ae544eb2302f03c8dd8bef01beb02c22dc39
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2015-09-03 13:40:11 +0300

    YT-2742 - Report alert to master for failed disks

--- yt/server/data_node/location.cpp
+++ yt/server/data_node/location.cpp
@@ -129,7 +129,7 @@ std::vector<TChunkDescriptor> TLocation::Scan()
     } catch (const std::exception& ex) {
         auto error = TError("Location scan failed") << ex;
         LOG_ERROR(error);
-        MakeDisabled();
+        MarkAsDisabled(ex);
     }
 
     return result;
@@ -145,7 +145,7 @@ void TLocation::Start()
     } catch (const std::exception& ex) {
         auto error = TError("Location startup failed") << ex;
         LOG_ERROR(error);
-        MakeDisabled();
+        MarkAsDisabled(ex);
     }
 }
 
@@ -332,11 +332,6 @@ void TLocation::CheckLockFile()
         }
     }
 
-    auto masterConnector = Bootstrap_->GetMasterConnector();
-    masterConnector->RegisterAlert(TError("Location at %v is disabled",
-        GetPath())
-        << error);
-
     THROW_ERROR error;
 }
 
@@ -346,8 +341,12 @@ void TLocation::OnHealthCheckFailed(const TError& error)
     YUNREACHABLE(); // Disable() exits the process.
 }
 
-void TLocation::MakeDisabled()
+void TLocation::MarkAsDisabled(const std::exception& ex)
 {
+    auto alert = TError("Location at %v is disabled", GetPath()) << ex;
+    auto masterConnector = Bootstrap_->GetMasterConnector();
+    masterConnector->RegisterAlert(alert);
+
     Enabled_.store(false);
 
     AvailableSpace_ = 0;
