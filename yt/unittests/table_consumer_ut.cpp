dc9d66d1f5a7eed9aafe43283e24bf542613eb02
Pavel Sushin <psushin@ytsaurus.tech>
2015-09-04 18:06:14 +0300

    Treat entities as null in table consumer

---
+++ yt/unittests/table_consumer_ut.cpp
@@ -0,0 +1,42 @@
+#include "stdafx.h"
+#include "framework.h"
+
+#include <ytlib/new_table_client/value_consumer-mock.h>
+#include <ytlib/new_table_client/table_consumer.h>
+#include <ytlib/new_table_client/unversioned_row.h>
+
+#include <core/ytree/fluent.h>
+
+namespace NYT {
+namespace NVersionedTableClient {
+namespace {
+
+using ::testing::InSequence;
+using ::testing::StrictMock;
+using ::testing::NiceMock;
+
+using namespace NYTree;
+using namespace NYson;
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TTableConsumer, EntityAsNull)
+{
+    auto mock = New<StrictMock<TMockValueConsumer>>(New<TNameTable>(), true);
+    EXPECT_CALL(*mock, OnBeginRow());
+    EXPECT_CALL(*mock, OnValue(MakeUnversionedSentinelValue(EValueType::Null, 0)));
+    EXPECT_CALL(*mock, OnEndRow());
+
+    std::unique_ptr<IYsonConsumer> consumer(new TTableConsumer(mock));
+    consumer->OnBeginMap();
+        consumer->OnKeyedItem("a");
+        consumer->OnEntity();
+    consumer->OnEndMap();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NVersionedTableClient
+} // namespace NYT
+
