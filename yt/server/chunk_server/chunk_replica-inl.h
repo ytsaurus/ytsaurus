dc021cc9725f178065a9625da4a9e5eb579360cc
Maxim Babenko <babenko@ytsaurus.tech>
2015-09-12 01:38:03 +0300

    Fix build

--- yt/server/chunk_server/chunk_replica-inl.h
+++ yt/server/chunk_server/chunk_replica-inl.h
@@ -9,64 +9,6 @@ namespace NChunkServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-FORCED_INLINE TChunkReplica::TChunkReplica()
-    : Value(InvalidNodeId | (0 << 28))
-{ }
-
-FORCED_INLINE TChunkReplica::TChunkReplica(ui32 value)
-    : Value(value)
-{ }
-
-FORCED_INLINE TChunkReplica::TChunkReplica(int nodeId, int index)
-    : Value(nodeId | (index << 28))
-{
-    YASSERT(nodeId >= 0 && nodeId <= MaxNodeId);
-    YASSERT(index >= 0 && index < ChunkReplicaIndexBound);
-}
-
-FORCED_INLINE int TChunkReplica::GetNodeId() const
-{
-    return Value & 0x0fffffff;
-}
-
-FORCED_INLINE int TChunkReplica::GetIndex() const
-{
-    return Value >> 28;
-}
-
-FORCED_INLINE void ToProto(ui32* value, TChunkReplica replica)
-{
-    *value = replica.Value;
-}
-
-FORCED_INLINE void FromProto(TChunkReplica* replica, ui32 value)
-{
-    replica->Value = value;
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-FORCED_INLINE TChunkIdWithIndex::TChunkIdWithIndex()
-    : Index(0)
-{ }
-
-FORCED_INLINETChunkIdWithIndex::TChunkIdWithIndex(const TChunkId& id, int index)
-    : Id(id)
-    , Index(index)
-{ }
-
-FORCED_INLINE bool operator == (const TChunkIdWithIndex& lhs, const TChunkIdWithIndex& rhs)
-{
-    return lhs.Id == rhs.Id && lhs.Index == rhs.Index;
-}
-
-FORCED_INLINE bool operator != (const TChunkIdWithIndex& lhs, const TChunkIdWithIndex& rhs)
-{
-    return !(lhs == rhs);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 template <class T>
 FORCED_INLINE TPtrWithIndex<T>::TPtrWithIndex()
     : Value_(0)
