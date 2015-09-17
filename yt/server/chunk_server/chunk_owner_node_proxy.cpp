ba5cef239a341130a275d8a6ed4b9efe64f39a17
Pavel Sushin <psushin@ytsaurus.tech>
2015-09-17 23:29:15 +0300

    Always do traversing in proper queue.
    Eliminate recursion on multiple ranges. Cosmetics.

--- yt/server/chunk_server/chunk_owner_node_proxy.cpp
+++ yt/server/chunk_server/chunk_owner_node_proxy.cpp
@@ -92,12 +92,7 @@ public:
             return;
         }
 
-        TraverseChunkTree(
-            CreatePreemptableChunkTraverserCallbacks(Bootstrap_),
-            this,
-            ChunkList_,
-            Ranges_[CurrentRangeIndex_].LowerLimit(),
-            Ranges_[CurrentRangeIndex_].UpperLimit());
+        TraverseCurrentRange();
     }
 
 private:
@@ -116,6 +111,16 @@ private:
     bool Finished_ = false;
 
     DECLARE_THREAD_AFFINITY_SLOT(AutomatonThread);
+
+    void TraverseCurrentRange()
+    {
+        TraverseChunkTree(
+            CreatePreemptableChunkTraverserCallbacks(Bootstrap_),
+            this,
+            ChunkList_,
+            Ranges_[CurrentRangeIndex_].LowerLimit(),
+            Ranges_[CurrentRangeIndex_].UpperLimit());
+    }
 
     void ReplySuccess()
     {
@@ -278,18 +283,15 @@ private:
     {
         VERIFY_THREAD_AFFINITY(AutomatonThread);
 
-        CurrentRangeIndex_ += 1;
+        if (Finished_) {
+            return;
+        }
+
+        ++CurrentRangeIndex_;
         if (CurrentRangeIndex_ == Ranges_.size()) {
-            if (CurrentRangeIndex_ == Ranges_.size() && !Finished_) {
-                ReplySuccess();
-            }
+            ReplySuccess();
         } else {
-            TraverseChunkTree(
-                CreatePreemptableChunkTraverserCallbacks(Bootstrap_),
-                this,
-                ChunkList_,
-                Ranges_[CurrentRangeIndex_].LowerLimit(),
-                Ranges_[CurrentRangeIndex_].UpperLimit());
+            TraverseCurrentRange();
         }
     }
 
