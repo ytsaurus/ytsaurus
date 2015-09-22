b212a61f1f65549555a5ead0443436c5333d3374
Pavel Sushin <psushin@ytsaurus.tech>
2015-09-23 01:01:04 +0300

    Fixes after merge

--- python-git/python/yt/wrapper/tests/test_operations.py
+++ python-git/python/yt/wrapper/tests/test_operations.py
@@ -286,7 +286,7 @@ class TestOperations(object):
         yt.write_table(table, ["x=1\n", "y=2\n"])
         op = yt.run_map(write_statistics, table, table, format=None, sync=False)
         op.wait()
-        assert op.get_job_statistics()["custom"] == {"row_count": {"$": {"completed": {"map": {"count": 1, "max": 1, "sum": 1, "min": 1}}}}}
+        assert op.get_job_statistics()["custom"] == {"row_count": {"$": {"completed": {"map": {"count": 2, "max": 1, "sum": 2, "min": 1}}}}}
         check(yt.read_table(table), ["x=1\n", "y=2\n"])
 
     @add_failed_operation_stderrs_to_error_message
