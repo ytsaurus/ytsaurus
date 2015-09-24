c9e9796b2af09b94bdc084a1bd604ace0304c770
Maxim Babenko <babenko@ytsaurus.tech>
2015-09-24 15:37:32 +0300

    Fix test

--- tests/integration/tests/test_scheduler_map.py
+++ tests/integration/tests/test_scheduler_map.py
@@ -277,7 +277,7 @@ class TestSchedulerMapCommands(YTEnvSetup):
         assert get(jobs_path + "/@count") == expected_count
         for job_id in ls(jobs_path):
             stderr_path = jobs_path + "/" + job_id + "/stderr"
-            assert get(stderr_path + "/@uncompressed_size") == len(expected_content)
+            assert get(stderr_path + "/@uncompressed_data_size") == len(expected_content)
             assert read_file(stderr_path) == expected_content
 
     # check that stderr is captured for successfull job
