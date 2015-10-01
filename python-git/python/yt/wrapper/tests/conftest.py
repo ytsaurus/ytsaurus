b89895ced15c77b20b6c9fcbd6475c68d08c1b7e
Maxim Babenko <babenko@ytsaurus.tech>
2015-10-01 20:06:33 +0300

    Don't abort cell prerequisite txs

--- python-git/python/yt/wrapper/tests/conftest.py
+++ python-git/python/yt/wrapper/tests/conftest.py
@@ -135,7 +135,7 @@ def test_method_teardown():
 
     for tx in yt.list("//sys/transactions", attributes=["title"]):
         title = tx.attributes.get("title", "")
-        if "Scheduler lock" in title or "Lease for node" in title:
+        if "Scheduler lock" in title or "Lease for node" in title or "Prerequisite for cell" in title:
             continue
         try:
             yt.abort_transaction(tx)
