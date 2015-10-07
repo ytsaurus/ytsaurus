7f86c3abee805c32629106274c12239ca59aa6a5
Maxim Akhmedov <max42@ytsaurus.tech>
2015-10-07 22:31:11 +0300

    Removed YAMR consumer.

--- yt/ytlib/formats/yamr_writer.h
+++ yt/ytlib/formats/yamr_writer.h
@@ -16,79 +16,7 @@ namespace NFormats {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-DEFINE_ENUM(EYamrConsumerState,
-    (None)
-    (ExpectColumnName)
-    (ExpectValue)
-    (ExpectAttributeName)
-    (ExpectAttributeValue)
-    (ExpectEndAttributes)
-    (ExpectEntity)
-);
-
-DEFINE_ENUM(EYamrConsumerValueType,
-    (ExpectKey)
-    (ExpectSubkey)
-    (ExpectValue)
-    (ExpectUnknown)
-);
-
 //! Note: only tabular format is supported.
-class TYamrConsumer
-    : public virtual TFormatsConsumerBase
-{
-public:
-    explicit TYamrConsumer(
-        TOutputStream* stream,
-        TYamrFormatConfigPtr config = New<TYamrFormatConfig>());
-
-    ~TYamrConsumer();
-
-    // IYsonConsumer overrides.
-    virtual void OnStringScalar(const TStringBuf& value) override;
-    virtual void OnInt64Scalar(i64 value) override;
-    virtual void OnUint64Scalar(ui64 value) override;
-    virtual void OnDoubleScalar(double value) override;
-    virtual void OnBooleanScalar(bool value) override;
-    virtual void OnEntity() override;
-    virtual void OnBeginList() override;
-    virtual void OnListItem() override;
-    virtual void OnEndList() override;
-    virtual void OnBeginMap() override;
-    virtual void OnKeyedItem(const TStringBuf& key) override;
-    virtual void OnEndMap() override;
-    virtual void OnBeginAttributes() override;
-    virtual void OnEndAttributes() override;
-
-private:
-    using EState = EYamrConsumerState;
-    using EValueType = EYamrConsumerValueType;
-
-    TOutputStream* Stream;
-    TYamrFormatConfigPtr Config;
-
-    TNullable<TStringBuf> Key;
-    TNullable<TStringBuf> Subkey;
-    TNullable<TStringBuf> Value;
-
-    TYamrTable Table;
-
-    EState State;
-    EValueType ValueType;
-    NTableClient::EControlAttribute ControlAttribute;
-
-    // To store Int64 and Double values converted to strings.
-    std::vector<Stroka> StringStorage_;
-
-    void WriteRow();
-    void WriteInLenvalMode(const TStringBuf& value);
-
-    void EscapeAndWrite(const TStringBuf& value, bool inKey);
-    
-};
-
-////////////////////////////////////////////////////////////////////////////////
-
 class TSchemalessYamrWriter
     : public TSchemalessFormatWriterBase {
 public:
