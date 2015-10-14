bd908f7e65c91c4c2c72b51a00e21b65932878ff
Maxim Babenko <babenko@ytsaurus.tech>
2015-10-14 11:40:19 +0300

    More changelog store retention fixes

--- yt/server/hydra/decorated_automaton.cpp
+++ yt/server/hydra/decorated_automaton.cpp
@@ -625,6 +625,8 @@ TDecoratedAutomaton::TDecoratedAutomaton(
 
     BuildingSnapshot_.clear();
     Reset();
+
+    AutomatonInvoker_->Invoke(BIND(&IAutomaton::Clear, Automaton_));
 }
 
 void TDecoratedAutomaton::OnStartLeading()
@@ -679,15 +681,6 @@ IInvokerPtr TDecoratedAutomaton::GetSystemInvoker()
     VERIFY_THREAD_AFFINITY_ANY();
 
     return SystemInvoker_;
-}
-
-void TDecoratedAutomaton::Clear()
-{
-    VERIFY_THREAD_AFFINITY(AutomatonThread);
-
-    Automaton_->Clear();
-    Reset();
-    AutomatonVersion_ = TVersion();
 }
 
 TFuture<void> TDecoratedAutomaton::SaveSnapshot(IAsyncOutputStreamPtr writer)
