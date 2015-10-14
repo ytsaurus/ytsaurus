ad8b5e1dc58240eb6d9326f808d0006e8590cefd
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2015-10-14 14:59:52 +0300

    Partially revert a731166
    Moving automaton Clear() call into constructor breaks world initialization.
    For now retain only changelog rotation fix.

--- yt/server/hydra/decorated_automaton.cpp
+++ yt/server/hydra/decorated_automaton.cpp
@@ -623,8 +623,6 @@ TDecoratedAutomaton::TDecoratedAutomaton(
 
     BuildingSnapshot_.clear();
     Reset();
-
-    AutomatonInvoker_->Invoke(BIND(&IAutomaton::Clear, Automaton_));
 }
 
 void TDecoratedAutomaton::OnStartLeading()
@@ -679,6 +677,15 @@ IInvokerPtr TDecoratedAutomaton::GetSystemInvoker()
     VERIFY_THREAD_AFFINITY_ANY();
 
     return SystemInvoker_;
+}
+
+void TDecoratedAutomaton::Clear()
+{
+    VERIFY_THREAD_AFFINITY(AutomatonThread);
+
+    Automaton_->Clear();
+    Reset();
+    AutomatonVersion_ = TVersion();
 }
 
 TFuture<void> TDecoratedAutomaton::SaveSnapshot(IAsyncOutputStreamPtr writer)
