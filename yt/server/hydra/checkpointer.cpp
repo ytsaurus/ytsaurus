cd850e8d6c2b6e40ee1ec5d8fa741c53fa30d134
Maxim Babenko <babenko@ytsaurus.tech>
2015-10-15 12:47:18 +0300

    Fix constness

--- yt/server/hydra/checkpointer.cpp
+++ yt/server/hydra/checkpointer.cpp
@@ -65,7 +65,7 @@ public:
 
 private:
     // NB: TSession cannot outlive its owner.
-    const TCheckpointer* Owner_;
+    TCheckpointer* const Owner_;
     const bool BuildSnapshot_;
     const NLogging::TLogger Logger;
 
