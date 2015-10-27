287fd11a48d8889a1091d92d1124e69778736297
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2015-10-27 18:25:58 +0300

    Reset to correct version of prepare_operation_tablets.py

--- scripts/cron/prepare_operation_tablets.py
+++ scripts/cron/prepare_operation_tablets.py
@@ -3,29 +3,16 @@
 import yt.yson as yson
 import yt.wrapper as yt
 
-from copy import deepcopy
-
+BY_ID_ARCHIVE = "//sys/operations_archive/ordered_by_id"
+BY_START_TIME_ARCHIVE = "//sys/operations_archive/ordered_by_start_time"
 SHARD_COUNT = 100
 
-def remove_by_bame(fields, name):
-    return filter(lambda item: item["name"] != name, fields)
+def create_ordered_by_id_table():
+    path = BY_ID_ARCHIVE
+    yt.remove(path, force=True)
+    yt.create("table", path, ignore_existing=True, recursive=True)
 
-def move_to_front(fields, name):
-    index = -1
-    for i, item in enumerate(fields):
-        if item["name"] == name:
-            index = i
-            break
-    fields = deepcopy(fields)
-
-    item = fields[index]
-    fields.pop(index)
-    fields.insert(0, item)
-
-    return fields
-
-def main():
-    fields = [
+    yt.set(path + "/@schema", [
         {"name": "id_hash",  "type": "uint64", "expression": "farm_hash(id)"},
         {"name": "id",  "type": "string"},
         {"name": "state", "type": "string"},
@@ -37,27 +24,36 @@ def main():
         {"name": "brief_spec", "type": "any"},
         {"name": "start_time", "type": "string"},
         {"name": "finish_time", "type": "string"},
-        {"name": "result", "type": "any"},
-        {"name": "filter_factors", "type": "string"}]
+        {"name": "result", "type": "any"}])
 
-    ordered_by_id = "//sys/operations_archive/ordered_by_id"
-    ordered_by_start_time = "//sys/operations_archive/ordered_by_start_time"
- 
-    for table in (ordered_by_id, ordered_by_start_time):
-        yt.remove(table, force=True)
-        yt.create("table", table, ignore_existing=True, recursive=True)
+    yt.set_attribute(path, "key_columns", ["id_hash", "id"])
 
-    # ordered_by_id
-    yt.set_attribute(ordered_by_id, "schema", fields)
-    yt.set_attribute(ordered_by_id, "key_columns", ["id_hash", "id"])
     pivot_keys = [[]] + [[yson.YsonUint64((i * 2 ** 64) / SHARD_COUNT)] for i in xrange(1, SHARD_COUNT)]
-    yt.reshard_table(ordered_by_id, pivot_keys)
-    yt.mount_table(ordered_by_id)
+    yt.reshard_table(path, pivot_keys)
+    yt.mount_table(path)
 
-    # ordered_by_start_time
-    yt.set_attribute(ordered_by_start_time, "schema", move_to_front(remove_by_bame(fields, "id_hash"), "start_time"))
-    yt.set_attribute(ordered_by_start_time, "key_columns", ["start_time"])
-    yt.mount_table(ordered_by_start_time)
+def create_ordered_by_start_time_table():
+    path = BY_START_TIME_ARCHIVE
+    yt.remove(path, force=True)
+    yt.create("table", path, ignore_existing=True, recursive=True)
+
+    yt.set(path + "/@schema", [
+        {"name": "start_time", "type": "string"},
+        {"name": "id_hash", "type": "uint64", "expression": "farm_hash(id)"},
+        {"name": "id", "type": "string"},
+        {"name": "dummy", "type": "string"}])
+    yt.set(path + "/@key_columns", ["start_time", "id_hash", "id"])
+    yt.mount_table(path)
+
+def prepare_tables():
+    yt.config.VERSION = "v3"
+    yt.config.http.HEADER_FORMAT = "yson"
+
+    create_ordered_by_id_table()
+    create_ordered_by_start_time_table()
+
+def main():
+    prepare_tables()
 
 if __name__ == "__main__":
     main()
