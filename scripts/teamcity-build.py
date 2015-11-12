0936add367b053f6f4f960e3da10a99429712a12
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2015-11-12 17:13:27 +0300

    Auto-deploy local YT from teamcity.

--- scripts/teamcity-build.py
+++ scripts/teamcity-build.py
@@ -377,7 +377,7 @@ def build_python_packages(options):
 
     for package in ["yandex-yt-python", "yandex-yt-python-tools",
                     "yandex-yt-python-yson", "yandex-yt-transfer-manager",
-                    "yandex-yt-python-fennel"]:
+                    "yandex-yt-python-fennel", "yandex-yt-local"]:
         with cwd(options.checkout_directory, "python", package):
             package_version = run_captured(
                 "dpkg-parsechangelog | grep Version | awk '{print $2}'", shell=True).strip()
