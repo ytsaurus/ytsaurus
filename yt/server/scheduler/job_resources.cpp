4c4362032e391624f9c9691a9bb727cbf8bcf74a
Pavel Sushin <psushin@ytsaurus.tech>
2015-12-02 18:20:20 +0300

    Make max prefetch window configurable

--- yt/server/scheduler/job_resources.cpp
+++ yt/server/scheduler/job_resources.cpp
@@ -10,7 +10,6 @@ namespace NScheduler {
 using namespace NChunkClient;
 using namespace NNodeTrackerClient::NProto;
 
-using NChunkClient::MaxPrefetchWindow;
 using NChunkClient::ChunkReaderMemorySize;
 
 ////////////////////////////////////////////////////////////////////
@@ -77,7 +76,7 @@ i64 GetInputIOMemorySize(
     if (stat.ChunkCount == 0)
         return 0;
 
-    int concurrentReaders = std::min(stat.ChunkCount, MaxPrefetchWindow);
+    int concurrentReaders = std::min(stat.ChunkCount, config->TableReader->MaxPrefetchWindow);
 
     // Group can be overcommited by one block.
     i64 groupSize = stat.MaxBlockSize + ioConfig->TableReader->GroupSize;
