8a3e54cf864ad709c9ce2e37a8d99f3ed3fd3d01
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2015-12-14 04:53:35 +0300

    Enable fuse tests

--- python-git/yt/wrapper/tests/test_fuse.py
+++ python-git/yt/wrapper/tests/test_fuse.py
@@ -11,7 +11,6 @@ import pytest
 import random
 import json
 
-@pytest.xfail(run=False)
 @pytest.mark.usefixtures("yt_env")
 class TestCachedYtClient(object):
     def test_list(self):
@@ -50,7 +49,6 @@ class TestCachedYtClient(object):
             assert real_attributes[name] == cached_attributes[name]
 
 
-@pytest.xfail(run=False)
 @pytest.mark.usefixtures("yt_env")
 class TestCypress(object):
     def test_readdir(self):
