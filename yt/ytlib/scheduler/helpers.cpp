43a474d9d4b7fc12b055516f3ef5fdb4451a3cb6
Andrey Kashin <acid@yandex-team.ru>
2015-12-21 22:08:52 +0300

    [scheduler] Revive Materializing operations

--- yt/ytlib/scheduler/helpers.cpp
+++ yt/ytlib/scheduler/helpers.cpp
@@ -94,6 +94,7 @@ bool IsOperationInProgress(EOperationState state)
     return
         state == EOperationState::Initializing ||
         state == EOperationState::Preparing ||
+        state == EOperationState::Materializing ||
         state == EOperationState::Pending ||
         state == EOperationState::Reviving ||
         state == EOperationState::Running ||
