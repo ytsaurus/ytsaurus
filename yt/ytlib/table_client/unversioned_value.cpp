b5b63f16954451968ae27e3faf55649d1eb2e2c1
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-12-24 21:03:11 +0300

    Restore unversioned_value.{h,cpp}

---
+++ yt/ytlib/table_client/unversioned_value.cpp
@@ -0,0 +1,60 @@
+#include "unversioned_value.h"
+
+#include <yt/core/misc/error.h>
+#include <yt/core/misc/farm_hash.h>
+
+namespace NYT {
+namespace NTableClient {
+
+////////////////////////////////////////////////////////////////////////////////
+
+ui64 GetHash(const TUnversionedValue& value)
+{
+    // NB: hash function may change in future. Use fingerprints for persistent hashing.
+    return GetFarmFingerprint(value);
+}
+
+// Forever-fixed Google FarmHash fingerprint.
+TFingerprint GetFarmFingerprint(const TUnversionedValue& value)
+{
+    auto type = value.Type;
+    switch (type) {
+        case EValueType::String:
+            return FarmFingerprint(value.Data.String, value.Length);
+
+        case EValueType::Int64:
+        case EValueType::Uint64:
+        case EValueType::Double:
+            // These types are aliased.
+            return FarmFingerprint(value.Data.Int64);
+
+        case EValueType::Boolean:
+            return FarmFingerprint(value.Data.Boolean);
+
+        case EValueType::Null:
+            return FarmFingerprint(0);
+
+        default:
+            // No idea how to hash other types.
+            THROW_ERROR_EXCEPTION(
+            EErrorCode::UnhashableType,
+                "Cannot hash values of type %Qlv; only scalar types are allowed for key columns",
+                type)
+                << TErrorAttribute("value", value);
+    }
+}
+
+// Forever-fixed Google FarmHash fingerprint.
+TFingerprint GetFarmFingerprint(const TUnversionedValue* begin, const TUnversionedValue* end)
+{
+    ui64 result = 0xdeadc0de;
+    for (const auto* value = begin; value < end; ++value) {
+        result = FarmFingerprint(result, GetFarmFingerprint(*value));
+    }
+    return result ^ (end - begin);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NTableClient
+} // namespace NYT
