271eb186ec5b7e3cc43f9db2946ca801dde0b066
Maxim Babenko <babenko@ytsaurus.tech>
2015-12-24 15:50:44 +0300

    Use UNUSED, Luke ;)

--- yt/ytlib/api/admin.cpp
+++ yt/ytlib/api/admin.cpp
@@ -46,7 +46,7 @@ public:
         , LeaderChannel_(Connection_->GetMasterChannel(EMasterChannelKind::Leader))
     {
         Logger.AddTag("Admin: %p", this);
-        Options_; // Mark variable as used.
+        UNUSED(Options_);
     }
 
 #define DROP_BRACES(...) __VA_ARGS__
