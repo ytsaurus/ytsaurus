3e0f7a5567c7fb76bf099a8badd4d4b221139802
Ruslan Savchenko <savrus@ytsaurus.tech>
2015-12-24 21:03:11 +0300

    Restore unversioned_value.{h,cpp}

--- yt/ytlib/table_client/unversioned_row.cpp
+++ yt/ytlib/table_client/unversioned_row.cpp
@@ -197,52 +197,6 @@ int ReadValue(const char* input, TUnversionedValue* value)
     return current - input;
 }
 
-ui64 GetHash(const TUnversionedValue& value)
-{
-    // NB: hash function may change in future. Use fingerprints for persistent hashing.
-    return GetFarmFingerprint(value);
-}
-
-// Forever-fixed Google FarmHash fingerprint.
-TFingerprint GetFarmFingerprint(const TUnversionedValue& value)
-{
-    auto type = value.Type;
-    switch (type) {
-        case EValueType::String:
-            return FarmFingerprint(value.Data.String, value.Length);
-
-        case EValueType::Int64:
-        case EValueType::Uint64:
-        case EValueType::Double:
-            // These types are aliased.
-            return FarmFingerprint(value.Data.Int64);
-
-        case EValueType::Boolean:
-            return FarmFingerprint(value.Data.Boolean);
-
-        case EValueType::Null:
-            return FarmFingerprint(0);
-
-        default:
-            // No idea how to hash other types.
-            THROW_ERROR_EXCEPTION(
-            EErrorCode::UnhashableType,
-                "Cannot hash values of type %Qlv; only scalar types are allowed for key columns",
-                type)
-                << TErrorAttribute("value", value);
-    }
-}
-
-// Forever-fixed Google FarmHash fingerprint.
-TFingerprint GetFarmFingerprint(const TUnversionedValue* begin, const TUnversionedValue* end)
-{
-    ui64 result = 0xdeadc0de;
-    for (const auto* value = begin; value < end; ++value) {
-        result = FarmFingerprint(result, GetFarmFingerprint(*value));
-    }
-    return result ^ (end - begin);
-}
-
 void Save(TStreamSaveContext& context, const TUnversionedValue& value)
 {
     auto* output = context.GetOutput();
