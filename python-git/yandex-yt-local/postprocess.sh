f7867a6c56457236d22fd61df1456d0efe8ca1f5
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2015-12-25 18:37:56 +0300

    yandex-yt-local postprocess.sh fixes

--- python-git/yandex-yt-local/postprocess.sh
+++ python-git/yandex-yt-local/postprocess.sh
@@ -8,10 +8,12 @@ YT="yt/wrapper/yt"
 
 YANDEX_YT_LOCAL_VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
 YANDEX_YT_PYTHON_VERSION="0.6.67-0"
-YANDEX_YT_VERSION="0.17.3-stable-without-yt~6586~6be3d00"
+YANDEX_YT_VERSION="0.17.4-stable-without-yt~6595~df8d03c"
 YANDEX_YT_YSON_BINDINGS_VERSION="0.2.23-0"
+NODEJS_VERSION="0.8.26"
 
-ARCHIVE_NAME="yt_local_${YANDEX_YT_LOCAL_VERSION}_archive.tgz"
+UBUNTU_CODENAME=$(lsb_release -c -s)
+ARCHIVE_NAME="yt_local_${YANDEX_YT_LOCAL_VERSION}_${UBUNTU_CODENAME}_archive.tgz"
 
 download_and_extract() {
     local package="$1"
@@ -34,21 +36,33 @@ download_and_extract yandex-yt $YANDEX_YT_VERSION
 download_and_extract yandex-yt-http-proxy $YANDEX_YT_VERSION
 download_and_extract yandex-yt-python-yson $YANDEX_YT_YSON_BINDINGS_VERSION
 download_and_extract yandex-yt-web-interface
+download_and_extract nodejs $NODEJS_VERSION
 
 mkdir -p "archive/bin"
 mkdir -p "archive/python/yt"
+mkdir -p "archive/python/yt_yson_bindings"
 mkdir -p "archive/yt-thor"
+mkdir -p "archive/node"
 
 cp -r yandex-yt/usr/bin/ytserver archive/bin
 cp -r yandex-yt-http-proxy/usr/lib/node_modules archive/
+
 cp -r yandex-yt-python/usr/share/pyshared/yt/* archive/python/yt
 cp -r yandex-yt-python/usr/bin/yt2 archive/bin/
+cp -r yandex-yt-python/usr/bin/mapreduce-yt archive/bin
+
 cp -r yandex-yt-local/usr/share/pyshared/yt/* archive/python/yt
 cp -r yandex-yt-local/usr/bin/yt_local archive/bin/
+
 cp -r yandex-yt-python-yson/usr/share/pyshared/yt/* archive/python/yt
+cp -r yandex-yt-python-yson/usr/share/pyshared/yt_yson_bindings/* archive/python/yt_yson_bindings
+cp -r yandex-yt-python-yson/usr/lib/pyshared/python2.7/yt/* archive/python/yt
+cp -r yandex-yt-python-yson/usr/lib/pyshared/python2.7/yt_yson_bindings/* archive/python/yt_yson_bindings
+
 cp -r yandex-yt-web-interface/usr/share/yt-thor/* archive/yt-thor
 
-archive_name="yt_local"
+cp -r nodejs/usr/* archive/node
+
 tar cvfz $ARCHIVE_NAME archive/
 cat $ARCHIVE_NAME | $YT upload //home/files/${ARCHIVE_NAME} --proxy locke
 
