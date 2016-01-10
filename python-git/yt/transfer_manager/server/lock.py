50500eed332332d01bfc120ee41a1e6f88803fd6
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-01-11 00:28:19 +0300

    [transfer-manager] Add forgotten file

---
+++ python-git/yt/transfer_manager/server/lock.py
@@ -0,0 +1,55 @@
+from threading import RLock
+from contextlib import contextmanager
+
+class CountedRLock(object):
+    def __init__(self):
+        self._lock = RLock()
+        self._counter = 0
+
+    def is_acquired(self):
+        return self._counter > 0
+
+    def _acquire(self, blocking):
+        if blocking:
+            if self._lock.acquire(blocking=0):
+                self._counter += 1
+            else:
+                self._lock.acquire(blocking=1)
+        else:
+            if self._lock.acquire(blocking=0):
+                self._counter += 1
+                return True
+            else:
+                return False
+
+    def acquire(self, blocking=1, count=1):
+        for i in xrange(count):
+            self._acquire(blocking=blocking)
+
+    def _release(self):
+        self._lock.release()
+        self._counter -= 1
+
+    def release(self, count=1):
+        for i in xrange(count):
+            self._release()
+
+    def __enter__(self):
+        self.acquire()
+        return self
+
+    def __exit__(self, type, value, tb):
+        self.release()
+    
+    def get_counter(self):
+        return self._counter
+
+    @contextmanager
+    def discharge(self):
+        acquired_count = self.get_counter()
+        self.release(count=acquired_count)
+        yield
+        self.acquire(count=acquired_count)
+
+
+
