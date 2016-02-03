67aac9c2849224b6a6e9efd09a84ccbae23cc06f
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-02-03 12:08:44 +0300

    Commit actual run_merge.sh script

--- scripts/cron/run_merge.sh
+++ scripts/cron/run_merge.sh
@@ -2,7 +2,9 @@
 
 QUEUE="//sys/cron/tables_to_merge"
 
-/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimum-number-of-chunks 1000 --filter-out //statbox
+/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimum-number-of-chunks 100 --filter-out //statbox
+
+/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimum-number-of-chunks 100 --root //statbox --account statbox-logs --append
 
 /opt/cron/tools/run_parallel.sh "/opt/cron/tools/merge.py $QUEUE" 40 "/opt/cron/merging_log_$YT_PROXY"
 
