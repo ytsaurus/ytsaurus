0912cf19353d0cbe03912a53597a2d8b641b9511
Andrey Kashin <acid@yandex-team.ru>
2016-02-05 12:45:13 +0300

    [scheduler] Update pools only on actual change

--- yt/server/scheduler/scheduler_strategy.h
+++ yt/server/scheduler/scheduler_strategy.h
@@ -47,6 +47,9 @@ struct ISchedulerStrategy
 
     virtual void ScheduleJobs(ISchedulingContext* schedulingContext) = 0;
 
+    //! Resets memoized state.
+    virtual void ResetState() = 0;
+
     //! Builds a YSON structure containing a set of attributes to be assigned to operation's node
     //! in Cypress during creation.
     virtual void BuildOperationAttributes(
