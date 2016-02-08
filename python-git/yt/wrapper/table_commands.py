729e293ab4925807b20c07debb728bab233d7974
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-02-08 17:09:03 +0300

    FIx passing sync option

--- python-git/yt/wrapper/table_commands.py
+++ python-git/yt/wrapper/table_commands.py
@@ -1136,7 +1136,7 @@ def run_sort(source_table, destination_table=None, sort_by=None,
     if get_config(client)["run_merge_instead_of_sort_if_input_tables_are_sorted"] \
             and all(sort_by == get_sorted_by(table.name, [], client=client) for table in source_table):
         run_merge(source_table, destination_table, "sorted",
-                  strategy=strategy, table_writer=table_writer, spec=spec, client=client)
+                  strategy=strategy, table_writer=table_writer, sync=sync, spec=spec, client=client)
         return
 
     spec = compose(
@@ -1405,6 +1405,7 @@ def _run_operation(binary, source_table, destination_table,
                     replication_factor=replication_factor,
                     compression_codec=compression_codec,
                     reducer_memory_limit=memory_limit,
+                    sync=sync,
                     strategy=strategy,
                     spec=spec)
                 return
