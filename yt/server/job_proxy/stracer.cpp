915172870c56b60df2fc80e3b825b0df707f1245
Maxim Babenko <babenko@ytsaurus.tech>
2016-02-09 23:26:27 +0300

    Avoid using handcrafted YSON serialization

--- yt/server/job_proxy/stracer.cpp
+++ yt/server/job_proxy/stracer.cpp
@@ -7,12 +7,12 @@
 
 #include <yt/core/misc/subprocess.h>
 #include <yt/core/misc/proc.h>
+#include <yt/core/misc/finally.h>
 
 #include <yt/core/tools/registry.h>
 #include <yt/core/tools/tools.h>
 
 #include <yt/core/ytree/fluent.h>
-#include <yt/core/ytree/serialize.h>
 
 namespace NYT {
 namespace NJobProxy {
@@ -23,8 +23,8 @@ using namespace NConcurrency;
 ////////////////////////////////////////////////////////////////////////////////
 
 static const auto& Logger = JobProxyLogger;
-static const TDuration TraceInterval = TDuration::Seconds(5);
-static const TDuration TraceTimeout = TDuration::Seconds(10);
+static const auto TraceInterval = TDuration::Seconds(5);
+static const auto TraceTimeout = TDuration::Seconds(10);
 static const int StraceConcurrencyFactor = 4;
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -39,39 +39,6 @@ REGISTER_TOOL(TStraceTool);
 
 ////////////////////////////////////////////////////////////////////////////////
 
-void Serialize(const TStrace& trace, NYson::IYsonConsumer* consumer)
-{
-    BuildYsonFluently(consumer)
-        .BeginMap()
-            .Item("trace").Value(trace.Trace)
-            .Item("process_name").Value(trace.ProcessName)
-            .Item("process_command_line").List(trace.ProcessCommandLine)
-        .EndMap();
-}
-
-void Deserialize(TStrace& value, INodePtr node)
-{
-    auto mapNode = node->AsMap();
-
-    value.Trace = ConvertTo<Stroka>(mapNode->GetChild("trace"));
-    value.ProcessName = ConvertTo<Stroka>(mapNode->GetChild("process_name"));
-    value.ProcessCommandLine = ConvertTo<std::vector<Stroka>>(mapNode->GetChild("process_command_line"));
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-void Serialize(const TStracerResult& stracerResult, NYson::IYsonConsumer* consumer)
-{
-    NYTree::Serialize(stracerResult.Traces, consumer);
-}
-
-void Deserialize(TStracerResult& value, INodePtr node)
-{
-    Deserialize(value.Traces, node);
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 TStrace DoStrace(int pid)
 {
     TStrace trace;
@@ -80,7 +47,7 @@ TStrace DoStrace(int pid)
         trace.ProcessName = GetProcessName(pid);
         trace.ProcessCommandLine = GetProcessCommandLine(pid);
     } catch (const std::exception& ex) {
-        LOG_ERROR(ex, "Failed to get process information for %v", pid);
+        LOG_ERROR(ex, "Failed to get information for process %v", pid);
     }
 
     TSubprocess tracer("/usr/bin/strace");
@@ -90,11 +57,11 @@ TStrace DoStrace(int pid)
         ToString(pid)
     });
 
-    auto cookie = TDelayedExecutor::Submit(BIND([&] () {
+    auto intCookie = TDelayedExecutor::Submit(BIND([&] () {
             try {
                 tracer.Kill(SIGINT);
             } catch (const std::exception& ex) {
-                LOG_ERROR(ex, "Failed to interrupt stracer %v", pid);
+                LOG_ERROR(ex, "Failed to interrupt stracer process %v", pid);
             }
         }),
         TraceInterval);
@@ -103,25 +70,19 @@ TStrace DoStrace(int pid)
             try {
                 tracer.Kill(SIGKILL);
             } catch (const std::exception& ex) {
-                LOG_ERROR(ex, "Failed to kill stracer %v", pid);
+                LOG_ERROR(ex, "Failed to kill stracer process %v", pid);
             }
         }),
         TraceTimeout);
 
-    TSubprocessResult tracerResult;
-    try {
-        tracerResult = tracer.Execute();
-
-        TDelayedExecutor::Cancel(cookie);
+    TFinallyGuard cookieGuard([&] () {
+        TDelayedExecutor::Cancel(intCookie);
         TDelayedExecutor::Cancel(killCookie);
-    } catch (const std::exception&) {
-        TDelayedExecutor::Cancel(cookie);
-        TDelayedExecutor::Cancel(killCookie);
-        throw;
-    }
+    });
 
+    auto tracerResult = tracer.Execute();
     if (!tracerResult.Status.IsOK()) {
-        THROW_ERROR_EXCEPTION("Failed to strace %v", pid)
+        THROW_ERROR_EXCEPTION("Failed to strace process %v", pid)
             << TErrorAttribute("stderr", ToString(tracerResult.Error))
             << tracerResult.Status;
     }
