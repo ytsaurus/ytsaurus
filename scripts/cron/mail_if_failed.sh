3adf6872c66ac6754815fbb0d6c55a9c5e916f5f
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-02-11 23:25:51 +0300

    Add proxy to mail message in mail_if_failed script

--- scripts/cron/mail_if_failed.sh
+++ scripts/cron/mail_if_failed.sh
@@ -11,7 +11,11 @@ sh -c "$COMMAND" |& tee "$OUTPUT"
 EXIT_CODE=$?
 
 if [ "$EXIT_CODE" != "0" ]; then
-    echo -e "'$COMMAND' failed with code ${EXIT_CODE}.\n\n$(tail -n 20 $OUTPUT)\n\nignat@yandex-team.ru" | /opt/cron/tools/hide_tokens.py | mail -s "Regular process failed." $EMAIL
+    TITLE="Regular process failed"
+    if [ -n "$YT_PROXY" ]; then
+        TITLE="$TITLE on $YT_PROXY"
+    fi
+    echo -e "'$COMMAND' failed with code ${EXIT_CODE}.\n\n$(tail -n 20 $OUTPUT)\n\nignat@yandex-team.ru" | /opt/cron/tools/hide_tokens.py | mail -s "$TITLE" "$EMAIL"
 fi
 
 rm "$OUTPUT"
