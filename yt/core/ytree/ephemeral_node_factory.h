fe51117df6203a7198c0bda99ef10dd49fc15beb
Maxim Babenko <babenko@ytsaurus.tech>
2016-02-23 09:41:07 +0300

    Fix misleading comment

--- yt/core/ytree/ephemeral_node_factory.h
+++ yt/core/ytree/ephemeral_node_factory.h
@@ -8,10 +8,6 @@ namespace NYTree {
 ////////////////////////////////////////////////////////////////////////////////
 
 //! Returns a factory for creating an ephemeral (non-persistent, in memory) YTree.
-/*!
- *  \note
- *  This factory is a singleton so returning a raw pointer is OK.
- */
 INodeFactoryPtr GetEphemeralNodeFactory(bool shouldHideAttributes = false);
 
 ////////////////////////////////////////////////////////////////////////////////
