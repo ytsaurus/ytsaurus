4ae4bfd8d41e9b44c1995ca2b1795197ea232e77
Maxim Babenko <babenko@ytsaurus.tech>
2016-03-03 22:13:28 +0300

    Evict channels from cache upon termination

--- yt/core/rpc/caching_channel_factory.cpp
+++ yt/core/rpc/caching_channel_factory.cpp
@@ -9,6 +9,7 @@ namespace NYT {
 namespace NRpc {
 
 using namespace NConcurrency;
+using namespace NYTree;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -30,20 +31,30 @@ public:
             }
         }
 
-        auto channel = UnderlyingFactory_->CreateChannel(address);
+        auto underlyingChannel = UnderlyingFactory_->CreateChannel(address);
+        auto wrappedChannel = New<TCachedChannel>(this, underlyingChannel, address);
 
         {
             TWriterGuard guard(SpinLock_);
             auto it = ChannelMap_.find(address);
             if (it == ChannelMap_.end()) {
-                YCHECK(ChannelMap_.insert(std::make_pair(address, channel)).second);
-                return channel;
+                YCHECK(ChannelMap_.insert(std::make_pair(address, wrappedChannel)).second);
+                return wrappedChannel;
             } else {
-                channel->Terminate(TError(
+                underlyingChannel->Terminate(TError(
                     NRpc::EErrorCode::TransportError,
-                    "Channel terminated"));
+                    "Cached channel terminated due to racy requests"));
                 return it->second;
             }
+        }
+    }
+
+    void EvictChannel(const Stroka& address, IChannel* channel)
+    {
+        TWriterGuard guard(SpinLock_);
+        auto it = ChannelMap_.find(address);
+        if (it != ChannelMap_.end() && it->second == channel) {
+            ChannelMap_.erase(it);
         }
     }
 
@@ -53,6 +64,56 @@ private:
     TReaderWriterSpinLock SpinLock_;
     yhash_map<Stroka, IChannelPtr> ChannelMap_;
 
+    class TCachedChannel
+        : public IChannel
+    {
+    public:
+        TCachedChannel(
+            TCachingChannelFactory* factory,
+            IChannelPtr underlyingChannel,
+            const Stroka& address)
+            : Factory_(factory)
+            , UnderlyingChannel_(std::move(underlyingChannel))
+            , Address_(address)
+        { }
+
+        virtual const Stroka& GetEndpointDescription() const override
+        {
+            return UnderlyingChannel_->GetEndpointDescription();
+        }
+
+        virtual const IAttributeDictionary& GetEndpointAttributes() const override
+        {
+            return UnderlyingChannel_->GetEndpointAttributes();
+        }
+
+        virtual IClientRequestControlPtr Send(
+            IClientRequestPtr request,
+            IClientResponseHandlerPtr responseHandler,
+            TNullable<TDuration> timeout,
+            bool requestAck) override
+        {
+            return UnderlyingChannel_->Send(
+                std::move(request),
+                std::move(responseHandler),
+                timeout,
+                requestAck);
+        }
+
+        virtual TFuture<void> Terminate(const TError& error) override
+        {
+            auto factory = Factory_.Lock();
+            if (factory) {
+                factory->EvictChannel(Address_, this);
+            }
+            return UnderlyingChannel_->Terminate(error);
+        }
+
+    private:
+        const TWeakPtr<TCachingChannelFactory> Factory_;
+        const IChannelPtr UnderlyingChannel_;
+        const Stroka Address_;
+    };
 };
 
 IChannelFactoryPtr CreateCachingChannelFactory(IChannelFactoryPtr underlyingFactory)
