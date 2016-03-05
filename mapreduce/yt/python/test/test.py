bf8a498e93904a16a278dc05a5bfa95981aa25bd
vartyukh <vartyukh@yandex-team.ru>
2016-03-05 12:44:46 +0300

    Simple examples/tests for local_yt/mapreduce-yt usage. YT-3918 YT-3916

---
+++ mapreduce/yt/python/test/test.py
@@ -0,0 +1,20 @@
+import yatest.common
+
+from mapreduce.yt.python.yt_stuff import YtStuff
+from mapreduce.yt.python.yt_stuff import yt_stuff
+
+
+def test_start_stop():
+    yt = YtStuff()
+    yt.start_local_yt()
+    yt.stop_local_yt()
+
+def test_fixture(yt_stuff):
+    pass
+
+def test_mapreduce_yt(yt_stuff):
+    yt_server = yt_stuff.get_server()
+    mapreduce_yt = yt_stuff.get_mapreduce_yt()
+    yatest.common.execute(
+        [mapreduce_yt, "-server", yt_server, "-createtable", "//table"]
+        )
