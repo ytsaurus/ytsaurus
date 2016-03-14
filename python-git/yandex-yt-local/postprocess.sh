225adcc241ddd657fab32798b7b6d5da5389e45b
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-03-14 18:27:36 +0300

    Improved postprocess.sh for yandex-yt-local

--- python-git/yandex-yt-local/postprocess.sh
+++ python-git/yandex-yt-local/postprocess.sh
@@ -6,17 +6,12 @@ export YT_TOKEN=1da6afc98d189e8ba59d2ea39f29d0f1  # Teamcity user
 export PYTHONPATH="$(pwd)"
 YT="$(pwd)/yt/wrapper/yt"
 
-YANDEX_YT_LOCAL_VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
-YANDEX_YT_PYTHON_VERSION="0.6.82-0"
-YANDEX_YT_VERSION="0.17.4-stable-without-yt~6595~df8d03c"
-YANDEX_YT_YSON_BINDINGS_VERSION="0.2.23-0"
-NODEJS_VERSION="0.8.26"
-
 UBUNTU_CODENAME=$(lsb_release -c -s)
 
-VERSIONS="${YANDEX_YT_LOCAL_VERSION}${YANDEX_YT_PYTHON_VERSION}${YANDEX_YT_VERSION}${YANDEX_YT_YSON_BINDINGS_VERSION}"
-HASH=$(echo -ne $VERSIONS | md5sum | head -c 8)
-ARCHIVE_NAME="yt_local_${HASH}_${UBUNTU_CODENAME}_archive.tgz"
+if [ "$(find $(pwd)/.. -name 'yandex-yt-local_*.deb' | wc -l)" = "0" ]; then
+    # Package missing
+    exit 0
+fi
 
 download_and_extract() {
     local package="$1"
@@ -29,59 +24,72 @@ download_and_extract() {
     dpkg -x ${package}_*.deb $package
 }
 
-if [ "$(find $(pwd)/.. -name 'yandex-yt-local_*.deb' | wc -l)" = "0" ]; then
-    # Package missing
-    exit 0
-fi
+yandex_yt_local_version=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
+yandex_yt_python_version="0.6.89-0"
+yandex_yt_versions="0.17.5-prestable-without-yt~7966~df46c24 18.2.19561-prestable-without-yt~a62ea2a"
+yandex_yt_yson_bindings_version="0.2.26-0"
+nodejs_version="0.8.26"
 
-TMP_DIR="$(mktemp -d /tmp/$(basename $0).XXXXXX)"
-find $(pwd)/.. -name 'yandex-yt-local_*.deb' -exec cp -r {} $TMP_DIR \;
-cd "$TMP_DIR"
+current_dir=$(pwd)
 
-dpkg -x yandex-yt-local_*.deb "yandex-yt-local"
-download_and_extract yandex-yt-python $YANDEX_YT_PYTHON_VERSION
-download_and_extract yandex-yt $YANDEX_YT_VERSION
-download_and_extract yandex-yt-http-proxy $YANDEX_YT_VERSION
-download_and_extract yandex-yt-python-driver $YANDEX_YT_VERSION
-download_and_extract yandex-yt-python-yson $YANDEX_YT_YSON_BINDINGS_VERSION
-download_and_extract yandex-yt-web-interface
-download_and_extract nodejs $NODEJS_VERSION
+for yt_version in $yandex_yt_versions; do
+    echo "Making archive with yandex-yt=$yt_version"
 
-mkdir -p "archive/bin"
-mkdir -p "archive/python/yt"
-mkdir -p "archive/python/yt_yson_bindings"
-mkdir -p "archive/python/yt_driver_bindings"
-mkdir -p "archive/yt-thor"
-mkdir -p "archive/node"
+    versions_str="${yandex_yt_local_version}${yandex_yt_python_version}${yt_version}${yandex_yt_yson_bindings_version}"
+    hash_str=$(echo -ne $VERSIONS | md5sum | head -c 8)
+    archive_name="yt_local_${hash_str}_${UBUNTU_CODENAME}_archive.tgz"
 
-cp -r yandex-yt/usr/bin/ytserver archive/bin
-cp -r yandex-yt-http-proxy/usr/lib/node_modules archive/
+    tmp_dir="$(mktemp -d /tmp/$(basename $0).XXXXXX)"
+    find $(pwd)/.. -name 'yandex-yt-local_*.deb' -exec cp -r {} $tmp_dir \;
+    cd "$tmp_dir"
 
-cp -r yandex-yt-python/usr/share/pyshared/yt/* archive/python/yt
-cp -r yandex-yt-python/usr/bin/yt2 archive/bin/
-cp -r yandex-yt-python/usr/bin/mapreduce-yt archive/bin
+    dpkg -x yandex-yt-local_*.deb "yandex-yt-local"
+    download_and_extract yandex-yt-python $yandex_yt_python_version
+    download_and_extract yandex-yt $yt_version
+    download_and_extract yandex-yt-http-proxy $yt_version
+    download_and_extract yandex-yt-python-driver $yt_version
+    download_and_extract yandex-yt-python-yson $yandex_yt_yson_bindings_version
+    download_and_extract yandex-yt-web-interface
+    download_and_extract nodejs $nodejs_version
 
-cp -r yandex-yt-local/usr/share/pyshared/yt/* archive/python/yt
-cp -r yandex-yt-local/usr/bin/yt_local archive/bin/
+    mkdir -p "archive/bin"
+    mkdir -p "archive/python/yt"
+    mkdir -p "archive/python/yt_yson_bindings"
+    mkdir -p "archive/python/yt_driver_bindings"
+    mkdir -p "archive/yt-thor"
+    mkdir -p "archive/node"
 
-# YSON bindings.
-cp -r yandex-yt-python-yson/usr/share/pyshared/yt_yson_bindings/* archive/python/yt_yson_bindings
-cp -r yandex-yt-python-yson/usr/lib/pyshared/python2.7/yt_yson_bindings/* archive/python/yt_yson_bindings
+    cp -r yandex-yt/usr/bin/ytserver archive/bin
+    cp -r yandex-yt-http-proxy/usr/lib/node_modules archive/
 
-# Driver bindings.
-cp -r yandex-yt-python-driver/usr/share/pyshared/yt_driver_bindings/* archive/python/yt_driver_bindings
-cp -r yandex-yt-python-driver/usr/lib/pyshared/python2.7/yt_driver_bindings/* archive/python/yt_driver_bindings
+    cp -r yandex-yt-python/usr/share/pyshared/yt/* archive/python/yt
+    cp -r yandex-yt-python/usr/bin/yt2 archive/bin/
+    cp -r yandex-yt-python/usr/bin/mapreduce-yt archive/bin
 
-cp -r yandex-yt-web-interface/usr/share/yt-thor/* archive/yt-thor
+    cp -r yandex-yt-local/usr/share/pyshared/yt/* archive/python/yt
+    cp -r yandex-yt-local/usr/bin/yt_local archive/bin/
 
-cp -r nodejs/usr/* archive/node
+    # YSON bindings.
+    cp -r yandex-yt-python-yson/usr/share/pyshared/yt_yson_bindings/* archive/python/yt_yson_bindings
+    cp -r yandex-yt-python-yson/usr/lib/pyshared/python2.7/yt_yson_bindings/* archive/python/yt_yson_bindings
 
-tar cvfz $ARCHIVE_NAME archive/
-cat $ARCHIVE_NAME | $YT upload //home/files/${ARCHIVE_NAME} --proxy locke
-$YT set //home/files/${ARCHIVE_NAME}/@packages_versions "{\
-      yandex-yt=\"$YANDEX_YT_VERSION\"; \
-      yandex-yt-local=\"$YANDEX_YT_LOCAL_VERSION\";\
-      yandex-yt-python=\"$YANDEX_YT_PYTHON_VERSION\";\
-      yandex-yt-python-yson=\"$YANDEX_YT_YSON_BINDINGS_VERSION\"}" --proxy locke
+    # Driver bindings.
+    cp -r yandex-yt-python-driver/usr/share/pyshared/yt_driver_bindings/* archive/python/yt_driver_bindings
+    cp -r yandex-yt-python-driver/usr/lib/pyshared/python2.7/yt_driver_bindings/* archive/python/yt_driver_bindings
 
-rm -rf "$TMP_DIR"
+    cp -r yandex-yt-web-interface/usr/share/yt-thor/* archive/yt-thor
+
+    cp -r nodejs/usr/* archive/node
+
+    tar cvfz $archive_name archive/
+    cat $archive_name | $YT upload //home/files/${archive_name} --proxy locke
+    $YT set //home/files/${archive_name}/@packages_versions "{\
+          yandex-yt=\"$yt_version\"; \
+          yandex-yt-local=\"$yandex_yt_local_version\";\
+          yandex-yt-python=\"$yandex_yt_python_version\";\
+          yandex-yt-python-yson=\"$yandex_yt_yson_bindings_version\"}" --proxy locke
+
+    cd "$current_dir"
+
+    rm -rf "$tmp_dir"
+done
