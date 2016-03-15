eee2345fd3da33d931ec4c871ed6b74b4c455e45
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-03-15 13:10:59 +0300

    Updated TM client test. Updated copy directions

--- python-git/yt/transfer_manager/client/tests/_test_client.py
+++ python-git/yt/transfer_manager/client/tests/_test_client.py
@@ -19,27 +19,27 @@ def _wait_task(task_id, client):
 def test_copy_between_clusters(backend_url):
     client = TransferManager(url=backend_url)
 
-    smith_client = Yt(proxy="smith")
-    table = smith_client.create_temp_table()
-    smith_client.write_table(table, ["a\tb\n", "c\td\n", "e\tf\n"], format="yamr")
+    plato_client = Yt(proxy="plato")
+    table = plato_client.create_temp_table()
+    plato_client.write_table(table, ["a\tb\n", "c\td\n", "e\tf\n"], format="yamr")
 
-    client.add_task("smith", table, "sakura", "tmp/yt/test_table", sync=True)
+    client.add_task("plato", table, "quine", "//tmp/test_table", sync=True)
 
-    task_id = client.add_task("sakura", "tmp/yt/test_table", "redwood", "tmp/yt/test_table",
+    task_id = client.add_task("plato", table, "sakura", "tmp/yt/client_test_table",
                               params={"mr_user": "userdata"})
     time.sleep(0.5)
     assert task_id in [task["id"] for task in client.get_tasks()]
     _wait_task(task_id, client)
 
-    task_id = client.add_task("redwood", "tmp/yt/test_table", "plato", "//tmp/test_table",
+    task_id = client.add_task("sakura", "tmp/yt/client_test_table", "plato", "//tmp/test_table",
                               sync=True, poll_period=10, params={"pool": "ignat"})
 
     assert client.get_task_info(task_id)["state"] == "completed"
     assert client.get_task_info(task_id)["pool"] == "ignat"
-    assert Yt(proxy="plato").read_table("//tmp/test_table", format="yamr").read() == "a\tb\nc\td\ne\tf\n"
+    assert plato_client.read_table("//tmp/test_table", format="yamr").read() == "a\tb\nc\td\ne\tf\n"
 
     # Abort/restart
-    task_id = client.add_task("redwood", "tmp/yt/test_table", "plato", "//tmp/test_table")
+    task_id = client.add_task("sakura", "tmp/yt/client_test_table", "plato", "//tmp/test_table")
     client.abort_task(task_id)
     time.sleep(0.5)
     assert client.get_task_info(task_id)["state"] == "aborted"
