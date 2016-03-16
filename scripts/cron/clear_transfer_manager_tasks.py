3a60eddd3d289fc0ff1739519c9817540a63167a
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-03-16 11:36:48 +0300

    YT-4205: Delete tasks from Transfer Manager with one batch delete request

--- scripts/cron/clear_transfer_manager_tasks.py
+++ scripts/cron/clear_transfer_manager_tasks.py
@@ -1,70 +1,75 @@
 #!/usr/bin/python
 
 import yt.logger as logger
-from dateutil.parser import parse
-from collections import namedtuple, defaultdict
-from datetime import datetime, timedelta
-from threading import Thread
+
+import json
 import argparse
 import logging
 import requests
+from dateutil.parser import parse
+from collections import namedtuple, defaultdict
+from datetime import datetime, timedelta
 
-Task = namedtuple("Oper", ["start_time", "finish_time", "id", "user", "state"]);
+Task = namedtuple("Task", ["start_time", "finish_time", "id", "user", "state"]);
 
 logger.set_formatter(logging.Formatter('%(asctime)-15s\t%(message)s'))
-
 
 def is_casual(task):
     return task.state in ["completed", "aborted"]
 
-
 def is_final(state):
     return state in ["completed", "aborted", "failed"]
 
-
 def remove_tasks(tasks_to_remove, url, token):
     for task in tasks_to_remove:
-        rsp = requests.get("%s/tasks/%s/" % (url, task))
-        if rsp.status_code != 200:
-            logger.info("Task %s is already removed", task)
-            continue
-        if not is_final(rsp.json()["state"]):
-            logger.info("Task (%s) is no more in final state", task)
-            continue
-        logger.info("Removing task %s", task)
-        rsp = requests.delete("%s/tasks/%s/" % (url, task),
-                              headers={"Authorization": "OAuth " + token})
-        if rsp.status_code != 200:
-            logger.error("Cannot remove task %s: %s", task, rsp.content)
-            # d3rp@: replace break to continue, YTADMIN-3292
-            continue
+        logger.info("Task %s will be removed", task)
 
+    headers = {
+        "Authorization": "OAuth " + token,
+        "Content-Type": "application/json"
+    }
 
-def clean_tasks(url, token, count, total_count, failed_timeout, max_regular_tasks_per_user, max_failed_tasks_per_user,
-                robots, thread_count):
+    rsp = requests.delete("{0}/tasks/".format(url),
+                          data=json.dumps(tasks_to_remove),
+                          headers=headers)
+
+    if str(rsp.status_code).startswith("5"):
+        logger.error("Batch deletion failed. Transfer Manager is not available")
+    elif str(rsp.status_code).startswith("4"):
+        logger.error("Batch deletion failed with error: %s", rsp.json()["message"])
+    else:
+        logger.info("Successfully deleted %d tasks", len(tasks_to_remove))
+
+def create_task(task_dict):
+    start_time = parse(
+        task_dict.get("start_time", task_dict["creation_time"])).replace(tzinfo=None)
+
+    finish_time = task_dict.get("finish_time", None)
+    user = task_dict.get("user", "unknown")
+    id_ = task_dict["id"]
+    state = task_dict["state"]
+
+    return Task(start_time, finish_time, id_, user, state)
+
+def clean_tasks(url, token, count, total_count, failed_timeout, max_regular_tasks_per_user,
+                max_failed_tasks_per_user, robots, batch_size):
     if robots is None:
         robots = []
 
-    logger.info("Request tasks")
-    rsp = requests.get(url + "/tasks/")
+    logger.info("Requesting all tasks")
+    rsp = requests.get("{0}/tasks/".format(url))
     logger.info("Tasks recieved")
 
-    tasks = [Task(
-        parse(v.get("start_time", v["creation_time"])).replace(tzinfo=None),
-        v.get("finish_time", None),
-        v["id"],
-        v.get("user", "unknown"),
-        v["state"])
-             for v in rsp.json()]
+    tasks = [create_task(task_dict) for task_dict in rsp.json()]
+    tasks.sort(key=lambda task: task.start_time, reverse=True)
 
     saved = 0
     to_remove = []
 
-    tasks.sort(key=lambda task: task.start_time, reverse=True)
-
     users_regular = defaultdict(int)
     users_failed = defaultdict(int)
     users = set()
+
     for task in tasks:
         if not is_final(task.state):
             continue
@@ -74,7 +79,8 @@ def clean_tasks(url, token, count, total_count, failed_timeout, max_regular_task
         time_since = datetime.utcnow() - task.start_time
         is_old = (time_since > failed_timeout)
 
-        is_regular = (task.user in robots) and not (task.state == "failed")
+        is_regular = (task.user in robots) and (task.state != "failed")
+
         if task.state == "failed":
             users_failed[task.user] += 1
             is_user_limit_exceeded = users_failed[task.user] > max_failed_tasks_per_user
@@ -88,45 +94,39 @@ def clean_tasks(url, token, count, total_count, failed_timeout, max_regular_task
         else:
             saved += 1
 
-    threads = []
+    batch_start_index = 0
+    while batch_start_index < len(to_remove):
+        batch_end_index = batch_start_index + batch_size
+        batch = to_remove[batch_start_index:batch_end_index]
 
-    tasks_per_thread = 1 + len(to_remove) / thread_count
-    for thread_index in xrange(thread_count):
-        start_index = thread_index * tasks_per_thread
-        end_index = min(len(to_remove), (thread_index + 1) * tasks_per_thread)
-        if start_index >= end_index:
-            break
-        tasks_for_thread = to_remove[start_index:end_index]
-        thread = Thread(target=remove_tasks, args=(tasks_for_thread, url, token))
-        thread.start()
-        threads.append(thread)
+        if batch:
+            remove_tasks(batch, url, token)
 
-    for thread in threads:
-        thread.join()
-
+        batch_start_index += batch_size
 
 def main():
-    parser = argparse.ArgumentParser(description='Clean transfer manager tasks.')
-    parser.add_argument('url', help="Url of transfer manager")
-    parser.add_argument('--token', help="Token of administer user")
-    parser.add_argument('--count', metavar='N', type=int, default=500,
-                        help='leave no more than N completed (without stderr) or aborted tasks')
-    parser.add_argument('--total-count', metavar='N', type=int, default=2000,
-                        help='leave no more that N tasks totally')
-    parser.add_argument('--failed-timeout', metavar='N', type=int, default=30,
-                        help='remove all failed task older than N days')
-    parser.add_argument('--max-regular-tasks-per-user', metavar='N', type=int, default=50,
-                        help='remove old task of user if limit exceeded')
-    parser.add_argument('--max-failed-tasks-per-user', metavar='N', type=int, default=50,
-                        help='remove old task of user if limit exceeded')
-    parser.add_argument('--robot', action="append", help='robot users that run tasks very often and can be ignored')
-    parser.add_argument('--thread-count', metavar='N', type=int, default=10,
-                        help='number of thread that remove tasks')
+    parser = argparse.ArgumentParser(description="Clean transfer manager tasks.")
+    parser.add_argument("url", help="Transfer Manager url")
+    parser.add_argument("--token", help="token of administer user")
+    parser.add_argument("--count", metavar="N", type=int, default=500,
+                        help="leave no more than N completed (without stderr) or aborted tasks")
+    parser.add_argument("--total-count", metavar="N", type=int, default=2000,
+                        help="leave no more that N tasks totally")
+    parser.add_argument("--failed-timeout", metavar="N", type=int, default=30,
+                        help="remove all failed task older than N days")
+    parser.add_argument("--max-regular-tasks-per-user", metavar="N", type=int, default=50,
+                        help="remove old task of user if limit exceeded")
+    parser.add_argument("--max-failed-tasks-per-user", metavar="N", type=int, default=50,
+                        help="remove old task of user if limit exceeded")
+    parser.add_argument("--robot", action="append", help="robot users that run tasks very often and can be ignored")
+    parser.add_argument("--batch-size", metavar="N", type=int, default=100,
+                        help="number of tasks to remove per one delete request")
 
-    args = parser.parse_args()
-    clean_tasks(args.url, args.token, args.count, args.total_count, timedelta(days=args.failed_timeout),
-                args.max_regular_tasks_per_user, args.max_failed_tasks_per_user, args.robot, args.thread_count)
+    args = dict(vars(parser.parse_args()))
+    args["failed_timeout"] = timedelta(days=args["failed_timeout"])
+    args["robots"] = args.pop("robot")
 
+    clean_tasks(**args)
 
 if __name__ == "__main__":
     main()
