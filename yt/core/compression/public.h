06ad05b6726c45b8faccdbf15f4f3430ce6b9cb7
Andrey Kashin <acid@yandex-team.ru>
2016-03-21 22:43:41 +0300

    YT-4255 - Refactor compression codecs names and levels.

--- yt/core/compression/public.h
+++ yt/core/compression/public.h
@@ -13,12 +13,38 @@ struct ICodec;
 DEFINE_ENUM_WITH_UNDERLYING_TYPE(ECodec, i8,
     ((None)                       (0))
     ((Snappy)                     (1))
-    ((Zlib6)                      (2))
-    ((Zlib9)                      (3))
     ((Lz4)                        (4))
     ((Lz4HighCompression)         (5))
     ((QuickLz)                    (6))
     ((Zstd)                       (7))
+
+    ((Brotli_1)                  (11))
+    ((Brotli_2)                  (12))
+    ((Brotli_3)                   (8))
+    ((Brotli_4)                  (13))
+    ((Brotli_5)                   (9))
+    ((Brotli_6)                  (14))
+    ((Brotli_7)                  (15))
+    ((Brotli_8)                  (10))
+    ((Brotli_9)                  (16))
+    ((Brotli_10)                 (17))
+    ((Brotli_11)                 (18))
+
+    ((Zlib_1)                    (19))
+    ((Zlib_2)                    (20))
+    ((Zlib_3)                    (21))
+    ((Zlib_4)                    (22))
+    ((Zlib_5)                    (23))
+    ((Zlib_6)                     (2))
+    ((Zlib_7)                    (24))
+    ((Zlib_8)                    (25))
+    ((Zlib_9)                     (3))
+
+    // Deprecated
+    ((Zlib6)                      (2))
+    ((GzipNormal)                 (2))
+    ((Zlib9)                      (3))
+    ((GzipBestCompression)        (3))
     ((Brotli3)                    (8))
     ((Brotli5)                    (9))
     ((Brotli8)                   (10))
@@ -27,22 +53,4 @@ DEFINE_ENUM_WITH_UNDERLYING_TYPE(ECodec, i8,
 ///////////////////////////////////////////////////////////////////////////////
 
 } // namespace NCompression
-} // namespace NYT
-
-namespace NYT {
-
-template <>
-inline NCompression::ECodec ParseEnum(
-    const Stroka& value,
-    typename TEnumTraits<NCompression::ECodec>::TType*)
-{
-    Stroka decodedValue = DecodeEnumValue(value);
-    if (decodedValue == "GzipNormal") {
-        decodedValue = "Zlib6";
-    } else if (decodedValue == "GzipBestCompression") {
-        decodedValue = "Zlib9";
-    }
-    return TEnumTraits<NCompression::ECodec>::FromString(decodedValue);
-}
-
 } // namespace NYT
