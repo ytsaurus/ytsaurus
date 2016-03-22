853be5eb38acdca37b8c98b9fae41ccb85f01e6b
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-03-22 12:49:36 +0300

    REduce user sessions fraud period on plato

--- scripts/cron/import_to_plato.py
+++ scripts/cron/import_to_plato.py
@@ -10,7 +10,7 @@ def main():
 
     importer = Importer("plato")
     importer.process_log("day", "user_sessions/{}", None, period=180, **user_session_args)
-    importer.process_log("day", "user_sessions/{}/frauds", "user_sessions_frauds/{}", period=270, **user_session_args) 
+    importer.process_log("day", "user_sessions/{}/frauds", "user_sessions_frauds/{}", period=180, **user_session_args)
     importer.process_log("day", "user_sessions/{}/spy_log", "user_sessions_spy_log/{}", period=180, **user_session_args)
     importer.process_log("day", "user_intents/{}", None, period=None)
     importer.process_log("day", "reqregscdata/{}/www", None, period=180, link=False)
