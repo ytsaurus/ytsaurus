61da093aaed6d141404531c679146316fa810fd0
Maxim Babenko <babenko@ytsaurus.tech>
2016-03-26 00:08:17 +0300

    YT-4348: Remove all tablet cells upon migration

--- yt/server/tablet_server/tablet_manager.cpp
+++ yt/server/tablet_server/tablet_manager.cpp
@@ -930,6 +930,8 @@ private:
 
     TPeriodicExecutorPtr CleanupExecutor_;
 
+    bool RemoveCells_ = false;
+
     DECLARE_THREAD_AFFINITY_SLOT(AutomatonThread);
 
 
@@ -963,6 +965,8 @@ private:
     {
         VERIFY_THREAD_AFFINITY(AutomatonThread);
 
+        // COMPAT(babenko): YT-4348
+        RemoveCells_ = context.GetVersion() < 210;
         if (context.GetVersion() >= 202) {
             TabletCellBundleMap_.LoadValues(context);
         }
@@ -974,6 +978,16 @@ private:
     virtual void OnAfterSnapshotLoaded() override
     {
         TMasterAutomatonPart::OnAfterSnapshotLoaded();
+
+        // COMPAT(babenko): YT-4348
+        if (RemoveCells_) {
+            auto objectManager = Bootstrap_->GetObjectManager();
+            for (const auto& pair : TabletCellMap_) {
+                auto* cell = pair.second;
+                YCHECK(cell->GetObjectRefCounter() == 1);
+                objectManager->UnrefObject(cell);
+            }
+        }
 
         NameToTabletCellBundleMap_.clear();
         for (const auto& pair : TabletCellBundleMap_) {
