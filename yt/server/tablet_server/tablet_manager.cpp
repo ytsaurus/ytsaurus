091c7453d01f2d16a8e60c809e8e645216acec85
Maxim Babenko <babenko@ytsaurus.tech>
2016-03-28 00:58:41 +0300

    Fix typo

--- yt/server/tablet_server/tablet_manager.cpp
+++ yt/server/tablet_server/tablet_manager.cpp
@@ -1972,7 +1972,7 @@ private:
     INodePtr FindCellNode(const TCellId& cellId)
     {
         auto cellMapNodeProxy = GetCellMapNode();
-        auto cellNodeProxy = cellMapNodeProxy->FindChild(ToString(cellId));
+        return cellMapNodeProxy->FindChild(ToString(cellId));
     }
 
 
