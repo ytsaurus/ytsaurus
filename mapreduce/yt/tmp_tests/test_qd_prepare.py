1554b8bc27df19c31516dfff0a0b94b3b5ad08f7
vartyukh <vartyukh@yandex-team.ru>
2016-04-04 14:49:59 +0300

    Temporary tests with mr_utils. Copied from quality/blender/wizards_clicks and marked as FAT. Needs to exprmntr@ and devtools@-team for FAT-tests work improvement.

---
+++ mapreduce/yt/tmp_tests/test_qd_prepare.py
@@ -0,0 +1,24 @@
+# coding: utf-8
+
+import yatest.common
+import mr_utils
+
+
+def test():
+    data_path = '.'
+    return mr_utils.mapreduce_test(
+        yatest.common.binary_path("quality/blender/wizards_clicks/wizards_clicks"),
+        args=[
+            'qd_prepare',
+            '--src', 'cnts_aggr_sample/dopp_word',
+            '--dst', 'cnts_aggr_sample/dopp_word/qd_prepare',
+            '--server', 'local',
+        ],
+        data_path=data_path,
+        input_tables=[
+            mr_utils.TableSpec(file_path='cnts_aggr_sample.dopp_word', mapreduce_io_flags=['-fs', ',']),
+        ],
+        output_tables=[
+            mr_utils.TableSpec(file_path="cnts_aggr_sample.dopp_word.qd_prepare"),
+        ],
+    )
