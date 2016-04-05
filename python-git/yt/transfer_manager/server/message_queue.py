7ca640cc895a2453f1a02bcfe77fab32a6940d55
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-04-05 13:54:07 +0300

    Implement non blocking message queue without cyclic links

--- python-git/yt/transfer_manager/server/message_queue.py
+++ python-git/yt/transfer_manager/server/message_queue.py
@@ -1,8 +1,46 @@
 from threading import Thread
 from Queue import Queue
 
+import os
 import struct
+import fcntl
 import cPickle as pickle
+
+class Stream(object):
+    def __init__(self):
+        self.blocks = []
+        self.length = 0
+        self.index = 0
+        self.pos = 0
+
+    def add(self, block):
+        self.blocks.append(block)
+        self.length += len(block)
+
+    def available(self):
+        return self.length
+
+    def extract(self, size):
+        assert size <= self.available()
+        original_size = size
+
+        result = []
+        while size > 0:
+            block = self.blocks[self.index]
+            if size >= len(block) - self.pos:
+                result.append(block[self.pos:])
+                size -= len(block) - self.pos
+                self.pos = 0
+                self.index += 1
+            else:
+                result.append(block[self.pos:self.pos + size])
+                self.pos += size
+                size = 0
+
+        self.length -= original_size
+
+        return "".join(result)
+
 
 class ReadingThread(Thread):
     def __init__(self, pipe, queue):
@@ -10,15 +48,38 @@ class ReadingThread(Thread):
         self.pipe = pipe
         self.queue = queue
 
+        self.finished = False
+        self.stream = Stream()
+        self.length = None
+
+        self._set_non_blocking()
+
+    def _set_non_blocking(self):
+        fd = self.pipe.fileno()
+        fl = fcntl.fcntl(fd, fcntl.F_GETFL)
+        fcntl.fcntl(fd, fcntl.F_SETFL, fl | os.O_NONBLOCK)
+
     def run(self):
-        while True:
-            length_str = self.pipe.read(4)
-            if not length_str:
-                return
-            if len(length_str) != 4:
-                raise RuntimeError("Incorrect message format: expected 4 bytes, but  for %d bytes found" % length_str)
-            length = struct.unpack("i", length_str)[0]
-            self.queue.put(pickle.loads(self.pipe.read(length)))
+        while not self.finished:
+            try:
+                self.stream.add(self.pipe.read())
+            except IOError:
+                pass
+
+            available = self.stream.available()
+            if self.length is None:
+                if available < 4:
+                    continue
+                length_str = self.stream.extract(4)
+                self.length = struct.unpack("i", length_str)[0]
+            else:
+                if available < self.length:
+                    continue
+                self.queue.put(pickle.loads(self.stream.extract(self.length)))
+                self.length = None
+
+    def finish(self):
+        self.finished = True
 
 # TODO(ignat): limit queue only to call get(), get_nowait(), empty()
 class MessageReader(Queue):
@@ -29,6 +90,10 @@ class MessageReader(Queue):
 
     def finished(self):
         return not self.thread.is_alive() and self.empty()
+
+    def delete(self):
+        self.thread.finish()
+        del self.__dict__["thread"]
 
 class MessageWriter(object):
     def __init__(self, stream):
