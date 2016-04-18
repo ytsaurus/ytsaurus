cc0ae49becb5e1c8e7a661094cc1d4e2c27796cf
monster <monster@yandex-team.ru>
2016-04-18 22:17:38 +0300

    More tests

--- mapreduce/yt/tests/native/cypress.cpp
+++ mapreduce/yt/tests/native/cypress.cpp
@@ -2,6 +2,8 @@
 
 #include <mapreduce/yt/interface/client.h>
 
+#include <util/stream/input.h>
+
 namespace NYT {
 namespace NNativeTest {
 
@@ -26,11 +28,17 @@ public:
 
     IClientPtr Client() const { return Client_; }
     const char* Node() const { return "tmp/node"; }
+    const char* Node2() const { return "tmp/node2"; }
+    const char* Node3() const { return "tmp/node3"; }
 
 private:
     void RemoveNodes()
     {
-        Client()->Remove(Node(), TRemoveOptions().Force(true));
+        TRemoveOptions options;
+        options.Force(true).Recursive(true);
+        Client()->Remove(Node(), options);
+        Client()->Remove(Node2(), options);
+        Client()->Remove(Node3(), options);
     }
 
     IClientPtr Client_;
@@ -42,13 +50,21 @@ YT_TEST(TCypress, CreateRemove)
 {
     Client()->Create(Node(), NT_STRING);
 
+    EXPECT_EQ(Client()->Exists(Node()), true);
+
     Client()->Create(Node(), NT_STRING,
         TCreateOptions().IgnoreExisting(true));
 
+    EXPECT_EQ(Client()->Exists(Node()), true);
+
     Client()->Remove(Node());
+
+    EXPECT_EQ(Client()->Exists(Node()), false);
 
     Client()->Remove(Node(),
         TRemoveOptions().Force(true));
+
+    EXPECT_EQ(Client()->Exists(Node()), false);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -91,6 +107,94 @@ YT_TEST(TCypress, SetGetBoolean)
     Client()->Set(Node(), true);
     EXPECT_EQ(Client()->Get(Node()).AsBool(), true);
     Client()->Remove(Node());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+YT_TEST(TCypress, List)
+{
+    Client()->Create(Node(), NT_MAP);
+
+    Stroka node(Node());
+    Client()->Create(node + "/int", NT_INT64);
+    Client()->Create(node + "/uint", NT_UINT64);
+    Client()->Create(node + "/double", NT_DOUBLE);
+    Client()->Create(node + "/bool", NT_BOOLEAN);
+    Client()->Create(node + "/string", NT_STRING);
+    Client()->Create(node + "/map", NT_MAP);
+    Client()->Create(node + "/list", NT_LIST);
+    Client()->Create(node + "/file", NT_FILE);
+    Client()->Create(node + "/table", NT_TABLE);
+    Client()->Create(node + "/document", NT_DOCUMENT);
+
+    auto list = Client()->List(Node(),
+        TListOptions().AttributeFilter(TAttributeFilter().AddAttribute("type")));
+
+    for (auto e : list) {
+        Cout << e.AsString() << ": "
+            << e.GetAttributes()["type"].AsString() << Endl;
+    }
+
+    Client()->Remove(Node(), TRemoveOptions().Force(true).Recursive(true));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+YT_TEST(TCypress, Copy)
+{
+    Client()->Create(Node(), NT_STRING);
+    Client()->Copy(Node(), Node2());
+
+    EXPECT_EQ(Client()->Exists(Node()), true);
+    EXPECT_EQ(Client()->Exists(Node2()), true);
+}
+
+YT_TEST(TCypress, Move)
+{
+    Client()->Create(Node(), NT_STRING);
+    Client()->Move(Node(), Node2());
+
+    EXPECT_EQ(Client()->Exists(Node()), false);
+    EXPECT_EQ(Client()->Exists(Node2()), true);
+}
+
+YT_TEST(TCypress, Link)
+{
+    Client()->Create(Node(), NT_STRING);
+    Client()->Link(Node(), Node2());
+
+    EXPECT_EQ(Client()->Exists(Node()), true);
+    EXPECT_EQ(Client()->Exists(Node2()), true);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+YT_TEST(TCypress, Concatenate)
+{
+    {
+        auto writer = Client()->CreateFileWriter(Node());
+        *writer << "foo";
+        writer->Finish();
+    }
+    {
+        auto writer = Client()->CreateFileWriter(Node2());
+        *writer << "bar";
+        writer->Finish();
+    }
+    Client()->Create(Node3(), NT_FILE);
+    yvector<TYPath> nodes{Node(), Node2()};
+    Client()->Concatenate(nodes, Node3());
+    {
+        auto reader = Client()->CreateFileReader(Node3());
+        TransferData(reader.Get(), &Cout);
+        Cout << Endl;
+    }
+    Client()->Concatenate(nodes, Node3(), TConcatenateOptions().Append(true));
+    {
+        auto reader = Client()->CreateFileReader(Node3());
+        TransferData(reader.Get(), &Cout);
+        Cout << Endl;
+    }
 }
 
 ////////////////////////////////////////////////////////////////////////////////
