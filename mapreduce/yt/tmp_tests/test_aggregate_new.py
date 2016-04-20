774100f35986bc78a99c1d29ddddaa3cd8c231ca
vartyukh <vartyukh@yandex-team.ru>
2016-04-20 12:50:22 +0300

    Use yt_test directly.

--- mapreduce/yt/tmp_tests/test_aggregate_new.py
+++ mapreduce/yt/tmp_tests/test_aggregate_new.py
@@ -1,12 +1,12 @@
 # coding: utf-8
 
 import yatest.common
-import mr_utils
+import yt_utils
 
 
 def test():
     data_path = '.'
-    return mr_utils.mapreduce_test(
+    return yt_utils.yt_test(
         yatest.common.binary_path("quality/blender/wizards_clicks/wizards_clicks"),
         args=[
             'aggregate',
