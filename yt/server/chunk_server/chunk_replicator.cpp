291cfc3d8483eac18a46b2db96edb0c7a322fee2
Maxim Babenko <babenko@ytsaurus.tech>
2016-04-23 00:13:19 +0300

    YT-4569: Allow up to 127 racks

--- yt/server/chunk_server/chunk_replicator.cpp
+++ yt/server/chunk_server/chunk_replicator.cpp
@@ -183,7 +183,7 @@ TChunkReplicator::TChunkStatistics TChunkReplicator::ComputeRegularChunkStatisti
     int replicaCount = 0;
     int decommissionedReplicaCount = 0;
     TNodePtrWithIndexList decommissionedReplicas;
-    TRackSet usedRacks = 0;
+    TRackSet usedRacks;
     int usedRackCount = 0;
 
     for (auto replica : chunk->StoredReplicas()) {
@@ -194,9 +194,9 @@ TChunkReplicator::TChunkStatistics TChunkReplicator::ComputeRegularChunkStatisti
             ++replicaCount;
         }
         const auto* rack = replica.GetPtr()->GetRack();
-        auto rackMask = rack == nullptr ? NullRackMask : rack->GetIndexMask();
-        if (!(usedRacks & rackMask)) {
-            usedRacks |= rackMask;
+        int rackIndex = rack ? rack->GetIndex() : NullRackIndex;
+        if (!usedRacks.test(rackIndex)) {
+            usedRacks.set(rackIndex);
             ++usedRackCount;
         }
     }
@@ -222,7 +222,9 @@ TChunkReplicator::TChunkStatistics TChunkReplicator::ComputeRegularChunkStatisti
         result.BalancingRemovalIndexes.push_back(GenericChunkReplicaIndex);
     }
 
-    if (replicationFactor > 1 && usedRackCount == 1 && usedRacks != NullRackMask) {
+    TRackSet nullRackMask;
+    nullRackMask.set(NullRackIndex);
+    if (replicationFactor > 1 && usedRackCount == 1 && usedRacks != nullRackMask) {
         // A regular chunk is considered placed unsafely if all of its replicas are placed in
         // one non-null rack. Also, for RF=1 rack awareness is effectively off.
         result.Status |= EChunkStatus::UnsafelyPlaced;
@@ -330,7 +332,7 @@ TChunkReplicator::TChunkStatistics TChunkReplicator::ComputeJournalChunkStatisti
     int sealedReplicaCount = 0;
     int unsealedReplicaCount = 0;
     TNodePtrWithIndexList decommissionedReplicas;
-    TRackSet usedRacks = 0;
+    TRackSet usedRacks;
     bool hasUnsafelyPlacedReplicas = false;
 
     for (auto replica : chunk->StoredReplicas()) {
@@ -347,13 +349,13 @@ TChunkReplicator::TChunkStatistics TChunkReplicator::ComputeJournalChunkStatisti
         }
         const auto* rack = replica.GetPtr()->GetRack();
         if (rack) {
-            auto rackMask = rack->GetIndexMask();
-            if (usedRacks & rackMask) {
+            int rackIndex = rack->GetIndex();
+            if (usedRacks.test(rackIndex)) {
                 // A journal chunk is considered placed unsafely if some non-null rack
                 // contains more than one of its replicas.
                 hasUnsafelyPlacedReplicas = true;
             } else {
-                usedRacks |= rackMask;
+                usedRacks.set(rackIndex);
             }
         }
     }
