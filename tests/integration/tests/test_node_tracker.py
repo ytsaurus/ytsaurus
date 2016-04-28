1b2aa2977de5aca6d617aa355e2a93a2b87c4745
Maxim Babenko <babenko@ytsaurus.tech>
2016-04-28 15:59:38 +0300

    Add tests for node tags

--- tests/integration/tests/test_node_tracker.py
+++ tests/integration/tests/test_node_tracker.py
@@ -10,6 +10,10 @@ class TestNodeTracker(YTEnvSetup):
     NUM_MASTERS = 1
     NUM_NODES = 3
 
+    DELTA_NODE_CONFIG = {
+        "tags" : [ "config_tag1", "config_tag2" ]
+    }
+
     def test_ban(self):
         nodes = ls("//sys/nodes")
         assert len(nodes) == 3
@@ -28,6 +32,31 @@ class TestNodeTracker(YTEnvSetup):
         node = ls("//sys/nodes")[0]
         with pytest.raises(YtError): remove("//sys/nodes/{0}/@resource_limits_overrides".format(node))
 
+    def test_user_tags_validation(self):
+        node = ls("//sys/nodes")[0]
+        with pytest.raises(YtError): set("//sys/nodes/{0}/@user_tags".format(node), 123)
+
+    def test_user_tags_update(self):
+        node = ls("//sys/nodes")[0]
+        set("//sys/nodes/{0}/@user_tags".format(node), ["user_tag"])
+        assert get("//sys/nodes/{0}/@user_tags".format(node)) == ["user_tag"]       
+        assert "user_tag" in get("//sys/nodes/{0}/@tags".format(node))       
+
+    def test_config_tags(self):
+        for node in ls("//sys/nodes"):
+            tags = get("//sys/nodes/{0}/@tags".format(node))
+            assert "config_tag1" in tags
+            assert "config_tag2" in tags
+
+    def test_rack_tags(self):
+        create_rack("r")
+        node = ls("//sys/nodes")[0]
+        assert "r" not in get("//sys/nodes/{0}/@tags".format(node))
+        set("//sys/nodes/{0}/@rack".format(node), "r")
+        assert "r" in get("//sys/nodes/{0}/@tags".format(node))
+        remove("//sys/nodes/{0}/@rack".format(node))
+        assert "r" not in get("//sys/nodes/{0}/@tags".format(node))
+        
 ##################################################################
 
 class TestNodeTrackerMulticell(TestNodeTracker):
