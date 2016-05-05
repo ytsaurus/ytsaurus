fffb6288ebcac89c0a08da8dc90d9288a74079fc
vartyukh <vartyukh@yandex-team.ru>
2016-05-05 17:31:12 +0300

    More test for TTable.

--- mapreduce/yt/tests/common/table.cpp
+++ mapreduce/yt/tests/common/table.cpp
@@ -10,16 +10,23 @@ using namespace NMR;
 
 namespace {
 
-class TFixture
+class TTableFixture
     : public NTest::TTest
 {
 public:
+    static constexpr auto TABLE = "tmp/table";
+    static constexpr auto SORTED_TABLE = "tmp/sorted_table";
+    static constexpr auto EMPTY_TABLE = "tmp/empty_table";
+    static constexpr auto UNEXIST_TABLE = "tmp/unexist_table";
+
     void SetUp() override {
         TTest::SetUp();
         Server.Reset(new TServer(ServerName()));
+        CreateTables();
     }
 
     void TearDown() override {
+        DropTables();
         TTest::TearDown();
     }
 
@@ -27,98 +34,98 @@ public:
         return *Server;
     }
 
-private:
-    THolder<TServer> Server;
-};
-
-
-void DoIteratorTest(TServer& server, const char* tableName) {
-    Cout << "=======" << tableName << "=======" << Endl;
-    TClient client(server);
-    TTable table(client, tableName);
-
-    {
-        yvector<const char*> keys = { "a", "b", "c", "d", "e", "g", "i", "j", "aa", "bb", "0", "ee", "", "zzz" };
-
-        for (auto& key : keys) {
-#define CALL_METHOD(table, method, key) \
-{   \
-    auto it = table.method(key); \
-    Cout << "\tKey: " << key     \
-        << " Method: " #method   \
-        << " IsValid: " << it.IsValid() \
-        << " GetTableIndex: " << it.GetTableIndex() \
-        << " GetRecordIndex: " << it.GetRecordIndex() \
-        << "\n"; \
-}
-
-        CALL_METHOD(table, Find, key);
-        CALL_METHOD(table, LowerBound, key);
-        CALL_METHOD(table, UpperBound, key);
-
-#undef CALL_METHOD
+protected:
+    void PrintTable(const char* tableName) {
+        Cout << "~~~~~~" << tableName << "~~~~~~" << Endl;
+        TClient client(GetServer());
+        TTable table(client, tableName);
+        for (auto&& it = table.Begin(); it != table.End(); ++it) {
+            Cout << it.GetKey().AsString()
+                << "\t" << it.GetSubKey().AsString()
+                << "\t" << it.GetValue().AsString()
+                << "\n";
         }
     }
 
-}
-/*
-void PrintTable(TServer& server, const char* tableName) {
-    Cout << "~~~~~~" << tableName << "~~~~~~" << Endl;
-    TClient client(server);
-    TTable table(client, tableName);
-    for (auto&& it = table.Begin(); it != table.End(); ++it) {
-        Cout << it.GetKey().AsString()
-            << "\t" << it.GetSubKey().AsString()
-            << "\t" << it.GetValue().AsString()
-            << "\n";
-    }
-}
-*/
-} // anonymous namespace
-
-YT_TEST(TFixture, IteratorMethods) {
-    const char* TABLE = "tmp/table";
-    {
-        TClient client(GetServer());
-        TUpdate update(client, TABLE);
-        update.AddSub("a", "a", "0");
-        update.AddSub("c", "c", "2");
-        update.AddSub("e", "e", "2");
-        update.AddSub("g", "g", "2");
-        update.AddSub("i", "i", "2");
-    }
-    const char* SORTED_TABLE = "tmp/sorted_table";
-    {
-        TClient client(GetServer());
-        TUpdate updateSorted(client, SORTED_TABLE, UM_SORTED);
-        updateSorted.AddSub("a", "a", "0");
-        updateSorted.AddSub("c", "c", "2");
-        updateSorted.AddSub("e", "e", "2");
-        updateSorted.AddSub("g", "g", "2");
-        updateSorted.AddSub("i", "i", "2");
-    }
-    const char* EMPTY_TABLE = "tmp/empty_table";
-    {
-        TClient client(GetServer());
-        TUpdate update(client, EMPTY_TABLE);
+private:
+    void CreateTables() {
+        {
+            TClient client(GetServer());
+            TUpdate update(client, TABLE);
+            update.AddSub("a", "a", "0");
+            update.AddSub("c", "c", "2");
+            update.AddSub("e", "e", "2");
+            update.AddSub("g", "g", "2");
+            update.AddSub("i", "i", "2");
+        }
+        {
+            TClient client(GetServer());
+            TUpdate updateSorted(client, SORTED_TABLE, UM_SORTED);
+            updateSorted.AddSub("a", "a", "0");
+            updateSorted.AddSub("c", "c", "2");
+            updateSorted.AddSub("e", "e", "2");
+            updateSorted.AddSub("g", "g", "2");
+            updateSorted.AddSub("i", "i", "2");
+        }
+        {
+            TClient client(GetServer());
+            TUpdate update(client, EMPTY_TABLE);
+        }
     }
 
-    const char* UNEXIST_TABLE = "tmp/unexist_table";
-
-    //PrintTable(GetServer(), TABLE);
-    //PrintTable(GetServer(), SORTED_TABLE);
-
-    DoIteratorTest(GetServer(), TABLE);
-    DoIteratorTest(GetServer(), SORTED_TABLE);
-    DoIteratorTest(GetServer(), EMPTY_TABLE);
-    DoIteratorTest(GetServer(), UNEXIST_TABLE);
-
-    {
+    void DropTables() {
         TClient client(GetServer());
         client.Drop(TABLE);
         client.Drop(SORTED_TABLE);
         client.Drop(EMPTY_TABLE);
     }
+
+    THolder<TServer> Server;
+};
+
+Stroka ItToString(const TTableIterator&& it) {
+    TStringStream ss;
+    ss << "IsValid: " << it.IsValid()
+        << " GetTableIndex: " << it.GetTableIndex()
+        << " GetRecordIndex: " << it.GetRecordIndex();
+    return ss.Str();
+}
+
+
+void TestTableMethods(TServer& server, const char* tableName) {
+    Cout << "=======" << tableName << "=======" << Endl;
+    TClient client(server);
+    TTable table(client, tableName);
+
+    { // Begin, End, IsEmpty, IsSorted, IsWriteLocked, GetRecordCount
+        Cout << "TTable.Begin: " << ItToString(table.Begin()) << "\n";
+        Cout << "TTable.End: " << ItToString(table.End()) << "\n";
+        Cout << "TTable.IsEmpty: " << table.IsEmpty() << "\n";
+        Cout << "TTable.IsSorted: " << table.IsSorted() << "\n";
+        Cout << "TTable.IsWriteLocked: " << table.IsWriteLocked() << "\n";
+        Cout << "TTable.GetRecordCount: " << table.GetRecordCount() << "\n";
+    }
+    { // Find, LowerBound, UpperBound.
+        yvector<const char*> keys = { "a", "b", "c", "d", "e", "g", "i", "j", "aa", "bb", "0", "ee", "", "zzz" };
+        for (auto& key : keys) {
+            Cout << "TTable.Find key='" << key << "': " << ItToString(table.Find(key)) << "\n";
+        }
+        for (auto& key : keys) {
+            Cout << "TTable.LowerBound key='" << key << "': " << ItToString(table.LowerBound(key)) << "\n";
+        }
+        for (auto& key : keys) {
+            Cout << "TTable.UpperBound key='" << key << "': " << ItToString(table.UpperBound(key)) << "\n";
+        }
+    }
+}
+
+} // anonymous namespace
+
+YT_TEST(TTableFixture, TestMethods) {
+    TestTableMethods(GetServer(), TABLE);
+    TestTableMethods(GetServer(), SORTED_TABLE);
+    TestTableMethods(GetServer(), EMPTY_TABLE);
+    TestTableMethods(GetServer(), UNEXIST_TABLE);
 }
 
 } // NCommonTest
