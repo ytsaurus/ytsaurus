4ae2dcb84b7bd10b9c66a044c78cf24397220c39
vartyukh <vartyukh@yandex-team.ru>
2016-05-06 12:46:59 +0300

    More tests for TTable.

--- mapreduce/yt/tests/common/table.cpp
+++ mapreduce/yt/tests/common/table.cpp
@@ -2,6 +2,8 @@
 
 #include <mapreduce/interface/all.h>
 
+#include <utility>
+
 
 namespace NYT {
 namespace NCommonTest {
@@ -52,20 +54,20 @@ private:
         {
             TClient client(GetServer());
             TUpdate update(client, TABLE);
-            update.AddSub("a", "a", "0");
-            update.AddSub("c", "c", "2");
-            update.AddSub("e", "e", "2");
-            update.AddSub("g", "g", "2");
-            update.AddSub("i", "i", "2");
+            update.AddSub("a", "a", "a");
+            update.AddSub("c", "c", "c");
+            update.AddSub("e", "e", "e");
+            update.AddSub("g", "g", "g");
+            update.AddSub("i", "i", "i");
         }
         {
             TClient client(GetServer());
             TUpdate updateSorted(client, SORTED_TABLE, UM_SORTED);
-            updateSorted.AddSub("a", "a", "0");
-            updateSorted.AddSub("c", "c", "2");
-            updateSorted.AddSub("e", "e", "2");
-            updateSorted.AddSub("g", "g", "2");
-            updateSorted.AddSub("i", "i", "2");
+            updateSorted.AddSub("a", "a", "a");
+            updateSorted.AddSub("c", "c", "c");
+            updateSorted.AddSub("e", "e", "e");
+            updateSorted.AddSub("g", "g", "g");
+            updateSorted.AddSub("i", "i", "i");
         }
         {
             TClient client(GetServer());
@@ -97,24 +99,77 @@ void TestTableMethods(TServer& server, const char* tableName) {
     TClient client(server);
     TTable table(client, tableName);
 
-    { // Begin, End, IsEmpty, IsSorted, IsWriteLocked, GetRecordCount
-        Cout << "TTable.Begin: " << ItToString(table.Begin()) << "\n";
-        Cout << "TTable.End: " << ItToString(table.End()) << "\n";
-        Cout << "TTable.IsEmpty: " << table.IsEmpty() << "\n";
-        Cout << "TTable.IsSorted: " << table.IsSorted() << "\n";
-        Cout << "TTable.IsWriteLocked: " << table.IsWriteLocked() << "\n";
-        Cout << "TTable.GetRecordCount: " << table.GetRecordCount() << "\n";
+    { // Args = {}: Begin, End, IsEmpty, IsSorted, IsWriteLocked, GetRecordCount.
+        auto headerTemplate = "TTable.%s: ";
+        Cout << Sprintf(headerTemplate, "Begin") << ItToString(table.Begin()) << "\n";
+        Cout << Sprintf(headerTemplate, "End")<< ItToString(table.End()) << "\n";
+        Cout << Sprintf(headerTemplate, "IsEmpty") << table.IsEmpty() << "\n";
+        Cout << Sprintf(headerTemplate, "IsSorted") << table.IsSorted() << "\n";
+        Cout << Sprintf(headerTemplate, "IsWriteLocked") << table.IsWriteLocked() << "\n";
+        Cout << Sprintf(headerTemplate, "GetRecordCount") << table.GetRecordCount() << "\n";
     }
-    { // Find, LowerBound, UpperBound.
-        yvector<const char*> keys = { "a", "b", "c", "d", "e", "g", "i", "j", "aa", "bb", "0", "ee", "", "zzz" };
-        for (auto& key : keys) {
-            Cout << "TTable.Find key='" << key << "': " << ItToString(table.Find(key)) << "\n";
+    { // Args = {key}: Find, LowerBound, UpperBound, GetValueByKey.
+        yvector<const char*> keys = {
+                "a", "b", "c",
+                "d", "e", "g",
+                "i", "j", "aa",
+                "bb", "0", "ee",
+                "", "zzz"
+            };
+        auto headerTemplate = "TTable.%s key='%s': ";
+        for (const auto& key : keys) {
+            Cout << Sprintf(headerTemplate, "Find", key)
+                << ItToString(table.Find(key)) << "\n";
         }
-        for (auto& key : keys) {
-            Cout << "TTable.LowerBound key='" << key << "': " << ItToString(table.LowerBound(key)) << "\n";
+        for (const auto& key : keys) {
+            Cout << Sprintf(headerTemplate, "LowerBound", key)
+                << ItToString(table.LowerBound(key)) << "\n";
         }
-        for (auto& key : keys) {
-            Cout << "TTable.UpperBound key='" << key << "': " << ItToString(table.UpperBound(key)) << "\n";
+        for (const auto& key : keys) {
+            Cout << Sprintf(headerTemplate, "UpperBound", key)
+                << ItToString(table.UpperBound(key)) << "\n";
+        }
+        for (const auto& key : keys) {
+            yvector<char> value;
+            bool res = table.GetValueByKey(key, &value);
+            Cout << Sprintf(headerTemplate, "GetValueByKey", key)
+                << res << " '" << TStringBuf(value.begin(), value.size()) << "'\n";
+        }
+    }
+    { // Args = {key, subkey}: FindSub, LowerBoundSub, UpperBoundSub, GetValueByKeySub
+        using TKeySubkey = std::pair<const char*, const char*>;
+        yvector<TKeySubkey> args = {
+                { "a", "a" },
+                { "a", "zzz" },
+                { "a", "" },
+                { "a", "0" },
+                { "b", "a"},
+                { "b", "b"},
+                { "i", "i"},
+                { "i", "a"},
+                { "i", "zzz"},
+                { "0", "0"},
+                { "zzz", "zzz"},
+                { "", "" }
+            };
+        auto headerTemplate = "TTable.%s key='%s' subkey='%s': ";
+        for (const auto& arg : args) {
+            Cout << Sprintf(headerTemplate, "FindSub", arg.first, arg.second)
+                << ItToString(table.FindSub(arg.first, arg.second)) << "\n";
+        }
+        for (const auto& arg : args) {
+            Cout << Sprintf(headerTemplate, "LowerBoundSub", arg.first, arg.second)
+                << ItToString(table.LowerBoundSub(arg.first, arg.second)) << "\n";
+        }
+        for (const auto& arg : args) {
+            Cout << Sprintf(headerTemplate, "UpperBoundSub", arg.first, arg.second)
+                << ItToString(table.UpperBoundSub(arg.first, arg.second)) << "\n";
+        }
+        for (const auto& arg : args) {
+            yvector<char> value;
+            bool res = table.GetValueByKeySub(arg.first, arg.second, &value);
+            Cout << Sprintf(headerTemplate, "GetValueByKeySub", arg.first, arg.second)
+                << res << " '" << TStringBuf(value.begin(), value.size()) << "'\n";
         }
     }
 }
