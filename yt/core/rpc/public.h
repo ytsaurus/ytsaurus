f6d9353bffe5ed42234ef11f5f6debbd6d640938
Maxim Babenko <babenko@ytsaurus.tech>
2016-05-18 20:50:43 +0300

    YT-4756: Consolidate Unavailable and Abandoned error codes

--- yt/core/rpc/public.h
+++ yt/core/rpc/public.h
@@ -83,7 +83,6 @@ DEFINE_ENUM(EErrorCode,
     ((NoSuchMethod)                 (103))
     ((Unavailable)                  (105))
     ((PoisonPill)                   (106))
-    ((Abandoned)                    (107))
     ((RequestQueueSizeLimitExceeded)(108))
 );
 
