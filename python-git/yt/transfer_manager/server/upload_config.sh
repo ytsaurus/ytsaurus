d95385728178bfca37e7e18cc2676265f94ac0eb
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-05-20 17:08:27 +0300

    Do not use old clusters in TM tests

--- python-git/yt/transfer_manager/server/upload_config.sh
+++ python-git/yt/transfer_manager/server/upload_config.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-yt2 upload //home/$(whoami)/transfer_manager.config --proxy plato <config.json
-yt2 upload //home/$(whoami)/kwworm --proxy plato <kwworm
+yt2 upload //home/$(whoami)/transfer_manager.config --proxy banach <config.json
+yt2 upload //home/$(whoami)/kwworm --proxy banach <kwworm
