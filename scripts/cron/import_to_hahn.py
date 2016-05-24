1879a6011dde8e8fedadbb00d6800e5cac626137
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-05-24 20:21:44 +0300

    Fix

--- scripts/cron/import_to_hahn.py
+++ scripts/cron/import_to_hahn.py
@@ -8,7 +8,7 @@ def main():
         "format_attribute": yson.loads("<key_column_names=[key];escape_carriage_return=true;has_subkey=true;subkey_column_names=[subkey]>yamred_dsv"),
         "ydf_attribute": "UserSessions.ReadRecord"}
 
-    importer = Importer("aristotle")
+    importer = Importer("hahn")
     importer.process_log("day", "tr/user_sessions/{}", None, 50, **user_session_args)
 
 if __name__ == "__main__":
