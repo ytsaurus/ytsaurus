f6c8b3f19ea157040801d040d12f140b22bf42e1
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2016-05-24 14:47:01 +0300

    Parallel sync for mirrors

--- scripts/mirrors-update-github.sh
+++ scripts/mirrors-update-github.sh
@@ -11,10 +11,16 @@ export LC_ALL=en_US.UTF-8
 
 ################################################################################
 
-for repo in $MIRRORS_DIRECTORY/github-*.git ; do
+sync() {
+    local repo="$1"
     echo "=== $repo"
     (cd $repo && git fetch github)
     (cd $repo && git branch -f upstream/HEAD $(git show-ref -s refs/upstream/HEAD))
     (cd $repo && git push origin)
+}
+
+for repo in $MIRRORS_DIRECTORY/github-*.git ; do
+    sync "$repo" &
 done
+wait
 
