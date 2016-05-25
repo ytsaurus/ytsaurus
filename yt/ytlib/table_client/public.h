a9a0498727474f4403d8396594395071cebc61a4
Pavel Sushin <psushin@ytsaurus.tech>
2016-05-25 14:34:50 +0300

    YT-4208: trim samples

--- yt/ytlib/table_client/public.h
+++ yt/ytlib/table_client/public.h
@@ -56,6 +56,10 @@ const int MaxColumnNameLength = 256;
 const int MaxColumnLockLength = 256;
 const int MaxColumnGroupLength = 256;
 
+// NB(psushin): increasing this parameter requires rewriting all chunks,
+// so one probaly should never want to do it.
+const int MaxSampleSize = 64 * 1024;
+
 const int DefaultPartitionTag = -1;
 
 extern const Stroka SystemColumnNamePrefix;
