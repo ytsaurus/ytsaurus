21b97298c609eb79eea1067120ce3b71b4bc1db5
sandello <sandello@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2016-05-25 15:59:57 +0000

    Add int64/uint64 cast functions in QL (for say@)
    __BYPASS_CHECKS__
    
    
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yt/17_5/yt@2315918 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- ytlib/query_client/builtin_function_registry_arc.cpp
+++ ytlib/query_client/builtin_function_registry_arc.cpp
@@ -4,6 +4,8 @@
 #include "udf/is_null_arc.h"
 #include "udf/sum_arc.h"
 #include "udf/farm_hash_arc.h"
+#include "udf/int64_arc.h"
+#include "udf/uint64_arc.h"
 
 namespace NYT {
 namespace NQueryClient {
@@ -27,6 +29,7 @@ IFunctionRegistryPtr CreateBuiltinFunctionRegistry()
         ECallingConvention::UnversionedValue));
 
     auto typeArg = TTypeArgument{0};
+
     auto sumConstraints = std::unordered_map<TTypeArgument, TUnionType>();
     sumConstraints[typeArg] = std::vector<EValueType>{
         EValueType::Int64,
@@ -60,6 +63,34 @@ IFunctionRegistryPtr CreateBuiltinFunctionRegistry()
         TSharedRef(
             farm_hash_bc,
             farm_hash_bc_len,
+            nullptr)));
+
+    auto castConstraints = std::unordered_map<TTypeArgument, TUnionType>();
+    castConstraints[typeArg] = std::vector<EValueType>{
+        EValueType::Int64,
+        EValueType::Uint64,
+        EValueType::Double};
+
+    registry->RegisterFunction(New<TUserDefinedFunction>(
+        "int64",
+        castConstraints,
+        std::vector<TType>{typeArg},
+        EValueType::Null,
+        EValueType::Int64,
+        TSharedRef(
+            int64_bc,
+            int64_bc_len,
+            nullptr)));
+
+    registry->RegisterFunction(New<TUserDefinedFunction>(
+        "uint64",
+        castConstraints,
+        std::vector<TType>{typeArg},
+        EValueType::Null,
+        EValueType::Uint64,
+        TSharedRef(
+            uint64_bc,
+            uint64_bc_len,
             nullptr)));
 
     return registry;
