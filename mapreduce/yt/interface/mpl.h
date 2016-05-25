63f846b95de072a75719fcafc2f2fc5eb2b137c0
halyavin <halyavin@yandex-team.ru>
2016-05-25 17:44:33 +0300

    Remove IsConstReference, IsClass, IsVoid, IsEnum and IsPrimitive.

--- mapreduce/yt/interface/mpl.h
+++ mapreduce/yt/interface/mpl.h
@@ -1,39 +1,19 @@
 #pragma once
 
+#include <type_traits>
+
 namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
 template <
     class TBase,
-    class TDerived,
-    bool = TTypeTraits<TBase>::IsClassType && TTypeTraits<TDerived>::IsClassType
+    class TDerived
 >
 struct TIsBaseOf
 {
-    using TYes = char;
-    typedef char (&TNo)[2];
-
-    template <class T>
-    static TYes Test(TDerived*, T);
-    static TNo Test(TBase*, int);
-
-    struct TConv
-    {
-        operator TBase*() const;
-        operator TDerived*();
-    };
-
     enum {
-        Value = sizeof(Test(TConv(), 0)) == sizeof(TYes)
-    };
-};
-
-template <class TBase, class TDerived>
-struct TIsBaseOf<TBase, TDerived, false>
-{
-    enum {
-        Value = false
+        Value = std::is_base_of<TBase, TDerived>::value && !std::is_same<TBase, TDerived>::value
     };
 };
 
