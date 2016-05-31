9650036418eb18ce6827fc385b90609884bf1384
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2016-05-31 14:25:05 +0300

    Slightly refactor command execution

--- yt/nodejs/lib/driver.js
+++ yt/nodejs/lib/driver.js
@@ -22,7 +22,7 @@ var _SIMPLE_EXECUTE_FORMAT = binding.CreateV8Node("json");
 
 function promisinglyPipe(source, destination)
 {
-    return new Q(function(resolve, reject) {
+    return new Q(function promisinglyPipe$impl(resolve, reject) {
         var debug = __DBG.Tagged("Pipe");
         var clean = false;
 
@@ -133,7 +133,7 @@ function YtDriver(config, echo)
     this.__DBG("New");
 }
 
-YtDriver.prototype.execute = function(
+YtDriver.prototype.execute = function YtDriver$execute(
     name, user,
     input_stream, input_compression,
     output_stream, output_compression,
@@ -143,91 +143,79 @@ YtDriver.prototype.execute = function(
 {
     this.__DBG("execute");
 
+    // Avoid capturing |this| to avoid back references.
     var wrapped_input_stream = new YtWritableStream(this.low_watermark, this.high_watermark);
     var wrapped_output_stream = new YtReadableStream(this.high_watermark);
+    var binding = this._binding;
+    var debug = this.__DBG;
 
-    var deferred = Q.defer();
-    var self = this;
-
-    var destroyer_state = {
-        fired: false,
-        objects: [wrapped_input_stream, wrapped_output_stream, input_stream, output_stream]
-    };
-
+    // Setup pipes.
     function destroyer() {
-        if (!destroyer_state.fired) {
-            for (var i = 0, n = destroyer_state.objects.length; i < n; ++i) {
-                destroyer_state.objects[i].destroy();
-            }
-            destroyer_state.objects = null;
-            destroyer_state.fired = true;
-        }
-    }
-
-    function undestroyer() {
-        destroyer_state.objects = null;
-        destroyer_state.fired = true;
+        wrapped_input_stream.destroy();
+        wrapped_output_stream.destroy();
+        input_stream.destroy();
+        output_stream.destroy();
     }
 
     var input_pipe_promise = promisinglyPipe(input_stream, wrapped_input_stream)
         .then(
         function ip_promise_then() {
-            self.__DBG("execute -> input_pipe_promise has been resolved");
+            debug("execute -> input_pipe_promise has been resolved");
+            // Close input stream here to allow driver command to terminate.
             wrapped_input_stream.end();
         },
         function ip_promise_catch(err) {
-            self.__DBG("execute -> input_pipe_promise has been rejected");
+            debug("execute -> input_pipe_promise has been rejected");
             destroyer();
-            deferred.reject(new YtError("Input pipe has been canceled", err));
+            return Q.reject(new YtError("Input pipe has been canceled", err));
         });
 
     var output_pipe_promise = promisinglyPipe(wrapped_output_stream, output_stream)
         .then(
         function op_promise_then() {
+            debug("execute -> output_pipe_promise has been resolved");
             // Do not close |output_stream| here since we have to write out trailers.
-            self.__DBG("execute -> output_pipe_promise has been resolved");
         },
         function op_promise_catch(err) {
-            self.__DBG("execute -> output_pipe_promise has been rejected");
+            debug("execute -> output_pipe_promise has been rejected");
             destroyer();
-            deferred.reject(new YtError("Output pipe has been canceled", err));
+            return Q.reject(new YtError("Output pipe has been canceled", err));
         });
 
-    var canceler = this._binding.Execute(name, user,
-        wrapped_input_stream._binding, input_compression,
-        wrapped_output_stream._binding, output_compression,
-        parameters, request_id,
-        function(result) {
-            self.__DBG("execute -> (on-execute callback)");
-
-            if (typeof(result_interceptor) === "function") {
-                self.__DBG("execute -> (interceptor)");
-                try {
-                    result_interceptor(result);
-                } catch (ex) {
+    var driver_promise = new Q(function YtDriver$execute$impl(resolve, reject) {
+        var future = binding.Execute(name, user,
+            wrapped_input_stream._binding, input_compression,
+            wrapped_output_stream._binding, output_compression,
+            parameters, request_id,
+            function(result) {
+                debug("execute -> (on-execute callback)");
+                if (typeof(result_interceptor) === "function") {
+                    debug("execute -> (interceptor)");
+                    try {
+                        result_interceptor(result);
+                    } catch (ex) {
+                    }
                 }
-            }
+                if (result.code === 0) {
+                    debug("execute -> execute_promise has been resolved");
+                    resolve(Array.prototype.slice.call(arguments));
+                } else {
+                    debug("execute -> execute_promise has been rejected");
+                    reject(result);
+                }
+            },
+            response_parameters_consumer);
 
-            if (result.code === 0) {
-                self.__DBG("execute -> execute_promise has been resolved");
-                deferred.resolve(Array.prototype.slice.call(arguments));
-            } else {
-                self.__DBG("execute -> execute_promise has been rejected");
-                deferred.reject(result);
-            }
-        },
-        response_parameters_consumer);
+        input_pipe_promise.error(function() { future.Cancel(); });
 
-    input_pipe_promise.error(function() { canceler.Cancel(); });
-
-    output_pipe_promise.error(function() { canceler.Cancel(); });
+        output_pipe_promise.error(function() { future.Cancel(); });
+    });
 
     process.nextTick(function() { pause.unpause(); });
 
     return Q
-        .all([deferred.promise, input_pipe_promise, output_pipe_promise])
-        .spread(function(result, ir, or) {
-            undestroyer();
+        .all([driver_promise, input_pipe_promise, output_pipe_promise])
+        .spread(function spread(result, ir, or) {
             return result;
         });
 };
