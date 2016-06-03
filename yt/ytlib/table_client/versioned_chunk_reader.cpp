e1dd411f89dd29290fe953b4ebcec75cc94c4e7e
Ruslan Savchenko <savrus@ytsaurus.tech>
2016-06-03 22:17:15 +0300

    YT-4883 Fix hang in TCacheBasedVersionedRangeChunkReader

--- yt/ytlib/table_client/versioned_chunk_reader.cpp
+++ yt/ytlib/table_client/versioned_chunk_reader.cpp
@@ -2099,9 +2099,14 @@ private:
             }
         }
 
-        while ((!UpperBoundCheckNeeded_ || BlockReader_->GetKey() < UpperBound_) &&
-               rows->size() < rows->capacity())
-        {
+        bool finished = false;
+
+        while (rows->size() < rows->capacity()) {
+            if (UpperBoundCheckNeeded_ && BlockReader_->GetKey() >= UpperBound_.Get()) {
+                finished = true;
+                break;
+            }
+
             auto row = CaptureRow(BlockReader_.get());
             if (row) {
                 rows->push_back(row);
@@ -2111,7 +2116,8 @@ private:
                 // End-of-block.
                 if (++BlockIndex_ >= ChunkMeta_->BlockMeta().blocks_size()) {
                     // End-of-chunk.
-                    return false;
+                    finished = true;
+                    break;
                 }
                 CreateBlockReader();
             }
@@ -2119,7 +2125,7 @@ private:
 
         PerformanceCounters_->StaticChunkRowReadCount += rows->size();
 
-        return true;
+        return !finished;
     }
 
     void CreateBlockReader()
