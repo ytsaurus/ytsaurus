a211218e60089eb3b77031e664a3e8c33f014d72
Vladislav Klyachin <klyachin@yandex-team.ru>
2016-06-08 18:55:55 +0300

    YT-4909: Pass shell parameters as a map, not as a string

--- python-git/yt/wrapper/job_shell.py
+++ python-git/yt/wrapper/job_shell.py
@@ -79,7 +79,7 @@ class JobShell(object):
             parameters["shell_id"] = self.shell_id
         command = {
             "job_id": self.job_id,
-            "parameters": yson.dumps(parameters, yson_format="text"),
+            "parameters": parameters
         }
         req.headers["X-YT-Parameters"] = yson.dumps(command, yson_format="text")
         req.headers["X-YT-Correlation-Id"] = generate_uuid()
