fe45a5348840e4ead92a9bcb9fa64eb25cfda9fe
Vladislav Klyachin <klyachin@yandex-team.ru>
2016-06-08 20:37:34 +0300

    YT-4904: Fix wrapper test on job shell

--- python-git/yt/wrapper/tests/test_job_commands.py
+++ python-git/yt/wrapper/tests/test_job_commands.py
@@ -2,11 +2,13 @@ import yt.wrapper as yt
 from yt.wrapper.job_shell import JobShell
 from yt.wrapper.http import get_api_commands
 
-from helpers import ENABLE_JOB_CONTROL, TEST_DIR
+from helpers import ENABLE_JOB_CONTROL, TEST_DIR, TESTS_SANDBOX
 
 import os
+import stat
 import pytest
 import re
+import tempfile
 import time
 
 @pytest.mark.usefixtures("yt_env")
@@ -19,7 +21,7 @@ class TestJobCommands(object):
         running_count = 0
         pending_count = 0
         while running_count == 0 or pending_count > 0:
-            time.sleep(0.2)
+            time.sleep(self._poll_frequency)
             try:
                 progress = yt.get(progress_path)
                 running_count = progress["running"]
@@ -29,7 +31,37 @@ class TestJobCommands(object):
                 if err.is_resolve_error():
                     continue
                 raise
-        return yt.list(jobs_path)
+
+        self.jobs = yt.list(jobs_path)
+
+        # Wait till all jobs are actually running.
+        while not all([os.path.exists(os.path.join(self._tmpdir, "started_" + job)) for job in self.jobs]):
+            time.sleep(self._poll_frequency)
+
+    def _resume_jobs(self):
+        if self.jobs is None:
+            raise RuntimeError('"_ensure_jobs_running" must be called before resuming jobs')
+
+        for job in self.jobs:
+            os.unlink(os.path.join(self._tmpdir, "started_" + job))
+
+        try:
+            os.rmdir(self._tmpdir)
+        except OSError:
+            sys.excepthook(*sys.exc_info())
+
+    def _create_tmpdir(self, prefix):
+        basedir = os.path.join(TESTS_SANDBOX, "tmp")
+        try:
+            if not os.path.exists(basedir):
+                os.mkdir(basedir)
+            tmpdir = tempfile.mkdtemp(
+                prefix="{0}_{1}_".format(prefix, os.getpid()),
+                dir=basedir)
+            os.chmod(tmpdir, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+        except OSError:
+            sys.excepthook(*sys.exc_info())
+        return tmpdir
 
     def _poll_until_prompt(self, shell):
         output = ""
@@ -51,10 +83,18 @@ class TestJobCommands(object):
         other_table = TEST_DIR + "/other_table"
         yt.write_table(table, [{"x": 1}, {"x": 2}])
 
-        op = yt.run_map("sleep 10; cat", table, other_table, format=yt.DsvFormat(), sync=False)
+        self._tmpdir = self._create_tmpdir("job_shell")
+        self._poll_frequency = 0.2
+        mapper = (
+            "(touch {0}/started_$YT_JOB_ID 2>/dev/null\n"
+            "cat\n"
+            "while [ -f {0}/started_$YT_JOB_ID ]; do sleep 0.1; done\n)"
+            .format(self._tmpdir))
 
-        jobs = self._ensure_jobs_running(op)
-        job_id = jobs[0]
+        op = yt.run_map(mapper, table, other_table, format=yt.DsvFormat(), sync=False)
+
+        self._ensure_jobs_running(op)
+        job_id = self.jobs[0]
 
         shell = JobShell(job_id, interactive=False)
         shell.make_request("spawn", term="screen-256color", height=50, width=132)
@@ -78,5 +118,5 @@ class TestJobCommands(object):
         with pytest.raises(yt.YtError):
             output = self._poll_until_prompt(shell)
 
-        op.abort()
-        assert op.get_state() == "aborted"
+        self._resume_jobs()
+        op.wait()
