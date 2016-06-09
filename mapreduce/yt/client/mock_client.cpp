3e7fbb9a0b6254cfecdb118fc3ac727ba0a48aa9
alexeyche <alexeyche@yandex-team.ru>
2016-06-09 19:34:44 +0300

    Need to mock all methods subj

--- mapreduce/yt/client/mock_client.cpp
+++ mapreduce/yt/client/mock_client.cpp
@@ -7,12 +7,171 @@ namespace NYT {
 ////////////////////////////////////////////////////////////////////////////////
 
 namespace {
+
     TNodeId GetDefaultGuid() {
         TGUID guid;
         CreateGuid(&guid);
         return guid;
     }
+
+    class TMockTransaction
+        : public ITransaction
+        , public TMockClient
+    {
+    public:
+        TMockTransaction()
+            : TransactionId_(GetDefaultGuid())
+        {
+        }
+
+        const TTransactionId& GetId() const override
+        {
+            return TransactionId_;
+        }
+
+        TLockId Lock(
+            const TYPath&,
+            ELockMode,
+            const TLockOptions&) override
+        {
+            return GetDefaultGuid();
+        }
+        void Commit() override {}
+        void Abort() override {}
+
+    private:
+        TTransactionId TransactionId_;
+    };
+
+    class TMockFileReader
+        : public IFileReader
+    {
+    protected:
+        size_t DoRead(void*, size_t) override {
+            return 0;
+        }
+    };
+
+    class TMockFileWriter
+        : public IFileWriter
+    {
+    public:
+        ~TMockFileWriter() override {}
+
+    protected:
+        void DoWrite(const void*, size_t) override {}
+        void DoFinish() override {}
+    };
+
+    class TMockNodeTableReader
+        : public INodeReaderImpl
+    {
+    public:
+        ~TMockNodeTableReader() override {}
+
+        const TNode& GetRow() const override {
+            return Default;
+        }
+
+        bool IsValid() const override {
+            return false;
+        }
+
+        void Next() override {}
+
+        ui32 GetTableIndex() const override {
+            return 0;
+        }
+
+        ui64 GetRowIndex() const override {
+            return 0;
+        }
+
+        void NextKey() override {}
+    private:
+        TNode Default;
+    };
+
+
+    class TMockNodeTableWriter
+        : public INodeWriterImpl
+    {
+    public:
+        ~TMockNodeTableWriter() override {}
+
+        void AddRow(const TNode&, size_t) override {}
+        void Finish() override {}
+    };
+
+    class TMockProtoTableReader
+        : public IProtoReaderImpl
+    {
+    public:
+        ~TMockProtoTableReader() override {}
+
+        void ReadRow(Message*) override {}
+        void SkipRow() override {}
+        bool IsValid() const override {
+            return false;
+        }
+        void Next() override {}
+        ui32 GetTableIndex() const override {
+            return 0;
+        }
+        ui64 GetRowIndex() const override {
+            return 0;
+        }
+        void NextKey() override {}
+    };
+
+    class TMockProtoTableWriter
+        : public IProtoWriterImpl
+    {
+    public:
+        ~TMockProtoTableWriter() override {}
+
+        void AddRow(const Message&, size_t) override {}
+        void Finish() override {}
+    };
+
+    class TMockYaMRTableReader
+        : public IYaMRReaderImpl
+    {
+    public:
+        ~TMockYaMRTableReader() override {}
+
+        const TYaMRRow& GetRow() const override {
+            return Default;
+        }
+        bool IsValid() const override {
+            return false;
+        }
+        void Next() override {}
+        ui32 GetTableIndex() const override {
+            return 0;
+        }
+        ui64 GetRowIndex() const override {
+            return 0;
+        }
+        void NextKey() override {}
+    private:
+        TYaMRRow Default;
+    };
+
+
+    class TMockYaMRTableWriter
+        : public IYaMRWriterImpl
+    {
+    public:
+        ~TMockYaMRTableWriter() override {}
+
+        void AddRow(const TYaMRRow&, size_t) override {}
+        void Finish() override {}
+    };
+
 } // namespace
+
+
 
 
 TNodeId TMockClient::Create(const TYPath&, ENodeType, const TCreateOptions&) {
@@ -50,35 +209,35 @@ void TMockClient::Concatenate(const yvector<TYPath>&, const TYPath&, const TConc
 }
 
 IFileReaderPtr TMockClient::CreateFileReader(const TRichYPath&, const TFileReaderOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockFileReader();
 }
 
 IFileWriterPtr TMockClient::CreateFileWriter(const TRichYPath&, const TFileWriterOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockFileWriter();
 }
 
 TIntrusivePtr<INodeReaderImpl> TMockClient::CreateNodeReader(const TRichYPath&, const TTableReaderOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockNodeTableReader();
 }
 
 TIntrusivePtr<IYaMRReaderImpl> TMockClient::CreateYaMRReader(const TRichYPath&, const TTableReaderOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockYaMRTableReader();
 }
 
 TIntrusivePtr<IProtoReaderImpl> TMockClient::CreateProtoReader(const TRichYPath&, const TTableReaderOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockProtoTableReader();
 }
 
 TIntrusivePtr<INodeWriterImpl> TMockClient::CreateNodeWriter(const TRichYPath&, const TTableWriterOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockNodeTableWriter();
 }
 
 TIntrusivePtr<IYaMRWriterImpl> TMockClient::CreateYaMRWriter(const TRichYPath&, const TTableWriterOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockYaMRTableWriter();
 }
 
 TIntrusivePtr<IProtoWriterImpl> TMockClient::CreateProtoWriter(const TRichYPath&, const TTableWriterOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockProtoTableWriter();
 }
 
 TOperationId TMockClient::Sort(const TSortOperationSpec&, const TOperationOptions&) {
@@ -121,11 +280,11 @@ TOperationId TMockClient::DoMapReduce(const TMapReduceOperationSpec&, IJob*, IJo
 }
 
 ITransactionPtr TMockClient::StartTransaction(const TStartTransactionOptions&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockTransaction();
 }
 
 ITransactionPtr TMockClient::AttachTransaction(const TTransactionId&) {
-    ythrow yexception() << "This operation is not supported by TMockClient";
+    return new TMockTransaction();
 }
 
 void TMockClient::InsertRows(const TYPath&, const TNode::TList&) {
