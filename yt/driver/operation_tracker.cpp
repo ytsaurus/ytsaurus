461d58eac785e2c37dfa18dcff18d0005c1fcbb4
Maxim Babenko <babenko@ytsaurus.tech>
2016-06-13 13:00:10 +0300

    Fix build of legacy YT driver

--- yt/driver/operation_tracker.cpp
+++ yt/driver/operation_tracker.cpp
@@ -104,201 +104,201 @@ Stroka TOperationTracker::FormatProgress(const TYsonString& progress)
 
 void TOperationTracker::DumpProgress()
 {
-    auto operationPath = GetOperationPath(OperationId);
-
-    TObjectServiceProxy proxy(Driver->GetConnection()->GetMasterChannelOrThrow(NApi::EMasterChannelKind::Leader));
-    auto batchReq = proxy.ExecuteBatch();
-
-    {
-        auto req = TYPathProxy::Get(operationPath + "/@state");
-        batchReq->AddRequest(req, "get_state");
-    }
-
-    {
-        auto req = TYPathProxy::Get(operationPath + "/@progress");
-        batchReq->AddRequest(req, "get_progress");
-    }
-
-    auto batchRspOrError = batchReq->Invoke().Get();
-    THROW_ERROR_EXCEPTION_IF_FAILED(batchRspOrError, "Error getting operation progress");
-    const auto& batchRsp = batchRspOrError.Value();
-
-    EOperationState state;
-    {
-        auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_state");
-        THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation state");
-        const auto& rsp = rspOrError.Value();
-        state = ConvertTo<EOperationState>(TYsonString(rsp->value()));
-    }
-
-    TYsonString progress;
-    {
-        auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_progress");
-        THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation progress");
-        const auto& rsp = rspOrError.Value();
-        progress = TYsonString(rsp->value());
-    }
-
-    if (!PrevProgress || *PrevProgress != progress) {
-        if (state == EOperationState::Running) {
-            printf("%s: %s\n",
-                ~ToString(state),
-                ~FormatProgress(progress));
-        } else {
-            printf("%s\n", ~ToString(state));
-        }
-        PrevProgress = progress;
-    }
+    //auto operationPath = GetOperationPath(OperationId);
+    //
+    //TObjectServiceProxy proxy(Driver->GetConnection()->GetMasterChannelOrThrow(NApi::EMasterChannelKind::Leader));
+    //auto batchReq = proxy.ExecuteBatch();
+    //
+    //{
+    //    auto req = TYPathProxy::Get(operationPath + "/@state");
+    //    batchReq->AddRequest(req, "get_state");
+    //}
+    //
+    //{
+    //    auto req = TYPathProxy::Get(operationPath + "/@progress");
+    //    batchReq->AddRequest(req, "get_progress");
+    //}
+    //
+    //auto batchRspOrError = batchReq->Invoke().Get();
+    //THROW_ERROR_EXCEPTION_IF_FAILED(batchRspOrError, "Error getting operation progress");
+    //const auto& batchRsp = batchRspOrError.Value();
+    //
+    //EOperationState state;
+    //{
+    //    auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_state");
+    //    THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation state");
+    //    const auto& rsp = rspOrError.Value();
+    //    state = ConvertTo<EOperationState>(TYsonString(rsp->value()));
+    //}
+    //
+    //TYsonString progress;
+    //{
+    //    auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_progress");
+    //    THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation progress");
+    //    const auto& rsp = rspOrError.Value();
+    //    progress = TYsonString(rsp->value());
+    //}
+    //
+    //if (!PrevProgress || *PrevProgress != progress) {
+    //    if (state == EOperationState::Running) {
+    //        printf("%s: %s\n",
+    //            ~ToString(state),
+    //            ~FormatProgress(progress));
+    //    } else {
+    //        printf("%s\n", ~ToString(state));
+    //    }
+    //    PrevProgress = progress;
+    //}
 }
 
 void TOperationTracker::DumpResult()
 {
-    auto operationPath = GetOperationPath(OperationId);
-    auto jobsPath = GetJobsPath(OperationId);
-
-    TObjectServiceProxy proxy(Driver->GetConnection()->GetMasterChannelOrThrow(NApi::EMasterChannelKind::Leader));
-    auto batchReq = proxy.ExecuteBatch();
-
-    {
-        auto req = TYPathProxy::Get(operationPath + "/@result");
-        batchReq->AddRequest(req, "get_op_result");
-    }
-
-    {
-        auto req = TYPathProxy::Get(operationPath + "/@start_time");
-        batchReq->AddRequest(req, "get_op_start_time");
-    }
-    {
-        auto req = TYPathProxy::Get(operationPath + "/@finish_time");
-        batchReq->AddRequest(req, "get_op_finish_time");
-    }
-
-    {
-        auto req = TYPathProxy::Get(jobsPath);
-        std::vector<Stroka> attributeKeys{
-            "job_type",
-            "state",
-            "address",
-            "error"
-        };
-        ToProto(req->mutable_attributes()->mutable_keys(), attributeKeys);
-        batchReq->AddRequest(req, "get_jobs");
-    }
-
-    auto batchRspOrError = batchReq->Invoke().Get();
-    THROW_ERROR_EXCEPTION_IF_FAILED(batchRspOrError, "Error getting operation result");
-    const auto& batchRsp = batchRspOrError.Value();
-    {
-        {
-            auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_op_result");
-            THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation result");
-            const auto& rsp = rspOrError.Value();
-            auto resultNode = ConvertToNode(TYsonString(rsp->value()));
-            auto error = ConvertTo<TError>(GetNodeByYPath(resultNode, "/error"));
-            THROW_ERROR_EXCEPTION_IF_FAILED(error);
-        }
-
-        TInstant startTime;
-        {
-            auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_op_start_time");
-            THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation start time");
-            const auto& rsp = rspOrError.Value();
-            startTime = ConvertTo<TInstant>(TYsonString(rsp->value()));
-        }
-
-        TInstant endTime;
-        {
-            auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_op_finish_time");
-            THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation finish time");
-            const auto& rsp = rspOrError.Value();
-            endTime = ConvertTo<TInstant>(TYsonString(rsp->value()));
-        }
-        TDuration duration = endTime - startTime;
-
-        printf("Operation completed successfully in %s\n", ~ToString(duration));
-    }
-
-    {
-        auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_jobs");
-        THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation jobs info");
-        const auto& rsp = rspOrError.Value();
-
-        TEnumIndexedVector<int, EJobType> totalJobCount;
-        TEnumIndexedVector<int, EJobType> completedJobCount;
-        TEnumIndexedVector<int, EJobType> failedJobCount;
-        TEnumIndexedVector<int, EJobType> abortedJobCount;
-
-        auto jobs = ConvertToNode(TYsonString(rsp->value()))->AsMap();
-        if (jobs->GetChildCount() == 0) {
-            return;
-        }
-
-        std::list<TJobId> failedJobIds;
-        std::list<TJobId> stdErrJobIds;
-        for (const auto& pair : jobs->GetChildren()) {
-            auto jobId = TJobId::FromString(pair.first);
-            auto job = pair.second->AsMap();
-
-            auto jobType = job->Attributes().Get<EJobType>("job_type");
-            auto jobState = job->Attributes().Get<EJobState>("state");
-            ++totalJobCount[jobType];
-            switch (jobState) {
-                case EJobState::Completed:
-                    ++completedJobCount[jobType];
-                    break;
-                case EJobState::Failed:
-                    ++failedJobCount[jobType];
-                    failedJobIds.push_back(jobId);
-                    break;
-                case EJobState::Aborted:
-                    ++abortedJobCount[jobType];
-                    break;
-                default:
-                    YUNREACHABLE();
-            }
-
-            if (job->FindChild("stderr")) {
-                stdErrJobIds.push_back(jobId);
-            }
-        }
-
-        printf("\n");
-        printf("%-16s %10s %10s %10s %10s\n", "Job type", "Total", "Completed", "Failed", "Aborted");
-        for (auto jobType : TEnumTraits<EJobType>::GetDomainValues()) {
-            if (totalJobCount[jobType] > 0) {
-                printf("%-16s %10d %10d %10d %10d\n",
-                    ~ToString(EJobType(jobType)),
-                    totalJobCount[jobType],
-                    completedJobCount[jobType],
-                    failedJobCount[jobType],
-                    abortedJobCount[jobType]);
-            }
-        }
-
-        if (!failedJobIds.empty()) {
-            printf("\n");
-            printf("%" PRISZT " job(s) have failed:\n", failedJobIds.size());
-            for (const auto& jobId : failedJobIds) {
-                auto job = jobs->GetChild(ToString(jobId));
-                auto error = ConvertTo<TError>(job->Attributes().Get<INodePtr>("error"));
-                printf("\n");
-                printf("Job %s on %s\n%s\n",
-                    ~ToString(jobId),
-                    ~job->Attributes().Get<Stroka>("address"),
-                    ~ToString(error));
-            }
-        }
-
-        if (!stdErrJobIds.empty()) {
-            printf("\n");
-            printf("%" PRISZT " stderr(s) have been captured, use the following commands to view:\n",
-                stdErrJobIds.size());
-            for (const auto& jobId : stdErrJobIds) {
-                printf("yt download '%s'\n",
-                    ~GetStderrPath(OperationId, jobId));
-            }
-        }
-    }
+    //auto operationPath = GetOperationPath(OperationId);
+    //auto jobsPath = GetJobsPath(OperationId);
+    //
+    //TObjectServiceProxy proxy(Driver->GetConnection()->GetMasterChannelOrThrow(NApi::EMasterChannelKind::Leader));
+    //auto batchReq = proxy.ExecuteBatch();
+    //
+    //{
+    //    auto req = TYPathProxy::Get(operationPath + "/@result");
+    //    batchReq->AddRequest(req, "get_op_result");
+    //}
+    //
+    //{
+    //    auto req = TYPathProxy::Get(operationPath + "/@start_time");
+    //    batchReq->AddRequest(req, "get_op_start_time");
+    //}
+    //{
+    //    auto req = TYPathProxy::Get(operationPath + "/@finish_time");
+    //    batchReq->AddRequest(req, "get_op_finish_time");
+    //}
+    //
+    //{
+    //    auto req = TYPathProxy::Get(jobsPath);
+    //    std::vector<Stroka> attributeKeys{
+    //        "job_type",
+    //        "state",
+    //        "address",
+    //        "error"
+    //    };
+    //    ToProto(req->mutable_attributes()->mutable_keys(), attributeKeys);
+    //    batchReq->AddRequest(req, "get_jobs");
+    //}
+    //
+    //auto batchRspOrError = batchReq->Invoke().Get();
+    //THROW_ERROR_EXCEPTION_IF_FAILED(batchRspOrError, "Error getting operation result");
+    //const auto& batchRsp = batchRspOrError.Value();
+    //{
+    //    {
+    //        auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_op_result");
+    //        THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation result");
+    //        const auto& rsp = rspOrError.Value();
+    //        auto resultNode = ConvertToNode(TYsonString(rsp->value()));
+    //        auto error = ConvertTo<TError>(GetNodeByYPath(resultNode, "/error"));
+    //        THROW_ERROR_EXCEPTION_IF_FAILED(error);
+    //    }
+    //
+    //    TInstant startTime;
+    //    {
+    //        auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_op_start_time");
+    //        THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation start time");
+    //        const auto& rsp = rspOrError.Value();
+    //        startTime = ConvertTo<TInstant>(TYsonString(rsp->value()));
+    //    }
+    //
+    //    TInstant endTime;
+    //    {
+    //        auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_op_finish_time");
+    //        THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation finish time");
+    //        const auto& rsp = rspOrError.Value();
+    //        endTime = ConvertTo<TInstant>(TYsonString(rsp->value()));
+    //    }
+    //    TDuration duration = endTime - startTime;
+    //
+    //    printf("Operation completed successfully in %s\n", ~ToString(duration));
+    //}
+    //
+    //{
+    //    auto rspOrError = batchRsp->GetResponse<TYPathProxy::TRspGet>("get_jobs");
+    //    THROW_ERROR_EXCEPTION_IF_FAILED(rspOrError, "Error getting operation jobs info");
+    //    const auto& rsp = rspOrError.Value();
+    //
+    //    TEnumIndexedVector<int, EJobType> totalJobCount;
+    //    TEnumIndexedVector<int, EJobType> completedJobCount;
+    //    TEnumIndexedVector<int, EJobType> failedJobCount;
+    //    TEnumIndexedVector<int, EJobType> abortedJobCount;
+    //
+    //    auto jobs = ConvertToNode(TYsonString(rsp->value()))->AsMap();
+    //    if (jobs->GetChildCount() == 0) {
+    //        return;
+    //    }
+    //
+    //    std::list<TJobId> failedJobIds;
+    //    std::list<TJobId> stdErrJobIds;
+    //    for (const auto& pair : jobs->GetChildren()) {
+    //        auto jobId = TJobId::FromString(pair.first);
+    //        auto job = pair.second->AsMap();
+    //
+    //        auto jobType = job->Attributes().Get<EJobType>("job_type");
+    //        auto jobState = job->Attributes().Get<EJobState>("state");
+    //        ++totalJobCount[jobType];
+    //        switch (jobState) {
+    //            case EJobState::Completed:
+    //                ++completedJobCount[jobType];
+    //                break;
+    //            case EJobState::Failed:
+    //                ++failedJobCount[jobType];
+    //                failedJobIds.push_back(jobId);
+    //                break;
+    //            case EJobState::Aborted:
+    //                ++abortedJobCount[jobType];
+    //                break;
+    //            default:
+    //                YUNREACHABLE();
+    //        }
+    //
+    //        if (job->FindChild("stderr")) {
+    //            stdErrJobIds.push_back(jobId);
+    //        }
+    //    }
+    //
+    //    printf("\n");
+    //    printf("%-16s %10s %10s %10s %10s\n", "Job type", "Total", "Completed", "Failed", "Aborted");
+    //    for (auto jobType : TEnumTraits<EJobType>::GetDomainValues()) {
+    //        if (totalJobCount[jobType] > 0) {
+    //            printf("%-16s %10d %10d %10d %10d\n",
+    //                ~ToString(EJobType(jobType)),
+    //                totalJobCount[jobType],
+    //                completedJobCount[jobType],
+    //                failedJobCount[jobType],
+    //                abortedJobCount[jobType]);
+    //        }
+    //    }
+    //
+    //    if (!failedJobIds.empty()) {
+    //        printf("\n");
+    //        printf("%" PRISZT " job(s) have failed:\n", failedJobIds.size());
+    //        for (const auto& jobId : failedJobIds) {
+    //            auto job = jobs->GetChild(ToString(jobId));
+    //            auto error = ConvertTo<TError>(job->Attributes().Get<INodePtr>("error"));
+    //            printf("\n");
+    //            printf("Job %s on %s\n%s\n",
+    //                ~ToString(jobId),
+    //                ~job->Attributes().Get<Stroka>("address"),
+    //                ~ToString(error));
+    //        }
+    //    }
+    //
+    //    if (!stdErrJobIds.empty()) {
+    //        printf("\n");
+    //        printf("%" PRISZT " stderr(s) have been captured, use the following commands to view:\n",
+    //            stdErrJobIds.size());
+    //        for (const auto& jobId : stdErrJobIds) {
+    //            printf("yt download '%s'\n",
+    //                ~GetStderrPath(OperationId, jobId));
+    //        }
+    //    }
+    //}
 }
 
 EOperationType TOperationTracker::GetOperationType(const TOperationId& operationId)
