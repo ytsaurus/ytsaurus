380c681e883c108af5a3e9f9e7c14206ef4a0697
Maxim Babenko <babenko@ytsaurus.tech>
2016-06-20 11:30:22 +0300

    Revert "Init TTabletCellBundle::TabletCells on snapshot load"
    This reverts commit 9af0eb83ce6e8484de4bc6f3048dddab61ed007e.

--- yt/server/tablet_server/tablet_manager.cpp
+++ yt/server/tablet_server/tablet_manager.cpp
@@ -1070,8 +1070,6 @@ private:
             if (transaction) {
                 YCHECK(TransactionToCellMap_.insert(std::make_pair(transaction, cell)).second);
             }
-            // TODO(babenko): consider serializing TabletCells
-            YCHECK(cell->GetCellBundle()->TabletCells().insert(cell).second);
         }
     }
 
