1a9452e7cd560cbb6cf0510a109bc690145ab73b
Maxim Babenko <babenko@ytsaurus.tech>
2016-06-20 18:02:32 +0300

    Drop redundant checks

--- yt/server/tablet_node/store_trimmer.cpp
+++ yt/server/tablet_node/store_trimmer.cpp
@@ -48,10 +48,6 @@ private:
 
     void OnScanSlot(TTabletSlotPtr slot)
     {
-        if (slot->GetAutomatonState() != EPeerState::Leading) {
-            return;
-        }
-
         auto tabletManager = slot->GetTabletManager();
         for (const auto& pair : tabletManager->Tablets()) {
             auto* tablet = pair.second;
