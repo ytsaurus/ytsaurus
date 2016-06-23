12a67ad06e494efea264d6552fb54d0ddafe3771
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-06-23 15:48:49 +0300

    Increased nightly compression parallelism

--- scripts/cron/run_compress.sh
+++ scripts/cron/run_compress.sh
@@ -4,6 +4,6 @@ QUEUE="//sys/cron/tables_to_compress"
 
 /opt/cron/tools/compress.py find --queue $QUEUE
 
-/opt/cron/tools/run_parallel.sh "/opt/cron/tools/compress.py run --queue $QUEUE" 10 "/opt/cron/compression_log_$YT_PROXY"
+/opt/cron/tools/run_parallel.sh "/opt/cron/tools/compress.py run --queue $QUEUE" 20 "/opt/cron/compression_log_$YT_PROXY"
 
 
