84d739f9ae91cbfb14d0e9fe713fbdaf5f3b612d
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-06-27 17:23:50 +0300

    yandex-yt-local changelog.

--- python-git/yandex-yt-local/postprocess.sh
+++ python-git/yandex-yt-local/postprocess.sh
@@ -120,12 +120,12 @@ EOF
 }
 
 YANDEX_YT_LOCAL_VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
-YANDEX_YT_PYTHON_VERSION="0.7.7-0"
+YANDEX_YT_PYTHON_VERSION="0.7.9-0"
 
 if [ "$UBUNTU_CODENAME" = "precise" ]; then
-    YANDEX_YT_VERSIONS="0.17.5-stable-without-yt~8971~c508a19 18.3.20137-stable~a682102"
+    YANDEX_YT_VERSIONS="0.17.5-stable-without-yt~8971~c508a19 18.4.20227-stable~44af695"
 elif [ "$UBUNTU_CODENAME" = "trusty" ]; then
-    YANDEX_YT_VERSIONS="0.17.5-prestable-without-yt~7757~df46c24 18.3.20137-stable~a682102"
+    YANDEX_YT_VERSIONS="0.17.5-stable-without-yt~8416~c508a19 18.4.20227-stable~44af695"
 else
     echo "Ubuntu $UBUNTU_CODENAME is not currently supported"
     exit 0
