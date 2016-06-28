1604ceac76968668555974819cdf98e9190c7972
Alexander Dmitriev <monster@yandex-team.ru>
2016-06-28 16:21:07 +0300

    Use correct transaction when requesting extended table attributes

--- yt/ytlib/table_client/schemaless_chunk_writer.cpp
+++ yt/ytlib/table_client/schemaless_chunk_writer.cpp
@@ -955,7 +955,7 @@ void TSchemalessTableWriter::DoOpen()
         TObjectServiceProxy proxy(channel);
 
         auto req = TCypressYPathProxy::Get(objectIdPath);
-        SetTransactionId(req, UploadTransaction_);
+        SetTransactionId(req, Transaction_);
         std::vector<Stroka> attributeKeys{
             "replication_factor",
             "compression_codec",
