fdc1b53d34541a0130e14ce21feb73caf49f0969
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-07-04 20:35:19 +0300

    Add logging of zero memory of task demand

--- yt/server/scheduler/operation_controller_detail.cpp
+++ yt/server/scheduler/operation_controller_detail.cpp
@@ -854,7 +854,11 @@ TJobResources TOperationControllerBase::TTask::GetMinNeededResources() const
         YCHECK(GetPendingJobCount() > 0);
         CachedMinNeededResources = GetMinNeededResourcesHeavy();
     }
-    return ApplyMemoryReserve(*CachedMinNeededResources);
+    auto result = ApplyMemoryReserve(*CachedMinNeededResources);
+    if (result.GetUserSlots() > 0 && result.GetMemory() == 0) {
+        LOG_WARNING("Detected min needed resources of task with non-zero user slots and zero memory");
+    }
+    return result;
 }
 
 void TOperationControllerBase::TTask::RegisterIntermediate(
