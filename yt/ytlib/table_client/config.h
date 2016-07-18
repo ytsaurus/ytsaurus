fb94cba9b2549098a4896c52ae01cb60726672fa
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-07-18 10:27:12 +0300

    Cosmetics

--- yt/ytlib/table_client/config.h
+++ yt/ytlib/table_client/config.h
@@ -222,7 +222,7 @@ public:
 
         RegisterValidator([&] () {
             if (EnableRangeIndex && !EnableRowIndex) {
-                THROW_ERROR_EXCEPTION("\"enable_row_index\" must be set when \"enable_range_index\" is set");
+                THROW_ERROR_EXCEPTION("\"enable_range_index\" must be set when \"enable_row_index\" is set");
             }
         });
     }
