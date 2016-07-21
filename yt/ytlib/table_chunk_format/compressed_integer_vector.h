d7faa9c900f829a361f499320d44538e0457605b
Ruslan Savchenko <savrus@ytsaurus.tech>
2016-07-21 17:37:11 +0300

    Revert "YT-5215: Optimize TCompressedUnsignedVectorReader"
    This reverts commit b6c582265f9d2e3a24cd91c66b1db83bd7eacafc.

--- yt/ytlib/table_chunk_format/compressed_integer_vector.h
+++ yt/ytlib/table_chunk_format/compressed_integer_vector.h
@@ -43,17 +43,10 @@ private:
     size_t Size_;
     ui8 Width_;
 
-    const T* Values_;
-    std::unique_ptr<T[]> ValuesHolder_;
+    std::vector<T> Values_;
 
     T GetValue(size_t index) const;
     void UnpackValues();
-    template <int Width>
-    void UnpackValuesUnrolled();
-    template <class S>
-    void UnpackValuesAligned();
-    void UnpackValuesFallback();
-    
 };
 
 ////////////////////////////////////////////////////////////////////////////////
