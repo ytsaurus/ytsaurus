cecec85d03d7baf249567f3ba64758bdddd82329
Innokentii Goncharov <inngonch@yandex-team.ru>
2016-07-25 19:01:55 +0300

    restore to existing table option

--- python-git/yt/tools/dump_restore_client.py
+++ python-git/yt/tools/dump_restore_client.py
@@ -1,26 +1,39 @@
-from yt.wrapper.common import run_with_retries
-from yt.tools.dynamic_tables import DynamicTablesClient, \
-    get_schema_and_key_columns, get_dynamic_table_attributes, \
-    split_in_groups, log_exception
+from datetime import datetime
+
+import yt.wrapper.common as yt_common
+import yt.tools.dynamic_tables as dt_module
 
 # Attribute prefix
 ATTRIBUTE_PREFIX = "_yt_dump_restore_"
 
 
-def save_attributes(dst, schema, key_columns, pivot_keys, yt):
-    """ Save dynamic table properties to simple attributes """
-    yt.set(dst + "/@" + ATTRIBUTE_PREFIX + "schema", schema)
-    yt.set(dst + "/@" + ATTRIBUTE_PREFIX + "key_columns", key_columns)
-    yt.set(dst + "/@" + ATTRIBUTE_PREFIX + "pivot_keys", pivot_keys)
+def _drop_sort_order(schema):
+    """
+    Drops sort_order from columns to make schema comparision
+    simplier (but key columns must be then compared separately)
+    """
+    result = []
+    for column in schema:
+        result_column = dict(column)  # copies it
+        if "sort_order" in column:
+            del result_column["sort_order"]
+        result.append(result_column)
+    return result
 
 
-def restore_attributes(src, yt):
-    """ Reverse of the `save_attributes`: load dynamic table
+def save_attributes(dst, yt, **attributes):
+    """ Save table properties to simple attributes """
+    for attr, value in attributes.iteritems():
+        yt.set(dst + "/@" + ATTRIBUTE_PREFIX + attr, value)
+
+
+def restore_attributes(src, yt, attributes):
+    """ Reverse of the `save_attributes`: load requested table
     properties from simple attributes """
-    schema = yt.get(src + "/@" + ATTRIBUTE_PREFIX + "schema")
-    key_columns = yt.get(src + "/@" + ATTRIBUTE_PREFIX + "key_columns")
-    pivot_keys = yt.get(src + "/@" + ATTRIBUTE_PREFIX + "pivot_keys")
-    return schema, key_columns, pivot_keys
+    result = []
+    for attr in attributes:
+        result.append(yt.get(src + "/@" + ATTRIBUTE_PREFIX + attr))
+    return tuple(result)
 
 
 def create_destination(dst, yt, force=False, attributes=None):
@@ -33,15 +46,34 @@ def create_destination(dst, yt, force=False, attributes=None):
     yt.create_table(dst, attributes=attributes)
 
 
-class DumpRestoreClient(DynamicTablesClient):
+def check_table_schema(dst, schema, key_columns, yt):
+    """ Check that table exists and has specified schema """
+    if not yt.exists(dst):
+        raise Exception("Destination table does not exists")
+    real_schema, real_key_columns = dt_module.get_schema_and_key_columns(yt, dst)
+    if _drop_sort_order(real_schema) != _drop_sort_order(schema):
+        raise Exception("Destination table schema does not match")
+    if real_key_columns != key_columns:
+        raise Exception("Destination table key columns does not match")
+
+
+class DumpRestoreClient(dt_module.DynamicTablesClient):
     def dump_table(self, src_table, dst_table, force=False, predicate=None):
         """ Dump dynamic table rows matching the predicate into a static table """
-        schema, key_columns = get_schema_and_key_columns(self.yt, src_table)
+        schema, key_columns = dt_module.get_schema_and_key_columns(self.yt, src_table)
         tablets = self.yt.get(src_table + "/@tablets")
         pivot_keys = sorted([tablet["pivot_key"] for tablet in tablets])
 
         create_destination(dst_table, yt=self.yt, force=force)
-        save_attributes(dst_table, schema, key_columns, pivot_keys, yt=self.yt)
+        save_attributes(
+            dst_table,
+            origin=src_table,
+            schema=schema,
+            key_columns=key_columns,
+            pivot_keys=pivot_keys,
+            time=yt_common.datetime_to_string(datetime.utcnow()),
+            predicate=predicate,
+            yt=self.yt)
 
         self.run_map_over_dynamic(
             None,  # empty mapper, i.e. dump not modified rows
@@ -49,17 +81,23 @@ class DumpRestoreClient(DynamicTablesClient):
             dst_table,
             predicate=predicate)
 
-    def restore_table(self, src_table, dst_table, force=False):
+    def restore_table(self, src_table, dst_table, force=False, to_existing=False):
         """ Insert static table rows into a new dynamic table """
-        schema, key_columns, pivot_keys = restore_attributes(src_table, yt=self.yt)
-
-        create_destination(
-            dst_table,
+        schema, key_columns, pivot_keys = restore_attributes(
+            src_table,
             yt=self.yt,
-            force=force,
-            attributes=get_dynamic_table_attributes(self.yt, schema, key_columns))
-        self.yt.reshard_table(dst_table, pivot_keys)
-        self.mount_table(dst_table)
+            attributes=("schema", "key_columns", "pivot_keys"))
+
+        if to_existing:
+            check_table_schema(dst_table, schema, key_columns, yt=self.yt)
+        else:
+            create_destination(
+                dst_table,
+                yt=self.yt,
+                force=force,
+                attributes=dt_module.get_dynamic_table_attributes(self.yt, schema, key_columns))
+            self.yt.reshard_table(dst_table, pivot_keys)
+            self.mount_table(dst_table)
 
         self.run_map_dynamic(
             None,  # empty mapper, i.e. restore not modified rows
@@ -68,7 +106,7 @@ class DumpRestoreClient(DynamicTablesClient):
 
     def erase_table(self, table, predicate=None):
         """ Delete all rows matching the predicate from a dynamic table """
-        _, key_columns = get_schema_and_key_columns(self.yt, table)
+        _, key_columns = dt_module.get_schema_and_key_columns(self.yt, table)
 
         def erase_mapper(rows):
             client = self.make_driver_yt_client()
@@ -79,9 +117,9 @@ class DumpRestoreClient(DynamicTablesClient):
 
                 return do_delete
 
-            for rowset in split_in_groups(rows, self.batch_size):
+            for rowset in dt_module.split_in_groups(rows, self.batch_size):
                 do_delete_these = make_deleter(rowset)
-                run_with_retries(do_delete_these, except_action=log_exception)
+                yt_common.run_with_retries(do_delete_these, except_action=dt_module.log_exception)
 
             if False:  # make this function into a generator
                 yield
@@ -93,3 +131,17 @@ class DumpRestoreClient(DynamicTablesClient):
                 out_table,
                 columns=key_columns,
                 predicate=predicate)
+
+    def get_schema_and_key_columns(self, table):
+        """
+        Unified way to get schema and key columns info (either from a real
+        dynamic table or from attributes saved with table dump)
+        """
+        if self.yt.get_attribute(table, "dynamic"):
+            schema, key_columns = dt_module.get_schema_and_key_columns(self.yt, table)
+        else:
+            schema, key_columns = restore_attributes(
+                table,
+                yt=self.yt,
+                attributes=("schema", "key_columns"))
+        return _drop_sort_order(schema), key_columns
