0cc6a9b97b85caeca79c4dc71251e8c8aef70d74
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-07-26 18:11:39 +0300

    YT-5269: Added tests for yt_local binary. Refactored tests, fixed YT environment leakage.

--- python-git/yt/local/tests/test_local_mode.py
+++ python-git/yt/local/tests/test_local_mode.py
@@ -1,5 +1,6 @@
 from yt.local import start, stop, delete
 from yt.wrapper.client import Yt
+from yt.common import remove_file
 import yt.wrapper as yt
 
 import yt.yson as yson
@@ -8,9 +9,12 @@ import yt.json as json
 import os
 import pytest
 import tempfile
+import subprocess
+import contextlib
 
 TESTS_LOCATION = os.path.dirname(os.path.abspath(__file__))
 TESTS_SANDBOX = os.environ.get("TESTS_SANDBOX", os.path.join(TESTS_LOCATION, "sandbox"))
+YT_LOCAL_BINARY = os.path.join(os.path.dirname(TESTS_LOCATION), "yt_local")
 
 def _read_pids_file(environment):
     if not os.path.exists(environment.pids_filename):
@@ -20,6 +24,32 @@ def _read_pids_file(environment):
 
 def _is_exists(environment):
     return os.path.exists(os.path.join(TESTS_SANDBOX, environment.id))
+
+@contextlib.contextmanager
+def local_yt(*args, **kwargs):
+    try:
+        environment = start(*args, **kwargs)
+        yield environment
+    finally:
+        stop(environment.id)
+
+class YtLocalBinary(object):
+    def __init__(self, root_path, port_locks_path):
+        self.root_path = root_path
+        self.port_locks_path = port_locks_path
+
+    def __call__(self, *args, **kwargs):
+        args_str = " ".join(args)
+        kwargs_str = " ".join("--{0} {1}".format(key.replace("_", "-"), value) for key, value in kwargs.iteritems())
+        command = "{0} {1} {2}".format(YT_LOCAL_BINARY, args_str, kwargs_str)
+        env = {
+            "YT_LOCAL_ROOT_PATH": self.root_path,
+            "YT_LOCAL_PORT_LOCKS_PATH": self.port_locks_path,
+            "PYTHONPATH": os.environ["PYTHONPATH"],
+            "PATH": os.environ["PATH"],
+            "YT_LOCAL_USE_PROXY_FROM_SOURCE": "1"
+        }
+        return subprocess.check_output(command, shell=True, env=env).strip()
 
 class TestLocalMode(object):
     @classmethod
@@ -31,6 +61,8 @@ class TestLocalMode(object):
         os.environ["YT_LOCAL_USE_PROXY_FROM_SOURCE"] = '1'
         # Add ports_lock_path argument to YTEnvironment for parallel testing.
         os.environ["YT_LOCAL_PORT_LOCKS_PATH"] = os.path.join(TESTS_SANDBOX, "ports")
+        cls.yt_local = YtLocalBinary(os.environ["YT_LOCAL_ROOT_PATH"],
+                                     os.environ["YT_LOCAL_PORT_LOCKS_PATH"])
 
     @classmethod
     def teardown_class(cls):
@@ -42,13 +74,12 @@ class TestLocalMode(object):
         del os.environ["YT_LOCAL_PORT_LOCKS_PATH"]
 
     def test_commands_sanity(self):
-        environment = start()
-        pids = _read_pids_file(environment)
-        assert len(pids) == 4
-        # Should not delete running instance
-        with pytest.raises(yt.YtError):
-            delete(environment.id)
-        stop(environment.id)
+        with local_yt() as environment:
+            pids = _read_pids_file(environment)
+            assert len(pids) == 4
+            # Should not delete running instance
+            with pytest.raises(yt.YtError):
+                delete(environment.id)
         # Should not stop already stopped instance
         with pytest.raises(yt.YtError):
             stop(environment.id)
@@ -60,31 +91,27 @@ class TestLocalMode(object):
         with pytest.raises(yt.YtError):
             start(master_count=0)
 
-        environment = start(master_count=3, node_count=0, scheduler_count=0, enable_debug_logging=True)
-        assert len(_read_pids_file(environment)) == 4  # + proxy
-        assert len(environment.configs["master"]) == 3
-        stop(environment.id)
+        with local_yt(master_count=3, node_count=0, scheduler_count=0,
+                      enable_debug_logging=True) as environment:
+            assert len(_read_pids_file(environment)) == 4  # + proxy
+            assert len(environment.configs["master"]) == 3
 
-        environment = start(node_count=5, scheduler_count=2, start_proxy=False)
-        assert len(environment.configs["node"]) == 5
-        assert len(environment.configs["scheduler"]) == 2
-        assert len(environment.configs["master"]) == 1
-        assert len(_read_pids_file(environment)) == 8
-        with pytest.raises(yt.YtError):
-            environment.get_proxy_address()
-        stop(environment.id)
+        with local_yt(node_count=5, scheduler_count=2, start_proxy=False) as environment:
+            assert len(environment.configs["node"]) == 5
+            assert len(environment.configs["scheduler"]) == 2
+            assert len(environment.configs["master"]) == 1
+            assert len(_read_pids_file(environment)) == 8
+            with pytest.raises(yt.YtError):
+                environment.get_proxy_address()
 
-        environment = start(node_count=1)
-        assert len(_read_pids_file(environment)) == 4  # + proxy
-        stop(environment.id)
+        with local_yt(node_count=1) as environment:
+            assert len(_read_pids_file(environment)) == 4  # + proxy
 
-        environment = start(node_count=0, scheduler_count=0, start_proxy=False)
-        assert len(_read_pids_file(environment)) == 1
-        stop(environment.id)
+        with local_yt(node_count=0, scheduler_count=0, start_proxy=False) as environment:
+            assert len(_read_pids_file(environment)) == 1
 
     def test_use_local_yt(self):
-        environment = start()
-        try:
+        with local_yt() as environment:
             proxy_port = environment.get_proxy_address().rsplit(":", 1)[1]
             client = Yt(proxy="localhost:{0}".format(proxy_port))
             client.config["tabular_data_format"] = yt.format.DsvFormat()
@@ -106,13 +133,10 @@ class TestLocalMode(object):
             assert [{"a": "b"}] == list(client.read_table(table))
 
             assert set(client.search("//test")) == set(["//test", "//test/folder", table])
-        finally:
-            stop(environment.id)
 
     def test_local_cypress_synchronization(self):
         local_cypress_path = os.path.join(TESTS_LOCATION, "local_cypress_tree")
-        environment = start(local_cypress_dir=local_cypress_path)
-        try:
+        with local_yt(local_cypress_dir=local_cypress_path) as environment:
             proxy_port = environment.get_proxy_address().rsplit(":", 1)[1]
             client = Yt(proxy="localhost:{0}".format(proxy_port))
             assert list(client.read_table("//table")) == [{"x": "1", "y": "1"}]
@@ -120,38 +144,82 @@ class TestLocalMode(object):
             assert client.get_attribute("//table", "myattr") == 4
             assert client.get_attribute("//subdir", "other_attr") == 42
             assert client.get_attribute("/", "root_attr") == "ok"
-        finally:
-            stop(environment.id)
 
     def test_preserve_state(self):
-        environment = start()
-        client = environment.create_client()
-        client.write_table("//home/my_table", [{"x": 1, "y": 2, "z": 3}])
-        stop(environment.id)
+        with local_yt() as environment:
+            client = environment.create_client()
+            client.write_table("//home/my_table", [{"x": 1, "y": 2, "z": 3}])
 
-        environment = start(id=environment.id)
-        client = environment.create_client()
-        assert list(client.read_table("//home/my_table")) == [{"x": 1, "y": 2, "z": 3}]
+        with local_yt(id=environment.id) as environment:
+            client = environment.create_client()
+            assert list(client.read_table("//home/my_table")) == [{"x": 1, "y": 2, "z": 3}]
 
     def test_configs_patches(self):
         patch = {"test_key": "test_value"}
-        with tempfile.NamedTemporaryFile(dir=TESTS_SANDBOX, delete=False) as yson_file:
-            yson.dump(patch, yson_file)
-        with tempfile.NamedTemporaryFile(dir=TESTS_SANDBOX, delete=False) as json_file:
-            json.dump(patch, json_file)
-
-        environment = start(
-            master_config=yson_file.name,
-            node_config=yson_file.name,
-            scheduler_config=yson_file.name,
-            proxy_config=json_file.name)
-
         try:
-            for service in ["master", "node", "scheduler", "proxy"]:
-                if isinstance(environment.configs[service], list):
-                    for config in environment.configs[service]:
-                        assert config["test_key"] == "test_value"
-                else:  # Proxy config
-                    assert environment.configs[service]["test_key"] == "test_value"
+            with tempfile.NamedTemporaryFile(dir=TESTS_SANDBOX, delete=False) as yson_file:
+                yson.dump(patch, yson_file)
+            with tempfile.NamedTemporaryFile(dir=TESTS_SANDBOX, delete=False) as json_file:
+                json.dump(patch, json_file)
+
+            with local_yt(master_config=yson_file.name,
+                          node_config=yson_file.name,
+                          scheduler_config=yson_file.name,
+                          proxy_config=json_file.name) as environment:
+                for service in ["master", "node", "scheduler", "proxy"]:
+                    if isinstance(environment.configs[service], list):
+                        for config in environment.configs[service]:
+                            assert config["test_key"] == "test_value"
+                    else:  # Proxy config
+                        assert environment.configs[service]["test_key"] == "test_value"
         finally:
-            stop(environment.id)
+            remove_file(yson_file.name, force=True)
+            remove_file(json_file.name, force=True)
+
+    def test_yt_local_binary(self):
+        env_id = self.yt_local("start", fqdn="localhost")
+        try:
+            client = Yt(proxy=self.yt_local("get_proxy", env_id))
+            assert "sys" in client.list("/")
+        finally:
+            self.yt_local("stop", env_id)
+
+        env_id = self.yt_local("start", fqdn="localhost", master_count=3, node_count=5, scheduler_count=2)
+        try:
+            client = Yt(proxy=self.yt_local("get_proxy", env_id))
+            assert len(client.list("//sys/nodes")) == 5
+            assert len(client.list("//sys/scheduler/instances")) == 2
+            assert len(client.list("//sys/primary_masters")) == 3
+        finally:
+            self.yt_local("stop", env_id, "--delete")
+
+        patch = {"exec_agent": {"job_controller": {"resource_limits": {"user_slots": 100}}}}
+        try:
+            with tempfile.NamedTemporaryFile(dir=TESTS_SANDBOX, delete=False) as node_config:
+                yson.dump(patch, node_config)
+            with tempfile.NamedTemporaryFile(dir=TESTS_SANDBOX, delete=False) as config:
+                yson.dump({"yt_local_test_key": "yt_local_test_value"}, config)
+
+            env_id = self.yt_local(
+                "start",
+                fqdn="localhost",
+                node_count=1,
+                node_config=node_config.name,
+                scheduler_count=1,
+                scheduler_config=config.name,
+                master_count=1,
+                master_config=config.name)
+
+            try:
+                client = Yt(proxy=self.yt_local("get_proxy", env_id))
+                node_address = client.list("//sys/nodes")[0]
+                assert client.get("//sys/nodes/{0}/@resource_limits/user_slots".format(node_address)) == 100
+                for subpath in ["primary_masters", "scheduler/instances"]:
+                    address = client.list("//sys/{0}".format(subpath))[0]
+                    assert client.get("//sys/{0}/{1}/orchid/config/yt_local_test_key"
+                                      .format(subpath, address)) == "yt_local_test_value"
+            finally:
+                self.yt_local("stop", env_id)
+        finally:
+            remove_file(node_config.name, force=True)
+            remove_file(config.name, force=True)
