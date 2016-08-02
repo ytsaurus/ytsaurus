3527788ec06bdb9c06e13070ef981852d50fd6b0
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-08-02 21:53:40 +0300

    JobController profiling

--- yt/server/job_agent/private.h
+++ yt/server/job_agent/private.h
@@ -4,12 +4,15 @@
 
 #include <yt/core/logging/log.h>
 
+#include <yt/core/profiling/profiler.h>
+
 namespace NYT {
 namespace NJobAgent {
 
 ////////////////////////////////////////////////////////////////////////////////
 
 extern const NLogging::TLogger JobTrackerServerLogger;
+extern const NProfiling::TProfiler Profiler;
 
 ////////////////////////////////////////////////////////////////////////////////
 
