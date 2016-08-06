ecd38deb1158352fecc346e778176da678aa375e
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-08-06 10:35:59 +0300

    set_account is more tolerant to deleted objects

--- python-git/yt/tools/binaries/set_account.py
+++ python-git/yt/tools/binaries/set_account.py
@@ -28,6 +28,8 @@ def main():
             except yt.YtResponseError as error:
                 if error.is_concurrent_transaction_lock_conflict():
                     logger.warning("Cannot set '%s' account to node '%s'", args.account, obj)
+                elif error.is_resolve_error():
+                    pass
                 else:
                     raise
             logger.info("Account '%s' set to node '%s'", args.account, obj)
