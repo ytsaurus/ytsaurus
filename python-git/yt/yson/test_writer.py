2143f1a8524c026f82d5485542989d68d51c1532
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-08-12 15:29:00 +0300

    Fix yson formatting

--- python-git/yt/yson/test_writer.py
+++ python-git/yt/yson/test_writer.py
@@ -97,6 +97,25 @@ class YsonWriterTestBase(object):
         assert "#" == self.dumps(None)
         assert "#" == self.dumps(YsonEntity())
 
+    def test_formatting(self):
+        assert '{\n    "a" = "b";\n}' == self.dumps({"a": "b"}, yson_format="pretty")
+        assert '{"a"="b";}' == self.dumps({"a": "b"})
+        canonical_result = """\
+{
+    "x" = {
+        "a" = [
+            1;
+            2;
+            3;
+        ];
+    };
+}"""
+        assert canonical_result == self.dumps({"x": {"a": [1, 2, 3]}}, yson_format="pretty")
+        assert '{"x"={"a"=[1;2;3;];};}' == self.dumps({"x": {"a": [1, 2, 3]}})
+        assert '"x"=1;\n' == self.dumps({"x": 1}, yson_type="map_fragment")
+        assert '"x" = 1;\n' == self.dumps({"x": 1}, yson_type="map_fragment", yson_format="pretty")
+        assert '1;\n2;\n3;\n' == self.dumps([1, 2, 3], yson_type="list_fragment")
+        assert '1;\n2;\n3;\n' == self.dumps([1, 2, 3], yson_type="list_fragment", yson_format="pretty")
 
 class TestWriterDefault(YsonWriterTestBase):
     @staticmethod
@@ -128,3 +147,17 @@ if yt_yson_bindings:
         def test_zero_byte(self):
             assert '"\\0"' == self.dumps("\x00")
             assert '\x01\x02\x00' == self.dumps("\x00", yson_format="binary")
+
+if yt_yson_bindings:
+    def test_equal_formatting():
+        def _assert_dumps_equal(obj, **kwargs):
+            assert yt.yson.writer.dumps(obj, **kwargs) == yt_yson_bindings.dumps(obj, **kwargs)
+
+        _assert_dumps_equal({"a": "b"})
+        _assert_dumps_equal({"a": {"b": [1, 2, 3]}})
+        _assert_dumps_equal({"a": "b"}, yson_format="pretty")
+        _assert_dumps_equal({"a": {"b": [1, 2, 3]}}, yson_format="pretty")
+        _assert_dumps_equal({"a": "b", "c": {"d": "e"}}, yson_type="map_fragment")
+        _assert_dumps_equal({"a": "b", "c": {"d": "e"}}, yson_type="map_fragment", yson_format="pretty")
+        _assert_dumps_equal([1, 2, 3, "four", "five"], yson_type="list_fragment")
+        _assert_dumps_equal([1, 2, 3, "four_five"], yson_type="list_fragment", yson_format="pretty")
