ae8aaf47c771723b5070b3be62575b7a19262d67
Maxim Babenko <babenko@ytsaurus.tech>
2016-08-12 16:28:53 +0300

    Forbid moving dynamic tables within transactions

--- yt/server/table_server/table_node.cpp
+++ yt/server/table_server/table_node.cpp
@@ -218,6 +218,9 @@ protected:
                 if (sourceNode->HasMountedTablets()) {
                     THROW_ERROR_EXCEPTION("Cannot move a dynamic table with mounted tablets");
                 }
+                if (sourceNode->IsDynamic() && factory->GetTransaction()) {
+                    THROW_ERROR_EXCEPTION("Cannot move a dynamic table within a transaction");
+                }
                 break;
 
             default:
