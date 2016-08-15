43a06618951ece5fe73387299934e31fa56ae23c
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-08-15 13:29:00 +0300

    Removed private imports (py3 module does not have them)

--- python-git/yt/wrapper/system_random.py
+++ python-git/yt/wrapper/system_random.py
@@ -1,10 +1,12 @@
-from random import Random, _hexlify, _urandom, RECIP_BPF
+from random import Random, RECIP_BPF
+from binascii import hexlify
+from os import urandom
 
 class SystemRandom(Random):
     """ Alternative implementation of SystemRandom that supports pickle and deepcopy """
 
     def random(self):
-        return (long(_hexlify(_urandom(7)), 16) >> 3) * RECIP_BPF
+        return (long(hexlify(urandom(7)), 16) >> 3) * RECIP_BPF
 
     def getrandbits(self, k):
         if k <= 0:
@@ -12,7 +14,7 @@ class SystemRandom(Random):
         if k != int(k):
             raise TypeError('number of bits should be an integer')
         bytes = (k + 7) // 8
-        x = long(_hexlify(_urandom(bytes)), 16)
+        x = long(hexlify(urandom(bytes)), 16)
         return x >> (bytes * 8 - k)
 
     def _stub(self, *args, **kwds):
