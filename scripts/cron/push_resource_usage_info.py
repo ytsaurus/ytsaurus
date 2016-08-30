63b48bf41609cc28a8c0b87bec064e88b84890ac
Andrey Kashin <acid@yandex-team.ru>
2016-08-30 16:43:45 +0300

    Push quota information to solomon

--- scripts/cron/push_resource_usage_info.py
+++ scripts/cron/push_resource_usage_info.py
@@ -18,13 +18,21 @@ from socket import error as SocketError
 from requests import HTTPError, ConnectionError, Timeout
 
 STATFACE_PUSH_URL = "https://upload.stat.yandex-team.ru/_api/report/data"
+SOLOMON_PUSH_URL = "http://api.solomon.search.yandex.net/push/json"
 
 PUSH_RETRIES_COUNT = 5
 PUSH_REQUEST_TIMEOUT = 20000
 
-REPORT_NAME = "YT/AccountsResourceUsage"
+STATFACE_REPORT_NAME = "YT/AccountsResourceUsage"
 
-NOW = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+def datetime_to_timestamp(dt):
+    return (dt - datetime(1970, 1, 1)).total_seconds()
+
+NOW = datetime.now()
+NOW_STR = NOW.strftime("%Y-%m-%d %H:%M:%S")
+
+UTC_NOW = datetime.utcnow()
+UTC_NOW_TS = datetime_to_timestamp(UTC_NOW)
 
 def get_account_statistics(account):
     resource_usage = account.attributes["resource_usage"]
@@ -48,38 +56,73 @@ def collect_accounts_data_for_cluster(cluster):
     result = []
     for account in client.list("//sys/accounts", attributes=["resource_usage", "resource_limits"]):
         account_data = get_account_statistics(account)
-        account_data["fielddate"] = NOW
-        account_data["cluster"] = cluster
         result.append(account_data)
     return result
 
-def push_cluster_data(accounts_data, headers):
-    data = {}
-    data["name"] = REPORT_NAME
-    data["scale"] = "h"
-    data["json_data"] = json.dumps({"values": accounts_data})
+def push_data_with_retries(url, data, headers):
     for attempt in xrange(PUSH_RETRIES_COUNT):
         current_time = datetime.now()
         try:
-            r = requests.post(STATFACE_PUSH_URL, data=data, headers=headers, timeout=PUSH_REQUEST_TIMEOUT)
+            r = requests.post(url, data=data, headers=headers, timeout=PUSH_REQUEST_TIMEOUT)
             r.raise_for_status()
             return
         except (Timeout, ConnectionError, HTTPError, SocketError) as error:
             if attempt + 1 == PUSH_RETRIES_COUNT:
                 raise
             logging.warning('HTTP POST request (url: %s) failed with error %s, message: "%s"',
-                    STATFACE_PUSH_URL, str(type(error)), error.message)
+                url, str(type(error)), error.message)
             backoff = get_backoff(PUSH_REQUEST_TIMEOUT, current_time)
             if backoff:
                 logging.warning("Sleep for %.2lf seconds before next retry", backoff)
                 time.sleep(backoff)
             logging.warning("New retry (%d) ...", attempt + 2)
 
+def convert_data_to_statface_format(cluster, accounts_data):
+    converted_accounts_data = accounts_data.copy()
+    for account_data in converted_accounts_data:
+        account_data["fielddate"] = NOW_STR
+        account_data["cluster"] = cluster
+    return converted_accounts_data
+
+def push_cluster_data_to_statface(cluster, accounts_data, headers):
+    data = {}
+    data["name"] = STATFACE_REPORT_NAME
+    data["scale"] = "h"
+    data["json_data"] = json.dumps({
+        "values": convert_data_to_statface_format(cluster, accounts_data)
+    })
+    push_data_with_retries(STATFACE_PUSH_URL, data, headers)
+
+def convert_data_to_solomon_format(accounts_data):
+    sensors = []
+    for account_data in accounts_data:
+        account = account_data["account"]
+        for key, value in account_data.items():
+            if key == "account":
+                continue
+
+            sensors.append({
+                "labels": {"account": str(account), "sensor": key},
+                "ts": UTC_NOW_TS,
+                "value": value
+            })
+    return sensors
+
+def push_cluster_data_to_solomon(cluster, accounts_data, headers):
+    data = {}
+    data["commonLabels"] = {
+        "project": "yt",
+        "cluster": cluster,
+        "service": "misc"
+    }
+    data["sensors"] = convert_data_to_solomon_format(accounts_data)
+    push_data_with_retries(SOLOMON_PUSH_URL, json.dumps(data), headers)
+
 def main():
     logging.basicConfig(level=logging.INFO, format="%(asctime)-15s\t%(levelname)s\t%(message)s")
 
     parser = argparse.ArgumentParser(description="Pushes accounts resource usage of various "
-                                                 "YT clusters to statface.")
+                                                 "YT clusters to statface and solomon.")
 
     parser.add_argument("--robot-login", default=os.environ.get("STATFACE_ROBOT_LOGIN"))
     parser.add_argument("--robot-password", default=os.environ.get("STATFACE_ROBOT_PASSWORD"))
@@ -91,9 +134,13 @@ def main():
         print >>sys.stderr, "Statface credentials are not set correctly"
         sys.exit(1)
 
-    headers = {
+    statface_headers = {
         "StatRobotUser": args.robot_login,
         "StatRobotPassword": args.robot_password
+    }
+
+    solomon_headers = {
+        "Content-Type": "application/json"
     }
 
     logging.info("Retrieving clusters configuration from %s", args.clusters_config_url)
@@ -110,7 +157,8 @@ def main():
         logging.info("Fetching accounts info from %s", cluster)
         try:
             accounts_data = collect_accounts_data_for_cluster(cluster)
-            push_cluster_data(accounts_data, headers)
+            push_cluster_data_to_statface(clusert, accounts_data, statface_headers)
+            push_cluster_data_to_solomon(cluster, accounts_data, solomon_headers)
         except exceptions:
             logging.exception("Failed to fetch account info from %s", cluster)
 
