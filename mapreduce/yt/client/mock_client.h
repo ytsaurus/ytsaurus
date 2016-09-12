b11b22856d86dc07a785a8b6eaf6f8759815c55f
agorodilov <agorodilov@yandex-team.ru>
2016-09-12 22:04:20 +0300

    REVIEW:189856 add override

--- mapreduce/yt/client/mock_client.h
+++ mapreduce/yt/client/mock_client.h
@@ -16,77 +16,77 @@ public:
 
     TNode Get(const TYPath&, const TGetOptions&) override final;
 
-    void Set(const TYPath&, const TNode&);
+    void Set(const TYPath&, const TNode&) override;
 
-    TNode::TList List(const TYPath&, const TListOptions&);
+    TNode::TList List(const TYPath&, const TListOptions&) override;
 
-    TNodeId Copy(const TYPath&, const TYPath&, const TCopyOptions&);
+    TNodeId Copy(const TYPath&, const TYPath&, const TCopyOptions&) override;
 
-    TNodeId Move(const TYPath&, const TYPath&, const TMoveOptions&);
+    TNodeId Move(const TYPath&, const TYPath&, const TMoveOptions&) override;
 
-    TNodeId Link(const TYPath&, const TYPath&, const TLinkOptions&);
+    TNodeId Link(const TYPath&, const TYPath&, const TLinkOptions&) override;
 
-    void Concatenate(const yvector<TYPath>&, const TYPath&, const TConcatenateOptions&);
+    void Concatenate(const yvector<TYPath>&, const TYPath&, const TConcatenateOptions&) override;
 
-    IFileReaderPtr CreateFileReader(const TRichYPath&, const TFileReaderOptions&);
+    IFileReaderPtr CreateFileReader(const TRichYPath&, const TFileReaderOptions&) override;
 
-    IFileWriterPtr CreateFileWriter(const TRichYPath&, const TFileWriterOptions&);
+    IFileWriterPtr CreateFileWriter(const TRichYPath&, const TFileWriterOptions&) override;
 
-    TIntrusivePtr<INodeReaderImpl> CreateNodeReader(const TRichYPath&, const TTableReaderOptions&);
+    TIntrusivePtr<INodeReaderImpl> CreateNodeReader(const TRichYPath&, const TTableReaderOptions&) override;
 
-    TIntrusivePtr<IYaMRReaderImpl> CreateYaMRReader(const TRichYPath&, const TTableReaderOptions&);
+    TIntrusivePtr<IYaMRReaderImpl> CreateYaMRReader(const TRichYPath&, const TTableReaderOptions&) override;
 
-    TIntrusivePtr<IProtoReaderImpl> CreateProtoReader(const TRichYPath&, const TTableReaderOptions&, const ::google::protobuf::Message*);
+    TIntrusivePtr<IProtoReaderImpl> CreateProtoReader(const TRichYPath&, const TTableReaderOptions&, const ::google::protobuf::Message*) override;
 
-    TIntrusivePtr<INodeWriterImpl> CreateNodeWriter(const TRichYPath&, const TTableWriterOptions&);
+    TIntrusivePtr<INodeWriterImpl> CreateNodeWriter(const TRichYPath&, const TTableWriterOptions&) override;
 
-    TIntrusivePtr<IYaMRWriterImpl> CreateYaMRWriter(const TRichYPath&, const TTableWriterOptions&);
+    TIntrusivePtr<IYaMRWriterImpl> CreateYaMRWriter(const TRichYPath&, const TTableWriterOptions&) override;
 
-    TIntrusivePtr<IProtoWriterImpl> CreateProtoWriter(const TRichYPath&, const TTableWriterOptions&, const ::google::protobuf::Message*);
+    TIntrusivePtr<IProtoWriterImpl> CreateProtoWriter(const TRichYPath&, const TTableWriterOptions&, const ::google::protobuf::Message*) override;
 
-    TOperationId Sort(const TSortOperationSpec&, const TOperationOptions&);
+    TOperationId Sort(const TSortOperationSpec&, const TOperationOptions&) override;
 
-    TOperationId Merge(const TMergeOperationSpec&, const TOperationOptions&);
+    TOperationId Merge(const TMergeOperationSpec&, const TOperationOptions&) override;
 
-    TOperationId Erase(const TEraseOperationSpec&, const TOperationOptions&);
+    TOperationId Erase(const TEraseOperationSpec&, const TOperationOptions&) override;
 
-    void AbortOperation(const TOperationId&);
+    void AbortOperation(const TOperationId&) override;
 
-    void WaitForOperation(const TOperationId&);
+    void WaitForOperation(const TOperationId&) override;
 
-    EOperationStatus CheckOperation(const TOperationId&);
+    EOperationStatus CheckOperation(const TOperationId&) override;
 
-    TOperationId DoMap(const TMapOperationSpec&, IJob*, const TOperationOptions&);
+    TOperationId DoMap(const TMapOperationSpec&, IJob*, const TOperationOptions&) override;
 
-    TOperationId DoReduce(const TReduceOperationSpec&, IJob*, const TOperationOptions&);
+    TOperationId DoReduce(const TReduceOperationSpec&, IJob*, const TOperationOptions&) override;
 
-    TOperationId DoJoinReduce(const TJoinReduceOperationSpec&, IJob*, const TOperationOptions&);
+    TOperationId DoJoinReduce(const TJoinReduceOperationSpec&, IJob*, const TOperationOptions&) override;
 
-    TOperationId DoMapReduce(const TMapReduceOperationSpec&, IJob*, IJob*, IJob*, const TMultiFormatDesc&, const TMultiFormatDesc&, const TMultiFormatDesc&, const TMultiFormatDesc&, const TOperationOptions&);
+    TOperationId DoMapReduce(const TMapReduceOperationSpec&, IJob*, IJob*, IJob*, const TMultiFormatDesc&, const TMultiFormatDesc&, const TMultiFormatDesc&, const TMultiFormatDesc&, const TOperationOptions&) override;
 
-    ITransactionPtr StartTransaction(const TStartTransactionOptions&);
+    ITransactionPtr StartTransaction(const TStartTransactionOptions&) override;
 
-    ITransactionPtr AttachTransaction(const TTransactionId&);
+    ITransactionPtr AttachTransaction(const TTransactionId&) override;
 
-    void AlterTable(const TYPath&, const TAlterTableOptions&);
+    void AlterTable(const TYPath&, const TAlterTableOptions&) override;
 
-    void MountTable(const TYPath&, const TMountTableOptions&);
+    void MountTable(const TYPath&, const TMountTableOptions&) override;
 
-    void UnmountTable(const TYPath&, const TUnmountTableOptions&);
+    void UnmountTable(const TYPath&, const TUnmountTableOptions&) override;
 
-    void RemountTable(const TYPath&, const TRemountTableOptions&);
+    void RemountTable(const TYPath&, const TRemountTableOptions&) override;
 
-    void ReshardTable(const TYPath&, const yvector<TKey>&, const TReshardTableOptions&);
+    void ReshardTable(const TYPath&, const yvector<TKey>&, const TReshardTableOptions&) override;
 
-    void ReshardTable(const TYPath&, i32, const TReshardTableOptions&);
+    void ReshardTable(const TYPath&, i32, const TReshardTableOptions&) override;
 
-    void InsertRows(const TYPath&, const TNode::TList&);
+    void InsertRows(const TYPath&, const TNode::TList&) override;
 
-    void DeleteRows(const TYPath&, const TNode::TList&);
+    void DeleteRows(const TYPath&, const TNode::TList&) override;
 
-    TNode::TList LookupRows(const TYPath&, const TNode::TList&, const TLookupRowsOptions&);
+    TNode::TList LookupRows(const TYPath&, const TNode::TList&, const TLookupRowsOptions&) override;
 
-    TNode::TList SelectRows(const Stroka&, const TSelectRowsOptions&);
+    TNode::TList SelectRows(const Stroka&, const TSelectRowsOptions&) override;
 };
 
 
