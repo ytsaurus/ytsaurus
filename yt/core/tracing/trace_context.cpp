204046880f581a1097212e999577d529871abec6
Maxim Babenko <babenko@ytsaurus.tech>
2016-09-15 19:16:30 +0300

    Revert "Micro-optimize tracing"
    This reverts commit e518d9fea4dcf8dd62668a5eea53cd21b33418f4.

--- yt/core/tracing/trace_context.cpp
+++ yt/core/tracing/trace_context.cpp
@@ -13,8 +13,6 @@ namespace NTracing {
 
 static const auto& Logger = TracingLogger;
 
-const TTraceContext NullTraceContext;
-
 ////////////////////////////////////////////////////////////////////////////////
 
 static ui64 GenerateId()
@@ -37,6 +35,26 @@ static TSpanId GenerateSpanId()
 
 ////////////////////////////////////////////////////////////////////////////////
 
+TTraceContext::TTraceContext()
+    : TraceId_(InvalidTraceId)
+    , SpanId_(InvalidSpanId)
+    , ParentSpanId_(InvalidSpanId)
+{ }
+
+TTraceContext::TTraceContext(
+    TTraceId traceId,
+    TSpanId spanId,
+    TSpanId parentSpanId)
+    : TraceId_(traceId)
+    , SpanId_(spanId)
+    , ParentSpanId_(parentSpanId)
+{ }
+
+bool TTraceContext::IsEnabled() const
+{
+    return TraceId_ != InvalidTraceId;
+}
+
 TTraceContext TTraceContext::CreateChild() const
 {
     return TTraceContext(TraceId_, GenerateSpanId(), SpanId_);
@@ -50,14 +68,95 @@ Stroka ToString(const TTraceContext& context)
         context.GetParentSpanId());
 }
 
+const TTraceContext NullTraceContext;
+
 ////////////////////////////////////////////////////////////////////////////////
 
-static NConcurrency::TFls<std::vector<TTraceContext>> TraceContextStack;
+TTraceContextGuard::TTraceContextGuard(const TTraceContext& context)
+    : Context_(context)
+    , Active_(context.IsEnabled())
+{
+    if (Active_) {
+        PushContext(context);
+    }
+}
+
+TTraceContextGuard::TTraceContextGuard(TTraceContextGuard&& other)
+    : Context_(other.Context_)
+    , Active_(other.Active_)
+{
+    other.Active_ = false;
+}
+
+TTraceContextGuard::~TTraceContextGuard()
+{
+    Release();
+}
+
+const TTraceContext& TTraceContextGuard::GetContext() const
+{
+    return Context_;
+}
+
+bool TTraceContextGuard::IsActive() const
+{
+    return Active_;
+}
+
+void TTraceContextGuard::Release()
+{
+    if (Active_) {
+        PopContext();
+        Active_ = false;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TNullTraceContextGuard::TNullTraceContextGuard()
+    : Active_(true)
+{
+    PushContext(NullTraceContext);
+}
+
+TNullTraceContextGuard::TNullTraceContextGuard(TNullTraceContextGuard&& other)
+    : Active_(other.Active_)
+{
+    other.Active_ = false;
+}
+
+TNullTraceContextGuard::~TNullTraceContextGuard()
+{
+    Release();
+}
+
+bool TNullTraceContextGuard::IsActive() const
+{
+    return Active_;
+}
+
+void TNullTraceContextGuard::Release()
+{
+    if (Active_) {
+        PopContext();
+        Active_ = false;
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+typedef NConcurrency::TFls<std::vector<TTraceContext>> TTraceContextStack;
+
+static TTraceContextStack& TraceContextStack()
+{
+    static TTraceContextStack stack;
+    return stack;
+}
 
 const TTraceContext& GetCurrentTraceContext()
 {
-    auto& stack = *TraceContextStack;
-    return stack.empty() ? NullTraceContext : stack.back();
+    auto& stack = TraceContextStack();
+    return stack->empty() ? NullTraceContext : stack->back();
 }
 
 bool IsTracingEnabled()
@@ -68,15 +167,15 @@ bool IsTracingEnabled()
 void PushContext(const TTraceContext& context)
 {
     LOG_TRACE("Push context %v", context);
-    TraceContextStack->push_back(context);
+    TraceContextStack()->push_back(context);
 }
 
 void PopContext()
 {
-    auto& stack = *TraceContextStack;
-    YCHECK(!stack.empty());
-    LOG_TRACE("Pop context %v", stack.back());
-    stack.pop_back();
+    auto& stack = TraceContextStack();
+    YCHECK(!stack->empty());
+    LOG_TRACE("Pop context %v", stack->back());
+    stack->pop_back();
 }
 
 TTraceContext CreateChildTraceContext()
