a6df87b7b0e523e649341c867d6886e34c4bdace
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2016-09-15 17:25:58 +0300

    YT-5524: Support three-value logic

--- yt/unittests/ql_helpers.h
+++ yt/unittests/ql_helpers.h
@@ -75,6 +75,11 @@ static TValue MakeString(const TStringBuf& value)
     return MakeUnversionedStringValue(value);
 }
 
+static TValue MakeNull()
+{
+    return MakeUnversionedSentinelValue(EValueType::Null);
+}
+
 template <class TTypedExpression, class... TArgs>
 static TConstExpressionPtr Make(TArgs&&... args)
 {
@@ -82,6 +87,8 @@ static TConstExpressionPtr Make(TArgs&&... args)
         EValueType::TheBottom,
         std::forward<TArgs>(args)...);
 }
+
+////////////////////////////////////////////////////////////////////////////////
 
 class TPrepareCallbacksMock
     : public IPrepareCallbacks
