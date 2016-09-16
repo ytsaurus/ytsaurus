42aec442b93ef4239c6bc78087f9fdeecc3de8c2
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-09-16 21:37:35 +0300

    cPickle -> six.moves.cPickle

--- python-git/yt/wrapper/pickling.py
+++ python-git/yt/wrapper/pickling.py
@@ -5,25 +5,22 @@ except ImportError:
 
 import yt
 
-frameworks = {
+FRAMEWORKS = {
     "dill": "yt.packages.dill",
     "cloudpickle": "yt.packages.cloudpickle",
-    "pickle": "cPickle",
+    "pickle": "yt.packages.six.moves.cPickle"
 }
 
-
 def import_framework_module(framework):
-    if framework not in frameworks:
+    if framework not in FRAMEWORKS:
         raise yt.YtError("Cannot find pickling framework {0}. Available frameworks: {1}."
-                         .format(framework, list(frameworks)))
-    return import_module(frameworks[framework])
-
+                         .format(framework, list(FRAMEWORKS)))
+    return import_module(FRAMEWORKS[framework])
 
 class Pickler(object):
     def __init__(self, framework):
         framework_module = import_framework_module(framework)
         self.dump, self.dumps = framework_module.dump, framework_module.dumps
-
 
 class Unpickler(object):
     def __init__(self, framework):
