09ad6686575c0fa61d8f5cb2e4c9e24f43afc3a9
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-09-22 16:25:38 +0300

    Do not merge tables in //sys

--- scripts/cron/run_merge.sh
+++ scripts/cron/run_merge.sh
@@ -2,7 +2,7 @@
 
 QUEUE="//sys/cron/tables_to_merge"
 
-/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimum-number-of-chunks 100 --filter-out //statbox --filter-out //home/qe
+/opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimum-number-of-chunks 100 --filter-out //statbox --filter-out //home/qe --filter-out //sys
 
 /opt/cron/tools/find_tables_to_merge.py --queue-path $QUEUE --minimum-number-of-chunks 100 --root //statbox --account statbox-logs --append
 
