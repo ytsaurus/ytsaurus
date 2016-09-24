be3d29da9230cf8203acd67b0dc835164d32e275
Pavel Sushin <psushin@ytsaurus.tech>
2016-09-24 22:47:11 +0300

    YT-5696: start blinker at a proper time

--- yt/server/job_proxy/user_job.cpp
+++ yt/server/job_proxy/user_job.cpp
@@ -187,7 +187,6 @@ public:
             LOG_INFO("Job process started");
 
             MemoryWatchdogExecutor_->Start();
-            InputPipeBlinker_->Start();
             if (BlockIOWatchdogExecutor_) {
                 BlockIOWatchdogExecutor_->Start();
             }
@@ -709,6 +708,10 @@ private:
 
                 // Notify node process that user job is fully prepared and running.
                 Host_->OnPrepared();
+
+                // Now writing pipe is definitely ready, so we can start blinking.
+                InputPipeBlinker_->Start();
+
                 JobStarted_ = true;
             } catch (const std::exception& ex) {
                 auto error = TError("Start action failed") << ex;
@@ -1219,7 +1222,6 @@ private:
     // NB(psushin): Read st before asking questions: st/YT-5629.
     void BlinkInputPipe() const
     {
-        /*
         // This method is called after preparation and before finalization.
         // Reader must be opened and ready, so open must succeed.
         // Still an error can occur in case of external forced sandbox clearance (e.g. in integration tests).
@@ -1229,7 +1231,6 @@ private:
         } else {
             LOG_WARNING(TError::FromSystem(), "Failed to blink input pipe");
         }
-        */
     }
 };
 
