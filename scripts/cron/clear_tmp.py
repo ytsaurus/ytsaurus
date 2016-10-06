54e6f6f9f8c58376ec7e83b7f5d0c02d16c1cf69
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-10-06 12:17:22 +0300

    Fix clear tmp to correctly remove links of file cache

--- scripts/cron/clear_tmp.py
+++ scripts/cron/clear_tmp.py
@@ -85,8 +85,11 @@ def main():
         # filter object by age, total size and count
         if (age > max_age or disk_space > args.max_disk_space or node_count > args.max_node_count or chunk_count > args.max_chunk_count) and age > safe_age:
             if "hash" in obj.attributes:
-                hash = obj.attributes["hash"]
-                link = os.path.join(os.path.dirname(obj), "hash", hash[-2:], obj.attributes["hash"])
+                hash_str = obj.attributes["hash"]
+                link = os.path.join(os.path.dirname(obj), "hash", hash_str[-2:], hash_str)
+                if link in links:
+                    add_to_remove(link)
+                link = os.path.join(os.path.dirname(obj), "hash", hash_str)
                 if link in links:
                     add_to_remove(link)
             add_to_remove(obj)
