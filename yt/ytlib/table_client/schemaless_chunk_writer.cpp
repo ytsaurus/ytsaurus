4e420c603ceb2fd766d9ce56dc33593d1ddf863c
Pavel Sushin <psushin@ytsaurus.tech>
2016-10-07 19:18:30 +0300

    YT-5769: fix chunk_row_count in block meta for partition chunks

--- yt/ytlib/table_client/schemaless_chunk_writer.cpp
+++ yt/ytlib/table_client/schemaless_chunk_writer.cpp
@@ -209,7 +209,6 @@ protected:
                 lastRow.Begin() + Schema_.GetKeyColumnCount());
         }
 
-        block.Meta.set_chunk_row_count(RowCount_);
         block.Meta.set_block_index(BlockMetaExt_.blocks_size());
 
         BlockMetaExtSize_ += block.Meta.ByteSize();
@@ -386,6 +385,7 @@ public:
 
             if (BlockWriter_->GetBlockSize() >= Config_->BlockSize) {
                 auto block = BlockWriter_->FlushBlock();
+                block.Meta.set_chunk_row_count(RowCount_);
                 RegisterBlock(block, row);
                 BlockWriter_.reset(new THorizontalSchemalessBlockWriter);
             }
@@ -412,6 +412,7 @@ private:
     {
         if (BlockWriter_->GetRowCount() > 0) {
             auto block = BlockWriter_->FlushBlock();
+            block.Meta.set_chunk_row_count(RowCount_);
             RegisterBlock(block, LastKey_.Get());
         }
 
@@ -569,6 +570,7 @@ private:
     void FinishBlock(int blockWriterIndex, TUnversionedRow lastRow)
     {
         auto block = BlockWriters_[blockWriterIndex]->DumpBlock(BlockMetaExt_.blocks_size(), RowCount_);
+        block.Meta.set_chunk_row_count(RowCount_);
         RegisterBlock(block, lastRow);
     }
 
