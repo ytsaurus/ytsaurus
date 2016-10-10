d3b9a511380b64e77763206c5da4513c1d47a8c8
Andrey Kashin <acid@yandex-team.ru>
2016-10-10 17:10:17 +0300

    YT-4538 - Implement greedy approximation for MaxPossibleUsageRatio
    - Move dominant resource ratio computation to separate function
    - Add test for MaxPossibleResourceUsage computation

--- yt/server/scheduler/fair_share_tree.h
+++ yt/server/scheduler/fair_share_tree.h
@@ -81,6 +81,8 @@ struct ISchedulerElement
     virtual void UpdateBottomUp(TDynamicAttributesList& dynamicAttributesList) = 0;
     virtual void UpdateTopDown(TDynamicAttributesList& dynamicAttributesList) = 0;
 
+    virtual TJobResources ComputePossibleResourceUsage(TJobResources limit) const = 0;
+
     virtual void UpdateDynamicAttributes(TDynamicAttributesList& dynamicAttributesList) = 0;
 
     virtual void PrescheduleJob(TFairShareContext& context, bool starvingOnly, bool aggressiveStarvationEnabled) = 0;
@@ -321,6 +323,8 @@ public:
 
     virtual void UpdateBottomUp(TDynamicAttributesList& dynamicAttributesList) override;
     virtual void UpdateTopDown(TDynamicAttributesList& dynamicAttributesList) override;
+
+    virtual TJobResources ComputePossibleResourceUsage(TJobResources limit) const override;
 
     virtual double GetFairShareStarvationToleranceLimit() const;
     virtual TDuration GetMinSharePreemptionTimeoutLimit() const;
@@ -684,6 +688,8 @@ public:
 
     virtual void UpdateBottomUp(TDynamicAttributesList& dynamicAttributesList) override;
     virtual void UpdateTopDown(TDynamicAttributesList& dynamicAttributesList) override;
+
+    virtual TJobResources ComputePossibleResourceUsage(TJobResources limit) const override;
 
     virtual void UpdateDynamicAttributes(TDynamicAttributesList& dynamicAttributesList) override;
 
