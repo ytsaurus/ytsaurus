33ec6b264b2196ee649fb89365cec70201cbf427
Maxim Babenko <babenko@ytsaurus.tech>
2016-10-12 20:33:21 +0300

    YT-5820: Abort postponed mutations with Unavailable error when leader stops

--- yt/server/hydra/distributed_hydra_manager.cpp
+++ yt/server/hydra/distributed_hydra_manager.cpp
@@ -1221,6 +1221,9 @@ private:
 
         LOG_INFO("Stopped leading");
 
+        // Save for later to respect the thread affinity.
+        auto leaderCommitter = ControlEpochContext_->LeaderCommitter;
+
         StopEpoch();
 
         YCHECK(ControlState_ == EPeerState::Leading || ControlState_ == EPeerState::LeaderRecovery);
@@ -1230,6 +1233,7 @@ private:
         VERIFY_THREAD_AFFINITY(AutomatonThread);
 
         AutomatonEpochContext_.Reset();
+        leaderCommitter->Stop();
         DecoratedAutomaton_->OnStopLeading();
         StopLeading_.Fire();
 
