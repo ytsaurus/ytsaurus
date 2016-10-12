0e4e5ebb1178ea5a79d3c0e133542c42e7b30dbb
Maxim Babenko <babenko@ytsaurus.tech>
2016-10-12 21:34:10 +0300

    YT-5819: Don't release the spinlock too early

--- yt/server/hydra/mutation_committer.cpp
+++ yt/server/hydra/mutation_committer.cpp
@@ -361,7 +361,7 @@ void TLeaderCommitter::Flush()
 
     auto guard = Guard(BatchSpinLock_);
     if (CurrentBatch_) {
-        FlushCurrentBatch(&guard);
+        FlushCurrentBatch();
     }
 }
 
@@ -433,11 +433,11 @@ void TLeaderCommitter::AddToBatch(
         recordData,
         std::move(localFlushResult));
     if (batch->GetMutationCount() >= Config_->MaxCommitBatchRecordCount) {
-        FlushCurrentBatch(&guard);
+        FlushCurrentBatch();
     }
 }
 
-void TLeaderCommitter::FlushCurrentBatch(TGuard<TSpinLock>* guard)
+void TLeaderCommitter::FlushCurrentBatch()
 {
     VERIFY_SPINLOCK_AFFINITY(BatchSpinLock_);
 
@@ -445,8 +445,6 @@ void TLeaderCommitter::FlushCurrentBatch(TGuard<TSpinLock>* guard)
     std::swap(currentBatch, CurrentBatch_);
     PrevBatchQuorumFlushResult_ = currentBatch->GetQuorumFlushResult();
     TDelayedExecutor::CancelAndClear(BatchTimeoutCookie_);
-
-    guard->Release();
 
     currentBatch->Flush();
 
@@ -480,7 +478,7 @@ void TLeaderCommitter::OnBatchTimeout(const TBatchPtr& batch)
 
     auto guard = Guard(BatchSpinLock_);
     if (batch == CurrentBatch_) {
-        FlushCurrentBatch(&guard);
+        FlushCurrentBatch();
     }
 }
 
