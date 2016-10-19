8db1b85ba7e88fce0d0174a2e058d49aed017e09
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-10-19 19:19:22 +0300

    Fixed process params

--- python-git/yt/wrapper/tests/test_driver.py
+++ python-git/yt/wrapper/tests/test_driver.py
@@ -1,15 +1,21 @@
-import yt.wrapper.driver
-import yt.wrapper.config
+from .helpers import TEST_DIR
+
+from yt.wrapper.common import parse_bool
+
+import yt.yson as yson
 
 from yt.packages.six.moves import reload_module
 
+import yt.wrapper as yt
+
 import time
+import pytest
 
 def test_heavy_proxies():
     from yt.wrapper.http_driver import HeavyProxyProvider
     from socket import error as SocketError
 
-    yt.wrapper.config["proxy"]["number_of_top_proxies_for_random_choice"] = 1
+    yt.config["proxy"]["number_of_top_proxies_for_random_choice"] = 1
 
     provider = HeavyProxyProvider(None)
     provider._get_light_proxy = lambda: "light_proxy"
@@ -23,7 +29,7 @@ def test_heavy_proxies():
     assert provider() == "host1"
 
     provider._discover_heavy_proxies = lambda: ["host2", "host3"]
-    yt.wrapper.config.HOST_BAN_PERIOD = 10
+    yt.config.HOST_BAN_PERIOD = 10
     time.sleep(0.01)
 
     assert provider() == "host2"
@@ -32,5 +38,16 @@ def test_heavy_proxies():
     assert provider() == "light_proxy"
 
 def teardown_function(function):
+    import yt.wrapper.http_driver
+    import yt.wrapper.config
     reload_module(yt.wrapper.http_driver)
     reload_module(yt.wrapper.config)
+
+@pytest.mark.usefixtures("yt_env")
+def test_process_params():
+    schema = yson.YsonList([{"name": "k", "type": "int64", "sort_order": "ascending"}])
+    schema.attributes["unique_keys"] = True
+
+    table = TEST_DIR + "/dynamic_table"
+    yt.create_table(table, attributes={"schema": schema})
+    assert parse_bool(yt.get(table + "/@schema/@unique_keys"))
