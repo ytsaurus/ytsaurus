c3f3981e8acf37a6f447c479883dabb724659017
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-10-21 11:44:45 +0300

    Fixed tests

--- python-git/yt/wrapper/tests/test_driver.py
+++ python-git/yt/wrapper/tests/test_driver.py
@@ -1,6 +1,6 @@
 from .helpers import TEST_DIR
 
-from yt.wrapper.common import parse_bool
+from yt.wrapper.common import parse_bool, update
 
 import yt.yson as yson
 
@@ -10,38 +10,39 @@ import yt.wrapper as yt
 
 import time
 import pytest
+from copy import deepcopy
 
 def test_heavy_proxies():
     from yt.wrapper.http_driver import HeavyProxyProvider
     from socket import error as SocketError
 
-    yt.config["proxy"]["number_of_top_proxies_for_random_choice"] = 1
+    config = deepcopy(yt.config.config)
+    try:
+        yt.config["proxy"]["number_of_top_proxies_for_random_choice"] = 1
 
-    provider = HeavyProxyProvider(None)
-    provider._get_light_proxy = lambda: "light_proxy"
-    provider._discover_heavy_proxies = lambda: ["host1", "host2"]
-    assert provider() == "host1"
+        provider = HeavyProxyProvider(None)
+        provider._get_light_proxy = lambda: "light_proxy"
+        provider._discover_heavy_proxies = lambda: ["host1", "host2"]
+        assert provider() == "host1"
 
-    provider.on_error_occured(SocketError())
-    assert provider() == "host2"
+        provider.on_error_occured(SocketError())
+        assert provider() == "host2"
 
-    provider.on_error_occured(SocketError())
-    assert provider() == "host1"
+        provider.on_error_occured(SocketError())
+        assert provider() == "host1"
 
-    provider._discover_heavy_proxies = lambda: ["host2", "host3"]
-    yt.wrapper.config["proxy"]["proxy_ban_timeout"] = 10
-    time.sleep(0.01)
+        provider._discover_heavy_proxies = lambda: ["host2", "host3"]
+        yt.config["proxy"]["proxy_ban_timeout"] = 10
+        time.sleep(0.01)
 
-    assert provider() == "host2"
+        assert provider() == "host2"
 
-    provider._discover_heavy_proxies = lambda: []
-    assert provider() == "light_proxy"
-
-def teardown_function(function):
-    import yt.wrapper.http_driver
-    import yt.wrapper.config
-    reload_module(yt.wrapper.http_driver)
-    reload_module(yt.wrapper.config)
+        provider._discover_heavy_proxies = lambda: []
+        assert provider() == "light_proxy"
+    finally:
+        reload_module(yt.http_driver)
+        reload_module(yt.config)
+        update(yt.config.config, config)
 
 @pytest.mark.usefixtures("yt_env")
 def test_process_params():
