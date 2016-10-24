953fbf053a255b71988edec8a5e8cca2815c07f5
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2016-10-24 14:40:09 +0300

    YT-5894 - Better error codes for tablets

--- yt/ytlib/api/client.cpp
+++ yt/ytlib/api/client.cpp
@@ -722,11 +722,8 @@ private:
         yhash_map<NTabletClient::TTabletCellId, TCellDescriptor> tabletCellReplicas;
 
         auto getAddress = [&] (const TTabletInfoPtr& tabletInfo) mutable {
-            if (tabletInfo->State != ETabletState::Mounted) {
-                // TODO(babenko): learn to work with unmounted tablets
-                THROW_ERROR_EXCEPTION("Tablet %v is not mounted",
-                    tabletInfo->TabletId);
-            }
+            // TODO(babenko): learn to work with unmounted tablets
+            ValidateTabletMounted(tableInfo, tabletInfo);
 
             auto insertResult = tabletCellReplicas.insert(std::make_pair(tabletInfo->CellId, TCellDescriptor()));
             auto& descriptor = insertResult.first->second;
