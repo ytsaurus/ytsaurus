6b73aaaf3f429be7747ada50a9dd79dc41967457
Max Akhmedov <max42@ytsaurus.tech>
2016-10-28 16:44:53 +0300

    Make some renames and fixes around TBlobTableWriter and TJobResult proto.

--- yt/server/misc/job_table_schema.cpp
+++ yt/server/misc/job_table_schema.cpp
@@ -1,4 +1,6 @@
-#include "stderr_table_schema.h"
+#include "job_table_schema.h"
+
+#include <yt/ytlib/table_client/row_base.h>
 
 namespace NYT {
 
@@ -9,7 +11,15 @@ using namespace NTableClient;
 NTableClient::TBlobTableSchema GetStderrBlobTableSchema()
 {
     TBlobTableSchema result;
+    result.BlobIdColumns.emplace_back("job_id", EValueType::String);
+    return result;
+}
+
+NTableClient::TBlobTableSchema GetCoreBlobTableSchema()
+{
+    TBlobTableSchema result;
     result.BlobIdColumns.emplace_back("job_id");
+    result.BlobIdColumns.emplace_back("core_id");
     return result;
 }
 
