f9ce992105bdf2558bcd1e947de668fc18884709
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2016-10-31 16:06:57 +0300

    New yandex-yt-python packaging

--- python-git/helpers.py
+++ python-git/helpers.py
@@ -1,18 +1,27 @@
 import os
-import subprocess32 as subprocess
+import sys
+
+try:
+    import subprocess32 as subprocess
+except ImportError:
+    import subprocess
 
 def get_version():
     if os.path.exists("VERSION"):
         return open("VERSION").read().strip()
-    return subprocess.check_output("dpkg-parsechangelog | grep Version | awk '{print $2}'", shell=True).strip()
+    version = subprocess.check_output("dpkg-parsechangelog | grep Version | awk '{print $2}'", shell=True)
+    if sys.version_info[0] >= 3:
+        version = version.decode("ascii")
+    return version.strip()
 
 def prepare_files(files):
     scripts = []
     data_files = []
     for file in files:
-        # in egg and debian cases strategy of binary distribution is different
+        file_name_suffix = str(sys.version_info[0])
+        # In egg and debian cases strategy of binary distribution is different
         if "DEB" in os.environ:
-            data_files.append(("/usr/bin", [file]))
+            data_files.append(("/usr/bin", [file + file_name_suffix]))
         else:
-            scripts.append(file)
+            scripts.append(file + file_name_suffix)
     return scripts, data_files
--- python-git/yandex-yt-python/setup.py
+++ python-git/yandex-yt-python/setup.py
@@ -29,13 +29,6 @@ def recursive(path):
     prefix = path.strip("/").replace("/", ".")
     return list(imap(lambda package: prefix + "." + package, find_packages(path))) + [prefix]
 
-def build_documentation_files(source, target):
-    result = []
-    for root, dirs, files in os.walk(source, topdown=False):
-        dest = os.path.join(target, root[len(source):])
-        result.append((dest, [os.path.join(root, file) for file in files]))
-    return result
-
 def main():
     requires = []
     if sys.version_info[:2] <= (2, 6):
@@ -43,17 +36,19 @@ def main():
 
     scripts, data_files = prepare_files([
         "yt/wrapper/bin/mapreduce-yt",
-        "yt/wrapper/bin/yt2",
+        "yt/wrapper/bin/yt",
         "yt/wrapper/bin/yt-fuse",
         "yt/wrapper/bin/yt-admin",
         "yt/wrapper/bin/yt-job-tool"])
+
     if "DEB" in os.environ:
-        if not os.path.exists("docs"):
-            subprocess.check_call("PYTHONPATH=$(pwd) sphinx-apidoc -F -o docs yt", shell=True)
-            subprocess.check_call("PYTHONPATH=$(pwd) sphinx-build -b html docs docs/_build/", shell=True)
-        data_files += build_documentation_files("docs/_build/", "/usr/share/doc/yandex-yt-python-docs")
-        data_files += [("/etc/bash_completion.d/", ["yandex-yt-python/yt_completion"])]
+        if sys.version_info[0] < 3:
+            data_files += [("/etc/bash_completion.d/", ["yandex-yt-python/yt_completion"])]
     else:
+        # NOTE: Binaries are put into EGG-INFO/scripts directory
+        # inside egg file and executed with special script wrappers.
+        # See https://setuptools.readthedocs.io/en/latest/formats.html#script-wrappers
+
         # We cannot create link in the egg, so we must put yt binary.
         scripts.append("yt/wrapper/bin/yt")
 
