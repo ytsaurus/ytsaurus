15ee93c1bb6c3904efaa225bd7b0783a6c83de8d
Amir Shakurov <shakurov@ytsaurus.tech>
2016-11-03 14:58:13 +0300

    Bump snapshot version.

--- yt/server/node_tracker_server/rack.cpp
+++ yt/server/node_tracker_server/rack.cpp
@@ -31,7 +31,7 @@ void TRack::Load(NCellMaster::TLoadContext& context)
     Load(context, Name_);
     Load(context, Index_);
     // COMPAT(shakurov)
-    if (context.GetVersion() >= 4242) {
+    if (context.GetVersion() >= 504) {
         Load(context, DataCenter_);
     }
 }
