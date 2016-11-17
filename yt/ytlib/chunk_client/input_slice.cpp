b420579089d41a70c9070258ddd11d480b5f2482
Maxim Babenko <babenko@ytsaurus.tech>
2016-11-18 00:37:05 +0300

    Fix typo leading to wrong slice data size

--- yt/ytlib/chunk_client/input_slice.cpp
+++ yt/ytlib/chunk_client/input_slice.cpp
@@ -221,7 +221,7 @@ TInputSlice::TInputSlice(
         SetRowCount(protoChunkSlice.row_count_override());
     }
     if (protoChunkSlice.has_uncompressed_data_size_override()) {
-        SetRowCount(protoChunkSlice.uncompressed_data_size_override());
+        SetDataSize(protoChunkSlice.uncompressed_data_size_override());
     }
 }
 
