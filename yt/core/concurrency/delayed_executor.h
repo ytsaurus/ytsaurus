0ebe2ba19e64b90365ba4b76978c8adfe8b36181
Maxim Babenko <babenko@ytsaurus.tech>
2016-11-22 13:39:03 +0300

    Improve TDelayedExecutor shutdown

--- yt/core/concurrency/delayed_executor.h
+++ yt/core/concurrency/delayed_executor.h
@@ -13,67 +13,85 @@ namespace NConcurrency {
 class TDelayedExecutor
 {
 public:
-    ~TDelayedExecutor();
-
     typedef TCallback<void(bool)> TDelayedCallback;
 
     //! Constructs a future that gets set when a given #delay elapses.
     /*!
-     * Note that during shutdown this future will be resolved prematurely with an error.
+     *  Note that during shutdown this future will be resolved prematurely with an error.
      */
     static TFuture<void> MakeDelayed(TDuration delay);
 
     //! Submits #callback for execution after a given #delay.
     /*!
-     * Passed callback is guaranteed to be invoked unless the cookie was cancelled.
-     * Flag is provided to the callback to indicate whenther this execution was aborted or not.
-     * \param callback A callback to execute.
-     * \param delay Execution delay.
-     * \return Opaque cancelation cookie.
+     *  #callback is guaranteed to be invoked exactly once unless the cookie was cancelled (cf. #Cancel).
+     *  The exact thread where the invocation takes place is unspecified.
+     *
+     *  |aborted| flag is provided to the callback to indicate whether this is a premature execution
+     *  due to shutdown or not.
+     *
+     *  Note that after shutdown has been initiated, #Submit may start invoking the newly-passed callbacks
+     *  immediately with |aborted = true|. It is guaranteed, though, that each #Submit call may only
+     *  cause an immediate execution of *its* callback but not others.
+     *
+     *  \param callback A callback to execute.
+     *  \param delay Execution delay.
+     *  \return An opaque cancelation cookie.
      */
     static TDelayedExecutorCookie Submit(TDelayedCallback callback, TDuration delay);
 
     //! Submits #closure for execution after a given #delay.
     /*!
-     * Passed closure is not guaranteed to be executed (for example, in case of program termination).
-     * \param closure A closure to execute.
-     * \param delay Execution delay.
-     * \return Opaque cancelation cookie.
+     *  \param closure A closure to execute.
+     *  \param delay Execution delay.
+     *  \return An opaque cancelation cookie.
      */
     static TDelayedExecutorCookie Submit(TClosure closure, TDuration delay);
 
     //! Submits #callback for execution at a given #deadline.
     /*!
-     * Flag passed to the callback indicates whether this execution was aborted or not.
      * \param callback A callback to execute.
      * \param deadline Execution deadline.
-     * \return Opaque cancelation cookie.
+     * \return An opaque cancelation cookie.
      */
     static TDelayedExecutorCookie Submit(TDelayedCallback callback, TInstant deadline);
 
     //! Submits #closure for execution at a given #deadline.
     /*!
-     * Passed closure is not guaranteed to be executed (for example, in case of program termination).
      * \param closure A closure to execute.
      * \param delay Execution deadline.
-     * \return Opaque cancelation cookie.
+     * \return An opaque cancelation cookie.
      */
     static TDelayedExecutorCookie Submit(TClosure closure, TInstant deadline);
 
     //! Cancels an earlier scheduled execution and clears the cookie.
+    /*!
+     *  This call is "safe", i.e. cannot lead to immediate execution of any callbacks even
+     *  during shutdown.
+     *
+     *  Cancelation should always be regarded as a hint. It is inherently racy and
+     *  is not guaranteed to be handled during and after shutdown.
+     */
     static void CancelAndClear(TDelayedExecutorCookie& cookie);
 
-    //! Terminates the scheduler thread.
+    //! Shuts the subsystem down.
     /*!
-     *  All subsequent #Submit calls are silently ignored.
+     *  Safe to call multiple times.
+     *
+     *  This call may block to wait for all registered callbacks to complete.
+     *  Some of the above callbacks (i.e. those for which the deadlines were not reached yet)
+     *  will get |aborted = true| in their parameters. This is somewhat racy, though, and should not
+     *  be relied upon.
+     *
+     *  For all calls to #Submit after the shutdown is finished, the callbacks are executed immediately
+     *  with |aborted = true|.
      */
     static void StaticShutdown();
 
 private:
-    class TImpl;
-
     TDelayedExecutor();
+    ~TDelayedExecutor();
 
+    class TImpl;
     static TImpl* GetImpl();
 
     Y_DECLARE_SINGLETON_FRIEND();
