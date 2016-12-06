deff37af4b9f028b3a87d2d2cb7df3edf101be8c
Maxim Babenko <babenko@ytsaurus.tech>
2016-12-06 13:25:42 +0300

    Forbid configuring cache media (reverted from commit 3540541982b02f81c47a2173e119b03944d7daae)

--- yt/server/chunk_server/chunk_properties.cpp
+++ yt/server/chunk_server/chunk_properties.cpp
@@ -208,27 +208,18 @@ void ValidateChunkProperties(
     if (!properties.IsValid()) {
         THROW_ERROR_EXCEPTION(
             "At least one medium should store replicas (including parity parts); "
-            "configuring otherwise would result in a data loss");
+                "configuring otherwise would result in a data loss");
     }
 
-    for (int index = 0; index < MaxMediumCount; ++index) {
-        const auto& mediumProperties = properties[index];
-        const auto* medium = chunkManager->GetMediumByIndex(index);
-        if (mediumProperties && medium->GetCache()) {
-            THROW_ERROR_EXCEPTION("Cache medium %Qv cannot be configured explicitly",
-                medium->GetName());
-        }
-    }
-
-    const auto* primaryMedium = chunkManager->GetMediumByIndex(primaryMediumIndex);
+    const auto* medium = chunkManager->GetMediumByIndex(primaryMediumIndex);
     const auto& primaryMediumProperties = properties[primaryMediumIndex];
-    if (!primaryMediumProperties) {
-        THROW_ERROR_EXCEPTION("Medium %Qv is not configured and cannot be made primary",
-            primaryMedium->GetName());
+    if (primaryMediumProperties.GetReplicationFactor() == 0) {
+        THROW_ERROR_EXCEPTION("Medium %Qv stores no chunk replicas and cannot be made primary",
+            medium->GetName());
     }
     if (primaryMediumProperties.GetDataPartsOnly()) {
         THROW_ERROR_EXCEPTION("Medium %Qv stores no parity parts and cannot be made primary",
-            primaryMedium->GetName());
+            medium->GetName());
     }
 }
 
