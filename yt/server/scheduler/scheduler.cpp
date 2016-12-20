fe2701a2ea925d7cb7d89a54a89a9294980ac80c
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2016-12-20 16:38:39 +0300

    YT-5200: Add pool name to scheduler event log

--- yt/server/scheduler/scheduler.cpp
+++ yt/server/scheduler/scheduler.cpp
@@ -1330,10 +1330,7 @@ private:
     void LogOperationFinished(TOperationPtr operation, ELogEventType logEventType, TError error)
     {
         LogEventFluently(logEventType)
-            .Item("operation_id").Value(operation->GetId())
-            .Item("operation_type").Value(operation->GetType())
-            .Item("spec").Value(operation->GetSpec())
-            .Item("authenticated_user").Value(operation->GetAuthenticatedUser())
+            .Do(BIND(&TImpl::BuildOperationInfoForEventLog, MakeStrong(this), operation))
             .Item("start_time").Value(operation->GetStartTime())
             .Item("finish_time").Value(operation->GetFinishTime())
             .Item("controller_time_statistics").Value(operation->ControllerTimeStatistics())
@@ -1628,9 +1625,7 @@ private:
         }
 
         LogEventFluently(ELogEventType::OperationStarted)
-            .Item("operation_id").Value(operation->GetId())
-            .Item("operation_type").Value(operation->GetType())
-            .Item("spec").Value(operation->GetSpec());
+            .Do(BIND(&TImpl::BuildOperationInfoForEventLog, MakeStrong(this), operation));
 
         // NB: Once we've registered the operation in Cypress we're free to complete
         // StartOperation request. Preparation will happen in a separate fiber in a non-blocking
@@ -1847,6 +1842,16 @@ private:
 
         LOG_DEBUG("Operation unregistered (OperationId: %v)",
             operation->GetId());
+    }
+
+    void BuildOperationInfoForEventLog(TOperationPtr operation, IYsonConsumer* consumer)
+    {
+        BuildYsonMapFluently(consumer)
+            .Item("operation_id").Value(operation->GetId())
+            .Item("operation_type").Value(operation->GetType())
+            .Item("spec").Value(operation->GetSpec())
+            .Item("authenticated_user").Value(operation->GetAuthenticatedUser());
+        Strategy_->BuildOperationInfoForEventLog(operation, consumer);
     }
 
     void LogOperationProgress(TOperationPtr operation)
@@ -2123,9 +2128,9 @@ private:
                 return;
         }
 
-        FinishOperation(operation);
-
         LogOperationFinished(operation, logEventType, error);
+
+        FinishOperation(operation);
     }
 
     void AbortAbortingOperation(TOperationPtr operation, TControllerTransactionsPtr controllerTransactions)
