4e9fced9794f41e3af394bc60ec7bda871ad0d04
Maxim Babenko <babenko@ytsaurus.tech>
2016-12-26 23:52:41 +0300

    Simplify logging

--- yt/ytlib/node_tracker_client/node_directory_synchronizer.cpp
+++ yt/ytlib/node_tracker_client/node_directory_synchronizer.cpp
@@ -87,7 +87,7 @@ private:
         try {
             DoSync();
         } catch (const std::exception& ex) {
-            LOG_DEBUG(ex, "Node directory synchronization failed");
+            LOG_DEBUG(TError(ex));
         }
     }
 };
