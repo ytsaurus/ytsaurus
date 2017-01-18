dd965c4272de6ddb6d0289e06f987096b7cada41
savrus <savrus@ytsaurus.tech>
2017-01-18 10:54:14 +0000

    Build YT 19.1 server
    __BYPASS_CHECKS__
    
    
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yt/19_1/yt@2681200 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- server/exec_agent/slot_location.cpp
+++ server/exec_agent/slot_location.cpp
@@ -255,7 +255,7 @@ TFuture<Stroka> TSlotLocation::MakeSandboxTmpfs(
                 THROW_ERROR_EXCEPTION("Sandbox tmpfs in disabled since node doesn't have root permissions");
             }
 
-            if (!sandboxPath.is_prefix(tmpfsPath)) {
+            if (!sandboxPath.StartsWith(tmpfsPath)) {
                 THROW_ERROR_EXCEPTION("Path of the tmpfs mount point must be inside the sandbox directory")
                     << TErrorAttribute("sandbox_path", sandboxPath)
                     << TErrorAttribute("tmpfs_path", tmpfsPath);
@@ -361,7 +361,7 @@ TFuture<void> TSlotLocation::CleanSandboxes(int slotIndex)
 
                 auto sandboxFullPath = NFS::CombinePaths(~NFs::CurrentWorkingDirectory(), sandboxPath);
                 auto isInsideSandbox = [&] (const Stroka& path) {
-                    return path == sandboxFullPath || (sandboxFullPath + "/").is_prefix(path);
+                    return path == sandboxFullPath || (sandboxFullPath + "/").StartsWith(path);
                 };
 
                 // Unmount all known tmpfs.
@@ -456,7 +456,7 @@ Stroka TSlotLocation::GetSandboxPath(int slotIndex, ESandboxKind sandboxKind) co
 bool TSlotLocation::IsInsideTmpfs(const Stroka& path) const
 {
     for (const auto& tmpfsPath : TmpfsPaths_) {
-        if (tmpfsPath.is_prefix(path)) {
+        if (tmpfsPath.StartsWith(path)) {
             return true;
         }
     }
@@ -468,8 +468,8 @@ void TSlotLocation::ValidateEnabled() const
 {
     if (!IsEnabled()) {
         THROW_ERROR_EXCEPTION(
-            EErrorCode::SlotLocationDisabled, 
-            "Slot location at %v is disabled", 
+            EErrorCode::SlotLocationDisabled,
+            "Slot location at %v is disabled",
             Config_->Path);
     }
 }
@@ -480,8 +480,8 @@ void TSlotLocation::Disable(const TError& error)
         return;
 
     auto alert = TError(
-        EErrorCode::SlotLocationDisabled, 
-        "Slot location at %v is disabled", 
+        EErrorCode::SlotLocationDisabled,
+        "Slot location at %v is disabled",
         Config_->Path) << error;
 
     LOG_ERROR(alert);
