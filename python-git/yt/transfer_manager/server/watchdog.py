fd7c4a4a867191c9e0c6690030b85c4b0f15c754
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-01-19 17:16:20 +0300

    YT-6206: Send mails if TM instance is killed

--- python-git/yt/transfer_manager/server/watchdog.py
+++ python-git/yt/transfer_manager/server/watchdog.py
@@ -10,10 +10,28 @@ import argparse
 import socket
 import subprocess
 import logging
+import smtplib
+from datetime import datetime
+from email.mime.text import MIMEText
 
 logger = logging.getLogger("Watchdog")
 
 SLEEP_TIME_BEFORE_START = 10
+
+MAIL_SERVER = "outbound-relay.yandex.net"
+MAIL_TOPIC = "Watchdog run " + datetime.utcnow().strftime("%Y-%m-%dT%H:%M:%S.%fZ")
+
+def send_email(text, recipients):
+    from_ = "tm-watchdog@" + socket.getfqdn()
+
+    msg = MIMEText(text)
+    msg["Subject"] = MAIL_TOPIC
+    msg["From"] = from_
+    msg["To"] = ", ".join(recipients)
+
+    s = smtplib.SMTP(MAIL_SERVER)
+    s.sendmail(from_, recipients, msg.as_string())
+    s.quit()
 
 def main():
     logger.setLevel(logging.INFO)
@@ -24,9 +42,13 @@ def main():
     parser.add_argument("--url", help="Transfer Manager url", required=True)
     parser.add_argument("--failed-attempt-count-to-kill", type=int, default=1)
     parser.add_argument("--attempt-timeout", type=int, default=10)
+    parser.add_argument("--mail-to", action="append", help="send notifications to specified emails")
     parser.add_argument("--cypress-path", help="Transfer Manager path in Cypress",
                         default="//sys/transfer_manager")
     args = parser.parse_args()
+
+    if not args.mail_to:
+        args.mail_to = ["asaitgalin@yandex-team.ru", "ignat@yandex-team.ru"]
 
     status = subprocess.check_output(["sv", "status", "transfer_manager"])
     if status.startswith("down"):
@@ -49,6 +71,9 @@ def main():
     if failed_attempts == args.failed_attempt_count_to_kill:
         logger.warning("All attempts failed. Killing Transfer Manager and aborting lock transaction")
 
+        send_email("Watchdog failed to ping TM after {0} attempts. "
+                   "Instance will be killed.".format(args.failed_attempt_count_to_kill), args.mail_to)
+
         locks = yt.get(yt.ypath_join(args.cypress_path, "lock/@locks"))
         if not locks:
             logger.info("No locks acquired. Killing Transfer Manager process")
@@ -70,6 +95,8 @@ def main():
         time.sleep(SLEEP_TIME_BEFORE_START)
 
         if socket.getfqdn() == yt.get_attribute(args.cypress_path, "address"):
+            send_email("Instance was killed but its transaction (id: {0}) "
+                       "is still alive. Aborting it.".format(transaction_id), args.mail_to)
             yt.abort_transaction(transaction_id)
             logger.info("Aborted lock transaction %s", transaction_id)
         else:
