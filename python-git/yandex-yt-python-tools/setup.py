679ddab9807cf296af542ec0d54d371a7bee2a40
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-01-20 15:48:35 +0300

    yandex-yt-python changelog. yandex-yt-python-tools changelog. yandex-yt-transfer-manager changelog.

--- python-git/yandex-yt-python-tools/setup.py
+++ python-git/yandex-yt-python-tools/setup.py
@@ -3,7 +3,7 @@ from helpers import get_version, prepare_files
 from setuptools import setup
 
 def main():
-    requires = ["yandex-yt >= 0.8.1", "sh", "subprocess32"]
+    requires = ["yandex-yt >= 0.8.4", "sh", "subprocess32"]
 
     scripts, data_files = prepare_files([
         "yt/tools/bin/import_from_mr.py",
