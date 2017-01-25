d9a36d1730a728f6894c015e70232fe53b0d2f44
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-01-26 00:40:50 +0300

    Added 19.1 to local mode archives

--- python-git/yandex-yt-local/postprocess.sh
+++ python-git/yandex-yt-local/postprocess.sh
@@ -133,7 +133,7 @@ YANDEX_YT_LOCAL_VERSION=$(dpkg-parsechangelog | grep Version | awk '{print $2}')
 YANDEX_YT_PYTHON_VERSION="0.8.3-0"
 
 if [ "$UBUNTU_CODENAME" = "precise" ]; then
-    YANDEX_YT_VERSIONS="18.5.21216-stable~ac560f8 19.0.21463-prestable~0f0fae5"
+    YANDEX_YT_VERSIONS="18.5.21216-stable~ac560f8 19.0.21463-prestable~0f0fae5 19.1.21667-stable~e598cca"
 elif [ "$UBUNTU_CODENAME" = "trusty" ]; then
     YANDEX_YT_VERSIONS="18.5.20825-stable~26131bb 19.0.21462-prestable~5acf879"
 else
