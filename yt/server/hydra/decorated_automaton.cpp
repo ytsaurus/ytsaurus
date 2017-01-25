48c498657b6df3283eb178bda3094db90c9c2667
Maxim Babenko <babenko@ytsaurus.tech>
2017-01-25 18:27:31 +0300

    More logging

--- yt/server/hydra/decorated_automaton.cpp
+++ yt/server/hydra/decorated_automaton.cpp
@@ -294,7 +294,6 @@ private:
         static_cast<TSnapshotParams&>(remoteParams) = params;
         return remoteParams;
     }
-
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -401,13 +400,16 @@ private:
 
     void OnFinished()
     {
+        LOG_INFO("Waiting for transfer loop to finish");
+
         WaitFor(AsyncTransferResult_)
             .ThrowOnError();
+
+        LOG_INFO("Waiting for snapshot writer to close");
 
         WaitFor(SnapshotWriter_->Close())
             .ThrowOnError();
     }
-
 };
 
 ////////////////////////////////////////////////////////////////////////////////
