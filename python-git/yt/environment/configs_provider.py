a3987178aa1b3b954321ae40754e7d47bcfa1cd7
Maxim Babenko <babenko@ytsaurus.tech>
2017-02-01 17:31:02 +0300

    Version 19.2 support

--- python-git/yt/environment/configs_provider.py
+++ python-git/yt/environment/configs_provider.py
@@ -624,11 +624,15 @@ class ConfigsProvider_19_0(ConfigsProvider_18_5):
 class ConfigsProvider_19_1(ConfigsProvider_19_0):
     pass
 
+class ConfigsProvider_19_2(ConfigsProvider_19_1):
+    pass
+
 VERSION_TO_CONFIGS_PROVIDER_CLASS = {
     (18, 3): ConfigsProvider_18_3,
     (18, 4): ConfigsProvider_18_4,
     (18, 5): ConfigsProvider_18_5,
     (19, 0): ConfigsProvider_19_0,
-    (19, 1): ConfigsProvider_19_1
+    (19, 1): ConfigsProvider_19_1,
+    (19, 2): ConfigsProvider_19_2
 }
 
