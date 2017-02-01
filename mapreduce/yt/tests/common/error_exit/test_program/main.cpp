a4c29c18bbf3a95e4d931e7dc5d0b51ce6bc6675
ermolovd <ermolovd@ytsaurus.tech>
2017-02-01 11:58:45 +0300

    YAMR client: better error exit When program exits with error (SIGTERM, SIGINT, exception) all uncommited transactions are aborted. YT-6354

---
+++ mapreduce/yt/tests/common/error_exit/test_program/main.cpp
@@ -0,0 +1,54 @@
+#include <mapreduce/lib/all.h>
+#include <util/system/env.h>
+
+using namespace NMR;
+
+class TFailingMap : public NMR::IMap {
+public:
+    TFailingMap() = default;
+
+    TFailingMap(int sleepSeconds)
+        : SleepSeconds(sleepSeconds)
+    { }
+
+    virtual void Start(ui32, ui64, TUpdate&) override
+    {
+        Sleep(TDuration::Seconds(SleepSeconds));
+        _exit(1); // error exit
+    }
+
+    virtual void Do(TValue, TValue, TUpdate&) override
+    { }
+
+private:
+    int SleepSeconds = 0;
+
+private:
+    OBJECT_METHODS(TFailingMap);
+    SAVELOAD(SleepSeconds);
+};
+
+REGISTER_SAVELOAD_CLASS(0x73BC2B28, TFailingMap);
+
+int main(int argc, const char** argv) {
+    NMR::Initialize(argc, argv);
+
+    const Stroka ytProxy = GetEnv("YT_PROXY");
+    const int sleepSeconds = FromString<int>(GetEnv("SLEEP_SECONDS"));
+    const Stroka inputTable = GetEnv("INPUT_TABLE");
+    const Stroka outputTable = GetEnv("OUTPUT_TABLE");
+    TServer server(ytProxy);
+    {
+        TClient client(server);
+        TUpdate update(client, inputTable);
+        update.Add("foo", "bar");
+    }
+
+    TMRParams params;
+    params.AddInputTable(inputTable);
+    params.AddOutputTable(outputTable);
+    params.WorkingTimeLimit.SetMaxJobFails(1);
+
+    THolder<TFailingMap> testMap(new TFailingMap(sleepSeconds));
+    server.Map(params, testMap.Get());
+}
