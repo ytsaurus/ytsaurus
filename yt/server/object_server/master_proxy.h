00ddf61ced56f537729b3d31faff68ecf6d3d660
Maxim Babenko <babenko@ytsaurus.tech>
2017-02-03 20:39:47 +0300

    Extract master_type_handler.* and master_proxy.*

--- yt/server/object_server/master_proxy.h
+++ yt/server/object_server/master_proxy.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "public.h"
-#include "object_detail.h"
-#include "type_handler_detail.h"
 
 #include <yt/server/cell_master/public.h>
 
@@ -11,13 +9,10 @@ namespace NObjectServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TMasterObject
-    : public TNonversionedObjectBase
-{
-public:
-    explicit TMasterObject(const TObjectId& id);
-
-};
+IObjectProxyPtr CreateMasterProxy(
+    NCellMaster::TBootstrap* bootstrap,
+    TObjectTypeMetadata* metadata,
+    TMasterObject* object);
 
 ////////////////////////////////////////////////////////////////////////////////
 
