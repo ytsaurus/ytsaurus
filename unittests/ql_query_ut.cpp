fa8825b03419c315cf6b68fe3c408c0f3ed8dcfc
rnefyodov <rnefyodov@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2017-02-03 19:41:33 +0000

    switch to LLVM_BC REVIEW:236242
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yt/19_1/yt@2710927 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- unittests/ql_query_ut.cpp
+++ unittests/ql_query_ut.cpp
@@ -1,8 +1,13 @@
 #include "framework.h"
 #include "ql_helpers.h"
 #include "udf/invalid_ir.h"
-#include "udf/malloc_udf.h"
-#include "udf/test_udfs.h"
+
+#ifndef YT_IN_ARCADIA
+#include "udf/malloc_udf.h" // Y_IGNORE
+#include "udf/test_udfs.h"  // Y_IGNORE
+#else
+#include <library/resource/resource.h>
+#endif
 
 #include <yt/ytlib/query_client/callbacks.h>
 #include <yt/ytlib/query_client/column_evaluator.h>
@@ -471,10 +476,14 @@ protected:
 
         ActionQueue_ = New<TActionQueue>("Test");
 
+#ifndef YT_IN_ARCADIA
         auto bcImplementations = TSharedRef(
             test_udfs_bc,
             test_udfs_bc_len,
             nullptr);
+#else
+        auto bcImplementations = TSharedRef::FromString(::NResource::Find("/llvm_bc/test_udfs"));
+#endif
 
         MergeFrom(TypeInferers_.Get(), *BuiltinTypeInferrersMap);
         MergeFrom(FunctionProfilers_.Get(), *BuiltinFunctionCG);
