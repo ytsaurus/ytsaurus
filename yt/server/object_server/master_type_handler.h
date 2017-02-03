6ec650f7643b1534c3f9c19fc427812eca744b21
Maxim Babenko <babenko@ytsaurus.tech>
2017-02-03 20:39:47 +0300

    Extract master_type_handler.* and master_proxy.*

--- yt/server/object_server/master_type_handler.h
+++ yt/server/object_server/master_type_handler.h
@@ -1,8 +1,6 @@
 #pragma once
 
 #include "public.h"
-#include "object_detail.h"
-#include "type_handler_detail.h"
 
 #include <yt/server/cell_master/public.h>
 
@@ -11,13 +9,7 @@ namespace NObjectServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TMasterObject
-    : public TNonversionedObjectBase
-{
-public:
-    explicit TMasterObject(const TObjectId& id);
-
-};
+IObjectTypeHandlerPtr CreateMasterTypeHandler(NCellMaster::TBootstrap* bootstrap);
 
 ////////////////////////////////////////////////////////////////////////////////
 
--- yt/server/object_server/object_manager.cpp
+++ yt/server/object_server/object_manager.cpp
@@ -3,8 +3,7 @@
 #include "config.h"
 #include "garbage_collector.h"
 #include "master.h"
-#include "object.h"
-#include "schema.h"
+#include "master_type_handler.h"
 
 #include <yt/server/cell_master/bootstrap.h>
 #include <yt/server/cell_master/hydra_facade.h>
