e5908e5a82c32d870cbf5f210696b9ed2895f429
zero323 <zero323@users.noreply.github.com>
2017-02-10 14:59:04 +0100

    Extend pyspark.sql.column annotations

--- taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/sql/_typing.pyi
+++ taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/sql/_typing.pyi
@@ -1,8 +1,12 @@
 from typing import Any, List, Optional, TypeVar, Union
+import datetime
+import decimal
 import pyspark.sql.column
 
 ColumnOrName = Union[pyspark.sql.column.Column, str]
 Literal = Union[bool, int, float, str]
+DecimalLiteral = decimal.Decimal
+DateTimeLiteral = Union[datetime.datetime, datetime.date]
 LiteralType = TypeVar('LiteralType', bool, int, float, str)
 
 
