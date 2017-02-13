2fe9e83cfd89b4d70569658c3e9246700a77b025
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2017-02-13 20:36:04 +0300

    YT-4954 - Support LookupRows in RPC proxy

--- yt/server/rpc_proxy/api_service.cpp
+++ yt/server/rpc_proxy/api_service.cpp
@@ -9,12 +9,20 @@
 
 #include <yt/ytlib/api/native_client.h>
 #include <yt/ytlib/api/native_connection.h>
+#include <yt/ytlib/api/rowset.h>
 
 #include <yt/ytlib/rpc_proxy/api_service_proxy.h>
 
 #include <yt/ytlib/security_client/public.h>
 
+#include <yt/ytlib/table_client/name_table.h>
+#include <yt/ytlib/table_client/row_buffer.h>
+
+#include <yt/ytlib/tablet_client/wire_protocol.h>
+
 #include <yt/core/concurrency/scheduler.h>
+
+#include <yt/core/misc/serialize.h>
 
 #include <yt/core/rpc/service_detail.h>
 
@@ -27,8 +35,13 @@ using namespace NConcurrency;
 using namespace NRpc;
 using namespace NCompression;
 using namespace NBlackbox;
+using namespace NTableClient;
+using namespace NTabletClient;
 
 ////////////////////////////////////////////////////////////////////////////////
+
+struct TApiServiceBufferTag
+{ };
 
 class TApiService
     : public TServiceBase
@@ -43,6 +56,7 @@ public:
         , Bootstrap_(bootstrap)
     {
         RegisterMethod(RPC_SERVICE_METHOD_DESC(GetNode));
+        RegisterMethod(RPC_SERVICE_METHOD_DESC(LookupRows));
     }
 
 private:
@@ -128,6 +142,8 @@ private:
             return;
         }
 
+        context->SetRequestInfo("Path: %v", request->path());
+
         // TODO(sandello): Inject options into req/rsp structure.
         auto options = TGetNodeOptions();
         client->GetNode(request->path(), options).Subscribe(BIND([=] (const TErrorOr<NYson::TYsonString>& result) {
@@ -138,6 +154,59 @@ private:
                 context->Reply();
             }
         }));
+    }
+
+    DECLARE_RPC_SERVICE_METHOD(NRpcProxy::NProto, LookupRows)
+    {
+        auto client = GetAuthenticatedClientOrAbortContext(context);
+        if (!client) {
+            return;
+        }
+
+        if (request->wire_format_version() != 1) {
+            context->Reply(TError("Unsupported wire format version"));
+            return;
+        }
+
+        if (request->Attachments().empty()) {
+            context->Reply(TError("Request is missing data"));
+            return;
+        }
+
+        auto nameTable = New<TNameTable>();
+        for (const auto& desc : request->rowset_column_descriptor()) {
+            YCHECK(nameTable->RegisterName(desc.name()) == desc.id());
+        }
+
+        TWireProtocolReader reader(
+            MergeRefsToRef<TApiServiceBufferTag>(request->Attachments()),
+            New<TRowBuffer>());
+        auto keys = reader.ReadUnversionedRowset(true);
+
+        context->SetRequestInfo("Path: %v, Rows: %v", request->path(), keys.Size());
+
+        client->LookupRows(request->path(), nameTable, keys).Subscribe(BIND([=] (const TErrorOr<IRowsetPtr>& result) {
+            if (!result.IsOK()) {
+                context->Reply(result);
+            } else {
+                const auto& rowset = result.Value();
+                const auto& schema = rowset->Schema();
+
+                int id = 0;
+                for (const auto& column : schema.Columns()) {
+                    auto* desc = response->add_rowset_column_descriptor();
+                    desc->set_id(id++);
+                    desc->set_type(static_cast<int>(column.Type));
+                    desc->set_name(column.Name);
+                }
+
+                TWireProtocolWriter writer;
+                writer.WriteUnversionedRowset(rowset->GetRows());
+                response->Attachments() = writer.Finish();
+                context->Reply();
+            }
+        }));
+
     }
 };
 
