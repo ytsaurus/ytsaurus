df2b41c8833147a05b72faaa13280361cf7dc093
ermolovd <ermolovd@ytsaurus.tech>
2017-02-15 18:07:03 +0300

    Support AttributeKey/ChildKey options for Lock operation YT-6495

--- mapreduce/yt/tests/native/cypress.cpp
+++ mapreduce/yt/tests/native/cypress.cpp
@@ -26,19 +26,26 @@ public:
         TTest::TearDown();
     }
 
-    IClientPtr Client() const { return Client_; }
-    const char* Node() const { return "tmp/node"; }
-    const char* Node2() const { return "tmp/node2"; }
-    const char* Node3() const { return "tmp/node3"; }
+    IClientPtr Client() const {
+        return Client_;
+    }
+
+    const char* Node() const {
+        return "tmp/node";
+    }
+
+    const char* Node2() const {
+        return "tmp/node2";
+    }
+
+    const char* Node3() const {
+        return "tmp/node3";
+    }
 
 private:
     void RemoveNodes()
     {
-        TRemoveOptions options;
-        options.Force(true).Recursive(true);
-        Client()->Remove(Node(), options);
-        Client()->Remove(Node2(), options);
-        Client()->Remove(Node3(), options);
+        Client()->Remove("//tmp/*", TRemoveOptions().Force(true).Recursive(true));
     }
 
     IClientPtr Client_;
@@ -165,6 +172,83 @@ YT_TEST(TCypress, Link)
 
     EXPECT_EQ(Client()->Exists(Node()), true);
     EXPECT_EQ(Client()->Exists(Node2()), true);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+YT_TEST(TCypress, Lock_ChildKey)
+{
+    Client()->Create("//tmp/map-node", NT_MAP);
+    Client()->Set("//tmp/map-node/child1", 1);
+    Client()->Set("//tmp/map-node/child2", 2);
+
+    auto tx1 = Client()->StartTransaction();
+
+    // wrong lock type
+    EXPECT_THROW(
+        tx1->Lock("//tmp/map-node", ELockMode::LM_EXCLUSIVE, TLockOptions().ChildKey("child1")),
+        yexception);
+
+    // should be ok
+    tx1->Lock("//tmp/map-node", ELockMode::LM_SHARED, TLockOptions().ChildKey("child1"));
+
+    tx1->Set("//tmp/map-node/child1", 11);
+
+    EXPECT_THROW(
+        tx1->Lock("//tmp/map-node", ELockMode::LM_EXCLUSIVE, TLockOptions().ChildKey("non-existent-key")),
+        yexception);
+
+    auto tx2 = Client()->StartTransaction();
+
+    // locked
+    EXPECT_THROW(tx2->Set("//tmp/map-node/child1", 12), yexception);
+
+    // lock is already taken
+    EXPECT_THROW(
+        tx2->Lock("//tmp/map-node", ELockMode::LM_SHARED, TLockOptions().ChildKey("child1")),
+        yexception);
+
+    // should be ok
+    tx2->Lock("//tmp/map-node", ELockMode::LM_SHARED, TLockOptions().ChildKey("child2"));
+    tx2->Set("//tmp/map-node/child2", 22);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+YT_TEST(TCypress, Lock_AttributeKey)
+{
+    Client()->Create("//tmp/table", NT_TABLE);
+    Client()->Set("//tmp/table/@attribute1", 1);
+    Client()->Set("//tmp/table/@attribute2", 2);
+
+    auto tx1 = Client()->StartTransaction();
+
+    // wrong lock type
+    EXPECT_THROW(
+        tx1->Lock("//tmp/table",
+            ELockMode::LM_EXCLUSIVE,
+            TLockOptions().AttributeKey("attribute1")),
+        yexception);
+
+    // should be ok
+    tx1->Lock("//tmp/table",
+        ELockMode::LM_SHARED,
+        TLockOptions().ChildKey("attribute1"));
+
+    tx1->Set("//tmp/table/@attribute1", 11);
+
+    auto tx2 = Client()->StartTransaction();
+
+    // lock is already taken
+    EXPECT_THROW(
+        tx2->Lock("//tmp/table",
+            ELockMode::LM_SHARED,
+            TLockOptions().ChildKey("attribute1")),
+        yexception);
+
+    EXPECT_THROW(
+        tx2->Set("//tmp/table/@attribute1", 12),
+        yexception);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
