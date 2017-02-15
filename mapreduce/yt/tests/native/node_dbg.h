b4f2be027868f4d19e9505ea47c46cac3c0c30c2
ermolovd <ermolovd@ytsaurus.tech>
2017-02-15 19:58:33 +0300

    REVIEW:YT-6445

---
+++ mapreduce/yt/tests/native/node_dbg.h
@@ -0,0 +1,23 @@
+#pragma once
+
+#include <mapreduce/yt/interface/node.h>
+#include <mapreduce/yt/common/node_visitor.h>
+
+#include <library/dbg_output/engine.h>
+#include <library/yson/writer.h>
+
+template <>
+struct TDumper<NYT::TNode>
+{
+    template <class S>
+    static inline void Dump(S& s, const NYT::TNode& node)
+    {
+        using namespace NYT;
+        if (node.GetType() == TNode::UNDEFINED) {
+            s.Stream() << "UNDEFINED";
+        } else {
+            TYsonWriter writer(&s.Stream(), EYsonFormat::YF_TEXT);
+            TNodeVisitor(&writer).Visit(node);
+        }
+    }
+};
