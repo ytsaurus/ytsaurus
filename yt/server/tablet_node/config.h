a0bef5b9c8186eccd749828b1d70e17e3bba3c8d
Maxim Babenko <babenko@ytsaurus.tech>
2017-02-17 21:03:49 +0300

    YT-6489: Get rid of MaxTransactionDuration

--- yt/server/tablet_node/config.h
+++ yt/server/tablet_node/config.h
@@ -236,7 +236,6 @@ class TTransactionManagerConfig
 {
 public:
     TDuration MaxTransactionTimeout;
-    TDuration MaxTransactionDuration;
     TDuration BarrierCheckPeriod;
     int MaxAbortedTransactionPoolSize;
 
@@ -244,8 +243,6 @@ public:
     {
         RegisterParameter("max_transaction_timeout", MaxTransactionTimeout)
             .GreaterThan(TDuration())
-            .Default(TDuration::Seconds(60));
-        RegisterParameter("max_transaction_duration", MaxTransactionDuration)
             .Default(TDuration::Seconds(60));
         RegisterParameter("barrier_check_period", BarrierCheckPeriod)
             .Default(TDuration::MilliSeconds(100));
