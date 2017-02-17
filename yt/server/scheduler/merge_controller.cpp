a493d9f19aa962e78e9f1455772f16dcd0212704
Pavel Sushin <psushin@ytsaurus.tech>
2017-02-17 21:37:04 +0300

    Fix build

--- yt/server/scheduler/merge_controller.cpp
+++ yt/server/scheduler/merge_controller.cpp
@@ -1981,7 +1981,7 @@ protected:
 
                     currentDataSize += sliceWeight.second;
 
-                    if (currentDataSize > > 2 * MaxDataSizePerJob && HasActiveTask()) {
+                    if (currentDataSize > 2 * MaxDataSizePerJob && HasActiveTask()) {
                         breakpointKey = GetKeyPrefixSuccessor(sliceWeight.first, ForeignKeyColumnCount, RowBuffer);
                         currentDataSize = 0;
                         EndTaskAtKey(breakpointKey);
