e07f365966cf5e743104564fb4683225af802c6f
Pavel Ostyakov <ostyakov@yandex-team.ru>
2017-02-22 19:55:46 +0300

    YT-6526: Remember path in LocalYt context manager

--- python-git/yt/local/__init__.py
+++ python-git/yt/local/__init__.py
@@ -3,17 +3,18 @@ from .commands import start, stop, delete, get_proxy, list_instances
 from .helpers import YTCheckingThread
 
 class LocalYt(object):
-    def __init__(self, *args, **kwargs):
+    def __init__(self, path=None, *args, **kwargs):
         self._args = args
         self._kwargs = kwargs
         self._local_yt_instance = None
+        self._path = path
 
     def __enter__(self):
         assert self._local_yt_instance is None
-        self._local_yt_instance = start(*self._args, **self._kwargs)
+        self._local_yt_instance = start(*self._args, path=self._path, **self._kwargs)
         return self._local_yt_instance.create_client()
 
     def __exit__(self, exc_type, exc_val, exc_tb):
         assert self._local_yt_instance is not None
-        stop(self._local_yt_instance.id)
+        stop(self._local_yt_instance.id, path=self._path)
         self._local_yt_instance = None
