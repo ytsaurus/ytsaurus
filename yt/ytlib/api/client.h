0a46cddc6d84d912b2d805de23a9c9d1d6d2de36
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2017-03-03 13:32:01 +0300

    Reduce default lookup priority from real-time to batch

--- yt/ytlib/api/client.h
+++ yt/ytlib/api/client.h
@@ -52,7 +52,7 @@ namespace NApi {
 
 struct TUserWorkloadDescriptor
 {
-    EUserWorkloadCategory Category = EUserWorkloadCategory::Realtime;
+    EUserWorkloadCategory Category = EUserWorkloadCategory::Batch;
     int Band = 0;
 
     operator TWorkloadDescriptor() const;
