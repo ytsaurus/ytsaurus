78a83ba6ee2afd0d8213185ccc54252b19ee48c7
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-03-04 15:25:04 +0300

    Added script for building standalone python

---
+++ python-git/build_standalone.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+
+CURRENT_DIR="$(pwd)"
+
+if [ "$#" -ne 2  ]; then
+    echo "Usage: build_standalone.sh package python-source-tree-root" && exit 1
+fi
+
+PACKAGE="$1" && shift
+PYTHON_SOURCE_TREE_ROOT="$1" && shift
+
+prepare_dir() {
+    local dir=$1 && shift
+    if [ -d "$dir" ]; then
+        rm -rf "$dir"
+    fi
+    mkdir "$dir"
+}
+
+BUILD_DIR="$(pwd)/build_python"
+INSTALL_DIR="$(pwd)/install_python"
+
+cleanup() {
+    rm -rf "$BUILD_DIR" "$INSTALL_DIR"
+    rm -rf setup.py requirements.txt debian build dist *.egg-info Makefile
+}
+
+cleanup
+
+prepare_dir "$BUILD_DIR"
+prepare_dir "$INSTALL_DIR"
+
+cd "$BUILD_DIR"
+
+"$PYTHON_SOURCE_TREE_ROOT/configure" --enable-unicode=ucs4 --prefix="$INSTALL_DIR" CFLAGS="-O2 -g -DNDEBUG"
+make -j $(grep -c ^processor /proc/cpuinfo) && make install
+
+cd "$CURRENT_DIR"
+
+export PYTHONHOME="$INSTALL_DIR"
+
+# Installing pip
+curl "https://bootstrap.pypa.io/get-pip.py" | "$INSTALL_DIR/bin/python"
+
+# Installing requirements
+if [ -f "$PACKAGE/requirements.txt" ]; then
+    "$INSTALL_DIR/bin/python" -m pip install \
+        -r "$PACKAGE/requirements.txt" \
+        --ignore-installed \
+        -i https://pypi.yandex-team.ru/simple
+fi
+
+# Preparing debian directory and setup.py
+prepare_dir debian
+cp "$PACKAGE/debian/changelog" debian
+cp "$PACKAGE/debian/control" debian
+cp "$PACKAGE/setup.py" .
+cp standalone-package/debian/rules debian
+cp standalone-package/debian/compat debian
+
+# Building package wheel and installing package
+"$INSTALL_DIR/bin/python" setup.py bdist_wheel
+"$INSTALL_DIR/bin/python" -m pip install dist/*.whl --ignore-installed
+
+# Replacing python in shebangs to newly built
+grep "#\!" -R "$INSTALL_DIR/bin" -l -I | xargs sed -i '1 s/^.*$/#\!\/opt\/'"$PACKAGE"'\/bin\/python/g'
+
+unset PYTHONHOME
+
+# Building package
+cp standalone-package/Makefile .
+PACKAGE="$PACKAGE" PYTHON_INSTALLDIR="$INSTALL_DIR" dpkg-buildpackage -b
+
+cleanup
