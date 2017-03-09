edf20cf8d36c08ded57a952e70c3b72ce753041e
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-03-09 16:21:11 +0300

    virtualenv-ed fennel

--- python-git/yandex-yt-fennel/setup.py
+++ python-git/yandex-yt-fennel/setup.py
@@ -1,22 +1,18 @@
-from helpers import get_version, prepare_files
+from helpers import get_version
 
 from setuptools import setup
 
 def main():
-    scripts, data_files = prepare_files(["yt/fennel/bin/new_fennel.py", "yt/fennel/bin/fennel_rotate.py"])
-
     setup(
-        name="yandex-yt-fennel",
-        version=get_version(),
-        packages = [ "yt.fennel" ],
+        name = "yandex-yt-fennel",
+        version = get_version(),
+        packages = ["yt.fennel"],
 
-        scripts = scripts,
-        data_files = data_files,
+        scripts = ["yt/fennel/bin/new_fennel.py", "yt/fennel/bin/fennel_rotate.py"],
 
-        author="Kolesnichenko Ignat",
-        author_email="ignat@yandex-team.ru",
+        author = "Kolesnichenko Ignat",
+        author_email = "ignat@yandex-team.ru",
     )
-
 
 if __name__ == "__main__":
     main()
