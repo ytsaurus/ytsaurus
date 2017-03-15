e20afd959bcea2b85a945a82df5a8ea233e114ad
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-03-15 13:32:51 +0300

    Fix clear_tmp.sh script

--- scripts/cron/clear_tmp.sh
+++ scripts/cron/clear_tmp.sh
@@ -1,6 +1,4 @@
 #!/bin/bash -eux
 
-CLEAR_TMP="/opt/cron/clear_tmp.py"
-
-$CLEAR_TMP --directory "//tmp" --account "tmp" --do-not-remove-objects-with-other-account
-$CLEAR_TMP --directory "//tmp/yt_wrapper/file_storage" --account "tmp_files" --do-not-remove-objects-with-other-account
+/opt/cron/clear_full_tmp.sh
+/opt/cron/clear_tmp_files.sh
