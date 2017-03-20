11a26ec1e39564afe3a3fe01ed545b7bea396445
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-03-20 17:25:51 +0300

    YT-5813: Support empty string for dict options in env variables

--- python-git/yt/wrapper/config.py
+++ python-git/yt/wrapper/config.py
@@ -134,7 +134,7 @@ class Config(types.ModuleType, client_state.ClientState):
             if isinstance(None, var_type):
                 var_type = str
             if var_type == dict:
-                var_type = lambda obj: self.yson_module.json_to_yson(self.json_module.loads(obj))
+                var_type = lambda obj: self.yson_module.json_to_yson(self.json_module.loads(obj)) if obj else {}
             return var_type
 
         def apply_type(type, key, value):
