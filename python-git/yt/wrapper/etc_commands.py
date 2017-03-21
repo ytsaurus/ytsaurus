a0034029b2c62b701a53196566ff2b977da3d649
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-03-21 20:17:38 +0300

    YT-6703: Support concurrency in execute_batch

--- python-git/yt/wrapper/etc_commands.py
+++ python-git/yt/wrapper/etc_commands.py
@@ -1,5 +1,6 @@
 from .driver import make_request, make_formatted_request
 from .format import YsonFormat
+from .common import set_param
 from yt.ypath import parse_ypath
 
 from yt.common import update
@@ -22,9 +23,13 @@ def make_parse_ypath_request(path, client=None):
 
     return result
 
-def execute_batch(requests, client=None):
+def execute_batch(requests, concurrency=None, client=None):
     """Executes `requests` in parallel as one batch request."""
-    return make_formatted_request("execute_batch", params={"requests": requests}, format=None, client=client)
+    params = {
+        "requests": requests
+    }
+    set_param(params, "concurrency", concurrency)
+    return make_formatted_request("execute_batch", params=params, format=None, client=client)
 
 def dump_job_context(job_id, path, client=None):
     """Dumps job input context to specified path."""
