ffd1e4fefdbf552d1d142a19cd6e51e102ea6aea
Pavel Sushin <psushin@ytsaurus.tech>
2017-03-22 19:55:48 +0300

    Sometimes unordered task is not created

--- yt/server/scheduler/map_controller.cpp
+++ yt/server/scheduler/map_controller.cpp
@@ -213,7 +213,8 @@ protected:
 
     virtual bool IsCompleted() const override
     {
-        return UnorderedTask->IsCompleted();
+        // Unordered task may be null, if all chunks were teleported.
+        return !UnorderedTask || UnorderedTask->IsCompleted();
     }
 
     virtual void CustomPrepare() override
