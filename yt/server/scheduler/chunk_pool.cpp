9b750edfc94fd5e66bd8f463d6067afefe43ab68
Vladislav Klyachin <klyachin@yandex-team.ru>
2017-04-03 15:51:58 +0300

    YT-6807: Fix unavailable chunks in unread data slices

--- yt/server/scheduler/chunk_pool.cpp
+++ yt/server/scheduler/chunk_pool.cpp
@@ -1135,6 +1135,7 @@ private:
 
         auto stat = suspendableStripe.GetStatistics();
 
+        suspendableStripe.SetExtractedCookie(extractedStripeList->Cookie);
         AddStripeToList(
             suspendableStripe.GetStripe(),
             stat.DataSize,
