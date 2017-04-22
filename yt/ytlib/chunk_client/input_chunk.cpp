a4db3481f297abddd9ec4b1ecc9f5b3a430063d3
Pavel Sushin <psushin@ytsaurus.tech>
2017-04-23 01:50:27 +0300

    Fix meta for versioned tables

--- yt/ytlib/chunk_client/input_chunk.cpp
+++ yt/ytlib/chunk_client/input_chunk.cpp
@@ -210,7 +210,7 @@ void ToProto(NProto::TChunkSpec* chunkSpec, const TInputChunkPtr& inputChunk, ED
 
     // This is the default intermediate table chunk format,
     // so we omit chunk_meta altogether to minimize job spec size.
-    if (inputChunk->TableChunkFormat_ != ETableChunkFormat::SchemalessHorizontal || dataSourceType == EDataSourceType::UnversionedTable) {
+    if (inputChunk->TableChunkFormat_ != ETableChunkFormat::SchemalessHorizontal || dataSourceType != EDataSourceType::UnversionedTable) {
         chunkSpec->mutable_chunk_meta()->set_type(static_cast<int>(EChunkType::Table));
         chunkSpec->mutable_chunk_meta()->set_version(static_cast<int>(inputChunk->TableChunkFormat_));
         chunkSpec->mutable_chunk_meta()->mutable_extensions();
