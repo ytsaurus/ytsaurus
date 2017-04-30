cd6337f0913c939267c92649b4eb2318dba1e077
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-04-30 16:14:46 +0300

    YT-6968: support alter_table_replica command instead of enable/disable_table_replica

--- python-git/yt/wrapper/batch_api.py
+++ python-git/yt/wrapper/batch_api.py
@@ -1,7 +1,7 @@
 from .cypress_commands import set, get, list, exists, remove, mkdir, copy, move, link, get_type, create, \
                               has_attribute, set_attribute, list_attributes
 from .acl_commands import check_permission, add_member, remove_member
-from .table_commands import create_table, row_count, is_sorted, is_empty, enable_table_replica, disable_table_replica
+from .table_commands import create_table, row_count, is_sorted, is_empty, alter_table_replica
 from .dynamic_table_commands import alter_table, mount_table, unmount_table, remount_table, \
                                     freeze_table, unfreeze_table, reshard_table
 from .operation_commands import suspend_operation, resume_operation, get_operation_attributes
