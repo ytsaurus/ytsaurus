e4b3f6fa29c7aa1c69044035618990ec2e564190
Maxim Babenko <babenko@ytsaurus.tech>
2017-05-17 09:17:36 +0300

    Fix imports

--- tests/integration/tests/test_scheduler_remote_copy.py
+++ tests/integration/tests/test_scheduler_remote_copy.py
@@ -1,8 +1,7 @@
 import pytest
 
-from yt_env_setup import YTEnvSetup
+from yt_env_setup import YTEnvSetup, wait
 from yt_commands import *
-from yt.environment import YTInstance
 from yt import yson
 
 import time
