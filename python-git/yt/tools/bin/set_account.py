ad4740aea3d00a8c449d78fa9512ee03e82ee7bf
Pavel Ostyakov <ostyakov@yandex-team.ru>
2017-05-18 16:43:09 +0300

    YT-5565: Batch requests in yt_set_account

--- python-git/yt/tools/bin/set_account.py
+++ python-git/yt/tools/bin/set_account.py
@@ -3,6 +3,9 @@
 import yt.logger as logger
 import yt.wrapper as yt
 from yt.wrapper.cli_helpers import die
+from yt.wrapper.batch_helpers import create_batch_client
+
+from yt.packages.six.moves import zip as izip
 
 import argparse
 import sys
@@ -18,21 +21,29 @@ def main():
     if args.proxy is not None:
         yt.config["proxy"]["url"] = args.proxy
 
-    for obj in yt.search(args.dir, attributes=["account", "type"]):
+    batch_client = create_batch_client()
+
+    processed_nodes = []
+    for obj in yt.search(args.dir, attributes=["account", "type"], enable_batch_mode=True):
         if obj.attributes["account"] != args.account:
-            try:
-                if obj.attributes["type"] == "link":
-                    yt.set(obj + "&/@account", args.account)
-                else:
-                    yt.set(obj + "/@account", args.account)
-            except yt.YtResponseError as error:
-                if error.is_concurrent_transaction_lock_conflict():
-                    logger.warning("Cannot set '%s' account to node '%s'", args.account, obj)
-                elif error.is_resolve_error():
-                    pass
-                else:
-                    raise
-            logger.info("Account '%s' set to node '%s'", args.account, obj)
+            if obj.attributes["type"] == "link":
+                set_result = batch_client.set(obj + "&/@account", args.account)
+            else:
+                set_result = batch_client.set(obj + "/@account", args.account)
+            processed_nodes.append((obj, set_result))
+
+    batch_client.commit_batch()
+
+    for obj, set_result in processed_nodes:
+        if set_result.get_error() is not None:
+            error = yt.YtResponseError(set_result.get_error())
+            if error.is_concurrent_transaction_lock_conflict():
+                logger.warning("Cannot set '%s' account to node '%s'", args.account, obj)
+            elif error.is_resolve_error():
+                pass
+            else:
+                raise error
+        logger.info("Account '%s' set to node '%s'", args.account, obj)
 
 
 if __name__ == "__main__":
