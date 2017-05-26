9115f3e0a71297c984290118b0594981e411f441
aozeritsky <aozeritsky@yandex-team.ru>
2017-05-26 14:14:11 +0300

    YT-7036; BalancingRpcClient fix style, add new createRpcClient method

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/BalancingRpcClient.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/BalancingRpcClient.java
@@ -1,8 +1,5 @@
 package ru.yandex.yt.ytclient.rpc;
 
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
-
 import java.time.Duration;
 import java.util.*;
 import java.util.concurrent.CancellationException;
