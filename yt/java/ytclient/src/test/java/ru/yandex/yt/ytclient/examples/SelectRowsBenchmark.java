f261abe4715b46b1b43a6c7f96ed32812e7a6ea3
aozeritsky <aozeritsky@yandex-team.ru>
2017-05-31 18:10:08 +0300

    YT-7036; style

--- yt/java/ytclient/src/test/java/ru/yandex/yt/ytclient/examples/SelectRowsBenchmark.java
+++ yt/java/ytclient/src/test/java/ru/yandex/yt/ytclient/examples/SelectRowsBenchmark.java
@@ -12,7 +12,6 @@ import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.concurrent.LinkedBlockingQueue;
 import java.util.concurrent.TimeUnit;
-import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.Lock;
 import java.util.concurrent.locks.ReentrantLock;
