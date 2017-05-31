556eb6ce8058169cd7d14cc585f5ef81f7cc5436
aozeritsky <aozeritsky@yandex-team.ru>
2017-05-31 19:49:52 +0300

    YT-7036; add examples as java application

---
+++ yt/java/ytclient/examples/run.sh
@@ -0,0 +1,47 @@
+#!/bin/bash
+
+basedir=$(dirname $0)
+
+CLASSPATH=""
+
+for f in `find -L $basedir -name "*.jar"`;
+do
+    CLASSPATH="$CLASSPATH":"$f"
+done
+
+if [ -z "$JVM_OPTS" ]
+then
+    JVM_OPTS="-Xmx256M -Djava.net.preferIPv6Addresses=true"
+fi
+
+if [ -n "$JAVA_HOME" ]
+then
+    JAVA="$JAVA_HOME/bin/java"
+elif [ -z "$JAVA" ]
+then
+    JAVA=java
+fi
+
+if [ -n "$LOG_DIR" ]
+then
+	JVM_OPTS="$JVM_OPTS -Dlogs.dir=$LOG_DIR"
+fi
+
+if [ -n "$LOG4J" ]
+then
+	JVM_OPTS="$JVM_OPTS -Dlog4j.configuration=$LOG4j"
+else
+	JVM_OPTS="$JVM_OPTS -Dlog4j.configuration=file:$basedir/etc/log4j.properties"
+fi
+
+if [ -n "$JVM_DEBUG_PORT" ]
+then
+	JVM_OPTS="$JVM_OPTS -agentlib:jdwp=transport=dt_socket,server=y,suspend=n,address=$JVM_DEBUG_PORT"
+fi
+
+echo "CLASSPATH=$CLASSPATH"
+echo "JVM_OPTS=$JVM_OPTS"
+export CLASSPATH
+
+exec $JAVA $JVM_OPTS ru.yandex.yt.ytclient.examples.SelectRowsBenchmark $@
+
