2e6c2259cfbe108a35081be6d7a31d71feb66c6e
Maxim Babenko <babenko@ytsaurus.tech>
2017-06-02 22:13:13 +0300

    YT-7093: Rollback object creation when FillAttributes fails

--- tests/integration/tests/test_media.py
+++ tests/integration/tests/test_media.py
@@ -424,6 +424,10 @@ class TestMedia(YTEnvSetup):
                 banned = True
         assert banned
 
+    def test_create_with_invalid_attrs_yt_7093(self):
+        with pytest.raises(YtError): create_medium("x", attributes={"priority": "hello"})
+        assert not exists("//sys/media/x")
+
 ################################################################################
 
 class TestMediaMulticell(TestMedia):
