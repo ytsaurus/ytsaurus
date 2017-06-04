aed0ad279ddeee4a096931704d56fd1f3f10f4da
Maxim Babenko <babenko@ytsaurus.tech>
2017-06-04 22:09:11 +0300

    Cache per-category config version

--- yt/core/logging/private.h
+++ yt/core/logging/private.h
@@ -9,7 +9,7 @@ namespace NLogging {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-extern const char* const SystemLoggingCategory;
+extern const char* const SystemLoggingCategoryName;
 extern const char* const DefaultStderrWriterName;
 extern const ELogLevel DefaultStderrMinLevel;
 extern const ELogLevel DefaultStderrQuietLevel;
