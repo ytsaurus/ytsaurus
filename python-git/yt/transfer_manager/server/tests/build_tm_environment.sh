7da93dbd79424e1d7437647e225646c3b309cbad
Pavel Ostyakov <ostyakov@yandex-team.ru>
2017-06-07 19:36:40 +0300

    YT-7107: Cached TM environment

--- python-git/yt/transfer_manager/server/tests/build_tm_environment.sh
+++ python-git/yt/transfer_manager/server/tests/build_tm_environment.sh
@@ -4,13 +4,16 @@
 TM_REQUIREMENTS=$1
 
 TM_VENV_PATH="tmvenv_tmp"
-YT_PATH="//home/files/test_data/transfer_manager/tmvenv.tar"
+
+SCRIPT_ABSOLUTE_FILENAME=$(readlink -e "$0")
+SCRIPT_DIRECTORY=$(dirname "$SCRIPT_ABSOLUTE_FILENAME")
+
+DISTRIB_CODENAME=$(lsb_release -a -s | gawk NR==4)
+CACHE_PATH=$(cat "$SCRIPT_DIRECTORY/cache_path")
+YT_PATH="$CACHE_PATH/$DISTRIB_CODENAME-tmvenv.tar"
 export YT_PROXY=locke
 
-virtualenv "$TM_VENV_PATH"
-source "$TM_VENV_PATH/bin/activate"
-cat "$TM_REQUIREMENTS" | grep yandex-yt -v | xargs -n 1 pip install
-deactivate
+bash "$SCRIPT_DIRECTORY/prepare_tm_environment.sh" "$TM_REQUIREMENTS" "$TM_VENV_PATH"
 
 cd "$TM_VENV_PATH"
 tar -cf tmvenv.tar *
