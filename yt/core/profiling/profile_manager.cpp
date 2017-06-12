edb665a5a75c42403c3ba8fe0cd15898abf86b4b
Maxim Babenko <babenko@ytsaurus.tech>
2017-06-12 19:16:01 +0300

    Reduce contention at ProfileManager spinlock

--- yt/core/profiling/profile_manager.cpp
+++ yt/core/profiling/profile_manager.cpp
@@ -240,24 +240,40 @@ private:
             TRspGet* response,
             const TCtxGetPtr& context)
         {
-            auto profilingManager = TProfileManager::Get()->Impl_;
-            TGuard<TForkAwareSpinLock> tagGuard(profilingManager->GetTagSpinLock());
-
-            context->SetRequestInfo();
-
             auto options = FromProto(request->options());
             auto fromTime = ParseInstant(options->Find<i64>("from_time"));
-            auto range = GetSamples(fromTime);
+            context->SetRequestInfo("FromTime: %v", fromTime);
+
+            auto samplesRange = GetSamples(fromTime);
+
+            yhash_map<TTagId, TTag> tagIdToValue;
+            for (auto it = samplesRange.first; it != samplesRange.second; ++it) {
+                const auto& sample = *it;
+                for (auto tagId : sample.TagIds) {
+                    tagIdToValue.emplace(tagId, TTag());
+                }
+            }
+
+            {
+                const auto& profilingManager = TProfileManager::Get()->Impl_;
+                TGuard<TForkAwareSpinLock> tagGuard(profilingManager->GetTagSpinLock());
+                for (auto& pair : tagIdToValue) {
+                    pair.second = profilingManager->GetTag(pair.first);
+                }
+            }
+
             response->set_value(BuildYsonStringFluently()
-                .DoListFor(range.first, range.second, [&] (TFluentList fluent, const TSamplesIterator& it) {
+                .DoListFor(samplesRange.first, samplesRange.second, [&] (TFluentList fluent, const TSamplesIterator& it) {
                     const auto& sample = *it;
                     fluent
                         .Item().BeginMap()
                             .Item("id").Value(sample.Id)
                             .Item("time").Value(static_cast<i64>(sample.Time.MicroSeconds()))
                             .Item("value").Value(sample.Value)
-                            .Item("tags").DoMapFor(sample.TagIds, [&] (TFluentMap fluent, TTagId id) {
-                                const auto& tag = profilingManager->GetTag(id);
+                            .Item("tags").DoMapFor(sample.TagIds, [&] (TFluentMap fluent, TTagId tagId) {
+                                auto it = tagIdToValue.find(tagId);
+                                YCHECK(it != tagIdToValue.end());
+                                const auto& tag = it->second;
                                 fluent
                                     .Item(tag.Key).Value(tag.Value);
                             })
