de8cd7aa4befdcfe592aa93f994b2d9198726319
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-06-16 07:33:35 +0300

    Fix build

--- python-git/yt/subprocess_wrapper.py
+++ python-git/yt/subprocess_wrapper.py
@@ -15,9 +15,11 @@ except ImportError:
 Popen = subprocess.Popen
 PIPE = subprocess.PIPE
 STDOUT = subprocess.STDOUT
+CalledProcessError = subprocess.CalledProcessError
 
 def check_call(*args, **kwargs):
     return subprocess.check_call(*args, **kwargs)
 
 def check_output(*args, **kwargs):
     return to_native_str(subprocess.check_output(*args, **kwargs))
+
