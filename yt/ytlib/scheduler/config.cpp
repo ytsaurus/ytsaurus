076f9d447e001b5f13375605392df45c603a582a
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-06-19 13:21:35 +0300

    Revert "Increased max chunk count per job since chunks are now fetched in parallel"
    This reverts commit c64e49e4af0fade33d4d49e15dc991f24c218c1f.

--- yt/ytlib/scheduler/config.cpp
+++ yt/ytlib/scheduler/config.cpp
@@ -708,7 +708,7 @@ TRemoteCopyOperationSpec::TRemoteCopyOperationSpec()
     RegisterParameter("cluster_connection", ClusterConnection)
         .Default();
     RegisterParameter("max_chunk_count_per_job", MaxChunkCountPerJob)
-        .Default(1000);
+        .Default(100);
     RegisterParameter("copy_attributes", CopyAttributes)
         .Default(false);
     RegisterParameter("attribute_keys", AttributeKeys)
