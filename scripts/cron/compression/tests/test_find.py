bf17abd5ab520232f38560eb8bc4f8f144c7a918
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-06-25 17:57:45 +0300

    Added new test and fixed find_tables_to_compress.py

--- scripts/cron/compression/tests/test_find.py
+++ scripts/cron/compression/tests/test_find.py
@@ -1,47 +1,44 @@
-import yt.local as yt_local
+from yt.local import LocalYt 
 
 from yt.packages.six.moves import xrange
-
-import yt.wrapper as yt
 
 import os
 import sys
 import subprocess
+import pytest
 
 FIND_SCRIPT_PATH = os.path.join(os.path.dirname(__file__), "../find_tables_to_compress.py")
 FIND_SCRIPT_DATA_PATH = os.path.join(os.path.dirname(__file__), "compress_script_cypress")
 SANDBOX_PATH = os.path.join(os.path.dirname(__file__), "../tests.sandbox")
 
-def test_find_script():
-    yt_env = yt_local.start(local_cypress_dir=FIND_SCRIPT_DATA_PATH,
-                            path=SANDBOX_PATH)
-    yt.config["proxy"]["url"] = yt_env.get_proxy_address()
-    try:
-        for worker_count in (1, 3, 15, 120):
-            yt.create("map_node", "//tasks_root")
-            for index in xrange(worker_count):
-                yt.create("list_node", "//tasks_root/" + str(index))
-            yt.set("//tasks_root/@alive_workers", list(map(str, xrange(worker_count))))
 
-            p = subprocess.Popen([sys.executable, FIND_SCRIPT_PATH, "--tasks-root", "//tasks_root"],
-                                 env={"YT_PROXY": yt_env.get_proxy_address()})
-            assert p.wait() == 0
+@pytest.mark.parametrize("worker_count", [1, 3, 15, 120])
+def test_find_script(worker_count):
+    with LocalYt(local_cypress_dir=FIND_SCRIPT_DATA_PATH, path=SANDBOX_PATH) as client:
+        client.create("map_node", "//tasks_root")
+        for index in xrange(worker_count):
+            client.create("list_node", "//tasks_root/" + str(index))
+        client.set("//tasks_root/@alive_workers", list(map(str, xrange(worker_count))))
 
-            tables = []
-            for lst in yt.list("//tasks_root"):
-                tables.extend(yt.get("//tasks_root/" + lst))
+        p = subprocess.Popen([sys.executable, FIND_SCRIPT_PATH, "--tasks-root", "//tasks_root"],
+                             env={"YT_PROXY": client.config["proxy"]["url"]})
+        assert p.wait() == 0
 
-            assert sorted([obj["table"] for obj in tables]) == sorted([
-                "//test/dir_with_force_compress/table",
-                "//test/dir_with_no_force_compress/table",
-                "//test/dir_with_opaque/table",
-                "//test/dir_with_recursive_compress_spec/table",
-                "//test/dir_with_recursive_compress_spec/other_subdir/table",
-                "//test/dir_with_custom_codecs/table"])
+        tables = []
+        for lst in client.list("//tasks_root"):
+            tables.extend(client.get("//tasks_root/" + lst))
 
-            zlib6_tables = [obj["table"] for obj in tables if obj["compression_codec"] == "zlib_6"]
-            assert zlib6_tables == ["//test/dir_with_custom_codecs/table"]
+        assert sorted([obj["table"] for obj in tables]) == sorted([
+            "//test/dir_with_force_compress/table",
+            "//test/dir_with_no_force_compress/table",
+            "//test/dir_with_opaque/table",
+            "//test/dir_with_recursive_compress_spec/table",
+            "//test/dir_with_recursive_compress_spec/other_subdir/table",
+            "//test/dir_with_custom_codecs/table",
+            "//test/dir_with_none_erasure_codec/table"])
 
-            yt.remove("//tasks_root", recursive=True)
-    finally:
-        yt_local.stop(yt_env.id, remove_working_dir=True, path=SANDBOX_PATH)
+        zlib6_tables = [obj["table"] for obj in tables if obj["compression_codec"] == "zlib_6"]
+        assert zlib6_tables == ["//test/dir_with_custom_codecs/table"]
+
+        none_erasure_codec_tables = [obj["table"] for obj in tables if obj["erasure_codec"] == "none"]
+        assert none_erasure_codec_tables == ["//test/dir_with_none_erasure_codec/table"]
