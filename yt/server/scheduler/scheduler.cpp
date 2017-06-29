e0a8303ac5d8d7543037cd68b5806a0cdc9e395a
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-06-29 19:06:20 +0300

    YT-7251: Do not use control thread in processing heartbeat

--- yt/server/scheduler/scheduler.cpp
+++ yt/server/scheduler/scheduler.cpp
@@ -300,11 +300,15 @@ public:
 
     bool IsConnected()
     {
+        VERIFY_THREAD_AFFINITY_ANY();
+
         return MasterConnector_->IsConnected();
     }
 
     void ValidateConnected()
     {
+        VERIFY_THREAD_AFFINITY_ANY();
+
         if (!IsConnected()) {
             THROW_ERROR_EXCEPTION(
                 NRpc::EErrorCode::Unavailable,
@@ -767,8 +771,23 @@ public:
             .Run();
     }
 
+    void LogOperations(yhash_set<TOperationId> operationsToLog)
+    {
+        VERIFY_THREAD_AFFINITY(ControlThread);
+
+        for (const auto& operationId : operationsToLog) {
+            auto operation = FindOperation(operationId);
+            if (!operation) {
+                continue;
+            }
+            LogOperationProgress(operation);
+        }
+    }
+
     void ProcessHeartbeat(TCtxHeartbeatPtr context)
     {
+        VERIFY_THREAD_AFFINITY_ANY();
+
         auto* request = &context->Request();
         auto nodeId = request->node_id();
 
@@ -779,14 +798,10 @@ public:
                 .Run(context))
             .ValueOrThrow();
 
-        // NB: Do heavy logging after responding to heartbeat.
-        for (const auto& operationId : operationsToLog) {
-            auto operation = FindOperation(operationId);
-            if (!operation) {
-                continue;
-            }
-            LogOperationProgress(operation);
-        }
+        GetControlInvoker()->Invoke(BIND(
+            &TImpl::LogOperations,
+            MakeStrong(this),
+            Passed(std::move(operationsToLog))));
     }
 
     // ISchedulerStrategyHost implementation
