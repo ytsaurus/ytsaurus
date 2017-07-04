42defddb2853ceef798dc0ed1baccb3c19731bca
aozeritsky <aozeritsky@yandex-team.ru>
2017-07-04 14:25:11 +0300

    code style

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/DefaultRpcFailoverPolicy.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/DefaultRpcFailoverPolicy.java
@@ -1,7 +1,7 @@
 package ru.yandex.yt.ytclient.rpc;
 
 /**
- * Created by aozeritsky on 28.06.2017.
+ * @author aozeritsky
  */
 public class DefaultRpcFailoverPolicy implements RpcFailoverPolicy {
     public boolean onError(RpcClientRequest request, Throwable error) {
