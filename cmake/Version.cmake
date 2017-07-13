3e0d8174e1af56bdc1cc2b84fe05ec9b879f0bc4
Maxim Babenko <babenko@ytsaurus.tech>
2017-07-13 10:37:15 +0300

    Some stubs for YP

--- cmake/Version.cmake
+++ cmake/Version.cmake
@@ -14,36 +14,62 @@ if (NOT YT_BUILD_GIT_DEPTH)
   set(YT_BUILD_GIT_DEPTH 0)
 endif()
 
-# Set the build version
+if(CMAKE_BUILD_TYPE STREQUAL "Debug")
+  set(YT_BUILD_TYPE "debug")
+endif()
+if(YT_USE_ASAN)
+  set(YT_BUILD_TYPE "asan")
+endif()
+if(YT_USE_TSAN)
+  set(YT_BUILD_TYPE "tsan")
+endif()
+if(YT_USE_MSAN)
+  set(YT_BUILD_TYPE "msan")
+endif()
+if(_is_clang)
+  set(YT_BUILD_TYPE "clang")
+endif()
+
+# Set the YT build version
 set(YT_VERSION_MAJOR 19)
 set(YT_VERSION_MINOR 2)
 set(YT_VERSION_PATCH ${YT_BUILD_GIT_DEPTH})
-
 set(YT_ABI_VERSION "${YT_VERSION_MAJOR}.${YT_VERSION_MINOR}")
-set(YT_VERSION "${YT_VERSION_MAJOR}.${YT_VERSION_MINOR}.${YT_VERSION_PATCH}")
 
+# Clone some version attributes from YT to YP
+set(YP_BUILD_BRANCH "${YT_BUILD_BRANCH}")
+set(YP_BUILD_NUMBER "${YT_BUILD_NUMBER}")
+set(YP_BUILD_VCS_NUMBER "${YT_BUILD_VCS_NUMBER}")
+set(YP_BUILD_GIT_DEPTH "${YT_BUILD_GIT_DEPTH}")
+set(YP_BUILD_TYPE "${YT_BUILD_TYPE}")
+
+# Set the YP build version
+set(YP_VERSION_MAJOR 0)
+set(YP_VERSION_MINOR 1)
+set(YP_VERSION_PATCH ${YP_BUILD_GIT_DEPTH})
+
+# Construct the full YT version
+set(YT_VERSION "${YT_VERSION_MAJOR}.${YT_VERSION_MINOR}.${YT_VERSION_PATCH}")
 set(YT_VERSION "${YT_VERSION}-${YT_BUILD_BRANCH}")
-if(CMAKE_BUILD_TYPE STREQUAL "Debug")
-  set(YT_VERSION "${YT_VERSION}~debug")
+if(YT_BUILD_TYPE)
+  set(YT_VERSION "${YT_VERSION}-${YT_BUILD_TYPE}")
 endif()
-if(YT_USE_ASAN)
-  set(YT_VERSION "${YT_VERSION}~asan")
+set(YT_VERSION "${YT_VERSION}~${YT_VCS_BUILD_NUMBER}")
+
+# Construct the full YP version
+set(YP_VERSION "${YP_VERSION_MAJOR}.${YP_VERSION_MINOR}.${YP_VERSION_PATCH}")
+set(YP_VERSION "${YP_VERSION}-${YP_BUILD_BRANCH}")
+if(YP_BUILD_TYPE)
+  set(YP_VERSION "${YP_VERSION}-${YP_BUILD_TYPE}")
 endif()
-if(YT_USE_TSAN)
-  set(YT_VERSION "${YT_VERSION}~tsan")
-endif()
-if(YT_USE_MSAN)
-  set(YT_VERSION "${YT_VERSION}~msan")
-endif()
-if(_is_clang)
-  set(YT_VERSION "${YT_VERSION}~clang")
-endif()
-set(YT_VERSION "${YT_VERSION}~${YT_BUILD_VCS_NUMBER}")
+set(YP_VERSION "${YP_VERSION}~${YP_BUILD_VCS_NUMBER}")
 
 # Underscore is forbidden in the version
 string(REPLACE "_" "-" YT_VERSION ${YT_VERSION})
-# Underscore is forbidden in the version
+string(REPLACE "_" "-" YP_VERSION ${YP_VERSION})
+# Slash is forbidden in the version
 string(REPLACE "/" "-" YT_VERSION ${YT_VERSION})
+string(REPLACE "/" "-" YP_VERSION ${YP_VERSION})
 # Dot is forbidden in proper keywords
 string(REPLACE "." "_" YT_ABI_VERSION_U ${YT_ABI_VERSION})
 
@@ -83,3 +109,7 @@ else()
   set(YT_BUILD_TIME "unknown")
 endif()
 
+# Clone some version attributes from YT to YP
+set(YP_BUILD_HOST "${YT_BUILD_HOST}")
+set(YP_BUILD_MACHINE "${YT_BUILD_MACHINE}")
+set(YP_BUILD_TIME "${YT_BUILD_TIME}")
