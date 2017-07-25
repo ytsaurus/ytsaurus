55a7105242fd7229ec4adef840ff79df9c2bbe31
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2017-07-25 13:51:02 +0300

    Hotfix for QL

--- yt/ytlib/query_client/folding_profiler.cpp
+++ yt/ytlib/query_client/folding_profiler.cpp
@@ -511,6 +511,7 @@ void TQueryProfiler::Profile(TCodegenSource* codegenSource, TConstQueryPtr query
     Fold(static_cast<int>(EFoldingObjectType::ScanOp));
 
     auto schema = query->GetRenamedSchema();
+    TSchemaProfiler::Profile(schema);
 
     size_t currentSlot = MakeCodegenScanOp(codegenSource, slotCount);
 
@@ -583,6 +584,7 @@ void TQueryProfiler::Profile(TCodegenSource* codegenSource, TConstQueryPtr query
             joinClause->CommonKeyPrefix);
 
         schema = joinClause->GetTableSchema(schema);
+        TSchemaProfiler::Profile(schema);
     }
 
     if (whereClause) {
@@ -603,6 +605,7 @@ void TQueryProfiler::Profile(TCodegenSource* codegenSource, TConstFrontQueryPtr 
     Fold(static_cast<int>(EFoldingObjectType::ScanOp));
 
     auto schema = query->GetRenamedSchema();
+    TSchemaProfiler::Profile(schema);
 
     size_t currentSlot = MakeCodegenScanOp(codegenSource, slotCount);
 
