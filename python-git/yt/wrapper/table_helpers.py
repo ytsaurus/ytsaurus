bba4fdb53f2053c27808bfc2bddad78a05040149
Pavel Ostyakov <ostyakov@yandex-team.ru>
2017-07-31 19:19:49 +0300

    Fix remove_tables

--- python-git/yt/wrapper/table_helpers.py
+++ python-git/yt/wrapper/table_helpers.py
@@ -134,7 +134,7 @@ def _remove_tables(tables, client=None):
         if exists_result:
             exists_tables.append(table)
 
-    type_results = batch_apply(get_type, tables, client=client)
+    type_results = batch_apply(get_type, exists_tables, client=client)
 
     tables_to_remove = []
     for table, table_type in izip(exists_tables, type_results):
