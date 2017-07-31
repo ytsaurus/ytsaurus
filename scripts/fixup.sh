406af67745d360d0eec0e1c4efde2e7ef56a9d42
Ivan Puzyrevskiy <sandello@yandex-team.ru>
2017-07-31 17:53:08 +0300

    Cosmetics

--- scripts/fixup.sh
+++ scripts/fixup.sh
@@ -1,9 +1,13 @@
 #!/bin/bash
+# This scripts adapts some style checks from Arcadia.
 
 git ls-files -z | while read -d $'\0' file
 do
     case "$file" in
         contrib/*|yt/contrib/*)
+            continue
+            ;;
+        yt/ytlib/query_client/lexer.cpp)
             continue
             ;;
         *.cpp)
@@ -20,5 +24,5 @@ do
     perl -pe 's/\s+$/\n/' -i "$file"  # remove trailing spaces
     perl -pe 's/\t/    /g' -i "$file"  # replace tabs with spaces
     perl -pe '$_ .= "\n" unless ($_ =~ /\n/)' -i "$file"  # add a terminating newline
-    perl -pe '$_ = "/" x 80 . "\n" if $_ =~ /^\s*\/+\s*$/' -i "$file"  # unify slashes
+    perl -pe 'if ($_ =~ /^(\s*)\/\/\/\/\/+\s*$/) { $_ = $1 . "/" x 80 . "\n" }' -i "$file"  # unify slashes
 done
