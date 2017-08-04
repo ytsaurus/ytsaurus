414514bbfb0778ec8a2b243aced96ad4993da540
Pavel Sushin <psushin@ytsaurus.tech>
2017-08-04 16:51:47 +0300

    Fix test

--- tests/integration/tests/test_scheduler_other.py
+++ tests/integration/tests/test_scheduler_other.py
@@ -1764,12 +1764,12 @@ class TestSchedulerConfig(YTEnvSetup):
             },
             "operation_options": {
                 "spec_template": {
-                    "data_size_per_job": 1000
+                    "data_weight_per_job": 1000
                 }
             },
             "map_operation_options": {
                 "spec_template": {
-                    "data_size_per_job": 2000,
+                    "data_weight_per_job": 2000,
                     "max_failed_job_count": 10
                 }
             },
@@ -1830,16 +1830,16 @@ class TestSchedulerConfig(YTEnvSetup):
         op = map(command="sleep 1000", in_=["//tmp/t_in"], out="//tmp/t_out", dont_track=True)
         time.sleep(1)
         for spec_type in ("spec", "full_spec"):
-            assert get("//sys/operations/{0}/@{1}/data_size_per_job".format(op.id, spec_type)) == 2000
-            assert get("//sys/scheduler/orchid/scheduler/operations/{0}/{1}/data_size_per_job".format(op.id, spec_type)) == 2000
+            assert get("//sys/operations/{0}/@{1}/data_weight_per_job".format(op.id, spec_type)) == 2000
+            assert get("//sys/scheduler/orchid/scheduler/operations/{0}/{1}/data_weight_per_job".format(op.id, spec_type)) == 2000
             assert get("//sys/scheduler/orchid/scheduler/operations/{0}/{1}/max_failed_job_count".format(op.id, spec_type)) == 10
         op.abort()
 
         op = reduce(command="sleep 1000", in_=["//tmp/t_in"], out="//tmp/t_out", reduce_by=["foo"], dont_track=True)
         time.sleep(1)
         for spec_type in ("spec", "full_spec"):
-            assert get("//sys/operations/{0}/@{1}/data_size_per_job".format(op.id, spec_type)) == 1000
-            assert get("//sys/scheduler/orchid/scheduler/operations/{0}/{1}/data_size_per_job".format(op.id, spec_type)) == 1000
+            assert get("//sys/operations/{0}/@{1}/data_weight_per_job".format(op.id, spec_type)) == 1000
+            assert get("//sys/scheduler/orchid/scheduler/operations/{0}/{1}/data_weight_per_job".format(op.id, spec_type)) == 1000
             assert get("//sys/scheduler/orchid/scheduler/operations/{0}/{1}/max_failed_job_count".format(op.id, spec_type)) == 10
         op.abort()
 
@@ -1849,7 +1849,7 @@ class TestSchedulerConfig(YTEnvSetup):
         create("table", "//tmp/t_out")
 
         op = map(command="cat", in_=["//tmp/t_in"], out="//tmp/t_out")
-        assert get("//sys/operations/{0}/@spec/data_size_per_job".format(op.id)) == 2000
+        assert get("//sys/operations/{0}/@spec/data_weight_per_job".format(op.id)) == 2000
         assert get("//sys/operations/{0}/@spec/max_failed_job_count".format(op.id)) == 10
 
         set("//sys/scheduler/config", {
@@ -1859,7 +1859,7 @@ class TestSchedulerConfig(YTEnvSetup):
         time.sleep(0.5)
 
         op = map(command="cat", in_=["//tmp/t_in"], out="//tmp/t_out")
-        assert get("//sys/operations/{0}/@spec/data_size_per_job".format(op.id)) == 2000
+        assert get("//sys/operations/{0}/@spec/data_weight_per_job".format(op.id)) == 2000
         assert get("//sys/operations/{0}/@spec/max_failed_job_count".format(op.id)) == 50
 
         environment = get("//sys/scheduler/orchid/scheduler/config/environment")
