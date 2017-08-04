de4fe97bceaed8fb7070830acdeaff3f3669a57e
Fedor Korotkiy <prime@yandex-team.ru>
2017-08-04 17:56:08 +0300

    Remove dead script

--- scripts/cron/detect_many_chunks_reason.sh
+++ scripts/cron/detect_many_chunks_reason.sh
@@ -2,6 +2,3 @@
 
 echo "Chunks per path:"
 ./find_tables_to_merge.py --ignore-suppress-nightly-merge --print-only | ./aggregate_chunk_table_infomation.py
-
-echo "Chunks per operation:"
-./find_operations_producing_many_chunks.py --minimum-number-of-chunks 500000
