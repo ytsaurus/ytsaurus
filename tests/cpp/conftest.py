c3d268bee2d0d13165ed0bd972c867bf3c338cf5
Ruslan Savchenko <savrus@ytsaurus.tech>
2017-08-08 12:28:47 +0300

    Add setting for c++ integraion tests

---
+++ tests/cpp/conftest.py
@@ -0,0 +1,13 @@
+import sys
+import os
+
+sys.path.insert(0, os.path.abspath('../../tests/integration'))
+sys.path.insert(0, os.path.abspath('../../python'))
+sys.path.append(os.path.abspath('.'))
+
+from yt_shell_test import CppFile
+
+
+def pytest_collect_file(parent, path):
+    if path.ext == '.cpp' and path.basename.startswith('test'):
+        return CppFile(path, parent)
