56425232e0777586a02da978e16b4bb1c5e64e10
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-08-15 14:09:27 +0300

    Revert "Disable expiration timeout by default"
    This reverts commit 4ff514ea9d7bd47c6caeb599c56e23b6dc92d3d4.

--- python-git/yt/wrapper/default_config.py
+++ python-git/yt/wrapper/default_config.py
@@ -314,8 +314,7 @@ default_config = {
     "remote_temp_tables_directory": "//tmp/yt_wrapper/table_storage",
 
     # Expiration timeout for temporary objects (in milliseconds).
-    # Can be None if timeout should not be set.
-    "temp_expiration_timeout": None,
+    "temp_expiration_timeout": 7 * 24 * 60 * 60 * 1000,
 
     "file_cache": {
         "replication_factor": 10,
