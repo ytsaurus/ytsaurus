e6c9c13d312e22774f7a4ee573a738a74fc00870
ermolovd <ermolovd@ytsaurus.tech>
2017-08-24 12:14:12 +0300

    More tests are moved to native_new Pull-request for branch users/ermolovd/local/b4eb736820

--- mapreduce/yt/tests/native_new/main.cpp
+++ mapreduce/yt/tests/native_new/main.cpp
@@ -1,9 +1,11 @@
 #include <library/unittest/utmain.h>
 
 #include <mapreduce/yt/interface/client.h>
+#include <mapreduce/yt/common/config.h>
 
 int main(int argc, const char** argv)
 {
+    NYT::TConfig::Get()->WaitLockPollInterval = TDuration::Zero();
     NYT::Initialize(argc, argv);
     return NUnitTest::RunMain(argc, const_cast<char**>(argv));
 }
