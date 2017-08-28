8d83f8a1d599a77b34d32b1f6f0e7fb31bce0674
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-08-28 11:17:50 +0300

    Do not print empty progress of operation

--- python-git/yt/wrapper/operation_commands.py
+++ python-git/yt/wrapper/operation_commands.py
@@ -185,7 +185,7 @@ class PrintOperationInfo(object):
     def __call__(self, state):
         if state.is_running():
             progress = get_operation_progress(self.operation, client=self.client)
-            if progress != self.progress:
+            if progress and progress != self.progress:
                 self.log(
                     "operation %s: %s",
                     self.operation,
