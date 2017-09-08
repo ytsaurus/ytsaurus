f05f740601a3a6ea6553fd0a57d2d70732319c4f
Vadim Skipin <vskipin@yandex-team.ru>
2017-09-08 18:03:45 +0300

    Introduce caching cookie authenticator (#2180)

--- yt/server/blackbox/public.h
+++ yt/server/blackbox/public.h
@@ -14,6 +14,7 @@ DECLARE_REFCOUNTED_CLASS(TDefaultBlackboxServiceConfig)
 DECLARE_REFCOUNTED_CLASS(TTokenAuthenticatorConfig)
 DECLARE_REFCOUNTED_CLASS(TCachingTokenAuthenticatorConfig)
 DECLARE_REFCOUNTED_CLASS(TCookieAuthenticatorConfig)
+DECLARE_REFCOUNTED_CLASS(TCachingCookieAuthenticatorConfig)
 
 DECLARE_REFCOUNTED_STRUCT(IBlackboxService)
 
@@ -23,7 +24,15 @@ DECLARE_REFCOUNTED_STRUCT(ITokenAuthenticator)
 struct TTokenCredentials
 {
     TString Token;
-    TString UserIp;
+    TString UserIP;
+};
+
+struct TCookieCredentials
+{
+    TString SessionId;
+    TString SslSessionId;
+    TString Host;
+    TString UserIP;
 };
 
 struct TAuthenticationResult
@@ -32,11 +41,19 @@ struct TAuthenticationResult
     TString Realm;
 };
 
-inline bool operator==(
+inline bool operator ==(
     const TTokenCredentials& lhs,
     const TTokenCredentials& rhs)
 {
-    return std::tie(lhs.Token, lhs.UserIp) == std::tie(rhs.Token, rhs.UserIp);
+    return std::tie(lhs.Token, lhs.UserIP) == std::tie(rhs.Token, rhs.UserIP);
+}
+
+inline bool operator ==(
+    const TCookieCredentials& lhs,
+    const TCookieCredentials& rhs)
+{
+    return std::tie(lhs.SessionId, lhs.SslSessionId, lhs.Host, lhs.UserIP) ==
+           std::tie(rhs.SessionId, rhs.SslSessionId, rhs.Host, rhs.UserIP);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -49,10 +66,23 @@ struct hash<NYT::NBlackbox::TTokenCredentials>
 {
     inline size_t operator()(const NYT::NBlackbox::TTokenCredentials& credentials) const
     {
-        size_t result  = 0;
+        size_t result = 0;
         NYT::HashCombine(result, credentials.Token);
-        NYT::HashCombine(result, credentials.UserIp);
+        NYT::HashCombine(result, credentials.UserIP);
         return result;
     }
 };
 
+template <>
+struct hash<NYT::NBlackbox::TCookieCredentials>
+{
+    inline size_t operator()(const NYT::NBlackbox::TCookieCredentials& credentials) const
+    {
+        size_t result = 0;
+        NYT::HashCombine(result, credentials.SessionId);
+        NYT::HashCombine(result, credentials.SslSessionId);
+        NYT::HashCombine(result, credentials.Host);
+        NYT::HashCombine(result, credentials.UserIP);
+        return result;
+    }
+};
