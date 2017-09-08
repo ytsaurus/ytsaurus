7c98c5e789167c6e6497924b4cf4ba98f66feaf5
ermolovd <ermolovd@ytsaurus.tech>
2017-09-08 16:42:17 +0300

    Pull-request for auto-branch users/ermolovd/local/69ec12cce0 * Multiple input reduce example

---
+++ mapreduce/yt/examples/tutorial/multiple_input_reduce_tnode/main.cpp
@@ -0,0 +1,70 @@
+#include <mapreduce/yt/interface/client.h>
+
+#include <util/stream/output.h>
+#include <util/system/user.h>
+
+using namespace NYT;
+
+class TFilterOutRobotsReduce
+    : public IReducer<TTableReader<TNode>, TTableWriter<TNode>>
+{
+public:
+    void Do(TReader* reader, TWriter* writer) override
+    {
+        TNode loginRow;
+        bool isRobot = false;
+        for (; reader->IsValid(); reader->Next()) {
+            const auto& curRow = reader->GetRow();
+
+            // У нас есть информация о том из какой таблицы пришла запись.
+            auto tableIndex = reader->GetTableIndex();
+            if (tableIndex == 0) {
+                // Таблица с логинами.
+                loginRow = curRow;
+            } else if (tableIndex == 1) {
+                isRobot = curRow["is_robot"].AsBool();
+            } else {
+                // Какая-то фигня, такого индекса быть не может.
+                Y_FAIL();
+            }
+        }
+
+        if (isRobot) {
+            writer->AddRow(loginRow);
+        }
+    }
+};
+REGISTER_REDUCER(TFilterOutRobotsReduce)
+
+int main(int argc, const char** argv) {
+    NYT::Initialize(argc, argv);
+
+    auto client = CreateClient("freud");
+
+    const TString sortedLoginTable = "//tmp/" + GetUsername() + "-tutorial-login-sorted";
+    const TString sortedIsRobotTable = "//tmp/" + GetUsername() + "-tutorial-is_robot-sorted";
+    const TString outputTable = "//tmp/" + GetUsername() + "-tutorial-robots";
+
+    client->Sort(
+        TSortOperationSpec()
+            .AddInput("//home/ermolovd/yt-tutorial/staff_unsorted")
+            .Output(sortedLoginTable)
+            .SortBy({"uid"}));
+
+    client->Sort(
+        TSortOperationSpec()
+            .AddInput("//home/ermolovd/yt-tutorial/is_robot_unsorted")
+            .Output(sortedIsRobotTable)
+            .SortBy({"uid"}));
+
+    client->Reduce(
+        TReduceOperationSpec()
+            .ReduceBy({"uid"})
+            .AddInput<TNode>(sortedLoginTable) // Таблицу с логинами мы добавляем первой, поэтому её TableIndex == 0
+            .AddInput<TNode>(sortedIsRobotTable) // Таблицу про роботов мы добавляем второй, поэтому её TableIndex == 1
+            .AddOutput<TNode>(outputTable),
+        new TFilterOutRobotsReduce);
+
+    Cout << "Output table: https://yt.yandex-team.ru/freud/#page=navigation&offsetMode=row&path=" << outputTable << Endl;
+    return 0;
+}
