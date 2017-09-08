1efd1e5d29684e78685cea7253b671569e9061b3
Pavel Sushin <psushin@ytsaurus.tech>
2017-09-08 17:22:54 +0300

    Allow larger attachments for big blocks

--- yt/core/bus/packet.h
+++ yt/core/bus/packet.h
@@ -25,7 +25,7 @@ DEFINE_BIT_ENUM_WITH_UNDERLYING_TYPE(EPacketFlags, ui16,
 
 constexpr ui32 PacketSignature = 0x78616d4f;
 constexpr int MaxPacketPartCount = 1 << 28;
-constexpr ui32 MaxPacketPartSize = 256ULL * 1024 * 1024;
+constexpr ui32 MaxPacketPartSize = 1024ULL * 1024 * 1024;
 constexpr ui32 NullPacketPartSize = 0xffffffff;
 constexpr int TypicalPacketPartCount = 64;
 constexpr int TypicalVariableHeaderSize = TypicalPacketPartCount * (sizeof (ui32) + sizeof (ui64));
