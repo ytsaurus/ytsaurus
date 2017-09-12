37a4b0661bd4f7385e2e4220b2e1ccdb98ab2eb1
ermolovd <ermolovd@ytsaurus.tech>
2017-09-12 15:28:40 +0300

    Pull-request for auto-branch users/ermolovd/local/713a84c3df * Style * Join-reduce example

---
+++ mapreduce/yt/examples/tutorial/join_reduce_tnode/main.cpp
@@ -0,0 +1,56 @@
+#include <mapreduce/yt/interface/client.h>
+
+#include <library/regex/pcre/regexp.h>
+
+#include <util/system/user.h>
+
+using namespace NYT;
+
+class TFilterVideoRegexp
+    : public IReducer<TTableReader<TNode>, TTableWriter<TNode>>
+{
+public:
+    void Do(TReader* reader, TWriter* writer) {
+        TMaybe<TRegExMatch> regex;
+        for (; reader->IsValid(); reader->Next()) {
+            auto row = reader->GetRow();
+            if (reader->GetTableIndex() == 0) { // таблица с хостами
+                const auto videoRegexp = row["video_regexp"].AsString();
+
+                // Дебажная печать, stderr можно будет посмотреть в web интерфейсе
+                Cerr << "Processing host: " << row["host"].AsString() << Endl;
+                if (!videoRegexp.empty()) {
+                    regex = TRegExMatch(videoRegexp);
+                }
+            } else { // таблица с урлами
+                if (regex && regex->Match(row["path"].AsString().c_str())) {
+                    writer->AddRow(row);
+                }
+            }
+        }
+    }
+};
+REGISTER_REDUCER(TFilterVideoRegexp);
+
+int main(int argc, const char** argv) {
+    NYT::Initialize(argc, argv);
+
+    auto client = CreateClient("freud");
+
+    const TString outputTable = "//tmp/" + GetUsername() + "-tutorial-join-reduce";
+
+    client->JoinReduce(
+        TJoinReduceOperationSpec()
+        .JoinBy({"host"})
+        .AddInput<TNode>(
+            TRichYPath("//home/ermolovd/yt-tutorial/host_video_regexp")
+            .Foreign(true)) // важно отметить хостовую таблицу как foreign
+        .AddInput<TNode>("//home/ermolovd/yt-tutorial/doc_title")
+        .AddOutput<TNode>(outputTable),
+        new TFilterVideoRegexp);
+
+    Cout << "Output table: https://yt.yandex-team.ru/freud/#page=navigation&offsetMode=row&path=" << outputTable << Endl;
+
+    return 0;
+}
+
--- mapreduce/yt/examples/tutorial/prepare_tutorial_data.py
+++ mapreduce/yt/examples/tutorial/prepare_tutorial_data.py
@@ -28,6 +28,33 @@ class Staff(object):
                 uid=int(uid),
                 is_robot=is_robot)
 
+class Urls(object):
+    def __init__(self):
+        with open("urls.json", "r") as inf:
+            data = json.loads(inf.read())
+
+        self.hosts = data["hosts"]
+        self.urls = data["urls"]
+
+
+    def iter_urls(self):
+        Document = collections.namedtuple("Document", ["host", "path", "title", "keywords"])
+        for d in self.urls:
+            yield Document(
+                host=d["Host"],
+                path=d["Path"],
+                title=d["Title"],
+                keywords=d["Keywords"],
+            )
+
+    def iter_host(self):
+        Host = collections.namedtuple("Host", ["host", "video_regexp"])
+        for d in self.hosts:
+            yield Host(
+                host=d["Host"],
+                video_regexp=d["VideoRegexp"]
+            )
+
 def main():
     bak_dir = "//home/ermolovd/yt-tutorial.bak"
     tutorial_dir = "//home/ermolovd/yt-tutorial"
@@ -56,6 +83,36 @@ def main():
                     "uid": p.uid,
                     "is_robot": p.is_robot
                 }
+
+        def doc_title_table():
+            for d in Urls().iter_urls():
+                yield {
+                    "host": d.host,
+                    "path": d.path,
+                    "title": d.title,
+                }
+
+        def doc_keywords_table():
+            for d in Urls().iter_urls():
+                yield {
+                    "host": d.host,
+                    "path": d.path,
+                    "keywords": d.keywords,
+                }
+
+        def host_table():
+            for h in Urls().iter_host():
+                yield {
+                    "host": h.host,
+                    "video_regexp": h.video_regexp,
+                }
+
+        yt.wrapper.write_table(yt.wrapper.TablePath(tutorial_table("host_video_regexp"), sorted_by=["host"]),
+                               sorted(host_table(), key=lambda x:(x["host"],)))
+        yt.wrapper.write_table(yt.wrapper.TablePath(tutorial_table("doc_title"), sorted_by=["host", "path"]),
+                               sorted(doc_title_table(), key=lambda x:(x["host"], x["path"])))
+        yt.wrapper.write_table(yt.wrapper.TablePath(tutorial_table("doc_keywords"), sorted_by=["host", "path"]),
+                                              sorted(doc_keywords_table(), key=lambda x:(x["host"], x["path"])))
 
         yt.wrapper.write_table(tutorial_table("staff_unsorted"), staff_unsorted())
         yt.wrapper.write_table(tutorial_table("is_robot_unsorted"), is_robot_unsorted())
