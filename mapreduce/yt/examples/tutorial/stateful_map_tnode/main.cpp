c38bfd3ab58e403ece6895169abea66f62fbacb2
ermolovd <ermolovd@ytsaurus.tech>
2017-09-12 19:28:09 +0300

    Pull-request for auto-branch users/ermolovd/local/68ec70c461 * Stateful job example

---
+++ mapreduce/yt/examples/tutorial/stateful_map_tnode/main.cpp
@@ -0,0 +1,59 @@
+#include <mapreduce/yt/interface/client.h>
+
+#include <library/string_utils/levenstein_diff/levenstein_diff.h>
+
+#include <util/stream/output.h>
+#include <util/system/user.h>
+
+using namespace NYT;
+
+class TFilterMapper
+    : public IMapper<TTableReader<TNode>, TTableWriter<TNode>>
+{
+public:
+    Y_SAVELOAD_JOB(Pattern_, MaxDistance_); // Заклинание, которое говорит, какие переменные нужно передавать на сервер.
+
+    TFilterMapper() = default; // У джобы обязательно должен быть конструктор по умолчанию
+
+    TFilterMapper(TString pattern, double maxDistance)
+        : Pattern_(std::move(pattern))
+        , MaxDistance_(maxDistance)
+    { }
+
+
+    void Do(TReader* reader, TWriter* writer) override {
+        for (; reader->IsValid(); reader->Next()) {
+            const auto& row = reader->GetRow();
+            const auto& name = row["name"].AsString();
+            if (NHunting::LevensteinDistance(name, Pattern_) <= MaxDistance_) {
+                writer->AddRow(row);
+            }
+        }
+    }
+
+private:
+    TString Pattern_;
+    size_t MaxDistance_ = 0;
+};
+REGISTER_MAPPER(TFilterMapper);
+
+int main(int argc, const char** argv) {
+    NYT::Initialize(argc, argv);
+
+    auto client = CreateClient("freud");
+
+    const TString outputTable = "//tmp/" + GetUsername() + "-tutorial-output";
+
+    client->Map(
+        TMapOperationSpec()
+        .AddInput<TNode>("//home/ermolovd/yt-tutorial/staff_unsorted")
+        .AddOutput<TNode>(outputTable),
+        new TFilterMapper("Arkady", 2)); // Мы создаём объект TFilterMapper, и конструктор заполняет поля Pattern_ и MaxDistance_.
+                                         // Библиотека сериализует поля, указанные в Y_SAVELOAD_JOB, и загружает их на сервер.
+                                         // На сервере вызывается конструктор по умолчанию и сериализованные поля загружаются.
+
+    Cout << "Output table: https://yt.yandex-team.ru/freud/#page=navigation&offsetMode=row&path=" << outputTable << Endl;
+
+    return 0;
+}
+
