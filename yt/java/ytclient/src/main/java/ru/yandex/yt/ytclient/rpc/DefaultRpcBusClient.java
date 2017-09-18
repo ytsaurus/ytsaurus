2818dd80ab5dec5ff2f3f21cd7320ca6948156dc
aozeritsky <aozeritsky@yandex-team.ru>
2017-09-18 17:09:57 +0300

    fix too optimistic metrics

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/DefaultRpcBusClient.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/DefaultRpcBusClient.java
@@ -57,8 +57,8 @@ public class DefaultRpcBusClient implements RpcClient {
         Histogram requestsResponseHistogramLocal;
 
         void updateAck(long millis) {
-            requestsResponseHistogramLocal.update(millis);
-            requestsResponseHistogram.update(millis);
+            requestsAckHistogramLocal.update(millis);
+            requestsAckHistogram.update(millis);
         }
 
         void updateResponse(long millis) {
