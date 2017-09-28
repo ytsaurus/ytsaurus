26b73d8234067c864800df27ab9c6a4e613afcea
akastornov <akastornov@yandex-team.ru>
2017-09-28 20:43:38 +0300

    yt-client python binding;

---
+++ mapreduce/yt/client/python/ut/test_yt_client.py
@@ -0,0 +1,267 @@
+import sys
+import time
+import yt_client
+from mapreduce.yt.python.yt_stuff import yt_stuff
+
+
+def test_get(yt_stuff):
+    client = yt_client.Client('localhost:{}'.format(yt_stuff.yt_proxy_port))
+    yt = yt_stuff.get_yt_client()
+    yt.create('map_node', '//test_get', ignore_existing=True)
+
+    # test get string
+    yt.create('string_node', '//test_get/string_node')
+    yt.set('//test_get/string_node', 'ABCDEFG')
+    assert client.get('//test_get/string_node') == 'ABCDEFG'
+
+    # test get int64
+    yt.create('int64_node', '//test_get/int64')
+    yt.set('//test_get/int64', 13)
+    assert client.get('//test_get/int64') == 13
+
+    # test get list
+    l = [1, 2, 'a', 'b', ['x', 'z'], {'a': 1, 'b': 2}]
+    yt.create('list_node', '//test_get/list')
+    yt.set('//test_get/list', l)
+    assert client.get('//test_get/list') == l
+
+    # test get table attrs
+    yt.create('table', '//test_get/table')
+    yt.set('//test_get/table/@attr1', 'string_attr')
+    yt.set('//test_get/table/@attr2', ['a', 'b'])
+    yt.set('//test_get/table/@attr3', {'a': 1, 'b': 2})
+    assert client.get('//test_get/table/@attr1') == 'string_attr'
+    assert client.get('//test_get/table/@attr2') == ['a', 'b']
+    assert client.get('//test_get/table/@attr3') == {'a': 1, 'b': 2}
+    all_attrs = client.get('//test_get/table/@')
+    assert 'attr1' in all_attrs
+    assert 'attr2' in all_attrs
+    assert 'attr3' in all_attrs
+    my_attrs = client.get('//test_get/table/@', attributes=['attr1', 'attr3'])
+    assert 'attr1' in my_attrs
+    assert 'attr3' in my_attrs
+    assert len(my_attrs) == 2
+
+    # test get non-existing
+    try:
+        client.get('//test_get/XXX')
+        assert False
+    except Exception as e:
+        assert 'Node //test_get has no child with key "XXX"' in str(e)
+
+    try:
+        client.get('//test_get/table/@attr100')
+        assert False
+    except Exception as e:
+        assert 'Attribute "attr100" is not found' in str(e)
+
+    # TODO: test max_size parameter(does it work??)
+
+
+def test_set(yt_stuff):
+    client = yt_client.Client('localhost:{}'.format(yt_stuff.yt_proxy_port))
+    yt = yt_stuff.get_yt_client()
+    yt.create('map_node', '//test_set', ignore_existing=True)
+
+    yt.create('string_node', '//test_set/string')
+    client.set('//test_set/string', 'XXX')
+    assert yt.get('//test_set/string') == 'XXX'
+    client.set('//test_set/string', 'ZZZ')
+    assert yt.get('//test_set/string') == 'ZZZ'
+    client.set('//test_set/string/@xxx', {'a': 1, 'b': 2})
+    assert yt.get('//test_set/string/@xxx') == {'a': 1, 'b': 2}
+    client.set('//test_set/string/@yyy', ['a', 2, 'c'])
+    assert yt.get('//test_set/string/@yyy') == ['a', 2, 'c']
+
+
+def test_exists(yt_stuff):
+    client = yt_client.Client('localhost:{}'.format(yt_stuff.yt_proxy_port))
+    yt = yt_stuff.get_yt_client()
+    yt.create('map_node', '//test_exists', ignore_existing=True)
+
+    yt.create('table', '//test_exists/table')
+    yt.create('string_node', '//test_exists/string')
+
+    assert client.exists('//test_exists')
+    assert client.exists('//test_exists/table')
+    assert client.exists('//test_exists/string')
+    assert not client.exists('//test_exists/tablexxx')
+    assert not client.exists('//test_exists/string/x')
+    assert not client.exists('//test_exists/asdasd')
+
+
+def test_create(yt_stuff):
+    client = yt_client.Client('localhost:{}'.format(yt_stuff.yt_proxy_port))
+    yt = yt_stuff.get_yt_client()
+    yt.create('map_node', '//test_create', ignore_existing=True)
+
+    # test create base node types
+    types = [
+        'string_node',
+        'int64_node',
+        'uint64_node',
+        'double_node',
+        'boolean_node',
+        'map_node',
+        'list_node',
+        'file',
+        'table',
+        'document',
+    ]
+
+    for _type in types:
+        client.create(_type, '//test_create/{}'.format(_type))
+        assert yt.exists('//test_create/{}'.format(_type))
+        assert yt.get('//test_create/{}/@type'.format(_type)) == _type
+
+    # test recursive parameter
+    try:
+        client.create('string_node', '//test_create/a/b/c/s')
+        assert False
+    except Exception:
+        pass
+    client.create('string_node', '//test_create/a/b/c/s', recursive=True)
+    assert yt.exists('//test_create/a/b/c/s')
+
+    # test ignore_existing parameter
+    _id = client.create('string_node', '//test_create/existing')
+    try:
+        client.create('string_node', '//test_create/existing')
+        assert False
+    except Exception:
+        pass
+    assert _id == client.create('string_node', '//test_create/existing', ignore_existing=True)
+    try:
+        client.create('table', '//test_create/existing', ignore_existing=True)
+        assert False
+    except Exception:
+        pass
+
+    # test force parameter
+    _id = client.create('string_node', '//test_create/force')
+    assert _id != client.create('table', '//test_create/force', force=True)
+
+    # test attributes parameter
+    client.create(
+        'file',
+        '//test_create/file_with_attrs',
+        attributes={'attr1': 1, 'attr2': 'abc', 'attr3': ['a', 1], 'attr4': {'a': 1, 'b': 2}}
+    )
+    assert yt.get('//test_create/file_with_attrs/@attr1') == 1
+    assert yt.get('//test_create/file_with_attrs/@attr2') == 'abc'
+    assert yt.get('//test_create/file_with_attrs/@attr3') == ['a', 1]
+    assert yt.get('//test_create/file_with_attrs/@attr4') == {'a': 1, 'b': 2}
+
+
+def wait_true(f, timeout):
+    start = time.time()
+    while not f():
+        assert time.time() - start < timeout
+        time.sleep(0.1)
+
+
+def retry(f, n):
+    for i in xrange(n):
+        try:
+            return f()
+        except Exception:
+            if i + 1 == n:
+                raise
+            time.sleep(0.5)
+            continue
+
+
+def test_mount_unmount(yt_stuff):
+    client = yt_client.Client('localhost:{}'.format(yt_stuff.yt_proxy_port))
+    yt = yt_stuff.get_yt_client()
+    yt.create('map_node', '//test_mount', ignore_existing=True)
+
+    client.create_table(
+        '//test_mount/table',
+        attributes={
+            'dynamic': 'true',
+            'schema': [
+                {'name': 'hash', 'type': 'uint64', 'sort_order': 'ascending', 'expression': 'farm_hash(key) % 128'},
+                {'name': 'key', 'type': 'string', 'sort_order': 'ascending'},
+                {'name': 'value', 'type': 'string'},
+            ],
+            'pivot_keys': [
+                [],
+                [yt_client.node_ui64(32)],
+                [yt_client.node_ui64(64)],
+                [yt_client.node_ui64(96)],
+                [yt_client.node_ui64(128)]
+            ],
+            'disable_tablet_balancer': 'true',
+        }
+    )
+
+    # test mount/unmount
+    retry(lambda: client.mount_table('//test_mount/table'), 10)
+    wait_true(lambda: all(t['state'] == 'mounted' for t in client.get('//test_mount/table/@tablets')), 15)
+    retry(lambda: client.unmount_table('//test_mount/table'), 10)
+    wait_true(lambda: all(t['state'] == 'unmounted' for t in client.get('//test_mount/table/@tablets')), 15)
+
+    # TODO: test first_tablet_index, last_tablet_index, cell_id, freeze
+
+
+def test_insert(yt_stuff):
+    client = yt_client.Client('localhost:{}'.format(yt_stuff.yt_proxy_port))
+    yt = yt_stuff.get_yt_client()
+    yt.create('map_node', '//test_insert', ignore_existing=True)
+
+    client.create_table(
+        '//test_insert/table',
+        attributes={
+            'dynamic': 'true',
+            'schema': [
+                {'name': 'hash', 'type': 'uint64', 'sort_order': 'ascending', 'expression': 'farm_hash(key) % 128'},
+                {'name': 'key', 'type': 'string', 'sort_order': 'ascending'},
+                {'name': 'value', 'type': 'string'},
+                {'name': 'count', 'type': 'uint64', 'aggregate': 'sum'}
+            ],
+            'pivot_keys': [
+                [],
+                [yt_client.node_ui64(32)],
+                [yt_client.node_ui64(64)],
+                [yt_client.node_ui64(96)],
+                [yt_client.node_ui64(128)]
+            ],
+            'disable_tablet_balancer': 'true',
+        }
+    )
+    retry(lambda: client.mount_table('//test_insert/table'), 10)
+    wait_true(lambda: all(t['state'] == 'mounted' for t in client.get('//test_insert/table/@tablets')), 15)
+
+    # test insert
+    client.insert_rows(
+        '//test_insert/table',
+        [
+            {'key': 'a', 'value': '1'},
+            {'key': 'b', 'value': '2'},
+        ]
+    )
+    rows = client.select_rows('key, value from [//test_insert/table] where key="a"')
+    assert len(rows) == 1
+    assert rows[0] == {'key': 'a', 'value': '1'}
+
+    # test update
+    client.insert_rows('//test_insert/table', [{'key': 'a'}])
+    client.insert_rows('//test_insert/table', [{'key': 'b'}], update=True)
+
+    rows = client.select_rows('key, value from [//test_insert/table] where key="a" or key="b"')
+    assert len(rows) == 2
+    kv = {row['key']: row['value'] for row in rows}
+    assert kv['a'] is None
+    assert kv['b'] == '2'
+
+    # test aggregate
+    client.insert_rows('//test_insert/table', [{'key': 'x', 'value': 'y', 'count': yt_client.node_ui64(1)}])
+    rows = client.select_rows('key, value, count from [//test_insert/table] where key="x"')
+    assert len(rows) == 1
+    assert rows[0]['count'] == 1
+    client.insert_rows('//test_insert/table', [{'key': 'x', 'value': 'y', 'count': yt_client.node_ui64(1)}], aggregate=True)
+    rows = client.select_rows('key, value, count from [//test_insert/table] where key="x"')
+    assert len(rows) == 1
+    assert rows[0]['count'] == 2
+
---
+++ mapreduce/yt/client/python/yt_client.pyx
@@ -0,0 +1,407 @@
+cimport cython
+from libc.stdlib cimport malloc, free
+from util.generic.vector cimport yvector
+from util.generic.hash cimport yhash
+from util.generic.string cimport TString
+from util.system.types cimport i64, ui64, ui32
+
+
+cdef extern from "mapreduce/yt/client/init.h" namespace "NYT":
+    void Initialize(int, const char**) except +
+
+
+def initialize(*args):
+    cdef const char **argv = <const char**>malloc(len(args) * sizeof(char*))
+    for i in xrange(len(args)):
+        argv[i] = <char*>args[i]
+    try:
+        Initialize(len(args), argv)
+    finally:
+        free(argv)
+
+
+cdef extern from "mapreduce/yt/node/node.h" namespace "NYT" nogil:
+    cdef cppclass TNode:
+        TNode() except +
+        TNode(const char*) except +
+        TNode(TString) except +
+        TNode(double) except +
+        TNode(bint) except +
+        TNode(i64) except +
+        TNode(ui64) except +
+
+        bint IsString()
+        bint IsInt64()
+        bint IsUint64()
+        bint IsDouble()
+        bint IsBool()
+        bint IsList()
+        bint IsMap()
+        bint IsEntity()
+        bint IsUndefined()
+
+        TString& AsString()
+        i64 AsInt64()
+        ui64 AsUint64()
+        double AsDouble()
+        bint AsBool()
+        yvector[TNode]& AsList()
+        yhash[TString, TNode]& AsMap()
+
+        @staticmethod
+        TNode CreateList()
+        @staticmethod
+        TNode CreateMap()
+
+        TNode operator()(TString, TNode)
+        TNode Add(TNode)
+
+
+class Node(object):
+    INT64 = 0
+    UINT64 = 1
+    _ALL_TYPES = {INT64, UINT64}
+
+    def __init__(self, data, node_type):
+        self.data = data
+        if node_type not in Node._ALL_TYPES:
+            raise Exception('unsupported node_type')
+        self.node_type = node_type
+
+
+def node_i64(i):
+    return Node(i, Node.INT64)
+
+
+def node_ui64(ui):
+    return Node(ui, Node.UINT64)
+
+
+cdef TString _to_TString(s):
+    assert isinstance(s, basestring)
+    if isinstance(s, unicode):
+        s = s.encode('UTF-8')
+    return TString(<const char*>s, len(s))
+
+
+cdef _TNode_to_pyobj(TNode node):
+    if node.IsString():
+        return node.AsString()
+    elif node.IsInt64():
+        return node.AsInt64()
+    elif node.IsUint64():
+        return node.AsUint64()
+    elif node.IsDouble():
+        return node.AsDouble()
+    elif node.IsBool():
+        return node.AsBool()
+    elif node.IsEntity():
+        return None
+    elif node.IsUndefined():
+        return None
+    elif node.IsList():
+        node_list = node.AsList()
+        return [_TNode_to_pyobj(n) for n in node_list]
+    elif node.IsMap():
+        node_map = node.AsMap()
+        return {p.first: _TNode_to_pyobj(p.second) for p in node_map}
+    else:
+        # should never happen
+        raise Exception()
+
+
+cdef TNode _pyobj_to_TNode(obj):
+    if isinstance(obj, Node):
+        if obj.node_type == Node.INT64:
+            return TNode(<i64>obj.data)
+        elif obj.node_type == Node.UINT64:
+            return TNode(<ui64>obj.data)
+        else:
+            # should never happen
+            raise Exception()
+    elif isinstance(obj, basestring):
+        return TNode(_to_TString(obj))
+    elif isinstance(obj, bool):
+        return TNode(<bint>obj)
+    elif isinstance(obj, int):
+        return TNode(<i64>obj)
+    elif isinstance(obj, float):
+        return TNode(<float>obj)
+    elif isinstance(obj, dict):
+        node = TNode.CreateMap()
+        for k, v in obj.iteritems():
+            node(_to_TString(k), _pyobj_to_TNode(v))
+        return node
+    elif isinstance(obj, list):
+        node = TNode.CreateList()
+        for x in obj:
+            node.Add(_pyobj_to_TNode(x))
+        return node
+    elif obj is None:
+        return TNode()
+    else:
+        raise Exception('Can\'t convert {} object to TNode'.format(type(obj)))
+
+
+cdef extern from "mapreduce/yt/interface/common.h" namespace "NYT" nogil:
+    cdef cppclass TAttributeFilter:
+        TAttributeFilter() except +
+        TAttributeFilter AddAttribute(TString)
+
+
+cdef extern from "util/datetime/base.h" nogil:
+    cdef cppclass TDuration:
+        @staticmethod
+        TDuration MilliSeconds(int)
+
+
+cdef extern from "mapreduce/yt/interface/client_method_options.h" namespace "NYT" nogil:
+    cdef enum ENodeType:
+        NT_STRING
+        NT_INT64
+        NT_UINT64
+        NT_DOUBLE
+        NT_BOOLEAN
+        NT_MAP
+        NT_LIST
+        NT_FILE
+        NT_TABLE
+        NT_DOCUMENT
+        NT_REPLICATED_TABLE
+        NT_TABLE_REPLICA
+
+    cdef cppclass EAtomicity:
+        pass
+
+    cdef cppclass EDurability:
+        pass
+
+    cdef cppclass TInsertRowsOptions:
+        TInsertRowsOptions() except +
+        TInsertRowsOptions Atomicity(EAtomicity)
+        TInsertRowsOptions Durability(EDurability)
+        TInsertRowsOptions Update(bint)
+        TInsertRowsOptions Aggregate(bint)
+        TInsertRowsOptions RequireSyncReplica(bint)
+
+    cdef cppclass TSelectRowsOptions:
+        TSelectRowsOptions() except +
+        TSelectRowsOptions Timeout(TDuration)
+        TSelectRowsOptions InputRowLimit(i64)
+        TSelectRowsOptions OutputRowLimit(i64)
+        TSelectRowsOptions RangeExpansionLimit(ui64)
+        TSelectRowsOptions FailOnIncompleteResult(bint)
+        TSelectRowsOptions VerboseLogging(bint)
+        TSelectRowsOptions EnableCodeCache(bint)
+
+
+    cdef cppclass TCreateClientOptions:
+        TCreateClientOptions() except +
+        TCreateClientOptions Token(TString)
+
+    cdef cppclass TCreateOptions:
+        TCreateOptions() except +
+        TCreateOptions Recursive(bint)
+        TCreateOptions IgnoreExisting(bint)
+        TCreateOptions Force(bint)
+        TCreateOptions Attributes(TNode)
+
+    cdef cppclass TGetOptions:
+        TGetOptions() except +
+        TGetOptions MaxSize(i64)
+        TGetOptions AttributeFilter(TAttributeFilter)
+
+    cdef cppclass TMountTableOptions:
+        TMountTableOptions() except +
+        TMountTableOptions FirstTabletIndex(i64)
+        TMountTableOptions LastTabletIndex(i64)
+        TMountTableOptions CellId(TTabletCellId)
+        TMountTableOptions Freeze(bint)
+
+    cdef cppclass TUnmountTableOptions:
+        TUnmountTableOptions() except +
+        TUnmountTableOptions FirstTabletIndex(i64)
+        TUnmountTableOptions LastTabletIndex(i64)
+        TUnmountTableOptions Force(bint)
+
+
+cdef _to_cypress_node_type(s):
+    if s == 'string_node':
+        return NT_STRING
+    elif s == 'int64_node':
+        return NT_INT64
+    elif s == 'uint64_node':
+        return NT_UINT64
+    elif s == 'double_node':
+        return NT_DOUBLE
+    elif s == 'boolean_node':
+        return NT_BOOLEAN
+    elif s == 'map_node':
+        return NT_MAP
+    elif s == 'list_node':
+        return NT_LIST
+    elif s == 'file':
+        return NT_FILE
+    elif s == 'table':
+        return NT_TABLE
+    elif s == 'document':
+        return NT_DOCUMENT
+    elif s == 'replicated_table':
+        return NT_REPLICATED_TABLE
+    elif s == 'table_replica':
+        return NT_TABLE_REPLICA
+    else:
+        raise Exception('unknown cypress node type {}'.format(s))
+
+
+cdef extern from "mapreduce/yt/interface/client_method_options.h" namespace "NYT::EAtomicity" nogil:
+    cdef EAtomicity _None "NYT::EAtomicity::None"
+    cdef EAtomicity Full
+
+
+cdef extern from "mapreduce/yt/interface/client_method_options.h" namespace "NYT::EDurability" nogil:
+    cdef EDurability Sync
+    cdef EDurability Async
+
+
+cdef extern from "mapreduce/yt/interface/fwd.h" namespace "NYT" nogil:
+    cdef cppclass TNodeId:
+        ui32 dw[4]
+
+    cdef cppclass TTabletCellId:
+        ui32 dw[4]
+
+    cdef cppclass IClient:
+        TNode Get(TString, TGetOptions) except +
+        void Set(TString, TNode) except +
+        bint Exists(TString) except +
+        TNodeId Create(TString, ENodeType, TCreateOptions) except +
+        void MountTable(TString, TMountTableOptions) except +
+        void UnmountTable(TString, TUnmountTableOptions) except +
+        void InsertRows(TString, yvector[TNode], TInsertRowsOptions) except +
+        yvector[TNode] SelectRows(TString, TSelectRowsOptions) except +
+
+    cdef cppclass IClientPtr:
+        IClient operator*()
+
+
+cdef extern from "mapreduce/yt/interface/client.h" namespace "NYT" nogil:
+    cdef IClientPtr CreateClient(TString, TCreateClientOptions)
+
+
+cdef class Client:
+    cdef IClientPtr _client
+
+    def __cinit__(self, proxy, token=None):
+        cdef TCreateClientOptions opts
+        if token:
+            opts.Token(_to_TString(token))
+        self._client = CreateClient(_to_TString(proxy), opts)
+
+    def get(self, path, max_size=None, attributes=None):
+        cdef TGetOptions opts
+        cdef TAttributeFilter attrs_filter
+        if max_size is not None:
+            opts.MaxSize(<i64>max_size)
+        if attributes is not None:
+            for attr in attributes:
+                attrs_filter.AddAttribute(_to_TString(attr))
+            opts.AttributeFilter(attrs_filter)
+        return _TNode_to_pyobj(cython.operator.dereference(self._client).Get(_to_TString(path), opts))
+
+    def set(self, path, value):
+        cython.operator.dereference(self._client).Set(_to_TString(path), _pyobj_to_TNode(value))
+
+    def exists(self, path):
+        return cython.operator.dereference(self._client).Exists(_to_TString(path))
+
+    def create(self, type, path, recursive=False, ignore_existing=False, force=False, attributes=None):
+        cdef TCreateOptions opts
+        if recursive:
+            opts.Recursive(True)
+        if ignore_existing:
+            opts.IgnoreExisting(True)
+        if force:
+            opts.Force(True)
+        if attributes is not None:
+            opts.Attributes(_pyobj_to_TNode(attributes))
+        cdef TNodeId node_id = cython.operator.dereference(self._client).Create(
+            _to_TString(path),
+            _to_cypress_node_type(type),
+            opts
+        )
+        return '-'.join(hex(node_id.dw[i])[2:] for i in xrange(4))
+
+    def create_table(self, path, recursive=None, ignore_existing=False, attributes=None):
+        return self.create('table', path, recursive=recursive, ignore_existing=ignore_existing, attributes=attributes)
+
+    def mount_table(self, path, first_tablet_index=None, last_tablet_index=None, cell_id=None, freeze=None):
+        cdef TMountTableOptions opts
+        cdef TTabletCellId ci
+        if first_tablet_index is not None:
+            opts.FirstTabletIndex(<i64>first_tablet_index)
+        if last_tablet_index is not None:
+            opts.LastTabletIndex(<i64>last_tablet_index)
+        if cell_id is not None:
+            dw = cell_id.split('-')
+            for i in xrange(4):
+                ci.dw[i] = int(dw[i], 16)
+            opts.CellId(ci)
+        if freeze is not None:
+            opts.Freeze(<bint>freeze)
+        cython.operator.dereference(self._client).MountTable(_to_TString(path), opts)
+
+    def unmount_table(self, path, first_tablet_index=None, last_tablet_index=None, force=False):
+        cdef TUnmountTableOptions opts
+        if first_tablet_index is not None:
+            opts.FirstTabletIndex(<i64>first_tablet_index)
+        if last_tablet_index is not None:
+            opts.LastTabletIndex(<i64>last_tablet_index)
+        if force:
+            opts.Force(<bint>force)
+        cython.operator.dereference(self._client).UnmountTable(_to_TString(path), opts)
+
+    def insert_rows(self, path, rows, update=None, aggregate=None, atomicity=None, durability=None, require_sync_replica=None):
+        cdef TInsertRowsOptions opts
+        if update is not None:
+            opts.Update(<bint>update)
+        if aggregate is not None:
+            opts.Aggregate(<bint>aggregate)
+        if atomicity is not None:
+            if atomicity == 'full':
+                opts.Atomicity(Full)
+            elif atomicity == 'none':
+                opts.Atomicity(_None)
+            else:
+                raise Exception('wrong atomicity: {}'.format(atomicity))
+        if durability is not None:
+            if durability == 'sync':
+                opts.Durability(Sync)
+            elif durability == 'async':
+                opts.Durability(Async)
+            else:
+                raise Exception('wrong durability: {}'.format(durability))
+        if require_sync_replica is not None:
+            opts.RequireSyncReplica(<bint>require_sync_replica)
+        cdef TString cpath = _to_TString(path)
+        cdef yvector[TNode] crows = _pyobj_to_TNode(rows).AsList()
+        with nogil:
+            cython.operator.dereference(self._client).InsertRows(cpath, crows, opts)
+
+    def select_rows(self, query, input_row_limit=None, output_row_limit=None, range_expansion_limit=1000, fail_on_inclomplete_result=True, verbose_logging=False, enable_code_cache=True, timeout=None):
+        cdef TSelectRowsOptions opts
+        if input_row_limit is not None:
+            opts.InputRowLimit(<i64>input_row_limit)
+        if output_row_limit is not None:
+            opts.OutputRowLimit(<i64>output_row_limit)
+        opts.RangeExpansionLimit(<ui64>range_expansion_limit)
+        opts.FailOnIncompleteResult(<bint>fail_on_inclomplete_result)
+        opts.VerboseLogging(<bint>verbose_logging)
+        opts.EnableCodeCache(<bint>enable_code_cache)
+        if timeout is not None:
+            opts.Timeout(TDuration.MilliSeconds(<int>timeout))
+        cdef TString cquery = _to_TString(query)
+        cdef yvector[TNode] rows
+        with nogil:
+            rows = cython.operator.dereference(self._client).SelectRows(cquery, opts)
+        return [_TNode_to_pyobj(row) for row in rows]
