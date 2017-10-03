747ef2ef37c8c7476b35452dbc4ae229629cb579
Ruslan Savchenko <savrus@ytsaurus.tech>
2017-10-03 19:10:10 +0300

    Add mutatio_wait_time metric: lag between commit and apply

--- yt/server/hydra/decorated_automaton.cpp
+++ yt/server/hydra/decorated_automaton.cpp
@@ -1058,6 +1058,13 @@ void TDecoratedAutomaton::DoApplyMutation(TMutationContext* context)
         LOG_DEBUG_UNLESS(IsRecovery(), "Skipping heartbeat mutation (Version: %v)",
             automatonVersion);
     } else {
+        auto syncTime = NProfiling::GetInstant() - context->GetTimestamp();
+        Profiler.Enqueue(
+            "/mutation_wait_time",
+            syncTime.MilliSeconds(),
+            NProfiling::EMetricType::Gauge,
+            CellManager_->GetCellIdTags());
+
         auto* descriptor = GetTypeDescriptor(mutationType);
 
         TMutationContextGuard contextGuard(context);
