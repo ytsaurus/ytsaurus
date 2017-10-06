1ef0b50026c3b7c525ec28ca83bf6c37e78023db
mikari <mikari@yandex-team.ru>
2017-10-06 14:00:05 +0300

    now it's possible to parse TNode from TStringBuf

--- mapreduce/yt/node/node_io.h
+++ mapreduce/yt/node/node_io.h
@@ -11,13 +11,13 @@ namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TNode NodeFromYsonString(const TString& input, EYsonType type = YT_NODE);
+TNode NodeFromYsonString(const TStringBuf input, EYsonType type = YT_NODE);
 TString NodeToYsonString(const TNode& node, EYsonFormat format = YF_TEXT);
 
 TNode NodeFromYsonStream(IInputStream* input, EYsonType type = YT_NODE);
 void NodeToYsonStream(const TNode& node, IOutputStream* output, EYsonFormat format = YF_TEXT);
 
-TNode NodeFromJsonString(const TString& input);
+TNode NodeFromJsonString(const TStringBuf input);
 TNode NodeFromJsonValue(const NJson::TJsonValue& input);
 
 ////////////////////////////////////////////////////////////////////////////////
