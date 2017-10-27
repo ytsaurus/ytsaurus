198da5884e71339fb6ca4f70c4e51e5c9ca6c2d8
aozeritsky <aozeritsky@yandex-team.ru>
2017-10-27 16:38:36 +0300

    YT-7858; new requests; review:346987

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/ApiServiceClient.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/ApiServiceClient.java
@@ -1,11 +1,13 @@
 package ru.yandex.yt.ytclient.proxy;
 
 import java.time.Duration;
+import java.util.List;
 import java.util.Objects;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.Executor;
 import java.util.concurrent.ForkJoinPool;
 import java.util.function.Function;
+import java.util.stream.Collectors;
 
 import com.google.protobuf.ByteString;
 import org.slf4j.Logger;
@@ -13,23 +15,37 @@ import org.slf4j.LoggerFactory;
 
 import ru.yandex.yt.rpcproxy.TReqAbortTransaction;
 import ru.yandex.yt.rpcproxy.TReqCommitTransaction;
+import ru.yandex.yt.rpcproxy.TReqFreezeTable;
+import ru.yandex.yt.rpcproxy.TReqGetInSyncReplicas;
 import ru.yandex.yt.rpcproxy.TReqGetNode;
 import ru.yandex.yt.rpcproxy.TReqLookupRows;
 import ru.yandex.yt.rpcproxy.TReqModifyRows;
+import ru.yandex.yt.rpcproxy.TReqMountTable;
 import ru.yandex.yt.rpcproxy.TReqPingTransaction;
+import ru.yandex.yt.rpcproxy.TReqRemountTable;
 import ru.yandex.yt.rpcproxy.TReqSelectRows;
 import ru.yandex.yt.rpcproxy.TReqSetNode;
 import ru.yandex.yt.rpcproxy.TReqStartTransaction;
+import ru.yandex.yt.rpcproxy.TReqTrimTable;
+import ru.yandex.yt.rpcproxy.TReqUnfreezeTable;
+import ru.yandex.yt.rpcproxy.TReqUnmountTable;
 import ru.yandex.yt.rpcproxy.TReqVersionedLookupRows;
 import ru.yandex.yt.rpcproxy.TRspAbortTransaction;
 import ru.yandex.yt.rpcproxy.TRspCommitTransaction;
+import ru.yandex.yt.rpcproxy.TRspFreezeTable;
+import ru.yandex.yt.rpcproxy.TRspGetInSyncReplicas;
 import ru.yandex.yt.rpcproxy.TRspGetNode;
 import ru.yandex.yt.rpcproxy.TRspLookupRows;
 import ru.yandex.yt.rpcproxy.TRspModifyRows;
+import ru.yandex.yt.rpcproxy.TRspMountTable;
 import ru.yandex.yt.rpcproxy.TRspPingTransaction;
+import ru.yandex.yt.rpcproxy.TRspRemountTable;
 import ru.yandex.yt.rpcproxy.TRspSelectRows;
 import ru.yandex.yt.rpcproxy.TRspSetNode;
 import ru.yandex.yt.rpcproxy.TRspStartTransaction;
+import ru.yandex.yt.rpcproxy.TRspTrimTable;
+import ru.yandex.yt.rpcproxy.TRspUnfreezeTable;
+import ru.yandex.yt.rpcproxy.TRspUnmountTable;
 import ru.yandex.yt.rpcproxy.TRspVersionedLookupRows;
 import ru.yandex.yt.ytclient.misc.YtGuid;
 import ru.yandex.yt.ytclient.misc.YtTimestamp;
@@ -137,6 +153,7 @@ public class ApiServiceClient {
         return RpcUtil.apply(builder.invoke(), response -> null);
     }
 
+    /* nodes */
     public CompletableFuture<YTreeNode> getNode(String path) {
         RpcClientRequestBuilder<TReqGetNode.Builder, RpcClientResponse<TRspGetNode>> builder = service.getNode();
         builder.body().setPath(path);
@@ -146,8 +163,8 @@ public class ApiServiceClient {
     public CompletableFuture<Void> setNode(String path, byte[] data) {
         RpcClientRequestBuilder<TReqSetNode.Builder, RpcClientResponse<TRspSetNode>> builder = service.setNode();
         builder.body()
-            .setPath(path)
-            .setValue(ByteString.copyFrom(data));
+                .setPath(path)
+                .setValue(ByteString.copyFrom(data));
         return RpcUtil.apply(builder.invoke(), response -> null);
     }
 
@@ -155,6 +172,61 @@ public class ApiServiceClient {
         return setNode(path, data.toBinary());
     }
 
+    public CompletableFuture<Boolean> existsNode() {
+        CompletableFuture<Boolean> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<YTreeNode> listNode() {
+        CompletableFuture<YTreeNode> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<YtGuid> createNode() {
+        CompletableFuture<YtGuid> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<Void> removeNode() {
+        CompletableFuture<Void> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<Void> lockNode() {
+        CompletableFuture<Void> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<YtGuid> copyNode() {
+        CompletableFuture<YtGuid> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<YtGuid> moveNode() {
+        CompletableFuture<YtGuid> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<YtGuid> linkNode() {
+        CompletableFuture<YtGuid> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<Void> concatenateNodes() {
+        CompletableFuture<Void> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    /* */
     public CompletableFuture<UnversionedRowset> lookupRows(LookupRowsRequest request, YtTimestamp timestamp) {
         RpcClientRequestBuilder<TReqLookupRows.Builder, RpcClientResponse<TRspLookupRows>> builder =
                 service.lookupRows();
@@ -241,9 +313,106 @@ public class ApiServiceClient {
         return RpcUtil.apply(builder.invoke(), response -> null);
     }
 
+    public CompletableFuture<List<YtGuid>> getInSyncReplicas(String path, YtTimestamp timestamp) {
+        RpcClientRequestBuilder<TReqGetInSyncReplicas.Builder, RpcClientResponse<TRspGetInSyncReplicas>> builder =
+                service.getInSyncReplicas();
+
+        builder.body().setPath(path);
+        builder.body().setTimestamp(timestamp.getValue());
+
+        return RpcUtil.apply(builder.invoke(),
+                response ->
+                        response.body().getReplicaIdsList().stream().map(YtGuid::fromProto).collect(Collectors.toList()));
+    }
+
+    public CompletableFuture<List<YtGuid>> getInSyncReplicas(String path) {
+        return getInSyncReplicas(path, YtTimestamp.NULL);
+    }
+
+    /* tables */
+    public CompletableFuture<Void> mountTable(String path, YtGuid cellId, boolean freeze) {
+        RpcClientRequestBuilder<TReqMountTable.Builder, RpcClientResponse<TRspMountTable>> builder =
+                service.mountTable();
+
+        builder.body().setPath(path);
+        builder.body().setFreeze(freeze);
+        if (cellId != null) {
+            builder.body().setCellId(cellId.toProto());
+        }
+        return RpcUtil.apply(builder.invoke(), response -> null);
+    }
+
+    public CompletableFuture<Void> mountTable(String path) {
+        return mountTable(path, null, false);
+    }
+
+    public CompletableFuture<Void> mountTable(String path, boolean freeze) {
+        return mountTable(path, null, freeze);
+    }
+
+    public CompletableFuture<Void> unmountTable(String path, boolean force) {
+        RpcClientRequestBuilder<TReqUnmountTable.Builder, RpcClientResponse<TRspUnmountTable>> builder =
+                service.unmountTable();
+        builder.body().setPath(path);
+        builder.body().setForce(force);
+        return RpcUtil.apply(builder.invoke(), response -> null);
+    }
+
+    public CompletableFuture<Void> unmountTable(String path) {
+        return unmountTable(path, false);
+    }
+
+    public CompletableFuture<Void> remountTable(String path) {
+        RpcClientRequestBuilder<TReqRemountTable.Builder, RpcClientResponse<TRspRemountTable>> builder =
+                service.remountTable();
+        builder.body().setPath(path);
+        return RpcUtil.apply(builder.invoke(), response -> null);
+    }
+
+    public CompletableFuture<Void> freezeTable(String path) {
+        RpcClientRequestBuilder<TReqFreezeTable.Builder, RpcClientResponse<TRspFreezeTable>> builder =
+                service.freezeTable();
+        builder.body().setPath(path);
+        return RpcUtil.apply(builder.invoke(), response -> null);
+    }
+
+    public CompletableFuture<Void> unfreezeTable(String path) {
+        RpcClientRequestBuilder<TReqUnfreezeTable.Builder, RpcClientResponse<TRspUnfreezeTable>> builder =
+                service.unfreezeTable();
+        builder.body().setPath(path);
+        return RpcUtil.apply(builder.invoke(), response -> null);
+    }
+
+    public CompletableFuture<Void> reshardTable() {
+        CompletableFuture<Void> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<Void> trimTable(String path, int tableIndex, long trimmedRowCount) {
+        RpcClientRequestBuilder<TReqTrimTable.Builder, RpcClientResponse<TRspTrimTable>> builder =
+                service.trimTable();
+        builder.body().setPath(path);
+        builder.body().setTabletIndex(tableIndex);
+        builder.body().setTrimmedRowCount(trimmedRowCount);
+        return RpcUtil.apply(builder.invoke(), response -> null);
+    }
+
+    public CompletableFuture<Void> alterTable() {
+        CompletableFuture<Void> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+
+    public CompletableFuture<Void> alterTableReplica() {
+        CompletableFuture<Void> r = new CompletableFuture<>();
+        r.completeExceptionally(new RuntimeException("unimplemented"));
+        return r;
+    }
+    /* */
+
     private <T, Response> CompletableFuture<T> handleHeavyResponse(CompletableFuture<Response> future,
-            Function<Response, T> fn)
-    {
+                                                                   Function<Response, T> fn) {
         return RpcUtil.applyAsync(future, fn, heavyExecutor);
     }
 }
