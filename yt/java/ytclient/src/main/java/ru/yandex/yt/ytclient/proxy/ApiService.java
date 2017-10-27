d90a10cb6c74c99f397e27e6fc2506f1da88bae9
aozeritsky <aozeritsky@yandex-team.ru>
2017-10-27 16:38:36 +0300

    YT-7858; new requests; review:346987

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/ApiService.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/ApiService.java
@@ -1,24 +1,64 @@
 package ru.yandex.yt.ytclient.proxy;
 
 import ru.yandex.yt.rpcproxy.TReqAbortTransaction;
+import ru.yandex.yt.rpcproxy.TReqAlterTable;
+import ru.yandex.yt.rpcproxy.TReqAlterTableReplica;
 import ru.yandex.yt.rpcproxy.TReqCommitTransaction;
+import ru.yandex.yt.rpcproxy.TReqConcatenateNodes;
+import ru.yandex.yt.rpcproxy.TReqCopyNode;
+import ru.yandex.yt.rpcproxy.TReqCreateNode;
+import ru.yandex.yt.rpcproxy.TReqExistsNode;
+import ru.yandex.yt.rpcproxy.TReqFreezeTable;
+import ru.yandex.yt.rpcproxy.TReqGenerateTimestamps;
+import ru.yandex.yt.rpcproxy.TReqGetInSyncReplicas;
 import ru.yandex.yt.rpcproxy.TReqGetNode;
+import ru.yandex.yt.rpcproxy.TReqLinkNode;
+import ru.yandex.yt.rpcproxy.TReqListNode;
+import ru.yandex.yt.rpcproxy.TReqLockNode;
 import ru.yandex.yt.rpcproxy.TReqLookupRows;
 import ru.yandex.yt.rpcproxy.TReqModifyRows;
+import ru.yandex.yt.rpcproxy.TReqMountTable;
+import ru.yandex.yt.rpcproxy.TReqMoveNode;
 import ru.yandex.yt.rpcproxy.TReqPingTransaction;
+import ru.yandex.yt.rpcproxy.TReqRemountTable;
+import ru.yandex.yt.rpcproxy.TReqRemoveNode;
+import ru.yandex.yt.rpcproxy.TReqReshardTable;
 import ru.yandex.yt.rpcproxy.TReqSelectRows;
 import ru.yandex.yt.rpcproxy.TReqSetNode;
 import ru.yandex.yt.rpcproxy.TReqStartTransaction;
+import ru.yandex.yt.rpcproxy.TReqTrimTable;
+import ru.yandex.yt.rpcproxy.TReqUnfreezeTable;
+import ru.yandex.yt.rpcproxy.TReqUnmountTable;
 import ru.yandex.yt.rpcproxy.TReqVersionedLookupRows;
 import ru.yandex.yt.rpcproxy.TRspAbortTransaction;
+import ru.yandex.yt.rpcproxy.TRspAlterTable;
+import ru.yandex.yt.rpcproxy.TRspAlterTableReplica;
 import ru.yandex.yt.rpcproxy.TRspCommitTransaction;
+import ru.yandex.yt.rpcproxy.TRspConcatenateNodes;
+import ru.yandex.yt.rpcproxy.TRspCopyNode;
+import ru.yandex.yt.rpcproxy.TRspCreateNode;
+import ru.yandex.yt.rpcproxy.TRspExistsNode;
+import ru.yandex.yt.rpcproxy.TRspFreezeTable;
+import ru.yandex.yt.rpcproxy.TRspGenerateTimestamps;
+import ru.yandex.yt.rpcproxy.TRspGetInSyncReplicas;
 import ru.yandex.yt.rpcproxy.TRspGetNode;
+import ru.yandex.yt.rpcproxy.TRspLinkNode;
+import ru.yandex.yt.rpcproxy.TRspListNode;
+import ru.yandex.yt.rpcproxy.TRspLockNode;
 import ru.yandex.yt.rpcproxy.TRspLookupRows;
 import ru.yandex.yt.rpcproxy.TRspModifyRows;
+import ru.yandex.yt.rpcproxy.TRspMountTable;
+import ru.yandex.yt.rpcproxy.TRspMoveNode;
 import ru.yandex.yt.rpcproxy.TRspPingTransaction;
+import ru.yandex.yt.rpcproxy.TRspRemountTable;
+import ru.yandex.yt.rpcproxy.TRspRemoveNode;
+import ru.yandex.yt.rpcproxy.TRspReshardTable;
 import ru.yandex.yt.rpcproxy.TRspSelectRows;
 import ru.yandex.yt.rpcproxy.TRspSetNode;
 import ru.yandex.yt.rpcproxy.TRspStartTransaction;
+import ru.yandex.yt.rpcproxy.TRspTrimTable;
+import ru.yandex.yt.rpcproxy.TRspUnfreezeTable;
+import ru.yandex.yt.rpcproxy.TRspUnmountTable;
 import ru.yandex.yt.rpcproxy.TRspVersionedLookupRows;
 import ru.yandex.yt.ytclient.rpc.DiscoverableRpcService;
 import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
@@ -38,10 +78,6 @@ public interface ApiService extends DiscoverableRpcService {
 
     RpcClientRequestBuilder<TReqAbortTransaction.Builder, RpcClientResponse<TRspAbortTransaction>> abortTransaction();
 
-    RpcClientRequestBuilder<TReqGetNode.Builder, RpcClientResponse<TRspGetNode>> getNode();
-
-    RpcClientRequestBuilder<TReqSetNode.Builder, RpcClientResponse<TRspSetNode>> setNode();
-
     RpcClientRequestBuilder<TReqLookupRows.Builder, RpcClientResponse<TRspLookupRows>> lookupRows();
 
     RpcClientRequestBuilder<TReqVersionedLookupRows.Builder, RpcClientResponse<TRspVersionedLookupRows>> versionedLookupRows();
@@ -49,4 +85,51 @@ public interface ApiService extends DiscoverableRpcService {
     RpcClientRequestBuilder<TReqSelectRows.Builder, RpcClientResponse<TRspSelectRows>> selectRows();
 
     RpcClientRequestBuilder<TReqModifyRows.Builder, RpcClientResponse<TRspModifyRows>> modifyRows();
+
+    RpcClientRequestBuilder<TReqGetInSyncReplicas.Builder, RpcClientResponse<TRspGetInSyncReplicas>> getInSyncReplicas();
+
+    RpcClientRequestBuilder<TReqGenerateTimestamps.Builder, RpcClientResponse<TRspGenerateTimestamps>> generateTimestamps();
+
+    /* nodes */
+    RpcClientRequestBuilder<TReqGetNode.Builder, RpcClientResponse<TRspGetNode>> getNode();
+
+    RpcClientRequestBuilder<TReqSetNode.Builder, RpcClientResponse<TRspSetNode>> setNode();
+
+    RpcClientRequestBuilder<TReqExistsNode.Builder, RpcClientResponse<TRspExistsNode>> existsNode();
+
+    RpcClientRequestBuilder<TReqListNode.Builder, RpcClientResponse<TRspListNode>> listNode();
+
+    RpcClientRequestBuilder<TReqCreateNode.Builder, RpcClientResponse<TRspCreateNode>> createNode();
+
+    RpcClientRequestBuilder<TReqRemoveNode.Builder, RpcClientResponse<TRspRemoveNode>> removeNode();
+
+    RpcClientRequestBuilder<TReqLockNode.Builder, RpcClientResponse<TRspLockNode>> lockNode();
+
+    RpcClientRequestBuilder<TReqCopyNode.Builder, RpcClientResponse<TRspCopyNode>> copyNode();
+
+    RpcClientRequestBuilder<TReqMoveNode.Builder, RpcClientResponse<TRspMoveNode>> moveNode();
+
+    RpcClientRequestBuilder<TReqLinkNode.Builder, RpcClientResponse<TRspLinkNode>> linkNode();
+
+    RpcClientRequestBuilder<TReqConcatenateNodes.Builder, RpcClientResponse<TRspConcatenateNodes>> concatenateNodes();
+
+    /* tables */
+    RpcClientRequestBuilder<TReqMountTable.Builder, RpcClientResponse<TRspMountTable>> mountTable();
+
+    RpcClientRequestBuilder<TReqUnmountTable.Builder, RpcClientResponse<TRspUnmountTable>> unmountTable();
+
+    RpcClientRequestBuilder<TReqRemountTable.Builder, RpcClientResponse<TRspRemountTable>> remountTable();
+
+    RpcClientRequestBuilder<TReqFreezeTable.Builder, RpcClientResponse<TRspFreezeTable>> freezeTable();
+
+    RpcClientRequestBuilder<TReqUnfreezeTable.Builder, RpcClientResponse<TRspUnfreezeTable>> unfreezeTable();
+
+    RpcClientRequestBuilder<TReqReshardTable.Builder, RpcClientResponse<TRspReshardTable>> reshardTable();
+
+    RpcClientRequestBuilder<TReqTrimTable.Builder, RpcClientResponse<TRspTrimTable>> trimTable();
+
+    RpcClientRequestBuilder<TReqAlterTable.Builder, RpcClientResponse<TRspAlterTable>> alterTable();
+
+    RpcClientRequestBuilder<TReqAlterTableReplica.Builder, RpcClientResponse<TRspAlterTableReplica>> alterTableReplica();
+    /* */
 }
