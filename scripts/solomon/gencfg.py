262bf54cb1a65de04f8864cb8ef59af8b9b5c859
Grigory Demchenko <gridem@yandex-team.ru>
2017-11-07 18:05:54 +0300

    Update unlink functionality

--- scripts/solomon/gencfg.py
+++ scripts/solomon/gencfg.py
@@ -312,7 +312,7 @@ class Resource(object):
     def create(self, dry_run):
         logging.debug("Creating resource '%s'%s", self.api_url_create, " (dry run)" if dry_run else "")
         if dry_run:
-            self.view_diff()
+            view_object("Create object %s %s" % (self.api_url_create, self.id), self.local)
             return
         rsp = requests.post(self.api_url_create, headers=self.headers, json=self._local_data)
         if not rsp.ok:
@@ -528,22 +528,26 @@ def update_cluster_services(token, cluster, type, yes):
 @cli.command()
 @click.option("--token", required=True)
 @click.option("--cluster", required=True)
+@click.option("--type", multiple=True)
 @click.option("--yes", is_flag=True)
-def unlink_cluster(token, cluster, yes):
+def unlink_cluster(token, cluster, type, yes):
     solomon_cluster = normalize_cluster_id(cluster)
     cluster_services = get_cluster_services()
+    types = get_cluster_types() if not type else type
 
-    resource = Resource("/projects/yt/clusters/%s" % solomon_cluster, "services", token)
-    resource.load()
+    for type in types:
+        solomon_cluster_type = get_solomon_cluster_type(solomon_cluster, type)
+        resource = Resource("/projects/yt/clusters/%s" % solomon_cluster_type, "services", token)
+        resource.load()
 
-    for item in resource.local:
-        if item["id"] not in cluster_services:
-            continue
-        shard_id = item["shardId"]
-        if yes:
-            Resource("/projects/yt/shards", shard_id, token).delete()
-        else:
-            view_object("Deleting shard '%s'" % shard_id, item)
+        for item in resource.local:
+            if item["id"] not in cluster_services:
+                continue
+            shard_id = item["shardId"]
+            if yes:
+                Resource("/projects/yt/shards", shard_id, token).delete()
+            else:
+                view_object("Deleting shard '%s'" % shard_id, item)
 
 
 @cli.command()
