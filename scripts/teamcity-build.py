5a66c4d8ce2635452f35a3f151ccd86be7094098
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2017-11-17 19:05:40 +0300

    Do not build YP by default

--- scripts/teamcity-build.py
+++ scripts/teamcity-build.py
@@ -155,6 +155,7 @@ def configure(options, build_context):
         "-DYT_BUILD_ENABLE_EXPERIMENTS:BOOL=ON",
         "-DYT_BUILD_ENABLE_TESTS:BOOL=ON",
         "-DYT_BUILD_ENABLE_GDB_INDEX:BOOL=ON",
+        "-DYT_BUILD_ENABLE_YP:BOOL=ON",
         "-DYT_BUILD_BRANCH={0}".format(options.branch),
         "-DYT_BUILD_NUMBER={0}".format(options.build_number),
         "-DYT_BUILD_VCS_NUMBER={0}".format(options.build_vcs_number[0:7]),
