7823378d548f51d8e23869c2716e6c464f39a3da
levysotsky <levysotsky@yandex-team.ru>
2017-11-20 12:39:23 +0300

    Fix tests to avoid timeouts Pull-request for branch users/levysotsky/fix_tests

--- mapreduce/yt/tests/check_initialized/test_check_initialized.py
+++ mapreduce/yt/tests/check_initialized/test_check_initialized.py
@@ -12,19 +12,18 @@ TEST_PROGRAM = yatest.common.binary_path('mapreduce/yt/tests/check_initialized/t
 
 def test_check_initialized(yt_stuff):
     yt_wrapper = yt_stuff.get_yt_wrapper()
-    for initialize in ['true', 'false']:
-        try:
-            process = yatest.common.execute(
-                [TEST_PROGRAM],
-                check_exit_code=True,
-                collect_cores=False,
-                env={
-                    'YT_LOG_LEVEL': 'DEBUG',
-                    'MR_RUNTIME': 'YT',
-                    'YT_PROXY': yt_stuff.get_server(),
-                    'INITIALIZE': initialize
-                },
-            )
-            assert initialize == 'true'
-        except Exception as e:
-            assert initialize == 'false' and 'NYT::Initialize() must be called prior to any operation' in str(e)
+    try:
+        process = yatest.common.execute(
+            [TEST_PROGRAM],
+            check_exit_code=True,
+            collect_cores=False,
+            env={
+                'YT_LOG_LEVEL': 'DEBUG',
+                'MR_RUNTIME': 'YT',
+                'YT_PROXY': yt_stuff.get_server(),
+                'INITIALIZE': 'false'
+            },
+        )
+        assert False, "The program must have been crashed"
+    except Exception as e:
+        assert 'NYT::Initialize() must be called prior to any operation' in str(e), "This exception must have been raised"
