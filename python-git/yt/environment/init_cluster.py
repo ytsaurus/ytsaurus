2d7d3abcd7564b82617b6f266fa605c932712c4b
Ruslan Savchenko <savrus@ytsaurus.tech>
2017-11-24 13:07:26 +0300

    Fix 'sys' tablet cell bundle initialization

--- python-git/yt/environment/init_cluster.py
+++ python-git/yt/environment/init_cluster.py
@@ -233,8 +233,7 @@ def initialize_world(client=None, idm=None, proxy_address=None, ui_address=None)
     if not client.exists("//sys/tablet_cell_bundles/sys"):
         client.create("tablet_cell_bundle", attributes={
             "name": "sys",
-            "changelog_account": "sys",
-            "snapshot_account": "sys"})
+            "options": {"changelog_account": "sys", "snapshot_account": "sys"}})
     else:
         logger.warning('Tablet cell bundle "sys" already exists')
 
