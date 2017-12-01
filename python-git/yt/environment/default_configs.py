c3b5f43d23467c7b3b5df6c57bbff45a34e674a7
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-12-01 13:44:13 +0300

    Add small default for exec_nodes_request_period

--- python-git/yt/environment/default_configs.py
+++ python-git/yt/environment/default_configs.py
@@ -145,6 +145,7 @@ b"""
         watchers_update_period = 100;
         nodes_attributes_update_period = 100;
         update_exec_node_descriptors_period = 100;
+        exec_nodes_request_period = 100;
         scheduling_tag_filter_expire_timeout = 100;
         node_shard_exec_nodes_cache_update_period = 100;
         chunk_list_release_batch_delay = 100;
