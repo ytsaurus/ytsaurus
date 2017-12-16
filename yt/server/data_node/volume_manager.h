a0e2cc246d71686aab1f77a7f99c2e9836dc9d92
Maxim Babenko <babenko@ytsaurus.tech>
2017-12-16 19:19:10 +0700

    Fix MacOS build

--- yt/server/data_node/volume_manager.h
+++ yt/server/data_node/volume_manager.h
@@ -12,7 +12,7 @@ namespace NDataNode {
 ////////////////////////////////////////////////////////////////////////////////
 
 struct IVolume
-    : public TRefCounted
+    : public virtual TRefCounted
 {
     virtual const TString& GetPath() const = 0;
 };
@@ -24,16 +24,18 @@ DEFINE_REFCOUNTED_TYPE(IVolume)
 //! Creates volumes from different layers.
 //! Useful for creation and reuse of rootfs volumes.
 struct IVolumeManager
-    : public TRefCounted
+    : public virtual TRefCounted
 {
     virtual TFuture<IVolumePtr> PrepareVolume(const std::vector<TArtifactKey>& layers) = 0;
 };
 
 DEFINE_REFCOUNTED_TYPE(IVolumeManager)
 
+#ifdef __linux__
 IVolumeManagerPtr CreatePortoVolumeManager(
     const TVolumeManagerConfigPtr& config,
     const NCellNode::TBootstrap* bootstrap);
+#endif
 
 ////////////////////////////////////////////////////////////////////////////////
 
