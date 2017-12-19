ab4f43138a8538093d7dec23e2249e109ae2e669
ignat <ignat@ytsaurus.tech>
2017-12-19 13:45:10 +0300

    Fix test_with_config

--- mapreduce/yt/python/test/test_with_config.py
+++ mapreduce/yt/python/test/test_with_config.py
@@ -1,9 +1,7 @@
-import sys
 import pytest
 import yatest.common
 
-from mapreduce.yt.python.yt_stuff import YtStuff, YtConfig
-from mapreduce.yt.python.yt_stuff import yt_stuff
+from mapreduce.yt.python.yt_stuff import YtStuff, YtConfig, yt_stuff
 
 from os import environ
 environ["YT_STUFF_MAX_START_RETRIES"] = "2"
@@ -15,9 +13,7 @@ def yt_config(request):
     return YtConfig(
         yt_id=TEST_ID,
         wait_tablet_cell_initialization=True,
-        operations_memory_limit=4 * 1024 * 1024 * 1024,
     )
-
 
 def test_fixture(yt_stuff):
     assert yt_stuff.config.yt_id == TEST_ID
