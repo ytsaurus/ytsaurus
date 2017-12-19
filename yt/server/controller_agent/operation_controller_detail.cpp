a506837fb5789208b7a894881ea64ddeda54d734
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-12-19 21:12:53 +0300

    YT-8114: Disconnect if snapshot remove failed

--- yt/server/controller_agent/operation_controller_detail.cpp
+++ yt/server/controller_agent/operation_controller_detail.cpp
@@ -392,9 +392,7 @@ void TOperationControllerBase::InitializeReviving(TControllerTransactionsPtr con
 
         Snapshot = TOperationSnapshot();
         auto error = WaitFor(MasterConnector->RemoveSnapshot(OperationId));
-        if (!error.IsOK()) {
-            LOG_WARNING(error, "Failed to remove snapshot");
-        }
+        YCHECK(error.IsOK() && "Failed to remove snapshot");
 
         InitializeTransactions();
         InitializeStructures();
