2a55e7077e1baed7f67f4cc21745c420a00a7552
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2017-12-20 11:20:12 +0300

    Set acl first in node update

--- yt/server/scheduler/master_connector.cpp
+++ yt/server/scheduler/master_connector.cpp
@@ -1183,6 +1183,32 @@ private:
 
         auto paths = GetCompatibilityOperationPaths(operation->GetId(), operation->GetStorageMode());
         for (const auto& operationPath : paths) {
+            // Set operation acl.
+            {
+                auto aclBatchReq = StartObjectBatchRequest();
+                auto req = TYPathProxy::Set(operationPath + "/@acl");
+                req->set_value(BuildYsonStringFluently()
+                    .Do(BIND(&TImpl::BuildOperationAcl, Unretained(this), operation))
+                    .GetData());
+                aclBatchReq->AddRequest(req, "set_acl");
+
+                auto aclBatchRspOrError = WaitFor(aclBatchReq->Invoke());
+                THROW_ERROR_EXCEPTION_IF_FAILED(aclBatchRspOrError);
+
+                auto rsp = aclBatchRspOrError.Value()->GetResponse("set_acl");
+                if (!rsp.IsOK()) {
+                    auto error = TError("Failed to set operation acl")
+                        << TErrorAttribute("operation_id", operation->GetId())
+                        << rsp;
+                    operation->MutableAlerts()[EOperationAlertType::InvalidAcl] = error;
+                    LOG_INFO(error);
+                } else {
+                    if (!operation->Alerts()[EOperationAlertType::InvalidAcl].IsOK()) {
+                        operation->MutableAlerts()[EOperationAlertType::InvalidAcl] = TError();
+                    }
+                }
+            }
+
             // Set suspended flag.
             {
                 auto req = TYPathProxy::Set(operationPath + "/@suspended");
@@ -1236,32 +1262,6 @@ private:
                         })
                     .GetData());
                 batchReq->AddRequest(req, "update_op_node");
-            }
-
-            // Set operation acl.
-            {
-                auto aclBatchReq = StartObjectBatchRequest();
-                auto req = TYPathProxy::Set(operationPath + "/@acl");
-                req->set_value(BuildYsonStringFluently()
-                    .Do(BIND(&TImpl::BuildOperationAcl, Unretained(this), operation))
-                    .GetData());
-                aclBatchReq->AddRequest(req, "set_acl");
-
-                auto aclBatchRspOrError = WaitFor(aclBatchReq->Invoke());
-                THROW_ERROR_EXCEPTION_IF_FAILED(aclBatchRspOrError);
-
-                auto rsp = aclBatchRspOrError.Value()->GetResponse("set_acl");
-                if (!rsp.IsOK()) {
-                    auto error = TError("Failed to set operation acl")
-                        << TErrorAttribute("operation_id", operation->GetId())
-                        << rsp;
-                    operation->MutableAlerts()[EOperationAlertType::InvalidAcl] = error;
-                    LOG_INFO(error);
-                } else {
-                    if (!operation->Alerts()[EOperationAlertType::InvalidAcl].IsOK()) {
-                        operation->MutableAlerts()[EOperationAlertType::InvalidAcl] = TError();
-                    }
-                }
             }
         }
 
