790a02120ae92d2c0ca277a2f2bc654713273485
Alexander Tiunov <astiunov@yandex-team.ru>
2017-12-22 00:08:41 +0300

    update proto version

--- yt/ytlib/job_tracker_client/job_tracker_service_proxy.h
+++ yt/ytlib/job_tracker_client/job_tracker_service_proxy.h
@@ -16,7 +16,7 @@ class TJobTrackerServiceProxy
 {
 public:
     DEFINE_RPC_PROXY(TJobTrackerServiceProxy, RPC_PROXY_DESC(JobTrackerService)
-        .SetProtocolVersion(14));
+        .SetProtocolVersion(15));
 
     DEFINE_RPC_PROXY_METHOD(NProto, Heartbeat);
 };
