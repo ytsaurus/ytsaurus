588c97e2b0a64301166940998b616ae2c48e6a91
Maxim Babenko <babenko@ytsaurus.tech>
2017-12-27 17:10:18 +0300

    Move forward declaration to scheduler/public.h

--- yt/ytlib/table_client/helpers.h
+++ yt/ytlib/table_client/helpers.h
@@ -10,6 +10,8 @@
 
 #include <yt/ytlib/cypress_client/public.h>
 
+#include <yt/ytlib/scheduler/public.h>
+
 #include <yt/core/yson/lexer.h>
 #include <yt/core/yson/public.h>
 
@@ -20,15 +22,6 @@
 #include <yt/core/misc/phoenix.h>
 
 namespace NYT {
-
-namespace NScheduler {
-namespace NProto {
-
-class TOutputResult;
-
-} // namespace NProto
-} // namespace NScheduler
-
 namespace NTableClient {
 
 ////////////////////////////////////////////////////////////////////////////////
