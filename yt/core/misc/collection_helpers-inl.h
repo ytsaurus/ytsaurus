e2bc16fa3c76d8753c95c1a1eda8cc37733823cd
Maxim Babenko <babenko@ytsaurus.tech>
2017-12-27 16:56:18 +0300

    Move MakeArray to collection_helpers.h

--- yt/core/misc/collection_helpers-inl.h
+++ yt/core/misc/collection_helpers-inl.h
@@ -107,4 +107,44 @@ void MergeFrom(TTarget* target, const TSource& source)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+template <size_t Index, class... Ts>
+struct TMakeArrayTraits;
+
+template <size_t Index>
+struct TMakeArrayTraits<Index>
+{
+    template <class V>
+    static void Do(V*)
+    { }
+};
+
+template <size_t Index, class T, class... Ts>
+struct TMakeArrayTraits<Index, T, Ts...>
+{
+    template <class V>
+    static void Do(V* array, const T& head, const Ts&... tail)
+    {
+        (*array)[Index] = head;
+        TMakeArrayTraits<Index + 1, Ts...>::Do(array, tail...);
+    }
+};
+
+template <class... Ts>
+auto MakeArray(
+    const Ts&... values)
+-> std::array<std::tuple_element_t<0, std::tuple<Ts...>>, sizeof...(Ts)>
+{
+    std::array<std::tuple_element_t<0, std::tuple<Ts...>>, sizeof...(Ts)> array;
+    TMakeArrayTraits<0, Ts...>::Do(&array, values...);
+    return array;
+}
+
+template <class T>
+std::array<T, 0> MakeArray()
+{
+    return std::array<T, 0>();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 } // namespace NYT
