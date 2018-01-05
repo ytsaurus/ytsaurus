8c64603eda0226023b5e5347584cd5b9189337a1
Maxim Babenko <babenko@ytsaurus.tech>
2018-01-05 11:54:12 +0300

    Rename controller_agent_operation_service -> controller_agent_service

--- yt/server/controller_agent/controller_agent_service.cpp
+++ yt/server/controller_agent/controller_agent_service.cpp
@@ -1,46 +1,30 @@
-#include "private.h"
-
+#include "controller_agent_service.h"
 #include "controller_agent.h"
+#include "private.h"
 
 #include <yt/server/cell_scheduler/bootstrap.h>
 
-#include <yt/ytlib/cypress_client/rpc_helpers.h>
+#include <yt/ytlib/scheduler/controller_agent_service_proxy.h>
 
-#include <yt/ytlib/job_tracker_client/job_spec_service_proxy.h>
-
-#include <yt/ytlib/scheduler/helpers.h>
-#include <yt/ytlib/scheduler/controller_agent_operation_service_proxy.h>
-
-#include <yt/ytlib/api/native_client.h>
-
-#include <yt/core/rpc/response_keeper.h>
 #include <yt/core/rpc/service_detail.h>
-
-#include <yt/core/ytree/permission.h>
 
 namespace NYT {
 namespace NControllerAgent {
 
 using namespace NRpc;
 using namespace NCellScheduler;
-using namespace NApi;
-using namespace NYTree;
-using namespace NYson;
-using namespace NCypressClient;
-using namespace NConcurrency;
-using namespace NJobTrackerClient;
 
 ////////////////////////////////////////////////////////////////////
 
-class TControllerAgentOperationService
+class TControllerAgentService
     : public TServiceBase
 {
 public:
-    explicit TControllerAgentOperationService(TBootstrap* bootstrap)
+    explicit TControllerAgentService(TBootstrap* bootstrap)
         : TServiceBase(
             // TODO(babenko): better queue
             bootstrap->GetControlInvoker(EControlQueue::Default),
-            NScheduler::TControllerAgentOperationServiceProxy::GetDescriptor(),
+            NScheduler::TControllerAgentServiceProxy::GetDescriptor(),
             ControllerAgentLogger)
         , Bootstrap_(bootstrap)
     {
@@ -58,7 +42,8 @@ private:
 
         auto operationId = FromProto<TOperationId>(request->operation_id());
 
-        context->SetRequestInfo("OperationId: %v", operationId);
+        context->SetRequestInfo("OperationId: %v",
+            operationId);
 
         controllerAgent->BuildOperationInfo(operationId, response);
 
@@ -73,7 +58,9 @@ private:
         auto operationId = FromProto<TOperationId>(request->operation_id());
         auto jobId = FromProto<TJobId>(request->job_id());
 
-        context->SetRequestInfo("OperationId: %v, JobId: %v", operationId, jobId);
+        context->SetRequestInfo("OperationId: %v, JobId: %v",
+            operationId,
+            jobId);
 
         response->set_info(controllerAgent->BuildJobInfo(operationId, jobId).GetData());
 
@@ -81,11 +68,11 @@ private:
     }
 };
 
-DEFINE_REFCOUNTED_TYPE(TControllerAgentOperationService)
+DEFINE_REFCOUNTED_TYPE(TControllerAgentService)
 
-IServicePtr CreateControllerAgentOperationService(TBootstrap* bootstrap)
+IServicePtr CreateControllerAgentService(TBootstrap* bootstrap)
 {
-    return New<TControllerAgentOperationService>(bootstrap);
+    return New<TControllerAgentService>(bootstrap);
 }
 
 ////////////////////////////////////////////////////////////////////
