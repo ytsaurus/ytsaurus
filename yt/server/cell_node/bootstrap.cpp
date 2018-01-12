ad04b49b293813c605d5f3a1bf3e7e1f619f38a8
Maxim Akhmedov <max42@ytsaurus.tech>
2018-01-12 04:44:00 +0300

    Move some logic from peer block manager to data node service, remove redundant orchid service.

--- yt/server/cell_node/bootstrap.cpp
+++ yt/server/cell_node/bootstrap.cpp
@@ -549,10 +549,6 @@ void TBootstrap::DoRun()
         CreateVirtualNode(TabletSlotManager->GetOrchidService()));
     SetNodeByYPath(
         OrchidRoot,
-        "/chunk_blocks",
-        CreateVirtualNode(ChunkBlockManager->GetOrchidService()));
-    SetNodeByYPath(
-        OrchidRoot,
         "/job_controller",
         CreateVirtualNode(JobController->GetOrchidService()
             ->Via(GetControlInvoker())));
--- yt/server/data_node/chunk_block_manager.cpp
+++ yt/server/data_node/chunk_block_manager.cpp
@@ -5,7 +5,6 @@
 #include "chunk_registry.h"
 #include "config.h"
 #include "location.h"
-#include "peer_block_distributor.h"
 
 #include <yt/server/cell_node/bootstrap.h>
 
@@ -19,11 +18,6 @@
 
 #include <yt/core/concurrency/thread_affinity.h>
 
-#include <yt/core/ytree/fluent.h>
-#include <yt/core/ytree/ypath_service.h>
-
-#include <util/random/random.h>
-
 namespace NYT {
 namespace NDataNode {
 
@@ -31,9 +25,6 @@ using namespace NObjectClient;
 using namespace NChunkClient;
 using namespace NNodeTrackerClient;
 using namespace NCellNode;
-using namespace NConcurrency;
-using namespace NYTree;
-using namespace NYson;
 
 using NChunkClient::NProto::TChunkMeta;
 using NChunkClient::NProto::TBlocksExt;
@@ -41,7 +32,6 @@ using NChunkClient::NProto::TBlocksExt;
 ////////////////////////////////////////////////////////////////////////////////
 
 static const auto& Logger = DataNodeLogger;
-static const auto& Profiler = DataNodeProfiler;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -71,7 +61,6 @@ public:
                 FormatEnum(EBlockType::CompressedData)))
         , Config_(config)
         , Bootstrap_(bootstrap)
-        , OrchidService_(IYPathService::FromProducer(BIND(&TImpl::BuildOrchid, MakeWeak(this))))
     { }
 
     TCachedBlockPtr FindCachedBlock(const TBlockId& blockId)
@@ -143,17 +132,6 @@ public:
     {
         VERIFY_THREAD_AFFINITY_ANY();
 
-        auto updateRecentlyReadBlockQueueCallback = BIND(
-            &TImpl::UpdateRecentlyReadBlockQueue,
-            MakeStrong(this),
-            chunkId,
-            blockIndexes);
-        auto updatePeerBlockDistributor = BIND(
-            &TImpl::UpdatePeerBlockDistributor,
-            MakeStrong(this),
-            chunkId,
-            blockIndexes);
-
         try {
             auto chunkRegistry = Bootstrap_->GetChunkRegistry();
             auto chunk = chunkRegistry->FindChunk(chunkId);
@@ -173,9 +151,7 @@ public:
                         blocks.push_back(block);
                     }
                 }
-                return MakeFuture(blocks)
-                    .Apply(updateRecentlyReadBlockQueueCallback)
-                    .Apply(updatePeerBlockDistributor);
+                return MakeFuture(blocks);
             }
 
             auto readGuard = TChunkReadGuard::AcquireOrThrow(chunk);
@@ -185,51 +161,15 @@ public:
             auto asyncResult = asyncBlocks
                 .Apply(BIND(&TImpl::OnBlocksRead, Passed(std::move(readGuard))));
 
-            if (type == EObjectType::Chunk || type == EObjectType::ErasureChunk) {
-                asyncResult = asyncResult
-                    .Apply(updateRecentlyReadBlockQueueCallback)
-                    .Apply(updatePeerBlockDistributor);
-            }
-
             return asyncResult;
         } catch (const std::exception& ex) {
             return MakeFuture<std::vector<TBlock>>(TError(ex));
         }
     }
 
-    struct TReadBlock
-    {
-        TBlockId BlockId;
-        i64 Size;
-        TInstant Time;
-        EBlockOrigin BlockOrigin;
-    };
-
-    std::vector<TReadBlock> GetRecentlyReadBlocks() const
-    {
-        // Note that after acquiring this lock we are effectively blocking the ReadBlockSet requests.
-        TReaderGuard guard(RecentlyReadBlockQueueLock_);
-
-        const auto& Profiler = NDataNode::Profiler;
-        PROFILE_TIMING("/recently_read_block_request_time") {
-            return std::vector<TReadBlock>(RecentlyReadBlockQueue_.begin(), RecentlyReadBlockQueue_.end());
-        }
-    }
-
-    IYPathServicePtr GetOrchidService()
-    {
-        return OrchidService_;
-    }
-
 private:
     const TDataNodeConfigPtr Config_;
     TBootstrap* const Bootstrap_;
-
-    IYPathServicePtr OrchidService_;
-
-    TReaderWriterSpinLock RecentlyReadBlockQueueLock_;
-    // This queue stores pairs <blockId, blockSize>.
-    std::deque<TReadBlock> RecentlyReadBlockQueue_;
 
     virtual i64 GetWeight(const TCachedBlockPtr& block) const override
     {
@@ -243,72 +183,6 @@ private:
         const std::vector<TBlock>& blocks)
     {
         return blocks;
-    }
-
-    std::vector<TBlock> UpdateRecentlyReadBlockQueue(
-        const TChunkId& chunkId,
-        const std::vector<int>& blockIndexes,
-        const std::vector<TBlock>& blocks)
-    {
-        std::vector<TReadBlock> readBlocks;
-        auto now = NProfiling::GetInstant();
-        // NB: blocks may contain less elements than blockIndexes.
-        for (int listIndex = 0; listIndex < std::min(blockIndexes.size(), blocks.size()); ++listIndex) {
-            const auto& block = blocks[listIndex];
-            if (block && RandomNumber<double>() < Config_->RecentlyReadBlockQueueSampleRate) {
-                int blockIndex = blockIndexes[listIndex];
-                readBlocks.emplace_back(TReadBlock{
-                    TBlockId(chunkId, blockIndex),
-                    static_cast<i64>(block.Size()),
-                    now,
-                    block.BlockOrigin});
-            }
-        }
-
-        if (!readBlocks.empty()) {
-            TWriterGuard guard(RecentlyReadBlockQueueLock_);
-            RecentlyReadBlockQueue_.insert(RecentlyReadBlockQueue_.end(), readBlocks.begin(), readBlocks.end());
-            if (RecentlyReadBlockQueue_.size() > Config_->RecentlyReadBlockQueueSize) {
-                RecentlyReadBlockQueue_.erase(
-                    RecentlyReadBlockQueue_.begin(),
-                    RecentlyReadBlockQueue_.end() - Config_->RecentlyReadBlockQueueSize);
-            }
-        }
-
-        return blocks;
-    }
-
-    std::vector<TBlock> UpdatePeerBlockDistributor(
-        const TChunkId& chunkId,
-        const std::vector<int>& blockIndexes,
-        const std::vector<TBlock>& blocks)
-    {
-        for (int listIndex = 0; listIndex < std::min(blockIndexes.size(), blocks.size()); ++listIndex) {
-            int blockIndex = blockIndexes[listIndex];
-            const auto& block = blocks[listIndex];
-            if (block) {
-                Bootstrap_->GetPeerBlockDistributor()->OnBlockRequested(TBlockId{chunkId, blockIndex}, block.Size());
-            }
-        }
-        return blocks;
-    }
-
-    void BuildOrchid(IYsonConsumer* consumer) const
-    {
-        BuildYsonFluently(consumer)
-            .BeginMap()
-                .Item("recently_read_blocks")
-                    .DoListFor(GetRecentlyReadBlocks(), [] (TFluentList fluent, const TReadBlock& readBlock) {
-                        fluent.Item()
-                            .BeginMap()
-                                .Item("chunk_id").Value(readBlock.BlockId.ChunkId)
-                                .Item("block_index").Value(readBlock.BlockId.BlockIndex)
-                                .Item("size").Value(readBlock.Size)
-                                .Item("time").Value(readBlock.Time)
-                                .Item("block_origin").Value(readBlock.BlockOrigin)
-                            .EndMap();
-                    })
-            .EndMap();
     }
 };
 
@@ -368,11 +242,6 @@ TFuture<std::vector<TBlock>> TChunkBlockManager::ReadBlockSet(
 std::vector<TCachedBlockPtr> TChunkBlockManager::GetAllBlocks() const
 {
     return Impl_->GetAll();
-}
-
-IYPathServicePtr TChunkBlockManager::GetOrchidService()
-{
-    return Impl_->GetOrchidService();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
