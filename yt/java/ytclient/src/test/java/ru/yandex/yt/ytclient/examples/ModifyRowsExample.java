f6883905650d2c13da41f72d2e6c4297307dd7c3
aozeritsky <aozeritsky@yandex-team.ru>
2018-01-24 19:17:31 +0300

    ModifyRowsExample

--- yt/java/ytclient/src/test/java/ru/yandex/yt/ytclient/examples/ModifyRowsExample.java
+++ yt/java/ytclient/src/test/java/ru/yandex/yt/ytclient/examples/ModifyRowsExample.java
@@ -1,6 +1,7 @@
 package ru.yandex.yt.ytclient.examples;
 
 import java.util.Arrays;
+import java.util.UUID;
 
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
@@ -8,19 +9,27 @@ import org.slf4j.LoggerFactory;
 import ru.yandex.yt.rpcproxy.ETransactionType;
 import ru.yandex.yt.ytclient.proxy.ApiServiceTransaction;
 import ru.yandex.yt.ytclient.proxy.ApiServiceTransactionOptions;
+import ru.yandex.yt.ytclient.proxy.LookupRowsRequest;
 import ru.yandex.yt.ytclient.proxy.ModifyRowsRequest;
+import ru.yandex.yt.ytclient.tables.ColumnSchema;
 import ru.yandex.yt.ytclient.tables.ColumnValueType;
 import ru.yandex.yt.ytclient.tables.TableSchema;
+import ru.yandex.yt.ytclient.wire.UnversionedRow;
+import ru.yandex.yt.ytclient.wire.UnversionedRowset;
+import ru.yandex.yt.ytclient.wire.UnversionedValue;
 import ru.yandex.yt.ytclient.ytree.YTreeBuilder;
+import ru.yandex.yt.ytclient.ytree.YTreeMapNode;
 
 public class ModifyRowsExample {
     private static final Logger logger = LoggerFactory.getLogger(ModifyRowsExample.class);
 
     public static void main(String[] args) {
         TableSchema schema = new TableSchema.Builder()
-                .addKey("dbname", ColumnValueType.STRING)
-                .addValue("server_schema", ColumnValueType.STRING)
-                .addValue("is_imported", ColumnValueType.BOOLEAN)
+                .addKey("timestamp", ColumnValueType.INT64)
+                .addKey("host", ColumnValueType.STRING)
+                .addKey("rack", ColumnValueType.STRING)
+                .addValue("utc_time", ColumnValueType.STRING)
+                .addValue("data", ColumnValueType.STRING)
                 .build();
         ExamplesUtil.runExample(client -> {
             ApiServiceTransactionOptions transactionOptions =
@@ -37,24 +46,56 @@ public class ModifyRowsExample {
                 logger.info("Transaction ping succeeded!");
 
                 ModifyRowsRequest request =
-                        new ModifyRowsRequest("//home/direct/tmp/snaury/dyn/mysql-sync-states", schema)
-                                .addInsert(Arrays.asList("foobar:1", "my schema", false))
+                        new ModifyRowsRequest("//home/dev/andozer/autorestart_nodes_copy", schema)
+                                .addInsert(Arrays.asList(10, "myhost1", "myrack1", "utc_time1", "data1"))
+                                .addInsert(Arrays.asList(11, "myhost2", "myrack2", "utc_time2", "data2"))
                                 .addUpdate(new YTreeBuilder()
                                         .beginMap()
-                                        .key("dbname").value("foobar:1")
-                                        .key("is_imported").value(true)
+                                        .key("timestamp").value(1486190036109192L)
+                                        .key("host").value("n0344-sas.hahn.yt.yandex.net")
+                                        .key("rack").value("SAS2.4.3-15")
+                                        .key("data").value("XXX " + UUID.randomUUID().toString())
                                         .buildMap()
                                         .mapValue())
                                 .addUpdate(new YTreeBuilder()
                                         .beginMap()
-                                        .key("dbname").value("foobar:2")
-                                        .key("server_schema").value("some foobar:2 schema")
+                                        .key("timestamp").value(1486190037953802L)
+                                        .key("host").value("s03-sas.hahn.yt.yandex.net")
+                                        .key("rack").value("SAS2.4.3-15")
+                                        .key("data").value("XXX " + UUID.randomUUID().toString())
                                         .buildMap()
                                         .mapValue());
                 long t0 = System.nanoTime();
                 transaction.modifyRows(request).join();
                 long t1 = System.nanoTime();
+
                 logger.info("Request time: {}ms", (t1 - t0) / 1000000.0);
+
+                t0 = System.nanoTime();
+
+                LookupRowsRequest lookup = new LookupRowsRequest("//home/dev/andozer/autorestart_nodes_copy", schema.toLookup())
+                        .addFilter(1486190036109192L, "n0344-sas.hahn.yt.yandex.net", "SAS2.4.3-15")
+                        .addFilter(1486190037953802L, "s03-sas.hahn.yt.yandex.net", "SAS2.4.3-15")
+                        .addLookupColumns("timestamp", "data");
+
+
+                t1 = System.nanoTime();
+                UnversionedRowset rowset = client.lookupRows(lookup).join();
+                long t2 = System.nanoTime();
+                logger.info("Request time: {}ms + {}ms", (t1 - t0) / 1000000.0, (t2 - t1) / 1000000.0);
+                logger.info("Result schema:");
+                for (ColumnSchema column : rowset.getSchema().getColumns()) {
+                    logger.info("    {}", column.getName());
+                }
+                for (UnversionedRow row : rowset.getRows()) {
+                    logger.info("Row:");
+                    for (UnversionedValue value : row.getValues()) {
+                        logger.info("    value: {}", value);
+                    }
+                }
+                for (YTreeMapNode row : rowset.getYTreeRows()) {
+                    logger.info("Row: {}", row);
+                }
 
                 transaction.commit().join();
                 logger.info("Transaction committed!");
