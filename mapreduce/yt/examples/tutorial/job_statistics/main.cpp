a4fe1d98f5777cac16b85b8de12e82ec9045b7ab
levysotsky <levysotsky@yandex-team.ru>
2018-01-31 17:27:22 +0300

    Custom job statistics example Pull-request for branch users/levysotsky/user_statistics_example

--- mapreduce/yt/examples/tutorial/job_statistics/main.cpp
+++ mapreduce/yt/examples/tutorial/job_statistics/main.cpp
@@ -3,21 +3,59 @@
 
 using namespace NYT;
 
+class TComputeEmailsMapper
+    : public IMapper<TTableReader<TNode>, TTableWriter<TNode>>
+{
+    public:
+        virtual void Do(TReader* reader, TWriter* writer) override
+        {
+            // Соберём несколько интересных статистик по сотрудникам
+            i64 fortyTwoCount = 0;
+            i64 shortLoginCount = 0;
+            for (; reader->IsValid(); reader->Next()) {
+                const auto& row = reader->GetRow();
+                auto login = row["login"].AsString();
+                TNode outRow;
+                outRow["name"] = row["name"];
+                outRow["email"] = login + "@yandex-team.ru";
+                writer->AddRow(outRow);
+
+                if (login.find("42") != TString::npos) {
+                    ++fortyTwoCount;
+                }
+                if (login.size() <= 4) {
+                    ++shortLoginCount;
+                }
+            }
+
+            // Пользовательскую статистику можно писать по пути (имени)
+            WriteCustomStatistics("names/with_42", fortyTwoCount);
+            WriteCustomStatistics(TNode() // А можно в виде мапы
+                ("names", TNode()
+                    ("short", shortLoginCount)));
+        }
+};
+REGISTER_MAPPER(TComputeEmailsMapper);
+
 int main(int argc, const char** argv) {
     NYT::Initialize(argc, argv);
 
     auto client = CreateClient("freud");
+    const TString outputTable = "//tmp/" + GetUsername() + "-tutorial-emails";
 
-    const TString outputTable = "//tmp/" + GetUsername() + "-tutorial-statistics";
-
-    auto operation = client->Sort(
-        TSortOperationSpec()
-            .AddInput("//home/ermolovd/yt-tutorial/staff_unsorted")
-            .Output(outputTable)
-            .SortBy({"login"}));
+    auto operation = client->Map(
+        TMapOperationSpec()
+            .AddInput<TNode>("//home/ermolovd/yt-tutorial/staff_unsorted")
+            .AddOutput<TNode>(outputTable),
+        new TComputeEmailsMapper);
 
     auto jobStatistics = operation->GetJobStatistics();
+    // Выведем системные статистики
     Cout << "time/total: " << jobStatistics.GetStatisticsAs<TDuration>("time/total").Sum() << Endl;
     Cout << "data/input/row_count: " << jobStatistics.GetStatistics("data/input/row_count").Sum() << Endl;
+
+    // NB: для получения пользовательских статистик используется другой метод
+    Cout << "names/with_42: " << jobStatistics.GetCustomStatistics("names/with_42").Sum() << Endl;
+    Cout << "names/short: " << jobStatistics.GetCustomStatistics("names/short").Sum() << Endl;
     return 0;
 }
