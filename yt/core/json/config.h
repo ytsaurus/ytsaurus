a40ed1534ffe8eccd2320aa5200ef860b4f3857c
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-02-02 14:25:41 +0300

    Decrease JSON buffer size

--- yt/core/json/config.h
+++ yt/core/json/config.h
@@ -67,7 +67,7 @@ public:
         RegisterParameter("support_infinity", SupportInfinity)
             .Default(false);
         RegisterParameter("buffer_size", BufferSize)
-            .Default(16 * 1024 * 1024);
+            .Default(16 * 1024);
         RegisterParameter("skip_null_values", SkipNullValues)
             .Default(false);
 
