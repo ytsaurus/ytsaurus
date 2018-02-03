6e24718271c36f2c22600a13a7ea7640746d4724
Alexey Ozeritskiy <aozeritsky@yandex-team.ru>
2018-02-03 13:31:03 +0300

    minor: 4096->4_KB

--- yt/ytlib/chunk_client/io_engine.cpp
+++ yt/ytlib/chunk_client/io_engine.cpp
@@ -151,7 +151,7 @@ private:
     NConcurrency::TThreadPool ThreadPool_;
 
     bool UseDirectIO_;
-    const i64 Alignment_ = 4096;
+    const i64 Alignment_ = 4_KB;
 
     bool DoFlushData(const std::shared_ptr<TFileHandle>& fh)
     {
@@ -471,7 +471,7 @@ private:
     NConcurrency::TAsyncSemaphore Semaphore_;
     std::atomic<bool> Alive_ = {true};
 
-    const i64 Alignment_ = 4096;
+    const i64 Alignment_ = 4_KB;
 
     TThread Thread_;
 
