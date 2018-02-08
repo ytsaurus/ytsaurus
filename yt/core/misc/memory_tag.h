db7a3c972936e340ab606dc028c861d33847468a
Maxim Akhmedov <max42@ytsaurus.tech>
2018-02-08 05:53:50 +0300

    Introduce memory tags and memory tag guards.

---
+++ yt/core/misc/memory_tag.h
@@ -0,0 +1,40 @@
+#pragma once
+
+#include <yt/core/misc/common.h>
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+using TMemoryTag = ui64;
+constexpr TMemoryTag NullMemoryTag = 0;
+constexpr TMemoryTag MaxMemoryTag = 1 << 16;
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TMemoryTagGuard
+{
+public:
+    TMemoryTagGuard() = default;
+    TMemoryTagGuard(TMemoryTag tag);
+    ~TMemoryTagGuard();
+
+    TMemoryTagGuard(const TMemoryTagGuard& other) = delete;
+    TMemoryTagGuard(TMemoryTagGuard&& other);
+private:
+    bool Active_ = false;
+    TMemoryTag PreviousTag_ = NullMemoryTag;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+// Implementations in memory_tag.cpp are merely stubs, the intended
+// implementations may be found in lf_allocX64.cpp.
+Y_WEAK void SetCurrentMemoryTag(TMemoryTag tag);
+Y_WEAK ssize_t GetMemoryUsageForTag(TMemoryTag tag);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
+
+////////////////////////////////////////////////////////////////////////////////
