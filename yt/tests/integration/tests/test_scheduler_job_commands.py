196dd9fe1aabbd651597e4c3d17e65e38db1c7d3
Andrey Saitgalin <asaitgalin@yandex-team.ru>
2018-02-09 19:20:35 +0300

    Fix tests, enabled test job prober

--- yt/tests/integration/tests/test_scheduler_job_commands.py
+++ yt/tests/integration/tests/test_scheduler_job_commands.py
@@ -42,7 +42,6 @@ porto_delta_node_config = {
 
 ##################################################################
 
-@pytest.mark.skipif("True")
 class TestJobProber(YTEnvSetup):
     NUM_MASTERS = 3
     NUM_NODES = 5
@@ -412,7 +411,6 @@ class TestJobProber(YTEnvSetup):
 
 ##################################################################
 
-@pytest.mark.skipif("True")
 @patch_porto_env_only(TestJobProber)
 class TestJobProberPorto(YTEnvSetup):
     DELTA_NODE_CONFIG = porto_delta_node_config
