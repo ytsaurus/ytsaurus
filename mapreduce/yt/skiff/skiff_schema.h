7df75b38f03c295cb56c08da008616dc07a744bb
levysotsky <levysotsky@yandex-team.ru>
2018-02-15 17:10:32 +0300

    Move skiff to mapreduce/yt Pull-request for branch users/levysotsky/move_skiff_to_mapreduce_yt

---
+++ mapreduce/yt/skiff/skiff_schema.h
@@ -0,0 +1,93 @@
+#pragma once
+
+#include "public.h"
+#include "wire_type.h"
+
+#include <util/generic/string.h>
+#include <util/generic/vector.h>
+#include <util/string/cast.h>
+
+namespace NSkiff {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TSimpleTypeSchema;
+using TSimpleTypeSchemaPtr = TIntrusivePtr<TSimpleTypeSchema>;
+
+template <EWireType WireType>
+class TComplexSchema;
+
+using TSkiffSchemaList = TVector<TSkiffSchemaPtr>;
+
+using TTupleSchema = TComplexSchema<EWireType::Tuple>;
+using TTupleSchemaPtr = TIntrusivePtr<TTupleSchema>;
+using TVariant8Schema = TComplexSchema<EWireType::Variant8>;
+using TVariant8SchemaPtr = TIntrusivePtr<TVariant8Schema>;
+using TVariant16Schema = TComplexSchema<EWireType::Variant16>;
+using TVariant16SchemaPtr = TIntrusivePtr<TVariant16Schema>;
+using TRepeatedVariant16Schema = TComplexSchema<EWireType::RepeatedVariant16>;
+using TRepeatedVariant16SchemaPtr = TIntrusivePtr<TRepeatedVariant16Schema>;
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TSkiffSchema
+    : public TThrRefBase
+{
+public:
+    EWireType GetWireType() const;
+    TSkiffSchemaPtr SetName(TString name);
+    const TString& GetName() const;
+
+    virtual TSkiffSchemaList GetChildren() const;
+
+protected:
+    explicit TSkiffSchema(EWireType type);
+
+private:
+    const EWireType Type_;
+    TString Name_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TSimpleTypeSchema
+    : public TSkiffSchema
+{
+public:
+    explicit TSimpleTypeSchema(EWireType type);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <EWireType WireType>
+class TComplexSchema
+    : public TSkiffSchema
+{
+public:
+    explicit TComplexSchema(TSkiffSchemaList elements);
+
+    virtual TSkiffSchemaList GetChildren() const override;
+
+private:
+    const TSkiffSchemaList Elements_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+bool IsSimpleType(EWireType type);
+TString GetShortDebugString(const TSkiffSchemaPtr& schema);
+void PrintShortDebugString(const TSkiffSchemaPtr& schema, IOutputStream* out);
+
+TSimpleTypeSchemaPtr CreateSimpleTypeSchema(EWireType type);
+TTupleSchemaPtr CreateTupleSchema(TSkiffSchemaList children);
+TVariant8SchemaPtr CreateVariant8Schema(TSkiffSchemaList children);
+TVariant16SchemaPtr CreateVariant16Schema(TSkiffSchemaList children);
+TRepeatedVariant16SchemaPtr CreateRepeatedVariant16Schema(TSkiffSchemaList children);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NSkiff
+
+#define SKIFF_SCHEMA_H
+#include "skiff_schema-inl.h"
+#undef SKIFF_SCHEMA_H
---
+++ mapreduce/yt/skiff/unchecked_writer.h
@@ -0,0 +1,44 @@
+#pragma once
+
+#include <util/generic/buffer.h>
+#include <util/stream/output.h>
+
+namespace NSkiff {
+    class TUncheckedSkiffWriter
+        : private IOutputStream
+    {
+    public:
+        explicit TUncheckedSkiffWriter(IOutputStream* underlying);
+
+        ~TUncheckedSkiffWriter();
+
+        void WriteDouble(double value);
+        void WriteBoolean(bool value);
+
+        void WriteInt64(i64 value);
+        void WriteUint64(ui64 value);
+
+        void WriteString32(TStringBuf value);
+
+        void WriteYson32(TStringBuf value);
+
+        void WriteVariant8Tag(ui8 tag);
+        void WriteVariant16Tag(ui16 tag);
+
+        using IOutputStream::Flush;
+        void Finish();
+
+    private:
+        void DoWrite(const void* data, size_t size) override final;
+        void DoFlush() override final;
+
+        template <typename T>
+        void WriteSimple(T data);
+
+    private:
+        IOutputStream* const Underlying_;
+        TBuffer Buffer_;
+        size_t RemainingBytes_;
+        char* Position_;
+    };
+}
