4dfa41cb4f318b039cb090778678f56f34d49776
Maxim Babenko <babenko@ytsaurus.tech>
2018-02-20 21:04:27 +0300

    Remove unused field

--- yt/server/controller_agent/config.h
+++ yt/server/controller_agent/config.h
@@ -481,8 +481,6 @@ public:
 
     TDuration OperationLogProgressBackoff;
 
-    TDuration OperationLogFairSharePeriod;
-
     TDuration OperationControllerFailTimeout;
 
     TDuration AvailableExecNodesCheckPeriod;
