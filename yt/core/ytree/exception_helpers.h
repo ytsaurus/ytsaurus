d2854099be6b02a05fb0e88d106242b1030b5a85
Maxim Babenko <babenko@ytsaurus.tech>
2018-02-22 16:38:07 +0300

    Don't suggest anything

--- yt/core/ytree/exception_helpers.h
+++ yt/core/ytree/exception_helpers.h
@@ -13,7 +13,6 @@ namespace NYTree {
 
 void ThrowInvalidNodeType(IConstNodePtr node, ENodeType expectedType, ENodeType actualType);
 void ThrowNoSuchChildKey(IConstNodePtr node, const TString& key);
-void ThrowNoSuchChildKeySuggestRecursive(IConstNodePtr node, const TString& key);
 void ThrowNoSuchChildIndex(IConstNodePtr node, int index);
 void ThrowNoSuchAttribute(const TString& key);
 void ThrowNoSuchBuiltinAttribute(const TString& key);
