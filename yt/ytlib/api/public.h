effa4c44811d41a26830a05fd50e18593081eeae
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-02-22 16:37:18 +0300

    Separate error for unavailable jobs archive

--- yt/ytlib/api/public.h
+++ yt/ytlib/api/public.h
@@ -26,6 +26,7 @@ DEFINE_ENUM(EUserWorkloadCategory,
 
 DEFINE_ENUM(EErrorCode,
     ((TooManyConcurrentRequests)                         (1900))
+    ((JobArchiveUnavailable)                             (1910))
 );
 
 DEFINE_ENUM(ERowModificationType,
