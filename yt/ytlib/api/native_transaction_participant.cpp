6306ce803a8e0357859c1ce79fd148539874a842
Maxim Babenko <babenko@ytsaurus.tech>
2018-02-24 19:10:35 +0300

    Revert "No need for cell directory sync in tx participant"
    This reverts commit 27604327e2649726792425ed292f09cab2023aa8.

--- yt/ytlib/api/native_transaction_participant.cpp
+++ yt/ytlib/api/native_transaction_participant.cpp
@@ -1,6 +1,7 @@
 #include "native_transaction_participant.h"
 
 #include <yt/ytlib/hive/cell_directory.h>
+#include <yt/ytlib/hive/cell_directory_synchronizer.h>
 #include <yt/ytlib/hive/transaction_participant.h>
 #include <yt/ytlib/hive/transaction_participant_service_proxy.h>
 
@@ -21,10 +22,12 @@ class TNativeTransactionParticipant
 public:
     TNativeTransactionParticipant(
         TCellDirectoryPtr cellDirectory,
+        TCellDirectorySynchronizerPtr cellDirectorySynchronizer,
         ITimestampProviderPtr timestampProvider,
         const TCellId& cellId,
         const TTransactionParticipantOptions& options)
         : CellDirectory_(std::move(cellDirectory))
+        , CellDirectorySynchronizer_(std::move(cellDirectorySynchronizer))
         , TimestampProvider_(std::move(timestampProvider))
         , CellId_(cellId)
         , Options_(options)
@@ -83,6 +86,7 @@ public:
 
 private:
     const TCellDirectoryPtr CellDirectory_;
+    const TCellDirectorySynchronizerPtr CellDirectorySynchronizer_;
     const ITimestampProviderPtr TimestampProvider_;
     const TCellId CellId_;
     const TTransactionParticipantOptions Options_;
@@ -109,8 +113,20 @@ private:
         if (channel) {
             return MakeFuture(channel);
         }
+        if (!CellDirectorySynchronizer_) {
+            return MakeNoChannelError();
+        }
+        return CellDirectorySynchronizer_->Sync().Apply(BIND([=, this_ = MakeStrong(this)] () {
+            auto channel = CellDirectory_->FindChannel(CellId_);
+            if (channel) {
+                return MakeFuture(channel);
+            }
+            return MakeNoChannelError();
+        }));
+    }
 
-        // NB: We rely on background Cell Directory sync.
+    TFuture<NRpc::IChannelPtr> MakeNoChannelError()
+    {
         return MakeFuture<NRpc::IChannelPtr>(TError(
             NRpc::EErrorCode::Unavailable,
             "No such participant cell %v",
@@ -120,12 +136,14 @@ private:
 
 ITransactionParticipantPtr CreateNativeTransactionParticipant(
     TCellDirectoryPtr cellDirectory,
+    TCellDirectorySynchronizerPtr cellDirectorySynchronizer,
     ITimestampProviderPtr timestampProvider,
     const TCellId& cellId,
     const TTransactionParticipantOptions& options)
 {
     return New<TNativeTransactionParticipant>(
         std::move(cellDirectory),
+        std::move(cellDirectorySynchronizer),
         std::move(timestampProvider),
         cellId,
         options);
