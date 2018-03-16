9eee8be3f73e1fafb16e1732a3409d4429692ca3
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-03-16 20:20:38 +0300

    Fix test_suspend_resume

--- python-git/yt/wrapper/tests/test_operations.py
+++ python-git/yt/wrapper/tests/test_operations.py
@@ -637,7 +637,7 @@ print(op.id)
             time.sleep(2.5)
             assert op.get_state() == "running"
             op.resume()
-            op.wait(timeout=10)
+            op.wait(timeout=20 * 1000)
             assert op.get_state() == "completed"
         finally:
             if op.get_state() not in ["completed", "failed", "aborted"]:
