67883b5b01247a1869be94ed0573de5067158a7a
Fedor Korotkiy <prime@yandex-team.ru>
2018-03-20 21:38:08 +0300

    YT-8611: Fix node start with disabled volume location

--- yt/server/misc/disk_location.cpp
+++ yt/server/misc/disk_location.cpp
@@ -5,11 +5,15 @@
 #include <yt/server/cell_node/bootstrap.h>
 #include <yt/server/data_node/master_connector.h>
 
+#include <yt/core/yson/string.h>
+
 #include <yt/core/misc/fs.h>
 
 namespace NYT {
 
 using namespace NCellNode;
+using namespace NYTree;
+using namespace NYson;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -25,6 +29,33 @@ TDiskLocation::TDiskLocation(
 bool TDiskLocation::IsEnabled() const
 {
     return Enabled_.load(); 
+}
+
+void TDiskLocation::ValidateLockFile() const
+{
+    LOG_INFO("Checking lock file");
+
+    auto lockFilePath = NFS::CombinePaths(Config_->Path, DisabledLockFileName);
+    if (!NFS::Exists(lockFilePath)) {
+        return;
+    }
+
+    TFile file(lockFilePath, OpenExisting | RdOnly | Seq | CloseOnExec);
+    TBufferedFileInput fileInput(file);
+
+    auto errorData = fileInput.ReadAll();
+    if (errorData.Empty()) {
+        THROW_ERROR_EXCEPTION("Empty lock file found");
+    }
+
+    TError error;
+    try {
+        error = ConvertTo<TError>(TYsonString(errorData));
+    } catch (const std::exception& ex) {
+        THROW_ERROR_EXCEPTION("Error parsing lock file contents")
+            << ex;
+    }
+    THROW_ERROR error;
 }
 
 void TDiskLocation::ValidateMinimumSpace() const
