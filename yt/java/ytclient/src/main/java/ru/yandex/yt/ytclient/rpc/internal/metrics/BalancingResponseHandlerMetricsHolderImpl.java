f9abf91b6c40bb02f3144f96b65bdad2532a4348
aozeritsky <aozeritsky@yandex-team.ru>
2018-03-23 18:17:21 +0300

    remove usage of deprecated class

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/internal/metrics/BalancingResponseHandlerMetricsHolderImpl.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/internal/metrics/BalancingResponseHandlerMetricsHolderImpl.java
@@ -4,7 +4,7 @@ import com.codahale.metrics.Counter;
 import com.codahale.metrics.MetricRegistry;
 import com.codahale.metrics.SharedMetricRegistries;
 
-import ru.yandex.yt.ytclient.rpc.BalancingRpcClient;
+import ru.yandex.yt.ytclient.proxy.YtClient;
 
 /**
  * @author dkondra
@@ -13,9 +13,9 @@ public class BalancingResponseHandlerMetricsHolderImpl implements BalancingRespo
     public static final BalancingResponseHandlerMetricsHolderImpl instance = new BalancingResponseHandlerMetricsHolderImpl();
 
     private static final MetricRegistry metrics = SharedMetricRegistries.getOrCreate("ytclient");
-    private static final Counter inflight = metrics.counter(MetricRegistry.name(BalancingRpcClient.class, "requests", "inflight"));
-    private static final Counter failover = metrics.counter(MetricRegistry.name(BalancingRpcClient.class,"requests", "failover"));
-    private static final Counter total = metrics.counter(MetricRegistry.name(BalancingRpcClient.class,"requests", "total"));
+    private static final Counter inflight = metrics.counter(MetricRegistry.name(YtClient.class, "requests", "inflight"));
+    private static final Counter failover = metrics.counter(MetricRegistry.name(YtClient.class,"requests", "failover"));
+    private static final Counter total = metrics.counter(MetricRegistry.name(YtClient.class,"requests", "total"));
 
     @Override
     public void inflightInc() {
