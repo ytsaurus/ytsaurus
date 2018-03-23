df477200c3b216f4fcb28bd242ef5d38be6e4f9d
ermolovd <ermolovd@ytsaurus.tech>
2018-03-23 12:59:51 +0300

    Library with main for yt unittests * Library with main for yt unittests

---
+++ mapreduce/yt/library/utlib/main.cpp
@@ -0,0 +1,11 @@
+#include <library/unittest/utmain.h>
+
+#include <mapreduce/yt/interface/client.h>
+#include <mapreduce/yt/common/config.h>
+
+int main(int argc, const char** argv)
+{
+    NYT::TConfig::Get()->LogLevel = "debug";
+    NYT::Initialize(argc, argv);
+    return NUnitTest::RunMain(argc, const_cast<char**>(argv));
+}
