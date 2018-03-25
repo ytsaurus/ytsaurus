fa99ef41dcbf574c7f2c5f9c7c757774a033bd10
Amir Shakurov <shakurov@ytsaurus.tech>
2018-03-25 23:59:23 +0300

    Split ghosts into two categories: ephemeral and weak.
    Ephemeral ghosts have zero weak ref counter and non-zero ephemeral ref
    counter. They are not persisted and vanish on recovery (unless they've
    been disposed of before that).
    
    Weak ghosts have non-zero weak ref counter (the value of the ephemeral
    ref counter doesn't matter). They are persisted and linger through
    epoch changes until their disposal.
    
    All ghosts have zero ref counter.

--- yt/server/cell_master/serialize.cpp
+++ yt/server/cell_master/serialize.cpp
@@ -7,7 +7,7 @@ namespace NCellMaster {
 
 int GetCurrentSnapshotVersion()
 {
-    return 704;
+    return 705;
 }
 
 bool ValidateSnapshotVersion(int version)
@@ -64,7 +64,8 @@ bool ValidateSnapshotVersion(int version)
         version == 701 ||
         version == 702 ||
         version == 703 ||
-        version == 704;
+        version == 704 ||
+        version == 705;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
