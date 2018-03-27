60925922d0324f97081adaa4460f6e8ca7cd7124
ignat <ignat@ytsaurus.tech>
2018-03-27 23:50:47 +0300

    Fix test/test_local_cypress.py

--- mapreduce/yt/python/test/test_local_cypress.py
+++ mapreduce/yt/python/test/test_local_cypress.py
@@ -1,16 +1,14 @@
 import pytest
 import yatest.common as yatest_common
 
-from mapreduce.yt.python.yt_stuff import YtStuff, YtConfig, yt_stuff
+from mapreduce.yt.python.yt_stuff import YtConfig, YtStuff
 
-@pytest.fixture(scope="module")
-def yt_config(request):
-    return YtConfig(
+
+def test_invalid_local_cypress_dir():
+    config = YtConfig(
         local_cypress_dir=yatest_common.source_path("mapreduce/yt/python/test/cypress_dir")
     )
 
-def test_invalid_local_cypress_dir(yt_stuff):
-    yt_wrapper = yt_stuff.get_yt_wrapper()
-    assert yt_wrapper.exists("//my_table")
-
-
+    with pytest.raises(Exception):
+        stuff = YtStuff(config)
+        stuff.start_local_yt()
