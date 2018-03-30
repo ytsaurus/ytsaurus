c8158b2b8bb28ff6aef201266a671866e403835e
Maxim Babenko <babenko@ytsaurus.tech>
2018-03-30 09:54:54 +0300

    Improve TestGetOperation

--- yt/tests/integration/tests/test_get_operation.py
+++ yt/tests/integration/tests/test_get_operation.py
@@ -12,13 +12,8 @@ def id_to_parts(id):
     id_lo = long(id_parts[0], 16) << 32 | int(id_parts[1], 16)
     return id_hi, id_lo
 
-def get_new_operation_path(op_id):
+def get_operation_path(op_id):
     return "//sys/operations/{}/{}".format("%02x" % (long(op_id.split("-")[3], 16) % 256), op_id)
-
-def get_operation_path(op_id, storage_mode):
-    if storage_mode == "hash_buckets":
-        return get_new_operation_path(op_id)
-    return "//sys/operations/" + op_id
 
 class TestGetOperation(YTEnvSetup):
     NUM_MASTERS = 1
@@ -33,8 +28,7 @@ class TestGetOperation(YTEnvSetup):
     def teardown(self):
         remove("//sys/operations_archive")
 
-    @pytest.mark.parametrize("storage_mode", ["simple_hash_buckets", "hash_buckets", "compatible"])
-    def test_get_operation(self, storage_mode):
+    def test_get_operation(self):
         create("table", "//tmp/t1")
         create("table", "//tmp/t2")
         write_table("//tmp/t1", [{"foo": "bar"}, {"foo": "baz"}, {"foo": "qux"}])
@@ -49,31 +43,35 @@ class TestGetOperation(YTEnvSetup):
                 "mapper": {
                     "input_format": "json",
                     "output_format": "json"
-                },
-                "testing": {
-                    "cypress_storage_mode": storage_mode,
-                },
+                }
             })
         wait_breakpoint()
 
-        def check(res1, res2):
-            for key in ["authenticated_user", "brief_progress", "brief_spec", "finish_time", "operation_type", "result", "start_time", "state", "suspended", "title", "weight", "spec", "unrecognized_spec", "full_spec"]:
-                ok1 = key in res1
-                ok2 = key in res2
-                assert ok1 == ok2, "{0} is missing in one of [res1, res2]".format(key)
-
-                if ok1:
-                    assert res1[key] == res2[key]
-
-        wait(lambda: exists(get_operation_path(op.id, storage_mode)))
+        wait(lambda: exists(get_operation_path(op.id)))
 
         res_get_operation = get_operation(op.id, include_scheduler=True)
-        res_cypress = get(get_operation_path(op.id, storage_mode) + "/@")
+        res_cypress = get(get_operation_path(op.id) + "/@")
         if "full_spec" in res_cypress:
             res_cypress["full_spec"].attributes.pop("opaque", None)
         res_orchid_progress = get("//sys/scheduler/orchid/scheduler/operations/{0}/progress".format(op.id))
 
-        check(res_get_operation, res_cypress)
+        def filter_attrs(attrs):
+            PROPER_ATTRS = ["authenticated_user",
+                            "brief_progress",
+                            "brief_spec",
+                            "finish_time",
+                            "operation_type",
+                            "result",
+                            "start_time",
+                            "state",
+                            "suspended",
+                            "title",
+                            "weight",
+                            "spec",
+                            "unrecognized_spec",
+                            "full_spec"]
+            return {key : attrs[key] for key in PROPER_ATTRS if key in attrs}
+        assert filter_attrs(res_get_operation) == filter_attrs(res_cypress)
 
         res_get_operation_progress = res_get_operation["progress"]
 
@@ -84,25 +82,20 @@ class TestGetOperation(YTEnvSetup):
         release_breakpoint()
         op.track()
 
-        res_cypress_finished = get(get_operation_path(op.id, storage_mode) + "/@")
+        res_cypress_finished = get(get_operation_path(op.id) + "/@")
         if "full_spec" in res_cypress_finished:
             res_cypress_finished["full_spec"].attributes.pop("opaque", None)
+        del res_cypress_finished["progress"]["build_time"]
 
         clean_operations(self.Env.create_native_client())
 
         res_get_operation_archive = get_operation(op.id)
+        del res_get_operation_archive["progress"]["build_time"]
 
         for key in res_get_operation_archive.keys():
             if key in res_cypress:
-                if key == "id":
-                    assert res_get_operation_archive[key] == op.id
-                else:
-                    assert res_get_operation_archive[key] == res_cypress_finished[key]
-            else:
-                print key
-                print res_get_operation_archive[key]
+                assert res_get_operation_archive[key] == res_cypress_finished[key]
 
-    @pytest.mark.parametrize("storage_mode", ["simple_hash_buckets", "hash_buckets", "compatible"])
     def test_attributes(self, storage_mode):
         create("table", "//tmp/t1")
         create("table", "//tmp/t2")
@@ -118,10 +111,7 @@ class TestGetOperation(YTEnvSetup):
                 "mapper": {
                     "input_format": "json",
                     "output_format": "json"
-                },
-                "testing": {
-                    "cypress_storage_mode": storage_mode,
-                },
+                }
             })
         wait_breakpoint()
 
@@ -129,7 +119,7 @@ class TestGetOperation(YTEnvSetup):
 
         for read_from in ("cache", "follower"):
             res_get_operation = get_operation(op.id, attributes=["progress", "state"], include_scheduler=True, read_from=read_from)
-            res_cypress = get(get_operation_path(op.id, storage_mode) + "/@", attributes=["progress", "state"])
+            res_cypress = get(get_operation_path(op.id) + "/@", attributes=["progress", "state"])
 
             assert sorted(list(res_get_operation)) == ["progress", "state"]
             assert sorted(list(res_cypress)) == ["progress", "state"]
@@ -154,21 +144,15 @@ class TestGetOperation(YTEnvSetup):
 
         op = map(in_="//tmp/t1",
             out="//tmp/t2",
-            command="cat",
-            spec={
-                "testing": {
-                    "cypress_storage_mode": "simple_hash_buckets"
-                }
-            })
+            command="cat")
 
         tx = start_transaction(timeout=300 * 1000)
-        lock(get_new_operation_path(op.id),
+        lock(get_operation_path(op.id),
             mode="shared",
             child_key="completion_transaction_id",
             transaction_id=tx)
 
         clean_operations(self.Env.create_native_client())
         assert not exists("//sys/operations/" + op.id)
-        assert exists(get_new_operation_path(op.id))
-
+        assert exists(get_operation_path(op.id))
         assert "state" in get_operation(op.id)
