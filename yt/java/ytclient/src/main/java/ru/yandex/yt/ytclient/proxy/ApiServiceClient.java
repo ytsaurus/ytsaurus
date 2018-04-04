9d1e0cf1de1134e0f31cfab95bb13fb373c607a2
aozeritsky <aozeritsky@yandex-team.ru>
2018-04-04 13:23:30 +0300

    more debug

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/ApiServiceClient.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/ApiServiceClient.java
@@ -120,6 +120,11 @@ public class ApiServiceClient {
     private final Option<RpcClient> rpcClient;
     private final RpcOptions rpcOptions;
 
+    @Override
+    public String toString() {
+        return "ApiServiceClient(" + rpcClient.toString() + ")";
+    }
+
     private ApiServiceClient(Option<RpcClient> client, RpcOptions options, ApiService service, Executor heavyExecutor) {
         this.service = Objects.requireNonNull(service);
         this.heavyExecutor = Objects.requireNonNull(heavyExecutor);
