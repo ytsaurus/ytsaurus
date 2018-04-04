8f0c8fa3a257ece935b4973b1d57a67a1bb3ff47
Fedor Korotkiy <prime@yandex-team.ru>
2018-04-05 01:53:18 +0300

    Query oauth tokens from cypress cache. (#2574)

--- yt/nodejs/tests/test_authentication.js
+++ yt/nodejs/tests/test_authentication.js
@@ -65,7 +65,7 @@ function stubRegistry()
 
     var logger = stubLogger();
     var profiler = stubProfiler();
-    var driver = { executeSimple: function(){ return Q.resolve(); } };
+    var driver = { executeSimple: function(){ return Q.resolve({}); } };
 
     YtRegistry.set("config", config);
     YtRegistry.set("logger", logger);
@@ -321,7 +321,7 @@ describe("YtAuthentication", function() {
         mock2
             .expects("executeSimple")
             .once()
-            .withExactArgs("get", sinon.match({ path: "//sys/tokens/some-token" }))
+            .withExactArgs("get", sinon.match({ path: "//sys/tokens", read_from: "cache" }))
             .returns(Q.reject(new YtError().withCode(500)));
         mock2
             .expects("executeSimple")
@@ -368,7 +368,7 @@ describe("YtAuthentication", function() {
         mock2
             .expects("executeSimple")
             .once()
-            .withExactArgs("get", sinon.match({ path: "//sys/tokens/some-token" }))
+            .withExactArgs("get", sinon.match({ path: "//sys/tokens" }))
             .returns(Q.reject(new YtError("Random error").withCode(500)));
         mock2
             .expects("executeSimple")
@@ -398,9 +398,10 @@ describe("YtAuthentication", function() {
             .expects("executeSimple")
             .once()
             .withExactArgs("get", sinon.match({
-                path: "//sys/tokens/unknown-token"
+                path: "//sys/tokens",
+                read_from: "cache",
             }))
-            .returns(Q.resolve("unknown-user"));
+            .returns(Q.resolve({"unknown-token": "unknown-user"}));
         mock
             .expects("executeSimple")
             .once()
@@ -423,9 +424,10 @@ describe("YtAuthentication", function() {
             .expects("executeSimple")
             .once()
             .withExactArgs("get", sinon.match({
-                path: "//sys/tokens/cypress-token"
+                path: "//sys/tokens",
+                read_from: "cache",
             }))
-            .returns(Q.resolve("cypress-user"));
+            .returns(Q.resolve({"cypress-token": "cypress-user"}));
         mock
             .expects("executeSimple")
             .once()
