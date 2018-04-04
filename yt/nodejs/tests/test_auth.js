484999be020deb3c5743021259cd7e5c6a239903
Fedor Korotkiy <prime@yandex-team.ru>
2018-04-05 01:53:18 +0300

    Query oauth tokens from cypress cache. (#2574)

--- yt/nodejs/tests/test_auth.js
+++ yt/nodejs/tests/test_auth.js
@@ -78,7 +78,7 @@ function stubRegistry()
 
     var logger = stubLogger();
     var profiler = stubProfiler();
-    var driver = { executeSimple: function(){ return Q.resolve(); } };
+    var driver = { executeSimple: function(){ return Q.resolve({}); } };
 
     YtRegistry.set("config", config);
     YtRegistry.set("logger", logger);
