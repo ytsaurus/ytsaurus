4ad8e50500d1b1db24ff6b55c6bf591483fcfb86
ignat <ignat@ytsaurus.tech>
2018-04-10 20:27:23 +0000

    [yp/python] Enable test for ssl
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@3543825 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- tests/test_ssl.py
+++ tests/test_ssl.py
@@ -2,15 +2,19 @@ import pytest
 
 from yp.client import YpClient
 
+import sys
+
 @pytest.mark.usefixtures("yp_env_configurable")
+@pytest.mark.skipif('not hasattr(sys, "extra_modules")')
 class TestSsl(object):
     ENABLE_SSL = True
     YP_MASTER_CONFIG = {}
 
-    # TODO(ignat): grpc bindings and python should be built with the same version of crypto library.
-    # It is not true in standard suite, but it would be true in arcadia and we enable this test there.
-    def DISABLED_test_simple(self, yp_env_configurable):
+    def test_simple(self, yp_env_configurable):
+        # NB: grpc bindings and python should be built with the same version of crypto library.
+        # It is not true in standard suite, therefore we disable test in that case.
         address = yp_env_configurable.yp_instance.yp_client_secure_grpc_address
-        client = YpClient(address, config={"enable_ssl": True})
+        root_certificate = yp_env_configurable.yp_instance.config["secure_client_grpc_server"]["addresses"][0]["credentials"]["pem_root_certs"]["value"]
+        client = YpClient(address, config={"enable_ssl": True, "root_certificate": {"value": root_certificate}})
         assert client.generate_timestamp() > 0
 
