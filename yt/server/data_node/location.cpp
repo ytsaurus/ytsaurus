f45fa104ffe15f1c83e799c2c74ed32141612f12
Alexey Ozeritskiy <aozeritsky@yandex-team.ru>
2018-04-27 13:50:23 +0300

    ; use save the same io-thread count in ThreadedIOEngine as in Location

--- yt/server/data_node/location.cpp
+++ yt/server/data_node/location.cpp
@@ -66,7 +66,18 @@ TLocation::TLocation(
     , MetaReadInvoker_(CreatePrioritizedInvoker(MetaReadQueue_->GetInvoker()))
     , WriteThreadPool_(New<TThreadPool>(Bootstrap_->GetConfig()->DataNode->WriteThreadCount, Format("DataWrite:%v", Id_)))
     , WritePoolInvoker_(WriteThreadPool_->GetInvoker())
-    , IOEngine_(CreateIOEngine(Config_->IOEngineType, Config_->IOConfig))
+    , IOEngine_(CreateIOEngine(
+        Config_->IOEngineType,
+        Config_->IOConfig
+        ? Config_->IOConfig
+        // TODO(aozeritsky) temporary workaround
+        : NYTree::ConvertTo<NYTree::INodePtr>(
+            Format(
+                "{ threads = %v; }",
+                Bootstrap_->GetConfig()->DataNode->WriteThreadCount +
+                Bootstrap_->GetConfig()->DataNode->ReadThreadCount
+        ))
+    ))
     , ThrottledReadsCounter_("/throttled_reads", {}, config->ThrottleCounterInterval)
     , ThrottledWritesCounter_("/throttled_writes", {}, config->ThrottleCounterInterval)
     , PutBlocksWallTimeCounter_("/put_blocks_wall_time", {}, NProfiling::EAggregateMode::All)
