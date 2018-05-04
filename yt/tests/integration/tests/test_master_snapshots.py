f4721c7c2cf2b5769c72c618f752c294ca051702
Dmitry Ermolov <ermolovd@ytsaurus.tech>
2018-05-04 18:17:34 +0300

    Fix broken master snapshot when table has modified schema inside transaction

--- yt/tests/integration/tests/test_master_snapshots.py
+++ yt/tests/integration/tests/test_master_snapshots.py
@@ -24,6 +24,35 @@ def check_schema():
     assert get("//tmp/table2/@schema") == get_schema(True)
     assert get("//tmp/table3/@schema") == get_schema(False)
 
+def check_forked_schema():
+    schema1 = make_schema(
+        [
+            {"name": "foo", "type": "string", "required": True}
+        ],
+        unique_keys=False,
+        strict=True,
+    )
+    schema2 = make_schema(
+        [
+            {"name": "foo", "type": "string", "required": True},
+            {"name": "bar", "type": "string", "required": True}
+        ],
+        unique_keys=False,
+        strict=True,
+    )
+
+    create("table", "//tmp/forked_schema_table", attributes={"schema": schema1})
+    tx = start_transaction()
+    lock("//tmp/forked_schema_table", mode="snapshot", tx=tx)
+
+    alter_table("//tmp/forked_schema_table", schema=schema2)
+
+    yield
+
+    assert get("//tmp/forked_schema_table/@schema") == schema2
+    assert get("//tmp/forked_schema_table/@schema", tx=tx) == schema1
+
+
 class TestSnapshot(YTEnvSetup):
     NUM_MASTERS = 3
     NUM_NODES = 0
@@ -32,6 +61,7 @@ class TestSnapshot(YTEnvSetup):
         CHECKER_LIST = [
             check_simple_node,
             check_schema,
+            check_forked_schema,
         ]
 
         checker_state_list = [iter(c()) for c in CHECKER_LIST]
