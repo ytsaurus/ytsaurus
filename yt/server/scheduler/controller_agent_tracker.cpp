d6f99a59652f2a5981f9bd05e17267ce65257284
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-05-08 23:22:09 +0300

    YT-8814: Fix passing aborted by scheduler flag to controller

--- yt/server/scheduler/controller_agent_tracker.cpp
+++ yt/server/scheduler/controller_agent_tracker.cpp
@@ -940,7 +940,7 @@ public:
                         protoEvent->set_interrupt_reason(static_cast<int>(*event.InterruptReason));
                     }
                     if (event.AbortedByScheduler) {
-                        protoEvent->set_aborted_by_scheduler(true);
+                        protoEvent->set_aborted_by_scheduler(*event.AbortedByScheduler);
                     }
                 });
 
