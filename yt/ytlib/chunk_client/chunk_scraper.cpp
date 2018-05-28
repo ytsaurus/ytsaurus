20c9e624003fffffbb43c7fcd9a269b2174a2ec0
Pavel Sushin <psushin@ytsaurus.tech>
2018-05-28 11:38:34 +0300

    Fix stopping chunk scraper

--- yt/ytlib/chunk_client/chunk_scraper.cpp
+++ yt/ytlib/chunk_client/chunk_scraper.cpp
@@ -89,7 +89,11 @@ public:
             ChunkIds_.size());
 
         Started_ = false;
-        return PeriodicExecutor_->Stop();
+
+        return LocateFuture_
+            .Apply(BIND([this, this_ = MakeStrong(this)] (const TError& /* error */) {
+                return PeriodicExecutor_->Stop();
+            }));
     }
 
 private:
@@ -100,6 +104,8 @@ private:
     const TChunkLocatedHandler OnChunkLocated_;
     IInvokerPtr Invoker_;
 
+    TFuture<void> LocateFuture_ = VoidFuture;
+
     //! Non-const since we would like to shuffle it, to avoid scraping the same chunks
     //! on every restart.
     std::vector<TChunkId> ChunkIds_;
@@ -108,7 +114,7 @@ private:
 
     TChunkServiceProxy Proxy_;
 
-    bool Started_ = false;
+    std::atomic<bool> Started_ = { false };
     int NextChunkIndex_ = 0;
 
     const NConcurrency::TPeriodicExecutorPtr PeriodicExecutor_;
@@ -122,13 +128,17 @@ private:
 
         auto chunkCount = std::min<int>(ChunkIds_.size(), Config_->MaxChunksPerRequest);
 
-        Throttler_->Throttle(chunkCount)
-            .Subscribe(BIND(&TScraperTask::DoLocateChunks, MakeWeak(this))
+        LocateFuture_ = Throttler_->Throttle(chunkCount)
+            .Apply(BIND(&TScraperTask::DoLocateChunks, MakeWeak(this))
                 .Via(Invoker_));
     }
 
     void DoLocateChunks(const TError& error)
     {
+        if (!Started_) {
+            return;
+        }
+
         auto finallyGuard = Finally([&] () {
             PeriodicExecutor_->ScheduleNext();
         });
