651f7f855621976af9eb9578b3a14b47b4a4bd91
Maxim Babenko <babenko@ytsaurus.tech>
2018-06-01 10:06:55 +0300

    Cosmetics

--- yt/server/security_server/request_tracker.cpp
+++ yt/server/security_server/request_tracker.cpp
@@ -190,7 +190,7 @@ void TRequestTracker::OnFlush()
         return;
     }
 
-    LOG_DEBUG("Starting user statistics commit for %v users",
+    LOG_DEBUG("Starting user statistics commit (UserCount: %)",
         Request_.entries_size());
 
     const auto& hydraFacade = Bootstrap_->GetHydraFacade();
