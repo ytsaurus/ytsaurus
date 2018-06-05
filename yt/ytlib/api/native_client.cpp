72ec60b6aef5e8f0ce77532c621f5077f044bee3
Maxim Akhmedov <max42@ytsaurus.tech>
2018-06-05 22:07:44 +0300

    Add controller address to get_operation

--- yt/ytlib/api/native_client.cpp
+++ yt/ytlib/api/native_client.cpp
@@ -3496,6 +3496,7 @@ private:
         "spec",
         "full_spec",
         "unrecognized_spec",
+        "controller_address",
         "brief_progress",
         "brief_spec",
         "start_time",
