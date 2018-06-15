a0be2a44e1601d95c4f81b987b340f5095c6faad
levysotsky <levysotsky@yandex-team.ru>
2018-06-15 11:01:30 +0300

    YT-8564: Change IClientPtr to IClientBasePtr Pull-request for branch users/levysotsky/change_lazy_sort_interface

--- mapreduce/yt/library/lazy_sort/lazy_sort.h
+++ mapreduce/yt/library/lazy_sort/lazy_sort.h
@@ -8,7 +8,7 @@ namespace NYT {
 // If `spec.SortBy' columns set is a prefix of "sorted_by" attributes of
 // all input tables, run merge operation. Otherwise, reluctantly run sort.
 IOperationPtr LazySort(
-    const IClientPtr& client,
+    const IClientBasePtr& client,
     const TSortOperationSpec& spec,
     const TOperationOptions& options = TOperationOptions());
 
