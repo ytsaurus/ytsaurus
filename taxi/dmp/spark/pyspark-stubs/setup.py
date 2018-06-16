999b5c7d6db93a49bff633b407da2dd888d21e82
zero323 <mszymkiewicz@gmail.com>
2018-06-16 18:43:47 +0200

    Add setup.py

---
+++ taxi/dmp/spark/pyspark-stubs/setup.py
@@ -0,0 +1,33 @@
+"""
+Setup for PySpark type annotations.
+
+Notes:
+
+- To avoid manual PYTHONPATH modification the package installation will overlay PySpark installation. If this is not acceptable please download source and add it to PYTHONPATH manually.
+
+"""
+
+
+from setuptools import setup
+import os
+import sys
+
+# find_packages doesn't seem to handle stub files
+# so we'll enumarate manually
+src_path = os.path.join('third_party', '3')
+
+def list_packages(src_path=src_path):
+    for root, _, _ in os.walk(os.path.join(src_path, 'pyspark')):
+        yield '.'.join(os.path.relpath(root, src_path).split(os.path.sep))
+
+
+setup(
+    name='pyspark-stubs',
+    package_dir={'': src_path},
+    version='2.3.0',
+    packages=list(list_packages()),
+    package_data={
+        '': ['*.pyi']
+    },
+    install_requires=['pyspark>=2.3.0<2.4.0']
+)
