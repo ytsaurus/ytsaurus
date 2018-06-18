ff7a8d2db2f249340f7d213bbb769f9c9348b70b
Maxim Akhmedov <max42@ytsaurus.tech>
2018-06-19 02:20:29 +0300

    YT-8958: Various fixes for DT columnar statistics.

--- yt/ytlib/table_client/columnar_statistics.h
+++ yt/ytlib/table_client/columnar_statistics.h
@@ -1,6 +1,7 @@
 #pragma once
 
 #include <yt/core/misc/common.h>
+#include <yt/core/misc/nullable.h>
 
 namespace NYT {
 namespace NTableClient {
@@ -11,6 +12,8 @@ struct TColumnarStatistics
 {
     //! Per-column total data weight for chunks whose meta contains columnar statistics.
     std::vector<i64> ColumnDataWeights;
+    //! Total weight of all write and delete timestamps.
+    TNullable<i64> TimestampTotalWeight;
     //! Total data weight of legacy chunks whose meta misses columnar statistics.
     i64 LegacyChunkDataWeight = 0;
 
