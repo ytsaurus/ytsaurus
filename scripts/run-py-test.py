d2c187aed522131772cd561648c03d72076076ac
Dmitry Ermolov <ermolovd@ytsaurus.tech>
2018-06-28 18:32:54 +0300

    Helper script to run tests

---
+++ scripts/run-py-test.py
@@ -0,0 +1,26 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import os
+import sys
+import logging
+
+
+def main():
+    this_file_path = os.path.realpath(__file__)
+    repo_root = os.path.realpath(os.path.join(os.path.dirname(this_file_path), ".."))
+
+    ya_build = os.path.join(repo_root, "ya-build")
+    env = {
+        "PYTHONPATH": "{python}:{install_dir}".format(
+            python=os.path.join(repo_root, "python"),
+            install_dir=os.path.join(repo_root, "ya-build")),
+        "YT_BINARY_PATH": ya_build,
+        "PATH": ya_build + ":" + os.environ["PATH"],
+    }
+
+    args = sys.argv[1:] + [env]
+    os.execlpe("py.test", "py.test", *args)
+
+if __name__ == "__main__":
+    main()
