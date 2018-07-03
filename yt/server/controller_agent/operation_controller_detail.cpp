e72d63bbbb392575aaca668aeee794d8ed29cb3e
Maxim Akhmedov <max42@ytsaurus.tech>
2018-07-03 17:39:17 +0300

    Revert "Fix cores." This reverts commit f8633ef71622671a02fb04cb78b916188590c75e.
    Revert "Fix partition task persistense."
    This reverts commit 504182695b2cf3c0ee7ec2ad39591808523e4b3e.
    
    Revert "More fixes around data balancing violation."
    This reverts commit b5d85670f33d784e1e1b8ec7793fb5ee4388b82f.
    
    Revert "Add logging around data balancing violation."
    This reverts commit a1e5bc547f92bfb5738b1a20b47e60b65eb41c31.

--- yt/server/controller_agent/operation_controller_detail.cpp
+++ yt/server/controller_agent/operation_controller_detail.cpp
@@ -6906,11 +6906,6 @@ void TOperationControllerBase::ValidateUserFileCount(TUserJobSpecPtr spec, const
     }
 }
 
-void TOperationControllerBase::OnExecNodesUpdated(
-    const TRefCountedExecNodeDescriptorMapPtr& oldExecNodes,
-    const TRefCountedExecNodeDescriptorMapPtr& newExecNodes)
-{ }
-
 void TOperationControllerBase::GetExecNodesInformation()
 {
     auto now = NProfiling::GetCpuInstant();
@@ -6919,9 +6914,7 @@ void TOperationControllerBase::GetExecNodesInformation()
     }
 
     ExecNodeCount_ = Host->GetExecNodeCount();
-    auto newExecNodesDescriptors = Host->GetExecNodeDescriptors(NScheduler::TSchedulingTagFilter(Spec_->SchedulingTagFilter));
-    OnExecNodesUpdated(ExecNodesDescriptors_, newExecNodesDescriptors);
-    ExecNodesDescriptors_ = std::move(newExecNodesDescriptors);
+    ExecNodesDescriptors_ = Host->GetExecNodeDescriptors(NScheduler::TSchedulingTagFilter(Spec_->SchedulingTagFilter));
     GetExecNodesInformationDeadline_ = now + NProfiling::DurationToCpuDuration(Config->ControllerExecNodeInfoUpdatePeriod);
     LOG_DEBUG("Exec nodes information updated (ExecNodeCount: %v)", ExecNodeCount_);
 }
