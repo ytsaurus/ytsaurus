1d6bbd5a6c591dcaca16172d6399968beb5b7e86
Lev Vysotsky <levysotsky@yandex-team.ru>
2018-07-13 09:56:16 +0300

    Add 'pools' and 'has_failed_jobs' columns to ordered_by_start_time table

--- python-git/yt/environment/init_operation_archive.py
+++ python-git/yt/environment/init_operation_archive.py
@@ -824,6 +824,25 @@ ACTIONS[23] = [
     set_table_ttl_1week("fail_contexts"),
 ]
 
+TRANSFORMS[24] = [
+    Convert(
+        "ordered_by_start_time",
+        table_info=TableInfo([
+                ("start_time", "int64"),
+                ("id_hi", "uint64"),
+                ("id_lo", "uint64"),
+            ], [
+                ("operation_type", "string"),
+                ("state", "string"),
+                ("authenticated_user", "string"),
+                ("filter_factors", "string"),
+                ("pool", "string"),
+                ("pools", "any"),
+                ("has_failed_jobs", "boolean"),
+            ],
+            in_memory=True)),
+]
+
 def swap_table(client, target, source, version):
     backup_path = target + ".bak.{0}".format(version)
     has_target = False
