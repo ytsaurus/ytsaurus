debad66e6394bbea212f0a295e5fe5fa3f65fc85
Fedor Korotkiy <prime@yandex-team.ru>
2018-07-21 18:43:16 +0300

    Run copy_ya_proto.sh

--- yt/copy_ya_proto.sh
+++ yt/copy_ya_proto.sh
@@ -11,17 +11,17 @@ YT_PROTOS="
     yt/core/rpc/proto/rpc.proto
     yt/core/rpc/grpc/proto/grpc.proto
 
-    yt/client/hive/timestamp_map.proto
-    yt/client/hive/cell_directory.proto
-    yt/client/rpc_proxy/proto/api_service.proto
-    yt/client/rpc_proxy/proto/discovery_service.proto
+    yt/client/hive/proto/timestamp_map.proto
+    yt/client/hive/proto/cluster_directory.proto
+    yt/client/api/rpc_proxy/proto/api_service.proto
+    yt/client/api/rpc_proxy/proto/discovery_service.proto
 
     yt/client/node_tracker_client/proto/node.proto
 "
 
 PROTO_PATHS=""
 
-if [ -d "../.git" ]; then
+if [ -d "../.git" ] || [ -f "../.git" ]; then
     YT_PREFIX=""
 else
     YT_PREFIX="/yt/19_3"
@@ -34,4 +34,4 @@ for path in $YT_PROTOS; do
     python ../replace-imports.py <"..$YT_PREFIX/$path" >"ya_proto/yt_proto/$path"
 done
 
-echo -e "SET(PROTO_SRCS${PROTO_PATHS}\n)" >ya_proto/ya.make.inc
+echo "SET(PROTO_SRCS${PROTO_PATHS}\n)" >ya_proto/ya.make.inc
