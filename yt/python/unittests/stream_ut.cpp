c1f9525944da5081a6809a671257fb8062b4e9bd
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-07-27 13:04:03 +0300

    Add unittests for python bindings

---
+++ yt/python/unittests/stream_ut.cpp
@@ -0,0 +1,51 @@
+#include <yt/core/test_framework/framework.h>
+
+#include <yt/python/common/stream.h>
+
+#include <util/stream/mem.h>
+
+namespace NYT {
+namespace NPython {
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TStreamReaderTest, Simple)
+{
+    TMemoryInput input("Hello world!");
+    TStreamReader reader(&input, /*blockSize*/ 2);
+    
+    EXPECT_FALSE(reader.IsFinished()); 
+    EXPECT_EQ(reader.Current(), reader.Begin());
+
+    reader.Advance(2);
+    EXPECT_EQ(reader.Current(), reader.End());
+    
+    reader.RefreshBlock();
+    reader.Advance(2);
+    EXPECT_EQ(reader.Current(), reader.End());
+
+    EXPECT_EQ("H", ToString(reader.ExtractPrefix(1)));
+    EXPECT_EQ("el", ToString(reader.ExtractPrefix(2)));
+    
+    reader.RefreshBlock();
+    reader.Advance(2);
+    reader.RefreshBlock();
+    reader.Advance(2);
+    reader.RefreshBlock();
+    reader.Advance(2);
+    EXPECT_EQ("lo worl", ToString(reader.ExtractPrefix(reader.End())));
+    
+    reader.RefreshBlock();
+    reader.Advance(2);
+    EXPECT_EQ("d!", ToString(reader.ExtractPrefix(2)));
+
+    EXPECT_TRUE(reader.IsFinished());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NControllerAgent
+} // namespace NYT
+
