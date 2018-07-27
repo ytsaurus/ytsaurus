96c1decdaeda7cd4c64ad0af9ccf1016c642a8c6
Maxim Akhmedov <max42@ytsaurus.tech>
2018-07-27 17:40:16 +0300

    YT-9152: error code for data slice limit exceeded.

--- yt/server/chunk_pools/public.h
+++ yt/server/chunk_pools/public.h
@@ -16,6 +16,11 @@ DECLARE_REFCOUNTED_STRUCT(TChunkStripeList)
 
 DECLARE_REFCOUNTED_STRUCT(IChunkSliceFetcherFactory)
 
+// TODO(max42): add these codes to the documentation.
+DEFINE_ENUM(EErrorCode,
+    ((DataSliceLimitExceeded)(20000))
+);
+
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace NChunkPools
