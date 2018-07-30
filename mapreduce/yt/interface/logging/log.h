2229e84db758f2dfbc21834de82ec91d7c21024c
ermolovd <ermolovd@ytsaurus.tech>
2018-07-30 16:38:14 +0300

    Split logging Pull-request for branch users/ermolovd/local/06c19012f5

--- mapreduce/yt/interface/logging/log.h
+++ mapreduce/yt/interface/logging/log.h
@@ -1,37 +1,8 @@
 #pragma once
 
-#include <util/generic/ptr.h>
-#include <util/generic/string.h>
-#include <util/system/compat.h>
-#include <util/system/src_location.h>
+#include "logger.h"
 
 namespace NYT {
-
-////////////////////////////////////////////////////////////////////////////////
-
-class ILogger
-    : public TThrRefBase
-{
-public:
-    enum ELevel
-    {
-        FATAL /* "fatal", "FATAL" */,
-        // We don't have such level as `warning', but we support it for compatibility with other APIs.
-        ERROR /* "error", "warning", "ERROR", "WARNING" */,
-        INFO /* "info", "INFO" */,
-        DEBUG /* "debug", "DEBUG" */
-    };
-
-    virtual void Log(ELevel level, const TSourceLocation& sourceLocation, const char* format, va_list args) = 0;
-};
-
-using ILoggerPtr = ::TIntrusivePtr<ILogger>;
-
-void SetLogger(ILoggerPtr logger);
-ILoggerPtr GetLogger();
-
-ILoggerPtr CreateStdErrLogger(ILogger::ELevel cutLevel);
-ILoggerPtr CreateFileLogger(ILogger::ELevel cutLevel, const TString& path, bool append = false);
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -43,10 +14,10 @@ inline void LogMessage(ILogger::ELevel level, const TSourceLocation& sourceLocat
     va_end(args);
 }
 
-#define LOG_DEBUG(...) { NYT::LogMessage(NYT::ILogger::DEBUG, __LOCATION__, __VA_ARGS__); }
-#define LOG_INFO(...)  { NYT::LogMessage(NYT::ILogger::INFO, __LOCATION__, __VA_ARGS__); }
-#define LOG_ERROR(...) { NYT::LogMessage(NYT::ILogger::ERROR, __LOCATION__, __VA_ARGS__); }
-
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace NYT
+
+#define LOG_DEBUG(...) { NYT::LogMessage(NYT::ILogger::DEBUG, __LOCATION__, __VA_ARGS__); }
+#define LOG_INFO(...)  { NYT::LogMessage(NYT::ILogger::INFO, __LOCATION__, __VA_ARGS__); }
+#define LOG_ERROR(...) { NYT::LogMessage(NYT::ILogger::ERROR, __LOCATION__, __VA_ARGS__); }
--- mapreduce/yt/interface/logging/logger.cpp
+++ mapreduce/yt/interface/logging/logger.cpp
@@ -1,4 +1,4 @@
-#include "log.h"
+#include "logger.h"
 
 #include <util/datetime/base.h>
 
