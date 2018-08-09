33e835f63d807070a3fb11a6792af7d2d07ac1b1
Ruslan Savchenko <savrus@ytsaurus.tech>
2018-08-09 19:14:03 +0300

    Add YCHECK in tablet tracker where ASAN reported overflow

--- yt/server/tablet_server/tablet_tracker.cpp
+++ yt/server/tablet_server/tablet_tracker.cpp
@@ -330,7 +330,7 @@ class TTabletTracker::TImpl::TBalancedCandidateTracker
     : public TTabletTracker::TImpl::ICandidateTracker
 {
 public:
-    explicit TBalancedCandidateTracker(const TBootstrap* bootstrap, bool verboseLogging)
+    TBalancedCandidateTracker(const TBootstrap* bootstrap, bool verboseLogging)
         : Bootstrap_(bootstrap)
         , VerboseLogging_(verboseLogging)
     {
@@ -539,10 +539,6 @@ private:
 
     void InitNodes()
     {
-        Nodes_.clear();
-        PeerTracker_.Clear();
-        Actions_.clear();
-
         const auto& nodeTracker = Bootstrap_->GetNodeTracker();
         const auto& tabletManager = Bootstrap_->GetTabletManager();
 
@@ -627,6 +623,7 @@ private:
 
         auto& queue = it->second;
         for (auto& nodeIndex : queue) {
+            YCHECK(nodeIndex < Nodes_.size());
             auto* node = &Nodes_[nodeIndex];
             if (node->GetTotalSlots() == node->GetSlots().size()) {
                 std::swap(nodeIndex, queue.back());
