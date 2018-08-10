7e9eae4c55544fd101a5d01f1768a46676a89579
Maxim Babenko <babenko@ytsaurus.tech>
2018-08-10 20:02:25 +0300

    Move stuff around

--- yt/core/alloc/core.cpp
+++ yt/core/alloc/core.cpp
@@ -1,5 +1,9 @@
-#include "alloc.h"
+// This file contains the core parts of YTAlloc but no malloc/free-bridge.
+// The latter bridge is placed into alloc.cpp, which includes (sic!) core.cpp.
+// This ensures that YTAlloc/YTFree calls are properly inlined into malloc/free.
+// Also core.cpp can be directly included in, e.g., benchmarks.
 
+#include "alloc.h"
 
 #include <util/system/tls.h>
 #include <util/system/align.h>
@@ -2964,10 +2968,10 @@ TString FormatCounters()
 } // namespace NYTAlloc
 } // namespace NYT
 
+namespace NYT {
+
 ////////////////////////////////////////////////////////////////////////////////
 // Implementation of memory tags API
-
-namespace NYT {
 
 TMemoryTag GetCurrentMemoryTag()
 {
@@ -2991,137 +2995,7 @@ size_t GetMemoryUsageForTag(TMemoryTag tag)
     return result;
 }
 
+////////////////////////////////////////////////////////////////////////////////
+
 } // namespace NYT
 
-////////////////////////////////////////////////////////////////////////////////
-// Malloc bridge
-
-using namespace NYT::NYTAlloc;
-
-void* operator new(size_t size)
-{
-    return YTAlloc(size);
-}
-
-void* operator new(size_t size, const std::nothrow_t&) noexcept
-{
-    return YTAlloc(size);
-}
-
-void operator delete(void* ptr) noexcept
-{
-    YTFree(ptr);
-}
-
-void operator delete(void* ptr, const std::nothrow_t&) noexcept
-{
-    YTFree(ptr);
-}
-
-void* operator new[](size_t size)
-{
-    return YTAlloc(size);
-}
-
-void* operator new[](size_t size, const std::nothrow_t&) noexcept
-{
-    return YTAlloc(size);
-}
-
-void operator delete[](void* ptr) noexcept
-{
-    YTFree(ptr);
-}
-
-void operator delete[](void* ptr, const std::nothrow_t&) noexcept
-{
-    YTFree(ptr);
-}
-
-extern "C" void* malloc(size_t size)
-{
-    return YTAlloc(size);
-}
-
-extern "C" void* valloc(size_t size)
-{
-    return YTAllocPageAligned(size);
-}
-
-extern "C" void* aligned_alloc(size_t alignment, size_t size)
-{
-    // Alignment must be a power of two.
-    YCHECK((alignment & (alignment - 1)) == 0);
-    // Alignment must be exceeed page size.
-    YCHECK(alignment <= PageSize);
-    if (alignment <= 16) {
-        // Proper alignment here is automatic.
-        return YTAlloc(size);
-    } else {
-        return YTAllocPageAligned(size);
-    }
-}
-
-extern "C" void* pvalloc(size_t size)
-{
-    return valloc(AlignUp(size, PageSize));
-}
-
-extern "C" int posix_memalign(void** ptrPtr, size_t alignment, size_t size)
-{
-    *ptrPtr = aligned_alloc(alignment, size);
-    return 0;
-}
-
-extern "C" void* memalign(size_t alignment, size_t size)
-{
-    return aligned_alloc(alignment, size);
-}
-
-extern "C" void* __libc_memalign(size_t alignment, size_t size)
-{
-    return aligned_alloc(alignment, size);
-}
-
-extern "C" void free(void* ptr)
-{
-    YTFree(ptr);
-}
-
-extern "C" void* calloc(size_t n, size_t elemSize)
-{
-    // Overflow check.
-    auto size = n * elemSize;
-    if (elemSize != 0 && size / elemSize != n) {
-        return nullptr;
-    }
-
-    void* result = YTAlloc(size);
-    ::memset(result, 0, size);
-    return result;
-}
-
-extern "C" void cfree(void* ptr)
-{
-    YTFree(ptr);
-}
-
-extern "C" void* realloc(void* oldPtr, size_t newSize)
-{
-    if (!oldPtr) {
-        return YTAlloc(newSize);
-    }
-
-    if (newSize == 0) {
-        YTFree(oldPtr);
-        return nullptr;
-    }
-
-    void* newPtr = YTAlloc(newSize);
-    size_t oldSize = YTGetSize(oldPtr);
-    ::memcpy(newPtr, oldPtr, std::min(oldSize, newSize));
-    YTFree(oldPtr);
-    return newPtr;
-}
-
-////////////////////////////////////////////////////////////////////////////////
