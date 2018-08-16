0fd0c838b53720afaeaeddad5cf3434cddba52a0
Alexey Ozeritskiy <aozeritsky@yandex-team.ru>
2018-08-16 10:41:54 +0300

    YT-9273: Fix tests

--- yt/nodejs/lib/middleware/application_upravlyator.js
+++ yt/nodejs/lib/middleware/application_upravlyator.js
@@ -10,8 +10,9 @@ exports.that = function Middleware__YtApplicationUpravlyator()
     var logger = YtRegistry.get("logger");
     var driver = YtRegistry.get("driver");
     var authority = YtRegistry.get("authority");
+    var robot_yt_idm = YtRegistry.get("robot_yt_idm");
 
-    var app = new YtApplicationUpravlyator(logger, driver, authority);
+    var app = new YtApplicationUpravlyator(logger, driver, authority, robot_yt_idm);
 
     return function(req, rsp, next) {
         return Q.cast(app.dispatch(req, rsp, next)).done();
