20ace8cfaea5e158a8ae1b804b868b6a1f97c012
Maxim Akhmedov <max42@ytsaurus.tech>
2018-08-27 21:55:29 +0300

    Fix tests.

--- yt/tests/integration/tests/test_clickhouse.py
+++ yt/tests/integration/tests/test_clickhouse.py
@@ -1,4 +1,4 @@
-from yt_env_setup import wait, YTEnvSetup
+from yt_env_setup import wait, YTEnvSetup, require_ytserver_root_privileges
 from yt_commands import *
 from yt.wrapper.clickhouse import get_clickhouse_clique_spec_builder
 from yt.wrapper.common import sanitize_structure
@@ -12,10 +12,23 @@ import psutil
 import subprocess
 import random
 
+@require_ytserver_root_privileges
 class TestClickhouse(YTEnvSetup):
     NUM_MASTERS = 1
     NUM_NODES = 5
     NUM_SCHEDULERS = 1
+
+    DELTA_NODE_CONFIG = {
+        "exec_agent": {
+            "slot_manager": {
+                "job_environment": {
+                    "type": "cgroups",
+                    "memory_watchdog_period": 100,
+                    "supported_cgroups": ["cpuacct", "blkio", "cpu"],
+                },
+            }
+        }
+    }
 
     def _start_clique(self, instance_count, max_failed_job_count=1, **kwargs):
         spec_builder = get_clickhouse_clique_spec_builder(instance_count,
@@ -57,8 +70,8 @@ class TestClickhouse(YTEnvSetup):
 
         create("map_node", "//sys/clickhouse")
         if self._clickhouse_client_binary is None or self._ytserver_clickhouse_binary is None:
-            pytest.skipTest("This test requires built clickhouse and ytserver-clickhouse binaries; "
-                            "they are available only when using ya as a build system")
+            pytest.skip("This test requires built clickhouse and ytserver-clickhouse binaries; "
+                        "they are available only when using ya as a build system")
 
         create("map_node", "//sys/clickhouse/config_files")
         create("map_node", "//sys/clickhouse/configuration")
