ffdcdcc1f7ca89b363c0c24514245a2db919f24c
a-square <a-square@yandex-team.ru>
2018-08-30 16:34:41 +0300

    Добавил буферизованный логгер чтобы точно не тормозить прод

--- mapreduce/yt/interface/logging/logger.h
+++ mapreduce/yt/interface/logging/logger.h
@@ -33,4 +33,11 @@ ILoggerPtr GetLogger();
 ILoggerPtr CreateStdErrLogger(ILogger::ELevel cutLevel);
 ILoggerPtr CreateFileLogger(ILogger::ELevel cutLevel, const TString& path, bool append = false);
 
+/**
+ * Create logger that writes to a file in a buffered manner.
+ * It should result in fewer system calls (useful if you expect a lot of log messages),
+ * but in case of a crash, you would lose some log messages that haven't been flushed yet.
+ */
+ILoggerPtr CreateBufferedFileLogger(ILogger::ELevel cutLevel, const TString& path, bool append = false);
+
 } // namespace NYT
