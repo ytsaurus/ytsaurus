a5ce9677193f627191cdd8232fe15d429767d500
Fedor Korotkiy <prime@yandex-team.ru>
2018-08-31 16:06:07 +0300

    Terminate idle channels

--- yt/core/rpc/caching_channel_factory.cpp
+++ yt/core/rpc/caching_channel_factory.cpp
@@ -13,13 +13,103 @@ using namespace NYTree;
 
 ////////////////////////////////////////////////////////////////////////////////
 
+class TCachedChannel
+    : public IChannel
+{
+public:
+    TCachedChannel(
+        TCachingChannelFactory* factory,
+        IChannelPtr underlyingChannel,
+        const TString& address)
+        : Factory_(factory)
+        , UnderlyingChannel_(std::move(underlyingChannel))
+        , Address_(address)
+        , LastAccess_(TInstant::Now())
+    { }
+
+    virtual const TString& GetEndpointDescription() const override
+    {
+        return UnderlyingChannel_->GetEndpointDescription();
+    }
+
+    virtual const IAttributeDictionary& GetEndpointAttributes() const override
+    {
+        return UnderlyingChannel_->GetEndpointAttributes();
+    }
+
+    void Touch()
+    {
+        LastAccess_.store(TInstant::Now());
+    }
+
+    bool IsIdleSince(TInstant time)
+    {
+        return GetRefCount() == 1 && LastAccess_.load() < time;
+    }
+
+    TInstant GetLastAccess()
+    {
+        return LastAccess_.load();
+    }
+
+    virtual IClientRequestControlPtr Send(
+        IClientRequestPtr request,
+        IClientResponseHandlerPtr responseHandler,
+        const TSendOptions& options) override
+    {
+        return UnderlyingChannel_->Send(
+            std::move(request),
+            std::move(responseHandler),
+            options);
+    }
+
+    virtual TFuture<void> Terminate(const TError& error) override;
+
+private:
+    const TWeakPtr<TCachingChannelFactory> Factory_;
+    const IChannelPtr UnderlyingChannel_;
+    const TString Address_;
+
+    std::atomic<TInstant> LastAccess_;
+};
+
+DECLARE_REFCOUNTED_CLASS(TCachedChannel)
+DEFINE_REFCOUNTED_TYPE(TCachedChannel)
+
+////////////////////////////////////////////////////////////////////////////////
+
 class TCachingChannelFactory
-    : public IChannelFactory
+    : public ICachingChannelFactory
 {
 public:
     explicit TCachingChannelFactory(IChannelFactoryPtr underlyingFactory)
         : UnderlyingFactory_(std::move(underlyingFactory))
     { }
+
+    virtual void TerminateIdleChannels(TDuration ttl) override
+    {
+        std::vector<std::pair<TString, TCachedChannelPtr>> idleChannels;
+        auto lastActiveTime = TInstant::Now() - ttl;
+
+        {
+            TWriterGuard guard(SpinLock_);
+            for (const auto& channel : ChannelMap_) {
+                if (channel.second->IsIdleSince(lastActiveTime)) {
+                    idleChannels.push_back(channel);
+                }
+            }
+
+            for (const auto& idle : idleChannels) {
+                ChannelMap_.erase(idle.first);
+            }
+        }
+
+        auto error = TError("Channel is idle")
+            << TErrorAttribute("ttl", ttl);
+        for (const auto& idle : idleChannels) {
+            idle.second->Terminate(error);
+        }
+    }
 
     virtual IChannelPtr CreateChannel(const TString& address) override
     {
@@ -27,6 +117,7 @@ public:
             TReaderGuard guard(SpinLock_);
             auto it = ChannelMap_.find(address);
             if (it != ChannelMap_.end()) {
+                it->second->Touch();
                 return it->second;
             }
         }
@@ -50,7 +141,7 @@ public:
     {
         TWriterGuard guard(SpinLock_);
         auto it = ChannelMap_.find(address);
-        if (it != ChannelMap_.end() && it->second == channel) {
+        if (it != ChannelMap_.end() && it->second.Get() == channel) {
             ChannelMap_.erase(it);
         }
     }
@@ -59,59 +150,25 @@ private:
     const IChannelFactoryPtr UnderlyingFactory_;
 
     TReaderWriterSpinLock SpinLock_;
-    THashMap<TString, IChannelPtr> ChannelMap_;
-
-    class TCachedChannel
-        : public IChannel
-    {
-    public:
-        TCachedChannel(
-            TCachingChannelFactory* factory,
-            IChannelPtr underlyingChannel,
-            const TString& address)
-            : Factory_(factory)
-            , UnderlyingChannel_(std::move(underlyingChannel))
-            , Address_(address)
-        { }
-
-        virtual const TString& GetEndpointDescription() const override
-        {
-            return UnderlyingChannel_->GetEndpointDescription();
-        }
-
-        virtual const IAttributeDictionary& GetEndpointAttributes() const override
-        {
-            return UnderlyingChannel_->GetEndpointAttributes();
-        }
-
-        virtual IClientRequestControlPtr Send(
-            IClientRequestPtr request,
-            IClientResponseHandlerPtr responseHandler,
-            const TSendOptions& options) override
-        {
-            return UnderlyingChannel_->Send(
-                std::move(request),
-                std::move(responseHandler),
-                options);
-        }
-
-        virtual TFuture<void> Terminate(const TError& error) override
-        {
-            auto factory = Factory_.Lock();
-            if (factory) {
-                factory->EvictChannel(Address_, this);
-            }
-            return UnderlyingChannel_->Terminate(error);
-        }
-
-    private:
-        const TWeakPtr<TCachingChannelFactory> Factory_;
-        const IChannelPtr UnderlyingChannel_;
-        const TString Address_;
-    };
+    THashMap<TString, TCachedChannelPtr> ChannelMap_;
 };
 
-IChannelFactoryPtr CreateCachingChannelFactory(IChannelFactoryPtr underlyingFactory)
+DEFINE_REFCOUNTED_TYPE(TCachingChannelFactory)
+
+////////////////////////////////////////////////////////////////////////////////
+
+TFuture<void> TCachedChannel::Terminate(const TError& error)
+{
+    auto factory = Factory_.Lock();
+    if (factory) {
+        factory->EvictChannel(Address_, this);
+    }
+    return UnderlyingChannel_->Terminate(error);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+ICachingChannelFactoryPtr CreateCachingChannelFactory(IChannelFactoryPtr underlyingFactory)
 {
     YCHECK(underlyingFactory);
 
