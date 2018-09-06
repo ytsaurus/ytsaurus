a9b7844544b594275b1ce3f4fed6305b3abc2bb8
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-09-06 18:06:25 +0300

    Support on-fly update of SchedulingTagFilterExpireTimeout

--- yt/server/scheduler/node_shard.cpp
+++ yt/server/scheduler/node_shard.cpp
@@ -110,6 +110,7 @@ void TNodeShard::UpdateConfig(const TSchedulerConfigPtr& config)
 
     SubmitJobsToStrategyExecutor_->SetPeriod(config->NodeShardSubmitJobsToStrategyPeriod);
     CachedExecNodeDescriptorsRefresher_->SetPeriod(config->NodeShardExecNodesCacheUpdatePeriod);
+    CachedResourceStatisticsByTags_->SetPeriod(Config_->SchedulingTagFilterExpireTimeout);
 }
 
 IInvokerPtr TNodeShard::OnMasterConnected()
