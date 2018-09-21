7c3ee4b2fbd12006932ff6810e980cb055d4211b
Dmitry Ermolov <ermolovd@ytsaurus.tech>
2018-09-21 14:45:22 +0300

    Introduce perl deb packages

--- scripts/teamcity-build.py
+++ scripts/teamcity-build.py
@@ -502,6 +502,8 @@ def package(options, build_context):
                 "yandex-yt-master.json",
                 "yandex-yt-node.json",
                 "yandex-yt-proxy.json",
+                "yandex-yt-perl.json",
+                "yandex-yt-perl-abi.json",
                 "yandex-yt-scheduler.json",
                 "yandex-yt-src.json",
             ]
