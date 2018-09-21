c8de339534f217962c8afff54fa5842031f27c93
Maxim Babenko <babenko@ytsaurus.tech>
2018-09-21 09:47:16 +0300

    "replicas" are no longer opaque in Orchid

--- yt/server/tablet_node/tablet_manager.cpp
+++ yt/server/tablet_node/tablet_manager.cpp
@@ -2868,9 +2868,6 @@ private:
     void BuildReplicaOrchidYson(const TTableReplicaInfo& replica, TFluentAny fluent)
     {
         fluent
-            .BeginAttributes()
-                .Item("opaque").Value(true)
-            .EndAttributes()
             .BeginMap()
                 .Item("cluster_name").Value(replica.GetClusterName())
                 .Item("replica_path").Value(replica.GetReplicaPath())
