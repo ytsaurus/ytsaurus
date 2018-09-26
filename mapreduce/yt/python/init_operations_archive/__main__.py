bf3b6752a1102dc8a5219d0af6e8534d3b8e2e6a
levysotsky <levysotsky@yandex-team.ru>
2018-09-26 17:19:05 +0300

    Add init_operations_archive program Pull-request for branch users/levysotsky/operations_archive_in_yt_stuff

---
+++ mapreduce/yt/python/init_operations_archive/__main__.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python
+
+import argparse
+
+import yt.environment.init_operation_archive
+import yt.wrapper
+
+def wait(condition, timeout=30):
+    start_time = time.time()
+    while not condition():
+        time.sleep(0.1)
+        if time.time() - start_time > timeout:
+            raise Exception("wait timeout ({}) exceeded".format(timeout))
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("yt_address")
+    args = parser.parse_args()
+
+    client = yt.wrapper.YtClient()
+    client.config["proxy"]["url"] = args.yt_address
+    client.config["proxy"]["enable_proxy_discovery"] = False
+
+    cell_id = client.create("tablet_cell", attributes={"size": 1})
+    wait(lambda: client.get("//sys/tablet_cells/{0}/@health".format(cell_id)) == "good")
+
+    yt.environment.init_operation_archive.create_tables_latest_version(client)
