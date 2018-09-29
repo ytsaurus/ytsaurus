da9b130bac841146ebf2220c5c0d328b17cadc39
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-09-29 16:36:01 +0300

    Disable yandex-yt-fennel build

--- python-git/yandex-yt-fennel/setup.py
+++ python-git/yandex-yt-fennel/setup.py
@@ -1,21 +1,23 @@
-from helpers import get_version
+# This package has migrated to arcadia.
 
-from setuptools import setup
-
-def main():
-    scripts = [
-        "yt/fennel/bin/new_fennel/new_fennel.py",
-        "yt/fennel/bin/fennel_rotate/fennel_rotate.py"
-    ]
-
-    setup(
-        name = "yandex-yt-fennel",
-        version = get_version(),
-        packages = ["yt.fennel"],
-        scripts = scripts,
-        author = "Kolesnichenko Ignat",
-        author_email = "ignat@yandex-team.ru",
-    )
-
-if __name__ == "__main__":
-    main()
+#from helpers import get_version
+#
+#from setuptools import setup
+#
+#def main():
+#    scripts = [
+#        "yt/fennel/bin/new_fennel/new_fennel.py",
+#        "yt/fennel/bin/fennel_rotate/fennel_rotate.py"
+#    ]
+#
+#    setup(
+#        name = "yandex-yt-fennel",
+#        version = get_version(),
+#        packages = ["yt.fennel"],
+#        scripts = scripts,
+#        author = "Kolesnichenko Ignat",
+#        author_email = "ignat@yandex-team.ru",
+#    )
+#
+#if __name__ == "__main__":
+#    main()
