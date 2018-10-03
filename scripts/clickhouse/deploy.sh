66b8ba803f3589ac6123885ad5af10682dc6bbf9
Maxim Akhmedov <max42@ytsaurus.tech>
2018-10-03 22:58:50 +0300

    Set executable script in CH deployment script.

--- scripts/clickhouse/deploy.sh
+++ scripts/clickhouse/deploy.sh
@@ -3,3 +3,4 @@ VERSION="$($BIN --version)"
 CYPRESS_PATH="//sys/clickhouse/bin/ytserver-clickhouse-$VERSION"
 echo "Deploying $BIN of version $VERSION to $CYPRESS_PATH"
 cat $BIN | pv | yt write-file $CYPRESS_PATH
+yt set $CYPRESS_PATH/@executable "%true"
