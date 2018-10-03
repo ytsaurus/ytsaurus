fda18643ea170c7456485e58101e5faccbb367bc
Maxim Akhmedov <max42@ytsaurus.tech>
2018-10-03 20:00:57 +0300

    Working on scripts around clickhouse.

---
+++ scripts/clickhouse/deploy.sh
@@ -0,0 +1,5 @@
+BIN="/home/max42/yt_arc/build/ytserver-clickhouse"
+VERSION="$($BIN --version)"
+CYPRESS_PATH="//sys/clickhouse/bin/ytserver-clickhouse-$VERSION"
+echo "Deploying $BIN of version $VERSION to $CYPRESS_PATH"
+cat $BIN | pv | yt write-file $CYPRESS_PATH
