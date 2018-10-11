70a6a9e46ad23a59ae02bedef6248e5389442348
Ruslan Savchenko <savrus@ytsaurus.tech>
2018-10-11 12:38:52 +0300

    Skew store rotation time

--- yt/server/tablet_node/config.h
+++ yt/server/tablet_node/config.h
@@ -99,6 +99,7 @@ public:
     TNullable<ui64> ForcedCompactionRevision;
 
     TNullable<TDuration> DynamicStoreAutoFlushPeriod;
+    TDuration DynamicStoreFlushPeriodSkew;
     TNullable<TDuration> AutoCompactionPeriod;
 
     bool EnableLookupHashTable;
@@ -232,6 +233,8 @@ public:
 
         RegisterParameter("dynamic_store_auto_flush_period", DynamicStoreAutoFlushPeriod)
             .Default(TDuration::Minutes(15));
+        RegisterParameter("dynamic_store_period_skew", DynamicStoreFlushPeriodSkew)
+            .Default(TDuration::Minutes(5));
         RegisterParameter("auto_compaction_period", AutoCompactionPeriod)
             .Default(Null);
 
