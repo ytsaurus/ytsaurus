1e40347275b29a1fcc373071cc47e9c785b1d5bc
varkasha <varkasha@yandex-team.ru>
2018-10-19 21:35:23 +0300

    jstyle

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/PeriodicDiscovery.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/PeriodicDiscovery.java
@@ -2,7 +2,15 @@ package ru.yandex.yt.ytclient.proxy;
 
 import java.net.InetSocketAddress;
 import java.time.Duration;
-import java.util.*;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.HashMap;
+import java.util.HashSet;
+import java.util.List;
+import java.util.Map;
+import java.util.Objects;
+import java.util.Random;
+import java.util.Set;
 import java.util.concurrent.TimeUnit;
 import java.util.concurrent.atomic.AtomicBoolean;
 import java.util.stream.Collectors;
