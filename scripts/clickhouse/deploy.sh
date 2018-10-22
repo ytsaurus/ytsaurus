7ee129086886f6b1f6bb747d1c48890214976c12
Maxim Akhmedov <max42@ytsaurus.tech>
2018-10-22 19:00:29 +0300

    Fix deploy.sh CH script

--- scripts/clickhouse/deploy.sh
+++ scripts/clickhouse/deploy.sh
@@ -1,6 +1,9 @@
 BIN="/home/max42/yt_arc/build/ytserver-clickhouse"
 VERSION="$($BIN --version)"
 CYPRESS_PATH="//sys/clickhouse/bin/ytserver-clickhouse-$VERSION"
+CYPRESS_PATH_LINK="//sys/clickhouse/bin/ytserver-clickhosue"
 echo "Deploying $BIN of version $VERSION to $CYPRESS_PATH"
 cat $BIN | pv | yt write-file $CYPRESS_PATH
 yt set $CYPRESS_PATH/@executable "%true"
+yt set $CYPRESS_PATH/@version "\"$VERSION\""
+yt link --force "$CYPRESS_PATH" "$CYPRESS_PATH_LINK"
