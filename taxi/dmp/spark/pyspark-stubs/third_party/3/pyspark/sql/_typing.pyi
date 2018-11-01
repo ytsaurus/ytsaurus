e519f57bab9efeb0623b060baca094496156279a
zero323 <mszymkiewicz@gmail.com>
2018-11-01 23:40:32 +0100

    Define ForEachWriter protocols

--- taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/sql/_typing.pyi
+++ taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/sql/_typing.pyi
@@ -1,7 +1,9 @@
 from typing import Any, List, Optional, TypeVar, Union
+from typing_extensions import Protocol
 import datetime
 import decimal
 import pyspark.sql.column
+import pyspark.sql.types
 
 ColumnOrName = Union[pyspark.sql.column.Column, str]
 Literal = Union[bool, int, float, str]
@@ -9,5 +11,14 @@ DecimalLiteral = decimal.Decimal
 DateTimeLiteral = Union[datetime.datetime, datetime.date]
 LiteralType = TypeVar('LiteralType', bool, int, float, str)
 
+class SupportsOpen(Protocol):
+    def open(self, partition_id: int, epoch_id: int) -> bool:
+        ...
 
+class SupportsProcess(Protocol):
+    def process(self, row: pyspark.sql.types.Row) -> None:
+        ...
 
+class SupportsClose(Protocol):
+    def close(self, error: Exception) -> None:
+        ...
