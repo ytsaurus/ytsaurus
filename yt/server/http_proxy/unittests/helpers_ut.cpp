2567da3e3df494b02495ce2f7d4fd7f2c5754a93
Fedor Korotkiy <prime@yandex-team.ru>
2018-11-06 18:08:15 +0300

    Fix query string handling

--- yt/server/http_proxy/unittests/helpers_ut.cpp
+++ yt/server/http_proxy/unittests/helpers_ut.cpp
@@ -15,6 +15,8 @@ TEST(TTestParseQuery, Sample)
     ParseQueryString("path[10]=0");
 
     EXPECT_THROW(ParseQueryString("path[10]=0&path[a]=1"), TErrorException);
+
+    ParseQueryString("path=//tmp/home/comdep-analytics/chaos-ad/adhoc/advertanalytics/4468/round_geo_detailed2&output_format[$value]=schemaful_dsv&output_format[$attributes][enable_column_names_header]=true&output_format[$attributes][missing_value_mode]=print_sentinel&output_format[$attributes][missing_value_sentinel]=&output_format[$attributes][columns][0]=auctions_media&output_format[$attributes][columns][1]=auctions_perf&output_format[$attributes][columns][2]=auctions_unsold&output_format[$attributes][columns][3]=block_id&output_format[$attributes][columns][4]=block_shows_media&output_format[$attributes][columns][5]=block_shows_perf&output_format[$attributes][columns][6]=block_shows_unsold&output_format[$attributes][columns][7]=cpm_media&output_format[$attributes][columns][8]=cpm_perf&output_format[$attributes][columns][9]=domain_root&output_format[$attributes][columns][10]=has_geo_cpms");
 }
 
 ////////////////////////////////////////////////////////////////////////////////
