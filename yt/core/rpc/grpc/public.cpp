d75c94ec6cf6c5dff6bdd5d357222a0f26418c30
Maxim Babenko <babenko@ytsaurus.tech>
2018-11-06 23:20:49 +0300

    Pass user tickets via yt-auth-user-ticket GRPC metadata

--- yt/core/rpc/grpc/public.cpp
+++ yt/core/rpc/grpc/public.cpp
@@ -12,6 +12,7 @@ const char* const UserAgentMetadataKey = "user-agent";
 const char* const AuthTokenMetadataKey = "yt-auth-token";
 const char* const AuthSessionIdMetadataKey = "yt-auth-session-id";
 const char* const AuthSslSessionIdMetadataKey = "yt-auth-ssl-session-id";
+const char* const AuthUserTicketMetadataKey = "yt-auth-user-ticket";
 const char* const ErrorMetadataKey = "yt-error-bin";
 const char* const MessageBodySizeMetadataKey = "yt-message-body-size";
 const char* const ProtocolVersionMetadataKey = "yt-protocol-version";
