737f7551c68fc00c0877e35c91e4e5c461b9a69e
Maxim Babenko <babenko@ytsaurus.tech>
2018-11-17 15:10:43 +0300

    Cosmetics

--- yt/core/misc/config.h
+++ yt/core/misc/config.h
@@ -56,15 +56,15 @@ DEFINE_REFCOUNTED_TYPE(TSlruCacheConfig)
  * unless it is expired.
  *
  * An entry is considered expired if at least one of the following conditions is true:
- * 1) last access was more than expire_after_access_time miliseconds ago,
- * 2) last update was more than expire_after_*_update_time miliseconds ago.
+ * 1) last access was more than ExpireAfterAccessTime ago,
+ * 2) last update was more than ExpireAfter*UpdateTime ago.
  *
  * To avoid client awaiting time on subsequent requests and keep the response
  * up to date, the cache updates entries in the background:
- * If request was successful, the cache performs the same request after refresh_time
- * miliseconds and updates the entry.
+ * If request was successful, the cache performs the same request after RefreshTime
+ * and updates the entry.
  * If request was unsuccessful, the entry (which contains error response) will be expired
- * after expire_after_failed_update_time.
+ * after ExpireAfterFailedUpdateTime.
  */
 class TAsyncExpiringCacheConfig
     : public virtual NYTree::TYsonSerializable
