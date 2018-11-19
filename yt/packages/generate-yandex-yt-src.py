f07a8e5f407ca7c2d06840e91d1441bb5ffe31cf
Dmitry Ermolov <ermolovd@ytsaurus.tech>
2018-11-19 15:17:42 +0300

    Do not require git to build YT with ya

--- yt/packages/generate-yandex-yt-src.py
+++ yt/packages/generate-yandex-yt-src.py
@@ -2,15 +2,49 @@
 # -*- coding: utf-8 -*-
 
 import argparse
-import subprocess
 import json
+import os
+import re
+import subprocess
 import sys
+
+def warn_and_exit(msg):
+    msg = "Cannot build yandex-yt-src description.\n" + msg.strip("\n")
+
+    sys.stderr.write(msg)
+    sys.stderr.write("\n")
+
+    for line in msg.split("\n"):
+        print >>sys.stdout, "// " + line
+    exit(0)
+
+
+def parse_git_version(git_version_str):
+    m = re.search(r"\b(\d+)[.](\d+)[.](\d+)\b", git_version_str)
+    if m is None:
+        raise RuntimeError, "Cannot parse git version string: `{}'".format(git_version_str)
+    return tuple(int(m.group(i)) for i in xrange(1, 4))
+
 
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("src_root")
-
     args = parser.parse_args()
+
+    if not os.path.exists(os.path.join(args.src_root, ".git")):
+        warn_and_exit("{} is not a git repository".format(os.path.abspath(args.src_root)))
+
+    try:
+        git_version_str = subprocess.check_output(["git", "--version"])
+    except subprocess.CalledProcessError as e:
+        warn_and_exit("git is not available, cannot create source package")
+
+    if parse_git_version(git_version_str) < (2, 11, 0):
+        warn_and_exit(
+            "Looks like your version of git doesn't support\n"
+            "`--recurse-submodules' option for `ls-files' command\n"
+        )
+
     output = subprocess.check_output(["git", "ls-files", "--recurse-submodules"], cwd=args.src_root)
     file_list = output.strip("\n").split('\n')
     commit = subprocess.check_output(["git", "rev-parse", "--short", "HEAD"], cwd=args.src_root)
