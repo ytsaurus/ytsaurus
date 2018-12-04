fe16eca69afc5070fe9349dc4c81f97a80f5b696
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2018-12-04 14:19:31 +0300

    Remove deprecated code from yt_driver_bindings

--- python-git/yt_driver_bindings/driver.py
+++ python-git/yt_driver_bindings/driver.py
@@ -7,18 +7,3 @@ class Request(object):
         self.user = user
         self.id = id
         self.token = token
-
-# Deprecated, will be deleted when new version of yandex-yt-python become stable.
-from yt.common import YtResponseError
-
-def chunk_iter(stream, response, size):
-    while True:
-        if response.is_set():
-            if not response.is_ok():
-                raise YtResponseError(response.error())
-            else:
-                break
-        yield stream.read(size)
-
-    while not stream.empty():
-        yield stream.read(size)
