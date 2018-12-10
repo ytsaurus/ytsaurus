c05478d400da34efffe6a4f0685f479eab9f8d1e
Renat Khayretdinov <renadeen@ytsaurus.tech>
2018-12-10 18:39:37 +0300

    Dancing around test

--- yt/tests/integration/yt_helpers.py
+++ yt/tests/integration/yt_helpers.py
@@ -9,15 +9,25 @@ from yt_commands import get, YtError
 class ProfileMetric(object):
     def __init__(self, path):
         self.path = path
+        self.tags = {}
 
-    def _get(self):
+    def _read_from_cypress(self):
         try:
-            return get(self.path)
+            entries = get(self.path)
         except YtError:
             return []
 
+        if len(self.tags) == 0:
+            return entries
+        result = []
+        for entry in entries:
+            for tag in self.tags:
+                if tag in entry["tags"] and self.tags[tag] == entry["tags"][tag]:
+                    result.append(entry)
+        return result
+
     def __enter__(self):
-        self.len_on_enter = len(self._get())
+        self.len_on_enter = len(self._read_from_cypress())
         return self
 
     def __exit__(self, exc_type, exc_val, exc_tb):
@@ -25,7 +35,7 @@ class ProfileMetric(object):
             # False return value makes python re-raise the exception happened inside "with" section.
             return False
         else:
-            self.profile = self._get()
+            self.profile = self._read_from_cypress()
 
     def get(self):
         return self.profile[self.len_on_enter:]
@@ -46,3 +56,9 @@ class ProfileMetric(object):
     @staticmethod
     def at_node(node, path):
         return ProfileMetric("//sys/nodes/{0}/orchid/profiling/{1}".format(node, path))
+
+    def with_tag(self, tag_name, tag_value):
+        if self.len_on_enter is not None:
+            raise Exception("Cannot add tag. Profiling metric already used.")
+        self.tags[tag_name] = tag_value
+        return self
