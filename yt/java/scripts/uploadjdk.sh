4409d77d19a606174580ef603dff4098583fda05
aozeritsky <aozeritsky@yandex-team.ru>
2018-12-11 13:42:32 +0300

    Fixes

--- yt/java/scripts/uploadjdk.sh
+++ yt/java/scripts/uploadjdk.sh
@@ -9,10 +9,12 @@ exit() {
 versions=(yandex-jdk8,8.181-tzdata2018f,jdk_8.tar.gz yandex-jdk10,10.0.2-tzdata2018f,jdk_10.tar.gz yandex-openjdk11,11.0.1-tzdata2018f,jdk_11.tar.gz)
 clusters=(hahn arnold)
 
+IFSSAVED=$IFS
 for i in ${versions[*]}
 do
     IFS=","
     set $i
+    IFS=$IFSSAVED
     jdk=$1    
     version=$2
     tar=$3
@@ -34,18 +36,20 @@ do
     cwd=`pwd`    
     cd $dir
     mkdir -p usr/bin
-    for CMD in $(ls ${_JDK_INSTALL_DIR}/bin); do
-        if [ -f "${_JDK_INSTALL_DIR}/bin/${CMD}" ]
+    bindir="${_JDK_INSTALL_DIR}/bin"
+    bindir="${bindir:1}"
+    for CMD in $(ls -1 ${bindir}); do
+        if [ -f "${bindir}/${CMD}" ]
         then
-            ln -sf "${_JDK_INSTALL_DIR}/bin/${CMD}" usr/bin/${CMD} 
+            ln -sf "/${bindir}/${CMD}" usr/bin/${CMD} 
         fi
     done
     tar czvf $cwd/$tar *
     cd ..
     echo "uploading $tar"
-    IFS=" "
     for cluster in ${clusters[*]}
     do
+#        cat $tar | yt --proxy $cluster upload //porto_layers/${tar}.tmp
         yt --proxy $cluster remove -f //porto_layers/${tar}.tmp
         cat $tar | yt --proxy $cluster upload //porto_layers/${tar}.tmp && yt --proxy $cluster move -f //porto_layers/${tar}.tmp //porto_layers/${tar}
         yt --proxy $cluster remove -f //porto_layers/${tar}.tmp
