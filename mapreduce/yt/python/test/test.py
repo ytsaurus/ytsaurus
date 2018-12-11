9fc4d3420453c51e08645fd8b390e3249a9db1b9
ignat <ignat@ytsaurus.tech>
2018-12-11 18:23:55 +0300

    Fix tests

--- mapreduce/yt/python/test/test.py
+++ mapreduce/yt/python/test/test.py
@@ -2,34 +2,36 @@ from mapreduce.yt.python.yt_stuff import YtStuff
 
 from os import environ
 
+import yt.wrapper
+
 environ["YT_STUFF_MAX_START_RETRIES"] = "2"
 
 
 def test_start_stop():
-    yt = YtStuff()
-    yt.start_local_yt()
-    yt.stop_local_yt()
+    yt_stuff = YtStuff()
+    yt_stuff.start_local_yt()
+    yt_stuff.stop_local_yt()
 
 
 def test_fixture(yt_stuff):
     pass
 
 
-def test_yt_wrapper(yt_stuff):
+def test_yt_client(yt_stuff):
     yt_client = yt_stuff.get_yt_client()
     assert not yt_client.exists("//hello/world/path")
 
 
 def test_scheme(yt_stuff):
-    wrapper = yt_stuff.yt_wrapper
+    client = yt_stuff.get_yt_client()
 
-    wrapper.create_table(
+    client.create_table(
         "//test/schema_test",
         recursive=True
     )
-    wrapper.write_table(
+    client.write_table(
         "//test/schema_test",
         """{"a"=1}""",
-        format=wrapper.YsonFormat(),
+        format=yt.wrapper.YsonFormat(),
         raw=True
     )
