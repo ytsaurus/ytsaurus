c9ff0651c381b3f003194a831d2ff6268ad69cc9
Andrei Tonkikh <antonkikh@yandex-team.ru>
2018-12-20 18:30:01 +0300

    Add functions to create TExecNodes

--- yt/tools/scheduler_simulator/unittests/control_thread_ut.cpp
+++ yt/tools/scheduler_simulator/unittests/control_thread_ut.cpp
@@ -1,6 +1,6 @@
 #include <yt/core/test_framework/framework.h>
 
-#include "operation_controller.h"
+#include "control_thread.h"
 
 #include <yt/core/yson/null_consumer.h>
 
@@ -17,8 +17,43 @@ namespace {
 using namespace NScheduler;
 using namespace NControllerAgent;
 using namespace NConcurrency;
+using namespace NNodeTrackerClient;
 
 using NJobTrackerClient::EJobType;
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+
+TExecNodePtr CreateExecNode(TJobResources resourceLimits)
+{
+    auto node = New<TExecNode>(/* nodeId */ 1, TNodeDescriptor(ToString("node1")));
+    node->Tags().insert("internal");
+    node->SetResourceLimits(resourceLimits);
+
+    NNodeTrackerClient::NProto::TDiskResources diskResources;
+    auto* diskReport = diskResources.add_disk_reports();
+    diskReport->set_limit(100_GB);
+    diskReport->set_usage(0);
+    node->SetDiskInfo(diskResources);
+
+    return node;
+}
+
+TExecNodePtr CreateExecNode(i64 cpu, i64 memory)
+{
+    TJobResources resourceLimits;
+
+    resourceLimits.SetCpu(TCpuResource(cpu));
+    resourceLimits.SetMemory(memory);
+
+    resourceLimits.SetUserSlots(10'0000);
+    resourceLimits.SetNetwork(100'000);
+
+    return CreateExecNode(resourceLimits);
+}
+
+} // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -34,6 +69,12 @@ protected:
 TEST_F(TControlThreadTest, TestAssertTrue)
 {
     EXPECT_TRUE(true);
+}
+
+TEST_F(TControlThreadTest, TestSimpleAllocation)
+{
+
+
 }
 
 } // namespace
