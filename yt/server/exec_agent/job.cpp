70160bba3747c10fea03d5a9031060d5ed99b33f
Pavel Sushin <psushin@ytsaurus.tech>
2018-12-31 16:17:49 +0300

    YT-9964: add GPU health check

--- yt/server/exec_agent/job.cpp
+++ yt/server/exec_agent/job.cpp
@@ -408,7 +408,7 @@ public:
 
     virtual void SetProfile(const TJobProfile& value) override
     {
-        VERIFY_THREAD_AFFINITY(ControllerThread);
+        VERIFY_THREAD_AFFINITY(ControlThread);
         Profile_ = value;
     }
 
@@ -486,7 +486,7 @@ public:
 
     std::optional<TJobProfile> GetProfile()
     {
-        VERIFY_THREAD_AFFINITY(ControllerThread);
+        VERIFY_THREAD_AFFINITY(ControlThread);
 
         return Profile_;
     }
