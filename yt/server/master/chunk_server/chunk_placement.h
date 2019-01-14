7c3df5365a5e12d00fa661566fad8f9f20185816
Amir Shakurov <shakurov@ytsaurus.tech>
2019-01-14 16:00:38 +0300

    Fast cluster node selection by (DC, medium) in TChunkPlacement.
    TChunkPlacement used to index cluster nodes by medium. This made
    selecting nodes limited to specific data centers slow.
    
    Now cluster nodes are indexed by (DC, medium) pairs. This is
    implemented by storing fill- and load-factor maps separately for each
    such pair. When selecting nodes from multiple data centers,
    corresponding maps are merged on the fly.
    
    As an added benefit, selecting the maps to merge is now the only place
    where DCs are actually used during a call to AllocateWriteTargets. In
    other words, we never look into individual nodes' DC because the nodes
    are already partitioned by them.
    
    For consistency, the same pattern was applied to media: all the
    medium-related checks (is the medium cache? does the node even has
    this medium?) are factored out and performed only when a node is (or
    isn't) placed into a partition. Once it's there, AllocateWriteTargets
    doesn't have to concern itself with media that much - it must only
    select the right partitions.
    
    The above doesn't apply to preffered hosts - they may come not from
    load factor maps but elsewhere and thus may be unvetted. Therefore
    media and DC are still checked explicitly for preffered nodes.

--- yt/server/master/chunk_server/chunk_placement.h
+++ yt/server/master/chunk_server/chunk_placement.h
@@ -2,10 +2,15 @@
 
 #include "public.h"
 #include "chunk_replica.h"
+#include "medium.h"
 
 #include <yt/server/master/cell_master/public.h>
 
+#include <yt/server/master/node_tracker_server/data_center.h>
+#include <yt/server/master/node_tracker_server/node_tracker.h>
 #include <yt/server/master/node_tracker_server/node.h>
+
+#include <yt/client/object_client/helpers.h>
 
 #include <yt/core/misc/optional.h>
 #include <yt/core/misc/small_set.h>
@@ -13,6 +18,96 @@
 #include <util/generic/map.h>
 
 namespace NYT::NChunkServer {
+
+////////////////////////////////////////////////////////////////////////////////
+
+struct TPlacementDomain
+{
+    // Here, nullptr signifies 'null' data center.
+    const NNodeTrackerServer::TDataCenter* DataCenter;
+    const TMedium* Medium;
+
+    bool operator==(const TPlacementDomain& rhs) const;
+    size_t GetHash() const;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NChunkServer
+
+template <>
+struct THash<NYT::NChunkServer::TPlacementDomain>
+{
+    size_t operator()(const NYT::NChunkServer::TPlacementDomain& domain) const
+    {
+        return domain.GetHash();
+    }
+};
+
+namespace NYT::NChunkServer {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! An iterator-like device that merges (sorted) iterator ranges on the fly (i.e. lazily).
+/*!
+ *  The iterator is reusable and non-copyable to avoid unnecessary allocations.
+ *
+ *  NB: this may require some rewriting when ranges make it to the C++ standard.
+ *  However, keep in mind that the number of allocations should be kept to the
+ *  minimum here. This may be hard to achieve with the classic view-iterator approach.
+ */
+template <class T, class TCompare>
+class TReusableMergeIterator
+{
+private:
+    struct TRange
+    {
+        TRange(const T& begin, const T& end)
+            : Begin(begin)
+            , End(end)
+        { }
+
+        T Begin;
+        T End;
+    };
+
+    struct TRangeCompare;
+
+public:
+    TReusableMergeIterator() = default;
+
+    TReusableMergeIterator(const TReusableMergeIterator&) = delete;
+    TReusableMergeIterator& operator=(const TReusableMergeIterator&) = delete;
+
+    // U's begin and end should be T.
+    template <class U>
+    void AddRange(U&& range);
+
+    void Reset();
+
+    decltype(*std::declval<T>()) operator*();
+    decltype(&*std::declval<T>()) operator->();
+    bool IsValid();
+    TReusableMergeIterator& operator++();
+
+private:
+    // The ranges are sorted by the their first elements (from largest to smallest).
+    // Empty ranges are immediately removed.
+    std::vector<TRange> Ranges_;
+    bool IsIterationInProgress_;
+};
+
+struct TFillFactorToNodeMapItemComparator
+{
+    TFillFactorToNodeMapItemComparator() = default;
+
+    bool operator()(TFillFactorToNodeMap::const_reference a, TFillFactorToNodeMap::const_reference b)
+    {
+        return a.first < b.first;
+    }
+};
+
+using TLoadFactorToNodeMapItemComparator = TFillFactorToNodeMapItemComparator;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -26,9 +121,9 @@ public:
         TChunkManagerConfigPtr config,
         NCellMaster::TBootstrap* bootstrap);
 
-    void OnNodeRegistered(TNode* node);
     void OnNodeUnregistered(TNode* node);
     void OnNodeUpdated(TNode* node);
+    void OnNodeDataCenterChanged(TNode* node, const NNodeTrackerServer::TDataCenter* oldDataCenter);
     void OnNodeDisposed(TNode* node);
 
     TNodeList AllocateWriteTargets(
@@ -52,7 +147,7 @@ public:
 
     TNode* GetRemovalTarget(TChunkPtrWithIndexes chunkWithIndexes);
 
-    bool HasBalancingTargets(TMedium* medium, double maxFillFactor);
+    bool HasBalancingTargets(const TDataCenterSet& dataCenters, TMedium* medium, double maxFillFactor);
 
     std::vector<TChunkPtrWithIndexes> GetBalancingChunks(
         TMedium* medium,
@@ -79,20 +174,28 @@ private:
 
     const TChunkManagerConfigPtr Config_;
     NCellMaster::TBootstrap* const Bootstrap_;
+    TReusableMergeIterator<TFillFactorToNodeIterator, TFillFactorToNodeMapItemComparator> FillFactorToNodeIterator;
+    TReusableMergeIterator<TLoadFactorToNodeIterator, TLoadFactorToNodeMapItemComparator> LoadFactorToNodeIterator;
 
-    using TFillFactorToNodeMaps = TPerMediumArray<TFillFactorToNodeMap>;
-    using TLoadFactorToNodeMaps = TPerMediumArray<TLoadFactorToNodeMap>;
+    using TFillFactorToNodeMaps = THashMap<TPlacementDomain, TFillFactorToNodeMap>;
+    using TLoadFactorToNodeMaps = THashMap<TPlacementDomain, TLoadFactorToNodeMap>;
 
-    //! Nodes listed here must pass #IsValidBalancingTarget test.
-    TFillFactorToNodeMaps MediumToFillFactorToNode_;
-    //! Nodes listed here must pass #IsValidWriteTarget test.
-    TLoadFactorToNodeMaps MediumToLoadFactorToNode_;
+    //! Nodes listed here must pass #IsValidBalancingTargetToInsert test.
+    TFillFactorToNodeMaps DomainToFillFactorToNode_;
+    //! Nodes listed here must pass #IsValidWriteTargetToInsert test.
+    TLoadFactorToNodeMaps DomainToLoadFactorToNode_;
+
+    void OnNodeRegistered(TNode* node);
 
     void InsertToFillFactorMaps(TNode* node);
-    void RemoveFromFillFactorMaps(TNode* node);
+    void RemoveFromFillFactorMaps(
+        TNode* node,
+        std::optional<const NNodeTrackerServer::TDataCenter*> overrideDataCenter = std::nullopt);
 
     void InsertToLoadFactorMaps(TNode* node);
-    void RemoveFromLoadFactorMaps(TNode* node);
+    void RemoveFromLoadFactorMaps(
+        TNode* node,
+        std::optional<const NNodeTrackerServer::TDataCenter*> overrideDataCenter = std::nullopt);
 
     TNodeList GetWriteTargets(
         TMedium* medium,
@@ -111,27 +214,17 @@ private:
         TChunk* chunk,
         double maxFillFactor);
 
-    bool IsValidWriteTarget(
-        TMedium* medium,
-        TNode* node);
+    bool IsValidWriteTargetToInsert(TMedium* medium, TNode* node);
+    bool IsValidWriteTargetToAllocate(TNode* node, TTargetCollector* collector, bool enableRackAwareness);
+    bool IsValidWriteTargetCore(TNode* node);
+    // Preferred nodes are special: they don't come from load-factor maps and
+    // thus may not have been vetted by #IsValidWriteTargetToInsert. Thus,
+    // additional checking of their media and DC is required.
+    bool IsValidPreferredWriteTargetToAllocate(TNode* node, TMedium* medium, const TDataCenterSet* dataCenters);
 
-    bool IsValidWriteTarget(
-        TMedium* medium,
-        const TDataCenterSet* dataCenters,
-        TNode* node,
-        TTargetCollector* collector,
-        bool enableRackAwareness);
-
-    bool IsValidBalancingTarget(
-        TMedium* medium,
-        TNode* node);
-
-    bool IsValidBalancingTarget(
-        TMedium* medium,
-        const TDataCenterSet* dataCenters,
-        TNode* node,
-        TTargetCollector* collector,
-        bool enableRackAwareness);
+    bool IsValidBalancingTargetToInsert(TMedium* medium, TNode* node);
+    bool IsValidBalancingTargetToAllocate(TNode* node, TTargetCollector* collector, bool enableRackAwareness);
+    bool IsValidBalancingTargetCore(TNode* node);
 
     bool IsValidRemovalTarget(TNode* node);
 
@@ -140,6 +233,11 @@ private:
         int mediumIndex,
         NChunkClient::ESessionType sessionType);
 
+    void PrepareFillFactorIterator(const TDataCenterSet* dataCenters, const TMedium* medium);
+    void PrepareLoadFactorIterator(const TDataCenterSet* dataCenters, const TMedium* medium);
+
+    template <class T>
+    void ForEachDataCenter(const TDataCenterSet* dataCenters, T callback);
 };
 
 DEFINE_REFCOUNTED_TYPE(TChunkPlacement)
@@ -147,3 +245,7 @@ DEFINE_REFCOUNTED_TYPE(TChunkPlacement)
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace NYT::NChunkServer
+
+#define CHUNK_PLACEMENT_INL_H_
+#include "chunk_placement-inl.h"
+#undef CHUNK_PLACEMENT_INL_H_
