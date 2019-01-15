5aa54c50db14d51fff4319ed2c90cff9c4932ff8
arivkin <arivkin@ytsaurus.tech>
2019-01-15 16:01:25 +0300

    Changed pool for public_clique

--- scripts/clickhouse/start_public_clique.sh
+++ scripts/clickhouse/start_public_clique.sh
@@ -1,1 +1,1 @@
-yt start-clickhouse-clique --instance-count 20 --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool=babenko; alias="*ch_public"}'
+yt start-clickhouse-clique --instance-count 32 --cpu-limit 2 --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}'
