5c932a3f11f3d282c242d5067a984eb78a64ea29
Amir Shakurov <shakurov@ytsaurus.tech>
2019-01-16 18:58:49 +0300

    Bring back robust check for 'tablet_id' attr in TTableMountCacheBase.
    (The code was (erroneously) removed with the latest COMPAT cleanup.)

--- yt/client/tablet_client/table_mount_cache_detail.cpp
+++ yt/client/tablet_client/table_mount_cache_detail.cpp
@@ -146,8 +146,11 @@ std::pair<bool, TTabletInfoPtr> TTableMountCacheBase::InvalidateOnError(const TE
     if (!error.IsOK()) {
         for (auto code : retriableCodes) {
             if (auto retriableError = error.FindMatching(code)) {
-                auto tabletId = retriableError->Attributes().Get<TTabletId>("tablet_id");
-                auto tabletInfo = FindTablet(tabletId);
+                auto tabletId = retriableError->Attributes().Find<TTabletId>("tablet_id");
+                if (!tabletId) {
+                    continue;
+                }
+                auto tabletInfo = FindTablet(*tabletId);
                 if (tabletInfo) {
                     YT_LOG_DEBUG(error, "Invalidating tablet in table mount cache (TabletId: %v, CellId: %v, MountRevision: %llx, Owners: %v)",
                         tabletInfo->TabletId,
