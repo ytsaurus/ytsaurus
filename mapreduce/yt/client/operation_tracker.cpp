723fdc43005bd90c16c39b582f42a2eac524ea05
ilnurkh <ilnurkh@yandex-team.ru>
2019-01-23 11:14:19 +0300

    do not fail if trying to wait operation started in other process

--- mapreduce/yt/client/operation_tracker.cpp
+++ mapreduce/yt/client/operation_tracker.cpp
@@ -10,13 +10,12 @@ void TOperationExecutionTimeTracker::Start(const TOperationId& operationId) {
     }
 }
 
-TDuration TOperationExecutionTimeTracker::Finish(const TOperationId& operationId) {
+TMaybe<TDuration> TOperationExecutionTimeTracker::Finish(const TOperationId& operationId) {
     TDuration duration;
     with_lock(Lock_) {
         auto i = StartTimes_.find(operationId);
         if (i == StartTimes_.end()) {
-            ythrow yexception() <<
-                "Operation " << GetGuidAsString(operationId) << " did not start";
+            return Nothing();
         }
         duration = TInstant::Now() - i->second;
         StartTimes_.erase(i);
