40f484ce861145451d17a332dd4918bd9f74d490
Maxim Babenko <babenko@ytsaurus.tech>
2019-01-24 16:45:21 +0300

    YT-10192: Part2: more semantic fixes + tests

--- yt/tests/integration/tests/test_cypress.py
+++ yt/tests/integration/tests/test_cypress.py
@@ -1805,6 +1805,75 @@ class TestCypress(YTEnvSetup):
         assert revision > attributes_revision
         assert revision == content_revision
 
+    def test_user_attribute_removal1_yt_10192(self):
+        tx1 = start_transaction()
+        tx2 = start_transaction(tx=tx1)
+
+        create("map_node", "//tmp/test_node")
+        set("//tmp/test_node/@user_attribute", "some_string", tx=tx1)
+
+        remove("//tmp/test_node/@user_attribute", tx=tx2)
+
+        assert not exists("//tmp/test_node/@user_attribute", tx=tx2)
+        assert exists("//tmp/test_node/@user_attribute", tx=tx1)
+
+    def test_user_attribute_removal2_yt_10192(self):
+        tx1 = start_transaction()
+        tx2 = start_transaction(tx=tx1)
+
+        create("map_node", "//tmp/test_node")
+        set("//tmp/test_node/@user_attribute", "some_string1", tx=tx1)
+        set("//tmp/test_node/@user_attribute", "some_string2", tx=tx2)
+        remove("//tmp/test_node/@user_attribute", tx=tx2)
+        assert not exists("//tmp/test_node/@user_attribute", tx=tx2)
+
+        copy("//tmp/test_node", "//tmp/copy0")
+        assert not exists("//tmp/copy0/@user_attribute")
+
+        copy("//tmp/test_node", "//tmp/copy1", tx=tx1)
+        assert get("//tmp/copy1/@user_attribute", tx=tx1) == "some_string1"
+
+        copy("//tmp/test_node", "//tmp/copy2", tx=tx2)
+        assert not exists("//tmp/copy0/@user_attribute", tx=tx2)
+
+    def test_user_attribute_removal3_yt_10192(self):
+        tx1 = start_transaction()
+        tx2 = start_transaction(tx=tx1)
+        tx3 = start_transaction(tx=tx2)
+
+        create("map_node", "//tmp/test_node")
+        set("//tmp/test_node/@user_attribute", "some_string1", tx=tx1)
+        remove("//tmp/test_node/@user_attribute", tx=tx2)
+        with pytest.raises(YtError):
+            remove("//tmp/test_node/@user_attribute", tx=tx3)
+        assert not exists("//tmp/test_node/@user_attribute", tx=tx3)
+
+        copy("//tmp/test_node", "//tmp/copy0")
+        assert not exists("//tmp/copy0/@user_attribute")
+
+        copy("//tmp/test_node", "//tmp/copy1", tx=tx1)
+        assert get("//tmp/copy1/@user_attribute", tx=tx1) == "some_string1"
+
+        copy("//tmp/test_node", "//tmp/copy2", tx=tx2)
+        assert not exists("//tmp/copy0/@user_attribute", tx=tx2)
+
+        copy("//tmp/test_node", "//tmp/copy3", tx=tx3)
+        assert not exists("//tmp/copy0/@user_attribute", tx=tx3)
+
+    def test_user_attribute_removal4_yt_10192(self):
+        tx1 = start_transaction()
+
+        create("map_node", "//tmp/test_node")
+        set("//tmp/test_node/@user_attribute", "some_string1", tx=tx1)
+        remove("//tmp/test_node/@user_attribute", tx=tx1)
+        assert not exists("//tmp/test_node/@user_attribute", tx=tx1)
+
+        copy("//tmp/test_node", "//tmp/copy0")
+        assert not exists("//tmp/copy0/@user_attribute")
+
+        copy("//tmp/test_node", "//tmp/copy2", tx=tx1)
+        assert not exists("//tmp/copy0/@user_attribute", tx=tx1)
+
 ##################################################################
 
 class TestCypressMulticell(TestCypress):
@@ -1883,18 +1952,6 @@ class TestCypressWithoutSet(YTEnvSetup):
     def test_force_set(self):
         create("int64_node", "//tmp/integer")
         set("//tmp/integer", 20, force=True)
-
-    def test_user_attribute_removal_yt_10192(self):
-        tx1 = start_transaction()
-        tx2 = start_transaction(tx=tx1)
-
-        create("map_node", "//tmp/test_node")
-        set("//tmp/test_node/@user_attribute", "some_string", tx=tx1)
-
-        remove("//tmp/test_node/@user_attribute", tx=tx2)
-
-        assert not exists("//tmp/test_node/@user_attribute", tx=tx2)
-        assert exists("//tmp/test_node/@user_attribute", tx=tx1)
 
 ##################################################################
 
