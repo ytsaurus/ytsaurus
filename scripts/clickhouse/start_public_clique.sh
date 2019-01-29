0ef6bc022c60f1ef46e165b28e74db2a51eade66
Maxim Akhmedov <max42@ytsaurus.tech>
2019-01-29 22:51:55 +0300

    Add query log and monitoring to public clique start script.

--- scripts/clickhouse/start_public_clique.sh
+++ scripts/clickhouse/start_public_clique.sh
@@ -1,1 +1,2 @@
-yt start-clickhouse-clique --instance-count 32 --cpu-limit 2 --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}'
+yt start-clickhouse-clique --enable-monitoring --instance-count 32 --cpu-limit 2 --enable-query-log --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}' 
+
