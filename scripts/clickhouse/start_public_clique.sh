a51081a85daa8e638eac65289ed565753723d4b8
Maxim Akhmedov <max42@ytsaurus.tech>
2019-01-30 14:13:59 +0300

    More cpu cores to clickhouse public clique.

--- scripts/clickhouse/start_public_clique.sh
+++ scripts/clickhouse/start_public_clique.sh
@@ -1,2 +1,2 @@
-yt start-clickhouse-clique --enable-monitoring --instance-count 32 --cpu-limit 2 --enable-query-log --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}' 
+yt start-clickhouse-clique --enable-monitoring --instance-count 16 --cpu-limit 8 --enable-query-log --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}' 
 
