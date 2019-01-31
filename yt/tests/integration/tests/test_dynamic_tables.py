3582e7e014e9d1d9182bd2df3f970a663b6b1f90
Maxim Babenko <babenko@ytsaurus.tech>
2019-01-31 21:49:42 +0300

    More robust peer liveness checks

--- yt/tests/integration/tests/test_dynamic_tables.py
+++ yt/tests/integration/tests/test_dynamic_tables.py
@@ -181,19 +181,22 @@ class DynamicTablesSingleCellBase(DynamicTablesBase):
             insert_rows("//tmp/t", rows)
             assert lookup_rows("//tmp/t", keys) == rows
 
-    def _check_cell_good_after_peer_decommission(self, cell_id):
-        if get("#{0}/@health".format(cell_id)) != "good":
-            return False
-
-        peers = get("#{0}/@peers".format(cell_id))
-        for peer in peers:
-            if "address" not in peer:
-                continue
-            address = peer["address"]
-            if get("//sys/nodes/{0}/@decommissioned".format(address)):
+    def _wait_for_cells_good_after_peer_decommission(self, cell_id):
+        def check():
+            if get("#{0}/@health".format(cell_id)) != "good":
                 return False
 
-        return True
+            peers = get("#{0}/@peers".format(cell_id))
+            for peer in peers:
+                if "address" not in peer:
+                    return False
+                address = peer["address"]
+                if get("//sys/nodes/{0}/@decommissioned".format(address)):
+                    return False
+
+            return True
+
+        wait(check)
 
     def test_follower_catchup(self):
         create_tablet_cell_bundle("b", attributes={"options": {"peer_count" : 2}})
@@ -206,7 +209,7 @@ class DynamicTablesSingleCellBase(DynamicTablesBase):
         follower_address = list(x["address"] for x in peers if x["state"] == "following")[0]
 
         set_node_decommissioned(follower_address, True)
-        wait(lambda: self._check_cell_good_after_peer_decommission(cell_id))
+        self._wait_for_cells_good_after_peer_decommission(cell_id)
 
         for i in xrange(0, 100):
             rows = [{"key": i, "value": "test"}]
@@ -230,7 +233,7 @@ class DynamicTablesSingleCellBase(DynamicTablesBase):
         follower_address = list(x["address"] for x in peers if x["state"] == "following")[0]
 
         set_node_decommissioned(leader_address, True)
-        wait(lambda: self._check_cell_good_after_peer_decommission(cell_id))
+        self._wait_for_cells_good_after_peer_decommission(cell_id)
 
         assert get("#" + cell_id + "/@health") == "good"
         peers = get("#" + cell_id + "/@peers")
@@ -253,7 +256,7 @@ class DynamicTablesSingleCellBase(DynamicTablesBase):
         cell_id = ls("//sys/tablet_cells")[0]
 
         self._decommission_all_peers(cell_id)
-        wait(lambda: self._check_cell_good_after_peer_decommission(cell_id))
+        self._wait_for_cells_good_after_peer_decommission(cell_id)
 
         assert lookup_rows("//tmp/t", keys) == rows
 
