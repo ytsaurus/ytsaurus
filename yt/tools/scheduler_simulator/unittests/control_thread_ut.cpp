31bec75dc076c10abb5cd8c81d6a4b9d1aadcadc
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-02-01 15:21:14 +0300

    Disable scheduler simulator unit tests

--- yt/tools/scheduler_simulator/unittests/control_thread_ut.cpp
+++ yt/tools/scheduler_simulator/unittests/control_thread_ut.cpp
@@ -230,7 +230,7 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TEST_F(TControlThreadTest, TestSingleOperation)
+TEST_F(TControlThreadTest, DISABLED_TestSingleOperation)
 {
     const auto earliestTime = TInstant::ParseIso8601("2019-01-01T12:00:00+00:00");
     auto simulatorConfig = New<TSchedulerSimulatorConfig>();
@@ -266,7 +266,7 @@ TEST_F(TControlThreadTest, TestSingleOperation)
     statisticsOutput.ExpectNoPreemption(operations[0]);
 }
 
-TEST_F(TControlThreadTest, TestTwoComplementaryOperations)
+TEST_F(TControlThreadTest, DISABLED_TestTwoComplementaryOperations)
 {
     const auto earliestTime = TInstant::ParseIso8601("2019-01-01T12:00:00+00:00");
     auto simulatorConfig = New<TSchedulerSimulatorConfig>();
@@ -309,7 +309,7 @@ TEST_F(TControlThreadTest, TestTwoComplementaryOperations)
     statisticsOutput.ExpectNoPreemption(operations[1]);
 }
 
-TEST_F(TControlThreadTest, TestNormalPreemption)
+TEST_F(TControlThreadTest, DISABLED_TestNormalPreemption)
 {
     const auto earliestTime = TInstant::ParseIso8601("2019-01-01T12:00:00+00:00");
     auto simulatorConfig = New<TSchedulerSimulatorConfig>();
