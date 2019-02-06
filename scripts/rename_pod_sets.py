49979d3c26fb984c3f44d0cfbda831b6afcca648
avitella <avitella@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-02-06 11:54:49 +0000

    Decrease boilerplate and dangarous code (temptation to access //yp/db directly) in migrations.
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@4406598 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- scripts/rename_pod_sets.py
+++ scripts/rename_pod_sets.py
@@ -1,7 +1,7 @@
 import argparse
 import sys
 
-from yp.local import TablePreparer, get_db_version, backup_yp
+from yp.local import DbManager, get_db_version, backup_yp
 from yt.wrapper import YtClient
 
 from yp_proto.yp.client.api.proto.object_type_pb2 import EObjectType
@@ -25,10 +25,10 @@ def main(argv):
     if args.commit:
         backup_yp(yt_client, args.yp_path)
 
-    table_preparer = TablePreparer(yt_client, args.yp_path, version=get_db_version(yt_client, args.yp_path))
+    db_manager = DbManager(yt_client, args.yp_path, version=get_db_version(yt_client, args.yp_path))
 
     pod_sets_mapping = {}
-    for row in yt_client.select_rows("[meta.id], [labels] from [{}]".format(table_preparer.get_table_path("pod_sets"))):
+    for row in yt_client.select_rows("[meta.id], [labels] from [{}]".format(db_manager.get_table_path("pod_sets"))):
         if row.get("labels", {}).get("deploy_engine", "") != "YP_LITE":
             continue
         if row["meta.id"] == row["labels"]["nanny_service_id"]:
@@ -43,19 +43,19 @@ def main(argv):
 
     def modify_pod_sets(row):
         yield modify_pod_set_id(row, "meta.id", pod_sets_mapping)
-    table_preparer.run_map("pod_sets", modify_pod_sets)
-    
+    db_manager.run_map("pod_sets", modify_pod_sets)
+
     def modify_pods(row):
         yield modify_pod_set_id(row, "meta.pod_set_id", pod_sets_mapping)
-    table_preparer.run_map("pods", modify_pods)
+    db_manager.run_map("pods", modify_pods)
 
     def modify_node_segment_to_pod_sets(row):
         yield modify_pod_set_id(row, "pod_set_id", pod_sets_mapping)
-    table_preparer.run_map("node_segment_to_pod_sets", modify_node_segment_to_pod_sets)
+    db_manager.run_map("node_segment_to_pod_sets", modify_node_segment_to_pod_sets)
 
     def modify_account_to_pod_sets(row):
         yield modify_pod_set_id(row, "pod_set_id", pod_sets_mapping)
-    table_preparer.run_map("account_to_pod_sets", modify_account_to_pod_sets)
+    db_manager.run_map("account_to_pod_sets", modify_account_to_pod_sets)
 
     def modify_parents(row):
         if row["object_type"] == EObjectType.Value("OT_POD"):
@@ -63,7 +63,7 @@ def main(argv):
         else:
             yield row
 
-    table_preparer.finalize()
+    db_manager.finalize()
 
 if __name__ == "__main__":
     main(sys.argv[1:])
