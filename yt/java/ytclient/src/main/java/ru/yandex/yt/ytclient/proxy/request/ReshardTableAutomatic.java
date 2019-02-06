1afe21b1389e6fd1ae39af8f3bc3309dd48df7c2
aozeritsky <aozeritsky@yandex-team.ru>
2019-02-06 18:38:26 +0300

    YT-10269: Add new methods stage 1

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/ReshardTableAutomatic.java
@@ -0,0 +1,22 @@
+package ru.yandex.yt.ytclient.proxy.request;
+
+import ru.yandex.yt.rpcproxy.TReqReshardTableAutomatic;
+
+public class ReshardTableAutomatic extends TableReq<ReshardTableAutomatic> {
+    private boolean keepActions = false;
+
+    public ReshardTableAutomatic(String path, boolean keepActions) {
+        super(path); this.keepActions = keepActions;
+    }
+
+    public ReshardTableAutomatic setKeepActions(boolean keepActions) {
+        this.keepActions = keepActions;
+        return this;
+    }
+
+    public TReqReshardTableAutomatic.Builder writeTo(TReqReshardTableAutomatic.Builder builder) {
+        super.writeTo(builder);
+        builder.setKeepActions(keepActions);
+        return builder;
+    }
+}
---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/StartOperation.java
@@ -0,0 +1,53 @@
+package ru.yandex.yt.ytclient.proxy.request;
+
+import com.google.protobuf.ByteString;
+
+import ru.yandex.inside.yt.kosher.impl.ytree.serialization.YTreeBinarySerializer;
+import ru.yandex.inside.yt.kosher.ytree.YTreeNode;
+import ru.yandex.yt.rpcproxy.EOperationType;
+import ru.yandex.yt.rpcproxy.TMutatingOptions;
+import ru.yandex.yt.rpcproxy.TReqStartOperation;
+import ru.yandex.yt.rpcproxy.TTransactionalOptions;
+
+public class StartOperation {
+    private final EOperationType type;
+    private final YTreeNode spec;
+
+    private TransactionalOptions transactionalOptions;
+    private MutatingOptions mutatingOptions;
+
+    public StartOperation(EOperationType type, YTreeNode spec) {
+        this.type = type;
+        this.spec = spec;
+    }
+
+    public StartOperation setTransactionOptions(TransactionalOptions transactionalOptions) {
+        this.transactionalOptions = transactionalOptions;
+        return this;
+    }
+
+    public StartOperation setMutatingOptions(MutatingOptions mutatingOptions) {
+        this.mutatingOptions = mutatingOptions;
+        return this;
+    }
+
+    public TReqStartOperation.Builder writeTo(TReqStartOperation.Builder builder) {
+        ByteString.Output output = ByteString.newOutput();
+
+        YTreeBinarySerializer.serialize(spec, output);
+
+        builder
+                .setType(type)
+                .setSpec(output.toByteString());
+
+        if (transactionalOptions != null) {
+            builder.setTransactionalOptions(transactionalOptions.writeTo(TTransactionalOptions.newBuilder()));
+        }
+
+        if (mutatingOptions != null) {
+            builder.setMutatingOptions(mutatingOptions.writeTo(TMutatingOptions.newBuilder()));
+        }
+
+        return builder;
+    }
+}
