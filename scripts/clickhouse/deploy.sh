67f34e2663f5d39dea76503fcc8212bd8c880f07
Maxim Akhmedov <max42@ytsaurus.tech>
2019-02-11 17:06:30 +0300

    Add more fancy clickhouse deployment scripts.

--- scripts/clickhouse/deploy.sh
+++ scripts/clickhouse/deploy.sh
@@ -1,9 +1,7 @@
 BIN="/home/max42/yt/build-rel/bin/ytserver-clickhouse"
 VERSION="$($BIN --version)"
 CYPRESS_PATH="//sys/clickhouse/bin/ytserver-clickhouse-$VERSION"
-CYPRESS_PATH_LINK="//sys/clickhouse/bin/ytserver-clickhouse"
 echo "Deploying $BIN of version $VERSION to $CYPRESS_PATH"
 cat $BIN | pv | yt write-file $CYPRESS_PATH
 yt set $CYPRESS_PATH/@executable "%true"
 yt set $CYPRESS_PATH/@version "\"$VERSION\""
-yt link --force "$CYPRESS_PATH" "$CYPRESS_PATH_LINK"
