20242317858b9230610aeb091533ccb0b8c7ca80
Maxim Akhmedov <max42@ytsaurus.tech>
2019-02-17 01:01:43 +0300

    Extract {,native} client cache to {client/api,ytlib/api/native}/client_cache.h, use former in TDriver.

---
+++ yt/client/api/client_cache.h
@@ -0,0 +1,50 @@
+#pragma once
+
+#include "private.h"
+
+#include <yt/core/misc/sync_cache.h>
+
+namespace NYT::NApi {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TCachedClient
+    : public TSyncCacheValueBase<TString, TCachedClient>
+{
+public:
+    TCachedClient(
+        const TString& user,
+        IClientPtr client);
+
+    const IClientPtr& GetClient();
+
+private:
+    const IClientPtr Client_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! An SLRU-cache based class for keeping a cache of clients for different users.
+/*
+ *  For NApi::NNative::IClient equivalent see ytlib/api/native/client_cache.h.
+ *
+ *  Cache is completely thread-safe.
+ */
+class TClientCache
+    : public TSyncSlruCacheBase<TString, TCachedClient>
+{
+public:
+    TClientCache(TSlruCacheConfigPtr config, IConnectionPtr connection, std::optional<TString> token = std::nullopt);
+
+    IClientPtr GetClient(const TString& user, const std::optional<TString>& userToken = std::nullopt);
+
+private:
+    IConnectionPtr Connection_;
+    std::optional<TString> Token_;
+};
+
+DEFINE_REFCOUNTED_TYPE(TClientCache);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NApi
---
+++ yt/ytlib/api/native/client_cache.h
@@ -0,0 +1,30 @@
+#include "private.h"
+
+#include <yt/ytlib/api/native/connection.h>
+
+#include <yt/client/api/client_cache.h>
+
+namespace NYT::NApi::NNative {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! An SLRU-cache based class for keeping a cache of clients for different users.
+/*
+ *  For NApi::IClient equivalent see client/api/client_cache.h.
+ *
+ *  Cache is completely thread-safe.
+ */
+class TClientCache
+    : public NApi::TClientCache
+{
+public:
+    TClientCache(TSlruCacheConfigPtr config, NApi::NNative::IConnectionPtr connection, std::optional<TString> token = std::nullopt);
+
+    IClientPtr GetClient(const TString& user, const std::optional<TString>& token = std::nullopt);
+};
+
+DEFINE_REFCOUNTED_TYPE(TClientCache)
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NApi::NNative
