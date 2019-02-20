fa22f56d8e5735356dd361b68f57bd89b978bb1d
ndnuriev <ndnuriev@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-02-20 12:58:38 +0000

    script yp-create-node-segment added.
    Note: mandatory check (NEED_CHECK) was skipped
    
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@4483084 41d65440-b5be-11dd-afe3-b2e846d9b4f8

---
+++ scripts/create_node_segment/__main__.py
@@ -0,0 +1,43 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+from yp.client import YpClient, find_token
+from yp.cli_helpers import ParseStructuredArgument
+from yt.common import get_value, update
+
+import argparse
+
+
+def main_impl(yp_client, arguments):
+    attributes  = {
+        "meta": {
+            "id": arguments.id,
+            "inherit_acl": arguments.inherit_acl,
+        },
+        "spec": {
+            "node_filter": "[/labels/segment]=\"{}\"".format(arguments.id),
+        }
+    }
+
+    yp_client.create_object("node_segment", attributes=attributes)
+
+
+def main(arguments):
+    config = get_value(arguments.config, {})
+    config = update(dict(token=find_token()), config)
+    with YpClient(arguments.address, "grpc", config) as yp_client:
+        main_impl(yp_client, arguments)
+
+
+def parse_arguments():
+    parser = argparse.ArgumentParser(description="Create node segment")
+    parser.add_argument("--address", required=True, help="Address of yp backend")
+    parser.add_argument("--config", required=False, help="YP client config", action=ParseStructuredArgument)
+    parser.add_argument("--id", required=True, help="Id of node segment")
+    parser.add_argument("--inherit-acl", required=False, default=False, action="store_true", help="Inherit acl")
+
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main(parse_arguments())
