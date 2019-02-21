109ee2c7405347a62c421570de010d8698d581f7
aozeritsky <aozeritsky@yandex-team.ru>
2019-02-21 16:58:37 +0300

    YQL-7059: Close clients

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/DestinationsSelector.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/DestinationsSelector.java
@@ -1,11 +1,12 @@
 package ru.yandex.yt.ytclient.proxy;
 
+import java.io.Closeable;
 import java.util.List;
 
 import ru.yandex.yt.ytclient.rpc.RpcClient;
 import ru.yandex.yt.ytclient.rpc.RpcOptions;
 
-public abstract class DestinationsSelector extends ApiServiceClient {
+public abstract class DestinationsSelector extends ApiServiceClient implements Closeable {
     public DestinationsSelector(RpcOptions options) {
         super(options);
     }
