9d694985dccdd034c47b06a25cb0bd364221eb6b
ignat <ignat@ytsaurus.tech>
2019-02-22 01:41:20 +0300

    Fix init_operations_archive recipe

--- mapreduce/yt/python/init_operations_archive/recipe/__main__.py
+++ mapreduce/yt/python/init_operations_archive/recipe/__main__.py
@@ -1,5 +1,3 @@
-import tempfile
-
 from library.python.testing.recipe import declare_recipe
 
 from mapreduce.yt.python.recipe.lib import start, stop
@@ -9,44 +7,36 @@ from mapreduce.yt.python.yt_stuff import YtConfig
 import yatest.common
 
 def start_and_init(args):
-    scheduler_config = tempfile.NamedTemporaryFile(delete=False)
-    scheduler_config.write("""
-        {
-            "scheduler" = {
-                "operations_cleaner"= {
-                    "enable"= %true;
-                    "analysis_period"= 100;
-                };
-                "enable_job_reporter" = %true;
-                "enable_job_stderr_reporter" = %true;
-                "enable_job_fail_context_reporter" = %true;
-                "enable_job_spec_reporter" = %true;
-            };
-        }
-    """)
-    scheduler_config.close()
+    scheduler_config = {
+        "scheduler": {
+            "operations_cleaner": {
+                "enable": True,
+                "analysis_period": 100,
+            },
+            "enable_job_reporter": True,
+            "enable_job_stderr_reporter": True,
+            "enable_job_fail_context_reporter": True,
+            "enable_job_spec_reporter": True,
+        },
+    }
 
-    node_config = tempfile.NamedTemporaryFile(delete=False)
-    node_config.write("""
-        {
-            "exec_agent"= {
-                "statistics_reporter"= {
-                    "enabled" = %true;
-                    "reporting_period" = 10;
-                    "min_repeat_delay" = 10;
-                    "max_repeat_delay" = 10;
-                };
-            };
-            "tablet_node" = {
-                "resource_limits" = {
-                    "slots" = 2;
-                };
-            };
-        }
-    """)
-    node_config.close()
+    node_config = {
+        "exec_agent": {
+            "statistics_reporter": {
+                "enabled": True,
+                "reporting_period": 10,
+                "min_repeat_delay": 10,
+                "max_repeat_delay": 10,
+            },
+        },
+        "tablet_node": {
+            "resource_limits": {
+                "slots": 2,
+            },
+        },
+    }
 
-    yt = start(args, YtConfig(scheduler_config=scheduler_config.name, node_config=node_config.name))
+    yt = start(args, YtConfig(scheduler_config=scheduler_config, node_config=node_config))
     yt_address = "localhost:" + str(yt.yt_proxy_port)
 
     yatest.common.execute([
