0ef3e52323dba20430b122eeb300a543740f76cd
Pavel Kiselev <kiselyovp@yandex-team.ru>
2019-02-26 18:44:00 +0300

    YT-8151, YT-9871: Batch ModifyRows requests in RPC proxy client and reorder them at RPC proxy server.
    Batching is enabled by the modify_rows_batch_capacity field in client config.

--- yt/tests/cpp/api_test_base.h
+++ yt/tests/cpp/api_test_base.h
@@ -26,6 +26,7 @@ protected:
 
     static void CreateClient(const TString& userName);
 };
+
 ////////////////////////////////////////////////////////////////////////////////
 
 class TDynamicTablesTestBase
