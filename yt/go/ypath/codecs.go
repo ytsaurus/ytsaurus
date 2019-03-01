6c1a8013b4df5c14a01763daf76ef997c6703ea7
prime <prime@yandex-team.ru>
2019-03-01 19:37:22 +0300

    Erasure and compression codecs. Note: mandatory check (NEED_CHECK) was skipped

---
+++ yt/go/ypath/codecs.go
@@ -0,0 +1,71 @@
+package ypath
+
+import "fmt"
+
+type CompressionCodec string
+
+func (c *CompressionCodec) UnmarshalText(text []byte) error {
+	*c = CompressionCodec(text)
+	return nil
+}
+
+func (c CompressionCodec) MarshalText() (text []byte, err error) {
+	return []byte(c), nil
+}
+
+func compressionLevel(codec string, n, max, min int) CompressionCodec {
+	if n < min {
+		n = min
+	}
+
+	if n > max {
+		n = max
+	}
+
+	return CompressionCodec(fmt.Sprintf("%s_%d", codec, n))
+}
+
+func CompressionBrotli(n int) CompressionCodec {
+	return compressionLevel("brotli", n, 1, 11)
+}
+
+func CompressionZLIB(n int) CompressionCodec {
+	return compressionLevel("zlib", n, 1, 9)
+}
+
+func CompressionZSTD(n int) CompressionCodec {
+	return compressionLevel("zstd", n, 1, 21)
+}
+
+func CompressionLZMA(n int) CompressionCodec {
+	return compressionLevel("lzma", n, 0, 9)
+}
+
+func CompressionBZIP2(n int) CompressionCodec {
+	return compressionLevel("bzip2", n, 1, 9)
+}
+
+const (
+	CompressionNode               = "none"
+	CompressionSnappy             = "snappy"
+	CompressionLZ4                = "lz4"
+	CompressionLZ4HighCompression = "lz4_high_compression"
+	CompressionQuickLZ            = "quick_lz"
+)
+
+type ErasureCodec string
+
+func (c *ErasureCodec) UnmarshalText(text []byte) error {
+	*c = ErasureCodec(text)
+	return nil
+}
+
+func (c ErasureCodec) MarshalText() (text []byte, err error) {
+	return []byte(c), nil
+}
+
+const (
+	ErasureNone        = "none"
+	ErasureReedSolomon = "reed_solomon_6_3"
+	ErasureLRC         = "lrc_12_2_2"
+)
