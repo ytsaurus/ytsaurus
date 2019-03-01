09a01e191e73d00017bf049c3915caea05349274
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-03-01 18:39:34 +0300

    YT-10403: Fix building driver bindings for python3

--- yt/python/driver-rpc-debian/yandex-yt-python-driver-rpc/setup.py
+++ yt/python/driver-rpc-debian/yandex-yt-python-driver-rpc/setup.py
@@ -19,8 +19,8 @@ def main():
         name = PACKAGE_NAME + os.environ.get("PYTHON_SUFFIX", ""),
         version = get_version(),
         packages = ["yt_driver_bindings"],
-        package_data = {"yt_driver_bindings": ["driver_lib.so"] },
-
+        package_data = {"yt_driver_bindings": ["driver_lib.so", "driver_lib.dbg.so",
+                                               "driver_lib.abi3.so", "driver_lib.dbg.abi3.so"]},
         author = "Ignat Kolesnichenko",
         author_email = "ignat@yandex-team.ru",
         description = "C++ bindings to driver.",
