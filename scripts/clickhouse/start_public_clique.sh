81aeea5577c40226ce28a383f632395217b2148b
Maxim Akhmedov <max42@ytsaurus.tech>
2019-03-07 18:46:11 +0300

    Update clickhouse scripts

--- scripts/clickhouse/start_public_clique.sh
+++ scripts/clickhouse/start_public_clique.sh
@@ -1,2 +1,7 @@
-yt start-clickhouse-clique --enable-monitoring --instance-count 16 --cpu-limit 8 --enable-query-log --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}' 
-
+yt start-clickhouse-clique \
+    --enable-monitoring \
+    --instance-count 16 \
+    --cpu-limit 8 \
+    --enable-query-log \
+    --spec '{owners=[yandex]; title="Public testing clique"; max_failed_job_count=10000; pool="chyt"; alias="*ch_public"}' \
+    --cypress-geodata-path //sys/clickhouse/geodata/geodata.tgz
