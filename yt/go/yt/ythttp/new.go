3c7b5c3274b092ea040c9a481e2e2dff3b47f127
prime <prime@yandex-team.ru>
2019-03-12 16:34:45 +0300

    Add cypress example. . Note: mandatory check (NEED_CHECK) was skipped

--- yt/go/yt/ythttp/new.go
+++ yt/go/yt/ythttp/new.go
@@ -9,3 +9,16 @@ import (
 func NewClient(c *yt.Config) (yt.Client, error) {
 	return httpclient.NewHTTPClient(c)
 }
+
+// NewClientFromEnv creates YT client configured from environment variables.
+//
+//   YT_PROXY - required variable specifying cluster address.
+//   YT_TOKEN - optional variable specifying token.
+func NewClientFromEnv() (yt.Client, error) {
+	c, err := yt.ClusterFromEnv()
+	if err != nil {
+		return nil, err
+	}
+
+	return httpclient.NewHTTPClient(c)
+}
