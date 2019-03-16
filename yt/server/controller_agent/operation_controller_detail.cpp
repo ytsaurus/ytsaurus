0e9a49c2a03c8c8069476f2d017e2e3299e366bc
Maxim Babenko <babenko@ytsaurus.tech>
2019-03-16 15:23:52 +0300

    Drop legacy controller agent snapshot compats

--- yt/server/controller_agent/operation_controller_detail.cpp
+++ yt/server/controller_agent/operation_controller_detail.cpp
@@ -7450,10 +7450,7 @@ void TOperationControllerBase::Persist(const TPersistenceContext& context)
     Persist(context, JobSplitter_);
     Persist(context, DataFlowGraph_);
     Persist(context, AvailableExecNodesObserved_);
-    // COMPAT(babenko)
-    if (context.GetVersion() >= 300015) {
-        Persist(context, BannedNodeIds_);
-    }
+    Persist(context, BannedNodeIds_);
     Persist(context, PathToOutputTable_);
     // COMPAT(levysotsky)
     if (context.GetVersion() >= 300031) {
@@ -7470,9 +7467,7 @@ void TOperationControllerBase::Persist(const TPersistenceContext& context)
         InitializeOrchid();
     }
 
-    if (context.GetVersion() >= 300020) {
-        Persist(context, BannedTreeIds_);
-    }
+    Persist(context, BannedTreeIds_);
 }
 
 void TOperationControllerBase::InitAutoMergeJobSpecTemplates()
