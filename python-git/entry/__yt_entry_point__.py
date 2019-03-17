6311283e56103375ce496d2437aaac0f4b451c8f
orivej <orivej@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-03-17 10:21:02 +0000

    Support unpickling YT operations created in __main__.py in Python 3. YT-10453
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yt/python/yt@4658045 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- python-git/entry/__yt_entry_point__.py
+++ python-git/entry/__yt_entry_point__.py
@@ -7,7 +7,11 @@ else:
     if py_main:
         import importlib
         mod_name = py_main.split(b':', 1)[0]
-        importlib.import_module(mod_name.decode('UTF-8'))
+        mod = importlib.import_module(mod_name.decode('UTF-8'))
+        if b':' not in py_main:
+            # __main__.py compatibility mode.
+            import sys
+            sys.modules['__main__'] = mod
     else:
         # Python 2 with __main__.py.
         __res.importer.load_module("__main__", fix_name="__yt_main__")
