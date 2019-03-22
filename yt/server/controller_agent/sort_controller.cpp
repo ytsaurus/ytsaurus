a25403ca7ce0bf95aec423dfbdb3ba9cc0e073d4
Pavel Sushin <psushin@ytsaurus.tech>
2019-03-22 21:12:09 +0300

    YT-10410: Fix data weight per map job for ordered map in map reduce operation.

--- yt/server/controller_agent/sort_controller.cpp
+++ yt/server/controller_agent/sort_controller.cpp
@@ -1578,6 +1578,7 @@ protected:
             options.Task = PartitionTask->GetTitle();
             options.MaxTotalSliceCount = Config->MaxTotalSliceCount;
             options.EnablePeriodicYielder = true;
+            options.ShouldSliceByRowIndices = true;
 
             PartitionPool = CreateOrderedChunkPool(
                 std::move(options),
