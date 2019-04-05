3f0b4bbe0a6f0fcd674717b8d660ad0e685b505d
iagorskiy <iagorskiy@yandex-team.ru>
2019-04-06 02:56:59 +0300

    Reworked yt locks and basic tests for it

--- yt/go/ytlock/lock_test.go
+++ yt/go/ytlock/lock_test.go
@@ -1,1 +1,15 @@
 package ytlock
+
+import (
+	"testing"
+
+	"a.yandex-team.ru/yt/go/ypath"
+
+	"github.com/stretchr/testify/require"
+)
+
+func TestLockWithOptions(t *testing.T) {
+	initialOpt := Options{FailIfMissing: true}
+	lock := NewLockWithOptions(nil, nil, ypath.Path(""), initialOpt)
+	require.Equal(t, lock.Options, initialOpt, "Lock should have given options")
+}
