a49095d609ab6c573fb8829c8092079719ffd3e6
risenberg <risenberg@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-04-08 15:58:05 +0000

    - move environment proto to yp.
    ([arc::pullid] 8fd9066a-605d2cc5-5d30f6e4-8eabe46c)
    
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@4933713 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- python/copy_ya_proto.sh
+++ python/copy_ya_proto.sh
@@ -6,7 +6,9 @@ YP_PROTOS="
     yp/client/api/proto/cluster_api.proto
     yp/client/api/proto/pod_agent.proto
     yp/client/api/proto/data_model.proto
+    yp/client/api/proto/deploy.proto
     yp/client/api/proto/enums.proto
+    yp/client/api/proto/environment.proto
     yp/client/api/proto/dynamic_resource.proto
     yp/client/api/proto/discovery_service.proto
     yp/client/api/proto/object_service.proto
