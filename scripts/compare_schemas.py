2e7c5a04a0d9802b8150a08da7eac88c9641614c
avitella <avitella@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-04-08 08:35:44 +0000

    testzzz...
    Note: mandatory check (NEED_CHECK) was skipped
    
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@4928504 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- scripts/compare_schemas.py
+++ scripts/compare_schemas.py
@@ -3,7 +3,9 @@ import difflib
 import json
 import jsondiff
 import sys
+import os
 
+from yt.environment import arcadia_interop
 from yt.wrapper import YtClient, ypath_join
 from yt.wrapper.common import generate_uuid, filter_dict
 from yp.local import YpInstance, INITIAL_DB_VERSION
@@ -57,24 +59,39 @@ def get_schemas(client):
 
     return canonize_obj(result)
 
+def insert_environ_path(path):
+    assert len(path) > 0
+    tokens = set(os.environ.get("PATH", "").split(os.pathsep))
+    if path not in tokens:
+        os.environ["PATH"] = os.pathsep.join([path, os.environ.get("PATH", "")])
+
 def main(argv):
     def parse_args():
         parser = argparse.ArgumentParser()
         parser.add_argument("--local-db-version", required=True, type=int, help="Migrate local yp instance to given version")
+        parser.add_argument("--arcadia-root", required=True, help="Path to arcadia root, need for YT local")
         parser.add_argument("--yp-address", required=True, help="YP cluster address")
         parser.add_argument("--sandbox-path", help="Path to sandbox directory")
         return parser.parse_args(argv)
 
     args = parse_args()
+
+    uuid = generate_uuid()
     if not args.sandbox_path:
-        args.sandbox_path = "yp_" + generate_uuid()
+        args.sandbox_path = "sandbox_" + uuid
+
+    os.makedirs(args.sandbox_path)
+    for subdir in ["yp", "yt_build"]:
+        os.makedirs(os.path.join(args.sandbox_path, subdir))
+
+    insert_environ_path(os.path.realpath(arcadia_interop.prepare_yt_environment(args.sandbox_path + "/yt_build", arcadia_root=args.arcadia_root)))
 
     remote_schemas = get_schemas(YtClient(args.yp_address))
 
     print "=========> Remote schemas:"
     print json.dumps(remote_schemas, indent=4)
 
-    yp_instance = YpInstance(args.sandbox_path, enable_ssl=True, db_version=INITIAL_DB_VERSION)
+    yp_instance = YpInstance(args.sandbox_path + "/yp", enable_ssl=True, db_version=INITIAL_DB_VERSION)
     yp_instance.prepare()
     yp_instance.migrate_database(args.local_db_version, backup=False)
 
