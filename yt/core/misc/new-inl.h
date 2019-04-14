10030c795783eb504f8bcaf5f3f41a303c1aca05
Maxim Babenko <babenko@ytsaurus.tech>
2019-04-14 20:15:55 +0300

    [ytalloc] Microoptimize for some typical cases
    * Add AllocateConstSize and use the latter in New
    * Add FreeNonNull
    * Do not zero garbage in per-thread cache of small chunks

--- yt/core/misc/new-inl.h
+++ yt/core/misc/new-inl.h
@@ -7,8 +7,6 @@
 
 #include "yt_alloc.h"
 
-#include <yt/core/misc/memory_tag.h>
-
 namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -68,26 +66,26 @@ Y_FORCE_INLINE void InitializeNewInstance(
 }
 
 template <class T, class... As>
-Y_FORCE_INLINE TIntrusivePtr<T> NewImpl(
+Y_FORCE_INLINE TIntrusivePtr<T> NewEpilogue(
+    void* ptr,
     TRefCountedTypeCookie cookie,
-    size_t extraSpaceSize,
     As&& ... args)
 {
-    auto totalSize = sizeof(T) + extraSpaceSize;
-    auto* ptr = NYTAlloc::Allocate(totalSize);
     auto* instance = static_cast<T*>(ptr);
 
     try {
         new (instance) T(std::forward<As>(args)...);
     } catch (const std::exception& ex) {
         // Do not forget to free the memory.
-        NYTAlloc::Free(ptr);
+        NYTAlloc::FreeNonNull(ptr);
         throw;
     }
 
     InitializeNewInstance(instance, ptr);
 #ifdef YT_ENABLE_REF_COUNTED_TRACKING
     InitializeRefCountedTracking(instance, cookie);
+#else
+    Y_UNUSED(cookie);
 #endif
 
     return TIntrusivePtr<T>(instance, false);
@@ -98,7 +96,15 @@ Y_FORCE_INLINE TIntrusivePtr<T> NewImpl(
 template <class T, class... As>
 Y_FORCE_INLINE TIntrusivePtr<T> New(As&&... args)
 {
-    return NewWithExtraSpace<T>(0, std::forward<As>(args)...);
+    auto* ptr = NYTAlloc::AllocateConstSize<sizeof(T)>();
+    return NDetail::NewEpilogue<T>(
+        ptr,
+#ifdef YT_ENABLE_REF_COUNTED_TRACKING
+        GetRefCountedTypeCookie<T>(),
+#else
+        NullRefCountedTypeCookie, // unused
+#endif
+        std::forward<As>(args)...);
 }
 
 template <class T, class... As>
@@ -106,13 +112,15 @@ TIntrusivePtr<T> NewWithExtraSpace(
     size_t extraSpaceSize,
     As&&... args)
 {
-    return NDetail::NewImpl<T>(
+    auto totalSize = sizeof(T) + extraSpaceSize;
+    auto* ptr = NYTAlloc::Allocate(totalSize);
+    return NDetail::NewEpilogue<T>(
+        ptr,
 #ifdef YT_ENABLE_REF_COUNTED_TRACKING
         GetRefCountedTypeCookie<T>(),
 #else
         NullRefCountedTypeCookie, // unused
 #endif
-        extraSpaceSize,
         std::forward<As>(args)...);
 }
 
@@ -121,13 +129,14 @@ Y_FORCE_INLINE TIntrusivePtr<T> NewWithLocation(
     const TSourceLocation& location,
     As&&... args)
 {
-    return NDetail::NewImpl<T>(
+    auto* ptr = NYTAlloc::AllocateConstSize<sizeof(T)>();
+    return NDetail::NewEpilogue<T>(
+        ptr,
 #ifdef YT_ENABLE_REF_COUNTED_TRACKING
         GetRefCountedTypeCookieWithLocation<T, TTag, Counter>(location),
 #else
         NullRefCountedTypeCookie, // unused
 #endif
-        0,
         std::forward<As>(args)...);
 }
 
