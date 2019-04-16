62b7cf9711e179c3e47167a535276f9e758faf81
proller <proller@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-04-16 21:29:07 +0000

    Update ClickHouse to 19.5.2.6
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yt/19_4/yt@4957419 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- server/clickhouse_server/private.h
+++ server/clickhouse_server/private.h
@@ -101,7 +101,7 @@ class KeyCondition;
 struct SelectQueryInfo;
 class Field;
 class StorageFactory;
-class ISecurityManager;
+class IUsersManager;
 class IExternalLoaderConfigRepository;
 class IRuntimeComponentsFactory;
 
