cb8efd229b4da60684d3bc4f61d18aa15a47f002
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-04-17 23:32:06 +0300

    [teamcity-build] prestable/19.4 -> master

--- python-git/teamcity-build.py
+++ python-git/teamcity-build.py
@@ -145,12 +145,12 @@ def checkout(options):
                 "git@github.yandex-team.ru:yt/yt.git",
                 "--recurse-submodules",
                 "--branch",
-                "prestable/19.4",
+                "master",
                 options.yt_source_directory
             ],
             cwd=options.yt_build_directory)
     else:
-        run(["git", "checkout", "prestable/19.4"], cwd=options.yt_source_directory)
+        run(["git", "checkout", "master"], cwd=options.yt_source_directory)
         run(["git", "pull"], cwd=options.yt_source_directory)
         run(["git", "submodule", "update", "--init", "--recursive"], cwd=options.yt_source_directory)
 
