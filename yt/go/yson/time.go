7498bb3c2059b188b5e5fc61fdfefd8b5e68490d
prime <prime@yandex-team.ru>
2019-05-08 16:47:42 +0300

    Update documentation. Note: mandatory check (NEED_CHECK) was skipped

--- yt/go/yson/time.go
+++ yt/go/yson/time.go
@@ -4,8 +4,10 @@ import "time"
 
 const ytTimeLayout = "2006-01-02T15:04:05.999999Z"
 
+// Time is alias for time.Time with YT specific time representation format.
 type Time time.Time
 
+// Duration is alias for time.Duration with YT specific time representation format.
 type Duration time.Duration
 
 // UnmarshalTime decodes time from YT-specific time format.
