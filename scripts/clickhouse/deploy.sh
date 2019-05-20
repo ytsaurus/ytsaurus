e245fa6f0e24ad88665efe8bbfd8cb2986027820
Maxim Akhmedov <max42@ytsaurus.tech>
2019-05-20 21:10:32 +0300

    Add argument to deploy.sh script.

--- scripts/clickhouse/deploy.sh
+++ scripts/clickhouse/deploy.sh
@@ -1,4 +1,8 @@
-BIN="/home/max42/yt/build-rel/bin/ytserver-clickhouse"
+if [[ "$1" == "" ]] ; then
+    BIN="/home/max42/yt/build-rel/bin/ytserver-clickhouse"
+else
+    BIN="$1"
+fi
 VERSION="$($BIN --version)"
 CYPRESS_PATH="//sys/clickhouse/bin/ytserver-clickhouse-$VERSION"
 echo "Deploying $BIN of version $VERSION to $CYPRESS_PATH"
