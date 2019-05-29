fae356e8e5d4347bcb2d2a94e4f35b1916703c1d
miroslav2 <miroslav2@yandex-team.ru>
2019-05-29 16:46:45 +0300

    Улучшение API для Mapped Request-ов - позволяем передавать Iterable, причем максимально эффективно

---
+++ yt/java/ytclient/src/test/java/ru/yandex/yt/ytclient/proxy/MappedModifyRowsRequestTest.java
@@ -0,0 +1,63 @@
+package ru.yandex.yt.ytclient.proxy;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+import ru.yandex.yt.rpcproxy.ERowModificationType;
+import ru.yandex.yt.ytclient.object.ObjectsMetadata;
+import ru.yandex.yt.ytclient.object.SmallObjectClass;
+import ru.yandex.yt.ytclient.wire.WireProtocolTest;
+
+public class MappedModifyRowsRequestTest {
+
+    @Test
+    public void testMapedModifyRowsRequest() {
+        final ObjectsMetadata<SmallObjectClass> metadata =
+                ObjectsMetadata.getMetadata(SmallObjectClass.class, value -> {
+
+                });
+
+        final MappedModifyRowsRequest<SmallObjectClass> request =
+                new MappedModifyRowsRequest<>("", metadata.getMappedSerializer());
+        final List<SmallObjectClass> sample1 = metadata.generateObjects(1);
+        final List<SmallObjectClass> sample2 = metadata.generateObjects(2);
+        final List<SmallObjectClass> sample3 = metadata.generateObjects(1);
+        final List<SmallObjectClass> sample4 = metadata.generateObjects(2);
+        final List<SmallObjectClass> sample5 = metadata.generateObjects(1);
+        final List<SmallObjectClass> sample6 = metadata.generateObjects(2);
+
+        request.addUpdate(sample1.get(0));
+        request.addUpdates(sample2);
+
+        request.addInsert(sample3.get(0));
+        request.addInserts(sample4);
+
+        request.addDelete(sample5.get(0));
+        request.addDeletes(sample6);
+
+        Assert.assertEquals(Arrays.asList(
+                ERowModificationType.RMT_WRITE, ERowModificationType.RMT_WRITE, ERowModificationType.RMT_WRITE,
+                ERowModificationType.RMT_WRITE, ERowModificationType.RMT_WRITE, ERowModificationType.RMT_WRITE,
+                ERowModificationType.RMT_DELETE, ERowModificationType.RMT_DELETE, ERowModificationType.RMT_DELETE),
+                request.getRowModificationTypes());
+
+        final List<SmallObjectClass> all = new ArrayList<>();
+        all.addAll(sample1);
+        all.addAll(sample2);
+        all.addAll(sample3);
+        all.addAll(sample4);
+        all.addAll(sample5);
+        all.addAll(sample6);
+
+        final List<byte[]> expect = metadata.serializeMappedObjects(all);
+
+        final List<byte[]> actual = new ArrayList<>();
+        request.serializeRowsetTo(actual);
+
+        Assert.assertArrayEquals(WireProtocolTest.mergeChunks(expect), WireProtocolTest.mergeChunks(actual));
+    }
+}
