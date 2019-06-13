163a4a446300dc27dd8ff0311a8215264a4107e4
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-06-14 02:10:03 +0300

    many_output tables test fix

--- python-git/yt/wrapper/tests/files/many_output.py
+++ python-git/yt/wrapper/tests/files/many_output.py
@@ -18,9 +18,9 @@ if __name__ == "__main__":
     for i in xrange(10 + 1):
         if is_yamr_mode:
             value = str(i)
-            rec = "\t".join([value] * 3) + '\n'
+            rec = ("\t".join([value] * 3) + '\n').encode("ascii")
             os.write(i + 3, rec)
         else:
-            rec = "x={0}\ty={0}\n".format(i)
+            rec = "x={0}\ty={0}\n".format(i).encode("ascii")
             os.write(i * 3 + 1, rec)
 
