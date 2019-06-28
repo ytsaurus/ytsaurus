9f5f31b702bf4e4b4eb2cda5343949f6c876db9a
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-06-28 21:23:52 +0300

    YT-10008: support gpu statistics in job_metrics, orchid and profiling.

--- yt/server/lib/job_agent/gpu_helpers.cpp
+++ yt/server/lib/job_agent/gpu_helpers.cpp
@@ -14,6 +14,8 @@
 #include <util/folder/iterator.h>
 
 #include <util/string/strip.h>
+#include <util/string/subst.h>
+#include <util/string/split.h>
 
 namespace NYT::NJobAgent {
 
@@ -25,6 +27,7 @@ static const auto& Logger = JobAgentServerLogger;
 
 static const auto FatalErrorMessage = "Unable to determine";
 static const auto MinorNumberMessage = "Minor Number";
+static const auto GpuUuidMessage = "GPU UUID";
 static const TString DevNvidiaPath("/dev/nvidia");
 static const TString DevPath("/dev");
 static const TString NvidiaDevicePrefix("nvidia");
@@ -35,7 +38,7 @@ static const THashSet<TString> MetaGpuDevices = {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-THashSet<int> GetHealthyGpuDeviceNumbers(TDuration checkTimeout)
+THashMap<TString, int> GetGpuIds(TDuration checkTimeout)
 {
     TSubprocess subprocess("nvidia-smi");
     subprocess.AddArguments({ "-q" });
@@ -56,39 +59,133 @@ THashSet<int> GetHealthyGpuDeviceNumbers(TDuration checkTimeout)
 
     auto nvidiaSmiResult = subprocess.Execute();
     if (!nvidiaSmiResult.Status.IsOK()) {
-        THROW_ERROR_EXCEPTION("Failed to check healthy GPUs: nvidia-smi exited with an error")
+        THROW_ERROR_EXCEPTION("Failed to check healthy GPUs: 'nvidia-smi -q' exited with an error")
             << nvidiaSmiResult.Status;
     }
 
     auto output = TString(nvidiaSmiResult.Output.Begin(), nvidiaSmiResult.Output.End());
     if (output.find(FatalErrorMessage) != TString::npos) {
-        THROW_ERROR_EXCEPTION("Failed to check healthy GPUs: nvidia-smi exited with fatal error");
+        THROW_ERROR_EXCEPTION("Failed to check healthy GPUs: 'nvidia-smi -q' exited with fatal error");
     }
 
-    THashSet<int> result;
+    THashMap<TString, int> result;
+
     size_t pos = 0;
     while (true) {
-        pos = output.find(MinorNumberMessage, pos);
+        // Process GPU UUID.
+        pos = output.find(GpuUuidMessage, pos);
         if (pos == TString::npos) {
             break;
         }
 
-        auto semicolonPos = output.find(":", pos);
-        auto eolPos = output.find("\n", pos);
-        if (semicolonPos == TString::npos || eolPos == TString::npos || eolPos <= semicolonPos) {
-            THROW_ERROR_EXCEPTION("Invalid nvidia-smi output format");
+        TString gpuId;
+        int gpuNumber;
+
+        {
+            auto semicolonPos = output.find(":", pos);
+            auto eolPos = output.find("\n", pos);
+            if (semicolonPos == TString::npos || eolPos == TString::npos || eolPos <= semicolonPos) {
+                THROW_ERROR_EXCEPTION("Invalid 'nvidia-smi -q' output format: failed to parse GPU UUID");
+            }
+            gpuId = StripString(output.substr(semicolonPos + 1, eolPos - semicolonPos - 1));
+
+            pos = eolPos;
         }
 
-        try {
-            auto deviceNumberString = output.substr(semicolonPos + 1, eolPos - semicolonPos - 1);
-            auto deviceNumber = FromString<int>(Strip(deviceNumberString));
-            result.insert(deviceNumber);
-        } catch (const std::exception& ex) {
-            THROW_ERROR_EXCEPTION("Failed to parse minor GPU device number")
-                << ex;
+        // Process GPU Minor Number.
+        pos = output.find(MinorNumberMessage, pos);
+        if (pos == TString::npos) {
+            THROW_ERROR_EXCEPTION("Invalid 'nvidia-smi -q' output format: failed to find Minor Number after GPU UUID");
         }
 
-        pos = eolPos;
+        {
+            auto semicolonPos = output.find(":", pos);
+            auto eolPos = output.find("\n", pos);
+            if (semicolonPos == TString::npos || eolPos == TString::npos || eolPos <= semicolonPos) {
+                THROW_ERROR_EXCEPTION("Invalid 'nvidia-smi -q' output format: failed to parse GPU Minor Number");
+            }
+
+            try {
+                auto deviceNumberString = output.substr(semicolonPos + 1, eolPos - semicolonPos - 1);
+                gpuNumber = FromString<int>(Strip(deviceNumberString));
+            } catch (const std::exception& ex) {
+                THROW_ERROR_EXCEPTION("Invalid 'nvidia-smi -q' output format: failed to parse GPU Minor Number")
+                    << ex;
+            }
+
+            pos = eolPos;
+        }
+
+        result.emplace(gpuId, gpuNumber);
+    }
+
+    return result;
+}
+
+std::vector<TGpuInfo> GetGpuInfos(TDuration checkTimeout)
+{
+    auto startTime = TInstant::Now();
+
+    auto gpuIdToNumber = GetGpuIds(checkTimeout);
+
+    auto remainingTimeout = checkTimeout - std::min((TInstant::Now() - startTime), checkTimeout);
+
+    if (remainingTimeout == TDuration::Zero()) {
+        THROW_ERROR_EXCEPTION("Getting gpu information timed out");
+    }
+
+    TSubprocess subprocess("nvidia-smi");
+    subprocess.AddArguments({"--query-gpu=uuid,name,utilization.gpu,utilization.memory,memory.used", "--format=csv,noheader,nounits"});
+
+    auto killCookie = TDelayedExecutor::Submit(
+        BIND([&] () {
+            try {
+                subprocess.Kill(SIGKILL);
+            } catch (const std::exception& ex) {
+                YT_LOG_ERROR(ex, "Failed to kill nvidia-smi process");
+            }
+        }),
+        remainingTimeout);
+
+    auto cookieGuard = Finally([&] () {
+        TDelayedExecutor::CancelAndClear(killCookie);
+    });
+
+    auto nvidiaSmiResult = subprocess.Execute();
+    if (!nvidiaSmiResult.Status.IsOK()) {
+        THROW_ERROR_EXCEPTION("Failed to check healthy GPUs: 'nvidia-smi --query-gpu' exited with an error")
+            << nvidiaSmiResult.Status;
+    }
+
+    auto output = TString(nvidiaSmiResult.Output.Begin(), nvidiaSmiResult.Output.End());
+    if (output.find(FatalErrorMessage) != TString::npos) {
+        THROW_ERROR_EXCEPTION("Failed to check healthy GPUs: 'nvidia-smi --query-gpu' exited with fatal error");
+    }
+
+    std::vector<TGpuInfo> result;
+    for (TStringBuf line : StringSplitter(output).Split('\n').SkipEmpty()) {
+        std::vector<TStringBuf> tokens = StringSplitter(line).Split(',');
+        if (tokens.size() != 5) {
+            THROW_ERROR_EXCEPTION(
+                "Invalid 'nvidia-smi --query-gpu' output format: expected 4 comma separated values, but got %Qv",
+                line);
+        }
+
+        TGpuInfo info;
+        auto gpuId = StripString(tokens[0]);
+        {
+            auto it = gpuIdToNumber.find(gpuId);
+            if (it == gpuIdToNumber.end()) {
+                THROW_ERROR_EXCEPTION("Invalid 'nvidia-smi --query-gpu' output, gpu id %Qv is not found in 'nvidia-smi -q' output",
+                    gpuId);
+            }
+            info.Index = it->second;
+        }
+        info.Name = SubstGlobalCopy(to_lower(TString(StripString(tokens[1]))), ' ', '_');
+        info.UtilizationGpuRate = FromString<i64>(StripString(tokens[2])) / 100.0;
+        info.UtilizationMemoryRate = FromString<i64>(StripString(tokens[3])) / 100.0;
+        info.MemoryUsed = FromString<i64>(StripString(tokens[4])) * 1_MB;
+        result.emplace_back(std::move(info));
     }
 
     return result;
@@ -154,6 +251,13 @@ std::vector<TGpuDeviceDescriptor> ListGpuDevices()
 TString GetGpuDeviceName(int deviceNumber)
 {
     return DevNvidiaPath + ToString(deviceNumber);
+}
+
+void ProfileGpuInfo(NProfiling::TProfiler& profiler, const TGpuInfo& gpuInfo, const NProfiling::TTagIdList& tagIds)
+{
+    profiler.Enqueue("/utilization_gpu_rate_x1000", gpuInfo.UtilizationGpuRate, NProfiling::EMetricType::Gauge, tagIds);
+    profiler.Enqueue("/utilization_memory_rate_x1000", gpuInfo.UtilizationMemoryRate, NProfiling::EMetricType::Gauge, tagIds);
+    profiler.Enqueue("/memory_used", gpuInfo.MemoryUsed, NProfiling::EMetricType::Gauge, tagIds);
 }
 
 ////////////////////////////////////////////////////////////////////////////////
