046d7c570f96f3fa0616a8123b59f078806b36c7
Maxim Akhmedov <max42@ytsaurus.tech>
2019-06-29 20:17:22 +0300

    CHYT-148: export CH metrics to Solomon.

--- yt/server/clickhouse_server/host.cpp
+++ yt/server/clickhouse_server/host.cpp
@@ -23,6 +23,8 @@
 #include <yt/core/profiling/profile_manager.h>
 
 #include <AggregateFunctions/registerAggregateFunctions.h>
+#include <Common/CurrentMetrics.h>
+#include <Common/ClickHouseRevision.h>
 #include <Common/Exception.h>
 #include <Common/StringUtils/StringUtils.h>
 #include <Common/config.h>
@@ -193,7 +195,7 @@ public:
 
         YT_LOG_DEBUG("Flushing profiling");
 
-        for (auto& [user, runningQueryCount] : UserToRunningInitialQueryCount_) {
+        for (const auto& [user, runningQueryCount] : UserToRunningInitialQueryCount_) {
             ServerProfiler.Enqueue(
                 "/running_initial_query_count",
                 runningQueryCount,
@@ -201,13 +203,24 @@ public:
                 {TProfileManager::Get()->RegisterTag("user", user)});
         }
 
-        for (auto& [user, runningQueryCount] : UserToRunningSecondaryQueryCount_) {
+        for (const auto& [user, runningQueryCount] : UserToRunningSecondaryQueryCount_) {
             ServerProfiler.Enqueue(
                 "/running_secondary_query_count",
                 runningQueryCount,
                 EMetricType::Gauge,
                 {TProfileManager::Get()->RegisterTag("user", user)});
         }
+
+        for (int index = 0; index < static_cast<int>(CurrentMetrics::end()); ++index) {
+            const auto* name = CurrentMetrics::getName(index);
+            auto value = CurrentMetrics::values[index].load();
+            ServerProfiler.Enqueue(
+                "/ch_metrics/" + CamelCaseToUnderscoreCase(TString(name)),
+                value,
+                EMetricType::Gauge);
+        }
+
+        YT_LOG_DEBUG("Profiling flushed");
     }
 
     void AdjustQueryCount(const TString& user, EQueryKind queryKind, int delta)
@@ -293,6 +306,9 @@ private:
         RegisterConcatenatingTableFunctions();
         RegisterTableDictionarySource(Bootstrap_);
         RegisterStorageTable();
+
+        CurrentMetrics::set(CurrentMetrics::Revision, ClickHouseRevision::get());
+        CurrentMetrics::set(CurrentMetrics::VersionInteger, ClickHouseRevision::getVersionInteger());
 
         // Initialize DateLUT early, to not interfere with running time of first query.
         YT_LOG_INFO("Initializing DateLUT");
