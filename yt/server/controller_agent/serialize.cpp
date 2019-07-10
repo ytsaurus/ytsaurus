7ded3911ed6ced662f69e6f81b992007ca140850
Renat Khayretdinov <renadeen@ytsaurus.tech>
2019-07-10 13:26:42 +0300

    Force advance contragent snapshot just before branching 19.6

--- yt/server/controller_agent/serialize.cpp
+++ yt/server/controller_agent/serialize.cpp
@@ -13,7 +13,7 @@ int GetCurrentSnapshotVersion()
 
 bool ValidateSnapshotVersion(int version)
 {
-    return version >= ToUnderlying(ESnapshotVersion::JobMetricsAggregationType) && version <= GetCurrentSnapshotVersion();
+    return version >= ToUnderlying(ESnapshotVersion::ForceAdvanceBefore19_6) && version <= GetCurrentSnapshotVersion();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
