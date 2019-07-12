7442aae2e1995965a547516a8b4645ee721b9675
zero323 <mszymkiewicz@gmail.com>
2019-07-12 22:12:56 +0200

    Fix version

--- taxi/dmp/spark/pyspark-stubs/setup.py
+++ taxi/dmp/spark/pyspark-stubs/setup.py
@@ -24,7 +24,7 @@ def list_packages(src_path=src_path):
 setup(
     name='pyspark-stubs',
     package_dir={'': src_path},
-    version='3.0.0dev0',
+    version='3.0.0.dev0',
     description='A collection of the Apache Spark stub files',
     long_description=(open('README.rst').read() if os.path.exists('README.rst')
                         else ''),
@@ -33,7 +33,7 @@ setup(
     package_data={
         '': ['*.pyi', 'py.typed']
     },
-    install_requires=['pyspark>=3.0.0dev0,<3.1.0'],
+    install_requires=['pyspark>=3.0.0.dev0,<3.1.0'],
     classifiers=[
         "Programming Language :: Python :: 3 :: Only",
         "Development Status :: 4 - Beta",
