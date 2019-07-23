0f817cbe45b301fefb201c46a29306fa1767c2f4
Maxim Akhmedov <max42@ytsaurus.tech>
2019-07-23 17:38:22 +0300

    Allow specifying custom build directory in run-py-test.

--- scripts/run-py-test.py
+++ scripts/run-py-test.py
@@ -14,6 +14,9 @@ RUN_PY_TEST_SET_SUID_BIT
 YT_BUILD_PYTHON_VERSION 
     possible values: "2.7", "3.4" (or any other python version that is installed on machine)
   Use specified version of python to run tests.
+
+RUN_PY_TEST_INSTALL_DIR
+  Use specified directory to run binaries from instead of the $S/ya-build.
 """
 
 from __future__ import print_function
@@ -57,13 +60,14 @@ def main():
     this_file_path = os.path.realpath(__file__)
     repo_root = os.path.realpath(os.path.join(os.path.dirname(this_file_path), ".."))
 
-    ya_build = os.path.join(repo_root, "ya-build")
+    ya_build = os.environ.get("RUN_PY_TEST_INSTALL_DIR", os.path.join(repo_root, "ya-build"))
     if not os.path.exists(ya_build) and "-h" not in sys.argv[1:] and "--help" not in sys.argv[1:]:
         print("ya-build directory cannot be found in repo root, tests are going to fail", file=sys.stderr)
         print("Please run (from repo root directory ):", file=sys.stderr)
         print("  $ {repo_root}/yall --install {ya_build}".format(repo_root=repo_root, ya_build=ya_build), file=sys.stderr)
         print("More convenient way is to configure yall so it will always use this install directory.", file=sys.stderr)
         print("  https://wiki.yandex-team.ru/yt/internal/ya/", file=sys.stderr)
+        print("You may also specify installation directory via RUN_PY_TEST_INSTALL_DIR env variable.", file=sys.stderr)
         print("ERROR occurred. Exiting...", file=sys.stderr)
         exit(1)
 
@@ -74,7 +78,7 @@ def main():
     env["PYTHONPATH"] = "{python}:{yp_python}:{install_dir}:{env_pythonpath}".format(
         python=os.path.join(repo_root, "python"),
         yp_python=os.path.join(repo_root, "yp", "python"),
-        install_dir=os.path.join(repo_root, "ya-build"),
+        install_dir=ya_build,
         env_pythonpath=os.environ.get("PYTHONPATH", ""))
 
     env["PATH"] = ya_build + ":" + os.environ["PATH"]
