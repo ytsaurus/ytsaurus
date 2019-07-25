35fcb2faba40b7858eb7665e3b0b0edf0ba458e7
Ilia Pauzner <ilpauzner@yandex-team.ru>
2019-07-25 10:02:53 +0300

    YT-7737: Batch operations polling. (#475)
    Run operations in batches (batch size can be specified), excessive locking removed.

--- python-git/yt/wrapper/operations_tracker.py
+++ python-git/yt/wrapper/operations_tracker.py
@@ -1,24 +1,32 @@
-from .config import get_config
+from .config import get_config, get_backend_type
 from .common import get_value
 from .errors import YtError, YtResponseError
 from .exceptions_catcher import ExceptionCatcher
 from .operation_commands import (Operation, _create_operation_failed_error, PrintOperationInfo,
-                                 get_operation_state, abort_operation, get_operation_url)
+                                 get_operation_attributes, get_operation_state, get_operation_url,
+                                 OperationState, abort_operation)
 from .spec_builders import SpecBuilder
 from .run_operation_commands import run_operation
+from .batch_helpers import batch_apply
 
 import yt.logger as logger
+from yt.packages.six.moves import xrange, zip
 
 from collections import namedtuple
 from time import sleep
-from collections import deque
+from collections import deque, defaultdict
 from threading import Thread, RLock
 from copy import deepcopy
+
+def copy_client(client):
+    from .client import YtClient
+    return YtClient(config=deepcopy(get_config(client)))
+
 
 class TrackerOperation(object):
     def __init__(self, id, client=None):
         self.id = id
-        self.client = client
+        self.client = copy_client(client)
         self.printer = PrintOperationInfo(id, client=client)
         self.url = get_operation_url(id, client=client)
 
@@ -29,7 +37,7 @@ class TrackerOperation(object):
         abort_operation(self.id, client=self.client)
 
 class _OperationsTrackingThread(Thread):
-    def __init__(self, poll_period, print_progress):
+    def __init__(self, poll_period, print_progress, batch_size):
         super(_OperationsTrackingThread, self).__init__()
         self._poll_period = poll_period
         self._print_progress = print_progress
@@ -41,41 +49,76 @@ class _OperationsTrackingThread(Thread):
 
         self.daemon = True
 
+        self.processing_operations_count = 0
+        
+        self.batch_size = batch_size
+
     def add(self, operation):
         with self._thread_lock:
             self._operations_to_track.append(operation)
 
     def run(self):
         while not self.finished:
-            with self._thread_lock:
-                self._check_one_operation()
+            self._check_operations()
 
             # NOTE: Wait time should decrease with the number of operations
             # because if poll period is fixed and number of operations is large then
             # last added operation can be removed from cypress faster than
             # tracker checks it.
-            coef = 1.0 / max(len(self._operations_to_track), 1)
+            with self._thread_lock:
+                coef = 1.0 / max(len(self._operations_to_track) + (2 * self.processing_operations_count / self.batch_size), 1)
             sleep(coef * self._poll_period / 1000.0)
 
-    def _check_one_operation(self):
-        if not self._operations_to_track:
-            return
+    def _check_operations(self):
+        cluster_to_operations = defaultdict(list)
+        cluster_to_states = defaultdict(list)
+        with self._thread_lock:
+            actual_batch_size = min(self.batch_size, len(self._operations_to_track))
+            for i in xrange(actual_batch_size):
+                operation = self._operations_to_track.popleft()
+                self.processing_operations_count += 1
+                if get_backend_type(operation.client) == "native" or operation.client is None:
+                    logger.warning("Batch polling for native back-end is not supported")
+                    proxy = None
+                else:
+                    proxy = operation.client.config.get("proxy", {}).get("url", None)
 
-        operation = self._operations_to_track.popleft()
+                cluster_to_operations[proxy].append(operation)
 
-        state = operation.get_state()
-        if self._print_progress:
-            operation.printer(state)
+        # NB: Not in _thread_lock.
+        # It is intentional since operation start request may be time-consuming.
+        for proxy in cluster_to_operations:
+            if proxy is None:
+                # We are working with native client, so polling for operation state one-by-one.
+                for operation in cluster_to_operations[proxy]:
+                    cluster_to_states[proxy].append(operation.get_state())
+            else:
+                # Batch request is sent from _some_ client.
+                client = copy_client(cluster_to_operations[proxy][0].client)
+                data = [op.id for op in cluster_to_operations[proxy]]
+                logger.debug("Fetching states of operations in %s", proxy)
 
-        if state.is_finished():
-            if state.is_unsuccessfully_finished():
-                self.errors.append(_create_operation_failed_error(operation, state))
-        else:
-            self._operations_to_track.append(operation)
+                def get_state(operation, client=client):
+                    return get_operation_attributes(operation, fields=["state"], client=client)
+
+                op_states_raw = batch_apply(get_state, data, client)
+                cluster_to_states[proxy] = [OperationState(operation_state["state"]) for operation_state in op_states_raw]
+
+        with self._thread_lock:
+            for proxy in cluster_to_operations:
+                for operation, state in zip(cluster_to_operations[proxy], cluster_to_states[proxy]):
+                    self.processing_operations_count -= 1
+                    if self._print_progress:
+                        operation.printer(state)
+                    if state.is_finished():
+                        if state.is_unsuccessfully_finished():
+                            self.errors.append(_create_operation_failed_error(operation, state))
+                    else:
+                        self._operations_to_track.append(operation)
 
     def get_operation_count(self):
         with self._thread_lock:
-            return len(self._operations_to_track)
+            return len(self._operations_to_track) + self.processing_operations_count
 
     def abort_operations(self):
         with self._thread_lock:
@@ -96,13 +139,13 @@ class OperationsTrackerBase(object):
 
     THREAD_CLASS = _OperationsTrackingThread
 
-    def __init__(self, poll_period=5000, abort_on_sigint=True, print_progress=True):
+    def __init__(self, poll_period=5000, abort_on_sigint=True, print_progress=True, batch_size=100):
         self.operations = {}
 
         self._poll_period = poll_period
         self._abort_on_sigint = abort_on_sigint
 
-        self._tracking_thread = self.THREAD_CLASS(poll_period, print_progress)
+        self._tracking_thread = self.THREAD_CLASS(poll_period, print_progress, batch_size)
         self._tracking_thread.start()
 
     def _add_operation(self, operation):
@@ -116,7 +159,6 @@ class OperationsTrackerBase(object):
 
         :param Operation operation: operation to track.
         """
-        from .client import YtClient
 
         if operation is None:
             return
@@ -128,8 +170,7 @@ class OperationsTrackerBase(object):
         if not operation.exists():
             raise YtError("Operation {0} is already tracked".format(operation.id))
 
-        client = YtClient(config=deepcopy(get_config(operation.client)))
-        tracker_operation = TrackerOperation(operation.id, client=client)
+        tracker_operation = TrackerOperation(operation.id, client=operation.client)
         self._add_operation(tracker_operation)
 
     def wait_all(self, check_result=True, abort_exceptions=(KeyboardInterrupt,), keep_finished=False):
@@ -194,18 +235,17 @@ class OperationsTracker(OperationsTrackerBase):
 
         :param str operation_id: operation id.
         """
-        from .client import YtClient
 
-        new_client = YtClient(config=deepcopy(get_config(client)))
         try:
-            operation = TrackerOperation(operation_id, client=new_client)
+            operation = TrackerOperation(operation_id, client=client)
         except YtResponseError as err:
             if not err.is_resolve_error():
                 raise
             logger.warning("Operation %s does not exist and is not added", operation_id)
             return
 
-        self._add_operation(operation)
+        tracker_operation = TrackerOperation(operation.id, client=operation.client)
+        self._add_operation(tracker_operation)
 
 SpecTask = namedtuple("SpecTask", ["spec_builder", "client", "enable_optimizations"])
 
@@ -232,23 +272,23 @@ class _OperationsTrackingPoolThread(_OperationsTrackingThread):
                           "to add method, not {0}".format(repr(spec_task)))
 
         with self._thread_lock:
-            self._queue.append(spec_task)
+            new_spec_task = SpecTask(spec_task.spec_builder, copy_client(spec_task.client), spec_task.enable_optimizations)
+            self._queue.append(new_spec_task)
 
-    def _check_one_operation(self):
-        super(_OperationsTrackingPoolThread, self)._check_one_operation()
-
+    def _check_operations(self):
+        super(_OperationsTrackingPoolThread, self)._check_operations()
         spec_tasks = []
         operations = []
         with self._thread_lock:
-            while self._pool_size is None or len(self._operations_to_track) < self._pool_size:
+            while self._pool_size is None or len(self._operations_to_track) + len(spec_tasks) < self._pool_size:
                 if len(self._queue) == 0:
                     break
                 spec_tasks.append(self._queue.popleft())
+            self.processing_operations_count += len(spec_tasks)
 
-        # N.B. Not in _thread_lock !
-        # It is intentional, since operation start sometimes is long and heavy enough.
+        # NB: Not in _thread_lock.
+        # It is intentional, operation start may be long operation.
         for spec_task in spec_tasks:
-            logger.info("Starting operation from pool")
             operation = run_operation(
                 spec_task.spec_builder,
                 sync=False,
@@ -258,10 +298,11 @@ class _OperationsTrackingPoolThread(_OperationsTrackingThread):
 
         with self._thread_lock:
             self._operations_to_track.extend(operations)
+            self.processing_operations_count -= len(operations)
 
     def get_operation_count(self):
         with self._thread_lock:
-            return len(self._operations_to_track) + len(self._queue)
+            return len(self._operations_to_track) + len(self._queue) + self.processing_operations_count
 
     def abort_operations(self):
         with self._thread_lock:
@@ -290,17 +331,14 @@ class OperationsTrackerPool(OperationsTrackerBase):
 
         :param SpecBuilder spec_builder: spec_builder to run operation and track it.
         """
-        from .client import YtClient
 
         enable_optimizations = get_value(enable_optimizations, self.enable_optimizations)
         client = get_value(client, self.client)
 
-        new_client = YtClient(config=deepcopy(get_config(client)))
-
         if not self._is_spec_builder(spec_builder):
             raise YtError("Spec builder is not valid")
 
-        task = SpecTask(spec_builder, new_client, enable_optimizations)
+        task = SpecTask(spec_builder, copy_client(client), enable_optimizations)
         self._tracking_thread.add(task)
 
     def map(self, spec_builders, enable_optimizations=None, client=None):
@@ -316,4 +354,4 @@ class OperationsTrackerPool(OperationsTrackerBase):
             raise YtError("Some of the spec builders are not valid")
 
         for spec_builder in spec_builders:
-            self._tracking_thread.add(SpecTask(spec_builder, client, enable_optimizations))
+            self._tracking_thread.add(SpecTask(spec_builder, copy_client(client), enable_optimizations))
