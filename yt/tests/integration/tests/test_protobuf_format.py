43bfc6829c2c36a9ff8d34cf333a657792321706
Lev Vysotsky <levysotsky@yandex-team.ru>
2019-07-27 18:51:10 +0300

    More relaxed typecasting in protobuf format.
    Any int to any int, any uint to any uint.
    Moreover, now there can be less schemas than tables in protobuf config
    (before the counts must have been equal).

--- yt/tests/integration/tests/test_protobuf_format.py
+++ yt/tests/integration/tests/test_protobuf_format.py
@@ -11,6 +11,92 @@ class TestProtobufFormat(YTEnvSetup):
     NUM_SCHEDULERS = 1
 
     @unix_only
+    def test_multi_output_map(self):
+        create("table", "//tmp/t_in")
+        write_table("//tmp/t_in", [
+            {
+                "int64_column": -42,
+                "uint64_column": yson.YsonUint64(25),
+                "double_column": 3.14,
+                "bool_column": True,
+                "string_column": "foo",
+                "any_column": [110, "xxx", {"foo": "bar"}],
+            },
+            {
+                "int64_column": -15,
+                "uint64_column": yson.YsonUint64(25),
+                "double_column": 2.7,
+                "bool_column": False,
+                "string_column": "qux",
+                "any_column": 234,
+            },
+        ])
+
+        create("table", "//tmp/t_out1")
+        create("table", "//tmp/t_out2")
+
+        table_config = {
+            "columns": [
+                {
+                    "name": "int64_column",
+                    "field_number": 1,
+                    "proto_type": "int64",
+                },
+                {
+                    "name": "uint64_column",
+                    "field_number": 2,
+                    "proto_type": "uint64",
+                },
+                {
+                    "name": "double_column",
+                    "field_number": 3,
+                    "proto_type": "double",
+                },
+                {
+                    "name": "bool_column",
+                    "field_number": 4,
+                    "proto_type": "bool",
+                },
+                {
+                    "name": "string_column",
+                    "field_number": 5,
+                    "proto_type": "string",
+                },
+                {
+                    "name": "any_column",
+                    "field_number": 6,
+                    "proto_type": "any",
+                },
+            ],
+        }
+
+        single_table_format = yson.YsonString("protobuf")
+        single_table_format.attributes["tables"] = [table_config]
+
+        input_format = single_table_format
+
+        output_format = yson.YsonString("protobuf")
+        output_format.attributes["tables"] = [table_config] * 2
+
+        map(in_="//tmp/t_in",
+            out=["//tmp/t_out1", "//tmp/t_out2"],
+            command="tee /dev/fd/4",
+            spec={
+                "mapper":{
+                    "input_format": input_format,
+                    "output_format": output_format,
+                }
+            })
+
+        protobuf_dump = read_table("//tmp/t_in", output_format=single_table_format)
+        # Check that column name is not in our table dump.
+        # It's simple check that read_table didn't return yson.
+        assert single_table_format.attributes["tables"][0]["columns"][0]["name"] not in protobuf_dump
+
+        assert read_table("//tmp/t_in") == read_table("//tmp/t_out1")
+        assert read_table("//tmp/t_in") == read_table("//tmp/t_out2")
+
+    @unix_only
     def test_id_map(self):
         create("table", "//tmp/t_in")
         write_table("//tmp/t_in", [
