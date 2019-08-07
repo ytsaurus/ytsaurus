1a16f4ecf650ddfbba258539470fe3a3ec9b1835
Lev Vysotsky <levysotsky@yandex-team.ru>
2019-08-07 10:43:07 +0300

    Add authors to teamcity messages

--- yt/tests/integration/tests/conftest.py
+++ yt/tests/integration/tests/conftest.py
@@ -14,19 +14,23 @@ def pytest_configure(config):
     ]:
         config.addinivalue_line("markers", line)
 
-def pytest_runtest_makereport(item, call, __multicall__):
-    rep = __multicall__.execute()
-    if hasattr(item, "cls") and hasattr(item.cls, "Env"):
-        rep.environment_path = item.cls.Env.path
-    return rep
-
 def _get_closest_marker(item, name):
     if hasattr(item, "get_closest_marker"):
         return item.get_closest_marker(name=name)
+    # For older versions of pytest.
     for item in reversed(item.listchain()):
         if item.get_marker(name) is not None:
             return item.get_marker(name)
     return None
+
+def pytest_runtest_makereport(item, call, __multicall__):
+    rep = __multicall__.execute()
+    if hasattr(item, "cls") and hasattr(item.cls, "Env"):
+        rep.environment_path = item.cls.Env.path
+    authors = _get_closest_marker(item, name="authors")
+    if authors is not None:
+        rep.nodeid += " ({})".format(", ".join(authors.args))
+    return rep
 
 def pytest_itemcollected(item):
     authors = _get_closest_marker(item, name="authors")
