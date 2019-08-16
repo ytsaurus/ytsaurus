56f3a66441b161304243ed84c9713a7524053845
Fedor Korotkiy <prime@yandex-team.ru>
2019-08-16 13:01:10 +0300

    Move profiling tests to separate file. Fix tests dependent on test order.

--- yt/tests/integration/tests/test_sorted_dynamic_tables.py
+++ yt/tests/integration/tests/test_sorted_dynamic_tables.py
@@ -155,87 +155,6 @@ class TestSortedDynamicTables(TestSortedDynamicTablesBase):
         actual = lookup_rows("//tmp/t", keys)
         assert_items_equal(actual, rows)
 
-    @authors("gridem")
-    def test_sorted_tablet_node_profiling(self):
-        sync_create_cells(1)
-        self._create_simple_table("//tmp/t")
-        sync_mount_table("//tmp/t")
-
-        tablet_profiling = self._get_table_profiling("//tmp/t")
-        select_profiling = self._get_profiling("//tmp/t")
-
-        def get_all_counters(count_name):
-            return (
-                tablet_profiling.get_counter("lookup/" + count_name),
-                tablet_profiling.get_counter("lookup/unmerged_" + count_name),
-                tablet_profiling.get_counter("select/" + count_name),
-                tablet_profiling.get_counter("select/unmerged_" + count_name),
-                tablet_profiling.get_counter("write/" + count_name),
-                tablet_profiling.get_counter("commit/" + count_name))
-
-        assert get_all_counters("row_count") == (0, 0, 0, 0, 0, 0)
-        assert get_all_counters("data_weight") == (0, 0, 0, 0, 0, 0)
-        assert tablet_profiling.get_counter("lookup/cpu_time") == 0
-        assert select_profiling.get_counter("select/cpu_time") == 0
-
-        rows = [{"key": 1, "value": "2"}]
-        keys = [{"key": 1}]
-        insert_rows("//tmp/t", rows)
-
-        wait(lambda: get_all_counters("row_count") == (0, 0, 0, 0, 1, 1) and \
-                     get_all_counters("data_weight") == (0, 0, 0, 0, 10, 10) and \
-                     tablet_profiling.get_counter("lookup/cpu_time") == 0 and \
-                     select_profiling.get_counter("select/cpu_time") == 0)
-
-        actual = lookup_rows("//tmp/t", keys)
-        assert_items_equal(actual, rows)
-
-        wait(lambda: get_all_counters("row_count") == (1, 1, 0, 0, 1, 1) and \
-                     get_all_counters("data_weight") == (10, 25, 0, 0, 10, 10) and \
-                     tablet_profiling.get_counter("lookup/cpu_time") > 0 and \
-                     select_profiling.get_counter("select/cpu_time") == 0)
-
-        actual = select_rows("* from [//tmp/t]")
-        assert_items_equal(actual, rows)
-
-        wait(lambda: get_all_counters("row_count") == (1, 1, 1, 1, 1, 1) and \
-                     get_all_counters("data_weight") == (10, 25, 10, 25, 10, 10) and \
-                     tablet_profiling.get_counter("lookup/cpu_time") > 0 and \
-                     select_profiling.get_counter("select/cpu_time") > 0)
-
-    @authors("gridem")
-    def test_sorted_default_enabled_tablet_node_profiling(self):
-        sync_create_cells(1)
-        self._create_simple_table("//tmp/t_unique_name")
-        sync_mount_table("//tmp/t_unique_name")
-
-        table_profiling = self._get_table_profiling("//tmp/t_unique_name")
-
-        def get_all_counters(count_name):
-            return (
-                table_profiling.get_counter("lookup/" + count_name),
-                table_profiling.get_counter("write/" + count_name),
-                table_profiling.get_counter("commit/" + count_name))
-
-        assert get_all_counters("row_count") == (0, 0, 0)
-        assert get_all_counters("data_weight") == (0, 0, 0)
-        assert table_profiling.get_counter("lookup/cpu_time") == 0
-
-        rows = [{"key": 1, "value": "2"}]
-        keys = [{"key": 1}]
-        insert_rows("//tmp/t_unique_name", rows)
-
-        wait(lambda: get_all_counters("row_count") == (0, 1, 1) and \
-                     get_all_counters("data_weight") == (0, 10, 10) and \
-                     table_profiling.get_counter("lookup/cpu_time") == 0)
-
-        actual = lookup_rows("//tmp/t_unique_name", keys)
-        assert_items_equal(actual, rows)
-
-        wait(lambda: get_all_counters("row_count") == (1, 1, 1) and \
-                     get_all_counters("data_weight") == (10, 10, 10) and \
-                     table_profiling.get_counter("lookup/cpu_time") > 0)
-
     @authors("babenko", "ignat")
     def test_reshard_unmounted(self):
         sync_create_cells(1)
@@ -1296,50 +1215,6 @@ class TestSortedDynamicTables(TestSortedDynamicTablesBase):
             path = "//tmp/t/@tablets/{0}/performance_counters/static_chunk_row_lookup_count".format(tablet)
             wait(lambda: get(path) > 0)
             assert get(path) == 200
-
-    @authors("iskhakovt")
-    @pytest.mark.parametrize("optimize_for", ["scan", "lookup"])
-    @pytest.mark.parametrize("in_memory_mode", ["none", "compressed"])
-    def test_data_weight_performance_counters(self, optimize_for, in_memory_mode):
-        sync_create_cells(1)
-        self._create_simple_table("//tmp/t", optimize_for=optimize_for, in_memory_mode=in_memory_mode, dynamic_store_auto_flush_period=YsonEntity())
-        sync_mount_table("//tmp/t")
-
-        path = "//tmp/t/@tablets/0/performance_counters"
-
-        insert_rows("//tmp/t", [{"key": 0, "value": "hello"}])
-
-        wait(lambda: get(path + "/dynamic_row_write_data_weight_count") > 0)
-
-        select_rows("* from [//tmp/t]")
-
-        # Dynamic read must change, lookup must not change
-        wait(lambda: get(path + "/dynamic_row_read_data_weight_count") > 0)
-        assert get(path + "/dynamic_row_lookup_data_weight_count") == 0
-
-        lookup_rows("//tmp/t", [{"key": 0}])
-
-        # Dynamic read lookup change, read must not change
-        wait(lambda: get(path + "/dynamic_row_lookup_data_weight_count") > 0)
-        assert get(path + "/dynamic_row_read_data_weight_count") == get(path + "/dynamic_row_lookup_data_weight_count")
-
-        # Static read/lookup must not change
-        assert get(path + "/static_chunk_row_read_data_weight_count") == 0
-        assert get(path + "/static_chunk_row_lookup_data_weight_count") == 0
-
-        sync_flush_table("//tmp/t")
-
-        select_rows("* from [//tmp/t]")
-
-        # Static read must change, lookup must not change
-        wait(lambda: get(path + "/static_chunk_row_read_data_weight_count") > 0)
-        assert get(path + "/static_chunk_row_lookup_data_weight_count") == 0
-
-        lookup_rows("//tmp/t", [{"key": 0}])
-
-        # Static lookup must change, read must not change
-        wait(lambda: get(path + "/static_chunk_row_lookup_data_weight_count") > 0)
-        assert get(path + "/static_chunk_row_read_data_weight_count") == get(path + "/static_chunk_row_lookup_data_weight_count")
 
     @authors("babenko")
     def test_store_rotation(self):
