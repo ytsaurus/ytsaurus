598706b630eec2adac71939fd64271e2d5844014
Maxim Babenko <babenko@ytsaurus.tech>
2019-08-22 22:13:13 +0300

    Extract client_transactions_impl.cpp

--- yt/ytlib/api/native/client.cpp
+++ yt/ytlib/api/native/client.cpp
@@ -718,39 +718,6 @@ TFuture<void> TClient::Terminate()
     return Combine(asyncResults);
 }
 
-TFuture<ITransactionPtr> TClient::StartNativeTransaction(
-    ETransactionType type,
-    const TTransactionStartOptions& options)
-{
-    return TransactionManager_->Start(type, options).Apply(
-        BIND([=, this_ = MakeStrong(this)] (const NTransactionClient::TTransactionPtr& transaction) {
-            auto wrappedTransaction = CreateTransaction(this_, transaction, Logger);
-            return wrappedTransaction;
-        }));
-}
-
-ITransactionPtr TClient::AttachNativeTransaction(
-    TTransactionId transactionId,
-    const TTransactionAttachOptions& options)
-{
-    auto wrappedTransaction = TransactionManager_->Attach(transactionId, options);
-    return CreateTransaction(this, std::move(wrappedTransaction), Logger);
-}
-
-TFuture<NApi::ITransactionPtr> TClient::StartTransaction(
-    ETransactionType type,
-    const TTransactionStartOptions& options)
-{
-    return StartNativeTransaction(type, options).As<NApi::ITransactionPtr>();
-}
-
-NApi::ITransactionPtr TClient::AttachTransaction(
-    TTransactionId transactionId,
-    const TTransactionAttachOptions& options)
-{
-    return AttachNativeTransaction(transactionId, options);
-}
-
 TFuture<IFileReaderPtr> TClient::CreateFileReader(
     const TYPath& path,
     const TFileReaderOptions& options)
