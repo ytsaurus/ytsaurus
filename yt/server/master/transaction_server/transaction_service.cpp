e8dc716f16f23e0930b3256a9c6f55e71231ad9d
Maxim Babenko <babenko@ytsaurus.tech>
2019-08-25 22:21:38 +0300

    Sync with tx coordinator cell in RegisterTransactionActions

--- yt/server/master/transaction_server/transaction_service.cpp
+++ yt/server/master/transaction_server/transaction_service.cpp
@@ -7,14 +7,20 @@
 
 #include <yt/server/master/transaction_server/proto/transaction_manager.pb.h>
 
+#include <yt/server/lib/hive/hive_manager.h>
+
 #include <yt/ytlib/transaction_client/transaction_service_proxy.h>
+
+#include <yt/client/object_client/helpers.h>
 
 namespace NYT::NTransactionServer {
 
 using namespace NRpc;
 using namespace NTransactionClient;
+using namespace NObjectClient;
 using namespace NHydra;
 using namespace NCellMaster;
+using namespace NConcurrency;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -77,15 +83,18 @@ private:
     {
         ValidatePeer(EPeerKind::Leader);
 
-        // Wait for transaction to appear on secondary master.
-        // XXX(babenko): sync with proper cell
-        SyncWithUpstream();
-
         auto transactionId = FromProto<TTransactionId>(request->transaction_id());
 
         context->SetRequestInfo("TransactionId: %v, ActionCount: %v",
             transactionId,
             request->actions_size());
+
+        auto cellTag = CellTagFromId(transactionId);
+        auto cellId = Bootstrap_->GetCellId(cellTag);
+
+        const auto& hiveManager = Bootstrap_->GetHiveManager();
+        WaitFor(hiveManager->SyncWith(cellId, true))
+            .ThrowOnError();
 
         const auto& transactionManager = Bootstrap_->GetTransactionManager();
         transactionManager
