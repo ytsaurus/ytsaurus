743f62739dc2ab92962ebe0d7ea4832a70b448bb
Maciej <zero323@users.noreply.github.com>
2019-08-31 09:54:58 +0200

    Add SizedIterable to describe ResultIterable input / type (#200)

--- taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/resultiterable.pyi
+++ taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/resultiterable.pyi
@@ -1,15 +1,15 @@
 # Stubs for pyspark.resultiterable (Python 3.5)
 #
 
-from typing import Any, Generic, Iterable, Iterator, TypeVar
-import collections
+from pyspark._typing import SizedIterable
+from typing import Generic, Iterable, Iterator, TypeVar
 
 T = TypeVar('T')
 
-class ResultIterable(collections.Iterable, Generic[T]):
-    data: Iterable[T]
+class ResultIterable(SizedIterable[T]):
+    data: SizedIterable[T]
     index: int
     maxindex: int
-    def __init__(self, data: Iterable[T]) -> None: ...
+    def __init__(self, data: SizedIterable[T]) -> None: ...
     def __iter__(self) -> Iterator[T]: ...
     def __len__(self) -> int: ...
