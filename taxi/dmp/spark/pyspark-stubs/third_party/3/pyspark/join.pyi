6dc673419c7a512eec79f08e799856cc4b2d2841
zero323 <mszymkiewicz@gmail.com>
2019-09-05 00:37:46 +0200

    Fix python_cogroup signature

--- taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/join.pyi
+++ taxi/dmp/spark/pyspark-stubs/third_party/3/pyspark/join.pyi
@@ -1,8 +1,9 @@
 # Stubs for pyspark.join (Python 3)
 #
 
-from typing import Hashable, List, Optional, Tuple, TypeVar
+from typing import Hashable, Iterable, List, Optional, Tuple, TypeVar
 
+from pyspark.resultiterable import ResultIterable
 import pyspark.rdd
 
 K = TypeVar('K', bound=Hashable)
@@ -13,4 +14,4 @@ def python_join(rdd: pyspark.rdd.RDD[Tuple[K, V]], other: pyspark.rdd.RDD[Tuple[
 def python_right_outer_join(rdd: pyspark.rdd.RDD[Tuple[K, V]], other: pyspark.rdd.RDD[Tuple[K, U]], numPartitions: int) -> pyspark.rdd.RDD[Tuple[K, Tuple[V, Optional[U]]]]: ...
 def python_left_outer_join(rdd: pyspark.rdd.RDD[Tuple[K, V]], other: pyspark.rdd.RDD[Tuple[K, U]], numPartitions: int) -> pyspark.rdd.RDD[Tuple[K, Tuple[Optional[V], U]]]: ...
 def python_full_outer_join(rdd: pyspark.rdd.RDD[Tuple[K, V]], other: pyspark.rdd.RDD[Tuple[K, U]], numPartitions: int) -> pyspark.rdd.RDD[Tuple[K, Tuple[Optional[V], Optional[U]]]]: ...
-def python_cogroup(rdds, numPartitions) -> pyspark.rdd.RDD[Tuple[K, Tuple[List[V], List[U]]]]: ...
+def python_cogroup(rdds: Iterable[pyspark.rdd.RDD[Tuple[K, V]]], numPartitions) -> pyspark.rdd.RDD[Tuple[K, Tuple[ResultIterable[V], ...]]]: ...
