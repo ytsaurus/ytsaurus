f4d3fc60c0e3c2a7b96f443663027dabbb6ad5e9
Nikita Morozov <nikitamorozov@yandex-team.ru>
2019-09-26 13:03:48 +0300

    YT-11353 Add yson token writer and separate syntax checker from pull parser

--- yt/core/yson/pull_parser.h
+++ yt/core/yson/pull_parser.h
@@ -4,6 +4,8 @@
 
 #include "detail.h"
 
+#include "syntax_checker.h"
+
 #include <yt/core/misc/variant.h>
 
 #include <yt/core/ytree/convert.h>
@@ -17,22 +19,6 @@
 namespace NYT::NYson {
 
 ////////////////////////////////////////////////////////////////////////////////
-
-DEFINE_ENUM_WITH_UNDERLYING_TYPE(EYsonItemType, ui8,
-    (EndOfStream)
-    (BeginMap)
-    (EndMap)
-    (BeginAttributes)
-    (EndAttributes)
-    (BeginList)
-    (EndList)
-    (EntityValue)
-    (BooleanValue)
-    (Int64Value)
-    (Uint64Value)
-    (DoubleValue)
-    (StringValue)
-);
 
 // TYsonItem represents single meaningful yson item.
 // We don't use std::variant for performance reasons.
@@ -108,82 +94,6 @@ private:
     const char* Current_ = nullptr;
     ui64 TotalReadBlocksSize_ = 0;
     bool Finished_ = false;
-};
-
-////////////////////////////////////////////////////////////////////////////////
-
-DEFINE_ENUM_WITH_UNDERLYING_TYPE(EYsonState, ui8,
-    (Terminated)
-    (ExpectValue)
-    (ExpectAttributelessValue)
-
-    // top level list fragment
-    (InsideListFragmentExpectAttributelessValue)
-    (InsideListFragmentExpectValue)
-    (InsideListFragmentExpectSeparator)
-
-    // top level map fragment
-    (InsideMapFragmentExpectKey)
-    (InsideMapFragmentExpectEquality)
-    (InsideMapFragmentExpectAttributelessValue)
-    (InsideMapFragmentExpectValue)
-    (InsideMapFragmentExpectSeparator)
-
-    (InsideMapExpectKey)
-    (InsideMapExpectEquality)
-    (InsideMapExpectAttributelessValue)
-    (InsideMapExpectValue)
-    (InsideMapExpectSeparator)
-
-    (InsideAttributeMapExpectKey)
-    (InsideAttributeMapExpectEquality)
-    (InsideAttributeMapExpectAttributelessValue)
-    (InsideAttributeMapExpectValue)
-    (InsideAttributeMapExpectSeparator)
-
-    (InsideListExpectAttributelessValue)
-    (InsideListExpectValue)
-    (InsideListExpectSeparator)
-);
-
-////////////////////////////////////////////////////////////////////////////////
-
-class TYsonSyntaxChecker
-{
-public:
-    explicit TYsonSyntaxChecker(EYsonType ysonType);
-
-    Y_FORCE_INLINE void OnSimpleNonstring(EYsonItemType itemType);
-    Y_FORCE_INLINE void OnString();
-    Y_FORCE_INLINE void OnFinish();
-    Y_FORCE_INLINE void OnEquality();
-    Y_FORCE_INLINE void OnSeparator();
-    Y_FORCE_INLINE void OnBeginList();
-    Y_FORCE_INLINE void OnEndList();
-    Y_FORCE_INLINE void OnBeginMap();
-    Y_FORCE_INLINE void OnEndMap();
-    Y_FORCE_INLINE void OnAttributesBegin();
-
-    Y_FORCE_INLINE void OnAttributesEnd();
-
-    Y_FORCE_INLINE size_t GetNestingLevel() const;
-    Y_FORCE_INLINE bool IsOnValueBoundary(size_t nestingLevel) const;
-
-private:
-    template <bool isString>
-    Y_FORCE_INLINE void OnSimple(EYsonItemType itemType);
-    Y_FORCE_INLINE void IncrementNestingLevel();
-    Y_FORCE_INLINE void DecrementNestingLevel();
-
-    static TStringBuf StateExpectationString(EYsonState state);
-    void ThrowUnexpectedToken(TStringBuf token);
-
-private:
-    SmallVector<EYsonState, 16> StateStack_;
-    // We don't use stack size, we compute depth level precisely to be compatible with old yson parser.
-    ui32 NestingLevel_ = 0;
-
-    static constexpr ui32 NestingLevelLimit = 64;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
