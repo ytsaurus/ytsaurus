5b58471b2b39a929b5f6871011951668272e617e
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-09-29 00:37:00 +0300

    YT-10128: Support building config from environment

--- python-git/yt/wrapper/default_config.py
+++ python-git/yt/wrapper/default_config.py
@@ -1,8 +1,15 @@
+from __future__ import print_function
+
 from . import common
 from .mappings import VerifiedDict
 
+import yt.yson as yson
+import yt.json_wrapper as json
+import yt.packages.six as six
 from yt.yson import YsonEntity
 
+import os
+import sys
 from copy import deepcopy
 
 # pydoc :: default_config :: begin
@@ -577,3 +584,190 @@ def get_default_config():
         template_dict=deepcopy(default_config),
         keys_to_ignore=["spec_defaults", "spec_overrides", "table_writer", "user_job_spec_defaults"],
         transform_func=transform_value)
+
+
+
+SHORTCUTS = {
+    "PROXY": "proxy/url",
+    "PROXY_SUFFIX": "proxy/default_suffix",
+    "TOKEN": "token",
+    "TOKEN_PATH": "token_path",
+    "USE_TOKEN": "enable_token",
+    "CHECK_TOKEN": "check_token",
+    "ACCEPT_ENCODING": "proxy/accept_encoding",
+    "CONTENT_ENCODING": "proxy/content_encoding",
+    "FORCE_IPV4": "proxy/force_ipv4",
+    "FORCE_IPV6": "proxy/force_ipv6",
+
+    "VERSION": "api_version",
+
+    "DRIVER_CONFIG": "driver_config",
+    "DRIVER_CONFIG_PATH": "driver_config_path",
+
+    "USE_HOSTS": "proxy/enable_proxy_discovery",
+    "HOSTS": "proxy/proxy_discovery_url",
+
+    "ALWAYS_SET_EXECUTABLE_FLAG_TO_FILE": "yamr_mode/always_set_executable_flag_on_files",
+    "USE_MAPREDUCE_STYLE_DESTINATION_FDS": "yamr_mode/use_yamr_style_destination_fds",
+    "TREAT_UNEXISTING_AS_EMPTY": "yamr_mode/treat_unexisting_as_empty",
+    "DELETE_EMPTY_TABLES": "yamr_mode/delete_empty_tables",
+    "USE_YAMR_SORT_REDUCE_COLUMNS": "yamr_mode/use_yamr_sort_reduce_columns",
+    "REPLACE_TABLES_WHILE_COPY_OR_MOVE": "yamr_mode/replace_tables_on_copy_and_move",
+    "CREATE_RECURSIVE": "yamr_mode/create_recursive",
+    "THROW_ON_EMPTY_DST_LIST": "yamr_mode/throw_on_missing_destination",
+    "RUN_MAP_REDUCE_IF_SOURCE_IS_NOT_SORTED": "yamr_mode/run_map_reduce_if_source_is_not_sorted",
+    "USE_NON_STRICT_UPPER_KEY": "yamr_mode/use_non_strict_upper_key",
+    "CHECK_INPUT_FULLY_CONSUMED": "yamr_mode/check_input_fully_consumed",
+    "FORCE_DROP_DST": "yamr_mode/abort_transactions_with_remove",
+    "USE_YAMR_STYLE_PREFIX": "yamr_mode/use_yamr_style_prefix",
+
+    "OPERATION_STATE_UPDATE_PERIOD": "operation_tracker/poll_period",
+    "STDERR_LOGGING_LEVEL": "operation_tracker/stderr_logging_level",
+    "IGNORE_STDERR_IF_DOWNLOAD_FAILED": "operation_tracker/ignore_stderr_if_download_failed",
+    "KEYBOARD_ABORT": "operation_tracker/abort_on_sigint",
+
+    "FILE_STORAGE": "remote_temp_files_directory",
+    "LOCAL_TMP_DIR": "local_temp_directory",
+
+    # Deprecated
+    "TEMP_TABLES_STORAGE": "remote_temp_tables_directory",
+    # Non-deprecated version of TEMP_TABLES_STORAGE
+    "TEMP_DIR": "remote_temp_tables_directory",
+
+    "PREFIX": "prefix",
+
+    "POOL": "pool",
+    "MEMORY_LIMIT": "memory_limit",
+    "SPEC": "spec_defaults",
+    "TABLE_WRITER": "table_writer",
+
+    "RETRY_READ": "read_retries/enable",
+    "USE_RETRIES_DURING_WRITE": "write_retries/enable",
+    "USE_RETRIES_DURING_UPLOAD": "write_retries/enable",
+
+    "CHUNK_SIZE": "write_retries/chunk_size",
+
+    "DETACHED": "detached",
+
+    "TABULAR_DATA_FORMAT": "tabular_data_format",
+
+    "CONFIG_PATH": "config_path",
+    "CONFIG_FORMAT": "config_format",
+
+    "ARGCOMPLETE_VERBOSE": "argcomplete_verbose",
+
+    "USE_YAMR_DEFAULTS": "yamr_mode/use_yamr_defaults",
+    "IGNORE_EMPTY_TABLES_IN_MAPREDUCE_LIST": "yamr_mode/ignore_empty_tables_in_mapreduce_list"
+}
+
+def get_config_from_env():
+    def _get_var_type(value):
+        var_type = type(value)
+        # Using int we treat "0" as false, "1" as "true"
+        if var_type == bool:
+            try:
+                value = int(value)
+            except:
+                pass
+        # None type is treated as str
+        if isinstance(None, var_type):
+            var_type = str
+        if var_type == dict:
+            var_type = lambda obj: yson.json_to_yson(json.loads(obj)) if obj else {}
+        return var_type
+
+    def _apply_type(type, key, value):
+        try:
+            return type(value)
+        except ValueError:
+            raise common.YtError("Incorrect value of option '{0}': failed to apply type '{1}' to '{2}'".format(key, type, value))
+
+    def _set(d, key, value):
+        parts = key.split("/")
+        for k in parts[:-1]:
+            d = d[k]
+        d[parts[-1]] = value
+
+    def _get(d, key):
+        parts = key.split("/")
+        for k in parts:
+            d = d.get(k)
+        return d
+
+    config = get_default_config()
+
+    if "YT_CONFIG_PATCHES" in os.environ:
+        try:
+            patches = yson._loads_from_native_str(os.environ["YT_CONFIG_PATCHES"],
+                                                  yson_type="list_fragment",
+                                                  always_create_attributes=False)
+        except yson.YsonError:
+            print("Failed to parse YT config patches from 'YT_CONFIG_PATCHES' environment variable", file=sys.stderr)
+            raise
+
+        try:
+            for patch in reversed(list(patches)):
+                common.update_inplace(config, patch)
+        except:
+            print("Failed to apply config from 'YT_CONFIG_PATCHES' environment variable", file=sys.stderr)
+            raise
+
+    # These options should be processed before reading config file
+    for opt_name in ["YT_CONFIG_PATH", "YT_CONFIG_FORMAT"]:
+        if opt_name in os.environ:
+            config[SHORTCUTS[opt_name[3:]]] = os.environ[opt_name]
+
+    config_path = config["config_path"]
+    if config_path is None:
+        home = None
+        try:
+            home = os.path.expanduser("~")
+        except KeyError:
+            pass
+
+        config_path = "/etc/ytclient.conf"
+        if home:
+            home_config_path = os.path.join(os.path.expanduser("~"), ".yt/config")
+            if os.path.isfile(home_config_path):
+                config_path = home_config_path
+
+        try:
+            open(config_path, "r")
+        except IOError:
+            config_path = None
+    if config_path and os.path.isfile(config_path):
+        load_func = None
+        format = config["config_format"]
+        if format == "yson":
+            load_func = yson.load
+        elif format == "json":
+            load_func = json.load
+        else:
+            raise common.YtError("Incorrect config_format '%s'" % format)
+        try:
+            common.update_inplace(config, load_func(open(config_path, "rb")))
+        except Exception:
+            print("Failed to parse YT config from " + config_path, file=sys.stderr)
+            raise
+
+    for key, value in six.iteritems(os.environ):
+        prefix = "YT_"
+        if not key.startswith(prefix):
+            continue
+
+        key = key[len(prefix):]
+        if key in SHORTCUTS:
+            name = SHORTCUTS[key]
+            var_type = _get_var_type(_get(config, name))
+            # NB: it is necessary to set boolean vaiable as 0 or 1.
+            if var_type is bool:
+                value = int(value)
+            _set(config, name, _apply_type(var_type, key, value))
+        # Some shortcuts cannot be backported one-to-one so they are processed manually.
+        elif key == "MERGE_INSTEAD_WARNING":
+            _set(config, "auto_merge_output/action", "merge" if int(value) else "log")
+        elif key == "CREATE_TABLES_UNDER_TRANSACTION":
+            print(value, bool(int(value)), file=sys.stderr)
+            _set(config, "yamr_mode/create_tables_outside_of_transaction", not bool(int(value)))
+
+    return config
