0dbe58fea95cbb59b0af1ef2d347d0a4f36492dd
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-09-29 20:46:44 +0300

    Revert "YT-10128: Support building config from environment"
    This reverts commit c0f4499804ac03359e3cf557c3e3a30e8462afb4.

--- python-git/yt/wrapper/tests/test_config.py
+++ python-git/yt/wrapper/tests/test_config.py
@@ -1,6 +1,5 @@
 from yt.common import update
 
-import yt.wrapper.default_config as default_config_module
 import yt.wrapper as yt
 
 import os
@@ -13,9 +12,11 @@ class TestConfig(object):
         env_merge_option = os.environ.get("YT_MERGE_INSTEAD_WARNING", None)
         try:
             os.environ["YT_MERGE_INSTEAD_WARNING"] = "1"
-            assert default_config_module.get_config_from_env()["auto_merge_output"]["action"] == "merge"
+            yt.config._update_from_env()
+            assert yt.config["auto_merge_output"]["action"] == "merge"
             os.environ["YT_MERGE_INSTEAD_WARNING"] = "0"
-            assert default_config_module.get_config_from_env()["auto_merge_output"]["action"] == "log"
+            yt.config._update_from_env()
+            assert yt.config["auto_merge_output"]["action"] == "log"
         finally:
             if env_merge_option is not None:
                 os.environ["YT_MERGE_INSTEAD_WARNING"] = env_merge_option
