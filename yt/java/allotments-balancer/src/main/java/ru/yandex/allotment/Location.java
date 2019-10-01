5a01c420b9c53bb2527833f3c2ebc9dc57379b6d
aozeritsky <aozeritsky@yandex-team.ru>
2019-10-01 14:59:32 +0300

    Colorize output Note: mandatory check (NEED_CHECK) was skipped

--- yt/java/allotments-balancer/src/main/java/ru/yandex/allotment/Location.java
+++ yt/java/allotments-balancer/src/main/java/ru/yandex/allotment/Location.java
@@ -8,10 +8,19 @@ public class Location {
     public final String id;
     public final int allotmentIndex;
     public final int primaryAllotmentIndex;
+    public final boolean isDead;
 
     public Location(Map<String, YTreeNode> attributes) {
         this.id = attributes.get("location_id").stringValue();
         this.allotmentIndex = attributes.get("allotment_index").intValue();
         this.primaryAllotmentIndex = attributes.get("primary_allotment_index").intValue();
+        this.isDead = false;
+    }
+
+    public Location(String id, int allotmentIndex, int primaryAllotmentIndex) {
+        this.id = id;
+        this.allotmentIndex = allotmentIndex;
+        this.primaryAllotmentIndex = primaryAllotmentIndex;
+        this.isDead = true;
     }
 }
