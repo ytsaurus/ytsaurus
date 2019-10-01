e2be284a69973bd172f81869d45e15f6323e350c
aozeritsky <aozeritsky@yandex-team.ru>
2019-10-01 14:20:17 +0300

    Allotments Balancer Note: mandatory check (NEED_CHECK) was skipped

---
+++ yt/java/allotments-balancer/src/main/java/ru/yandex/allotment/Node.java
@@ -0,0 +1,78 @@
+package ru.yandex.allotment;
+
+import java.time.ZoneId;
+import java.time.ZonedDateTime;
+import java.time.format.DateTimeFormatter;
+import java.util.List;
+import java.util.Map;
+
+import ru.yandex.bolts.collection.Cf;
+import ru.yandex.inside.yt.kosher.Yt;
+import ru.yandex.inside.yt.kosher.cypress.YPath;
+import ru.yandex.inside.yt.kosher.impl.ytree.builder.YTree;
+import ru.yandex.inside.yt.kosher.ytree.YTreeNode;
+
+public class Node {
+    public final String addr;
+
+    public final List<Location> locations;
+
+    // location -> index
+    public final Map<String, Integer> allotmentAssignment;
+
+    private final DateTimeFormatter format = DateTimeFormatter.ofPattern("yyyy-MM-dd'T'HH:mm:ss.SSSSSS'Z'").withZone(ZoneId.of("UTC"));
+    public long lastSeenTime;
+
+    public boolean dirty = false;
+
+    public Node(String addr, Map<String, YTreeNode> attributes) {
+        this.addr = addr;
+        this.allotmentAssignment = Cf.hashMap();
+
+        Map<String, YTreeNode> allotmentAssignmentRow = attributes.get("allotments_assignment").asMap();
+
+        for (Map.Entry<String, YTreeNode> entity : allotmentAssignmentRow.entrySet()) {
+            this.allotmentAssignment.put(entity.getKey(), entity.getValue().intValue());
+        }
+
+        this.locations = Cf.arrayList();
+
+        for (YTreeNode locationRow : attributes.get("statistics").asMap().get("locations").asList()) {
+            Map<String, YTreeNode> locationAttributes = locationRow.asMap();
+            if (!locationAttributes.containsKey("location_id")) {
+                continue;
+            }
+
+            Location location = new Location(locationAttributes);
+
+            this.locations.add(location);
+        }
+
+        String lastSeenTimeRow = attributes.get("last_seen_time").stringValue();
+
+        lastSeenTime = ZonedDateTime.parse(lastSeenTimeRow, format).toEpochSecond();
+    }
+
+    public Node(String addr, Yt yt) {
+        this(addr, yt.cypress().get(YPath.simple("//sys/nodes").child(addr).allAttributes()).asMap());
+    }
+
+
+    public void assignAllotmentToLocation(String locationId, int allotmentIndex) {
+        allotmentAssignment.put(locationId, allotmentIndex);
+        dirty = true;
+    }
+
+    public void upload(Yt yt, boolean dryRun) {
+        if (!dirty) {
+            return;
+        }
+
+        System.out.println(String.format("%s -> %s", addr, allotmentAssignment));
+
+        if (!dryRun) {
+            yt.cypress().set(YPath.simple("//sys/nodes").child(addr).attribute("allotments_assignment"),
+                    YTree.builder().value(allotmentAssignment).build());
+        }
+    }
+}
