a3cbe08a53fa17b3903dd041dcd0ffc1be494505
Maxim Babenko <babenko@ytsaurus.tech>
2019-10-02 23:18:23 +0300

    Invoke MlockallCurrentProcess for all latency-sensitive binaries

--- yt/server/node/cell_node/program.h
+++ yt/server/node/cell_node/program.h
@@ -53,6 +53,7 @@ protected:
         NYTAlloc::SetLibunwindBacktraceProvider();
         NYTAlloc::ConfigureFromEnv();
         NYTAlloc::EnableStockpile();
+        NYTAlloc::MlockallCurrentProcess();
 
         if (HandlePdeathsigOptions()) {
             return;
--- yt/server/rpc_proxy/program.h
+++ yt/server/rpc_proxy/program.h
@@ -42,6 +42,7 @@ protected:
         NYTAlloc::SetLibunwindBacktraceProvider();
         NYTAlloc::ConfigureFromEnv();
         NYTAlloc::EnableStockpile();
+        NYTAlloc::MlockallCurrentProcess();
 
         if (HandlePdeathsigOptions()) {
             return;
