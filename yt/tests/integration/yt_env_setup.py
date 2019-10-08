e8eb7489293924f3e2446ca67c314a506e00924c
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2019-10-08 07:30:46 +0300

    Add test with multiple node unregistrations in scheduler

--- yt/tests/integration/yt_env_setup.py
+++ yt/tests/integration/yt_env_setup.py
@@ -424,25 +424,29 @@ CONTROLLER_AGENTS_SERVICE = "controller_agents"
 NODES_SERVICE = "nodes"
 MASTER_CELL_SERVICE = "master_cell"
 
+# TODO(ignat): move it to python-repo
 class Restarter(object):
-    def __init__(self, Env, components):
+    def __init__(self, Env, components, *args, **kwargs):
         self.Env = Env
         self.components = components
         if type(self.components) == str:
             self.components = [self.components]
-        self.kill_dict = {SCHEDULERS_SERVICE: self.Env.kill_schedulers,
-                          CONTROLLER_AGENTS_SERVICE: self.Env.kill_controller_agents,
-                          NODES_SERVICE: self.Env.kill_nodes,
-                          MASTER_CELL_SERVICE: self.Env.kill_master_cell}
+        self.kill_args = args
+        self.kill_kwargs = kwargs
+
         self.start_dict = {SCHEDULERS_SERVICE: self.Env.start_schedulers,
                            CONTROLLER_AGENTS_SERVICE: self.Env.start_controller_agents,
                            NODES_SERVICE: self.Env.start_nodes,
                            MASTER_CELL_SERVICE: self.Env.start_master_cell}
+        self.name_dict = {SCHEDULERS_SERVICE: "scheduler",
+                          CONTROLLER_AGENTS_SERVICE: "controller_agent",
+                          NODES_SERVICE: "node",
+                          MASTER_CELL_SERVICE: "master"}
 
     def __enter__(self):
         for comp_name in self.components:
             try:
-                self.kill_dict[comp_name]()
+                self.Env.kill_service(self.name_dict[comp_name], *self.kill_args, **self.kill_kwargs)
             except KeyError:
                 logging.error("Failed to kill {}. No such component.".format(comp_name))
                 raise
