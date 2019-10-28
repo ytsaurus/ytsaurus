4421eddf9d19ca6b33a77e6a555fea8b0b627cfe
Vladislav Bidzilya <bidzilya@yandex-team.ru>
2019-10-28 12:28:32 +0300

    Set robot-yt-openstack in SVN_SSH during ya tests run

--- scripts/teamcity-build.py
+++ scripts/teamcity-build.py
@@ -208,7 +208,7 @@ def get_git_depth(options):
 def ya_make_env(options):
     return {
         "YA_CACHE_DIR": get_ya_cache_dir(options),
-        "SVN_SSH": "ssh -v",
+        "SVN_SSH": "ssh -v -l robot-yt-openstack ",
     }
 
 def ya_make_definition_args(options):
