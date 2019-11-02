eed1372d4f890e63e4e81287f16ab8783e75664a
Maxim Babenko <babenko@ytsaurus.tech>
2019-11-02 12:42:19 +0300

    Disable flaky test_set_final_job_state_metrics

--- yt/tests/integration/tests/test_scheduler_vanilla.py
+++ yt/tests/integration/tests/test_scheduler_vanilla.py
@@ -402,7 +402,7 @@ class TestSchedulerVanillaCommands(YTEnvSetup):
         assert op.get_job_count("failed", from_orchid=False) == 0
 
     @authors("gritukan")
-    def test_set_final_job_state_metrics(self):
+    def DISABLED_test_set_final_job_state_metrics(self):
         nodes = ls("//sys/nodes")
 
         metric = Metric.at_node(nodes[0], "job_controller/job_final_state")
