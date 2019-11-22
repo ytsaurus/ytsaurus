e102d56763f4228008626459bf4d878a9e94acbd
Alexandra Belousova <sashbel@yandex-team.ru>
2019-11-22 17:24:56 +0300

    Start SHS in yt vanilla job

--- taxi/dmp/spark/spark-over-yt/spark-launcher/src/main/scala/ru/yandex/spark/launcher/MasterLauncher.scala
+++ taxi/dmp/spark/spark-over-yt/spark-launcher/src/main/scala/ru/yandex/spark/launcher/MasterLauncher.scala
@@ -1,24 +1,20 @@
 package ru.yandex.spark.launcher
 
-import com.google.common.net.HostAndPort
 import com.twitter.scalding.Args
 import org.apache.log4j.Logger
-import ru.yandex.spark.discovery.{CypressDiscoveryService, DiscoveryService}
 import ru.yandex.spark.yt.utils.YtClientConfiguration
 
-import scala.annotation.tailrec
 import scala.concurrent.duration._
 import scala.language.postfixOps
 
-object MasterLauncher extends App {
+object MasterLauncher extends App with SparkLauncher {
   private val log = Logger.getLogger(getClass)
   val masterArgs = MasterLauncherArgs(args)
-  val discoveryService = new CypressDiscoveryService(masterArgs.ytConfig, masterArgs.discoveryPath)
 
-  try {
+  run(masterArgs.ytConfig, masterArgs.discoveryPath){discoveryService =>
     log.info("Start master")
-    val (masterAddress, masterThread) = SparkLauncher.startMaster(masterArgs.port, masterArgs.webUiPort, masterArgs.opts)
-    val masterAlive = discoveryService.waitAlive(masterAddress.hostAndPort, (5 minutes).toMillis)
+    val masterAddress = startMaster(masterArgs.port, masterArgs.webUiPort, masterArgs.opts)
+    val masterAlive = discoveryService.waitAlive(masterAddress.hostAndPort, 5 minutes)
     if (!masterAlive) {
       throw new RuntimeException("Master is not started")
     }
@@ -29,17 +25,12 @@ object MasterLauncher extends App {
     log.info("Master registered")
 
     try {
-      DiscoveryService.checkPeriodically(masterThread.isAlive)
+      checkPeriodically(sparkThreadIsAlive)
       log.info("Master thread is not alive!")
     } finally {
       log.info("Removing master address")
       discoveryService.removeAddress(masterArgs.id)
     }
-  } finally {
-    log.info("Closing discovery service")
-    discoveryService.close()
-    log.info("Discovery service closed")
-    log.info(s"Threads: ${Thread.activeCount()}")
   }
 }
 
@@ -54,8 +45,8 @@ case class MasterLauncherArgs(id: String,
 object MasterLauncherArgs {
   def apply(args: Args): MasterLauncherArgs = MasterLauncherArgs(
     args.required("id"),
-    args.optional("port").map(_.toInt).getOrElse(7077),
-    args.optional("web-ui-port").map(_.toInt).getOrElse(8080),
+    args.required("port").toInt,
+    args.required("web-ui-port").toInt,
     YtClientConfiguration(args.optional),
     args.optional("discovery-path").getOrElse(sys.env("SPARK_DISCOVERY_PATH")),
     args.required("operation-id"),
