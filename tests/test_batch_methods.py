6ae1c9853fad02c606dc4627c1d22c2450c32239
vett0 <vett0@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2019-11-27 12:07:55 +0000

    Add batch remove to YpClient
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@6014486 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- tests/test_batch_methods.py
+++ tests/test_batch_methods.py
@@ -1,3 +1,7 @@
+from yp.common import YpNoSuchObjectError
+
+from yt.packages.six.moves import xrange
+
 import pytest
 
 
@@ -36,3 +40,38 @@ class TestBatchMethods(object):
         assert len(select()) == 0
         yp_client.commit_transaction(transaction_id)
         assert set(select()) == set(["pod1", "pod2"])
+
+    def test_remove_objects_doesnt_change_order(self, yp_env):
+        yp_client = yp_env.yp_client
+
+        def _prepare_objects(pod_set_count, pod_count_per_pod_set):
+            pod_set_ids = yp_client.create_objects(("pod_set", {}) for _ in xrange(pod_set_count))
+            pods_attrs = [
+                {"meta": {"pod_set_id": pod_set_id}}
+                for pod_set_id in pod_set_ids
+                for _ in xrange(pod_count_per_pod_set)
+            ]
+            pod_ids = yp_client.create_objects(("pod", pod_attrs) for pod_attrs in pods_attrs)
+
+            return pod_set_ids, pod_ids
+
+        POD_SET_COUNT = 4
+        POD_COUNT_PER_POD_SET = 5
+
+        pod_set_ids, pod_ids = _prepare_objects(POD_SET_COUNT, POD_COUNT_PER_POD_SET)
+
+        assert all(map(lambda response: response[1] in pod_set_ids, yp_client.get_objects("pod", pod_ids, selectors=["/meta/id", "/meta/pod_set_id"])))
+
+        # There are no pods after removing pod sets associated with pods, so removing pods should raise YpNoSuchObjectError
+        with pytest.raises(YpNoSuchObjectError):
+            yp_client.remove_objects([("pod_set", pod_set_id) for pod_set_id in pod_set_ids] + [("pod", pod_id) for pod_id in pod_ids])
+
+        # Verify that remove_objects doesn't remove any object as it should be transactional
+        assert len(yp_client.select_objects("pod_set", selectors=["/meta/id"])) == POD_SET_COUNT
+        assert len(yp_client.select_objects("pod", selectors=["/meta/id"])) == POD_SET_COUNT * POD_COUNT_PER_POD_SET
+
+        yp_client.remove_objects([("pod", pod_id) for pod_id in pod_ids] + [("pod_set", pod_set_id) for pod_set_id in pod_set_ids])
+
+        assert len(yp_client.select_objects("pod_set", selectors=["/meta/id"])) == 0
+        assert len(yp_client.select_objects("pod", selectors=["/meta/id"])) == 0
+
