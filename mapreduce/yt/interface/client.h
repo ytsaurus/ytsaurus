01130afcc6f8a353dd5e7124b9543503cdc9e2ab
ermolovd <ermolovd@ytsaurus.tech>
2019-11-27 19:56:46 +0300

    Introduce doxyt -- script for generating doxygen documentation for YT Wrapper

--- mapreduce/yt/interface/client.h
+++ mapreduce/yt/interface/client.h
@@ -1,5 +1,30 @@
 #pragma once
 
+///
+/// @file mapreduce/yt/interface/client.h
+///
+/// Main header of the C++ YT Wrapper.
+
+///
+/// @mainpage C++ library for working with YT
+///
+/// This library provides possibilities to work with YT as a [MapReduce](https://en.wikipedia.org/wiki/MapReduce) sytem. It allows:
+///   - to read/write tables and files
+///   - to run operations
+///   - to work with transactions.
+///
+/// This library provides only basic functions for working with dynamic tables.
+/// To access full powers of YT dynamic tables one should use
+/// [yt/client](https://a.yandex-team.ru/arc/trunk/arcadia/yt/19_4/yt/client) library.
+///
+/// Entry points to this library.
+///   - @ref NYT::Initialize() initialization function for this library
+///   - @ref NYT::IClient main interface to work with YT cluster
+///   - @ref NYT::CreateClient() function that creates client for particular cluster
+///   - @ref NYT::IOperationClient ancestor of IClient containing the set of methods to run operations
+///
+/// Tutorial on using this library can be found [here](https://wiki.yandex-team.ru/yt/userdoc/cppapi/tutorial/).
+
 #include "fwd.h"
 
 #include "client_method_options.h"
@@ -57,25 +82,30 @@ class ILock
 public:
     virtual ~ILock() = default;
 
+    /// Get cypress node id of lock itself.
     virtual const TLockId& GetId() const = 0;
 
-    // Returns locked Cypress node id.
+    /// Get cypress node id of locked object.
     virtual TNodeId GetLockedNodeId() const = 0;
 
-    // Returns future that will be set once lock is in "acquired" state.
-    //
-    // Note that future might contain exception if some error occurred
-    // e.g. lock transaction was aborted.
+    ///
+    /// @brief Get future that will be set once lock is in "acquired" state.
+    ///
+    /// Note that future might contain exception if some error occurred
+    /// e.g. lock transaction was aborted.
     virtual const NThreading::TFuture<void>& GetAcquiredFuture() const = 0;
 
-    // Convenient wrapper that waits until lock is in "acquired" state.
-    // Throws exception if timeout exceeded or some error occurred
-    // e.g. lock transaction was aborted.
+    ///
+    /// @brief Wait until lock is in "acquired" state.
+    ///
+    /// Throws exception if timeout exceeded or some error occurred
+    /// e.g. lock transaction was aborted.
     void Wait(TDuration timeout = TDuration::Max());
 };
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/// Base class for @ref NYT::IClient and @ref NYT::ITransaction
 class IClientBase
     : public TThrRefBase
     , public ICypressClient
@@ -86,21 +116,23 @@ public:
     [[nodiscard]] virtual ITransactionPtr StartTransaction(
         const TStartTransactionOptions& options = TStartTransactionOptions()) = 0;
 
-    //
-    // Change properties of table:
-    //   - switch table between dynamic/static mode
-    //   - or change table schema
+    ///
+    /// @brief Change properties of table.
+    ///
+    /// Allows to:
+    ///   - switch table between dynamic/static mode
+    ///   - or change table schema
     virtual void AlterTable(
         const TYPath& path,
         const TAlterTableOptions& options = TAlterTableOptions()) = 0;
 
-    //
-    // Create batch request object that allows to execute several light requests in parallel.
-    // https://wiki.yandex-team.ru/yt/userdoc/api/#executebatch18.4
+    ///
+    /// @brief Create batch request object that allows to execute several light requests in parallel.
+    ///
+    /// @see [YT doc](https://yt.yandex-team.ru/docs//api/commands.html#execute-batch)
     virtual TBatchRequestPtr CreateBatchRequest() = 0;
 
-    //
-    // Return 'this' for IClient and the underlying client for ITransaction.
+    /// @brief Get root client outside of all transactions.
     virtual IClientPtr GetParentClient() = 0;
 };
 
@@ -112,49 +144,51 @@ class ITransaction
 public:
     virtual const TTransactionId& GetId() const = 0;
 
-    //
-    // Try to lock given path.
-    //
-    // Lock will be held until transaction is commited/aborted or `Unlock` method is called.
-    // Lock modes:
-    //   LM_EXCLUSIVE: if exclusive lock is taken no other transaction can take exclusive or shared lock.
-    //   LM_SHARED: if shared lock is taken other transactions can take shared lock but not exclusive.
-    //
-    //   LM_SNAPSHOT: snapshot lock always succeeds, when snapshot lock is taken current transaction snapshots object.
-    //   It will not see changes that occured to it in other transactions.
-    //
-    // Exclusive/shared lock can be waitable or not.
-    // If nonwaitable lock cannot be taken exception is thrown.
-    // If waitable lock cannot be taken it is created in pending state and client can wait until it actually taken.
-    // Check TLockOptions::Waitable and ILock::GetAcquiredFuture for more details.
+    ///
+    /// \brief Try to lock given path.
+    ///
+    /// Lock will be held until transaction is commited/aborted or `Unlock` method is called.
+    /// Lock modes:
+    ///  LM_EXCLUSIVE: if exclusive lock is taken no other transaction can take exclusive or shared lock.
+    ///  LM_SHARED: if shared lock is taken other transactions can take shared lock but not exclusive.
+    ///
+    ///  LM_SNAPSHOT: snapshot lock always succeeds, when snapshot lock is taken current transaction snapshots object.
+    ///  It will not see changes that occured to it in other transactions.
+    ///
+    /// Exclusive/shared lock can be waitable or not.
+    /// If nonwaitable lock cannot be taken exception is thrown.
+    /// If waitable lock cannot be taken it is created in pending state and client can wait until it actually taken.
+    /// Check TLockOptions::Waitable and ILock::GetAcquiredFuture for more details.
     virtual ILockPtr Lock(
         const TYPath& path,
         ELockMode mode,
         const TLockOptions& options = TLockOptions()) = 0;
 
-    //
-    // Remove all the locks (including pending ones)
-    // for this transaction from a Cypress node at `path`.
-    //
-    // If the locked version of the node differs from the original one,
-    // an error will be thrown.
-    //
-    // Command is successful even if the node has no locks.
-    // Only explicit (created by `Lock` method) locks are removed.
+    ///
+    /// \brief Remove all the locks (including pending ones) for this transaction from a Cypress node at `path`.
+    ///
+    /// If the locked version of the node differs from the original one,
+    /// an error will be thrown.
+    ///
+    /// Command is successful even if the node has no locks.
+    /// Only explicit (created by `Lock` method) locks are removed.
     virtual void Unlock(
         const TYPath& path,
         const TUnlockOptions& options = TUnlockOptions()) = 0;
 
-    //
-    // Commit transaction. All changes that are made by transactions become visible globaly or to parent transaction.
+    ///
+    /// \brief Commit transaction.
+    ///
+    /// All changes that are made by transactions become visible globaly or to parent transaction.
     virtual void Commit() = 0;
 
-    //
-    // Abort transaction. All changes that are made by current transaction are lost.
+    ///
+    /// \brief Abort transaction.
+    ///
+    /// All changes that are made by current transaction are lost.
     virtual void Abort() = 0;
 
-    //
-    // Ping transaction.
+    /// Ping transaction.
     virtual void Ping() = 0;
 };
 
@@ -164,12 +198,12 @@ class IClient
     : virtual public IClientBase
 {
 public:
-    //
-    // Attach to existing transaction.
-    //
-    // Returned object WILL NOT ping transaction automatically.
-    // Otherwise returened object is similar to the object returned by StartTransaction
-    // and it can see all the changes made inside the transaction.
+    ///
+    /// @brief Attach to existing transaction.
+    ///
+    /// Returned object WILL NOT ping transaction automatically.
+    /// Otherwise returened object is similar to the object returned by StartTransaction
+    /// and it can see all the changes made inside the transaction.
     [[nodiscard]] virtual ITransactionPtr AttachTransaction(
         const TTransactionId& transactionId) = 0;
 
@@ -185,19 +219,22 @@ public:
         const TYPath& path,
         const TRemountTableOptions& options = TRemountTableOptions()) = 0;
 
-    // Switch dynamic table from `mounted' into `frozen' state.
-    // When table is in frozen state all its data is flushed to disk and writes are disabled.
-    //
-    // NOTE: this function launches the process of switching, but doesn't wait until switching is accomplished.
-    // Waiting has to be performed by user.
+    ///
+    /// @brief Switch dynamic table from `mounted' into `frozen' state.
+    ///
+    /// When table is in frozen state all its data is flushed to disk and writes are disabled.
+    ///
+    /// NOTE: this function launches the process of switching, but doesn't wait until switching is accomplished.
+    /// Waiting has to be performed by user.
     virtual void FreezeTable(
         const TYPath& path,
         const TFreezeTableOptions& options = TFreezeTableOptions()) = 0;
 
-    // Switch dynamic table from `frozen' into `mounted' state.
-    //
-    // NOTE: this function launches the process of switching, but doesn't wait until switching is accomplished.
-    // Waiting has to be performed by user.
+    ///
+    /// @brief Switch dynamic table from `frozen' into `mounted' state.
+    ///
+    /// NOTE: this function launches the process of switching, but doesn't wait until switching is accomplished.
+    /// Waiting has to be performed by user.
     virtual void UnfreezeTable(
         const TYPath& path,
         const TUnfreezeTableOptions& options = TUnfreezeTableOptions()) = 0;
@@ -243,99 +280,117 @@ public:
     // Is not supported since YT 19.2 version
     virtual void DisableTableReplica(const TReplicaId& replicaid) = 0;
 
-    // Change properties of table replica.
-    // Allows to enable/disable replica and/or change its mode.
+    ///
+    /// @brief Change properties of table replica.
+    ///
+    /// Allows to enable/disable replica and/or change its mode.
     virtual void AlterTableReplica(
         const TReplicaId& replicaId,
         const TAlterTableReplicaOptions& alterTableReplicaOptions) = 0;
 
     virtual ui64 GenerateTimestamp() = 0;
 
-    // Return YT username of current client.
+    /// Return YT username of current client.
     virtual TAuthorizationInfo WhoAmI() = 0;
 
-    // Get operation attributes.
+    /// Get operation attributes.
     virtual TOperationAttributes GetOperation(
         const TOperationId& operationId,
         const TGetOperationOptions& options = TGetOperationOptions()) = 0;
 
-    // List operations satisfying given filters.
+    /// List operations satisfying given filters.
     virtual TListOperationsResult ListOperations(
         const TListOperationsOptions& options = TListOperationsOptions()) = 0;
 
-    // Update operation runtime parameters.
+    /// Update operation runtime parameters.
     virtual void UpdateOperationParameters(
         const TOperationId& operationId,
         const TUpdateOperationParametersOptions& options) = 0;
 
-    // Get job attributes.
+    /// Get job attributes.
     virtual TJobAttributes GetJob(
         const TOperationId& operationId,
         const TJobId& jobId,
         const TGetJobOptions& options = TGetJobOptions()) = 0;
 
-    // List jobs satisfying given filters.
+    /// List jobs satisfying given filters.
     virtual TListJobsResult ListJobs(
         const TOperationId& operationId,
         const TListJobsOptions& options = TListJobsOptions()) = 0;
 
-    // Get the input of a running or failed job.
-    // (TErrorResponse exception is thrown if it is missing).
+    ///
+    /// @brief Get the input of a running or failed job.
+    ///
+    /// TErrorResponse exception is thrown if job is missing.
     virtual IFileReaderPtr GetJobInput(
         const TJobId& jobId,
         const TGetJobInputOptions& options = TGetJobInputOptions()) = 0;
 
-    // Get fail context of a failed job.
-    // (TErrorResponse exception is thrown if it is missing).
+    ///
+    /// @brief Get fail context of a failed job.
+    ///
+    /// TErrorResponse exception is thrown if it is missing.
     virtual IFileReaderPtr GetJobFailContext(
         const TOperationId& operationId,
         const TJobId& jobId,
         const TGetJobFailContextOptions& options = TGetJobFailContextOptions()) = 0;
 
-    // Get stderr of a running or failed job
-    // (TErrorResponse exception is thrown if it is missing).
+    ///
+    /// @brief Get stderr of a running or failed job
+    ///
+    /// TErrorResponse exception is thrown if it is missing.
     virtual IFileReaderPtr GetJobStderr(
         const TOperationId& operationId,
         const TJobId& jobId,
         const TGetJobStderrOptions& options = TGetJobStderrOptions()) = 0;
 
-    // Create rbtorrent for given table written in special format
-    // https://wiki.yandex-team.ru/yt/userdoc/blob_tables/#shag3.sozdajomrazdachu
+    ///
+    /// @brief Create rbtorrent for given table written in special format
+    ///
+    /// [More info.](https://wiki.yandex-team.ru/yt/userdoc/blob_tables/#shag3.sozdajomrazdachu)
     virtual TString SkyShareTable(const TYPath& tablePath) = 0;
 
-    // Create a set of rbtorrents, one torrent for each value of keyColumns columns
-    // Return: list of nodes, each node has two fields
-    // * key: list of key columns values
-    // * rbtorrent: rbtorrent string
+    ///
+    /// @brief Create a set of rbtorrents, one torrent for each value of keyColumns columns
+    ///
+    /// @return list of nodes, each node has two fields
+    /// * key: list of key columns values
+    /// * rbtorrent: rbtorrent string
     virtual TNode::TListType SkyShareTableByKey(
         const TYPath& tablePath,
         const TKeyColumns& keyColumns) = 0;
 
-    // Check if 'user' has 'permission' to access a Cypress node at 'path'.
-    // For tables access to columns specified in 'options.Columns_' can be checked
-    // (see https://wiki.yandex-team.ru/yt/userdoc/columnaracl).
-    //
-    // If access is denied (the returned result has '.Action == ESecurityAction::Deny')
-    // because of a 'deny' rule, the "denying" object name and id
-    // and "denied" subject name an id may be returned.
+    ///
+    /// @brief Check if 'user' has 'permission' to access a Cypress node at 'path'.
+    ///
+    /// For tables access to columns specified in 'options.Columns_' can be checked
+    /// (see https://wiki.yandex-team.ru/yt/userdoc/columnaracl).
+    ///
+    /// If access is denied (the returned result has '.Action == ESecurityAction::Deny')
+    /// because of a 'deny' rule, the "denying" object name and id
+    /// and "denied" subject name an id may be returned.
     virtual TCheckPermissionResponse CheckPermission(
         const TString& user,
         EPermission permission,
         const TYPath& path,
         const TCheckPermissionOptions& options = TCheckPermissionOptions()) = 0;
 
-    // Suspend operation.
-    // Jobs will be aborted.
+    ///
+    /// @brief Suspend operation.
+    ///
+    /// Jobs will be aborted.
     virtual void SuspendOperation(
         const TOperationId& operationId,
         const TSuspendOperationOptions& options = TSuspendOperationOptions()) = 0;
 
-    // Resume previously suspended operation.
+    /// Resume previously suspended operation.
     virtual void ResumeOperation(
         const TOperationId& operationId,
         const TResumeOperationOptions& options = TResumeOperationOptions()) = 0;
 };
 
+
+/// Create a client for particular mapreduce cluster.
 IClientPtr CreateClient(
     const TString& serverName,
     const TCreateClientOptions& options = TCreateClientOptions());
