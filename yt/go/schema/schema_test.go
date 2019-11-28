3ce940d2529be6e8896e8eb21b35b6b71caea39e
iudovin <iudovin@yandex-team.ru>
2019-11-28 16:33:09 +0300

    Add implementation for schema merging

--- yt/go/schema/schema_test.go
+++ yt/go/schema/schema_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"a.yandex-team.ru/library/go/ptr"
 	"a.yandex-team.ru/yt/go/yson"
 )
 
@@ -24,10 +25,41 @@ func TestSchemaMarshalYSON(t *testing.T) {
 func TestSchemaEquality(t *testing.T) {
 	s0 := MustInfer(&testBasicTypes{})
 
-	strict := true
 	s1 := s0
-	s1.Strict = &strict
+	s1.Strict = ptr.Bool(true)
 
 	require.True(t, s0.Equal(s1))
 	require.False(t, s0.Equal(s1.WithUniqueKeys()))
+}
+
+func TestMergeSameSchemas(t *testing.T) {
+	s0 := MustInfer(&testBasicTypes{})
+	s1 := MustInfer(&testBasicTypes{})
+	require.Equal(t, MergeSchemas(s0, s1), s0)
+}
+
+func TestMergeSchemas(t *testing.T) {
+	s0 := Schema{
+		Strict: ptr.Bool(true),
+		Columns: []Column{
+			{Type: TypeString, Name: "A", SortOrder: SortAscending},
+			{Type: TypeFloat64, Name: "B"},
+			{Type: TypeAny, Name: "C"},
+		},
+	}
+	s1 := Schema{
+		Strict: ptr.Bool(true),
+		Columns: []Column{
+			{Type: TypeFloat64, Name: "B", SortOrder: SortAscending},
+			{Type: TypeInt64, Name: "A"},
+			{Type: TypeBoolean, Name: "D"},
+		},
+	}
+	sm := MergeSchemas(s0, s1)
+	require.Equal(t, sm.Columns, []Column{
+		{Type: TypeAny, Name: "A"},
+		{Type: TypeFloat64, Name: "B"},
+		{Type: TypeAny, Name: "C"},
+		{Type: TypeBoolean, Name: "D"},
+	})
 }
