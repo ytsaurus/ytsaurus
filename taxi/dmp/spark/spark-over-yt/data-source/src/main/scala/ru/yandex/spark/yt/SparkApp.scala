6ed83cddbe12c9872308411e12b01217f6336b39
Alexandra Belousova <sashbel@yandex-team.ru>
2019-12-03 11:56:32 +0300

    Tmpfs usage, fix inferSchema for large dirs, improve SparkApp, default ytClient

--- taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/SparkApp.scala
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/SparkApp.scala
@@ -3,11 +3,12 @@ package ru.yandex.spark.yt
 import org.apache.log4j.Logger
 import org.apache.spark.SparkConf
 import org.apache.spark.sql.SparkSession
+import ru.yandex.yt.ytclient.proxy.YtClient
 
 trait SparkApp extends App {
   private val log = Logger.getLogger(getClass)
 
-  def run(args: Array[String], spark: SparkSession): Unit
+  def run(args: Array[String])(implicit spark: SparkSession, yt: YtClient): Unit
 
   def sparkConf: SparkConf = new SparkConf()
 
@@ -15,7 +16,7 @@ trait SparkApp extends App {
     try {
       val spark = createSparkSession(sparkConf)
       try {
-        run(args, spark)
+        run(args)(spark, YtClientProvider.ytClient)
       } finally {
         log.info("Stopping SparkSession")
         spark.stop()
