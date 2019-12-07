3f4febd0e14662a24d859fbb03840e6f5d2171fe
Andrei Tonkikh <antonkikh@yandex-team.ru>
2019-12-07 16:19:57 +0300

    Rename core/math from core library and rename to core/numeric

--- yt/core/numeric/util.h
+++ yt/core/numeric/util.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include <util/system/compiler.h>
+
 #include <type_traits>
 #include <cstring>
 
@@ -19,10 +21,9 @@ TTo BitCast(const TFrom &src) noexcept
     return dst;
 }
 
-
 // See: |std::midpoint| from header <numeric> from C++2a.
 // Remove this implementation and use the standard one when it becomes available.
-template<typename TInt>
+template <class TInt>
 TInt Midpoint(TInt a, TInt b) noexcept
 {
     static_assert(std::is_integral_v<TInt>);
@@ -31,7 +32,16 @@ TInt Midpoint(TInt a, TInt b) noexcept
 
     using TUInt = std::make_unsigned_t<TInt>;
 
-    return TInt(TUInt(a) + (TUInt(b) - TUInt(a)) / 2);
+    int k = 1;
+    TUInt mn = a;
+    TUInt mx = b;
+    if (a > b) {
+        k = -1;
+        mn = b;
+        mx = a;
+    }
+
+    return a + k * TInt(TUInt(mx - mn) >> 1);
 }
 
 } // namespace NYT
