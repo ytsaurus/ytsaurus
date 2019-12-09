0703651fde6eae97a991a6323d8844fb9295a0ff
Alexey Lukyanchikov <lukyan@ytsaurus.tech>
2019-12-10 02:13:05 +0300

    Cosmetics in Explain

--- yt/ytlib/query_client/explain.h
+++ yt/ytlib/query_client/explain.h
@@ -2,23 +2,18 @@
 
 #include "public.h"
 
+#include "query_preparer.h"
+
 #include <yt/core/yson/string.h>
 
-#include <yt/ytlib/api/native/client_impl.h>
-
-#include <yt/ytlib/query_client/query_preparer.h>
-
 namespace NYT::NQueryClient {
-
-using namespace NApi;
 
 ////////////////////////////////////////////////////////////////////////////////
 
 NYson::TYsonString BuildExplainYson(
     const TString& queryString,
-    const TExplainOptions& options,
     const std::unique_ptr<TPlanFragment>& fragment,
-    const NNative::TClientPtr& client);
+    TStringBuf udfRegistryPath);
 
 ////////////////////////////////////////////////////////////////////////////////
 
