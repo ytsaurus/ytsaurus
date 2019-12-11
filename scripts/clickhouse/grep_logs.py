98fe6a2882da7a2ea14e5ad9adc8d5387e0fe422
Maxim Akhmedov <max42@ytsaurus.tech>
2019-12-12 01:21:39 +0300

    Improvements around grep_logs.py

--- scripts/clickhouse/grep_logs.py
+++ scripts/clickhouse/grep_logs.py
@@ -5,17 +5,19 @@ import yt.wrapper as yt
 import argparse
 import logging
 import sys
+import datetime
+import dateutil.parser
 
 logger = logging.getLogger(__name__)
 
 LOG_LEVEL_LONG_NAMES = ["TRACE", "DEBUG", "INFO", "WARNING", "ERROR", "FATAL"]
 LOG_LEVEL_SHORT_NAMES = [level[:1] for level in LOG_LEVEL_LONG_NAMES]
 
-def setup_logging():
+def setup_logging(verbose):
     logger.setLevel(logging.DEBUG)
     formatter = logging.Formatter("%(asctime)s %(levelname)s\t%(message)s")
     stderr_handler = logging.StreamHandler()
-    stderr_handler.setLevel(logging.DEBUG)
+    stderr_handler.setLevel([logging.FATAL, logging.INFO, logging.DEBUG][min(verbose, 2)])
     stderr_handler.setFormatter(formatter)
     logger.addHandler(stderr_handler)
 
@@ -48,20 +50,90 @@ def parse_trace_id(trace_id):
     return trace_id.replace("-", "").lower()
 
 
-def select_rows(query, limit, batch_size):
-    for index in xrange(0, limit, batch_size):
-        subquery = "{} LIMIT {} OFFSET {}".format(query, batch_size, index)
-        logger.debug("Running batch from %d to %d", index, index + batch_size)
-        empty = True
-        for row in yt.select_rows(query):
-            empty = False
+def select_rows(query):
+    logger.debug("Making query: %s", query)
+    return yt.select_rows(query)
+
+
+def parse_timedelta(timedelta):
+    try:
+        quantity = int(timedelta[:-1])
+    except ValueError:
+        return None
+    if timedelta.endswith("s"):
+        return datetime.timedelta(seconds=quantity)
+    elif timedelta.endswith("m"):
+        return datetime.timedelta(minutes=quantity)
+    elif timedelta.endswith("h"):
+        return datetime.timedelta(hours=quantity)
+    elif timedelta.endswith("d"):
+        return datetime.timedelta(days=quantity)
+    else:
+        return None
+
+
+def parse_ts(ts, default=None):
+    if ts is None:
+        return default
+    add = datetime.timedelta()
+    if ts.endswith("Z"):
+        ts = ts[:-1]
+        add = datetime.timedelta(hours=3)
+    try:
+        return dateutil.parser.parse(ts) + add
+    except ValueError:
+        return None
+
+
+def format_ts(ts):
+    return ts.isoformat().replace("T", " ").replace(".", ",")
+
+LIMIT = 1000000
+
+def format_query(input_table_path, conditions):
+    return "* FROM [{}] WHERE {} ORDER BY (timestamp, line_index) LIMIT {}".format(input_table_path, " AND ".join(conditions), LIMIT)
+
+
+def select_rows_batched(input_table_path, conditions, from_ts, to_ts, window_size, tail):
+    if window_size is None:
+        logger.debug("Window batching is disabled, making single query")
+        for row in select_rows(format_query(input_table_path, conditions)):
             yield row
-        if empty:
-            return
+    else:
+        cur_ts = from_ts
+        while cur_ts <= to_ts:
+            lhs = format_ts(cur_ts)
+            rhs = format_ts(min(to_ts, cur_ts + window_size))
+            logger.debug("Running batch of size %s from %s to %s", window_size, lhs, rhs)
+            new_conditions = conditions + ["timestamp BETWEEN '{}' AND '{}'".format(lhs, rhs)]
+            subquery = format_query(input_table_path, new_conditions)
+            logger.debug("Subquery: %s", subquery)
+            row_count = 0
+            last_row_ts = None
+            incomplete = False
+            for row in yt.select_rows(subquery):
+                row_count += 1
+                last_row_ts = parse_ts(row["timestamp"])
+                yield row
+            logger.debug("Last batch consisted of %d rows, last row ts = %s", row_count, last_row_ts)
+            if row_count == LIMIT:
+                incomplete = True
+                new_window_size = window_size // 2
+                logger.warning("Last batch hit the limit, decreasing window-size from %s to %s", window_size, new_window_size)
+                window_size = new_window_size
+            elif tail:
+                incomplete = True
+            if incomplete:
+                if not last_row_ts:
+                    logger.debug("Retrying last batch")
+                    continue
+                else:
+                    logger.debug("Moving current ts to last row ts")
+                    cur_ts = last_row_ts
+            else:
+                cur_ts += window_size
 
 def main():
-    setup_logging()
-
     parser = argparse.ArgumentParser(description="Grep logs from log tailer dynamic table")
     parser.add_argument("operation_alias", help="Operation alias of clique to grep (either with * or without)")
     parser.add_argument("--message-like", help="Message pattern substring")
@@ -71,17 +143,41 @@ def main():
     parser.add_argument("--log-level", help="Minimum log level to grep, default is debug; case-insensitive, either "
                                             "first char or full level name", default="debug")
     parser.add_argument("--expression", help="Arbitrary boolean condition to append to query")
-    parser.add_argument("--limit", help="Row count limit", default=10**9)
-    parser.add_argument("--batch-size", help="Select query batch size; default is 10000", default=10000)
+    parser.add_argument("--limit", help="Leave that many lines", type=int, default=10**9)
     parser.add_argument("--omit-job-id", help="Do not print job-id", action="store_true")
     parser.add_argument("--job-id", help="Job id to filter; implies --omit-job-id")
+    parser.add_argument("--from-ts", help="'From' timestamp, empty means the creation time of table; "
+                                          "for example 2019-07-12 10:00 or 05:05:42")
+    parser.add_argument("--to-ts", help="'To' timestamp, empty means now() + 5min; see --from-ts for examples")
+    parser.add_argument("--window-size", help="Size of time window for batching; for example 10min, 5sec, 1hr, 1day")
+    parser.add_argument("--tail", help="Imitate tail -f; from-ts is assumed to be now() - 10 sec", action="store_true")
+    parser.add_argument('-v', '--verbose', action='count', default=0, help="Log stuff, more v's for more logging")
     args = parser.parse_args()
+
+    setup_logging(args.verbose)
 
     logger.debug("Arguments: %s", args)
 
     log_level = parse_log_level(args.log_level)
     if log_level is None:
         parser.error("Invalid log level: {}".format(log_level))
+        sys.exit(1)
+
+    window_size = None
+    if args.window_size is not None:
+        window_size = parse_timedelta(args.window_size)
+        if window_size is None:
+            parser.error("Invalid window size: {}".format(args.window_size))
+            sys.exit(1)
+
+    from_ts = parse_ts(args.from_ts, default=datetime.datetime.min)
+    if from_ts is None:
+        parser.error("Invalid from ts: {}".format(args.from_ts))
+        sys.exit(1)
+
+    to_ts = parse_ts(args.to_ts, default=datetime.datetime.now() + datetime.timedelta(minutes=5))
+    if to_ts is None:
+        parser.error("Invalid to ts: {}".format(args.to_ts))
         sys.exit(1)
 
     operation_alias = args.operation_alias
@@ -98,7 +194,12 @@ def main():
         logger.info("Trace id: %s", trace_id)
 
     input_table_path = get_input_table_path(artifact_path, trace_id_present=trace_id is not None, log_level=log_level)
-    logger.info("Input table: %s", input_table_path)
+    creation_time = parse_ts(yt.get(input_table_path + "/@creation_time"))
+    logger.info("Input table: %s, creation_time: %s", input_table_path, format_ts(creation_time))
+    from_ts = max(from_ts, creation_time)
+
+    if args.tail:
+        from_ts = datetime.datetime.now() - datetime.timedelta(seconds=10)
 
     omit_job_id = args.omit_job_id or args.job_id is not None
     logger.info("Omit job id: %s", omit_job_id)
@@ -120,16 +221,18 @@ def main():
     if args.job_id:
         conditions.append("job_id = '{}'".format(args.job_id))
 
-    query = "* FROM [{}] WHERE {} ORDER BY (timestamp, line_index) LIMIT 10000".format(input_table_path, " AND ".join(conditions))
-    logger.info("Query: %s", query)
-    print >>sys.stderr, "=" * 80
+    logger.info("Base query: %s", format_query(input_table_path, conditions))
+    logger.info("=" * 80)
 
-    rows = select_rows(query, args.limit, args.batch_size)
-    for row in rows:
+    rows = select_rows_batched(input_table_path, conditions, from_ts, to_ts, window_size, args.tail)
+    for index, row in enumerate(rows):
+        if index >= args.limit:
+            break
         job_id_prefix = "[{}]\t".format(row["job_id"]) if not omit_job_id else ""
         print "{}{}\t{}\t{}\t{}\t{}\t{}\t{}".format(job_id_prefix, row["timestamp"], row["log_level"],
                                                     row["category"], row["message"], row["thread_id"], row["fiber_id"],
                                                     row["trace_id"])
+
 
 if __name__ == "__main__":
     main()
