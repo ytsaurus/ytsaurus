19088898e3d73e18d5559a57eb7974c352de3b9d
Maxim Akhmedov <max42@ytsaurus.tech>
2019-12-11 11:29:49 +0300

    Support interrupts in clickhouse-trampoline.

--- yt/server/clickhouse_trampoline/clickhouse-trampoline.py
+++ yt/server/clickhouse_trampoline/clickhouse-trampoline.py
@@ -8,9 +8,47 @@ import os.path
 import shutil
 import glob
 import subprocess
+import signal
 import library.python.svn_version
 
 logger = logging.getLogger("clickhouse-trampoline")
+
+class SigintHandler(object):
+    def __init__(self, ytserver_clickhouse_process, interrupt_child):
+        self.original_sigint_handler = signal.getsignal(signal.SIGINT)
+        self.sigint_index = 0
+        self.ytserver_clickhouse_process = ytserver_clickhouse_process
+        self.interrupt_child = interrupt_child
+        logger.info("Installing custom SIGINT handler")
+        signal.signal(signal.SIGINT, self.handle)
+
+    def handle(self, signum, frame):
+        assert signum == signal.SIGINT
+        assert frame is not None
+        if self.sigint_index == 0:
+            self.sigint_index = 1
+            logger.info("Caught first SIGINT")
+            # NB: should not sent SIGINT to ytserver-clickhouse here as it should happen automatically in
+            # YT environment.
+            if self.interrupt_child:
+                logger.info("Sending SIGINT to ytserver-clickhouse child, pid = %d",
+                            self.ytserver_clickhouse_process.pid)
+                self.ytserver_clickhouse_process.send_signal(signal.SIGINT)
+                logger.info("Sent SIGINT to ytserver-clickhouse")
+            else:
+                logger.info("ytserver-clickhouse child should probably receive first SIGINT")
+        elif self.sigint_index == 1:
+            logger.info("Caught second SIGINT, restoring original SIGINT handler and interrupting ourselves")
+            signal.signal(signal.SIGINT, self.original_sigint_handler)
+            os.kill(os.getpid(), signal.SIGINT)
+            assert False
+
+
+def sigint_handler(signum, frame):
+    assert signum == signal.SIGINT
+    logger.info("Caught SIGINT")
+
+    raise IOError("Couldn't open device!")
 
 
 def extract_geodata():
@@ -47,7 +85,11 @@ def run_ytserver_clickhouse(ytserver_clickhouse_bin, monitoring_port):
     args += ["--tcp-port", os.environ["YT_PORT_2"]]
     args += ["--http-port", os.environ["YT_PORT_3"]]
     logger.info("Going to invoke following command: %s", args)
-    process = subprocess.Popen(args)
+    # NB: without preexec_fn=os.setpgrp any signal coming to the parent will always be immediately propagated to
+    # children.
+    # See https://stackoverflow.com/questions/3791398/how-to-stop-python-from-propagating-signals-to-subprocesses for
+    # more details.
+    process = subprocess.Popen(args, preexec_fn=os.setpgrp)
     logger.info("Process started, pid = %d", process.pid)
     return process
 
@@ -65,16 +107,28 @@ def print_version():
     print "smth~" + library.python.svn_version.commit_id()
 
 
-def main():
-    logging.basicConfig(format="%(asctime)s %(levelname)s\t%(message)s", level=logging.DEBUG)
+def setup_logging():
+    logger.setLevel(logging.DEBUG)
+    formatter = logging.Formatter("%(asctime)s %(levelname)s\t%(message)s")
+    stderr_handler = logging.StreamHandler()
+    stderr_handler.setLevel(logging.DEBUG)
+    stderr_handler.setFormatter(formatter)
+    logger.addHandler(stderr_handler)
 
+def main():
     parser = argparse.ArgumentParser(description="Process that setups environment for running ytserver-clickhouse")
     parser.add_argument("--version", action="store_true", help="Print commit this binary is built from")
     parser.add_argument("ytserver_clickhouse_bin", nargs="?", help="ytserver-clickhouse binary path")
     parser.add_argument("--prepare-geodata", action="store_true", help="Extract archive with geodata")
     parser.add_argument("--monitoring-port", help="Port for monitoring HTTP server")
     parser.add_argument("--core-dump-destination", help="Path where to move all core dumps that appear after execution")
+    parser.add_argument("--interrupt-child", action="store_true",
+                        help="Whether child should be interrupted on first SIGINT coming to trampoline; note that it "
+                             "happens automatically in YT job environment, so this option should be used only for "
+                             "manual trampoline invocations")
     args = parser.parse_args()
+
+    setup_logging()
 
     logger.info("Trampoline started, args = %s", args)
     if args.version:
@@ -89,6 +143,7 @@ def main():
         extract_geodata()
     patch_config(args.prepare_geodata)
     ytserver_clickhouse_process = run_ytserver_clickhouse(args.ytserver_clickhouse_bin, args.monitoring_port)
+    sigint_handler = SigintHandler(ytserver_clickhouse_process, args.interrupt_child)
     exit_code = ytserver_clickhouse_process.wait()
     logger.info("ytserver-clickhouse exit code is %d", exit_code)
     if args.core_dump_destination:
