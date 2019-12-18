e821e8d62df243e2f3a86bd7c411f8705389c03e
Renat Khayretdinov <renadeen@ytsaurus.tech>
2019-12-18 13:26:49 +0300

    YT-11832: Recode IDM pools table (#4067)

--- scripts/scheduler/migrate_pools.py
+++ scripts/scheduler/migrate_pools.py
@@ -6,14 +6,18 @@ import logging
 
 import yt.wrapper as yt
 from transform_pools_helper import transform
+from recode_idm_pools_helper import recode_idm
 
 SCHEDULER_POOL_OBJECT_ID = "3e9"
 SCHEDULER_POOL_TREE_OBJECT_ID = "3ea"
+IDM_POOLS_TABLE_PATH = "//sys/idm/pools"
+
 
 def get_schema_id(root_id, object_id):
     schema_id = root_id.split("-")
     schema_id[2] = schema_id[2][:-4] + "8" + object_id
     return "-".join(schema_id)
+
 
 def migrate(cluster, current_pool_trees_path, backup_path, tmp_path):
     client = yt.YtClient(cluster)
@@ -31,9 +35,24 @@ def migrate(cluster, current_pool_trees_path, backup_path, tmp_path):
     if client.exists(tmp_path):
         raise Exception("Path {} already exists".format(tmp_path))
 
+    should_migrate_idm = client.exists(IDM_POOLS_TABLE_PATH)
+    if should_migrate_idm:
+        logging.info("Disabling IDM")
+        client.set("//sys/idm/@disabled", True)
+
+        if yt.exists("//sys/idm/lock/@locks/0/transaction_id"):
+            idm_tx = client.get("//sys/idm/lock/@locks/0/transaction_id")
+            yt.abort_transaction(idm_tx)
+    else:
+        logging.info("Skipping IDM pool table migration")
+
     logging.info("Transforming from %s to %s", current_pool_trees_path, tmp_path)
     transform(cluster, current_pool_trees_path, cluster, tmp_path)
     logging.info("Transformation successful!")
+
+    if should_migrate_idm:
+        logging.info("Migrating IDM pools table.")
+        recode_idm(cluster, current_pool_trees_path, tmp_path, IDM_POOLS_TABLE_PATH)
 
     logging.info("Creating links to pool and pool tree schema")
     client.link("#" + get_schema_id(client.get("//@id"), SCHEDULER_POOL_OBJECT_ID), "//sys/schemas/scheduler_pool", force=True)
@@ -54,6 +73,10 @@ def migrate(cluster, current_pool_trees_path, backup_path, tmp_path):
 
     logging.info("Moving forward transformed config: %s to %s", tmp_path, current_pool_trees_path)
     client.move(tmp_path, current_pool_trees_path)
+
+    if should_migrate_idm:
+        logging.info("Enabling IDM")
+        client.set("//sys/idm/@disabled", False)
 
     logging.info("Migration successful!")
 
