a774f7ff91e51e3b8471f78cb86449f80746b914
Aleksandra Zhikhareva <aleksandra-zh@ytsaurus.tech>
2019-12-18 16:51:40 +0300

    Set cache expiration times to lower values

--- python-git/yt/environment/configs_provider.py
+++ python-git/yt/environment/configs_provider.py
@@ -503,10 +503,14 @@ class ConfigsProvider_19(ConfigsProvider):
                 "hard_backoff_time": 100
             },
             "cell_directory_synchronizer": {
-                "sync_period": 500
+                "sync_period": 500,
+                "success_expiration_time": 500,
+                "failure_expiration_time": 500
             },
             "cluster_directory_synchronizer": {
-                "sync_period": 500
+                "sync_period": 500,
+                "success_expiration_time": 500,
+                "failure_expiration_time": 500
             },
             "table_mount_cache": {
                 "expire_after_successful_update_time": 0,
