1dd50856a1203176cd1d07cb4430b3da853d9987
Maxim Babenko <babenko@ytsaurus.tech>
2019-12-23 02:02:01 +0300

    Extract TCellHydraJanitor

---
+++ yt/server/master/cell_server/cell_hydra_janitor.h
@@ -0,0 +1,28 @@
+#pragma once
+
+#include "public.h"
+
+#include <yt/server/master/cell_master/public.h>
+
+namespace NYT::NCellServer {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TCellHydraJanitor
+    : public TRefCounted
+{
+public:
+    explicit TCellHydraJanitor(NCellMaster::TBootstrap* bootstrap);
+
+    void Initialize();
+
+private:
+    class TImpl;
+    const TIntrusivePtr<TImpl> Impl_;
+};
+
+DEFINE_REFCOUNTED_TYPE(TCellHydraJanitor)
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NCellServer
