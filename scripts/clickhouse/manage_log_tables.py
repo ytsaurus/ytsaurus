246629d218b4cf9c91234c10469261a65264d26b
Maxim Akhmedov <max42@ytsaurus.tech>
2019-12-29 19:05:26 +0300

    Add manage_log_tables.py script.

---
+++ scripts/clickhouse/manage_log_tables.py
@@ -0,0 +1,91 @@
+#!/usr/bin/python
+
+import argparse
+import logging
+import sys
+import yt.wrapper as yt
+
+logger = logging.getLogger(__name__)
+
+args = None
+
+def remerge_tables(input_table_paths):
+    logger.info("Remerging %d tables", len(input_table_paths))
+
+    to_move = []
+
+    with yt.OperationsTrackerPool(args.pool_size, poll_period=args.pool_poll_period) as tracker:
+        for input_table_path in input_table_paths:
+            output_table_path = input_table_path + ".static"
+            schema = yt.get(input_table_path + "/@schema")
+            logger.debug("Creating table %s with schema %s", output_table_path, schema)
+            if not args.dry_run:
+                yt.create("table", output_table_path, attributes={"schema": schema}, force=True)
+
+            spec_builder = yt.spec_builders.MergeSpecBuilder() \
+                .input_table_paths(input_table_path) \
+                .output_table_path(output_table_path) \
+                .mode("ordered") \
+                .begin_job_io() \
+                    .table_writer({"block_size": 256 * 1024, "desired_chunk_size": 100 * 1024**2}) \
+                ._end_job_io()
+
+            if not args.dry_run:
+                tracker.add(spec_builder)
+
+            input_table_backup_path = input_table_path + args.backup_suffix
+
+            to_move += [(input_table_path, input_table_backup_path),
+                        (output_table_path, input_table_path)]
+
+    for src_path, dst_path in to_move:
+        logger.debug("Moving %s to %s", src_path, dst_path)
+        if not args.dry_run:
+            yt.move(src_path, dst_path)
+
+
+    for input_table_path in input_table_paths:
+        logger.debug("Altering table %s to dynamic", input_table_path)
+        if not args.dry_run:
+            yt.alter_table(input_table_path, dynamic=True)
+
+    logger.info("Remerged %d tables", len(input_table_paths))
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--pool-size", type=int, default=10, help="Maximum number of concurrently running merge operations")
+    parser.add_argument("--pool-poll-period", type=int, default=1000, help="Poll period in ms")
+    parser.add_argument("--dry-run", action="store_true", help="Only log what is going to happen")
+    parser.add_argument("-v", "--verbose", action="store_true", help="Print lots of debugging information")
+    parser.add_argument("--backup-suffix", default=".bak", help="Suffix for backing up input tables")
+    parser.add_argument("--remerge-tables", action="store_true", help="Remerge tables")
+    args = parser.parse_args()
+
+    if args.verbose:
+        logger.setLevel(logging.DEBUG)
+    if not logger.handlers:
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter("%(asctime)s %(levelname)s\t%(message)s")
+        handler.setFormatter(formatter)
+        logger.handlers.append(handler)
+
+    input_table_paths = list(map(str.strip, sys.stdin.readlines()))
+
+    for input_table_path in input_table_paths:
+        logger.debug("Unmounting table %s", input_table_path)
+        if not args.dry_run:
+            yt.unmount_table(input_table_path, sync=True)
+
+    if args.remerge_tables:
+        remerge_tables(input_table_paths)
+
+    for input_table_path in input_table_paths:
+        logger.debug("Setting up parameters on %s", input_table_path)
+        if not args.dry_run:
+            pass
+
+    for input_table_path in input_table_paths:
+        logger.debug("Mounting table %s", input_table_path)
+        if not args.dry_run:
+            yt.mount_table(input_table_path, sync=True)
