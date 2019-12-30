129853aac5710d60292c9ce224fba3a6b5790de0
Maxim Akhmedov <max42@ytsaurus.tech>
2019-12-30 15:32:41 +0300

    Improve manage_log_tables.py

--- scripts/clickhouse/manage_log_tables.py
+++ scripts/clickhouse/manage_log_tables.py
@@ -9,6 +9,10 @@ logger = logging.getLogger(__name__)
 
 args = None
 
+def get_table_kind(input_table_path):
+    return "ordered_by_trace_id" if "ordered_by_trace_id" in input_table_path else "ordered_normally"
+
+
 def remerge_tables(input_table_paths):
     logger.info("Remerging %d tables", len(input_table_paths))
 
@@ -18,9 +22,11 @@ def remerge_tables(input_table_paths):
         for input_table_path in input_table_paths:
             output_table_path = input_table_path + ".static"
             schema = yt.get(input_table_path + "/@schema")
-            logger.debug("Creating table %s with schema %s", output_table_path, schema)
+            table_kind = get_table_kind(output_table_path)
+            logger.debug("Creating table %s with schema %s and setting log tailer attributes on it as for order kind %s", output_table_path, schema, table_kind)
             if not args.dry_run:
                 yt.create("table", output_table_path, attributes={"schema": schema}, force=True)
+                yt.clickhouse.set_log_tailer_table_attributes(table_kind, output_table_path, 7 * 24 * 60 * 60 * 1000)
 
             spec_builder = yt.spec_builders.MergeSpecBuilder() \
                 .input_table_paths(input_table_path) \
@@ -48,6 +54,10 @@ def remerge_tables(input_table_paths):
         logger.debug("Altering table %s to dynamic", input_table_path)
         if not args.dry_run:
             yt.alter_table(input_table_path, dynamic=True)
+
+        logger.debug("Resharding table %s", input_table_path)
+        if not args.dry_run:
+            yt.clickhouse.set_log_tailer_table_dynamic_attributes(table_kind, input_table_path)
 
     logger.info("Remerged %d tables", len(input_table_paths))
 
@@ -79,11 +89,13 @@ if __name__ == "__main__":
 
     if args.remerge_tables:
         remerge_tables(input_table_paths)
-
-    for input_table_path in input_table_paths:
-        logger.debug("Setting up parameters on %s", input_table_path)
-        if not args.dry_run:
-            pass
+    else:
+        for input_table_path in input_table_paths:
+            logger.debug("Setting up parameters on %s", input_table_path)
+            if not args.dry_run:
+                table_kind = get_table_kind(input_table_path)
+                yt.clickhouse.set_log_tailer_table_attributes(table_kind, input_table_path, 7 * 24 * 60 * 60 * 1000)
+                yt.clickhouse.set_log_tailer_table_dynamic_attributes(table_kind, input_table_paths)
 
     for input_table_path in input_table_paths:
         logger.debug("Mounting table %s", input_table_path)
