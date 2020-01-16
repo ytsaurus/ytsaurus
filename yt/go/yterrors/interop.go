d1010bfc4d16095d7289aba69e06eadae1e8a121
prime <prime@yandex-team.ru>
2020-01-16 16:41:59 +0300

    Improve interop between YT errors and go errors. - `yterrors.FromError` корректно превращает цепочку go1.13 ошибок в цепочку YT ошибок. - Изменено форматирование YT ошибок. - При печати через `Error()` или `fmt.Sprintf("%+v", err)` стиль сообщения приводится к го-шному. - При сериализации в yson будет использоваться YT стиль. Конвертация происходит в момент создания ошибки. - Убран префикс `yt` из ошибок.

---
+++ yt/go/yterrors/interop.go
@@ -0,0 +1,132 @@
+package yterrors
+
+import (
+	"errors"
+	"strings"
+	"unicode"
+)
+
+// FromError converts any error to YT error, if it not already YT error.
+//
+// Nested errors are converted to nested YT errors.
+//
+// err is stored internally and is accessible using errors.As and errors.Is. Original error is discarded during serialization.
+//
+// If err is already a YT error, returns err unmodified.
+func FromError(err error) error {
+	switch v := err.(type) {
+	case *Error:
+		return v
+
+	case nil:
+		return nil
+
+	default:
+		return convertError(v)
+	}
+}
+
+func convertError(err error) *Error {
+	yt := new(Error)
+	yt.Code = 1
+	yt.Message = err.Error()
+	yt.origError = err
+
+	if inner := errors.Unwrap(err); inner != nil {
+		yt.Message = strings.TrimSuffix(yt.Message, inner.Error())
+		yt.Message = strings.TrimSuffix(yt.Message, ": ")
+
+		yt.InnerErrors = []*Error{FromError(inner).(*Error)}
+	}
+
+	yt.Message = capitalize(yt.Message)
+	return yt
+}
+
+func (yt *Error) Is(target error) bool {
+	if yt.origError == nil {
+		return false
+	}
+
+	return errors.Is(yt.origError, target)
+}
+
+func (yt *Error) As(target interface{}) bool {
+	if yt.origError == nil {
+		return false
+	}
+
+	return errors.As(yt.origError, target)
+}
+
+// Unwrap always returns last inner error.
+func (yt *Error) Unwrap() error {
+	if len(yt.InnerErrors) == 0 {
+		return nil
+	}
+
+	return yt.InnerErrors[len(yt.InnerErrors)-1]
+}
+
+// capitalize converts go style error message to YT style.
+func capitalize(msg string) string {
+	if len(msg) == 0 {
+		return msg
+	}
+
+	runes := []rune(msg)
+	for i, r := range runes {
+		if r == ' ' || i+1 == len(runes) {
+			runes[0] = unicode.ToUpper(runes[0])
+			break
+		}
+
+		if r == ':' {
+			break
+		}
+	}
+
+	for i, r := range runes {
+		if r != ':' {
+			continue
+		}
+
+		if i+2 >= len(runes) {
+			continue
+		}
+
+		if runes[i+1] == ' ' {
+			runes[i+2] = unicode.ToUpper(runes[i+2])
+		}
+	}
+	return string(runes)
+}
+
+// uncapitilize converts YT style error message to go style.
+func uncapitalize(msg string) string {
+	if len(msg) == 0 {
+		return msg
+	}
+
+	runes := []rune(msg)
+	if len(runes) == 1 || !unicode.IsUpper(runes[1]) {
+		runes[0] = unicode.ToLower(runes[0])
+	}
+
+	for i, r := range runes {
+		if r != ';' {
+			continue
+		}
+
+		if i+2 >= len(runes) {
+			continue
+		}
+
+		if runes[i+1] == ' ' {
+			if i+3 == len(runes) || !unicode.IsUpper(runes[i+3]) {
+				runes[i+2] = unicode.ToLower(runes[i+2])
+			}
+		}
+	}
+	return string(runes)
+}
---
+++ yt/go/yterrors/interop_test.go
@@ -0,0 +1,147 @@
+package yterrors
+
+import (
+	"encoding/json"
+	"errors"
+	"fmt"
+	"os"
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+	"github.com/stretchr/testify/require"
+)
+
+func TestNilError(t *testing.T) {
+	require.Nil(t, FromError(nil))
+}
+
+func TestIsWrapping(t *testing.T) {
+	unrelatedErr := errors.New("unrelated")
+	innerErr := errors.New("inner")
+	err := Err("operation failed", innerErr)
+
+	require.True(t, errors.Is(err, innerErr))
+	require.False(t, errors.Is(err, unrelatedErr))
+}
+
+func TestAsWrapping(t *testing.T) {
+	innerErr := &os.PathError{Path: "//home", Err: errors.New("open failed")}
+	err := Err("operation failed", innerErr)
+
+	var pathErr *os.PathError
+	require.True(t, errors.As(err, &pathErr))
+	require.Equal(t, innerErr, pathErr)
+
+	var typeErr *json.UnsupportedTypeError
+	require.False(t, errors.As(err, &typeErr))
+}
+
+func TestErrorChainConversion(t *testing.T) {
+	innerErr := errors.New("inner")
+	middleErr := fmt.Errorf("middle: %w", innerErr)
+	outerErr := fmt.Errorf("outer: %w", middleErr)
+
+	yt := FromError(outerErr).(*Error)
+	assert.Equal(t, yt.Error(), outerErr.Error())
+
+	expected := &Error{
+		origError: outerErr,
+
+		Code:    1,
+		Message: "Outer",
+		InnerErrors: []*Error{
+			{
+				origError: middleErr,
+
+				Code:    1,
+				Message: "Middle",
+				InnerErrors: []*Error{
+					{
+						origError: innerErr,
+
+						Code:    1,
+						Message: "Inner",
+					},
+				},
+			},
+		},
+	}
+
+	// spew.Config.ContinueOnMethod = true
+	// spew.Fdump(os.Stderr, yt)
+	// spew.Fdump(os.Stderr, expected)
+
+	require.Equal(t, yt, expected)
+}
+
+func TestYTErrorInsideGoError(t *testing.T) {
+	ytErr := Err("Request limit exceeded").(*Error)
+	goErr := fmt.Errorf("write failed: %w", ytErr)
+
+	converted := FromError(goErr).(*Error)
+	expected := &Error{
+		origError: goErr,
+		Code:      1,
+		Message:   "Write failed",
+
+		InnerErrors: []*Error{ytErr},
+	}
+
+	assert.Equal(t, expected, converted)
+	assert.Equal(t, "write failed: request limit exceeded", goErr.Error())
+}
+
+func TestCaseConversions(t *testing.T) {
+	for _, testCase := range []struct {
+		in, lower, upper string
+	}{
+		{
+			in:    "write error",
+			upper: "Write error",
+		},
+		{
+			in:    "error",
+			upper: "Error",
+		},
+		{
+			in:    "Write error",
+			lower: "write error",
+		},
+		{
+			in:    "write error",
+			upper: "Write error",
+		},
+		{
+			in:    "Write error; Permission denied",
+			lower: "write error; permission denied",
+		},
+		{
+			in:    "Write error; YT permission denied",
+			lower: "write error; YT permission denied",
+		},
+		{
+			in:    "write error: permission denied",
+			upper: "Write error: Permission denied",
+		},
+		{
+			in:    "idm: request failed",
+			upper: "idm: Request failed",
+		},
+		{
+			in:    "yson: type error",
+			upper: "yson: Type error",
+		},
+		{
+			in:    "EOF",
+			lower: "EOF",
+		},
+	} {
+		if testCase.lower != "" {
+			assert.Equal(t, testCase.lower, uncapitalize(testCase.in))
+		}
+
+		if testCase.upper != "" {
+			assert.Equal(t, testCase.upper, capitalize(testCase.in))
+		}
+	}
+}
