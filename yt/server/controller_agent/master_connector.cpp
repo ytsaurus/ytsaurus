f65e45643df1c4f5b2cafa5d35c6a6442b0476ed
Renat Khayretdinov <renadeen@ytsaurus.tech>
2020-01-17 11:11:29 +0300

    YT-12007: Profile operation progress archive failures (#4126)

--- yt/server/controller_agent/master_connector.cpp
+++ yt/server/controller_agent/master_connector.cpp
@@ -82,9 +82,10 @@ public:
     TImpl(
         TControllerAgentConfigPtr config,
         TBootstrap* bootstrap)
-        : Config_(config)
+        : Config_(std::move(config))
         , InitialConfig_(Config_)
         , Bootstrap_(bootstrap)
+        , UpdateOperationProgressFailuresCounter_("/operation_archive/update_progress_failures")
     { }
 
     void Initialize()
@@ -248,6 +249,8 @@ private:
     TPeriodicExecutorPtr AlertsExecutor_;
 
     TEnumIndexedVector<EControllerAgentAlertType, TError> Alerts_;
+
+    NProfiling::TMonotonicCounter UpdateOperationProgressFailuresCounter_;
 
     struct TUnstageRequest
     {
@@ -790,11 +793,13 @@ private:
             MakeSharedRange(SmallVector<TUnversionedRow, 1>{1, row}, std::move(rowBuffer)));
 
         auto error = WaitFor(transaction->Commit()
+            .ToUncancelable()
             .WithTimeout(Config_->OperationProgressArchivationTimeout));
 
         if (!error.IsOK()) {
             YT_LOG_WARNING("Operation progress update in Archive failed (TransactionId: %v)",
                 transaction->GetId());
+            ControllerAgentProfiler.Increment(UpdateOperationProgressFailuresCounter_);
         } else {
             YT_LOG_DEBUG("Operation progress updated successfully (TransactionId: %v, DataWeight: %v)",
                 transaction->GetId(),
