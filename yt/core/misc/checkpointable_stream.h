8196d82531aebc46e19543e6f23168ebf2171565
Maxim Babenko <babenko@ytsaurus.tech>
2020-01-20 09:14:16 +0300

    Drop unused function, refactoring

--- yt/core/misc/checkpointable_stream.h
+++ yt/core/misc/checkpointable_stream.h
@@ -30,12 +30,6 @@ struct ICheckpointableOutputStream
 std::unique_ptr<ICheckpointableInputStream> CreateCheckpointableInputStream(
     IInputStream* underlyingStream);
 
-//! Wraps a given input stream constructing another one whose binary content
-//! can be parsed by a checkpointable stream parser as a single block.
-//! Used for migrating pre-0.17 snapshots that were not checkpointable.
-std::unique_ptr<IInputStream> EscapsulateAsCheckpointableInputStream(
-    IInputStream* underlyingStream);
-
 //! Wraps an output stream making it checkpointable.
 std::unique_ptr<ICheckpointableOutputStream> CreateCheckpointableOutputStream(
     IOutputStream* underlyingStream);
@@ -43,7 +37,7 @@ std::unique_ptr<ICheckpointableOutputStream> CreateCheckpointableOutputStream(
 //! Wraps a checkpointable output stream adding some buffering.
 std::unique_ptr<ICheckpointableOutputStream> CreateBufferedCheckpointableOutputStream(
     ICheckpointableOutputStream* underlyingStream,
-    size_t bufferSize = 8192);
+    size_t bufferSize = 8_KB);
 
 ////////////////////////////////////////////////////////////////////////////////
 
