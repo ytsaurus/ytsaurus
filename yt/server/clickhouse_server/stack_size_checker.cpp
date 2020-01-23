a82e8cd6a078cefb6e6fbb133b185f39ec9ac170
Maxim Akhmedov <max42@ytsaurus.tech>
2020-01-23 23:59:46 +0300

    Rewrite check stack size in CHYT.

---
+++ yt/server/clickhouse_server/stack_size_checker.cpp
@@ -0,0 +1,14 @@
+// Do not remove this cpp file from ya.make!
+
+int IgnoreMe()
+{
+    return 42;
+}
+
+void checkStackSize()
+{
+    // TODO(max42): CHYT-268.
+    // Right now we do not check anything. Maybe in Q5.
+    // Do not remove this method, as it will enable back the original CH check, which
+    // does not work properly with our fibers.
+}
