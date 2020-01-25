d18e002d02056d52ec33b7a8dd7cd6ec84532e89
Maxim Akhmedov <max42@ytsaurus.tech>
2020-01-26 01:18:09 +0300

    CHYT-291: make NULLs comparable to everything.

--- yt/server/clickhouse_server/private.cpp
+++ yt/server/clickhouse_server/private.cpp
@@ -16,3 +16,16 @@ const NProfiling::TProfiler ServerProfiler("/server");
 // We do not want ClickHouse allocator to use raw mmaps as ytalloc already
 // does that by himself.
 __attribute__((__used__)) extern const size_t MMAP_THRESHOLD = static_cast<size_t>(-1);
+
+namespace DB {
+
+////////////////////////////////////////////////////////////////////////////////
+
+// This is an override of weak symbol from Common/FieldVisitors.h.
+// In order to use DB::KeyCondition for input pruning, we should configure
+// it such that NULLs are going first as it is in YT.
+__attribute__((__used__)) extern const bool NULLS_LAST = false;
+
+////////////////////////////////////////////////////////////////////////////////
+
+}
