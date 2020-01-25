7487e6423a241a8cf90327fc90f6d0b9ff811457
Maxim Akhmedov <max42@ytsaurus.tech>
2020-01-25 12:47:59 +0300

    CHYT-292: Disable mmaps in CH.

--- yt/server/clickhouse_server/private.cpp
+++ yt/server/clickhouse_server/private.cpp
@@ -11,3 +11,8 @@ const NProfiling::TProfiler ServerProfiler("/server");
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace NYT::NClickHouseServer
+
+// This is an override of weak symbol from Common/Allocator.h.
+// We do not want ClickHouse allocator to use raw mmaps as ytalloc already
+// does that by himself.
+__attribute__((__used__)) extern const size_t MMAP_THRESHOLD = static_cast<size_t>(-1);
