a4791a8a911867e4f73d2f1cb3b68788524acf89
Grigory Reznikov <gritukan@ytsaurus.tech>
2020-01-29 00:07:28 +0300

    Add environment variable for sandbox cleanup disabling (#4093)

--- yt/server/node/exec_agent/slot_location.cpp
+++ yt/server/node/exec_agent/slot_location.cpp
@@ -27,6 +27,7 @@
 
 #include <yt/core/ytree/convert.h>
 
+#include <util/system/env.h>
 #include <util/system/fs.h>
 
 #include <util/folder/path.h>
@@ -37,6 +38,10 @@ using namespace NConcurrency;
 using namespace NTools;
 using namespace NYson;
 using namespace NYTree;
+
+////////////////////////////////////////////////////////////////////////////////
+
+static constexpr auto DisableSandboxCleanupEnv = "YT_DISABLE_SANDBOX_CLEANUP";
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -462,6 +467,12 @@ TFuture<void> TSlotLocation::CleanSandboxes(int slotIndex)
     return BIND([=, this_ = MakeStrong(this)] () {
         ValidateEnabled();
 
+        if (!ShouldCleanSandboxes()) {
+            YT_LOG_WARNING("Sandbox cleanup is disabled by environment variable %v; should be used for testing purposes only",
+                DisableSandboxCleanupEnv);
+            return;
+        }
+
         {
             TWriterGuard guard(SlotsLock_);
 
@@ -671,6 +682,11 @@ NNodeTrackerClient::NProto::TDiskResourcesInfo TSlotLocation::GetDiskInfo() cons
 {
     auto guard = TReaderGuard(DiskInfoLock_);
     return DiskInfo_;
+}
+
+bool TSlotLocation::ShouldCleanSandboxes() const
+{
+    return GetEnv(DisableSandboxCleanupEnv) != "1";
 }
 
 ////////////////////////////////////////////////////////////////////////////////
