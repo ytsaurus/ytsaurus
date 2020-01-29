b08f8e2e77ff569a0e61218184dcf6f74404ddb1
Alexandra Belousova <sashbel@yandex-team.ru>
2020-01-29 13:57:50 +0300

    Set yt token in configuration from jupyter

--- taxi/dmp/spark/spark-over-yt/data-source/src/main/python/pyspark_yt/__init__.py
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/python/pyspark_yt/__init__.py
@@ -16,7 +16,7 @@ def spark_session(conf=SparkConf()):
         spark._jvm.ru.yandex.spark.yt.fs.YtClientProvider.close()
 
 
-def create_spark_session(spark_id, yt_cluster=None, config_path=None, spark_conf=None):
+def create_spark_session(spark_id, yt_cluster=None, discovery_dir=None, config_path=None, spark_conf=None):
     if config_path is not None:
         with open(config_path) as f:
             config = yaml.load(f)
@@ -27,7 +27,7 @@ def create_spark_session(spark_id, yt_cluster=None, config_path=None, spark_conf
     yt_user = config.get("yt_user") or os.getenv("YT_USER") or os.getenv("USER")
     yt_token = config.get("yt_token") or os.getenv("YT_TOKEN") or default_token()
     yt_client = create_yt_client(yt_proxy, yt_token)
-    discovery_dir = config.get("discovery_dir") or default_discovery_dir(yt_user)
+    discovery_dir = discovery_dir or config.get("discovery_dir") or default_discovery_dir(yt_user)
     log_dir = config.get("log_dir") or default_base_log_dir(discovery_dir)
     master = get_spark_master(spark_id, discovery_dir, rest=False, yt_client=yt_client)
 
@@ -35,5 +35,6 @@ def create_spark_session(spark_id, yt_cluster=None, config_path=None, spark_conf
     set_conf(conf, base_spark_conf(yt_proxy, yt_user, log_dir))
     set_conf(conf, default_dynamic_allocation_conf())
     set_conf(conf, spark_conf)
+    conf.set("spark.hadoop.yt.token", yt_token)
 
     return SparkSession.builder.config(conf=conf).master(master).getOrCreate()
