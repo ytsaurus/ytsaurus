80a38d2862c045a08f6488ecf9550a05dc14f1eb
vett0 <vett0@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2020-01-29 09:16:01 +0000

    Allow creating additional objects after creating part of them. Continue creating/updating objects during next runs of script. Set BatchYpClient flush retries to 5. Split functions. Clean up code
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@6266082 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- scripts/library/batch_client.py
+++ scripts/library/batch_client.py
@@ -6,9 +6,15 @@ from yt.packages.six.moves import xrange
 
 import contextlib
 import logging
+import time
 
 
 class BatchYpClient(object):
+    _FORWARDED_METHODS = (
+        "generate_timestamp",
+        "select_objects",
+    )
+
     def __init__(self, yp_client, batch_size, retries_count=1):
         self._yp_client = yp_client
         self._batch_size = batch_size
@@ -18,23 +24,31 @@ class BatchYpClient(object):
         self._batch_remove = []
         self._batch_update = []
 
+    def __getattr__(self, name):
+        if name in self._FORWARDED_METHODS:
+            return getattr(self._yp_client, name)
+        raise AttributeError(name)
+
     def _commit_create_requests(self):
         if self._batch_create:
-            logging.debug("Commiting YP client create request batch")
+            logging.debug("Committing batch of YP client create requests")
             self._yp_client.create_objects(self._batch_create)
             self._batch_create = []
+            logging.debug("Committed successfully batch of YP client create requests")
 
     def _commit_remove_requests(self):
         if self._batch_remove:
-            logging.debug("Commiting YP client remove request batch")
+            logging.debug("Committing batch of YP client remove requests")
             self._yp_client.remove_objects(self._batch_remove)
             self._batch_remove = []
+            logging.debug("Committed successfully batch of YP client remove requests")
 
     def _commit_update_requests(self):
         if self._batch_update:
-            logging.debug("Commiting YP client update request batch")
+            logging.debug("Committing batch of YP client update requests")
             self._yp_client.update_objects(self._batch_update)
             self._batch_update = []
+            logging.debug("Committed successfully batch of YP client update requests")
 
     def create_object(self, object_type, attributes):
         self._try_flush(self._commit_remove_requests)
@@ -72,7 +86,7 @@ class BatchYpClient(object):
             except YtResponseError:
                 if retry_number < self._retries_count - 1:
                     logging.exception("Flush failed")
-                    retry_number += 1
+                    time.sleep(1)
                 else:
                     raise
             else:
--- scripts/stress_test_pod_disruption_budget_controller/__main__.py
+++ scripts/stress_test_pod_disruption_budget_controller/__main__.py
@@ -10,21 +10,13 @@ from yt.common import get_value
 
 from copy import deepcopy
 import argparse
-import enum
 import logging
 import os
 
-DEFAULT_ID_PREFIX = "stress-test-pdbcontroller-"
+DEFAULT_ID_PREFIX = "stress-test-pdb-controller-"
 
 DEFAULT_POD_DISRUPTION_BUDGET_ATTRS = dict(spec=dict(max_pods_unavailable=2, max_pod_disruptions_between_syncs=1))
 DEFAULT_POD_SET_ATTRS = dict(spec=dict(node_segment_id="default"))
-DEFAULT_POD_ATTRS = dict(spec=dict(enable_scheduling=True))
-
-
-class ObjectTypes(enum.Enum):
-    POD = "pod"
-    POD_SET = "pod_set"
-    POD_DISRUPTION_BUDGET = "pod_disruption_budget"
 
 
 def configure_logging(verbose):
@@ -34,101 +26,102 @@ def configure_logging(verbose):
     )
 
 
-def select_objects_batch(yp_client, object_type, filter_query, selectors):
-    return [response[0] for response in batch_select(
-        yp_client, object_type, filter_query, selectors)]
+def _get_filter(id_prefix):
+    def _upper_bound_prefix(prefix):
+        return prefix[:-1] + chr(ord(prefix[-1]) + 1)
+
+    return "[/meta/id]>'{}' AND [/meta/id]<'{}'".format(id_prefix, _upper_bound_prefix(id_prefix))
 
 
-def create_objects_batch(batch_yp_client, object_type, object_ids, default_object_attrs, dry_run, object_type_name=None):
-    if object_type_name is None:
-        object_type_name = object_type
+def _select_object_ids_batch(yp_client, object_type, id_prefix):
+    filter_query = _get_filter(id_prefix)
+    return [response[0] for response in batch_select(yp_client, object_type, filter_query, ["/meta/id"])]
 
-    for object_id in object_ids:
-        logging.debug("Creating {} (id: '{}')".format(object_type_name, object_id))
+
+def _create_objects(yp_client, object_type, default_object_attrs, object_count, id_prefix, dry_run):
+    object_id_prefix = "{}{}-".format(id_prefix, object_type)
+
+    created_object_ids = set(_select_object_ids_batch(yp_client, object_type, id_prefix))
+    all_object_ids = [object_id_prefix + str(index + 1) for index in xrange(object_count)]
+    object_ids_to_create = [pod_set_id for pod_set_id in all_object_ids if pod_set_id not in created_object_ids]
+
+    logging.info("Creating {} {}s with id prefix '{}'".format(len(object_ids_to_create), object_type, id_prefix))
+    for object_id in object_ids_to_create:
+        logging.debug("Creating {} (id: '{}')".format(object_type, object_id))
         if not dry_run:
             object_attrs = deepcopy(default_object_attrs)
             if "meta" not in object_attrs:
                 object_attrs["meta"] = {}
             object_attrs["meta"]["id"] = object_id
-            batch_yp_client.create_object(object_type, object_attrs)
+            yp_client.create_object(object_type, object_attrs)
+
+    return all_object_ids
 
 
-def remove_objects_batch(batch_yp_client, object_type, object_ids, dry_run, object_type_name=None):
-    if object_type_name is None:
-        object_type_name = object_type
+def _remove_objects(yp_client, object_type, id_prefix, dry_run):
+    object_ids = _select_object_ids_batch(yp_client, object_type, id_prefix)
 
+    logging.info("Removing {} {}s".format(len(object_ids), object_type))
     for object_id in object_ids:
-        logging.debug("Removing {} (id: '{}')".format(object_type_name, object_id))
+        logging.debug("Removing {} (id: '{}')".format(object_type, object_id))
         if not dry_run:
-            batch_yp_client.remove_object(object_type, object_id)
+            yp_client.remove_object(object_type, object_id)
 
 
-def create_objects(yp_client, dry_run, podset_count, pod_count_per_podset, id_prefix):
+def _get_pod_set_ids_for_update_indices(yp_client, podset_count, id_prefix):
+    filter_by_prefix_query = _get_filter(id_prefix)
+    created_pod_sets = batch_select(yp_client, "pod_set", filter_by_prefix_query, ["/meta/id", "/spec/pod_disruption_budget_id"])
+
+    pod_set_ids_for_update_indices_set = set(range(podset_count))
+    for index in xrange(len(created_pod_sets)):
+        if created_pod_sets[index][1]:
+            created_object_id = int(created_pod_sets[index][0].split("-")[-1]) - 1
+            if created_object_id in pod_set_ids_for_update_indices_set:
+                pod_set_ids_for_update_indices_set.remove(created_object_id)
+
+    return list(sorted(pod_set_ids_for_update_indices_set))
+
+
+def _update_pod_sets(batch_yp_client, dry_run, podset_count, id_prefix):
+    pod_set_ids_for_update_indices = _get_pod_set_ids_for_update_indices(batch_yp_client, podset_count, id_prefix)
+
+    pod_set_id_prefix = "{}{}-".format(id_prefix, "pod_set")
+    pod_disruption_budget_id_prefix = "{}{}-".format(id_prefix, "pod_disruption_budget")
+
+    pod_set_ids = [pod_set_id_prefix + str(index + 1) for index in pod_set_ids_for_update_indices]
+    pod_disruption_budget_id_prefix = [pod_disruption_budget_id_prefix + str(index + 1) for index in pod_set_ids_for_update_indices]
+
+    logging.info("Attaching {} pod sets to pod disruption budgets".format(len(pod_set_ids_for_update_indices)))
+    for pod_set_id, pod_disruption_budget_id in zip(pod_set_ids, pod_disruption_budget_id_prefix):
+        logging.debug("Attaching pod set (id: '{}') to pod disruption budget (id: '{}')".format(pod_set_id, pod_disruption_budget_id))
+        if not dry_run:
+            batch_yp_client.update_object("pod_set", pod_set_id, set_updates=[dict(
+                path="/spec/pod_disruption_budget_id",
+                value=pod_disruption_budget_id,
+            )])
+
+
+def create_objects(yp_client, dry_run, podset_count, id_prefix):
     assert podset_count > 0, "--podset-count must be greater than zero"
-    assert pod_count_per_podset >= 0, "--pod-count-per-podset must be not less than zero"
 
-    POD_DISRUPTION_BUDGET_ID_PREFIX = id_prefix + "pdbudget-"
-    POD_SET_ID_PREFIX = id_prefix + "podset-"
-    POD_ID_PREFIX = id_prefix + "pod-"
+    logging.info("Running in 'create' mode. Pod set and pod disruption budget count: {}, id prefix: '{}'".format(podset_count, id_prefix))
 
-    logging.info("Running in '--create' mode. Pod set count: {}, pod count per pod set: {} (total {}) with id prefix '{}'".format(
-        podset_count, pod_count_per_podset, podset_count * pod_count_per_podset, id_prefix))
+    with create_batch_yp_client(yp_client, batch_size=100, retries_count=5) as batch_yp_client:
+        _create_objects(batch_yp_client, "pod_set", DEFAULT_POD_SET_ATTRS, podset_count, id_prefix, dry_run)
 
-    with create_batch_yp_client(yp_client, batch_size=100) as batch_yp_client:
-        pod_set_ids = [POD_SET_ID_PREFIX + str(index + 1) for index in xrange(podset_count)]
-        logging.info("Creating {} pod sets with ids from '{}' to '{}'".format(podset_count, pod_set_ids[0], pod_set_ids[-1]))
-        create_objects_batch(batch_yp_client, ObjectTypes.POD_SET.value, pod_set_ids, DEFAULT_POD_SET_ATTRS, dry_run, "pod set")
-
-        pod_disruption_budget_ids = [POD_DISRUPTION_BUDGET_ID_PREFIX + str(index + 1) for index in xrange(podset_count)]
-        logging.info("Creating {} pod disruption budget objects with ids from '{}' to '{}'".format(
-            podset_count, pod_disruption_budget_ids[0], pod_disruption_budget_ids[-1]))
-        create_objects_batch(batch_yp_client, ObjectTypes.POD_DISRUPTION_BUDGET.value, pod_disruption_budget_ids,
-                             DEFAULT_POD_DISRUPTION_BUDGET_ATTRS, dry_run, "pod disruption budget object")
-
-        logging.info("Creating {} pods with id prefix '{}'".format(podset_count * pod_count_per_podset, id_prefix))
-        for pod_set_index in xrange(podset_count):
-            pod_attrs = dict(deepcopy(DEFAULT_POD_ATTRS), meta=dict(pod_set_id=pod_set_ids[pod_set_index]))
-            pod_ids = [
-                POD_ID_PREFIX + "{}-{}".format(pod_set_index + 1, pod_index + 1)
-                for pod_index in xrange(pod_count_per_podset)
-            ]
-            create_objects_batch(batch_yp_client, ObjectTypes.POD.value, pod_ids, pod_attrs, dry_run, "pod")
-
-        logging.info("Attaching pod sets to pod disruption budget objects")
-        for pod_set_id, pd_budget_id in zip(pod_set_ids, pod_disruption_budget_ids):
-            logging.debug("Attaching pod set (id: '{}') to pod disruption budget object (id: '{}')".format(pod_set_id, pd_budget_id))
-            if not dry_run:
-                batch_yp_client.update_object(ObjectTypes.POD_SET.value, pod_set_id, set_updates=[dict(
-                    path="/spec/pod_disruption_budget_id",
-                    value=pd_budget_id,
-                )])
-
-
-def remove_objects(yp_client, dry_run, id_prefix):
-    logging.info("Running in mode '--remove'. Removing pod sets (with attached pods) and pod disruption budget objects"
-                 " with id prefix '{}'".format(id_prefix))
-
-    def _upper_bound_prefix(prefix):
-        return prefix[:-1] + chr(ord(prefix[-1]) + 1)
-
-    filter_by_prefix_query = "[/meta/id]>'{}' AND [/meta/id]<'{}'".format(id_prefix, _upper_bound_prefix(id_prefix))
-    selector_meta_id = ["/meta/id"]
-
-    with create_batch_yp_client(yp_client, batch_size=100) as batch_yp_client:
-        pod_set_ids = select_objects_batch(yp_client, ObjectTypes.POD_SET.value, filter_by_prefix_query, selector_meta_id)
-        logging.info("Removing {} pod sets".format(len(pod_set_ids)))
-        remove_objects_batch(batch_yp_client, ObjectTypes.POD_SET.value, pod_set_ids, dry_run, "pod set")
-
-        pod_disruption_budget_ids = select_objects_batch(yp_client, ObjectTypes.POD_DISRUPTION_BUDGET.value, filter_by_prefix_query, selector_meta_id)
-        logging.info("Removing {} pod disruption budget objects".format(len(pod_disruption_budget_ids)))
-        remove_objects_batch(batch_yp_client, ObjectTypes.POD_DISRUPTION_BUDGET.value, pod_disruption_budget_ids,
-                             dry_run, "pod disruption budget object")
+        _create_objects(batch_yp_client, "pod_disruption_budget", DEFAULT_POD_DISRUPTION_BUDGET_ATTRS, podset_count, id_prefix, dry_run)
 
         batch_yp_client.flush()
 
-        pod_ids = select_objects_batch(yp_client, ObjectTypes.POD.value, filter_by_prefix_query, selector_meta_id)
-        logging.info("Removing {} pods".format(len(pod_ids)))
-        remove_objects_batch(batch_yp_client, ObjectTypes.POD.value, pod_ids, dry_run, "pod")
+        _update_pod_sets(batch_yp_client, dry_run, podset_count, id_prefix)
+
+
+def remove_objects(yp_client, dry_run, id_prefix):
+    logging.info("Running in 'remove' mode. Removing pod sets and pod disruption budgets with id prefix '{}'".format(id_prefix))
+
+    with create_batch_yp_client(yp_client, batch_size=100, retries_count=5) as batch_yp_client:
+        _remove_objects(batch_yp_client, "pod_set", id_prefix, dry_run)
+        _remove_objects(batch_yp_client, "pod_disruption_budget", id_prefix, dry_run)
 
 
 def main():
@@ -167,12 +160,6 @@ def main():
         type=int,
         required=True,
         help="Pod set count",
-    )
-    create_objects_parser.add_argument(
-        "--pod-count-per-podset",
-        type=int,
-        required=True,
-        help="Pod count per pod set",
     )
 
     remove_objects_parser = subparsers.add_parser("remove", help="'remove' - mode for removing objects.", parents=[global_parser])
