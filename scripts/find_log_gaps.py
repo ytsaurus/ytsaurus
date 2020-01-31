50ee43ce7544e25c7db02d09bc413ec7fe7cb2ef
Maxim Akhmedov <max42@ytsaurus.tech>
2020-01-31 16:12:11 +0300

    Fix find_log_gaps.py

--- scripts/find_log_gaps.py
+++ scripts/find_log_gaps.py
@@ -11,13 +11,13 @@ def get_time(line):
     except:
         return None
 
-if __name__ == "__main__": 
+if __name__ == "__main__":
     parser = argparse.ArgumentParser()
     parser.add_argument("-n", "--count", help="Number of largest time intervals to show", default=10, type=int)
 
     args = parser.parse_args()
     n = args.count
-    
+
     lines = sys.stdin.readlines()
     lines = list(map(str.strip, lines))
     lines = list(filter(lambda s: len(s) > 0, lines))
@@ -33,11 +33,12 @@ if __name__ == "__main__":
 
     pairs = []
     for i in range(len(lines) - 1):
-        pairs.append(((get_time(lines[i + 1]) - get_time(lines[i])).microseconds // 1000, i, lines[i], lines[i + 1]))
-    
+        diff = get_time(lines[i + 1]) - get_time(lines[i])
+        pairs.append((diff.total_seconds(), i, lines[i], lines[i + 1]))
+
     pairs = sorted(pairs, reverse=True)
     for duration_ms, i, lhs_line, rhs_line in pairs[:min(len(pairs), n)]:
-        print "{} ms, {}..{}".format(duration_ms, i, i + 1)
+        print "{} sec, {}..{}".format(duration_ms, i, i + 1)
         print lhs_line
         print rhs_line
         print "---"
