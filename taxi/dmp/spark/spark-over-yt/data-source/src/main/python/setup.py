2462112e3826f671171a0b45555d9f179faeafc8
Alexandra Belousova <sashbel@yandex-team.ru>
2020-02-06 12:40:25 +0300

    SPYT-18 Update jupyter example

--- taxi/dmp/spark/spark-over-yt/data-source/src/main/python/setup.py
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/python/setup.py
@@ -5,7 +5,7 @@ setuptools.setup(
     version="0.0.1",
     author="Alexandra Belousova",
     author_email="sashbel@yandex-team.ru",
-    description="Spark over YT utils",
+    description="Spark over YT high-level client",
     url="https://github.yandex-team.ru/taxi-dwh/spark-over-yt",
     packages=setuptools.find_packages(),
 )
