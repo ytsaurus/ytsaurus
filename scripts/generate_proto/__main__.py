062ff68fda22579c810d6696389cb487b0bd52e7
buglloc <buglloc@41d65440-b5be-11dd-afe3-b2e846d9b4f8>
2020-02-10 21:48:04 +0000

    [yp/go] renamed protos api -> ypapi
    В самом начале сделал глупость и выбрал в качестве имени Go-пакетов с proto/yson `api` (по аналогии с package name самих proto). Но оно довольно фиговое, не информативное и путающее, поэтому хочется это недоразумение исправить :)
    
    git-svn-id: svn+ssh://arcadia.yandex.ru/arc/trunk/arcadia/yp@6352481 41d65440-b5be-11dd-afe3-b2e846d9b4f8

--- scripts/generate_proto/__main__.py
+++ scripts/generate_proto/__main__.py
@@ -158,7 +158,7 @@ option python_package = "yp_proto.yp.client.api.proto";
 option java_package = "ru.yandex.yp.client.api";
 option java_outer_classname = "Autogen";
 
-option go_package = "a.yandex-team.ru/yp/go/proto/api";
+option go_package = "a.yandex-team.ru/yp/go/proto/ypapi";
 
 """
     print_imports()
