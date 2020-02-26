cded98b24f3b46dc393a919fe64c53a38a499860
prime <prime@yandex-team.ru>
2020-02-26 17:17:33 +0300

    Package implementing continuous production profiling

---
+++ yt/go/pproflog/pprof_test.go
@@ -0,0 +1,42 @@
+package pproflog
+
+import (
+	"context"
+	"fmt"
+	"io/ioutil"
+	"testing"
+	"time"
+
+	"github.com/stretchr/testify/require"
+
+	"a.yandex-team.ru/yt/go/yttest"
+)
+
+func TestLogProfile(t *testing.T) {
+	tmp, err := ioutil.TempDir("", "pprof")
+	require.NoError(t, err)
+
+	t.Logf("logging profiles to %s", tmp)
+
+	go func() {
+		for {
+			_ = fmt.Sprintf("%d %s", 1, "Hello")
+		}
+	}()
+
+	opts := Options{
+		Dir:               tmp,
+		Keep:              3,
+		ProfilingPeriod:   time.Second,
+		ProfilingDuration: 100 * time.Millisecond,
+	}
+
+	ctx, cancel := context.WithTimeout(context.Background(), 5*time.Second)
+	defer cancel()
+
+	l, stopLogger := yttest.NewLogger(t)
+	defer stopLogger()
+
+	err = LogProfile(ctx, l, opts)
+	require.NoError(t, err)
+}
