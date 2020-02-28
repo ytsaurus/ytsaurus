9c5384948b9063bdca560667fb696d5442281de4
Maxim Akhmedov <max42@ytsaurus.tech>
2020-02-28 18:02:33 +0300

    Add standalone yt_start_clickhouse_clique binary.

--- scripts/clickhouse/deploy.py
+++ scripts/clickhouse/deploy.py
@@ -6,39 +6,60 @@ import yt.wrapper as yt
 import yt.yson as yson
 import sys
 import os
+import os.path
 import re
+import datetime
 
+def is_multiplatform_bin(kind):
+    return kind == "yt-start-clickhouse-clique"
+
+def get_bin_path(src_dir, kind, platform="linux"):
+    assert not is_multiplatform_bin(kind) or platform is not None
+    platform_dir = platform if is_multiplatform_bin(kind) else ""
+    platform_ext = ".exe" if platform == "windows" else ""
+    return os.path.join(src_dir, platform_dir, kind + platform_ext)
+    
+def get_version(src_dir, kind):
+    bin_path = get_bin_path(src_dir, kind)
+    args = [bin_path, "--version"]
+    print >>sys.stderr, "Invoking {} to find out commit".format(args)
+    version = subprocess.check_output(args, stderr=subprocess.STDOUT).strip()
+
+    if kind in ("ytserver-clickhouse", "clickhouse-trampoline", "ytserver-log-tailer"):
+        yt_commit = re.search("~([0-9a-z]*)", version).groups()[0]
+        return (yt_commit, version)
+    else:
+        yt_commit = re.search("git:([0-9a-z]*)", version).groups()[0]
+        return (yt_commit, datetime.datetime.now().strftime("%Y%m%d%H%M%S") + "-" + yt_commit)
+        
 def main():
     parser = argparse.ArgumentParser(description="Deploy CHYT binary")
-    parser.add_argument("--src", help="Path of binary to be deployed; by default one from /home/max42/yt/build-rel/bin is taken")
+    parser.add_argument("--src-dir", default="../../../build-rel/bin", help="Path containing binary to be deployed; by default, ../../../build-rel/bin")
     parser.add_argument("--tags", metavar="TAG", nargs="*", help="tags that will be appended like +tag1+tag2", default=[])
     parser.add_argument("--kind", default="ytserver-clickhouse",
-                        choices=["ytserver-clickhouse", "clickhouse-trampoline", "ytserver-log-tailer"],
+                        choices=["ytserver-clickhouse", "clickhouse-trampoline", "ytserver-log-tailer", "yt-start-clickhouse-clique"],
                         help="Kind of binary to be deployed")
     args = parser.parse_args()
 
-    src = args.src or ("/home/max42/yt/build-rel/bin/" + args.kind)
+    yt_commit, version = get_version(args.src_dir, args.kind) 
 
-    print >>sys.stderr, "Invoking {} to find out commit".format([src, "--version"])
-    version = subprocess.check_output([src, "--version"]).strip()
-    yt_commit = re.match(".*~([0-9a-z]*).*", version).groups()[0]
-
-    kind_to_server_path = {
-        "ytserver-clickhouse": "clickhouse_server",
-        "clickhouse-trampoline": "clickhouse_trampoline",
-        "ytserver-log-tailer": "log_tailer",
+    kind_to_repo_path = {
+        "ytserver-clickhouse": "/yt/server/clickhouse_server",
+        "clickhouse-trampoline": "/yt/server/clickhouse_trampoline",
+        "ytserver-log-tailer": "/yt/server/log_tailer",
+        "yt-start-clickhouse-clique": "/scripts/clickhouse",
     }
 
-    server_path = kind_to_server_path[args.kind]
+    repo_path = kind_to_repo_path[args.kind]
 
     attrs = {
         "executable": True,
         "yt_version": version,
-        "yt_version_url": yson.to_yson_type("https://github.yandex-team.ru/yt/yt/tree/" + yt_commit + "/yt/server/" + server_path, attributes={"_type_tag": "url"}),
+        "yt_version_url": yson.to_yson_type("https://github.yandex-team.ru/yt/yt/tree/" + yt_commit + repo_path, attributes={"_type_tag": "url"}),
     }
 
     if args.kind == "ytserver-clickhouse":
-        ch_version, ch_commit = subprocess.check_output([src, "--clickhouse-version"]).strip().split()
+        ch_version, ch_commit = subprocess.check_output([get_bin_path(args.src_dir, args.kind), "--clickhouse-version"]).strip().split()
         attrs.update({
             "ch_version": ch_version,
             "ch_commit": ch_commit,
@@ -46,11 +67,20 @@ def main():
         })
 
     tags = "".join("+" + tag for tag in args.tags)
-    filename = "{0}-{1}{2}".format(args.kind, version, tags)
-    cypress_path = "//sys/clickhouse/bin/" + filename
-    print >>sys.stderr, "Deploying {} to {}".format(src, cypress_path)
-    yt.create("file", cypress_path, attributes=attrs, ignore_existing=True)
-    yt.write_file(cypress_path, open(src), filename_hint=filename, size_hint=os.stat(src).st_size)
+
+    platforms = ["linux"]
+    if is_multiplatform_bin(args.kind):
+        platforms += ["darwin", "windows"]
+
+    for platform in platforms:
+        platform_suffix = "." + platform if is_multiplatform_bin(args.kind) else ""
+        platform_ext = ".exe" if platform == "windows" else ""
+        filename = "{0}{1}-{2}{3}{4}".format(args.kind, platform_suffix, version, tags, platform_ext)
+        cypress_path = "//sys/clickhouse/bin/" + filename
+        bin_path = get_bin_path(args.src_dir, args.kind, platform=platform)
+        print >>sys.stderr, "[{}] Deploying {} to {}".format(platform, bin_path, cypress_path)
+        yt.create("file", cypress_path, attributes=attrs, ignore_existing=True)
+        yt.write_file(cypress_path, open(bin_path), filename_hint=filename, size_hint=os.stat(bin_path).st_size)
 
 if __name__ == "__main__":
     main()
--- scripts/clickhouse/manage_log_tables_make/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import manage_log_tables
-
-__all__ = ["manage_log_tables"]
