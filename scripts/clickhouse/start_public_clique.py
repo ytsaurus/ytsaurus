0a1f105fd01e4743a7b330618e7cf44114695ba0
Maxim Akhmedov <max42@ytsaurus.tech>
2020-02-28 19:59:21 +0300

    Remove log tailer specification from start_public_clique.py.

--- scripts/clickhouse/start_public_clique.py
+++ scripts/clickhouse/start_public_clique.py
@@ -37,7 +37,6 @@ def main():
         enable_monitoring=True,
         enable_job_tables=True,
         cypress_geodata_path="//sys/clickhouse/geodata/geodata.tgz",
-        cypress_ytserver_log_tailer_path="//sys/clickhouse/bin/ytserver-log-tailer-19.8.34144-local-ya~b00d808768+max42+now-correct",
         cypress_ytserver_clickhouse_path=args.bin,
         spec={
             "acl": [{
