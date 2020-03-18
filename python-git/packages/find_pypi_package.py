109bed912d872838f43cd1816210003ef267e683
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2020-03-18 11:04:19 +0300

    Fix find_pypi_package.py

--- python-git/packages/find_pypi_package.py
+++ python-git/packages/find_pypi_package.py
@@ -3,7 +3,7 @@
 import os
 import sys
 
-sys.path.insert(0, os.path.join(os.path.dirname(__file__), "teamcity-build/python/python_packaging"))
+sys.path.insert(0, os.path.join(os.path.dirname(__file__), "../teamcity-build/python/python_packaging"))
 
 from helpers import get_version
 
