d460f51a88fbfddc92c6623442197cc19d11ee7b
Aleksandra Belousova <sashbel@yandex-team.ru>
2020-03-25 18:46:14 +0300

    SPYT-46 Read spark configuration from YT (#608)
    * SPYT-46 Read spark configuration from YT
    
    * SPYT-46 Use YSON document instead of JSON file
    
    * SPYT-46 Add operation-spec and spark-worker-timeout arguments
    
    * SPYT-46 Update tests
    
    * SPYT-46 Single quotes to double
    
    * SPYT-46 Fix spark-worker-timeout default
    
    * SPYT-46 SparkDefaultArguments
    
    * SPYT-46 Fix SparkDefaultArguments

--- python-git/yt/wrapper/tests/test_spark.py
+++ python-git/yt/wrapper/tests/test_spark.py
@@ -1,106 +1,104 @@
-import json
-
-import pytest
 import yt
-from yt.wrapper.cypress_commands import create
-from yt.wrapper.file_commands import write_file
+from yt.wrapper.cypress_commands import create, set
 from yt.wrapper.spark import build_spark_operation_spec, find_spark_cluster, SparkDiscovery, _read_launch_config, \
     _parse_memory, _format_memory
 from yt.wrapper.spec_builders import VanillaSpecBuilder
 
+import pytest
 
-@pytest.mark.usefixtures('yt_env_with_rpc')
+
+@pytest.mark.usefixtures("yt_env_with_rpc")
 class TestSpark(object):
     @property
     def config(self):
         return {
-            'port_max_retries': 200,
-            'shuffle_service_port': 27000,
-            'spark_launcher_name': 'spark-test.jar',
-            'spark_name': 'spark-test',
-            'spark_yt_base_path': '//base/path',
-            'start_port': 27001
+            "environment": {
+                "IS_SPARK_CLUSTER": "true",
+                "JAVA_HOME": "/opt/jdk8"
+            },
+            "file_paths": ["//base/path/spark.tgz", "//base/path/spark-yt-launcher.jar"],
+            "layer_paths": ["//layer1", "//layer2"],
+            "spark_yt_base_path": "//base/path",
+            "spark_conf": {
+                "spark.test": "test"
+            }
         }
 
     def test_build_spark_operation_spec(self):
         if yt.wrapper.config["backend"] == "native" or yt.wrapper.config["backend"] == "rpc":
             return
-        actual = build_spark_operation_spec(operation_alias='test_alias',
-                                            spark_discovery=SparkDiscovery('//tmp/spark-discovery'),
+        actual = build_spark_operation_spec(operation_alias="test_alias",
+                                            spark_discovery=SparkDiscovery("//tmp/spark-discovery"),
                                             dynamic_config=self.config,
                                             spark_worker_core_count=1,
                                             spark_worker_memory_limit=2147483648,
                                             spark_worker_count=1,
+                                            spark_worker_timeout="10m",
                                             spark_worker_tmpfs_limit=2147483648,
                                             spark_master_memory_limit=2147483648,
                                             spark_history_server_memory_limit=2147483648,
-                                            pool='test_pool')
+                                            pool="test_pool",
+                                            operation_spec={"max_failed_job_count": 10})
 
         task_spec = {
-            'environment': {'IS_SPARK_CLUSTER': 'true',
-                            'JAVA_HOME': '/opt/jdk8',
-                            'SPARK_DISCOVERY_PATH': '//tmp/spark-discovery/discovery',
-                            'SPARK_HOME': 'spark-test'},
-            'file_paths': ['//base/path/spark-test.tgz', '//base/path/spark-test.jar'],
-            'layer_paths': ['//home/sashbel/delta/jdk/layer_with_jdk_lastest.tar.gz',
-                            '//home/sashbel/delta/python/layer_with_python37.tar.gz',
-                            '//porto_layers/base/xenial/porto_layer_search_ubuntu_xenial_app_lastest.tar.gz'],
-            'memory_reserve_factor': 1.0,
-            'restart_completed_jobs': True
+            "environment": {
+                "IS_SPARK_CLUSTER": "true",
+                "JAVA_HOME": "/opt/jdk8",
+                "SPARK_DISCOVERY_PATH": "//tmp/spark-discovery/discovery"
+            },
+            "file_paths": ["//base/path/spark.tgz", "//base/path/spark-yt-launcher.jar"],
+            "layer_paths": ["//layer1", "//layer2"],
+            "memory_reserve_factor": 1.0,
+            "restart_completed_jobs": True
         }
 
         expected = VanillaSpecBuilder() \
-            .begin_task('master') \
+            .begin_task("master") \
             .job_count(1) \
-            .command('tar --warning=no-unknown-keyword -xf spark-test.tgz && '
-                     '/opt/jdk8/bin/java -Xmx512m -cp spark-test.jar ru.yandex.spark.launcher.MasterLauncher '
-                     '--port 27001 --opts "\'-Dspark.master.rest.enabled=true -Dspark.master.rest.port=27001 '
-                     '-Dspark.hadoop.fs.yt.impl=ru.yandex.spark.yt.fs.YtFileSystem -Dspark.port.maxRetries=200 '
-                     '-Dspark.shuffle.service.port=27000 \'" --operation-id $YT_OPERATION_ID --web-ui-port 27001') \
+            .command("tar --warning=no-unknown-keyword -xf spark.tgz && "
+                     "/opt/jdk8/bin/java -Xmx512m -cp spark-yt-launcher.jar -Dspark.test=test "
+                     "ru.yandex.spark.launcher.MasterLauncher ") \
             .memory_limit(2147483648) \
             .cpu_limit(2) \
             .end_task() \
-            .begin_task('history') \
+            .begin_task("history") \
             .job_count(1) \
-            .command('tar --warning=no-unknown-keyword -xf spark-test.tgz && '
-                     '/opt/jdk8/bin/java -Xmx512m -cp spark-test.jar ru.yandex.spark.launcher.HistoryServerLauncher '
-                     '--port 27001 --opts "\'-Dspark.history.fs.cleaner.enabled=true '
-                     '-Dspark.hadoop.fs.yt.impl=ru.yandex.spark.yt.fs.YtFileSystem '
-                     '-Dspark.port.maxRetries=200 -Dspark.shuffle.service.port=27000 \'" '
-                     '--log-path yt:///tmp/spark-discovery/logs/event_log') \
+            .command("tar --warning=no-unknown-keyword -xf spark.tgz && "
+                     "/opt/jdk8/bin/java -Xmx512m -cp spark-yt-launcher.jar -Dspark.test=test "
+                     "ru.yandex.spark.launcher.HistoryServerLauncher "
+                     "--log-path yt:///tmp/spark-discovery/logs/event_log") \
             .memory_limit(2147483648) \
             .cpu_limit(1) \
             .end_task() \
-            .begin_task('workers') \
+            .begin_task("workers") \
             .job_count(1) \
-            .command('tar --warning=no-unknown-keyword -xf spark-test.tgz && '
-                     '/opt/jdk8/bin/java -Xmx512m -cp spark-test.jar ru.yandex.spark.launcher.WorkerLauncher '
-                     '--port 27001 --opts "\'-Dspark.worker.cleanup.enabled=true -Dspark.shuffle.service.enabled=true '
-                     '-Dspark.hadoop.fs.yt.impl=ru.yandex.spark.yt.fs.YtFileSystem -Dspark.port.maxRetries=200 '
-                     '-Dspark.shuffle.service.port=27000 \'" --cores 1 --memory 2G --web-ui-port 27001') \
+            .command("tar --warning=no-unknown-keyword -xf spark.tgz && "
+                     "/opt/jdk8/bin/java -Xmx512m -cp spark-yt-launcher.jar -Dspark.test=test "
+                     "ru.yandex.spark.launcher.WorkerLauncher "
+                     "--cores 1 --memory 2G --wait-master-timeout 10m") \
             .memory_limit(4294967296) \
             .cpu_limit(3) \
-            .tmpfs_path('tmpfs') \
+            .tmpfs_path("tmpfs") \
             .end_task() \
-            .secure_vault({'YT_TOKEN': None, 'YT_USER': 'root'}) \
-            .max_failed_job_count(5) \
-            .max_stderr_count(150) \
-            .title('test_alias') \
+            .secure_vault({"YT_TOKEN": None, "YT_USER": "root"}) \
             .spec({
-                'annotations': {'is_spark': True},
-                'pool': 'test_pool',
-                'stderr_table_path': '//tmp/spark-discovery/logs/stderr'
+                "annotations": {"is_spark": True},
+                "pool": "test_pool",
+                "stderr_table_path": "//tmp/spark-discovery/logs/stderr",
+                "max_failed_job_count": 10,
+                "max_stderr_count": 150,
+                "title": "test_alias"
             })
 
         actual_tasks = {}
-        for task_name, task in actual._spec['tasks'].items():
+        for task_name, task in actual._spec["tasks"].items():
             del task._user_spec["environment"]["YT_PROXY"]  # exclude YT_PROXY from assert because of random port
             actual_tasks[task_name] = (task._spec, task._user_spec)
-        expected_tasks = dict([(k, (v._spec, task_spec)) for k, v in expected._spec['tasks'].items()])
+        expected_tasks = dict([(k, (v._spec, task_spec)) for k, v in expected._spec["tasks"].items()])
         assert actual_tasks == expected_tasks
 
-        actual._spec['tasks'] = {}
-        expected._spec['tasks'] = {}
+        actual._spec["tasks"] = {}
+        expected._spec["tasks"] = {}
         assert actual._spec == expected._spec
         assert actual._user_spec == expected._user_spec
 
@@ -111,7 +109,7 @@ class TestSpark(object):
         create("map_node", "//tmp/spark-discovery/discovery/operation/1234-5678", recursive=True)
         create("map_node", "//tmp/spark-discovery/discovery/shs/host2:4", recursive=True)
 
-        res = find_spark_cluster('//tmp/spark-discovery')
+        res = find_spark_cluster("//tmp/spark-discovery")
         assert res.master_endpoint == "host1:1"
         assert res.master_web_ui_url == "host1:2"
         assert res.master_rest_endpoint == "host1:3"
@@ -119,8 +117,8 @@ class TestSpark(object):
         assert res.shs_url == "host2:4"
 
     def test_read_launch_config(self):
-        path = "//tmp/spark-launch.json"
-        write_file(path, json.dumps(self.config).encode('utf-8'))
+        path = "//tmp/spark-launch-conf"
+        set(path, self.config)
         res = _read_launch_config(path)
         assert res == self.config
 
