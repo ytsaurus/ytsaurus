a2cc89487275ed4f7d1c9e75bc13d4a3ba68a6b7
Ignat Kolesnichenko <ignat@ytsaurus.tech>
2020-03-29 12:03:50 +0300

    YT-12279: move scripts, debian and git-depth inside yt/

--- scripts/clickhouse/find_log_tailer_log_loss/__init__.py
+++
@@ -1,3 +0,0 @@
-from . import find_log_tailer_log_loss
-
-__all__ = ["find_log_tailer_log_loss"]
--- scripts/clickhouse/yt_start_clickhouse_clique_make/build_all_platforms.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-for platform in linux darwin windows ; do
-    echo "Building for platform $platform"
-    mkdir -p ../../../../build-rel/bin/$platform
-    ya make -I ../../../../build-rel/bin/$platform -r --target-platform ${platform}
-done
