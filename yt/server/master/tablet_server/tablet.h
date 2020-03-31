09bd01fae28ab733bd968277451b5fa4c5d9d3b2
Maxim Babenko <babenko@ytsaurus.tech>
2020-03-31 17:18:11 +0300

    Cosmetics

--- yt/server/master/tablet_server/tablet.h
+++ yt/server/master/tablet_server/tablet.h
@@ -34,7 +34,7 @@ struct TTabletCellStatisticsBase
     i64 CompressedDataSize = 0;
     i64 MemorySize = 0;
     i64 DynamicMemoryPoolSize = 0;
-    NChunkClient::TMediumMap<i64> DiskSpacePerMedium = NChunkClient::TMediumMap<i64>();
+    NChunkClient::TMediumMap<i64> DiskSpacePerMedium;
     int ChunkCount = 0;
     int PartitionCount = 0;
     int StoreCount = 0;
