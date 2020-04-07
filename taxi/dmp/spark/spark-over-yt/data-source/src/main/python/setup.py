4c1fd2aad74b7563bddce356414d410b5d59fe03
Alexandra Belousova <sashbel@yandex-team.ru>
2020-04-07 19:34:48 +0300

    Release yandex-spyt 0.0.8

--- taxi/dmp/spark/spark-over-yt/data-source/src/main/python/setup.py
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/python/setup.py
@@ -2,14 +2,14 @@ import setuptools
 
 setuptools.setup(
     name="yandex-spyt",
-    version="0.0.8b2",
+    version="0.0.8",
     author="Alexandra Belousova",
     author_email="sashbel@yandex-team.ru",
     description="Spark over YT high-level client",
     url="https://github.yandex-team.ru/taxi-dwh/spark-over-yt",
     packages=setuptools.find_packages(),
     install_requires=[
-        "yandex-pyspark==2.4.4.post1",
+        "yandex-pyspark==2.4.4.post2",
         "yandex-yt>=0.9.29",
         "pyyaml>=5.3.1"
     ],
