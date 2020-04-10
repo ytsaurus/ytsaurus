24bd3783d2b63da3f34b42a59a573afa153a86ba
Grigory Reznikov <gritukan@ytsaurus.tech>
2020-04-10 11:15:32 +0300

    YT-12665: Optimize TColumnarStatisticsFetcher memory usage (#4497)

--- yt/client/table_client/columnar_statistics.h
+++ yt/client/table_client/columnar_statistics.h
@@ -8,6 +8,16 @@ namespace NYT::NTableClient {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+struct TLightweightColumnarStatistics
+{
+    //! Sum of per-column data weight for chunks whose meta contains columnar statistics.
+    i64 ColumnDataWeightsSum = 0;
+    //! Total weight of all write and delete timestamps.
+    std::optional<i64> TimestampTotalWeight;
+    //! Total data weight of legacy chunks whose meta misses columnar statistics.
+    i64 LegacyChunkDataWeight = 0;
+};
+
 struct TColumnarStatistics
 {
     //! Per-column total data weight for chunks whose meta contains columnar statistics.
@@ -20,6 +30,8 @@ struct TColumnarStatistics
     TColumnarStatistics& operator +=(const TColumnarStatistics& other);
 
     static TColumnarStatistics MakeEmpty(int columnCount);
+
+    TLightweightColumnarStatistics MakeLightweightStatistics() const;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
