f8d343b53ea755a23ab012d662d83da8c3abc0ae
Aleksandra Zhikhareva <aleksandra-zh@ytsaurus.tech>
2020-04-17 12:03:27 +0300

    YT-11779: Distributed Throttler

---
+++ yt/ytlib/distributed_throttler/config.cpp
@@ -0,0 +1,37 @@
+#include "config.h"
+
+#include <yt/ytlib/discovery_client/config.h>
+
+#include <yt/core/concurrency/config.h>
+
+namespace NYT::NDistributedThrottler {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TDistributedThrottlerConfig::TDistributedThrottlerConfig()
+{
+    RegisterParameter("member_client", MemberClient)
+        .DefaultNew();
+    RegisterParameter("discovery_client", DiscoveryClient)
+        .DefaultNew();
+
+    RegisterParameter("rpc_timeout", RpcTimeout)
+        .Default(TDuration::Seconds(5));
+
+    RegisterParameter("limit_update_period", LimitUpdatePeriod)
+        .Default(TDuration::Seconds(1));
+    RegisterParameter("leader_update_period", LeaderUpdatePeriod)
+        .Default(TDuration::Seconds(10));
+
+    RegisterParameter("distribute_limits_uniformly", DistributeLimitsUniformly)
+        .Default(true);
+    RegisterParameter("extra_limit_ratio", ExtraLimitRatio)
+        .Default(0.1);
+    RegisterParameter("ema_alpha", EmaAlpha)
+        .Default(0.1);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NDistributedThrottler
+
---
+++ yt/ytlib/distributed_throttler/config.h
@@ -0,0 +1,39 @@
+#pragma once
+
+#include "public.h"
+
+#include <yt/core/ytree/attributes.h>
+
+#include <yt/core/ytree/yson_serializable.h>
+
+#include <yt/ytlib/discovery_client/public.h>
+
+namespace NYT::NDistributedThrottler {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TDistributedThrottlerConfig
+    : public NYTree::TYsonSerializable
+{
+public:
+    NDiscoveryClient::TMemberClientConfigPtr MemberClient;
+    NDiscoveryClient::TDiscoveryClientConfigPtr DiscoveryClient;
+
+    TDuration RpcTimeout;
+
+    TDuration LimitUpdatePeriod;
+    TDuration LeaderUpdatePeriod;
+
+    bool DistributeLimitsUniformly;
+    double ExtraLimitRatio;
+    double EmaAlpha;
+
+    TDistributedThrottlerConfig();
+};
+
+DEFINE_REFCOUNTED_TYPE(TDistributedThrottlerConfig)
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NDistributedThrottler
+
