21321796e883a081b172ae0bc3185bc99a2291f4
Aleksandra Belousova <sashbel@sandbox-408100153>
2020-04-20 18:33:22 +0300

    Release client 0.1.0, yandex-spyt 0.1.0

--- taxi/dmp/spark/spark-over-yt/project/CommonPlugin.scala
+++ taxi/dmp/spark/spark-over-yt/project/CommonPlugin.scala
@@ -14,7 +14,7 @@ object CommonPlugin extends AutoPlugin {
     resolvers += "YandexMediaReleases" at "http://artifactory.yandex.net/artifactory/yandex_media_releases",
     resolvers += "YandexSparkReleases" at "http://artifactory.yandex.net/artifactory/yandex_spark_releases",
     resolvers += MavenCache("local-maven", Path.userHome / ".m2" / "repository"),
-    version in ThisBuild := "0.1.0-SNAPSHOT",
+    version in ThisBuild := "0.1.0",
     organization := "ru.yandex",
     name := s"spark-yt-${name.value}",
     scalaVersion := "2.12.8",
