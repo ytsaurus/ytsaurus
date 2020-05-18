f4aab35d7578b90955f4e52fe87c927c8e59d75d
ignat <ignat@ytsaurus.tech>
2020-05-18 09:42:14 +0300

    Fix cpp integration tests

--- yt/yt/tests/cpp/conftest.py
+++ yt/yt/tests/cpp/conftest.py
@@ -2,7 +2,6 @@ import sys
 import os
 
 sys.path.insert(0, os.path.abspath("../../tests/integration"))
-sys.path.insert(0, os.path.abspath("../../../python"))
 sys.path.append(os.path.abspath("."))
 
 from yt_shell_test import CppFile
