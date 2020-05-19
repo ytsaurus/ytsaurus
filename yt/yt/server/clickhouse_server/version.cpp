99385d3e19b824a421d35b0a6abe097afce2b5c3
max42 <max42@ytsaurus.tech>
2020-05-19 15:09:44 +0300

    Improve CHYT versioning.

--- yt/yt/server/clickhouse_server/version.cpp
+++ yt/yt/server/clickhouse_server/version.cpp
@@ -12,13 +12,17 @@ TString GetVersion()
 {
     TString branch = GetBranch();
     auto pos = branch.find("chyt/");
+    TString hash(GetProgramHash());
+    if (hash.size() >= 10) {
+        hash.resize(10);
+    }
     if (pos != TString::npos) {
         pos += 5;
-        return branch.substr(pos) + "." + ToString(GetVersionPatch()) + "~" + GetProgramCommitId();
+        return branch.substr(pos) + "." + ToString(GetVersionPatch()) + "~" + hash;
     } else if (branch.find("trunk") != TString::npos) {
-        return "0.0." + ToString(GetProgramSvnRevision()) + "~" + GetProgramCommitId();
+        return "0.0." + ToString(GetProgramSvnRevision()) + "-trunk~" + hash;
     } else {
-        return "0.0." + ToString(GetProgramSvnRevision()) + "-local~" + GetProgramCommitId();
+        return "0.0." + ToString(GetProgramSvnRevision()) + "-local~" + hash;
     }
 }
 
