089d24f253cadd7cbfb00da2f79085cf8ba44223
babenko <babenko@ytsaurus.tech>
2020-05-24 19:07:30 +0300

    Fix 19.8 compatibility

--- yt/yt/server/node/data_node/job.cpp
+++ yt/yt/server/node/data_node/job.cpp
@@ -721,7 +721,7 @@ public:
             config,
             bootstrap)
         , JobSpecExt_(JobSpec_.GetExtension(TRepairChunkJobSpecExt::repair_chunk_job_spec_ext))
-        , ChunkId_(FromProto<TChunkId>(JobSpecExt_.chunk_id()))
+        , ChunkId_(FixChunkId(FromProto<TChunkId>(JobSpecExt_.chunk_id())))
         , SourceReplicas_(FromProto<TChunkReplicaList>(JobSpecExt_.source_replicas()))
         , TargetReplicas_(FromProto<TChunkReplicaWithMediumList>(JobSpecExt_.target_replicas()))
     { }
@@ -733,6 +733,17 @@ private:
     const TChunkReplicaWithMediumList TargetReplicas_;
 
     const NNodeTrackerClient::TNodeDirectoryPtr NodeDirectory_ = New<NNodeTrackerClient::TNodeDirectory>();
+
+
+    // COMPAT(babenko): pre-20.2 master servers may send encoded chunk id, which is inappropriate.
+    static TChunkId FixChunkId(TChunkId chunkId)
+    {
+        auto type = TypeFromId(chunkId);
+        if (type >= MinErasureChunkPartType && type <= MaxErasureChunkPartType) {
+            return ReplaceTypeInId(chunkId, EObjectType::ErasureChunk);
+        }
+        return chunkId;
+    }
 
     IChunkReaderPtr CreateReader(int partIndex)
     {
