4e39cf80b3ebf9f46c75abc2c19103aefa28a5f9
ignat <ignat@ytsaurus.tech>
2020-05-31 20:28:22 +0300

    YT-9624: create lock node automatically under transaction

--- yt/python/yt/tools/bin/lock.py
+++ yt/python/yt/tools/bin/lock.py
@@ -12,6 +12,9 @@ import argparse
 import socket
 import subprocess
 
+class SuccessfullyFinished(BaseException):
+    pass
+
 def main():
     parser = argparse.ArgumentParser(description="Run command under lock")
     parser.add_argument("path")
@@ -30,54 +33,60 @@ def main():
     if args.create_lock:
         yt.create("map_node", args.path, ignore_existing=True, recursive=True)
 
-    with yt.Transaction(attributes={"title": "yt_lock transaction"}) as tx:
-        try:
-            yt.lock(args.path)
-        except yt.YtResponseError as error:
-            if error.is_concurrent_transaction_lock_conflict():
-                logger.info("Lock conflict (path %s)", args.path)
-                sys.exit(args.conflict_exit_code)
-            raise
+    try:
+        with yt.Transaction(attributes={"title": "yt_lock transaction"}) as tx:
+            yt.create("map_node", args.path, ignore_existing=True)
+            try:
+                yt.lock(args.path)
+            except yt.YtResponseError as error:
+                if error.is_concurrent_transaction_lock_conflict():
+                    logger.info("Lock conflict (path %s)", args.path)
+                    sys.exit(args.conflict_exit_code)
+                raise
 
-        if args.set_address:
-            if args.address_path is not None:
-                with yt.Transaction(transaction_id=YT_NULL_TRANSACTION_ID):
-                    yt.set(args.address_path, socket.getfqdn())
+            if args.set_address:
+                if args.address_path is not None:
+                    with yt.Transaction(transaction_id=YT_NULL_TRANSACTION_ID):
+                        yt.set(args.address_path, socket.getfqdn())
+                else:
+                    yt.set(args.path + "/@address", socket.getfqdn())
+
+            def handler():
+                tx.__exit__(None, None, None)
+                sys.exit(1)
+
+            signal.signal(signal.SIGTERM, lambda signum, frame: handler())
+
+            if len(args.command) == 1:
+                # Support for commands like `yt_lock.py ... "sleep 1000"`
+                command = args.command[0]
+                run_shell = True
             else:
-                yt.set(args.path + "/@address", socket.getfqdn())
+                # Support for commands like `yt_lock.py ... sleep 1000`
+                command = args.command
+                run_shell = False
+            logger.info("Running command %s", args.command)
+            proc = subprocess.Popen(command, stdout=sys.stdout, stderr=sys.stderr, shell=run_shell, env=os.environ.copy(),
+                                    preexec_fn=set_pdeathsig)
 
-        def handler():
-            tx.__exit__(None, None, None)
-            sys.exit(1)
+            while True:
+                if not tx._ping_thread.is_alive():
+                    logger.error("Pinging thread failed. Terminating command.")
+                    proc.send_signal(2)
+                    time.sleep(args.step)
+                    proc.terminate()
+                    time.sleep(args.step)
+                    proc.kill()
+                    break
 
-        signal.signal(signal.SIGTERM, lambda signum, frame: handler())
+                if proc.poll() is not None:
+                    break
 
-        if len(args.command) == 1:
-            # Support for commands like `yt_lock.py ... "sleep 1000"`
-            command = args.command[0]
-            run_shell = True
-        else:
-            # Support for commands like `yt_lock.py ... sleep 1000`
-            command = args.command
-            run_shell = False
-        logger.info("Running command %s", args.command)
-        proc = subprocess.Popen(command, stdout=sys.stdout, stderr=sys.stderr, shell=run_shell, env=os.environ.copy(),
-                                preexec_fn=set_pdeathsig)
-
-        while True:
-            if not tx._ping_thread.is_alive():
-                logger.error("Pinging thread failed. Terminating command.")
-                proc.send_signal(2)
                 time.sleep(args.step)
-                proc.terminate()
-                time.sleep(args.step)
-                proc.kill()
-                break
 
-            if proc.poll() is not None:
-                break
-
-            time.sleep(args.step)
+            raise SuccessfullyFinished()
+    except SuccessfullyFinished:
+        pass
 
     sys.exit(proc.returncode)
 
