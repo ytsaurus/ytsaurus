634c9652a8c3a20e20ac075b7d3bd7c336703cce
k-malov <k-malov@ytsaurus.tech>
2020-06-03 15:52:45 +0300

    send http request with retries ()

--- yt/python/yt/infra_api/api.py
+++ yt/python/yt/infra_api/api.py
@@ -4,6 +4,9 @@ import requests
 import time
 import typing
 
+from requests import adapters
+from urllib3.util import retry
+
 
 class InfraError(Exception):
     pass
@@ -22,16 +25,40 @@ class EVENT(object):
 class InfraClient(object):
     VERSION = "v1"
 
-    def __init__(self, backend_url, token):
+    def __init__(self, backend_url, token, max_retries=None, backoff_factor=None):
         self.backend_url = backend_url
         self.token = token
 
+        if max_retries is None:
+            max_retries = 3
+
+        if backoff_factor is None:
+            backoff_factor = 0.5
+
+        self.max_retries = max_retries
+        self.backoff_factor = backoff_factor
+
+    def _build_http_session(self):
+        session = requests.Session()
+        retry_strategy = retry.Retry(
+            total=self.max_retries,
+            backoff_factor = self.backoff_factor,
+            status_forcelist=[500, 502, 503, 504],
+            method_whitelist=["GET"],
+        )
+        adapter = adapters.HTTPAdapter(max_retries=retry_strategy)
+        session.mount("https://", adapter)
+        session.mount("http://", adapter)
+        return session
+
     def _request(self, method, url, params=None, body=None):
+        http_session = self._build_http_session()
+
         headers = {}
         if self.token is not None:
             headers["Authorization"] = "OAuth {0}".format(self.token)
 
-        response = requests.request(
+        response = http_session.request(
             method=method,
             url="https://{0}/{1}/{2}".format(self.backend_url, self.VERSION, url),
             params=params,
