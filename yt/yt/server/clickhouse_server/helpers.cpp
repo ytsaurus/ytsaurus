4201695077f181416b083db77f9079ee9c82cec2
max42 <max42@ytsaurus.tech>
2020-06-24 00:26:56 +0300

    Properly register system database.

--- yt/yt/server/clickhouse_server/helpers.cpp
+++ yt/yt/server/clickhouse_server/helpers.cpp
@@ -62,6 +62,7 @@ void RegisterNewUser(DB::AccessControlManager& accessControlManager, TString use
     auto user = std::make_unique<DB::User>();
     user->setName(userName);
     user->access.grant(DB::AccessFlags::allFlags(), "YT" /* database */);
+    user->access.grant(DB::AccessFlags::allFlags(), "system" /* database */);
     user->access.grant(DB::AccessType::CREATE_TEMPORARY_TABLE);
     user->access.grant(DB::AccessType::dictGet);
 
