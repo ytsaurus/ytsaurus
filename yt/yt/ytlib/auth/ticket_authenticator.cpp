84eb672a4bcfe75d28c69acb1883ff1dc10b24d1
bidzilya <bidzilya@yandex-team.ru>
2020-07-29 15:47:11 +0300

    Cosmetic

--- yt/yt/ytlib/auth/ticket_authenticator.cpp
+++ yt/yt/ytlib/auth/ticket_authenticator.cpp
@@ -88,7 +88,7 @@ private:
         return TError(NRpc::EErrorCode::InvalidCredentials,
             "Ticket does not provide an allowed scope")
             << TErrorAttribute("scopes", scopes)
-            << TErrorAttribute("allowedScopes", allowedScopes);
+            << TErrorAttribute("allowed_scopes", allowedScopes);
     }
 
     TAuthenticationResult OnBlackboxCallResult(const TString& ticketHash, const INodePtr& data)
