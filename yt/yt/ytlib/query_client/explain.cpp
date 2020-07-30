817283384cdcd7362c38a0624228598d98554470
lexolordan <lexolordan@yandex-team.ru>
2020-07-30 20:15:25 +0300

    YT-12300: explain with groupped data ranges

--- yt/yt/ytlib/query_client/explain.cpp
+++ yt/yt/ytlib/query_client/explain.cpp
@@ -1,10 +1,13 @@
 #include "coordinator.h"
 #include "explain.h"
 #include "functions.h"
+#include "helpers.h"
 #include "query_helpers.h"
 #include "query_preparer.h"
 
 #include <yt/client/api/public.h>
+
+#include <yt/client/api/rowset.h>
 
 #include <yt/client/table_client/unversioned_row.h>
 
@@ -19,8 +22,11 @@ using namespace NConcurrency;
 ////////////////////////////////////////////////////////////////////////////////
 
 void GetQueryInfo(
+    NApi::NNative::IConnectionPtr connection,
     const TConstQueryPtr query,
-    TFluentAny& fluent)
+    const TDataRanges& dataSource,
+    TFluentAny& fluent,
+    bool isSubquery)
 {
     const auto& keyColumns = query->GetKeyColumns();
     const auto& expression = query->WhereClause;
@@ -80,6 +86,41 @@ void GetQueryInfo(
                         .Item().Value(ToString(range.second))
                     .EndList();
                 });
+
+            if (!isSubquery) {
+                TQueryOptions options;
+                auto Logger = MakeQueryLogger(query);
+                auto rowBuffer = New<TRowBuffer>(TQueryExecutorRowBufferTag{});
+
+                auto allSplits = InferRanges(connection, query, dataSource, options, rowBuffer, Logger);
+
+                THashMap<TString, std::vector<TDataRanges>> groupsByAddress;
+                for (const auto& split : allSplits) {
+                    const auto& address = split.second;
+                    groupsByAddress[address].push_back(split.first);
+                }
+
+                std::vector<std::pair<std::vector<TDataRanges>, TString>> groupedSplits;
+                for (const auto& group : groupsByAddress) {
+                    groupedSplits.emplace_back(group.second, group.first);
+                }
+
+                fluent.Item("grouped_ranges")
+                    .DoMapFor(groupedSplits, [&] (auto fluent, auto rangesByNode) {
+                        fluent.Item(rangesByNode.second)
+                        .DoListFor(rangesByNode.first, [&] (auto fluent, auto ranges) {
+                            fluent.Item()
+                            .BeginList()
+                            .DoFor(ranges.Ranges, [&] (auto fluent, auto range) {
+                                fluent.Item()
+                                .BeginList()
+                                    .Item().Value(ToString(range.first))
+                                    .Item().Value(ToString(range.second))
+                                .EndList();
+                            }).EndList();
+                        });
+                    });
+            }
         })
         .EndMap();
 }
@@ -98,18 +139,20 @@ void GetFrontQueryInfo(
 }
 
 NYson::TYsonString BuildExplainQueryYson(
+    NApi::NNative::IConnectionPtr connection,
     const TString& queryString,
     const std::unique_ptr<TPlanFragment>& fragment,
     TStringBuf udfRegistryPath)
 {
     const auto& query = fragment->Query;
+    const TDataRanges& dataSource = fragment->Ranges;
 
     return BuildYsonStringFluently()
         .BeginMap()
         .Item("udf_registry_path").Value(udfRegistryPath)
         .Item("query")
         .Do([&] (auto fluent) {
-            GetQueryInfo(query, fluent);
+            GetQueryInfo(connection, query, dataSource, fluent, false);
         })
         .Do([&] (TFluentMap fluent) {
             auto refiner = [] (TConstExpressionPtr expr, const TKeyColumns& keyColumns) {
@@ -123,7 +166,7 @@ NYson::TYsonString BuildExplainQueryYson(
                     .DoFor(coordinatedQuery.second, [&] (auto fluent, const auto& subquery) {
                         fluent.Item(ToString(subquery->Id))
                             .Do([&] (auto fluent) {
-                                GetQueryInfo(subquery, fluent);
+                                GetQueryInfo(connection, subquery, dataSource, fluent, true);
                             });
                     })
                 .EndMap();
--- yt/yt/ytlib/query_client/explain.h
+++ yt/yt/ytlib/query_client/explain.h
@@ -1,5 +1,7 @@
 #pragma once
 
+#include "executor.h"
+
 #include "public.h"
 
 #include "query_preparer.h"
@@ -11,6 +13,7 @@ namespace NYT::NQueryClient {
 ////////////////////////////////////////////////////////////////////////////////
 
 NYson::TYsonString BuildExplainQueryYson(
+    NApi::NNative::IConnectionPtr connection,
     const TString& queryString,
     const std::unique_ptr<TPlanFragment>& fragment,
     TStringBuf udfRegistryPath);
