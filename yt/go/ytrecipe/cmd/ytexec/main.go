461b1504e5f95ac44abf7320e6e432beecb44b3a
iaz1607 <iaz1607@yandex-team.ru>
2020-08-21 19:50:01 +0300

    fix ytexec

--- yt/go/ytrecipe/cmd/ytexec/main.go
+++ yt/go/ytrecipe/cmd/ytexec/main.go
@@ -1,7 +1,14 @@
 package main
 
-import "a.yandex-team.ru/yt/go/ytrecipe/internal/ytexec"
+import (
+	"a.yandex-team.ru/yt/go/mapreduce"
+	"a.yandex-team.ru/yt/go/ytrecipe/internal/ytexec"
+	"os"
+)
 
 func main() {
+	if mapreduce.InsideJob() {
+		os.Exit(mapreduce.JobMain())
+	}
 	ytexec.Main()
 }
