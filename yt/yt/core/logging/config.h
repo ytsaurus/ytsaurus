3c3b26c687f4208739b7d650fa5681710a25d55e
ignat <ignat@ytsaurus.tech>
2020-08-23 20:53:00 +0300

    Rename ControlMessages to SystemMessages

--- yt/yt/core/logging/config.h
+++ yt/yt/core/logging/config.h
@@ -20,7 +20,7 @@ public:
     bool EnableCompression;
     size_t CompressionLevel;
     THashMap<TString, NYTree::INodePtr> CommonFields;
-    bool EnableControlMessages;
+    bool EnableSystemMessages;
 
     TWriterConfig()
     {
@@ -38,7 +38,8 @@ public:
             .Default(6);
         RegisterParameter("common_fields", CommonFields)
             .Default();
-        RegisterParameter("enable_control_messages", EnableControlMessages)
+        RegisterParameter("enable_system_messages", EnableSystemMessages)
+            .Alias("enable_control_messages")
             .Default(true);
 
         RegisterPostprocessor([&] () {
