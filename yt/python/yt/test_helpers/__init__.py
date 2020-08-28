ca74b6ef8eda25007308e0d63ba07595687079c5
s-v-m <s-v-m@yandex-team.ru>
2020-08-28 13:33:51 +0300

    Add support for running multicell local yt

--- yt/python/yt/test_helpers/__init__.py
+++ yt/python/yt/test_helpers/__init__.py
@@ -4,13 +4,6 @@ except ImportError:
     yatest_common = None
 
 import os
-import time
-import inspect
-
-try:
-    xrange
-except NameError:  # Python 3
-    xrange = range
 
 try:
     from unittest.util import unorderable_list_difference
@@ -60,6 +53,8 @@ except ImportError:
             result[item] = result.get(item, 0) + 1
         return result
 
+from yt.common import wait, WaitFailed
+
 def assert_items_equal(actual_seq, expected_seq):
     # It is simplified version of the same method of unittest.TestCase
     try:
@@ -82,28 +77,6 @@ def assert_items_equal(actual_seq, expected_seq):
 def are_almost_equal(lhs, rhs, decimal_places=4):
     eps = 10**(-decimal_places)
     return abs(lhs - rhs) < eps
-
-class WaitFailed(Exception):
-    pass
-
-def wait(predicate, error_message=None, iter=100, sleep_backoff=0.3, ignore_exceptions=False):
-    for _ in xrange(iter):
-        try:
-            if predicate():
-                return
-        except:
-            if ignore_exceptions:
-                time.sleep(sleep_backoff)
-                continue
-            raise
-        time.sleep(sleep_backoff)
-
-    if inspect.isfunction(error_message):
-        error_message = error_message()
-    if error_message is None:
-        error_message = "Wait failed"
-    error_message += " (timeout = {0})".format(iter * sleep_backoff)
-    raise WaitFailed(error_message)
 
 # TODO(ignat): move it to arcadia_interop.
 def get_tmpfs_path():
