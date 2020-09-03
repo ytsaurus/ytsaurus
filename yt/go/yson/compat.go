e04b77dada9a4c2a63fd2b87d7b7518f72f377ac
verytable <verytable@ytsaurus.tech>
2020-09-03 13:21:15 +0300

    PR from branch users/verytable/idm-fixes Ветка, отведённая от https://a.yandex-team.ru/review/1387000/details после коммита f2587f6c238a8a9c5b2a1730276c32d50a5eb29c. [yt/idm-integration] Add subject to role request/withdraw responses.

---
+++ yt/go/yson/compat.go
@@ -0,0 +1,20 @@
+package yson
+
+// IsTrue is a helper function for compatibility with C++ ConvertTo<bool>().
+//
+// It returns true, if value is bool(true) or string("true").
+//
+// Early versions of YSON was missing separate wire type for bool. Instead, string "true" was used to specify true value.
+// C++ code still accepts string value where bool is expected, for compatibility reasons.
+func IsTrue(value interface{}) bool {
+	switch v := value.(type) {
+	case bool:
+		return v
+	case string:
+		return v == "true"
+	case nil:
+		return false
+	default:
+		return false
+	}
+}
