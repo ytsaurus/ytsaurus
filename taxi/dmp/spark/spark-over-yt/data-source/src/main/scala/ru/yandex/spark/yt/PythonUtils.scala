b1dc9b829c0a4d82ef6a8efcaf5eb1e2f12a55ac
Alexandra Belousova <sashbel@yandex-team.ru>
2020-09-03 16:31:51 +0300

    Add method to convert column from struct to yson binary

--- taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/PythonUtils.scala
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/PythonUtils.scala
@@ -1,8 +1,10 @@
 package ru.yandex.spark.yt
 
-import org.apache.spark.sql.{Column, DataFrame, DataFrameReader}
+import org.apache.spark.sql.{Column, DataFrame, DataFrameReader, Row}
 import org.apache.spark.sql.types.StructType
 import ru.yandex.spark.yt.fs.GlobalTableSettings
+import org.apache.spark.sql.functions._
+import ru.yandex.inside.yt.kosher.impl.ytree.serialization.YsonEncoder
 
 object PythonUtils {
   def setPathFilesCount(path: String, filesCount: Int): Unit = {
@@ -15,5 +17,17 @@ object PythonUtils {
 
   def withYsonColumn(dataFrame: DataFrame, name: String, column: Column): DataFrame = {
     dataFrame.withYsonColumn(name, column)
+  }
+
+  def serializeColumnToYson(dataFrame: DataFrame, oldName: String, newName: String, skipNulls: Boolean): DataFrame = {
+    val dataType = dataFrame.schema.fields(dataFrame.schema.fieldIndex(oldName)).dataType
+    val broadcastDataType = dataFrame.sparkSession.sparkContext.broadcast(dataType)
+    val columnToYsonUdf = dataType match {
+      case _: StructType =>
+        udf((row: Row) =>
+          YsonEncoder.encode(row, broadcastDataType.value, skipNulls)
+        )
+    }
+    dataFrame.withColumn(newName, columnToYsonUdf(col(oldName)))
   }
 }
