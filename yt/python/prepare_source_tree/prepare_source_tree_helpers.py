310a892f0e43d3ef2bf42ada45f08c87a7a3e141
ignat <ignat@ytsaurus.tech>
2020-09-10 20:35:47 +0300

    Do not ignore preparation errors in new system python tests

--- yt/python/prepare_source_tree/prepare_source_tree_helpers.py
+++ yt/python/prepare_source_tree/prepare_source_tree_helpers.py
@@ -10,6 +10,23 @@ logger = logging.getLogger("prepare_source_tree")
 logger.addHandler(logging.StreamHandler())
 logger.setLevel(logging.INFO)
 
+def chown_r(path, uid, gid):
+    logger.info("Chown %s with uid %d and gid %d", path, uid, gid)
+    for root, dirs, files in os.walk(path):
+        for d in dirs:
+            os.chown(os.path.join(root, d), uid, gid)
+        for f in files:
+            if os.path.islink(os.path.join(root, f)):
+                continue
+            os.chown(os.path.join(root, f), uid, gid)
+
+def chmod_r(path, permissions):
+    logger.info("Chmod %s with permissions %o", path, permissions)
+    for root, dirs, files in os.walk(path):
+        for f in files:
+            if os.path.islink(os.path.join(root, f)):
+                continue
+            os.chmod(os.path.join(root, f), permissions)
 
 def rm_rf(path):
     """remove recursive"""
@@ -26,8 +43,12 @@ def cp_r(path, dest_dir):
     assert os.path.isdir(dest_dir)
     if os.path.isdir(path):
         shutil.copytree(path, os.path.join(dest_dir, os.path.basename(path)))
+        # We explicitely chmod subtree to overcome readonly files problem in distbuild.
+        chmod_r(os.path.join(dest_dir, os.path.basename(path)), 0o755)
     else:
         shutil.copy2(path, dest_dir)
+        # We explicitely chmod subtree to overcome readonly files problem in distbuild.
+        os.chmod(os.path.join(dest_dir, os.path.basename(path)), 0o755)
 
 
 def replace(path, dest_dir):
