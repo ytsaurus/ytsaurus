e6d970abc9a5311b8e00be5d6d79db090bab848f
ermolovd <ermolovd@ytsaurus.tech>
2020-09-17 10:18:17 +0300

    Keep old behaviour

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/BackwardCompatibility.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/BackwardCompatibility.java
@@ -1,14 +1,10 @@
 package ru.yandex.yt.ytclient.proxy;
 
-import java.util.ArrayList;
 import java.util.List;
-import java.util.concurrent.CompletableFuture;
-import java.util.concurrent.ExecutionException;
 
 import ru.yandex.lang.NonNullApi;
 import ru.yandex.lang.NonNullFields;
 import ru.yandex.yt.ytclient.rpc.RpcClient;
-import ru.yandex.yt.ytclient.rpc.RpcClientPool;
 
 /**
  * Miscellaneous stuff to help with backward compatibility.
@@ -25,27 +21,6 @@ public class BackwardCompatibility {
      * Get list of rpcclients that can be used to send messages to YtClient.
      */
     static public List<RpcClient> selectDestinations(YtClient client) {
-        final int RESULT_SIZE = 3;
-        final CompletableFuture<Void> releaseFuture = new CompletableFuture<>();
-
-        RpcClientPool clientPool = client.getClientPool();
-        List<RpcClient> result = new ArrayList<>();
-        try {
-            for (int i = 0; i < RESULT_SIZE; ++i) {
-                CompletableFuture<RpcClient> clientFuture = clientPool.peekClient(releaseFuture);
-                try {
-                    result.add(clientFuture.get());
-                } catch (ExecutionException | InterruptedException error) {
-                    if (result.isEmpty()) {
-                        throw new RuntimeException(error);
-                    } else {
-                        break;
-                    }
-                }
-            }
-        } finally {
-            releaseFuture.complete(null);
-        }
-        return result;
+        return client.selectDestinations();
     }
 }
