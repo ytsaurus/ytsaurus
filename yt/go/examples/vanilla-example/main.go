be1214ffc698e32970f6e3b89f0cc2f107efd94e
verytable <verytable@ytsaurus.tech>
2020-09-19 12:47:16 +0300

    Add vanilla operation usage example. Добавлены примеры использования mr.Vanilla, yc.ListJobs, yc.GetJobStderr.

---
+++ yt/go/examples/vanilla-example/main.go
@@ -0,0 +1,91 @@
+package main
+
+import (
+	"context"
+	"fmt"
+	"os"
+	"os/exec"
+	"time"
+
+	"a.yandex-team.ru/library/go/core/xerrors"
+	"a.yandex-team.ru/yt/go/mapreduce"
+	"a.yandex-team.ru/yt/go/mapreduce/spec"
+	"a.yandex-team.ru/yt/go/yt"
+	"a.yandex-team.ru/yt/go/yt/ythttp"
+)
+
+const cluster = "freud"
+
+type EchoJob struct {
+	mapreduce.Untyped
+}
+
+func (c *EchoJob) Do(ctx mapreduce.JobContext, in mapreduce.Reader, out []mapreduce.Writer) error {
+	_, _ = fmt.Fprint(os.Stderr, "- Wait, is it all Vanilla?\n")
+
+	cmd := exec.Command("/bin/echo", "- Always has been.")
+	cmd.Stderr = os.Stderr
+	cmd.Stdout = os.Stderr
+
+	return cmd.Run()
+}
+
+func init() {
+	mapreduce.Register(&EchoJob{})
+}
+
+func Example() error {
+	yc, err := ythttp.NewClientCli(cluster)
+	if err != nil {
+		return err
+	}
+
+	jobs := map[string]mapreduce.Job{"job": &EchoJob{}}
+	s := spec.Vanilla().AddVanillaTask("job", 1)
+	s.MaxFailedJobCount = 1
+
+	mr := mapreduce.New(yc)
+	op, err := mr.Vanilla(s, jobs)
+	if err != nil {
+		return err
+	}
+
+	fmt.Printf("Operation: %s\n", yt.WebUIOperationURL(cluster, op.ID()))
+	if err := op.Wait(); err != nil {
+		return err
+	}
+
+	return dumpStderr(yc, op.ID())
+}
+
+// dumpStderr downloads and prints stderr of all operation jobs.
+func dumpStderr(yc yt.Client, id yt.OperationID) error {
+	ctx, cancel := context.WithTimeout(context.Background(), time.Second*10)
+	defer cancel()
+
+	result, err := yc.ListJobs(ctx, id, nil)
+	if err != nil {
+		return xerrors.Errorf("unable to list operation jobs: %w", err)
+	}
+
+	for _, job := range result.Jobs {
+		data, err := yc.GetJobStderr(ctx, id, job.ID, nil)
+		if err != nil {
+			return xerrors.Errorf("unable to get stderr of job %s: %w", job.ID, err)
+		}
+		fmt.Printf("\nstderr of job %s:\n%s", job.ID, data)
+	}
+
+	return nil
+}
+
+func main() {
+	if mapreduce.InsideJob() {
+		os.Exit(mapreduce.JobMain())
+	}
+
+	if err := Example(); err != nil {
+		_, _ = fmt.Fprintf(os.Stderr, "error: %+v\n", err)
+		os.Exit(1)
+	}
+}
