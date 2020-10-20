e5e46909982655e3dbd3ec682d87bdf84c66d11f
ignat <ignat@ytsaurus.tech>
2020-10-20 11:37:19 +0300

    Make auto-generated version of YtClient

---
+++ yt/python/yt/wrapper/bin/generate_client_impl/__main__.py
@@ -0,0 +1,131 @@
+from __future__ import print_function
+
+from yt.packages.six import PY3
+
+import os
+import argparse
+import inspect
+from copy import deepcopy
+
+
+def main():
+    os.environ["YT_OLD_STYLE_CLIENT"] = "1"
+
+    from yt.wrapper.cypress_commands import _KwargSentinelClass, _MapOrderSorted
+    from yt.wrapper import client_api
+
+    def dump_value(value):
+        if isinstance(value, str):
+            return repr(value)
+        elif isinstance(value, _KwargSentinelClass):
+            return "_KwargSentinelClass()"
+        elif isinstance(value, _MapOrderSorted):
+            return "_MapOrderSorted()"
+        else:
+            return str(value)
+
+    parser = argparse.ArgumentParser(description="Generate client_impl")
+    parser.add_argument("output")
+    cli_args = parser.parse_args()
+
+    with open(cli_args.output, "w") as fout:
+        fout.write('''# This file is auto-generate by yt/python/yt/wrapper/bin/generate_client_impl, please do not edit it manually!
+
+from .cypress_commands import _KwargSentinelClass, _MapOrderSorted
+from .client_helpers import initialize_client
+from .client_state import ClientState
+from . import client_api
+
+class YtClient(ClientState):
+    """Implements YT client."""
+
+    def __init__(self, proxy=None, token=None, config=None):
+        super(YtClient, self).__init__()
+        initialize_client(self, proxy, token, config)
+''')
+        for name in client_api.all_names:
+            func = getattr(client_api, name)
+            doc = func.__doc__
+
+            is_class = False
+            if inspect.isclass(func):
+                func = func.__dict__["__init__"]
+                is_class = True
+
+            if PY3:
+                arg_spec = inspect.getfullargspec(func)
+                var_args = arg_spec.varargs
+                var_kwargs = arg_spec.varkw
+                defaults = arg_spec.defaults
+            else:
+                arg_spec = inspect.getargspec(func)
+                var_args = arg_spec.varargs
+                var_kwargs = arg_spec.keywords
+                defaults = arg_spec.defaults
+
+            if defaults:
+                defaults = list(defaults)
+            else:
+                defaults = []
+
+            all_args = deepcopy(arg_spec.args)
+            if all_args and all_args[-1] == "client":
+                all_args.pop()
+                if defaults:
+                    defaults.pop()
+
+            if defaults:
+                args = all_args[:-len(defaults)]
+                kwargs_names = all_args[-len(defaults):]
+            else:
+                args = all_args
+                kwargs_names = []
+
+            if is_class:
+                args.pop(0)
+
+            if var_args:
+                args.append("*" + var_args)
+
+            kwargs = ["{}={}".format(key, dump_value(value)) for key, value in zip(kwargs_names, defaults)]
+            kwargs_passed = ["{}={}".format(key, key) for key in kwargs_names]
+            if var_kwargs:
+                kwargs.append("**" + var_kwargs)
+                kwargs_passed.append("**" + var_kwargs)
+
+            if args and kwargs:
+                fout.write('''
+    def {name}(self, {args}, {kwargs}):
+        \"\"\"
+        {doc}
+        \"\"\"
+        return client_api.{name}({args}, client=self, {kwargs_passed})
+'''.format(name=name, args=", ".join(args), kwargs=", ".join(kwargs), kwargs_passed=", ".join(kwargs_passed), doc=doc))
+            elif args:
+                fout.write('''
+    def {name}(self, {args}):
+        \"\"\"
+        {doc}
+        \"\"\"
+        return client_api.{name}({args}, client=self)
+'''.format(name=name, args=", ".join(args), doc=doc))
+            elif kwargs:
+                fout.write('''
+    def {name}(self, {kwargs}):
+        \"\"\"
+        {doc}
+        \"\"\"
+        return client_api.{name}(client=self, {kwargs_passed})
+'''.format(name=name, kwargs=", ".join(kwargs), kwargs_passed=", ".join(kwargs_passed), doc=doc))
+            else:
+                fout.write('''
+    def {name}(self):
+        \"\"\"
+        {doc}
+        \"\"\"
+        return client_api.{name}(client=self)
+'''.format(name=name, doc=doc))
+
+
+if __name__ == "__main__":
+    main()
--- yt/python/yt/wrapper/client.py
+++ yt/python/yt/wrapper/client.py
@@ -1,27 +1,40 @@
 from .config import get_config, set_option
 from .client_state import ClientState
-from .client_helpers import create_class_method, initialize_client
+from .client_helpers import create_class_method, initialize_client, are_signatures_equal
 from . import client_api
 
-
+import os
 from copy import deepcopy
 
 
-class YtClient(ClientState):
-    """Implements YT client."""
+old_style_client = os.environ.get("YT_OLD_STYLE_CLIENT")
 
-    def __init__(self, proxy=None, token=None, config=None):
-        super(YtClient, self).__init__()
-        initialize_client(self, proxy, token, config)
+if not old_style_client:
+    try:
+        from .client_impl import YtClient
+
+        for name in client_api.all_names:
+            assert are_signatures_equal(getattr(YtClient, name), create_class_method(getattr(client_api, name)))
+    except ImportError as err:
+        old_style_client = True
+
+if old_style_client:
+    class YtClient(ClientState):
+        """Implements YT client."""
+
+        def __init__(self, proxy=None, token=None, config=None):
+            super(YtClient, self).__init__()
+            initialize_client(self, proxy, token, config)
+
+    for name in client_api.all_names:
+        setattr(YtClient, name, create_class_method(getattr(client_api, name)))
+
 
 def create_client_with_command_params(client=None, **kwargs):
     """Creates new client with command params."""
     new_client = YtClient(config=deepcopy(get_config(client)))
     set_option("COMMAND_PARAMS", kwargs, new_client)
     return new_client
-
-for name in client_api.all_names:
-    setattr(YtClient, name, create_class_method(getattr(client_api, name)))
 
 # Backward compatibility.
 Yt = YtClient
