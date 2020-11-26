76604e7628caf51c09f27a7f512872fdff0ac07d
ermolovd <ermolovd@ytsaurus.tech>
2020-11-26 14:36:45 +0300

    YT-13716: introduce CommitTransaction and AbortTransaction, log their arguments

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/AbortTransaction.java
@@ -0,0 +1,40 @@
+package ru.yandex.yt.ytclient.proxy.request;
+
+import javax.annotation.Nonnull;
+
+import ru.yandex.inside.yt.kosher.common.GUID;
+import ru.yandex.yt.rpcproxy.TReqAbortTransaction;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
+import ru.yandex.yt.ytclient.rpc.RpcUtil;
+
+/**
+ * Request for aborting transaction.
+ *
+ * @see <a href="https://docs.yandex-team.ru/yt/api/commands#abort_tx">
+ *     abort_tx documentation
+ *     </a>
+ */
+public class AbortTransaction extends RequestBase<AbortTransaction> implements HighLevelRequest<TReqAbortTransaction.Builder> {
+    final GUID transactionId;
+
+    public AbortTransaction(GUID transactionId) {
+        this.transactionId = transactionId;
+    }
+
+    @Override
+    public void writeTo(RpcClientRequestBuilder<TReqAbortTransaction.Builder, ?> builder) {
+        builder.body().setTransactionId(RpcUtil.toProto(transactionId));
+    }
+
+    @Nonnull
+    @Override
+    protected AbortTransaction self() {
+        return this;
+    }
+
+    @Override
+    protected void writeArgumentsLogString(@Nonnull StringBuilder sb) {
+        super.writeArgumentsLogString(sb);
+        sb.append("TransactionId: ").append(transactionId).append(";");
+    }
+}
