9d398a80cbd34b7cf4cdc85ec662156097fd2450
ignat <ignat@ytsaurus.tech>
2020-12-01 17:26:00 +0300

    Support enable option for event log

--- yt/yt/ytlib/event_log/config.cpp
+++ yt/yt/ytlib/event_log/config.cpp
@@ -6,6 +6,8 @@ namespace NYT::NEventLog {
 
 TEventLogManagerConfig::TEventLogManagerConfig()
 {
+    RegisterParameter("enable", Enable)
+        .Default(true);
     RegisterParameter("path", Path)
         .Default();
     RegisterParameter("pending_rows_flush_period", PendingRowsFlushPeriod)
