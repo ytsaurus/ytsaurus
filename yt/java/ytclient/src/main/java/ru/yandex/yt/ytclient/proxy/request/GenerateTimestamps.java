1ff549b9d0b545f0c8e14566cf7fdae49abf3c3c
ermolovd <ermolovd@ytsaurus.tech>
2020-12-02 17:53:24 +0300

    YT-13716: log GetTabletInfos, GenerateTimestamps

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/GenerateTimestamps.java
@@ -0,0 +1,37 @@
+package ru.yandex.yt.ytclient.proxy.request;
+
+import javax.annotation.Nonnull;
+
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
+import ru.yandex.yt.rpcproxy.TReqGenerateTimestamps;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
+
+@NonNullFields
+@NonNullApi
+public class GenerateTimestamps
+        extends RequestBase<GenerateTimestamps>
+        implements HighLevelRequest<TReqGenerateTimestamps.Builder>
+{
+    private final int count;
+
+    public GenerateTimestamps(int count) {
+        this.count = count;
+    }
+
+    @Override
+    public void writeTo(RpcClientRequestBuilder<TReqGenerateTimestamps.Builder, ?> builder) {
+        builder.body().setCount(count);
+    }
+
+    @Override
+    protected void writeArgumentsLogString(@Nonnull StringBuilder sb) {
+        sb.append("Count: ").append(count).append("; ");
+        super.writeArgumentsLogString(sb);
+    }
+
+    @Override
+    protected GenerateTimestamps self() {
+        return this;
+    }
+}
---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/GetTabletInfos.java
@@ -0,0 +1,51 @@
+package ru.yandex.yt.ytclient.proxy.request;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import javax.annotation.Nonnull;
+
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
+import ru.yandex.yt.rpcproxy.TReqGetTabletInfos;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
+
+@NonNullFields
+@NonNullApi
+public class GetTabletInfos extends RequestBase<GetTabletInfos> implements HighLevelRequest<TReqGetTabletInfos.Builder> {
+    private final String path;
+    private final List<Integer> tabletIndexes = new ArrayList<>();
+
+    public GetTabletInfos(String path) {
+        this.path = path;
+    }
+
+    public GetTabletInfos addTabletIndex(int idx) {
+        tabletIndexes.add(idx);
+        return this;
+    }
+
+    public GetTabletInfos setTabletIndexes(List<Integer> tabletIndexes) {
+        this.tabletIndexes.clear();
+        this.tabletIndexes.addAll(tabletIndexes);
+        return this;
+    }
+
+    @Override
+    public void writeTo(RpcClientRequestBuilder<TReqGetTabletInfos.Builder, ?> builder) {
+        builder.body().setPath(path);
+        builder.body().addAllTabletIndexes(tabletIndexes);
+    }
+
+    @Override
+    protected void writeArgumentsLogString(StringBuilder sb) {
+        sb.append("Path: ").append(path).append("; TabletIndexes: ").append(tabletIndexes).append("; ");
+        super.writeArgumentsLogString(sb);
+    }
+
+    @Nonnull
+    @Override
+    protected GetTabletInfos self() {
+        return this;
+    }
+}
