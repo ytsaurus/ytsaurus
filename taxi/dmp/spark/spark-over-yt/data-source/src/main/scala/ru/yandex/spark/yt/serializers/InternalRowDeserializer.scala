25975660910ad0e9b1d14fba79f0b0dda510be04
Alexandra Belousova <sashbel@yandex-team.ru>
2020-12-09 17:02:48 +0300

    Fix YsonType reading for atomic types

--- taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/serializers/InternalRowDeserializer.scala
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/serializers/InternalRowDeserializer.scala
@@ -1,11 +1,15 @@
 package ru.yandex.spark.yt.serializers
 
+import java.io.ByteArrayOutputStream
+
 import org.apache.spark.sql.catalyst.InternalRow
 import org.apache.spark.sql.catalyst.expressions.GenericInternalRow
 import org.apache.spark.sql.sources.Filter
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.yson.{YsonBinary, YsonType}
+import org.apache.spark.sql.yson.YsonType
 import org.apache.spark.unsafe.types.UTF8String
+import ru.yandex.inside.yt.kosher.impl.ytree.builder.YTreeBuilder
+import ru.yandex.inside.yt.kosher.impl.ytree.serialization.YTreeBinarySerializer
 import ru.yandex.inside.yt.kosher.impl.ytree.serialization.spark.YsonDecoder
 import ru.yandex.yt.ytclient.`object`.{WireRowDeserializer, WireValueDeserializer}
 import ru.yandex.yt.ytclient.tables.ColumnValueType
@@ -56,13 +60,28 @@ class InternalRowDeserializer(schema: StructType) extends WireRowDeserializer[In
         case IntegerType => addValue(value.toInt)
         case ShortType => addValue(value.toShort)
         case ByteType => addValue(value.toByte)
+        case YsonType => addValue(toYsonBytes(value))
       }
     }
   }
 
-  override def onBoolean(value: Boolean): Unit = addValue(value)
+  override def onBoolean(value: Boolean): Unit = {
+    if (_index < _values.length) {
+      indexedSchema(_index) match {
+        case BooleanType => addValue(value)
+        case YsonType => addValue(toYsonBytes(value))
+      }
+    }
+  }
 
-  override def onDouble(value: Double): Unit = addValue(value)
+  override def onDouble(value: Double): Unit = {
+    if (_index < _values.length) {
+      indexedSchema(_index) match {
+        case DoubleType => addValue(value)
+        case YsonType => addValue(toYsonBytes(value))
+      }
+    }
+  }
 
   override def onBytes(bytes: Array[Byte]): Unit = {
     indexedSchema(_index) match {
@@ -72,6 +91,13 @@ class InternalRowDeserializer(schema: StructType) extends WireRowDeserializer[In
       case _ @ (ArrayType(_, _) | StructType(_) | MapType(_, _, _)) =>
         addValue(YsonDecoder.decode(bytes, indexedDataTypes(_index)))
     }
+  }
+
+  def toYsonBytes(value: Any): Array[Byte] = {
+    val output = new ByteArrayOutputStream(64)
+    val node = new YTreeBuilder().value(value).build()
+    YTreeBinarySerializer.serialize(node, output)
+    output.toByteArray
   }
 }
 
