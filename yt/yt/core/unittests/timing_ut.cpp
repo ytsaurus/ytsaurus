f35888b1f4251e4b481db6f06f78f8daa945c3b0
babenko <babenko@ytsaurus.tech>
2020-12-13 13:27:10 +0300

    Refactor timing routines 1) Micro-optimize CPU time to wall time conversions 2) Resolve YT-12693 ``` Running /home/babenko/arc/build/yt/yt/core/benchmarks/benchmarks Run on (56 X 3200 MHz CPU s) CPU Caches: L1 Data 32 KiB (x28) L1 Instruction 32 KiB (x28) L2 Unified 256 KiB (x28) L3 Unified 35840 KiB (x2) Load Average: 2.62, 3.03, 2.86 ***WARNING*** CPU scaling is enabled, the benchmark real time measurements may be noisy and will incur extra overhead. ----------------------------------------------------------- Benchmark Time CPU Iterations ----------------------------------------------------------- BM_GetCpuInstant 8.93 ns 8.92 ns 58106755 BM_GetInstant 9.44 ns 9.44 ns 71285184 BM_InstantNow 19.8 ns 19.8 ns 35421075 ```

---
+++ yt/yt/core/unittests/timing_ut.cpp
@@ -0,0 +1,44 @@
+#include <yt/core/test_framework/framework.h>
+
+#include <yt/core/profiling/timing.h>
+
+namespace NYT::NProfiling {
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class T>
+i64 DiffMS(T a, T b)
+{
+    return a >= b
+        ? static_cast<i64>(a.MilliSeconds()) - static_cast<i64>(b.MilliSeconds())
+        : DiffMS(b ,a);
+}
+
+TEST(TTimingTest, GetInstant)
+{
+    EXPECT_LE(DiffMS(GetInstant(), TInstant::Now()), 10);
+}
+
+TEST(TTimingTest, InstantVSCpuInstant)
+{
+    auto instant1 = TInstant::Now();
+    auto cpuInstant = InstantToCpuInstant(instant1);
+    auto instant2 = CpuInstantToInstant(cpuInstant);
+    EXPECT_LE(DiffMS(instant1, instant2), 10);
+}
+
+TEST(TTimingTest, DurationVSCpuDuration)
+{
+    auto cpuInstant1 = GetCpuInstant();
+    constexpr auto duration1 = TDuration::MilliSeconds(100);
+    Sleep(duration1);
+    auto cpuInstant2 = GetCpuInstant();
+    auto duration2 = CpuDurationToDuration(cpuInstant2 - cpuInstant1);
+    EXPECT_LE(DiffMS(duration1, duration2), 10);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT::NProfiling
