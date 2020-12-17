0c666a4ecd2153608b862a8cad1a014361f3f29f
lukyan <lukyan@ytsaurus.tech>
2020-12-18 01:35:39 +0300

    Enable test

--- yt/yt/tests/integration/tests/test_sorted_dynamic_tables.py
+++ yt/yt/tests/integration/tests/test_sorted_dynamic_tables.py
@@ -386,7 +386,6 @@ class TestSortedDynamicTables(TestSortedDynamicTablesBase):
         assert get(path) == 1
 
     @authors("lukyan")
-    @pytest.mark.skip(reason="Currently broken")
     def test_lookup_cache_stress(self):
         sync_create_cells(1)
 
