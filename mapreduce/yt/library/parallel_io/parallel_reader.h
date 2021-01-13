10ce4840461d68aaff457c6b848aaa6b72394aae
levysotsky <levysotsky@yandex-team.ru>
2021-01-13 10:58:53 +0300

    YT-14145: Postprocessing in parallel reader

--- mapreduce/yt/library/parallel_io/parallel_reader.h
+++ mapreduce/yt/library/parallel_io/parallel_reader.h
@@ -1,5 +1,10 @@
 #pragma once
 
+///
+/// @file mapreduce/yt/library/parallel_io/parallel_reader.h
+///
+/// Functions to read tables in parallel.
+
 #include <mapreduce/yt/interface/client.h>
 #include <mapreduce/yt/interface/io.h>
 
@@ -7,59 +12,98 @@ namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/// @brief Options for parallel reading.
 struct TParallelTableReaderOptions
     : public TIOOptions<TParallelTableReaderOptions>
 {
-    // Ordered == true: produce rows in the same order as the usual reader.
-    // Ordered == false: allow to produce rows in any order.
-    //
-    // NOTE: Unordered mode can be much faster
-    // due to ability to read from different chunks and thus from different nodes.
+    /// @brief Whether table rows are returned in order.
+    ///
+    /// `Ordered == true`: produce rows in the same order as the usual reader.
+    /// `Ordered == false`: allow to produce rows in any order.
+    ///
+    /// @note Unordered mode can be much faster due to ability to read from different chunks and thus from different nodes.
     FLUENT_FIELD_DEFAULT(bool, Ordered, true);
 
-    // Maximum number of rows allowed to be stored in memory
-    // is calculated as Min(BufferedRowCountLimit, MemoryLimit / "Average weight row in table")
+    /// @brief Memory limit allowed to be used by reader.
+    ///
+    /// Maximum number of rows allowed to be stored in memory
+    /// is calculated as `Min(BufferedRowCountLimit, MemoryLimit / AverageRowWeight)`.
     FLUENT_FIELD_DEFAULT(size_t, MemoryLimit, 300'000'000);
 
-    // Number of created reader threads.
-    //
-    // NOTE: Actual number of running threads depends on the underlying readers,
-    // e.g. each YSON TNode reader spawns additional parser thread,
-    // so total number of threads will be doubled.
+    /// @brief Number of created reader threads.
+    ///
+    /// @brief Actual number of running threads depends on the underlying readers,
+    /// e.g. each YSON TNode reader spawns additional parser thread,
+    /// so total number of threads will be doubled.
     FLUENT_FIELD_DEFAULT(size_t, ThreadCount, 10);
 
-    // Allows to fine tune format that is used for reading tables.
+    /// @brief Allows to fine tune format that is used for reading tables.
     FLUENT_FIELD_OPTION(TFormatHints, FormatHints);
 
-    // Size of batch to operate with (in bytes).
+    /// @brief Size of batch to operate with (in bytes).
     FLUENT_FIELD_DEFAULT(size_t, BatchSizeBytes, 4'000'000);
 
-    // Number of RichYPath ranges each reader will use.
-    // Makes sense only for ordered reader.
+    /// @brief Number of RichYPath ranges each reader will use.
+    ///
+    /// @note Makes sense only for ordered reader.
     FLUENT_FIELD_DEFAULT(int, RangeCount, 300);
 
-    // Maximum number of rows allowed to be stored in memory.
-    // Together with ThreadCount it determines the read range size
-    // and thus the number of requests that will be issued.
-    //
-    // NOTE: Actual memory consumption depends on the underlying readers,
-    // e.g. each YSON TNode reader allocates additional buffer for parsed rows.
-    //
-    // This field is deprecated, use `MemoryLimit` instead
+    /// @brief Maximum number of rows allowed to be stored in memory.
+    ///
+    /// @deprecated
+    ///
+    /// Together with ThreadCount it determines the read range size
+    /// and thus the number of requests that will be issued.
+    ///
+    /// @note Actual memory consumption depends on the underlying readers,
+    /// e.g. each YSON TNode reader allocates additional buffer for parsed rows.
+    ///
+    /// This field is deprecated, use `MemoryLimit` instead.
     FLUENT_FIELD_DEFAULT(size_t, BufferedRowCountLimit, 1'000'000'000);
 };
 
-template <typename T>
-TTableReaderPtr<T> CreateParallelTableReader(
+/// @brief Create parallel reader of several tables.
+template <typename TRow>
+TTableReaderPtr<TRow> CreateParallelTableReader(
     const IClientBasePtr& client,
     const TVector<TRichYPath>& paths,
-    const TParallelTableReaderOptions& options = TParallelTableReaderOptions());
+    const TParallelTableReaderOptions& options = TParallelTableReaderOptions{});
 
-template <typename T>
-TTableReaderPtr<T> CreateParallelTableReader(
+/// @brief Create parallel reader of a table.
+template <typename TRow>
+TTableReaderPtr<TRow> CreateParallelTableReader(
     const IClientBasePtr& client,
     const TRichYPath& path,
-    const TParallelTableReaderOptions& options = TParallelTableReaderOptions());
+    const TParallelTableReaderOptions& options = TParallelTableReaderOptions{});
+
+/// @brief Callback to be called (from several threads) for each row batch.
+template <typename TRow>
+using TParallelReaderRowProcessor = std::function<void(TTableReaderPtr<TRow> reader, int tableIndex)>;
+
+/// @brief Read several tables in parallel and apply `processor` to the rows.
+///
+/// @param processor Function taking a reader and table index of a batch of rows.
+///
+/// @note `options.Ordered` must be false. For ordered processing use @ref NYT::CreateParallelTableReader.
+/// @note `processor` is called from several threads and thus must be thread-safe.
+///
+/// Any exception thrown inside `processor` will be propagated to the caller.
+template <typename TRow>
+void ReadTablesInParallel(
+    const IClientBasePtr& client,
+    const TVector<TRichYPath>& paths,
+    TParallelReaderRowProcessor<TRow> processor,
+    const TParallelTableReaderOptions& options = TParallelTableReaderOptions{});
+
+/// @brief Read a table in parallel and apply `processor` to the rows.
+///
+/// @see @ref NYT::ReadTablesInParallel.
+template <typename TRow>
+void ReadTableInParallel(
+    const IClientBasePtr& client,
+    const TRichYPath& path,
+    TParallelReaderRowProcessor<TRow> processor,
+    const TParallelTableReaderOptions& options = TParallelTableReaderOptions{});
 
 ////////////////////////////////////////////////////////////////////////////////
 
