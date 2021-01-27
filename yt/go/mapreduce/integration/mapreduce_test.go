7b8bc46e8c66517677d674e56808ae41f3e94ccf
tserakhau <tserakhau@yandex-team.ru>
2021-01-27 18:44:01 +0300

    YT-XXXX: Do not upload self binary when SkipSelfUpload

--- yt/go/mapreduce/integration/mapreduce_test.go
+++ yt/go/mapreduce/integration/mapreduce_test.go
@@ -5,6 +5,7 @@ import (
 
 	"github.com/stretchr/testify/require"
 
+	"a.yandex-team.ru/yt/go/mapreduce"
 	"a.yandex-team.ru/yt/go/mapreduce/spec"
 	"a.yandex-team.ru/yt/go/yttest"
 )
@@ -36,4 +37,31 @@ func TestOutputTableCreation(t *testing.T) {
 	var output []interface{}
 	require.NoError(t, env.DownloadSlice(outputPath, &output))
 	require.Empty(t, output)
+}
+
+func TestSkipSelfUpload(t *testing.T) {
+	t.Parallel()
+
+	env, cancel := yttest.NewEnv(t)
+	defer cancel()
+
+	inputPath := env.TmpPath()
+	outputPath := env.TmpPath()
+
+	input := []TestRow{
+		{A: 2, B: "bar"},
+	}
+	require.NoError(t, env.UploadSlice(inputPath, input))
+	op, err := env.MR.MapReduce(
+		&MapJob{"test-map"}, &ReduceJob{Field: "test-reduce"},
+		spec.MapReduce().
+			ReduceByColumns("a").
+			SortByColumns("a"). // todo verytable@ remove when https://st.yandex-team.ru/YT-14073 is merged
+			AddInput(inputPath).
+			AddOutput(outputPath).
+			AddSecureVaultVar("TEST", "FOO"),
+		mapreduce.SkipSelfUpload(),
+	)
+	require.NoError(t, err)
+	require.Error(t, op.Wait())
 }
