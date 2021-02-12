2177d049dc828d7fa8a7b33acd7a5c33556a8c5e
levysotsky <levysotsky@yandex-team.ru>
2021-02-12 15:54:04 +0300

    Create yt.wrapper.testlib

---
+++ yt/python/yt/wrapper/testlib/helpers.py
@@ -0,0 +1,259 @@
+from __future__ import print_function
+
+from yt.packages.six import iteritems, integer_types, text_type, binary_type, b, PY3
+from yt.packages.six.moves import xrange
+
+from yt.test_helpers import wait, get_tests_sandbox as get_tests_sandbox_impl
+from yt.test_helpers.job_events import JobEvents
+
+from yt.testlib import (yatest_common, authors, check_rows_equality, set_config_option, set_config_options)  # noqa
+
+import yt.yson as yson
+import yt.subprocess_wrapper as subprocess
+import yt.environment.arcadia_interop as arcadia_interop
+
+from yt.wrapper.errors import YtRetriableError
+import yt.wrapper as yt
+
+import collections
+import glob
+import os
+import random
+import shutil
+import stat
+import string
+import sys
+import tempfile
+import threading
+from contextlib import contextmanager
+from copy import deepcopy
+
+
+TEST_DIR = "//home/wrapper_tests"
+
+def get_tests_location():
+    if yatest_common is None:
+        return os.path.dirname(os.path.abspath(__file__))
+    else:
+        return yatest_common.source_path("yt/python/yt/wrapper/tests")
+
+def get_tests_sandbox():
+    return get_tests_sandbox_impl(
+        os.environ.get("TESTS_SANDBOX", os.path.dirname(os.path.abspath(__file__)) + ".sandbox")
+    )
+
+def get_test_files_dir_path():
+    return os.path.join(get_tests_location(), "files")
+
+def get_test_file_path(name, use_files=True):
+    if yatest_common is not None:
+        import library.python.resource
+        file_path = os.path.join(yatest_common.work_path(), "tmp_files", name)
+        dir_path = os.path.dirname(file_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with open(file_path, "wb") as fout:
+            files_dir = "files/" if use_files else ""
+            resource_path = "/yt_python_test/" + files_dir + name
+            fout.write(library.python.resource.find(resource_path))
+        os.chmod(file_path, 0o744)
+        return file_path
+    else:
+        if use_files:
+            return os.path.join(get_tests_location(), "files", name)
+        else:
+            return os.path.join(get_tests_location(), name)
+
+def get_binary_path(name):
+    if yatest_common is not None:
+        import library.python.resource
+        file_path = os.path.join(yatest_common.work_path(), "tmp_files", name)
+        dir_path = os.path.dirname(file_path)
+        if not os.path.exists(dir_path):
+            os.makedirs(dir_path)
+        with open(file_path, "wb") as fout:
+            resource_path = "/binaries/" + name
+            fout.write(library.python.resource.find(resource_path))
+        os.chmod(file_path, 0o744)
+        return file_path
+    else:
+        return os.path.join(get_tests_location(), "../bin", name)
+
+def get_python():
+    if yatest_common is None:
+        return sys.executable
+    else:
+        if PY3:
+            return arcadia_interop.search_binary_path("yt-python3")
+        else:
+            return arcadia_interop.search_binary_path("yt-python")
+
+def _filter_simple_types(obj):
+    if isinstance(obj, integer_types) or \
+            isinstance(obj, float) or \
+            obj is None or \
+            isinstance(obj, yson.YsonType) or \
+            isinstance(obj, (binary_type, text_type)):
+        return obj
+    elif isinstance(obj, list):
+        return [_filter_simple_types(item) for item in obj]
+    elif isinstance(obj, collections.Mapping):
+        return dict([(key, _filter_simple_types(value)) for key, value in iteritems(obj)])
+    return None
+
+def get_environment_for_binary_test(yt_env, enable_request_logging=True):
+    binaries_dir = os.path.join(os.path.dirname(get_tests_location()), "bin")
+
+    if yatest_common is None:
+        python_binary = sys.executable
+        yt_binary = os.path.join(binaries_dir, "yt")
+        mapreduce_binary = os.path.join(binaries_dir, "mapreduce-yt")
+    else:
+        python_binary = get_python()
+        yt_binary = get_binary_path("yt")
+        mapreduce_binary = get_binary_path("mapreduce-yt")
+
+    env = {
+        "PYTHON_BINARY": python_binary,
+        "YT_USE_TOKEN": "0",
+        "YT_VERSION": yt.config["api_version"],
+        "YT_PRINT_BACKTRACE": "1",
+        "YT_CLI_PATH": yt_binary,
+        "MAPREDUCE_YT_CLI_PATH": mapreduce_binary,
+    }
+    if yatest_common is None:
+        env["PYTHONPATH"] = os.environ["PYTHONPATH"]
+
+    config = deepcopy(_filter_simple_types(yt.config.config))
+
+    if config["backend"] == "native":
+        _, filename = tempfile.mkstemp(dir=get_tests_sandbox(), prefix="binary_test_driver_config")
+        with open(filename, "wb") as f:
+            yson.dump({"driver": config["driver_config"], "logging": yt_env.env.configs["driver_logging"]}, f)
+
+        config["driver_config"] = None
+        config["driver_config_path"] = filename
+
+    config["enable_request_logging"] = enable_request_logging
+
+    env["YT_CONFIG_PATCHES"] = yson._dumps_to_native_str(config)
+    return env
+
+def build_python_egg(egg_contents_dir, temp_dir=None):
+    dir_ = tempfile.mkdtemp(dir=temp_dir)
+
+    for obj in os.listdir(egg_contents_dir):
+        src = os.path.join(egg_contents_dir, obj)
+        dst = os.path.join(dir_, obj)
+        if os.path.isdir(src):
+            shutil.copytree(src, dst)
+        else:  # file
+            shutil.copy2(src, dst)
+
+    _, egg_filename = tempfile.mkstemp(dir=temp_dir, suffix=".egg")
+    try:
+        subprocess.check_call([get_python(), "setup.py", "bdist_egg"], cwd=dir_)
+
+        eggs = glob.glob(os.path.join(dir_, "dist", "*.egg"))
+        assert len(eggs) == 1
+
+        shutil.copy2(eggs[0], egg_filename)
+        return egg_filename
+    finally:
+        shutil.rmtree(dir_, ignore_errors=True)
+
+def dumps_yt_config():
+    config = _filter_simple_types(yt.config.config)
+    return yson._dumps_to_native_str(config)
+
+def run_python_script_with_check(yt_env, script):
+    dir_ = yt_env.env.path
+
+    with tempfile.NamedTemporaryFile(mode="w", dir=dir_, suffix=".py", delete=False) as f:
+        f.write(script)
+        f.close()
+
+        proc = subprocess.Popen([sys.executable, f.name], stdin=subprocess.PIPE, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+
+        out, err = proc.communicate(b(dumps_yt_config()))
+        assert proc.returncode == 0, err
+
+        return out, err
+
+# By default, accounts have empty resource limits upon creation.
+def get_default_resource_limits(client):
+    GB = 1024 ** 3
+    TB = 1024 ** 4
+
+    result = {"node_count": 500000,
+            "chunk_count": 1000000,
+            "master_memory":
+            {
+                "total": 100 * GB, 
+                "chunk_host": 100 * GB,
+                "per_cell": {}
+            }
+    }
+
+    # Backwards compatibility.
+    if client.exists("//sys/media"):
+        result["disk_space_per_medium"] = {"default": 10 * TB}
+    else:
+        result["disk_space"] = 10 * TB
+
+    return result
+
+def sync_create_cell():
+    tablet_id = yt.create("tablet_cell", attributes={"size": 1})
+    wait(lambda : yt.get("//sys/tablet_cells/{0}/@health".format(tablet_id)) == "good")
+
+def wait_record_in_job_archive(operation_id, job_id):
+    operation_id_hash_pair = yt.common.uuid_hash_pair(operation_id)
+    job_id_hash_pair = yt.common.uuid_hash_pair(job_id)
+    # Jobs
+    key = {}
+    key["operation_id_hi"], key["operation_id_lo"] = operation_id_hash_pair.hi, operation_id_hash_pair.lo
+    key["job_id_hi"], key["job_id_lo"] = job_id_hash_pair.hi, job_id_hash_pair.lo
+    wait(lambda: any(yt.lookup_rows("//sys/operations_archive/jobs", [key], column_names=["operation_id_hi"])))
+    # Job specs
+    key = {}
+    key["job_id_hi"], key["job_id_lo"] = job_id_hash_pair.hi, job_id_hash_pair.lo
+    wait(lambda: any(yt.lookup_rows("//sys/operations_archive/job_specs", [key], column_names=["spec_version"])))
+
+def get_operation_path(operation_id):
+    return "//sys/operations/{:02x}/{}".format(int(operation_id.split("-")[-1], 16) % 256, operation_id)
+
+def create_job_events():
+    tmpdir = tempfile.mkdtemp(prefix="job_events", dir=get_tests_sandbox())
+    os.chmod(tmpdir, stat.S_IRWXU | stat.S_IRWXG | stat.S_IRWXO)
+    return JobEvents(tmpdir)
+
+@contextmanager
+def failing_heavy_request(module, n_fails, assert_exhausted=True):
+    make_transactional_request = module._make_transactional_request
+    fail_state = dict(fails_left=n_fails, exhausted=False)
+    lock = threading.Lock()
+
+    def failing_make_transactional_request(*args, **kwargs):
+        with lock:
+            if fail_state["fails_left"] > 0:
+                if "data" in kwargs:
+                    list(kwargs["data"])  # exhaust data generator
+                fail_state["fails_left"] -= 1
+                raise YtRetriableError
+            else:
+                fail_state["exhausted"] = True
+        return make_transactional_request(*args, **kwargs)
+
+    module._make_transactional_request = failing_make_transactional_request
+    try:
+        yield
+    finally:
+        module._make_transactional_request = make_transactional_request
+
+    if assert_exhausted:
+        assert fail_state["exhausted"]
+
+def random_string(length):
+    char_set = string.ascii_lowercase + string.digits + string.ascii_uppercase
+    return "".join(random.choice(char_set) for _ in xrange(length))
