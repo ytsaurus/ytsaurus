e20ac12b12bb2a27308e5ccdd85bedf9b951befa
ignat <ignat@ytsaurus.tech>
2021-02-14 18:14:53 +0300

    YT-14240: handle TRACE log level as DEBUG in python

--- yt/python/yt/logger.py
+++ yt/python/yt/logger.py
@@ -13,6 +13,9 @@ def set_log_level_from_config(logger):
     else:
         if logger_config.LOG_LEVEL.upper() == "NOTSET":
             raise Exception("LOG_LEVEL couldn't be defined as NOTSET")
+        # Intentionally override trace with debug for compatibility with C++ logging library.
+        if logger_config.LOG_LEVEL.upper() == "TRACE":
+            logger_config.LOG_LEVEL = "DEBUG"
         logger.setLevel(level=logging.__dict__[logger_config.LOG_LEVEL.upper()])
 
 logging.getLogger("yt.packages.requests.packages.urllib3").setLevel(logging.WARNING)
