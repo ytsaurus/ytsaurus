fa8957c7f8fb89ed074af42f74d3466632660f33
ermolovd <ermolovd@ytsaurus.tech>
2021-02-25 13:12:46 +0300

    Eliminate unnecessary RequestBuilderBase

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/RpcClientRequestBuilder.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/RpcClientRequestBuilder.java
@@ -1,6 +1,5 @@
 package ru.yandex.yt.ytclient.rpc;
 
-import java.time.Duration;
 import java.util.List;
 import java.util.concurrent.CompletableFuture;
 import java.util.concurrent.ScheduledExecutorService;
@@ -18,11 +17,6 @@ import ru.yandex.yt.rpc.TRequestHeader;
  */
 public interface RpcClientRequestBuilder<RequestType extends MessageLite.Builder, ResponseType> {
     /**
-     * Имя пользователя по умолчанию
-     */
-    String ROOT_USER_NAME = "root";
-
-    /**
      * Мутабельное тело запроса
      */
     RequestType body();
@@ -31,61 +25,6 @@ public interface RpcClientRequestBuilder<RequestType extends MessageLite.Builder
      * Мутабельный список аттачей к запросу
      */
     List<byte[]> attachments();
-
-    /**
-     * Имя пользователя
-     */
-    default String getUser() {
-        TRequestHeader.Builder header = header();
-        return header.hasUser() ? header.getUser() : ROOT_USER_NAME;
-    }
-
-    /**
-     * Выставляет имя пользователя
-     */
-    default void setUser(String user) {
-        if (user.equals(ROOT_USER_NAME)) {
-            header().clearUser();
-        } else {
-            header().setUser(user);
-        }
-    }
-
-    /**
-     * Возвращает true, если запрос является повторным
-     */
-    default boolean isRetry() {
-        return header().getRetry();
-    }
-
-    /**
-     * Помечает запрос как повторый, если retry == true
-     */
-    default void setRetry(boolean retry) {
-        header().setRetry(retry);
-    }
-
-    /**
-     * Выставляет id мутации
-     */
-    default void setMutationId(GUID mutationId) {
-        if (mutationId.isEmpty()) {
-            header().clearMutationId();
-        } else {
-            header().setMutationId(RpcUtil.toProto(mutationId));
-        }
-    }
-
-    /**
-     * Выставляет таймаут равным timeout
-     */
-    default void setTimeout(Duration timeout) {
-        if (timeout == null) {
-            header().clearTimeout();
-        } else {
-            header().setTimeout(RpcUtil.durationToMicros(timeout));
-        }
-    }
 
     /**
      * Делает асинхронное выполнение запроса
@@ -132,17 +71,5 @@ public interface RpcClientRequestBuilder<RequestType extends MessageLite.Builder
      */
     default String getMethod() {
         return header().getMethod();
-    }
-
-    /**
-     * Возвращает timeout, в течение которого должен быть получен ответ на запрос
-     */
-    default Duration getTimeout() {
-        TRequestHeader.Builder header = header();
-        if (header.hasTimeout()) {
-            return RpcUtil.durationFromMicros(header.getTimeout());
-        } else {
-            return null;
-        }
     }
 }
--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/internal/RequestWithResponseBuilder.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/internal/RequestWithResponseBuilder.java
@@ -4,6 +4,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.concurrent.CancellationException;
 import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.ScheduledExecutorService;
 
 import com.google.protobuf.MessageLite;
 import com.google.protobuf.Parser;
@@ -11,24 +12,160 @@ import com.google.protobuf.Parser;
 import ru.yandex.lang.NonNullApi;
 import ru.yandex.yt.rpc.TRequestHeader;
 import ru.yandex.yt.rpc.TResponseHeader;
+import ru.yandex.yt.rpc.TStreamingFeedbackHeader;
+import ru.yandex.yt.rpc.TStreamingPayloadHeader;
+import ru.yandex.yt.ytclient.proxy.internal.FailoverRpcExecutor;
 import ru.yandex.yt.ytclient.rpc.RpcClient;
+import ru.yandex.yt.ytclient.rpc.RpcClientPool;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestControl;
 import ru.yandex.yt.ytclient.rpc.RpcClientResponse;
 import ru.yandex.yt.ytclient.rpc.RpcClientResponseHandler;
+import ru.yandex.yt.ytclient.rpc.RpcClientStreamControl;
 import ru.yandex.yt.ytclient.rpc.RpcOptions;
+import ru.yandex.yt.ytclient.rpc.RpcRequest;
+import ru.yandex.yt.ytclient.rpc.RpcStreamConsumer;
+import ru.yandex.yt.ytclient.rpc.RpcUtil;
 
 @NonNullApi
-public class RequestWithResponseBuilder<RequestType extends MessageLite.Builder, ResponseType extends MessageLite> extends RequestBuilderBase<RequestType, RpcClientResponse<ResponseType>> {
+public class RequestWithResponseBuilder<RequestType extends MessageLite.Builder, ResponseType extends MessageLite>
+        implements RpcClientRequestBuilder<RequestType, RpcClientResponse<ResponseType>>
+{
+    private final TRequestHeader.Builder header;
+    private final RequestType body;
+    private final List<byte[]> attachments = new ArrayList<>();
+    private final RpcOptions options;
     private final Parser<ResponseType> parser;
 
     public RequestWithResponseBuilder(
             TRequestHeader.Builder header, RequestType body,
             Parser<ResponseType> parser,
             RpcOptions options) {
-        super(header, body, options);
+        this.header = header;
+        this.body = body;
+        this.options = options;
+        if (options.getGlobalTimeout() == null) {
+            header().clearTimeout();
+        } else {
+            header().setTimeout(RpcUtil.durationToMicros(options.getGlobalTimeout()));
+        }
         this.parser = parser;
     }
 
     @Override
+    public RpcOptions getOptions() {
+        return this.options;
+    }
+
+    @Override
+    public TRequestHeader.Builder header() {
+        return header;
+    }
+
+    @Override
+    public RequestType body() {
+        return body;
+    }
+
+    @Override
+    public List<byte[]> attachments() {
+        return attachments;
+    }
+
+    @Override
+    public RpcRequest<?> getRpcRequest() {
+        return new RpcRequest<>(header.build(), body.build(), attachments);
+    }
+
+    @Override
+    public CompletableFuture<RpcClientResponse<ResponseType>> invoke(RpcClient client) {
+        CompletableFuture<RpcClientResponse<ResponseType>> result = new CompletableFuture<>();
+        try {
+            RpcClientResponseHandler handler = createHandler(result);
+            RpcClientRequestControl control = client.send(client, getRpcRequest(), handler, getOptions());
+            result.whenComplete((ignoredResult, ignoredException) -> control.cancel());
+        } catch (Throwable e) {
+            result.completeExceptionally(e);
+        }
+        return result;
+    }
+
+    @Override
+    public CompletableFuture<RpcClientResponse<ResponseType>> invokeVia(ScheduledExecutorService executor, RpcClientPool clientPool) {
+        CompletableFuture<RpcClientResponse<ResponseType>> result = new CompletableFuture<>();
+        try {
+            RpcClientResponseHandler handler = createHandler(result);
+            // NB. we use attemptCount 3 since it corresponds to old default behaviour
+            final int attemptCount = 3;
+            RpcClientRequestControl control = FailoverRpcExecutor.execute(
+                    executor,
+                    clientPool,
+                    getRpcRequest(),
+                    handler,
+                    getOptions(),
+                    attemptCount);
+            result.whenComplete((ignoredResult, ignoredException) -> control.cancel());
+        } catch (Throwable e) {
+            result.completeExceptionally(e);
+        }
+        return result;
+    }
+
+    @Override
+    public RpcClientStreamControl startStream(RpcClient client, RpcStreamConsumer consumer) {
+        return client.startStream(client, getRpcRequest(), consumer, getOptions());
+    }
+
+    @Override
+    public CompletableFuture<RpcClientStreamControl> startStream(
+            ScheduledExecutorService executor,
+            RpcClientPool clientPool,
+            RpcStreamConsumer consumer)
+    {
+        CompletableFuture<Void> clientReleaseFuture = new CompletableFuture<>();
+        RpcStreamConsumer wrappedConsumer = new RpcStreamConsumer() {
+            @Override
+            public void onStartStream(RpcClientStreamControl control) {
+                consumer.onStartStream(control);
+            }
+
+            @Override
+            public void onFeedback(RpcClient sender, TStreamingFeedbackHeader header, List<byte[]> attachments) {
+                consumer.onFeedback(sender, header, attachments);
+            }
+
+            @Override
+            public void onPayload(RpcClient sender, TStreamingPayloadHeader header, List<byte[]> attachments) {
+                consumer.onPayload(sender, header, attachments);
+            }
+
+            @Override
+            public void onResponse(RpcClient sender, TResponseHeader header, List<byte[]> attachments) {
+                consumer.onResponse(sender, header, attachments);
+                clientReleaseFuture.complete(null);
+            }
+
+            @Override
+            public void onError(Throwable cause) {
+                consumer.onError(cause);
+                clientReleaseFuture.complete(null);
+            }
+
+            @Override
+            public void onCancel(CancellationException cancel) {
+                consumer.onCancel(cancel);
+                clientReleaseFuture.complete(null);
+            }
+
+            @Override
+            public void onWakeup() {
+                consumer.onWakeup();
+            }
+        };
+        CompletableFuture<RpcClient> clientFuture = clientPool.peekClient(clientReleaseFuture);
+        return clientFuture.thenApply(client -> client.startStream(client, getRpcRequest(), wrappedConsumer, getOptions()));
+    }
+
     public RpcClientResponseHandler createHandler(CompletableFuture<RpcClientResponse<ResponseType>> result) {
         return new RpcClientResponseHandler() {
             @Override
@@ -57,5 +194,10 @@ public class RequestWithResponseBuilder<RequestType extends MessageLite.Builder,
                 result.completeExceptionally(cancel);
             }
         };
+    }
+
+    @Override
+    public String toString() {
+        return String.format("%s/%s/%s", getService(), getMethod(), getRequestId());
     }
 }
