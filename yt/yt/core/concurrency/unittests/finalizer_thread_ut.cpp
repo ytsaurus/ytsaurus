a131f635b8306b211035673b664dab0fb9b7d0ad
prime <prime@yandex-team.ru>
2021-02-26 16:32:54 +0300

    Improve diagnostics for finalizer thread.

---
+++ yt/yt/core/concurrency/unittests/finalizer_thread_ut.cpp
@@ -0,0 +1,30 @@
+#include <yt/core/test_framework/framework.h>
+
+#include <yt/core/concurrency/action_queue.h>
+#include <yt/core/concurrency/scheduler_api.h>
+
+#include <yt/core/actions/future.h>
+
+namespace NYT::NConcurrency {
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TFinalizerThreadTest, DISABLED_DebugShutdown)
+{
+    auto actionQueue = New<TActionQueue>("ShutdownHang");
+
+    BIND([actionQueue] {
+        auto promise = NewPromise<void>();
+        WaitFor(promise.ToFuture())
+            .ThrowOnError();
+    })
+        .AsyncVia(actionQueue->GetInvoker())
+        .Run();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT::NConcurrency
+
