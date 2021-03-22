0a5f66c989cfda0512697ac04e9a4fabf4b623a3
iaz1607 <iaz1607@yandex-team.ru>
2021-03-22 17:08:42 +0300

    decrease split factor in dummy test

--- yt/go/ytrecipe/test/testchunks/chunks_test.go
+++ yt/go/ytrecipe/test/testchunks/chunks_test.go
@@ -7,33 +7,3 @@ func TestChunks0(t *testing.T) {
 
 func TestChunks1(t *testing.T) {
 }
-
-func TestChunks2(t *testing.T) {
-}
-
-func TestChunks3(t *testing.T) {
-}
-
-func TestChunks4(t *testing.T) {
-}
-
-func TestChunks5(t *testing.T) {
-}
-
-func TestChunks6(t *testing.T) {
-}
-
-func TestChunks7(t *testing.T) {
-}
-
-func TestChunks8(t *testing.T) {
-}
-
-func TestChunks9(t *testing.T) {
-}
-
-func TestChunks10(t *testing.T) {
-}
-
-func TestChunks11(t *testing.T) {
-}
