68e6075362f1b644b711d3f6795fa0ebf5defa6d
akozhikhov <akozhikhov@ytsaurus.tech>
2021-03-25 17:51:21 +0300

    Again increase partition count in DynamicTablesSingleCellBase

--- yt/yt/tests/integration/tests/test_dynamic_tables.py
+++ yt/yt/tests/integration/tests/test_dynamic_tables.py
@@ -181,7 +181,7 @@ class DynamicTablesBase(YTEnvSetup):
 
 
 class DynamicTablesSingleCellBase(DynamicTablesBase):
-    NUM_TEST_PARTITIONS = 4
+    NUM_TEST_PARTITIONS = 8
 
     DELTA_NODE_CONFIG = {
         "exec_agent": {
