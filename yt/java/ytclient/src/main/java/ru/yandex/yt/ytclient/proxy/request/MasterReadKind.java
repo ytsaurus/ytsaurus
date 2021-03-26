d860e36e65a99bbd628d1a7dc05f668bc495bd17
ermolovd <ermolovd@ytsaurus.tech>
2021-03-26 14:52:59 +0300

    YT-14557: rename ReadFrom to MasterReadKind and move to ytclient <!-- DEVEXP BEGIN --> ![review](https://codereview.in.yandex-team.ru/badges/review-in_progress-yellow.svg) [![artembelov](https://codereview.in.yandex-team.ru/badges/artembelov-...-yellow.svg)](https://staff.yandex-team.ru/artembelov) [![k--zaitsev](https://codereview.in.yandex-team.ru/badges/k--zaitsev-...-yellow.svg)](https://staff.yandex-team.ru/k-zaitsev) <!-- DEVEXP END -->

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/MasterReadKind.java
@@ -0,0 +1,29 @@
+package ru.yandex.yt.ytclient.proxy.request;
+
+import ru.yandex.yt.rpcproxy.EMasterReadKind;
+
+public enum MasterReadKind {
+    Leader("leader", EMasterReadKind.MRK_LEADER),
+    Follower("follower", EMasterReadKind.MRK_FOLLOWER),
+    Cache("cache", EMasterReadKind.MRK_CACHE),
+    MasterCache("master_cache", EMasterReadKind.MRK_MASTER_CACHE);
+
+    private final String wireName;
+    private final EMasterReadKind protoValue;
+
+    MasterReadKind(String wireName, EMasterReadKind protoValue) {
+        this.wireName = wireName;
+        this.protoValue = protoValue;
+    }
+
+    public EMasterReadKind getProtoValue() {
+        return protoValue;
+    }
+
+    /**
+     * Get string that is used in http interface.
+     */
+    public String getWireName() {
+        return wireName;
+    }
+}
