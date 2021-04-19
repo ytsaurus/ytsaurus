9b8b06ed2788ee28577dc5184fa9bccd4f00ba98
max42 <max42@ytsaurus.tech>
2021-04-19 13:25:57 +0300

    CHYT-584: more tests, ensure no spurious reincarnations.

--- yt/chyt/controller/internal/integration/strawberry_test.go
+++ yt/chyt/controller/internal/integration/strawberry_test.go
@@ -19,6 +19,15 @@ import (
 
 var root = ypath.Path("//tmp/strawberry")
 
+func abortAll(t *testing.T, ytc yt.Client) {
+	ops, err := yt.ListAllOperations(context.TODO(), ytc, &yt.ListOperationsOptions{State: yt.StateRunning.Ptr()})
+	require.NoError(t, err)
+	for _, op := range ops {
+		err := ytc.AbortOperation(context.TODO(), op.ID, &yt.AbortOperationOptions{})
+		require.NoError(t, err)
+	}
+}
+
 func prepare(t *testing.T) (yt.Client, *strawberry.Agent) {
 	env, cancel := yttest.NewEnv(t)
 	t.Cleanup(cancel)
@@ -33,6 +42,8 @@ func prepare(t *testing.T) (yt.Client, *strawberry.Agent) {
 		PassPeriod:            yson.Duration(time.Millisecond * 500),
 		RevisionCollectPeriod: yson.Duration(time.Millisecond * 100),
 	}
+
+	abortAll(t, env.YT)
 
 	agent := strawberry.NewAgent("test", env.YT, l, map[string]strawberry.Controller{
 		"sleep": sleep.NewController(l.WithName("strawberry"), env.YT, root, "test", nil),
@@ -55,6 +66,12 @@ func createNode(t *testing.T, ytc yt.Client, alias string) {
 func removeNode(t *testing.T, ytc yt.Client, alias string) {
 	t.Logf("removing node %v", alias)
 	err := ytc.RemoveNode(context.TODO(), root.Child(alias), nil)
+	require.NoError(t, err)
+}
+
+func setAttr(t *testing.T, ytc yt.Client, alias string, attr string, value interface{}) {
+	t.Logf("setting attribute %v/@%v", alias, attr)
+	err := ytc.SetNode(context.TODO(), root.Child(alias).Attr(attr), value, nil)
 	require.NoError(t, err)
 }
 
@@ -112,36 +129,78 @@ func waitAliases(t *testing.T, ytc yt.Client, expected []string) {
 	t.FailNow()
 }
 
+func waitIncarnation(t *testing.T, ytc yt.Client, alias string, expected int64) {
+	t.Logf("waiting for alias %v incarnation %v", alias, expected)
+	for i := 0; i < 30; i++ {
+		op := getOp(t, ytc, alias)
+		if op != nil {
+			annotations := op.RuntimeParameters.Annotations
+			incarnation, ok := annotations["strawberry_incarnation"]
+			require.True(t, ok)
+			require.LessOrEqual(t, incarnation, expected)
+			if reflect.DeepEqual(incarnation, expected) {
+				t.Logf("alias %v is at incarnation %v", alias, expected)
+				return
+			}
+		}
+		time.Sleep(time.Millisecond * 300)
+	}
+	t.FailNow()
+}
+
 func TestOperationBeforeStart(t *testing.T) {
 	ytc, agent := prepare(t)
+	t.Cleanup(agent.Stop)
 
-	createNode(t, ytc, "test")
+	createNode(t, ytc, "test1")
 	agent.Start()
 
-	_ = waitOp(t, ytc, "test")
+	_ = waitOp(t, ytc, "test1")
 }
 
 func TestOperationAfterStart(t *testing.T) {
 	ytc, agent := prepare(t)
+	t.Cleanup(agent.Stop)
 
 	agent.Start()
 	time.Sleep(time.Millisecond * 500)
 
-	createNode(t, ytc, "test")
+	createNode(t, ytc, "test2")
 
-	_ = waitOp(t, ytc, "test")
+	_ = waitOp(t, ytc, "test2")
 }
 
 func TestAbortDangling(t *testing.T) {
 	ytc, agent := prepare(t)
+	t.Cleanup(agent.Stop)
 
 	agent.Start()
-	createNode(t, ytc, "test1")
-	waitAliases(t, ytc, []string{"test1"})
-	createNode(t, ytc, "test2")
-	waitAliases(t, ytc, []string{"test1", "test2"})
-	removeNode(t, ytc, "test1")
-	waitAliases(t, ytc, []string{"test2"})
-	removeNode(t, ytc, "test2")
+	createNode(t, ytc, "test3")
+	waitAliases(t, ytc, []string{"test3"})
+	createNode(t, ytc, "test4")
+	waitAliases(t, ytc, []string{"test3", "test4"})
+	removeNode(t, ytc, "test3")
+	waitAliases(t, ytc, []string{"test4"})
+	removeNode(t, ytc, "test4")
 	waitAliases(t, ytc, []string{})
+}
+
+func TestReincarnations(t *testing.T) {
+	ytc, agent := prepare(t)
+	t.Cleanup(agent.Stop)
+
+	agent.Start()
+	createNode(t, ytc, "test5")
+	waitIncarnation(t, ytc, "test5", 1)
+	setAttr(t, ytc, "test5", "strawberry_pool", "foo")
+	waitIncarnation(t, ytc, "test5", 2)
+	setAttr(t, ytc, "test5", "strawberry_pool", "bar")
+	waitIncarnation(t, ytc, "test5", 3)
+	agent.Stop()
+	waitIncarnation(t, ytc, "test5", 3)
+	agent.Start()
+	time.Sleep(time.Second * 2)
+	waitIncarnation(t, ytc, "test5", 3)
+	setAttr(t, ytc, "test5", "strawberry_pool", "baz")
+	waitIncarnation(t, ytc, "test5", 4)
 }
