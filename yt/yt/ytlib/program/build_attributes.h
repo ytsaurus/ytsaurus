c20c8744f456c72b0fe2f98be583132dc13e5d55
max42 <max42@ytsaurus.tech>
2021-05-03 17:56:56 +0300

    Add --yson CLI flag for reporting build information in YSON.

--- yt/yt/ytlib/program/build_attributes.h
+++ yt/yt/ytlib/program/build_attributes.h
@@ -2,9 +2,15 @@
 
 #include <yt/yt/core/ytree/public.h>
 
+#include <yt/yt/core/yson/public.h>
+
 namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
+
+//! Build build (pun intended) attributes as a YSON string a-la /orchid/service. If service name is not provided,
+//! it is omitted from the result.
+void BuildBuildAttributes(NYson::IYsonConsumer* consumer, const char* serviceName = nullptr);
 
 void SetBuildAttributes(NYTree::IYPathServicePtr orchidRoot, const char* serviceName);
 
