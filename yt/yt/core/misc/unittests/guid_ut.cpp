427d546cffb0158f8557f3c2496427e5a4386b46
babenko <babenko@ytsaurus.tech>
2021-05-05 18:35:04 +0300

    Revert "Cleanup guid byte order mess (also fix jaeger metric)"

--- yt/yt/core/misc/unittests/guid_ut.cpp
+++ yt/yt/core/misc/unittests/guid_ut.cpp
@@ -5,8 +5,6 @@
 #include <yt/yt/core/misc/format.h>
 #include <yt/yt/core/misc/string.h>
 
-#include <util/string/hex.h>
-
 namespace NYT {
 namespace {
 
@@ -36,12 +34,12 @@ TString CanonicalToString(TGuid value)
         value.Parts32[0]);
 }
 
-const ui32 TrickyValues[] = {
-    0, 0x1, 0x12, 0x123, 0x1234, 0x12345, 0x123456, 0x1234567, 0x12345678
-};
-
 TEST(TGuidTest, FormatAllTricky)
 {
+    const ui32 TrickyValues[] = {
+        0, 0x1, 0x12, 0x123, 0x1234, 0x12345, 0x123456, 0x1234567, 0x12345678
+    };
+
     for (ui32 a : TrickyValues) {
         for (ui32 b : TrickyValues) {
             for (ui32 c : TrickyValues) {
@@ -59,14 +57,6 @@ TEST(TGuidTest, FormatAllSymbols)
     const auto Value = TGuid::FromString("12345678-abcdef01-12345678-abcdef01");
     EXPECT_EQ(CanonicalToString(Value), ToString(Value));
 }
-
-TEST(TGuidTest, ByteOrder)
-{
-    auto guid = TGuid::FromStringHex32("12345678ABCDEF0112345678ABCDEF01");
-    TString bytes{reinterpret_cast<const char*>(&(guid.Parts32[0])), 16};
-    EXPECT_EQ(HexEncode(bytes), "12345678ABCDEF0112345678ABCDEF01");
-}
-
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace
--- yt/yt/library/tracing/jaeger/tracer.cpp
+++ yt/yt/library/tracing/jaeger/tracer.cpp
@@ -239,7 +239,6 @@ void TJaegerTracer::DequeueAll(const TJaegerTracerConfigPtr& config)
         MemoryUsage_.Update(QueueMemory_);
         QueueSize_ += batch.spans_size();
         TraceQueueSize_.Update(QueueSize_);
-        TracesDequeued_.Increment(batch.spans_size());
 
         batch.Clear();
     };
