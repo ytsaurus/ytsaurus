5c1fe0a347a6fd70f78829912a11afae85bc7f04
prime <prime@yandex-team.ru>
2021-05-11 18:22:30 +0300

    PR from branch users/prime/guid_try_2 Reverse byte order Revert "Revert "Cleanup guid byte order mess (also fix jaeger metric)"" This reverts commit ca39249a4b56919ee53d662e971f39b0e0351a6b, reversing changes made to 49a14c1c155d1358038ef4937c253dc2ef4b7591.

--- yt/go/guid/guid_test.go
+++ yt/go/guid/guid_test.go
@@ -15,10 +15,12 @@ func TestParseString(t *testing.T) {
 	require.NoError(t, err)
 
 	a, b, c, d := guid.Parts()
-	require.Equal(t, uint32(0x1), a)
-	require.Equal(t, uint32(0x84), b)
-	require.Equal(t, uint32(0x3ff012f), c)
-	require.Equal(t, uint32(0xbe9399a3), d)
+	require.Equal(t, uint32(0x1), d)
+	require.Equal(t, uint32(0x84), c)
+	require.Equal(t, uint32(0x3ff012f), b)
+	require.Equal(t, uint32(0xbe9399a3), a)
+
+	require.Equal(t, GUID{0xa3, 0x99, 0x93, 0xbe, 0x2f, 0x01, 0xff, 0x03, 0x84, 0x00, 0x00, 0x00, 0x01, 0x00, 0x00, 0x00}, guid)
 }
 
 func TestParseInvalidString(t *testing.T) {
