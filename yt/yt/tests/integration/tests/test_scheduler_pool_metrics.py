31ed76cb389e1915517b462e565e8a074c279ec1
ignat <ignat@ytsaurus.tech>
2021-06-01 10:32:50 +0300

    YT-14881: Pepify and refactor tests: part 3

--- yt/yt/tests/integration/tests/test_scheduler_pool_metrics.py
+++ yt/yt/tests/integration/tests/test_scheduler_pool_metrics.py
@@ -4,13 +4,48 @@ from yt_env_setup import (
     Restarter,
     CONTROLLER_AGENTS_SERVICE,
 )
-from yt_commands import *  # noqa
-from yt_helpers import *
 
-from yt.yson import *
+from yt_commands import (  # noqa
+    authors, print_debug, wait, wait_assert, wait_breakpoint, release_breakpoint, with_breakpoint,
+    events_on_fs, reset_events_on_fs,
+    create, ls, get, set, copy, move, remove, link, exists,
+    create_account, create_network_project, create_tmpdir, create_user, create_group,
+    create_pool, create_pool_tree,
+    create_data_center, create_rack,
+    make_ace, check_permission, add_member,
+    make_batch_request, execute_batch, get_batch_error,
+    start_transaction, abort_transaction, commit_transaction, lock,
+    insert_rows, select_rows, lookup_rows, delete_rows, trim_rows, alter_table,
+    read_file, write_file, read_table, write_table, write_local_file,
+    map, reduce, map_reduce, join_reduce, merge, vanilla, sort, erase,
+    run_test_vanilla, run_sleeping_vanilla,
+    abort_job, list_jobs, get_job, abandon_job, interrupt_job,
+    get_job_fail_context, get_job_input, get_job_stderr, get_job_spec,
+    dump_job_context, poll_job_shell,
+    abort_op, complete_op, suspend_op, resume_op,
+    get_operation, list_operations, clean_operations,
+    get_operation_cypress_path, scheduler_orchid_pool_path,
+    scheduler_orchid_default_pool_tree_path, scheduler_orchid_operation_path,
+    scheduler_orchid_default_pool_tree_config_path, scheduler_orchid_path,
+    scheduler_orchid_node_path, scheduler_orchid_pool_tree_config_path,
+    sync_create_cells, sync_mount_table, sync_unmount_table,
+    sync_freeze_table, sync_unfreeze_table,
+    get_first_chunk_id, get_singular_chunk_id, get_chunk_replication_factor, multicell_sleep,
+    update_nodes_dynamic_config, update_controller_agent_config,
+    update_op_parameters, enable_op_detailed_logs,
+    set_node_banned, set_banned_flag, set_account_disk_space_limit,
+    check_all_stderrs,
+    create_test_tables, create_dynamic_table, PrepareTables,
+    get_statistics,
+    make_random_string, raises_yt_error,
+    get_driver, Driver)
 
+from yt_helpers import Profiler
+
+import pytest
 from flaky import flaky
 
+import os
 import time
 
 ##################################################################
@@ -521,6 +556,3 @@ class TestPoolMetrics(YTEnvSetup):
         wait(lambda: profiler.gauge("scheduler/undistributed_resources/cpu").get() == 1)
         wait(lambda: profiler.gauge("scheduler/undistributed_resource_flow/cpu").get() == 0)
         wait(lambda: profiler.gauge("scheduler/undistributed_burst_guarantee_resources/cpu").get() == 0)
-
-
-##################################################################
