a1ef0f521feef4005982c9fd690469481042c94e
eshcherbin <eshcherbin@ytsaurus.tech>
2021-06-18 16:36:56 +0300

    Mark test_pass_preemption_reason_to_node as flaky [RUN LARGE TESTS]

--- yt/yt/tests/integration/tests/test_scheduler_strategy.py
+++ yt/yt/tests/integration/tests/test_scheduler_strategy.py
@@ -1485,6 +1485,7 @@ class TestSchedulerPreemption(YTEnvSetup):
             assert get(scheduler_orchid_pool_path("<Root>") + "/resource_usage/cpu") <= total_cpu_limit
 
     @authors("eshcherbin")
+    @flaky(max_runs=3)
     def test_pass_preemption_reason_to_node(self):
         update_controller_agent_config("enable_operation_progress_archivation", True)
 
