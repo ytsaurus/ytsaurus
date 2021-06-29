560be8e4cdde36db88171784cacc0878f558ebe9
ignat <ignat@ytsaurus.tech>
2021-06-29 15:18:05 +0300

    Pepify bindings

--- yt/yt/python/yt_driver_rpc_bindings/driver.py
+++ yt/yt/python/yt_driver_rpc_bindings/driver.py
@@ -1,5 +1,5 @@
 class Request(object):
-    def __init__ (self, command_name, parameters=None, input_stream=None, output_stream=None, user=None, id=None, token=None):
+    def __init__(self, command_name, parameters=None, input_stream=None, output_stream=None, user=None, id=None, token=None):
         self.command_name = command_name
         self.parameters = parameters
         self.input_stream = input_stream
--- yt/yt/python/yt_yson_bindings/__init__.py
+++ yt/yt/python/yt_yson_bindings/__init__.py
@@ -1,22 +1,22 @@
 try:
-    from .yson_lib import (load, loads, dump, dumps, loads_proto, dumps_proto,
+    from .yson_lib import (load, loads, dump, dumps, loads_proto, dumps_proto,  # noqa
                            load_skiff, load_skiff_structured, dump_skiff, dump_skiff_structured,
                            SkiffRecord, SkiffSchema, SkiffTableSwitch, SkiffOtherColumns)
 except ImportError:
-    from yson_lib import (load, loads, dump, dumps, loads_proto, dumps_proto,
+    from yson_lib import (load, loads, dump, dumps, loads_proto, dumps_proto,  # noqa
                           load_skiff, load_skiff_structured, dump_skiff, dump_skiff_structured,
                           SkiffRecord, SkiffSchema, SkiffTableSwitch, SkiffOtherColumns)
 
 try:
-    from .yson_lib import parse_ypath
+    from .yson_lib import parse_ypath  # noqa
 except ImportError:
     try:
-        from yson_lib import parse_ypath
+        from yson_lib import parse_ypath  # noqa
     except ImportError:
         pass
 
 try:
-    from .version import VERSION, COMMIT
+    from .version import VERSION, COMMIT  # noqa
     __version__ = VERSION
 except:
     __version__ = VERSION = COMMIT = "unknown"
