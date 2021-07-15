0e654b847a882a90ecc997d4588d688c6741e4b2
levysotsky <levysotsky@yandex-team.ru>
2021-07-15 12:45:02 +0300

    Fix query for is_sorted

--- yt/python/yt/wrapper/batch_helpers.py
+++ yt/python/yt/wrapper/batch_helpers.py
@@ -5,11 +5,25 @@ def create_batch_client(raise_errors=False, max_batch_size=None, client=None):
     batch_executor = BatchExecutor(raise_errors, max_batch_size, client)
     return batch_executor.get_client()
 
-def batch_apply(function, data, client=None):
+def batch_apply(function, data, raise_errors=True, client=None):
     """Applies function to each element from data in a batch mode and returns result."""
-    batch_client = create_batch_client(raise_errors=True, client=client)
+    # TODO: fix in YT-6615
+    from .batch_execution import YtBatchRequestFailedError
+
+    batch_client = create_batch_client(raise_errors=raise_errors, client=client)
     results = []
     for item in data:
         results.append(function(item, client=batch_client))
     batch_client.commit_batch()
-    return [result.get_result() if result is not None else None for result in results]
+    unpacked_results = []
+    errors = []
+    for result in results:
+        if result is None:
+            unpacked_results.append(None)
+        elif result.is_ok():
+            unpacked_results.append(result.get_result())
+        else:
+            errors.append(result.get_error())
+    if errors:
+        raise YtBatchRequestFailedError("Batch request failed", inner_errors=errors)
+    return unpacked_results
