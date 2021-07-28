7e055ddedb7a26faea1ffacb255879b775bcbd5c
pogorelov <pogorelov@ytsaurus.tech>
2021-07-28 16:53:05 +0300

    Remove old compat tests

--- yt/yt/tests/integration/compat/latest_20_3/test_scheduler_merge.py
+++
@@ -1,9 +0,0 @@
-from original_tests.yt.yt.tests.integration.tests.test_scheduler_merge \
-    import TestSchedulerMergeCommands as BaseTestMergeCommands
-
-
-class TestMergeCommandsCompatUpToCA(BaseTestMergeCommands):
-    ARTIFACT_COMPONENTS = {
-        "20_3": ["master", "node", "job-proxy", "exec", "tools"],
-        "trunk": ["scheduler", "controller-agent", "proxy", "http-proxy"],
-    }
--- yt/yt/tests/integration/compat/latest_20_3/test_scheduler_reduce.py
+++
@@ -1,9 +0,0 @@
-from original_tests.yt.yt.tests.integration.tests.test_scheduler_reduce \
-    import TestSchedulerReduceCommands as BaseTestReduceCommands
-
-
-class TestReduceCommandsCompatUpToCA(BaseTestReduceCommands):
-    ARTIFACT_COMPONENTS = {
-        "20_3": ["master", "node", "job-proxy", "exec", "tools"],
-        "trunk": ["scheduler", "controller-agent", "proxy", "http-proxy"],
-    }
