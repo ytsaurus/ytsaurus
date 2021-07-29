1972856aca1008f6f0eb06f5a005edb29b8cff41
Alexandra Belousova <sashbel@yandex-team.ru>
2021-07-29 16:07:53 +0300

    Update spark fork version

--- taxi/dmp/spark/spark-over-yt/spark-submit/src/main/scala/org/apache/spark/deploy/rest/RestSubmissionClientWrapper.scala
+++ taxi/dmp/spark/spark-over-yt/spark-submit/src/main/scala/org/apache/spark/deploy/rest/RestSubmissionClientWrapper.scala
@@ -1,10 +1,9 @@
 package org.apache.spark.deploy.rest
 
 object RestSubmissionClientWrapper {
-  def create(master: String) = new RestSubmissionClient(master)
+  def create(master: String) = new RestSubmissionClient(master = master, sparkConf = None)
 
   def requestSubmissionStatus(client: RestSubmissionClient, id: String): SubmissionStatusResponse = {
     client.requestSubmissionStatus(id).asInstanceOf[SubmissionStatusResponse]
   }
-
 }
