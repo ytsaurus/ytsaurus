041c440bf630ed39b5c3a6527253bbd41f585974
ignat <ignat@ytsaurus.tech>
2021-08-22 08:40:14 +0300

    Fix chyt tests

--- yt/chyt/tests/server/test_http_proxy.py
+++ yt/chyt/tests/server/test_http_proxy.py
@@ -7,7 +7,7 @@ from yt_commands import (get, write_table, authors, raises_yt_error, abort_job, 
 
 from yt.common import wait
 
-from yt_helpers import Profiler
+from yt.test_helpers.profiler import Profiler
 
 import yt.environment.init_operation_archive as init_operation_archive
 
@@ -34,7 +34,9 @@ class TestClickHouseHttpProxy(ClickHouseTestBase):
         self._setup()
 
     def _get_proxy_metric(self, metric_name):
-        return Profiler.at_proxy(self.Env.get_http_proxy_address()).counter(metric_name)
+        return Profiler.at_proxy(
+            self.Env.create_native_client(),
+            self.Env.get_http_proxy_address()).counter(metric_name)
 
     @authors("evgenstf")
     def test_instance_choice(self):
