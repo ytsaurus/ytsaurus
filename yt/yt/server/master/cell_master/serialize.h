377f0a0faf72d6b182264dbdd97322ba3caa0fd1
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2021-08-30 17:59:35 +0300

    Move merge sessions registration from OnAfterSnapshotLoaded to OnLeaderActive

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -182,6 +182,7 @@ DEFINE_ENUM(EMasterReign,
     ((XdeltaAggregation)                                            (1655))  // leasid
     ((RefHunkChunks)                                                (1656))  // babenko
     ((PersistNodesBeingMerged)                                      (1657))  // aleksandra-zh
+    ((RescheduleMergeOnLeaderActive)                                (1658))  // aleksandra-zh
     // 21.3 starts here.
     ((DropPoolTreeInternedAttributes)                               (1700))  // ignat
 );
