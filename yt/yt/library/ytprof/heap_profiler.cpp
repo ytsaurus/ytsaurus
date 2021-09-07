342caa59a0f3928e9922432ab1f52c0c7c9d2ed1
prime <prime@yandex-team.ru>
2021-09-07 12:56:45 +0300

    PR from branch users/prime/fill_drop_frames

--- yt/yt/library/ytprof/heap_profiler.cpp
+++ yt/yt/library/ytprof/heap_profiler.cpp
@@ -4,6 +4,7 @@
 #include "backtrace.h"
 
 #include <util/generic/hash_set.h>
+#include <util/string/join.h>
 
 #include <tcmalloc/malloc_extension.h>
 
@@ -16,25 +17,30 @@ NProto::Profile ConvertAllocationProfile(const tcmalloc::Profile& snapshot)
     NProto::Profile profile;
     profile.add_string_table();
 
-    profile.add_string_table("allocations");
-    profile.add_string_table("count");
-    profile.add_string_table("space");
-    profile.add_string_table("bytes");
-    profile.add_string_table("memory_tag");
+    auto addString = [&] (TString str) {
+        auto index = profile.string_table_size();
+        profile.add_string_table(str);
+        return index;
+    };
 
     auto sampleType = profile.add_sample_type();
-    sampleType->set_type(1);
-    sampleType->set_unit(2);
+    sampleType->set_type(addString("allocations"));
+    sampleType->set_unit(addString("count"));
 
     sampleType = profile.add_sample_type();
-    sampleType->set_type(3);
-    sampleType->set_unit(4);
+    sampleType->set_type(addString("space"));
+    sampleType->set_unit(addString("bytes"));
 
     auto periodType = profile.mutable_period_type();
-    periodType->set_type(3);
-    periodType->set_unit(4);
+    periodType->set_type(sampleType->type());
+    periodType->set_unit(sampleType->unit());
 
     profile.set_period(snapshot.Period());
+
+    auto memoryTagId = addString("memory_tag");
+    auto allocatedSizeId = addString("allocated_size");
+    auto requestedSizeId = addString("requested_size");
+    auto requestedAlignmentId = addString("requested_alignment");
 
     THashMap<void*, ui64> locations;
     snapshot.Iterate([&] (const tcmalloc::Profile::Sample& sample) {
@@ -45,9 +51,21 @@ NProto::Profile ConvertAllocationProfile(const tcmalloc::Profile& snapshot)
         auto memoryTag = sample.stack[0];
         if (memoryTag != 0) {
             auto label = sampleProto->add_label();
-            label->set_key(5);
+            label->set_key(memoryTagId);
             label->set_num(reinterpret_cast<i64>(memoryTag));
         }
+
+        auto allocatedSizeLabel = sampleProto->add_label();
+        allocatedSizeLabel->set_key(allocatedSizeId);
+        allocatedSizeLabel->set_num(sample.allocated_size);
+
+        auto requestedSizeLabel = sampleProto->add_label();
+        requestedSizeLabel->set_key(requestedSizeId);
+        requestedSizeLabel->set_num(sample.requested_size);
+
+        auto requestedAlignmentLabel = sampleProto->add_label();
+        requestedAlignmentLabel->set_key(requestedAlignmentId);
+        requestedAlignmentLabel->set_num(sample.requested_alignment);
 
         for (int i = 1; i < sample.depth; i++) {
             auto ip = sample.stack[i];
@@ -68,6 +86,13 @@ NProto::Profile ConvertAllocationProfile(const tcmalloc::Profile& snapshot)
             locations[ip] = locationId;
         }
     });
+
+    profile.set_drop_frames(addString(JoinSeq("|", {
+        ".*SampleifyAllocation",
+        ".*AllocSmall",
+        "slow_alloc",
+        "TBasicString::TBasicString",
+    })));
 
     Symbolize(&profile, true);
     return profile;
