4b85f8c04c41a0ad460348ff77f45933e65fae1e
renadeen <renadeen@ytsaurus.tech>
2021-09-10 14:27:00 +0300

    YT-15145: Actualize parents of ephemeral pools accordingly to user default parent pool map.

--- yt/yt/server/scheduler/scheduler_tree.h
+++ yt/yt/server/scheduler/scheduler_tree.h
@@ -112,7 +112,6 @@ struct ISchedulerTree
     virtual void BuildUserToEphemeralPoolsInDefaultPool(NYTree::TFluentAny fluent) const = 0;
 
     virtual void BuildFairShareInfo(NYTree::TFluentMap fluent) const = 0;
-    
     //! Raised when operation considered running in tree.
     DECLARE_INTERFACE_SIGNAL(void(TOperationId), OperationRunning);
 };
