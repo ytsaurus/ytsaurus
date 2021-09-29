13ed40d9bc3ed6a19ff0e6af8698fb94dcca4102
Alexandra Belousova <sashbel@yandex-team.ru>
2021-09-29 10:53:59 +0300

    SPYT-184: fix OperationStatus

--- taxi/dmp/spark/spark-over-yt/yt-wrapper/src/main/scala/ru/yandex/spark/yt/wrapper/operation/OperationStatus.scala
+++ taxi/dmp/spark/spark-over-yt/yt-wrapper/src/main/scala/ru/yandex/spark/yt/wrapper/operation/OperationStatus.scala
@@ -1,51 +1,43 @@
 package ru.yandex.spark.yt.wrapper.operation
 
 sealed abstract class OperationStatus(val isFinished: Boolean,
-                                      val isSuccess: Boolean) {
-  val name: String = {
-    val className = this.getClass.getSimpleName.toLowerCase
-    if (className.endsWith("$")) {
-      className.dropRight(1)
-    } else {
-      className
-    }
-  }
-}
+                                      val isSuccess: Boolean,
+                                      val name: String)
 
 object OperationStatus {
-  case object Unknown extends OperationStatus(false, false)
+  case object Unknown extends OperationStatus(false, false, "unknown")
 
-  case object Starting extends OperationStatus(false, false)
+  case object Starting extends OperationStatus(false, false, "starting")
 
-  case object Initializing extends OperationStatus(false, false)
+  case object Initializing extends OperationStatus(false, false, "initializing")
 
-  case object Preparing extends OperationStatus(false, false)
+  case object Preparing extends OperationStatus(false, false, "preparing")
 
-  case object Materializing extends OperationStatus(false, false)
+  case object Materializing extends OperationStatus(false, false, "materializing")
 
-  case object Reviving extends OperationStatus(false, false)
+  case object Reviving extends OperationStatus(false, false, "reviving")
 
-  case object ReviveInitializing extends OperationStatus(false, false)
+  case object ReviveInitializing extends OperationStatus(false, false, "revive_initializing")
 
-  case object Orphaned extends OperationStatus(false, false)
+  case object Orphaned extends OperationStatus(false, false, "orphaned")
 
-  case object WaitingForAgent extends OperationStatus(false, false)
+  case object WaitingForAgent extends OperationStatus(false, false, "waiting_for_agent")
 
-  case object Pending extends OperationStatus(false, false)
+  case object Pending extends OperationStatus(false, false, "pending")
 
-  case object Running extends OperationStatus(false, false)
+  case object Running extends OperationStatus(false, false, "running")
 
-  case object Completing extends OperationStatus(false, false)
+  case object Completing extends OperationStatus(false, false, "completing")
 
-  case object Completed extends OperationStatus(true, true)
+  case object Completed extends OperationStatus(true, true, "completed")
 
-  case object Aborting extends OperationStatus(false, false)
+  case object Aborting extends OperationStatus(false, false, "aborting")
 
-  case object Aborted extends OperationStatus(true, false)
+  case object Aborted extends OperationStatus(true, false, "aborted")
 
-  case object Failing extends OperationStatus(false, false)
+  case object Failing extends OperationStatus(false, false, "failing")
 
-  case object Failed extends OperationStatus(true, false)
+  case object Failed extends OperationStatus(true, false, "failed")
 
   def getByName(name: String): OperationStatus = {
     Seq(Unknown, Starting, Initializing, Preparing,
