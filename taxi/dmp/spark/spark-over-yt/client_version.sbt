0a00e01efab38ffccab4ae1f4c1d6f3daa783bdc
Alexandra Belousova <sashbel@yandex-team.ru>
2021-09-30 16:35:43 +0300

    Add logs to GlobalTransactionSparkListener

--- taxi/dmp/spark/spark-over-yt/client_version.sbt
+++ taxi/dmp/spark/spark-over-yt/client_version.sbt
@@ -1,4 +1,4 @@
 import spyt.SpytPlugin.autoImport._
 
 ThisBuild / spytClientVersion := "1.17.6-SNAPSHOT"
-ThisBuild / spytClientPythonVersion := "1.17.6b1"
+ThisBuild / spytClientPythonVersion := "1.17.6b6"
--- taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/format/GlobalTransactionSparkListener.scala
+++ taxi/dmp/spark/spark-over-yt/data-source/src/main/scala/ru/yandex/spark/yt/format/GlobalTransactionSparkListener.scala
@@ -2,19 +2,24 @@ package ru.yandex.spark.yt.format
 
 import org.apache.spark.scheduler.{SparkListenerApplicationEnd, UserDefinedSparkListener}
 import org.apache.spark.sql.SparkSession
+import org.slf4j.LoggerFactory
 import ru.yandex.spark.yt.fs.YtClientConfigurationConverter.ytClientConfiguration
 import ru.yandex.spark.yt.wrapper.YtWrapper
 import ru.yandex.spark.yt.wrapper.client.YtClientProvider
 
 class GlobalTransactionSparkListener(spark: SparkSession) extends UserDefinedSparkListener {
+  private val log = LoggerFactory.getLogger(getClass)
 
   import ru.yandex.spark.yt.format.conf.SparkYtConfiguration.GlobalTransaction
   import ru.yandex.spark.yt.fs.conf._
 
   override def onListenerStart(): Unit = {
+    log.info("GlobalTransactionSparkListener started")
     if (spark.ytConf(GlobalTransaction.Enabled)) {
+      log.info("Global transaction enabled, creating transaction")
       val yt = YtClientProvider.ytClient(ytClientConfiguration(spark))
       val transaction = YtWrapper.createTransaction(None, spark.ytConf(GlobalTransaction.Timeout))(yt)
+      log.info(s"Global transaction id is ${transaction.getId.toString}")
       spark.setYtConf(GlobalTransaction.Id, transaction.getId.toString)
     }
   }
@@ -23,6 +28,7 @@ class GlobalTransactionSparkListener(spark: SparkSession) extends UserDefinedSpa
     spark.getYtConf(GlobalTransaction.Id).foreach { transaction =>
       val yt = YtClientProvider.ytClient(ytClientConfiguration(spark))
       YtWrapper.commitTransaction(transaction)(yt)
+      log.info(s"Global transaction $transaction committed")
     }
   }
 }
