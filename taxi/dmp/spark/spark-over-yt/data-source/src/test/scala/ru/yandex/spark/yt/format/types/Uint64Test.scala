ec80be5ae17b41e189a220bdf91f5d8c72971e00
Aleksandra Belousova <sashbel@yandex-team.ru>
2021-10-13 18:34:37 +0300

    SPYT-193: Refactor uint64 tests, add sort aggregation test, update spark dependency (#87)

---
+++ taxi/dmp/spark/spark-over-yt/data-source/src/test/scala/ru/yandex/spark/yt/format/types/Uint64Test.scala
@@ -0,0 +1,144 @@
+package ru.yandex.spark.yt.format.types
+
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.types.LongType
+import org.apache.spark.sql.yson.UInt64Long.{fromStringUdf, toStringUdf}
+import org.apache.spark.sql.yson.{UInt64Long, UInt64Type}
+import org.scalatest.{FlatSpec, Matchers}
+import ru.yandex.spark.yt.test.{LocalSpark, TestUtils, TmpDir}
+import ru.yandex.yt.ytclient.tables.{ColumnValueType, TableSchema}
+import ru.yandex.spark.yt._
+
+class Uint64Test extends FlatSpec with Matchers with LocalSpark with TmpDir with TestUtils {
+  behavior of "YtDataSource"
+
+  import spark.implicits._
+
+  it should "cast UInt64Long to Long" in {
+    import org.apache.spark.sql.functions._
+    val df = Seq(UInt64Long(1L), UInt64Long(2L), null)
+      .toDF("a").withColumn("a", col("a").cast(LongType))
+
+    df.select("a").collect() should contain theSameElementsAs Seq(
+      Row(1L), Row(2L), Row(null)
+    )
+  }
+
+  it should "cast Long to UInt64Long" in {
+    import org.apache.spark.sql.functions._
+    val df = Seq(Some(1L), Some(2L), None)
+      .toDF("a").withColumn("a", col("a").cast(UInt64Type))
+
+    df.select("a").collect() should contain theSameElementsAs Seq(
+      Row(UInt64Long(1L)), Row(UInt64Long(2L)), Row(null)
+    )
+  }
+
+  it should "cast UInt64Long to String" in {
+    import org.apache.spark.sql.functions._
+    val df = Seq(UInt64Long(1L), UInt64Long(2L), null)
+      .toDF("a").withColumn("a", toStringUdf(col("a")))
+
+    df.select("a").collect() should contain theSameElementsAs Seq(
+      Row("1"), Row("2"), Row(null)
+    )
+  }
+
+  it should "cast String to UInt64Long" in {
+    import org.apache.spark.sql.functions._
+    val df = Seq("1", "2", null)
+      .toDF("a").withColumn("a", fromStringUdf(col("a")))
+
+    df.select("a").collect() should contain theSameElementsAs Seq(
+      Row(UInt64Long(1L)), Row(UInt64Long(2L)), Row(null)
+    )
+  }
+
+  it should "read UInt64Long" in {
+    writeTableFromYson(Seq(
+      """{a = 1}""",
+      """{a = 2}""",
+      """{a = #}"""
+    ), tmpPath, new TableSchema.Builder()
+      .setUniqueKeys(false)
+      .addValue("a", ColumnValueType.UINT64)
+      .build()
+    )
+
+    val res = spark.read.yt(tmpPath)
+    res.select("a").collect() should contain theSameElementsAs Seq(
+      Row(UInt64Long(1L)), Row(UInt64Long(2L)), Row(null)
+    )
+  }
+
+  it should "write UInt64Long" in {
+    Seq(UInt64Long(1L), UInt64Long(2L), null)
+      .toDF("a").write.yt(tmpPath)
+
+    val res = spark.read.yt(tmpPath)
+    res.select("a").collect() should contain theSameElementsAs Seq(
+      Row(UInt64Long(1L)), Row(UInt64Long(2L)), Row(null)
+    )
+  }
+
+  it should "fail when need unchecked cast UInt64Long" in {
+    Seq(UInt64Long(1L), UInt64Long(2L), UInt64Long(2L), UInt64Long(2L), UInt64Long(3L), UInt64Long(1L))
+      .toDF("a").write.yt(tmpPath)
+
+    val res = spark.read.yt(tmpPath)
+
+    //    res.agg(countDistinct("a")).collect() returns correct value: Array(Row(3))
+    a[IllegalStateException] shouldBe thrownBy {
+      res.groupBy("a").count().collect()
+    }
+  }
+
+  it should "write and read big uint64" in {
+    val df = Seq(UInt64Long(1L), UInt64Long("9223372036854775816"), UInt64Long("9223372036854775813"), null)
+      .toDF("a")
+
+    val res = df
+      .withColumn("a", 'a.cast(LongType))
+      .withColumn("a", 'a + 1)
+      .withColumn("a", 'a.cast(UInt64Type))
+
+    res.collect() should contain theSameElementsAs Seq(
+      Row(UInt64Long(2L)),
+      Row(UInt64Long("9223372036854775817")),
+      Row(UInt64Long("9223372036854775814")),
+      Row(null)
+    )
+  }
+
+  it should "join dataframes by uint64 column" in {
+    val df1 = Seq(UInt64Long(1L) -> "a1", UInt64Long(2L) -> "b1").toDF("a", "b")
+    val df2 = Seq(UInt64Long(1L) -> "a2", UInt64Long(2L) -> "b2", UInt64Long(3L) -> "c2").toDF("a", "c")
+
+    df1.join(df2, Seq("a"), "outer").collect() should contain theSameElementsAs Seq(
+      Row(UInt64Long(1L), "a1", "a2"),
+      Row(UInt64Long(2L), "b1", "b2"),
+      Row(UInt64Long(3L), null, "c2")
+    )
+  }
+
+  it should "execute SortAggregate on dataframe with uint64 column" in {
+    val data = Seq(
+      (UInt64Long(1L), "a", 1),
+      (UInt64Long(2L), "b", 1),
+      (UInt64Long(3L), "c", 1)
+    )
+    data.toDF("a", "b", "c").write.yt(tmpPath)
+
+    val res = withConf(org.apache.spark.sql.internal.SQLConf.USE_OBJECT_HASH_AGG, "false") {
+      spark.read.yt(tmpPath).dropDuplicates("c").collect()
+    }
+
+    res.length shouldEqual 1
+    res should contain oneElementOf Seq(
+      Row(UInt64Long(1L), "a", 1),
+      Row(UInt64Long(2L), "b", 1),
+      Row(UInt64Long(3L), "c", 1)
+    )
+  }
+
+}
