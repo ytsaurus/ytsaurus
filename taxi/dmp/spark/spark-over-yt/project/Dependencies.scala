3d2b341bc303a6b90c81a86e5abdea12d5e114f6
Alexandra Belousova <sashbel@yandex-team.ru>
2021-10-15 16:39:52 +0300

    Update spark version

--- taxi/dmp/spark/spark-over-yt/project/Dependencies.scala
+++ taxi/dmp/spark/spark-over-yt/project/Dependencies.scala
@@ -5,7 +5,7 @@ object Dependencies {
   lazy val circeYamlVersion = "0.12.0"
   lazy val scalatestVersion = "3.0.8"
   lazy val sparkVersion = "3.0.1"
-  lazy val sparkForkVersion = "3.0.1-fork-1.19.0"
+  lazy val sparkForkVersion = "3.0.1-fork-1.20.0"
   lazy val yandexIcebergVersion = "8690334"
   lazy val slf4jVersion = "1.7.28"
   lazy val scalatraVersion = "2.7.0"
