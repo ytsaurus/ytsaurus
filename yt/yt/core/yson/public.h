899907d29bba3c64bf064eeec852be88c281ea8b
nadya73 <nadya73@ytsaurus.tech>
2021-10-18 13:38:32 +0300

    YT-15289: Remove yt/yt/core enums, add using Remove yt/yt/core enums

--- yt/yt/core/yson/public.h
+++ yt/yt/core/yson/public.h
@@ -2,34 +2,14 @@
 
 #include <yt/yt/core/misc/public.h>
 
+#include <library/cpp/yson/public.h>
+
 namespace NYT::NYson {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-//! The data format.
-DEFINE_ENUM(EYsonFormat,
-    // Binary.
-    // Most compact but not human-readable.
-    (Binary)
-
-    // Text.
-    // Not so compact but human-readable.
-    // Does not use indentation.
-    // Uses escaping for non-text characters.
-    (Text)
-
-    // Text with indentation.
-    // Extremely verbose but human-readable.
-    // Uses escaping for non-text characters.
-    (Pretty)
-);
-
-// NB: -1 is used for serializing null TYsonString.
-DEFINE_ENUM_WITH_UNDERLYING_TYPE(EYsonType, i8,
-    ((Node)          (0))
-    ((ListFragment)  (1))
-    ((MapFragment)   (2))
-);
+using ::NYson::EYsonFormat;
+using ::NYson::EYsonType;
 
 enum class ETokenType;
 
