ece1959fbb0e5fbc5433775a4d0ce61cc70b2327
nadya73 <nadya73@ytsaurus.tech>
2021-11-11 13:26:26 +0300

    YT-15470: Add default RetryPolicy Add default retry policy

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/RpcErrorCode.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/rpc/RpcErrorCode.java
@@ -6,6 +6,8 @@ public enum RpcErrorCode {
     Canceled(2),
     Timeout(3),
 
+    TooManyRequests(429),
+
     ProxyBanned(2100),
 
     TransportError(100),
@@ -14,12 +16,35 @@ public enum RpcErrorCode {
     NoSuchMethod(103),
     Unavailable(105),
     PoisonPill(106),
-    RequestQueueSizeLimitExceeded(108),
+    RpcRequestQueueSizeLimitExceeded(108),
     AuthenticationError(109),
     InvalidCsrfToken(110),
     InvalidCredentials(111),
     StreamingNotSupported(112),
-    TableMountInfoNotReady(1707);
+
+    TooManyOperations(202),
+
+    SessionAlreadyExists(703),
+    ChunkAlreadyExists(704),
+    WindowError(705),
+    BlockContentMismatch(706),
+    InvalidBlockChecksum(721),
+    BlockOutOfRange(722),
+    MissingExtension(724),
+    NoSuchBlock(707),
+    NoSuchChunk(708),
+    NoSuchChunkList(717),
+    NoSuchChunkTree(713),
+    NoSuchChunkView(727),
+    NoSuchMedium(719),
+
+    RequestQueueSizeLimitExceeded(904),
+
+    TransactionLockConflict(1700),
+    AllWritesDisabled(1703),
+    TableMountInfoNotReady(1707),
+
+    OperationProgressOutdated(1911);
 
     @SuppressWarnings("checkstyle:VisibilityModifier")
     public final int code;
