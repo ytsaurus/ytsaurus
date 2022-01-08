8adf23d0df670bfd1215ad5accb04bd0ef112183
babenko <babenko@ytsaurus.tech>
2022-01-08 16:12:25 +0300

    Drop master object service cache bypass support

--- yt/yt/ytlib/object_client/caching_object_service.cpp
+++ yt/yt/ytlib/object_client/caching_object_service.cpp
@@ -37,24 +37,6 @@ using namespace NProfiling;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-struct TSubrequestResponse
-{
-    TSubrequestResponse() = default;
-
-    TSubrequestResponse(
-        TSharedRefArray message,
-        NHydra::TRevision revision,
-        double byteRate)
-        : Message(std::move(message))
-        , Revision(revision)
-        , ByteRate(byteRate)
-    { }
-
-    TSharedRefArray Message;
-    NHydra::TRevision Revision;
-    double ByteRate = 0.0;
-};
-
 class TCachingObjectService
     : public ICachingObjectService
     , public TServiceBase
@@ -97,87 +79,6 @@ public:
 private:
     DECLARE_RPC_SERVICE_METHOD(NObjectClient::NProto, Execute);
 
-    class TMasterRequest
-        : public TRefCounted
-    {
-    public:
-        TMasterRequest(
-            IChannelPtr channel,
-            TCtxExecutePtr context,
-            const NLogging::TLogger& logger)
-            : Context_(std::move(context))
-            , Logger(logger)
-            , Proxy_(std::move(channel))
-        {
-            Request_ = Proxy_.Execute();
-            SetAuthenticationIdentity(Request_, Context_->GetAuthenticationIdentity());
-            MergeRequestHeaderExtensions(&Request_->Header(), Context_->RequestHeader());
-        }
-
-        TFuture<TSubrequestResponse> Add(TSharedRefArray subrequestMessage)
-        {
-            Request_->add_part_counts(subrequestMessage.Size());
-            Request_->Attachments().insert(
-                Request_->Attachments().end(),
-                subrequestMessage.Begin(),
-                subrequestMessage.End());
-
-            auto promise = NewPromise<TSubrequestResponse>();
-            Promises_.push_back(promise);
-            return promise;
-        }
-
-        void Invoke()
-        {
-            YT_LOG_DEBUG("Running cache bypass request (RequestId: %v, SubrequestCount: %v)",
-                Context_->GetRequestId(),
-                Promises_.size());
-            Request_->Invoke().Subscribe(BIND(&TMasterRequest::OnResponse, MakeStrong(this)));
-        }
-
-    private:
-        const TCtxExecutePtr Context_;
-        const NLogging::TLogger Logger;
-
-        TObjectServiceProxy Proxy_;
-        TObjectServiceProxy::TReqExecutePtr Request_;
-        std::vector<TPromise<TSubrequestResponse>> Promises_;
-
-
-        void OnResponse(const TObjectServiceProxy::TErrorOrRspExecutePtr& rspOrError)
-        {
-            if (!rspOrError.IsOK()) {
-                YT_LOG_DEBUG("Cache bypass request failed (RequestId: %v)",
-                    Context_->GetRequestId());
-                for (auto& promise : Promises_) {
-                    promise.Set(rspOrError);
-                }
-                return;
-            }
-
-            YT_LOG_DEBUG("Cache bypass request succeeded (RequestId: %v)",
-                Context_->GetRequestId());
-
-            const auto& rsp = rspOrError.Value();
-            YT_VERIFY(rsp->part_counts_size() == std::ssize(Promises_));
-
-            int attachmentIndex = 0;
-            const auto& attachments = rsp->Attachments();
-            for (int subresponseIndex = 0; subresponseIndex < rsp->part_counts_size(); ++subresponseIndex) {
-                int partCount = rsp->part_counts(subresponseIndex);
-                auto parts = std::vector<TSharedRef>(
-                    attachments.begin() + attachmentIndex,
-                    attachments.begin() + attachmentIndex + partCount);
-                Promises_[subresponseIndex].Set({
-                    TSharedRefArray(std::move(parts), TSharedRefArray::TMoveParts{}),
-                    NHydra::NullRevision,
-                    1.0
-                });
-                attachmentIndex += partCount;
-            }
-        }
-    };
-
     const TCachingObjectServiceConfigPtr Config_;
     const TObjectServiceCachePtr Cache_;
     const TCellId CellId_;
@@ -200,8 +101,7 @@ DEFINE_RPC_SERVICE_METHOD(TCachingObjectService, Execute)
     int attachmentIndex = 0;
     const auto& attachments = request->Attachments();
 
-    std::vector<TFuture<TSubrequestResponse>> asyncMasterResponseMessages;
-    TIntrusivePtr<TMasterRequest> masterRequest;
+    std::vector<TFuture<TObjectServiceCacheEntryPtr>> cacheEntryFutures;
 
     for (int subrequestIndex = 0; subrequestIndex < request->part_counts_size(); ++subrequestIndex) {
         int partCount = request->part_counts(subrequestIndex);
@@ -222,162 +122,143 @@ DEFINE_RPC_SERVICE_METHOD(TCachingObjectService, Execute)
 
         const auto& ypathExt = subrequestHeader.GetExtension(TYPathHeaderExt::ypath_header_ext);
 
-        if (subrequestHeader.HasExtension(TCachingHeaderExt::caching_header_ext)) {
-            const auto& cachingRequestHeaderExt = subrequestHeader.GetExtension(TCachingHeaderExt::caching_header_ext);
+        if (!subrequestHeader.HasExtension(TCachingHeaderExt::caching_header_ext)) {
+            THROW_ERROR_EXCEPTION("Subrequest is lacking caching header");
+        }
 
-            auto refreshRevision = cachingRequestHeaderExt.refresh_revision();
+        const auto& cachingRequestHeaderExt = subrequestHeader.GetExtension(TCachingHeaderExt::caching_header_ext);
 
-            TObjectServiceCacheKey key(
-                CellTagFromId(CellId_),
-                cachingRequestHeaderExt.disable_per_user_cache() ? TString() : context->GetAuthenticationIdentity().User,
-                ypathExt.target_path(),
-                subrequestHeader.service(),
-                subrequestHeader.method(),
-                subrequestMessage[1]);
+        auto refreshRevision = cachingRequestHeaderExt.refresh_revision();
 
-            if (ypathExt.mutating()) {
-                THROW_ERROR_EXCEPTION("Cannot cache responses for mutating requests");
+        TObjectServiceCacheKey key(
+            CellTagFromId(CellId_),
+            cachingRequestHeaderExt.disable_per_user_cache() ? TString() : context->GetAuthenticationIdentity().User,
+            ypathExt.target_path(),
+            subrequestHeader.service(),
+            subrequestHeader.method(),
+            subrequestMessage[1]);
+
+        if (ypathExt.mutating()) {
+            THROW_ERROR_EXCEPTION("Cannot cache responses for mutating requests");
+        }
+
+        if (subrequestMessage.Size() > 2) {
+            THROW_ERROR_EXCEPTION("Cannot cache responses for requests with attachments");
+        }
+
+        YT_LOG_DEBUG("Serving subrequest from cache (RequestId: %v, SubrequestIndex: %v, Key: %v)",
+            requestId,
+            subrequestIndex,
+            key);
+
+        auto expireAfterSuccessfulUpdateTime = FromProto<TDuration>(cachingRequestHeaderExt.expire_after_successful_update_time());
+        auto expireAfterFailedUpdateTime = FromProto<TDuration>(cachingRequestHeaderExt.expire_after_failed_update_time());
+
+        auto cacheTtlRatio = CacheTtlRatio_.load();
+        auto nodeExpireAfterSuccessfulUpdateTime = expireAfterSuccessfulUpdateTime * cacheTtlRatio;
+        auto nodeExpireAfterFailedUpdateTime = expireAfterFailedUpdateTime * cacheTtlRatio;
+
+        bool cachingEnabled = CachingEnabled_.load(std::memory_order_relaxed) && !cachingRequestHeaderExt.disable_second_level_cache();
+        auto cookie = Cache_->BeginLookup(
+            requestId,
+            key,
+            cachingEnabled ? nodeExpireAfterSuccessfulUpdateTime : expireAfterSuccessfulUpdateTime,
+            cachingEnabled ? nodeExpireAfterFailedUpdateTime : expireAfterFailedUpdateTime,
+            refreshRevision);
+
+        cacheEntryFutures.push_back(cookie.GetValue());
+
+        if (cookie.IsActive()) {
+            TObjectServiceProxy proxy(MasterChannel_);
+            auto req = proxy.Execute();
+            req->add_part_counts(subrequestMessage.Size());
+            req->Attachments().insert(
+                req->Attachments().end(),
+                subrequestMessage.Begin(),
+                subrequestMessage.End());
+            SetCurrentAuthenticationIdentity(req);
+
+            if (cachingEnabled) {
+                auto* balancingHeaderExt = req->Header().MutableExtension(NRpc::NProto::TBalancingExt::balancing_ext);
+                balancingHeaderExt->set_enable_stickiness(true);
+                balancingHeaderExt->set_sticky_group_size(1);
+
+                auto* cachingHeaderExt = req->Header().MutableExtension(NYTree::NProto::TCachingHeaderExt::caching_header_ext);
+                cachingHeaderExt->set_disable_per_user_cache(cachingRequestHeaderExt.disable_per_user_cache());
+                cachingHeaderExt->set_expire_after_successful_update_time(ToProto<i64>(expireAfterSuccessfulUpdateTime - nodeExpireAfterSuccessfulUpdateTime));
+                cachingHeaderExt->set_expire_after_failed_update_time(ToProto<i64>(expireAfterFailedUpdateTime - nodeExpireAfterFailedUpdateTime));
+                cachingHeaderExt->set_refresh_revision(refreshRevision);
+            } else {
+                req->Header().ClearExtension(NYTree::NProto::TCachingHeaderExt::caching_header_ext);
+                req->Header().ClearExtension(NRpc::NProto::TBalancingExt::balancing_ext);
             }
 
-            if (subrequestMessage.Size() > 2) {
-                THROW_ERROR_EXCEPTION("Cannot cache responses for requests with attachments");
-            }
+            req->Invoke().Apply(
+                BIND([this, this_ = MakeStrong(this), cookie = std::move(cookie), requestId] (
+                    const TObjectServiceProxy::TErrorOrRspExecutePtr& rspOrError) mutable
+                {
+                    if (!rspOrError.IsOK()) {
+                        YT_LOG_WARNING(rspOrError, "Cache population request failed (Key: %v)", cookie.GetKey());
+                        cookie.Cancel(rspOrError);
+                        return;
+                    }
 
-            YT_LOG_DEBUG("Serving subrequest from cache (RequestId: %v, SubrequestIndex: %v, Key: %v)",
-                requestId,
-                subrequestIndex,
-                key);
+                    const auto& rsp = rspOrError.Value();
+                    YT_VERIFY(rsp->part_counts_size() == 1);
+                    auto responseMessage = TSharedRefArray(rsp->Attachments(), TSharedRefArray::TCopyParts{});
 
-            auto expireAfterSuccessfulUpdateTime = FromProto<TDuration>(cachingRequestHeaderExt.expire_after_successful_update_time());
-            auto expireAfterFailedUpdateTime = FromProto<TDuration>(cachingRequestHeaderExt.expire_after_failed_update_time());
+                    TResponseHeader responseHeader;
+                    if (!TryParseResponseHeader(responseMessage, &responseHeader)) {
+                        YT_LOG_WARNING("Error parsing cache population response header (Key: %v)", cookie.GetKey());
+                        cookie.Cancel(TError(NRpc::EErrorCode::ProtocolError, "Error parsing response header"));
+                        return;
+                    }
 
-            auto cacheTtlRatio = CacheTtlRatio_.load();
-            auto nodeExpireAfterSuccessfulUpdateTime = expireAfterSuccessfulUpdateTime * cacheTtlRatio;
-            auto nodeExpireAfterFailedUpdateTime = expireAfterFailedUpdateTime * cacheTtlRatio;
+                    auto responseError = FromProto<TError>(responseHeader.error());
+                    auto revision = rsp->revisions_size() > 0 ? rsp->revisions(0) : NHydra::NullRevision;
 
-            bool cachingEnabled = CachingEnabled_.load(std::memory_order_relaxed) && !cachingRequestHeaderExt.disable_second_level_cache();
-            auto cookie = Cache_->BeginLookup(
-                requestId,
-                key,
-                cachingEnabled ? nodeExpireAfterSuccessfulUpdateTime : expireAfterSuccessfulUpdateTime,
-                cachingEnabled ? nodeExpireAfterFailedUpdateTime : expireAfterFailedUpdateTime,
-                refreshRevision);
+                    bool cachingEnabled = rsp->caching_enabled();
+                    if (CachingEnabled_.exchange(cachingEnabled) != cachingEnabled) {
+                        YT_LOG_INFO("Changing next level object service cache mode (Enable: %v)", cachingEnabled);
+                    }
 
-            asyncMasterResponseMessages.push_back(
-                cookie.GetValue().Apply(BIND([] (const TObjectServiceCacheEntryPtr& entry) {
-                    return TSubrequestResponse{
-                        entry->GetResponseMessage(),
-                        entry->GetRevision(),
-                        entry->GetByteRate()
-                    };
-                })));
-
-            if (cookie.IsActive()) {
-                TObjectServiceProxy proxy(MasterChannel_);
-                auto req = proxy.Execute();
-                req->add_part_counts(subrequestMessage.Size());
-                req->Attachments().insert(
-                    req->Attachments().end(),
-                    subrequestMessage.Begin(),
-                    subrequestMessage.End());
-                SetCurrentAuthenticationIdentity(req);
-
-                if (cachingEnabled) {
-                    auto* balancingHeaderExt = req->Header().MutableExtension(NRpc::NProto::TBalancingExt::balancing_ext);
-                    balancingHeaderExt->set_enable_stickiness(true);
-                    balancingHeaderExt->set_sticky_group_size(1);
-
-                    auto* cachingHeaderExt = req->Header().MutableExtension(NYTree::NProto::TCachingHeaderExt::caching_header_ext);
-                    cachingHeaderExt->set_disable_per_user_cache(cachingRequestHeaderExt.disable_per_user_cache());
-                    cachingHeaderExt->set_expire_after_successful_update_time(ToProto<i64>(expireAfterSuccessfulUpdateTime - nodeExpireAfterSuccessfulUpdateTime));
-                    cachingHeaderExt->set_expire_after_failed_update_time(ToProto<i64>(expireAfterFailedUpdateTime - nodeExpireAfterFailedUpdateTime));
-                    cachingHeaderExt->set_refresh_revision(refreshRevision);
-                } else {
-                    req->Header().ClearExtension(NYTree::NProto::TCachingHeaderExt::caching_header_ext);
-                    req->Header().ClearExtension(NRpc::NProto::TBalancingExt::balancing_ext);
-                }
-
-                req->Invoke().Apply(
-                    BIND([this, this_ = MakeStrong(this), cookie = std::move(cookie), requestId] (
-                        const TObjectServiceProxy::TErrorOrRspExecutePtr& rspOrError) mutable
-                    {
-                        if (!rspOrError.IsOK()) {
-                            YT_LOG_WARNING(rspOrError, "Cache population request failed (Key: %v)", cookie.GetKey());
-                            cookie.Cancel(rspOrError);
-                            return;
-                        }
-
-                        const auto& rsp = rspOrError.Value();
-                        YT_VERIFY(rsp->part_counts_size() == 1);
-                        auto responseMessage = TSharedRefArray(rsp->Attachments(), TSharedRefArray::TCopyParts{});
-
-                        TResponseHeader responseHeader;
-                        if (!TryParseResponseHeader(responseMessage, &responseHeader)) {
-                            YT_LOG_WARNING("Error parsing cache population response header (Key: %v)", cookie.GetKey());
-                            cookie.Cancel(TError(NRpc::EErrorCode::ProtocolError, "Error parsing response header"));
-                            return;
-                        }
-
-                        auto responseError = FromProto<TError>(responseHeader.error());
-                        auto revision = rsp->revisions_size() > 0 ? rsp->revisions(0) : NHydra::NullRevision;
-
-                        bool cachingEnabled = rsp->caching_enabled();
-                        if (CachingEnabled_.exchange(cachingEnabled) != cachingEnabled) {
-                            YT_LOG_INFO("Changing next level object service cache mode (Enable: %v)", cachingEnabled);
-                        }
-
-                        Cache_->EndLookup(requestId, std::move(cookie), responseMessage, revision, responseError.IsOK());
-                    }));
-            }
-        } else {
-            YT_LOG_DEBUG("Subrequest does not support caching, bypassing cache (RequestId: %v, SubrequestIndex: %v)",
-                requestId,
-                subrequestIndex);
-
-            if (!masterRequest) {
-                masterRequest = New<TMasterRequest>(MasterChannel_, context, Logger);
-            }
-
-            asyncMasterResponseMessages.push_back(masterRequest->Add(subrequestMessage));
+                    Cache_->EndLookup(requestId, std::move(cookie), responseMessage, revision, responseError.IsOK());
+                }));
         }
     }
 
-    if (masterRequest) {
-        masterRequest->Invoke();
-    }
-
-    AllSucceeded(asyncMasterResponseMessages)
-        .Subscribe(BIND([=, this_ = MakeStrong(this)] (const TErrorOr<std::vector<TSubrequestResponse>>& masterResponseMessagesOrError) {
-            if (!masterResponseMessagesOrError.IsOK()) {
-                context->Reply(masterResponseMessagesOrError);
+    AllSucceeded(cacheEntryFutures)
+        .Subscribe(BIND([=, this_ = MakeStrong(this)] (const TErrorOr<std::vector<TObjectServiceCacheEntryPtr>>& cacheEntriesOrError) {
+            if (!cacheEntriesOrError.IsOK()) {
+                context->Reply(cacheEntriesOrError);
                 return;
             }
 
-            const auto& masterResponseMessages = masterResponseMessagesOrError.Value();
+            const auto& cacheEntries = cacheEntriesOrError.Value();
 
             auto& responseAttachments = response->Attachments();
             for (int subrequestIndex = 0; subrequestIndex < request->part_counts_size(); ++subrequestIndex) {
-                const auto& subrequestResponse = masterResponseMessages[subrequestIndex];
+                const auto& cacheEntry = cacheEntries[subrequestIndex];
                 if (request->has_current_sticky_group_size()) {
                     auto currentStickyGroupSize = request->current_sticky_group_size();
-                    auto totalSize = subrequestResponse.ByteRate * currentStickyGroupSize;
+                    auto totalSize = cacheEntry->GetByteRate() * currentStickyGroupSize;
                     auto advisedStickyGroupSize = 1 + static_cast<int>(totalSize / EntryByteRateLimit_.load());
                     response->add_advised_sticky_group_size(advisedStickyGroupSize);
                 }
-                const auto& masterResponseMessage = subrequestResponse.Message;
-                response->add_part_counts(masterResponseMessage.Size());
+                const auto& responseMessage = cacheEntry->GetResponseMessage();
+                response->add_part_counts(responseMessage.Size());
                 responseAttachments.insert(
                     responseAttachments.end(),
-                    masterResponseMessage.Begin(),
-                    masterResponseMessage.End());
+                    responseMessage.Begin(),
+                    responseMessage.End());
             }
 
-            for (const auto& [message, revision, rate] : masterResponseMessages) {
-                if (revision == NHydra::NullRevision) {
+            for (const auto& cacheEntry : cacheEntries) {
+                if (cacheEntry->GetRevision() == NHydra::NullRevision) {
                     response->clear_revisions();
                     break;
                 }
-                response->add_revisions(revision);
+                response->add_revisions(cacheEntry->GetRevision());
             }
 
             response->set_caching_enabled(true);
