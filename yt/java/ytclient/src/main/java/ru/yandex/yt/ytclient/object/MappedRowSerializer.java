a9affd6669b80c942a7f231ae338ba52e36c5a2f
nadya73 <nadya73@ytsaurus.tech>
2022-01-13 13:09:44 +0300

    YT-15673: Return typeV3 instead ColumnValueType in YTreeSerializer::getColumnValueType Return typeV3 instead ColumnValueType in YTreeSerializer::getColumnValueType

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/object/MappedRowSerializer.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/object/MappedRowSerializer.java
@@ -18,6 +18,7 @@ import ru.yandex.inside.yt.kosher.impl.ytree.object.serializers.YTreeOptionSeria
 import ru.yandex.inside.yt.kosher.impl.ytree.object.serializers.YTreeOptionalSerializer;
 import ru.yandex.inside.yt.kosher.impl.ytree.serialization.YTreeBinarySerializer;
 import ru.yandex.inside.yt.kosher.impl.ytree.serialization.YTreeStateSupport;
+import ru.yandex.type_info.TiType;
 import ru.yandex.yson.ClosableYsonConsumer;
 import ru.yandex.yson.YsonConsumer;
 import ru.yandex.yt.rpcproxy.TRowsetDescriptor;
@@ -90,7 +91,7 @@ public class MappedRowSerializer<T> implements WireRowSerializer<T> {
         }
     }
 
-    static ColumnValueType asType(YTreeSerializer<?> serializer) {
+    static TiType asType(YTreeSerializer<?> serializer) {
         return serializer.getColumnValueType();
     }
 
@@ -105,9 +106,12 @@ public class MappedRowSerializer<T> implements WireRowSerializer<T> {
             } else {
                 hasKeys |= isKeyField;
 
-                builder.add(new ColumnSchema(field.key, asType(serializer),
-                        isKeyField ? field.sortOrder : null, null, null,
-                        field.aggregate, null, isKeyField));
+                ColumnSchema.Builder columnSchemaBuilder = ColumnSchema.builder(field.key, asType(serializer))
+                        .setAggregate(field.aggregate);
+                if (field.sortOrder != null) {
+                    columnSchemaBuilder.setSortOrder(field.sortOrder);
+                }
+                builder.add(columnSchemaBuilder.build());
             }
         }
 
