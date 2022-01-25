41b9bf4c9f10f15c30e9cb5054f3e2ab5e14a533
ignat <ignat@ytsaurus.tech>
2022-01-25 11:10:02 +0300

    Better logging in python API tests

--- yt/python/yt/wrapper/driver.py
+++ yt/python/yt/wrapper/driver.py
@@ -73,12 +73,7 @@ def make_request(command_name,
                  client=None):
     backend = get_backend_type(client)
 
-    enable_params_logging = get_config(client)["enable_logging_for_params_changes"]
-    if enable_params_logging:
-        logger.debug("Start deepcopy (pid: %d)", os.getpid())
     command_params = deepcopy(get_option("COMMAND_PARAMS", client))
-    if enable_params_logging:
-        logger.debug("Finish deepcopy (pid: %d)", os.getpid())
 
     for key in _DEFAULT_COMMAND_PARAMS:
         if key in command_params and command_params[key] == _DEFAULT_COMMAND_PARAMS[key]:
