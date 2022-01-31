83183cb455800e4be4e01b09642bb9914bc65b09
renadeen <renadeen@ytsaurus.tech>
2022-02-01 01:30:00 +0300

    Some yson struct in ytserver.

--- yt/yt/server/node/cellar_node/config.h
+++ yt/yt/server/node/cellar_node/config.h
@@ -4,7 +4,7 @@
 
 #include <yt/yt/server/lib/cellar_agent/config.h>
 
-#include <yt/yt/core/ytree/yson_serializable.h>
+#include <yt/yt/core/ytree/yson_struct.h>
 
 #include <yt/yt/core/concurrency/config.h>
 
@@ -13,7 +13,7 @@ namespace NYT::NCellarNode {
 ////////////////////////////////////////////////////////////////////////////////
 
 class TMasterConnectorConfig
-    : public NYTree::TYsonSerializable
+    : public NYTree::TYsonStruct
 {
 public:
     //! Period between consequent cellar node heartbeats.
@@ -25,15 +25,9 @@ public:
     //! Timeout of the cellar node heartbeat RPC request.
     TDuration HeartbeatTimeout;
 
-    TMasterConnectorConfig()
-    {
-        RegisterParameter("heartbeat_period", HeartbeatPeriod)
-            .Default(TDuration::Seconds(5));
-        RegisterParameter("heartbeat_period_splay", HeartbeatPeriodSplay)
-            .Default(TDuration::Seconds(1));
-        RegisterParameter("heartbeat_timeout", HeartbeatTimeout)
-            .Default(TDuration::Seconds(60));
-    }
+    REGISTER_YSON_STRUCT(TMasterConnectorConfig);
+
+    static void Register(TRegistrar registrar);
 };
 
 DEFINE_REFCOUNTED_TYPE(TMasterConnectorConfig)
@@ -41,7 +35,7 @@ DEFINE_REFCOUNTED_TYPE(TMasterConnectorConfig)
 ////////////////////////////////////////////////////////////////////////////////
 
 class TMasterConnectorDynamicConfig
-    : public NYTree::TYsonSerializable
+    : public NYTree::TYsonStruct
 {
 public:
     //! Period between consequent cellar node heartbeats.
@@ -50,13 +44,9 @@ public:
     //! Splay for cellar node heartbeats.
     std::optional<TDuration> HeartbeatPeriodSplay;
 
-    TMasterConnectorDynamicConfig()
-    {
-        RegisterParameter("heartbeat_period", HeartbeatPeriod)
-            .Default();
-        RegisterParameter("heartbeat_period_splay", HeartbeatPeriodSplay)
-            .Default();
-    }
+    REGISTER_YSON_STRUCT(TMasterConnectorDynamicConfig);
+
+    static void Register(TRegistrar registrar);
 };
 
 DEFINE_REFCOUNTED_TYPE(TMasterConnectorDynamicConfig)
@@ -64,20 +54,16 @@ DEFINE_REFCOUNTED_TYPE(TMasterConnectorDynamicConfig)
 ////////////////////////////////////////////////////////////////////////////////
 
 class TCellarNodeDynamicConfig
-    : public NYTree::TYsonSerializable
+    : public NYTree::TYsonStruct
 {
 public:
     NCellarAgent::TCellarManagerDynamicConfigPtr CellarManager;
 
     TMasterConnectorDynamicConfigPtr MasterConnector;
 
-    TCellarNodeDynamicConfig()
-    {
-        RegisterParameter("cellar_manager", CellarManager)
-            .DefaultNew();
-        RegisterParameter("master_connector", MasterConnector)
-            .DefaultNew();
-    }
+    REGISTER_YSON_STRUCT(TCellarNodeDynamicConfig);
+
+    static void Register(TRegistrar registrar);
 };
 
 DEFINE_REFCOUNTED_TYPE(TCellarNodeDynamicConfig)
@@ -85,21 +71,16 @@ DEFINE_REFCOUNTED_TYPE(TCellarNodeDynamicConfig)
 ////////////////////////////////////////////////////////////////////////////////
 
 class TCellarNodeConfig
-    : public NYTree::TYsonSerializable
+    : public NYTree::TYsonStruct
 {
 public:
     NCellarAgent::TCellarManagerConfigPtr CellarManager;
 
     TMasterConnectorConfigPtr MasterConnector;
 
-    TCellarNodeConfig()
-    {
-        RegisterParameter("cellar_manager", CellarManager)
-            .DefaultNew();
+    REGISTER_YSON_STRUCT(TCellarNodeConfig);
 
-        RegisterParameter("master_connector", MasterConnector)
-            .DefaultNew();
-    }
+    static void Register(TRegistrar registrar);
 };
 
 DEFINE_REFCOUNTED_TYPE(TCellarNodeConfig)
