048f132d74bab49da4660adc77b2b101c931a906
akozhikhov <akozhikhov@ytsaurus.tech>
2022-02-03 11:25:41 +0300

    Cosmetics in error message

--- yt/yt/ytlib/hive/cell_directory.cpp
+++ yt/yt/ytlib/hive/cell_directory.cpp
@@ -173,7 +173,7 @@ public:
     {
         auto channel = FindChannelByCellId(cellId, peerKind);
         if (!channel) {
-            THROW_ERROR_EXCEPTION("No cell with %v is known",
+            THROW_ERROR_EXCEPTION("No cell with id %v is known",
                 cellId);
         }
         return channel;
