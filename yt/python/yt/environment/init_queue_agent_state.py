8da7d54fd127ffde2ca9d6621e8ae794a0c49593
max42 <max42@ytsaurus.tech>
2022-03-19 18:55:37 +0300

    YT-16644: Introduce init_queue_agent_state script.

---
+++ yt/python/yt/environment/init_queue_agent_state.py
@@ -0,0 +1,32 @@
+QUEUE_TABLE_SCHEMA = [
+    {"name": "cluster", "type": "string", "sort_order": "ascending"},
+    {"name": "path", "type": "string", "sort_order": "ascending"},
+    {"name": "row_revision", "type": "uint64"},
+    {"name": "revision", "type": "uint64"},
+    {"name": "object_type", "type": "string"},
+    {"name": "dynamic", "type": "boolean"},
+    {"name": "sorted", "type": "boolean"},
+]
+
+CONSUMER_TABLE_SCHEMA = [
+    {"name": "cluster", "type": "string", "sort_order": "ascending"},
+    {"name": "path", "type": "string", "sort_order": "ascending"},
+    {"name": "row_revision", "type": "uint64"},
+    {"name": "revision", "type": "uint64"},
+    {"name": "target_cluster", "type": "string"},
+    {"name": "target_path", "type": "string"},
+    {"name": "object_type", "type": "string"},
+    {"name": "treat_as_consumer", "type": "boolean"},
+    {"name": "schema", "type": "any"},
+    {"name": "vital", "type": "boolean"},
+]
+
+
+def create_tables(client, root="//sys/queue_agents", skip_queues=False, skip_consumers=False, queue_table_schema=None,
+                  consumer_table_schema=None):
+    queue_table_schema = queue_table_schema or QUEUE_TABLE_SCHEMA
+    consumer_table_schema = consumer_table_schema or CONSUMER_TABLE_SCHEMA
+    if not skip_queues:
+        client.create("table", root + "/queues", attributes={"dynamic": True, "schema": queue_table_schema})
+    if not skip_consumers:
+        client.create("table", root + "/consumers", attributes={"dynamic": True, "schema": consumer_table_schema})
