624514240d3df87c79f44a24f60797c344eae659
egor-gutrov <egor-gutrov@yandex-team.ru>
2022-04-15 17:32:00 +0300

    Fixed preserve_expiration_timeout parameter passing

--- yt/yt/ytlib/api/native/client_cypress_impl.cpp
+++ yt/yt/ytlib/api/native/client_cypress_impl.cpp
@@ -68,6 +68,7 @@ void SetCloneNodeBaseRequestParameters(
     req->set_preserve_creation_time(options.PreserveCreationTime);
     req->set_preserve_modification_time(options.PreserveModificationTime);
     req->set_preserve_expiration_time(options.PreserveExpirationTime);
+    req->set_preserve_expiration_timeout(options.PreserveExpirationTimeout);
     req->set_preserve_owner(options.PreserveOwner);
     req->set_preserve_acl(options.PreserveAcl);
     req->set_recursive(options.Recursive);
