6638073fd959cc38f85cf4bba598436509ae4639
babenko <babenko@ytsaurus.tech>
2022-06-09 00:16:30 +0300

    Reduce common.h

--- yt/yt/core/misc/common.h
+++ yt/yt/core/misc/common.h
@@ -12,17 +12,11 @@
 
 #include <util/generic/hash.h>
 #include <util/generic/hash_set.h>
-#include <util/generic/singleton.h>
 #include <util/generic/string.h>
 
-#include <util/string/cast.h>
-#include <util/string/printf.h>
-
-#include <library/cpp/deprecated/atomic/atomic.h>
 #include <util/system/compiler.h>
 #include <util/system/defaults.h>
 
-#include <any>
 #include <list>
 #include <map>
 #include <queue>
