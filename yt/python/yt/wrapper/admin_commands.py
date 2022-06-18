dac9be475189328721946699b35c47d043b74676
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2022-06-18 23:42:28 +0300

    Suspend/resume tablet cells script

--- yt/python/yt/wrapper/admin_commands.py
+++ yt/python/yt/wrapper/admin_commands.py
@@ -1,6 +1,15 @@
 from .driver import make_request
 
 
+def build_snapshot(cell_id=None, client=None):
+    """Builds snapshot of a given cell."""
+    params = {
+        "cell_id": cell_id,
+    }
+
+    return make_request("build_snapshot", params=params, client=client)
+
+
 def switch_leader(cell_id=None, new_leader_address=None, client=None):
     """Switch leader of given master cell."""
     params = {
@@ -16,3 +25,21 @@ def add_switch_leader_parser(subparsers):
     parser.set_defaults(func=switch_leader)
     parser.add_argument("--cell-id")
     parser.add_argument("--new-leader-address", type=str)
+
+
+def suspend_tablet_cells(cell_ids, client=None):
+    """Suspends listed tablet cells."""
+    params = {
+        "cell_ids": cell_ids,
+    }
+
+    return make_request("suspend_tablet_cells", params=params, client=client)
+
+
+def resume_tablet_cells(cell_ids, client=None):
+    """Resumes listed tablet cells."""
+    params = {
+        "cell_ids": cell_ids,
+    }
+
+    return make_request("resume_tablet_cells", params=params, client=client)
