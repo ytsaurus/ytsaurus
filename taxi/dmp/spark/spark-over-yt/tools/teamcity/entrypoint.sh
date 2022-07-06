420dd0574bb71007307003938946a49bfe310738
devsagul <devsagul@yandex-team.ru>
2022-07-06 11:31:16 +0300

    fix tools: create users inside docker-compose entrypoint Relates: SPYT-24 Tests: local

--- taxi/dmp/spark/spark-over-yt/tools/teamcity/entrypoint.sh
+++ taxi/dmp/spark/spark-over-yt/tools/teamcity/entrypoint.sh
@@ -1,9 +1,10 @@
-chown -R $USER $HOME
+groupadd -o -g $HOST_DOCKER_GROUP_ID host_docker
+useradd -M -d $HOME -g host_docker -G docker -o -u $UID -s /bin/sh $USER
+
 chown -R $USER /cache
 chown -R $USER /reports
 
 export YT_TOKEN=non_existent_token
-export HOME=/app
 su --shell /bin/sh --preserve-environment --command 'sbt -Duser.home=/app --sbt-dir /cache/sbt --sbt-boot /cache/sbt/boot --ivy /cache/ivy test' $USER
 code=$?
 su --shell /bin/sh --preserve-environment --command 'find -type d -name test-reports -exec cp -r --parents {} /reports \;' $USER
