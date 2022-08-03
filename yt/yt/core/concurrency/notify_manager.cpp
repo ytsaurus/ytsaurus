de9a839748bceaca52c86718501f58387506958c
lukyan <lukyan@ytsaurus.tech>
2022-08-04 02:30:58 +0300

    YT-16118: Simplify notify manager and make it work with not sequentially consistent queues

--- yt/yt/core/concurrency/notify_manager.cpp
+++ yt/yt/core/concurrency/notify_manager.cpp
@@ -18,58 +18,62 @@ TNotifyManager::TNotifyManager(TIntrusivePtr<NThreading::TEventCount> eventCount
     : EventCount_(std::move(eventCount))
 { }
 
-TCpuInstant TNotifyManager::ResetMinEnqueuedAt()
+TCpuInstant TNotifyManager::GetMinEnqueuedAt() const
 {
-    // Disables notifies of already enqueued actions from invoke and
-    // allows to set MinEnqueuedAt in NotifyFromInvoke for new actions.
-    return MinEnqueuedAt_.exchange(std::numeric_limits<TCpuInstant>::max());
+    return MinEnqueuedAt_.load(std::memory_order_acquire);
 }
 
-void TNotifyManager::NotifyFromInvoke(TCpuInstant cpuInstant, bool updateEnqueuedAt, int threadCount)
+TCpuInstant TNotifyManager::UpdateMinEnqueuedAt(TCpuInstant newMinEnqueuedAt)
 {
-    if (updateEnqueuedAt) {
-        auto minEnqueuedAt = std::numeric_limits<TCpuInstant>::max();
-        MinEnqueuedAt_.compare_exchange_strong(minEnqueuedAt, cpuInstant);
-    }
-
-    auto minEnqueuedAt = MinEnqueuedAt_.load(std::memory_order_acquire);
-    auto waitTime = CpuDurationToDuration(cpuInstant - minEnqueuedAt);
-
-    // TODO(lukyan): Explicitly check running thread count.
-    auto waiters = WaitingThreads.load();
-
-    bool noRunningThreads = waiters == threadCount;
-
-    YT_LOG_TRACE("Notify from invoke (Waiters: %v, WaitTime: %v, MinEnqueuedAt: %v, Decision: %v)",
-        waiters,
-        waitTime,
-        minEnqueuedAt,
-        noRunningThreads || waitTime > WaitLimit);
-
-    if (noRunningThreads || waitTime > WaitLimit) {
-        NotifyOne(cpuInstant);
-    }
-}
-
-void TNotifyManager::NotifyAfterFetch(TCpuInstant cpuInstant, TCpuInstant newMinEnqueuedAt)
-{
-    // Can be set in NotifyFromInvoke.
     auto minEnqueuedAt = MinEnqueuedAt_.load();
 
-    // Expecting max value sentinel in minEnqueuedAt or minEnqueuedAt from NotifyFromInvoke.
-    while (newMinEnqueuedAt < minEnqueuedAt) {
-        YT_VERIFY(newMinEnqueuedAt != std::numeric_limits<TCpuInstant>::max());
+    while (newMinEnqueuedAt > minEnqueuedAt) {
         if (MinEnqueuedAt_.compare_exchange_weak(minEnqueuedAt, newMinEnqueuedAt)) {
             minEnqueuedAt = newMinEnqueuedAt;
             break;
         }
     }
 
+    return minEnqueuedAt;
+}
+
+void TNotifyManager::ResetMinEnqueuedAtIfEqual(TCpuInstant expected)
+{
+    // Resetting MinEnqueuedAt is needed to force NotifyOne from NotifyFromInvoke when there are no
+    // running threads.
+    // MinEnqueuedAt denotes no running threads.
+    MinEnqueuedAt_.compare_exchange_strong(expected, 0);
+}
+
+void TNotifyManager::NotifyFromInvoke(TCpuInstant cpuInstant)
+{
+    auto minEnqueuedAt = GetMinEnqueuedAt();
+    auto waitTime = CpuDurationToDuration(cpuInstant - minEnqueuedAt);
+
+    auto waiters = GetWaiters();
+
+    YT_LOG_TRACE("Notify from invoke (Waiters: %v, WaitTime: %v, MinEnqueuedAt: %v, Decision: %v)",
+        waiters,
+        waitTime,
+        minEnqueuedAt,
+        waitTime > WaitLimit);
+
+    if (waitTime > WaitLimit) {
+        NotifyOne(cpuInstant);
+    }
+}
+
+void TNotifyManager::NotifyAfterFetch(TCpuInstant cpuInstant, TCpuInstant newMinEnqueuedAt)
+{
+    auto minEnqueuedAt = UpdateMinEnqueuedAt(newMinEnqueuedAt);
+
+    YT_VERIFY(minEnqueuedAt > 0);
+
     // If there are actions and wait time is small do not wakeup other threads.
     auto waitTime = CpuDurationToDuration(cpuInstant - minEnqueuedAt);
 
     if (waitTime > WaitLimit) {
-        auto waiters = WaitingThreads.load();
+        auto waiters = GetWaiters();
         YT_LOG_TRACE("Notify after fetch (Waiters: %v, WaitTime: %v, MinEnqueuedAt: %v)",
             waiters,
             waitTime,
@@ -77,6 +81,9 @@ void TNotifyManager::NotifyAfterFetch(TCpuInstant cpuInstant, TCpuInstant newMin
 
         NotifyOne(cpuInstant);
     }
+
+    // Reset LockedInstant to suppress action stuck warnings in case of progress.
+    LockedInstant_ = cpuInstant;
 }
 
 void TNotifyManager::Wait(NThreading::TEventCount::TCookie cookie, std::function<bool()> isStopping)
@@ -97,17 +104,19 @@ void TNotifyManager::Wait(NThreading::TEventCount::TCookie cookie, std::function
                 break;
             }
 
-            // Check wait time.
-            auto minEnqueuedAt = MinEnqueuedAt_.load();
-            auto cpuInstant = GetCpuInstant();
-            auto waitTime = CpuDurationToDuration(cpuInstant - minEnqueuedAt);
+            if (GetQueueSize() > 0) {
+                // Check wait time.
+                auto minEnqueuedAt = GetMinEnqueuedAt();
+                auto cpuInstant = GetCpuInstant();
+                auto waitTime = CpuDurationToDuration(cpuInstant - minEnqueuedAt);
 
-            if (waitTime > WaitLimit) {
-                YT_LOG_DEBUG("Wake up by timeout (WaitTime: %v, MinEnqueuedAt: %v)",
-                    waitTime,
-                    minEnqueuedAt);
+                if (waitTime > WaitLimit) {
+                    YT_LOG_DEBUG("Wake up by timeout (WaitTime: %v, MinEnqueuedAt: %v)",
+                        waitTime,
+                        minEnqueuedAt);
 
-                break;
+                    break;
+                }
             }
 
             cookie = EventCount_->PrepareWait();
@@ -154,7 +163,6 @@ NThreading::TEventCount* TNotifyManager::GetEventCount()
 // Returns true if was locked.
 bool TNotifyManager::UnlockNotifies()
 {
-    LockedInstant_ = std::numeric_limits<TCpuInstant>::max();
     return NotifyLock_.exchange(false);
 }
 
@@ -163,20 +171,42 @@ void TNotifyManager::NotifyOne(TCpuInstant cpuInstant)
     if (!NotifyLock_.exchange(true)) {
         LockedInstant_ = cpuInstant;
         YT_LOG_TRACE("Futex Wake (MinEnqueuedAt: %v)",
-            MinEnqueuedAt_.load());
+            GetMinEnqueuedAt());
         EventCount_->NotifyOne();
     } else {
         auto lockedInstant = LockedInstant_.load();
         auto waitTime = CpuDurationToDuration(cpuInstant - lockedInstant);
         if (waitTime > TDuration::Seconds(30)) {
-            // Notifications are locked during more than 30 seconds.
+            // Notifications are locked during more than 90 seconds.
             YT_LOG_WARNING("Action is probably stuck (MinEnqueuedAt: %v, LockedInstant: %v, WaitTime: %v, QueueSize: %v)",
-                MinEnqueuedAt_.load(),
+                GetMinEnqueuedAt(),
                 lockedInstant,
                 waitTime,
                 GetQueueSize());
         }
     }
+}
+
+int TNotifyManager::GetWaiters()
+{
+#ifndef NDEBUG
+    return WaitingThreads_.load();
+#else
+    return -1;
+#endif
+}
+
+void TNotifyManager::IncrementWaiters()
+{
+#ifndef NDEBUG
+    ++WaitingThreads_;
+#endif
+}
+void TNotifyManager::DecrementWaiters()
+{
+#ifndef NDEBUG
+    --WaitingThreads_;
+#endif
 }
 
 ////////////////////////////////////////////////////////////////////////////////
