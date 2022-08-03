ce851ce61c7d49eff3c3f142fcc2100c3e70e21f
gritukan <gritukan@ytsaurus.tech>
2022-08-03 14:36:07 +0300

    Temporary disable Lev's compat tests

--- yt/yt/tests/integration/compat/latest_22_2/test_map_operation.py
+++ yt/yt/tests/integration/compat/latest_22_2/test_map_operation.py
@@ -1,12 +1,19 @@
 from original_tests.yt.yt.tests.integration.tests.controller.test_map_operation \
     import TestSchedulerMapCommands as BaseTestMapCommands
 
+from yt_commands import authors
+
 
 class TestMapCommandsCompatNewCA(BaseTestMapCommands):
     ARTIFACT_COMPONENTS = {
         "22_2": ["master", "node", "job-proxy", "exec", "tools"],
         "trunk": ["scheduler", "controller-agent", "proxy", "http-proxy"],
     }
+
+    # TODO(levysotsky, gritukan): Drop me!
+    @authors("levysotsky")
+    def test_rename_with_both_columns_in_filter(self):
+        pass
 
 
 class TestMapCommandsCompatNewNodes(BaseTestMapCommands):
