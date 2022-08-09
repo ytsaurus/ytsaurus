7e69b820201800cc85d54fdd05d869e4a16435fc
nadya73 <nadya73@ytsaurus.tech>
2022-08-09 10:42:57 +0300

    YT-17458: concatenate via ConcatenateNodes [iceberg/inside-yt] concatenate via ConcatenateNodes

--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/ConcatenateNodes.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/proxy/request/ConcatenateNodes.java
@@ -2,38 +2,55 @@ package ru.yandex.yt.ytclient.proxy.request;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.stream.Collectors;
 
 import javax.annotation.Nonnull;
 
 import ru.yandex.inside.yt.kosher.cypress.YPath;
+import ru.yandex.inside.yt.kosher.impl.ytree.builder.YTreeBuilder;
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
 import ru.yandex.yt.rpcproxy.TMutatingOptions;
 import ru.yandex.yt.rpcproxy.TReqConcatenateNodes;
 import ru.yandex.yt.rpcproxy.TTransactionalOptions;
 import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
 
+@NonNullApi
+@NonNullFields
 public class ConcatenateNodes
         extends MutateNode<ConcatenateNodes>
         implements HighLevelRequest<TReqConcatenateNodes.Builder> {
-    private final String[] srcPaths;
-    private final String dstPath;
+    private final List<YPath> srcPaths;
+    private final YPath dstPath;
 
     public ConcatenateNodes(String[] from, String to) {
-        this.srcPaths = from;
-        this.dstPath = to;
+        this(
+                Arrays.stream(from).map(YPath::simple).collect(Collectors.toList()),
+                YPath.simple(to)
+        );
     }
 
     public ConcatenateNodes(List<YPath> source, YPath dest) {
-        this((String[]) source.stream().map(YPath::toString).toArray(), dest.toString());
+        this.srcPaths = source;
+        this.dstPath = dest;
+    }
+
+    public List<YPath> getSourcePaths() {
+        return srcPaths;
+    }
+
+    public YPath getDestinationPath() {
+        return dstPath;
     }
 
     @Override
     public void writeTo(RpcClientRequestBuilder<TReqConcatenateNodes.Builder, ?> requestBuilder) {
         TReqConcatenateNodes.Builder builder = requestBuilder.body();
-        for (String s : srcPaths) {
-            builder.addSrcPaths(s);
+        for (YPath s : srcPaths) {
+            builder.addSrcPaths(s.toString());
         }
 
-        builder.setDstPath(dstPath);
+        builder.setDstPath(dstPath.toString());
 
         if (transactionalOptions != null) {
             builder.setTransactionalOptions(transactionalOptions.writeTo(TTransactionalOptions.newBuilder()));
@@ -46,8 +63,21 @@ public class ConcatenateNodes
 
     @Override
     protected void writeArgumentsLogString(@Nonnull StringBuilder sb) {
-        sb.append("SourcePaths: ").append(Arrays.toString(srcPaths)).append("; DstPath: ").append(dstPath).append("; ");
+        sb
+                .append("SourcePaths: ")
+                .append(Arrays.toString(srcPaths.toArray()))
+                .append("; DstPath: ")
+                .append(dstPath.toString())
+                .append("; ");
         super.writeArgumentsLogString(sb);
+    }
+
+    @Override
+    public YTreeBuilder toTree(YTreeBuilder builder) {
+        return builder
+                .apply(super::toTree)
+                .key("source_paths").value(srcPaths, (b2, t) -> t.toTree(b2))
+                .key("destination_path").apply(dstPath::toTree);
     }
 
     @Nonnull
