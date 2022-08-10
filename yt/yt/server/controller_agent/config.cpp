d7a0b1eba9920420581ba68a5c6797e18fe595c6
eshcherbin <eshcherbin@ytsaurus.tech>
2022-08-11 00:21:05 +0300

    YT-17479: Do not aggregate min needed resource by job type.

--- yt/yt/server/controller_agent/config.cpp
+++ yt/yt/server/controller_agent/config.cpp
@@ -939,6 +939,9 @@ void TControllerAgentConfig::Register(TRegistrar registrar)
     registrar.Parameter("enable_table_column_renaming", &TThis::EnableTableColumnRenaming)
         .Default(false);
 
+    registrar.Parameter("aggregate_min_needed_resources_per_job_type", &TThis::AggregateMinNeededResourcesPerJobType)
+        .Default(false);
+
     registrar.Preprocessor([&] (TControllerAgentConfig* config) {
         config->EventLog->MaxRowWeight = 128_MB;
         if (!config->EventLog->Path) {
