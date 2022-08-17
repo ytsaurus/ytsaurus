33fe7a5f5f85e387d0bcde11b28eb0c81b21d3fb
nadya73 <nadya73@ytsaurus.tech>
2022-08-17 13:04:33 +0300

    YT-17495: Move jars processor to ytclient Move JarsProcessor to ytclient

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/operations/JarsProcessor.java
@@ -0,0 +1,27 @@
+package ru.yandex.yt.ytclient.operations;
+
+import java.util.Collections;
+import java.util.Set;
+
+import ru.yandex.inside.yt.kosher.cypress.YPath;
+import ru.yandex.yt.ytclient.proxy.TransactionalClient;
+
+public interface JarsProcessor {
+    /**
+     * Detects classpath and uploads it to YT.
+     *
+     * @return Files that will be copied into task CWD and inclueded in classpath.
+     */
+    Set<YPath> uploadJars(TransactionalClient yt, MapperOrReducer<?, ?> mapperOrReducer, boolean isLocalMode);
+
+    /**
+     * For automatic uploading task resources that are not part of the classpath.
+     * You will need your own implementation for this.
+     *
+     * @return Files that will be copied into task CWD.
+     * @see MapperOrReducerSpec#additionalFiles
+     */
+    default Set<YPath> uploadResources(TransactionalClient yt, MapperOrReducer<?, ?> mapperOrReducer) {
+        return Collections.emptySet();
+    }
+}
