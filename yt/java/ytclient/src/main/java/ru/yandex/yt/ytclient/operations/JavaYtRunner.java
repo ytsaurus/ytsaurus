ec9aa8f83e246cb4cfd81a3357fbed8f4bbb4c20
nadya73 <nadya73@ytsaurus.tech>
2022-08-17 20:43:31 +0300

    YT-17495: Move MapperOrReducerSpec

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/operations/JavaYtRunner.java
@@ -0,0 +1,35 @@
+package ru.yandex.yt.ytclient.operations;
+
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import ru.yandex.inside.yt.kosher.common.JavaOptions;
+
+public class JavaYtRunner {
+    protected JavaYtRunner() {
+    }
+
+    public static String normalizeClassName(String clazz) {
+        return clazz.replace("$", "dollar_char");
+    }
+
+    public static String denormalizeClassName(String name) {
+        return name.replace("dollar_char", "$");
+    }
+
+    public static String command(
+            String javaBinary,
+            String classPath,
+            @Nullable String libraryPath,
+            JavaOptions javaOptions,
+            String mainClazz,
+            List<String> args) {
+        String javaPath = "-cp " + classPath;
+        if (libraryPath != null) {
+            javaPath += " -Djava.library.path=" + libraryPath;
+        }
+        return javaBinary + " " + String.join(" ", javaOptions.getOptions()) + " " + javaPath + " "
+                + normalizeClassName(mainClazz) + (args.isEmpty() ? "" : " " + String.join(" ", args));
+    }
+}
--- yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/operations/SpecPreparationContext.java
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/operations/SpecPreparationContext.java
@@ -1,4 +1,125 @@
 package ru.yandex.yt.ytclient.operations;
 
+import java.time.Duration;
+import java.util.Arrays;
+import java.util.List;
+
+import javax.annotation.Nullable;
+
+import ru.yandex.inside.yt.kosher.cypress.YPath;
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
+
 public class SpecPreparationContext {
+    private final YPath tmpDir;
+    private final YPath cacheDir;
+    private final JarsProcessor jarsProcessor;
+    private final boolean isLocalMode;
+    private final String javaBinary;
+    private final List<String> javaOptions;
+    private final Duration jarsUploadTimeout;
+    private final int fileCacheReplicationFactor;
+
+    SpecPreparationContext(Builder builder) {
+        this.tmpDir = builder.tmpDir;
+        this.cacheDir = builder.cacheDir;
+        this.jarsProcessor = builder.jarsProcessor;
+        this.isLocalMode = builder.isLocalMode;
+        this.javaBinary = builder.javaBinary;
+        this.javaOptions = builder.javaOptions;
+        this.jarsUploadTimeout = builder.jarsUploadTimeout;
+        this.fileCacheReplicationFactor = builder.fileCacheReplicationFactor;
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public YPath getTmpDir() {
+        return tmpDir;
+    }
+
+    public JarsProcessor getJarsProcessor() {
+        return jarsProcessor;
+    }
+
+    public boolean isLocalMode() {
+        return isLocalMode;
+    }
+
+    public String getJavaBinary() {
+        return javaBinary;
+    }
+
+    public List<String> getJavaOptions() {
+        return javaOptions;
+    }
+
+    @NonNullApi
+    @NonNullFields
+    public static class Builder {
+        private YPath tmpDir = YPath.simple("//tmp/yt_wrapper/file_storage");
+        @Nullable
+        private YPath cacheDir = YPath.simple("//tmp/yt_wrapper/file_storage").child("new_cache");;
+        @Nullable
+        private JarsProcessor jarsProcessor;
+        private boolean isLocalMode = false;
+        private String javaBinary = "";  // TODO
+        private List<String> javaOptions = Arrays.asList(); // TODO
+        private Duration jarsUploadTimeout = Duration.ofMinutes(10);
+        private int fileCacheReplicationFactor = 10;
+
+        public Builder setTmpDir(YPath tmpDir) {
+            this.tmpDir = tmpDir;
+            return this;
+        }
+
+        public Builder setJarsProcessor(JarsProcessor jarsProcessor) {
+            this.jarsProcessor = jarsProcessor;
+            return this;
+        }
+
+        public Builder setIsLocalMode(boolean isLocalMode) {
+            this.isLocalMode = isLocalMode;
+            return this;
+        }
+
+        public Builder setJavaBinary(String javaBinary) {
+            this.javaBinary = javaBinary;
+            return this;
+        }
+
+        public Builder setJavaOptions(List<String> javaOptions) {
+            this.javaOptions = javaOptions;
+            return this;
+        }
+
+        public Builder setCacheDir(@Nullable YPath cacheDir) {
+            this.cacheDir = cacheDir;
+            return this;
+        }
+
+        public Builder setJarsUploadTimeout(Duration jarsUploadTimeout) {
+            this.jarsUploadTimeout = jarsUploadTimeout;
+            return this;
+        }
+
+        public Builder setFileCacheReplicationFactor(int fileCacheReplicationFactor) {
+            this.fileCacheReplicationFactor = fileCacheReplicationFactor;
+            return this;
+        }
+
+        public SpecPreparationContext build() {
+            if (jarsProcessor == null) {
+                jarsProcessor = new SingleUploadFromClassPathJarsProcessor(
+                        tmpDir.child("jars"),
+                        cacheDir,
+                        false,
+                        jarsUploadTimeout,
+                        fileCacheReplicationFactor
+                );
+            }
+            return new SpecPreparationContext(this);
+        }
+    }
 }
