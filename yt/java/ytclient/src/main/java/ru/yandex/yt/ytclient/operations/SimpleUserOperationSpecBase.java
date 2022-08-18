b95e4566348cdb4cc4908ca8a3c39bdd704795b4
nadya73 <nadya73@ytsaurus.tech>
2022-08-18 08:43:52 +0300

    YT-17495: Move UserOperationSpecBase to ytclient Move MapSpec/ReduceSpec

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/operations/SimpleUserOperationSpecBase.java
@@ -0,0 +1,73 @@
+package ru.yandex.yt.ytclient.operations;
+
+import java.util.Optional;
+
+import javax.annotation.Nullable;
+
+import ru.yandex.inside.yt.kosher.common.DataSize;
+import ru.yandex.inside.yt.kosher.impl.ytree.builder.YTreeBuilder;
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
+
+// Base class for map and reduce operations spec
+@NonNullApi
+@NonNullFields
+public class SimpleUserOperationSpecBase extends UserOperationSpecBase {
+    private final @Nullable Integer jobCount;
+    private final @Nullable DataSize maxDataSizePerJob;
+    private final @Nullable DataSize dataSizePerJob;
+
+    protected SimpleUserOperationSpecBase(Builder<?> builder) {
+        super(builder);
+
+        jobCount = builder.jobCount;
+        dataSizePerJob = builder.dataSizePerJob;
+        maxDataSizePerJob = builder.maxDataSizePerJob;
+    }
+
+    public Optional<Integer> getJobCount() {
+        return Optional.ofNullable(jobCount);
+    }
+
+    public Optional<DataSize> getMaxDataSizePerJob() {
+        return Optional.ofNullable(maxDataSizePerJob);
+    }
+
+    public Optional<DataSize> getDataSizePerJob() {
+        return Optional.ofNullable(dataSizePerJob);
+    }
+
+    protected YTreeBuilder dumpToSpec(YTreeBuilder mapBuilder) {
+        return mapBuilder
+                .key("started_by").apply(SpecUtils::startedBy)
+                .when(jobCount != null, b -> b.key("job_count").value(jobCount))
+                .when(maxDataSizePerJob != null,
+                        b -> b.key("max_data_size_per_job").value(maxDataSizePerJob.toBytes()))
+                .when(dataSizePerJob != null,
+                        b -> b.key("data_size_per_job").value(dataSizePerJob.toBytes()))
+                .apply(super::toTree);
+    }
+
+    @NonNullApi
+    @NonNullFields
+    protected abstract static class Builder<T extends Builder<T>> extends UserOperationSpecBase.Builder<T> {
+        private @Nullable Integer jobCount;
+        private @Nullable DataSize maxDataSizePerJob;
+        private @Nullable DataSize dataSizePerJob;
+
+        public T setJobCount(@Nullable Integer jobCount) {
+            this.jobCount = jobCount;
+            return self();
+        }
+
+        public T setMaxDataSizePerJob(@Nullable DataSize maxDataSizePerJob) {
+            this.maxDataSizePerJob = maxDataSizePerJob;
+            return self();
+        }
+
+        public T setDataSizePerJob(@Nullable DataSize dataSizePerJob) {
+            this.dataSizePerJob = dataSizePerJob;
+            return self();
+        }
+    }
+}
