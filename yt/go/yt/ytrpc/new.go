198c674299fafd93ba4424e771931875ea97e001
prime <prime@yandex-team.ru>
2022-09-01 15:05:37 +0300

    Fix outdated doc

--- yt/go/yt/ytrpc/new.go
+++ yt/go/yt/ytrpc/new.go
@@ -22,7 +22,7 @@ func checkNotInsideJob(c *yt.Config) error {
 
 // NewClient creates new client from config.
 //
-// Note! Table, Tablet, Mount and File clients have stub implementations.
+// Note! Table and File clients have stub implementations.
 // If you need on of those use http client instead.
 func NewClient(c *yt.Config) (yt.Client, error) {
 	if err := checkNotInsideJob(c); err != nil {
