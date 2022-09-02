60f0c4c6026608f2b2d58deb7f64452eb0d89ecc
abodrov <abodrov@yandex-team.ru>
2022-09-02 16:58:46 +0300

    Add dataclass_transform to yt_dataclass Обнаружил, что на yt_dataclass нет декоратора dataclass_transform. Это классный декоратор, благодаря которому vscode будет знать, что у класса появляется конструктор: https://docs.python.org/3.11/library/typing.html#typing.dataclass_transform Возможно, я накрутил что-то неправильно сделал - я, например, не знаю, есть ли yt_dataclass какой-то класс типа [field](https://docs.python.org/3/library/dataclasses.html#dataclasses.field).

--- yt/python/yt/wrapper/schema/types.py
+++ yt/python/yt/wrapper/schema/types.py
@@ -10,11 +10,14 @@ try:
     import yandex.type_info.typing as ti
     import copy
     import dataclasses
-    from typing_extensions import Annotated
+    from typing_extensions import Annotated, dataclass_transform
 except ImportError:
-    pass
+    if not typing.TYPE_CHECKING:
+        def dataclass_transform():
+            return lambda x: x
 
 
+@dataclass_transform()
 def yt_dataclass(cls):
     """ Decorator for classes representing table rows and embedded structures.
     """
