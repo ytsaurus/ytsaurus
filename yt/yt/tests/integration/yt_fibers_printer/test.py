3ab70198bbb620a68f553bd65a8c40c37dce26b9
ignat <ignat@ytsaurus.tech>
2022-09-19 14:01:15 +0300

    Migrate yt_fibers_printer test on yt:exec

--- yt/yt/tests/integration/yt_fibers_printer/test.py
+++ yt/yt/tests/integration/yt_fibers_printer/test.py
@@ -1,67 +1,85 @@
+from yt_commands import authors
+
+from yt.environment import arcadia_interop
+
+import logging
+import os
 import signal
 import subprocess
 
-import yatest.common as yc
+import pytest
 
-import os
-
-
-gdbpath = yc.gdb_path()
-gdbinit = yc.source_path('devtools/gdb/__init__.py')
-gdbtest = yc.binary_path('yt/yt/tests/integration/yt_fibers_printer/gdbtest/gdbtest')
+root_logger = logging.getLogger()
 
 signal.signal(signal.SIGTTOU, signal.SIG_IGN)
 
 
+def get_gdb_context():
+    yc = arcadia_interop.yatest_common
+    return dict(
+        gdbpath=yc.gdb_path(),
+        gdbinit=yc.source_path("devtools/gdb/__init__.py"),
+        gdbtest=yc.binary_path("yt/yt/tests/integration/yt_fibers_printer/gdbtest/gdbtest"),
+    )
+
+
 def copy_exe():
+    gdb_context = get_gdb_context()
     try:
-        subprocess.check_output(['cp', gdbtest, os.path.join(yc.output_path(), "executable")], stderr=subprocess.STDOUT)
-    except subprocess.CalledProcessError as exc:
-        print('Failed to copy exe\n{}'.format(exc.output.decode('UTF-8')))
+        subprocess.check_output([
+            "cp",
+            gdb_context["gdbtest"],
+            os.path.join(arcadia_interop.yatest_common.output_path(), "executable")],
+            stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError:
+        root_logger.exception("Failed to copy exe")
 
 
 def gdb(*commands):
-    cmd = [gdbpath, '-nx', '-batch', '-ix', gdbinit, '-ex', 'set charset UTF-8']
+    gdb_context = get_gdb_context()
+    cmd = [gdb_context["gdbpath"], "-nx", "-batch", "-ix", gdb_context["gdbinit"], "-ex", "set charset UTF-8"]
     for c in commands:
-        cmd += ['-ex', c]
-    cmd += [gdbtest]
+        cmd += ["-ex", c]
+    cmd += [gdb_context["gdbtest"]]
     env = os.environ.copy()
-    # strings are not printed correctly in gdb overwise.
-    env['LC_ALL'] = 'en_US.UTF-8'
+
+    # NB: strings are not printed correctly in gdb overwise.
+    env["LC_ALL"] = "en_US.UTF-8"
     try:
         out = subprocess.check_output(cmd, stderr=subprocess.STDOUT, env=env)
     except subprocess.CalledProcessError as exc:
-        with open(os.path.join(yc.output_path(), "gdb-errlog.txt"), "w") as f_p:
-            f_p.write('gdb return code: {}\n'.format(exc.returncode))
-            f_p.write(exc.output.decode('UTF-8'))
-            f_p.close()
+        root_logger.exception("gdb exited with code {}".format(exc.returncode))
         copy_exe()
         raise
     return out
 
 
 def print_fibers():
-    return gdb('b StopHere', 'run', 'print_yt_fibers')
+    return gdb("b StopHere", "run", "print_yt_fibers")
 
 
+@authors("shishmak")
 def test_fibers_printer():
-    actual_output = print_fibers().decode('UTF-8')
-    with open(os.path.join(yc.output_path(), "actual_output.txt"), "w") as f_p:
-        f_p.write(actual_output)
-        f_p.close()
+    if arcadia_interop.yatest_common is None:
+        pytest.skip()
+
+    actual_output = print_fibers().decode("UTF-8")
+    with open(os.path.join(arcadia_interop.yatest_common.output_path(), "actual_output.txt"), "w") as output:
+        output.write(actual_output)
+
     assert len(actual_output) > 0
     fibers_count = 0
     foo_count = 0
     bar_count = 0
     async_stop_count = 0
-    for line in actual_output.split('\n'):
-        if line.find('0x0000000000000000 in ?? ()') != -1:
+    for line in actual_output.split("\n"):
+        if line.find("0x0000000000000000 in ?? ()") != -1:
             fibers_count += 1
-        if line.find('Foo') != -1:
+        if line.find("Foo") != -1:
             foo_count += 1
-        if line.find('Bar') != -1:
+        if line.find("Bar") != -1:
             bar_count += 1
-        if line.find('AsyncStop') != -1:
+        if line.find("AsyncStop") != -1:
             async_stop_count += 1
     assert fibers_count >= 2
     assert foo_count == 6
