8ffba687deba9245df28b6d8f4d66bbc0e73866f
nadya73 <nadya73@ytsaurus.tech>
2022-09-23 18:33:44 +0300

    YT-17631: add immutable CheckPermission and ConcatenateNodes CheckPermission, ConcatenateNodes

---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/request/CheckPermission.java
@@ -0,0 +1,160 @@
+package ru.yandex.yt.ytclient.request;
+
+import java.util.Collections;
+import java.util.HashSet;
+import java.util.Objects;
+import java.util.Set;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
+import ru.yandex.yt.rpcproxy.TMasterReadOptions;
+import ru.yandex.yt.rpcproxy.TPrerequisiteOptions;
+import ru.yandex.yt.rpcproxy.TReqCheckPermission;
+import ru.yandex.yt.rpcproxy.TTransactionalOptions;
+import ru.yandex.yt.ytclient.proxy.request.HighLevelRequest;
+import ru.yandex.yt.ytclient.proxy.request.MasterReadOptions;
+import ru.yandex.yt.ytclient.proxy.request.MutatingOptions;
+import ru.yandex.yt.ytclient.proxy.request.PrerequisiteOptions;
+import ru.yandex.yt.ytclient.proxy.request.TransactionalOptions;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
+
+@NonNullApi
+@NonNullFields
+public class CheckPermission
+        extends MutateNode<CheckPermission.Builder>
+        implements HighLevelRequest<TReqCheckPermission.Builder> {
+    private final String user;
+    private final String path;
+    private final int permissions;
+    private final Set<String> columns;
+    @Nullable
+    private final MasterReadOptions masterReadOptions;
+
+    CheckPermission(Builder builder) {
+        super(builder);
+        this.user = Objects.requireNonNull(builder.user);
+        this.path = Objects.requireNonNull(builder.path);
+        this.permissions = Objects.requireNonNull(builder.permissions);
+        this.columns = Objects.requireNonNull(builder.columns);
+        this.masterReadOptions = builder.masterReadOptions;
+    }
+
+    public CheckPermission(String user, String path, int permissions) {
+        this(user, path, permissions, Collections.emptySet());
+    }
+
+    public CheckPermission(String user, String path, int permissions, Set<String> columns) {
+        this(builder().setUser(user).setPath(path).setPermissions(permissions).setColumns(columns));
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    @Override
+    public void writeTo(RpcClientRequestBuilder<TReqCheckPermission.Builder, ?> requestBuilder) {
+        TReqCheckPermission.Builder builder = requestBuilder.body();
+
+        builder
+                .setUser(user)
+                .setPath(path)
+                .setPermission(permissions)
+                .setColumns(TReqCheckPermission.TColumns.newBuilder().addAllItems(columns));
+
+        if (transactionalOptions != null) {
+            builder.setTransactionalOptions(transactionalOptions.writeTo(TTransactionalOptions.newBuilder()));
+        }
+        if (prerequisiteOptions != null) {
+            builder.setPrerequisiteOptions(prerequisiteOptions.writeTo(TPrerequisiteOptions.newBuilder()));
+        }
+        if (additionalData != null) {
+            builder.mergeFrom(additionalData);
+        }
+        if (masterReadOptions != null) {
+            builder.setMasterReadOptions(masterReadOptions.writeTo(TMasterReadOptions.newBuilder()));
+        }
+    }
+
+    @Override
+    protected void writeArgumentsLogString(@Nonnull StringBuilder sb) {
+        sb.append("Path: ").append(path).append("; ");
+        sb.append("User: ").append(user).append("; ");
+        sb.append("Permissions: ").append(permissions).append("; ");
+        super.writeArgumentsLogString(sb);
+    }
+
+    public Builder toBuilder() {
+        Builder builder = builder()
+                .setUser(user)
+                .setPath(path)
+                .setPermissions(permissions)
+                .setColumns(columns)
+                .setTransactionalOptions(transactionalOptions != null
+                        ? new TransactionalOptions(transactionalOptions)
+                        : null)
+                .setPrerequisiteOptions(prerequisiteOptions != null
+                        ? new PrerequisiteOptions(prerequisiteOptions)
+                        : null)
+                .setTimeout(timeout)
+                .setRequestId(requestId)
+                .setUserAgent(userAgent)
+                .setTraceId(traceId, traceSampled)
+                .setAdditionalData(additionalData);
+
+        builder.setMutatingOptions(new MutatingOptions(mutatingOptions));
+        if (masterReadOptions != null) {
+            builder.setMasterReadOptions(new MasterReadOptions(masterReadOptions));
+        }
+        return builder;
+    }
+
+    public static class Builder extends MutateNode.Builder<Builder> {
+        @Nullable
+        private String user;
+        @Nullable
+        private String path;
+        @Nullable
+        private Integer permissions;
+        @Nullable
+        private Set<String> columns = Collections.emptySet();
+        @Nullable
+        private MasterReadOptions masterReadOptions;
+
+        public Builder setUser(String user) {
+            this.user = user;
+            return self();
+        }
+
+        public Builder setPath(String path) {
+            this.path = path;
+            return self();
+        }
+
+        public Builder setPermissions(int permissions) {
+            this.permissions = permissions;
+            return self();
+        }
+
+        public Builder setColumns(Set<String> columns) {
+            this.columns = new HashSet<>(columns);
+            return self();
+        }
+
+        public Builder setMasterReadOptions(MasterReadOptions masterReadOptions) {
+            this.masterReadOptions = masterReadOptions;
+            return self();
+        }
+
+        public CheckPermission build() {
+            return new CheckPermission(this);
+        }
+
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+}
---
+++ yt/java/ytclient/src/main/java/ru/yandex/yt/ytclient/request/ConcatenateNodes.java
@@ -0,0 +1,210 @@
+package ru.yandex.yt.ytclient.request;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.stream.Collectors;
+
+import javax.annotation.Nonnull;
+import javax.annotation.Nullable;
+
+import ru.yandex.inside.yt.kosher.cypress.YPath;
+import ru.yandex.inside.yt.kosher.impl.ytree.builder.YTreeBuilder;
+import ru.yandex.lang.NonNullApi;
+import ru.yandex.lang.NonNullFields;
+import ru.yandex.yt.rpcproxy.TMutatingOptions;
+import ru.yandex.yt.rpcproxy.TReqConcatenateNodes;
+import ru.yandex.yt.rpcproxy.TTransactionalOptions;
+import ru.yandex.yt.ytclient.proxy.request.HighLevelRequest;
+import ru.yandex.yt.ytclient.proxy.request.MutatingOptions;
+import ru.yandex.yt.ytclient.proxy.request.PrerequisiteOptions;
+import ru.yandex.yt.ytclient.proxy.request.TransactionalOptions;
+import ru.yandex.yt.ytclient.rpc.RpcClientRequestBuilder;
+
+@NonNullApi
+@NonNullFields
+public class ConcatenateNodes extends MutateNode<ConcatenateNodes.Builder>
+        implements HighLevelRequest<TReqConcatenateNodes.Builder> {
+    private final List<YPath> sourcePaths;
+    private final YPath destinationPath;
+
+    ConcatenateNodes(BuilderBase<?> builder) {
+        super(builder);
+        this.sourcePaths = Objects.requireNonNull(builder.sourcePaths);
+        this.destinationPath = Objects.requireNonNull(builder.destinationPath);
+    }
+
+    public ConcatenateNodes(String[] from, String to) {
+        this(
+                Arrays.stream(from).map(YPath::simple).collect(Collectors.toList()),
+                YPath.simple(to)
+        );
+    }
+
+    public ConcatenateNodes(List<YPath> source, YPath dest) {
+        this(builder().setSourcePaths(source).setDestinationPath(dest));
+    }
+
+    public static Builder builder() {
+        return new Builder();
+    }
+
+    public List<YPath> getSourcePaths() {
+        return sourcePaths;
+    }
+
+    public YPath getDestinationPath() {
+        return destinationPath;
+    }
+
+    @Override
+    public void writeTo(RpcClientRequestBuilder<TReqConcatenateNodes.Builder, ?> requestBuilder) {
+        TReqConcatenateNodes.Builder builder = requestBuilder.body();
+        for (YPath s : sourcePaths) {
+            builder.addSrcPaths(s.toString());
+        }
+
+        builder.setDstPath(destinationPath.toString());
+
+        if (transactionalOptions != null) {
+            builder.setTransactionalOptions(transactionalOptions.writeTo(TTransactionalOptions.newBuilder()));
+        }
+        builder.setMutatingOptions(mutatingOptions.writeTo(TMutatingOptions.newBuilder()));
+        if (additionalData != null) {
+            builder.mergeFrom(additionalData);
+        }
+    }
+
+    @Override
+    protected void writeArgumentsLogString(@Nonnull StringBuilder sb) {
+        sb
+                .append("SourcePaths: ")
+                .append(Arrays.toString(sourcePaths.toArray()))
+                .append("; DstPath: ")
+                .append(destinationPath)
+                .append("; ");
+        super.writeArgumentsLogString(sb);
+    }
+
+    @Override
+    public YTreeBuilder toTree(YTreeBuilder builder) {
+        return builder
+                .apply(super::toTree)
+                .key("source_paths").value(sourcePaths, (b2, t) -> t.toTree(b2))
+                .key("destination_path").apply(destinationPath::toTree);
+    }
+
+    @Override
+    public Builder toBuilder() {
+        Builder builder = builder()
+                .setSourcePaths(sourcePaths)
+                .setDestinationPath(destinationPath)
+                .setTransactionalOptions(transactionalOptions != null
+                        ? new TransactionalOptions(transactionalOptions)
+                        : null)
+                .setPrerequisiteOptions(prerequisiteOptions != null
+                        ? new PrerequisiteOptions(prerequisiteOptions)
+                        : null)
+                .setTimeout(timeout)
+                .setRequestId(requestId)
+                .setUserAgent(userAgent)
+                .setTraceId(traceId, traceSampled)
+                .setAdditionalData(additionalData);
+
+        builder.setMutatingOptions(new MutatingOptions(mutatingOptions));
+        return builder;
+    }
+
+    public static class Builder extends BuilderBase<Builder> {
+        @Override
+        protected Builder self() {
+            return this;
+        }
+    }
+
+    public abstract static class BuilderBase<T extends BuilderBase<T>> extends MutateNode.Builder<T>
+            implements HighLevelRequest<TReqConcatenateNodes.Builder> {
+        @Nullable
+        private List<YPath> sourcePaths;
+        @Nullable
+        private YPath destinationPath;
+
+        protected BuilderBase() {
+        }
+
+        protected BuilderBase(BuilderBase<?> builder) {
+            super(builder);
+            if (builder.sourcePaths != null) {
+                this.sourcePaths = new ArrayList<>(builder.sourcePaths);
+            }
+            this.destinationPath = builder.destinationPath;
+        }
+
+        public T setSourcePaths(List<YPath> sourcePaths) {
+            this.sourcePaths = sourcePaths;
+            return self();
+        }
+
+        public T setDestinationPath(YPath destinationPath) {
+            this.destinationPath = destinationPath;
+            return self();
+        }
+
+        public ConcatenateNodes build() {
+            return new ConcatenateNodes(this);
+        }
+
+        public List<YPath> getSourcePaths() {
+            return Objects.requireNonNull(sourcePaths);
+        }
+
+        public YPath getDestinationPath() {
+            return Objects.requireNonNull(destinationPath);
+        }
+
+
+        @Override
+        public void writeTo(RpcClientRequestBuilder<TReqConcatenateNodes.Builder, ?> requestBuilder) {
+            Objects.requireNonNull(sourcePaths);
+            Objects.requireNonNull(destinationPath);
+            TReqConcatenateNodes.Builder builder = requestBuilder.body();
+            for (YPath s : sourcePaths) {
+                builder.addSrcPaths(s.toString());
+            }
+
+            builder.setDstPath(destinationPath.toString());
+
+            if (transactionalOptions != null) {
+                builder.setTransactionalOptions(transactionalOptions.writeTo(TTransactionalOptions.newBuilder()));
+            }
+            builder.setMutatingOptions(mutatingOptions.writeTo(TMutatingOptions.newBuilder()));
+            if (additionalData != null) {
+                builder.mergeFrom(additionalData);
+            }
+        }
+
+        @Override
+        protected void writeArgumentsLogString(@Nonnull StringBuilder sb) {
+            Objects.requireNonNull(sourcePaths);
+            Objects.requireNonNull(destinationPath);
+            sb
+                    .append("SourcePaths: ")
+                    .append(Arrays.toString(sourcePaths.toArray()))
+                    .append("; DstPath: ")
+                    .append(destinationPath.toString())
+                    .append("; ");
+            super.writeArgumentsLogString(sb);
+        }
+
+        @Override
+        public YTreeBuilder toTree(YTreeBuilder builder) {
+            Objects.requireNonNull(sourcePaths);
+            Objects.requireNonNull(destinationPath);
+            return builder
+                    .apply(super::toTree)
+                    .key("source_paths").value(sourcePaths, (b2, t) -> t.toTree(b2))
+                    .key("destination_path").apply(destinationPath::toTree);
+        }
+    }
+}
