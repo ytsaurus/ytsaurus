0daf2cd14d423605d0d426112a9f24bc073ddfc0
akozhikhov <akozhikhov@ytsaurus.tech>
2022-09-29 14:24:34 +0300

    Fix test_seed_replicas compat test

--- yt/yt/tests/integration/compat/latest_22_2/test_remote_copy_operation.py
+++ yt/yt/tests/integration/compat/latest_22_2/test_remote_copy_operation.py
@@ -1,12 +1,18 @@
 from original_tests.yt.yt.tests.integration.controller.test_remote_copy_operation \
     import TestSchedulerRemoteCopyCommands as BaseTestRemoteCopyCommands
 
+from yt_commands import authors
+
 
 class TestRemoteCopyCommandsCompatUpNewCA(BaseTestRemoteCopyCommands):
     ARTIFACT_COMPONENTS = {
         "22_2": ["master", "node", "job-proxy", "exec", "tools"],
         "trunk": ["scheduler", "controller-agent", "proxy", "http-proxy"],
     }
+
+    @authors("akozhikhov")
+    def test_seed_replicas(self):
+        pass
 
 
 class TestRemoteCopyCommandsCompatNewNodes(BaseTestRemoteCopyCommands):
