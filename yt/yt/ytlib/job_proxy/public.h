0c3f2c1aa48e6a442962f6701bfe2079ff9c0465
babenko <babenko@ytsaurus.tech>
2022-10-04 09:31:27 +0300

    Move job proxy error codes to yt/client

--- yt/yt/ytlib/job_proxy/public.h
+++ yt/yt/ytlib/job_proxy/public.h
@@ -1,24 +1,10 @@
 #pragma once
 
-#include <yt/yt/core/misc/public.h>
+#include <yt/yt/client/job_proxy/public.h>
 
 namespace NYT::NJobProxy {
 
 ////////////////////////////////////////////////////////////////////////////////
-
-YT_DEFINE_ERROR_ENUM(
-    ((MemoryLimitExceeded)       (1200))
-    ((MemoryCheckFailed)         (1201))
-    ((JobTimeLimitExceeded)      (1202))
-    ((UnsupportedJobType)        (1203))
-    ((JobNotPrepared)            (1204))
-    ((UserJobFailed)             (1205))
-    ((UserJobProducedCoreFiles)  (1206))
-    ((ShallowMergeFailed)        (1207))
-    ((JobNotRunning)             (1208))
-    ((InterruptionUnsupported)   (1209))
-    ((InterruptionTimeout)       (1210))
-);
 
 DECLARE_REFCOUNTED_STRUCT(IJobSpecHelper)
 DECLARE_REFCOUNTED_STRUCT(IUserJobIOFactory)
