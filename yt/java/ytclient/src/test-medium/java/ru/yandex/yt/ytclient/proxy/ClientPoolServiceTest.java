3612b9fe7eefaeb2159419c209cc4bcff7b430c7
alex-shishkin <alex-shishkin@ytsaurus.tech>
2022-10-24 20:23:52 +0300

    YT-17470: simplify ipv6 regex

--- yt/java/ytclient/src/test-medium/java/ru/yandex/yt/ytclient/proxy/ClientPoolServiceTest.java
+++ yt/java/ytclient/src/test-medium/java/ru/yandex/yt/ytclient/proxy/ClientPoolServiceTest.java
@@ -19,8 +19,17 @@ public class ClientPoolServiceTest extends TestCase {
         builder.setBalancerAddress("[2a02:6b8:c27:12d8:0:f411:0:1f]", 123);
         assertThat(builder.balancerAddress, is("[2a02:6b8:c27:12d8:0:f411:0:1f]:123"));
 
+        builder.setBalancerAddress("[2a02:6b8:c10:1605:0:f408::]", 27004);
+        assertThat(builder.balancerAddress, is("[2a02:6b8:c10:1605:0:f408::]:27004"));
+
+        builder.setBalancerAddress("[::1]", 8080);
+        assertThat(builder.balancerAddress, is("[::1]:8080"));
+
         assertThrows(IllegalArgumentException.class,
                 () -> builder.setBalancerAddress("[2a02:6b8:c27:12d8:0:f411:0:1f]:123", 123));
+
+        assertThrows(IllegalArgumentException.class,
+                () -> builder.setBalancerAddress("[192.168.0.1:80]", 80));
 
         assertThrows(IllegalArgumentException.class,
                 () -> builder.setBalancerAddress("host", 10000000));
