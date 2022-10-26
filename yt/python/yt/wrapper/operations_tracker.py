4210ccce085eacc028e198f28d8065b26bcb7602
babenko <babenko@ytsaurus.tech>
2022-10-26 13:30:48 +0300

    Fix grammar

--- yt/python/yt/wrapper/operations_tracker.py
+++ yt/python/yt/wrapper/operations_tracker.py
@@ -268,7 +268,7 @@ class OperationsTrackerBase(object):
             return
         logger.warning(
             "OperationsTracker is supposed to be used as context manager, "
-            "cf. https://yt.yandex-team.ru/docs/api/python/userdoc#operations_tracker_class")
+            "see https://yt.yandex-team.ru/docs/api/python/userdoc#operations_tracker_class")
         self._tracking_thread.stop()
 
 
