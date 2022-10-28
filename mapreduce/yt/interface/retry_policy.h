83560776e02bb9697e1aa262067aec90ea7b2bb2
ermolovd <ermolovd@ytsaurus.tech>
2022-10-29 00:09:47 +0300

    Documentation for retry config

--- mapreduce/yt/interface/retry_policy.h
+++ mapreduce/yt/interface/retry_policy.h
@@ -53,27 +53,37 @@ public:
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// Configuration controlling retries of single request.
+/// A configuration that controls retries of a single request.
 struct TRetryConfig
 {
-    /// RetriesTimeLimit controls how long retries can go on.
+    ///
+    /// @brief How long retries of a single YT request can go on.
+    ///
     /// If this limit is reached while retry count is not yet exceeded @ref TRequestRetriesTimeout exception is thrown.
     TDuration RetriesTimeLimit = TDuration::Max();
 };
 
+/// The library uses this class to undestand how to retry individual requests.
 class IRetryConfigProvider
     : public virtual TThrRefBase
 {
 public:
-    //
-    // CreateRetryConfig is called before ANY request.
-    // Returned config controls retries of this request.
-    //
-    // Must be theread safe since it can be used from different threads
-    // to perform internal library requests (e.g. pings).
-    //
-    // NOTE: some methods (e.g. IClient::Map) involve multiple requests to YT and therefore
-    // this method will be called several times during execution of single method.
+    ///
+    /// @brief Gets retry policy for single request.
+    ///
+    /// CreateRetryConfig is called before ANY request.
+    /// Returned config controls retries of this request.
+    ///
+    /// Must be theread safe since it can be used from different threads
+    /// to perform internal library requests (e.g. pings).
+    ///
+    /// Some methods (e.g. IClient::Map) involve multiple requests to YT and therefore
+    /// this method will be called several times during execution of single method.
+    ///
+    /// If user needs to limit overall retries inside long operation they might create
+    /// retry policy that knows about overall deadline
+    /// @ref NYT::TRetryConfig::RetriesTimeLimit taking into account that overall deadline.
+    /// (E.g. when deadline reached it returns zero limit for retries).
     virtual TRetryConfig CreateRetryConfig() = 0;
 };
 
