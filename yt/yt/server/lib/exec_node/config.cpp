5d6798c8e4231478c4bad2d97b6c121c60e843ff
ignat <ignat@ytsaurus.tech>
2022-11-08 21:08:57 +0300

    Wait job proxy heartbeat sent before abort

--- yt/yt/server/lib/exec_node/config.cpp
+++ yt/yt/server/lib/exec_node/config.cpp
@@ -461,8 +461,8 @@ void TExecNodeConfig::Register(TRegistrar registrar)
     registrar.Parameter("job_proxy_upload_debug_artifact_chunks", &TThis::JobProxyUploadDebugArtifactChunks)
         .Default(false);
 
-    registrar.Parameter("job_proxy_delay_before_abort", &TThis::JobProxyDelayBeforeAbort)
-        .Default(TDuration::Zero());
+    registrar.Parameter("job_proxy_send_heartbeat_before_abort", &TThis::JobProxySendHeartbeatBeforeAbort)
+        .Default(false);
 
     registrar.Parameter("test_root_fs", &TThis::TestRootFS)
         .Default(false);
