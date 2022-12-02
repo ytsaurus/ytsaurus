452b2ff6573dd82693114ee095fe497d0d7c61f8
k-malov <k-malov@ytsaurus.tech>
2022-12-02 14:49:27 +0300

    add multicluster support to registry

--- yt/go/docker_registry/internal/ytauth/accesscontroller.go
+++ yt/go/docker_registry/internal/ytauth/accesscontroller.go
@@ -8,49 +8,101 @@ import (
 	dcontext "github.com/distribution/distribution/v3/context"
 	"github.com/distribution/distribution/v3/registry/auth"
 
+	"a.yandex-team.ru/yt/go/ypath"
 	"a.yandex-team.ru/yt/go/yt"
-	"a.yandex-team.ru/yt/go/yt/ythttp"
+	"a.yandex-team.ru/yt/go/yterrors"
 
 	"a.yandex-team.ru/yt/go/docker_registry/internal/utils"
 )
 
 const (
-	authName         = "yt"
-	paramClusterName = "cluster"
-	resourceType     = "repository"
-	actionPush       = "push"
-	allowAccess      = "allow"
-	homeDirectory    = "home"
+	authName                     = "yt"
+	accessResourceTypeRepository = "repository"
+	accessResourceTypeRegistry   = "registry"
+	accessResourceActionPush     = "push"
+	accessResourceActionPull     = "pull"
+	accessResourceActionDelete   = "delete"
 )
 
-type accessController struct {
-	cluster string
+type Clients interface {
+	Init() error
+	GetClientFromContext(ctx context.Context) (yt.Client, error)
+	GetClients() []yt.Client
 }
 
-func (ac *accessController) validateYTPermissions(ctx context.Context, token string, accessRecords []auth.Access) error {
-	if len(accessRecords) == 0 {
-		return nil
-	}
+type accessController struct {
+	clients Clients
+}
 
-	logger, stop := utils.GetLogger()
-	defer stop()
-
-	yc, err := ythttp.NewClient(&yt.Config{Proxy: ac.cluster, Token: token, Logger: logger})
+func findExistingYTNode(ctx context.Context, c yt.Client, p ypath.YPath) (ypath.YPath, error) {
+	ok, err := c.NodeExists(ctx, p, nil)
 	if err != nil {
-		return err
+		return p, err
+	}
+	if ok {
+		return p, nil
 	}
 
-	for _, ar := range accessRecords {
-		if ar.Resource.Type != "repository" || ar.Action != "push" {
-			continue
-		}
-		p := utils.GetSchedulerHintsDocumentPath(ar.Resource.Name)
-		createNodeOptions := &yt.CreateNodeOptions{Recursive: true, IgnoreExisting: true}
-		if _, err := yc.CreateNode(ctx, p, yt.NodeDocument, createNodeOptions); err != nil {
-			return err
-		}
+	p, _, err = ypath.Split(p.YPath())
+	if err != nil {
+		return p, err
 	}
-	return nil
+	if p.YPath().String() == "/" {
+		return p, nil
+	}
+	return findExistingYTNode(ctx, c, p)
+}
+
+func isYTWriteAuthorized(ctx context.Context, c yt.Client, userID string, p ypath.YPath) (bool, error) {
+	p, err := findExistingYTNode(ctx, c, p)
+	if err != nil {
+		if yterrors.ContainsErrorCode(err, yterrors.CodeAuthorizationError) {
+			return false, nil
+		}
+		return false, err
+	}
+
+	r, err := c.CheckPermission(ctx, userID, yt.PermissionWrite, p, nil)
+	if err != nil {
+		return false, err
+	}
+	if r.Action != yt.ActionAllow {
+		return false, nil
+	}
+
+	var account string
+	err = c.GetNode(ctx, p.YPath().Attr("account"), &account, nil)
+	if err != nil {
+		return false, err
+	}
+
+	ap := ypath.Root.JoinChild("sys", "accounts", account)
+	r, err = c.CheckPermission(ctx, userID, yt.PermissionUse, ap, nil)
+	if err != nil {
+		return false, err
+	}
+	return r.Action == yt.ActionAllow, nil
+}
+
+func isYTActionAuthorized(ctx context.Context, c yt.Client, userID string, p ypath.YPath, permission yt.Permission) (bool, error) {
+	r, err := c.CheckPermission(ctx, userID, permission, p, nil)
+	if err != nil {
+		// Should allow read access to non existing object due
+		// docker push runs pull first
+		if yterrors.ContainsErrorCode(err, yterrors.CodeResolveError) {
+			return true, nil
+		}
+		return false, err
+	}
+	return r.Action == yt.ActionAllow, nil
+}
+
+func isYTReadAuthorized(ctx context.Context, c yt.Client, userID string, p ypath.YPath) (bool, error) {
+	return isYTActionAuthorized(ctx, c, userID, p, yt.PermissionRead)
+}
+
+func isYTDeleteAuthorized(ctx context.Context, c yt.Client, userID string, p ypath.YPath) (bool, error) {
+	return isYTActionAuthorized(ctx, c, userID, p, yt.PermissionRemove)
 }
 
 func (ac *accessController) Authorized(ctx context.Context, accessRecords ...auth.Access) (context.Context, error) {
@@ -73,24 +125,63 @@ func (ac *accessController) Authorized(ctx context.Context, accessRecords ...aut
 		return nil, &badRequestError{err}
 	}
 
-	userLogin, err := authenticateUserViaBlackBox(ctx, userToken)
+	userID, err := authenticateUserViaBlackBox(ctx, userToken)
 	if err != nil {
 		return nil, &authenticationError{err}
 	}
 
-	if err := ac.validateYTPermissions(ctx, userToken, accessRecords); err != nil {
-		return nil, &authenticationError{err}
+	if len(accessRecords) == 0 {
+		return enrichContextWithUserInfo(ctx, userID, userToken), nil
 	}
 
-	return enrichContextWithUserInfo(ctx, userLogin, userToken), nil
+	yc, err := ac.clients.GetClientFromContext(ctx)
+	if err != nil {
+		return nil, &internalServerError{err}
+	}
+
+	for _, ar := range accessRecords {
+		if ar.Resource.Type != accessResourceTypeRepository {
+			// FIXME: ?
+			continue
+		}
+
+		if ar.Action == accessResourceActionPull {
+			ok, err := isYTReadAuthorized(ctx, yc, userID, utils.GetSchedulerHintsDocumentPath(ar.Resource.Name))
+			if err != nil {
+				return nil, &internalServerError{err}
+			}
+			if !ok {
+				return nil, &unauthorizedError{err}
+			}
+		}
+		if ar.Action == accessResourceActionPush {
+			ok, err := isYTWriteAuthorized(ctx, yc, userID, utils.GetSchedulerHintsDocumentPath(ar.Resource.Name))
+			if err != nil {
+				return nil, &internalServerError{err}
+			}
+			if !ok {
+				return nil, &unauthorizedError{err}
+			}
+		}
+		if ar.Action == accessResourceActionDelete {
+			ok, err := isYTDeleteAuthorized(ctx, yc, userID, utils.GetSchedulerHintsDocumentPath(ar.Resource.Name))
+			if err != nil {
+				return nil, &internalServerError{err}
+			}
+			if !ok {
+				return nil, &unauthorizedError{err}
+			}
+		}
+	}
+	return enrichContextWithUserInfo(ctx, userID, userToken), nil
 }
 
 func newAccessController(options map[string]interface{}) (auth.AccessController, error) {
-	clusterName, ok := options[paramClusterName]
-	if !ok || fmt.Sprint(clusterName) == "" {
-		return nil, fmt.Errorf("no %s parameter provided", paramClusterName)
+	c := &utils.YTClients{}
+	if err := c.Init(); err != nil {
+		return nil, err
 	}
-	return &accessController{cluster: clusterName.(string)}, nil
+	return &accessController{clients: c}, nil
 }
 
 func init() {
