867d14726ef6be32ffcf0c41de98e9cbb4aeb7d6
k-malov <k-malov@ytsaurus.tech>
2023-01-12 13:23:15 +0300

    add tvm support for bb oauth

--- yt/go/docker_registry/internal/ytauth/blackbox.go
+++ yt/go/docker_registry/internal/ytauth/blackbox.go
@@ -4,16 +4,35 @@ import (
 	"context"
 	"encoding/base64"
 	"fmt"
+	"os"
 	"strings"
 
 	"a.yandex-team.ru/library/go/yandex/blackbox"
 	"a.yandex-team.ru/library/go/yandex/blackbox/httpbb"
+	"a.yandex-team.ru/library/go/yandex/tvm/tvmtool"
 )
 
+const (
+	QloudTVMTokenEnvName = "QLOUD_TVM_TOKEN"
+	TVMAppAlias          = "yt-docker-registry"
+)
+
+func getBlackBoxClient() (*httpbb.Client, error) {
+	opts := make([]httpbb.Option, 1)
+	if _, ok := os.LookupEnv(QloudTVMTokenEnvName); ok {
+		tvmClient, err := tvmtool.NewQloudClient(tvmtool.WithSrc(TVMAppAlias))
+		if err != nil {
+			return nil, fmt.Errorf("failed to create tvm client: %w", err)
+		}
+		opts = append(opts, httpbb.WithTVM(tvmClient))
+	}
+	return httpbb.NewIntranet(opts...)
+}
+
 func authenticateUserViaBlackBox(ctx context.Context, userToken string) (string, error) {
-	bb, err := httpbb.NewIntranet()
+	bb, err := getBlackBoxClient()
 	if err != nil {
-		return "", err
+		return "", fmt.Errorf("failed to create blackbox client %w", err)
 	}
 
 	tokenReq := blackbox.OAuthRequest{
