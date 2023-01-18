09bd91888696d86f92db54cf99412a63cd1d44aa
nadya73 <nadya73@ytsaurus.tech>
2023-01-18 18:48:22 +0300

    No dependency from iceberg in opensource No dependency from iceberg in opensource

--- mapreduce/yt/python/recipe/test_java/TestYtRecipe.java
+++ mapreduce/yt/python/recipe/test_java/TestYtRecipe.java
@@ -6,21 +6,24 @@ import org.junit.Test;
 import org.junit.Assert;
 import tech.ytsaurus.core.cypress.CypressNodeType;
 import tech.ytsaurus.core.cypress.YPath;
-
-import ru.yandex.inside.yt.kosher.Yt;
-import ru.yandex.inside.yt.kosher.impl.YtUtils;
-
+import tech.ytsaurus.client.YTsaurusClient;
+import tech.ytsaurus.client.request.CreateNode;
+import tech.ytsaurus.client.request.ExistsNode;
+import tech.ytsaurus.client.rpc.RpcCredentials;
 
 public class TestYtRecipe {
     @Test
     public void test() throws IOException {
         String ytProxy = System.getenv("YT_PROXY");
-        Yt yt = YtUtils.http(ytProxy, "");
+        YTsaurusClient yt = YTsaurusClient.builder()
+            .setCluster(ytProxy)
+            .setRpcCredentials(new RpcCredentials("root", ""))
+            .build();
 
         YPath node = YPath.simple("//tmp/table");
 
-        Assert.assertFalse(yt.cypress().exists(node));
-        yt.cypress().create(node, CypressNodeType.TABLE);
-        Assert.assertTrue(yt.cypress().exists(node));
+        Assert.assertFalse(yt.existsNode(ExistsNode.builder().setPath(node).build()).join());
+        yt.createNode(new CreateNode(node, CypressNodeType.TABLE)).join();
+        Assert.assertTrue(yt.existsNode(ExistsNode.builder().setPath(node).build()).join());
     }
 }
