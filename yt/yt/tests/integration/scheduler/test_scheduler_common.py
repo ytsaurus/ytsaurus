1b46404946c3884bf5dd2c97654d9961debd2c22
max42 <max42@ytsaurus.tech>
2023-02-03 02:54:52 +0300

    YT-18296: fix flaky test

--- yt/yt/tests/integration/scheduler/test_scheduler_common.py
+++ yt/yt/tests/integration/scheduler/test_scheduler_common.py
@@ -1112,7 +1112,7 @@ class TestSchedulerMaxChunkPerJob(YTEnvSetup):
             out="//tmp/t_output",
         )
         wait_breakpoint(job_count=1)
-        wait(lambda: get(op.get_path() + "/@brief_progress/jobs")["total"] > 0)
+        wait(lambda: get(op.get_path() + "/@brief_progress/jobs", ignore_exceptions=True)["total"] > 0)
         counters = get(op.get_path() + "/@brief_progress/jobs")
         assert counters["running"] + counters["pending"] == 10
         release_breakpoint()
