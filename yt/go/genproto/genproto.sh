95b61439d43d0c5ed4d56dcc6b0f409b27262380
verytable <verytable@ytsaurus.tech>
2023-02-03 21:53:19 +0300

    Remove go_package renames.

--- yt/go/genproto/genproto.sh
+++ yt/go/genproto/genproto.sh
@@ -2,10 +2,6 @@
 
 GIT_WORK_DIR=$(git rev-parse --show-toplevel)
 
-# TODO(verytable): remove the following lines when DTCC-1308 is resolved
-(cd $GIT_WORK_DIR && find ./yt/yt_proto -type f -exec sed -i -e 's/a.yandex-team.ru/ytsaurus.tech/g' {} \;)
-(cd $GIT_WORK_DIR && find ./yt/yt/core/rpc/unittests/lib/ -type f -exec sed -i -e 's/a.yandex-team.ru/ytsaurus.tech/g' {} \;)
-
 (cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I ./yt $(find ./yt/yt_proto/yt/client/api/rpc_proxy -iname "*.proto"))
 (cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I ./yt $(find ./yt/yt_proto/yt/core/rpc/proto -iname "*.proto"))
 (cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I ./yt $(find ./yt/yt_proto/yt/core/tracing/proto -iname "*.proto"))
