497a06cb734a2a5c82bf76221a83bcc5bc7cb3ac
verytable <verytable@ytsaurus.tech>
2023-02-08 03:50:25 +0300

    Add tools build tag and generate command for protoc-gen-go.

--- yt/go/genproto/deps.go
+++ yt/go/genproto/deps.go
@@ -1,7 +1,12 @@
+//go:build tools
+// +build tools
+
 package tools
 
 import (
 	_ "google.golang.org/protobuf/cmd/protoc-gen-go"
 )
+
+//go:generate go install google.golang.org/protobuf/cmd/protoc-gen-go
 
 //go:generate ./genproto.sh
