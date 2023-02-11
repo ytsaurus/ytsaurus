aaae03c40f722bf4b644a53b29b1abcda2b7a66c
verytable <verytable@ytsaurus.tech>
2023-02-12 01:51:14 +0300

    Temporarily add -I /usr/local/include to genproto.sh.

--- yt/go/genproto/genproto.sh
+++ yt/go/genproto/genproto.sh
@@ -10,13 +10,13 @@ else
   GIT_WORK_DIR="${GITHUB_WORKSPACE}"
 fi
 
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/client/api/rpc_proxy -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/core/rpc/proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/core/tracing/proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/core/yson/proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/core/ytree/proto/attributes.proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/core/misc/proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt/core/rpc/unittests/lib/my_service.proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/client/chaos_client/proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/client/chunk_client/proto -iname "*.proto"))
-(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/include -I ./yt $(find ./yt/yt_proto/yt/client/hive/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/client/api/rpc_proxy -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/core/rpc/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/core/tracing/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/core/yson/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/core/ytree/proto/attributes.proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/core/misc/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt/core/rpc/unittests/lib/my_service.proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/client/chaos_client/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/client/chunk_client/proto -iname "*.proto"))
+(cd $GIT_WORK_DIR && protoc --go_opt=module=ytsaurus.tech --go_out=. -I /usr/local/include -I ./yt $(find ./yt/yt_proto/yt/client/hive/proto -iname "*.proto"))
