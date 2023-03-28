29963a3817f8b0cd780788aa2236cd2ff21319cd
gudqeit <gudqeit@ytsaurus.tech>
2023-03-28 16:06:43 +0300

    CHYT: fix branch in binary version
    Все бинарники стали иметь вид: ytserver-clickhouse-0.0.11137005-local~3a5c77a85c+sb (ветка не была корректной).
    Это появилось после пр: https://a.yandex-team.ru/arcadia/commit/rXXXXXX, GetBranch() была в build/scripts/c_templates/svnversion.h и добавилась еще в yt/yt/build/build.h c дефолтом https://a.yandex-team.ru/arcadia/yt/yt/build/ya.make?rev=rXXXXXX#L11.
    Последний inc больше не используется, поэтому можно его убрать.

--- yt/chyt/server/version.cpp
+++ yt/chyt/server/version.cpp
@@ -2,27 +2,25 @@
 
 #include <build/scripts/c_templates/svnversion.h>
 
-#include <yt/yt/build/build.h>
-
 namespace NYT::NClickHouseServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
 TString GetCHYTVersion()
 {
-    TString branch = GetBranch();
+    TString branch = ::GetBranch();
     auto pos = branch.find("chyt/");
-    TString hash(GetProgramHash());
+    TString hash(::GetProgramHash());
     if (hash.size() >= 10) {
         hash.resize(10);
     }
     if (pos != TString::npos) {
         pos += 5;
-        return branch.substr(pos) + "." + ToString(GetArcadiaPatchNumber()) + "~" + hash;
+        return branch.substr(pos) + "." + ToString(::GetArcadiaPatchNumber()) + "~" + hash;
     } else if (branch.find("trunk") != TString::npos) {
-        return "0.0." + ToString(GetProgramSvnRevision()) + "-trunk~" + hash;
+        return "0.0." + ToString(::GetProgramSvnRevision()) + "-trunk~" + hash;
     } else {
-        return "0.0." + ToString(GetProgramSvnRevision()) + "-local~" + hash;
+        return "0.0." + ToString(::GetProgramSvnRevision()) + "-local~" + hash;
     }
 }
 
