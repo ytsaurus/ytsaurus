b8b399a7c17b6bb54130b1a31cdd9244cd412bdd
dakovalkov <dakovalkov@ytsaurus.tech>
2023-05-11 08:02:38 +0300

    CHYT-955: Try to fix test_query_log_eviction 2

--- yt/chyt/tests/server/test_query_log.py
+++ yt/chyt/tests/server/test_query_log.py
@@ -39,10 +39,10 @@ class TestQueryLog(ClickHouseTestBase):
             time.sleep(6)
             assert len(clique.make_query("select * from system.query_log")) == 0
 
-    @authors("max42")
+    @authors("max42", "dakovalkov")
     def test_query_log_eviction(self):
         query_log_flush_period = 3
-        query_log_older_flush_preiod = 1
+        query_log_older_flush_preiod = 2
 
         with Clique(1, config_patch=self._get_query_log_patch(query_log_flush_period, query_log_older_flush_preiod)) as clique:
             timespan = 15
@@ -61,6 +61,11 @@ class TestQueryLog(ClickHouseTestBase):
                 identifier_to_state[identifier] = {"state": "unseen", "started_at": now}
 
                 all_entries = clique.make_query("select * from system.query_log", verbose=False)
+                # NB: Flushing buffer table is not atomic. Query can be temporary missing from the log
+                # if it's being transfered from query_log to query_log_older during "select * from system.query_log" query.
+                # To guarantee that all queries are present in the result, we run the query two times.
+                all_entries.extend(clique.make_query("select * from system.query_log", verbose=False))
+
                 seen_now = {None}
                 for entry in all_entries:
                     if not entry["query"]:
@@ -70,6 +75,7 @@ class TestQueryLog(ClickHouseTestBase):
                         found_identifier = entry["query"][foo_index:foo_index + 6]
                         seen_now.add(found_identifier)
                         state = identifier_to_state[found_identifier]
+                        assert state["state"] in ("unseen", "seen")
                         if state["state"] == "unseen":
                             state["state"] = "seen"
                             state["seen_at"] = now
@@ -85,10 +91,10 @@ class TestQueryLog(ClickHouseTestBase):
             if state["state"] in ("seen", "evicted"):
                 assert state["seen_at"] - state["started_at"] <= 0.5
             if state["state"] == "evicted":
-                assert state["evicted_at"] - state["started_at"] >= query_log_older_flush_preiod
-                assert state["evicted_at"] - state["started_at"] <= (query_log_flush_period + query_log_older_flush_preiod) * 2
+                assert state["evicted_at"] - state["started_at"] >= query_log_older_flush_preiod - 1
+                assert state["evicted_at"] - state["started_at"] <= (query_log_flush_period + query_log_older_flush_preiod) + 3
 
-            if state["started_at"] <= timespan - (query_log_flush_period + query_log_older_flush_preiod) - 5:
+            if state["started_at"] <= timespan - (query_log_flush_period + query_log_older_flush_preiod) - 3:
                 assert state["state"] == "evicted"
-            if state["started_at"] <= timespan - 5:
+            if state["started_at"] <= timespan - 2:
                 assert state["state"] in ("seen", "evicted")
