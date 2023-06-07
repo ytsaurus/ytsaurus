6138eea62c31cc84cf8e4b8935ba5b99a01f7c71
Sasha Melentyev <sasha@melentyev.io>
2023-06-07 21:03:10 +0300

    feat(sdk/go): add init attributes for create node options

--- yt/go/yt/tables.go
+++ yt/go/yt/tables.go
@@ -79,9 +79,7 @@ type CreateTableOption func(options *CreateNodeOptions)
 
 func WithSchema(schema schema.Schema) CreateTableOption {
 	return func(options *CreateNodeOptions) {
-		if options.Attributes == nil {
-			options.Attributes = map[string]interface{}{}
-		}
+		options.initAttributes()
 
 		options.Attributes["schema"] = schema
 	}
@@ -89,9 +87,7 @@ func WithSchema(schema schema.Schema) CreateTableOption {
 
 func WithInferredSchema(row interface{}) CreateTableOption {
 	return func(options *CreateNodeOptions) {
-		if options.Attributes == nil {
-			options.Attributes = map[string]interface{}{}
-		}
+		options.initAttributes()
 
 		options.Attributes["schema"] = schema.MustInfer(row)
 	}
@@ -111,9 +107,7 @@ func WithRecursive() CreateTableOption {
 
 func WithAttributes(attrs map[string]interface{}) CreateTableOption {
 	return func(options *CreateNodeOptions) {
-		if options.Attributes == nil {
-			options.Attributes = map[string]interface{}{}
-		}
+		options.initAttributes()
 
 		for k, v := range attrs {
 			if _, ok := options.Attributes[k]; !ok {
