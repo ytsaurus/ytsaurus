8c4ceb4ff74111eab33a53c5871803250efa8cfd
ignat <ignat@ytsaurus.tech>
2023-06-09 14:38:02 +0300

    YT-19289: Drop concrete version of python in run_tests.sh

--- yt/yt/tests/integration/run_tests.sh
+++ yt/yt/tests/integration/run_tests.sh
@@ -8,4 +8,4 @@ set -eu
 
 ln -fs ${SOURCE_ROOT}/yt/yt/tests/conftest_lib/conftest_queries.py ${SOURCE_ROOT}/yt/yt/tests/integration/conftest.py
 
-PYTHONPATH="${SOURCE_ROOT}/yt/yt/tests:${SOURCE_ROOT}/yt/yt/tests/library:$PYTHON_ROOT:$PYTHONPATH" YT_BUILD_ROOT=${BUILD_ROOT} HDD_PATH=${TESTS_SANDBOX} YT_TESTS_SANDBOX=${TESTS_SANDBOX} python3.8 -m pytest -s -v $@
+PYTHONPATH="${SOURCE_ROOT}/yt/yt/tests:${SOURCE_ROOT}/yt/yt/tests/library:$PYTHON_ROOT:$PYTHONPATH" YT_BUILD_ROOT=${BUILD_ROOT} HDD_PATH=${TESTS_SANDBOX} YT_TESTS_SANDBOX=${TESTS_SANDBOX} python3 -m pytest -s -v $@
