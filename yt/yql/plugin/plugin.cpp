6ce5721b11e5e0866c1b2b125899d9646b19171c
Maxim Akhmedov <Maxim Akhmedov>
2023-06-09 16:28:29 +0200

    Try fixing YQL build

--- yt/yql/plugin/plugin.cpp
+++ yt/yql/plugin/plugin.cpp
@@ -1,15 +1,11 @@
 #include "plugin.h"
 
-#include <ydb/library/yql/utils/log/log.h>
-
 namespace NYT::NYqlPlugin {
 
 ////////////////////////////////////////////////////////////////////////////////
 
 Y_WEAK std::unique_ptr<IYqlPlugin> CreateYqlPlugin(TYqlPluginOptions& /*options*/) noexcept
 {
-    YQL_LOG(FATAL) << "No YQL plugin implementation is available; link against either "
-                   << "yt/yql/plugin/native or yt/yql/plugin/dynamic";
     exit(1);
 }
 
