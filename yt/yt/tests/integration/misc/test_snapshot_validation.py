a5945de58421deca657a6e675fe340795b14ce96
h0pless <h0pless@ytsaurus.tech>
2023-06-26 21:11:24 +0300

    YT-19344: Add dry run test

--- yt/yt/tests/integration/misc/test_snapshot_validation.py
+++ yt/yt/tests/integration/misc/test_snapshot_validation.py
@@ -1,12 +1,15 @@
 from yt_env_setup import YTEnvSetup
 
 from yt_commands import (
-    authors, create, ls, read_file, sync_create_cells, build_snapshot, build_master_snapshots,
-    print_debug)
+    authors, create, create_account, create_dynamic_table, execute_command, sync_mount_table,
+    ls, read_file, sync_create_cells, sync_flush_table, write_table, insert_rows, build_snapshot,
+    get, build_master_snapshots, print_debug)
 
 import yt.yson as yson
 
 from flaky import flaky
+
+from pytest import fail
 
 import subprocess
 import os
@@ -23,88 +26,152 @@ import os.path
 
 class TestSnapshotValidation(YTEnvSetup):
     NUM_MASTERS = 1
+    USE_DYNAMIC_TABLES = True
 
-    @authors("ifsmirnov")
-    def test_master_snapshot_validation(self):
-        create("table", "//tmp/t")
-        build_master_snapshots()
+    def _update_config(self, config_path):
+        config = None
+        with open(config_path, "rb") as fh:
+            config = yson.loads(fh.read())
+        config["data_node"]["store_locations"] = []
+        config["exec_node"]["slot_manager"]["locations"] = []
+        with open(config_path, "wb") as fh:
+            fh.write(yson.dumps(config, yson_format="pretty"))
 
-        snapshot_dir = os.path.join(self.path_to_run, "runtime_data", "master", "0", "snapshots")
-        snapshots = [name for name in os.listdir(snapshot_dir) if name.endswith("snapshot")]
-        assert len(snapshots) > 0
-        snapshot_path = os.path.join(snapshot_dir, snapshots[0])
+    def _populate_master_changelog(self):
+        create_account("cool_account")
+        create("table", "//tmp/table1")
+        create("table", "//tmp/table2", attributes={"account": "cool_account", "schema": [
+            {"name": "title", "type": "string"},
+            {"name": "score", "type": "int64"},
+            {"name": "is_funny", "type": "any"}]})
+        write_table("//tmp/table2", {"title": "AHIT", "score": 79, "is_funny": "yes"})
+        write_table("<append=%true>//tmp/table2", {"title": "TBOI", "score": 86, "is_funny": "yes"})
+        write_table("//tmp/table1", {"misery": "a little line", "happiness": "a little cross"})
 
-        config_path = os.path.join(self.path_to_run, "configs", "master-0-0.yson")
+    def _populate_tablet_cell_changelog(self, lower_key, upper_key):
+        for key in range(lower_key, upper_key):
+            rows = [{"key": key, "value": "test" * key}]
+            insert_rows("//tmp/dyntable", rows)
+        sync_flush_table("//tmp/dyntable")
 
-        binary = os.path.join(self.bin_path, "ytserver-master")
-
+    def _run_validation(
+            self,
+            binary,
+            snapshot_path,
+            changelog_path,
+            build_snaphot_dir,
+            config_path,
+            cell_id=None,
+            snapshot_meta=None):
         # NB: Sleep after initialize is required since the main thread otherwise can halt before
         # some other thread uses network.
         command = [
             binary,
-            "--validate-snapshot",
-            snapshot_path,
-            "--config",
-            config_path,
+            "--validate-snapshot", snapshot_path,
+            "--replay-changelogs", changelog_path,
+            "--build-snapshot", build_snaphot_dir,
+            "--config", config_path,
             "--sleep-after-initialize",
         ]
 
+        if cell_id is not None:
+            command += ["--cell-id", cell_id]
+
+        if snapshot_meta is not None:
+            command += ["--snapshot-meta", yson.dumps(snapshot_meta)]
+
+        timeout = 30
         proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
         try:
-            stdout, stderr = proc.communicate(timeout=30)
+            stdout, stderr = proc.communicate(timeout=timeout)
         except subprocess.TimeoutExpired:
             proc.kill()
-            assert False, "Snapshot validation timed out"
-
+            fail("Snapshot validation timed out (Timeout: {})".format(timeout))
         print_debug("Snapshot validation finished (stdout: {}, stderr: {})".format(stdout, stderr))
+        return proc.returncode
 
-        assert proc.returncode == 0
+    @authors("h0pless")
+    def test_master_dry_run(self):
+        build_master_snapshots()
+        self._populate_master_changelog()
+        build_master_snapshots()
 
-    @authors("akozhikhov")
+        result_dir = os.path.join(self.path_to_run, "runtime_data", "master", "0")
+        assert os.path.exists(result_dir)
+
+        snapshot_dir = os.path.join(result_dir, "snapshots")
+        changelog_dir = os.path.join(result_dir, "changelogs")
+
+        snapshots = [name for name in os.listdir(snapshot_dir) if name.endswith("snapshot")]
+        snapshots.sort()
+        assert len(snapshots) > 1
+
+        snapshot_id = snapshots[-2].split(".")[0]
+        print_debug("Using snapshot and changelog with id ", snapshot_id)
+        snapshot_path = os.path.join(snapshot_dir, snapshot_id + ".snapshot")
+        changelog_path = os.path.join(changelog_dir, snapshot_id + ".log")
+
+        config_path = os.path.join(self.path_to_run, "configs", "master-0-0.yson")
+        binary = os.path.join(self.bin_path, "ytserver-master")
+
+        assert self._run_validation(
+            binary,
+            snapshot_path,
+            changelog_path,
+            result_dir,
+            config_path) == 0, "Snapshot validation failed"
+
+    @authors("akozhikhov, h0pless")
     # FIXME(akozhikhov): YT-15664
     @flaky(max_runs=3)
-    def test_tablet_cell_snapshot_validation(self):
+    def test_tablet_cell_dry_run(self):
         [cell_id] = sync_create_cells(1)
+        create_dynamic_table("//tmp/dyntable", schema=[
+            {"name": "key", "type": "int64", "sort_order": "ascending"},
+            {"name": "value", "type": "string"}])
+        sync_mount_table("//tmp/dyntable")
 
-        snapshot_path = "//sys/tablet_cells/{}/snapshots".format(cell_id)
-        assert not ls(snapshot_path)
+        self._populate_tablet_cell_changelog(1, 5)
         build_snapshot(cell_id=cell_id)
-        snapshots = ls(snapshot_path)
-        assert snapshots
+        self._populate_tablet_cell_changelog(5, 12)
+        build_snapshot(cell_id=cell_id)
+
+        snapshot_dir = "//sys/tablet_cells/{}/snapshots".format(cell_id)
+        changelog_dir = "//sys/tablet_cells/{}/changelogs".format(cell_id)
+
+        snapshots = ls(snapshot_dir)
+        snapshots.sort()
+        assert len(snapshots) > 1
 
         binary = os.path.join(self.bin_path, "ytserver-node")
-
         config_path = os.path.join(self.path_to_run, "configs", "node-0.yson")
+        self._update_config(config_path)
 
-        config = None
-        with open(config_path, "rb") as fh:
-            config = b" ".join(fh.read().splitlines())
-            config = yson.loads(config)
-
-        config["data_node"]["store_locations"] = []
-        config["exec_node"]["slot_manager"]["locations"] = []
-
-        with open(config_path, "wb") as fh:
-            fh.write(yson.dumps(config, yson_format="pretty"))
-
-        snapshot = read_file("{}/{}".format(snapshot_path, snapshots[0]))
+        snapshot_id = snapshots[-2]
+        print_debug("Using snapshot and changelog with id ", snapshot_id)
+        snapshot_path = "{}/{}".format(snapshot_dir, snapshot_id)
+        snapshot = read_file(snapshot_path)
         with open("snapshot_file", "wb") as fh:
             fh.write(snapshot)
 
-        command = [
+        snapshot_meta = get(snapshot_path + "/@", attributes=[
+            "last_mutation_term",
+            "timestamp",
+            "last_segment_id",
+            "random_seed",
+            "last_record_id",
+            "sequence_number",
+            "state_hash"])
+
+        changelog_path = "{}/{}".format(changelog_dir, snapshot_id)
+        with open("changelog_file", 'wb') as fh:
+            fh.write(execute_command("read_journal", {"path": changelog_path}))
+
+        assert self._run_validation(
             binary,
-            "--validate-snapshot",
             "./snapshot_file",
-            "--config",
+            "./changelog_file",
+            ".",
             config_path,
-            "--sleep-after-initialize",
-        ]
-
-        proc = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
-        try:
-            stdout, stderr = proc.communicate(timeout=30)
-        except subprocess.TimeoutExpired:
-            proc.kill()
-            assert False, "Snapshot validation timed out"
-
-        print_debug("Snapshot validation finished (stdout: {}, stderr: {})".format(stdout, stderr))
+            cell_id,
+            snapshot_meta) == 0, "Snapshot validation failed"
