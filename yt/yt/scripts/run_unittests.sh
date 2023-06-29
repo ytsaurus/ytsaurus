b2d90d9c57b053be02ccef45d10a30ec162ea4ba
ignat <ignat@ytsaurus.tech>
2023-06-29 23:15:09 +0300

    Upload test results to nebius.testmo

--- yt/yt/scripts/run_unittests.sh
+++ yt/yt/scripts/run_unittests.sh
@@ -4,7 +4,8 @@
 # unittester-containers requires porto to be installed.
 for unittester_binary in $(find . -name "unittester-*" -type f | grep -v "unittester-containers"); do
     echo "Running ${unittester_binary}"
-    ${unittester_binary}
+    unittester_name="$(basename ${unittester_binary})"
+    ${unittester_binary} --gtest_output="xml:junit-${unittester_name}.xml"
 done
 
 for unittester_binary in $(find . -name "*-ut" -type f); do
