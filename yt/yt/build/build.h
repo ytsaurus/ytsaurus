fe9b3a0438a0927ad7e31cf5afd65b3ebb226c74
nadya73 <nadya73@ytsaurus.tech>
2023-07-13 18:10:52 +0300

    YT-18804: Support --version in os binaries
    --version in os

--- yt/yt/build/build.h
+++ yt/yt/build/build.h
@@ -9,6 +9,7 @@ int GetVersionMinor();
 int GetVersionPatch();
 const char* GetBranch();
 const char* GetVersion();
+const char* GetVersionType();
 const char* GetBuildHost();
 const char* GetBuildTime();
 
