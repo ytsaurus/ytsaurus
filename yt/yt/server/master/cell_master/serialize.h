a30007241875ca39cf8f1ad03495cb27c65b3fd6
ifsmirnov <ifsmirnov@ytsaurus.tech>
2023-07-18 21:08:47 +0300

    YT-19530: Do not include new mount config keys in user attribute list

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -119,6 +119,7 @@ DEFINE_ENUM(EMasterReign,
     ((DropNodesWithFlavorsVectorFromSnapshot)                       (2345))  // shakurov
     ((FixSymlinkCyclicityCheck)                                     (2346))  // h0pless
     ((FixChunkCreationTimeHistograms)                               (2347))  // gritukan
+    ((IncludeOnlyOldStyleMountConfigAttributesInList)               (2348))  // ifsmirnov
     // 23.2 starts here.
     ((TabletServants)                                               (2400))  // ifsmirnov
     ((MediumBase)                                                   (2401))  // gritukan
