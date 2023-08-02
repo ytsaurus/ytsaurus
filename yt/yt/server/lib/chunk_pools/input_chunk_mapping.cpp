7590e1528af720a1d1c9a9a97e5e66aa4b1ee8ed
galtsev <galtsev@ytsaurus.tech>
2023-08-02 14:04:05 +0300

    Remove two excessive log messages

--- yt/yt/server/lib/chunk_pools/input_chunk_mapping.cpp
+++ yt/yt/server/lib/chunk_pools/input_chunk_mapping.cpp
@@ -42,7 +42,6 @@ TChunkStripePtr TInputChunkMapping::GetMappedStripeGuarded(const TChunkStripePtr
     YT_VERIFY(stripe);
 
     if (Substitutes_.empty()) {
-        YT_LOG_DEBUG("Input chunk mapping has not modified the stripe");
         return stripe;
     }
 
@@ -135,8 +134,6 @@ TChunkStripePtr TInputChunkMapping::GetMappedStripeGuarded(const TChunkStripePtr
             mappedStripe->DataSlices.emplace_back(dataSlice);
         }
     }
-
-    YT_LOG_DEBUG("Input chunk mapping has modified the stripe (ChunksMapped: %v)", chunksMapped);
 
     return mappedStripe;
 }
