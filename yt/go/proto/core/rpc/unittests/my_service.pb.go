3d9ba3376132361f13bc5450fdba0f580b356563
cezarnik <cezarnik@yandex-team.com>
2023-08-09 00:45:37 +0300

    Support custom metadata in YT RPC client/server

--- yt/go/proto/core/rpc/unittests/my_service.pb.go
+++ yt/go/proto/core/rpc/unittests/my_service.pb.go
@@ -1592,6 +1592,91 @@ func (x *TRspGetTraceBaggage) GetBaggage() string {
 	return ""
 }
 
+type TReqCustomMetadata struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+}
+
+func (x *TReqCustomMetadata) Reset() {
+	*x = TReqCustomMetadata{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes[38]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TReqCustomMetadata) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TReqCustomMetadata) ProtoMessage() {}
+
+func (x *TReqCustomMetadata) ProtoReflect() protoreflect.Message {
+	mi := &file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes[38]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TReqCustomMetadata.ProtoReflect.Descriptor instead.
+func (*TReqCustomMetadata) Descriptor() ([]byte, []int) {
+	return file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDescGZIP(), []int{38}
+}
+
+type TRspCustomMetadata struct {
+	state         protoimpl.MessageState
+	sizeCache     protoimpl.SizeCache
+	unknownFields protoimpl.UnknownFields
+
+	ParsedCustomMetadata map[string][]byte `protobuf:"bytes,1,rep,name=parsed_custom_metadata,json=parsedCustomMetadata" json:"parsed_custom_metadata,omitempty" protobuf_key:"bytes,1,opt,name=key" protobuf_val:"bytes,2,opt,name=value"`
+}
+
+func (x *TRspCustomMetadata) Reset() {
+	*x = TRspCustomMetadata{}
+	if protoimpl.UnsafeEnabled {
+		mi := &file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes[39]
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		ms.StoreMessageInfo(mi)
+	}
+}
+
+func (x *TRspCustomMetadata) String() string {
+	return protoimpl.X.MessageStringOf(x)
+}
+
+func (*TRspCustomMetadata) ProtoMessage() {}
+
+func (x *TRspCustomMetadata) ProtoReflect() protoreflect.Message {
+	mi := &file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes[39]
+	if protoimpl.UnsafeEnabled && x != nil {
+		ms := protoimpl.X.MessageStateOf(protoimpl.Pointer(x))
+		if ms.LoadMessageInfo() == nil {
+			ms.StoreMessageInfo(mi)
+		}
+		return ms
+	}
+	return mi.MessageOf(x)
+}
+
+// Deprecated: Use TRspCustomMetadata.ProtoReflect.Descriptor instead.
+func (*TRspCustomMetadata) Descriptor() ([]byte, []int) {
+	return file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDescGZIP(), []int{39}
+}
+
+func (x *TRspCustomMetadata) GetParsedCustomMetadata() map[string][]byte {
+	if x != nil {
+		return x.ParsedCustomMetadata
+	}
+	return nil
+}
+
 var File_yt_yt_core_rpc_unittests_lib_my_service_proto protoreflect.FileDescriptor
 
 var file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDesc = []byte{
@@ -1677,10 +1762,25 @@ var file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDesc = []byte{
 	0x65, 0x22, 0x2f, 0x0a, 0x13, 0x54, 0x52, 0x73, 0x70, 0x47, 0x65, 0x74, 0x54, 0x72, 0x61, 0x63,
 	0x65, 0x42, 0x61, 0x67, 0x67, 0x61, 0x67, 0x65, 0x12, 0x18, 0x0a, 0x07, 0x62, 0x61, 0x67, 0x67,
 	0x61, 0x67, 0x65, 0x18, 0x01, 0x20, 0x02, 0x28, 0x09, 0x52, 0x07, 0x62, 0x61, 0x67, 0x67, 0x61,
-	0x67, 0x65, 0x42, 0x3b, 0x5a, 0x39, 0x61, 0x2e, 0x79, 0x61, 0x6e, 0x64, 0x65, 0x78, 0x2d, 0x74,
-	0x65, 0x61, 0x6d, 0x2e, 0x72, 0x75, 0x2f, 0x79, 0x74, 0x2f, 0x67, 0x6f, 0x2f, 0x70, 0x72, 0x6f,
-	0x74, 0x6f, 0x2f, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x72, 0x70, 0x63, 0x2f, 0x75, 0x6e, 0x69, 0x74,
-	0x74, 0x65, 0x73, 0x74, 0x73, 0x3b, 0x6d, 0x79, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63, 0x65,
+	0x67, 0x65, 0x22, 0x14, 0x0a, 0x12, 0x54, 0x52, 0x65, 0x71, 0x43, 0x75, 0x73, 0x74, 0x6f, 0x6d,
+	0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x22, 0xc9, 0x01, 0x0a, 0x12, 0x54, 0x52, 0x73,
+	0x70, 0x43, 0x75, 0x73, 0x74, 0x6f, 0x6d, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x12,
+	0x6a, 0x0a, 0x16, 0x70, 0x61, 0x72, 0x73, 0x65, 0x64, 0x5f, 0x63, 0x75, 0x73, 0x74, 0x6f, 0x6d,
+	0x5f, 0x6d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x18, 0x01, 0x20, 0x03, 0x28, 0x0b, 0x32,
+	0x34, 0x2e, 0x4e, 0x4d, 0x79, 0x52, 0x70, 0x63, 0x2e, 0x54, 0x52, 0x73, 0x70, 0x43, 0x75, 0x73,
+	0x74, 0x6f, 0x6d, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x2e, 0x50, 0x61, 0x72, 0x73,
+	0x65, 0x64, 0x43, 0x75, 0x73, 0x74, 0x6f, 0x6d, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61,
+	0x45, 0x6e, 0x74, 0x72, 0x79, 0x52, 0x14, 0x70, 0x61, 0x72, 0x73, 0x65, 0x64, 0x43, 0x75, 0x73,
+	0x74, 0x6f, 0x6d, 0x4d, 0x65, 0x74, 0x61, 0x64, 0x61, 0x74, 0x61, 0x1a, 0x47, 0x0a, 0x19, 0x50,
+	0x61, 0x72, 0x73, 0x65, 0x64, 0x43, 0x75, 0x73, 0x74, 0x6f, 0x6d, 0x4d, 0x65, 0x74, 0x61, 0x64,
+	0x61, 0x74, 0x61, 0x45, 0x6e, 0x74, 0x72, 0x79, 0x12, 0x10, 0x0a, 0x03, 0x6b, 0x65, 0x79, 0x18,
+	0x01, 0x20, 0x01, 0x28, 0x09, 0x52, 0x03, 0x6b, 0x65, 0x79, 0x12, 0x14, 0x0a, 0x05, 0x76, 0x61,
+	0x6c, 0x75, 0x65, 0x18, 0x02, 0x20, 0x01, 0x28, 0x0c, 0x52, 0x05, 0x76, 0x61, 0x6c, 0x75, 0x65,
+	0x3a, 0x02, 0x38, 0x01, 0x42, 0x3b, 0x5a, 0x39, 0x61, 0x2e, 0x79, 0x61, 0x6e, 0x64, 0x65, 0x78,
+	0x2d, 0x74, 0x65, 0x61, 0x6d, 0x2e, 0x72, 0x75, 0x2f, 0x79, 0x74, 0x2f, 0x67, 0x6f, 0x2f, 0x70,
+	0x72, 0x6f, 0x74, 0x6f, 0x2f, 0x63, 0x6f, 0x72, 0x65, 0x2f, 0x72, 0x70, 0x63, 0x2f, 0x75, 0x6e,
+	0x69, 0x74, 0x74, 0x65, 0x73, 0x74, 0x73, 0x3b, 0x6d, 0x79, 0x73, 0x65, 0x72, 0x76, 0x69, 0x63,
+	0x65,
 }
 
 var (
@@ -1695,7 +1795,7 @@ func file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDescGZIP() []byte {
 	return file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDescData
 }
 
-var file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes = make([]protoimpl.MessageInfo, 38)
+var file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes = make([]protoimpl.MessageInfo, 41)
 var file_yt_yt_core_rpc_unittests_lib_my_service_proto_goTypes = []interface{}{
 	(*TReqSomeCall)(nil),                  // 0: NMyRpc.TReqSomeCall
 	(*TRspSomeCall)(nil),                  // 1: NMyRpc.TRspSomeCall
@@ -1735,15 +1835,19 @@ var file_yt_yt_core_rpc_unittests_lib_my_service_proto_goTypes = []interface{}{
 	(*TRspServerNotWriting)(nil),          // 35: NMyRpc.TRspServerNotWriting
 	(*TReqGetTraceBaggage)(nil),           // 36: NMyRpc.TReqGetTraceBaggage
 	(*TRspGetTraceBaggage)(nil),           // 37: NMyRpc.TRspGetTraceBaggage
-	(*misc.TGuid)(nil),                    // 38: NYT.NProto.TGuid
+	(*TReqCustomMetadata)(nil),            // 38: NMyRpc.TReqCustomMetadata
+	(*TRspCustomMetadata)(nil),            // 39: NMyRpc.TRspCustomMetadata
+	nil,                                   // 40: NMyRpc.TRspCustomMetadata.ParsedCustomMetadataEntry
+	(*misc.TGuid)(nil),                    // 41: NYT.NProto.TGuid
 }
 var file_yt_yt_core_rpc_unittests_lib_my_service_proto_depIdxs = []int32{
-	38, // 0: NMyRpc.TRspPassCall.mutation_id:type_name -> NYT.NProto.TGuid
-	1,  // [1:1] is the sub-list for method output_type
-	1,  // [1:1] is the sub-list for method input_type
-	1,  // [1:1] is the sub-list for extension type_name
-	1,  // [1:1] is the sub-list for extension extendee
-	0,  // [0:1] is the sub-list for field type_name
+	41, // 0: NMyRpc.TRspPassCall.mutation_id:type_name -> NYT.NProto.TGuid
+	40, // 1: NMyRpc.TRspCustomMetadata.parsed_custom_metadata:type_name -> NMyRpc.TRspCustomMetadata.ParsedCustomMetadataEntry
+	2,  // [2:2] is the sub-list for method output_type
+	2,  // [2:2] is the sub-list for method input_type
+	2,  // [2:2] is the sub-list for extension type_name
+	2,  // [2:2] is the sub-list for extension extendee
+	0,  // [0:2] is the sub-list for field type_name
 }
 
 func init() { file_yt_yt_core_rpc_unittests_lib_my_service_proto_init() }
@@ -2208,6 +2312,30 @@ func file_yt_yt_core_rpc_unittests_lib_my_service_proto_init() {
 				return nil
 			}
 		}
+		file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes[38].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*TReqCustomMetadata); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
+		file_yt_yt_core_rpc_unittests_lib_my_service_proto_msgTypes[39].Exporter = func(v interface{}, i int) interface{} {
+			switch v := v.(*TRspCustomMetadata); i {
+			case 0:
+				return &v.state
+			case 1:
+				return &v.sizeCache
+			case 2:
+				return &v.unknownFields
+			default:
+				return nil
+			}
+		}
 	}
 	type x struct{}
 	out := protoimpl.TypeBuilder{
@@ -2215,7 +2343,7 @@ func file_yt_yt_core_rpc_unittests_lib_my_service_proto_init() {
 			GoPackagePath: reflect.TypeOf(x{}).PkgPath(),
 			RawDescriptor: file_yt_yt_core_rpc_unittests_lib_my_service_proto_rawDesc,
 			NumEnums:      0,
-			NumMessages:   38,
+			NumMessages:   41,
 			NumExtensions: 0,
 			NumServices:   0,
 		},
