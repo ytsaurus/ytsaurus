d1393d1b07a0d67cf1b0187ae88f0370de5331a5
ni-stoiko <ni-stoiko@ytsaurus.tech>
2023-08-30 12:26:15 +0300

    YT-19556: Removing usage of MemoryTag
    Clean RPC from CurrentMemoryTag. Update rpc_allocation_tags_ut
    
    Restore memory_tag_ut.cpp
    
    Remove MemoryTag from core

--- yt/yt/library/ytprof/memory_tag_compat/compat.cpp
+++ yt/yt/library/ytprof/memory_tag_compat/compat.cpp
@@ -6,16 +6,6 @@ namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TMemoryTag GetCurrentMemoryTag()
-{
-    return static_cast<TMemoryTag>(NYTProf::GetMemoryTag());
-}
-
-void SetCurrentMemoryTag(TMemoryTag tag)
-{
-    NYTProf::SetMemoryTag(static_cast<NYTProf::TMemoryTag>(tag));
-}
-
 size_t GetMemoryUsageForTag(TMemoryTag tag)
 {
     return NYTProf::GetEstimatedMemoryUsage(static_cast<NYTProf::TMemoryTag>(tag));
