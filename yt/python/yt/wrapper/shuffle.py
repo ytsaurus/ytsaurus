5ec933320a11f6c1b02e5ed219af742ecb1ab86c
denvr <denvr@ytsaurus.tech>
2023-08-31 17:26:06 +0300

    YT-19821 - independed shuffle

--- yt/python/yt/wrapper/shuffle.py
+++ yt/python/yt/wrapper/shuffle.py
@@ -2,6 +2,7 @@ from .common import forbidden_inside_job
 from .run_operation_commands import run_map_reduce
 from .cypress_commands import get
 from .ypath import TablePath
+from .format import SkiffFormat
 
 
 @forbidden_inside_job
@@ -15,15 +16,44 @@ def shuffle_table(table, sync=True, temp_column_name="__random_number", spec=Non
 
     . seealso::  :ref:`operation_parameters`.
     """
-    def shuffle_mapper(row):
-        import random
-        row[temp_column_name] = random.random()
-        yield row
 
-    def shuffle_reducer(_, rows):
-        for row in rows:
-            del row[temp_column_name]
-            yield row
+    shuffle_mapper = """python3 -c '
+import sys, struct, random
+i = sys.stdin.buffer
+o = sys.stdout.buffer
+while(not i.closed):
+  hdr = i.read(6)
+  if not hdr:
+    break
+  ti, yson_len = struct.unpack_from("<HI", hdr, 0)
+  o.write(struct.pack("<HdI", ti, random.random(), yson_len))
+  d = i.read(yson_len)
+  if len(d) != yson_len:
+    raise RuntimeError()
+  o.write(d)'
+"""
+
+    shuffle_reducer = """python3 -c '
+import sys, struct
+i = sys.stdin.buffer
+o = sys.stdout.buffer
+while(not i.closed):
+  hdr = i.read(14)
+  if not hdr:
+    break
+  ti, rnd, yson_len = struct.unpack_from("<HdI", hdr, 0)
+  o.write(struct.pack("<HI", ti, yson_len))
+  d = i.read(yson_len)
+  if len(d) != yson_len:
+    raise RuntimeError()
+  o.write(d)'
+"""
+
+    mapper_input_format = SkiffFormat([{"wire_type": "tuple", "children": [{"name": "$other_columns", "wire_type": "yson32"}]}])
+    mapper_output_format = SkiffFormat([{"wire_type": "tuple", "children": [{"name": temp_column_name, "wire_type": "double"}, {"name": "$other_columns", "wire_type": "yson32"}]}])
+
+    reducer_input_format = mapper_output_format
+    reducer_output_format = mapper_input_format
 
     schema = None
     attributes = get(table + "/@", attributes=["schema", "schema_mode"], client=client)
@@ -34,4 +64,6 @@ def shuffle_table(table, sync=True, temp_column_name="__random_number", spec=Non
                 del elem["sort_order"]
 
     return run_map_reduce(shuffle_mapper, shuffle_reducer, table, TablePath(table, schema=schema, client=client),
+                          map_input_format=mapper_input_format, map_output_format=mapper_output_format,
+                          reduce_input_format=reducer_input_format, reduce_output_format=reducer_output_format,
                           reduce_by=temp_column_name, sync=sync, spec=spec, client=client)
