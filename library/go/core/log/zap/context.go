d1755bb8a5d98b2b4ff61f1499ce902a727c698b
antmat <antmat@yandex-team.com>
2023-09-14 18:56:43 +0300

    feat: new approach for context fields logging

---
+++ library/go/core/log/zap/context.go
@@ -0,0 +1,31 @@
+package zap
+
+import (
+	"context"
+
+	"go.uber.org/zap"
+	"go.uber.org/zap/zapcore"
+	"go.ytsaurus.tech/library/go/core/log/ctxlog"
+)
+
+type ctxField struct {
+	ctx context.Context
+}
+
+// MarshalLogObject implements zapcore.ObjectMarshaler to append context fields directly to encoder in a lazy manner
+func (c ctxField) MarshalLogObject(encoder zapcore.ObjectEncoder) error {
+	fields := ctxlog.ContextFields(c.ctx)
+	for _, f := range fields {
+		zapifyField(f).AddTo(encoder)
+	}
+	return nil
+}
+
+// Context creates a log field from context - all fields bound with ctxlog.WithFields will be added.
+func Context(ctx context.Context) zap.Field {
+	return zap.Field{
+		Key:       "",
+		Type:      zapcore.InlineMarshalerType,
+		Interface: ctxField{ctx: ctx},
+	}
+}
