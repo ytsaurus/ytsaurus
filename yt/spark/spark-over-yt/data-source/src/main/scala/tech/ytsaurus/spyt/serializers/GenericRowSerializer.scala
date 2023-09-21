9c31e9ad780d8c662e85e79378bc40e73eb29bf1
alex-shishkin <alex-shishkin@ytsaurus.tech>
2023-09-21 19:52:38 +0300

    SPYT-463: Fix dataframe serialization

--- yt/spark/spark-over-yt/data-source/src/main/scala/tech/ytsaurus/spyt/serializers/GenericRowSerializer.scala
+++ yt/spark/spark-over-yt/data-source/src/main/scala/tech/ytsaurus/spyt/serializers/GenericRowSerializer.scala
@@ -1,9 +1,9 @@
 package tech.ytsaurus.spyt.serializers
 
 import org.apache.spark.sql.types._
-import org.apache.spark.sql.yson.{UInt64Type, YsonType}
+import org.apache.spark.sql.yson.{UInt64Type, YsonBinary, YsonType}
 import org.apache.spark.sql.{DataFrame, Row}
-import tech.ytsaurus.client.rows.{WireProtocolWriteable, WireProtocolWriter, WireRowSerializer}
+import tech.ytsaurus.client.rows.{UnversionedRow, UnversionedValue, WireProtocolWriter}
 import tech.ytsaurus.core.tables.{ColumnValueType, TableSchema}
 import tech.ytsaurus.spyt.serialization.YsonEncoder
 import tech.ytsaurus.spyt.serializers.SchemaConverter.Unordered
@@ -11,81 +11,59 @@ import tech.ytsaurus.spyt.serializers.SchemaConverter.Unordered
 import java.util.Base64
 
 // TODO(alex-shishkin): Supported type v1 only
-class GenericRowSerializer(schema: StructType) extends WireRowSerializer[Row] {
+class GenericRowSerializer(schema: StructType) {
   private val tableSchema = SchemaConverter.tableSchema(schema, Unordered, Map.empty)
 
-  override def getSchema: TableSchema = tableSchema
-
-  private def valueId(id: Int, idMapping: Array[Int]): Int = {
-    if (idMapping != null) {
-      idMapping(id)
-    } else id
+  private def boxValue(i: Int, value: Any): UnversionedValue = {
+    new UnversionedValue(i, tableSchema.getColumnType(i), false, value)
   }
 
-  private def writeHeader(writeable: WireProtocolWriteable, idMapping: Array[Int], aggregate: Boolean,
-                          i: Int, length: Int): Unit = {
-    writeable.writeValueHeader(valueId(i, idMapping), tableSchema.getColumnType(i), aggregate, length)
-  }
-
-  private def writeBytes(writeable: WireProtocolWriteable, idMapping: Array[Int], aggregate: Boolean,
-                         i: Int, bytes: Array[Byte]): Unit = {
-    writeHeader(writeable, idMapping, aggregate, i, bytes.length)
-    writeable.onBytes(bytes)
-  }
-
-  override def serializeRow(row: Row,
-                            writeable: WireProtocolWriteable,
-                            keyFieldsOnly: Boolean,
-                            aggregate: Boolean,
-                            idMapping: Array[Int]): Unit = {
-    writeable.writeValueCount(row.length)
-    for {
-      i <- 0 until row.length
-    } {
-      if (row.isNullAt(i)) {
-        writeable.writeValueHeader(valueId(i, idMapping), ColumnValueType.NULL, aggregate, 0)
-      } else {
-        val sparkField = schema(i)
-        sparkField.dataType match {
-          case BinaryType | YsonType => writeBytes(writeable, idMapping, aggregate, i, row.getAs[Array[Byte]](i))
-          case StringType => writeBytes(writeable, idMapping, aggregate, i, row.getString(i).getBytes)
-          case t@(ArrayType(_, _) | StructType(_) | MapType(_, _, _)) =>
-            val skipNulls = sparkField.metadata.contains("skipNulls") && sparkField.metadata.getBoolean("skipNulls")
-            writeBytes(writeable, idMapping, aggregate, i, YsonEncoder.encode(row.get(i), t, skipNulls))
-          case atomic =>
-            writeHeader(writeable, idMapping, aggregate, i, 0)
-            atomic match {
-              case ByteType => writeable.onInteger(row.getByte(i))
-              case ShortType => writeable.onInteger(row.getShort(i))
-              case IntegerType => writeable.onInteger(row.getInt(i))
-              case LongType => writeable.onInteger(row.getLong(i))
-              case BooleanType => writeable.onBoolean(row.getBoolean(i))
-              case FloatType => writeable.onDouble(row.getFloat(i))
-              case DoubleType => writeable.onDouble(row.getDouble(i))
-              case UInt64Type => writeable.onInteger(row.getLong(i))
-              case DateType => writeable.onInteger(row.getLong(i))
-              case TimestampType => writeable.onInteger(row.getLong(i) / 1000000)
-            }
-        }
+  def serializeValue(row: Row, i: Int): UnversionedValue = {
+    if (row.isNullAt(i)) {
+      new UnversionedValue(i, ColumnValueType.NULL, false, null)
+    } else {
+      val sparkField = schema(i)
+      sparkField.dataType match {
+        case BinaryType => boxValue(i, row.getAs[Array[Byte]](i))
+        case YsonType => boxValue(i, row.getAs[YsonBinary](i).bytes)
+        case StringType => boxValue(i, row.getString(i).getBytes)
+        case t@(ArrayType(_, _) | StructType(_) | MapType(_, _, _)) =>
+          val skipNulls = sparkField.metadata.contains("skipNulls") && sparkField.metadata.getBoolean("skipNulls")
+          boxValue(i, YsonEncoder.encode(row.get(i), t, skipNulls))
+        case atomic =>
+          atomic match {
+            case ByteType => boxValue(i, row.getByte(i).toLong)
+            case ShortType => boxValue(i, row.getShort(i).toLong)
+            case IntegerType => boxValue(i, row.getInt(i).toLong)
+            case LongType => boxValue(i, row.getLong(i))
+            case BooleanType => boxValue(i, row.getBoolean(i))
+            case FloatType => boxValue(i, row.getFloat(i).toDouble)
+            case DoubleType => boxValue(i, row.getDouble(i))
+            case UInt64Type => boxValue(i, row.getLong(i))
+            case DateType => boxValue(i, row.getLong(i))
+            case TimestampType => boxValue(i, row.getLong(i) / 1000000)
+          }
       }
     }
+  }
+
+  def serializeRow(row: Row): UnversionedRow = {
+    import scala.collection.JavaConverters._
+    new UnversionedRow((0 until row.length).map(i => serializeValue(row, i)).toList.asJava)
+  }
+
+  def serializeTable(rows: Array[Row]): Seq[Array[Byte]] = {
+    import scala.collection.JavaConverters._
+    val writer = new WireProtocolWriter
+    writer.writeTableSchema(tableSchema)
+    writer.writeSchemafulRowset(rows.map(serializeRow).toList.asJava)
+    val result = writer.finish
+    result.asScala
   }
 }
 
 object GenericRowSerializer {
-  def dfToYTFormatWithBase64(df: DataFrame): String = Base64.getEncoder.encodeToString(dfToYTFormat(df))
+  def dfToYTFormatWithBase64(df: DataFrame): Seq[String] = dfToYTFormat(df).map(Base64.getEncoder.encodeToString)
 
-  def dfToYTFormat(df: DataFrame): Array[Byte] = rowsToYTFormat(df.schema, df.collect())
-
-  def rowsToYTFormat(schema: StructType, rows: Array[Row]): Array[Byte] = {
-    serializeRows(new GenericRowSerializer(schema), rows)
-  }
-
-  def serializeRows[T](wireRowSerializer: WireRowSerializer[T], rows: Array[T]): Array[Byte] = {
-    import scala.collection.JavaConverters._
-    val writer = new WireProtocolWriter
-    writer.writeUnversionedRowset(rows.toList.asJava, wireRowSerializer)
-    val result = writer.finish
-    result.get(0)
-  }
+  def dfToYTFormat(df: DataFrame): Seq[Array[Byte]] = new GenericRowSerializer(df.schema).serializeTable(df.collect())
 }
--- yt/spark/spark-over-yt/data-source/src/test/scala/tech/ytsaurus/spyt/serializers/DataFrameSerializerTest.scala
+++ yt/spark/spark-over-yt/data-source/src/test/scala/tech/ytsaurus/spyt/serializers/DataFrameSerializerTest.scala
@@ -1,5 +1,6 @@
 package tech.ytsaurus.spyt.serializers
 
+import org.apache.spark.sql.types._
 import org.scalatest.{FlatSpec, Matchers}
 import tech.ytsaurus.core.tables.{ColumnValueType, TableSchema}
 import tech.ytsaurus.spyt._
@@ -14,6 +15,11 @@ class DataFrameSerializerTest extends FlatSpec with Matchers with LocalSpark
     .addValue("c", ColumnValueType.DOUBLE)
     .build()
 
+  private val anySchema = TableSchema.builder()
+    .setUniqueKeys(false)
+    .addValue("value", ColumnValueType.ANY)
+    .build()
+
   it should "serialize dataframe to byte array" in {
     writeTableFromYson(Seq(
       """{a = 0; b = #; c = 0.0}"""
@@ -21,9 +27,15 @@ class DataFrameSerializerTest extends FlatSpec with Matchers with LocalSpark
 
     val res = spark.read.yt(tmpPath)
     val tableBytes = GenericRowSerializer.dfToYTFormat(res)
-    val answer = Array(1, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 3, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0,
-      1, 0, 2, 0, 0, 0, 0, 0, 2, 0, 5, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0)
-    tableBytes shouldBe answer
+    val answer = Array(Array(-99, 0, 0, 0, 0, 0, 0, 0, 10, 49, 10, 1, 97, 16, 3, 72, 0, 90, 40, 123, 34, 116, 121,
+      112, 101, 95, 110, 97, 109, 101, 34, 61, 34, 111, 112, 116, 105, 111, 110, 97, 108, 34, 59, 34, 105, 116, 101,
+      109, 34, 61, 34, 105, 110, 116, 54, 52, 34, 59, 125, 10, 50, 10, 1, 98, 16, 16, 72, 0, 90, 41, 123, 34, 116, 121,
+      112, 101, 95, 110, 97, 109, 101, 34, 61, 34, 111, 112, 116, 105, 111, 110, 97, 108, 34, 59, 34, 105, 116, 101,
+      109, 34, 61, 34, 115, 116, 114, 105, 110, 103, 34, 59, 125, 10, 50, 10, 1, 99, 16, 5, 72, 0, 90, 41, 123, 34,
+      116, 121, 112, 101, 95, 110, 97, 109, 101, 34, 61, 34, 111, 112, 116, 105, 111, 110, 97, 108, 34, 59, 34, 105,
+      116, 101, 109, 34, 61, 34, 100, 111, 117, 98, 108, 101, 34, 59, 125, 24, 0, 0, 0, 0, 1, 0, 0, 0, 0, 0, 0, 0, 3,
+      0, 0, 0, 0, 0, 0, 0, 2, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0, 0))
+    tableBytes should contain theSameElementsAs answer
   }
 
   it should "serialize dataframe to base64" in {
@@ -34,8 +46,48 @@ class DataFrameSerializerTest extends FlatSpec with Matchers with LocalSpark
 
     val res = spark.read.yt(tmpPath)
     val tableBytes = GenericRowSerializer.dfToYTFormatWithBase64(res)
-    val answer = "AgAAAAAAAAADAAAAAAAAAAAAAwAAAAAAAQAAAAAAAAABABAAAQAAAGEAAAAAAAAAAgAFAAAAAAAzMzMzMzPTPwMAAAAAAAAAAAAD" +
-      "AAAAAAACAAAAAAAAAAEAEAABAAAAYgAAAAAAAAACAAUAAAAAAAAAAAAAAOA/"
-    tableBytes shouldBe answer
+    val answer = Array(
+      "nQAAAAAAAAAKMQoBYRADSABaKHsidHlwZV9uYW1lIj0ib3B0aW9uYWwiOyJpdGVtIj0iaW50NjQiO30KMgoBYhAQSABaKXsidHlwZV9uYW1l" +
+        "Ij0ib3B0aW9uYWwiOyJpdGVtIj0ic3RyaW5nIjt9CjIKAWMQBUgAWil7InR5cGVfbmFtZSI9Im9wdGlvbmFsIjsiaXRlbSI9ImRvdWJsZSI" +
+        "7fRgAAAAAAgAAAAAAAAADAAAAAAAAAAAAAAAAAAAAAQAAAAAAAAABAAAAAAAAAGEAAAAAAAAAMzMzMzMz0z8DAAAAAAAAAAAAAAAAAAAAAgA" +
+        "AAAAAAAABAAAAAAAAAGIAAAAAAAAAAAAAAAAA4D8="
+    )
+    tableBytes should contain theSameElementsAs answer
+  }
+
+  it should "serialize lists" in {
+    writeTableFromYson(Seq(
+      "{value = [[1]; [2; 3]]}",
+      "{value = [[4]; #]}"
+    ), tmpPath, anySchema)
+
+    val res = spark.read
+      .schemaHint("value" -> ArrayType(ArrayType(LongType)))
+      .yt(tmpPath)
+
+    val tableBytes = GenericRowSerializer.dfToYTFormatWithBase64(res)
+    val answer = Array(
+      "OAAAAAAAAAAKNAoFdmFsdWUQEUgAWid7InR5cGVfbmFtZSI9Im9wdGlvbmFsIjsiaXRlbSI9Inlzb24iO30YAAIAAAAAAAAAAQAAAAAAAAA" +
+        "AAAAAAAAAAA4AAAAAAAAAW1sCAl07WwIEOwIGXV0AAAEAAAAAAAAAAAAAAAAAAAAIAAAAAAAAAFtbAghdOyNd"
+    )
+    tableBytes should contain theSameElementsAs answer
+  }
+
+  it should "serialize map" in {
+    writeTableFromYson(Seq(
+      """{value = [[1; "2"]; [3; "4"]]}""",
+      """{value = [[5; "6"]; [7; "8"]]}"""
+    ), tmpPath, anySchema)
+
+    val res = spark.read
+      .schemaHint("value" -> MapType(LongType, StringType))
+      .yt(tmpPath)
+
+    val tableBytes = GenericRowSerializer.dfToYTFormatWithBase64(res)
+    val answer = Array(
+      "OAAAAAAAAAAKNAoFdmFsdWUQEUgAWid7InR5cGVfbmFtZSI9Im9wdGlvbmFsIjsiaXRlbSI9Inlzb24iO30YAAIAAAAAAAAAAQAAAAAAAAAAA" +
+        "AAAAAAAABEAAAAAAAAAewECMz0BAjQ7AQIxPQECMn0AAAAAAAAAAQAAAAAAAAAAAAAAAAAAABEAAAAAAAAAewECNz0BAjg7AQI1PQECNn0AAAAAAAAA"
+    )
+    tableBytes should contain theSameElementsAs answer
   }
 }
