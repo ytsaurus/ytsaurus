5d55881ba48c284314a57c503f74911253c5b0b8
omgronny <omgronny@ytsaurus.tech>
2023-09-25 14:05:04 +0300

    YT-19785: [23/1] Style fixes for style fixes for child_pools_by_pool orchid

--- yt/yt/server/scheduler/fair_share_tree.cpp
+++ yt/yt/server/scheduler/fair_share_tree.cpp
@@ -2956,7 +2956,8 @@ private:
         fluent
             .DoFor(parentPool->EnabledChildren(), [&] (TFluentMap fluent, const TSchedulerElementPtr& pool) {
                 fluent.Item(pool->GetId())
-                    .BeginMap().Do(std::bind(&TFairShareTree::BuildPoolInfo,
+                    .BeginMap().Do(
+                        std::bind(&TFairShareTree::BuildPoolInfo,
                         std::cref(treeSnapshot),
                         static_cast<TSchedulerPoolElement*>(pool.Get()),
                         std::cref(filter),
