db288fc900bc360949e435f8691b422339217a8e
Natalya Chizhonkova <gudqeit@sandbox-542939141>
2023-09-27 10:20:53 +0300

    fix python packages

---
+++ yt/python/packages/ytsaurus-native-driver/setup.py
@@ -0,0 +1,48 @@
+PACKAGE_NAME = "ytsaurus-native-driver"
+
+MAJOR_VERSION = "1.0"
+
+
+def main():
+    from setuptools import setup
+    from setuptools.dist import Distribution
+
+    from yt_setup.helpers import get_package_version
+
+    version = get_package_version(MAJOR_VERSION)
+
+    class BinaryDistribution(Distribution):
+        def is_pure(self):
+            return False
+
+        # Python 3 specific
+        def has_ext_modules(self):
+            return True
+
+    setup(
+        name=PACKAGE_NAME,
+        version=version,
+        packages=["yt_driver_bindings"],
+        package_data={
+            "yt_driver_bindings": [
+                "driver_lib.so",
+                "driver_lib_d.so",
+                "driver_lib.abi3.so",
+                "driver_lib.abi3_d.so",
+            ],
+        },
+
+        author="YTsaurus",
+        author_email="dev@ytsaurus.tech",
+        license="Apache 2.0",
+
+        description="C++ native driver.",
+        keywords="yt ytsaurus python bindings driver",
+
+        include_package_data=True,
+        distclass=BinaryDistribution,
+    )
+
+
+if __name__ == "__main__":
+    main()
