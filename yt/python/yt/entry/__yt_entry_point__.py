7603a03673ef143a56242c090a157b676bdf0acc
denvr <denvr@ytsaurus.tech>
2023-10-04 16:06:23 +0300

    YT-19848 - arcadia python yt

--- yt/python/yt/entry/__yt_entry_point__.py
+++ yt/python/yt/entry/__yt_entry_point__.py
@@ -3,8 +3,10 @@ try:
 except ImportError:
     pass  # Not Arcadia.
 else:
-    py_main = __res.find("PY_MAIN")  # None, "mod:func" or "mod".
-    if py_main:
+    py_main = __res.find("PY_MAIN")  # None, "some_mod:some_func" or "some_mod" or ":main".
+    if py_main == b":main":  # PY_MAIN=:main (python repl) - arcadia standalone python interpreter
+        pass
+    elif py_main is not None and py_main != b":main":  # PY_MAIN=some_mod:some_func or some_mod - arcadia single binary file
         import importlib
         module_name = py_main.split(b":", 1)[0]
         module = importlib.import_module(module_name.decode("utf-8"))
@@ -12,9 +14,10 @@ else:
             # __main__.py compatibility mode.
             import sys
             sys.modules["__main__"] = module
-    else:
-        # Python 2 with __main__.py.
+    elif py_main is None:  # Python 2 with __main__.py.
         __res.importer.load_module("__main__", fix_name="__yt_main__")
+    else:
+        raise RuntimeError("Unknown PY_MAIN ({})".format(py_main))
 
 import yt.wrapper
 
