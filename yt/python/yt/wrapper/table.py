06f79fe78a411e1d2f6d73592ca2ba642896cf72
alexrak <alexrak@yandex-team.com>
2023-10-04 12:11:58 +0300

    Add expiration_timeout to TempTable

--- yt/python/yt/wrapper/table.py
+++ yt/python/yt/wrapper/table.py
@@ -2,7 +2,7 @@ from contextlib import contextmanager
 
 
 @contextmanager
-def TempTable(path=None, prefix=None, attributes=None, client=None):
+def TempTable(path=None, prefix=None, attributes=None, expiration_timeout=None, client=None):
     """Creates temporary table in given path with given prefix on scope enter and \
        removes it on scope exit.
 
@@ -11,7 +11,7 @@ def TempTable(path=None, prefix=None, attributes=None, client=None):
     from .cypress_commands import remove
     from .table_commands import create_temp_table
 
-    table = create_temp_table(path, prefix, attributes, client=client)
+    table = create_temp_table(path, prefix, attributes, expiration_timeout, client=client)
     try:
         yield table
     finally:
