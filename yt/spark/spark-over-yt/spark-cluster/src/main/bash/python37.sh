2f4dd47073a7a513dc6a0a9035dad6255ac12990
atokarew <atokarew@yandex-team.com>
2023-10-18 12:06:49 +0300

    SPYT-512 Explicitly uninstalling yandex-yt in porto layers

--- yt/spark/spark-over-yt/spark-cluster/src/main/bash/python37.sh
+++ yt/spark/spark-over-yt/spark-cluster/src/main/bash/python37.sh
@@ -16,21 +16,24 @@ if [ -e "/bin/systemctl" ]; then
 fi
 apt-get update
 
+PYTHON_VERSION="python3.7"
+
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update --allow-unauthenticated
 apt-get install -y build-essential manpages-dev libpcap-dev libpq-dev libssl-dev libffi-dev
-apt-get install -y python3.7 python3.7-dev python3.7-distutils python3.7-venv
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.7
-python3.7 -m pip install --upgrade pip
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple numpy==1.21.6
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple ytsaurus-client==0.13.7
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple pyarrow==12.0.1
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple pandas==1.1.5
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple cyson
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple scipy==1.7.3
-python3.7 -m pip install -i https://pypi.yandex-team.ru/simple pyyaml==6.0
+apt-get install -y $PYTHON_VERSION $PYTHON_VERSION-dev $PYTHON_VERSION-distutils $PYTHON_VERSION-venv
+curl -sS https://bootstrap.pypa.io/get-pip.py | $PYTHON_VERSION
+$PYTHON_VERSION -m pip install --upgrade pip
+$PYTHON_VERSION -m pip uninstall yandex-yt
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple numpy==1.21.6
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple ytsaurus-client==0.13.7
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple pyarrow==12.0.1
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple pandas==1.1.5
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple cyson
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple scipy==1.7.3
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple pyyaml==6.0
 
-mkdir -p /opt/python3.7/bin
-ln -s /usr/bin/python3.7 /opt/python3.7/bin/python
-ln -s /usr/bin/python3.7 /opt/python3.7/bin/python3.7
+mkdir -p /opt/$PYTHON_VERSION/bin
+ln -s /usr/bin/$PYTHON_VERSION /opt/$PYTHON_VERSION/bin/python
+ln -s /usr/bin/$PYTHON_VERSION /opt/$PYTHON_VERSION/bin/$PYTHON_VERSION
--- yt/spark/spark-over-yt/spark-cluster/src/main/bash/python38.sh
+++ yt/spark/spark-over-yt/spark-cluster/src/main/bash/python38.sh
@@ -16,21 +16,24 @@ if [ -e "/bin/systemctl" ]; then
 fi
 apt-get update
 
+PYTHON_VERSION="python3.8"
+
 apt-get install -y software-properties-common
 add-apt-repository -y ppa:deadsnakes/ppa
 apt-get update --allow-unauthenticated
 apt-get install -y build-essential manpages-dev libpcap-dev libpq-dev libssl-dev libffi-dev
-apt-get install -y python3.8 python3.8-dev python3.8-distutils python3.8-venv
-curl -sS https://bootstrap.pypa.io/get-pip.py | python3.8
-python3.8 -m pip install --upgrade pip
-python3.8 -m pip install -i https://pypi.yandex-team.ru/simple ytsaurus-client==0.13.7
-python3.8 -m pip install -i https://pypi.yandex-team.ru/simple pyarrow==12.0.1
-python3.8 -m pip install -i https://pypi.yandex-team.ru/simple pandas==1.5.3
-# python3.8 -m pip install -i https://pypi.yandex-team.ru/simple cython==3.0.0b1
-# python3.8 -m pip install -i https://pypi.yandex-team.ru/simple cyson
-python3.8 -m pip install -i https://pypi.yandex-team.ru/simple scipy==1.10.1
-python3.8 -m pip install -i https://pypi.yandex-team.ru/simple numpy==1.24.2
+apt-get install -y $PYTHON_VERSION $PYTHON_VERSION-dev $PYTHON_VERSION-distutils $PYTHON_VERSION-venv
+curl -sS https://bootstrap.pypa.io/get-pip.py | $PYTHON_VERSION
+$PYTHON_VERSION -m pip install --upgrade pip
+$PYTHON_VERSION -m pip uninstall yandex-yt
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple ytsaurus-client==0.13.7
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple pyarrow==12.0.1
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple pandas==1.5.3
+# $PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple cython==3.0.0b1
+# $PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple cyson
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple scipy==1.10.1
+$PYTHON_VERSION -m pip install -i https://pypi.yandex-team.ru/simple numpy==1.24.2
 
-mkdir -p /opt/python3.8/bin
-ln -s /usr/bin/python3.8 /opt/python3.8/bin/python
-ln -s /usr/bin/python3.8 /opt/python3.8/bin/python3.8
+mkdir -p /opt/$PYTHON_VERSION/bin
+ln -s /usr/bin/$PYTHON_VERSION /opt/$PYTHON_VERSION/bin/python
+ln -s /usr/bin/$PYTHON_VERSION /opt/$PYTHON_VERSION/bin/$PYTHON_VERSION
