7fef17c82bb0b5f9a6cf7312c897e09673911530
annashest18 <annashest18@yandex-team.com>
2023-10-22 13:19:53 +0300

    add using http-proxy for reading table from YT
    add using http-proxy for reading table from YT
    Нам нужна возможность ходить  в YT через HTTP proxy для чтения таблиц, используя С++ клиент не из контура Яндекса, к сожалению, сейчас такой возможности нет. В этом ПР черновик изменения, которого нам достаточно
    https://a.yandex-team.ru/review/4676436/details - тут это же изменение в YT + коммит с тем, как мы планируем использовать

--- yt/cpp/mapreduce/http/ut/connection_pool_ut.cpp
+++ yt/cpp/mapreduce/http/ut/connection_pool_ut.cpp
@@ -16,6 +16,19 @@
 
 using namespace NYT;
 
+namespace {
+    void ParseFirstLine(const TString firstLine, TString& method, TString& host , ui64& port, TString& command) {
+        size_t idx = firstLine.find_first_of(' ');
+        method = firstLine.substr(0, idx);
+        size_t idx2 = firstLine.find_first_of(':', idx + 1);
+        host = firstLine.substr(idx + 1, idx2 - idx - 1);
+        idx = firstLine.find_first_of('/', idx2 + 1);
+        port = std::atoi(firstLine.substr(idx2 + 1, idx - idx2 - 1).c_str());
+        idx2 = firstLine.find_first_of(' ', idx + 1);
+        command = firstLine.substr(idx, idx2 - idx);
+    }
+}
+
 THolder<TSimpleServer> CreateSimpleHttpServer() {
     TPortManager pm;
     int port = pm.GetPort();
@@ -34,6 +47,41 @@ THolder<TSimpleServer> CreateSimpleHttpServer() {
                     for (size_t i = 0; i != 10000; ++i) {
                         httpOutput << "The grass was greener";
                     }
+                    httpOutput.Flush();
+                }
+            } catch (const std::exception&) {
+            }
+        });
+}
+
+THolder<TSimpleServer> CreateProxyHttpServer() {
+    TPortManager pm;
+    int port = pm.GetPort();
+    return MakeHolder<TSimpleServer>(
+        port,
+        [] (IInputStream* input, IOutputStream* output) {
+            try {
+                while (true) {
+                    THttpInput httpInput(input);
+                    const TString inputStr = httpInput.FirstLine();
+                    auto headers = httpInput.Headers();
+                    TString method, command, host;
+                    ui64 port;
+                    ParseFirstLine(inputStr, method, host, port, command);
+
+                    THttpRequest request;
+                    const TString hostName = ::TStringBuilder() << host << ":" << port;
+                    request.Connect(hostName);
+                    auto header = THttpHeader(method, command);
+                    request.StartRequest(header);
+                    request.FinishRequest();
+                    auto res = request.GetResponseStream();
+                    THttpOutput httpOutput(output);
+                    httpOutput.EnableKeepAlive(true);
+                    auto strRes = res->ReadAll();
+                    httpOutput << "HTTP/1.1 200 OK\r\n";
+                    httpOutput << "\r\n";
+                    httpOutput << strRes;
                     httpOutput.Flush();
                 }
             } catch (const std::exception&) {
@@ -85,6 +133,25 @@ Y_UNIT_TEST_SUITE(NConnectionPoolSuite) {
             THttpRequest request;
             request.Connect(hostName);
             request.StartRequest(THttpHeader("GET", "foo"));
+            request.FinishRequest();
+            request.GetResponseStream();
+        }
+    }
+
+    Y_UNIT_TEST(TestProxy) {
+        auto simpleServer = CreateSimpleHttpServer();
+        auto simpleServer2 = CreateProxyHttpServer();
+
+        const TString hostName = ::TStringBuilder() << "localhost:" << simpleServer->GetPort();
+        const TString hostName2 = ::TStringBuilder() << "localhost:" << simpleServer2->GetPort();
+
+        for (size_t i = 0; i != 10; ++i) {
+            THttpRequest request;
+            request.Connect(hostName2);
+            auto header = THttpHeader("GET", "foo");
+            header.SetProxyAddress(hostName2);
+            header.SetHostPort(hostName);
+            request.StartRequest(header);
             request.FinishRequest();
             request.GetResponseStream();
         }
