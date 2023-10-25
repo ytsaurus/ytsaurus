c8c84fe35e4eced4972023e48129d979cdcdafb2
sabdenovch <sabdenovch@ytsaurus.tech>
2023-10-25 14:49:38 +0300

    YT-20307: Move SecondaryIndex reign to after 23.2
    Since Ada and Socrates masters haven't been updated since 16th of october at least and problem started on the 18th of october (https://a.yandex-team.ru/review/4669694/details), rewriting history of reigns will most likely work, because no cluster saw bad history.
    
    Note: mutually exclusive with https://a.yandex-team.ru/review/4743995/details

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -171,9 +171,10 @@ DEFINE_ENUM(EMasterReign,
     ((ReplicateAlienClusterRegistry)                                (2436))  // ponasenko-rs
     ((ChunkMergerModeUnderTransaction)                              (2437))  // cherepashka
     ((FixSystemTransactionReplication)                              (2438))  // h0pless
-    ((SecondaryIndex)                                               (2439))  // sabdenovch
-    ((QueueAgentStageForChaos)                                      (2440))  // nadya73
-    ((FixMergerStatistics)                                          (2441))  // aleksandra-zh
+    ((QueueAgentStageForChaos)                                      (2439))  // nadya73
+    ((FixMergerStatistics)                                          (2440))  // aleksandra-zh
+    // 24.1 starts here.
+    ((SecondaryIndex)                                               (2500))  // sabdenovch
 );
 
 ////////////////////////////////////////////////////////////////////////////////
