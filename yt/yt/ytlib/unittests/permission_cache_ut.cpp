af28804e381d69b0cdde1f13f2b890449b71d2e0
dakovalkov <dakovalkov@ytsaurus.tech>
2023-11-03 10:18:00 +0300

    Cherry pick commit rXXXXXX, CHYT-1036: Fix defaults for permission cache config

---
+++ yt/yt/ytlib/unittests/permission_cache_ut.cpp
@@ -0,0 +1,46 @@
+#include <gtest/gtest.h>
+
+#include <yt/yt/ytlib/security_client/config.h>
+
+#include <yt/yt/client/api/client.h>
+
+#include <yt/yt/core/ytree/convert.h>
+
+#include <library/cpp/yt/yson_string/string.h>
+
+namespace NYT::NSecurityClient {
+namespace {
+
+using namespace NApi;
+using namespace NYson;
+using namespace NYTree;
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(PermissionCacheConfig, Simple)
+{
+    auto config = New<TPermissionCacheConfig>();
+    EXPECT_EQ(config->MasterReadOptions->ReadFrom, EMasterChannelKind::Cache);
+
+    auto ysonConfig = TYsonString(TYsonStringBuf("{}"));
+    config = ConvertTo<TPermissionCacheConfigPtr>(ysonConfig);
+    EXPECT_EQ(config->MasterReadOptions->ReadFrom, EMasterChannelKind::Cache);
+
+    ysonConfig = TYsonString(TYsonStringBuf("{master_read_options={}}"));
+    config = ConvertTo<TPermissionCacheConfigPtr>(ysonConfig);
+    EXPECT_EQ(config->MasterReadOptions->ReadFrom, EMasterChannelKind::Cache);
+
+    ysonConfig = TYsonString(TYsonStringBuf("{master_read_options={read_from=follower}}"));
+    config = ConvertTo<TPermissionCacheConfigPtr>(ysonConfig);
+    EXPECT_EQ(config->MasterReadOptions->ReadFrom, EMasterChannelKind::Follower);
+
+    // COMPAT(dakovalkov)
+    ysonConfig = TYsonString(TYsonStringBuf("{master_read_options={read_from=follower};read_from=leader}"));
+    config = ConvertTo<TPermissionCacheConfigPtr>(ysonConfig);
+    EXPECT_EQ(config->MasterReadOptions->ReadFrom, EMasterChannelKind::Leader);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT::NSecurityClient
