4f6f77f5d42a3b2b65fec03fdfff9f629c65d7f5
alex-shishkin <alex-shishkin@ytsaurus.tech>
2023-11-20 13:14:36 +0300

    SPYT-530: Add alternative CLI path

--- yt/spark/spark-over-yt/data-source/src/main/bin/spyt_cli.py
+++ yt/spark/spark-over-yt/data-source/src/main/bin/spyt_cli.py
@@ -2,8 +2,14 @@
 import argparse
 import importlib.machinery
 import importlib.util
+import inspect
+import logging
 import os
+import subprocess
 import sys
+
+
+logger = logging.getLogger(__name__)
 
 
 def import_path(path):
@@ -18,8 +24,19 @@ def import_path(path):
 
 
 def run(command, args):
-    module = import_path("spark-{}-yt".format(command))
-    module.main(args)
+    required_module = "spark-{}-yt".format(command)
+    module = None
+    try:
+        module = import_path(required_module)
+    except OSError as e:
+        logger.info(f"Cannot import required module: {e}")
+    if module is not None:
+        module.main(args)
+    else:
+        logger.info("Fallback to subprocess run")
+        script_directory = os.path.dirname(os.path.abspath(inspect.getfile(inspect.currentframe())))
+        script = os.path.join(script_directory, required_module)
+        subprocess.run([script] + args)
 
 
 def main():
