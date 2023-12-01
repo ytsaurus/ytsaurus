7690b94716e93a69e21136c72caae47e9fbf3e72
snermolaev <snermolaev@yandex-team.com>
2023-12-01 17:00:42 +0300

    Revert commit rXXXXXX, External build system generator release 64

--- yt/java/ytsaurus-client/build.gradle.kts
+++ yt/java/ytsaurus-client/build.gradle.kts
@@ -1,6 +1,11 @@
 plugins {
     `java-library`
+    `maven-publish`
+    `signing`
 }
+
+group = "tech.ytsaurus"
+version = project.properties["version"]
 
 repositories {
     mavenCentral()
@@ -27,6 +32,22 @@ dependencies {
     api("io.netty:netty-transport:4.1.42.Final")
     api("io.netty:netty-transport-native-epoll:4.1.42.Final:linux-x86_64")
     api("org.lz4:lz4-java:1.6.0")
+    testImplementation("javax.persistence:persistence-api:1.0")
+    testImplementation(project(":yt:java:ytsaurus-testlib"))
+    testImplementation("com.google.protobuf:protobuf-java:3.22.5")
+    testImplementation("junit:junit:4.13")
+    testImplementation("org.apache.logging.log4j:log4j-core:2.13.1")
+    testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.1")
+    testImplementation("javax.persistence:persistence-api:1.0")
+    testImplementation(project(":yt:java:ytsaurus-testlib"))
+    testImplementation("com.google.protobuf:protobuf-java:3.22.5")
+    testImplementation("com.fasterxml.jackson.core:jackson-core:2.11.3")
+    testImplementation("com.fasterxml.jackson.core:jackson-databind:2.11.3")
+    testImplementation("junit:junit:4.13")
+    testImplementation("org.apache.logging.log4j:log4j-core:2.13.1")
+    testImplementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.1")
+    testImplementation("org.hamcrest:hamcrest:2.2")
+    testImplementation("org.testcontainers:testcontainers:1.17.0")
 }
 
 tasks.test {
@@ -57,3 +78,89 @@ tasks {
     }.mustRunAfter("test")
 }
 
+version = project.properties["version"]
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "ytsaurus-client"
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("YTsaurus Java SDK")
+                description.set("YTsaurus client")
+                url.set("https://github.com/ytsaurus/ytsaurus")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("nadya73")
+                        name.set("Nadezhda Savchenko")
+                        email.set("nadya73@ytsaurus.tech")
+                        organization.set("YTsaurus")
+                        organizationUrl.set("https://ytsaurus.tech")
+                    }
+                    developer {
+                        id.set("ermolovd")
+                        name.set("Dmitry Ermolov")
+                        email.set("ermolovd@ytsaurus.tech")
+                        organization.set("YTsaurus")
+                        organizationUrl.set("https://ytsaurus.tech")
+
+                    }
+                    developer {
+                        id.set("tinarsky")
+                        name.set("Aleksei Tinarskii")
+                        email.set("tinarsky@ytsaurus.tech")
+                        organization.set("YTsaurus")
+                        organizationUrl.set("https://ytsaurus.tech")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/ytsaurus/ytsaurus.git")
+                    developerConnection.set("scm:git:ssh://github.com/ytsaurus/ytsaurus.git")
+                    url.set("https://github.com/ytsaurus/ytsaurus")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials {
+                username = project.properties["ossrhUsername"].toString()
+                password = project.properties["ossrhPassword"].toString()
+            }
+        }
+    }
+
+}
+
+signing {
+    setRequired({
+        !version.toString().endsWith("SNAPSHOT")
+    })
+
+    val signingKey: String? by project
+    val signingPassword: String? by project
+
+    useInMemoryPgpKeys(signingKey, signingPassword)
+
+    sign(publishing.publications["mavenJava"])
+}
--- yt/yt_proto/yt/formats/build.gradle.kts
+++ yt/yt_proto/yt/formats/build.gradle.kts
@@ -5,8 +5,12 @@ val buildProtoDir = File("${buildDir}", "__proto__")
 plugins {
     id("java-library")
     id("com.google.protobuf") version "0.8.19"
+    `maven-publish`
+    `signing`
 }
 
+group = "tech.ytsaurus"
+version = project.properties["version"]
 
 repositories {
     mavenCentral()
@@ -26,6 +30,7 @@ dependencies {
 
 val prepareProto = tasks.register<Copy>("prepareProto") {
     from(rootDir) {
+        include("yt/yt_proto/yt/formats/extension.proto")
         include("yt/yt_proto/yt/formats/yamr.proto")
     }
     into(buildProtoDir)
@@ -33,4 +38,90 @@ val prepareProto = tasks.register<Copy>("prepareProto") {
 
 afterEvaluate {
     tasks.getByName("extractProto").dependsOn(prepareProto)
+}
+version = project.properties["version"]
+
+publishing {
+    publications {
+        create<MavenPublication>("mavenJava") {
+            artifactId = "ytsaurus-proto-formats"
+            from(components["java"])
+
+            versionMapping {
+                usage("java-api") {
+                    fromResolutionOf("runtimeClasspath")
+                }
+                usage("java-runtime") {
+                    fromResolutionResult()
+                }
+            }
+            pom {
+                name.set("YTsaurus proto formats library")
+                description.set("YTsaurus proto formats library")
+                url.set("https://github.com/ytsaurus/ytsaurus")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("nadya73")
+                        name.set("Nadezhda Savchenko")
+                        email.set("nadya73@ytsaurus.tech")
+                        organization.set("YTsaurus")
+                        organizationUrl.set("https://ytsaurus.tech")
+                    }
+                    developer {
+                        id.set("ermolovd")
+                        name.set("Dmitry Ermolov")
+                        email.set("ermolovd@ytsaurus.tech")
+                        organization.set("YTsaurus")
+                        organizationUrl.set("https://ytsaurus.tech")
+
+                    }
+                    developer {
+                        id.set("tinarsky")
+                        name.set("Aleksei Tinarskii")
+                        email.set("tinarsky@ytsaurus.tech")
+                        organization.set("YTsaurus")
+                        organizationUrl.set("https://ytsaurus.tech")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/ytsaurus/ytsaurus.git")
+                    developerConnection.set("scm:git:ssh://github.com/ytsaurus/ytsaurus.git")
+                    url.set("https://github.com/ytsaurus/ytsaurus")
+                }
+            }
+        }
+    }
+
+    repositories {
+        maven {
+            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
+
+            credentials {
+                username = project.properties["ossrhUsername"].toString()
+                password = project.properties["ossrhPassword"].toString()
+            }
+        }
+    }
+
+}
+
+signing {
+    setRequired({
+        !version.toString().endsWith("SNAPSHOT")
+    })
+
+    val signingKey: String? by project
+    val signingPassword: String? by project
+
+    useInMemoryPgpKeys(signingKey, signingPassword)
+
+    sign(publishing.publications["mavenJava"])
 }
