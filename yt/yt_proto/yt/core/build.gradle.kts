7a16919d84c748ec00d27c73d9d72b5083cfa4d7
robot-ya-builder <robot-ya-builder@yandex-team.com>
2023-12-05 11:10:55 +0300

    External build system generator release 65
    Update tools: yexport, os-yexport

--- yt/yt_proto/yt/core/build.gradle.kts
+++ yt/yt_proto/yt/core/build.gradle.kts
@@ -37,18 +37,6 @@ protobuf {
 
 val prepareProto = tasks.register<Copy>("prepareProto") {
     from(rootDir) {
-        include("yt/yt_proto/yt/core/crypto/proto/crypto.proto")
-        include("yt/yt_proto/yt/core/misc/proto/bloom_filter.proto")
-        include("yt/yt_proto/yt/core/misc/proto/error.proto")
-        include("yt/yt_proto/yt/core/misc/proto/guid.proto")
-        include("yt/yt_proto/yt/core/misc/proto/protobuf_helpers.proto")
-        include("yt/yt_proto/yt/core/tracing/proto/span.proto")
-        include("yt/yt_proto/yt/core/tracing/proto/tracing_ext.proto")
-        include("yt/yt_proto/yt/core/bus/proto/bus.proto")
-        include("yt/yt_proto/yt/core/rpc/proto/rpc.proto")
-        include("yt/yt_proto/yt/core/yson/proto/protobuf_interop.proto")
-        include("yt/yt_proto/yt/core/ytree/proto/attributes.proto")
-        include("yt/yt_proto/yt/core/ytree/proto/request_complexity_limits.proto")
         include("yt/yt_proto/yt/core/ytree/proto/ypath.proto")
     }
     into(buildProtoDir)
--- yt/yt_proto/yt/formats/build.gradle.kts
+++ yt/yt_proto/yt/formats/build.gradle.kts
@@ -37,7 +37,6 @@ protobuf {
 
 val prepareProto = tasks.register<Copy>("prepareProto") {
     from(rootDir) {
-        include("yt/yt_proto/yt/formats/extension.proto")
         include("yt/yt_proto/yt/formats/yamr.proto")
     }
     into(buildProtoDir)
