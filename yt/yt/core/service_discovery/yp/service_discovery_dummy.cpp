fc1dbfd14f64c802d52bc1170a285f51355ce9e9
Grigory Reznikov <gritukan@nebius.com>
2023-12-11 13:40:24 +0000

    wip

--- yt/yt/core/service_discovery/yp/service_discovery_dummy.cpp
+++ yt/yt/core/service_discovery/yp/service_discovery_dummy.cpp
@@ -1,11 +1,17 @@
 #include "service_discovery.h"
 
+#include "config.h"
+
 namespace NYT::NServiceDiscovery::NYP {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-IServiceDiscoveryPtr CreateServiceDiscovery(TServiceDiscoveryConfigPtr /*config*/)
+IServiceDiscoveryPtr CreateServiceDiscovery(TServiceDiscoveryConfigPtr config)
 {
+    // In opensource, we do not have any service discovery implementation,
+    // so it must be disabled.
+    YT_VERIFY(!config->Enable);
+
     return nullptr;
 }
 
