11032bc82d45ea305df7f07d49ed98b91eef930c
babenko <babenko@ytsaurus.tech>
2023-12-21 15:35:37 +0300

    Streamline Sequoia tables creation and init

--- yt/yt/tests/library/yt_env_setup.py
+++ yt/yt/tests/library/yt_env_setup.py
@@ -743,12 +743,12 @@ class YTEnvSetup(object):
                 table.get_path(),
                 attributes={
                     "dynamic": True,
+                    "enable_shared_write_locks": True,
                     "schema": table.schema,
                     "tablet_cell_bundle": "sequoia",
                     "account": "sequoia",
                 },
                 driver=ground_driver)
-            yt_commands.set("{}/@enable_shared_write_locks".format(table.get_path()), True, driver=ground_driver)
             yt_commands.mount_table(table.get_path(), driver=ground_driver)
 
         for table in yt_sequoia_helpers.SEQUOIA_TABLES:
