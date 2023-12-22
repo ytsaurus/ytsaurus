00cf19ce324dae9c63c2e455ef376bb7b1d86df3
robot-ya-builder <robot-ya-builder@yandex-team.com>
2023-12-22 09:56:11 +0300

    Automatic release build for ymake, os_ymake
    From hash: [d1b331ce7bcc9b0c951a4e3380c5c647588b3b40](https://a.yandex-team.ru/arcadia/commit/d1b331ce7bcc9b0c951a4e3380c5c647588b3b40)
    From revision: [13131219](https://a.yandex-team.ru/arcadia/commit/rXXXXXX)
    [CI flow](https://a.yandex-team.ru/projects/ya_make/ci/releases/flow?dir=devtools%2Fya&id=release-ymake&version=144)
    Flow triggered by user: [svidyuk](https://staff.yandex-team.ru/svidyuk)
    
    Update tools: ymake, os_ymake

--- yt/java/ytsaurus-client-examples/map-reduce-entity-example/build.gradle.kts
+++ yt/java/ytsaurus-client-examples/map-reduce-entity-example/build.gradle.kts
@@ -19,6 +19,7 @@ dependencies {
     implementation(project(":yt:java:annotations"))
     implementation(project(":yt:java:ytsaurus-client"))
     implementation("javax.persistence:persistence-api:1.0")
+    implementation("com.google.protobuf:protobuf-java:3.22.5")
     implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.1")
 }
 
