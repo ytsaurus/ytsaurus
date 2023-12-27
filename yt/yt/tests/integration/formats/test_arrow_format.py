e2de12d8a5e5cb4d632c8ca4918b87a35aaf2bf1
nadya02 <nadya02@ytsaurus.tech>
2023-12-27 11:31:48 +0300

    YT-20700: Add support for multiple tables in arrow writer

--- yt/yt/tests/integration/formats/test_arrow_format.py
+++ yt/yt/tests/integration/formats/test_arrow_format.py
@@ -11,7 +11,6 @@ import yt.yson as yson
 import pyarrow as pa
 import pandas as pd
 
-
 HELLO_WORLD = b"\xd0\x9f\xd1\x80\xd0\xb8\xd0\xb2\xd0\xb5\xd1\x82, \xd0\xbc\xd0\xb8\xd1\x80!"
 GOODBYE_WORLD = b"\xd0\x9f\xd0\xbe\xd0\xba\xd0\xb0, \xd0\xbc\xd0\xb8\xd1\x80!"
 
@@ -397,6 +396,195 @@ class TestMapArrowFormat(YTEnvSetup):
         )
 
         assert read_table("//tmp/t_in{int,uint,double}") == read_table("//tmp/t_out")
+
+    @authors("nadya02")
+    def test_multi_table(self):
+        schema = [
+            {"name": "string", "type_v3": "string"},
+        ]
+
+        schema2 = [
+            {"name": "int", "type_v3": "int64"},
+        ]
+
+        create(
+            "table",
+            "//tmp/t1",
+            attributes={
+                "schema": schema,
+            },
+            force=True,
+        )
+
+        create(
+            "table",
+            "//tmp/t2",
+            attributes={
+                "schema": schema2,
+            },
+            force=True,
+        )
+
+        write_table(
+            "//tmp/t1",
+            [
+                {
+                    "string": "foobar"
+                },
+                {
+                    "string": "lol"
+                },
+            ],
+        )
+
+        write_table(
+            "//tmp/t2",
+            [
+                {
+                    "int": 53
+                },
+                {
+                    "int": 42
+                },
+                {
+                    "int": 179
+                }
+            ]
+        )
+
+        create(
+            "table",
+            "//tmp/t_out",
+            force=True,
+        )
+
+        format = yson.YsonString(b"arrow")
+
+        op = map(
+            in_=["//tmp/t1", "//tmp/t2"],
+            out=["//tmp/t_out"],
+            command="cat 1>&2",
+            spec={"mapper": {"input_format": format}},
+        )
+
+        assert read_table("//tmp/t_out") == []
+
+        job_ids = op.list_jobs()
+        stderr_bytes = op.read_stderr(job_ids[0])
+        buffer = pa.py_buffer(stderr_bytes)
+
+        reader = pa.BufferReader(buffer)
+        table1 = pa.ipc.open_stream(reader).read_all()
+
+        reader.read(4)
+        table2 = pa.ipc.open_stream(reader).read_all()
+        if table1.column_names[0] == "int":
+            table1, table2 = table2, table1
+
+        column_names = table1.column_names
+        assert column_names[0] == "string"
+        assert table1[column_names[0]].to_pylist() == [b'foobar', b'lol']
+
+        column_names = table2.column_names
+        assert column_names[0] == "int"
+        assert table2[column_names[0]].to_pylist() == [53, 42, 179]
+
+    @authors("nadya02")
+    def test_multi_table_with_same_column(self):
+        schema = [
+            {"name": "column", "type_v3": "string"},
+            {"name": "int", "type_v3": "int64"},
+        ]
+
+        schema2 = [
+            {"name": "column", "type_v3": "int64"},
+        ]
+
+        create(
+            "table",
+            "//tmp/t1",
+            attributes={
+                "schema": schema,
+            },
+            force=True,
+        )
+
+        create(
+            "table",
+            "//tmp/t2",
+            attributes={
+                "schema": schema2,
+            },
+            force=True,
+        )
+
+        write_table(
+            "//tmp/t1",
+            [
+                {
+                    "column": "foobar",
+                    "int": 53,
+                },
+                {
+                    "column": "lol",
+                    "int": 42,
+                },
+            ],
+        )
+
+        write_table(
+            "//tmp/t2",
+            [
+                {
+                    "column": 53
+                },
+                {
+                    "column": 42
+                },
+                {
+                    "column": 179
+                }
+            ]
+        )
+
+        create(
+            "table",
+            "//tmp/t_out",
+            force=True,
+        )
+
+        format = yson.YsonString(b"arrow")
+
+        op = map(
+            in_=["//tmp/t1", "//tmp/t2"],
+            out=["//tmp/t_out"],
+            command="cat 1>&2",
+            spec={"mapper": {"input_format": format}},
+        )
+
+        assert read_table("//tmp/t_out") == []
+
+        job_ids = op.list_jobs()
+        stderr_bytes = op.read_stderr(job_ids[0])
+        buffer = pa.py_buffer(stderr_bytes)
+
+        reader = pa.BufferReader(buffer)
+        table1 = pa.ipc.open_stream(reader).read_all()
+
+        reader.read(4)
+        table2 = pa.ipc.open_stream(reader).read_all()
+        if len(table1.column_names) == 1:
+            table1, table2 = table2, table1
+
+        column_names = table1.column_names
+        assert column_names[0] == "column"
+        assert column_names[1] == "int"
+        assert table1[column_names[0]].to_pylist() == [b'foobar', b'lol']
+        assert table1[column_names[1]].to_pylist() == [53, 42]
+
+        column_names = table2.column_names
+        assert column_names[0] == "column"
+        assert table2[column_names[0]].to_pylist() == [53, 42, 179]
 
 
 @authors("nadya73")
