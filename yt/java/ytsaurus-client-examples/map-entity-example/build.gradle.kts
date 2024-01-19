f30023727714fa6501f9ff7871d6858735cbebb1
robot-ya-builder <robot-ya-builder@yandex-team.com>
2024-01-19 14:42:34 +0300

    External build system generator release 75
    Update tools: yexport, os-yexport

--- yt/java/ytsaurus-client-examples/map-entity-example/build.gradle.kts
+++ yt/java/ytsaurus-client-examples/map-entity-example/build.gradle.kts
@@ -20,7 +20,10 @@ dependencies {
     implementation(project(":yt:java:ytsaurus-client"))
     implementation("javax.persistence:persistence-api:1.0")
     implementation("com.google.protobuf:protobuf-java:3.22.5")
-    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.1")
+
+    implementation("org.apache.logging.log4j:log4j-slf4j-impl:2.13.1") {
+        exclude(group = "org.slf4j", module = "slf4j-api")
+    }
 }
 
 tasks.test {
