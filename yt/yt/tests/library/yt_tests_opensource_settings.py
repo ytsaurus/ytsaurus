19b957833eaa00308e33bf55972e548af66b70d4
Konstantin Khlebnikov <koct9i@gmail.com>
2024-01-23 11:04:57 +0100

    Fix tests for unsupported erasure codecs

--- yt/yt/tests/library/yt_tests_opensource_settings.py
+++ yt/yt/tests/library/yt_tests_opensource_settings.py
@@ -1,1 +1,4 @@
 has_tvm_service_support = False
+
+# See yt/yt/library/erasure/impl/codec_opensource.cpp
+supported_erasure_codes = ["isa_lrc_12_2_2", "reed_solomon_3_3", "isa_reed_solomon_6_3"]
---
+++ yt/yt/tests/library/yt_tests_settings.py
@@ -0,0 +1,3 @@
+has_tvm_service_support = True
+
+supported_erasure_codes = ["isa_lrc_12_2_2", "lrc_12_2_2", "reed_solomon_6_3", "reed_solomon_3_3", "isa_reed_solomon_6_3"]
