421c98b581abc50f9a34e15d551c9700ebf80f40
Konstantin Khlebnikov <koct9i@gmail.com>
2024-01-29 15:41:27 +0100

    Add option for setting hard memory limit for slot containers
    This adds controller agent operation options:
    "set_slot_container_memory_limit" and "slot_container_memory_overhead".

--- yt/yt/server/lib/job_proxy/config.cpp
+++ yt/yt/server/lib/job_proxy/config.cpp
@@ -225,6 +225,9 @@ void TJobProxyInternalConfig::Register(TRegistrar registrar)
     registrar.Parameter("container_cpu_limit", &TThis::ContainerCpuLimit)
         .Default();
 
+    registrar.Parameter("slot_container_memory_limit", &TThis::SlotContainerMemoryLimit)
+        .Default();
+
     registrar.Preprocessor([] (TThis* config) {
         config->SolomonExporter->EnableSelfProfiling = false;
         config->SolomonExporter->WindowSize = 1;
