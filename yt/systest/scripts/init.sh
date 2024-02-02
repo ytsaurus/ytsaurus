d9e8888ea963792fa13a52ccf6ffe247b2ae2933
orlovorlov <orlovorlov@ytsaurus.tech>
2024-02-02 20:31:52 +0300

    YT-20317 Increase disk limit for account sys

--- yt/systest/scripts/init.sh
+++ yt/systest/scripts/init.sh
@@ -5,9 +5,11 @@ set -e
 # Matches exec node configuration in cluster.yaml
 cluster_cpu=320
 cluster_ram=1028966789939
+cluster_disk=98956046499840
 
 pool_name="systest"
 
 yt list /
 yt create scheduler_pool --attributes="{pool_tree=default;name=${pool_name}}"
 yt set //sys/pool_trees/default/${pool_name}/@strong_guarantee_resources "{cpu=${cluster_cpu};memory=${cluster_ram}}"
+yt set //sys/accounts/sys/@resource_limits/disk_space_per_medium/default $cluster_disk
