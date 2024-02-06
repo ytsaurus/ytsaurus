9d69c16bcb29b0122706032e7d23c2a7daea3b9b
Kirill Sibirev <sibirev@nebius.com>
2024-02-06 17:36:57 +0100

    Revert "Fix inifinite hanging in integration tests"
    This reverts commit 120848c1f5c548838d7080926eaba3504dabe12f.

--- yt/go/yt/integration/admin_client_test.go
+++ yt/go/yt/integration/admin_client_test.go
@@ -8,14 +8,9 @@ import (
 	"github.com/stretchr/testify/require"
 
 	"go.ytsaurus.tech/library/go/ptr"
-
 	"go.ytsaurus.tech/yt/go/guid"
 	"go.ytsaurus.tech/yt/go/ypath"
 	"go.ytsaurus.tech/yt/go/yt"
-)
-
-const (
-	testOpsTimeout = time.Second * 10
 )
 
 func TestAdminClient(t *testing.T) {
@@ -92,7 +87,7 @@ func (s *Suite) TestIssueRevokeToken(t *testing.T, yc yt.Client) {
 func (s *Suite) TestAddRemoveMaintenance(t *testing.T, yc yt.Client) {
 	t.Parallel()
 
-	ctx, cancel := context.WithTimeout(s.Ctx, testOpsTimeout)
+	ctx, cancel := context.WithTimeout(s.Ctx, time.Second*30)
 	defer cancel()
 
 	// Get node
