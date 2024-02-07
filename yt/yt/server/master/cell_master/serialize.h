629e4c31c39fff9f5a445fc4e8778c05895e5509
babenko <babenko@ytsaurus.tech>
2024-02-07 13:51:36 +0300

    Fix response hash in PRK

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -228,6 +228,7 @@ DEFINE_ENUM(EMasterReign,
     ((SaneTxActionAbort)                                            (2526))  // kvk1920
     ((ValueDictionaryCompression)                                   (2527))  // akozhikhov
     ((SaneTxActionAbortFix)                                         (2528))  // kvk1920
+    ((FixResponseHash)                                              (2529))  // babenko
     // 24.2 starts here.
 );
 
