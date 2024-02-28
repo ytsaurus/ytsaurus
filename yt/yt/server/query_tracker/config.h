84ef6567021bb7397d32b0e844f914574bee9bde
mpereskokova <mpereskokova@ytsaurus.tech>
2024-02-28 14:52:43 +0300

    [23.2] Cherry pick commit rXXXXXX, Cancellation for YQL queries 56d94176f08a6d70d62e5d2e3c957b840d325f5f

--- yt/yt/server/query_tracker/config.h
+++ yt/yt/server/query_tracker/config.h
@@ -103,6 +103,7 @@ class TQueryTrackerDynamicConfig
 public:
     TDuration ActiveQueryAcquisitionPeriod;
     TDuration ActiveQueryLeaseTimeout;
+    TDuration QueryAcquisitionGracePeriod;
     TDuration ActiveQueryPingPeriod;
     TDuration QueryFinishBackoff;
     TDuration HealthCheckPeriod;
