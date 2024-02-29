f8e5fa13871e3a47379e67c5b946c0fa707da196
galtsev <galtsev@ytsaurus.tech>
2024-02-29 14:35:35 +0300

    Style: fix the argument name a2081cf66ae10e9aff9f5fc7c9e136f82b35420e

--- yt/python/yt/wrapper/spec_builders.py
+++ yt/python/yt/wrapper/spec_builders.py
@@ -404,8 +404,8 @@ class UserJobSpecBuilder(object):
         return _set_spec_value(self, "layer_paths", paths)
 
     @spec_option("Docker image for root fs layer")
-    def docker_image(self, paths):
-        return _set_spec_value(self, "docker_image", paths)
+    def docker_image(self, docker_image):
+        return _set_spec_value(self, "docker_image", docker_image)
 
     @spec_option("The format of tabular data")
     def format(self, format):
