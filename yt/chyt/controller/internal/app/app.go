82b3e2b62c3610acffdc12e95e73921eccab78db
gudqeit <gudqeit@ytsaurus.tech>
2024-02-29 14:50:05 +0300

    [chyt] Non-blocking check that controller is not stopping abf6eb98631becc1010b2bb2251ffcd675085309

--- yt/chyt/controller/internal/app/app.go
+++ yt/chyt/controller/internal/app/app.go
@@ -172,6 +172,8 @@ func (app *App) Run(stopCh <-chan struct{}) {
 	if app.HTTPMonitoringServer != nil {
 		go app.HTTPMonitoringServer.Run()
 	}
+
+L:
 	for {
 		app.l.Debug("trying to acquire lock")
 		lost, err := app.acquireLock()
@@ -204,8 +206,10 @@ func (app *App) Run(stopCh <-chan struct{}) {
 			}
 		}
 
-		if _, ok := <-stopCh; !ok {
-			break
+		select {
+		case <-stopCh:
+			break L
+		default:
 		}
 	}
 
