d5b58092fe37ba549ffb87b8ce721c1fc3569798
sabdenovch <sabdenovch@ytsaurus.tech>
2024-03-04 19:39:03 +0300

    Small optimization (prefetch index table info) 340315e88ed2281d36c3513f36537b30901b9e17

--- yt/yt/ytlib/api/native/client_dynamic_tables_impl.cpp
+++ yt/yt/ytlib/api/native/client_dynamic_tables_impl.cpp
@@ -399,6 +399,9 @@ std::vector<TTableMountInfoPtr> GetQueryTableInfos(
     const TStickyTableMountInfoCachePtr& cache)
 {
     std::vector<TYPath> paths{query->Table.Path};
+    if (query->WithIndex) {
+        paths.push_back(query->WithIndex->Path);
+    }
     for (const auto& join : query->Joins) {
         if (const auto* tableJoin = std::get_if<NAst::TJoin>(&join)) {
             paths.push_back(tableJoin->Table.Path);
@@ -1456,8 +1459,6 @@ TSelectRowsResult TClient::DoSelectRowsOnce(
 
     auto cache = New<TStickyTableMountInfoCache>(Connection_->GetTableMountCache());
     GetQueryTableInfos(astQuery, cache);
-
-    TransformWithIndexStatement(&parsedQuery->AstHead, cache);
 
     TransformWithIndexStatement(&parsedQuery->AstHead, cache);
 
