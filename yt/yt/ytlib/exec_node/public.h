d32969bdd765686314d4a291d221db5716215fc5
pogorelov <pogorelov@ytsaurus.tech>
2024-03-07 20:54:51 +0300

    YT-21236: Process correctly NoSuchJob error in api c46f93fce184350e00f22b7ec941e3ebe84d1a94

--- yt/yt/ytlib/exec_node/public.h
+++ yt/yt/ytlib/exec_node/public.h
@@ -1,10 +1,48 @@
 #pragma once
 
+#include <yt/yt/core/misc/error_code.h>
+
 #include <library/cpp/yt/misc/enum.h>
 
 namespace NYT::NExecNode {
 
 ////////////////////////////////////////////////////////////////////////////////
+
+YT_DEFINE_ERROR_ENUM(
+    ((ConfigCreationFailed)                  (1100))
+    ((AbortByScheduler)                      (1101))
+    ((ResourceOverdraft)                     (1102))
+    ((WaitingJobTimeout)                     (1103))
+    ((SlotNotFound)                          (1104))
+    ((JobEnvironmentDisabled)                (1105))
+    ((JobProxyConnectionFailed)              (1106))
+    ((ArtifactCopyingFailed)                 (1107))
+    ((NodeDirectoryPreparationFailed)        (1108))
+    ((SlotLocationDisabled)                  (1109))
+    ((QuotaSettingFailed)                    (1110))
+    ((RootVolumePreparationFailed)           (1111))
+    ((NotEnoughDiskSpace)                    (1112))
+    ((ArtifactDownloadFailed)                (1113))
+    ((JobProxyPreparationTimeout)            (1114))
+    ((JobPreparationTimeout)                 (1115))
+    ((FatalJobPreparationTimeout)            (1116))
+    ((JobProxyFailed)                        (1120))
+    ((SetupCommandFailed)                    (1121))
+    ((GpuLayerNotFetched)                    (1122))
+    ((GpuJobWithoutLayers)                   (1123))
+    ((TmpfsOverflow)                         (1124))
+    ((GpuCheckCommandFailed)                 (1125))
+    ((GpuCheckCommandIncorrect)              (1126))
+    ((JobProxyUnavailable)                   (1127))
+    ((NodeResourceOvercommit)                (1128))
+    ((LayerUnpackingFailed)                  (1129))
+    ((TmpfsLayerImportFailed)                (1130))
+    ((SchedulerJobsDisabled)                 (1131))
+    ((DockerImagePullingFailed)              (1132))
+    ((InvalidImage)                          (1133))
+    ((AbortByControllerAgent)                (1134))
+    ((NoSuchJob)                             (1135))
+);
 
 DEFINE_ENUM(EJobPhase,
     ((Missing)                      (100))
