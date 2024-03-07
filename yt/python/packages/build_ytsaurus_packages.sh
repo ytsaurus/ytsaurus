d600dc7bcf7b663cff8197536b35e8ccf32ace96
ignat <ignat@ytsaurus.tech>
2024-03-07 06:22:08 +0300

    Fix build_ytsaurus_packages.sh to properly process native-driver package 4db7b95ddb7aff532ebf09d62afab0f356b8e865

--- yt/python/packages/build_ytsaurus_packages.sh
+++ yt/python/packages/build_ytsaurus_packages.sh
@@ -90,7 +90,9 @@ for package in ${packages[@]}; do
     package_undescored=$(echo -e $package | sed -e s/-/_/g)
     dist_dir="${package_undescored}_dist"
 
-    if [[ ${package} == "ytsaurus-native-driver" ]] || [[ ${package} == "ytsaurus-yson" ]]; then
+    if [[ ${package} == "ytsaurus-native-driver" ]]; then
+        python3 setup.py bdist_wheel --py-limited-api cp34 --dist-dir ${dist_dir}
+    elif [[ ${package} == "ytsaurus-yson" ]]; then
         python3 setup.py bdist_wheel --py-limited-api cp34 --dist-dir ${dist_dir}
         for wheel in ${dist_dir}/${package_undescored}*.whl; do
             auditwheel repair "$wheel" -w "${dist_dir}" --plat manylinux2014_x86_64
