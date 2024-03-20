b84269069b68f402cbf802e5754d1d0675541635
Dmitrii Gadeev <kruft@nebius.com>
2024-03-20 05:33:40 +0000

    configurable primary_medium for odin_checks table

--- yt/odin/bin/yt_odin_init_db_cluster/__main__.py
+++ yt/odin/bin/yt_odin_init_db_cluster/__main__.py
@@ -12,10 +12,11 @@ def main():
     parser.add_argument("--table", required=True)
     parser.add_argument("--proxy", required=True)
     parser.add_argument("--tablet-cell-bundle", default="odin")
+    parser.add_argument("--primary-medium", default="default")
     args = parser.parse_args()
 
     client = yt.YtClient(proxy=args.proxy)
-    init_yt_table(args.table, client, bundle=args.tablet_cell_bundle)
+    init_yt_table(args.table, client, bundle=args.tablet_cell_bundle, primary_medium=args.primary_medium)
 
 
 if __name__ == "__main__":
