a2bbd29279fa4efa9dc6905f2c9805a7316fa90d
grigminakov <grigminakov@yandex-team.com>
2024-03-20 19:40:22 +0300

    YTORM-1002: Effective fetchRootObject implementation
    Добавляет опцию `fetchRootOptimizationLevel`, которая принимает следюущие значения:
     - `none`: режим "как раньше"
     - `schema_only`: оптимизация на уровне схемы
     - `squash_expression_builders`: на уровне схемы и внутри простых (у которых есть ExpressionBuilder) атрибутов
    0a88894f1b3d2bbb28c916b2b88fc0a9c011507e

--- yt/yt/orm/library/attributes/merge_attributes.h
+++ yt/yt/orm/library/attributes/merge_attributes.h
@@ -2,7 +2,11 @@
 
 #include <yt/yt/core/ypath/public.h>
 
+#include <yt/yt/core/yson/consumer.h>
+
 #include <library/cpp/yt/yson_string/string.h>
+
+#include <ranges>
 
 namespace NYT::NOrm::NAttributes {
 
@@ -15,6 +19,35 @@ struct TAttributeValue
     bool IsEtc = false;
 };
 
+////////////////////////////////////////////////////////////////////////////////
+
+void SortAndRemoveNestedPaths(std::vector<NYPath::TYPath>& paths);
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <std::ranges::range TRange, class TPathProj, class TIsEtcProj>
+void ValidateSortedPaths(const TRange& paths, TPathProj pathProj, TIsEtcProj etcProj);
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TMergeAttributesHelper
+{
+public:
+    TMergeAttributesHelper(NYson::IYsonConsumer* consumer);
+
+    // Path should be provided in lexicographical order and
+    // validated through `ValidateSortedPath()` call.
+    void ToNextPath(NYPath::TYPathBuf path, bool isEtc);
+
+    void Finalize();
+
+private:
+    NYson::IYsonConsumer* Consumer_;
+    std::vector<TString> PathToCurrentMap_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 NYson::TYsonString MergeAttributes(
     std::vector<TAttributeValue> attributeValues,
     NYson::EYsonFormat format = NYson::EYsonFormat::Binary);
@@ -22,3 +55,7 @@ NYson::TYsonString MergeAttributes(
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace NYT::NOrm::NAttributes
+
+#define MERGE_ATTRIBUTES_INL_H_
+#include "merge_attributes-inl.h"
+#undef MERGE_ATTRIBUTES_INL_H_
