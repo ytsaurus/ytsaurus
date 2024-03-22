fe73f9fc91b9672ca26fe7e7c7430d57be7e79cf
Konstantin Khlebnikov <koct9i@gmail.com>
2024-03-22 13:48:16 +0300

    Catch and wrap only ImportError exception in spyt when cannot import yt client
    I hereby agree to the terms of the CLA available at: https://yandex.ru/legal/cla/?lang=en
    
    ---
    8bdae1b74e0e526bd89f83e1199d86c0f26ee6b0
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/259

--- yt/spark/spark-over-yt/spyt-package/src/main/python/spyt/dependency_utils.py
+++ yt/spark/spark-over-yt/spyt-package/src/main/python/spyt/dependency_utils.py
@@ -2,7 +2,7 @@ def require_yt_client():
     try:
         import yt.wrapper  # noqa: F401
         from yt.wrapper import YtClient  # noqa: F401
-    except Exception as e:
+    except ImportError as e:
         raise ImportError(
             "Please install ytsaurus-client (or yandex-yt for internal Yandex users). "
             "These libraries cannot be installed at the same time"
