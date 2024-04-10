aed99cd2f5c2c1990d896d0af737af85eade4086
Konstantin Khlebnikov <koct9i@gmail.com>
2024-04-10 15:08:35 +0300

    [RFC] cleanup memory accounting
    - Rename misnamed "Rss" into "ResidentAnon" and fix collecting from cgroup v2
      This kind of memory usually represents heap area allocated by malloc() by
      brk() or mmap(MAP_PRIVATE|MAP_ANONYMOUS).
    
      Copy-on-write part of mmap(MAP_PRIVATE|MAP_FILE) also accounted here.
    
      Usually it is called "resident anonymous memory", "RssAnon", "AnonPages",
      "nr_anon_pages", "NR_ANON_MAPPED", "private", "anon", or (wrongly) "rss".
    
    - Add common statistics PeakResidentAnon
    
    - Fetch tmpfs usage from cgroup
      Cgroup has accounting for shmem/tmpfs memory.
    
    ---
    e7b6785baebcd7fbdb23675449fb9501fd7a9590
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/426

--- yt/yt/library/containers/cgroup.h
+++ yt/yt/library/containers/cgroup.h
@@ -215,7 +215,8 @@ public:
 
     struct TStatistics
     {
-        TErrorOr<i64> Rss;
+        TErrorOr<i64> ResidentAnon;
+        TErrorOr<i64> TmpfsUsage;
         TErrorOr<i64> MappedFile;
         TErrorOr<i64> MinorPageFaults;
         TErrorOr<i64> MajorPageFaults;
