daab77bbcdaf7a4194aedd5645c78a4cae1cda8b
mpereskokova <mpereskokova@ytsaurus.tech>
2024-04-12 16:11:01 +0300

    YT-20570: Add expiration to QT orchid 0089847642d0b140187bbcfc0e638c58b9be081b

--- yt/yt/server/query_tracker/bootstrap.cpp
+++ yt/yt/server/query_tracker/bootstrap.cpp
@@ -230,6 +230,7 @@ void TBootstrap::UpdateCypressNode()
     TCypressRegistrarOptions options{
         .RootPath = Format("%v/instances/%v", Config_->Root, ToYPathLiteral(SelfAddress_)),
         .OrchidRemoteAddresses = TAddressMap{{NNodeTrackerClient::DefaultNetworkName, SelfAddress_}},
+        .ExpireSelf = true,
         .AttributesOnStart = BuildAttributeDictionaryFluently()
             .Item("annotations").Value(Config_->CypressAnnotations)
             .Finish(),
