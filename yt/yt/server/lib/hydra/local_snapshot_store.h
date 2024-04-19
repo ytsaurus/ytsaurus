1a5de628b825f945288f450a005e43e16c678e4c
babenko <babenko@ytsaurus.tech>
2024-04-19 14:09:20 +0300

    Remove legacy snapshot store and raw snapshot readers 9bc519a2c5df2801dd67b1bc1659b164a3a6dd49

--- yt/yt/server/lib/hydra/local_snapshot_store.h
+++ yt/yt/server/lib/hydra/local_snapshot_store.h
@@ -2,28 +2,13 @@
 
 #include "public.h"
 
-#include "snapshot.h"
-
 namespace NYT::NHydra {
 
 ///////////////////////////////////////////////////////////////////////////////
 
-// COMPAT(shakurov)
-struct ILegacySnapshotStore
-    : public ISnapshotStore
-{
-    virtual ISnapshotReaderPtr CreateRawReader(int snapshotId, i64 offset) = 0;
-    virtual ISnapshotWriterPtr CreateRawWriter(int snapshotId) = 0;
-};
-
-DEFINE_REFCOUNTED_TYPE(ILegacySnapshotStore)
-
-// COMPAT(shakurov): change return type to ISnapshotStorePtr after removing old Hydra.
-TFuture<ILegacySnapshotStorePtr> CreateLocalSnapshotStore(
+TFuture<ISnapshotStorePtr> CreateLocalSnapshotStore(
     TLocalSnapshotStoreConfigPtr config,
     IInvokerPtr ioInvoker);
-
-////////////////////////////////////////////////////////////////////////////////
 
 ISnapshotReaderPtr CreateUncompressedHeaderlessLocalSnapshotReader(
     TString fileName,
