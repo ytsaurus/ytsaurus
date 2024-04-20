e2a1fd352e5c92f69364413b5a5203c6a23f0b9e
Andrey Chulkov <achulkov2@nebius.com>
2024-04-21 01:31:50 +0300

    Make non-existent medium error fatal to fix #531
    No description
    
    ---
    4b8436a16b16e8e21af2228fc863e7e6c2de497a
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/542

--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -3101,6 +3101,7 @@ bool TJob::IsFatalError(const TError& error)
             !AbortJobIfAccountLimitExceeded_
         ) ||
         error.FindMatching(NSecurityClient::EErrorCode::NoSuchAccount) ||
+        error.FindMatching(NChunkClient::EErrorCode::NoSuchMedium) ||
         error.FindMatching(NNodeTrackerClient::EErrorCode::NoSuchNetwork) ||
         error.FindMatching(NTableClient::EErrorCode::InvalidDoubleValue) ||
         error.FindMatching(NTableClient::EErrorCode::IncomparableTypes) ||
