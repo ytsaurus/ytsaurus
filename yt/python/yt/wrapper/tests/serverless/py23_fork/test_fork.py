dd07d1a2d4684b1644c6bc49b7ce7caf08f69d06
ignat <ignat@ytsaurus.tech>
2024-04-20 18:07:31 +0300

    Fix test_fork for OS 29937bc72300b118a4f68485779dbaf252b0f205

--- yt/python/yt/wrapper/tests/serverless/py23_fork/test_fork.py
+++ yt/python/yt/wrapper/tests/serverless/py23_fork/test_fork.py
@@ -1,10 +1,13 @@
-import os
+from yt.testlib import authors  # noqa
 
 import yt.yson
 import yt.ypath
 import yt.wrapper
 
+import os
 
+
+@authors("denvr")
 def test_fork():
     if os.environ["TEST_NAME"] == "py3test":
         assert yt.yson.__file__ == 'yt/python/yt/yson/__init__.py'
