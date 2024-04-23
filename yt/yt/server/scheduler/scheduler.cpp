e02b1cbce676511aa79ed350402009c483b04410
ignat <ignat@ytsaurus.tech>
2024-04-23 11:08:27 +0300

    Fix profiling total_resource_usage and total_resource_limits in scheduler cbd0182e31a448da4efba579337bbd7bf9aa1107

--- yt/yt/server/scheduler/scheduler.cpp
+++ yt/yt/server/scheduler/scheduler.cpp
@@ -317,6 +317,9 @@ public:
             .Counter("/metering/guarantees/record_count");
         GuaranteesMeteringUsageQuantityCounter_ = SchedulerProfiler
             .Counter("/metering/guarantees/usage_quantity");
+
+        TotalResourceLimitsProfiler_.Init(SchedulerProfiler.WithPrefix("/total_resource_limits"));
+        TotalResourceUsageProfiler_.Init(SchedulerProfiler.WithPrefix("/total_resource_usage"));
     }
 
     const NApi::NNative::IClientPtr& GetClient() const
@@ -2061,8 +2064,8 @@ private:
 
         Strategy_->OnMasterConnected();
 
-        TotalResourceLimitsProfiler_.Init(SchedulerProfiler.WithPrefix("/total_resource_limits"));
-        TotalResourceUsageProfiler_.Init(SchedulerProfiler.WithPrefix("/total_resource_usage"));
+        TotalResourceLimitsProfiler_.Start();
+        TotalResourceUsageProfiler_.Start();
 
         SchedulerProfiler.AddFuncGauge("/jobs/registered_job_count", MakeStrong(this), [this] {
             return NodeManager_->GetActiveAllocationCount();
@@ -2088,8 +2091,8 @@ private:
 
     void DoCleanup()
     {
-        TotalResourceLimitsProfiler_.Reset();
-        TotalResourceUsageProfiler_.Reset();
+        TotalResourceLimitsProfiler_.Stop();
+        TotalResourceUsageProfiler_.Stop();
 
         {
             auto error = TError(EErrorCode::MasterDisconnected, "Master disconnected");
--- yt/yt/ytlib/scheduler/job_resources_helpers.cpp
+++ yt/yt/ytlib/scheduler/job_resources_helpers.cpp
@@ -191,14 +191,18 @@ TJobResourcesProfiler::TJobResourcesProfiler()
 
 void TJobResourcesProfiler::Init(const NProfiling::TProfiler& profiler, NProfiling::EMetricType metricType)
 {
-    Producer_->SetEnabled(true);
     profiler.AddProducer("", Producer_);
     MetricType_ = metricType;
 }
 
-void TJobResourcesProfiler::Reset()
+void TJobResourcesProfiler::Stop()
 {
     Producer_->SetEnabled(false);
+}
+
+void TJobResourcesProfiler::Start()
+{
+    Producer_->SetEnabled(true);
 }
 
 void TJobResourcesProfiler::Update(const TJobResources& resources)
