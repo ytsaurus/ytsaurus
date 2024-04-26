55cc246b58865e0dea0c4dba5772c6cd2cc427fe
arkady-e1ppa <arkady-e1ppa@yandex-team.com>
2024-04-26 21:44:05 +0300

    [23.2] Fix data race on Socket_ in TcpConnection f9db9b26a8e25b2f4cb429625f3640043a72a730

--- yt/yt/core/bus/tcp/connection.cpp
+++ yt/yt/core/bus/tcp/connection.cpp
@@ -191,7 +191,7 @@ void TTcpConnection::Close()
 
     EncodedFragments_.clear();
 
-    FlushStatistics();
+    FlushStatistics(/*guarded*/ false);
 }
 
 void TTcpConnection::Start()
@@ -245,7 +245,7 @@ void TTcpConnection::RunPeriodicCheck()
         return;
     }
 
-    FlushStatistics();
+    FlushStatistics(/*guarded*/ false);
 
     auto now = NProfiling::GetCpuInstant();
 
@@ -414,7 +414,7 @@ void TTcpConnection::Open(TGuard<NThreading::TSpinLock>& guard)
     }
 
     UpdateConnectionCount(+1);
-    FlushStatistics();
+    FlushStatistics(/*guarded*/ true);
 
     // Go online and start event processing.
     auto previousPendingControl = static_cast<EPollControl>(PendingControl_.fetch_and(~static_cast<ui64>(EPollControl::Offline)));
@@ -1738,10 +1738,20 @@ void TTcpConnection::InitSocketTosLevel(TTosLevel tosLevel)
     }
 }
 
-void TTcpConnection::FlushStatistics()
+void TTcpConnection::FlushStatistics(bool guarded)
 {
-    UpdateTcpStatistics();
-    FlushBusStatistics();
+    auto doFlush = [&] {
+        UpdateTcpStatistics();
+        FlushBusStatistics();
+    };
+
+    if (guarded) {
+        doFlush();
+        return;
+    }
+
+    auto guard = Guard(Lock_);
+    doFlush();
 }
 
 template <class T, class U>
