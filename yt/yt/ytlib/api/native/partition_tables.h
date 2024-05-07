824b23e57ad32be64b1a300bad02b3e3b4add5a4
galtsev <galtsev@ytsaurus.tech>
2024-05-07 18:50:18 +0300

    [23.2] YT-20661: Allow reversed ranges in `partition_tables` request 78a83f368140dfc95aec31a900626a567df66aea

--- yt/yt/ytlib/api/native/partition_tables.h
+++ yt/yt/ytlib/api/native/partition_tables.h
@@ -63,7 +63,6 @@ private:
     void FixLimitsInOrderedDynamicStore(
         size_t tableIndex,
         const std::vector<NChunkClient::TInputChunkPtr>& inputChunks);
-    void ValidatePaths();
 };
 
 ////////////////////////////////////////////////////////////////////////////////
