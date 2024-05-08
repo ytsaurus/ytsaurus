f201d8e81bb34aebb7c86c6f84adf2e5b15debd5
galtsev <galtsev@ytsaurus.tech>
2024-05-08 10:33:10 +0300

    [23.2] YT-20886: Account for multiple ordered dynamic stores per shard e273b0c8ef19cafdde5b2350d18ffea1636b75f7

--- yt/yt/ytlib/api/native/partition_tables.cpp
+++ yt/yt/ytlib/api/native/partition_tables.cpp
@@ -383,7 +383,7 @@ void TMultiTablePartitioner::FixLimitsInOrderedDynamicStore(
         auto& inputChunk = inputChunks[chunkIndex];
 
         // Rows in ordered dynamic stores go after rows in static stores of the ordered dynamic table.
-        i64 lowerRowIndex = maxStaticStoreUpperRowIndexForTablet[inputChunk->GetTabletIndex()];
+        auto& lowerRowIndex = maxStaticStoreUpperRowIndexForTablet[inputChunk->GetTabletIndex()];
 
         if (!inputChunk->LowerLimit()) {
             inputChunk->LowerLimit() = std::make_unique<TLegacyReadLimit>();
@@ -396,7 +396,8 @@ void TMultiTablePartitioner::FixLimitsInOrderedDynamicStore(
         }
         if (!inputChunk->UpperLimit()->HasRowIndex()) {
             YT_VERIFY(inputChunk->GetTotalRowCount() >= 0);
-            inputChunk->UpperLimit()->SetRowIndex(lowerRowIndex + inputChunk->GetTotalRowCount());
+            lowerRowIndex += inputChunk->GetTotalRowCount();
+            inputChunk->UpperLimit()->SetRowIndex(lowerRowIndex);
         }
     }
 }
