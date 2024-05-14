b00cbbda1642b712bfea0a04afba97e9ccf9ec25
omgronny <omgronny@ytsaurus.tech>
2024-05-14 15:40:04 +0300

    Fix test_scheduler_throttles_heartbeats_by_complexity 32b56e0a9ff830039faaf0a579d74b02ded836e5

--- yt/yt/tests/integration/scheduler/test_scheduler_node_interaction.py
+++ yt/yt/tests/integration/scheduler/test_scheduler_node_interaction.py
@@ -783,7 +783,6 @@ class TestSchedulingHeartbeatThrottling(YTEnvSetup):
         create_pool("small_pool", pool_tree="small_tree")
 
         concurrent_complexity_limit_reached_count = profiler_factory().at_scheduler().counter("scheduler/node_heartbeat/concurrent_complexity_limit_reached_count")
-        unscheduled_resources_by_throttling_count = profiler_factory().at_scheduler().counter("scheduler/unscheduled_node_resources/cpu", tags={"reason": "throttling"})
 
         def get_total_scheduling_heartbeat_complexity():
             total_scheduling_heartbeat_complexity_orchid_path = scheduler_orchid_path() + "/scheduler/node_shards/total_scheduling_heartbeat_complexity"
@@ -807,7 +806,6 @@ class TestSchedulingHeartbeatThrottling(YTEnvSetup):
 
         wait(lambda: get_total_scheduling_heartbeat_complexity() == 15)
         assert concurrent_complexity_limit_reached_count.get_delta() == 0
-        assert unscheduled_resources_by_throttling_count.get_delta() == 0
 
         run_test_vanilla(
             command="sleep 0.5",
@@ -820,4 +818,3 @@ class TestSchedulingHeartbeatThrottling(YTEnvSetup):
 
         wait(lambda: get_total_scheduling_heartbeat_complexity() == 18)
         wait(lambda: concurrent_complexity_limit_reached_count.get_delta() > 0)
-        wait(lambda: unscheduled_resources_by_throttling_count.get_delta() > 0)
