d32d76080e696e87c9da611b17b082426a0829f2
denvr <denvr@ytsaurus.tech>
2024-05-14 13:33:21 +0300

    Yt cli pydoc f09a8453c31079a03e313da0cbb01564b4ddbb3f

--- yt/python/yt/cli/yt_binary.py
+++ yt/python/yt/cli/yt_binary.py
@@ -2565,7 +2565,7 @@ def add_run_command_with_lock_parser(add_parser):
     parser.set_defaults(func=run_command_with_lock_handler)
 
 
-def main_func():
+def _prepare_config_parser():
     config_parser = ArgumentParser(add_help=False)
     config_parser.add_argument("--proxy", help="specify cluster to run command, "
                                                "by default YT_PROXY from environment")
@@ -2581,6 +2581,11 @@ def main_func():
                                help="turn on pinging ancestor transactions")
     config_parser.add_argument("--trace", action="store_true",
                                help="trace execution of request using jaeger")
+    return config_parser
+
+
+def _prepare_parser():
+    config_parser = _prepare_config_parser()
 
     parser = ArgumentParser(parents=[config_parser],
                             formatter_class=RawDescriptionHelpFormatter,
@@ -2746,6 +2751,13 @@ def main_func():
     add_admin_parser(subparsers)
 
     add_dirtable_parser(subparsers)
+
+    return parser
+
+
+def main_func():
+    config_parser = _prepare_config_parser()
+    parser = _prepare_parser()
 
     if "_ARGCOMPLETE" in os.environ:
         completers.autocomplete(parser, append_space=False)
