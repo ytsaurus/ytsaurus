0a033fd3a5cba7d7c76655efae8f55455cf2402f
Maksim Zuev <chiffa@nebius.com>
2024-05-15 20:41:17 +0200

    Fix _is_py_type_compatible_with_ti_type for int enums

--- yt/python/yt/wrapper/schema/types.py
+++ yt/python/yt/wrapper/schema/types.py
@@ -229,7 +229,7 @@ def _is_py_type_compatible_with_ti_type(py_type, ti_type):
     elif py_type is datetime.timedelta:
         return ti_type == ti.Interval
     elif issubclass(py_type, enum.IntEnum):
-        return ti_type in _get_integer_info()["all"] or ti_type in _get_time_types()
+        return ti_type in _get_integer_info()["all"]
     # StrEnum was added in python3.11
     elif hasattr(enum, 'StrEnum') and issubclass(py_type, getattr(enum, 'StrEnum')):
         return ti_type in (ti.Utf8, ti.String)
