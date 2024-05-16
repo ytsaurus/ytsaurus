e31711057f0c89a12077ee3fef18f10ed607ddcd
h0pless <h0pless@ytsaurus.tech>
2024-05-16 13:16:54 +0300

    Improve support for DontSerializeDefault; Add DontSerializeDefault to fix snapshot divergence when rolling 23.2 6c74b74964ce5f7dd983f5b24d3826b030f84695

--- yt/yt/server/master/cell_master/config.cpp
+++ yt/yt/server/master/cell_master/config.cpp
@@ -130,7 +130,8 @@ void TMasterCellDescriptor::Register(TRegistrar registrar)
 void TTestConfig::Register(TRegistrar registrar)
 {
     registrar.Parameter("frozen_hive_edges", &TThis::FrozenHiveEdges)
-        .Default();
+        .Default()
+        .DontSerializeDefault();
 
     registrar.Postprocessor([] (TThis* config) {
         for (const auto& edge : config->FrozenHiveEdges) {
