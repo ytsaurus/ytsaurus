ytsaurus/ytsaurus

commit c437f3bb48e030c05e702df95251030cca8687d3

Cosmetics

yt/yt/library/containers/cri/cri_executor.cpp
@@ -144,7 +144,9 @@ private:
         ContainerDescriptor_ = WaitFor(Executor_->CreateContainer(ContainerSpec_, PodDescriptor_, PodSpec_))
             .ValueOrThrow();
 
-        YT_LOG_DEBUG("Spawning process (Command: %v, Container: %v)", ContainerSpec_->Command[0], ContainerDescriptor_);
+        YT_LOG_DEBUG("Spawning process (Command: %v, Container: %v)",
+            ContainerSpec_->Command[0],
+            ContainerDescriptor_);
         WaitFor(Executor_->StartContainer(ContainerDescriptor_))
             .ThrowOnError();
 
@@ -538,12 +540,12 @@ public:
                 }));
         }
 
-        auto pullComplete = NewPromise<void>();
+        auto pullPromise = NewPromise<void>();
         {
             auto guard = Guard(SpinLock_);
             if (auto* pullFuture = InflightImagePulls_.FindPtr(image.Image)) {
                 YT_LOG_DEBUG("Waiting for in-flight docker image pull (Image: %v)", image);
-                return pullFuture->Apply(BIND([=, this, this_ = MakeStrong(this)] (const TError&) {
+                return pullFuture->Apply(BIND([=, this, this_ = MakeStrong(this)] {
                     // Ignore errors and retry pull after end of previous concurrent attempt.
                     return PullImage(image, /*always*/ false, authConfig, podSpec);
                 }));
@@ -551,7 +553,7 @@ public:
 
             // Future for in-flight pull should not be canceled by waiters,
             // but could become canceled when original pull is canceled.
-            auto pullFuture = pullComplete.ToFuture().ToUncancelable();
+            auto pullFuture = pullPromise.ToFuture().ToUncancelable();
             EmplaceOrCrash(InflightImagePulls_, image.Image, pullFuture);
             pullFuture.Subscribe(BIND([=, this, weakThis = MakeWeak(this)] (const TError&) {
                 if (auto this_ = weakThis.Lock()) {
@@ -570,15 +572,20 @@ public:
             FillPodSandboxConfig(req->mutable_sandbox_config(), *podSpec);
         }
 
-        YT_LOG_DEBUG("Docker image pull started (Image: %v, Authenticated: %v)", image, (bool)authConfig);
+        YT_LOG_DEBUG("Docker image pull started (Image: %v, Authenticated: %v)",
+            image,
+            authConfig.operator bool());
         auto pullFuture = req->Invoke();
-        pullComplete.SetFrom(pullFuture);
+        pullPromise.SetFrom(pullFuture);
 
-        return pullFuture.Apply(BIND([=] (const TErrorOr<TCriImageApi::TRspPullImagePtr>& rspOrError) -> TCriImageDescriptor {
-            YT_LOG_DEBUG_UNLESS(rspOrError.IsOK(), rspOrError, "Docker image pull failed (Image: %v)", image);
+        return pullFuture.Apply(BIND([=] (const TErrorOr<TCriImageApi::TRspPullImagePtr>& rspOrError) {
+            YT_LOG_DEBUG_UNLESS(rspOrError.IsOK(), rspOrError, "Docker image pull failed (Image: %v)",
+                image);
             const auto& rsp = rspOrError.ValueOrThrow();
             const auto& imageId = rsp->image_ref();
-            YT_LOG_DEBUG("Docker image pull finished (Image: %v, ImageId: %v)", image, imageId);
+            YT_LOG_DEBUG("Docker image pull finished (Image: %v, ImageId: %v)",
+                image,
+                imageId);
             return TCriImageDescriptor{.Image = imageId};
         }));
     }

commit 53a708a43847b60fd364747ffffe71ae56a4d851

Always retry chained docker image pull
Without unused "error" argument retry does not happens.

Fixes: c437f3bb48e ("Cosmetics")
(cherry picked from commit 0df372153236f06dd16feb3321dafaecac68921d)

yt/yt/library/containers/cri/cri_executor.cpp
@@ -592,8 +592,10 @@ public:
             auto guard = Guard(SpinLock_);
             if (auto* pullFuture = InflightImagePulls_.FindPtr(image.Image)) {
                 YT_LOG_DEBUG("Waiting for in-flight docker image pull (Image: %v)", image);
-                return pullFuture->ToImmediatelyCancelable().Apply(BIND([=, this, this_ = MakeStrong(this)] {
+                return pullFuture->ToImmediatelyCancelable()
+                    .Apply(BIND([=, this, this_ = MakeStrong(this)] (const TError& error) {
                         // Ignore errors and retry pull after end of previous concurrent attempt.
+                        Y_UNUSED(error);
                         return PullImage(image, /*always*/ false, authConfig, podSpec);
                     }));
             }