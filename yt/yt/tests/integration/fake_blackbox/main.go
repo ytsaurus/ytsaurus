4ab6df9bed28cc34062b6bf6e42c9d788b2cf8b9
nadya73 <nadya73@ytsaurus.tech>
2024-05-29 02:29:34 +0300

    YT-21837: Fix TestAuth
    Fix TestAuth
    70b9da19bea32aeb60f0bf7c1f7ca433e371eb17

--- yt/yt/tests/integration/fake_blackbox/main.go
+++ yt/yt/tests/integration/fake_blackbox/main.go
@@ -64,10 +64,8 @@ func replyOKUserTicket(w http.ResponseWriter, login string) {
 	_, _ = fmt.Fprintf(w, `{
         "users" : [
             {
-                "login": %q,
-                "attributes": {
-                    "1008": %q
-                },
+                "login" : %q,
+                "attributes" : {"1008": %q}
             }
         ]
     }`, login, login)
