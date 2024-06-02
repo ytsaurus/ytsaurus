9b26f31cf616c77fdf72705d1c5c5946bd6b2b87
Andrey Chulkov <achulkov2@nebius.com>
2024-06-02 00:12:54 +0000

    Add dynamic table log writer

--- yt/yt/core/logging/log_writer_detail.h
+++ yt/yt/core/logging/log_writer_detail.h
@@ -5,6 +5,8 @@
 
 #include <yt/yt/library/profiling/sensor.h>
 
+#include <yt/yt/core/actions/callback.h>
+
 namespace NYT::NLogging {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -38,37 +40,89 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//! A mix-in thread-safe class intended to report the size of the current log segment.
+class TSegmentSizeReporter
+{
+public:
+    TSegmentSizeReporter(TString logWriterName);
+
+    void IncrementSegmentSize(i64 size);
+    void ResetSegmentSize(i64 size);
+
+private:
+    std::atomic<i64> CurrentSegmentSize_{0};
+    NProfiling::TGauge CurrentSegmentSizeGauge_;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! Implements the rate-limiting part of ILogWriter.
+//! System log events are always allowed to pass through.
+//! Requires an underlying Write implementation to be provided upon construction.
+//!
+//! NB(achulkov2): The writeImpl solution is a bit sad, but I see no other way to work
+//! around the dynamically interpreted ILogWriter/IFileLogWriter interface. Ideally, we
+//! need to get rid of dynamic casts in TLogManager by abstracting the file-writer related
+//! logic. Then we could switch to a wrapper-like solution, similar to our channels.
+class TRateLimitingLogWriterBase
+    : public virtual ILogWriter
+{
+public:
+    //! The return value of the callback corresponds to the number of bytes logged by writing this event.
+    TRateLimitingLogWriterBase(
+        TCallback<i64(const TLogEvent&)> writeImpl,
+        TString name,
+        TLogWriterConfigPtr config);
+
+    //! Can produce zero or multiple synchronous calls to the underlying write implementation.
+    void Write(const TLogEvent& event) override;
+
+    // There are no overrides for Reload and Flush intentionally.
+    // You need to implement them yourself.
+
+    void SetRateLimit(std::optional<i64> limit) override;
+
+    void SetCategoryRateLimits(const THashMap<TString, i64>& categoryRateLimits) override;
+
+private:
+    const TCallback<i64(const TLogEvent&)> WriteImpl_;
+    const TString Name_;
+    const TLogWriterConfigPtr Config_;
+    const TLoggingCategory* SystemCategory_ = nullptr;
+
+    // These fields are only accessed via the main logging thread and during
+    // log manager's Initialize call, so the lack of synchronization should be fine.
+    TRateLimitCounter RateLimit_;
+    THashMap<TStringBuf, TRateLimitCounter> CategoryToRateLimit_;
+
+    void WriteLogSkippedEvent(i64 eventsSkipped, TStringBuf skippedBy);
+
+    TRateLimitCounter* GetCategoryRateLimitCounter(TStringBuf category);
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
 class TStreamLogWriterBase
     : public virtual ILogWriter
+    , public TRateLimitingLogWriterBase
+    , public TSegmentSizeReporter
 {
 public:
     TStreamLogWriterBase(
         std::unique_ptr<ILogFormatter> formatter,
-        TString name);
+        const TString& name,
+        TLogWriterConfigPtr config);
 
-    void Write(const TLogEvent& event) override;
+    i64 WriteImpl(const TLogEvent& event);
+
     void Flush() override;
     void Reload() override;
-
-    void SetRateLimit(std::optional<i64> limit) override;
-    void SetCategoryRateLimits(const THashMap<TString, i64>& categoryRateLimits) override;
 
 protected:
     virtual IOutputStream* GetOutputStream() const noexcept = 0;
     virtual void OnException(const std::exception& ex);
 
-    void ResetCurrentSegment(i64 size);
-
-    TRateLimitCounter* GetCategoryRateLimitCounter(TStringBuf category);
-
     const std::unique_ptr<ILogFormatter> Formatter_;
-    const TString Name_;
-
-    TRateLimitCounter RateLimit_;
-    THashMap<TStringBuf, TRateLimitCounter> CategoryToRateLimit_;
-
-    i64 CurrentSegmentSize_ = 0;
-    NProfiling::TGauge CurrentSegmentSizeGauge_;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -2657,7 +2657,7 @@ TJobProxyInternalConfigPtr TJob::CreateConfig()
     // This replace logic is used for testing puproses.
     proxyConfig->Logging->UpdateWriters([&] (const IMapNodePtr& writerConfigNode) {
         auto writerConfig = ConvertTo<NLogging::TLogWriterConfigPtr>(writerConfigNode);
-        if (writerConfig->Type != NLogging::TFileLogWriterConfig::Type) {
+        if (writerConfig->Type != NLogging::TFileLogWriterConfig::WriterType) {
             return writerConfigNode;
         }
 
@@ -2673,7 +2673,7 @@ TJobProxyInternalConfigPtr TJob::CreateConfig()
                 NFS::JoinPaths(ToString(GetId()), "job_proxy.log"));
         }
 
-        return writerConfig->BuildFullConfig(fileLogWriterConfig);
+        return ConvertTo<IMapNodePtr>(fileLogWriterConfig);
     });
 
     if (proxyConfig->StderrPath) {
