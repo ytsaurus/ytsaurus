2695424a78dbf6cececec954383e4d67f081150b
yuryalekseev <yuryalekseev@ytsaurus.tech>
2024-06-03 10:44:12 +0300

    YT-21726: Address review comments. 285063403a76a2ad3cb9279d96fdd795cc9ef507

--- yt/yt/server/node/exec_node/volume_manager.cpp
+++ yt/yt/server/node/exec_node/volume_manager.cpp
@@ -1571,7 +1571,7 @@ DEFINE_REFCOUNTED_TYPE(TLayer)
 class TTmpfsLayerCacheCounters
 {
 public:
-    TTmpfsLayerCacheCounters(NProfiling::TProfiler profiler)
+    explicit TTmpfsLayerCacheCounters(NProfiling::TProfiler profiler)
         : Profiler_(std::move(profiler))
     { }
 
@@ -1598,7 +1598,6 @@ private:
         return key;
     }
 
-private:
     const NProfiling::TProfiler Profiler_;
 
     YT_DECLARE_SPIN_LOCK(NThreading::TSpinLock, Lock_);
