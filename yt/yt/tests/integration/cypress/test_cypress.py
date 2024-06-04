3ccedb4c6dbc69391dc45a0b2134d6bbc38f7019
kvk1920 <kvk1920@ytsaurus.tech>
2024-06-04 23:41:17 +0300

    Increase NUM_TEST_PARTITIONS for TestCypressMirroredTx 9905a941efb07d5af6a984041dccade88eda6f93

--- yt/yt/tests/integration/cypress/test_cypress.py
+++ yt/yt/tests/integration/cypress/test_cypress.py
@@ -4180,6 +4180,7 @@ class TestCypressMirroredTx(TestCypressShardedTxCTxS):
     ENABLE_CYPRESS_TRANSACTIONS_IN_SEQUOIA = True
     ENABLE_TMP_ROOTSTOCK = False
     NUM_CYPRESS_PROXIES = 1
+    NUM_TEST_PARTITIONS = 6
 
     DELTA_CONTROLLER_AGENT_CONFIG = {
         "commit_operation_cypress_node_changes_via_system_transaction": True,
