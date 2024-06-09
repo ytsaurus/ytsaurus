6f33a88c2ce293ec8265bdfecdb02e0ce85b6099
Dmitriy Fedorov <dmifedorov@nebius.com>
2024-06-09 21:58:33 +0000

    some fixes

--- yt/python/docker/test_image/run_wrapper_tests.sh
+++ yt/python/docker/test_image/run_wrapper_tests.sh
@@ -5,5 +5,5 @@ YT_PYTHON_DIR=$(realpath "${SCRIPT_DIR}/../")
 
 docker run -it -e PYTHONPATH="/source:${PYTHONPATH}" \
  --mount type=bind,source=${YT_PYTHON_DIR}/yt,target=/source/yt \
-ghcr.io/dmi-feo/yt_wrapper_tests_base_image:0.0.4 \
+sha256:d1956f09a8e3962757be62e5b6acd61649505c0eddb41a3f81ce7d5eb078883d \
  bash -c "cp -r /workdir/python_build/yt/packages /source/yt && pytest /source/yt/wrapper/tests/$1 ${@:2}"
