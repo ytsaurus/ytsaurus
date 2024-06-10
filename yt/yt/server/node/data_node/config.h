efbb04d0ecef128789f79f674c523ba67ae848f9
Aleksandr Gaev <aleksandr.gaev@nebius.com>
2024-06-10 16:12:56 +0300

    Data nodes disk throttlers for uncategorized load
    Replace unlimited disk throttlers with uncategorized disk throttlers that:
    - combine in and out load
    - are configurable
    
    Please cherry-pick it to stable/23.2 and stable/24.1
    
    ---
    4c9782de28168f526a7cb69149b4c24c56b13048
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/611

--- yt/yt/server/node/data_node/config.h
+++ yt/yt/server/node/data_node/config.h
@@ -94,6 +94,10 @@ public:
     //! Configuration for various per-location throttlers.
     TEnumIndexedArray<EChunkLocationThrottlerKind, NConcurrency::TThroughputThrottlerConfigPtr> Throttlers;
 
+    //! Configuration for uncategorized throttler.
+    bool EnableUncategorizedThrottler;
+    NConcurrency::TThroughputThrottlerConfigPtr UncategorizedThrottler;
+
     //! IO engine type.
     NIO::EIOEngineType IOEngineType;
 
@@ -147,6 +151,9 @@ public:
 
     TEnumIndexedArray<EChunkLocationThrottlerKind, NConcurrency::TThroughputThrottlerConfigPtr> Throttlers;
     std::optional<TDuration> ThrottleDuration;
+
+    std::optional<bool> EnableUncategorizedThrottler;
+    NConcurrency::TThroughputThrottlerConfigPtr UncategorizedThrottler;
 
     std::optional<i64> CoalescedReadMaxGapSize;
 
