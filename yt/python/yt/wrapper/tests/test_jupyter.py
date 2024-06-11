de74ba64043c59e1004484cdff07a7b2fc4eef3f
Dmitriy Fedorov <dmifedorov@nebius.com>
2024-06-11 11:23:15 +0000

    py wrapper: fix jupyter notebooks for operations in separate cells

--- yt/python/yt/wrapper/tests/test_jupyter.py
+++ yt/python/yt/wrapper/tests/test_jupyter.py
@@ -6,17 +6,30 @@ import yt.wrapper as yt
 from yt.testlib import authors
 
 
-PROGRAM = Template("""
+CELL_01 = Template("""
+class Foo:
+    def bar(self):
+        pass
+""")
+
+
+CELL_02 = Template("""
 class Mapper:
     def __call__(self, input_row):
+        Foo().bar()
         yield input_row
+""")
 
 
+CELL_03 = Template("""
 from yt.wrapper.client import YtClient
 yt_cli = YtClient(proxy="$yt_proxy")
 yt_cli.write_table("//tmp/foo", [{"x": 1}])
 yt_cli.run_map(Mapper(), "//tmp/foo", "//tmp/foo")
 """)
+
+
+CELLS = [CELL_01, CELL_02, CELL_03]
 
 
 @pytest.mark.usefixtures("yt_env_v4")
@@ -31,6 +44,8 @@ class TestJupyter:
         km, kc = await jp_start_kernel()
         assert km.kernel_name == "python3"
         proxy_url = yt.config.config["proxy"]["url"]
-        script_to_run = PROGRAM.substitute(yt_proxy=proxy_url)
-        msg = await kc.execute(script_to_run, reply=True)
-        assert msg["content"]["status"] == "ok"
+
+        for cell in CELLS:
+            script_to_run = cell.substitute(yt_proxy=proxy_url)
+            msg = await kc.execute(script_to_run, reply=True)
+            assert msg["content"]["status"] == "ok"
