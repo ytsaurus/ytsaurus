646ac70c08d73dcc9ce626b58cf53f074428e642
pazus <pazus@yandex-team.com>
2024-06-19 08:09:30 +0300

    Update arcadia protobuf to 3.25.3 20f3895d893b877de556b7797c92edf535ccf891

--- contrib/java/com/google/protobuf/protobuf-kotlin/3.25.3/ya.make
+++ contrib/java/com/google/protobuf/protobuf-kotlin/3.25.3/ya.make
@@ -3,12 +3,12 @@ JAVA_CONTRIB()
 LICENSE(BSD-3-Clause)
 
 PEERDIR(
-    contrib/java/com/google/protobuf/protobuf-java/3.22.5
+    contrib/java/com/google/protobuf/protobuf-java/3.25.3
     contrib/java/org/jetbrains/kotlin/kotlin-stdlib/1.6.0
 )
 
-JAR_RESOURCE(5119763728)
+JAR_RESOURCE(6385637995)
 
-SRC_RESOURCE(5119763726)
+SRC_RESOURCE(6385637852)
 
 END()
--- yt/java/ytsaurus-testlib/src/main/proto/build.gradle.kts
+++ yt/java/ytsaurus-testlib/src/main/proto/build.gradle.kts
@@ -18,7 +18,7 @@ java {
 }
 
 dependencies {
-    api("com.google.protobuf:protobuf-java:3.22.5")
+    api("com.google.protobuf:protobuf-java:3.25.3")
 
     protobuf(files(File(buildProtoDir, "yt/java/ytsaurus-testlib/src/main/proto")))
 }
@@ -26,7 +26,7 @@ dependencies {
 protobuf {
     protoc {
         // Download from repositories
-        artifact = "com.google.protobuf:protoc:3.22.5"
+        artifact = "com.google.protobuf:protoc:3.25.3"
     }
 
 }
