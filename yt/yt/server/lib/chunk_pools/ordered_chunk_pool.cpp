bbfc83e0fd2f553b226c503403e3fd5b719c7d67
Andrey Chulkov <achulkov2@nebius.com>
2024-06-20 14:45:36 +0000

    Address PR #423 babenko@ comments

--- yt/yt/server/lib/chunk_pools/ordered_chunk_pool.cpp
+++ yt/yt/server/lib/chunk_pools/ordered_chunk_pool.cpp
@@ -441,7 +441,8 @@ private:
 
         // Unbounded dynamic store cannot be split.
         if (dataSlice->GetSingleUnversionedChunkSlice()->GetInputChunk()->IsOrderedDynamicStore() &&
-            !dataSlice->GetSingleUnversionedChunkSlice()->UpperLimit().RowIndex) {
+            !dataSlice->GetSingleUnversionedChunkSlice()->UpperLimit().RowIndex)
+        {
             return false;
         }
 
