249cc706853dec51478bba42c068cc224944937e
pogorelov <pogorelov@ytsaurus.tech>
2024-06-20 21:26:26 +0300

    Better fix exe node crash on CA disconnected 93ec7f1e224fa7186b79cd766d88659ae9e5324c

--- yt/yt/server/node/exec_node/controller_agent_connector.cpp
+++ yt/yt/server/node/exec_node/controller_agent_connector.cpp
@@ -660,6 +660,10 @@ TControllerAgentConnectorPool::GetControllerAgentConnector(
 {
     VERIFY_INVOKER_THREAD_AFFINITY(Bootstrap_->GetJobInvoker(), JobThread);
 
+    if (!agentDescriptor) {
+        return nullptr;
+    }
+
     return GetOrCrash(ControllerAgentConnectors_, agentDescriptor);
 }
 
