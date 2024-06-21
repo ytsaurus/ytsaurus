75527108828940873a6d293fe68731f74794e3a4
Dmitrii Gadeev <kruft@nebius.com>
2024-06-21 15:58:17 +0500

    token argument in db-init script

--- yt/odin/bin/yt_odin_init_db_cluster/__main__.py
+++ yt/odin/bin/yt_odin_init_db_cluster/__main__.py
@@ -11,12 +11,13 @@ def main():
     parser = argparse.ArgumentParser()
     parser.add_argument("--table", required=True)
     parser.add_argument("--proxy", required=True)
+    parser.add_argument("--token", required=True)
     parser.add_argument("--tablet-cell-bundle", default="odin")
     parser.add_argument("--primary-medium", default="default")
     args = parser.parse_args()
 
-    client = yt.YtClient(proxy=args.proxy)
-    init_yt_table(args.table, client, bundle=args.tablet_cell_bundle, primary_medium=args.primary_medium)
+    client = yt.YtClient(proxy=args.proxy, token=args.token)
+    init_yt_table(args.table, client, bundle=args.tablet_cell_bundle)
 
 
 if __name__ == "__main__":
