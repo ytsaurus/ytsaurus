8cf2c4bd35ff4bb6cf5f2516afcd147e1fcfc01f
Dmitriy Fedorov <dmifedorov@nebius.com>
2024-06-22 22:07:49 +0000

    reorg

--- yt/python/docker/test_image/README.md
+++
@@ -1,9 +0,0 @@
-## Run tests
-```
-./run_wrapper_tests.sh test_cypress_commands.py
-```
-
-## Create new image
-```
-make docker tag=ytsaurus/pywrapper_tests_base_image:1.2.3
-```
--- yt/python/docker/test_image/run_wrapper_tests.sh
+++
@@ -1,9 +0,0 @@
-#!/usr/bin/env bash
-
-SCRIPT_DIR=$(dirname "$0")
-YT_PYTHON_DIR=$(realpath "${SCRIPT_DIR}/../../")
-
-docker run -it -e PYTHONPATH="/source:${PYTHONPATH}" \
- --mount type=bind,source=${YT_PYTHON_DIR}/yt,target=/source/yt \
-sha256:d1956f09a8e3962757be62e5b6acd61649505c0eddb41a3f81ce7d5eb078883d \
- bash -c "cp -r /workdir/python_build/yt/packages /source/yt && pytest /source/yt/wrapper/tests/$1 ${@:2}"
