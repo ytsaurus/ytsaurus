391e7d40acfcb294e8b4327a843cb4c91a429f3a
Dmitriy Fedorov <dmifedorov@nebius.com>
2024-06-23 11:38:25 +0000

    scripts

---
+++ yt/python/yt/run_wrapper_tests.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+SCRIPT_DIR=$(dirname "$0")
+YT_PYTHON_DIR=$(realpath "${SCRIPT_DIR}/../../")
+
+docker run -it -e PYTHONPATH="/source:${PYTHONPATH}" \
+ --mount type=bind,source=${YT_PYTHON_DIR}/yt,target=/source/yt \
+sha256:0c0eb701f7cd0a61fc735c844e1b8762a63c66df8ebd03675e529be0eea6d7d1 \
+ bash -c "cp -r /workdir/python_build/yt/packages /source/yt && pytest /source/yt/wrapper/tests/$1 ${@:2}"
