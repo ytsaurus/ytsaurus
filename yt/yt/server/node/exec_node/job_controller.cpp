c099bd934cf4a7c968cc2a533d02bb2a0de20937
pogorelov <pogorelov@ytsaurus.tech>
2024-06-25 14:54:44 +0300

    Start allocations in FIFO order 05d6907c256b5f0cc90cd5667b0fc05eb6d21d2c

--- yt/yt/server/node/exec_node/job_controller.cpp
+++ yt/yt/server/node/exec_node/job_controller.cpp
@@ -523,7 +523,7 @@ private:
 
     THashMap<TJobId, TJobPtr> IdToJob_;
     THashMap<TAllocationId, TAllocationPtr> IdToAllocations_;
-    std::vector<TAllocationPtr> AllocationsWaitingForResources_;
+    std::queue<TAllocationPtr> AllocationsWaitingForResources_;
     THashMap<TOperationId, THashSet<TJobPtr>> OperationIdToJobs_;
 
     THashSet<TJobPtr> JobsWaitingForCleanup_;
@@ -708,7 +708,7 @@ private:
             waitingForResourcesTimeout,
             Bootstrap_->GetJobInvoker());
 
-        AllocationsWaitingForResources_.push_back(std::move(allocation));
+        AllocationsWaitingForResources_.push(std::move(allocation));
 
         ScheduleStartAllocations();
     }
@@ -1512,20 +1512,23 @@ private:
 
         StartAllocationsScheduled_ = false;
 
-        auto allocationsToStart = std::move(AllocationsWaitingForResources_);
-
         if (AreJobsDisabled()) {
-            for (const auto& allocation : allocationsToStart) {
+            for (; !empty(AllocationsWaitingForResources_); AllocationsWaitingForResources_.pop()) {
+                const auto& allocation = AllocationsWaitingForResources_.front();
                 allocation->Abort(MakeJobsDisabledError());
             }
 
             return;
         }
 
-        for (auto& allocation : allocationsToStart) {
+        for (; !empty(AllocationsWaitingForResources_);) {
+            const auto& allocation = AllocationsWaitingForResources_.front();
+
             auto allocationId = allocation->GetId();
             if (!IdToAllocations_.contains(allocationId) || allocation->GetState() != EAllocationState::Waiting) {
                 YT_LOG_DEBUG("No such allocation, it seems to be aborted (AllocationId: %v)", allocationId);
+
+                AllocationsWaitingForResources_.pop();
                 continue;
             }
 
@@ -1533,8 +1536,8 @@ private:
 
             try {
                 if (!resourceAcquiringContext.TryAcquireResourcesFor(allocation->GetResourceHolder())) {
-                    YT_LOG_DEBUG("Allocation was not started (AllocationId: %v)", allocationId);
-                    AllocationsWaitingForResources_.push_back(std::move(allocation));
+                    YT_LOG_DEBUG("Allocation was not started, starting waing for resources (AllocationId: %v)", allocationId);
+                    break;
                 } else {
                     YT_LOG_DEBUG("Allocation started (AllocationId: %v)", allocationId);
                     allocation->OnResourcesAcquired();
@@ -1547,6 +1550,8 @@ private:
                     "Unexpected exception during starting allocations (CurrentAllocationId: %v)",
                     allocationId);
             }
+
+            AllocationsWaitingForResources_.pop();
         }
     }
 
