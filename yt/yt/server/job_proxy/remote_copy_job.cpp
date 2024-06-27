909a7c9490d4dc3300c3d82d7c07c0bb21f7ff97
coteeq <coteeq@yandex-team.com>
2024-06-27 09:20:03 +0300

    Report chunk as failed if repair is impossible 1e6bad4672639406ec4866c2e31b43f4fb1791e3

--- yt/yt/server/job_proxy/remote_copy_job.cpp
+++ yt/yt/server/job_proxy/remote_copy_job.cpp
@@ -510,7 +510,11 @@ private:
         if (repairChunk) {
             auto copyStarted = TInstant::Now();
 
-            WaitUntilErasureChunkCanBeRepaired(erasureCodec, &copyFutures);
+            auto waitError = WaitUntilErasureChunkCanBeRepaired(erasureCodec, &copyFutures);
+            if (!waitError.IsOK()) {
+                FailedChunkIds_.push_back(inputChunkId);
+            }
+            waitError.ThrowOnError();
 
             // COMPAT(gritukan)
             TDuration erasureChunkRepairDelay;
@@ -597,7 +601,7 @@ private:
     }
 
     //! Waits until enough parts were copied to perform repair.
-    void WaitUntilErasureChunkCanBeRepaired(
+    TError WaitUntilErasureChunkCanBeRepaired(
         NErasure::ICodec* erasureCodec,
         std::vector<TFuture<void>>* copyFutures)
     {
@@ -638,8 +642,7 @@ private:
             }));
         }
 
-        WaitFor(callbackContext->CanStartRepair.ToFuture())
-            .ThrowOnError();
+        return WaitFor(callbackContext->CanStartRepair.ToFuture());
     }
 
     void RepairErasureChunk(
