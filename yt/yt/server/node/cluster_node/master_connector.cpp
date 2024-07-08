b51020be413340c60b65a6fb3396bdabdbb14abf
proller <proller@github.com>
2024-07-08 15:54:11 +0000

    fix

--- yt/yt/server/node/cluster_node/master_connector.cpp
+++ yt/yt/server/node/cluster_node/master_connector.cpp
@@ -580,7 +580,7 @@ private:
         req->set_exec_node_is_not_data_node(Bootstrap_->GetConfig()->ExecNodeIsNotDataNode);
 
         if (Bootstrap_->GetConfig()->Rack) {
-            req->set_rack(Bootstrap_->GetConfig()->Rack);
+            req->set_rack(*Bootstrap_->GetConfig()->Rack);
         }
         if (Bootstrap_->GetConfig()->DataCenter) {
             req->set_data_center(*Bootstrap_->GetConfig()->DataCenter);
