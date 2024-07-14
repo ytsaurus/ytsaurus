9825e6b8039120812aaed162f70753e1e5859678
babenko <babenko@ytsaurus.tech>
2024-07-14 14:58:18 +0300

    Constexpr for logger alias be0a42f2b27c1616332dc29ea15b89cb90a27b91

--- yt/yt/server/node/data_node/ally_replica_manager.cpp
+++ yt/yt/server/node/data_node/ally_replica_manager.cpp
@@ -49,7 +49,7 @@ using NYT::FromProto;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-const static auto& Logger = DataNodeLogger;
+static constexpr auto& Logger = DataNodeLogger;
 constexpr auto ProfilingPeriod = TDuration::Seconds(1);
 constexpr auto QueueFlushPeriod = TDuration::Seconds(1);
 
