74a5d9be7ba178f40db13d0ec957a3900a3180f9
dtorilov <dtorilov@ytsaurus.tech>
2024-07-17 16:23:11 +0300

    Cosmetics a6472f766491b3fae65cf8e3b53748973a7682d4

---
+++ yt/yt/library/query/base/ast_visitors-inl.h
@@ -0,0 +1,386 @@
+#ifndef AST_VISITORS_INL_H_
+#error "Direct inclusion of this file is not allowed, include ast_visitors.h"
+// For the sake of sane code completion.
+#include "ast_visitors.h"
+#endif
+
+namespace NYT::NQueryClient::NAst {
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TResult, class TDerived, class TNode>
+TDerived* TAbstractAstVisitor<TResult, TDerived, TNode>::Derived()
+{
+    return static_cast<TDerived*>(this);
+}
+
+template <class TResult, class TDerived, class TNode>
+TResult TAbstractAstVisitor<TResult, TDerived, TNode>::Visit(TNode node)
+{
+    CheckStackDepth();
+    auto* expr = Derived()->GetExpression(node);
+
+    if (auto* literalExpr = expr->template As<TLiteralExpression>()) {
+        return Derived()->OnLiteral(literalExpr);
+    } else if (auto* referenceExpr = expr->template As<TReferenceExpression>()) {
+        return Derived()->OnReference(referenceExpr);
+    } else if (auto* aliasExpr = expr->template As<TAliasExpression>()) {
+        return Derived()->OnAlias(aliasExpr);
+    } else if (auto* unaryOp = expr->template As<TUnaryOpExpression>()) {
+        return Derived()->OnUnary(unaryOp);
+    } else if (auto* binaryOp = expr->template As<TBinaryOpExpression>()) {
+        return Derived()->OnBinary(binaryOp);
+    } else if (auto* functionExpr = expr->template As<TFunctionExpression>()) {
+        return Derived()->OnFunction(functionExpr);
+    } else if (auto* inExpr = expr->template As<TInExpression>()) {
+        return Derived()->OnIn(inExpr);
+    } else if (auto* betweenExpr = expr->template As<TBetweenExpression>()) {
+        return Derived()->OnBetween(betweenExpr);
+    } else if (auto* transformExpr = expr->template As<TTransformExpression>()) {
+        return Derived()->OnTransform(transformExpr);
+    } else if (auto* caseExpr = expr->template As<TCaseExpression>()) {
+        return Derived()->OnCase(caseExpr);
+    } else if (auto* likeExpr = expr->template As<TLikeExpression>()) {
+        return Derived()->OnLike(likeExpr);
+    }
+    YT_ABORT();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TDerived, class TNode>
+TDerived* TAbstractAstVisitor<void, TDerived, TNode>::Derived()
+{
+    return static_cast<TDerived*>(this);
+}
+
+template <class TDerived, class TNode>
+void TAbstractAstVisitor<void, TDerived, TNode>::Visit(TNode node)
+{
+    CheckStackDepth();
+    auto* expr = Derived()->GetExpression(node);
+
+    if (auto* literalExpr = expr->template As<TLiteralExpression>()) {
+        return Derived()->OnLiteral(literalExpr);
+    } else if (auto* referenceExpr = expr->template As<TReferenceExpression>()) {
+        return Derived()->OnReference(referenceExpr);
+    } else if (auto* aliasExpr = expr->template As<TAliasExpression>()) {
+        return Derived()->OnAlias(aliasExpr);
+    } else if (auto* unaryOp = expr->template As<TUnaryOpExpression>()) {
+        return Derived()->OnUnary(unaryOp);
+    } else if (auto* binaryOp = expr->template As<TBinaryOpExpression>()) {
+        return Derived()->OnBinary(binaryOp);
+    } else if (auto* functionExpr = expr->template As<TFunctionExpression>()) {
+        return Derived()->OnFunction(functionExpr);
+    } else if (auto* inExpr = expr->template As<TInExpression>()) {
+        return Derived()->OnIn(inExpr);
+    } else if (auto* betweenExpr = expr->template As<TBetweenExpression>()) {
+        return Derived()->OnBetween(betweenExpr);
+    } else if (auto* transformExpr = expr->template As<TTransformExpression>()) {
+        return Derived()->OnTransform(transformExpr);
+    } else if (auto* caseExpr = expr->template As<TCaseExpression>()) {
+        return Derived()->OnCase(caseExpr);
+    } else if (auto* likeExpr = expr->template As<TLikeExpression>()) {
+        return Derived()->OnLike(likeExpr);
+    }
+    YT_ABORT();
+}
+
+template <class TDerived, class TNode>
+void TAbstractAstVisitor<void, TDerived, TNode>::Visit(const std::vector<TNode>& tuple)
+{
+    for (const auto& element : tuple) {
+        Visit(element);
+    }
+}
+
+template <class TDerived, class TNode>
+void TAbstractAstVisitor<void, TDerived, TNode>::Visit(const std::optional<std::vector<TNode>>& nullableTuple)
+{
+    if (nullableTuple) {
+        Visit(*nullableTuple);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TResult, class TDerived>
+TExpressionPtr TBaseAstVisitor<TResult, TDerived>::GetExpression(TExpressionPtr expr)
+{
+    return expr;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnLiteral(const TLiteralExpressionPtr /*literalExpr*/)
+{ }
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnReference(const TReferenceExpressionPtr /*referenceExpr*/)
+{ }
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnAlias(const TAliasExpressionPtr /*aliasExpr*/)
+{ }
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnUnary(const TUnaryOpExpressionPtr unaryExpr)
+{
+    Visit(unaryExpr->Operand);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnBinary(const TBinaryOpExpressionPtr binaryExpr)
+{
+    Visit(binaryExpr->Lhs);
+    Visit(binaryExpr->Rhs);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnFunction(const TFunctionExpressionPtr functionExpr)
+{
+    Visit(functionExpr->Arguments);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnIn(const TInExpressionPtr inExpr)
+{
+    Visit(inExpr->Expr);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnBetween(const TBetweenExpressionPtr betweenExpr)
+{
+    Visit(betweenExpr->Expr);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnTransform(const TTransformExpressionPtr transformExpr)
+{
+    Visit(transformExpr->Expr);
+    Visit(transformExpr->DefaultExpr);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnCase(const TCaseExpressionPtr caseExpr)
+{
+    Visit(caseExpr->OptionalOperand);
+    for (const auto& whenThenExpression : caseExpr->WhenThenExpressions) {
+        Visit(whenThenExpression.first);
+        Visit(whenThenExpression.second);
+    }
+    Visit(caseExpr->DefaultExpression);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::OnLike(const TLikeExpressionPtr likeExpr)
+{
+    Visit(likeExpr->Text);
+    Visit(likeExpr->Pattern);
+    Visit(likeExpr->EscapeCharacter);
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::Visit(const std::vector<TExpressionPtr>& tuple)
+{
+    for (auto expr : tuple) {
+        Visit(expr);
+    }
+}
+
+template <class TDerived>
+void TAstVisitor<TDerived>::Visit(const std::optional<std::vector<TExpressionPtr>>& nullableTuple)
+{
+    if (nullableTuple) {
+        return Visit(*nullableTuple);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+template <class TDerived>
+TRewriter<TDerived>::TRewriter(TObjectsHolder* head)
+    : Head(head)
+{ }
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnLiteral(TLiteralExpressionPtr literalExpr)
+{
+    return literalExpr;
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnReference(TReferenceExpressionPtr referenceExpr)
+{
+    return referenceExpr;
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnAlias(TAliasExpressionPtr aliasExpr)
+{
+    auto* newExpression = Visit(aliasExpr->Expression);
+    if (newExpression == aliasExpr->Expression) {
+        return aliasExpr;
+    } else {
+        return Head->New<TAliasExpression>(NullSourceLocation, newExpression, aliasExpr->Name);
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnUnary(TUnaryOpExpressionPtr unaryExpr)
+{
+    auto newOperand = Visit(unaryExpr->Operand);
+    if (newOperand == unaryExpr->Operand) {
+        return unaryExpr;
+    } else {
+        return Head->New<TUnaryOpExpression>(NullSourceLocation, unaryExpr->Opcode, std::move(newOperand));
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnBinary(TBinaryOpExpressionPtr binaryExpr)
+{
+    auto newLhs = Visit(binaryExpr->Lhs);
+    auto newRhs = Visit(binaryExpr->Rhs);
+
+    if (newLhs == binaryExpr->Lhs && newRhs == binaryExpr->Rhs) {
+        return binaryExpr;
+    } else {
+        return Head->New<TBinaryOpExpression>(
+            NullSourceLocation,
+            binaryExpr->Opcode,
+            std::move(newLhs),
+            std::move(newRhs));
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnFunction(TFunctionExpressionPtr functionExpr)
+{
+    auto newArguments = Visit(functionExpr->Arguments);
+
+    if (newArguments == functionExpr->Arguments) {
+        return functionExpr;
+    } else {
+        return Head->New<TFunctionExpression>(
+            NullSourceLocation,
+            functionExpr->FunctionName,
+            std::move(newArguments));
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnIn(TInExpressionPtr inExpr)
+{
+    auto expression = Visit(inExpr->Expr);
+
+    if (expression == inExpr->Expr) {
+        return inExpr;
+    } else {
+        return Head->New<TInExpression>(NullSourceLocation, std::move(expression), inExpr->Values);
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnBetween(TBetweenExpressionPtr betweenExpr)
+{
+    auto expression = Visit(betweenExpr->Expr);
+
+    if (expression == betweenExpr->Expr) {
+        return betweenExpr;
+    } else {
+        return Head->New<TBetweenExpression>(NullSourceLocation, std::move(expression), betweenExpr->Values);
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnTransform(TTransformExpressionPtr transformExpr)
+{
+    auto expression = Visit(transformExpr->Expr);
+    auto newDefault = Visit(transformExpr->DefaultExpr);
+
+    if (newDefault == transformExpr->DefaultExpr && expression == transformExpr->Expr) {
+        return transformExpr;
+    } else {
+        return Head->New<TTransformExpression>(
+            NullSourceLocation,
+            std::move(expression),
+            transformExpr->From,
+            transformExpr->To,
+            std::move(newDefault));
+    }
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnCase(TCaseExpressionPtr caseExpr)
+{
+    auto newOptionalOperand = Visit(caseExpr->OptionalOperand);
+    auto newDefaultExpression = Visit(caseExpr->DefaultExpression);
+
+    int expressionCount = caseExpr->WhenThenExpressions.size();
+    std::vector<std::pair<TExpressionList, TExpressionList>> newWhenThenExpressions(expressionCount);
+    for (int index = 0; index < expressionCount; ++index) {
+        newWhenThenExpressions[index] = {
+            Visit(caseExpr->WhenThenExpressions[index].first),
+            Visit(caseExpr->WhenThenExpressions[index].second)
+        };
+    }
+
+    if (newWhenThenExpressions == caseExpr->WhenThenExpressions &&
+        newOptionalOperand == caseExpr->OptionalOperand &&
+        newDefaultExpression == caseExpr->DefaultExpression)
+    {
+        return caseExpr;
+    }
+
+    return Head->New<TCaseExpression>(
+        NullSourceLocation,
+        std::move(newOptionalOperand),
+        std::move(newWhenThenExpressions),
+        std::move(newDefaultExpression));
+}
+
+template <class TDerived>
+TExpressionPtr TRewriter<TDerived>::OnLike(TLikeExpressionPtr likeExpr)
+{
+    auto newText = Visit(likeExpr->Text);
+    auto newPattern = Visit(likeExpr->Pattern);
+    auto newEscape = Visit(likeExpr->EscapeCharacter);
+
+    if (newText == likeExpr->Text && newPattern == likeExpr->Pattern && newEscape == likeExpr->EscapeCharacter) {
+        return likeExpr;
+    } else {
+        return Head->New<TLikeExpression>(
+            NullSourceLocation,
+            std::move(newText),
+            likeExpr->Opcode,
+            std::move(newPattern),
+            std::move(newEscape));
+    }
+}
+
+template <class TDerived>
+std::vector<TExpressionPtr> TRewriter<TDerived>::Visit(const std::vector<TExpressionPtr>& tuple)
+{
+    int size = tuple.size();
+    std::vector<TExpressionPtr> newTuple(size);
+    for (int index = 0; index < size; ++index) {
+        newTuple[index] = Visit(tuple[index]);
+    }
+    return newTuple;
+}
+
+template <class TDerived>
+std::optional<std::vector<TExpressionPtr>> TRewriter<TDerived>::Visit(const std::optional<std::vector<TExpressionPtr>>& nullableTuple)
+{
+    if (!nullableTuple) {
+        return std::nullopt;
+    } else {
+        return Visit(*nullableTuple);
+    }
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NQueryClient::NAst
--- yt/yt/library/query/base/ast_visitors.h
+++ yt/yt/library/query/base/ast_visitors.h
@@ -10,105 +10,26 @@ namespace NYT::NQueryClient::NAst {
 template <class TResult, class TDerived, class TNode>
 struct TAbstractAstVisitor
 {
-    TDerived* Derived()
-    {
-        return static_cast<TDerived*>(this);
-    }
+    TDerived* Derived();
 
-    TResult Visit(TNode node)
-    {
-        CheckStackDepth();
-        auto* expr = Derived()->GetExpression(node);
-
-        if (auto* literalExpr = expr->template As<TLiteralExpression>()) {
-            return Derived()->OnLiteral(literalExpr);
-        } else if (auto* referenceExpr = expr->template As<TReferenceExpression>()) {
-            return Derived()->OnReference(referenceExpr);
-        } else if (auto* aliasExpr = expr->template As<TAliasExpression>()) {
-            return Derived()->OnAlias(aliasExpr);
-        } else if (auto* unaryOp = expr->template As<TUnaryOpExpression>()) {
-            return Derived()->OnUnary(unaryOp);
-        } else if (auto* binaryOp = expr->template As<TBinaryOpExpression>()) {
-            return Derived()->OnBinary(binaryOp);
-        } else if (auto* functionExpr = expr->template As<TFunctionExpression>()) {
-            return Derived()->OnFunction(functionExpr);
-        } else if (auto* inExpr = expr->template As<TInExpression>()) {
-            return Derived()->OnIn(inExpr);
-        } else if (auto* betweenExpr = expr->template As<TBetweenExpression>()) {
-            return Derived()->OnBetween(betweenExpr);
-        } else if (auto* transformExpr = expr->template As<TTransformExpression>()) {
-            return Derived()->OnTransform(transformExpr);
-        } else if (auto* caseExpr = expr->template As<TCaseExpression>()) {
-            return Derived()->OnCase(caseExpr);
-        } else if (auto* likeExpr = expr->template As<TLikeExpression>()) {
-            return Derived()->OnLike(likeExpr);
-        }
-        YT_ABORT();
-    }
+    TResult Visit(TNode node);
 };
 
 template <class TDerived, class TNode>
 struct TAbstractAstVisitor<void, TDerived, TNode>
 {
-    TDerived* Derived()
-    {
-        return static_cast<TDerived*>(this);
-    }
+    TDerived* Derived();
 
-    void Visit(TNode node)
-    {
-        CheckStackDepth();
-        auto* expr = Derived()->GetExpression(node);
-
-        if (auto* literalExpr = expr->template As<TLiteralExpression>()) {
-            return Derived()->OnLiteral(literalExpr);
-        } else if (auto* referenceExpr = expr->template As<TReferenceExpression>()) {
-            return Derived()->OnReference(referenceExpr);
-        } else if (auto* aliasExpr = expr->template As<TAliasExpression>()) {
-            return Derived()->OnAlias(aliasExpr);
-        } else if (auto* unaryOp = expr->template As<TUnaryOpExpression>()) {
-            return Derived()->OnUnary(unaryOp);
-        } else if (auto* binaryOp = expr->template As<TBinaryOpExpression>()) {
-            return Derived()->OnBinary(binaryOp);
-        } else if (auto* functionExpr = expr->template As<TFunctionExpression>()) {
-            return Derived()->OnFunction(functionExpr);
-        } else if (auto* inExpr = expr->template As<TInExpression>()) {
-            return Derived()->OnIn(inExpr);
-        } else if (auto* betweenExpr = expr->template As<TBetweenExpression>()) {
-            return Derived()->OnBetween(betweenExpr);
-        } else if (auto* transformExpr = expr->template As<TTransformExpression>()) {
-            return Derived()->OnTransform(transformExpr);
-        } else if (auto* caseExpr = expr->template As<TCaseExpression>()) {
-            return Derived()->OnCase(caseExpr);
-        } else if (auto* likeExpr = expr->template As<TLikeExpression>()) {
-            return Derived()->OnLike(likeExpr);
-        }
-        YT_ABORT();
-    }
-
-    void Visit(const std::vector<TNode>& tuple)
-    {
-        for (const auto& element : tuple) {
-            Visit(element);
-        }
-    }
-
-    void Visit(const std::optional<std::vector<TNode>>& nullableTuple)
-    {
-        if (nullableTuple) {
-            Visit(*nullableTuple);
-        }
-    }
+    void Visit(TNode node);
+    void Visit(const std::vector<TNode>& tuple);
+    void Visit(const std::optional<std::vector<TNode>>& nullableTuple);
 };
 
 template <class TResult, class TDerived>
 struct TBaseAstVisitor
     : TAbstractAstVisitor<TResult, TDerived, TExpressionPtr>
 {
-    TExpressionPtr GetExpression(TExpressionPtr expr)
-    {
-        return expr;
-    }
+    TExpressionPtr GetExpression(TExpressionPtr expr);
 };
 
 template <class TDerived>
@@ -118,77 +39,19 @@ struct TAstVisitor
     using TBase = TBaseAstVisitor<void, TDerived>;
     using TBase::Visit;
 
-    void OnLiteral(const TLiteralExpressionPtr /*literalExpr*/)
-    { }
-
-    void OnReference(const TReferenceExpressionPtr /*referenceExpr*/)
-    { }
-
-    void OnAlias(const TAliasExpressionPtr /*referenceExpr*/)
-    { }
-
-    void OnUnary(const TUnaryOpExpressionPtr unaryExpr)
-    {
-        Visit(unaryExpr->Operand);
-    }
-
-    void OnBinary(const TBinaryOpExpressionPtr binaryExpr)
-    {
-        Visit(binaryExpr->Lhs);
-        Visit(binaryExpr->Rhs);
-    }
-
-    void OnFunction(const TFunctionExpressionPtr functionExpr)
-    {
-        Visit(functionExpr->Arguments);
-    }
-
-    void OnIn(const TInExpressionPtr inExpr)
-    {
-        Visit(inExpr->Expr);
-    }
-
-    void OnBetween(const TBetweenExpressionPtr betweenExpr)
-    {
-        Visit(betweenExpr->Expr);
-    }
-
-    void OnTransform(const TTransformExpressionPtr transformExpr)
-    {
-        Visit(transformExpr->Expr);
-        Visit(transformExpr->DefaultExpr);
-    }
-
-    void OnCase(const TCaseExpressionPtr caseExpr)
-    {
-        Visit(caseExpr->OptionalOperand);
-        for (const auto& whenThenExpression : caseExpr->WhenThenExpressions) {
-            Visit(whenThenExpression.first);
-            Visit(whenThenExpression.second);
-        }
-        Visit(caseExpr->DefaultExpression);
-    }
-
-    void OnLike(const TLikeExpressionPtr likeExpr)
-    {
-        Visit(likeExpr->Text);
-        Visit(likeExpr->Pattern);
-        Visit(likeExpr->EscapeCharacter);
-    }
-
-    void Visit(const std::vector<TExpressionPtr>& tuple)
-    {
-        for (auto expr : tuple) {
-            Visit(expr);
-        }
-    }
-
-    void Visit(const std::optional<std::vector<TExpressionPtr>>& nullableTuple)
-    {
-        if (nullableTuple) {
-            return Visit(*nullableTuple);
-        }
-    }
+    void OnLiteral(const TLiteralExpressionPtr literalExpr);
+    void OnReference(const TReferenceExpressionPtr referenceExpr);
+    void OnAlias(const TAliasExpressionPtr aliasExpr);
+    void OnUnary(const TUnaryOpExpressionPtr unaryExpr);
+    void OnBinary(const TBinaryOpExpressionPtr binaryExpr);
+    void OnFunction(const TFunctionExpressionPtr functionExpr);
+    void OnIn(const TInExpressionPtr inExpr);
+    void OnBetween(const TBetweenExpressionPtr betweenExpr);
+    void OnTransform(const TTransformExpressionPtr transformExpr);
+    void OnCase(const TCaseExpressionPtr caseExpr);
+    void OnLike(const TLikeExpressionPtr likeExpr);
+    void Visit(const std::vector<TExpressionPtr>& tuple);
+    void Visit(const std::optional<std::vector<TExpressionPtr>>& nullableTuple);
 };
 
 template <class TDerived>
@@ -199,174 +62,22 @@ struct TRewriter
 
     TObjectsHolder* Head;
 
-    explicit TRewriter(TObjectsHolder* head)
-        : Head(head)
-    { }
+    explicit TRewriter(TObjectsHolder* head);
 
-    TExpressionPtr OnLiteral(TLiteralExpressionPtr literal)
-    {
-        return literal;
-    }
+    TExpressionPtr OnLiteral(TLiteralExpressionPtr literalExpr);
+    TExpressionPtr OnReference(TReferenceExpressionPtr referenceExpr);
+    TExpressionPtr OnAlias(TAliasExpressionPtr aliasExpr);
+    TExpressionPtr OnUnary(TUnaryOpExpressionPtr unaryExpr);
+    TExpressionPtr OnBinary(TBinaryOpExpressionPtr binaryExpr);
+    TExpressionPtr OnFunction(TFunctionExpressionPtr functionExpr);
+    TExpressionPtr OnIn(TInExpressionPtr inExpr);
+    TExpressionPtr OnBetween(TBetweenExpressionPtr betweenExpr);
+    TExpressionPtr OnTransform(TTransformExpressionPtr transformExpr);
+    TExpressionPtr OnCase(TCaseExpressionPtr caseExprExpr);
+    TExpressionPtr OnLike(TLikeExpressionPtr likeExpr);
 
-    TExpressionPtr OnReference(TReferenceExpressionPtr reference)
-    {
-        return reference;
-    }
-
-    TExpressionPtr OnAlias(TAliasExpressionPtr alias)
-    {
-
-        auto* newExpression = Visit(alias->Expression);
-        if (newExpression == alias->Expression) {
-            return alias;
-        } else {
-            return Head->New<TAliasExpression>(NullSourceLocation, newExpression, alias->Name);
-        }
-    }
-
-    TExpressionPtr OnUnary(TUnaryOpExpressionPtr unary)
-    {
-        auto newOperand = Visit(unary->Operand);
-        if (newOperand == unary->Operand) {
-            return unary;
-        } else {
-            return Head->New<TUnaryOpExpression>(NullSourceLocation, unary->Opcode, std::move(newOperand));
-        }
-    }
-
-    TExpressionPtr OnBinary(TBinaryOpExpressionPtr binary)
-    {
-        auto newLhs = Visit(binary->Lhs);
-        auto newRhs = Visit(binary->Rhs);
-
-        if (newLhs == binary->Lhs && newRhs == binary->Rhs) {
-            return binary;
-        } else {
-            return Head->New<TBinaryOpExpression>(
-                NullSourceLocation,
-                binary->Opcode,
-                std::move(newLhs),
-                std::move(newRhs));
-        }
-    }
-
-    TExpressionPtr OnFunction(TFunctionExpressionPtr function)
-    {
-        auto newArguments = Visit(function->Arguments);
-
-        if (newArguments == function->Arguments) {
-            return function;
-        } else {
-            return Head->New<TFunctionExpression>(
-                NullSourceLocation,
-                function->FunctionName,
-                std::move(newArguments));
-        }
-    }
-
-    TExpressionPtr OnIn(TInExpressionPtr in)
-    {
-        auto expression = Visit(in->Expr);
-
-        if (expression == in->Expr) {
-            return in;
-        } else {
-            return Head->New<TInExpression>(NullSourceLocation, std::move(expression), in->Values);
-        }
-    }
-
-    TExpressionPtr OnBetween(TBetweenExpressionPtr between)
-    {
-        auto expression = Visit(between->Expr);
-
-        if (expression == between->Expr) {
-            return between;
-        } else {
-            return Head->New<TBetweenExpression>(NullSourceLocation, std::move(expression), between->Values);
-        }
-    }
-
-    TExpressionPtr OnTransform(TTransformExpressionPtr transform)
-    {
-        auto expression = Visit(transform->Expr);
-        auto newDefault = Visit(transform->DefaultExpr);
-
-        if (newDefault == transform->DefaultExpr && expression == transform->Expr) {
-            return transform;
-        } else {
-            return Head->New<TTransformExpression>(
-                NullSourceLocation,
-                std::move(expression),
-                transform->From,
-                transform->To,
-                std::move(newDefault));
-        }
-    }
-
-    TExpressionPtr OnCase(TCaseExpressionPtr caseExpr)
-    {
-        auto newOptionalOperand = Visit(caseExpr->OptionalOperand);
-        auto newDefaultExpression = Visit(caseExpr->DefaultExpression);
-
-        int expressionCount = caseExpr->WhenThenExpressions.size();
-        std::vector<std::pair<TExpressionList, TExpressionList>> newWhenThenExpressions(expressionCount);
-        for (int index = 0; index < expressionCount; ++index) {
-            newWhenThenExpressions[index] = {
-                Visit(caseExpr->WhenThenExpressions[index].first),
-                Visit(caseExpr->WhenThenExpressions[index].second)
-            };
-        }
-
-        if (newWhenThenExpressions == caseExpr->WhenThenExpressions &&
-            newOptionalOperand == caseExpr->OptionalOperand &&
-            newDefaultExpression == caseExpr->DefaultExpression)
-        {
-            return caseExpr;
-        }
-
-        return Head->New<TCaseExpression>(
-            NullSourceLocation,
-            std::move(newOptionalOperand),
-            std::move(newWhenThenExpressions),
-            std::move(newDefaultExpression));
-    }
-
-    TExpressionPtr OnLike(TLikeExpressionPtr like)
-    {
-        auto newText = Visit(like->Text);
-        auto newPattern = Visit(like->Pattern);
-        auto newEscape = Visit(like->EscapeCharacter);
-
-        if (newText == like->Text && newPattern == like->Pattern && newEscape == like->EscapeCharacter) {
-            return like;
-        } else {
-            return Head->New<TLikeExpression>(
-                NullSourceLocation,
-                std::move(newText),
-                like->Opcode,
-                std::move(newPattern),
-                std::move(newEscape));
-        }
-    }
-
-    std::vector<TExpressionPtr> Visit(const std::vector<TExpressionPtr>& tuple)
-    {
-        int size = tuple.size();
-        std::vector<TExpressionPtr> newTuple(size);
-        for (int index = 0; index < size; ++index) {
-            newTuple[index] = Visit(tuple[index]);
-        }
-        return newTuple;
-    }
-
-    std::optional<std::vector<TExpressionPtr>> Visit(const std::optional<std::vector<TExpressionPtr>>& nullableTuple)
-    {
-        if (!nullableTuple) {
-            return std::nullopt;
-        } else {
-            return Visit(*nullableTuple);
-        }
-    }
+    std::vector<TExpressionPtr> Visit(const std::vector<TExpressionPtr>& tuple);
+    std::optional<std::vector<TExpressionPtr>> Visit(const std::optional<std::vector<TExpressionPtr>>& nullableTuple);
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -442,3 +153,7 @@ private:
 ////////////////////////////////////////////////////////////////////////////////
 
 } // namespace NYT::NQueryClient::NAst
+
+#define AST_VISITORS_INL_H_
+#include "ast_visitors-inl.h"
+#undef AST_VISITORS_INL_H_
