2aa2e59b07e1ebcb36c49b44c3dcfa7b5df27a46
robot-piglet <robot-piglet@yandex-team.com>
2024-08-05 18:00:42 +0300

    Intermediate changes

--- yt/go/mapreduce/io_other.go
+++
@@ -1,8 +0,0 @@
-//go:build !linux
-// +build !linux
-
-package mapreduce
-
-func (c *jobContext) initPipes(state *jobState, nOutputPipes int) error {
-	panic("this should never be called on windows")
-}
