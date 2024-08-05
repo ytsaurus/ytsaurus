11f7120e9efbc08bd56fa10a4859e8a90ebb6ca1
grigminakov <grigminakov@yandex-team.com>
2024-08-05 15:06:11 +0300

    YTORM-1111: Support  'fetch_root_object' option in history query 4954dba5ee9b47a9c45d0717f5f7fcb8f11ef6d2

--- yt/yt/orm/library/attributes/merge_attributes-inl.h
+++ yt/yt/orm/library/attributes/merge_attributes-inl.h
@@ -18,6 +18,11 @@ template <std::ranges::range TRange, class TPathProj, class TIsEtcProj>
 void ValidateSortedPaths(const TRange& paths, TPathProj pathProj, TIsEtcProj etcProj)
 {
     auto begin = paths.begin();
+
+    THROW_ERROR_EXCEPTION_IF(
+        begin != paths.end() && std::invoke(pathProj, *begin).empty() && !std::invoke(etcProj, *begin),
+        "Merging on empty path is supported for etc schemas only");
+
     while ((begin = std::ranges::adjacent_find(
         begin,
         paths.end(),
