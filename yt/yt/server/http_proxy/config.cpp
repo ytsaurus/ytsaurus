5df3881dcb41af5012ce3a983e9fce183e0fd6d5
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2024-08-14 02:58:09 +0300

    Try to keep proxies alive for a little longer when masters are in read-only mode 5a24d88ed4baaf432a834d672e5e8c6a2722d7f6

--- yt/yt/server/http_proxy/config.cpp
+++ yt/yt/server/http_proxy/config.cpp
@@ -45,8 +45,12 @@ void TCoordinatorConfig::Register(TRegistrar registrar)
         .Default(TDuration::Seconds(5));
     registrar.Parameter("death_age", &TThis::DeathAge)
         .Default(TDuration::Minutes(2));
+    registrar.Parameter("read_only_death_age", &TThis::ReadOnlyDeathAge)
+        .Default(TDuration::Minutes(20));
     registrar.Parameter("cypress_timeout", &TThis::CypressTimeout)
         .Default(TDuration::Seconds(30));
+    registrar.Parameter("orchid_timeout", &TThis::OrchidTimeout)
+        .Default(TDuration::Seconds(5));
 
     registrar.Parameter("show_ports", &TThis::ShowPorts)
         .Default(false);
