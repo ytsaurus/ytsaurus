c70d593f7277552b1d1806035229866184574234
Grigory Reznikov <gritukan@tracto.ai>
2024-08-16 09:16:07 +0300

    [24.1] Bind Infiniband devices to job container in CRI job environment
    No description
    
    ---
    Co-authored-by: Grigory Reznikov <gritukan@nebius.com>
    1c8c7e5df9c17c0ff8b9ce4570c48a126924d6c9
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/783

--- yt/yt/library/containers/cri/cri_executor.h
+++ yt/yt/library/containers/cri/cri_executor.h
@@ -61,6 +61,20 @@ struct TCriBindMount
     bool ReadOnly;
 };
 
+DEFINE_BIT_ENUM(ECriBindDevicePermissions,
+    ((Read)   (0x01))
+    ((Write)  (0x02))
+    ((Create) (0x04))
+);
+
+struct TCriBindDevice
+{
+    TString ContainerPath;
+    TString HostPath;
+
+    ECriBindDevicePermissions Permissions;
+};
+
 struct TCriCredentials
 {
     std::optional<i64> Uid;
@@ -81,6 +95,8 @@ struct TCriContainerSpec
 
     std::vector<TCriBindMount> BindMounts;
 
+    std::vector<TCriBindDevice> BindDevices;
+
     TCriCredentials Credentials;
 
     TCriContainerResources Resources;
@@ -96,6 +112,9 @@ struct TCriContainerSpec
 
     //! Environment variable to set in the container.
     THashMap<TString, TString> Environment;
+
+    //! Capabilities to add to the container.
+    std::vector<TString> CapabilitiesToAdd;
 };
 
 DEFINE_REFCOUNTED_TYPE(TCriContainerSpec)
