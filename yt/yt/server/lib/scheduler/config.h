2b22e5efb92e74a879aa96568f1712d80ce01b37
eshcherbin <eshcherbin@ytsaurus.tech>
2024-08-24 23:08:44 +0300

    YT-21939: Add job resource demand restrictions in tree d30c1f255907581d5b7a9c28ea078c472c56925a

--- yt/yt/server/lib/scheduler/config.h
+++ yt/yt/server/lib/scheduler/config.h
@@ -433,9 +433,10 @@ public:
 
     bool EnableGuaranteePriorityScheduling;
 
-    THashSet<EJobResourceType> NecessaryResourcesForOperation;
-
     bool EnableFastChildFunctionSummationInFifoPools;
+
+    TJobResourcesConfigPtr MinJobResourceLimits;
+    TJobResourcesConfigPtr MaxJobResourceLimits;
 
     REGISTER_YSON_STRUCT(TFairShareStrategyTreeConfig);
 
