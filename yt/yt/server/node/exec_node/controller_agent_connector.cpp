830e3a66f1990d52b4014cf1aa0ecd257c042934
eshcherbin <eshcherbin@ytsaurus.tech>
2024-08-26 17:14:50 +0300

    Rename trace contexts in scheduler and CA connector 944146aa37b28afd038ceb675fd57a8aec954048

--- yt/yt/server/node/exec_node/controller_agent_connector.cpp
+++ yt/yt/server/node/exec_node/controller_agent_connector.cpp
@@ -262,7 +262,7 @@ TError TControllerAgentConnectorPool::TControllerAgentConnector::DoSendHeartbeat
     TTraceContextPtr requestTraceContext;
 
     if (currentConfig->EnableTracing) {
-        requestTraceContext = TTraceContext::NewRoot("AgentHeartbeatRequest");
+        requestTraceContext = TTraceContext::NewRoot("ControllerAgentHeartbeat");
         requestTraceContext->SetRecorded();
         requestTraceContext->AddTag("node_id", ToString(nodeId));
 
