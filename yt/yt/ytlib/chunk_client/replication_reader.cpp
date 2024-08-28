9707a481a0bf5a62b8d4309b7d538d928da1fb1c
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2024-08-28 13:05:15 +0200

    Set requst multiplexing parallelism in chunk client

--- yt/yt/ytlib/chunk_client/replication_reader.cpp
+++ yt/yt/ytlib/chunk_client/replication_reader.cpp
@@ -2464,6 +2464,7 @@ private:
         auto req = proxy.GetBlockRange();
         req->SetResponseHeavy(true);
         req->SetMultiplexingBand(SessionOptions_.MultiplexingBand);
+        req->SetMultiplexingParallelism(SessionOptions_.MultiplexingParallelism);
         SetRequestWorkloadDescriptor(req, WorkloadDescriptor_);
         ToProto(req->mutable_chunk_id(), ChunkId_);
         req->set_first_block_index(FirstBlockIndex_);
@@ -2743,6 +2744,7 @@ private:
         auto req = proxy.GetChunkMeta();
         req->SetResponseHeavy(true);
         req->SetMultiplexingBand(SessionOptions_.MultiplexingBand);
+        req->SetMultiplexingParallelism(SessionOptions_.MultiplexingParallelism);
         SetRequestWorkloadDescriptor(req, WorkloadDescriptor_);
         req->set_enable_throttling(true);
         ToProto(req->mutable_chunk_id(), ChunkId_);
@@ -3572,6 +3574,7 @@ private:
         auto req = proxy.GetBlockSet();
         req->SetResponseHeavy(true);
         req->SetMultiplexingBand(queuedBatch.Session->SessionOptions_.MultiplexingBand);
+        req->SetMultiplexingParallelism(queuedBatch.Session->SessionOptions_.MultiplexingParallelism);
         SetRequestWorkloadDescriptor(req, queuedBatch.Session->SessionOptions_.WorkloadDescriptor);
         ToProto(req->mutable_chunk_id(), ChunkId_);
 
