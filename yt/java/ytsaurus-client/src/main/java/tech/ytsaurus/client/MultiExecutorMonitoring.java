43915f91a2650d12aef7e70a152ff2935c561c05
Georgii Semenov <georgii.semenov@vkteam.ru>
2024-08-29 19:36:12 +0300

    Add MultiExecutorMonitoring to monitor MultiExecutor progress
    For metrics and monitoring it is really important to follow hedging client execution, so it would be evident which clusters fail. I suggest the following API to extend current MultiYtClient functionality
    
    ---
    c32f3dd3085df8d824ae346a91a048019e5f33a5
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/808
    
    Co-authored-by: tinarsky <tinarsky@ytsaurus.tech>

---
+++ yt/java/ytsaurus-client/src/main/java/tech/ytsaurus/client/MultiExecutorMonitoring.java
@@ -0,0 +1,46 @@
+package tech.ytsaurus.client;
+
+import java.time.Duration;
+
+/**
+ * MultiExecutorMonitoring is executor callback to monitor hedging request execution.
+ */
+public interface MultiExecutorMonitoring {
+    /**
+     * Report successful request execution.
+     *
+     * @param clusterName cluster which was first to respond successfully
+     * @param time        that was spent to execute request
+     */
+    void reportRequestSuccess(String clusterName, Duration time);
+
+    /**
+     * Report failed request execution.
+     *
+     * @param time spent to poll all clusters and fail
+     */
+    void reportRequestFailure(Duration time);
+
+    /**
+     * Report that specific cluster has failed to process the request.
+     *
+     * @param clusterName cluster which has failed
+     * @param time        time within which exactly this one hedging request has failed
+     */
+    void reportRequestHedgingFailure(String clusterName, Duration time);
+}
+
+class NoopMultiExecutorMonitoring implements MultiExecutorMonitoring {
+
+    @Override
+    public void reportRequestSuccess(String clusterName, Duration time) {
+    }
+
+    @Override
+    public void reportRequestFailure(Duration time) {
+    }
+
+    @Override
+    public void reportRequestHedgingFailure(String clusterName, Duration time) {
+    }
+}
