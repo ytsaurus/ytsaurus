24f6504d21f000d5fa4a03fb5d522798fccf81f7
yurial <yurial@ytsaurus.tech>
2024-09-02 16:55:47 +0300

    Static check: TKV key is hashable if required 0f4c5bff08f5629814c939cacdcb373df2fb521a

--- yt/cpp/roren/interface/private/hash.h
+++ yt/cpp/roren/interface/private/hash.h
@@ -1,12 +1,20 @@
 #pragma once
 
+#include <util/str_stl.h>
+
+#include <type_traits>
+
 namespace NRoren::NPrivate {
+
+////////////////////////////////////////////////////////////////////////////////
 
 template <class T>
 struct TRorenHash
     : public std::hash<T>
 {
 };
+
+////////////////////////////////////////////////////////////////////////////////
 
 template <typename T, typename = std::void_t<>>
 struct IsHashable_ : std::false_type {};
@@ -15,5 +23,14 @@ struct IsHashable_<T, std::void_t<decltype(std::declval<TRorenHash<T>>()(std::de
 template <typename T>
 constexpr bool IsHashable = IsHashable_<T>::value;
 
-} // NRoren::NPrivate
+////////////////////////////////////////////////////////////////////////////////
+
+template <class T, class V>
+struct IsKeyHashRequired_ : std::false_type {};
+template <typename T>
+constexpr bool IsKeyHashRequired = IsKeyHashRequired_<T, void>::value;
+
+////////////////////////////////////////////////////////////////////////////////
+
+}  // NRoren::NPrivate
 
