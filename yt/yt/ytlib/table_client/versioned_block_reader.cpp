7723bf024c975aff9b9667a1ea5ba6c4f3350f46
whatsername <whatsername@yandex-team.com>
2024-09-04 02:14:56 +0300

    Support composite in block reader 699677909b96c80c3f25932e45a98c285a936e36

--- yt/yt/ytlib/table_client/versioned_block_reader.cpp
+++ yt/yt/ytlib/table_client/versioned_block_reader.cpp
@@ -201,11 +201,11 @@ void TSimpleVersionedBlockParser::ReadKeyValue(
 
         case EValueType::String:
         case EValueType::Any:
+        case EValueType::Composite:
             ReadStringLike(value, ptr);
             break;
 
         case EValueType::Null:
-        case EValueType::Composite:
         case EValueType::Min:
         case EValueType::Max:
         case EValueType::TheBottom:
