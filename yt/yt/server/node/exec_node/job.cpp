4b6aefa81c89bc6a4da969f00fa1f8543633e734
pogorelov <pogorelov@ytsaurus.tech>
2024-09-17 22:51:03 +0300

    YT-22836: Fix crash in supervisor service commit_hash:01d5db3e1be5cc7d0083a77adae6fbe65fbb626a

--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -1024,6 +1024,10 @@ NClusterNode::TJobResources TJob::GetResourceUsage() const
 {
     VERIFY_THREAD_AFFINITY(JobThread);
 
+    if (!ResourceHolder_) {
+        THROW_ERROR_EXCEPTION("Job has already released resources");
+    }
+
     return ResourceHolder_->GetResourceUsage();
 }
 
