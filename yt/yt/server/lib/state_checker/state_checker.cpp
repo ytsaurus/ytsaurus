56b28f0b85faaa7089986e00c5f83846a49d214e
Marina Preskokova <mpereskokova@ytsaurus.tech>
2024-09-19 11:56:39 +0300

    Codestyle

--- yt/yt/server/lib/state_checker/state_checker.cpp
+++ yt/yt/server/lib/state_checker/state_checker.cpp
@@ -7,20 +7,22 @@
 
 namespace NYT::NStateChecker {
 
-using namespace NLogging;
+using namespace NApi;
 using namespace NConcurrency;
+using namespace NLogging;
+using namespace NYson;
 using namespace NYTree;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TStateChecker::TStateChecker(IInvokerPtr invoker, NApi::IClientPtr nativeClient, TString instancePath, TDuration stateCheckPeriod)
+TStateChecker::TStateChecker(IInvokerPtr invoker, IClientPtr nativeClient, TYPath instancePath, TDuration stateCheckPeriod)
     : Logger(StateCheckerLogger())
     , Invoker_(std::move(invoker))
     , NativeClient_(std::move(nativeClient))
     , InstancePath_(std::move(instancePath))
 {
     Banned_.store(false);
-    StateCheckerExecutor_ = New<NConcurrency::TPeriodicExecutor>(
+    StateCheckerExecutor_ = New<TPeriodicExecutor>(
         Invoker_,
         BIND(&TStateChecker::DoCheckState, MakeStrong(this)),
         stateCheckPeriod);
@@ -36,7 +38,7 @@ void TStateChecker::SetPeriod(TDuration stateCheckPeriod)
     StateCheckerExecutor_->SetPeriod(stateCheckPeriod);
 }
 
-bool TStateChecker::IsComponentBanned()
+bool TStateChecker::IsComponentBanned() const
 {
     return Banned_.load();
 }
@@ -47,25 +49,37 @@ void TStateChecker::DoCheckState()
 {
     VERIFY_INVOKER_AFFINITY(Invoker_);
 
-    YT_LOG_DEBUG("Checking component state started");
+    YT_LOG_DEBUG("Started checking component state");
     auto logFinally = Finally([&] {
-        YT_LOG_DEBUG("Checking component state finished (Banned: %v)", Banned_.load());
+        YT_LOG_DEBUG("Finished checking component state (Banned: %v)", Banned_.load());
     });
 
-    auto options = NApi::TGetNodeOptions{
-        .Attributes = NYTree::TAttributeFilter({NApi::BannedAttributeName}),
+    auto options = TGetNodeOptions{
+        .Attributes = TAttributeFilter({BannedAttributeName}),
     };
 
     try {
         auto yson = WaitFor(NativeClient_->GetNode(InstancePath_, options))
             .ValueOrThrow();
-        auto instance = ConvertTo<NYTree::INodePtr>(yson);
+        auto instance = ConvertToNode(yson);
 
-        auto banned = instance->Attributes().Get<bool>(NApi::BannedAttributeName);
-        Banned_.store(banned);
+        auto banned = false;
+        if (instance->Attributes().Contains(BannedAttributeName)) {
+            banned = instance->Attributes().Get<bool>(BannedAttributeName);
+        }
+
+        auto previousBanned = Banned_.exchange(banned);
+        if (previousBanned != banned) {
+            auto orchidBannedPath = InstancePath_ + "/orchid/@banned";
+
+            YT_LOG_DEBUG("Updating orchid node (Path: %v, Banned: %v)", orchidBannedPath, banned);
+            WaitFor(NativeClient_->SetNode(orchidBannedPath, ConvertToYsonString(banned)))
+                .ThrowOnError();
+            YT_LOG_DEBUG("Orchid node updated");
+        }
 
     } catch (std::exception& ex) {
-        YT_LOG_ERROR(ex, "Checking component state failed");
+        YT_LOG_ERROR(ex, "Failed checking component state");
     }
 }
 
--- yt/yt/server/lib/state_checker/state_checker.h
+++ yt/yt/server/lib/state_checker/state_checker.h
@@ -10,20 +10,19 @@ class TStateChecker
     : public TRefCounted
 {
 public:
-    TStateChecker() = delete;
-    TStateChecker(IInvokerPtr invoker, NApi::IClientPtr nativeClient, TString instancePath, TDuration stateCheckPeriod);
+    TStateChecker(IInvokerPtr invoker, NApi::IClientPtr nativeClient, NYPath::TYPath instancePath, TDuration stateCheckPeriod);
 
     void Start();
     void SetPeriod(TDuration stateCheckPeriod);
 
-    bool IsComponentBanned();
+    bool IsComponentBanned() const;
 
 private:
     const NLogging::TLogger Logger;
 
     const IInvokerPtr Invoker_;
     const NApi::IClientPtr NativeClient_;
-    const TString InstancePath_;
+    const NYPath::TYPath InstancePath_;
 
     NConcurrency::TPeriodicExecutorPtr StateCheckerExecutor_;
     std::atomic<bool> Banned_ = false;
