95eb4402d939c5ec7e9f3342e56a941c4afc1113
sabdenovch <sabdenovch@ytsaurus.tech>
2024-09-26 22:26:53 +0300

    YT-22869: Fix detailed profiling in selects commit_hash:9b005dd4c1e28c175a950f0e50b363d766d88074

--- yt/yt/ytlib/api/native/client_dynamic_tables_impl.cpp
+++ yt/yt/ytlib/api/native/client_dynamic_tables_impl.cpp
@@ -543,7 +543,6 @@ private:
         auto mountCacheWaitTime = timer.GetElapsedTime();
 
         if (DetailedProfilingInfo_ && tableInfo->EnableDetailedProfiling) {
-            DetailedProfilingInfo_->EnableDetailedTableProfiling = true;
             DetailedProfilingInfo_->MountCacheWaitTime += mountCacheWaitTime;
         }
 
@@ -1538,7 +1537,10 @@ TSelectRowsResult TClient::DoSelectRowsOnce(
     auto* astQuery = &std::get<NAst::TQuery>(parsedQuery->AstHead.Ast);
 
     auto cache = New<TStickyTableMountInfoCache>(Connection_->GetTableMountCache());
+
+    NProfiling::TWallTimer timer;
     GetQueryTableInfos(astQuery, cache);
+    auto getMountInfoTime = timer.GetElapsedTime();
 
     TransformWithIndexStatement(&parsedQuery->AstHead, cache);
 
@@ -1706,7 +1708,7 @@ TSelectRowsResult TClient::DoSelectRowsOnce(
         });
     }
 
-    NProfiling::TWallTimer timer;
+    timer.Restart();
     const auto& permissionCache = Connection_->GetPermissionCache();
     auto permissionCheckErrors = WaitFor(permissionCache->GetMany(permissionKeys))
         .ValueOrThrow();
@@ -1719,15 +1721,10 @@ TSelectRowsResult TClient::DoSelectRowsOnce(
     auto permissionCacheWaitTime = timer.GetElapsedTime();
 
     if (options.DetailedProfilingInfo) {
-        const auto& path = astQuery->Table.Path;
-        timer.Restart();
-        auto tableInfo = WaitFor(cache->GetTableInfo(path))
-            .ValueOrThrow();
-        auto mountCacheWaitTime = timer.GetElapsedTime();
-        if (tableInfo->EnableDetailedProfiling) {
+        if (tableInfos[0]->EnableDetailedProfiling) {
             options.DetailedProfilingInfo->EnableDetailedTableProfiling = true;
-            options.DetailedProfilingInfo->TablePath = path;
-            options.DetailedProfilingInfo->MountCacheWaitTime += mountCacheWaitTime;
+            options.DetailedProfilingInfo->TablePath = tableInfos[0]->Path;
+            options.DetailedProfilingInfo->MountCacheWaitTime += getMountInfoTime;
             options.DetailedProfilingInfo->PermissionCacheWaitTime += permissionCacheWaitTime;
         }
     }
