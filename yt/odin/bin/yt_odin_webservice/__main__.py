e4f01dcdda158f0484d53721a98a6530b1470ec6
Dmitriy Fedorov <dmifedorov@tracto.ai>
2024-09-27 14:17:52 +0200

    Export odin check statuses in prometheus format

--- yt/odin/bin/yt_odin_webservice/__main__.py
+++ yt/odin/bin/yt_odin_webservice/__main__.py
@@ -26,6 +26,8 @@ import uuid
 import calendar
 import datetime
 import traceback
+from typing import Optional, Dict
+
 
 app = Flask("yt_odin_webservice")
 
@@ -135,6 +137,20 @@ def create_solomon_timed_out_sensors(records):
     return sensors
 
 
+def get_prometheus_metric_line(
+        metric_name: str, metric_value: float, timestamp: Optional[int], labels: Optional[Dict[str, str]] = None
+):
+    label_str = "{" + ",".join(f"{k}=\"{v}\"" for k, v in labels.items()) + "}" if labels else ""
+    result_parts = [
+        f"{metric_name}{label_str}",
+        str(metric_value),
+    ]
+    if timestamp is not None:
+        result_parts.append(f"{timestamp}000")
+    result_parts.append("\n")
+    return " ".join(result_parts)
+
+
 @app.route("/solomon")
 def solomon_duration():
     # NB: we assume `now` is a UTC datetime.
@@ -149,6 +165,31 @@ def solomon_duration():
     duration_sensors = create_solomon_duration_sensors(db_records)
     timed_out_sensors = create_solomon_timed_out_sensors(db_records)
     return jsonify({"sensors": duration_sensors + timed_out_sensors})
+
+
+@app.route("/prom")
+def prometheus():
+    now = datetime.datetime.now(datetime.timezone.utc)
+    period = datetime.timedelta(seconds=int(request.args.get("period", 60)))
+    start_timestamp = int(calendar.timegm((now - period).utctimetuple()))
+    stop_timestamp = int(calendar.timegm(now.utctimetuple()))
+    db_records = extract_all_records(start_timestamp, stop_timestamp)
+
+    last_records_per_service = {}
+    for record in db_records:
+        if (
+                record.service not in last_records_per_service
+                or last_records_per_service[record.service].timestamp < record.timestamp
+        ):
+            last_records_per_service[record.service] = record
+
+    return "".join(
+        get_prometheus_metric_line(
+            service, record.state, record.timestamp,
+            {"sensor": "state", "cluster": record.cluster},
+        )
+        for service, record in last_records_per_service.items()
+    )
 
 
 @app.route("/exists/<string:cluster>")
