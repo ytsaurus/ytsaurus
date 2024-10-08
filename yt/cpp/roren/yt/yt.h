6c664fc6df78ef005ab4786bacf37f9bffc397e0
ermolovd <ermolovd@ytsaurus.tech>
2024-10-08 16:47:03 +0300

    Docstring commit_hash:8bb59634efc6e96226c1a89038a29ead156e7f5b

--- yt/cpp/roren/yt/yt.h
+++ yt/cpp/roren/yt/yt.h
@@ -10,6 +10,14 @@ namespace NRoren {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+/// \@brief Create pipeline that is going to be executed over YT.
+///
+/// \@param cluster is YT cluster to run pipeline.
+/// \@param workingDir is a directory for storing temporary data.
+///    `//tmp` could be used, though it is not recommended for production processes:
+///    https://yt.yandex-team.ru/docs/user-guide/best-practice/howtorunproduction#zakazhite-neobhodimye-resursy
+/// \@param config
+
 TPipeline MakeYtPipeline(const TString& cluster, const TString& workingDir);
 TPipeline MakeYtPipeline(TYtPipelineConfig config);
 
