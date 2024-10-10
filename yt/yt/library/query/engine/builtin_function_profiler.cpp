47c9ad5dcc2e372cd5d99afc4ced51bca70c3f24
sabdenovch <sabdenovch@ytsaurus.tech>
2024-10-10 12:04:27 +0300

    YT-23057: Allow any/composite in argmin commit_hash:474c78c761dd0603c177208877b4c01c47d865c0

--- yt/yt/library/query/engine/builtin_function_profiler.cpp
+++ yt/yt/library/query/engine/builtin_function_profiler.cpp
@@ -1036,6 +1036,8 @@ private:
 
             for (int index = 0; index < std::ssize(args); ++index) {
                 switch (argTypes[index]) {
+                    case EValueType::Any:
+                    case EValueType::Composite:
                     case EValueType::String:
                         stateSize = builder->CreateAdd(stateSize, args[index].GetLength());
                         stateSize = builder->CreateAdd(stateSize, builder->getInt32(4));
@@ -1063,7 +1065,9 @@ private:
 
         for (int index = 0; index < std::ssize(args); ++index) {
             switch (argTypes[index]) {
-                case EValueType::String: {
+                case EValueType::Any:
+                case EValueType::Composite:
+                case EValueType::String:
                     builder->CreateStore(args[index].GetLength(), iterator);
                     iterator = builder->CreateGEP(builder->getInt8Ty(), iterator, builder->getInt64(4));
                     builder->CreateMemCpy(
@@ -1074,7 +1078,6 @@ private:
                         args[index].GetLength());
                     iterator = builder->CreateGEP(builder->getInt8Ty(), iterator, args[index].GetLength());
                     break;
-                }
                 case EValueType::Double:
                     builder->CreateStore(
                         builder->CreateZExt(args[index].GetTypedData(builder), builder->getDoubleTy()),
@@ -1113,6 +1116,8 @@ private:
 
         for (int index = 0; index < std::ssize(argTypes); ++index) {
             switch (argTypes[index]) {
+                case EValueType::Any:
+                case EValueType::Composite:
                 case EValueType::String: {
                     Value* length = builder->CreateLoad(builder->getInt32Ty(), iterator);
                     iterator = builder->CreateGEP(builder->getInt8Ty(), iterator, builder->getInt32(4));
@@ -1122,7 +1127,7 @@ private:
                             builder->getFalse(),
                             length,
                             iterator,
-                            EValueType::String));
+                            argTypes[index]));
                     iterator = builder->CreateGEP(builder->getInt8Ty(), iterator, length);
                     break;
                 }
