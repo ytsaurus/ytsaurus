3800566de49684e04f6efea610a4d8dcd81472c6
grigminakov <grigminakov@yandex-team.com>
2024-10-11 10:17:23 +0300

    YTORM: Support Attributes{Detecting|Removing}Consumer commit_hash:9264b7fe1b5eda5157673a0cd43ee8bb68784404

---
+++ yt/yt/orm/library/attributes/tests/unittests/attribute_processing_ut.cpp
@@ -0,0 +1,77 @@
+#include <yt/yt/orm/library/attributes/helpers.h>
+#include <yt/yt/orm/library/attributes/yson_builder.h>
+
+#include <yt/yt/core/ytree/fluent.h>
+
+#include <yt/yt/core/test_framework/framework.h>
+
+namespace NYT::NOrm::NAttributes::NTests {
+namespace {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TAttributesDetectingConsumerTest, JustWorks)
+{
+    bool attributesDetected = false;
+    auto consumer = CreateAttributesDetectingConsumer([&attributesDetected] () {
+        attributesDetected = true;
+    });
+
+    NYTree::BuildYsonFluently(consumer.get())
+        .BeginMap()
+        .EndMap();
+
+    ASSERT_FALSE(attributesDetected);
+
+    NYTree::BuildYsonFluently(consumer.get())
+        .BeginAttributes()
+        .EndAttributes()
+        .BeginMap()
+        .EndMap();
+
+    ASSERT_TRUE(attributesDetected);
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TEST(TAttributesRemovingConsumerTest, JustWorks)
+{
+    TYsonStringBuilder builder(NYson::EYsonFormat::Text);
+    auto consumer = CreateAttributesRemovingConsumer(builder.GetConsumer());
+
+    NYTree::BuildYsonFluently(consumer.get())
+        .BeginAttributes()
+            .Item("key").Entity()
+        .EndAttributes()
+        .Entity();
+
+    ASSERT_EQ(builder.Flush().AsStringBuf(), "#");
+}
+
+TEST(TAttributesRemovingConsumerTest, NestedAttributes)
+{
+    TYsonStringBuilder builder(NYson::EYsonFormat::Text);
+    auto consumer = CreateAttributesRemovingConsumer(builder.GetConsumer());
+
+    NYTree::BuildYsonFluently(consumer.get())
+        .BeginAttributes()
+            .Item("key")
+                .BeginAttributes()
+                    .Item("key").Entity()
+                .EndAttributes()
+                .Entity()
+        .EndAttributes()
+        .BeginMap()
+            .Item("key")
+                .BeginAttributes()
+                .EndAttributes()
+                .Value("value")
+        .EndMap();
+
+    ASSERT_EQ(builder.Flush().AsStringBuf(), "{\"key\"=\"value\";}");
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT::NOrm::NAttributes::NTests
