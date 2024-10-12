caf195dbc2df617cdcbdcefa916207bd62d207fc
ermolovd <ermolovd@ytsaurus.tech>
2024-10-12 21:58:27 +0300

    Better documentation for YT commit_hash:48de9b4f997db8772ffcaee8fa6b10ca9b1fc644

--- yt/cpp/roren/yt/yt.h
+++ yt/cpp/roren/yt/yt.h
@@ -1,26 +1,90 @@
 #pragma once
 
+/// @file yt.h
+///
+/// Main header for YT executor for the Roren library.
+///
+/// Contains functions to create YT pipeline and transforms to read/write YT tables.
+
 #include <yt/cpp/roren/interface/fwd.h>
 #include <yt/cpp/roren/yt/proto/config.pb.h>
 
-#include "yt_read.h"
+#include <yt/cpp/mapreduce/interface/common.h>
+
+#include "yt_io_private.h"
+#include "yt_proto_io.h"
+
 #include "yt_write.h"
 
 namespace NRoren {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-/// \@brief Create pipeline that is going to be executed over YT.
 ///
-/// \@param cluster is YT cluster to run pipeline.
-/// \@param workingDir is a directory for storing temporary data.
-///    `//tmp` could be used, though it is not recommended for production processes:
-///    https://yt.yandex-team.ru/docs/user-guide/best-practice/howtorunproduction#zakazhite-neobhodimye-resursy
-/// \@param config
+/// @brief Create pipeline that executes on YT.
+///
+/// @param cluster is YT cluster to run pipeline.
+/// @param workingDir is a directory for storing temporary data.
+///    `//tmp` could be used, though it is not recommended for production processes
+///    ([documentation](https://yt.yandex-team.ru/docs/user-guide/best-practice/howtorunproduction#zakazhite-neobhodimye-resursy))
 
 TPipeline MakeYtPipeline(const TString& cluster, const TString& workingDir);
+
+///
+/// @brief Create pipeline that executes on YT.
+///
+/// Similar to @ref NRoren::MakeYtPipeline(const TString&, const TString&) but provides additional options.
+///
+/// @param config proto config for running YT pipeline with additional options
 TPipeline MakeYtPipeline(TYtPipelineConfig config);
 
 ////////////////////////////////////////////////////////////////////////////////
+
+///
+/// @brief Read YT table
+///
+/// @param path path to YT table, all RichYPath attributes that can be used on operations (i.e. columns, row ranges)
+///     can be used here
+template <typename T>
+TReadTransform<T> YtRead(NYT::TRichYPath path)
+{
+    NPrivate::IRawYtReadPtr inner;
+    if constexpr (std::is_same_v<T, NYT::TNode>) {
+        inner = NPrivate::MakeYtNodeInput(std::move(path));
+    } else if constexpr (std::derived_from<T, ::google::protobuf::Message>) {
+        inner = NPrivate::MakeYtProtoRead<T>(std::move(path));
+    } else {
+        static_assert(TDependentFalse<T>, "Only NYT::TNode or protobuf messages can be used as elements");
+    }
+    return TReadTransform<T>{inner};
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+///
+/// @brief Write YT table
+///
+/// @param path path to YT table, all RichYPath attributes can can be used on operation output tables can be used here, except schema
+///
+/// @param schema schema of output table
+TYtWriteTransform YtWrite(const NYT::TRichYPath& path, const NYT::TTableSchema& schema);
+
+////////////////////////////////////////////////////////////////////////////////
+
+///
+/// @breif Write sorte YT table
+///
+/// PCollection will be saved to specified table and sort operation invoked if required
+TYtWriteTransform YtSortedWrite(
+    const NYT::TRichYPath& path,
+    const NYT::TTableSchema& schema,
+    const NYT::TSortColumns& columnsToSort);
+
+TYtWriteTransform YtSortedWrite(
+    const NYT::TRichYPath& path,
+    const NYT::TTableSchema& sortedSchema);
+
+////////////////////////////////////////////////////////////////////////////////
+
 
 } // namespace NRoren
--- yt/cpp/roren/yt/yt_read.h
+++ yt/cpp/roren/yt/yt_read.h
@@ -1,36 +1,5 @@
 #pragma once
 
-#include "yt_io_private.h"
-#include "yt_proto_io.h"
-
-#include <yt/cpp/roren/interface/transforms.h>
-#include <yt/cpp/roren/interface/private/serializable.h>
-#include <yt/cpp/roren/interface/private/raw_transform.h>
-#include <yt/cpp/roren/interface/private/row_vtable.h>
-
-#include <yt/cpp/mapreduce/interface/common.h>
-
-#include <util/stream/mem.h>
-#include <util/ysaveload.h>
-
-namespace NRoren {
-
-////////////////////////////////////////////////////////////////////////////////
-
-template <typename T>
-TReadTransform<T> YtRead(NYT::TRichYPath path)
-{
-    NPrivate::IRawYtReadPtr inner;
-    if constexpr (std::is_same_v<T, NYT::TNode>) {
-        inner = NPrivate::MakeYtNodeInput(std::move(path));
-    } else if constexpr (std::derived_from<T, ::google::protobuf::Message>) {
-        inner = NPrivate::MakeYtProtoRead<T>(std::move(path));
-    } else {
-        static_assert(TDependentFalse<T>, "unknown YT reader");
-    }
-    return TReadTransform<T>{inner};
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
-} // namespace NRoren
+// Backward compatibility.
+// Should be removed after fixing clients
+#include "yt.h"
