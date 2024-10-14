76abbbcef981810f01fc5be924741cd31d4b8652
sabdenovch <sabdenovch@ytsaurus.tech>
2024-10-14 19:00:23 +0300

    Fix stockpile dynamification commit_hash:1e2f9ce1517b704132b590ce616b3e768f2efdcb

--- yt/yt/library/program/config.cpp
+++ yt/yt/library/program/config.cpp
@@ -186,6 +186,8 @@ void TSingletonsDynamicConfig::Register(TRegistrar registrar)
         .DefaultNew();
     registrar.Parameter("tcmalloc", &TThis::TCMalloc)
         .Optional();
+    registrar.Parameter("stockpile", &TThis::Stockpile)
+        .Optional();
     registrar.Parameter("protobuf_interop", &TThis::ProtobufInterop)
         .DefaultNew();
 }
