e047f03f6468bd23d25540db358bb13d50f6b896
Nadezhda Savchenko <nadya73@ytsaurus.tech>
2024-10-14 09:31:00 +0300

    Don't build YQL python udfs in cache heater
    No description
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/876
    
    Co-authored-by: nadya73 <nadya73@ytsaurus.tech>
    commit_hash:eb6f3d640158ebd84864b57cad802a817a54c621

--- yt/docker/ya-build/query-tracker/build_yql_agent.sh
+++ yt/docker/ya-build/query-tracker/build_yql_agent.sh
@@ -11,6 +11,35 @@
 set -eux
 shopt -s expand_aliases
 
+build_python_udfs="no"
+
+print_usage() {
+    cat << EOF
+Usage: $script_name [-h|--help]
+                    [--build-python-udfs (default: $build_python_udfs)]
+EOF
+    exit 1
+}
+
+# Parse options
+while [[ $# -gt 0 ]]; do
+    key="$1"
+    case $key in
+        --build-python-udfs)
+        build_python_udfs="$2"
+        shift 2
+        ;;
+        -h|--help)
+        print_usage
+        shift
+        ;;
+        *)  # unknown option
+        echo "Unknown argument $1"
+        print_usage
+        ;;
+    esac
+done
+
 export YTSAURUS_SOURCE_PATH=$(realpath $YTSAURUS_SOURCE_PATH)
 export YDB_SOURCE_PATH=$(realpath $YDB_SOURCE_PATH)
 export YQL_BUILD_PATH=$(realpath $YQL_BUILD_PATH)
@@ -59,22 +88,24 @@ do
     strip --remove-section=.gnu_debuglink ${YDB_SOURCE_PATH}/ydb/library/yql/udfs/common/${udf_name}/*.so
 done
 
-# Build yql system python udfs inside a docker container.
-docker container run --rm --name yql-python-udfs-build \
-  -v $YTSAURUS_SOURCE_PATH:/ytsaurus \
-  -v $YDB_SOURCE_PATH:/ydb \
-  -v $YQL_BUILD_PATH:/yql_build \
-  --env YDB_SOURCE_PATH=/ydb \
-  --env YQL_BUILD_PATH=/yql_build \
-  --env "BUILD_FLAGS=$BUILD_FLAGS" \
-  mirror.gcr.io/ubuntu:focal \
-  /bin/bash -c \
-  "apt update && \
-   DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install -y software-properties-common build-essential && \
-   DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC add-apt-repository -y ppa:deadsnakes/ppa && \
-   apt update && \
-   DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install -y python3.8-dev python3.9-dev python3.10-dev python3.11-dev python3.12-dev && \
-   /ytsaurus/yt/docker/ya-build/query-tracker/build_system_python_udfs.sh"
+if [ "$build_python_udfs" == "yes" ]; then
+  # Build yql system python udfs inside a docker container.
+  docker container run --rm --name yql-python-udfs-build \
+    -v $YTSAURUS_SOURCE_PATH:/ytsaurus \
+    -v $YDB_SOURCE_PATH:/ydb \
+    -v $YQL_BUILD_PATH:/yql_build \
+    --env YDB_SOURCE_PATH=/ydb \
+    --env YQL_BUILD_PATH=/yql_build \
+    --env "BUILD_FLAGS=$BUILD_FLAGS" \
+    mirror.gcr.io/ubuntu:focal \
+    /bin/bash -c \
+    "apt update && \
+     DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install -y software-properties-common build-essential && \
+     DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC add-apt-repository -y ppa:deadsnakes/ppa && \
+     apt update && \
+     DEBIAN_FRONTEND=noninteractive TZ=Etc/UTC apt install -y python3.8-dev python3.9-dev python3.10-dev python3.11-dev python3.12-dev && \
+     /ytsaurus/yt/docker/ya-build/query-tracker/build_system_python_udfs.sh"
+fi
 
 # Change ownership of files to the current user. Useful if building locally.
 if [ $(id -u) -ne 0 ]; then
