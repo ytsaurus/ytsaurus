f7d25268fac4cfb47e723eb55d4c31ebe14f9f4b
osidorkin <osidorkin@yandex-team.com>
2024-10-17 14:49:15 +0300

    YT-23191: Add or skip entire collocation during PeriodicMigrateLeftovers commit_hash:c53d3ee982775f8e5102d86a752db94a8c58d1bf

---
+++ yt/yt/server/node/chaos_node/unittests/replication_card_batcher_ut.cpp
@@ -0,0 +1,111 @@
+#include <gtest/gtest.h>
+
+#include <yt/yt/server/node/chaos_node/replication_card.h>
+#include <yt/yt/server/node/chaos_node/replication_card_batcher.h>
+#include <yt/yt/server/node/chaos_node/replication_card_collocation.h>
+
+#include <yt/yt/client/chaos_client/helpers.h>
+
+#include <yt/yt/client/tablet_client/config.h>
+
+namespace NYT::NChaosNode {
+namespace {
+
+using namespace NChaosClient;
+using namespace NHydra;
+using namespace NObjectClient;
+using namespace ::testing;
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TReplcationCardBatcherTest
+    : public ::testing::Test
+{
+public:
+    TReplcationCardBatcherTest()
+    { }
+
+    static TReplicationCard* AddReplicationCard(
+        EReplicationCardState state,
+        TEntityMap<TReplicationCard>* replicationCardMap)
+    {
+        auto replicationCardId = MakeReplicationCardId(TObjectId::Create());
+        auto replicationCardHolder = std::make_unique<TReplicationCard>(replicationCardId);
+        replicationCardHolder->SetState(state);
+        return replicationCardMap->Insert(replicationCardId, std::move(replicationCardHolder));
+    }
+
+    static void AddReplicationCards(
+        int count,
+        EReplicationCardState state,
+        TEntityMap<TReplicationCard>* replicationCardMap)
+    {
+        for (int i = 0; i < count; ++i) {
+            AddReplicationCard(state, replicationCardMap);
+        }
+    }
+
+    static TReplicationCardCollocation* AddReplicationCardCollocation(
+        int count,
+        TEntityMap<TReplicationCardCollocation>* collocationMap,
+        TEntityMap<TReplicationCard>* replicationCardMap)
+    {
+        auto collocationId = MakeReplicationCardCollocationId(TObjectId::Create());
+        auto collocationHolder = std::make_unique<TReplicationCardCollocation>(collocationId);
+        auto* collocation = collocationMap->Insert(collocationId, std::move(collocationHolder));
+        collocation->SetSize(count);
+
+        for (int i = 0; i < count; ++i) {
+            auto* replicationCard = AddReplicationCard(EReplicationCardState::Normal, replicationCardMap);
+            collocation->ReplicationCards().insert(replicationCard);
+            replicationCard->SetCollocation(collocation);
+        }
+
+        return collocation;
+    }
+};
+
+TEST_F(TReplcationCardBatcherTest, TestNoCollocations)
+{
+    TEntityMap<TReplicationCard> replicationCardMap;
+    AddReplicationCards(10, EReplicationCardState::Normal, &replicationCardMap);
+    AddReplicationCards(10, EReplicationCardState::RevokingShortcutsForMigration, &replicationCardMap);
+
+    auto batch = BuildReadyToMigrateReplicationCardBatch(replicationCardMap, 5);
+    EXPECT_EQ(batch.size(), 5ull);
+    for (const auto& cardId : batch) {
+        EXPECT_EQ(replicationCardMap.Get(cardId)->GetState(), EReplicationCardState::Normal);
+    }
+
+    batch = BuildReadyToMigrateReplicationCardBatch(replicationCardMap, 15);
+    EXPECT_EQ(batch.size(), 10ull);
+    for (const auto& cardId : batch) {
+        EXPECT_EQ(replicationCardMap.Get(cardId)->GetState(), EReplicationCardState::Normal);
+    }
+}
+
+TEST_F(TReplcationCardBatcherTest, TestSmallCollocations)
+{
+    TEntityMap<TReplicationCard> replicationCardMap;
+    TEntityMap<TReplicationCardCollocation> collocationMap;
+    auto* collocation = AddReplicationCardCollocation(10, &collocationMap, &replicationCardMap);
+    auto* collocation2 = AddReplicationCardCollocation(10, &collocationMap, &replicationCardMap);
+
+
+    auto batch = BuildReadyToMigrateReplicationCardBatch(replicationCardMap, 5);
+    EXPECT_EQ(batch.size(), 10ull);
+
+    auto* card = *collocation->ReplicationCards().begin();
+    card->SetState(EReplicationCardState::RevokingShortcutsForMigration);
+    batch = BuildReadyToMigrateReplicationCardBatch(replicationCardMap, 15);
+    EXPECT_EQ(batch.size(), 10ull);
+
+    collocation2->SetState(EReplicationCardCollocationState::Emigrating);
+    batch = BuildReadyToMigrateReplicationCardBatch(replicationCardMap, 15);
+    EXPECT_TRUE(batch.empty());
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace
+} // namespace NYT::NDataNode
