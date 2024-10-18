6fe1f23775044e2a9d86652124d79a25a1d7d6f4
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2024-10-18 12:49:48 +0300

    Handle stop on ctrl-C in ytsaurus local docker image
    Stop on ctrl-c this:
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/893
    commit_hash:c724a76f527e285b08b0954c38b584c9d6974af5

--- yt/docker/local/start.sh
+++ yt/docker/local/start.sh
@@ -1,6 +1,6 @@
 #!/usr/bin/env bash
 
-yt_local start --proxy-port 80 \
+exec yt_local start --proxy-port 80 \
 --local-cypress-dir /var/lib/yt/local-cypress \
 --fqdn localhost \
 --ytserver-all-path /usr/bin/ytserver-all \
