272d8a1f1685fba466c9a3fa8f95e2fb7f1a962b
ermolovd <ermolovd@ytsaurus.tech>
2024-10-20 18:03:08 +0300

    Rewrite TYtSortedWriteTransform commit_hash:d1befa9d3488cbac9b9cc4c4834e8a6e12faeb0a

--- yt/cpp/roren/yt/transforms.h
+++ yt/cpp/roren/yt/transforms.h
@@ -13,28 +13,14 @@ namespace NRoren {
 ////////////////////////////////////////////////////////////////////////////////
 
 class TYtWriteTransform
-    : public NPrivate::IWithAttributes
+    : public NPrivate::TAttributes
 {
 public:
-    TYtWriteTransform(
-        const NYT::TRichYPath& path,
-        const NYT::TTableSchema& schema,
-        const NYT::TSortColumns& columnsToSort,
-        bool uniqueKeys)
-        : Path_(path)
-        , Schema_(schema)
-        , ColumnsToSort_(columnsToSort)
-        , UniqueKeys_(uniqueKeys)
-        , RawWrite_(MakeIntrusive<NPrivate::TRawDummyWriter>(NPrivate::MakeRowVtable<void>()))
-    { }
-
     TYtWriteTransform(
         const NYT::TRichYPath& path,
         const NYT::TTableSchema& schema)
         : Path_(path)
         , Schema_(schema)
-        , ColumnsToSort_(std::nullopt)
-        , RawWrite_(MakeIntrusive<NPrivate::TRawDummyWriter>(NPrivate::MakeRowVtable<void>()))
     { }
 
     TString GetName() const
@@ -45,25 +31,15 @@ public:
     template <typename TInputRow>
     void ApplyTo(const TPCollection<TInputRow>& pCollection) const
     {
-        RawWrite_ = ColumnsToSort_ ? CreateSortedWrite<TInputRow>() : CreateWrite<TInputRow>();
-
         const auto& rawPipeline = NPrivate::GetRawPipeline(pCollection);
         auto* rawInputNode = NPrivate::GetRawDataNode(pCollection).Get();
-        auto transformNode = rawPipeline->AddTransform(RawWrite_, {rawInputNode});
+        auto rawWrite = CreateWrite<TInputRow>();
+        NPrivate::MergeAttributes(*rawWrite, *this);
+        auto transformNode = rawPipeline->AddTransform(rawWrite, {rawInputNode});
         Y_ABORT_UNLESS(transformNode->GetTaggedSinkNodeList().size() == 0);
     }
 
 private:
-    void SetAttribute(const TString& key, const std::any& value) override
-    {
-        RawWrite_->SetAttribute(key, value);
-    }
-
-    const std::any* GetAttribute(const TString& key) const override
-    {
-        return RawWrite_->GetAttribute(key);
-    }
-
     template <class TInputRow>
     NPrivate::IRawYtWritePtr  CreateWrite() const
     {
@@ -76,25 +52,60 @@ private:
         }
     }
 
+private:
+    const NYT::TRichYPath Path_;
+    const NYT::TTableSchema Schema_;
+};
+
+class TYtSortedWriteTransform
+    : public NPrivate::TAttributes
+{
+public:
+    TYtSortedWriteTransform(
+        const NYT::TRichYPath& path,
+        const NYT::TTableSchema& schema,
+        const NYT::TSortColumns& columnsToSort,
+        bool uniqueKeys)
+        : Path_(path)
+        , Schema_(schema)
+        , ColumnsToSort_(columnsToSort)
+        , UniqueKeys_(uniqueKeys)
+    { }
+
+    TString GetName() const
+    {
+        return "Write";
+    }
+
+    template <typename TInputRow>
+    void ApplyTo(const TPCollection<TInputRow>& pCollection) const
+    {
+        const auto& rawPipeline = NPrivate::GetRawPipeline(pCollection);
+        auto* rawInputNode = NPrivate::GetRawDataNode(pCollection).Get();
+        auto rawWrite = CreateSortedWrite<TInputRow>();
+        NPrivate::MergeAttributes(*rawWrite, *this);
+        auto transformNode = rawPipeline->AddTransform(rawWrite, {rawInputNode});
+        Y_ABORT_UNLESS(transformNode->GetTaggedSinkNodeList().size() == 0);
+    }
+
+private:
     template <class TInputRow>
     NPrivate::IRawYtWritePtr  CreateSortedWrite() const
     {
         if constexpr (std::is_same_v<TInputRow, NYT::TNode>) {
-            return NPrivate::MakeYtNodeSortedWrite(Path_, Schema_, *ColumnsToSort_, UniqueKeys_);
+            return NPrivate::MakeYtNodeSortedWrite(Path_, Schema_, ColumnsToSort_, UniqueKeys_);
         } else if constexpr (std::is_base_of_v<::google::protobuf::Message, TInputRow>) {
-            return NPrivate::MakeYtProtoSortedWrite<TInputRow>(Path_, Schema_, *ColumnsToSort_, UniqueKeys_);
+            return NPrivate::MakeYtProtoSortedWrite<TInputRow>(Path_, Schema_, ColumnsToSort_, UniqueKeys_);
         } else {
             static_assert(TDependentFalse<TInputRow>, "unknown YT writer");
         }
     }
 
 private:
-    NYT::TRichYPath Path_;
-    NYT::TTableSchema Schema_;
-    std::optional<NYT::TSortColumns> ColumnsToSort_;
-    bool UniqueKeys_;
-
-    mutable NPrivate::IRawWritePtr RawWrite_;
+    const NYT::TRichYPath Path_;
+    const NYT::TTableSchema Schema_;
+    const NYT::TSortColumns ColumnsToSort_;
+    const bool UniqueKeys_;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
