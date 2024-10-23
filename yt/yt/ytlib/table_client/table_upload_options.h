76c8ef23198921f79b3be0339607c60acdee66bf
babenko <babenko@ytsaurus.tech>
2024-10-24 00:08:45 +0300

    Avoid uninitialized fields in TTableUploadOptions commit_hash:d997bc06896dc31e26fb8b5c32900defdb6f975f

--- yt/yt/ytlib/table_client/table_upload_options.h
+++ yt/yt/ytlib/table_client/table_upload_options.h
@@ -47,19 +47,19 @@ private:
 
 struct TTableUploadOptions
 {
-    NChunkClient::EUpdateMode UpdateMode;
-    NCypressClient::ELockMode LockMode;
+    NChunkClient::EUpdateMode UpdateMode = NChunkClient::EUpdateMode::Overwrite;
+    NCypressClient::ELockMode LockMode = NCypressClient::ELockMode::Exclusive;
     TEpochSchema TableSchema;
     TMasterTableSchemaId SchemaId;
     ETableSchemaModification SchemaModification;
-    ETableSchemaMode SchemaMode;
-    EOptimizeFor OptimizeFor;
+    ETableSchemaMode SchemaMode = ETableSchemaMode::Strong;
+    EOptimizeFor OptimizeFor = EOptimizeFor::Lookup;
     std::optional<NChunkClient::EChunkFormat> ChunkFormat;
-    NCompression::ECodec CompressionCodec;
-    NErasure::ECodec ErasureCodec;
-    bool EnableStripedErasure;
+    NCompression::ECodec CompressionCodec = NCompression::ECodec::None;
+    NErasure::ECodec ErasureCodec = NErasure::ECodec::None;
+    bool EnableStripedErasure = false;
     std::optional<std::vector<NSecurityClient::TSecurityTag>> SecurityTags;
-    bool PartiallySorted;
+    bool PartiallySorted = false;
 
     TTableSchemaPtr GetUploadSchema() const;
 
