b70682322685540abba9c12bdb444a6ceca936f4
dgolear <dgolear@ytsaurus.tech>
2024-10-24 17:10:57 +0300

    YTORM: Add unittests for attribute matching commit_hash:9211d1b6018578cc59b6ee54abcd8d1878a8bb31

--- yt/yt/orm/library/attributes/tests/unittests/attribute_path_ut.cpp
+++ yt/yt/orm/library/attributes/tests/unittests/attribute_path_ut.cpp
@@ -7,7 +7,7 @@ namespace {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TEST(TAttributePathTest, IsAttributesRelated)
+TEST(TAttributePathTest, AreAttributesRelated)
 {
     EXPECT_TRUE(AreAttributesRelated("", "/a/b"));
     EXPECT_TRUE(AreAttributesRelated("/a", "/a/b"));
@@ -17,6 +17,24 @@ TEST(TAttributePathTest, IsAttributesRelated)
     EXPECT_FALSE(AreAttributesRelated("/b", "/a/b"));
     EXPECT_FALSE(AreAttributesRelated("/b/a", "/b/b"));
     EXPECT_FALSE(AreAttributesRelated("/bb", "/b/b"));
+}
+
+TEST(TAttributePathTest, MatchAttributePathToPattern)
+{
+    EXPECT_EQ(EAttributePathMatchResult::None, MatchAttributePathToPattern("/spec", "/status"));
+    EXPECT_EQ(EAttributePathMatchResult::None, MatchAttributePathToPattern("/status", "/spec"));
+    EXPECT_EQ(EAttributePathMatchResult::None, MatchAttributePathToPattern("/status", "/spec"));
+
+    EXPECT_EQ(EAttributePathMatchResult::Full, MatchAttributePathToPattern("/status", "/status"));
+    EXPECT_EQ(EAttributePathMatchResult::Full, MatchAttributePathToPattern("/spec/*", "/spec/foo"));
+    EXPECT_EQ(EAttributePathMatchResult::Full, MatchAttributePathToPattern("/spec/*", "/spec/0"));
+
+    EXPECT_EQ(EAttributePathMatchResult::PatternIsPrefix, MatchAttributePathToPattern("/spec/*", "/spec/foo/bar"));
+    EXPECT_EQ(EAttributePathMatchResult::PatternIsPrefix, MatchAttributePathToPattern("/spec/foo", "/spec/foo/bar"));
+
+    EXPECT_EQ(EAttributePathMatchResult::PathIsPrefix, MatchAttributePathToPattern("/spec/*", "/spec"));
+    EXPECT_EQ(EAttributePathMatchResult::PathIsPrefix, MatchAttributePathToPattern("/spec/foo", "/spec"));
+    EXPECT_EQ(EAttributePathMatchResult::PathIsPrefix, MatchAttributePathToPattern("/spec/foo/*", "/spec"));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
