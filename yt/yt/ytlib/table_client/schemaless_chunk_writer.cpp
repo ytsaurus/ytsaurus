915b757bf766a6ea389f1e9b3af9bdbf35751234
apollo1321 <apollo1321@yandex-team.com>
2024-10-29 14:55:48 +0300

    Fix include order: schemaless_chunk_writer.cpp
    Fix include order using `yt-include-order` clang-tidy check: <HIDDEN_URL>
    commit_hash:3558fbe26b0468189e5b7414b147da230a2304bb

--- yt/yt/ytlib/table_client/schemaless_chunk_writer.cpp
+++ yt/yt/ytlib/table_client/schemaless_chunk_writer.cpp
@@ -2,104 +2,89 @@
 
 #include "chunk_meta_extensions.h"
 #include "config.h"
-#include "partitioner.h"
-#include "schema.h"
-#include "schemaless_block_writer.h"
-#include "table_ypath_proxy.h"
 #include "helpers.h"
+#include "partitioner.h"
+#include "schemaless_block_writer.h"
 #include "skynet_column_evaluator.h"
+#include "table_ypath_proxy.h"
 #include "timestamped_schema_helpers.h"
 #include "versioned_chunk_writer.h"
 
 #include <yt/yt/ytlib/table_chunk_format/column_writer.h>
-#include <yt/yt/ytlib/table_chunk_format/schemaless_column_writer.h>
 #include <yt/yt/ytlib/table_chunk_format/data_block_writer.h>
+#include <yt/yt/ytlib/table_chunk_format/schemaless_column_writer.h>
 
 #include <yt/yt/ytlib/chunk_client/block_cache.h>
 #include <yt/yt/ytlib/chunk_client/chunk_writer.h>
 #include <yt/yt/ytlib/chunk_client/deferred_chunk_meta.h>
 #include <yt/yt/ytlib/chunk_client/dispatcher.h>
 #include <yt/yt/ytlib/chunk_client/encoding_chunk_writer.h>
-#include <yt/yt/ytlib/chunk_client/multi_chunk_writer_base.h>
 #include <yt/yt/ytlib/chunk_client/helpers.h>
+#include <yt/yt/ytlib/chunk_client/multi_chunk_writer_base.h>
 
 #include <yt/yt/ytlib/cypress_client/cypress_ypath_proxy.h>
 #include <yt/yt/ytlib/cypress_client/rpc_helpers.h>
 
+#include <yt/yt/ytlib/object_client/helpers.h>
 #include <yt/yt/ytlib/object_client/object_service_proxy.h>
 
-#include <yt/yt/ytlib/transaction_client/helpers.h>
 #include <yt/yt/ytlib/transaction_client/transaction_listener.h>
-#include <yt/yt/ytlib/transaction_client/config.h>
 
 #include <yt/yt/ytlib/table_client/table_upload_options.h>
 
-#include <yt/yt/library/query/engine_api/column_evaluator.h>
-
-#include <yt/yt/ytlib/object_client/helpers.h>
-
-#include <yt/yt/ytlib/api/native/connection.h>
 #include <yt/yt/ytlib/api/native/client.h>
 #include <yt/yt/ytlib/api/native/config.h>
+#include <yt/yt/ytlib/api/native/connection.h>
+
+#include <yt/yt/library/query/engine_api/column_evaluator.h>
 
 #include <yt/yt/client/object_client/helpers.h>
 
-#include <yt/yt/client/misc/io_tags.h>
-
 #include <yt/yt/client/transaction_client/timestamp_provider.h>
 
+#include <yt/yt/client/table_client/check_schema_compatibility.h>
 #include <yt/yt/client/table_client/helpers.h>
 #include <yt/yt/client/table_client/logical_type.h>
 #include <yt/yt/client/table_client/name_table.h>
 #include <yt/yt/client/table_client/row_buffer.h>
-#include <yt/yt/client/table_client/schemaless_row_reorderer.h>
-#include <yt/yt/client/table_client/check_schema_compatibility.h>
-#include <yt/yt/client/table_client/composite_compare.h>
 
-#include <yt/yt/client/api/client.h>
-#include <yt/yt/client/api/transaction.h>
-#include <yt/yt/client/api/config.h>
 #include <yt/yt/client/api/distributed_table_session.h>
+#include <yt/yt/client/api/transaction.h>
 
 #include <yt/yt/client/ypath/rich.h>
 
-#include <yt/yt/core/concurrency/scheduler.h>
-
 #include <yt/yt/core/misc/random.h>
-
-#include <yt/yt/core/ytree/helpers.h>
 
 #include <util/generic/cast.h>
 #include <util/generic/ylimits.h>
-#include <util/generic/buffer.h>
 
 #include <utility>
 
 namespace NYT::NTableClient {
 
-using namespace NChunkClient;
-using namespace NChunkClient::NProto;
-using namespace NConcurrency;
-using namespace NCypressClient;
-using namespace NObjectClient;
-using namespace NTableClient::NProto;
-using namespace NTableChunkFormat;
-using namespace NTabletClient;
-using namespace NRpc;
 using namespace NApi;
-using namespace NTransactionClient;
+using namespace NApi;
+using namespace NChunkClient::NProto;
+using namespace NChunkClient;
+using namespace NConcurrency;
+using namespace NCrypto;
+using namespace NCypressClient;
 using namespace NNodeTrackerClient;
+using namespace NObjectClient;
+using namespace NQueryClient;
+using namespace NRpc;
+using namespace NTableChunkFormat;
+using namespace NTableClient::NProto;
+using namespace NTabletClient;
+using namespace NTracing;
+using namespace NTransactionClient;
 using namespace NYPath;
 using namespace NYTree;
 using namespace NYson;
-using namespace NApi;
-using namespace NQueryClient;
-using namespace NTracing;
-using namespace NCrypto;
 
-using NYT::ToProto;
 using NYT::FromProto;
 using NYT::TRange;
+using NYT::ToProto;
 
 static const i64 PartitionRowCountThreshold = 1000 * 1000;
 static const i64 PartitionRowCountLimit = std::numeric_limits<i32>::max() - PartitionRowCountThreshold;
