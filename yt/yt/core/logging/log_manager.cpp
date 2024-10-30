03999d920c94fba02733345cbc16317c6e8ea945
babenko <babenko@ytsaurus.tech>
2024-10-30 15:11:17 +0300

    Fix regression: static anchors are not updated properly commit_hash:b95315cbb0cd8f83561f54793265029a219314a4

--- yt/yt/core/logging/log_manager.cpp
+++ yt/yt/core/logging/log_manager.cpp
@@ -493,10 +493,6 @@ public:
         ::TSourceLocation sourceLocation,
         TStringBuf message)
     {
-        if (anchor->Registered.exchange(true)) {
-            return;
-        }
-
         auto guard = Guard(SpinLock_);
         auto config = Config_.Acquire();
         anchor->SourceLocation = sourceLocation;
@@ -511,12 +507,13 @@ public:
         if (auto it = AnchorMap_.find(anchorMessage)) {
             return it->second;
         }
+        auto config = Config_.Acquire();
         auto anchor = std::make_unique<TLoggingAnchor>();
-        anchor->Registered = true;
         anchor->AnchorMessage = std::move(anchorMessage);
         auto* rawAnchor = anchor.get();
         DynamicAnchors_.push_back(std::move(anchor));
         DoRegisterAnchor(rawAnchor);
+        DoUpdateAnchor(config, rawAnchor);
         return rawAnchor;
     }
 
