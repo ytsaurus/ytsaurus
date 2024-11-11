3e2983efc77e1956648b6ac7255858da909501b6
dakovalkov <dakovalkov@ytsaurus.tech>
2024-11-11 23:51:24 +0300

    CHYT-1218: Keep port during http redirect properly (Cherry pick commit rXXXXXX) commit_hash:f2a975188f49fc35660572d8b8918c3a2128c07b

--- yt/yt/server/http_proxy/helpers.cpp
+++ yt/yt/server/http_proxy/helpers.cpp
@@ -366,12 +366,11 @@ void RedirectToDataProxy(const IRequestPtr& request, const IResponseWriterPtr& r
         } else {
             protocol = "http";
         }
-        auto port = url.Port ? Format(":%v", *url.Port) : "";
 
-        auto location = Format("%v://%v%v%v?%v",
+        auto location = Format("%v://%v:%v%v?%v",
             protocol,
             target->GetHost(),
-            port,
+            request->GetPort(),
             url.Path,
             url.RawQuery);
 
