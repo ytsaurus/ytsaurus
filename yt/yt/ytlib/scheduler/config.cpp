92a76992b9bb3ca0421ccf7e5f11f93999996f14
arkady-e1ppa <arkady-e1ppa@ytsaurus.tech>
2024-11-18 18:19:56 +0300

    babenko-issues of rXXXXXX, rXXXXXX commit_hash:c269b25b6fd905ac1729c4bb42d2cdb2759fa990

--- yt/yt/ytlib/scheduler/config.cpp
+++ yt/yt/ytlib/scheduler/config.cpp
@@ -194,12 +194,12 @@ void TJobIOConfig::Register(TRegistrar registrar)
         .DefaultNew();
 
     registrar.Parameter("buffer_row_count", &TThis::BufferRowCount)
-        .Default(10 * 1000)
+        .Default(10'000)
         .GreaterThan(0);
     registrar.Parameter("use_adaptive_buffer_row_count", &TThis::UseAdaptiveRowCount)
         .Default();
     registrar.Parameter("adaptive_row_count_upper_bound", &TThis::AdaptiveRowCountUpperBound)
-        .Default(40 * 1000)
+        .Default(40'000)
         .GreaterThan(0);
 
     registrar.Parameter("pipe_capacity", &TThis::PipeCapacity)
