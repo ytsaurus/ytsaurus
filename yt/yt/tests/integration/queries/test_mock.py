b16f852d414b125f4d955a546788e0367011875b
Marina Preskokova <mpereskokova@ytsaurus.tech>
2024-11-21 14:59:08 +0300

    Fix queries finishing
    Now, if qt crashes before completing the query, the query will remain incomplete forever.
    
    This error occurred after introducing strong previous state conditions in state changing.
    https://github.com/ytsaurus/ytsaurus/blame/main/yt/yt/server/query_tracker/handler_base.cpp#L342
    
    Fixing it here
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/919
    commit_hash:a4fd944d042b050620d20968ef1a47d2ebac2130

--- yt/yt/tests/integration/queries/test_mock.py
+++ yt/yt/tests/integration/queries/test_mock.py
@@ -246,6 +246,72 @@ class TestQueryTrackerBan(YTEnvSetup):
         wait(lambda: query.get_state() == "running", ignore_exceptions=True)
 
 
+class TestQueryTrackerQueryRestart(YTEnvSetup):
+    DELTA_DRIVER_CONFIG = {
+        "cluster_connection_dynamic_config_policy": "from_cluster_directory",
+    }
+
+    def _insert_query(self, state, query="run_forever", settings={}, error="", is_abort=False):
+        guid = generate_uuid()
+        query = {
+            "query_id": guid,
+            "engine": "mock",
+            "user": "root",
+            "query": query,
+            "incarnation": 0,
+            "start_time": 0,
+            "progress": {},
+            "annotations": {},
+            "state": state,
+            "settings": settings
+        }
+        if is_abort:
+            query["abort_request"] = {"attributes": {}, "code": 100, "message": error}
+        else:
+            query["error"] = {"attributes": {}, "code": 100, "message": error}
+
+        insert_rows("//sys/query_tracker/active_queries", [query])
+        return guid
+
+    @authors("mpereskokova")
+    def test_query_tracker_aborting_query(self, query_tracker):
+        error = "test_abort"
+        guid = self._insert_query("aborting", error, is_abort=True)
+
+        query = Query(guid)
+        with raises_yt_error("aborted"):
+            query.track()
+
+    @authors("mpereskokova")
+    def test_query_tracker_failing_query(self, query_tracker):
+        guid = self._insert_query("failing")
+
+        query = Query(guid)
+        with raises_yt_error("failed"):
+            query.track()
+
+    @authors("mpereskokova")
+    def test_query_tracker_completing_query(self, query_tracker):
+        guid = self._insert_query("completing")
+
+        query = Query(guid)
+        query.track()
+
+    @authors("mpereskokova")
+    def test_query_tracker_running_query(self, query_tracker):
+        guid = self._insert_query("running", query="complete_after", settings={"duration": 100})
+
+        query = Query(guid)
+        query.track()
+
+    @authors("mpereskokova")
+    def test_query_tracker_pending_query(self, query_tracker):
+        guid = self._insert_query("pending", query="complete_after", settings={"duration": 100})
+
+        query = Query(guid)
+        query.track()
+
+
 class TestAccessControl(YTEnvSetup):
     NUM_TEST_PARTITIONS = 16
 
