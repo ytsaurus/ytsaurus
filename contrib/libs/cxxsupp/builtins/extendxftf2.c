b2d14eb14acb39717a265581220ac839b23d8f4c
mikhnenko <mikhnenko@yandex-team.com>
2024-11-24 13:46:42 +0300

    Update  compiler_rt to 18.1.8 commit_hash:047370d533b6571fd08cc42584b9f72ca1f8d98d

--- contrib/libs/cxxsupp/builtins/extendxftf2.c
+++ contrib/libs/cxxsupp/builtins/extendxftf2.c
@@ -1,4 +1,4 @@
-//===-- lib/extendsftf2.c - single -> quad conversion -------------*- C -*-===//
+//===-- lib/extendxftf2.c - long double -> quad conversion --------*- C -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +6,19 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Assumption: long double is a IEEE 80 bit floating point type padded to 128
+// bits.
+
 #define QUAD_PRECISION
 #include "fp_lib.h"
 
-#if defined(CRT_HAS_TF_MODE)
-#define SRC_SINGLE
+#if defined(CRT_HAS_TF_MODE) && __LDBL_MANT_DIG__ == 64 && defined(__x86_64__)
+#define SRC_80
 #define DST_QUAD
 #include "fp_extend_impl.inc"
 
-COMPILER_RT_ABI dst_t __extendsftf2(src_t a) { return __extendXfYf2__(a); }
+COMPILER_RT_ABI tf_float __extendxftf2(xf_float a) {
+  return __extendXfYf2__(a);
+}
 
 #endif
--- contrib/libs/cxxsupp/builtins/trunctfxf2.c
+++ contrib/libs/cxxsupp/builtins/trunctfxf2.c
@@ -1,4 +1,4 @@
-//===-- lib/truncdfsf2.c - quad -> double conversion --------------*- C -*-===//
+//===-- lib/trunctfsf2.c - long double -> quad conversion ---------*- C -*-===//
 //
 // Part of the LLVM Project, under the Apache License v2.0 with LLVM Exceptions.
 // See https://llvm.org/LICENSE.txt for license information.
@@ -6,14 +6,18 @@
 //
 //===----------------------------------------------------------------------===//
 
+// Assumption: long double is a IEEE 80 bit floating point type padded to 128
+// bits.
+
 #define QUAD_PRECISION
 #include "fp_lib.h"
 
-#if defined(CRT_HAS_TF_MODE)
+#if defined(CRT_HAS_TF_MODE) && __LDBL_MANT_DIG__ == 64 && defined(__x86_64__)
+
 #define SRC_QUAD
-#define DST_DOUBLE
+#define DST_80
 #include "fp_trunc_impl.inc"
 
-COMPILER_RT_ABI dst_t __trunctfdf2(src_t a) { return __truncXfYf2__(a); }
+COMPILER_RT_ABI xf_float __trunctfxf2(tf_float a) { return __truncXfYf2__(a); }
 
 #endif
