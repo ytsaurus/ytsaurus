d01163b0c5335b16877ee0deb08f1ec78919ac03
don-dron <don-dron@ytsaurus.tech>
2024-11-26 17:18:26 +0300

    YT-23635: Add bytes per write to dyn. config commit_hash:2b105df0d786dae25a9f20a8d99dddec874a816a

--- yt/yt/server/node/data_node/config.h
+++ yt/yt/server/node/data_node/config.h
@@ -1083,6 +1083,9 @@ public:
 
     TP2PConfigPtr P2P;
 
+    //! Desired number of bytes per disk write in a blob chunks.
+    std::optional<i64> BytesPerWrite;
+
     TDuration IOStatisticsUpdateTimeout;
 
     std::optional<double> ReadMetaTimeoutFraction;
