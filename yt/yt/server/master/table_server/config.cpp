9ff09416a1d15b2d3e5efe2f7d9cabc295917f68
kvk1920 <kvk1920@ytsaurus.tech>
2024-12-09 16:15:07 +0300

    Optimize alter-table verb commit_hash:dbdf35744550a5a2b4a9b94f9b325b60f1dde718

--- yt/yt/server/master/table_server/config.cpp
+++ yt/yt/server/master/table_server/config.cpp
@@ -4,6 +4,14 @@ namespace NYT::NTableServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+void TDynamicTableManagerConfig::Register(TRegistrar registrar)
+{
+    registrar.Parameter("max_schema_memory_usage_to_log", &TThis::MaxSchemaMemoryUsageToLog)
+        .GreaterThan(0)
+        .Default(8_KB)
+        .DontSerializeDefault();
+}
+
 void TTableManagerConfig::Register(TRegistrar /*registrar*/)
 { }
 
