831a65b188b33bb4550dd8226695304916657527
Ignat Kolesnichenko <ignat@yandex-team.ru>
2024-12-09 14:12:29 +0000

    Update manylinux distribution

--- yt/python/packages/build_python_libraries_with_docker.sh
+++ yt/python/packages/build_python_libraries_with_docker.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -eu
 
-CONTAINER_ID=$(docker container run --name yson-bindings-build --entrypoint /bin/bash -v $SOURCE_ROOT:/ytsaurus -v $BUILD_PATH:/build -dit quay.io/pypa/manylinux2014_x86_64)
+CONTAINER_ID=$(docker container run --name yson-bindings-build --entrypoint /bin/bash -v $SOURCE_ROOT:/ytsaurus -v $BUILD_PATH:/build -dit quay.io/pypa/manylinux_2_28_x86_64)
 
 docker exec $CONTAINER_ID /ytsaurus/yt/python/packages/build_manylinux_bindings.sh
