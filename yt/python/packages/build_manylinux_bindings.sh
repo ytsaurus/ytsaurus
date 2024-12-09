ff4857d63295d6047c2424076d7bc63eba93bc9a
Ignat Kolesnichenko <ignat@yandex-team.ru>
2024-12-09 15:10:05 +0000

    Revert "Set OS_SDK=local in bindings build"
    This reverts commit 68bc6416c53c94f036770a147713b4d4dc1c42e5.

--- yt/python/packages/build_manylinux_bindings.sh
+++ yt/python/packages/build_manylinux_bindings.sh
@@ -6,7 +6,7 @@ python3 /ytsaurus/ya make \
     /ytsaurus/yt/yt/python/yson_shared \
     /ytsaurus/yt/yt/python/driver/rpc_shared \
     -T --build=relwithdebinfo \
-    -DUSE_ARCADIA_PYTHON=no -DUSE_LOCAL_PYTHON -DPYTHON_CONFIG=python3-config -DPYTHON_BIN=python3 -DOS_SDK=local \
+    -DUSE_ARCADIA_PYTHON=no -DUSE_LOCAL_PYTHON -DPYTHON_CONFIG=python3-config -DPYTHON_BIN=python3 -DOS_SDK=ubuntu-14 \
     --output /build
 
 strip --remove-section=.gnu_debuglink /build/yt/yt/python/yson_shared/*.so /build/yt/yt/python/driver/rpc_shared/*.so
