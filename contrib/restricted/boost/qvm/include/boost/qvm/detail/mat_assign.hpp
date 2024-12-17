ca2f75c9f0c711175295208dd2e8db2b076d44f9
robot-contrib <robot-contrib@yandex-team.com>
2024-12-17 07:23:52 +0300

    Update contrib/restricted/boost/qvm to 1.87.0 commit_hash:db25806e86a21da784e6605fe077741ccb2c6ef1

--- contrib/restricted/boost/qvm/include/boost/qvm/detail/mat_assign.hpp
+++ contrib/restricted/boost/qvm/include/boost/qvm/detail/mat_assign.hpp
@@ -1,8 +1,7 @@
 #ifndef BOOST_QVM_DETAIL_MAT_ASSIGN_HPP_INCLUDED
 #define BOOST_QVM_DETAIL_MAT_ASSIGN_HPP_INCLUDED
 
-// Copyright 2008-2022 Emil Dotchevski and Reverge Studios, Inc.
-
+// Copyright 2008-2024 Emil Dotchevski and Reverge Studios, Inc.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
--- contrib/restricted/boost/qvm/include/boost/qvm/vec_mat_operations2.hpp
+++ contrib/restricted/boost/qvm/include/boost/qvm/vec_mat_operations2.hpp
@@ -1,5 +1,4 @@
-// Copyright 2008-2022 Emil Dotchevski and Reverge Studios, Inc.
-
+// Copyright 2008-2024 Emil Dotchevski and Reverge Studios, Inc.
 // Distributed under the Boost Software License, Version 1.0. (See accompanying
 // file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
