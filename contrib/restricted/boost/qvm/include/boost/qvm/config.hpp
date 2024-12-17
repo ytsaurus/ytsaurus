26c3a4154e454388fb0050241a881d7a4e741f03
robot-contrib <robot-contrib@yandex-team.com>
2024-12-17 07:23:52 +0300

    Update contrib/restricted/boost/qvm to 1.87.0 commit_hash:db25806e86a21da784e6605fe077741ccb2c6ef1

--- contrib/restricted/boost/qvm/include/boost/qvm/config.hpp
+++ contrib/restricted/boost/qvm/include/boost/qvm/config.hpp
@@ -1,10 +1,9 @@
-// Copyright 2008-2022 Emil Dotchevski and Reverge Studios, Inc.
-
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
 #ifndef BOOST_QVM_CONFIG_HPP_INCLUDED
 #define BOOST_QVM_CONFIG_HPP_INCLUDED
+
+// Copyright 2008-2024 Emil Dotchevski and Reverge Studios, Inc.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #if defined( BOOST_STRICT_CONFIG ) || defined( BOOST_QVM_NO_WORKAROUNDS )
 #   define BOOST_QVM_WORKAROUND( symbol, test ) 0
--- contrib/restricted/boost/qvm/include/boost/qvm/vec_traits.hpp
+++ contrib/restricted/boost/qvm/include/boost/qvm/vec_traits.hpp
@@ -1,10 +1,9 @@
-// Copyright 2008-2022 Emil Dotchevski and Reverge Studios, Inc.
-
-// Distributed under the Boost Software License, Version 1.0. (See accompanying
-// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
-
 #ifndef BOOST_QVM_VEC_TRAITS_HPP_INCLUDED
 #define BOOST_QVM_VEC_TRAITS_HPP_INCLUDED
+
+// Copyright 2008-2024 Emil Dotchevski and Reverge Studios, Inc.
+// Distributed under the Boost Software License, Version 1.0. (See accompanying
+// file LICENSE_1_0.txt or copy at http://www.boost.org/LICENSE_1_0.txt)
 
 #include <boost/qvm/is_scalar.hpp>
 #include <boost/qvm/enable_if.hpp>
