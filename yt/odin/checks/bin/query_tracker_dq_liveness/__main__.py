294a0cbd797fcf549a5c79df8755c6218e5cda21
Aleksandr Gaev <aleksandr.gaev@tracto.ai>
2024-12-19 14:14:05 +0100

    Add `pragma DqEngine = 'force';` to dq liveness odin check

--- yt/odin/checks/bin/query_tracker_dq_liveness/__main__.py
+++ yt/odin/checks/bin/query_tracker_dq_liveness/__main__.py
@@ -29,7 +29,7 @@ def run_check(yt_client, logger, options, states):
         states,
         soft_timeout,
         "yql",
-        "select x + 1 as result from `{table}`;",
+        "pragma DqEngine = 'force'; select x + 1 as result from `{table}`;",
         Data(SCHEMA, SOURCE_DATA, RESULT_DATA),
         progress_check=progress_check,
     )
