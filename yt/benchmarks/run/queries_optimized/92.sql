7518dbfebdb2dc8a705ddd6034bfa58578a5eca7
orlovorlov <orlovorlov@ytsaurus.tech>
2025-01-09 23:25:09 +0300

    YT-23110 Add manually optimized query commit_hash:3ee0ff070447334162b53c4a9423f89d7f4c19b2

---
+++ yt/benchmarks/run/queries_optimized/92.sql
@@ -0,0 +1,35 @@
+
+-- NB: Subqueries
+$bla = (
+         SELECT
+         web_sales.ws_item_sk bla_item_sk,
+            avg(ws_ext_discount_amt)  bla_ext_discount_amt
+         FROM
+            web_sales
+           cross join date_dim
+         WHERE
+        cast(d_date as date) between cast('2001-03-12' as date) and
+                             (cast('2001-03-12' as date) + DateTime::IntervalFromDays(90))
+          and d_date_sk = ws_sold_date_sk
+          group by web_sales.ws_item_sk
+      );
+
+-- start query 1 in stream 0 using template query92.tpl and seed 2031708268
+select
+   sum(ws_ext_discount_amt)  as `Excess Discount Amount`
+from
+    web_sales
+   cross join date_dim
+   cross join item
+   join $bla bla on (item.i_item_sk = bla.bla_item_sk)
+where
+d_date_sk = ws_sold_date_sk
+and i_item_sk = ws_item_sk
+and cast(d_date as date) between cast('2001-03-12' as date) and
+        (cast('2001-03-12' as date) + DateTime::IntervalFromDays(90))
+and ws_ext_discount_amt
+     > 1.3 * bla.bla_ext_discount_amt
+order by `Excess Discount Amount`
+limit 100;
+
+-- end query 1 in stream 0 using template query92.tpl
