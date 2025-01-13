5b72aad7cafe1f0b44dd76acced1a52ecd4e7264
cherepashka <cherepashka@ytsaurus.tech>
2025-01-13 15:45:32 +0300

    YT-23931: Added extra ref & unref
    * Changelog entry
      Type: fix
      Component: master
    
    Fix for race between tx coordinator tx commit & cell with exported object unref exported object
    commit_hash:f4e249955e9b460f3322390188b74ddad59353a6

--- yt/python/yt/environment/default_config.py
+++ yt/python/yt/environment/default_config.py
@@ -124,6 +124,7 @@ def get_dynamic_master_config():
 
     transaction_manager = {
         forbid_transaction_actions_for_cypress_transactions = %true;
+        enable_extra_ref_exported_objects = %true;
     };
 
     multicell_manager = {
