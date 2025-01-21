1ba48266158a06c102997f720d374b32629bbcfc
babenko <babenko@ytsaurus.tech>
2025-01-21 23:28:09 +0300

    Move ConfigureCoverageOutput to TServerProgram commit_hash:92c1bd6c0b2c04b0de3e27c26f14feaf6401da72

--- yt/yt/library/server_program/server_program.cpp
+++ yt/yt/library/server_program/server_program.cpp
@@ -24,13 +24,41 @@
 
 #include <library/cpp/yt/mlock/mlock.h>
 
+#include <util/string/subst.h>
+#include <util/system/env.h>
+
+#if defined(_linux_) && defined(CLANG_COVERAGE)
+extern "C" int __llvm_profile_write_file(void);
+extern "C" void __llvm_profile_set_filename(const char* name);
+#endif
+
 namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace {
+
+void ConfigureCoverageOutput()
+{
+#if defined(_linux_) && defined(CLANG_COVERAGE)
+    // YT tests use pid namespaces. We can't use process id as unique identifier for output file.
+    if (auto profileFile = GetEnv("LLVM_PROFILE_FILE"); !profileFile.empty()) {
+        SubstGlobal(profileFile, "%e", "ytserver-all");
+        SubstGlobal(profileFile, "%p", ToString(TInstant::Now().NanoSeconds()));
+        __llvm_profile_set_filename(profileFile.c_str());
+    }
+#endif
+}
+
+} // namespace
 
 ////////////////////////////////////////////////////////////////////////////////
 
 TServerProgramBase::TServerProgramBase()
     : ServiceDirectory_(NFusion::CreateServiceDirectory())
-{ }
+{
+    ConfigureCoverageOutput();
+}
 
 void TServerProgramBase::SetMainThreadName(const std::string& name)
 {
