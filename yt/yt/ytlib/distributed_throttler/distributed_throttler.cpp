993600f99bc5072a1e87af2a44022bbdce964aa6
yuryalekseev <yuryalekseev@ytsaurus.tech>
2025-01-22 08:07:53 +0300

    YT-21713: Address review comments. commit_hash:53f7f4355f56969bbf8c9f489d0cb4973fe565de

--- yt/yt/ytlib/distributed_throttler/distributed_throttler.cpp
+++ yt/yt/ytlib/distributed_throttler/distributed_throttler.cpp
@@ -817,7 +817,7 @@ private:
         }
 
         // Update throttlers global data.
-        ThrottlerToGlobalUsage_.Store(std::move(newThrottlerToGlobalUsage));
+        ThrottlerToGlobalUsage_.Store(newThrottlerToGlobalUsage.Get());
     }
 
     void ForgetDeadThrottlers()
