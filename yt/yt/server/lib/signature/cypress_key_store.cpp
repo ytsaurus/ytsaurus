c1ad67732e0053ae2bd07292a50c12ba73fd23e9
pavook <pavook@ytsaurus.tech>
2025-01-29 23:25:12 +0300

    Rework cypress keystore initialization
    * Hide away implementation, provide Create... functions
    * Remove inner initialization future
    commit_hash:610b44fd8c868a1c5da77a169299bf061a9b95da

--- yt/yt/server/lib/signature/cypress_key_store.cpp
+++ yt/yt/server/lib/signature/cypress_key_store.cpp
@@ -2,6 +2,7 @@
 
 #include "config.h"
 #include "key_info.h"
+#include "key_store.h"
 #include "private.h"
 
 #include <yt/yt/ytlib/api/native/client.h>
@@ -24,93 +25,128 @@ using namespace NThreading;
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TCypressKeyReader::TCypressKeyReader(TCypressKeyReaderConfigPtr config, IClientPtr client)
-    : Config_(std::move(config))
-    , Client_(std::move(client))
-{ }
+namespace {
 
-TFuture<TKeyInfoPtr> TCypressKeyReader::FindKey(const TOwnerId& ownerId, const TKeyId& keyId)
+class TCypressKeyReader
+    : public IKeyStoreReader
 {
-    auto keyNodePath = MakeCypressKeyPath(Config_->Path, ownerId, keyId);
-    auto result = Client_->GetNode(keyNodePath);
+public:
+    TCypressKeyReader(TCypressKeyReaderConfigPtr config, NApi::IClientPtr client)
+        : Config_(std::move(config))
+        , Client_(std::move(client))
+    { }
 
-    return result.Apply(BIND([] (const TYsonString& str) {
-        return ConvertTo<TKeyInfoPtr>(str);
-    }));
-}
+    TFuture<TKeyInfoPtr> FindKey(const TOwnerId& ownerId, const TKeyId& keyId) override
+    {
+        auto keyNodePath = MakeCypressKeyPath(Config_->Path, ownerId, keyId);
+        auto result = Client_->GetNode(keyNodePath);
+
+        return result.Apply(BIND([] (const TYsonString& str) {
+            return ConvertTo<TKeyInfoPtr>(str);
+        }));
+    }
+
+private:
+    TCypressKeyReaderConfigPtr Config_;
+    NApi::IClientPtr Client_;
+};
+
+DEFINE_REFCOUNTED_TYPE(TCypressKeyReader)
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TCypressKeyWriter::TCypressKeyWriter(TCypressKeyWriterConfigPtr config, IClientPtr client)
-    : Config_(std::move(config))
-    , Client_(std::move(client))
-{ }
-
-TFuture<void> TCypressKeyWriter::Initialize()
+class TCypressKeyWriter
+    : public IKeyStoreWriter
 {
-    TCreateNodeOptions options;
-    options.IgnoreExisting = true;
+public:
+    TCypressKeyWriter(TCypressKeyWriterConfigPtr config, IClientPtr client)
+        : Config_(std::move(config))
+        , Client_(std::move(client))
+    { }
 
-    Initialization_ = Client_->CreateNode(
-        YPathJoin(Config_->Path, ToYPathLiteral(Config_->OwnerId)),
-        EObjectType::MapNode,
-        std::move(options)).AsVoid();
+    //! Initialize() should be called before all other calls.
+    TFuture<void> Initialize()
+    {
+        TCreateNodeOptions options;
+        options.IgnoreExisting = true;
 
-    YT_LOG_INFO("Initialized Cypress key writer");
+        auto ownerNodePath = YPathJoin(Config_->Path, ToYPathLiteral(Config_->OwnerId));
 
-    return Initialization_;
+        YT_LOG_INFO("Initializing Cypress key writer (OwnerNodePath: %v)", ownerNodePath);
+
+        return Client_->CreateNode(
+            ownerNodePath,
+            EObjectType::MapNode,
+            std::move(options)).AsVoid();
+    }
+
+    const TOwnerId& GetOwner() override
+    {
+        return Config_->OwnerId;
+    }
+
+    TFuture<void> RegisterKey(const TKeyInfoPtr& keyInfo) override
+    {
+        auto [ownerId, keyId] = std::visit([] (const auto& meta) {
+            return std::pair(meta.OwnerId, meta.KeyId);
+        }, keyInfo->Meta());
+
+        YT_LOG_DEBUG("Registering key (OwnerId: %v, KeyId: %v)", ownerId, keyId);
+
+        // We should not register keys belonging to other owners.
+        YT_VERIFY(ownerId == Config_->OwnerId);
+
+        auto keyNodePath = MakeCypressKeyPath(Config_->Path, ownerId, keyId);
+
+        auto keyExpirationTime = std::visit([] (const auto& meta) {
+            return meta.ExpiresAt;
+        }, keyInfo->Meta());
+
+        auto attributes = CreateEphemeralAttributes();
+        attributes->Set("expiration_time", keyExpirationTime + Config_->KeyDeletionDelay);
+
+        TCreateNodeOptions options;
+        options.Attributes = attributes;
+        auto node = Client_->CreateNode(
+            keyNodePath,
+            EObjectType::Document,
+            std::move(options));
+
+        return node.Apply(
+            BIND([
+                    this,
+                    keyNodePath = std::move(keyNodePath),
+                    keyInfo,
+                    this_ = MakeStrong(this)
+                ] (NCypressClient::TNodeId /*nodeId*/) mutable {
+                    return Client_->SetNode(
+                        keyNodePath,
+                        ConvertToYsonString(std::move(keyInfo)));
+                }));
+    }
+
+private:
+    const TCypressKeyWriterConfigPtr Config_;
+    const NApi::IClientPtr Client_;
+};
+
+DEFINE_REFCOUNTED_TYPE(TCypressKeyWriter)
+
+} // namespace
+
+////////////////////////////////////////////////////////////////////////////////
+
+IKeyStoreReaderPtr CreateCypressKeyReader(TCypressKeyReaderConfigPtr config, IClientPtr client)
+{
+    return New<TCypressKeyReader>(std::move(config), std::move(client));
 }
 
-const TOwnerId& TCypressKeyWriter::GetOwner()
+TFuture<IKeyStoreWriterPtr> CreateCypressKeyWriter(TCypressKeyWriterConfigPtr config, IClientPtr client)
 {
-    return Config_->OwnerId;
-}
-
-TFuture<void> TCypressKeyWriter::RegisterKey(const TKeyInfoPtr& keyInfo)
-{
-    YT_VERIFY(keyInfo);
-
-    return Initialization_.Apply(BIND([this, keyInfo, this_ = MakeStrong(this)] () {
-        return DoRegister(std::move(keyInfo));
+    auto writer = New<TCypressKeyWriter>(std::move(config), std::move(client));
+    return writer->Initialize().Apply(BIND([writer = std::move(writer)] () -> IKeyStoreWriterPtr {
+        return writer;
     }));
-}
-
-TFuture<void> TCypressKeyWriter::DoRegister(const TKeyInfoPtr& keyInfo)
-{
-    auto [ownerId, keyId] = std::visit([] (const auto& meta) {
-        return std::pair{meta.OwnerId, meta.KeyId};
-    }, keyInfo->Meta());
-
-    YT_LOG_DEBUG("Registering key (OwnerId: %v, KeyId: %v)", ownerId, keyId);
-
-    // We should not register keys belonging to other owners.
-    YT_VERIFY(ownerId == Config_->OwnerId);
-
-    auto keyNodePath = MakeCypressKeyPath(Config_->Path, ownerId, keyId);
-
-    auto keyExpirationTime = std::visit([] (const auto& meta) {
-        return meta.ExpiresAt;
-    }, keyInfo->Meta());
-
-    auto attributes = CreateEphemeralAttributes();
-    attributes->Set("expiration_time", keyExpirationTime + Config_->KeyDeletionDelay);
-
-    TCreateNodeOptions options;
-    options.Attributes = attributes;
-    auto node = Client_->CreateNode(
-        keyNodePath,
-        EObjectType::Document,
-        std::move(options));
-
-    return node.Apply(
-        BIND([
-                this,
-                keyNodePath = std::move(keyNodePath),
-                keyInfo,
-                this_ = MakeStrong(this)
-            ] (NCypressClient::TNodeId /*nodeId*/) mutable {
-                return Client_->SetNode(keyNodePath, ConvertToYsonString(std::move(keyInfo)));
-            }));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
--- yt/yt/server/lib/signature/cypress_key_store.h
+++ yt/yt/server/lib/signature/cypress_key_store.h
@@ -2,58 +2,19 @@
 
 #include "public.h"
 
-#include "key_store.h"
-
 #include <yt/yt/ytlib/api/native/public.h>
 
 namespace NYT::NSignature {
 
 ////////////////////////////////////////////////////////////////////////////////
 
-class TCypressKeyReader
-    : public IKeyStoreReader
-{
-public:
-    TCypressKeyReader(
-        TCypressKeyReaderConfigPtr config,
-        NApi::IClientPtr client);
+IKeyStoreReaderPtr CreateCypressKeyReader(
+    TCypressKeyReaderConfigPtr config,
+    NApi::IClientPtr client);
 
-    TFuture<TKeyInfoPtr> FindKey(const TOwnerId& ownerId, const TKeyId& keyId) override;
-
-private:
-    TCypressKeyReaderConfigPtr Config_;
-    NApi::IClientPtr Client_;
-};
-
-DEFINE_REFCOUNTED_TYPE(TCypressKeyReader)
-
-////////////////////////////////////////////////////////////////////////////////
-
-class TCypressKeyWriter
-    : public IKeyStoreWriter
-{
-public:
-    TCypressKeyWriter(
-        TCypressKeyWriterConfigPtr config,
-        NApi::IClientPtr client);
-
-    //! Initialize() should be called at least once before all other calls.
-    TFuture<void> Initialize();
-
-    const TOwnerId& GetOwner() override;
-
-    TFuture<void> RegisterKey(const TKeyInfoPtr& keyInfo) override;
-
-private:
-    const TCypressKeyWriterConfigPtr Config_;
-    const NApi::IClientPtr Client_;
-
-    TFuture<void> Initialization_;
-
-    TFuture<void> DoRegister(const TKeyInfoPtr& keyInfo);
-};
-
-DEFINE_REFCOUNTED_TYPE(TCypressKeyWriter)
+TFuture<IKeyStoreWriterPtr> CreateCypressKeyWriter(
+    TCypressKeyWriterConfigPtr config,
+    NApi::IClientPtr client);
 
 ////////////////////////////////////////////////////////////////////////////////
 
