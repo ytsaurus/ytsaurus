e978aaca33ff45e3c21d65beb825853c06ed2bfc
ermolovd <ermolovd@ytsaurus.tech>
2025-01-31 10:08:19 +0300

    Cleanup TTempTable code commit_hash:7189cf3880cca7a79142d33571f3f42d398fe54f

--- yt/cpp/mapreduce/util/temp_table.h
+++ yt/cpp/mapreduce/util/temp_table.h
@@ -6,13 +6,24 @@ namespace NYT {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+///
+/// @brief RAII class for working with temporary tables. Constructor of this class creates temporary table and destructor removes it.
+///
+/// CAVEAT: when using with transactions destructor of TTempTable should be called before commiting transaction.
 class TTempTable
 {
 public:
+    ///
+    /// @brief Constructor creates temporary table
+    ///
+    /// @param client -- YT client or transaction object.
+    /// @param prefix -- table name prefix
+    /// @param directory -- path to directory where temporary table will be created.
+    /// @param options -- options to be passed for table creation (might be useful if we want to set table attributes)
     explicit TTempTable(
         IClientBasePtr client,
         const TString& prefix = {},
-        const TYPath& path = {},
+        const TYPath& directory = {},
         const TCreateOptions& options = {});
 
     TTempTable(const TTempTable&) = delete;
@@ -25,9 +36,12 @@ public:
 
     static TTempTable CreateAutoremovingTable(IClientBasePtr client, TYPath path, const TCreateOptions& options);
 
+    /// Return full path to the table.
     TString Name() const &;
     TString Name() && = delete;
-    TString Release(); // Release table and return its name. Table will not be deleted by TTempTable after this call
+
+    /// Release table and return its path. Table will not be deleted by TTempTable destructor after this call.
+    TString Release();
 
 private:
     struct TPrivateConstuctorTag
