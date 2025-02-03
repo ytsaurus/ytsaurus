7b777df3b40ee2b6d1b1366661bac87ea3df098a
lishy2 <lishy2@yandex-team.com>
2025-02-03 16:04:20 +0300

    YTORM-1021: Implement automated index resolving
    index policy
    
    constant folding
    commit_hash:bab78e825f726dd2bd6fff46d95c681be9836f6a

--- yt/yt/orm/library/query/misc.cpp
+++ yt/yt/orm/library/query/misc.cpp
@@ -26,6 +26,54 @@ bool IsTargetReference(const TExpressionList& exprs, const NQueryClient::NAst::T
     return false;
 }
 
+bool IsConstant(const TExpressionPtr expr)
+{
+    if (auto* literal = expr->As<TLiteralExpression>()) {
+        return true;
+    }
+    if (auto *function = expr->As<TFunctionExpression>()) {
+        bool constantArguments = true;
+        for (const auto arguments : function->Arguments) {
+            constantArguments &= IsConstant(arguments);
+        }
+        return constantArguments;
+    }
+    return false;
+}
+
+bool IsSingleConstant(const TExpressionList& exprs)
+{
+    if (exprs.size() != 1) {
+        return false;
+    }
+    return IsConstant(exprs[0]);
+}
+
+std::vector<TReference> ExtractAllReferences(const TExpressionList& exprs)
+{
+    std::vector<TReference> refs;
+    for (int i = 0; i < std::ssize(exprs); ++i) {
+        if (auto* refExpr = exprs[i]->As<TReferenceExpression>()) {
+            refs.push_back(refExpr->Reference);
+        } else {
+            return {};
+        }
+    }
+    return refs;
+}
+
+std::optional<TReference> TryExtractReference(const TExpressionList& exprs)
+{
+    if (exprs.size() != 1) {
+        return std::nullopt;
+    }
+    if (auto* refExpr = exprs[0]->As<TReferenceExpression>()) {
+        return refExpr->Reference;
+    } else {
+        return std::nullopt;
+    }
+}
+
 bool IsAnyExprATargetReference(const TExpressionList& exprs, const NQueryClient::NAst::TReference& reference)
 {
     return std::ranges::any_of(exprs, [&] (const TExpressionPtr& expr) {
