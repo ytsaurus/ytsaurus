5961bbf4909bd27cd0956a31a41a3d99ed7b6cc4
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2025-02-03 17:56:57 +0300

    Pass test service binary to go bus rpc test via environment
    No description
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1051
    
    Co-authored-by: ignat <ignat@ytsaurus.tech>
    commit_hash:a8944e9c408c40563cffc7534184b86cdcf9cd6e

--- yt/go/bus/test_service_binary.go
+++ yt/go/bus/test_service_binary.go
@@ -8,7 +8,7 @@ import (
 const envTestServiceBinaryPath = "TEST_SERVICE_BINARY_PATH"
 
 func GetTestServiceBinary(t *testing.T) string {
-	path := os.Getenv(envTestServiceBinaryPath)
+	path := os.ExpandEnv(os.Getenv(envTestServiceBinaryPath))
 	if path == "" {
 		t.Skipf("Skipping testing as there is no test service binary found in %q.", envTestServiceBinaryPath)
 	}
