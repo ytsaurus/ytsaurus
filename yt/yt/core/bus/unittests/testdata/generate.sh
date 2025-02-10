c3450ff95011c1f08c47dc3c85ae4e7bd3174a60
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2025-02-10 17:07:59 +0100

    yt/yt/core/bus: add test for TLS with ECDSA
    Now bus TLS supports ECDSA certificates.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@tracto.ai>

--- yt/yt/core/bus/unittests/testdata/generate.sh
+++ yt/yt/core/bus/unittests/testdata/generate.sh
@@ -22,3 +22,16 @@ openssl req -new -x509 -noenc -newkey rsa:4096 -keyout key_with_ip_in_san.pem -o
     -addext "basicConstraints=critical,CA:FALSE" \
     -addext "keyUsage=digitalSignature,keyEncipherment" \
     -addext "extendedKeyUsage=serverAuth,clientAuth"
+
+openssl req -x509 -noenc -newkey ec:<(openssl ecparam -name prime256v1) -keyout ca_key_ec.pem -out ca_ec.pem \
+    -days 3650 -subj "/C=RU/L=Moscow/O=Yandex/CN=Localhost CA" \
+    -addext "basicConstraints=critical,CA:TRUE,pathlen:0" \
+    -addext "keyUsage=keyCertSign"
+
+openssl req -new -x509 -noenc -newkey ec:<(openssl ecparam -name prime256v1) -keyout key_ec.pem -out cert_ec.pem \
+    -CA ca_ec.pem -CAkey ca_key_ec.pem \
+    -days 3650 -subj "/C=RU/L=Moscow/O=Yandex/CN=localhost" \
+    -addext "subjectAltName=DNS:localhost,IP:127.0.0.1,IP:::1" \
+    -addext "basicConstraints=critical,CA:FALSE" \
+    -addext "keyUsage=digitalSignature,keyEncipherment" \
+    -addext "extendedKeyUsage=serverAuth,clientAuth"
