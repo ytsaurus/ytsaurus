d4c3a9386fc6988eeed57a83944b1e80e070104e
osidorkin <osidorkin@ytsaurus.tech>
2025-02-11 10:36:43 +0300

    YT-24162: s/remove_if/erase_if/, add test commit_hash:e0ba33ea0093096bcd95a88353729d33d58ff519

---
+++ yt/yt/server/master/chaos_server/helpers.cpp
@@ -0,0 +1,33 @@
+#include "helpers.h"
+
+#include <yt/yt/core/actions/future.h>
+
+namespace NYT::NChaosServer {
+
+////////////////////////////////////////////////////////////////////////////////
+
+TErrorOr<int> GetMinimalTabletCount(std::vector<TErrorOr<int>> tabletCounts)
+{
+    std::erase_if(
+        tabletCounts,
+        [] (const auto& tabletCount) {
+            return !tabletCount.IsOK();
+        });
+
+    auto minElementIt = std::min_element(
+        tabletCounts.begin(),
+        tabletCounts.end(),
+        [] (const auto& a, const auto& b) {
+            return a.Value() < b.Value();
+        });
+
+    if (minElementIt == tabletCounts.end()) {
+        return TError("Failed to get tablet count from any replica");
+    }
+
+    return minElementIt->Value();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NChaosServer
