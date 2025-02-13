611fafdd3059152d1ac2c6601c948a367827fb8b
Nikita Sokolov <faucct@tracto.ai>
2025-02-13 11:03:14 +0100

    extract TestWritingRows

--- yt/yt/tests/integration/proxies/test_rpc_proxy.py
+++ yt/yt/tests/integration/proxies/test_rpc_proxy.py
@@ -730,6 +730,45 @@ class TestRpcProxyBase(YTEnvSetup):
 
 
 @pytest.mark.enabled_multidaemon
+class TestWritingRows(TestRpcProxyBase):
+    @authors("faucct")
+    def test_writing_large_rows(self):
+        MB = 1024 ** 2
+        data = [{"index": 0, "str": "F" * (17 * MB)}]
+        create("table", "//tmp/table", attributes={"dynamic": False, "schema": self._schema})
+        write_table("//tmp/table", data, table_writer={"max_row_weight": 20 * MB})
+        assert read_table("//tmp/table") == data
+
+    @authors("faucct")
+    def test_writing_large_rows_129(self):
+        MB = 1024 ** 2
+        data = [{"index": 0, "str": "F" * (129 * MB)}]
+        create("table", "//tmp/table", attributes={"dynamic": False, "schema": self._schema})
+        with raises_yt_error(f'Expected <= {128 * MB}, found {130 * MB}'):
+            write_table("//tmp/table", data, table_writer={"max_row_weight": 130 * MB})
+        with raises_yt_error() as e:
+            write_table("//tmp/table", data, table_writer={"max_row_weight": 128 * MB})
+        assert 'Memory limit exceeded while parsing YSON stream: ' in str(e[0]) or 'Row weight is too large' in str(e[0])
+        assert read_table("//tmp/table") == []
+
+    @authors("faucct")
+    @pytest.mark.parametrize("atomicity", ["none", "full"])
+    def test_writing_large_dynamic_rows(self, atomicity):
+        MB = 1024 ** 2
+        data = [{"index": 0, "str": "F" * (17 * MB)}]
+        create("table", "//tmp/dynamic", attributes={"dynamic": True, "schema": self._schema, "atomicity": atomicity})
+        sync_create_cells(1)
+        sync_mount_table("//tmp/dynamic")
+        with raises_yt_error(f'Value of type "string" is too long: length {17 * MB}, limit {16 * MB}'):
+            insert_rows("//tmp/dynamic", data, table_writer={"max_row_weight": 20 * MB})
+        sync_unmount_table("//tmp/dynamic")
+        assert read_table("//tmp/dynamic") == []
+
+
+##################################################################
+
+
+@pytest.mark.enabled_multidaemon
 class TestRpcProxyClientRetries(TestRpcProxyBase):
     NUM_NODES = 2
 
@@ -898,39 +937,6 @@ class TestOperationsRpcProxy(TestRpcProxyBase):
         self._prepare_output_table()
 
         assert select_rows("* from [//tmp/t_out] LIMIT " + str(2 * size)) == original_table
-
-    @authors("faucct")
-    def test_writing_large_rows(self):
-        MB = 1024 ** 2
-        data = [{"index": 0, "str": "F" * (17 * MB)}]
-        create("table", "//tmp/table", attributes={"dynamic": False, "schema": self._schema})
-        write_table("//tmp/table", data, table_writer={"max_row_weight": 20 * MB})
-        assert read_table("//tmp/table") == data
-
-    @authors("faucct")
-    def test_writing_large_rows_129(self):
-        MB = 1024 ** 2
-        data = [{"index": 0, "str": "F" * (129 * MB)}]
-        create("table", "//tmp/table", attributes={"dynamic": False, "schema": self._schema})
-        with raises_yt_error(f'Expected <= {128 * MB}, found {130 * MB}'):
-            write_table("//tmp/table", data, table_writer={"max_row_weight": 130 * MB})
-        with raises_yt_error() as e:
-            write_table("//tmp/table", data, table_writer={"max_row_weight": 128 * MB})
-        assert 'Memory limit exceeded while parsing YSON stream: ' in str(e[0]) or 'Row weight is too large' in str(e[0])
-        assert read_table("//tmp/table") == []
-
-    @authors("faucct")
-    @pytest.mark.parametrize("atomicity", ["none", "full"])
-    def test_writing_large_dynamic_rows(self, atomicity):
-        MB = 1024 ** 2
-        data = [{"index": 0, "str": "F" * (17 * MB)}]
-        create("table", "//tmp/dynamic", attributes={"dynamic": True, "schema": self._schema, "atomicity": atomicity})
-        sync_create_cells(1)
-        sync_mount_table("//tmp/dynamic")
-        with raises_yt_error(f'Value of type "string" is too long: length {17 * MB}, limit {16 * MB}'):
-            insert_rows("//tmp/dynamic", data, table_writer={"max_row_weight": 20 * MB})
-        sync_unmount_table("//tmp/dynamic")
-        assert read_table("//tmp/dynamic") == []
 
     @authors("kiselyovp")
     def test_abort_operation(self):
