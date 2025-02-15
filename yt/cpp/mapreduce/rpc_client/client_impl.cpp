8d913789990d32fd82d00d514a7656fb733cc6a7
hiddenpath <hiddenpath@ytsaurus.tech>
2025-02-15 19:51:48 +0300

    YT-23616: Add implementation of method for creating rpc client commit_hash:5854a0271a0a2a26dc9891ff1502889edbf38a18

---
+++ yt/cpp/mapreduce/rpc_client/client_impl.cpp
@@ -0,0 +1,64 @@
+#include "raw_client.h"
+
+#include <yt/cpp/mapreduce/client/client.h>
+#include <yt/cpp/mapreduce/client/init.h>
+
+#include <yt/cpp/mapreduce/common/retry_lib.h>
+
+#include <yt/cpp/mapreduce/interface/client_method_options.h>
+#include <yt/cpp/mapreduce/interface/tvm.h>
+
+#include <yt/yt/client/api/rpc_proxy/config.h>
+#include <yt/yt/client/api/rpc_proxy/connection.h>
+
+namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+IClientPtr CreateRpcClient(
+    const TString& serverName,
+    const TCreateClientOptions& options)
+{
+    auto context = NDetail::CreateClientContext(serverName, options);
+
+    auto globalTxId = GetGuid(context.Config->GlobalTxId);
+
+    auto retryConfigProvider = options.RetryConfigProvider_;
+    if (!retryConfigProvider) {
+        retryConfigProvider = CreateDefaultRetryConfigProvider();
+    }
+
+    auto connectionConfig = New<NApi::NRpcProxy::TConnectionConfig>();
+    connectionConfig->SetDefaults();
+    connectionConfig->ClusterUrl = context.ServerName;
+    if (context.ProxyAddress) {
+        connectionConfig->ProxyAddresses = {*context.ProxyAddress};
+    }
+
+    NApi::TClientOptions clientOptions;
+    clientOptions.Token = context.Token;
+    if (context.ServiceTicketAuth) {
+        clientOptions.ServiceTicketAuth = context.ServiceTicketAuth->Ptr;
+    }
+    if (context.ImpersonationUser) {
+        clientOptions.User = *context.ImpersonationUser;
+    }
+
+    auto connection = NApi::NRpcProxy::CreateConnection(connectionConfig);
+
+    auto rawClient = MakeIntrusive<NDetail::TRpcRawClient>(
+        connection->CreateClient(clientOptions),
+        context.Config);
+
+    NDetail::EnsureInitialized();
+
+    return new NDetail::TClient(
+        std::move(rawClient),
+        context,
+        globalTxId,
+        CreateDefaultClientRetryPolicy(retryConfigProvider, context.Config));
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT
