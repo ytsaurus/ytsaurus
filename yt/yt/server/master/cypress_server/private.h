a66270a48fc2646a6b4d2a07041a073099ecb71a
h0pless <h0pless@ytsaurus.tech>
2025-02-18 14:47:19 +0300

    YT-24191: Fix an issue, when nodes, held by transactions, would interfere with expiration_time cleanup commit_hash:b0c2fecf7474f2ef612742e76adf18a8460ec482

--- yt/yt/server/master/cypress_server/private.h
+++ yt/yt/server/master/cypress_server/private.h
@@ -4,11 +4,15 @@
 
 #include <yt/yt/core/logging/log.h>
 
+#include <yt/yt/library/profiling/sensor.h>
+
 namespace NYT::NCypressServer {
 
 ////////////////////////////////////////////////////////////////////////////////
 
 YT_DEFINE_GLOBAL(const NLogging::TLogger, CypressServerLogger, "Cypress");
+
+YT_DEFINE_GLOBAL(const NProfiling::TProfiler, ExpirationTrackerProfiler, "/expiration_tracker");
 
 DECLARE_REFCOUNTED_CLASS(TAccessTracker)
 DECLARE_REFCOUNTED_CLASS(TExpirationTracker)
