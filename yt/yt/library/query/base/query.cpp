c1e41d118ef503dde86f9537a0cd57982cbb424c
nadya73 <nadya73@ytsaurus.tech>
2025-02-19 17:23:01 +0300

    [24.2] YT-24255, YT-16386: Return original type of type_v1 columns in web_json
    Fix build
    
    Fix logical type in TReferenceExpression
    
    Revert "Revert [24.2] YT-16386: Return original type of type_v1 columns in web_json"
    This reverts commit cabe09bf3de1e53c86de18d4443db3cc1e49eb3b, reversing
    changes made to 342983b3e52dec08e2baadca6b6ea84e8e92ddd1.
    commit_hash:81241f684ea36b8d3891e7614affb6c09c51439c

--- yt/yt/library/query/base/query.cpp
+++ yt/yt/library/query/base/query.cpp
@@ -51,10 +51,17 @@ TLogicalTypePtr ToQLType(const NTableClient::TLogicalTypePtr& columnType)
 
 TExpression::TExpression(NTableClient::TLogicalTypePtr type)
     : LogicalType(std::move(type))
+    , OriginalLogicalType(LogicalType)
+{ }
+
+TExpression::TExpression(NTableClient::TLogicalTypePtr type, NTableClient::TLogicalTypePtr originalType)
+    : LogicalType(std::move(type))
+    , OriginalLogicalType(std::move(originalType))
 { }
 
 TExpression::TExpression(EValueType type)
     : LogicalType(MakeLogicalType(GetLogicalType(type), /*required*/ false))
+    , OriginalLogicalType(LogicalType)
 { }
 
 EValueType TExpression::GetWireType() const
@@ -76,11 +83,11 @@ TLiteralExpression::TLiteralExpression(EValueType type, TOwningValue value)
 ////////////////////////////////////////////////////////////////////////////////
 
 TReferenceExpression::TReferenceExpression(const NTableClient::TLogicalTypePtr& type)
-    : TExpression(ToQLType(type))
+    : TExpression(ToQLType(type), type)
 { }
 
 TReferenceExpression::TReferenceExpression(const NTableClient::TLogicalTypePtr& type, TString columnName)
-    : TExpression(ToQLType(type))
+    : TExpression(ToQLType(type), type)
     , ColumnName(std::move(columnName))
 { }
 
@@ -344,12 +351,14 @@ void TProjectClause::AddProjection(TConstExpressionPtr expression, TString name)
     AddProjection(TNamedItem(std::move(expression), std::move(name)));
 }
 
-TTableSchemaPtr TProjectClause::GetTableSchema() const
+TTableSchemaPtr TProjectClause::GetTableSchema(bool castToQLType) const
 {
     TSchemaColumns result;
+    result.reserve(Projections.size());
 
     for (const auto& item : Projections) {
-        result.emplace_back(item.Name, item.Expression->LogicalType);
+        auto logicalType = castToQLType ? item.Expression->LogicalType : item.Expression->OriginalLogicalType;
+        result.emplace_back(item.Name, std::move(logicalType));
     }
 
     return New<TTableSchema>(std::move(result));
@@ -418,10 +427,10 @@ TTableSchemaPtr TQuery::GetRenamedSchema() const
     return Schema.GetRenamedSchema();
 }
 
-TTableSchemaPtr TQuery::GetTableSchema() const
+TTableSchemaPtr TQuery::GetTableSchema(bool castToQLType) const
 {
     if (ProjectClause) {
-        return ProjectClause->GetTableSchema();
+        return ProjectClause->GetTableSchema(castToQLType);
     }
 
     if (GroupClause) {
@@ -453,10 +462,10 @@ TTableSchemaPtr TFrontQuery::GetRenamedSchema() const
     return Schema;
 }
 
-TTableSchemaPtr TFrontQuery::GetTableSchema() const
+TTableSchemaPtr TFrontQuery::GetTableSchema(bool castToQLType) const
 {
     if (ProjectClause) {
-        return ProjectClause->GetTableSchema();
+        return ProjectClause->GetTableSchema(castToQLType);
     }
 
     if (GroupClause) {
