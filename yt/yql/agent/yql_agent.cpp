b80f62bae0db336452e05f7a95cf9fa58377759f
mpereskokova <mpereskokova@ytsaurus.tech>
2025-02-19 13:53:46 +0300

    [Yql Agent] Fix abortion; Add logging commit_hash:9de4c10d11698c9bfed77365e00d4671de2bee7f

--- yt/yql/agent/yql_agent.cpp
+++ yt/yql/agent/yql_agent.cpp
@@ -449,6 +449,7 @@ private:
 
         try {
             auto abortResult = YqlPlugin_->Abort(queryId);
+            YT_LOG_DEBUG("Plugin abortion is finished (QueryId: %v)", queryId);
             if (auto ysonError = abortResult.YsonError) {
                 error = ConvertTo<TError>(TYsonString(*ysonError));
             }
