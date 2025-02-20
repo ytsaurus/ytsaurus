0bb147a616f221e2bda5877405ca1635089a835e
lukyan <lukyan@ytsaurus.tech>
2025-02-20 10:24:22 +0300

    Move ast helpers to ast file commit_hash:155ffb9b7208f82cbbc726a9be94afbd014d520c

--- yt/yt/library/query/base/ast.h
+++ yt/yt/library/query/base/ast.h
@@ -565,4 +565,28 @@ NYPath::TYPath GetMainTablePath(const TQuery& query);
 
 ////////////////////////////////////////////////////////////////////////////////
 
+NAst::TExpressionPtr BuildAndExpression(
+    TObjectsHolder* holder,
+    NAst::TExpressionPtr lhs,
+    NAst::TExpressionPtr rhs);
+
+NAst::TExpressionPtr BuildOrExpression(
+    TObjectsHolder* holder,
+    NAst::TExpressionPtr lhs,
+    NAst::TExpressionPtr rhs);
+
+NAst::TExpressionPtr BuildConcatenationExpression(
+    TObjectsHolder* holder,
+    NAst::TExpressionPtr lhs,
+    NAst::TExpressionPtr rhs,
+    const TString& separator);
+
+//! For commutative operations only.
+NAst::TExpressionPtr BuildBinaryOperationTree(
+    TObjectsHolder* holder,
+    std::vector<NAst::TExpressionPtr> leaves,
+    EBinaryOp opCode);
+
+////////////////////////////////////////////////////////////////////////////////
+
 } // namespace NYT::NQueryClient::NAst
