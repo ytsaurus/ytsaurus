b60c2f819cb24a0aa5590f2c09c8433edee8b19a
babenko <babenko@ytsaurus.tech>
2025-02-27 20:34:53 +0300

    Schedule global chunk refresh once refresh is re-enabled via dynamic config commit_hash:c45b06be5e19c86764766fbfa681a066769a0194

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -228,6 +228,7 @@ DEFINE_ENUM(EMasterReign,
     ((FixReshardOfOrderedTablesWithHunks)                           (2838))  // akozhikhov
     ((FixTabletSizeCalculationForCellAssignmentWhenMounting)        (2839))  // alexelexa
     ((ResetHunkSpecificMediaAndRecomputeTabletStatistics)           (2840))  // shakurov
+    ((PrepareModifyReplicasRefreshFlagsChecks)                      (2841))  // babenko
     // 25.2 starts here.
     ((Start_25_2)                                                   (2900))  // ponasenko-rs
     ((TabletTransactionSerializationType)                           (2901))  // ponasenko-rs
