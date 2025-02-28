2c26ed775d0b974a48d6e16159c792a63b2dec47
ifsmirnov <ifsmirnov@ytsaurus.tech>
2025-02-28 03:22:06 +0300

    Persist forgotten AuxiliaryTabletStatistics field commit_hash:144754bdcec754d4c4b723d8990723092f8eca55

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -229,6 +229,7 @@ DEFINE_ENUM(EMasterReign,
     ((FixTabletSizeCalculationForCellAssignmentWhenMounting)        (2839))  // alexelexa
     ((ResetHunkSpecificMediaAndRecomputeTabletStatistics)           (2840))  // shakurov
     ((PrepareModifyReplicasRefreshFlagsChecks)                      (2841))  // babenko
+    ((PersistAuxiliaryNodeStatistics)                               (2842))  // ifsmirnov
     // 25.2 starts here.
     ((Start_25_2)                                                   (2900))  // ponasenko-rs
     ((TabletTransactionSerializationType)                           (2901))  // ponasenko-rs
