bc4274d2d0b1a948e12b2834afb79520baaa628f
sabdenovch <sabdenovch@ytsaurus.tech>
2025-03-04 22:25:05 +0300

    YT-24369: Fix `first` function unusability with Push Down Group By commit_hash:1c68a4b5c5c5dc12dc95888cd61e8beb1b03fc02

--- yt/yt/library/query/base/push_down_group_by.cpp
+++ yt/yt/library/query/base/push_down_group_by.cpp
@@ -60,7 +60,7 @@ std::pair<TAggregateItemList, TAggregateItemList> GetPureAndMixedAggregateItems(
     auto purityChecker = TPurityChecker(joinRenamedSchema);
 
     auto isAggregateFunctionGood = [] (TStringBuf function) {
-        return function == "min" || function == "max" || function == "sum";
+        return function == "first" || function == "min" || function == "max" || function == "sum";
     };
 
     for (const auto& aggregate : groupByClause.AggregateItems) {
