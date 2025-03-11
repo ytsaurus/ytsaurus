e06621601229d9dec7ac8def33fa46df2b9a60c8
vityaman <vityaman.dev@yandex.ru>
2025-03-11 17:28:57 +0300

    YQL-19616 Split tests
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/reflect/sql_reflect_ut.cpp
+++ yql/essentials/sql/v1/reflect/sql_reflect_ut.cpp
@@ -7,31 +7,44 @@
 
 using namespace NSQLReflect;
 
-Y_UNIT_TEST_SUITE(SqlReflectTests) {
-    Y_UNIT_TEST(Sandbox) {
-        auto meta = GetLexerGrammar();
+namespace {
+    auto meta = GetLexerGrammar();
+}
 
+Y_UNIT_TEST_SUITE(SqlReflectTests) {
+    Y_UNIT_TEST(Keywords) {
         UNIT_ASSERT_VALUES_EQUAL(meta.Keywords.contains("SELECT"), true);
         UNIT_ASSERT_VALUES_EQUAL(meta.Keywords.contains("INSERT"), true);
         UNIT_ASSERT_VALUES_EQUAL(meta.Keywords.contains("WHERE"), true);
         UNIT_ASSERT_VALUES_EQUAL(meta.Keywords.contains("COMMIT"), true);
+    }
 
+    Y_UNIT_TEST(Punctuation) {
         UNIT_ASSERT_VALUES_EQUAL(meta.Punctuation.contains("LPAREN"), true);
+        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("LPAREN"), "(");
+        
         UNIT_ASSERT_VALUES_EQUAL(meta.Punctuation.contains("MINUS"), true);
-        UNIT_ASSERT_VALUES_EQUAL(meta.Punctuation.contains("NAMESPACE"), true);
+        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("MINUS"), "-");
 
+        UNIT_ASSERT_VALUES_EQUAL(meta.Punctuation.contains("NAMESPACE"), true);
+        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("NAMESPACE"), "::");
+
+    }
+
+    Y_UNIT_TEST(Other) {
         UNIT_ASSERT_VALUES_EQUAL(meta.Other.contains("REAL"), true);
         UNIT_ASSERT_VALUES_EQUAL(meta.Other.contains("STRING_VALUE"), true);
         UNIT_ASSERT_VALUES_EQUAL(meta.Other.contains("STRING_MULTILINE"), false);
 
-        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("LPAREN"), "(");
-        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("MINUS"), "-");
-        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("NAMESPACE"), "::");
-
-        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("FLOAT_EXP"), "E (PLUS | MINUS)? DECDIGITS");
-        UNIT_ASSERT_VALUES_EQUAL(meta.ContentByName.at("STRING_MULTILINE"), "(DOUBLE_COMMAT .*? DOUBLE_COMMAT)+ COMMAT?");
+        UNIT_ASSERT_VALUES_EQUAL(
+            meta.ContentByName.at("FLOAT_EXP"), 
+            "E (PLUS | MINUS)? DECDIGITS");
+        UNIT_ASSERT_VALUES_EQUAL(
+            meta.ContentByName.at("STRING_MULTILINE"), 
+            "(DOUBLE_COMMAT .*? DOUBLE_COMMAT)+ COMMAT?");
         UNIT_ASSERT_VALUES_EQUAL(
             meta.ContentByName.at("REAL"),
             "(DECDIGITS DOT DIGIT* FLOAT_EXP? | DECDIGITS FLOAT_EXP) (F | P (F ('4' | '8') | N)?)?");
     }
+
 } // Y_UNIT_TEST_SUITE(SqlReflectTests)
