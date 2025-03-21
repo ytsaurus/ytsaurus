e5027f3d29f4024f6f00e910a1a4aa52c96de2ba
eshcherbin <eshcherbin@ytsaurus.tech>
2025-03-21 13:29:40 +0300

    YT-24562: Add waiting_for_resources_on_node_timeout to pool config commit_hash:0d2a7a7b4b7990a7f25d6603022ed9cc523cbe25

--- yt/yt/server/lib/misc/interned_attributes.h
+++ yt/yt/server/lib/misc/interned_attributes.h
@@ -591,6 +591,7 @@
     XX(Virtual, virtual) \
     XX(Vital, vital) \
     XX(VitalQueueConsumer, vital_queue_consumer) \
+    XX(WaitingForResourcesOnNodeTimeout, waiting_for_resources_on_node_timeout) \
     XX(Weight, weight) \
     XX(WeakRefCounter, weak_ref_counter) \
     XX(WithAlertsNodeCount, with_alerts_node_count) \
