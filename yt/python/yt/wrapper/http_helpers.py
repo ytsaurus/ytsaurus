941c9719615604b2db019887df3fd9075bc10f98
Andrey Chulkov <achulkov2@tracto.ai>
2025-03-21 18:02:16 +0300

    Add impersonation support in yt wrapper
    Sibling of #1054. Separated for easier cherry-picking. Contains more extensive tests, since this is mostly wrapper-intended functionality.
    
    **New PR to keep authorship after the first commit was reverted.**
    
    * Changelog entry
    Type: feature
    Component: python-sdk
    
    Add support for setting impersonation_user.
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1146
    commit_hash:c384eae3211204d9cd5595eee2c660e97fb4d743

--- yt/python/yt/wrapper/http_helpers.py
+++ yt/python/yt/wrapper/http_helpers.py
@@ -740,9 +740,9 @@ def get_user_name(token=None, headers=None, client=None):
 
     version = get_http_api_version(client=client)
 
+    headers = headers or {}
+
     if version in ("v3", "v4"):
-        if headers is None:
-            headers = {}
         if token is not None:
             headers["Authorization"] = "OAuth " + token.strip()
         data = None
@@ -752,6 +752,10 @@ def get_user_name(token=None, headers=None, client=None):
             return None
         data = "token=" + token.strip()
         verb = "login"
+
+    impersonation_user = get_config(client).get("impersonation_user")
+    if impersonation_user is not None:
+        headers["X-YT-User-Name"] = impersonation_user
 
     response = make_request_with_retries(
         "post",
