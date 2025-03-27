6a3b52bceb8b99da4a30fd19ea656a3be0f9ad0c
alexelexa <alexelexa@ytsaurus.tech>
2025-03-27 18:02:40 +0300

    YT-24553: Do not count compaction reads when calculating read performance counters commit_hash:4dadd9736b87636f97f05ab1f4a515c72fabe021

--- yt/yt/tests/integration/dynamic_tables/test_compaction_partitioning.py
+++ yt/yt/tests/integration/dynamic_tables/test_compaction_partitioning.py
@@ -1093,6 +1093,29 @@ class TestCompactionPartitioning(TestSortedDynamicTablesBase):
 
         wait(_check_stores_queue_size(0))
 
+    @authors("alexelexa")
+    def test_performance_counters(self):
+        sync_create_cells(1)
+
+        self._create_simple_table("//tmp/t")
+        set("//tmp/t/@enable_compaction_and_partitioning", False)
+        set("//tmp/t/@tablet_balancer_config/enable_auto_reshard", False)
+        sync_mount_table("//tmp/t")
+
+        for i in range(10):
+            insert_rows("//tmp/t", [{"key": i, "value": str(i)}])
+            sync_flush_table("//tmp/t")
+
+        assert get("//tmp/t/@tablets/0/statistics/chunk_count") >= 10
+        set("//tmp/t/@enable_compaction_and_partitioning", True)
+
+        set("//tmp/t/@forced_compaction_revision", 1)
+        remount_table("//tmp/t")
+
+        wait(lambda: get("//tmp/t/@tablets/0/statistics/chunk_count") < 10)
+        assert get("//tmp/t/@tablets/0/performance_counters/static_chunk_row_read_count") == 0
+        wait(lambda: get("//tmp/t/@tablets/0/performance_counters/compaction_data_weight_count") == get("//tmp/t/@data_weight"))
+
 
 ################################################################################
 
