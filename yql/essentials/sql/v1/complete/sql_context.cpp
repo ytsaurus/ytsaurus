627aadf92b5504fe163431e285a1d90336349c03
vityaman <vityaman.dev@yandex.ru>
2025-03-28 08:55:59 +0300

    YQL-19747 Fix winnt.h name conflict
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/complete/sql_context.cpp
+++ yql/essentials/sql/v1/complete/sql_context.cpp
@@ -4,13 +4,17 @@
 #include "sql_syntax.h"
 
 #include <yql/essentials/core/issue/yql_issue.h>
+
+#include <util/generic/algorithm.h>
+#include <util/stream/output.h>
+
+#ifdef TOKEN_QUERY // Conflict with the winnt.h
+#undef TOKEN_QUERY
+#endif
 #include <yql/essentials/parser/antlr_ast/gen/v1_antlr4/SQLv1Antlr4Lexer.h>
 #include <yql/essentials/parser/antlr_ast/gen/v1_antlr4/SQLv1Antlr4Parser.h>
 #include <yql/essentials/parser/antlr_ast/gen/v1_ansi_antlr4/SQLv1Antlr4Lexer.h>
 #include <yql/essentials/parser/antlr_ast/gen/v1_ansi_antlr4/SQLv1Antlr4Parser.h>
-
-#include <util/generic/algorithm.h>
-#include <util/stream/output.h>
 
 namespace NSQLComplete {
 
