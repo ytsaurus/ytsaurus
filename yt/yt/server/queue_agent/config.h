22dc19e851f80436e0f7dda068949964f3cca315
apachee <apachee@ytsaurus.tech>
2025-04-02 11:31:00 +0300

    [queues] YT-24642: Flag for switching queue export implementation to old one
    * Changelog entry
    Type: feature
    Component: queue-agent
    
    Add flag to select queue export implementation.
    commit_hash:e1da8573f0713f924376c3c24874e11328201f55

--- yt/yt/server/queue_agent/config.h
+++ yt/yt/server/queue_agent/config.h
@@ -100,6 +100,15 @@ DEFINE_REFCOUNTED_TYPE(TQueueAgentConfig)
 
 ////////////////////////////////////////////////////////////////////////////////
 
+DEFINE_ENUM(EQueueExporterImplementation,
+    // Implementation before YT-23208.
+    (Old)
+    // Implementation after YT-23208.
+    (New)
+);
+
+////////////////////////////////////////////////////////////////////////////////
+
 // NB(apachee): Separate config for exports for future refactoring. See YT-23208.
 
 struct TQueueExporterDynamicConfig
@@ -108,6 +117,9 @@ struct TQueueExporterDynamicConfig
     static const TExponentialBackoffOptions DefaultRetryBackoff;
 
     bool Enable;
+
+    // COMPAT(apachee): To differentiate queue export implementations.
+    EQueueExporterImplementation Implementation;
 
     //! Queue exporter pass period. Defines the minimum duration between 2 consecutive export iterations.
     TDuration PassPeriod;
--- yt/yt/server/queue_agent/private.h
+++ yt/yt/server/queue_agent/private.h
@@ -18,6 +18,8 @@ YT_DEFINE_GLOBAL(const NLogging::TLogger, QueueAgentLogger, "QueueAgent");
 YT_DEFINE_GLOBAL(const NLogging::TLogger, QueueControllerLogger, "QueueController");
 YT_DEFINE_GLOBAL(const NLogging::TLogger, ConsumerControllerLogger, "ConsumerController");
 YT_DEFINE_GLOBAL(const NLogging::TLogger, QueueExporterLogger, "QueueExporter");
+// COMPAT(apachee): For old queue export implementation.
+YT_DEFINE_GLOBAL(const NLogging::TLogger, QueueStaticTableExporterLogger, "QueueStaticTableExporterLogger");
 YT_DEFINE_GLOBAL(const NLogging::TLogger, QueueExportManagerLogger, "QueueExportManager");
 YT_DEFINE_GLOBAL(const NLogging::TLogger, QueueAgentShardingManagerLogger, "QueueAgentShardingManager");
 YT_DEFINE_GLOBAL(const NLogging::TLogger, CypressSynchronizerLogger, "CypressSynchronizer");
@@ -72,6 +74,8 @@ DECLARE_REFCOUNTED_STRUCT(TQueueAgentComponentDynamicConfig)
 DECLARE_REFCOUNTED_CLASS(TDynamicConfigManager)
 
 DECLARE_REFCOUNTED_STRUCT(IQueueExporter)
+// COMPAT(apachee): For old queue export implementation.
+DECLARE_REFCOUNTED_CLASS(TQueueExporterOld)
 
 DECLARE_REFCOUNTED_STRUCT(IQueueExportManager)
 
@@ -142,6 +146,9 @@ DECLARE_REFCOUNTED_STRUCT(IBootstrap)
 
 DECLARE_REFCOUNTED_CLASS(TQueueTabletExportProgress)
 DECLARE_REFCOUNTED_CLASS(TQueueExportProgress)
+// COMPAT(apachee): For old queue export implementation.
+DECLARE_REFCOUNTED_CLASS(TQueueTabletExportProgressOld)
+DECLARE_REFCOUNTED_CLASS(TQueueExportProgressOld)
 
 ////////////////////////////////////////////////////////////////////////////////
 
