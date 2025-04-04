070eba8806db936ceb155ce33fb3c85b2947fb69
lukyan <lukyan@ytsaurus.tech>
2025-04-04 13:41:00 +0300

    YT-23575: Refactor column resolution commit_hash:fc065553c4415716d1b165808c64f1e3622021cb

--- yt/yt/library/query/base/push_down_group_by.cpp
+++ yt/yt/library/query/base/push_down_group_by.cpp
@@ -97,7 +97,7 @@ bool IsGroupKeyTooSelective(const TNamedItemList& groupKey, const TJoinClause& j
     const auto& originalSchema = joinClause.Schema.Original;
 
     THashMap<std::string, int> nameToIndex;
-    for (const auto& descriptor : joinClause.Schema.Mapping) {
+    for (const auto& descriptor : joinClause.Schema.GetOrderedSchemaMapping()) {
         InsertOrCrash(nameToIndex, std::pair{descriptor.Name, descriptor.Index});
     }
 
