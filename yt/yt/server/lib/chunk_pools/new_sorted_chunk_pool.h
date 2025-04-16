72e1eca52550a142f96acb1de82e859e09df7c1f
apollo1321 <apollo1321@ytsaurus.tech>
2025-04-17 00:11:55 +0300

    Drop unused fields in sorted pool
    Drop unused fields in sorted pool
    commit_hash:bf4f4e5600e1f36f485eb38562f372fce9137d50

--- yt/yt/server/lib/chunk_pools/new_sorted_chunk_pool.h
+++ yt/yt/server/lib/chunk_pools/new_sorted_chunk_pool.h
@@ -1,7 +1,5 @@
 #pragma once
 
-#include "new_sorted_job_builder.h"
-
 #include "sorted_chunk_pool.h"
 
 namespace NYT::NChunkPools {
@@ -11,7 +9,7 @@ namespace NYT::NChunkPools {
 ISortedChunkPoolPtr CreateNewSortedChunkPool(
     const TSortedChunkPoolOptions& options,
     IChunkSliceFetcherFactoryPtr chunkSliceFetcherFactory,
-    TInputStreamDirectory dataSourceDirectory);
+    TInputStreamDirectory inputStreamDirectory);
 
 ////////////////////////////////////////////////////////////////////////////////
 
