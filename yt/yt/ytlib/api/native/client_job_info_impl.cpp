92a7aa9864fc33b43eabca3027d40b2ecb363052
Aleksandr Gaev <aleksandr.gaev@tracto.ai>
2025-04-17 20:19:48 +0200

    Close access to //sys/operations, use operations archive client in get-job to access it

--- yt/yt/ytlib/api/native/client_job_info_impl.cpp
+++ yt/yt/ytlib/api/native/client_job_info_impl.cpp
@@ -2395,7 +2395,7 @@ TListJobsResult TClient::DoListJobs(
     // Get jobs from controller agent.
     auto controllerAgentAddress = FindControllerAgentAddressFromCypress(
         operationId,
-        MakeStrong(this));
+        GetOperationsArchiveClient());
     auto controllerAgentResultFuture = DoListJobsFromControllerAgentAsync(
         operationId,
         controllerAgentAddress,
@@ -2591,7 +2591,7 @@ std::optional<TJob> TClient::DoGetJobFromControllerAgent(
 {
     auto controllerAgentAddress = FindControllerAgentAddressFromCypress(
         operationId,
-        MakeStrong(this));
+        GetOperationsArchiveClient());
     if (!controllerAgentAddress) {
         return {};
     }
