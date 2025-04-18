00875b235beac896fca987bcba1201e6bee19a8c
vityaman <vityaman.dev@yandex.ru>
2025-04-18 16:14:45 +0300

    YQL-19747 Remove Insert/Remove Namespace
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/complete/name/service/namespacing.cpp
+++ yql/essentials/sql/v1/complete/name/service/namespacing.cpp
@@ -32,37 +32,4 @@ namespace NSQLComplete {
         return pragma;
     }
 
-    void InsertNamespace(TString& name, const TStringBuf delimeter, const TNamespaced& namespaced) {
-        if (!namespaced.Namespace.empty()) {
-            name.prepend(delimeter);
-            name.prepend(namespaced.Namespace);
-        }
-    }
-
-    void InsertNamespace(TGenericName& name, const TNameRequest& request) {
-        std::visit([&](auto& name) -> size_t {
-            using T = std::decay_t<decltype(name)>;
-            if constexpr (std::is_same_v<T, TPragmaName>) {
-                InsertNamespace(name.Indentifier, ".", *request.Constraints.Pragma);
-            }
-            if constexpr (std::is_same_v<T, TFunctionName>) {
-                InsertNamespace(name.Indentifier, "::", *request.Constraints.Function);
-            }
-            return 0;
-        }, name);
-    }
-
-    void InsertNamespace(TVector<TGenericName>& names, const TNameRequest& request) {
-        for (auto& name : names) {
-            InsertNamespace(name, request);
-        }
-    }
-
-    void RemoveNamespace(TString& name, const TStringBuf delimeter, const TNamespaced& namespaced) {
-        if (namespaced.Namespace.empty()) {
-            return;
-        }
-        name.remove(0, namespaced.Namespace.size() + delimeter.size());
-    }
-
 } // namespace NSQLComplete
