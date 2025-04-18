932a4c227f96952a252b17d374934499465168fe
vityaman <vityaman.dev@yandex.ru>
2025-04-18 17:05:59 +0300

    YQL-19747 Pass ranking to Union NameService
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/complete/name/service/union/name_service.h
+++ yql/essentials/sql/v1/complete/name/service/union/name_service.h
@@ -1,9 +1,11 @@
 #pragma once
 
 #include <yql/essentials/sql/v1/complete/name/service/name_service.h>
+#include <yql/essentials/sql/v1/complete/name/service/ranking/ranking.h>
 
 namespace NSQLComplete {
 
-    INameService::TPtr MakeUnionNameService(TVector<INameService::TPtr> children);
+    INameService::TPtr MakeUnionNameService(
+        TVector<INameService::TPtr> children, IRanking::TPtr ranking);
 
 } // namespace NSQLComplete
