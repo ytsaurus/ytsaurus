35b1d987cbc437b3abf313cbee4dfad00334f86c
ifsmirnov <ifsmirnov@ytsaurus.tech>
2025-04-22 19:51:34 +0300

    Use sparse profiling counters for bundle controller alarms commit_hash:bef90f5d1d51d571339dd51c44a4c379a0e4072a

--- yt/yt/server/cell_balancer/bundle_controller.cpp
+++ yt/yt/server/cell_balancer/bundle_controller.cpp
@@ -1001,6 +1001,7 @@ private:
             auto counter = Profiler
                 .WithTag("tablet_cell_bundle", bundleName)
                 .WithTag("alarm_id", alert.Id)
+                .WithSparse()
                 .Counter("/scan_bundles_alarms_count");
             it = bundle.IdToCounter.emplace(alert.Id, std::move(counter)).first;
         }
