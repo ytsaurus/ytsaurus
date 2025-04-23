1e62adfc8793664cf79385f17c2f2305e74774bb
Nikita Sokolov <faucct@tracto.ai>
2025-04-23 13:07:23 +0200

    transaction commits and aborts should validate ACLs

--- yt/yt/server/master/transaction_server/transaction_manager.cpp
+++ yt/yt/server/master/transaction_server/transaction_manager.cpp
@@ -698,6 +698,9 @@ public:
 
         auto transactionId = transaction->GetId();
 
+        const auto& securityManager = Bootstrap_->GetSecurityManager();
+        securityManager->ValidatePermission(transaction, EPermission::Write);
+
         auto state = transaction->GetPersistentState();
         if (state == ETransactionState::Committed) {
             YT_LOG_DEBUG("Transaction is already committed (TransactionId: %v)",
@@ -851,7 +854,6 @@ public:
             options.CommitTimestampClusterTag,
             time);
 
-        const auto& securityManager = Bootstrap_->GetSecurityManager();
         securityManager->ChargeUser(user, {EUserWorkloadType::Write, 1, time});
     }
 
@@ -1867,6 +1869,7 @@ public:
                 transactionId,
                 prerequisiteTransactionIds,
                 std::move(mutationId),
+                NRpc::GetCurrentAuthenticationIdentity(),
                 isRetry)
                     .AsyncVia(EpochAutomatonInvoker_));
     }
@@ -1875,6 +1878,7 @@ public:
         TTransactionId transactionId,
         std::vector<TTransactionId> prerequisiteTransactionIds,
         NRpc::TMutationId mutationId,
+        NRpc::TAuthenticationIdentity authenticationIdentity,
         bool isRetry,
         const TErrorOr<TTimestamp>& timestampOrError)
     {
@@ -1896,14 +1900,14 @@ public:
                 transactionId,
                 std::move(prerequisiteTransactionIds),
                 commitTimestamp,
-                NRpc::GetCurrentAuthenticationIdentity());
+                authenticationIdentity);
         }
 
         auto request = BuildCommitCypressTransactionRequest(
             transactionId,
             commitTimestamp,
             prerequisiteTransactionIds,
-            NRpc::GetCurrentAuthenticationIdentity());
+            authenticationIdentity);
 
         auto mutation = CreateMutation(HydraManager_, request);
         mutation->SetMutationId(mutationId, isRetry);
@@ -2519,6 +2523,10 @@ private:
 
         auto transactionId = FromProto<TTransactionId>(request->transaction_id());
         auto* transaction = GetTransactionOrThrow(transactionId);
+
+        const auto& securityManager = Bootstrap_->GetSecurityManager();
+        TAuthenticatedUserGuard userGuard(securityManager);
+        securityManager->ValidatePermission(transaction, EPermission::Write);
 
         auto prerequisiteTransactionIds = FromProto<std::vector<TTransactionId>>(request->prerequisite_transaction_ids());
 
