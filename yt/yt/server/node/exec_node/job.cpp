5e2605c701a250c616b4b1f6abbfa3a71623f404
pogorelov <pogorelov@ytsaurus.tech>
2025-04-25 09:57:45 +0300

    Fix segfault in exe node commit_hash:dcf84ac49505996d36a8847f7683130216c4352d

--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -1480,6 +1480,10 @@ TBriefJobInfo TJob::GetBriefInfo() const
     }
 
     auto tryGetMonitoringDescriptor = [&] () -> std::optional<std::string> {
+        if (!UserJobSpec_) {
+            return std::nullopt;
+        }
+
         const auto& monitoringConfig = UserJobSpec_->monitoring_config();
         if (!monitoringConfig.enable()) {
             return std::nullopt;
