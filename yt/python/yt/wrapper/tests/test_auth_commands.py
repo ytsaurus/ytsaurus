8570abaaab24dd2950a4733b054d2f22162e39a9
pavel-bash <pavel@nebius.com>
2025-04-28 14:19:19 +0200

    PR fix

--- yt/python/yt/wrapper/tests/test_auth_commands.py
+++ yt/python/yt/wrapper/tests/test_auth_commands.py
@@ -15,16 +15,16 @@ class TestAuthCommands(object):
     @authors("pavel-bash")
     @add_failed_operation_stderrs_to_error_message
     def test_password_strength_validation(self):
-        password = ''.join(random.choice(string.ascii_letters) for i in range(11))
+        password = "".join(random.choice(string.ascii_letters) for i in range(11))
         yt.set_user_password("admin", password)
         with set_config_option("enable_password_strength_validation", True):
             with pytest.raises(ValueError):
                 yt.set_user_password("admin", password)
 
-        password = ''.join(random.choice(string.ascii_letters) for i in range(12))
+        password = "".join(random.choice(string.ascii_letters) for i in range(12))
         yt.set_user_password("admin", password)
 
-        password = ''.join(random.choice(string.ascii_letters) for i in range(129))
+        password = "".join(random.choice(string.ascii_letters) for i in range(129))
         yt.set_user_password("admin", password)
         with set_config_option("enable_password_strength_validation", True):
             with pytest.raises(ValueError):
