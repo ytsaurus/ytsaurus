79fdfdf62386bce00528b465dbc13f55d2543125
savrus <savrus@ytsaurus.tech>
2025-04-30 15:52:07 +0300

    YT-24945 Restart chaos agent and table puller on remount with new mount config commit_hash:7873ffa478d3aae73de85a528481db9c6b0a8ff3

--- yt/yt/server/node/tablet_node/tablet_manager.cpp
+++ yt/yt/server/node/tablet_node/tablet_manager.cpp
@@ -1419,6 +1419,12 @@ private:
                 StopTableReplicaEpoch(&replicaInfo);
                 StartTableReplicaEpoch(tablet, &replicaInfo);
             }
+
+            if (auto replicationCardId = tablet->GetReplicationCardId()) {
+                StopChaosReplicaEpoch(tablet);
+                RemoveChaosAgent(tablet);
+                StartChaosReplicaEpoch(tablet, replicationCardId);
+            }
         }
     }
 
@@ -4172,6 +4178,12 @@ private:
             CreateSerializedInvoker(Bootstrap_->GetTableReplicatorPoolInvoker()),
             Bootstrap_->GetInThrottler(EWorkloadCategory::SystemTabletReplication),
             Bootstrap_->GetNodeMemoryUsageTracker()->WithCategory(EMemoryCategory::ChaosReplicationIncoming)));
+    }
+
+    void RemoveChaosAgent(TTablet* tablet)
+    {
+        tablet->SetChaosAgent(nullptr);
+        tablet->SetTablePuller(nullptr);
     }
 
     void StartChaosReplicaEpoch(TTablet* tablet, TReplicationCardId replicationCardId)
