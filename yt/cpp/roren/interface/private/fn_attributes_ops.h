574a12e2a9ac6b3879efafe06c2b41d2c0e5aeee
yurial <yurial@ytsaurus.tech>
2025-05-07 13:42:50 +0300

    Add flow/test_medium/30_stateful commit_hash:70ab65b6d512a9dbe562ce45c3df494dfbe62eee

--- yt/cpp/roren/interface/private/fn_attributes_ops.h
+++ yt/cpp/roren/interface/private/fn_attributes_ops.h
@@ -18,6 +18,10 @@ public:
     static void SetIsMove(TFnAttributes& attributes, bool isMove = true);
     static bool GetIsMove(const TFnAttributes& attributes);
     static const std::vector<TString> GetResourceFileList(const TFnAttributes& attributes);
+    static void SetStateIds(TFnAttributes& attributes, decltype(TFnAttributes::StateIds_) stateIds);
+    static decltype(TFnAttributes::StateIds_) GetStateIds(const TFnAttributes& attributes);
+    static void SetTimerIds(TFnAttributes& attributes, decltype(TFnAttributes::TimerIds_) timerIds);
+    static decltype(TFnAttributes::TimerIds_) GetTimerIds(const TFnAttributes& attributes);
 };
 
 ////////////////////////////////////////////////////////////////////////////////
