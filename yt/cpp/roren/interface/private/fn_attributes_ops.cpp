b275f303b376735b4f3c2e92e994ba103c82463b
yurial <yurial@ytsaurus.tech>
2025-05-07 13:42:50 +0300

    Add flow/test_medium/30_stateful commit_hash:70ab65b6d512a9dbe562ce45c3df494dfbe62eee

--- yt/cpp/roren/interface/private/fn_attributes_ops.cpp
+++ yt/cpp/roren/interface/private/fn_attributes_ops.cpp
@@ -12,6 +12,8 @@ void TFnAttributesOps::Merge(TFnAttributes& destination, const TFnAttributes& so
     for (const auto& resourceFile : source.ResourceFileList_) {
         destination.ResourceFileList_.push_back(resourceFile);
     }
+    destination.StateIds_.insert(source.StateIds_.begin(), source.StateIds_.end());
+    destination.TimerIds_.insert(source.TimerIds_.begin(), source.TimerIds_.end());
 }
 
 bool TFnAttributesOps::GetIsPure(const TFnAttributes& attributes)
@@ -32,6 +34,26 @@ bool TFnAttributesOps::GetIsMove(const TFnAttributes& attributes)
 const std::vector<TString> TFnAttributesOps::GetResourceFileList(const TFnAttributes& attributes)
 {
     return attributes.ResourceFileList_;
+}
+
+void TFnAttributesOps::SetStateIds(TFnAttributes& attributes, decltype(TFnAttributes::StateIds_) stateIds)
+{
+    attributes.StateIds_ = std::move(stateIds);
+}
+
+decltype(TFnAttributes::StateIds_) TFnAttributesOps::GetStateIds(const TFnAttributes& attributes)
+{
+    return attributes.StateIds_;
+}
+
+void TFnAttributesOps::SetTimerIds(TFnAttributes& attributes, decltype(TFnAttributes::TimerIds_) timerIds)
+{
+    attributes.TimerIds_ = std::move(timerIds);
+}
+
+decltype(TFnAttributes::TimerIds_) TFnAttributesOps::GetTimerIds(const TFnAttributes& attributes)
+{
+    return attributes.TimerIds_;
 }
 
 ////////////////////////////////////////////////////////////////////////////////
