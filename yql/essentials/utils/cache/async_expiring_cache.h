dff3e284411aee790aea745c8ca98471b901b997
vityaman <vityaman.dev@yandex.ru>
2025-05-09 14:41:16 +0300

    YQL-19747: Add bad stress test
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/utils/cache/async_expiring_cache.h
+++ yql/essentials/utils/cache/async_expiring_cache.h
@@ -1,5 +1,142 @@
 #pragma once
 
+#include <concepts>
+
+#include <library/cpp/threading/future/future.h>
+#include <library/cpp/containers/concurrent_hash/concurrent_hash.h>
+
+#include <util/datetime/base.h>
+#include <util/system/rwlock.h>
+
 namespace NYql {
 
-}
+    namespace NPrivate {
+
+        template <class T>
+        concept CHashable = requires(const T& x) {
+            { std::hash<T>()(x) } -> std::convertible_to<std::size_t>;
+        };
+
+        template <class T>
+        concept CCacheKey = std::regular<T> && CHashable<T>;
+
+        template <class T>
+        concept CCacheValue = std::regular<T>;
+
+    } // namespace NPrivate
+
+    template <NPrivate::CCacheKey TKey, NPrivate::CCacheValue TValue>
+    class TAsyncExpiringCache: public TThrRefBase {
+    private:
+        struct TEntry {
+            NThreading::TFuture<TValue> Value;
+            bool IsReferenced = true;
+            bool IsUpdated = true;
+        };
+
+        using TStorage = TConcurrentHashMap<TKey, TEntry>;
+        using TBucket = typename TStorage::TBucket;
+        using TBucketGuard = typename TStorage::TBucketGuard;
+        using TActualMap = typename TStorage::TActualMap;
+
+    public:
+        using TPtr = TIntrusivePtr<TAsyncExpiringCache>;
+        using TQuery = std::function<NThreading::TFuture<TValue>(const TKey&)>;
+
+        TAsyncExpiringCache(
+            size_t updateFrequency,
+            size_t evictionFrequency,
+            TQuery query)
+            : Query_(std::move(query))
+            , UpdateFrequency_(updateFrequency)
+            , EvictionFrequency_(evictionFrequency)
+        {
+        }
+
+        NThreading::TFuture<TValue> Get(const TKey& key) {
+            TBucket& bucket = Storage_.GetBucketForKey(key);
+            TBucketGuard guard(bucket.GetMutex());
+
+            if (TEntry* entry = bucket.TryGetUnsafe(key)) {
+                entry->IsReferenced = true;
+                return entry->Value;
+            }
+
+            NThreading::TFuture<TValue> value = Query_(key);
+
+            TActualMap& map = bucket.GetMap();
+            map[key] = TEntry{
+                .Value = value,
+                .IsReferenced = true,
+                .IsUpdated = true,
+            };
+
+            return value;
+        }
+
+        void OnTick() {
+            Tick_.fetch_add(1);
+
+            if (Tick_ % UpdateFrequency_ == 0) {
+                OnUpdate();
+            }
+
+            if (Tick_ % EvictionFrequency_ == 0) {
+                OnEvict();
+            }
+        }
+
+    private:
+        void OnUpdate() {
+            ForEachBucket([query = Query_](TActualMap& bucket) {
+                for (auto& [key, entry] : bucket) {
+                    if (entry.IsUpdated) {
+                        entry.IsUpdated = false;
+                        continue;
+                    }
+
+                    entry.IsUpdated = true;
+                    entry.Value = query(key);
+                }
+            });
+        }
+
+        void OnEvict() {
+            ForEachBucket([](TActualMap& bucket) {
+                TVector<TKey> abandoned;
+                for (auto& [key, entry] : bucket) {
+                    if (entry.IsReferenced) {
+                        entry.IsReferenced = false;
+                    } else {
+                        abandoned.emplace_back(key);
+                    }
+                }
+                for (const TKey& key : abandoned) {
+                    bucket.erase(key);
+                }
+            });
+        }
+
+        template <class Action>
+        void ForEachBucket(Action&& action) {
+            for (TBucket& bucket : Storage_.Buckets) {
+                TBucketGuard guard(bucket.GetMutex());
+                action(bucket.GetMap());
+            }
+        }
+
+        TQuery Query_;
+        TStorage Storage_;
+
+        std::atomic<size_t> Tick_ = 0;
+        size_t UpdateFrequency_;
+        size_t EvictionFrequency_;
+    };
+
+    struct TAsyncExpiringCacheConfig {
+        TDuration TickPeriod = TDuration::Seconds(5);
+        size_t UpdateFrequency = 1;
+        size_t EvictionFrequency = 3;
+    };
+
+} // namespace NYql
--- yql/essentials/utils/cache/async_expiring_cache_ut.cpp
+++ yql/essentials/utils/cache/async_expiring_cache_ut.cpp
@@ -2,11 +2,97 @@
 
 #include <library/cpp/testing/unittest/registar.h>
 
+#include <library/cpp/threading/future/async.h>
+#include <library/cpp/threading/cancellation/cancellation_token.h>
+#include <library/cpp/iterator/enumerate.h>
+
+#include <util/system/mutex.h>
+#include <util/thread/pool.h>
+#include <util/random/normal.h>
+
+#include <random>
+
 using namespace NYql;
 
 Y_UNIT_TEST_SUITE(TAsyncExpiringCacheTests) {
 
-    Y_UNIT_TEST(HelloWorld) {
+    Y_UNIT_TEST(Stress) {
+        using K = size_t;
+        using V = size_t;
+
+        TAsyncExpiringCacheConfig config = {
+            .TickPeriod = TDuration::MilliSeconds(100),
+            .UpdateFrequency = 1,
+            .EvictionFrequency = 3,
+        };
+
+        TMutex mutex;
+        std::mt19937 random(213131);
+        std::bernoulli_distribution failure(0.75);
+        std::uniform_int_distribution<int> latency(0, 10);
+
+        const auto async = [&](auto& pool, auto f) {
+            return NThreading::Async(f, *pool);
+        };
+
+        const auto service_pool = CreateThreadPool(/* threadCount = */ 128);
+
+        const auto serve = [&](K key) -> NThreading::TFuture<V> {
+            return async(service_pool, [&, key]() {
+                TDuration delay;
+                bool is_failed;
+                with_lock (mutex) {
+                    delay = TDuration::MilliSeconds(latency(random));
+                    is_failed = failure(random);
+                }
+
+                Sleep(delay);
+
+                if (is_failed) {
+                    throw yexception() << "o_o";
+                }
+
+                return key;
+            });
+        };
+
+        auto cache = MakeIntrusive<TAsyncExpiringCache<K, V>>(
+            config.UpdateFrequency,
+            config.EvictionFrequency,
+            serve);
+
+        NThreading::TCancellationTokenSource activity;
+        auto refresher = async(service_pool, [config, cache, token = activity.Token()]() {
+            while (!token.IsCancellationRequested()) {
+                Sleep(config.TickPeriod);
+                cache->OnTick();
+            }
+        });
+
+        const auto client_pool = CreateThreadPool(/* threadCount = */ 8);
+
+        const auto map = [](K key) -> K { return key / 10 % 1000; };
+
+        TVector<NThreading::TFuture<V>> futures;
+        for (size_t i = 0; i < 10'000; ++i) {
+            futures.emplace_back(async(client_pool, [cache, i, map]() {
+                return cache->Get(map(i));
+            }));
+        }
+        NThreading::WaitAll(futures).Wait(TDuration::Seconds(32));
+
+        for (auto [i, f] : Enumerate(futures)) {
+            UNIT_ASSERT(f.IsReady());
+            if (f.HasValue()) {
+                UNIT_ASSERT_VALUES_EQUAL(map(i), f.GetValue());
+            } else {
+                UNIT_ASSERT(f.HasException());
+                UNIT_ASSERT_EXCEPTION_CONTAINS(f.TryRethrow(), yexception, "o_o");
+            }
+        }
+
+        activity.Cancel();
+        refresher.Wait(TDuration::Seconds(8));
     }
 
 } // Y_UNIT_TEST_SUITE(TAsyncExpiringCacheTests)
