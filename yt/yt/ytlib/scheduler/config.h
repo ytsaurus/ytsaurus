e5a97dc160e9509578e30a97d81bffda223b4751
Nikita Sokolov <faucct@tracto.ai>
2025-05-14 16:51:07 +0200

    satisfy comments

--- yt/yt/ytlib/scheduler/config.h
+++ yt/yt/ytlib/scheduler/config.h
@@ -1421,7 +1421,7 @@ struct TUserJobSpec
     //! Command line arguments for the GPU check binary.
     std::optional<std::vector<TString>> GpuCheckBinaryArgs;
 
-    int CookieGroupSize;
+    int CookieGroupSize; //! Job replicas processing the cookie.
 
     //! Force running speculative job after this timeout. Has higher priority than `JobSpeculationTimeout`
     //! from TOperationBaseSpec.
