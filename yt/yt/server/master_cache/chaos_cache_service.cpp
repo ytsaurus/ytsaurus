688d6117cf2bd858e39521a71b6595db0b3c2d38
osidorkin <osidorkin@ytsaurus.tech>
2025-05-16 20:26:47 +0300

    YT-24924: Extend replication card fetch options on master caches when replication progress is requested so both rpc_proxy requests and tablet node requests could be served from the same cache entry commit_hash:d21491dbb50497dde2d871faf81b2ba5480373ef

--- yt/yt/server/master_cache/chaos_cache_service.cpp
+++ yt/yt/server/master_cache/chaos_cache_service.cpp
@@ -147,6 +147,20 @@ IReplicationCardsWatcherClientPtr CreateReplicationCardsWatcherClientWithCallbac
     return watcherClient;
 }
 
+const TReplicationCardFetchOptions& ExtendFetchOptions(const TReplicationCardFetchOptions& fetchOptions)
+{
+    if (MinimalFetchOptions.Contains(fetchOptions)) {
+        return MinimalFetchOptions;
+    }
+
+    if (FetchOptionsWithProgress.Contains(fetchOptions)) {
+        return FetchOptionsWithProgress;
+    }
+
+    // Seems to be request from master.
+    return fetchOptions;
+}
+
 ////////////////////////////////////////////////////////////////////////////////
 
 class TChaosCacheService
@@ -217,9 +231,7 @@ DEFINE_RPC_SERVICE_METHOD(TChaosCacheService, GetReplicationCard)
         ? request->refresh_era()
         : InvalidReplicationEra;
 
-    const auto& extendedFetchOptions = MinimalFetchOptions.Contains(fetchOptions)
-        ? MinimalFetchOptions
-        : fetchOptions;
+    const auto& extendedFetchOptions = ExtendFetchOptions(fetchOptions);
 
     TFuture<TReplicationCardPtr> replicationCardFuture;
     const auto& requestHeader = context->GetRequestHeader();
