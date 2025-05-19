e391ce7e33126056fce0ca5eaf023e9eaa74f4f9
Nikita Sokolov <faucct@tracto.ai>
2025-05-19 10:33:55 +0200

    ValidateCookieGroupSize

--- yt/yt/server/controller_agent/controllers/operation_controller_detail.cpp
+++ yt/yt/server/controller_agent/controllers/operation_controller_detail.cpp
@@ -734,6 +734,18 @@ TOperationControllerInitializeResult TOperationControllerBase::InitializeRevivin
     return result;
 }
 
+void TOperationControllerBase::ValidateCookieGroupSize()
+{
+    if (GetOffloadingPoolTrees().size() > 1) {
+        for (const auto& userJobSpec : GetUserJobSpecs()) {
+            if (userJobSpec->CookieGroupSize > 1) {
+                THROW_ERROR_EXCEPTION("Cannot combine offloading pool trees and cookie_group_size")
+                    << TErrorAttribute("task_title", userJobSpec->TaskTitle);
+            }
+        }
+    }
+}
+
 void TOperationControllerBase::ValidateSecureVault()
 {
     if (!SecureVault_) {
@@ -753,6 +765,7 @@ TOperationControllerInitializeResult TOperationControllerBase::InitializeClean()
         Spec_->Title);
 
     auto initializeAction = BIND([this_ = MakeStrong(this), this] {
+        ValidateCookieGroupSize();
         ValidateSecureVault();
         InitializeClients();
         InitializeInputTransactions();
--- yt/yt/server/controller_agent/controllers/operation_controller_detail.h
+++ yt/yt/server/controller_agent/controllers/operation_controller_detail.h
@@ -716,6 +716,7 @@ protected:
     void PrepareInputTables();
     bool HasDiskRequestsWithSpecifiedAccount() const;
     void InitAccountResourceUsageLeases();
+    void ValidateCookieGroupSize();
     void ValidateSecureVault();
 
     // Preparation.
