0c91cccf59b4dff16b2696dec778311a2920cb7a
Anton Romanov <Anton.Romanov@ydb.tech>
2025-05-21 14:28:07 +0300

    Fix multilevel optionals.
    Type: fix
    Component: query-tracker
    
    ---
    Co-authored-by: Tony-Romanov [150126326+Tony-Romanov@users.noreply.github.com]
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1272
    commit_hash:5846c585acd406fc24ce013297bd0e96efae050a

--- yt/yql/tests/agent/test_simple.py
+++ yt/yql/tests/agent/test_simple.py
@@ -228,6 +228,17 @@ class TestSimpleQueriesYql(TestQueriesYqlBase):
                "Yson": [7, 'str', -3.14]
                }])
 
+    @authors("a-romanov")
+    def test_double_optional(self, query_tracker, yql_agent):
+        self._test_simple_query("""
+            $list = ["abc"u, null];
+            select $list, $list[0], $list[1], $list[2],
+            (just($list[0]), just($list[1]), just($list[2])),
+            {$list[0], $list[1], $list[2]};
+        """, [{'column0': ['abc', None], 'column1': ['abc'], 'column2': [None], 'column3': None,
+               'column4': [[['abc']], [[None]], [None]],
+               'column5': [[None, None], [[None], None], [['abc'], None]]}])
+
 
 class TestYqlAgentBan(TestQueriesYqlBase):
     NUM_YQL_AGENTS = 1
