f892ae7f813571a2f6f5b1d13d8f2d3630172b7f
vityaman <vityaman.dev@yandex.ru>
2025-05-26 15:21:16 +0300

    YQL-19747: Pass tables via argv
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/complete/check/check_complete.h
+++ yql/essentials/sql/v1/complete/check/check_complete.h
@@ -1,9 +1,14 @@
 #pragma once
 
 #include <util/generic/string.h>
+#include <util/generic/hash.h>
+#include <util/generic/vector.h>
 
 namespace NSQLComplete {
 
-    bool CheckComplete(TStringBuf query, TString& error);
+    bool CheckComplete(
+        TStringBuf query,
+        THashMap<TString, TVector<TString>> tablesByCluster,
+        TString& error);
 
 } // namespace NSQLComplete
