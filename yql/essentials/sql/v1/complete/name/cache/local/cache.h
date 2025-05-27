a40616b41d8cc6d91bc0a7076a55d376c091b5dc
vityaman <vityaman.dev@yandex.ru>
2025-05-27 19:29:08 +0300

    YQL-19747: Test local cache
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/complete/name/cache/local/cache.h
+++ yql/essentials/sql/v1/complete/name/cache/local/cache.h
@@ -8,75 +8,86 @@
 namespace NSQLComplete {
 
     struct TLocalCacheConfiguration {
-        size_t Capacity = 0;
-        TDuration TTL = TDuration::Seconds(1);
+        size_t Capacity = 256 * 1024 * 1024;
+        TDuration TTL = TDuration::Seconds(8);
     };
+
+    namespace NPrivate {
+
+        template <CCacheKey TKey, CCacheValue TValue, CSizeProvider<TValue> TSizeProvider>
+        class TLocalCache: public ICache<TKey, TValue> {
+        private:
+            using TEntry = ICache<TKey, TValue>::TEntry;
+
+            struct TCell {
+                TValue Value;
+                TInstant Death;
+            };
+
+            struct TEntrySizeProvider {
+                size_t operator()(const TCell& entry) const {
+                    return TSizeProvider()(entry.Value);
+                };
+            };
+
+            using TCache = TThreadSafeLFUCacheWithSizeProvider<TKey, TCell, TEntrySizeProvider, TKey>;
+
+            class TCallbacks: public TCache::ICallbacks {
+            public:
+                TKey GetKey(TKey key) const override {
+                    return key;
+                }
+
+                TCell* CreateObject(TKey /* value */) const override {
+                    Y_ABORT();
+                }
+            };
+
+        public:
+            TLocalCache(
+                TIntrusivePtr<ITimeProvider> clock,
+                TLocalCacheConfiguration config)
+                : Clock_(std::move(clock))
+                , Config_(std::move(config))
+                , Origin_(Callbacks_)
+            {
+            }
+
+            NThreading::TFuture<TEntry> Get(const TKey& key) const override {
+                TEntry entry;
+                if (typename TCache::TPtr ptr = Origin_.GetOrNull(key)) {
+                    entry.Value = ptr->Value;
+                    entry.IsExpired = (ptr->Death < Clock_->Now());
+                }
+                return NThreading::MakeFuture(std::move(entry));
+            }
+
+            NThreading::TFuture<void> Update(const TKey& key, TValue value) const override {
+                TCell* entry = new TCell{
+                    .Value = std::move(value),
+                    .Death = Clock_->Now() + Config_.TTL,
+                };
+                Origin_.Update(key, typename TCache::TPtr(entry));
+                return NThreading::MakeFuture();
+            }
+
+        private:
+            TIntrusivePtr<ITimeProvider> Clock_;
+            TLocalCacheConfiguration Config_;
+            TCallbacks Callbacks_;
+            mutable TCache Origin_;
+        };
+
+    } // namespace NPrivate
 
     template <
         NPrivate::CCacheKey TKey,
         NPrivate::CCacheValue TValue,
         NPrivate::CSizeProvider<TValue> TSizeProvider = TUniformSizeProvider<TValue>>
-    class TLocalCache: public ICache<TKey, TValue> {
-    private:
-        using TEntry = ICache<TKey, TValue>::TEntry;
-
-        struct TCell {
-            TValue Value;
-            TInstant Death;
-        };
-
-        struct TEntrySizeProvider {
-            size_t operator()(const TCell& entry) const {
-                return TSizeProvider()(entry.Value);
-            };
-        };
-
-        using TCache = TThreadSafeLFUCacheWithSizeProvider<TKey, TCell, TEntrySizeProvider, TKey>;
-
-        class TCallbacks: public TCache::ICallbacks {
-        public:
-            TKey GetKey(TKey key) const override {
-                return key;
-            }
-
-            TCell* CreateObject(TKey /* value */) const override {
-                Y_ABORT();
-            }
-        };
-
-    public:
-        TLocalCache(
-            TIntrusivePtr<ITimeProvider> clock,
-            TLocalCacheConfiguration config)
-            : Clock_(std::move(clock))
-            , Config_(std::move(config))
-            , Origin_(Callbacks_)
-        {
-        }
-
-        NThreading::TFuture<TEntry> Get(const TKey& key) const override {
-            TEntry entry;
-            if (typename TCache::TPtr ptr = Origin_.GetOrNull(key)) {
-                entry.Value = ptr->Value;
-                entry.IsExpired = (ptr->Death < Clock_->Now());
-            }
-            return NThreading::MakeFuture(std::move(entry));
-        }
-
-        NThreading::TFuture<void> Update(const TKey& key, TValue value) const override {
-            TCell* entry = new TCell{
-                .Value = std::move(value),
-                .Death = Clock_->Now() + Config_.TTL,
-            };
-            Origin_.Update(key, typename TCache::TPtr(entry));
-            return NThreading::MakeFuture();
-        }
-
-    private:
-        TIntrusivePtr<ITimeProvider> Clock_;
-        TLocalCacheConfiguration Config_;
-        TCallbacks Callbacks_;
-        mutable TCache Origin_;
-    };
+    ICache<TKey, TValue>::TPtr MakeLocalCache(
+        TIntrusivePtr<ITimeProvider> clock,
+        TLocalCacheConfiguration config) {
+        return new NPrivate::TLocalCache<TKey, TValue, TSizeProvider>(std::move(clock), std::move(config));
+    }
 
 } // namespace NSQLComplete
--- yql/essentials/sql/v1/complete/name/cache/local/cache_ut.cpp
+++ yql/essentials/sql/v1/complete/name/cache/local/cache_ut.cpp
@@ -6,11 +6,66 @@
 
 using namespace NSQLComplete;
 
+class TPausedClock: public ITimeProvider {
+public:
+    TInstant Now() override {
+        return Now_;
+    }
+
+    void Skip(TDuration duration) {
+        Now_ += duration;
+    }
+
+private:
+    TInstant Now_ = CreateDeterministicTimeProvider(1)->Now();
+};
+
+TIntrusivePtr<TPausedClock> MakePausedClock() {
+    return new TPausedClock();
+}
+
 Y_UNIT_TEST_SUITE(LocalCacheTests) {
 
-    Y_UNIT_TEST(Example) {
-        auto cache = new TLocalCache<int, int>(CreateDefaultTimeProvider(), {});
-        Y_DO_NOT_OPTIMIZE_AWAY(cache);
+    Y_UNIT_TEST(OnEmpty_WhenGet_ThenReturnsExpiredDefault) {
+        auto cache = MakeLocalCache<int, int>(CreateDefaultTimeProvider(), {});
+
+        auto entry = cache->Get(1).GetValueSync();
+
+        UNIT_ASSERT_VALUES_EQUAL(entry.Value, 0);
+        UNIT_ASSERT_VALUES_EQUAL(entry.IsExpired, true);
+    }
+
+    Y_UNIT_TEST(OnEmpty_WhenUpdate_ThenGetReturnsNew) {
+        auto cache = MakeLocalCache<int, int>(CreateDefaultTimeProvider(), {});
+
+        cache->Update(1, 1).GetValueSync();
+
+        auto entry = cache->Get(1).GetValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(entry.Value, 1);
+        UNIT_ASSERT_VALUES_EQUAL(entry.IsExpired, false);
+    }
+
+    Y_UNIT_TEST(OnExistingKey_WhenUpdate_ThenGetReturnsNew) {
+        auto cache = MakeLocalCache<int, int>(CreateDefaultTimeProvider(), {});
+        cache->Update(1, 1);
+
+        cache->Update(1, 2).GetValueSync();
+
+        auto entry = cache->Get(1).GetValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(entry.Value, 2);
+        UNIT_ASSERT_VALUES_EQUAL(entry.IsExpired, false);
+    }
+
+    Y_UNIT_TEST(OnExistingKey_WhenExpires_ThenReturnedExpiredPast) {
+        auto clock = MakePausedClock();
+        auto cache = MakeLocalCache<int, int>(clock, {.TTL = TDuration::Minutes(2)});
+        cache->Update(1, 1);
+
+        clock->Skip(TDuration::Minutes(2) + TDuration::Seconds(1));
+
+        auto entry = cache->Get(1).GetValueSync();
+        UNIT_ASSERT_VALUES_EQUAL(entry.Value, 1);
+        UNIT_ASSERT_VALUES_EQUAL(entry.IsExpired, true);
     }
 
 } // Y_UNIT_TEST_SUITE(LocalCacheTests)
