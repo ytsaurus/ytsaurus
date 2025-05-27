bd473f3d1364861329a5b5d6b29f7478063390c2
vityaman <vityaman.dev@yandex.ru>
2025-05-27 23:03:02 +0300

    YQL-19747: Use thread local cache
    Signed-off-by: vityaman <vityaman.dev@yandex.ru>

--- yql/essentials/sql/v1/complete/name/cache/local/cache.h
+++ yql/essentials/sql/v1/complete/name/cache/local/cache.h
@@ -2,12 +2,14 @@
 
 #include <yql/essentials/sql/v1/complete/name/cache/cache.h>
 
-#include <library/cpp/cache/thread_safe_cache.h>
+#include <library/cpp/cache/cache.h>
 #include <library/cpp/time_provider/time_provider.h>
+
+#include <util/system/rwlock.h>
 
 namespace NSQLComplete {
 
-    struct TLocalCacheConfiguration {
+    struct TLocalCacheConfig {
         size_t Capacity = 256 * 1024 * 1024;
         TDuration TTL = TDuration::Seconds(8);
     };
@@ -24,58 +26,55 @@ namespace NSQLComplete {
                 TInstant Death;
             };
 
-            struct TEntrySizeProvider {
+            struct TCellSizeProvider {
                 size_t operator()(const TCell& entry) const {
                     return TSizeProvider()(entry.Value);
                 };
             };
 
-            using TCache = TThreadSafeLFUCacheWithSizeProvider<TKey, TCell, TEntrySizeProvider, TKey>;
-
-            class TCallbacks: public TCache::ICallbacks {
-            public:
-                TKey GetKey(TKey key) const override {
-                    return key;
-                }
-
-                TCell* CreateObject(TKey /* value */) const override {
-                    Y_ABORT();
-                }
-            };
+            using TStorage = TLFUCache<
+                TKey, TCell,
+                TNoopDelete, std::allocator<TKey>,
+                TCellSizeProvider>;
 
         public:
-            TLocalCache(
-                TIntrusivePtr<ITimeProvider> clock,
-                TLocalCacheConfiguration config)
+            TLocalCache(TIntrusivePtr<ITimeProvider> clock, TLocalCacheConfig config)
                 : Clock_(std::move(clock))
                 , Config_(std::move(config))
-                , Origin_(Callbacks_, /* maxSize = */ Config_.Capacity)
+                , Origin_(/* maxSize = */ Config_.Capacity)
             {
             }
 
             NThreading::TFuture<TEntry> Get(const TKey& key) const override {
                 TEntry entry;
-                if (typename TCache::TPtr ptr = Origin_.GetOrNull(key)) {
-                    entry.Value = ptr->Value;
-                    entry.IsExpired = (ptr->Death < Clock_->Now());
+                {
+                    TReadGuard _(Mutex_);
+                    if (auto it = Origin_.Find(key); it != Origin_.End()) {
+                        entry.Value = it->Value;
+                        entry.IsExpired = (it->Death < Clock_->Now());
+                    }
                 }
                 return NThreading::MakeFuture(std::move(entry));
             }
 
             NThreading::TFuture<void> Update(const TKey& key, TValue value) const override {
-                TCell* entry = new TCell{
+                TCell entry = {
                     .Value = std::move(value),
                     .Death = Clock_->Now() + Config_.TTL,
                 };
-                Origin_.Update(key, typename TCache::TPtr(entry));
+                {
+                    TWriteGuard _(Mutex_);
+                    Origin_.Update(key, std::move(entry));
+                }
                 return NThreading::MakeFuture();
             }
 
         private:
             TIntrusivePtr<ITimeProvider> Clock_;
-            TLocalCacheConfiguration Config_;
-            TCallbacks Callbacks_;
-            mutable TCache Origin_;
+            TLocalCacheConfig Config_;
+
+            TRWMutex Mutex_;
+            mutable TStorage Origin_;
         };
 
     } // namespace NPrivate
@@ -86,7 +85,7 @@ namespace NSQLComplete {
         NPrivate::CSizeProvider<TValue> TSizeProvider = TUniformSizeProvider<TValue>>
     ICache<TKey, TValue>::TPtr MakeLocalCache(
         TIntrusivePtr<ITimeProvider> clock,
-        TLocalCacheConfiguration config) {
+        TLocalCacheConfig config) {
         return new NPrivate::TLocalCache<TKey, TValue, TSizeProvider>(std::move(clock), std::move(config));
     }
 
