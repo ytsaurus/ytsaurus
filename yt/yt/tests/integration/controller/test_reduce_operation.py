5c721c8735fdb2138eb7434a6430e6609d597887
Nikita Sokolov <faucct@tracto.ai>
2025-05-27 16:36:06 +0200

    even more tests

--- yt/yt/tests/integration/controller/test_reduce_operation.py
+++ yt/yt/tests/integration/controller/test_reduce_operation.py
@@ -936,28 +936,37 @@ echo {v = 2} >&7
             sorted_by=["key"],
         )
 
+        with pytest.raises(YtError, match="User job failed"):
+            reduce(
+                in_="//tmp/in",
+                out="//tmp/out",
+                command='if [ "$YT_JOB_COOKIE_GROUP_INDEX" == 0 ]; then cat; echo primary>&2; else echo secondary; fi',
+                reduce_by=["key"],
+                spec={"reducer": {"cookie_group_size": 2}, "job_count": 1},
+            )
         op = reduce(
             in_="//tmp/in",
             out="//tmp/out",
-            command='if [ "$YT_JOB_COOKIE_GROUP_INDEX" == 0 ]; then cat 1>&2; else echo HI>&2; fi',
+            command='if [ "$YT_JOB_COOKIE_GROUP_INDEX" == 0 ]; then cat; echo primary>&2; else echo secondary>&2; fi',
             reduce_by=["key"],
-            spec={
-                "job_io": {"control_attributes": {"enable_key_switch": "true"}},
-                "reducer": {"format": yson.loads(b"<lenval=true>yamr"), "cookie_group_size": 2},
-                "job_count": 1,
-            },
+            spec={"reducer": {"cookie_group_size": 2}, "job_count": 1},
         )
 
         job_ids = op.list_jobs()
         assert len(job_ids) == 2
-        stderrs_bytes = {binascii.hexlify(op.read_stderr(job_id)) for job_id in job_ids}
+        stderrs_bytes = {op.read_stderr(job_id).decode() for job_id in job_ids}
 
         assert stderrs_bytes == {
-            b"010000006100000000feffffff010000006200000000010000006200000000",
-            binascii.hexlify("HI\n".encode()),
+            "primary\n",
+            "secondary\n",
         }
 
         assert not get("//tmp/out/@sorted")
+        assert read_table("//tmp/out") == [
+            {"key": "a", "value": ""},
+            {"key": "b", "value": ""},
+            {"key": "b", "value": ""},
+        ]
 
     @authors("monster", "klyachin")
     def test_key_switch_yson(self):
