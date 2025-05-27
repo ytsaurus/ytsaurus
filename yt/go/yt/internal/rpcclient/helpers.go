5ab02532b8c04f23f632b575c8dc9c77f59177f8
Nikita Sokolov <faucct@tracto.ai>
2025-05-27 22:02:16 +0300

    Support GetJob in Go SDK
    * Changelog entry
    Type: feature
    Component: go-sdk
    
    Support GetJob.
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1296
    
    Co-authored-by: tinarsky <tinarsky@ytsaurus.tech>
    Co-authored-by: tinarsky <tinarsky@ytsaurus.tech>
    commit_hash:18580bc53ad62c9cc45aa369f75242547fd49497

--- yt/go/yt/internal/rpcclient/helpers.go
+++ yt/go/yt/internal/rpcclient/helpers.go
@@ -1063,6 +1063,20 @@ func makeListJobsResult(result *rpc_proxy.TListJobsResult) (*yt.ListJobsResult, 
 	return ret, nil
 }
 
+func makeGetJobResult(result *rpc_proxy.TRspGetJob) (*yt.JobStatus, error) {
+	if result == nil {
+		return nil, nil
+	}
+
+	var info yt.JobStatus
+
+	if err := yson.Unmarshal(result.Info, &info); err != nil {
+		return nil, xerrors.Errorf("unable to deserialize job: %w", err)
+	}
+
+	return &info, nil
+}
+
 func convertTabletRangeOptions(opts *yt.TabletRangeOptions) *rpc_proxy.TTabletRangeOptions {
 	if opts == nil {
 		return nil
