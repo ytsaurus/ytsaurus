0133d5911af4cbb36218fe904acfbbe90da329f2
cherepashka <cherepashka@ytsaurus.tech>
2025-05-30 21:38:39 +0300

    YT-24864: Enable cypress proxies in test_master_cell_addition commit_hash:22f2fd705d0ce0f0d3b87c423e763cce4ca8be37

--- yt/yt/tests/library/yt_master_cell_addition_base.py
+++ yt/yt/tests/library/yt_master_cell_addition_base.py
@@ -6,6 +6,7 @@ from yt_env_setup import (
     NODES_SERVICE,
     MASTERS_SERVICE,
     CHAOS_NODES_SERVICE,
+    CYPRESS_PROXIES_SERVICE,
 )
 
 from yt_commands import (
@@ -34,10 +35,7 @@ class MasterCellAdditionBase(YTEnvSetup):
     DEFER_SCHEDULER_START = True
     DEFER_CONTROLLER_AGENT_START = True
     DEFER_CHAOS_NODE_START = True
-
-    # TODO(kvk1920): there is alert in cluster directory change for most of
-    # components. Fix it and enable Cypress proxies.
-    NUM_CYPRESS_PROXIES = 0
+    # NB: It is impossible to defer start cypress proxies, since some setup handlers rely on their availability.
 
     PRIMARY_CLUSTER_INDEX = 0
 
@@ -55,7 +53,13 @@ class MasterCellAdditionBase(YTEnvSetup):
 
             assert secondary_master_cells_to_start >= 0
 
+            # Cypress proxies must be available to execute sync requests, so they should be started before masters.
+            env.kill_cypress_proxies()
+            if cls.get_param("NUM_CYPRESS_PROXIES", cluster_index) != 0:
+                env.start_cypress_proxies()
+
             # NB: The last secondary master cell on primary cluster is not started here.
+            # It is important to start cells synchroniously, so the last registered one - is the one we will add/remove.
             for cell_index in range(secondary_master_cells_to_start):
                 env.start_master_cell(cell_index + 1)
 
@@ -89,6 +93,13 @@ class MasterCellAdditionBase(YTEnvSetup):
 
     @classmethod
     def modify_controller_agent_config(cls, config, cluster_index):
+        cls._collect_cell_ids_and_maybe_stash_last_cell(
+            config["cluster_connection"],
+            cluster_index,
+            cls.get_param("REMOVE_LAST_MASTER_BEFORE_START", cluster_index))
+
+    @classmethod
+    def modify_cypress_proxy_config(cls, config, cluster_index):
         cls._collect_cell_ids_and_maybe_stash_last_cell(
             config["cluster_connection"],
             cluster_index,
@@ -161,6 +172,7 @@ class MasterCellAdditionBase(YTEnvSetup):
         cls.Env.rewrite_chaos_node_configs()
         cls.Env.rewrite_scheduler_configs()
         cls.Env.rewrite_controller_agent_configs()
+        cls.Env.rewrite_cypress_proxies_configs()
 
     @classmethod
     def _abort_world_initializer_transactions(cls):
@@ -189,8 +201,10 @@ class MasterCellAdditionBase(YTEnvSetup):
 
         optional_services = cls._get_optional_services()
         services = [service for service in optional_services if service is not None]
+        services_to_patch = services + [CYPRESS_PROXIES_SERVICE]
 
-        with Restarter(cls.Env, services):
+        with Restarter(cls.Env, services, sync=False):
+            cls.Env.kill_cypress_proxies()
             drivers = cls._kill_drivers()
 
             cls._build_readonly_snapshot()
@@ -202,9 +216,12 @@ class MasterCellAdditionBase(YTEnvSetup):
                 for peer_index, _ in enumerate(configs["master"][tag]):
                     cls.proceed_master_config(configs["master"][tag][peer_index], cls.PRIMARY_CLUSTER_INDEX, remove_last_master=True)
                     configs["master"][tag][peer_index]["hive_manager"]["allowed_for_removal_master_cell_tags"] = [13]
-            for service in services:
+            for service in services_to_patch:
                 # Config keys are services in the singular, drop plural.
                 service_name_singular = service[:-1]
+                if service == CYPRESS_PROXIES_SERVICE:
+                    # This one is an exception.
+                    service_name_singular = "cypress_proxy"
                 for _, config in enumerate(configs[service_name_singular]):
                     cls._collect_cell_ids_and_maybe_stash_last_cell(config["cluster_connection"], cls.PRIMARY_CLUSTER_INDEX, remove_last_master=True)
                     if service == NODES_SERVICE:
@@ -219,15 +236,17 @@ class MasterCellAdditionBase(YTEnvSetup):
             cls.Env.rewrite_master_configs()
             cls._rewrite_optional_services_configs()
 
-            cls.Env.start_master_cell(cell_index=0, set_config=False)
+            cls.Env.start_cypress_proxies()
+            cls.Env.start_master_cell(cell_index=0, set_config=False, sync=False)
             secondary_masters_to_start = cls.get_param("NUM_SECONDARY_MASTER_CELLS", cls.PRIMARY_CLUSTER_INDEX) - 1
             for cell_index in range(secondary_masters_to_start):
-                cls.Env.start_master_cell(cell_index=cell_index + 1, set_config=False)
-            wait_drivers()
+                cls.Env.start_master_cell(cell_index=cell_index + 1, set_config=False, sync=False)
             master_exit_read_only()
             # Cell 13 was dropped.
             wait_no_peers_in_read_only(secondary_cell_tags=["11", "12"])
+            wait_drivers()
 
+        cls.Env.synchronize()
         cls._abort_world_initializer_transactions()
 
         def _move_files(directory):
@@ -253,7 +272,7 @@ class MasterCellAdditionBase(YTEnvSetup):
         optional_services = cls._get_optional_services()
         services = [service for service in optional_services if downtime and service is not None]
 
-        with Restarter(cls.Env, services):
+        with Restarter(cls.Env, services, sync=False):
             # Restart drivers to apply new master cells configuration.
             cls._kill_drivers()
 
@@ -266,12 +285,11 @@ class MasterCellAdditionBase(YTEnvSetup):
 
                 cls.Env.rewrite_master_configs()
             wait_drivers()
-
-            master_exit_read_only_sync()
-            cls.Env.synchronize()
-
             if downtime:
                 cls._rewrite_optional_services_configs()
+
+            master_exit_read_only_sync()
+        cls.Env.synchronize()
 
         cls._abort_world_initializer_transactions()
 
