3818efdb361b702962266eebe3968f7a074ed064
Nikita Sokolov <faucct@tracto.ai>
2025-06-02 18:08:19 +0200

    JobReporter should report JobCookieGroupIndex and MainJobId

--- yt/yt/ytlib/api/native/client_job_info_impl.cpp
+++ yt/yt/ytlib/api/native/client_job_info_impl.cpp
@@ -1612,6 +1612,7 @@ static std::vector<TJob> ParseJobsFromArchiveResponse(
             .PoolTree = record.PoolTree,
             .MonitoringDescriptor = record.MonitoringDescriptor,
             .JobCookie = record.JobCookie,
+            .JobCookieGroupIndex = record.JobCookieGroupIndex,
             .ArchiveFeatures = record.ArchiveFeatures.value_or(TYsonString()),
             .OperationIncarnation = record.OperationIncarnation,
             .GangRank = record.GangRank,
@@ -1677,6 +1678,10 @@ static std::vector<TJob> ParseJobsFromArchiveResponse(
 
         if (record.JobCompetitionId) {
             job.JobCompetitionId = TJobId(TGuid::FromString(*record.JobCompetitionId));
+        }
+
+        if (record.MainJobId) {
+            job.MainJobId = TJobId(TGuid::FromString(*record.MainJobId));
         }
 
         if (record.ProbingJobCompetitionId) {
