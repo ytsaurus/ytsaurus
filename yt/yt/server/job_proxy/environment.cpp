2f0fc06fb5e2dd31c03cd1be562fb1364392c6b8
ignat <ignat@ytsaurus.tech>
2025-06-07 21:18:10 +0300

    YT-25300: Fix network profiling commit_hash:52bee3aa5090a9b37d82938ed53b40bca8483ea0

--- yt/yt/server/job_proxy/environment.cpp
+++ yt/yt/server/job_proxy/environment.cpp
@@ -1,4 +1,5 @@
 #include "environment.h"
+#include "private.h"
 
 #include <yt/yt/server/lib/exec_node/config.h>
 #include <yt/yt/server/lib/exec_node/gpu_helpers.h>
@@ -423,6 +424,8 @@ public:
 
             launcher->SetIPAddresses(addresses, Options_.EnableNat64);
         }
+
+        launcher->SetNetworkInterface(TString(JobNetworkInterface));
 
         launcher->SetEnablePorto(Options_.EnablePorto);
         launcher->SetIsolate(Options_.EnablePorto != EEnablePorto::Full);
