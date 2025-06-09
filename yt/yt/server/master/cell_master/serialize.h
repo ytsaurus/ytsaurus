b2244d78c4199e896cad139679cf9999269822fc
h0pless <h0pless@ytsaurus.tech>
2025-06-09 13:25:27 +0300

    Don't validate lock count on an external cell commit_hash:62416cf398ccfe2182e9c44c2b282e713da650fe

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -182,6 +182,7 @@ DEFINE_ENUM(EMasterReign,
     ((ChunkLocationCounterId)                                       (2945))  // aleksandra-zh
     ((SysOperationsInSequoia)                                       (2946))  // kvk1920
     ((CheckNodeWriteSessions)                                       (2947))  // koloshmet
+    ((DontValidateLockCountOnExternalCells)                         (2948))  // h0pless
 );
 
 static_assert(TEnumTraits<EMasterReign>::IsMonotonic, "Master reign enum is not monotonic");
