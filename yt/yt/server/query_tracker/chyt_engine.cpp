ba512c5b6b7f7517969a37f0d5a96233c867dbfe
Konstantin Khlebnikov <khlebnikov@tracto.ai>
2025-06-12 17:56:58 +0200

    yt/query_tracker: pass native client to ChytEngine constructor
    Get rid of DynamicPointerCast.
    
    Signed-off-by: Konstantin Khlebnikov <khlebnikov@tracto.ai>

--- yt/yt/server/query_tracker/chyt_engine.cpp
+++ yt/yt/server/query_tracker/chyt_engine.cpp
@@ -400,11 +400,11 @@ class TChytEngine
     : public IQueryEngine
 {
 public:
-    TChytEngine(IClientPtr stateClient, TYPath stateRoot)
+    TChytEngine(NNative::IClientPtr stateClient, TYPath stateRoot)
         : StateClient_(std::move(stateClient))
         , StateRoot_(std::move(stateRoot))
         , ControlQueue_(New<TActionQueue>("MockEngineControl"))
-        , ClusterDirectory_(DynamicPointerCast<NNative::IConnection>(StateClient_->GetConnection())->GetClusterDirectory())
+        , ClusterDirectory_(StateClient_->GetNativeConnection()->GetClusterDirectory())
         , ChannelFactory_(CreateCachingChannelFactory(CreateTcpBusChannelFactory(New<NYT::NBus::TBusConfig>())))
     { }
 
@@ -419,7 +419,7 @@ public:
     }
 
 private:
-    const IClientPtr StateClient_;
+    const NNative::IClientPtr StateClient_;
     const TYPath StateRoot_;
     const TActionQueuePtr ControlQueue_;
     TChytEngineConfigPtr ChytConfig_;
@@ -429,7 +429,7 @@ private:
 
 ////////////////////////////////////////////////////////////////////////////////
 
-IQueryEnginePtr CreateChytEngine(const IClientPtr& stateClient, const TYPath& stateRoot)
+IQueryEnginePtr CreateChytEngine(const NNative::IClientPtr& stateClient, const TYPath& stateRoot)
 {
     return New<TChytEngine>(stateClient, stateRoot);
 }
