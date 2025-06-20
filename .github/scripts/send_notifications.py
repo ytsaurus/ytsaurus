0448220271f2f951ed50c3eacaf28391b1625ed2
Semyon Poltorak <epsilond1@ytsaurus.tech>
2025-06-20 23:31:33 +0300

    add script for send alerts

---
+++ .github/scripts/send_notifications.py
@@ -0,0 +1,124 @@
+import argparse
+import requests
+
+CONCLUSION_SUCCESS = 'success'
+CONCLUSION_FAILURE = 'failure'
+
+
+def send_notify(args, wf_state):
+    message = '\n'.join((
+        f'Workflow *{args.workflow}* {wf_state}: {args.git_server_url}/{args.repo}/actions/runs/{args.current_job_id}',
+        f'Git {args.ref_type}: *{args.ref}*.',
+        f'Commit: ```{args.commit_message}```',
+    ))
+
+    print(message)
+
+    url = 'https://api.telegram.org/bot{}/sendMessage'.format(args.tg_token)
+    data = {'chat_id': args.tg_chat_id, 'text': message, 'parse_mode': 'Markdown'}
+    response = requests.post(url, data=data)
+    print(response.json())
+    response.raise_for_status()
+
+
+def main():
+    parser = argparse.ArgumentParser(
+        prog='Get status of the last completed action to stdout',
+    )
+
+    parser.add_argument(
+        '--git-token',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--repo',
+        default='ytsaurus/ytsaurus',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--workflow',
+        help='For example `cpp.yaml`',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--ref',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--ref-type',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--tg-token',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--tg-chat-id',
+        type=int,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--current-job-conclusion',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--current-job-id',
+        type=int,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--commit-message',
+        type=str,
+        required=True,
+    )
+
+    parser.add_argument(
+        '--git-server-url',
+        type=str,
+        required=True,
+    )
+
+    args = parser.parse_args()
+
+    headers = {'Authorization': f'Bearer {args.git_token}'}
+    r = requests.get(f'https://api.github.com/repos/{args.repo}/actions/workflows/{args.workflow}/runs?status=completed&{args.ref_type}={args.ref}',
+                     headers=headers)
+    r.raise_for_status()
+
+    runs = r.json()['workflow_runs']
+    if runs:
+        last = runs[0]
+        prev_conclusion = last['conclusion']
+        print(prev_conclusion)
+        state_to_alert = {
+            (CONCLUSION_SUCCESS, CONCLUSION_FAILURE): "❌ failed",
+            (CONCLUSION_FAILURE, CONCLUSION_SUCCESS): "✅ fixed",
+        }
+
+        wf_state = state_to_alert.get((prev_conclusion, args.current_job_conclusion))
+        if wf_state:
+            send_notify(args, wf_state)
+        else:
+            print('State is not changed')
+    else:
+        print('No runs')
+
+
+if __name__ == '__main__':
+    main()
