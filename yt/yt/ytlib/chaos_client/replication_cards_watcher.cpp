5cd28fa585fe4d20db4c7bf39920de0f7ec78ccb
osidorkin <osidorkin@ytsaurus.tech>
2025-06-24 13:39:32 +0300

    YT-25469: Fix core due to copy card pointer outside the lock commit_hash:ed9d36c8149da536bfd53a410f84f79c49a2773b

--- yt/yt/ytlib/chaos_client/replication_cards_watcher.cpp
+++ yt/yt/ytlib/chaos_client/replication_cards_watcher.cpp
@@ -287,23 +287,29 @@ public:
             if (it != WatchersByCardId_.end()) {
                 auto& entry = it->second;
                 entry->LastSeenWatchers.store(TInstant::Now());
+                auto entryGuard = Guard(entry->Lock);
                 if (entry->CurrentCacheTimestamp > cacheTimestamp) {
                     auto replicationCard = entry->ReplicationCard;
                     auto timestamp = entry->CurrentCacheTimestamp;
+                    entryGuard.Release();
                     readGuard.Release();
+
                     YT_LOG_DEBUG(
                         "Replication card updated between watches "
                         "(ReplicationCardId: %v, CurrentCacheTimestamp: %v, CacheTimestamp: %v)",
-                        replicationCardId, timestamp, cacheTimestamp);\
+                        replicationCardId, timestamp, cacheTimestamp);
+
                     callbacks->OnReplicationCardChanged(replicationCard, timestamp);
                     return EReplicationCardWatherState::Normal;
                 }
 
-                auto entryGuard = Guard(entry->Lock);
                 entry->WatcherEntries.push_back(TReplicationCardWatcherEntry{
                     .Callbacks = std::move(callbacks),
                     .RequestStartTime = TInstant::Now(),
                 });
+
+                entryGuard.Release();
+                readGuard.Release();
 
                 YT_LOG_DEBUG("Added request to watchers list (ReplicationCardId: %v)",
                     replicationCardId);
