bd5d92a8bcb0c45743bc4d0fec0c6da8dee4ebdb
apachee <apachee@ytsaurus.tech>
2025-06-25 16:07:20 +0300

    [queues] YT-25456: Fix export progress merge for the purpose of automatic trim
    * Changelog entry
    Type: fix
    Component: queue-agent
    
    Fix potential data loss in case of multiple exports per queue
    due to incorrect merging of queue export progresses.
    commit_hash:308aab848ef2c8f76e512c088e6500911ce9cec3

---
+++ yt/yt/server/queue_agent/unittests/queue_export_ut.cpp
@@ -0,0 +1,68 @@
+#include <yt/yt/server/queue_agent/helpers.h>
+#include <yt/yt/server/queue_agent/queue_exporter.h>
+
+#include <yt/yt/core/misc/collection_helpers.h>
+
+#include <library/cpp/testing/gtest/gtest.h>
+
+#include <tuple>
+
+namespace NYT::NQueueAgent {
+namespace {
+
+using namespace NYson;
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TAggregateQueueProgressTest
+    : public ::testing::TestWithParam<std::tuple<
+        TQueueExportProgressPtr,
+        TQueueExportProgressPtr,
+        std::optional<std::vector<i64>>>>
+{ };
+
+TEST_P(TAggregateQueueProgressTest, Test)
+{
+    auto [lhs, rhs, expectedResult] = GetParam();
+
+    TAggregatedQueueExportsProgress progress{};
+    progress.MergeWith(TAggregatedQueueExportsProgress::FromQueueExportProrgess(lhs));
+    progress.MergeWith(TAggregatedQueueExportsProgress::FromQueueExportProrgess(rhs));
+
+    auto aggregateProgress = AggregateQueueExports(THashMap{
+        std::pair{TString("lhs"), lhs},
+        std::pair{TString("rhs"), rhs},
+    });
+    EXPECT_EQ(progress, aggregateProgress);
+
+    ASSERT_EQ(progress.HasExports, expectedResult.has_value());
+    if (expectedResult.has_value()) {
+        auto expectedResultValue = *expectedResult;
+        for (const auto& [tabletIndex, rowCount] : progress.TabletIndexToRowCount) {
+            EXPECT_TRUE(tabletIndex >= 0 && tabletIndex < std::ssize(expectedResultValue) && expectedResultValue[tabletIndex] == rowCount);
+        }
+        for (int i = 0; i < std::ssize(expectedResultValue); i++) {
+            if (expectedResultValue[i] == -1) {
+                EXPECT_FALSE(progress.TabletIndexToRowCount.contains(i));
+            } else {
+                EXPECT_EQ(expectedResultValue[i], GetOrDefault(progress.TabletIndexToRowCount, i, -1));
+            }
+        }
+    }
+}
+
+static const auto LhsExportProgress = ConvertTo<TQueueExportProgressPtr>(TYsonString(TString("{tablets={\"1\"={row_count=1};\"3\"={row_count=1};\"4\"={row_count=2}}}")));
+static const auto RhsExportProgress = ConvertTo<TQueueExportProgressPtr>(TYsonString(TString("{tablets={\"2\"={row_count=1};\"3\"={row_count=2};\"4\"={row_count=1}}}")));
+
+INSTANTIATE_TEST_SUITE_P(
+    TAggregateQueueProgressTest,
+    TAggregateQueueProgressTest,
+    ::testing::Values(
+        std::tuple{nullptr, nullptr, std::nullopt},
+        std::tuple{LhsExportProgress, nullptr, std::vector<i64>{-1, 1, -1, 1, 2}},
+        std::tuple{nullptr, RhsExportProgress, std::vector<i64>{-1, -1, 1, 2, 1}},
+        std::tuple{LhsExportProgress, RhsExportProgress, std::vector<i64>{-1, 0, 0, 1, 1}}
+    ));
+
+} // namespace
+} // namespace NYT::NQueueAgent
