0072d5c37a67acab3d82529b23b66bd19854e97a
denvr <denvr@ytsaurus.tech>
2025-06-27 16:32:21 +0300

    YT-16324 - Generate yt_dataclass from table schema commit_hash:eba37de55d3d11d324b2e2a6971ed3a29b18f052

--- yt/python/yt/wrapper/schema/__init__.py
+++ yt/python/yt/wrapper/schema/__init__.py
@@ -9,12 +9,16 @@ from .helpers import check_schema_module_available, is_schema_module_available
 
 from ..errors import YtError
 
+import re
+import typing
+
 if is_schema_module_available():
     from .types import (  # noqa
         Int8, Int16, Int32, Int64, Uint8, Uint16, Uint32, Uint64,
         Float, Double,
         Date, Datetime, Timestamp, Interval,
-        YsonBytes, OtherColumns, FormattedPyDatetime)
+        YsonBytes, OtherColumns, FormattedPyDatetime,
+        _PY_TYPE_BY_TI_TYPE)
 
     from .types import RowIteratorProtocol as RowIterator, ContextProtocol as Context  # noqa
     from .variant import Variant  # noqa
@@ -73,3 +77,57 @@ def _create_row_py_schema(
                 yt_fields.append((name, column.type))
     struct_schema = internal_schema._create_struct_schema(py_type, yt_fields, allow_other_columns=True, schema_runtime_context=schema_runtime_context)
     return internal_schema.RowSchema(struct_schema, control_attributes=control_attributes, schema_runtime_context=schema_runtime_context)
+
+
+def make_dataclass_from_table_schema(table_type: TableSchema, type_name: typing.Optional[str] = None) -> typing.Type:
+    """Create yt_dataclass class from TableSchema object on the fly.
+    :param TableSchema table_type: YT table schema.
+    :param str type_name: name of new class (usefull for debug), optional.
+    :return: yt_dataclass class to create records
+    """
+    if isinstance(table_type, TableSchema):
+        if not type_name:
+            type_name = f"YtGeneratedDataclass_{id(table_type)}"
+        obj = {
+            "__annotations__": {},
+        }
+        for column in table_type.columns:
+            name = column.name
+            py_type = _make_py_type_from_ti_type(column, type_name)
+            if not re.match(r"^[a-z_][a-z0-9_]*$", name, re.IGNORECASE):
+                raise ValueError(f"Wrong table column name: \"{name}\"")
+            obj[name] = None
+            obj["__annotations__"][name] = py_type
+        return yt_dataclass(type(type_name, (object, ), obj))
+    else:
+        raise RuntimeError(f"Unknown table type \"{type(table_type)}\"")
+
+
+def _make_py_type_from_ti_type(table_type: typing.Union[ColumnSchema, ti.type_base.Primitive, ti.Type], type_name: typing.Optional[str] = None) -> typing.Type:
+    if isinstance(table_type, ColumnSchema):
+        return _make_py_type_from_ti_type(table_type.type, type_name)
+    elif isinstance(table_type, ti.type_base.Primitive):
+        if table_type in _PY_TYPE_BY_TI_TYPE:
+            return _PY_TYPE_BY_TI_TYPE[table_type]
+        else:
+            raise ValueError(f"Unknown type \"{table_type}\"")
+    elif isinstance(table_type, ti.Type) and table_type.name == "Optional":
+        return typing.Optional[_make_py_type_from_ti_type(table_type.item, type_name)]
+    elif isinstance(table_type, ti.Type) and table_type.name == "List":
+        return typing.List[_make_py_type_from_ti_type(table_type.item, type_name)]
+    elif isinstance(table_type, ti.Type) and table_type.name == "Tuple":
+        py_type = typing.Tuple
+        py_type.__args__ = tuple([_make_py_type_from_ti_type(item, type_name) for item in table_type.items])
+        return py_type
+    elif isinstance(table_type, ti.Type) and table_type.name == "Dict":
+        return typing.Dict[_make_py_type_from_ti_type(table_type.key, type_name), _make_py_type_from_ti_type(table_type.value, type_name)]
+    elif isinstance(table_type, ti.Type) and table_type.name == "Struct":
+        obj = {
+            "__annotations__": {},
+        }
+        for name, yt_type in table_type.items:
+            obj[name] = None
+            obj["__annotations__"][name] = _make_py_type_from_ti_type(yt_type, type_name)
+        return yt_dataclass(type(f"{type_name}_{table_type.name.upper()}", (object, ), obj))
+    else:
+        raise RuntimeError(f"Unknown field type {table_type}")
