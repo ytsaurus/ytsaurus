70a159abec91f42cf8ff2b67af2ae313dce9912c
dave11ar <dave11ar@ytsaurus.tech>
2025-06-30 03:14:11 +0300

    YT-25496: Fix schema validation for versioned lookup
    YT-25496: Fix schema validation for versioned lookup
    commit_hash:5d438da4ac497394b889e7b20b8b9f5662cfea94

--- yt/yt/ytlib/api/native/client_dynamic_tables_impl.cpp
+++ yt/yt/ytlib/api/native/client_dynamic_tables_impl.cpp
@@ -985,7 +985,9 @@ TLookupRowsResult<IRowset> TClient::DoLookupRowsOnce(
     tableInfo->ValidateDynamic();
     tableInfo->ValidateSorted();
 
-    auto schema = options.VersionedReadOptions.ReadMode == NTableClient::EVersionedIOMode::LatestTimestamp
+    bool isTimestampedLookup = options.VersionedReadOptions.ReadMode == NTableClient::EVersionedIOMode::LatestTimestamp;
+
+    auto schema = isTimestampedLookup
         ? ToLatestTimestampSchema(tableInfo->Schemas[ETableSchemaKind::Primary])
         : tableInfo->Schemas[ETableSchemaKind::Primary];
 
@@ -1001,8 +1003,10 @@ TLookupRowsResult<IRowset> TClient::DoLookupRowsOnce(
             *options.FallbackTableSchema,
             *schema,
             GetSchemaUpdateEnabledFeatures(),
-            true,
-            false);
+            /*isTableDynamic*/ true,
+            /*isTableEmpty*/ false,
+            /*allowAlterKeyColumnToAny*/ false,
+            {.AllowTimestampColumns = isTimestampedLookup});
     }
 
     auto idMapping = BuildColumnIdMapping(*schema, nameTable);
