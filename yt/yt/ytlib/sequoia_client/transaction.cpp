9bacc1056ffd73a3e5720638ba2dd7135a15d75c
kvk1920 <kvk1920@ytsaurus.tech>
2025-07-01 15:52:30 +0300

    [trunk] Fix data race in Sequoia tx commit_hash:77bac1ea55d17a8a17b3f6b05971953935b9f0fa

--- yt/yt/ytlib/sequoia_client/transaction.cpp
+++ yt/yt/ytlib/sequoia_client/transaction.cpp
@@ -139,6 +139,20 @@ TPerTransactionTypeCounters* GetPerTransactionTypeCounters(ESequoiaTransactionTy
 
 ////////////////////////////////////////////////////////////////////////////////
 
+ISequoiaTransaction::TGuardedRowBuffer::TGuardedRowBuffer(
+    NThreading::TSpinLock* lock,
+    const TRowBufferPtr& rowBuffer)
+    : Guard_(Guard(*lock))
+    , RowBuffer_(rowBuffer)
+{ }
+
+const TRowBufferPtr& ISequoiaTransaction::TGuardedRowBuffer::Get() const
+{
+    return RowBuffer_;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
 class TSequoiaTransaction
     : public ISequoiaTransaction
 {
@@ -427,9 +441,9 @@ public:
             NCellMasterClient::EMasterCellRole::SequoiaNodeHost);
     }
 
-    const TRowBufferPtr& GetRowBuffer() const override
+    TGuardedRowBuffer GetGuardedRowBuffer() override
     {
-        return RowBuffer_;
+        return TGuardedRowBuffer(&Lock_, RowBuffer_);
     }
 
     const ISequoiaClientPtr& GetClient() const override
