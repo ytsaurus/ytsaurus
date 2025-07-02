d8f442bfa14de71e24919acf993fcd42c4939f17
tinarsky <tinarsky@ytsaurus.tech>
2025-07-02 17:26:04 +0300

    [java] Fix all publish.gradle.kts commit_hash:ebbfd74e608a598794f11c7f37f7803eb59f5671

--- yt/java/yson-json-converter/build.gradle.kts
+++ yt/java/yson-json-converter/build.gradle.kts
@@ -91,8 +91,9 @@ publishing {
 
     repositories {
         maven {
-            val releasesRepoUrl = uri("https://s01.oss.sonatype.org/service/local/staging/deploy/maven2/")
-            val snapshotsRepoUrl = uri("https://s01.oss.sonatype.org/content/repositories/snapshots/")
+            val releasesRepoUrl = uri("https://ossrh-staging-api.central.sonatype.com/service/local/staging/deploy/maven2/")
+            val snapshotsRepoUrl = uri("https://ossrh-staging-api.central.sonatype.com/content/repositories/snapshots/")
+            name = "ossrh-staging-api"
             url = if (version.toString().endsWith("SNAPSHOT")) snapshotsRepoUrl else releasesRepoUrl
 
             credentials {
