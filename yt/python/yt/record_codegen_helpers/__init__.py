ba65f9c016d8f6b6c3c03176ad0cf35c50245082
apachee <apachee@ytsaurus.tech>
2025-07-07 16:35:23 +0300

    YT-25099: Add support for type_v3 in record codegen
    Adds column_type_v3 in record codegen field description.
    Example usage:
    ```
    - cpp_name: Value
      cpp_type: std::optional<NYson::TYsonString>
      column_name: value
      column_type_v3:
        type_name: optional
        item:
          type_name: list
          item: string
    ```
    commit_hash:11bafe0bbc55babce6482c0e66ae30ee74b5b6f2

--- yt/python/yt/record_codegen_helpers/__init__.py
+++ yt/python/yt/record_codegen_helpers/__init__.py
@@ -1,6 +1,8 @@
 from dataclasses import dataclass
 
-from typing import Optional, List
+from typing import Any, Optional, List
+
+import json
 
 
 @dataclass
@@ -8,12 +10,28 @@ class Field:
     cpp_name: str
     cpp_type: str
     column_name: str
-    column_type: str
+    column_type: Optional[str]
+    column_type_v3: Optional[Any]
     required: Optional[bool]
     sort_order: Optional[str]
     lock: Optional[str]
     aggregate: Optional[str]
     expression: Optional[str]
+
+    def get_type_v3_string(self):
+        if self.column_type_v3 is None:
+            raise ValueError('can\'t get type_v3 string: "column_type_v3" field is not set')
+        column_type_v3_json_string = json.dumps(self.column_type_v3)
+        # Add quotes (jinja2 does not have something ready)
+        return json.dumps(column_type_v3_json_string)
+
+    def __post_init__(self):
+        if self.column_type is None and self.column_type_v3 is None:
+            raise ValueError('one of "column_type" and "column_type_v3" fields must be specified')
+        if self.column_type is not None and self.column_type_v3 is not None:
+            raise ValueError('fields "column_type" and "column_type_v3" are mutually exclusive')
+        if self.required is not None and self.column_type_v3 is not None:
+            raise ValueError('field "required" is incompatible with field "column_type_v3"')
 
 
 @dataclass
