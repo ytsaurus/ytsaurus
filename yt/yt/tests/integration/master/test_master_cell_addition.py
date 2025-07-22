682faba5ed614968f8735befc0c162cc55c376a9
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2025-07-22 21:34:05 +0300

    Temporarily disable test_add_cell commit_hash:d1e9f2c9402cc1a2a819ef7daeaa76cb02525400

--- yt/yt/tests/integration/master/test_master_cell_addition.py
+++ yt/yt/tests/integration/master/test_master_cell_addition.py
@@ -309,6 +309,7 @@ class TestDynamicMasterCellPropagation(MasterCellAdditionBase):
 
     @authors("cherepashka")
     def test_add_cell(self):
+        pytest.skip("aleksandra-zh will fix this soon.")
         create("portal_entrance", "//tmp/p1", attributes={"exit_cell_tag": 12})
         tx = start_transaction(timeout=120000)
         create("table", "//tmp/p1/t", tx=tx)  # replicate tx to cell 12
