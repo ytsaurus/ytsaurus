74fb55f243b2787f557f816d90c32dd02f7decbc
kvk1920 <kvk1920@ytsaurus.tech>
2025-07-23 13:20:54 +0300

    Drop fix_requisition_update compat
    Drop enable_fix_requisition_update compat
    
    YT-18571: Fix reigns in 24.2
    commit_hash:d41117e17a67b078525aad3d6864b915997184bb

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -252,6 +252,7 @@ DEFINE_ENUM(EMasterReign,
     ((Int64InHistogramSnapshot)                                     (2745))  // babenko
     ((SecondaryIndexUnfoldedColumnApi)                              (2746))  // sabdenovch
     ((FixCompositeKeyDeserialization)                               (2747))  // ermolovd
+    ((DropEnableFixRequisitionUpdateCompat)                         (2748))  // kvk1920
 );
 
 static_assert(TEnumTraits<EMasterReign>::IsMonotonic, "Master reign enum is not monotonic");
