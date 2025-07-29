f0ca5f8627acf1683f653ab5f7685117f1cbd1d8
robot-contrib <robot-contrib@yandex-team.com>
2025-07-29 21:06:22 +0300

    Update contrib/clickhouse to 24.8.14.39 commit_hash:338749b8cb78d3b68f60dc535360935ca9e975b0

--- contrib/clickhouse/src/Server/WebUIRequestHandler.cpp
+++ contrib/clickhouse/src/Server/WebUIRequestHandler.cpp
@@ -84,8 +84,6 @@ void JavaScriptWebUIRequestHandler::handleRequest(HTTPServerRequest & request, H
         response.setStatusAndReason(DBPoco::Net::HTTPResponse::HTTP_NOT_FOUND);
         *response.send() << "Not found.\n";
     }
-
-    handle(request, response, {reinterpret_cast<const char *>(gresource_binary_htmlData), gresource_binary_htmlSize});
 }
 
 }
