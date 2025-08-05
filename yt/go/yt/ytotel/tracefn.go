7309f4d08b67f29c89a64bea972dafd967029811
Matvei Karpov <matveichk@gmail.com>
2025-08-05 14:27:00 +0700

    Go SDK: Refactored ytotel.TraceFn

--- yt/go/yt/ytotel/tracefn.go
+++ yt/go/yt/ytotel/tracefn.go
@@ -17,28 +17,31 @@ type spanContextProvider interface {
 }
 
 func TraceFn(ctx context.Context) (traceID guid.GUID, spanID uint64, flags byte, ok bool) {
-	if otSpan := opentracing.SpanFromContext(ctx); otSpan != nil {
-		spanCtx := otSpan.Context()
-
-		if bridgeSpanCtx, ok := spanCtx.(spanContextProvider); ok {
-			traceIDBytes := bridgeSpanCtx.TraceID()
-			spanIDBytes := bridgeSpanCtx.SpanID()
-
-			if len(traceIDBytes) == 16 {
-				high := binary.BigEndian.Uint64(traceIDBytes[:8])
-				low := binary.BigEndian.Uint64(traceIDBytes[8:])
-				traceID = guid.FromHalves(low, high)
-			}
-
-			if len(spanIDBytes) == 8 {
-				spanID = binary.BigEndian.Uint64(spanIDBytes[:])
-			}
-
-			flags := byte(bridgeSpanCtx.TraceFlags())
-
-			return traceID, spanID, flags, true
-		}
+	otSpan := opentracing.SpanFromContext(ctx)
+	if otSpan == nil {
+		return
 	}
 
-	return
+	spanCtx := otSpan.Context()
+	bridgeSpanCtx, ok := spanCtx.(spanContextProvider)
+	if !ok {
+		return
+	}
+
+	traceIDBytes := bridgeSpanCtx.TraceID()
+	spanIDBytes := bridgeSpanCtx.SpanID()
+
+	if len(traceIDBytes) == 16 {
+		high := binary.BigEndian.Uint64(traceIDBytes[:8])
+		low := binary.BigEndian.Uint64(traceIDBytes[8:])
+		traceID = guid.FromHalves(low, high)
+	}
+
+	if len(spanIDBytes) == 8 {
+		spanID = binary.BigEndian.Uint64(spanIDBytes[:])
+	}
+
+	flags = byte(bridgeSpanCtx.TraceFlags())
+
+	return traceID, spanID, flags, true
 }
