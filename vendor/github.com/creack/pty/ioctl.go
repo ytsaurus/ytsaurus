203c0932448ee173c5dafdf5751d95a513318557
ilyabatkov4 <ilyabatkov4@yandex-team.com>
2025-08-14 20:00:44 +0300

    Qdrant go client
    add qdrant
    commit_hash:1a15de041a4b52b1795d7c3a484eabf64cc12d04

--- vendor/github.com/creack/pty/ioctl.go
+++ vendor/github.com/creack/pty/ioctl.go
@@ -6,15 +6,20 @@ package pty
 import "os"
 
 func ioctl(f *os.File, cmd, ptr uintptr) error {
+	return ioctlInner(f.Fd(), cmd, ptr) // Fall back to blocking io.
+}
+
+// NOTE: Unused. Keeping for reference.
+func ioctlNonblock(f *os.File, cmd, ptr uintptr) error {
 	sc, e := f.SyscallConn()
 	if e != nil {
-		return ioctl_inner(f.Fd(), cmd, ptr) // fall back to blocking io (old behavior)
+		return ioctlInner(f.Fd(), cmd, ptr) // Fall back to blocking io (old behavior).
 	}
 
 	ch := make(chan error, 1)
 	defer close(ch)
 
-	e = sc.Control(func(fd uintptr) { ch <- ioctl_inner(fd, cmd, ptr) })
+	e = sc.Control(func(fd uintptr) { ch <- ioctlInner(fd, cmd, ptr) })
 	if e != nil {
 		return e
 	}
