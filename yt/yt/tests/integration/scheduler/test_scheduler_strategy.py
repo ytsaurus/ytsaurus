b25afdd6e96174178becf7df380ceb604c9f9104
eshcherbin <eshcherbin@ytsaurus.tech>
2025-08-29 17:04:52 +0300

    [25.3] YT-20704: Remove promised fair share from orchid, profiling, tests, scheduler monitor and meta scheduler commit_hash:35aeddd6991ea93cf82f20ec073916172624eccc

--- yt/yt/tests/integration/scheduler/test_scheduler_strategy.py
+++ yt/yt/tests/integration/scheduler/test_scheduler_strategy.py
@@ -118,7 +118,7 @@ class TestResourceUsage(YTEnvSetup, PrepareTables):
         )
 
     @authors("ignat")
-    def test_scheduler_promised_fair_share(self):
+    def test_estimated_guarantee_share(self):
         total_resource_limits = get("//sys/scheduler/orchid/scheduler/cluster/resource_limits")
 
         create_pool(
@@ -134,26 +134,26 @@ class TestResourceUsage(YTEnvSetup, PrepareTables):
         # Wait for fair share update.
         time.sleep(1)
 
-        def get_pool_promised_fair_share_resources(pool):
-            return get(scheduler_orchid_pool_path(pool) + "/promised_fair_share_resources")
+        def get_pool_estimated_guarantee_resources(pool):
+            return get(scheduler_orchid_pool_path(pool) + "/estimated_guarantee_resources")
 
-        def get_pool_promised_dominant_fair_share(pool):
-            return get(scheduler_orchid_pool_path(pool) + "/promised_dominant_fair_share")
+        def get_pool_dominant_estimated_guarantee_share(pool):
+            return get(scheduler_orchid_pool_path(pool) + "/dominant_estimated_guarantee_share")
 
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("big_pool"), 1.0)
-        assert get_pool_promised_fair_share_resources("big_pool") == total_resource_limits
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("big_pool"), 1.0)
+        assert get_pool_estimated_guarantee_resources("big_pool") == total_resource_limits
 
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("small_pool"), 0)
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("subpool_3"), 0)
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("subpool_4"), 0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("small_pool"), 0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("subpool_3"), 0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("subpool_4"), 0)
 
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("subpool_1"), 1.0 / 4.0)
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("subpool_2"), 3.0 / 4.0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("subpool_1"), 1.0 / 4.0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("subpool_2"), 3.0 / 4.0)
 
         self._prepare_tables()
 
-        def get_operation_promised_dominant_fair_share(op):
-            return op.get_runtime_progress("scheduling_info_per_pool_tree/default/promised_dominant_fair_share", 0.0)
+        def get_operation_dominant_estimated_guarantee_share(op):
+            return op.get_runtime_progress("scheduling_info_per_pool_tree/default/dominant_estimated_guarantee_share", 0.0)
 
         op = map(
             track=False,
@@ -167,9 +167,9 @@ class TestResourceUsage(YTEnvSetup, PrepareTables):
         # Wait for fair share update.
         time.sleep(1)
 
-        assert are_almost_equal(get_operation_promised_dominant_fair_share(op), 1.0 / 5.0)
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("subpool_1"), 1.0 / 5.0)
-        assert are_almost_equal(get_pool_promised_dominant_fair_share("subpool_2"), 3.0 / 5.0)
+        assert are_almost_equal(get_operation_dominant_estimated_guarantee_share(op), 1.0 / 5.0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("subpool_1"), 1.0 / 5.0)
+        assert are_almost_equal(get_pool_dominant_estimated_guarantee_share("subpool_2"), 3.0 / 5.0)
 
         release_breakpoint()
         op.track()
