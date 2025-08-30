2e2d5867ada1ea60e830aeeb9608e6cd48b84280
renadeen <renadeen@ytsaurus.tech>
2025-08-30 13:35:35 +0300

    [25.3] YT-25777: Add gpu discovery method via gpu agent
    * Changelog entry
    Type:        feature
    Component:   gpu-node
    Add gpu discovery method via gpu-agent. This method should be used in open source.
    commit_hash:bf7f25cdfc43ee3f03a77908614e959c60e5ebf4

---
+++ yt/yt/library/gpu/gpu_info_provider_detail.h
@@ -0,0 +1,41 @@
+#pragma once
+
+#include "gpu_info_provider.h"
+#include "private.h"
+#include "public.h"
+
+#include <library/cpp/yt/yson/consumer.h>
+
+namespace NYT::NGpu {
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TGpuInfoProviderBase
+    : public IGpuInfoProvider
+{
+    std::vector<TGpuInfo> GetGpuInfos(TDuration timeout) const override;
+    std::vector<TRdmaDeviceInfo> GetRdmaDeviceInfos(TDuration timeout) const override;
+
+    void ApplyNetworkServiceLevel(const std::vector<TString>& deviceIds, TNetworkPriority networkServiceLevel, TDuration timeout) override;
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+class TGrpcGpuInfoProviderBase
+    : public TGpuInfoProviderBase
+{
+public:
+    explicit TGrpcGpuInfoProviderBase(TGrpcGpuInfoProviderConfigBasePtr config);
+
+protected:
+    const TGrpcGpuInfoProviderConfigBasePtr BaseGrpcConfig_;
+
+    const NRpc::IChannelPtr Channel_;
+
+private:
+    NRpc::IChannelPtr CreateGrpcChannel();
+};
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NGpu
