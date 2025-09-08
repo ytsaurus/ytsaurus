6a49bbc0d4e53047981b74a654df1b8f95dd57bb
tinarsky <tinarsky@ytsaurus.tech>
2025-09-08 15:46:11 +0300

    [java] YT-26005 Fix OperationContext.getTableIndex when using ReducerWithKey
    * Changelog entry
    Type: fix
    Component: java-sdk
    
    Fix OperationContext.getTableIndex() when using ReducerWithKey.
    commit_hash:6c5e3e0133e20793931c57153c2c6dd5376ea039

--- yt/java/ytsaurus-client/src/test-integration/java/tech/ytsaurus/client/operations/ReduceOperationTest.java
+++ yt/java/ytsaurus-client/src/test-integration/java/tech/ytsaurus/client/operations/ReduceOperationTest.java
@@ -10,6 +10,7 @@ import org.junit.Test;
 import tech.ytsaurus.client.YTsaurusClientTestBase;
 import tech.ytsaurus.client.request.ReduceOperation;
 import tech.ytsaurus.client.request.SortOperation;
+import tech.ytsaurus.core.operations.OperationContext;
 import tech.ytsaurus.core.operations.Yield;
 import tech.ytsaurus.core.tables.ColumnValueType;
 import tech.ytsaurus.core.tables.TableSchema;
@@ -165,5 +166,164 @@ public class ReduceOperationTest extends YTsaurusClientTestBase {
         builder.addValue("name", ColumnValueType.STRING);
         builder.addValue("count", ColumnValueType.INT64);
         return builder.build().toWrite();
+    }
+
+    public static class MultiInputIndexReducerYTreeMapNode
+            implements ReducerWithKey<YTreeMapNode, YTreeMapNode, Long> {
+        @Override
+        public Long key(YTreeMapNode entry) {
+            return entry.getLong("key");
+        }
+
+        @Override
+        public void reduce(Long key, Iterator<YTreeMapNode> entries, Yield<YTreeMapNode> yield,
+                           Statistics statistics, OperationContext context) {
+            while (entries.hasNext()) {
+                YTreeMapNode row = entries.next();
+                long idx = context.getTableIndex();
+
+                YTreeMapNode outputRow = YTree.builder().beginMap()
+                        .key("key").value(key)
+                        .key("data").value(row.getString("data"))
+                        .key("tableIdx").value(idx)
+                        .buildMap();
+                yield.yield(outputRow);
+            }
+        }
+
+        @Override
+        public boolean trackIndices() {
+            return true;
+        }
+    }
+
+    private TableSchema createKeyDataSchema() {
+        TableSchema.Builder builder = new TableSchema.Builder();
+        builder.setUniqueKeys(false);
+        builder.addValue("key", ColumnValueType.INT64);
+        builder.addValue("data", ColumnValueType.STRING);
+        return builder.build().toWrite();
+    }
+
+    private TableSchema createKeyDataIdxSchema() {
+        TableSchema.Builder builder = new TableSchema.Builder();
+        builder.setUniqueKeys(false);
+        builder.addValue("key", ColumnValueType.INT64);
+        builder.addValue("data", ColumnValueType.STRING);
+        builder.addValue("tableIdx", ColumnValueType.INT64);
+        return builder.build().toWrite();
+    }
+
+    @Test
+    public void testMultiInputTableIndexInContextYTreeMapNode() {
+        testMultiInput(new MultiInputIndexReducerYTreeMapNode());
+    }
+
+    @Entity
+    public static class KeyDataEntityInput {
+        public long key;
+        public String data;
+    }
+
+    @Entity
+    public static class TableIndexEntityOutput {
+        public long key;
+        public String data;
+        public long tableIdx;
+    }
+
+    public static class MultiInputIndexEntityReducer
+            implements ReducerWithKey<KeyDataEntityInput, TableIndexEntityOutput, Long> {
+        @Override
+        public Long key(KeyDataEntityInput entry) {
+            return entry.key;
+        }
+
+        @Override
+        public void reduce(Long key, Iterator<KeyDataEntityInput> entries, Yield<TableIndexEntityOutput> yield,
+                           Statistics statistics, OperationContext context) {
+            while (entries.hasNext()) {
+                KeyDataEntityInput in = entries.next();
+                TableIndexEntityOutput out = new TableIndexEntityOutput();
+                out.key = in.key;
+                out.data = in.data;
+                out.tableIdx = context.getTableIndex();
+                yield.yield(out);
+            }
+        }
+
+        @Override
+        public boolean trackIndices() {
+            return true;
+        }
+    }
+
+    @Test
+    public void testMultiInputTableIndexInContextEntity() {
+        testMultiInput(new MultiInputIndexEntityReducer());
+    }
+
+    private void testMultiInput(Reducer<?, ?> reducer) {
+        var ytFixture = createYtFixture();
+        var yt = ytFixture.getYt();
+        var a = ytFixture.getTestDirectory().child("multiTable-a");
+        var b = ytFixture.getTestDirectory().child("multiTable-b");
+        var out = ytFixture.getTestDirectory().child("multiTable-out");
+
+        var inSchema = createKeyDataSchema();
+        var outSchema = createKeyDataIdxSchema();
+
+        writeTable(yt, a, inSchema, List.of(
+                YTree.builder().beginMap().key("key").value(1).key("data").value("a").buildMap(),
+                YTree.builder().beginMap().key("key").value(2).key("data").value("a").buildMap()
+        ));
+        writeTable(yt, b, inSchema, List.of(
+                YTree.builder().beginMap().key("key").value(1).key("data").value("b").buildMap(),
+                YTree.builder().beginMap().key("key").value(2).key("data").value("b").buildMap()
+        ));
+
+        Operation sortOp1 = yt.sort(SortOperation.builder()
+                .setSpec(SortSpec.builder()
+                        .setInputTables(a)
+                        .setOutputTable(a)
+                        .setSortBy("key")
+                        .build())
+                .build()).join();
+        sortOp1.watchAndThrowIfNotSuccess().join();
+
+        Operation sortOp2 = yt.sort(SortOperation.builder()
+                .setSpec(SortSpec.builder()
+                        .setInputTables(b)
+                        .setOutputTable(b)
+                        .setSortBy("key")
+                        .build())
+                .build()).join();
+        sortOp2.watchAndThrowIfNotSuccess().join();
+
+        Operation reduceOp = yt.reduce(ReduceOperation.builder()
+                .setSpec(ReduceSpec.builder()
+                        .setReducerSpec(new ReducerSpec(reducer))
+                        .setInputTables(a, b)
+                        .setOutputTables(out.withSchema(outSchema.toYTree()))
+                        .setReduceBy("key")
+                        .build())
+                .build()).join();
+
+        reduceOp.watchAndThrowIfNotSuccess().join();
+
+        List<YTreeMapNode> rows = readTable(yt, out);
+
+        List<YTreeMapNode> expected = List.of(
+                YTree.builder().beginMap().key("key").value(1).key("data").value("a").key("tableIdx").value(0)
+                        .buildMap(),
+                YTree.builder().beginMap().key("key").value(1).key("data").value("b").key("tableIdx").value(1)
+                        .buildMap(),
+                YTree.builder().beginMap().key("key").value(2).key("data").value("a").key("tableIdx").value(0)
+                        .buildMap(),
+                YTree.builder().beginMap().key("key").value(2).key("data").value("b").key("tableIdx").value(1)
+                        .buildMap()
+        );
+
+        Assert.assertEquals(expected, rows);
     }
 }
