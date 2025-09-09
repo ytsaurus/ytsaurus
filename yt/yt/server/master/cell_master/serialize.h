08f33627bdd6e238a000d334b01d87926d4b5114
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2025-09-09 14:23:22 +0300

    Duplicate transaction manager early exits and exceptions in transaction supervisors to ensure strongly ordered transactions are not stuck commit_hash:871154518e145f874d7a5b1717bf3ea47f3bce7c

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -181,6 +181,7 @@ DEFINE_ENUM(EMasterReign,
     ((PreserveUnflushedTimestampForUnmountedTablets)                (3026))  // ifsmirnov
     ((LocalUserRequestThrottlers)                                   (3027))  // faucct
     ((FixResolvePrerequisitePathToLocalObjectForSymlinks)           (3028))  // cherepashka
+    ((SequencerStateFix)                                            (3029))  // aleksandra-zh
 );
 
 static_assert(TEnumTraits<EMasterReign>::IsMonotonic, "Master reign enum is not monotonic");
