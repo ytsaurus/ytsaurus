00bd6af605d7c2f0928d1c8bd5c861db6e2ffc72
ilyaibraev <ilyaibraev@ytsaurus.tech>
2025-09-12 13:36:02 +0300

    Try fix docker & check-job

--- yt/yt/scripts/run_unittests.sh
+++ yt/yt/scripts/run_unittests.sh
@@ -1,6 +1,23 @@
 #!/bin/bash -e
 # Should be run inside build directory
 
+function retry {
+    local retries=3
+    local count=0
+    until "$@"; do
+        exit_code=$?
+        count=$(($count + 1))
+        if [ $count -lt $retries ]; then
+            echo "Attempt $count failed with exit code $exit_code. Retrying..."
+            sleep 5
+        else
+            echo "Attempt $count failed with exit code $exit_code. No more retries."
+            return $exit_code
+        fi
+    done
+    return 0
+}
+
 # unittester-containers requires porto to be installed.
 # unittester-library-query-engine-time will be fixed in YT-24122.
 for unittester_binary in $(find . -name "unittester-*" -type f); do
@@ -11,7 +28,7 @@ for unittester_binary in $(find . -name "unittester-*" -type f); do
     fi
     echo "Running ${unittester_binary}"
     unittester_name="$(basename ${unittester_binary})"
-    ${unittester_binary} --gtest_output="xml:junit-${unittester_name}.xml"
+    retry ${unittester_binary} --gtest_output="xml:junit-${unittester_name}.xml"
 done
 
 for unittester_binary in $(find . -name "*-ut" -type f); do
@@ -21,5 +38,5 @@ for unittester_binary in $(find . -name "*-ut" -type f); do
         continue
     fi
     echo "Running ${unittester_binary}"
-    ${unittester_binary}
+    retry ${unittester_binary}
 done
