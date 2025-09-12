63b71105948537b97e5e9446ff871b7119e043e1
marydrobotun <marydrobotun@gmail.com>
2025-09-12 18:58:01 +0000

    added log_once function

--- yt/python/yt/wrapper/tests/serverless/test_misc.py
+++ yt/python/yt/wrapper/tests/serverless/test_misc.py
@@ -1,7 +1,10 @@
+import yt.logger as yt_logger
 import yt.wrapper as yt
 from typing import get_type_hints
 
 from yt.testlib import authors
+
+from unittest.mock import Mock
 
 
 @authors("denvr")
@@ -14,3 +17,14 @@ def test_config_types():
                 _check_keys(type_hints[param_name], param_value)
 
     _check_keys(yt.default_config.DefaultConfigType, yt.default_config.default_config)
+
+
+def test_log_once():
+    tmp_log = yt_logger.LOGGER.log
+    logger_mock = Mock()
+    yt_logger.LOGGER.log = logger_mock
+    yt_logger.log_once(30, 'test')
+    yt_logger.log_once(30, 'test')
+    yt_logger.log_once(30, 'test')
+    assert logger_mock.call_count == 1
+    yt_logger.LOGGER.log = tmp_log
