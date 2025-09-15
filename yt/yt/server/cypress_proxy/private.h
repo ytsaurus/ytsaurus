cff8f6fde4648518c3a1ad240f204fba2b1c7951
grphil <grphil@ytsaurus.tech>
2025-09-15 16:38:07 +0300

    YT-25813: Add recursive resource usage attribute support to sequoia
    Add support for special attributes calculation on cypress proxy, added recursive_resource_usage attribute support to sequoia
    commit_hash:7c59dcf1e88307cbc0e2a23e533008e32920dc88

--- yt/yt/server/cypress_proxy/private.h
+++ yt/yt/server/cypress_proxy/private.h
@@ -10,6 +10,8 @@ namespace NYT::NCypressProxy {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+DECLARE_REFCOUNTED_CLASS(ISequoiaAttributeFetcher)
+
 struct TRequestExecutedPayload
 { };
 
