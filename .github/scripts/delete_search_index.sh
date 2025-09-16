e7fa08930075cdbd8f609dc4b01ca7d81d44c011
Olesya Gorbacheva <ogorbacheva@yandex-team.ru>
2025-09-16 15:16:15 +0300

    Algolia in YTsaurus docs
    New docs release flow with Algolia search:
    - Added new github action docs_release_testing
    - Added backup action with old search engine
    - Added script for deleting search index
    
    ---
    
    Pull Request resolved: https://github.com/ytsaurus/ytsaurus/pull/1452
    
    Co-authored-by: ogorbacheva <ogorbacheva@ytsaurus.tech>
    Co-authored-by: ogorbacheva <ogorbacheva@ytsaurus.tech>
    Co-authored-by: ogorbacheva <ogorbacheva@ytsaurus.tech>
    commit_hash:b19a50637d0b455f75cfb35f702798e36de53d17

---
+++ .github/scripts/delete_search_index.sh
@@ -0,0 +1,37 @@
+#!/bin/bash
+
+#ALGOLIA_APP_ID="app-id"
+#ALGOLIA_API_KEY="admin-api-key"
+MIN_INDEX_COUNT=10      # Minimum number of indexes to keep.
+COUNT_TO_DELETE=1       # Number of indexes to delete per run.
+
+# Get list of indexes.
+response=$(curl -s -H "X-Algolia-API-Key: $ALGOLIA_API_KEY" \
+    -H "X-Algolia-Application-Id: $ALGOLIA_APP_ID" \
+    "https://${ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes")
+
+# Count the number of indexes.
+count=$(echo "$response" | jq '.items | length')
+
+echo "Total indices: $count"
+
+if [ "$count" -le "$MIN_INDEX_COUNT" ]; then
+  echo "Deletion not requiered, there are $count indices now (minimum is $MIN_INDEX_COUNT)."
+  exit 0
+fi
+
+# Sort by updatedAt (oldest first).
+indices=$(echo "$response" | jq -c '.items | sort_by(.updatedAt)[] | {name,updatedAt}')
+
+# Get N oldest.
+to_delete=$(echo "$indices" | head -n "$COUNT_TO_DELETE" | jq -r '.name')
+
+echo "Indexes to delete: $to_delete"
+
+for idx in $to_delete; do
+  echo "Deleting index: $idx"
+  curl -s -X DELETE \
+       -H "X-Algolia-API-Key: $ALGOLIA_API_KEY" \
+       -H "X-Algolia-Application-Id: $ALGOLIA_APP_ID" \
+       "https://${ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes/$idx"
+done
