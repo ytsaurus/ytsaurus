631fe0e1130f4242dcfbed6f1d18f19cb23bcf5f
Olesya Gorbacheva <ogorbacheva@yandex-team.ru>
2025-09-16 13:50:42 +0300

    Translate comments to english

--- .github/scripts/delete_search_index.sh
+++ .github/scripts/delete_search_index.sh
@@ -2,34 +2,34 @@
 
 #ALGOLIA_APP_ID="app-id"
 #ALGOLIA_API_KEY="admin-api-key"
-MIN_INDEX_COUNT=10      # Минимальное количество индексов, которые нужно оставить
-COUNT_TO_DELETE=1       # Сколько удалить за один запуск
+MIN_INDEX_COUNT=10      # Minimum number of indexes to keep.
+COUNT_TO_DELETE=1       # Number of indexes to delete per run.
 
-# Получить список индексов
+# Get list of indexes.
 response=$(curl -s -H "X-Algolia-API-Key: $ALGOLIA_API_KEY" \
     -H "X-Algolia-Application-Id: $ALGOLIA_APP_ID" \
     "https://${ALGOLIA_APP_ID}-dsn.algolia.net/1/indexes")
 
-# Посчитать количество индексов
+# Count the number of indexes.
 count=$(echo "$response" | jq '.items | length')
 
-echo "Всего индексов: $count"
+echo "Total indices: $count"
 
 if [ "$count" -le "$MIN_INDEX_COUNT" ]; then
-  echo "Удаление не требуется, индексов $count (минимум: $MIN_INDEX_COUNT)."
+  echo "Deletion not requiered, there are $count indices now (minimum is $MIN_INDEX_COUNT)."
   exit 0
 fi
 
-# Отсортировать по updatedAt (самые старые – первые)
+# Sort by updatedAt (oldest first).
 indices=$(echo "$response" | jq -c '.items | sort_by(.updatedAt)[] | {name,updatedAt}')
 
-# Получить N самых старых
+# Get N oldest.
 to_delete=$(echo "$indices" | head -n "$COUNT_TO_DELETE" | jq -r '.name')
 
-echo "Удаляю индексы: $to_delete"
+echo "Indexes to delete: $to_delete"
 
 for idx in $to_delete; do
-  echo "Удаляю индекс: $idx"
+  echo "Deleting index: $idx"
   curl -s -X DELETE \
        -H "X-Algolia-API-Key: $ALGOLIA_API_KEY" \
        -H "X-Algolia-Application-Id: $ALGOLIA_APP_ID" \
