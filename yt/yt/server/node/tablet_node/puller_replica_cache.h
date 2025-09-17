5d3282b8a86a6268e831053c68107a21f5260daf
tea-mur <tea-mur@ytsaurus.tech>
2025-09-17 10:33:44 +0300

    YT-25267: Caching tablet ids of the replicas that pull from the tablet
    YT-25267: Caching replicas, pulling from tablet
    commit_hash:1eb9bcf26f14bf79de08f3ab610bc5c173f87c71

---
+++ yt/yt/server/node/tablet_node/puller_replica_cache.h
@@ -0,0 +1,30 @@
+#pragma once
+
+#include "public.h"
+
+#include <yt/yt/client/chaos_client/public.h>
+
+namespace NYT::NTabletNode {
+
+////////////////////////////////////////////////////////////////////////////////
+
+//! This cache stores tablet ids of the replicas, that recently pulled data from the tablet.
+struct IPullerReplicaCache
+    : public virtual TRefCounted
+{
+    virtual void OnPull(const TTabletId& pullerTabletId) = 0;
+};
+
+DEFINE_REFCOUNTED_TYPE(IPullerReplicaCache)
+
+////////////////////////////////////////////////////////////////////////////////
+
+IPullerReplicaCachePtr GetDisabledPullerReplicaCache();
+
+IPullerReplicaCachePtr CreatePullerReplicaCache(
+    TTablet* tablet,
+    NChaosClient::TReplicationCardId replicationCardId);
+
+////////////////////////////////////////////////////////////////////////////////
+
+} // namespace NYT::NTabletNode
