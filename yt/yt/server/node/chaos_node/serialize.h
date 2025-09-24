9e1103f093075072f3ac821aa1110265f5320dac
ponasenko-rs <ponasenko-rs@ytsaurus.tech>
2025-09-25 00:34:01 +0300

    YT-26255: Persist signature of chaos transaction commit_hash:d214607f822f1e94ee3b7a6776a4c5603969e391

--- yt/yt/server/node/chaos_node/serialize.h
+++ yt/yt/server/node/chaos_node/serialize.h
@@ -25,8 +25,10 @@ DEFINE_ENUM(EChaosReign,
     ((IntroduceChaosObjectAndLease)             (300102)) // gryzlov-ad
     ((FixTransactionActionAbort)                (300103)) // kvk1920
     ((TransactionActionStates)                  (300104)) // babenko
+    ((PersistTransactionSignature_25_2)         (300105)) // ponasenko-rs
     // 25.3 starts here
     ((AddLastCoordinatorCommitTimestamp)        (300201)) // aleksandra-zh
+    ((PersistTransactionSignature)              (300202)) // ponasenko-rs
 );
 
 static_assert(TEnumTraits<EChaosReign>::IsMonotonic, "Chaos reign enum is not monotonic");
