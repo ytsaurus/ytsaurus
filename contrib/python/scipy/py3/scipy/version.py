33710a843cb566616695a96a01db49faaa40f470
robot-piglet <robot-piglet@yandex-team.com>
2025-09-25 18:14:14 +0300

    Intermediate changes commit_hash:756a85825380f76f173d751aad3f91c4442046e9

--- contrib/python/scipy/py3/scipy/version.py
+++ contrib/python/scipy/py3/scipy/version.py
@@ -2,10 +2,10 @@
 """
 Module to expose more detailed version info for the installed `scipy`
 """
-version = "1.16.1"
+version = "1.16.2"
 full_version = version
 short_version = version.split('.dev')[0]
-git_revision = "0cf8e9541b1a2457992bf4ec2c0c669da373e497"
+git_revision = "b1296b9b4393e251511fe8fdd3e58c22a1124899"
 release = 'dev' not in version and '+' not in version
 
 if not release:
