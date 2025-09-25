ytsaurus/ytsaurus

commit 54f81642716dde0d81a02603418c463cb7e48677

add ssl config for s3 storage commit_hash:0aa02bf106b7efa521fe2e117f3f61f93d3c7f4f

yt/yt/library/s3/client.cpp
@@ -1,5 +1,6 @@
 #include "client.h"
 
+#include <yt/yt/core/crypto/config.h>
 #include <yt/yt/core/crypto/crypto.h>
 
 #include <yt/yt/core/net/address.h>
@@ -15,6 +16,7 @@
 namespace NYT::NS3 {
 
 using namespace NConcurrency;
+using namespace NCrypto;
 using namespace NNet;
 
 using TPocoXmlDocumentPtr = Poco::XML::AutoPtr<Poco::XML::Document>;
@@ -378,10 +380,12 @@ public:
     TClient(
         TS3ClientConfigPtr config,
         ICredentialsProviderPtr credentialProvider,
+        TSslContextConfigPtr sslContextConfig,
         IPollerPtr poller,
         IInvokerPtr executionInvoker)
         : Config_(std::move(config))
         , CredentialProvider_(std::move(credentialProvider))
+        , SslContextConfig_(std::move(sslContextConfig))
         , Poller_(std::move(poller))
         , ExecutionInvoker_(std::move(executionInvoker))
     { }
@@ -413,7 +417,7 @@ public:
             Client_ = CreateHttpClient(
                 Config_,
                 s3Address,
-                useTls,
+                SslContextConfig_,
                 Poller_,
                 ExecutionInvoker_);
             return Client_->Start();
@@ -502,6 +506,7 @@ private:
     TS3ClientConfigPtr Config_;
     ICredentialsProviderPtr CredentialProvider_;
     TNetworkAddress S3Address_;
+    TSslContextConfigPtr SslContextConfig_;
 
     IPollerPtr Poller_;
     IInvokerPtr ExecutionInvoker_;
@@ -516,12 +521,14 @@ private:
 IClientPtr CreateClient(
     TS3ClientConfigPtr config,
     ICredentialsProviderPtr credentialProvider,
+    TSslContextConfigPtr sslContextConfig,
     IPollerPtr poller,
     IInvokerPtr executionInvoker)
 {
     return New<TClient>(
         std::move(config),
         std::move(credentialProvider),
+        std::move(sslContextConfig),
         std::move(poller),
         std::move(executionInvoker));
 }

yt/yt/library/s3/http.cpp
@@ -6,6 +6,7 @@
 #include <yt/yt/core/concurrency/async_rw_lock.h>
 #include <yt/yt/core/concurrency/thread_pool_poller.h>
 
+#include <yt/yt/core/crypto/config.h>
 #include <yt/yt/core/crypto/tls.h>
 
 #include <yt/yt/core/http/stream.h>
@@ -216,9 +217,10 @@ void PrepareHttpRequest(
 
 namespace {
 
-NYT::NCrypto::TSslContextPtr CreateSslContext()
+NYT::NCrypto::TSslContextPtr CreateSslContext(const NYT::NCrypto::TSslContextConfigPtr& config, NYT::NCrypto::TCertificatePathResolver pathResolver = nullptr)
 {
     auto sslContext = New<NYT::NCrypto::TSslContext>();
+    sslContext->ApplyConfig(config, std::move(pathResolver));
     sslContext->Commit();
     return sslContext;
 }
@@ -232,13 +234,13 @@ public:
     THttpClient(
         NHttp::TClientConfigPtr config,
         TNetworkAddress address,
-        bool useTls,
+        const NYT::NCrypto::TSslContextConfigPtr& sslContextConfig,
         IPollerPtr poller,
         IInvokerPtr invoker)
         : Config_(config)
         , Address_(std::move(address))
-        , Dialer_(useTls
-            ? CreateSslContext()->CreateDialer(
+        , Dialer_(sslContextConfig
+            ? CreateSslContext(sslContextConfig)->CreateDialer(
                 config->Dialer,
                 std::move(poller),
                 S3Logger())
@@ -328,14 +330,14 @@ private:
 IHttpClientPtr CreateHttpClient(
     NHttp::TClientConfigPtr config,
     NNet::TNetworkAddress address,
-    bool useTls,
+    const NYT::NCrypto::TSslContextConfigPtr& sslContextConfig,
     NConcurrency::IPollerPtr poller,
     IInvokerPtr invoker)
 {
     return New<THttpClient>(
         std::move(config),
         std::move(address),
-        useTls,
+        sslContextConfig,
         std::move(poller),
         std::move(invoker));
 }

yt/yt/library/s3/http.h
@@ -3,6 +3,8 @@
 #include "credential_provider.h"
 #include "public.h"
 
+#include <yt/yt/core/crypto/config.h>
+
 #include <yt/yt/core/http/http.h>
 
 namespace NYT::NS3 {
@@ -70,7 +72,7 @@ DEFINE_REFCOUNTED_TYPE(IHttpClient)
 IHttpClientPtr CreateHttpClient(
     NHttp::TClientConfigPtr config,
     NNet::TNetworkAddress address,
-    bool useTls,
+    const NCrypto::TSslContextConfigPtr& sslContextConfig,
     NConcurrency::IPollerPtr poller,
     IInvokerPtr invoker);
 

yt/yt/library/s3/client.h
@@ -351,6 +351,7 @@ DEFINE_REFCOUNTED_TYPE(IClient)
 IClientPtr CreateClient(
     TS3ClientConfigPtr config,
     ICredentialsProviderPtr credentialProvider,
+    NCrypto::TSslContextConfigPtr sslContextConfig,
     NConcurrency::IPollerPtr poller,
     IInvokerPtr executionInvoker);
 

yt/yt/library/s3/unittests/client_ut.cpp
@@ -53,6 +53,7 @@ private:
         S3Client_ = CreateClient(
             std::move(clientConfig),
             S3CredentialProvider_,
+            /*sslContextConfig*/ nullptr,
             Poller_,
             Poller_->GetInvoker());
 

yt/yt/tools/import_table/lib/import_table.cpp
@@ -152,6 +152,7 @@ NS3::IClientPtr CreateS3Client(
     auto client = NS3::CreateClient(
         std::move(clientConfig),
         std::move(credentialProvider),
+        /*sslContextConfig*/ nullptr,
         poller,
         poller->GetInvoker());
 

yt/yt/tools/import_table/unittests/import_table_ut.cpp
@@ -836,6 +836,7 @@ private:
         auto client = NS3::CreateClient(
             std::move(clientConfig),
             NS3::CreateStaticCredentialProvider("key", "secret"),
+            /*sslContextConfig*/ nullptr,
             poller,
             poller->GetInvoker());
 

commit 42a2ec309f1c88884eef92ed77d9b268a2ebff40

yt/yt/library/s3: fix tls config
Pass "useTls" as separate flag. Null tls-config doesn't mean "http".
Use built-in CA bundle is config is not provided like https client.
And pass host name into dialer context for certificate verification.

Fixes: 54f81642716 ("add ssl config for s3 storage commit_hash:0aa02bf106b7efa521fe2e117f3f61f93d3c7f4f")
Signed-off-by: Konstantin Khlebnikov <khlebnikov@tracto.ai>

yt/yt/library/s3/client.cpp
@@ -417,6 +417,7 @@ public:
             Client_ = CreateHttpClient(
                 Config_,
                 s3Address,
+                useTls,
                 SslContextConfig_,
                 Poller_,
                 ExecutionInvoker_);

yt/yt/library/s3/http.cpp
@@ -220,7 +220,11 @@ namespace {
 NYT::NCrypto::TSslContextPtr CreateSslContext(const NYT::NCrypto::TSslContextConfigPtr& config, NYT::NCrypto::TCertificatePathResolver pathResolver = nullptr)
 {
     auto sslContext = New<NYT::NCrypto::TSslContext>();
-    sslContext->ApplyConfig(config, std::move(pathResolver));
+    if (config) {
+        sslContext->ApplyConfig(config, std::move(pathResolver));
+    } else {
+        sslContext->UseBuiltinOpenSslX509Store();
+    }
     sslContext->Commit();
     return sslContext;
 }
@@ -234,12 +238,13 @@ public:
     THttpClient(
         NHttp::TClientConfigPtr config,
         TNetworkAddress address,
+        bool useTls,
         const NYT::NCrypto::TSslContextConfigPtr& sslContextConfig,
         IPollerPtr poller,
         IInvokerPtr invoker)
         : Config_(config)
         , Address_(std::move(address))
-        , Dialer_(sslContextConfig
+        , Dialer_(useTls
             ? CreateSslContext(sslContextConfig)->CreateDialer(
                 config->Dialer,
                 std::move(poller),
@@ -286,7 +291,10 @@ private:
             request.Region,
             request.Service);
 
-        auto connection = WaitFor(Dialer_->Dial(Address_))
+        auto dialerContext = New<TDialerContext>();
+        dialerContext->Host = request.Host;
+
+        auto connection = WaitFor(Dialer_->Dial(Address_, dialerContext))
             .ValueOrThrow();
 
         auto input = New<THttpInput>(
@@ -341,6 +349,7 @@ private:
 IHttpClientPtr CreateHttpClient(
     NHttp::TClientConfigPtr config,
     NNet::TNetworkAddress address,
+    bool useTls,
     const NYT::NCrypto::TSslContextConfigPtr& sslContextConfig,
     NConcurrency::IPollerPtr poller,
     IInvokerPtr invoker)
@@ -348,6 +357,7 @@ IHttpClientPtr CreateHttpClient(
     return New<THttpClient>(
         std::move(config),
         std::move(address),
+        useTls,
         sslContextConfig,
         std::move(poller),
         std::move(invoker));

yt/yt/library/s3/http.h
@@ -72,6 +72,7 @@ DEFINE_REFCOUNTED_TYPE(IHttpClient)
 IHttpClientPtr CreateHttpClient(
     NHttp::TClientConfigPtr config,
     NNet::TNetworkAddress address,
+    bool useTls,
     const NCrypto::TSslContextConfigPtr& sslContextConfig,
     NConcurrency::IPollerPtr poller,
     IInvokerPtr invoker);