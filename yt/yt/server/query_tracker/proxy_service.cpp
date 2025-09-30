7882bbfc641d6160d98fbe0ecf070273dc75efed
mpereskokova <mpereskokova@ytsaurus.tech>
2025-09-30 13:25:34 +0300

    [25.1] YT-26097: Tutorials commit_hash:891d6226c78a61dc09719c4609de0954dcc6574b

--- yt/yt/server/query_tracker/proxy_service.cpp
+++ yt/yt/server/query_tracker/proxy_service.cpp
@@ -247,7 +247,7 @@ private:
 
     DECLARE_RPC_SERVICE_METHOD(NQueryTrackerClient::NProto, ListQueries)
     {
-        YT_VERIFY(NRpcProxy::NProto::TReqListQueries::GetDescriptor()->field_count() == 13);
+        YT_VERIFY(NRpcProxy::NProto::TReqListQueries::GetDescriptor()->field_count() == 14);
         YT_VERIFY(NRpcProxy::NProto::TRspListQueries::GetDescriptor()->field_count() == 3);
 
         auto rpcRequest = request->rpc_proxy_request();
