5eb738cd0bd7a82929cce7100bd5b76456aefadd
danilalexeev <danilalexeev@ytsaurus.tech>
2025-10-08 20:00:55 +0300

    YT-25434: Validate ground reign in Sequoia client (see rXXXXXX) commit_hash:e15a918df54cd6fb41a49e84bda7d90dd7d66517

--- yt/yt/ytlib/sequoia_client/sequoia_reign.cpp
+++ yt/yt/ytlib/sequoia_client/sequoia_reign.cpp
@@ -1,4 +1,8 @@
 #include "sequoia_reign.h"
+#include "public.h"
+
+#include <yt/yt/ytlib/api/native/config.h>
+#include <yt/yt/ytlib/api/native/connection.h>
 
 namespace NYT::NSequoiaClient {
 
@@ -7,6 +11,33 @@ namespace NYT::NSequoiaClient {
 ESequoiaReign GetCurrentSequoiaReign() noexcept
 {
     return TEnumTraits<ESequoiaReign>::GetMaxValue();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+EGroundReign GetCurrentGroundReign()
+{
+    return TEnumTraits<EGroundReign>::GetMaxValue();
+}
+
+////////////////////////////////////////////////////////////////////////////////
+
+TFuture<void> ValidateClusterGroundReign(
+    const NApi::NNative::IClientPtr& client,
+    const NYTree::TYPath& rootPath)
+{
+    auto attributesPath = rootPath + "/@";
+    return client->GetNode(attributesPath)
+        .Apply(BIND([] (NYson::TYsonString value) {
+            auto attributes = NYTree::ConvertToAttributes(value);
+            auto actualReign = EGroundReign(
+                attributes->Get<int>("ground_reign", static_cast<int>(EGroundReign::Unknown)));
+            auto expectedReign = GetCurrentGroundReign();
+            THROW_ERROR_EXCEPTION_IF(actualReign != expectedReign,
+                "Ground reigns differ (Expected: %v, Actual: %v)",
+                expectedReign,
+                actualReign);
+        }));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
