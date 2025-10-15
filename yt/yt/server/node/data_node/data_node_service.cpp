a03a74bb264cecb5b55ab1b256030b99bf01e731
tea-mur <tea-mur@ytsaurus.tech>
2025-10-15 17:13:50 +0300

    YT-25873: IO tracking for data node lookups (LookupRows) commit_hash:523566f6ffbabf5f36a2e498def5225479a32a4a

--- yt/yt/server/node/data_node/data_node_service.cpp
+++ yt/yt/server/node/data_node/data_node_service.cpp
@@ -1774,7 +1774,7 @@ private:
 
         auto chunkReadSession = CreateOffloadedChunkReadSession(
             Bootstrap_,
-            std::move(chunk),
+            chunk,
             readSessionId,
             workloadDescriptor,
             std::move(columnFilter),
@@ -1798,9 +1798,8 @@ private:
             netThrottling.Enabled,
             netThrottling.QueueSize);
 
-        //TODO(tea-mur): IO tracking for LookupRows
         context->ReplyFrom(chunkReadSession->Lookup(request->Attachments())
-            .Apply(BIND([=, this, this_ = MakeStrong(this)] (const TSharedRef& result) {
+            .Apply(BIND([=, this, this_ = MakeStrong(this), chunk = std::move(chunk)] (const TSharedRef& result) {
                 response->Attachments().push_back(result);
                 response->set_fetched_rows(true);
 
@@ -1812,6 +1811,21 @@ private:
                         std::memory_order::relaxed);
                 }
                 ToProto(response->mutable_chunk_reader_statistics(), chunkReaderStatistics);
+
+                 auto bytesReadFromDisk =
+                    chunkReaderStatistics->DataBytesReadFromDisk.load(std::memory_order::relaxed) +
+                    chunkReaderStatistics->MetaBytesReadFromDisk.load(std::memory_order::relaxed);
+                const auto& ioTracker = Bootstrap_->GetIOTracker();
+
+                if (bytesReadFromDisk > 0 && ioTracker->IsEnabled()) {
+                    auto ioRequests =
+                        chunkReaderStatistics->DataIORequests.load(std::memory_order::relaxed) +
+                        chunkReaderStatistics->MetaIORequests.load(std::memory_order::relaxed);
+
+                    ioTracker->Enqueue(
+                        TIOCounters{.Bytes = bytesReadFromDisk, .IORequests = ioRequests},
+                        MakeReadIOTags(context->GetMethod(), chunk->GetLocation(), context, chunk->GetId(), readSessionId));
+                }
 
                 context->SetComplete();
 
