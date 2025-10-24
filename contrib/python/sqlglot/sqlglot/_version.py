1a0e9391d1e9f76b36cc111f3ed5cf6ff180b022
robot-contrib <robot-contrib@yandex-team.com>
2025-10-24 22:25:44 +0300

    Update contrib/python/sqlglot to 27.25.2 commit_hash:31329ed59a16f56d1177062e8b0c6cf661390882

--- contrib/python/sqlglot/sqlglot/_version.py
+++ contrib/python/sqlglot/sqlglot/_version.py
@@ -28,7 +28,7 @@ version_tuple: VERSION_TUPLE
 commit_id: COMMIT_ID
 __commit_id__: COMMIT_ID
 
-__version__ = version = '27.24.2'
-__version_tuple__ = version_tuple = (27, 24, 2)
+__version__ = version = '27.25.2'
+__version_tuple__ = version_tuple = (27, 25, 2)
 
 __commit_id__ = commit_id = None
