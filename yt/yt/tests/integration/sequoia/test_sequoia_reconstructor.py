9663fbf610ae328e559c63cc5071c01cb5843fa8
grphil <grphil@ytsaurus.tech>
2025-11-06 16:00:15 +0300

    YT-18545: Add single cell sequoia reconstructor
    Add single cell version of sequoia reconstructor.
    commit_hash:f2a333e8b4e0036e8499cf5e2933296275e8dd49

---
+++ yt/yt/tests/integration/sequoia/test_sequoia_reconstructor.py
@@ -0,0 +1,375 @@
+from yt_env_setup import YTEnvSetup
+
+from yt_commands import (
+    authors, create, create_user, make_ace, remove, write_table, start_transaction, abort_transaction, commit_transaction, lock, link, set)
+
+from sequoia_reconstructor import check_sequoia_tables_reconstruction
+
+import pytest
+
+##################################################################
+
+
+class TestSequoiaReconstructor(YTEnvSetup):
+    ENABLE_MULTIDAEMON = False  # We will build snapshots.
+    USE_SEQUOIA = True
+    ENABLE_TMP_ROOTSTOCK = True
+    ENABLE_CYPRESS_TRANSACTIONS_IN_SEQUOIA = True
+    NUM_TEST_PARTITIONS = 2
+
+    MASTER_CELL_DESCRIPTORS = {
+        "10": {"roles": ["sequoia_node_host"]},
+    }
+
+    DELTA_DYNAMIC_MASTER_CONFIG = {
+        "sequoia_manager": {
+            "enable_ground_update_queues": True
+        },
+    }
+
+    DELTA_CYPRESS_PROXY_CONFIG = {
+        "user_directory_synchronizer": {
+            "sync_period": 100,
+            "sync_period_splay": 100,
+        },
+        "dynamic_config_manager": {
+            "update_period": 100,
+        },
+    }
+
+    @authors("grphil")
+    def test_simple_reconstruction(self):
+        check_sequoia_tables_reconstruction(self)
+
+        create("table", "//tmp/some_dir/table", recursive=True)
+        check_sequoia_tables_reconstruction(self)
+
+        write_table("//tmp/some_dir/table", [{"x": "hello"}])
+        check_sequoia_tables_reconstruction(self)
+
+        remove("//tmp/some_dir/table")
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_simple_transactions(self):
+        tx = start_transaction(timeout=100000)
+        check_sequoia_tables_reconstruction(self)
+
+        create("map_node", "//tmp/dir", tx=tx)
+        create("table", "//tmp/dir/table", tx=tx)
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx)
+        check_sequoia_tables_reconstruction(self)
+
+        tx = start_transaction(timeout=100000)
+        check_sequoia_tables_reconstruction(self)
+        remove("//tmp/dir/table", tx=tx)
+        check_sequoia_tables_reconstruction(self)
+        abort_transaction(tx)
+        check_sequoia_tables_reconstruction(self)
+
+        tx = start_transaction(timeout=100000)
+        check_sequoia_tables_reconstruction(self)
+        remove("//tmp/dir/table", tx=tx)
+        check_sequoia_tables_reconstruction(self)
+        commit_transaction(tx)
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_complex_transactions(self):
+        tx1 = start_transaction(timeout=100000)
+        create("map_node", "//tmp/dir", tx=tx1)
+
+        tx2 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/table", tx=tx2)
+        check_sequoia_tables_reconstruction(self)
+
+        tx3 = start_transaction(timeout=100000, tx=tx2)
+        remove("//tmp/dir/table", tx=tx3)
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx3)
+        check_sequoia_tables_reconstruction(self)
+
+        tx3 = start_transaction(timeout=100000, tx=tx2)
+        remove("//tmp/dir/table", tx=tx3)
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx2)
+        check_sequoia_tables_reconstruction(self)
+
+        tx2 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/table", tx=tx2)
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx2)
+        check_sequoia_tables_reconstruction(self)
+
+        tx11 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/t1", tx=tx11)
+
+        tx12 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/t2", tx=tx12)
+
+        tx13 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/t3", tx=tx13)
+
+        tx131 = start_transaction(timeout=100000, tx=tx13)
+        create("table", "//tmp/dir/t31", tx=tx131)
+
+        tx1311 = start_transaction(timeout=100000, tx=tx131)
+        remove("//tmp/dir/t3", tx=tx1311)
+
+        tx14 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/t4", tx=tx14)
+
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx11)
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx12)
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx1311)
+        commit_transaction(tx131)
+        commit_transaction(tx13)
+        commit_transaction(tx14)
+        commit_transaction(tx1)
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_node_snapshots(self):
+        create("map_node", "//tmp/dir")
+
+        tx1 = start_transaction(timeout=100000)
+        create("table", "//tmp/dir/t1", tx=tx1)
+
+        tx2 = start_transaction(timeout=100000, tx=tx1)
+        create("table", "//tmp/dir/t2", tx=tx2)
+
+        tx3 = start_transaction(timeout=100000, tx=tx2)
+        create("table", "//tmp/dir/t3", tx=tx3)
+
+        tx4 = start_transaction(timeout=100000, tx=tx3)
+        create("table", "//tmp/dir/t4", tx=tx4)
+
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx4)
+        commit_transaction(tx3)
+        check_sequoia_tables_reconstruction(self)
+
+        tx21 = start_transaction(timeout=100000, tx=tx2)
+        tx22 = start_transaction(timeout=100000, tx=tx2)
+
+        lock("//tmp/dir/t3", tx=tx21, mode="snapshot")
+        lock("//tmp/dir/t4", tx=tx22, mode="snapshot")
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx21)
+        abort_transaction(tx22)
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx2)
+
+        tx2 = start_transaction(timeout=100000, tx=tx1)
+        tx21 = start_transaction(timeout=100000, tx=tx2)
+        tx22 = start_transaction(timeout=100000, tx=tx2)
+
+        lock("//tmp/dir/t2", tx=tx21, mode="snapshot")
+        lock("//tmp/dir/t2", tx=tx22, mode="snapshot")
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx21)
+        abort_transaction(tx22)
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx2)
+        commit_transaction(tx1)
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx1 = start_transaction(timeout=100000)
+        lock("//tmp/dir", tx=tx1, mode="snapshot")
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx2 = start_transaction(timeout=100000, tx=tx1)
+        tx21 = start_transaction(timeout=100000, tx=tx2)
+        tx22 = start_transaction(timeout=100000, tx=tx2)
+
+        lock("//tmp/dir", tx=tx22, mode="snapshot")
+        lock("//tmp/dir/t1", tx=tx21, mode="snapshot")
+
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx21)
+        abort_transaction(tx22)
+
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx2)
+        commit_transaction(tx1)
+
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_dependent_transactions(self):
+        tx1 = start_transaction(timeout=100000)
+        tx11 = start_transaction(timeout=100000, tx=tx1)
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx111 = start_transaction(timeout=100000, tx=tx11)
+        tx112 = start_transaction(timeout=100000, tx=tx11)
+
+        tx2 = start_transaction(timeout=100000)
+        tx21 = start_transaction(timeout=100000, tx=tx2, prerequisite_transaction_ids=[tx111])
+        tx22 = start_transaction(timeout=100000, tx=tx2, prerequisite_transaction_ids=[tx112])
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx3 = start_transaction(timeout=100000, prequisite_transaction_ids=[tx21, tx22])
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx31 = start_transaction(timeout=100000, tx=tx3, prerequisite_transaction_ids=[tx2])
+        start_transaction(timeout=100000, tx=tx31, prerequisite_transaction_ids=[tx11])
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx4 = start_transaction(timeout=100000, prequisite_transaction_ids=[tx21, tx22, tx11, tx3])
+
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx4)
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx111)
+        check_sequoia_tables_reconstruction(self)
+
+        abort_transaction(tx112)
+        check_sequoia_tables_reconstruction(self)
+
+        commit_transaction(tx2)
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_transaction_attributes(self):
+        start_transaction(timeout=100000, attributes={"title": "tx1", "non_sequoia_name": "no value"})
+        start_transaction(timeout=100000, attributes={"title": "tx2", "operation_title": "title"})
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_links(self):
+        create("map_node", "//tmp/dir1")
+        create("map_node", "//tmp/dir1/dir2")
+
+        tx1 = start_transaction(timeout=100000)
+        create("table", "//tmp/dir1/dir2/t1", tx=tx1)
+        create("table", "//tmp/dir1/t2", tx=tx1)
+
+        check_sequoia_tables_reconstruction(self)
+
+        tx2 = start_transaction(timeout=100000, tx=tx1)
+        link("//tmp/dir1/dir2/t1", "//tmp/dir1/t1", tx=tx2)
+        link("//tmp/dir1/t2", "//tmp/dir1/dir2/t2", tx=tx1)
+
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_remove_return(self):
+        for remove_depth in range(3):
+            for return_depth in range(3):
+                table = f"//tmp/t{remove_depth}_{return_depth}"
+                tx = start_transaction(timeout=100000)
+                create("table", table, tx=tx)
+                for _ in range(remove_depth):
+                    tx = start_transaction(timeout=100000, tx=tx)
+                remove(table, tx=tx)
+                for _ in range(return_depth):
+                    tx = start_transaction(timeout=100000, tx=tx)
+                create("table", table, tx=tx)
+
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_replace_remove(self):
+        for replace_depth in range(3):
+            for remove_depth in range(3):
+                table = f"//tmp/t{replace_depth}_{remove_depth}"
+                tx = start_transaction(timeout=100000)
+                create("table", table, tx=tx)
+                for _ in range(replace_depth):
+                    tx = start_transaction(timeout=100000, tx=tx)
+                remove(table, tx=tx)
+                create("table", table, tx=tx)
+                for _ in range(remove_depth):
+                    tx = start_transaction(timeout=100000, tx=tx)
+                remove(table, tx=tx)
+
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    @pytest.mark.parametrize("start_initial_transaction", [True, False])
+    def test_recreate_and_remove(self, start_initial_transaction):
+        if start_initial_transaction:
+            t1 = start_transaction(timeout=100000)
+            create("table", "//tmp/t", tx=t1)
+            t2 = start_transaction(timeout=100000, tx=t1)
+        else:
+            create("table", "//tmp/t")
+            t2 = start_transaction(timeout=100000)
+
+        remove("//tmp/t", tx=t2)
+        t3 = start_transaction(timeout=100000, tx=t2)
+        t4 = start_transaction(timeout=100000, tx=t3)
+        create("table", "//tmp/t", tx=t4)
+        t5 = start_transaction(timeout=100000, tx=t4)
+        remove("//tmp/t", tx=t5)
+        check_sequoia_tables_reconstruction(self)
+        commit_transaction(t5)
+        check_sequoia_tables_reconstruction(self)
+        commit_transaction(t4)
+        check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_multiple_replacements(self):
+        transactions = []
+        tx = start_transaction(timeout=100000)
+        transactions.append(tx)
+
+        create("table", "//tmp/t", tx=tx)
+
+        for _ in range(5):
+            tx = start_transaction(timeout=100000, tx=tx)
+            transactions.append(tx)
+
+            remove("//tmp/t", tx=tx)
+            create("table", "//tmp/t", tx=tx)
+
+        check_sequoia_tables_reconstruction(self)
+        for tx in transactions[::-1]:
+            commit_transaction(tx)
+            check_sequoia_tables_reconstruction(self)
+
+    @authors("grphil")
+    def test_acl(self):
+        create_user("u")
+
+        create("table", "//tmp/t", attributes={
+            "inherit_acl": False,
+            "acl": [make_ace("allow", "u", "write")],
+        })
+        check_sequoia_tables_reconstruction(self)
+
+        set("//tmp/t/@inherit_acl", True)
+        check_sequoia_tables_reconstruction(self)
+
+        set("//tmp/t/@acl/end", make_ace("deny", "everyone", "read"))
+        check_sequoia_tables_reconstruction(self)
+
+        remove("//tmp/t")
+        check_sequoia_tables_reconstruction(self)
---
+++ yt/yt/tests/library/sequoia_reconstructor.py
@@ -0,0 +1,166 @@
+import os
+import shlex
+import shutil
+import subprocess
+
+from native_snapshot_exporter import SnapshotBuilderBase
+from yt_commands import get, wait
+from yt_sequoia_helpers import get_ground_driver, select_rows_from_ground
+from yt_helpers import master_exit_read_only_sync
+
+from yt.sequoia_tools import DESCRIPTORS
+import yt.yson as yson
+
+##################################################################
+
+PROC_TIMEOUT = 30
+
+NODE_ID_TO_PATH = "node_id_to_path"
+NODE_FORKS = "node_forks"
+NODE_SNAPSHOTS = "node_snapshots"
+CHILD_NODE = "child_node"
+CHILD_FORKS = "child_forks"
+TRANSACTIONS = "transactions"
+TRANSACTION_DESCENDANTS = "transaction_descendants"
+DEPENDENT_TRANSACTIONS = "dependent_transactions"
+TRANSACTION_REPLICAS = "transaction_replicas"
+PATH_TO_NODE_ID = "path_to_node_id"
+PATH_FORKS = "path_forks"
+ACLS = "acls"
+
+TABLE_NAMES = [
+    NODE_ID_TO_PATH,
+    NODE_FORKS,
+    NODE_SNAPSHOTS,
+    CHILD_NODE,
+    TRANSACTIONS,
+    CHILD_FORKS,
+    TRANSACTION_REPLICAS,
+    TRANSACTION_DESCENDANTS,
+    DEPENDENT_TRANSACTIONS,
+    PATH_TO_NODE_ID,
+    PATH_FORKS,
+    ACLS,
+]
+
+RETIRABLE_TABLES = [
+    ACLS,
+]
+
+##################################################################
+
+
+class SequoiaReconstructor(SnapshotBuilderBase):
+    def reconstructed_table_path(self, table_name: str) -> str:
+        return os.path.join(self.output_dir, f"{table_name}.yson")
+
+    def create_reconstructor_config(self, table_names):
+        self.output_dir = os.path.join(self.path_to_run, "reconstructed_sequoia")
+        if os.path.exists(self.output_dir):
+            shutil.rmtree(self.output_dir)
+        os.mkdir(self.output_dir)
+
+        config = {"single_cell": True}
+        for table_name in table_names:
+            config[f"{table_name}_output"] = {"file_name": self.reconstructed_table_path(table_name)}
+
+        return yson.dumps(yson.json_to_yson(config)).decode("utf-8")
+
+    # Masters enter read only during build_snapshot_and_reconstruct_sequoia.
+    # We should exit read only after sequoia is checked.
+    def build_snapshot_and_reconstruct_sequoia(self, table_names):
+        self.build_master_snapshot(set_read_only=True)
+
+        reconstructor_config = self.create_reconstructor_config(table_names)
+
+        return self._reconstruct_sequoia(
+            self.binary,
+            self.snapshot_path,
+            self.config_path,
+            reconstructor_config,
+            table_names)
+
+    def _reconstruct_sequoia(
+            self,
+            binary,
+            snapshot_path,
+            config_path,
+            reconstructor_config,
+            table_names):
+        command = [
+            binary,
+            "--reconstruct-sequoia", snapshot_path,
+            "--config", config_path,
+            "--sequoia-reconstructor-config", reconstructor_config,
+        ]
+
+        command = shlex.join(command)
+        subprocess.run(
+            command,
+            shell=True,
+            check=True,
+            timeout=PROC_TIMEOUT
+        )
+
+        tables = dict()
+
+        for table in table_names:
+            table_path = self.reconstructed_table_path(table)
+            with open(table_path, "rb") as f:
+                tables[table] = yson.load(f)
+
+        return tables
+
+
+def _check_all_items_in_list(list_to_check, expected_items):
+    expected = expected_items.copy()
+    for item in list_to_check:
+        found = False
+        for expected_item in expected:
+            if str(item) == str(expected_item):
+                expected.remove(expected_item)
+                found = True
+                break
+
+        if not found:
+            return False
+    return True
+
+
+def check_sequoia_tables_reconstruction(yt_env, table_names=TABLE_NAMES):
+    reconstructor = SequoiaReconstructor(
+        os.path.join(yt_env.path_to_run, "primary"),
+        yt_env.bin_path)
+    tables = reconstructor.build_snapshot_and_reconstruct_sequoia(table_names)
+
+    descriptors = DESCRIPTORS.as_dict()
+
+    def check_tables_are_same(table_name):
+        schema = get(f"{descriptors[table_name].get_default_path()}/@schema", driver=get_ground_driver())
+        ground_result = select_rows_from_ground(f"* from [{descriptors[table_name].get_default_path()}]")
+        ground_table = []
+
+        for row in ground_result:
+            table_row = []
+            for column in schema:
+                if "expression" in column:
+                    continue
+                table_row.append(row[column["name"]])
+            ground_table.append(table_row)
+
+        if table_name not in tables or tables[table_name] is None:
+            reconstructed_table = []
+        else:
+            reconstructed_table = [list(i) for i in tables[table_name]]
+
+        return _check_all_items_in_list(ground_table, reconstructed_table) and _check_all_items_in_list(reconstructed_table, ground_table)
+
+    non_retriable_tables = [table_name for table_name in table_names if table_name not in RETIRABLE_TABLES]
+    for table_name in non_retriable_tables:
+        assert check_tables_are_same(table_name)
+
+    master_exit_read_only_sync()
+    retriable_tables = [table_name for table_name in table_names if table_name in RETIRABLE_TABLES]
+
+    for table_name in retriable_tables:
+        wait(lambda: check_tables_are_same(table_name))
