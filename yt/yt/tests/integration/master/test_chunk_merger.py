730b494bb5f3cb08beaa3f5419973a8a0417551c
cherepashka <cherepashka@ytsaurus.tech>
2025-11-24 13:29:04 +0300

    YT-25733: Hope that fixed test_chunk_tail commit_hash:490fac10b76928a4bc971aac0986ea8052ad802f

--- yt/yt/tests/integration/master/test_chunk_merger.py
+++ yt/yt/tests/integration/master/test_chunk_merger.py
@@ -607,7 +607,7 @@ class TestChunkMerger(YTEnvSetup):
         wait(lambda: get("//tmp/t/@chunk_count") == 5)
 
         traversal_info2 = get("//tmp/t/@chunk_merger_traversal_info")
-        assert traversal_info2["chunk_count"] > traversal_info1["chunk_count"]
+        wait(lambda: traversal_info2["chunk_count"] > traversal_info1["chunk_count"])
         assert traversal_info2["config_version"] == traversal_info1["config_version"]
 
         set("//sys/@config/chunk_manager/chunk_merger/max_chunk_count", 10)
