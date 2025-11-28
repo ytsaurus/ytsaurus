05e4090e66d8593e3c1bacce5d316c4277e7133d
Daria Zhukova <cherepashka@gmail.com>
2025-11-29 02:32:27 +0300

    Medium descriptor refactoring

--- yt/yt/server/node/data_node/data_node_service.cpp
+++ yt/yt/server/node/data_node/data_node_service.cpp
@@ -896,7 +896,7 @@ private:
             subresponse->set_disk_queue_size(diskThrottling.QueueSize);
 
             if (chunk) {
-                subresponse->set_medium_index(chunk->GetLocation()->GetMediumDescriptor().Index);
+                subresponse->set_medium_index(chunk->GetLocation()->GetMediumDescriptor()->GetIndex());
             }
 
             YT_LOG_DEBUG_UNLESS(diskThrottling.Error.IsOK(), diskThrottling.Error);
@@ -972,7 +972,7 @@ private:
         response->set_disk_queue_size(diskThrottling.QueueSize);
 
         if (chunk) {
-            response->set_medium_index(chunk->GetLocation()->GetMediumDescriptor().Index);
+            response->set_medium_index(chunk->GetLocation()->GetMediumDescriptor()->GetIndex());
         }
 
         YT_LOG_DEBUG_UNLESS(diskThrottling.Error.IsOK(), diskThrottling.Error);
