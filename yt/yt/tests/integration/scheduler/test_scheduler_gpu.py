4cf900ffb987abb21e342fd0ae57cc0c86a6180e
severovv <severovv@ytsaurus.tech>
2025-12-01 21:57:21 +0300

    YT-26780: fix test_tag_filters in gpu planner commit_hash:379d2cc8c2fb1996a5a04836eeadf7ad64101c63

--- yt/yt/tests/integration/scheduler/test_scheduler_gpu.py
+++ yt/yt/tests/integration/scheduler/test_scheduler_gpu.py
@@ -107,6 +107,9 @@ class TestDryRunGpuSchedulingPolicy(YTEnvSetup):
     def _get_operation_from_orchid(self, operation, tree="gpu"):
         return get(scheduler_new_orchid_pool_tree_path(tree) + f"/gpu_assignment_plan/operations/{operation.id}")
 
+    def _get_node_from_orchid(self, node, tree="gpu"):
+        return get(scheduler_new_orchid_pool_tree_path(tree) + f"/gpu_assignment_plan/nodes/{node}")
+
     def _get_operation_assignments_from_orchid(self, operation, tree="gpu"):
         return self._get_operation_from_orchid(operation, tree=tree)["assignments"]
 
@@ -654,13 +657,6 @@ class TestDryRunGpuSchedulingPolicy(YTEnvSetup):
     def test_tag_filters(self):
         nodes = list(ls("//sys/cluster_nodes"))
 
-        non_schedulable_op = run_sleeping_vanilla(
-            task_patch={"gpu_limit": 8, "enable_gpu_layers": False},
-            job_count=1,
-            spec={"scheduling_tag_filter": f"{nodes[0]}&{nodes[1]}"},
-        )
-        time.sleep(1)
-
         first_node_only = run_sleeping_vanilla(
             task_patch={"gpu_limit": 8, "enable_gpu_layers": False},
             job_count=1,
@@ -668,23 +664,34 @@ class TestDryRunGpuSchedulingPolicy(YTEnvSetup):
         )
         wait(lambda: len(first_node_only.get_running_jobs()) == 1)
 
-        any_node = run_sleeping_vanilla(
+        waits_for_first = run_sleeping_vanilla(
             task_patch={"gpu_limit": 8, "enable_gpu_layers": False},
+            job_count=1,
+            spec={"scheduling_tag_filter": nodes[0]},
+        )
+
+        self._wait_for_operations_in_orchid(operation_count=2)
+        self._wait_for_assignments_in_orchid(first_node_only, assignment_count=1)
+
+        node1 = self._get_node_from_orchid(nodes[0])
+        assert len(node1["assignments"]) == 1
+        assert node1["assignments"][0]["operation_id"] == first_node_only.id
+
+        time.sleep(5)
+        assert len(self._get_operation_assignments_from_orchid(waits_for_first)) == 0
+
+        any_node = run_sleeping_vanilla(
+            task_patch={"gpu_limit": 7, "enable_gpu_layers": False},
             job_count=1,
         )
         wait(lambda: len(any_node.get_running_jobs()) == 1)
 
         self._wait_for_operations_in_orchid(operation_count=3)
+        self._wait_for_assignments_in_orchid(any_node, assignment_count=1)
 
-        node1 = get(scheduler_new_orchid_pool_tree_path("gpu") + f"/gpu_assignment_plan/nodes/{nodes[0]}")
-        assert len(node1["assignments"]) == 1
-        assert node1["assignments"][0]["operation_id"] == first_node_only.id
-
-        node2 = get(scheduler_new_orchid_pool_tree_path("gpu") + f"/gpu_assignment_plan/nodes/{nodes[1]}")
+        node2 = self._get_node_from_orchid(nodes[1])
         assert len(node2["assignments"]) == 1
         assert node2["assignments"][0]["operation_id"] == any_node.id
-
-        assert len(non_schedulable_op.get_running_jobs()) == 0
 
 
 ##################################################################
