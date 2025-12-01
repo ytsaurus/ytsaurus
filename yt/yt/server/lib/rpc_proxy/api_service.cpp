51ce80d6136d61f0737dd1e853e9df23255436c0
osidorkin <osidorkin@ytsaurus.tech>
2025-12-01 15:56:13 +0300

    Trivial: less context size for lambdas commit_hash:d06c58cffcb02f0563249fb4e98e2717fa3758b7

--- yt/yt/server/lib/rpc_proxy/api_service.cpp
+++ yt/yt/server/lib/rpc_proxy/api_service.cpp
@@ -104,7 +104,6 @@
 #include <library/cpp/yt/misc/cast.h>
 
 #include <algorithm>
-#include <ranges>
 
 namespace NYT::NRpcProxy {
 
@@ -1261,7 +1260,7 @@ private:
             using TResult = typename decltype(future)::TValueType;
 
             future.Subscribe(
-                BIND([=, this, this_ = MakeStrong(this)] (const TErrorOr<TResult>& resultOrError) {
+                BIND([this, this_ = MakeStrong(this)] (const TErrorOr<TResult>& resultOrError) {
                     if (!resultOrError.IsOK()) {
                         HandleError(resultOrError);
                         return;
@@ -1368,16 +1367,20 @@ private:
             count,
             clockClusterTag);
 
-        const auto& connection = client->GetNativeConnection();
+        auto connection = client->GetNativeConnection();
         if (clockClusterTag == InvalidCellTag) {
             connection->GetClockManager()->ValidateDefaultClock("Unable to generate timestamps");
         }
 
-        const auto& timestampProvider = connection->GetTimestampProvider();
-
         ExecuteCall(
             context,
-            [=, Logger = Logger] {
+            [
+                connection = std::move(connection),
+                clockClusterTag,
+                count,
+                Logger = Logger
+            ] {
+                const auto& timestampProvider = connection->GetTimestampProvider();
                 return timestampProvider->GenerateTimestamps(count, clockClusterTag).ApplyUnique(
                     BIND([connection, clockClusterTag, count, Logger] (TErrorOr<TTimestamp>&& providerResult) {
                         if (providerResult.IsOK() ||
