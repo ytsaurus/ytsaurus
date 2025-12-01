4d94e1f39ef8888320f5c9cd4825128b675c8f04
mikari <mikari@yandex-team.com>
2025-12-01 22:05:28 +0300

    Added GetStructType to yson struct meta commit_hash:e115d82c31200be8f847a51df7f8b764f716364f

--- yt/yt/flow/lib/serializer/validate.cpp
+++ yt/yt/flow/lib/serializer/validate.cpp
@@ -8,9 +8,9 @@ namespace NYT::NFlow::NYsonSerializer {
 
 void ValidateYsonStruct(const NYTree::TYsonStructPtr& ysonStruct)
 {
-    ysonStruct->GetMeta()->Traverse([](const auto& context) {
+    ysonStruct->GetMeta()->Traverse([ysonStruct](const auto& context) {
         if (context.Parameter->IsRequired()) {
-            THROW_ERROR_EXCEPTION("Parameter %Qv does not have default value", context.Path);
+            THROW_ERROR_EXCEPTION("Parameter [%v]:%v does not have default value", TypeName(ysonStruct->GetMeta()->GetStructType()), context.Path);
         }
     });
 }
