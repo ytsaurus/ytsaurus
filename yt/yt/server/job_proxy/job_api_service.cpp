780a66c5fdf3568af62df035d458630ba3b9be2d
dann239 <dann239@ytsaurus.tech>
2025-12-02 15:18:00 +0300

    YT-26594 Add last_progress_save field to job_proxy orchid commit_hash:57f31828ce5d25eadb0652c593295b87ea137cba

--- yt/yt/server/job_proxy/job_api_service.cpp
+++ yt/yt/server/job_proxy/job_api_service.cpp
@@ -1,5 +1,7 @@
 #include "job_api_service.h"
 
+#include "job_proxy.h"
+
 #include <yt/yt/server/lib/job_proxy/config.h>
 #include <yt/yt/server/lib/job_proxy/job_api_service_proxy.h>
 
@@ -19,34 +21,52 @@ class TJobApiService
 public:
     TJobApiService(
         TJobApiServiceConfigPtr config,
-        IInvokerPtr controlInvoker)
+        IInvokerPtr controlInvoker,
+        TWeakPtr<TJobProxy> jobProxy)
         : NRpc::TServiceBase(
             std::move(controlInvoker),
             TJobApiServiceProxy::GetDescriptor(),
             JobApiLogger())
         , Config_(std::move(config))
+        , JobProxy_(jobProxy)
     {
         RegisterMethod(RPC_SERVICE_METHOD_DESC(ProgressSaved));
     }
 
 private:
     TJobApiServiceConfigPtr Config_;
+    TWeakPtr<TJobProxy> JobProxy_;
 
     DECLARE_RPC_SERVICE_METHOD(NJobProxy::JobApi::NProto, ProgressSaved)
     {
-        YT_LOG_INFO("Received a snapshot notification");
+        YT_LOG_INFO("Received a progress save notification");
+
+        TInstant progressSaveTime = TInstant::Now();
+        GetJobProxy()->OnProgressSaved(progressSaveTime);
+
         context->SetRequestInfo();
         context->Reply();
+    }
+
+    TJobProxyPtr GetJobProxy()
+    {
+        auto jobProxy = JobProxy_.Lock();
+        if (!jobProxy) {
+            THROW_ERROR_EXCEPTION("Job proxy is dead");
+        }
+        return jobProxy;
     }
 };
 
 NRpc::IServicePtr CreateJobApiService(
     NJobProxy::TJobApiServiceConfigPtr config,
-    IInvokerPtr controlInvoker)
+    IInvokerPtr controlInvoker,
+    TWeakPtr<TJobProxy> jobProxy)
 {
     return New<TJobApiService>(
         std::move(config),
-        std::move(controlInvoker));
+        std::move(controlInvoker),
+        std::move(jobProxy));
 }
 
 ////////////////////////////////////////////////////////////////////////////////
