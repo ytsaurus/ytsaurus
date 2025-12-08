951829c4ce344ef0f897d74ad4de6c428773e0e1
theevilbird <theevilbird@ytsaurus.tech>
2025-12-08 18:55:32 +0300

    YT-26335: Fix GetMaxReplicasPerFailureDomain commit_hash:2fa173335b89e85120df8d908be4fa10c91aba6a

--- yt/yt/server/master/chunk_server/config.cpp
+++ yt/yt/server/master/chunk_server/config.cpp
@@ -809,6 +809,10 @@ void TDynamicChunkManagerConfig::Register(TRegistrar registrar)
         .Default(100)
         .DontSerializeDefault();
 
+    registrar.Parameter("enable_recalculation_max_replicas_per_failure_domain", &TThis::EnableRecalculationMaxReplicasPerFailureDomain)
+        .Default(false)
+        .DontSerializeDefault();
+
     registrar.Postprocessor([] (TThis* config) {
         auto& jobTypeToThrottler = config->JobTypeToThrottler;
         for (auto jobType : TEnumTraits<EJobType>::GetDomainValues()) {
