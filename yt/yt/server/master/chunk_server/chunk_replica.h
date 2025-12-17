84cfedd5f9c1da2eb2342ae0fa54db7f5563466b
shakurov <shakurov@ytsaurus.tech>
2025-12-17 13:47:38 +0300

    Explicitly default TAugmentedPtr copy ctor & assignment; change casting ctor signatures to match commit_hash:836e383fb96761399b08236ff6caff158556f895

--- yt/yt/server/master/chunk_server/chunk_replica.h
+++ yt/yt/server/master/chunk_server/chunk_replica.h
@@ -41,10 +41,12 @@ public:
     TAugmentedPtr(T* ptr, int firstIndex, int secondIndex, EChunkReplicaState = EChunkReplicaState::Generic)
         requires (WithReplicaState && IndexCount == 2);
 
-    explicit TAugmentedPtr(TAugmentedPtr<T, true, IndexCount, TAugmentationAccessor> other)
+    TAugmentedPtr(const TAugmentedPtr& other) = default;
+
+    explicit TAugmentedPtr(const TAugmentedPtr<T, true, IndexCount, TAugmentationAccessor>& other)
         requires (!WithReplicaState);
 
-    explicit TAugmentedPtr(TAugmentedPtr<T, false, IndexCount, TAugmentationAccessor> other)
+    explicit TAugmentedPtr(const TAugmentedPtr<T, false, IndexCount, TAugmentationAccessor>& other)
         requires WithReplicaState;
 
     TAugmentedPtr(
@@ -52,6 +54,7 @@ public:
         EChunkReplicaState state)
         requires WithReplicaState;
 
+    TAugmentedPtr& operator=(const TAugmentedPtr& other) = default;
 
     T* GetPtr() const;
 
