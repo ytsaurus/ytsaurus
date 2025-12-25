0443e11d771f0515ec644759fa43b8318dd17e3d
cherepashka <cherepashka@ytsaurus.tech>
2025-12-25 23:43:14 +0300

    [25.3] YT-26920: Fix reconfiguration and expiration for TAsyncExpiringCache
    * Changelog entry
      Type: fix
      Component: core
    
    Fixed enabling refresh and expiration without batch\_update on reconfiguration and fix expiration with disabled refresh
    commit_hash:52df7bbc4bd48f6e74cb40105bca5184bf919585

--- yt/yt/core/misc/async_expiring_cache-inl.h
+++ yt/yt/core/misc/async_expiring_cache-inl.h
@@ -335,7 +335,8 @@ void TAsyncExpiringCache<TKey, TValue>::ForceRefresh(const TKey& key, const T& v
     if (auto it = Map_.find(key); it != Map_.end() && it->second->Promise.IsSet()) {
         auto valueOrError = it->second->Promise.Get();
         if (valueOrError.IsOK() && valueOrError.Value() == value) {
-            NConcurrency::TDelayedExecutor::CancelAndClear(it->second->ProbationCookie);
+            NConcurrency::TDelayedExecutor::CancelAndClear(it->second->RefreshCookie);
+            NConcurrency::TDelayedExecutor::CancelAndClear(it->second->ExpirationCookie);
 
             auto accessDeadline = now + NProfiling::DurationToCpuDuration(config->ExpireAfterAccessTime);
             auto newEntry = New<TEntry>(accessDeadline);
@@ -430,9 +431,19 @@ void TAsyncExpiringCache<TKey, TValue>::ScheduleEntryUpdate(
     const TKey& key,
     const TAsyncExpiringCacheConfigPtr& config)
 {
+    ScheduleEntryRefresh(entry, key, config);
+    ScheduleEntryExpiration(entry, key, config);
+}
+
+template <class TKey, class TValue>
+void TAsyncExpiringCache<TKey, TValue>::ScheduleEntryRefresh(
+    const TEntryPtr& entry,
+    const TKey& key,
+    const TAsyncExpiringCacheConfigPtr& config)
+{
     if (config->RefreshTime && *config->RefreshTime) {
-        NConcurrency::TDelayedExecutor::CancelAndClear(entry->ProbationCookie);
-        entry->ProbationCookie = NConcurrency::TDelayedExecutor::Submit(
+        NConcurrency::TDelayedExecutor::CancelAndClear(entry->RefreshCookie);
+        entry->RefreshCookie = NConcurrency::TDelayedExecutor::Submit(
             BIND_NO_PROPAGATE(
                 &TAsyncExpiringCache::InvokeGet,
                 MakeWeak(this),
@@ -440,20 +451,33 @@ void TAsyncExpiringCache<TKey, TValue>::ScheduleEntryUpdate(
                 key),
             *config->RefreshTime);
     }
+}
 
+template <class TKey, class TValue>
+void TAsyncExpiringCache<TKey, TValue>::ScheduleEntryExpiration(
+    const TEntryPtr& entry,
+    const TKey& key,
+    const TAsyncExpiringCacheConfigPtr& config)
+{
     if (config->ExpirationPeriod && *config->ExpirationPeriod) {
-        NConcurrency::TDelayedExecutor::MakeDelayed(
-            *config->ExpirationPeriod,
-            NRpc::TDispatcher::Get()->GetHeavyInvoker())
-            .Subscribe(BIND_NO_PROPAGATE([key, weakEntry = MakeWeak(entry), this, weakThis_ = MakeWeak(this)] (const TError& /*error*/) {
+        NConcurrency::TDelayedExecutor::CancelAndClear(entry->ExpirationCookie);
+        entry->ExpirationCookie = NConcurrency::TDelayedExecutor::Submit(
+            BIND_NO_PROPAGATE([key, weakEntry = MakeWeak(entry), config, this, weakThis_ = MakeWeak(this)] {
                 auto this_ = weakThis_.Lock();
                 if (!this_) {
                     return;
                 }
-                if (auto entry = weakEntry.Lock()) {
-                    TryEraseExpired(entry, key, EEraseReason::Expiration);
+                auto entry = weakEntry.Lock();
+                if (!entry) {
+                    return;
                 }
-            }));
+                if (!TryEraseExpired(entry, key, EEraseReason::Expiration)) {
+                    // Somebody accessed or updated entry.
+                    auto guard = NThreading::WriterGuard(SpinLock_);
+                    ScheduleEntryExpiration(entry, key, config);
+                }
+            }),
+            CpuInstantToInstant(std::min(entry->AccessDeadline.load(), entry->UpdateDeadline.load())));
     }
 }
 
@@ -466,7 +490,8 @@ void TAsyncExpiringCache<TKey, TValue>::Clear()
     if (!config->BatchUpdate) {
         for (const auto& [key, entry] : Map_) {
             if (entry->Promise.IsSet()) {
-                NConcurrency::TDelayedExecutor::CancelAndClear(entry->ProbationCookie);
+                NConcurrency::TDelayedExecutor::CancelAndClear(entry->RefreshCookie);
+                NConcurrency::TDelayedExecutor::CancelAndClear(entry->ExpirationCookie);
             }
         }
     }
@@ -619,7 +644,8 @@ bool TAsyncExpiringCache<TKey, TValue>::TryEraseExpired(const TEntryPtr& entry, 
 template <class TKey, class TValue>
 void TAsyncExpiringCache<TKey, TValue>::Erase(THashMap<TKey, TEntryPtr>::iterator it)
 {
-    NConcurrency::TDelayedExecutor::CancelAndClear(it->second->ProbationCookie);
+    NConcurrency::TDelayedExecutor::CancelAndClear(it->second->RefreshCookie);
+    NConcurrency::TDelayedExecutor::CancelAndClear(it->second->ExpirationCookie);
     OnRemoved(it->first);
     Map_.erase(it);
 }
@@ -737,6 +763,15 @@ void TAsyncExpiringCache<TKey, TValue>::RefreshAllItems()
 }
 
 template <class TKey, class TValue>
+void TAsyncExpiringCache<TKey, TValue>::ScheduleAllEntriesUpdate(const TAsyncExpiringCacheConfigPtr& config)
+{
+    auto guard = NThreading::WriterGuard(SpinLock_);
+    for (const auto& [key, entry] : Map_) {
+        ScheduleEntryUpdate(entry, key, config);
+    }
+}
+
+template <class TKey, class TValue>
 void TAsyncExpiringCache<TKey, TValue>::Reconfigure(TAsyncExpiringCacheConfigPtr newConfig)
 {
     auto oldConfig = Config_.Acquire();
@@ -760,8 +795,20 @@ void TAsyncExpiringCache<TKey, TValue>::Reconfigure(TAsyncExpiringCacheConfigPtr
         if (!oldConfig->ExpirationPeriod && newConfig->ExpirationPeriod && *newConfig->ExpirationPeriod) {
             ExpirationExecutor_->Start();
         }
+    } else {
+        auto needToUpdateEntries = (!oldConfig->RefreshTime && newConfig->RefreshTime && *newConfig->RefreshTime) ||
+            (!oldConfig->ExpirationPeriod && newConfig->ExpirationPeriod && *newConfig->ExpirationPeriod);
+        if (needToUpdateEntries) {
+            ScheduleAllEntriesUpdate(newConfig);
+        }
     }
     Config_.Store(std::move(newConfig));
+}
+
+template <class TKey, class TValue>
+int TAsyncExpiringCache<TKey, TValue>::GetSize() const
+{
+    return Map_.size();
 }
 
 ////////////////////////////////////////////////////////////////////////////////
