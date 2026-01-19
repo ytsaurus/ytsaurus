dab70efddff2edadf98fa26a569b07d0e9a14946
lukyan <lukyan@ytsaurus.tech>
2026-01-19 23:59:24 +0300

    YT-25378: Optimize nested row merger commit_hash:d71238a1561a50278efbdf178e8ca92e04ba6f65

--- yt/yt/library/row_merger/row_merger.h
+++ yt/yt/library/row_merger/row_merger.h
@@ -55,7 +55,7 @@ private:
     std::vector<int> AggregateColumnIds_;
     std::vector<std::vector<NTableClient::TVersionedValue>> AggregateValues_;
 
-    TNestedTableMerger NestedMerger_;
+    TNestedTableMerger NestedMerger_{true};
     std::vector<TMutableRange<NTableClient::TVersionedValue>> NestedKeyColumns_;
     std::vector<TMutableRange<NTableClient::TVersionedValue>> NestedValueColumns_;
 
