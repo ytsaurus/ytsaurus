876acd20ff0ef5df5a191b83e8f5e342025ef74c
dann239 <dann239@ytsaurus.tech>
2026-01-19 14:39:27 +0300

    Clone singleton configs in MergeAllSingletonConfigsFrom commit_hash:66b51e8c591b69d38fc80713d54deeb9de9175c4

--- yt/yt/server/node/exec_node/bootstrap.cpp
+++ yt/yt/server/node/exec_node/bootstrap.cpp
@@ -482,9 +482,8 @@ private:
 
         newJobProxyConfigTemplate->MergeAllSingletonConfigsFrom(*TSingletonManager::GetConfig());
         {
-            auto config = CloneYsonStruct(newJobProxyConfigTemplate->GetSingletonConfig<NNet::TAddressResolverConfig>());
+            auto config = newJobProxyConfigTemplate->GetSingletonConfig<NNet::TAddressResolverConfig>();
             config->LocalHostNameOverride = NNet::GetLocalHostName();
-            newJobProxyConfigTemplate->SetSingletonConfig<NNet::TAddressResolverConfig>(std::move(config));
         }
 
         newJobProxyConfigTemplate->SetSingletonConfig(GetConfig()->ExecNode->JobProxy->JobProxyLogging->LogManagerTemplate);
