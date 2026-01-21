c3be1721561047c0ba9fbd86507eb56d13c16fd0
imakunin <imakunin@yandex-team.com>
2026-01-21 13:19:42 +0300

    set reuse socket addr
    * Changelog entry
    Type: fix
    Component: odin
    
    Set SO_REUSEADDR for Odin-server socket
    commit_hash:612e144b11d4c7b3f55eb6970645a8f74b31a4f5

--- yt/odin/bin/yt_odin/__main__.py
+++ yt/odin/bin/yt_odin/__main__.py
@@ -77,6 +77,7 @@ def acquire_yt_lock(proxy, token, token_env_variable, locks_dir, timeout, queue,
 
 def run_logging_server(port):
     socket_receiver = socketserver.ThreadingTCPServer(("localhost", port), LogRecordStreamHandler)
+    socket_receiver.allow_reuse_address = True
     socket_receiver.serve_forever()
 
 
