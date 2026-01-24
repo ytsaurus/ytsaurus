c4f55cf6a977748be949e589ffbb1655a9f1d17b
apachee <apachee@ytsaurus.tech>
2026-01-25 01:54:50 +0300

    YT-27164: Use native connection options for NApi::CreateConnection commit_hash:d162c97c5cb4eb2bb1f2487ed793826f2d08d3c8

--- yt/yt/ytlib/api/connection.h
+++ yt/yt/ytlib/api/connection.h
@@ -2,6 +2,8 @@
 
 #include "public.h"
 
+#include <yt/yt/ytlib/api/native/connection.h>
+
 #include <yt/yt/library/auth_server/public.h>
 
 #include <yt/yt/client/api/connection.h>
@@ -12,10 +14,12 @@ namespace NYT::NApi {
 
 ////////////////////////////////////////////////////////////////////////////////
 
+//! Creates a native or RPC connection depending on the config.
+//!
+//! \note Native part of connection options is ignored for an RPC connection.
 IConnectionPtr CreateConnection(
     NYTree::INodePtr config,
-    TConnectionOptions options = {},
-    NAuth::IDynamicTvmServicePtr tvmService = nullptr);
+    NNative::TConnectionOptions options = {});
 
 ////////////////////////////////////////////////////////////////////////////////
 
