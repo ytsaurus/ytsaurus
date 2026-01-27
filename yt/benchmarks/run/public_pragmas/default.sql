50cdf891c57fb634d2424992d88c8a72a18d6447
pavook <pavook@ytsaurus.tech>
2026-01-27 20:09:56 +0300

    Set yt.MaxJobCount="200" on yqlbench commit_hash:05672ef40bcab04559134153fa4316403b4a41fb

--- yt/benchmarks/run/public_pragmas/default.sql
+++ yt/benchmarks/run/public_pragmas/default.sql
@@ -47,3 +47,4 @@ pragma FilterPushdownOverJoinOptionalSide;
 pragma yt.TableContentMinAvgChunkSize="0";
 pragma yt.TemporaryCompressionCodec="zstd_1";
 pragma yt.IntermediateReplicationFactor="1";
+pragma yt.MaxJobCount="200";
