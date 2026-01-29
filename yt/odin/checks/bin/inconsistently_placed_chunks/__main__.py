cf4b3ca5f8db5a15fa3b41b2067c523712776b06
grphil <grphil@ytsaurus.tech>
2026-01-29 15:25:17 +0300

    YT-25876: Add default values for changed options for virtual chunk map checks in odin commit_hash:bb4927c289edcd2900c369e075f8b33c8ff80bf8

--- yt/odin/checks/bin/inconsistently_placed_chunks/__main__.py
+++ yt/odin/checks/bin/inconsistently_placed_chunks/__main__.py
@@ -3,17 +3,23 @@ from yt_odin_checks.lib.virtual_chunk_map_helpers import check_virtual_map_age
 
 from datetime import timedelta
 
+DEFAULT_MAX_AGE = timedelta(hours=4)
+DEFAULT_CHUNK_COUNT_THRESHOLD = 1000000
+
 
 def run_check(yt_client, logger, options, states):
     message = check_virtual_map_age(yt_client, logger, options, "inconsistently_placed")
     if message is None:
         return states.UNKNOWN_STATE
 
+    max_age = options.get("max_age_minutes", DEFAULT_MAX_AGE)
+    chunk_count_threshold = options.get("chunk_count_threshold", DEFAULT_CHUNK_COUNT_THRESHOLD)
+
     if message["age"] is None or message["count"] is None:
         return states.FULLY_AVAILABLE_STATE
-    if message["age"] >= timedelta(minutes=options["max_age_minutes"]):
+    if message["age"] >= max_age:
         return states.UNAVAILABLE_STATE, message
-    elif message["count"] >= options["chunk_count_threshold"]:
+    elif message["count"] >= chunk_count_threshold:
         return states.PARTIALLY_AVAILABLE_STATE, message
     else:
         return states.FULLY_AVAILABLE_STATE
--- yt/odin/checks/lib/virtual_chunk_map_helpers/__init__.py
+++ yt/odin/checks/lib/virtual_chunk_map_helpers/__init__.py
@@ -1,6 +1,9 @@
 from datetime import datetime
 import yt.common
 
+DEFAULT_MAX_SAMPLE_SIZE = 10
+DEFAULT_CHUNK_COUNT_THRESHOLD = 1000000
+
 
 def get_chunk_sample(yt_client, options, check_name, logger):
     yt_client.config["proxy"]["request_timeout"] = 57000
@@ -9,13 +12,15 @@ def get_chunk_sample(yt_client, options, check_name, logger):
     count_path = f"//sys/@{check_name}_chunk_count"
     full_list_path = f"//sys/{check_name}_chunks"
 
+    max_sample_size = options.get("max_size", DEFAULT_MAX_SAMPLE_SIZE)
+
     # COMPAT(grphil): some checks have no virtual maps, some checks have no maps at all.
     if yt_client.exists(sample_path):
-        chunk_ids = yt_client.list(sample_path, max_size=options["max_size"])
+        chunk_ids = yt_client.list(sample_path, max_size=max_sample_size)
         chunk_count = yt_client.get(count_path)
     elif yt_client.exists(full_list_path):
         logger.warning(f"Using full chunk list for {check_name}, may have slow response time")
-        chunk_ids = yt_client.list(full_list_path, max_size=options["max_size"])
+        chunk_ids = yt_client.list(full_list_path, max_size=max_sample_size)
         chunk_count = yt_client.get(f"{full_list_path}/@count")
     else:
         logger.warning(f"Can not load chunk list for {check_name}")
@@ -31,7 +36,7 @@ def check_virtual_map_size(yt_client, logger, options, states, check_name, count
 
     logger.info(f"{check_name} chunk number is %s", chunk_count)
     if len(chunk_ids) > count_threshold:
-        logger.info(f"Sample of {check_name} chunks: %s", " ".join(chunk_ids[:options["max_size"]]))
+        logger.info(f"Sample of {check_name} chunks: %s", " ".join(chunk_ids))
 
         batch_client = yt_client.create_batch_client()
         batch_responses = {}
@@ -59,10 +64,11 @@ def check_virtual_map_age(yt_client, logger, options, check_name):
 
     logger.info(f"{check_name} chunk count is {chunk_count}")
     if chunk_count > 0:
-        logger.info(f"Sample of {check_name} chunks: {" ".join(chunk_ids[:options["max_size"]])}")
+        logger.info(f"Sample of {check_name} chunks: {" ".join(chunk_ids)}")
 
     flag_path = f"//sys/admin/odin/{check_name}_chunks_present_since_flag"
-    count_threshold = options["chunk_count_threshold"]
+
+    count_threshold = options.get("chunk_count_threshold", DEFAULT_CHUNK_COUNT_THRESHOLD)
 
     if chunk_count >= count_threshold:
         if not yt_client.exists(flag_path):
