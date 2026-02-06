f7d8116a673cef1d6c2955eab157e2f593a7cc1e
yuryalekseev <yuryalekseev@ytsaurus.tech>
2026-02-06 12:53:05 +0300

    YT-26582: Address review comments. commit_hash:7f92d3f4eacbcc581dc83dec407c826ff8870125

--- yt/yt/server/node/exec_node/job_workspace_builder.cpp
+++ yt/yt/server/node/exec_node/job_workspace_builder.cpp
@@ -701,19 +701,25 @@ private:
         auto slotPath = slot->GetSlotPath();
         if (Context_.RootVolume && !Context_.UserSandboxOptions.EnableRootVolumeDiskQuota) {
             return slot->RbindRootVolume(Context_.RootVolume, slotPath)
-                .Apply(BIND([slot, this, this_ = MakeStrong(this)] (const TErrorOr<IVolumePtr>& volumeOrError) {
-                    Context_.RootVolume.Reset();
+                .Apply(BIND(
+                    [
+                        slot,
+                        this,
+                        this_ = MakeStrong(this)
+                    ] (const TErrorOr<IVolumePtr>& volumeOrError) {
+                        Context_.RootVolume.Reset();
 
-                    if (!volumeOrError.IsOK()) {
-                        YT_LOG_WARNING(volumeOrError, "Failed to prepare root volume");
+                        if (!volumeOrError.IsOK()) {
+                            YT_LOG_WARNING(volumeOrError, "Failed to prepare root volume");
 
-                        THROW_ERROR_EXCEPTION(NExecNode::EErrorCode::RootVolumePreparationFailed, "Failed to prepare root volume")
-                            << volumeOrError;
-                    }
+                            THROW_ERROR_EXCEPTION(NExecNode::EErrorCode::RootVolumePreparationFailed, "Failed to prepare root volume")
+                                << volumeOrError;
+                        }
 
-                    ResultHolder_.RootVolume = std::move(volumeOrError.Value());
-            }).AsyncVia(Invoker_))
-            .ToImmediatelyCancelable();
+                        ResultHolder_.RootVolume = std::move(volumeOrError.Value());
+                    })
+                    .AsyncVia(Invoker_))
+                .ToImmediatelyCancelable();
         } else {
             ResultHolder_.RootVolume = std::move(Context_.RootVolume);
             YT_LOG_DEBUG("Root volume binding is not needed");
