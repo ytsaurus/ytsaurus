07e9a9d8b53620c2f0f44a8989347cfb60f79245
ermolovd <ermolovd@ytsaurus.tech>
2026-02-09 17:49:22 +0300

    YT-26760: operation_link_pattern is taken from dynamic configuration
    * Changelog entry
    Type: feature
    Component: cpp-sdk
    
    C++ SDK supports operation_link_pattern dynamic configuration
    commit_hash:a937500f7ce866436ee8732c923fb4b362e199a1

--- yt/cpp/mapreduce/interface/patchable_field.h
+++ yt/cpp/mapreduce/interface/patchable_field.h
@@ -1,8 +1,18 @@
 #pragma once
 
-#include "client.h"
+#include "fwd.h"
+
+#include <library/cpp/yson/node/node.h>
 
 namespace NYT {
+
+////////////////////////////////////////////////////////////////////////////////
+
+namespace NDetail {
+
+const TNode::TMapType& GetDynamicConfiguration(const IClientPtr& client, const TString& configProfile);
+
+} // namespace NDetail
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -15,19 +25,22 @@ public:
     static constexpr char ConfigProfile[] = "default";
 
     TPatchableField(TString name, T defaultValue)
-        : Name_(move(name))
+        : Name_(std::move(name))
         , Value_(std::move(defaultValue))
     { }
 
     const T& Get(const IClientPtr& client)
     {
         if (!Patched_) {
-            const auto& clusterConfig = client->GetDynamicConfiguration(ConfigProfile);
+            const auto& clusterConfig = NDetail::GetDynamicConfiguration(client, ConfigProfile);
             auto iter = clusterConfig.find(Name_);
             if (!iter.IsEnd()) {
-                Value_ = iter->second.As<T>();
+                if (iter->second.IsOfType<T>()) {
+                    Value_ = iter->second.As<T>();
+                } else {
+                    ythrow yexception() << "operation_link_pattern must be string";
+                }
             }
-            Patched_ = true;
         }
         return Value_;
     }
@@ -39,7 +52,7 @@ public:
     }
 
 private:
-    const TString Name_;
+    TString Name_;
     T Value_;
     bool Patched_ = false;
 };
