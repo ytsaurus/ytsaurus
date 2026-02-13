c992bc7bcbf0b16d2cb22276c44615602489bb71
alexelexa <alexelexa@ytsaurus.tech>
2026-02-13 18:27:46 +0300

    Reset old futures on state provider restart commit_hash:47a240b423e4ed752a47ff7c7af32a3d432516fb

--- yt/yt/server/tablet_balancer/cluster_state_provider.cpp
+++ yt/yt/server/tablet_balancer/cluster_state_provider.cpp
@@ -124,6 +124,7 @@ void TClusterStateProvider::Start()
         BundlesFuture_.Reset();
         NodesFuture_.Reset();
         UnhealthyBundlesFuture_.Reset();
+        BannedReplicasFuture_.Reset();
     }
 
     PollExecutor_->Start();
