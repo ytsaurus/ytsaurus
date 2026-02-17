7a93006ec1c62d41ff8fe6538ae6811b5ec3d058
robot-contrib <robot-contrib@yandex-team.com>
2026-02-17 21:01:37 +0300

    Update contrib/python/sqlglot to 28.9.0 commit_hash:a44c05f9e533c87942f61c12b7a002750b2eadfd

--- contrib/python/sqlglot/sqlglot/typing/duckdb.py
+++ contrib/python/sqlglot/sqlglot/typing/duckdb.py
@@ -6,9 +6,44 @@ from sqlglot.typing import EXPRESSION_METADATA
 EXPRESSION_METADATA = {
     **EXPRESSION_METADATA,
     **{
+        expr_type: {"returns": exp.DataType.Type.BIGINT}
+        for expr_type in {
+            exp.Day,
+            exp.DayOfMonth,
+            exp.DayOfWeek,
+            exp.DayOfYear,
+            exp.Hour,
+            exp.Minute,
+            exp.Month,
+            exp.Quarter,
+            exp.Second,
+            exp.Week,
+            exp.Year,
+        }
+    },
+    **{
         expr_type: {"returns": exp.DataType.Type.INT128}
         for expr_type in {
             exp.Factorial,
         }
     },
+    **{
+        expr_type: {"returns": exp.DataType.Type.DOUBLE}
+        for expr_type in {
+            exp.Acosh,
+            exp.Asinh,
+            exp.Atanh,
+            exp.Atan2,
+            exp.Acos,
+            exp.Asin,
+            exp.Atan,
+            exp.Cos,
+            exp.Cot,
+            exp.Rand,
+            exp.Sin,
+            exp.Tan,
+            exp.TimeToUnix,
+        }
+    },
+    exp.ToDays: {"returns": exp.DataType.Type.INTERVAL},
 }
