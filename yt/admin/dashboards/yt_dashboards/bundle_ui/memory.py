c24717a20cff14ec2260ca0935c80626f1fc2a2d
tea-mur <tea-mur@ytsaurus.tech>
2026-04-01 14:14:46 +0300

    YT-26094: Removed aggregated oom threshold metrics from dashboards commit_hash:d26770f0438b42f215dadd85b357516411d5e37f

--- yt/admin/dashboards/yt_dashboards/bundle_ui/memory.py
+++ yt/admin/dashboards/yt_dashboards/bundle_ui/memory.py
@@ -6,7 +6,7 @@ from yt_dashboard_generator.backends.monitoring.sensors import MonitoringExpr
 from yt_dashboard_generator.specific_tags.tags import DuplicateTag
 from yt_dashboards.jobs_monitor import BYTES_LABEL
 
-from .resources import memory_guarantee, anon_memory_limit, anon_memory_usage, oom_tracker_threshold
+from .resources import memory_guarantee, anon_memory_limit, anon_memory_usage
 
 from ..common.sensors import *
 
@@ -33,7 +33,6 @@ dynamic memory almost always means that write throughput is too large.
             .cell("Memory usage per category (not shown per-host)", MultiSensor(
                     memory_guarantee.alias("Container Memory Guarantee"),
                     anon_memory_limit.alias("Anon Memory Limit"),
-                    oom_tracker_threshold.alias("OOM tracker threshold"),
                     anon_memory_usage.alias("Anon Memory Usage"),
                     MonitoringExpr(TabNode("yt.cluster_node.memory_usage.used")
                         .sensor_stack()
