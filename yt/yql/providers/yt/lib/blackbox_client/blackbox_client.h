ac4e6bdb69505c1d4a22c4dfe8bd79a71ca5998d
ziganshinmr <ziganshinmr@ytsaurus.tech>
2026-04-08 16:39:00 +0300

    Secure tmp folders
    Add support for automated allocation of secure TmpFolder on YT clusters for each user
    commit_hash:2d06fc7616f65d9814c7a5ca33a4da582fb5b016

---
+++ yt/yql/providers/yt/lib/blackbox_client/blackbox_client.h
@@ -0,0 +1,19 @@
+#pragma once
+
+#include <yt/yql/providers/yt/lib/tvm_client/tvm_client.h>
+
+#include <yql/essentials/core/credentials/yql_credentials.h>
+#include <yql/essentials/providers/common/proto/gateways_config.pb.h>
+
+#include <util/generic/ptr.h>
+
+namespace NYql {
+
+class IBlackboxClient : public TThrRefBase {
+public:
+    using TPtr = TIntrusivePtr<IBlackboxClient>;
+
+    virtual TString GetUserTicket(const TUserCredentials& credentials) = 0;
+};
+
+}; // namespace NYql
