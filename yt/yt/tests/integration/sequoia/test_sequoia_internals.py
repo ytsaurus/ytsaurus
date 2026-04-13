15cf934a9f0b71b87553f99eb38625480870607d
h0pless <h0pless@ytsaurus.tech>
2026-04-13 12:17:08 +0300

    Change per user weight throttler to be disabled by default commit_hash:f0057dbf935e863a58a3f305b1a41ff610b64208

--- yt/yt/tests/integration/sequoia/test_sequoia_internals.py
+++ yt/yt/tests/integration/sequoia/test_sequoia_internals.py
@@ -605,6 +605,7 @@ class TestSequoiaInternals(YTEnvSetup):
 
         set("//sys/cypress_proxies/@config", {
             "object_service": {
+                "enable_per_user_request_weight_throttling": True,
                 "distributed_throttler": {
                     "member_client": {
                         "attribute_update_period": 300,
