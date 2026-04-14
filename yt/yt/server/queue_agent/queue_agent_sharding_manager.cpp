32fd67567dda237d7c6abd2a8aba20e9f508c153
panesher <panesher@ytsaurus.tech>
2026-04-14 21:26:18 +0300

    [25.3] [queues] fix YT-27495: fix pass metrics commit_hash:db2cb36ef51b415643b2465836638f9abf9f40ff

--- yt/yt/server/queue_agent/queue_agent_sharding_manager.cpp
+++ yt/yt/server/queue_agent/queue_agent_sharding_manager.cpp
@@ -65,11 +65,11 @@ public:
         , DiscoveryClient_(std::move(discoveryClient))
         , QueueAgentStage_(std::move(queueAgentStage))
         , DynamicStateRoot_(std::move(dynamicStateRoot))
+        , Profiler_(profiler)
         , PassExecutor_(New<TPeriodicExecutor>(
             ControlInvoker_,
             BIND(&TQueueAgentShardingManager::Pass, MakeWeak(this)),
             DynamicConfig_.Acquire()->PassPeriod))
-        , PassProfiler_(profiler.WithPrefix("/queue_agent_sharding_manager"))
         , OrchidService_(IYPathService::FromProducer(BIND(&TQueueAgentShardingManager::BuildOrchid, MakeWeak(this)))
             ->Via(ControlInvoker_))
         , SyncBannedQueueAgentInstancesFrequency_(CalculateSyncBannedQueueAgentInstancesFrequency(*DynamicConfig_.Acquire()))
@@ -114,9 +114,11 @@ private:
     const IDiscoveryClientPtr DiscoveryClient_;
     const std::string QueueAgentStage_;
     const TYPath DynamicStateRoot_;
+    const TProfiler Profiler_;
     const TPeriodicExecutorPtr PassExecutor_;
-    const TPassProfiler PassProfiler_;
     const IYPathServicePtr OrchidService_;
+
+    TIntrusivePtr<TPassProfiler> PassProfiler_;
 
     std::atomic<bool> Active_ = false;
     //! Current pass iteration error.
@@ -147,7 +149,9 @@ private:
 
         PassInstant_ = TInstant::Now();
         ++PassIndex_;
-        PassProfiler_.OnStart(PassIndex_, PassInstant_);
+        if (PassProfiler_) {
+            PassProfiler_->OnStart(PassIndex_, PassInstant_);
+        }
 
         try {
             GuardedPass();
@@ -160,11 +164,17 @@ private:
                 "Error performing queue agent manager pass",
                 /*tags*/ {},
                 ex));
-            PassProfiler_.OnError();
+
+            // NB(panesher): We want to see error even if we are not leading.
+            if (PassProfiler_) {
+                PassProfiler_->OnError();
+            }
         }
 
         AlertCollector_->PublishAlerts();
-        PassProfiler_.OnFinish(TInstant::Now() - PassInstant_);
+        if (PassProfiler_) {
+            PassProfiler_->OnFinish(TInstant::Now() - PassInstant_);
+        }
     }
 
     //! Uses FarmFingerprint as a deterministic (both platform and time-agnostic) hash.
@@ -286,10 +296,14 @@ private:
         if (filteredQueueAgents[0].Id != MemberClient_->GetId()) {
             YT_LOG_DEBUG("Queue agent is not leading, skipping pass (LeadingHost: %v)", filteredQueueAgents[0].Id);
             Active_ = false;
+            PassProfiler_ = nullptr;
             return;
-        } else {
+        } else if (!Active_) {
+            YT_LOG_INFO("Sharding manager leadership acquired");
             Active_ = true;
+            PassProfiler_ = New<TPassProfiler>(Profiler_.WithPrefix("/queue_agent_sharding_manager"));
         }
+        YT_VERIFY(PassProfiler_);
 
         // Collect rows from dynamic state.
 
