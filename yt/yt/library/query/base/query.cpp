ac7a9a2b933f0640886b9899b0eeb90b565dc553
sabdenovch <sabdenovch@ytsaurus.tech>
2026-04-22 10:37:36 +0300

    Microoptimize QL commit_hash:213d9698507d6cbc7fb35e5db835a36fd6c01c0a

--- yt/yt/library/query/base/query.cpp
+++ yt/yt/library/query/base/query.cpp
@@ -253,8 +253,8 @@ TAggregateItem::TAggregateItem(
     : Arguments(std::move(arguments))
     , Name(name)
     , AggregateFunction(aggregateFunction)
-    , StateType(stateType)
-    , ResultType(resultType)
+    , StateType(std::move(stateType))
+    , ResultType(std::move(resultType))
 { }
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -283,10 +283,15 @@ TKeyColumns TMappedSchema::GetKeyColumns() const
 
 TTableSchemaPtr TMappedSchema::GetRenamedSchema() const
 {
+    auto orderedMapping = GetOrderedSchemaMapping();
+
     TSchemaColumns result;
-    for (const auto& item : GetOrderedSchemaMapping()) {
-        result.emplace_back(item.Name, Original->Columns()[item.Index].LogicalType());
+    result.reserve(orderedMapping.size());
+
+    for (auto& item : orderedMapping) {
+        result.emplace_back(std::move(item.Name), Original->Columns()[item.Index].LogicalType());
     }
+
     return New<TTableSchema>(std::move(result));
 }
 
@@ -539,13 +544,13 @@ TKeyColumns TQuery::GetKeyColumns() const
 
 TTableSchemaPtr TQuery::GetReadSchema() const
 {
-    TSchemaColumns result;
+    auto orderedMapping = Schema.GetOrderedSchemaMapping();
 
-    for (const auto& item : Schema.GetOrderedSchemaMapping()) {
-        auto& columnSchema = result.emplace_back(
-            Schema.Original->Columns()[item.Index].Name(),
-            Schema.Original->Columns()[item.Index].LogicalType());
-        columnSchema.SetStableName(Schema.Original->Columns()[item.Index].StableName());
+    TSchemaColumns result;
+    result.reserve(orderedMapping.size());
+
+    for (const auto& item : orderedMapping) {
+        result.emplace_back(Schema.Original->Columns()[item.Index]);
     }
 
     return New<TTableSchema>(std::move(result));
