99d39eb295031c72b15b3fb3028ba6364a91f3bd
severovv <severovv@ytsaurus.tech>
2026-05-07 22:32:18 +0300

    YT-27811: add limits tests to allocating gpu policy commit_hash:28aed1dfb1e638726c3b34924a7170f6485da07c

--- yt/yt/server/scheduler/strategy/policy/gpu/scheduling_policy_detail.cpp
+++ yt/yt/server/scheduler/strategy/policy/gpu/scheduling_policy_detail.cpp
@@ -992,6 +992,7 @@ void TSchedulingPolicy::ScheduleAllocations(
             /*allowLimitsOvercommit*/ false,
             /*additionalLocalResourceLimits*/ {},
             &availableResourceLimits);
+        auto precommittedResources = assignment->ResourceUsage;
 
         auto availableResources = Min(availableResourceLimits, schedulingHeartbeatContext->GetNodeFreeResourcesWithDiscount());
 
@@ -1027,8 +1028,7 @@ void TSchedulingPolicy::ScheduleAllocations(
 
             RemoveAssignment(assignment, /*strict*/ false);
 
-            operationElement->DecreaseHierarchicalResourceUsagePrecommit(
-                assignment->ResourceUsage);
+            operationElement->DecreaseHierarchicalResourceUsagePrecommit(precommittedResources);
 
             nodeShardInvoker->Invoke(BIND(
                 &TPoolTreeOperationElement::OnScheduleAllocationFailed,
