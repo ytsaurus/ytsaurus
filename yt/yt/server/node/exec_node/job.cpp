37d0ad05101a30199787f5077091e51e9fbb9d93
maybenotilya <maybenotilya@ytsaurus.tech>
2026-05-12 16:43:38 +0300

    YT-27835: Add Job Proxy Rpc server dynamic config commit_hash:b4f7a93ee5cef15e00afd57d5ef5bb328d4d9be3

--- yt/yt/server/node/exec_node/job.cpp
+++ yt/yt/server/node/exec_node/job.cpp
@@ -3499,6 +3499,7 @@ TJobProxyInternalConfigPtr TJob::CreateConfig()
             tcmallocConfig->HeapSizeLimit->DumpMemoryProfileOnViolation = true;
         }
 
+        proxyInternalConfig->RpcServerDynamic = proxyDynamicConfig->RpcServer;
         proxyInternalConfig->JobProxyApiService = proxyDynamicConfig->JobProxyApiService;
 
         proxyInternalConfig->EnableGrpcServer = proxyDynamicConfig->EnableGrpcServer;
