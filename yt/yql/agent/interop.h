3a81dfec01dc1ed3609c848da2ea48721eb928d0
ngc224 <ngc224@ytsaurus.tech>
2026-05-15 14:55:18 +0300

    Revert "YT-26692: Fix multiple full results"
    This reverts commit a4f7e5633853d5452d428df71d59e065673093db, reversing
    changes made to 523c828749f290a6e67fa6644c96b04ba11fa026.
    commit_hash:19fa5c18db3532d74b25dcbd3a91a2aec0adbab8

--- yt/yql/agent/interop.h
+++ yt/yql/agent/interop.h
@@ -44,7 +44,7 @@ struct TYqlRowset
     std::vector<NTableClient::TUnversionedRow> ResultRows;
     NTableClient::TRowBufferPtr RowBuffer;
     bool Incomplete = false;
-    std::vector<TYqlRefPtr> References;
+    TYqlRefPtr References;
 };
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -54,7 +54,7 @@ struct TWireYqlRowset
     TError Error;
     TSharedRef WireRowset;
     bool Incomplete = false;
-    std::vector<TYqlRefPtr> References;
+    TYqlRefPtr References;
 };
 
 std::vector<TWireYqlRowset> BuildRowsets(
