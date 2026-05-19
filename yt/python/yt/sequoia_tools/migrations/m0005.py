e769b2cc3c154ddc9a135d723cd398dc24bb6122
grphil <grphil@ytsaurus.tech>
2026-05-19 20:23:36 +0300

    YT-28218: Fix sequoia-tool migration command
    * Fix "aggregate" field drop on table conversion.
    * `chunk_replicas::stored_replicas` column requires reformatting on map operation.
    commit_hash:7b5f65a702cd1e0f5868bda4bb1aab96dc2e323c

--- yt/python/yt/sequoia_tools/migrations/m0005.py
+++ yt/python/yt/sequoia_tools/migrations/m0005.py
@@ -3,6 +3,12 @@
 from .. import action_builder as builder, actions, app as sequoia_app, config as cfg
 
 
+def chunk_replicas_table_mapper(row):
+    result = dict([(key, row.get(key)) for key in row])
+    result["stored_replicas"] = [result["stored_replicas"], []]
+    yield result
+
+
 def add_small_chunk_id_hash_to_chunk_replicas_table(app: sequoia_app.SequoiaTool) -> actions.ActionPlan:
     return (
         builder.ActionBuilder(app, name="add-small-chunk-id-hash-to-chunk-replicas-table", version=5)
@@ -14,7 +20,10 @@ def add_small_chunk_id_hash_to_chunk_replicas_table(app: sequoia_app.SequoiaTool
                 builder.ConversionAction(
                     ctx,
                     source="chunk_replicas",
-                    use_default_mapper=True)
+                    operation="map",
+                    operation_args={
+                        "binary": chunk_replicas_table_mapper,
+                    })
             ])
         .then()
         .build())
