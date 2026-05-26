c42e8d544755f7711e7e393063c74e221a35e30b
kvk1920 <kvk1920@ytsaurus.tech>
2026-05-26 15:55:19 +0300

    YT-28308: fix tx abort request being stuck due to user ban
    * Changelog entry
    Type: fix
    Component: master
    
    Fix rare stuck transaction finish if transaction leases were used and user was banned between lease revocation and transaction finish.
    commit_hash:da33063637255a9a54d01f03d4ed806323f8ee8d

--- yt/yt/server/master/cell_master/serialize.h
+++ yt/yt/server/master/cell_master/serialize.h
@@ -202,6 +202,7 @@ DEFINE_ENUM(EMasterReign,
     ((ExpectedPrepareSignature_26_1)                                (3248))  // atalmenev
     ((HunkChunkTreeStatisticsOverhaul)                              (3249))  // akozhikhov
     ((TableBackupAttributes)                                        (3250))  // grphil
+    ((FixStuckTransactionAbortForBannedUsers_26_1)                  (3251))  // kvk1920
 
     // 26.2 starts here.
     ((Start_26_2)                                                   (3300))  // ermolovd
@@ -220,6 +221,7 @@ DEFINE_ENUM(EMasterReign,
     ((NativeClientUnfoldedIndexPartialBackCompat_26_2)              (3313))  // sabdenovch
     ((ExpectedPrepareSignature)                                     (3314))  // atalmenev
     ((TableBackupAttributes_26_2)                                   (3315))  // grphil
+    ((FixStuckTransactionAbortForBannedUsers_26_2)                  (3316))  // kvk1920
 );
 
 static_assert(TEnumTraits<EMasterReign>::IsMonotonic, "Master reign enum is not monotonic");
