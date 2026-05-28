13de72851e104c708d878e5b1096a55fd16c8929
buyval01 <buyval01@ytsaurus.tech>
2026-05-28 08:43:56 +0300

    CHYT-687: Add unweighted profiler counters to TTableSchemaCache commit_hash:8953018c480d4061ad6d1ca54870b77beae69868

--- yt/chyt/server/table_schema_cache.cpp
+++ yt/chyt/server/table_schema_cache.cpp
@@ -33,13 +33,20 @@ i64 TCachedTableSchema::GetWeight() const
 TTableSchemaCache::TTableSchemaCache(
     TSlruCacheConfigPtr config,
     TProfiler profiler)
-    : TSyncSlruCacheBase(std::move(config), std::move(profiler))
-{ }
+    : TSyncSlruCacheBase(std::move(config), profiler.WithPrefix("/weight"))
+    , HitCounter_(profiler.Counter("/hit"))
+    , MissCounter_(profiler.Counter("/miss"))
+{
+    profiler.AddFuncGauge("/size", MakeStrong(this), [this] {
+        return EntryCount_.load();
+    });
+}
 
 TTableSchemaPtr TTableSchemaCache::Get(TObjectId schemaId)
 {
     auto result = Find(schemaId);
     if (result) {
+        HitCounter_.Increment();
         return result->GetTableSchema();
     }
     return nullptr;
@@ -48,7 +55,19 @@ TTableSchemaPtr TTableSchemaCache::Get(TObjectId schemaId)
 void TTableSchemaCache::Insert(TObjectId schemaId, TTableSchemaPtr schema)
 {
     auto cachedSchema = New<TCachedTableSchema>(schemaId, std::move(schema));
-    TryInsert(std::move(cachedSchema));
+    if (TryInsert(std::move(cachedSchema))) {
+        MissCounter_.Increment();
+    }
+}
+
+void TTableSchemaCache::OnAdded(const TCachedTableSchemaPtr& /*value*/)
+{
+    EntryCount_.fetch_add(1);
+}
+
+void TTableSchemaCache::OnRemoved(const TCachedTableSchemaPtr& /*value*/)
+{
+    EntryCount_.fetch_sub(1);
 }
 
 i64 TTableSchemaCache::GetWeight(const TCachedTableSchemaPtr& value) const
