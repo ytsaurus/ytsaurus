8dc6cca1197d374fb3e18157d0ac795e60c12f3c
pechatnov <pechatnov@yandex-team.com>
2026-05-29 12:56:53 +0300

    Move lag rows to the top of the general dashboard commit_hash:758eecaf1e6989bf32417dffdc44b3b888efcf43

--- yt/admin/dashboards/yt_dashboards/flow/general.py
+++ yt/admin/dashboards/yt_dashboards/flow/general.py
@@ -170,11 +170,11 @@ def build_logging():
 
 def build_flow_general():
     def fill(d):
+        d.add(build_lags())
+        d.add(COMPUTATION_CELL_GENERATOR.build_event_lag_rowset())
         d.add(build_resource_usage("controller", add_component_to_title=True))
         d.add(build_resource_usage("worker", add_component_to_title=True))
         d.add(build_flow_status())
-        d.add(build_lags())
-        d.add(COMPUTATION_CELL_GENERATOR.build_event_lag_rowset())
         d.add(COMPUTATION_CELL_GENERATOR.build_message_rate_rowset())
         d.add(build_epoch_timings())
         d.add(COMPUTATION_CELL_GENERATOR.build_resources_rowset())
