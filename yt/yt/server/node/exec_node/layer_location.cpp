27fe782567bc730b9cc0087b9130f68efbd19032
yuryalekseev <yuryalekseev@ytsaurus.tech>
2026-06-05 08:03:34 +0300

    [26.1] YT-28388: Fix TLayerLocation::RemoveLayers(). commit_hash:26e07195ea141fc1d10589fbee681744118054e1

--- yt/yt/server/node/exec_node/layer_location.cpp
+++ yt/yt/server/node/exec_node/layer_location.cpp
@@ -1452,8 +1452,11 @@ void TLayerLocation::RemoveLayers(
 {
     auto startTime = TInstant::Now();
 
+    // See PORTO-460 for "//" prefix.
+    auto portoPlace = (!Config_->LocationIsAbsolute && !place.starts_with("//") ? "//" : "") + place;
+
     auto Logger = ExecNodeLogger()
-        .WithTag("Place: %v", place);
+        .WithTag("Place: %v", portoPlace);
 
     YT_LOG_DEBUG(
         "Removing layers from porto place (Timeout: %v)",
@@ -1463,7 +1466,7 @@ void TLayerLocation::RemoveLayers(
 
     auto executor = FastLayerExecutor_ ? FastLayerExecutor_ : LayerExecutor_;
 
-    auto layerIds = WaitFor(executor->ListLayers(place).WithTimeout(timeout))
+    auto layerIds = WaitFor(executor->ListLayers(portoPlace).WithTimeout(timeout))
         .ValueOrThrow();
 
     std::vector<TFuture<void>> removeFutures;
@@ -1475,7 +1478,7 @@ void TLayerLocation::RemoveLayers(
         removedLayers.push_back(layerId);
         removeFutures.push_back(executor->RemoveLayer(
             layerId,
-            place,
+            portoPlace,
             false /*async*/));
     }
 
