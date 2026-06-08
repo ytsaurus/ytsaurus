d41ed7ba6baa71efb18be56181a471f60c64d02f
Anton Romanov <anton.romanoff@gmail.com>
2026-06-08 13:01:06 +0200

    Tutorial for views.

--- yt/yt/scripts/tutorial/generate_tutorial/generate_tutorial.py
+++ yt/yt/scripts/tutorial/generate_tutorial/generate_tutorial.py
@@ -480,7 +480,7 @@ CREATE VIEW `$joint` AS
 DO BEGIN
 SELECT * WITHOUT o.nomenclature_id, p.nomenclature_id, p.date
 FROM RANGE('$orders','','','','plain') AS o
-INNER JOIN `$nomenclature`VIEW plain AS n
+INNER JOIN `$nomenclature` VIEW plain AS n
 ON o.nomenclature_id = n.id
 INNER JOIN `$price` AS p
 ON p.nomenclature_id = n.id AND p.date = o.date
