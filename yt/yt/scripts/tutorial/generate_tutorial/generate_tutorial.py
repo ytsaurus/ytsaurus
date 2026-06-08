b5133728e8a400f2464a884bdbca4637e8450208
Anton Romanov <anton.romanoff@gmail.com>
2026-06-08 11:06:39 +0200

    Add view.

--- yt/yt/scripts/tutorial/generate_tutorial/generate_tutorial.py
+++ yt/yt/scripts/tutorial/generate_tutorial/generate_tutorial.py
@@ -310,6 +310,33 @@ class GenerateOrders(yt.wrapper.TypedJob):
                     table_index=input_row.price_date,
                 )
 
+def track_query(yt_client, query_id, stage):
+    last_state = ""
+    while True:
+        state = yt_client.get_query(query_id, attributes=["state"], stage=stage)["state"]
+        if state != last_state:
+            print(f"Query {query_id}: {state}")
+            last_state = state
+
+        if state in ("completed", "failed", "aborted"):
+            return state
+
+        time.sleep(1.0)
+
+def apply_paths(script: str, args: argparse.Namespace):
+    path_to_nomenclature_table = args.yt_directory + "/nomenclature"
+    path_to_prices_table = args.yt_directory + "/price"
+    path_to_price_map_table = args.yt_directory + "/price_map"
+    path_to_orders_directory = args.yt_directory + "/orders"
+    path_to_joint_view = args.yt_directory + "/orders_with_prices"
+    return Template(script).safe_substitute(
+        nomenclature=path_to_nomenclature_table,
+        price=path_to_prices_table,
+        orders=path_to_orders_directory,
+        joint=path_to_joint_view,
+        start_date=datetime.date.today() - timedelta(days=args.days_to_generate - 1),
+        end_date=datetime.date.today(),
+    )
 
 def generate_data(args: argparse.Namespace):
     client = yt.wrapper.YtClient(proxy=args.proxy, token=os.environ["YT_TOKEN"])
@@ -318,6 +345,7 @@ def generate_data(args: argparse.Namespace):
     path_to_prices_table = args.yt_directory + "/price"
     path_to_price_map_table = args.yt_directory + "/price_map"
     path_to_orders_directory = args.yt_directory + "/orders"
+    path_to_joint_view = args.yt_directory + "/orders_with_prices"
 
     if not client.exists(args.yt_directory) and not args.create_directory:
         raise TutorialGenerateError(f"No such directory: {args.yt_directory}")
@@ -333,6 +361,7 @@ def generate_data(args: argparse.Namespace):
             path_to_prices_table,
             path_to_price_map_table,
             path_to_orders_directory,
+            path_to_joint_view,
         ]:
             if client.exists(table):
                 raise TutorialGenerateError(f"Table {table} exists and force flag is false")
@@ -427,6 +456,37 @@ def generate_data(args: argparse.Namespace):
     client.set(f"{path_to_nomenclature_table}/@enable_dynamic_store_read", True)
     client.mount_table(path=path_to_nomenclature_table, sync=True)
 
+    settings = {"yql_version": "2025.05"}
+    if args.cluster_name:
+        settings["cluster"] = args.cluster_name
+
+    if args.force:
+        query_id = client.start_query(
+            engine = "yql",
+            settings = settings,
+            access_control_objects = ["nobody"],
+            stage = args.stage,
+            query = apply_paths('drop view if exists `$joint`', args)
+        )
+        track_query(client, query_id, args.stage)
+
+    query_id = client.start_query(
+        engine = "yql",
+        settings = settings,
+        access_control_objects = ["nobody"],
+        stage = args.stage,
+        query = apply_paths('''
+CREATE VIEW `$joint` AS
+DO BEGIN
+SELECT * WITHOUT o.nomenclature_id, p.nomenclature_id, p.date
+FROM RANGE('$orders','','','','plain') AS o
+INNER JOIN `$nomenclature`VIEW plain AS n
+ON o.nomenclature_id = n.id
+INNER JOIN `$price` AS p
+ON p.nomenclature_id = n.id AND p.date = o.date
+END DO; ''', args)
+    )
+    track_query(client, query_id, args.stage)
 
 def upload_tutorials(args: argparse.Namespace):
     client = yt.wrapper.YtClient(proxy=args.proxy, token=os.environ["YT_TOKEN"])
@@ -468,13 +528,7 @@ def upload_tutorials(args: argparse.Namespace):
                 print(f"Engine: {root.split("/")[1]}")
                 file_path = os.path.join(root, file)
                 with open(file_path, "r") as f:
-                    script = Template(f.read()).safe_substitute(
-                        nomenclature=path_to_tables_directory + "/nomenclature",
-                        price=path_to_tables_directory + "/price",
-                        orders=path_to_tables_directory + "/orders",
-                        start_date=datetime.date.today() - timedelta(days=args.days_to_generate - 1),
-                        end_date=datetime.date.today(),
-                    )
+                    script = apply_paths(f.read(), args)
                     query_id = yt.wrapper.driver.make_request(
                         "start_query",
                         {
