5a3d9f367c7fcd382458b897a36d1fbcf790ef31
osidorkin <osidorkin@ytsaurus.tech>
2026-06-10 12:11:10 +0300

    YT-28369: Pass extra tags next to registry not to lose them on their way to per queue profilers commit_hash:9f28b0926701d2641a4eb642bb5b79d977e52cd1

--- yt/yt/core/concurrency/profiling_helpers.h
+++ yt/yt/core/concurrency/profiling_helpers.h
@@ -15,7 +15,8 @@ NProfiling::TTagSet GetBucketTags(
 NProfiling::TTagSet GetQueueTags(
     const std::string& threadName,
     const std::string& bucketName,
-    const std::string& queueName);
+    const std::string& queueName,
+    const NProfiling::TTagSet& extraTags = {});
 
 ////////////////////////////////////////////////////////////////////////////////
 
