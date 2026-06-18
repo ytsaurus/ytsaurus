7fbb30b34a1f2b1f4e36ae6b5de03c79bfbfefd1
epsilond1 <epsilond1@ytsaurus.tech>
2026-06-18 17:06:17 +0300

    YT-27549: More flexible method for read token
    more flexible method for read token
    commit_hash:8f2589c5dd25fae6006781fef09c1a3b2b361f13

--- yt/admin/ytsaurus_ci/consts.py
+++ yt/admin/ytsaurus_ci/consts.py
@@ -3,4 +3,7 @@ import os
 BASE_DIR = os.path.dirname(os.path.abspath(__file__))
 COMPONENTS_PATH = "configs/components.yaml"
 SNAPSHOTS_PATH = "snapshots"
-CLOUD_FUNCTION_TOKEN_PATH = os.path.expanduser("~/.yt/ytsaurus_ci_token")
+CLOUD_FUNCTION_TOKEN_PATHS = [
+    os.path.expanduser("~/.yt/ytsaurus_ci_token"),
+    os.path.expanduser("~/.yc/cf_token"),
+]
