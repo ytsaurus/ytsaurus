2108aff54944a5f9c20bbd40f83102e0283c6f00
pogorelov <pogorelov@ytsaurus.tech>
2026-06-25 10:43:59 +0300

    YT-27729: Move thread limit config to operation spec; add threshold attr to alert
    Replace controller_agent options thread_limit_multiplier/initial_thread_limit
    with thread_limit_formula in TUserJobSpec. The new field is a TArithmeticFormula
    evaluated against the job cpu_limit (variable "cpu"); default is constant 10000.
    
    Also attach a threshold attribute to the high_job_thread_count alert showing
    the value computed by max_job_thread_count_formula for the violating job.
    commit_hash:c4edc35141a21b2b488d052d7c6e0f59b96a9f57

--- yt/yt/server/controller_agent/controllers/private.cpp
+++ yt/yt/server/controller_agent/controllers/private.cpp
@@ -1,5 +1,7 @@
 #include "private.h"
 
+#include <yt/yt/ytlib/controller_agent/serialize.h>
+
 namespace NYT::NControllerAgent::NControllers {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -8,6 +10,8 @@ void THighThreadCountJobInfo::RegisterMetadata(auto&& registrar)
 {
     PHOENIX_REGISTER_FIELD(1, JobId);
     PHOENIX_REGISTER_FIELD(2, ThreadCount);
+    PHOENIX_REGISTER_FIELD(3, Threshold,
+        .SinceVersion(ESnapshotVersion::HighThreadCountJobThreshold));
 }
 
 PHOENIX_DEFINE_TYPE(THighThreadCountJobInfo);
