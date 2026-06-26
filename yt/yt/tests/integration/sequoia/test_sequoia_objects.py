3466db2d6f126d44f937282d2e56916d5eaaecda
grphil <grphil@ytsaurus.tech>
2026-06-26 12:27:29 +0300

    Add tests for sequoia validation heartbeats replica fill
    Add tests for sequoia validation heartbeats replica fill
    commit_hash:db4cc7afa7817f9a1a327db3ac27969953f1ee46

--- yt/yt/tests/integration/sequoia/test_sequoia_objects.py
+++ yt/yt/tests/integration/sequoia/test_sequoia_objects.py
@@ -941,6 +941,53 @@ class TestSequoiaReplicas(YTEnvSetup):
         remove("//tmp/t")
         self._clear_hosts()
 
+    @authors("grphil")
+    def test_fill_replicas_using_validation_heartbeat_fixes(self):
+        if not get("//sys/@config/chunk_manager/sequoia_chunk_replicas/blob_chunk_replicas/store_sequoia_replicas_on_master"):
+            pytest.skip("store_sequoia_replicas_on_master is enabled")
+
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/enable", False)
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/blob_chunk_replicas/store_in_sequoia", False)
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/enable_sequoia_chunk_refresh", True)
+
+        create("table", "//tmp/t")
+
+        # Create multiple chunks
+        chunk_count = 10
+        for i in range(chunk_count):
+            write_table("<append=%true>//tmp/t", [{str(i): i}])
+
+        chunk_ids = get("//tmp/t/@chunk_ids")
+        for chunk_id in chunk_ids:
+            wait(lambda: len(get(f"#{chunk_id}/@stored_replicas")) == 3)
+
+        set("//sys/@config/chunk_manager/data_node_tracker/enable_validation_full_heartbeats", False)
+        time.sleep(1)
+
+        set("//sys/@config/cell_master/logging/message_level_overrides",
+            {
+                "Sequoia replicas validation failed": "debug",
+            })
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/fix_sequoia_replicas_if_replica_validation_failed", True)
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/enable", True)
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/blob_chunk_replicas/store_in_sequoia", True)
+
+        set("//sys/@config/chunk_manager/data_node_tracker/enable_validation_full_heartbeats", True)
+
+        time.sleep(2)
+
+        for chunk_id in chunk_ids:
+            wait(lambda: len(get(f"#{chunk_id}/@stored_sequoia_replicas")) == 3)
+            # We additionally get stored replicas to trigger validation of replica fetch.
+            get(f"#{chunk_id}/@stored_sequoia_replicas")
+
+        set("//sys/@config/cell_master/logging/message_level_overrides", {})
+        set("//sys/@config/chunk_manager/sequoia_chunk_replicas/fix_sequoia_replicas_if_replica_validation_failed", False)
+
+        time.sleep(2)
+
+        remove("//tmp/t")
+
 
 class TestSequoiaReplicasNonBatchHeartbeats(TestSequoiaReplicas):
     ENABLE_MULTIDAEMON = False  # There are component restarts.
