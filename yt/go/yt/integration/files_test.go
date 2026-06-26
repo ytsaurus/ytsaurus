86336d86a835bc3771e87e610773947ebc155504
tinarsky <tinarsky@ytsaurus.tech>
2026-06-26 15:23:42 +0300

    [yt/go] High level retriable file writer
    * Changelog entry
      Type: feature
      Component: go-sdk
    
    Add yt.WriteFile, a high level file writer that writes data in retriable batches.
    commit_hash:0fe9472fc918c16fb845f9906f7e55df021ef101

--- yt/go/yt/integration/files_test.go
+++ yt/go/yt/integration/files_test.go
@@ -75,3 +75,38 @@ func TestFiles(t *testing.T) {
 		})
 	})
 }
+
+func TestHighLevelFileWriter(t *testing.T) {
+	t.Parallel()
+
+	env := yttest.New(t)
+
+	t.Run("BigWrite", func(t *testing.T) {
+		name := tmpPath()
+
+		w, err := yt.WriteFile(env.Ctx, env.YT, name, yt.WithFileBatchSize(100))
+		require.NoError(t, err)
+
+		const testSize = 1024
+		content := make([]byte, testSize)
+		for i := range content {
+			content[i] = byte(i)
+			_, err := w.Write(content[i : i+1])
+			require.NoError(t, err)
+		}
+
+		exists, err := env.YT.NodeExists(env.Ctx, name, nil)
+		require.NoError(t, err)
+		require.False(t, exists, "File should not be visible because it is written inside tx")
+
+		require.NoError(t, w.Close())
+
+		r, err := env.YT.ReadFile(env.Ctx, name, nil)
+		require.NoError(t, err)
+		defer func() { _ = r.Close() }()
+
+		got, err := io.ReadAll(r)
+		require.NoError(t, err)
+		require.Equal(t, content, got)
+	})
+}
