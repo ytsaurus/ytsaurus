2f2a051dc12e97346620ea9f1210be3defbe160a
mpereskokova <mpereskokova@ytsaurus.tech>
2026-07-07 13:05:40 +0300

    YT-28585: Rollback debug commit_hash:50cca62cfe6d52e16d3c2aa1e35572361380d30c

--- yt/yql/plugin/lib/progress_merger.h
+++ yt/yql/plugin/lib/progress_merger.h
@@ -18,7 +18,6 @@ public:
     using NYql::TNodeProgress::TNodeProgress;
 
     void Serialize(::NYson::TYsonWriter& yson) const;
-    bool HasStages() const;
 };
 
 //////////////////////////////////////////////////////////////////////////////
