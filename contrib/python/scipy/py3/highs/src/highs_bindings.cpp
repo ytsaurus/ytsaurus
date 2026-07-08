458efa5546989bfdff91c6fcecc8e40fab83dbc3
robot-piglet <robot-piglet@yandex-team.com>
2026-07-08 20:02:17 +0300

    Intermediate changes commit_hash:745a0e17d1313f6f480fd83487758c6afebc8e66

--- contrib/python/scipy/py3/highs/src/highs_bindings.cpp
+++ contrib/python/scipy/py3/highs/src/highs_bindings.cpp
@@ -1,4 +1,4 @@
-#define PYBIND11_DETAILED_ERROR_MESSAGES 1
+// #define PYBIND11_DETAILED_ERROR_MESSAGES 1
 #include <pybind11/functional.h>
 #include <pybind11/numpy.h>
 #include <pybind11/pybind11.h>
