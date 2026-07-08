0d609f851efef1e9e1c6d433f425b7240f360a1e
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2026-07-08 14:59:42 +0300

    Restore full hb throttlers and make them better commit_hash:63c67f47ce1cb3cdfcf85a15d40f87868100ed31

--- yt/yt/server/master/chunk_server/config.h
+++ yt/yt/server/master/chunk_server/config.h
@@ -305,6 +305,13 @@ struct TDynamicDataNodeTrackerConfig
 
     int MaxConcurrentChunkReplicasDuringIncrementalHeartbeat;
 
+    // COMPAT(danilalexeev): YT-23781.
+    int MaxConcurrentFullHeartbeats;
+
+    int MaxConcurrentLocationFullHeartbeats;
+
+    int MaxConcurrentIncrementalHeartbeats;
+
     TDanglingLocationCleanerConfigPtr DanglingLocationCleaner;
 
     // COMPAT(danilalexeev): YT-23781.
@@ -318,6 +325,9 @@ struct TDynamicDataNodeTrackerConfig
     // COMPAT(grphil)
     bool IgnoreReplicasWithChangedStateDuringValidation;
 
+    // COMPAT(cherepashka)
+    bool EnableChunkReplicasThrottlingInHeartbeats;
+
     bool EnableLocationIndexesInDataNodeHeartbeats;
 
     // COMPAT(cherepashka)
@@ -330,6 +340,8 @@ struct TDynamicDataNodeTrackerConfig
     bool CheckLocationConvergenceByIndexAndUuidOnConfirmation;
 
     bool VerifyAllLocationsAreReportedInFullHeartbeats;
+
+    TDuration ExpectedDataNodeHeartbeatDuration;
 
     REGISTER_YSON_STRUCT(TDynamicDataNodeTrackerConfig);
 
