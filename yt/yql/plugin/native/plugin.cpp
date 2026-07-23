4e9ebf9589369bcce0141f6fa2c3643981776a53
vitya-smirnov <vitya-smirnov@yandex-team.com>
2026-07-23 09:15:31 +0300

    YQL-21333: Refactor sql/v1/translation commit_hash:9ad4de6a95ef5173379f58dc372a6b208386f34b

--- yt/yql/plugin/native/plugin.cpp
+++ yt/yql/plugin/native/plugin.cpp
@@ -57,7 +57,7 @@
 #include <yql/essentials/utils/backtrace/backtrace.h>
 #include <yql/essentials/utils/log/context.h>
 #include <yql/essentials/utils/log/log.h>
-#include <yql/essentials/sql/v1/sql.h>
+#include <yql/essentials/sql/v1/translation/sql.h>
 #include <yql/essentials/sql/v1/lexer/antlr4/lexer.h>
 #include <yql/essentials/sql/v1/lexer/antlr4_ansi/lexer.h>
 #include <yql/essentials/sql/v1/proto_parser/antlr4/proto_parser.h>
