9c5e6776c05e7a773a5979129c3f92d8f9e684ae
pechatnov <pechatnov@yandex-team.com>
2026-07-28 09:07:44 +0300

    Support reading from chaos queues
     Support reading from chaos queues
    commit_hash:e73593e4222ca72911443a557b2430f10636ef57

--- yt/yt/flow/library/cpp/connectors/queue/queue_info.cpp
+++ yt/yt/flow/library/cpp/connectors/queue/queue_info.cpp
@@ -60,17 +60,11 @@ void TQueueInfoController::TryUpdatePartitionCount()
 {
     try {
         TGetNodeOptions options;
-        options.Attributes = {"tablet_count", "type"};
+        options.Attributes = {"tablet_count"};
         auto ysonString = WaitFor(Client_->GetNode(Spec_->QueuePath.GetPath(), options))
             .ValueOrThrow();
         auto node = NYTree::ConvertToNode(ysonString);
-        const auto& attributes = node->Attributes();
-        auto type = attributes.Get<NObjectClient::EObjectType>("type");
-        if (type == NObjectClient::EObjectType::ChaosReplicatedTable) {
-            THROW_ERROR_EXCEPTION("No support for chaos yet")
-                << TErrorAttribute("queue_path", Spec_->QueuePath);
-        }
-        State_->CachedPartitionCount = attributes.Get<int>("tablet_count");
+        State_->CachedPartitionCount = node->Attributes().Get<int>("tablet_count");
         YT_TLOG_INFO("Queue topic partition count was updated")
             .With("CurrentPartitionCount", State_->CachedPartitionCount);
         ErrorState_->ClearError();
