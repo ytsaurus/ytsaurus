51edfc4c96d790d9c1744ed2944fc88a5fc11e4f
apachee <apachee@ytsaurus.tech>
2026-07-31 12:30:16 +0300

    YT-28844: Set default timeout for intra queue agent requests commit_hash:b2f2ec209536bd25a025e1330d228b8ef94c0a10

--- yt/yt/server/queue_agent/queue_agent.cpp
+++ yt/yt/server/queue_agent/queue_agent.cpp
@@ -33,6 +33,8 @@
 
 #include <yt/yt/core/misc/collection_helpers.h>
 
+#include <yt/yt/core/rpc/helpers.h>
+
 #include <yt/yt/core/ypath/token.h>
 
 #include <yt/yt/core/ytree/fluent.h>
@@ -511,7 +513,10 @@ TQueueAgent::TQueueAgent(
         DynamicConfig_->PassPeriod))
     , PassProfiler_(QueueAgentProfiler())
     , AgentId_(std::move(agentId))
-    , QueueAgentChannelFactory_(nativeConnection->GetChannelFactory())
+    , BaseQueueAgentChannelFactory_(nativeConnection->GetChannelFactory())
+    , QueueAgentChannelFactory_(NRpc::CreateDefaultTimeoutChannelFactory(
+        BaseQueueAgentChannelFactory_,
+        DynamicConfig_->QueueAgentChannelRequestTimeout))
     , QueueExportManager_(CreateQueueExportManager(
         nativeConnection,
         std::move(queueAgentUser),
@@ -604,6 +609,10 @@ void TQueueAgent::OnDynamicConfigChanged(
     PassExecutor_->SetPeriod(newConfig->PassPeriod);
 
     ControllerThreadPool_->SetThreadCount(newConfig->ControllerThreadCount);
+
+    QueueAgentChannelFactory_.Store(NRpc::CreateDefaultTimeoutChannelFactory(
+        BaseQueueAgentChannelFactory_,
+        newConfig->QueueAgentChannelRequestTimeout));
 
     {
         auto guard = ReaderGuard(ObjectLock_);
@@ -1111,8 +1120,11 @@ NYTree::IYPathServicePtr TQueueAgent::RedirectYPathRequest(const std::string& ho
 {
     YT_ASSERT_THREAD_AFFINITY_ANY();
 
-    YT_LOG_DEBUG("Redirecting orchid request (QueueAgentHost: %v, RemoteRoot: %v)", host, remoteRoot);
-    auto leaderChannel = QueueAgentChannelFactory_->CreateChannel(host);
+    YT_LOG_DEBUG("Redirecting orchid request (QueueAgentHost: %v, RemoteRoot: %v)",
+        host,
+        remoteRoot);
+
+    auto leaderChannel = QueueAgentChannelFactory_.Acquire()->CreateChannel(host);
     return CreateOrchidYPathService({
         .Channel = std::move(leaderChannel),
         .RemoteRoot = std::string(remoteRoot),
