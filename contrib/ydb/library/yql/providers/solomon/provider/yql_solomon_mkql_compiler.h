afcce67b4c7da23d3934017106029d0f08d0d50e
mpereskokova <mpereskokova@ytsaurus.tech>
2026-08-06 14:41:53 +0300

    Remove solomon from OS yql native plugin commit_hash:44b707ffe76ac649d9adc4d76442ddfeb0974002

--- contrib/ydb/library/yql/providers/solomon/provider/yql_solomon_mkql_compiler.h
+++
@@ -1,9 +0,0 @@
-#pragma once
-
-#include <yql/essentials/providers/common/mkql/yql_provider_mkql.h>
-
-namespace NYql {
-
-void RegisterDqSolomonMkqlCompilers(NCommon::TMkqlCallableCompilerBase& compiler);
-
-}
