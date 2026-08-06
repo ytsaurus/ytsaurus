1432d28aa9a636168f34c18af3a47383aa398533
dtorilov <dtorilov@ytsaurus.tech>
2026-08-06 21:55:42 +0300

    Cosmetics commit_hash:d3acf8213dad7af42f5a4d590bef6e823c2837a7

--- yt/yt/library/query/unittests/ql_query_ut.cpp
+++ yt/yt/library/query/unittests/ql_query_ut.cpp
@@ -11471,7 +11471,7 @@ TEST_F(TQueryEvaluateTest, YsonLengthComposite)
     };
 
     auto resultSplit = MakeSplit({
-        {"length", EValueType::Int64}
+        {"length", EValueType::Int64},
     });
 
     auto result = YsonToRows({
@@ -11503,7 +11503,7 @@ TEST_F(TQueryEvaluateTest, YsonLengthCompositeStruct)
     };
 
     auto resultSplit = MakeSplit({
-        {"length", EValueType::Int64}
+        {"length", EValueType::Int64},
     });
 
     auto result = YsonToRows({
@@ -11529,7 +11529,7 @@ TEST_F(TQueryEvaluateTest, YsonLengthCompositeNestedList)
     };
 
     auto resultSplit = MakeSplit({
-        {"length", EValueType::Int64}
+        {"length", EValueType::Int64},
     });
 
     auto result = YsonToRows({
