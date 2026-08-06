c91fcbe2cd0b9bf8a2fbae078885b9d423f22b62
yaishenka <yaishenka@ytsaurus.tech>
2026-08-06 12:12:51 +0300

    YT-29029: Fix precision error when comparing operation demand with module capacity in scheduling segments commit_hash:e348f0cd5f7882e274bef63b37d3ffcb4f7a3302

--- yt/yt/server/scheduler/strategy/policy/scheduling_segment_manager.cpp
+++ yt/yt/server/scheduler/strategy/policy/scheduling_segment_manager.cpp
@@ -382,7 +382,7 @@ std::optional<TSchedulingSegmentManager::TOperationsToPreempt> TSchedulingSegmen
         }
 
         auto neededDemand = operationDemand - context->RemainingCapacityPerModule[module];
-        if (neededDemand <= 0.0) {
+        if (neededDemand <= ResourceAmountPrecision) {
             return TOperationsToPreempt{
                 .Module = module,
             };
@@ -710,7 +710,8 @@ void TSchedulingSegmentManager::AssignOperationsToModules(TUpdateSchedulingSegme
 
             case ESchedulingSegmentModuleAssignmentHeuristic::MinRemainingFeasibleCapacity:
                 isModuleBetter = [operationDemand] (double remainingCapacity, double bestRemainingCapacity) {
-                    return remainingCapacity >= operationDemand && bestRemainingCapacity > remainingCapacity;
+                    return remainingCapacity + ResourceAmountPrecision >= operationDemand &&
+                        bestRemainingCapacity > remainingCapacity;
                 };
                 initialBestRemainingCapacity = std::numeric_limits<double>::max();
                 break;
