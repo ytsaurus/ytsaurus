63bd85cfc22d4b28ec361e4c186c110d5173195b
k-malov <k-malov@ytsaurus.tech>
2026-08-07 11:00:05 +0300

    Drop port and tcp:// prefix from cache key factor for non-MTN addresses
    Drop port and tcp:// prefix from cache key factor for non-MTN addresses
    
    Otherwise the same token from the same IP but different source ports
    produces different cache keys, leading to constant cache misses.
    commit_hash:117a5cad79f4878ebd1f59e2ad6d6c3ac1b32da2

--- yt/yt/library/auth_server/helpers.cpp
+++ yt/yt/library/auth_server/helpers.cpp
@@ -68,7 +68,12 @@ std::string GetBlackboxCacheKeyFactorFromUserIP(
         return Format("project_id=%v", TMtnAddress(address.ToIP6Address()).GetProjectId());
     }
 
-    return Format("ip=%v", ToString(address));
+    return Format(
+        "ip=%v",
+        ToString(address, TNetworkAddressFormatOptions{
+            .IncludePort = false,
+            .IncludeTcpProtocol = false,
+        }));
 }
 
 std::string GetLoginForTvmId(TTvmId tvmId)
