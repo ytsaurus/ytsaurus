dad351df1af6d9d3d82fb6e5560756a7d0e811b4
denvr <denvr@ytsaurus.tech>
2026-08-12 18:39:47 +0300

    YT-28872 - More clear test commit_hash:225bc393d5e48155cf821d84589b2971d1019ff4

--- yt/python/yt/wrapper/tests/test_retries.py
+++ yt/python/yt/wrapper/tests/test_retries.py
@@ -24,9 +24,11 @@ class TestHTTPRetries(object):
         response_502_empty.raw = urllib3.HTTPResponse()
 
         # one 502 with empty body
+        data = None
         with inject_http_error(client, "list", 0, 1, 1, response=response_502_empty) as cnt:
-            assert client.list("/") == ["sys", "tmp"]
+            data = client.list("/")
         assert cnt.filtered_raises == 1
+        assert data == ["sys", "tmp"]
 
         response_502_good_json = yt_requests.Response()
         response_502_good_json.status_code = 502
@@ -38,12 +40,14 @@ class TestHTTPRetries(object):
             f"{response_502_good_json.status_code} Server Error: {response_502_good_json.reason} for url: {response_502_good_json.url}",
             response=response_502_good_json,
         )
-        # one 502 with json body
+        # one 502 with json body, expects yt error
+        data = None
         with pytest.raises(yt.errors.YtResponseError) as ex:
             with inject_http_error(client, "list", 0, 1, 1, response=response_502_good_json) as cnt:
-                assert client.list("/") == ["sys", "tmp"]
+                data = client.list("/")
         assert cnt.filtered_raises == 1
         assert ex.value.inner_errors == [{'error': 'Bad Gateway'}]
+        assert data is None
         # TODO(denvr) - continue retries, no inner errors
 
         response_502_bad_json = deepcopy(response_502_good_json)
@@ -51,9 +55,16 @@ class TestHTTPRetries(object):
         response_502_bad_json.raw = urllib3.HTTPResponse()
         error_502_bad_json = deepcopy(error_502)
         error_502_bad_json.response._content = b"no json data"
-        # 10 502 with no json body (breaks retry count limit)
+        # 502 with no json body
+        data = None
         with pytest.raises(yt.errors.YtIncorrectResponse) as ex:
             with inject_http_error(client, "list", 0, 10, 1, response=response_502_bad_json) as cnt:
-                assert client.list("/") == ["sys", "tmp"]
+                data = client.list("/")
         assert cnt.filtered_raises == 10
         assert ex.value.message == "Response body can not be decoded from JSON"
+        assert data is None, "Retries did not help"
+
+        data = None
+        with inject_http_error(client, "list", 0, 1, 1, response=response_502_bad_json) as cnt:
+            data = client.list("/")
+        assert data == ["sys", "tmp"], "Retries helps"
