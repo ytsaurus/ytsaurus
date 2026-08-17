a07677aec5795c641b3d6ebff63f2e43a3db79bc
dave11ar <dave11ar@ytsaurus.tech>
2026-08-17 13:28:59 +0300

    YT-29209: Add exclude settings for dynamic tables experiments commit_hash:7d14ece7318d99ae680754dfb8374acfdccd3eec

--- yt/yt/server/lib/tablet_node/table_settings.h
+++ yt/yt/server/lib/tablet_node/table_settings.h
@@ -68,8 +68,12 @@ struct TTableConfigExperiment
 
     //! If not null, take only tables whose paths match the RE.
     NRe2::TRe2Ptr PathRe;
-    //! If not null, take only tables of the specified tablet cell bundle.
+    //! If not null, exclude tables whose paths match the RE, has a higher priority than |PathRe|.
+    NRe2::TRe2Ptr ExcludePathRe;
+    //! If not null, take only tables whose tablet cell bundles match the RE.
     NRe2::TRe2Ptr TabletCellBundle;
+    //! If not null, exclude tables whose tablet cell bundles match the RE, has a higher priority than |TabletCellBundle|.
+    NRe2::TRe2Ptr ExcludeTabletCellBundle;
     //! If not null, take only tables wih specified memory mode.
     NRe2::TRe2Ptr InMemoryMode;
     //! If not null, take only sorted/non-sorted tables respectively.
