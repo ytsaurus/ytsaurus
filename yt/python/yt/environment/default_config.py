54b0f1391fa15d3c870f46285b3d73c973f81c47
ponasenko-rs <ponasenko-rs@ytsaurus.tech>
2026-08-19 13:15:26 +0300

    Remove unrecognized enable_secondary_master_registration commit_hash:25b86dca04e791431c06ef6a5c9dc8e9b168195d

--- yt/python/yt/environment/default_config.py
+++ yt/python/yt/environment/default_config.py
@@ -2,13 +2,9 @@ import yt.yson as yson
 
 
 # TODO(babenko): drop settings mirrored in get_dynamic_master_config below
-# COMPAT(aleksandra-zh): enable_secondary_master_registration should be removed after compat tests
-# are bumped to 25.1.
 def get_master_config():
     return {
         "enable_provision_lock": False,
-
-        "enable_secondary_master_registration": True,
 
         "timestamp_provider": {
             "soft_backoff_time": 100,
