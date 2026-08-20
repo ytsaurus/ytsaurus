924188f29df3477de7b31bf8fe8e2b0688a02ad4
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2026-08-20 14:27:38 +0300

    Remove unused Hydra code commit_hash:dc8ebc6f568b5a5ce151a4cfd9804b7ec065e75a

--- yt/yt/server/lib/hydra/changelog_discovery.h
+++ yt/yt/server/lib/hydra/changelog_discovery.h
@@ -35,21 +35,4 @@ TFuture<TChangelogInfo> DiscoverChangelog(
 
 ////////////////////////////////////////////////////////////////////////////////
 
-struct TChangelogQuorumInfo
-{
-    //! The lower bound for the number of committed records.
-    int RecordCountLo;
-    //! The upper bound for the number of committed records.
-    int RecordCountHi;
-};
-
-//! Given #changelogId, computes the quorum info.
-TFuture<TChangelogQuorumInfo> ComputeChangelogQuorumInfo(
-    TDistributedHydraManagerConfigPtr config,
-    NElection::TCellManagerPtr cellManager,
-    int changelogId,
-    int localRecordCount);
-
-////////////////////////////////////////////////////////////////////////////////
-
 } // namespace NYT::NHydra
