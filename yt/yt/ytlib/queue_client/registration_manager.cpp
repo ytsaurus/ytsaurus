3fc52e5853f03f610b456ea40d90ef6e2b33cf56
panesher <panesher@ytsaurus.tech>
2026-08-20 12:02:21 +0300

    fix babenko issues after [queues] feature YT-29126: return all named consumer registration for list registration without queue_consumer_name
    fix babenko issues after [queues] feature YT-29126: return all named consumer registration for list registration without queue_consumer_name
    commit_hash:59fd72ea40b1b9068e3d507a8212f314240cf460

--- yt/yt/ytlib/queue_client/registration_manager.cpp
+++ yt/yt/ytlib/queue_client/registration_manager.cpp
@@ -1,14 +1,14 @@
 #include "registration_manager.h"
 
-#include "registration_manager_base.h"
-#include "registration_manager_new.h"
 #include "config.h"
 #include "dynamic_state.h"
 #include "helpers.h"
+#include "registration_manager_base.h"
+#include "registration_manager_new.h"
 
 #include <yt/yt/ytlib/api/native/client.h>
-#include <yt/yt/ytlib/api/native/connection.h>
 #include <yt/yt/ytlib/api/native/config.h>
+#include <yt/yt/ytlib/api/native/connection.h>
 
 #include <yt/yt/ytlib/hive/cluster_directory.h>
 
@@ -159,8 +159,14 @@ protected:
             if (resolvedQueue && *resolvedQueue != keyQueue) {
                 continue;
             }
-            if (resolvedConsumer && *resolvedConsumer != keyConsumer) {
-                continue;
+            if (resolvedConsumer) {
+                if (resolvedConsumer->GetQueueConsumerName()) {
+                    if (*resolvedConsumer != keyConsumer) {
+                        continue;
+                    }
+                } else if (ToTablePath(*resolvedConsumer) != ToTablePath(keyConsumer)) {
+                    continue;
+                }
             }
 
             result.push_back(registration);
