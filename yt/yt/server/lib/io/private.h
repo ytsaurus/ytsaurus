a450c6eedff308a012c748d27234764b870f2678
vvshlyaga <vvshlyaga@ytsaurus.tech>
2026-08-21 11:32:13 +0300

    YT-28006: order IO requests by fair-share state
    Orders IO requests by io_consumed / io_fair_share_weight while preserving FIFO within each slot.
    
    Relates: YT-28006
    YT-28006: order IO requests by fair-share state
    
    * Changelog entry
      Type: feature
      Component: data-node
    
    Added fair-share-aware ordering of data node I/O requests using the supplied I/O consumption and fair-share weight, while preserving FIFO for requests with equal priority.
    commit_hash:b05af7a95aae58b0fa13c4fae06a6970bc751a59

--- yt/yt/server/lib/io/private.h
+++ yt/yt/server/lib/io/private.h
@@ -4,6 +4,9 @@
 
 #include <yt/yt/core/logging/log.h>
 
+#include <compare>
+#include <optional>
+
 namespace NYT::NIO {
 
 ////////////////////////////////////////////////////////////////////////////////
@@ -12,6 +15,12 @@ YT_DEFINE_LEAKY_GLOBAL(const NLogging::TLogger, IOLogger, "IO");
 YT_DEFINE_LEAKY_GLOBAL(const NLogging::TLogger, StructuredIORawLogger, "IORaw");
 YT_DEFINE_LEAKY_GLOBAL(const NLogging::TLogger, StructuredIOAggregateLogger, "IOAggregate");
 YT_DEFINE_LEAKY_GLOBAL(const NLogging::TLogger, StructuredIOPathAggregateLogger, "IOPathAggr");
+
+////////////////////////////////////////////////////////////////////////////////
+
+std::partial_ordering CompareIOFairShareStates(
+    const std::optional<TIOFairShareState>& lhs,
+    const std::optional<TIOFairShareState>& rhs);
 
 ////////////////////////////////////////////////////////////////////////////////
 
