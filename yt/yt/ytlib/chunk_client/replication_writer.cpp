a7ee0a716b098652c7bf4b13e33ea193beccfee1
depression <depression@ytsaurus.tech>
2026-08-25 11:39:02 +0300

    YT-26997: Remove option enable_direct_io commit_hash:5ea97bae177d1462c92ee4558a0c9d36869a0161

--- yt/yt/ytlib/chunk_client/replication_writer.cpp
+++ yt/yt/ytlib/chunk_client/replication_writer.cpp
@@ -1062,7 +1062,6 @@ private:
         SetRequestWorkloadDescriptor(req, Config_->WorkloadDescriptor);
         ToProto(req->mutable_session_id(), SessionId_);
         req->set_sync_on_close(Config_->SyncOnClose);
-        req->set_enable_direct_io(Config_->EnableDirectIO);
         req->set_disable_send_blocks(disableSendBlocks);
         req->set_use_probe_put_blocks(isOffshore ? false : Config_->UseProbePutBlocks);
         req->set_preallocate_disk_space(isOffshore ? false : Config_->PreallocateDiskSpace);
