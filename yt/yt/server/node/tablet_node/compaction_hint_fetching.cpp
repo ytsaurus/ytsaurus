8dac110ad75eb669f99b933da815524849d9e1fc
dave11ar <dave11ar@ytsaurus.tech>
2026-08-26 15:07:13 +0300

    [26.1] YT-29207: Add trace id in compaction hint fetching
    YT-29207: Add trace id in compaction hint fetching
    commit_hash:f36ab8b16c24740dba489632c46d3007826a1a54

--- yt/yt/server/node/tablet_node/compaction_hint_fetching.cpp
+++ yt/yt/server/node/tablet_node/compaction_hint_fetching.cpp
@@ -7,12 +7,15 @@
 #include <yt/yt/core/concurrency/periodic_executor.h>
 #include <yt/yt/core/concurrency/throughput_throttler.h>
 
+#include <yt/yt/core/tracing/trace_context.h>
+
 namespace NYT::NTabletNode {
 
 using namespace NYTree;
 using namespace NLogging;
 using namespace NProfiling;
 using namespace NConcurrency;
+using namespace NTracing;
 
 ////////////////////////////////////////////////////////////////////////////////
 
@@ -31,6 +34,8 @@ void TCompactionHintFetchPipeline::Enqueue()
 void TCompactionHintFetchPipeline::Fetch()
 {
     YT_VERIFY(std::holds_alternative<std::monostate>(Payload_));
+
+    TTraceContextGuard traceContextGuard(TTraceContext::NewRoot("CompactionHintFetcher"));
 
     const auto& Logger = GetFetcher()->Context().Logger;
 
