dd6c677775bba80d1dc34eab97e2cfdf11c66070
mpereskokova <mpereskokova@ytsaurus.tech>
2026-08-26 19:52:47 +0300

    Remove warnings from errors back commit_hash:a59a58c41cb3d495994c3cf27361e45b3cc26962

--- yt/yql/plugin/native/plugin.cpp
+++ yt/yql/plugin/native/plugin.cpp
@@ -817,7 +817,6 @@ public:
             .Progress = progress,
             .TaskInfo = MaybeToOptional(program->GetTasksInfo()),
             .Ast = MaybeToOptional(program->GetQueryAst()),
-            .YsonError = program->Issues().Empty() ? std::nullopt : std::make_optional(IssuesToYtErrorYson(program->Issues())),
         };
     }
 
