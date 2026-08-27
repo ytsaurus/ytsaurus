15ea34d2445a27e3ad7c263dfc44a8149d6d3dd5
kvk1920 <kvk1920@ytsaurus.tech>
2026-08-27 09:43:31 +0300

    Fix test_ground_connection_synchronization commit_hash:2ab6747df769d9352ec0577c7bc0c03d5e32e05f

--- yt/yt/ytlib/sequoia_client/connection.cpp
+++ yt/yt/ytlib/sequoia_client/connection.cpp
@@ -52,6 +52,11 @@ public:
             std::move(groundClientFuture));
     }
 
+    TInstant GetLastReconfigurationTime() const override
+    {
+        return LastReconfigurationTime_.load(std::memory_order::relaxed);
+    }
+
 private:
     const TWeakPtr<IConnection> LocalConnection_;
     TAtomicIntrusivePtr<TSequoiaConnectionConfig> Config_;
@@ -60,6 +65,7 @@ private:
     YT_DECLARE_SPIN_LOCK(NThreading::TReaderWriterSpinLock, Lock_);
     TFuture<IClientPtr> GroundClientFuture_;
     TClientCachePtr ClientCache_;
+    std::atomic<TInstant> LastReconfigurationTime_ = {};
 
     void Initialize()
     {
@@ -78,6 +84,7 @@ private:
             auto guard = WriterGuard(Lock_);
             GroundClientFuture_ = std::move(groundClientFuture);
             ClientCache_ = std::move(clientCache);
+            LastReconfigurationTime_.store(TInstant::Now(), std::memory_order::relaxed);
         }
     }
 
