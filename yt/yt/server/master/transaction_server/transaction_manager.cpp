7d487d9d39c93b84e0ed5cdae4551df14bbc33bf
kvk1920 <kvk1920@ytsaurus.tech>
2026-08-27 10:11:12 +0300

    YT-29449: Fix mirrored tx finish validation commit_hash:d1b7327e0753f6449a36eaa9a8ef84a451f47556

--- yt/yt/server/master/transaction_server/transaction_manager.cpp
+++ yt/yt/server/master/transaction_server/transaction_manager.cpp
@@ -4723,7 +4723,8 @@ private:
 
     void ValidateMirroredTransactionFinish(TTransactionId transactionId, bool commit)
     {
-        if (IsCypressTransactionMirroredToSequoia(transactionId) &&
+        if (CellTagFromId(transactionId) == Bootstrap_->GetCellTag() &&
+            IsCypressTransactionMirroredToSequoia(transactionId) &&
             IsMirroringToSequoiaEnabled() &&
             !GetSequoiaContext())
         {
