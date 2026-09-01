d214c056337282ebf6ff27bf21303e661d678ab0
eshcherbin <eshcherbin@ytsaurus.tech>
2026-09-01 12:18:02 +0300

    Remove duplicate TYsonMapFragmentBatcher definition commit_hash:47078441d4197818786a58f1291c811d25e74e4c

--- yt/yt/server/scheduler/helpers.cpp
+++ yt/yt/server/scheduler/helpers.cpp
@@ -508,41 +508,4 @@ std::vector<std::pair<TInstant, TInstant>> SplitTimeIntervalByHours(TInstant sta
 
 ////////////////////////////////////////////////////////////////////////////////
 
-TYsonMapFragmentBatcher::TYsonMapFragmentBatcher(
-    std::vector<NYson::TYsonString>* batchOutput,
-    int maxBatchSize,
-    EYsonFormat format)
-    : BatchOutput_(batchOutput)
-    , MaxBatchSize_(maxBatchSize)
-    , BatchWriter_(CreateYsonWriter(&BatchStream_, format, EYsonType::MapFragment, /*enableRaw*/ false))
-{ }
-
-void TYsonMapFragmentBatcher::Flush()
-{
-    BatchWriter_->Flush();
-
-    if (BatchStream_.Empty()) {
-        return;
-    }
-
-    BatchOutput_->push_back(TYsonString(BatchStream_.Str(), EYsonType::MapFragment));
-    BatchSize_ = 0;
-    BatchStream_.Clear();
-}
-
-void TYsonMapFragmentBatcher::OnMyKeyedItem(TStringBuf key)
-{
-    BatchWriter_->OnKeyedItem(key);
-    Forward(
-        BatchWriter_.get(),
-        /*onFinished*/ [&] {
-            ++BatchSize_;
-            if (BatchSize_ == MaxBatchSize_) {
-                Flush();
-            }
-        });
-}
-
-////////////////////////////////////////////////////////////////////////////////
-
 } // namespace NYT::NScheduler
