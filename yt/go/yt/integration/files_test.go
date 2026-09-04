8e69e8058c6399dc5897ea010f9f1b08de8291ba
dsmarkin <dsmarkin@ytsaurus.tech>
2026-09-04 13:55:11 +0300

    [yt] YT-28736 support MD5 computation in high-level file writer
    * Changelog entry
      Type: fix
      Component: yt
    
    Add support MD5 in high-level writer to preserve existing MD5 checksums when transferring YT files
    commit_hash:83e5fb565b4c578d79b516cde3371edfca9b548c

--- yt/go/yt/integration/files_test.go
+++ yt/go/yt/integration/files_test.go
@@ -2,6 +2,8 @@ package integration
 
 import (
 	"context"
+	"crypto/md5"
+	"fmt"
 	"io"
 	"testing"
 	"time"
@@ -108,6 +110,149 @@ func TestHighLevelFileWriter(t *testing.T) {
 		got, err := io.ReadAll(r)
 		require.NoError(t, err)
 		require.Equal(t, content, got)
+	})
+
+	t.Run("ComputeMD5", func(t *testing.T) {
+		name := tmpPath()
+		const batchSize = 64 * 1024
+		content := make([]byte, 3*batchSize)
+		for i := range content {
+			content[i] = byte(i)
+		}
+
+		w, err := yt.WriteFile(
+			env.Ctx,
+			env.YT,
+			name,
+			yt.WithWriteFileBatchSize(batchSize),
+			yt.WithWriteFileComputeMD5(true),
+		)
+		require.NoError(t, err)
+		for begin := 0; begin < len(content); begin += batchSize {
+			end := min(begin+batchSize, len(content))
+			_, err = w.Write(content[begin:end])
+			require.NoError(t, err)
+		}
+		require.NoError(t, w.Close())
+
+		var fileMD5 string
+		require.NoError(t, env.YT.GetNode(env.Ctx, name.Attr("md5"), &fileMD5, nil))
+		require.Equal(t, fmt.Sprintf("%x", md5.Sum(content)), fileMD5)
+	})
+
+	t.Run("ComputeMD5Overwrite", func(t *testing.T) {
+		name := tmpPath()
+
+		w, err := yt.WriteFile(env.Ctx, env.YT, name)
+		require.NoError(t, err)
+		_, err = w.Write([]byte("old"))
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+
+		w, err = yt.WriteFile(env.Ctx, env.YT, name,
+			yt.WithWriteFileComputeMD5(true),
+			yt.WithWriteFileCreateOptions(&yt.CreateNodeOptions{IgnoreExisting: true}),
+		)
+		require.NoError(t, err)
+		_, err = w.Write([]byte("new"))
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+
+		r, err := env.YT.ReadFile(env.Ctx, name, nil)
+		require.NoError(t, err)
+		defer func() { _ = r.Close() }()
+
+		got, err := io.ReadAll(r)
+		require.NoError(t, err)
+		require.Equal(t, []byte("new"), got)
+
+		var fileMD5 string
+		require.NoError(t, env.YT.GetNode(env.Ctx, name.Attr("md5"), &fileMD5, nil))
+		require.Equal(t, fmt.Sprintf("%x", md5.Sum([]byte("new"))), fileMD5)
+	})
+
+	t.Run("ComputeMD5CloseWithoutWriteDoesNotOverwrite", func(t *testing.T) {
+		name := tmpPath()
+
+		w, err := yt.WriteFile(env.Ctx, env.YT, name)
+		require.NoError(t, err)
+		_, err = w.Write([]byte("old"))
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+
+		w, err = yt.WriteFile(env.Ctx, env.YT, name,
+			yt.WithWriteFileComputeMD5(true),
+			yt.WithWriteFileCreateOptions(&yt.CreateNodeOptions{IgnoreExisting: true}),
+		)
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+
+		r, err := env.YT.ReadFile(env.Ctx, name, nil)
+		require.NoError(t, err)
+		defer func() { _ = r.Close() }()
+
+		got, err := io.ReadAll(r)
+		require.NoError(t, err)
+		require.Equal(t, []byte("old"), got)
+
+		exists, err := env.YT.NodeExists(env.Ctx, name.Attr("md5"), nil)
+		require.NoError(t, err)
+		require.False(t, exists)
+	})
+
+	t.Run("ComputeMD5Append", func(t *testing.T) {
+		name := tmpPath()
+
+		w, err := yt.WriteFile(env.Ctx, env.YT, name, yt.WithWriteFileComputeMD5(true))
+		require.NoError(t, err)
+		_, err = w.Write([]byte("abacaba"))
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+
+		w, err = yt.WriteFile(env.Ctx, env.YT, "<append=%true>"+name,
+			yt.WithWriteFileComputeMD5(true),
+			yt.WithWriteFileCreateOptions(&yt.CreateNodeOptions{IgnoreExisting: true}),
+		)
+		require.NoError(t, err)
+		_, err = w.Write([]byte("new"))
+		require.NoError(t, err)
+		require.NoError(t, w.Close())
+
+		r, err := env.YT.ReadFile(env.Ctx, name, nil)
+		require.NoError(t, err)
+		defer func() { _ = r.Close() }()
+
+		got, err := io.ReadAll(r)
+		require.NoError(t, err)
+		require.Equal(t, []byte("abacabanew"), got)
+
+		var fileMD5 string
+		require.NoError(t, env.YT.GetNode(env.Ctx, name.Attr("md5"), &fileMD5, nil))
+		require.Equal(t, fmt.Sprintf("%x", md5.Sum([]byte("abacabanew"))), fileMD5)
+	})
+
+	t.Run("ComputeMD5AppendToEmptyFileWithoutMD5", func(t *testing.T) {
+		name := tmpPath()
+
+		_, err := env.YT.CreateNode(env.Ctx, name, yt.NodeFile, &yt.CreateNodeOptions{Recursive: true})
+		require.NoError(t, err)
+
+		lw, err := env.YT.WriteFile(env.Ctx, name, nil)
+		require.NoError(t, err)
+		require.NoError(t, lw.Close())
+
+		exists, err := env.YT.NodeExists(env.Ctx, name.Attr("md5"), nil)
+		require.NoError(t, err)
+		require.False(t, exists)
+
+		w, err := yt.WriteFile(env.Ctx, env.YT, "<append=%true>"+name,
+			yt.WithWriteFileComputeMD5(true),
+			yt.WithWriteFileCreateOptions(&yt.CreateNodeOptions{IgnoreExisting: true}),
+		)
+		require.NoError(t, err)
+		_, err = w.Write([]byte("new"))
+		require.NoError(t, err)
+		require.ErrorContains(t, w.Close(), "has no computed MD5 hash")
 	})
 }
 
