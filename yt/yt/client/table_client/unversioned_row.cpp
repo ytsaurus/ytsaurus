b52531a15cee7f430a07b321eb9952a48244e917
osidorkin <osidorkin@ytsaurus.tech>
2026-09-05 17:16:28 +0300

    YT-29589: Add error codes to ValidateNonKeyColumnsAgainstLock
    Добавлены явные коды ошибок в ValidateNonKeyColumnsAgainstLock (yt/yt/client/table_client/unversioned_row.cpp):
    - NTableClient::EErrorCode::SchemaViolation для проверок mapped id вне диапазона и unexpected column.
    - Новый код NTableClient::EErrorCode::RequiredWriteLockMissing (330) для отсутствия write-lock (Exclusive/SharedWrite) на non-key колонке.
    
    Тикет: YT-29589
    commit_hash:219507ea5691c85d7eae5bab6386f7539a10641d

--- yt/yt/client/table_client/unversioned_row.cpp
+++ yt/yt/client/table_client/unversioned_row.cpp
@@ -1300,12 +1300,16 @@ bool ValidateNonKeyColumnsAgainstLock(
         if (mappedId < 0 || mappedId >= std::ssize(schema.Columns())) {
             int size = nameTable->GetSize();
             if (value.Id >= size) {
-                THROW_ERROR_EXCEPTION("Expected value id in range [0:%v] but got %v",
+                THROW_ERROR_EXCEPTION(
+                    NTableClient::EErrorCode::SchemaViolation,
+                    "Expected value id in range [0:%v] but got %v",
                     size - 1,
                     value.Id);
             }
 
-            THROW_ERROR_EXCEPTION("Unexpected column %Qv",
+            THROW_ERROR_EXCEPTION(
+                NTableClient::EErrorCode::SchemaViolation,
+                "Unexpected column %Qv",
                 nameTable->GetName(value.Id));
         }
 
@@ -1320,7 +1324,9 @@ bool ValidateNonKeyColumnsAgainstLock(
             hasNonKeyColumns = true;
 
             if (lockType != ELockType::Exclusive && lockType != ELockType::SharedWrite) {
-                THROW_ERROR_EXCEPTION("No write lock taken for column %Qv",
+                THROW_ERROR_EXCEPTION(
+                    NTableClient::EErrorCode::RequiredWriteLockMissing,
+                    "No write lock taken for column %Qv",
                     nameTable->GetName(value.Id));
             }
         }
