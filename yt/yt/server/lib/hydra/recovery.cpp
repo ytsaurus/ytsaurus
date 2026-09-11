338a83b99ba2b29b5c5b4b6ce8347a13ffb0d9cd
aleksandra-zh <aleksandra-zh@ytsaurus.tech>
2026-09-11 23:30:53 +0300

    Cosmetics for babenko-issues commit_hash:b722816ea235db320a30edf198f80b9259b8c08f

--- yt/yt/server/lib/hydra/recovery.cpp
+++ yt/yt/server/lib/hydra/recovery.cpp
@@ -310,7 +310,7 @@ void TRecovery::FinishRecovery()
         for (int changelogIdToTruncate = latestChangelogId; changelogIdToTruncate > TargetState_.SegmentId; --changelogIdToTruncate) {
             auto errorOrChangelogToTruncate = WaitFor(ChangelogStore_->TryOpenChangelog(changelogIdToTruncate));
             if (!errorOrChangelogToTruncate.IsOK()) {
-                YT_TLOG_INFO("Changelog does not exist, skipping")
+                YT_TLOG_INFO("Error openning changelog")
                     .With("ChangelogId", changelogIdToTruncate);
                 continue;
             }
