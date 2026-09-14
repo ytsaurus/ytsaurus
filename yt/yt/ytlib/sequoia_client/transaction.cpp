4be68f778ca3c4e8ab9de7028ece3688285ab8be
atalmenev <atalmenev@ytsaurus.tech>
2026-09-14 12:56:48 +0300

    YT-29433: Fix transaction signature accounting race on write retry commit_hash:91a98abaed45ca9d797162ae5d2a95703fc4e4ef

--- yt/yt/ytlib/sequoia_client/transaction.cpp
+++ yt/yt/ytlib/sequoia_client/transaction.cpp
@@ -1017,6 +1017,10 @@ private:
     {
         YT_ASSERT_INVOKER_AFFINITY(SerializedInvoker_);
 
+        for (const auto& [tabletId, tabletCommitSession] : TabletCommitSessions_) {
+            tabletCommitSession->CalculateBatchSignatures();
+        }
+
         std::vector<TFuture<void>> futures;
         futures.reserve(TabletCommitSessions_.size());
         if (SequoiaTransactionOptions_.SequenceTabletCommitSessions) {
