e4a1d8deb050f67166ac09ad6777f0ad3d88afc0
ziganshinmr <ziganshinmr@ytsaurus.tech>
2026-09-16 14:31:46 +0300

    Token resolver support in YQL over YT Flow
    - Support dynamic token resolution in YQL over YT Flow
    - Pass token resolver implementation from YQL plugin/qtworker
    commit_hash:562f70df2856eb68c65aabb40c3f2996f0f12c85

--- yt/yql/providers/ytflow/gateway/yql_ytflow_prepare_common.h
+++ yt/yql/providers/ytflow/gateway/yql_ytflow_prepare_common.h
@@ -12,6 +12,8 @@ public:
     void Init(TContext& prepareCtx)
     {
         RunOptions = prepareCtx.RunOptions;
+        YtTokenResolver = prepareCtx.YtTokenResolver;
+        Credentials = prepareCtx.Credentials;
     }
 
     TYtflowSettings::TConstPtr GetConfig() const
@@ -36,8 +38,21 @@ public:
         return GetConfig()->GetPipelinePath();
     }
 
+protected:
+    const IYtTokenResolver::TPtr& GetYtTokenResolver() const
+    {
+        return YtTokenResolver;
+    }
+
+    const TCredentials& GetCredentials() const
+    {
+        return *Credentials;
+    }
+
 private:
     IYtflowGateway::TRunOptions RunOptions;
+    IYtTokenResolver::TPtr YtTokenResolver;
+    TCredentials::TPtr Credentials;
 };
 
 using TSettingsVisitor = std::function<void (const ::google::protobuf::Any&)>;
