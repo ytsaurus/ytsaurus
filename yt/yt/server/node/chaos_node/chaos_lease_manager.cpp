2cc894d9b53ce246a86db93d8b9ded626db937a1
osidorkin <osidorkin@ytsaurus.tech>
2026-09-16 12:37:11 +0300

    YT-27912: Trivial: style commit_hash:f7cb20bb9a4a7ce506be5a6bfd2b2ac9d481d9e2

--- yt/yt/server/node/chaos_node/chaos_lease_manager.cpp
+++ yt/yt/server/node/chaos_node/chaos_lease_manager.cpp
@@ -555,7 +555,8 @@ private:
 
         if (parentId) {
             auto* parent = GetChaosLeaseOrThrow(parentId);
-            if (parent->GetState() == EChaosLeaseState::RevokingShortcutsForRemoval && parent->Coordinators().empty())
+            if (parent->GetState() == EChaosLeaseState::RevokingShortcutsForRemoval &&
+                parent->Coordinators().empty())
             {
                 TryRemoveLeaseBottomUp(parent);
             }
@@ -568,7 +569,9 @@ private:
     {
         auto reign = static_cast<EChaosReign>(GetCurrentMutationContext()->Request().Reign);
 
-        if (chaosLease->GetState() == EChaosLeaseState::RevokingShortcutsForRemoval && chaosLease->Coordinators().empty()) {
+        if (chaosLease->GetState() == EChaosLeaseState::RevokingShortcutsForRemoval &&
+            chaosLease->Coordinators().empty())
+        {
             // COMPAT(gryzlov-ad)
             if (reign >= EChaosReign::ChaosLeaseRemoveLeaseOnlyAfterChildren) {
                 TryRemoveLeaseBottomUp(chaosLease);
