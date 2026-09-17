d89208b4f3335dde1052f079c0d618b603fc5660
pogorelov <pogorelov@ytsaurus.tech>
2026-09-17 18:22:43 +0300

    YT-29721: Mark operation controller cancellation as canceled
    Set EErrorCode::Canceled on the operation controller cancellation reason. Promise abandonment under the controller cancellation token preserves that code, so JobTracker in-barrier waiters take the normal cancellation path instead of emitting an ALERT.
    commit_hash:500fc73f21d3743f3a446f6c93fe5b91501713c9

--- yt/yt/server/controller_agent/job_tracker.cpp
+++ yt/yt/server/controller_agent/job_tracker.cpp
@@ -730,21 +730,26 @@ TJobTracker::TInBarrier::TInBarrier(const TOutBarrier& outBarrier)
 { }
 
 template <NMpl::CInvocable<void(const TError&)> TCallback>
-void TJobTracker::TInBarrier::Wait(TCallback&& onCanceled) const
+void TJobTracker::TInBarrier::Wait(TCallback&& onCanceled, const NLogging::TLogger& Logger) const
 {
     if (Future_.IsSet()) {
         return;
     }
 
-    auto callback = BIND([onCanceled = std::forward<TCallback>(onCanceled)] (const TError& error) mutable {
+    auto callback = BIND([
+        onCanceled = std::forward<TCallback>(onCanceled),
+        Logger = Logger
+    ] (const TError& error) mutable {
         if (error.IsOK()) {
             return;
         }
 
         if (error.GetCode() == NYT::EErrorCode::Canceled) {
+            YT_TLOG_DEBUG("New job settling barrier cancelled")
+                .With(error);
             std::forward<TCallback>(onCanceled)(error);
         } else {
-            YT_TLOG_ALERT("Unexpected exception while waiting for in barrier")
+            YT_TLOG_ALERT("Unexpected exception while waiting for new job settling barrier")
                 .With(error);
         }
     });
@@ -2410,11 +2415,11 @@ void TJobTracker::WaitForNewSettleJobBarrier(TAllocationInfo& allocationInfo, co
 
                 AccountWaitingOnBarrier(/*created*/ true);
 
-                Barrier_.Wait(/*onCanceled*/ [jobTracker = &JobTracker_, Logger = this->Logger] (const TError& error) {
-                    YT_TLOG_DEBUG("Cancelled new job settling barrier")
-                        .With(error);
-                    jobTracker->CancelledSettleJobRequestWaitingOnBarrierCount_.Increment();
-                });
+                Barrier_.Wait(
+                    /*onCanceled*/ [jobTracker = &JobTracker_] (const TError& /*error*/) {
+                        jobTracker->CancelledSettleJobRequestWaitingOnBarrierCount_.Increment();
+                    },
+                    Logger);
             }
 
             ~TBarierAwaiter()
