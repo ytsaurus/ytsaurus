da2d87f2833190228a9325fbb467e5ba9be2589f
ivpiskarev <ivpiskarev@ytsaurus.tech>
2026-09-17 14:49:40 +0300

    revert comment change & fix cosmetics 15134612 commit_hash:159ac019dbe878b59ec14a6fe26d13713481b7b2

--- yt/yt/server/lib/hydra/hydra_manager.h
+++ yt/yt/server/lib/hydra/hydra_manager.h
@@ -104,7 +104,6 @@ struct ISimpleHydraManager
      */
     virtual NLogging::ELogLevel GetUnknownAutomatonPartsLogLevel() const = 0;
 
-
     //! Raised within the automaton thread when the peer has started leading
     //! and enters recovery.
     DECLARE_INTERFACE_SIGNAL(void(), StartLeading);
