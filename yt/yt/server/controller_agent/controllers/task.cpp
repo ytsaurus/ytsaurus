f993e9a24d916e30b1c1a3320568242d64d68cf4
yuryalekseev <yuryalekseev@ytsaurus.tech>
2026-09-20 14:34:24 +0300

    [26.2] Fix disk_request allocation in case of NBD disk. commit_hash:a204787d9a9a8a1c0aafc57292111c78b4049fab

--- yt/yt/server/controller_agent/controllers/task.cpp
+++ yt/yt/server/controller_agent/controllers/task.cpp
@@ -531,7 +531,6 @@ NScheduler::TAllocationStartDescriptor TTask::CreateAllocationStartDescriptor(
         auto& attributes = startDescriptor.AllocationAttributes;
 
         attributes.CudaToolkitVersion = userJobSpec->CudaToolkitVersion;
-        // Do not set disk_request allocation attributes in case of NBD disk.
         for (const auto& [_, volume] : userJobSpec->Volumes) {
             if (!volume->DiskRequest) {
                 continue;
@@ -541,6 +540,9 @@ NScheduler::TAllocationStartDescriptor TTask::CreateAllocationStartDescriptor(
                 attributes.DiskRequest.MediumIndex = diskRequest->MediumIndex;
                 attributes.DiskRequest.DiskSpace = diskRequest->DiskSpace;
                 attributes.DiskRequest.InodeCount = diskRequest->InodeCount;
+            } else if (volume->DiskRequest->GetType() == NExecNode::EVolumeType::Nbd) {
+                // NBD uses no local disk; keep exec node from falling back to MinRequiredDiskSpace.
+                attributes.DiskRequest.DiskSpace = attributes.DiskRequest.DiskSpace.value_or(0);
             }
         }
         attributes.PortCount = userJobSpec->PortCount;
